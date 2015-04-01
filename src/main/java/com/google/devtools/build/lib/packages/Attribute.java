d90db7fc816339c9f40f51cc893524b5b9584846
Laurent Le Brun <laurentlb@google.com>
2015-04-01 14:16:30 +0000

    Skylark: Improve documentation for configuration transitions.
    --
    MOS_MIGRATED_REVID=90061800

--- src/main/java/com/google/devtools/build/lib/packages/Attribute.java
+++ src/main/java/com/google/devtools/build/lib/packages/Attribute.java
@@ -89,7 +89,9 @@ public final class Attribute implements Comparable<Attribute> {
    * label list attribute.
    */
   @SkylarkModule(name = "ConfigurationTransition", doc =
-      "Declares how the configuration should change when following a dependency.")
+      "Declares how the configuration should change when following a dependency. "
+    + "It can be either <a href=\"#modules._top_level.DATA_CFG\">DATA_CFG</a> or "
+    + "<a href=\"#modules._top_level.HOST_CFG\">HOST_CFG</a>.")
   public enum ConfigurationTransition implements Transition {
     /** No transition, i.e., the same configuration as the current. */
     NONE,
--- src/main/java/com/google/devtools/build/lib/rules/SkylarkRuleClassFunctions.java
+++ src/main/java/com/google/devtools/build/lib/rules/SkylarkRuleClassFunctions.java
@@ -85,11 +85,11 @@ public class SkylarkRuleClassFunctions {
 
   //TODO(bazel-team): proper enum support
   @SkylarkBuiltin(name = "DATA_CFG", returnType = ConfigurationTransition.class,
-      doc = "The default runfiles collection state.")
+      doc = "Experimental. Specifies a transition to the data configuration.")
   private static final Object dataTransition = ConfigurationTransition.DATA;
 
   @SkylarkBuiltin(name = "HOST_CFG", returnType = ConfigurationTransition.class,
-      doc = "The default runfiles collection state.")
+      doc = "Specifies a transition to the host configuration.")
   private static final Object hostTransition = ConfigurationTransition.HOST;
 
   private static final LateBoundLabel<BuildConfiguration> RUN_UNDER =
