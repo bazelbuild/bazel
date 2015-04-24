307ecd815aa02768b7318888a3a9126a041111c9
Greg Estren <gregce@google.com>
2015-04-24 21:43:23 +0000

    Description redacted. -- MOS_MIGRATED_REVID=92016074

--- src/test/java/com/google/devtools/build/lib/analysis/constraints/ConstraintsTest.java
+++ src/test/java/com/google/devtools/build/lib/analysis/constraints/ConstraintsTest.java
@@ -15,16 +15,11 @@ package com.google.devtools.build.lib.analysis.constraints;
 
 import static com.google.common.truth.Truth.assertThat;
 
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Multimap;
 import com.google.devtools.build.lib.analysis.BaseRuleClasses;
 import com.google.devtools.build.lib.analysis.ConfiguredRuleClassProvider;
 import com.google.devtools.build.lib.analysis.RuleDefinition;
 import com.google.devtools.build.lib.analysis.RuleDefinitionEnvironment;
 import com.google.devtools.build.lib.analysis.config.BuildConfiguration;
-import com.google.devtools.build.lib.analysis.util.BuildViewTestCase;
 import com.google.devtools.build.lib.packages.Attribute;
 import com.google.devtools.build.lib.packages.Rule;
 import com.google.devtools.build.lib.packages.RuleClass;
@@ -34,14 +29,12 @@ import com.google.devtools.build.lib.syntax.Label;
 import com.google.devtools.build.lib.testutil.TestRuleClassProvider;
 import com.google.devtools.build.lib.testutil.UnknownRuleConfiguredTarget;
 
-import java.util.Collection;
-import java.util.List;
 import java.util.Set;
 
 /**
  * Tests for the constraint enforcement system.
  */
-public class ConstraintsTest extends BuildViewTestCase {
+public class ConstraintsTest extends AbstractConstraintsTest {
 
   @Override
   public void setUp() throws Exception {
@@ -50,131 +43,6 @@ public class ConstraintsTest extends BuildViewTestCase {
     scratchFile("helpers/BUILD",
         "sh_library(name = 'implicit', srcs = ['implicit.sh'])",
         "sh_library(name = 'latebound', srcs = ['latebound.sh'])");
-  }
-
-  /**
-   * Creates an environment group on the scratch filesystem consisting of the specified
-   * environments and specified defaults, set via a builder-style interface. The package name
-   * is the same as the group name.
-   */
-  private class EnvironmentGroupMaker {
-    private final String name;
-    private Set<String> environments;
-    private Set<String> defaults;
-    private final Multimap<String, String> fulfillsMap = HashMultimap.create();
-
-    public EnvironmentGroupMaker(String name) {
-      this.name = name;
-    }
-
-    public EnvironmentGroupMaker setEnvironments(String... environments) {
-      this.environments = ImmutableSet.copyOf(environments);
-      return this;
-    }
-
-    public EnvironmentGroupMaker setDefaults(String... environments) {
-      this.defaults = ImmutableSet.copyOf(environments);
-      return this;
-    }
-
-    /**
-     * Declares that env1 fulfills env2.
-     */
-    public EnvironmentGroupMaker setFulfills(String env1, String env2) {
-      fulfillsMap.put(env1, env2);
-      return this;
-    }
-
-    private final void make() throws Exception {
-      StringBuilder builder = new StringBuilder();
-      for (String env : environments) {
-        builder.append("environment(name = '" + env + "',\n")
-            .append(getAttrDef("fulfills", fulfillsMap.get(env).toArray(new String[0])))
-            .append(")\n");
-      }
-      builder.append("environment_group(\n")
-          .append("    name = '" + name + "',\n")
-          .append(getAttrDef("environments", environments.toArray(new String[0])) + ",\n")
-          .append(getAttrDef("defaults", defaults.toArray(new String[0])) + ",\n")
-          .append(")");
-      scratchFile("" + name + "/BUILD", builder.toString());
-    }
-  }
-
-  /**
-   * Returns a rule definition of the given name, type and custom attribute settings.
-   */
-  private static String getRuleDef(String ruleType, String ruleName, String... customAttributes) {
-    String ruleDef =
-        ruleType + "(\n"
-        + "    name = '" + ruleName + "',\n"
-        + "    srcs = ['" + ruleName + ".sh'],\n";
-    for (String customAttribute : customAttributes) {
-      ruleDef += "    " + customAttribute + ",\n";
-    }
-    ruleDef += ")\n";
-    return ruleDef;
-  }
-
-  /**
-   * Given the inputs, returns the string "attrName = [':label1', ':label2', etc.]"
-   */
-  private static String getAttrDef(String attrName, String... labels) {
-    String attrDef = "    " + attrName + " = [";
-    for (String label : labels) {
-      attrDef += "'" + label + "', ";
-    }
-    attrDef += "]";
-    return attrDef;
-  }
-
-  /**
-   * The core constraint semantics check that if rule A depends on rule B, B must support all of
-   * A's environments. To model this in the tests below, we construct two rules: a "depending"
-   * rule (i.e. A) that depends on a "dependency" rule (i.e. B). Each test can construct its
-   * own instance of these rules with its own environments specifications by calling this method
-   * and {@link #getDependencyRule} with appropriate environment settings passed in as custom
-   * attributes.
-   *
-   * <p>This method constructs and returns the depending rule (i.e. A).
-   */
-  private static String getDependingRule(String... customAttributes) {
-    List<String> attrsAsList = Lists.newArrayList(customAttributes);
-    attrsAsList.add(getAttrDef("deps", "dep"));
-    return getRuleDef("sh_library", "main", attrsAsList.toArray(new String[0]));
-  }
-
-  /**
-   * Returns the rule that {@link #getDependingRule} depends on. This rule must support every
-   * environment supported by the other one for their constraint relationship to be considered
-   * valid.
-   */
-  private static String getDependencyRule(String... customAttributes) {
-    return getRuleDef("sh_library", "dep", customAttributes);
-  }
-
-  /**
-   * Returns the attribute definition that constrains a rule to the given environments. Inputs
-   * are expected to be package-relative labels (e.g. {@code "foo_env"}).
-   */
-  private static String constrainedTo(String... environments) {
-    return getAttrDef("restricted_to", environments);
-  }
-
-  /**
-   * Returns the attribute definition that designates a rule compatible with the given environments.
-   */
-  private static String compatibleWith(String... environments) {
-    return getAttrDef("compatible_with", environments);
-  }
-
-  /**
-   * Returns the environments supported by a rule.
-   */
-  private Collection<Label> supportedEnvironments(String ruleName, String ruleDef)
-      throws Exception {
-    return ConstraintSemantics.getSupportedEnvironments(
-        getRuleContext(scratchConfiguredTarget("hello", ruleName, ruleDef))).getEnvironments();
   }
 
   /**
