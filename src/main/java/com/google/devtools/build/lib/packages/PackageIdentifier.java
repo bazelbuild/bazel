a71d47046c4e9e95bfc69c33c9071d5f6ee536e5
Janak Ramakrishnan <janakr@google.com>
2015-05-06 16:55:00 +0000

    Normalize path fragment before saving it in package identifier.
    --
    MOS_MIGRATED_REVID=92934993

--- src/main/java/com/google/devtools/build/lib/packages/PackageIdentifier.java
+++ src/main/java/com/google/devtools/build/lib/packages/PackageIdentifier.java
@@ -208,7 +208,7 @@ public final class PackageIdentifier implements Comparable<PackageIdentifier>, S
     Preconditions.checkNotNull(repository);
     Preconditions.checkNotNull(pkgName);
     this.repository = repository;
-    this.pkgName = Canonicalizer.fragments().intern(pkgName);
+    this.pkgName = Canonicalizer.fragments().intern(pkgName.normalize());
   }
 
   private Object writeReplace() throws ObjectStreamException {
