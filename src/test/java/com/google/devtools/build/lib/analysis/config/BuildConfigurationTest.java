4eefe3d827510595f22be58c4ada1064d460ce5a
Lukacs Berki <lberki@google.com>
2015-05-21 08:11:26 +0000

    A minor refactoring of our tests.
    --
    MOS_MIGRATED_REVID=94165994

--- src/test/java/com/google/devtools/build/lib/analysis/config/BuildConfigurationTest.java
+++ src/test/java/com/google/devtools/build/lib/analysis/config/BuildConfigurationTest.java
@@ -145,7 +145,7 @@ public class BuildConfigurationTest extends ConfigurationTestCase {
     }
 
     BuildConfigurationCollection master = createCollection(
-        "--multi_cpu=k8", "--multi_cpu=piii", "--ignore_java_and_python_cpu");
+        "--multi_cpu=k8", "--multi_cpu=piii", "--ignore_java_cpu", "--ignore_python_cpu");
     assertThat(master.getTargetConfigurations()).hasSize(2);
     // Note: the cpus are sorted alphabetically.
     assertEquals("k8", master.getTargetConfigurations().get(0).getCpu());
@@ -165,10 +165,10 @@ public class BuildConfigurationTest extends ConfigurationTestCase {
       BuildConfigurationCollection master;
       if (order == 0) {
         master = createCollection(
-            "--multi_cpu=k8", "--multi_cpu=piii", "--ignore_java_and_python_cpu");
+            "--multi_cpu=k8", "--multi_cpu=piii", "--ignore_java_cpu", "--ignore_python_cpu");
       } else {
         master = createCollection(
-            "--multi_cpu=piii", "--multi_cpu=k8", "--ignore_java_and_python_cpu");
+            "--multi_cpu=piii", "--multi_cpu=k8", "--ignore_java_cpu", "--ignore_python_cpu");
       }
       assertThat(master.getTargetConfigurations()).hasSize(2);
       assertEquals("k8", master.getTargetConfigurations().get(0).getCpu());
