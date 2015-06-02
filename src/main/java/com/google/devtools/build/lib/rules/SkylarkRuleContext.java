4a411d0de29468863d39c2b840b95380ecefb1cb
Laurent Le Brun <laurentlb@google.com>
2015-06-02 13:17:26 +0000

    Skylark - delete deprecated ctx.target(s)
    --
    MOS_MIGRATED_REVID=95001990

--- src/main/java/com/google/devtools/build/lib/rules/SkylarkRuleContext.java
+++ src/main/java/com/google/devtools/build/lib/rules/SkylarkRuleContext.java
@@ -308,24 +308,6 @@ public final class SkylarkRuleContext {
     return filesObject;
   }
 
-  /**
-   * See {@link RuleContext#getPrerequisite(String, Mode)}.
-   */
-  @SkylarkCallable(name = "target", structField = true, documented = false)
-  public SkylarkClassObject getTarget() throws FuncallException {
-    // TODO(bazel-team): Remove function in June 2015.
-    throw new FuncallException("ctx.target has been removed, use ctx.attr instead");
-  }
-
-  /**
-   * See {@link RuleContext#getPrerequisites(String, Mode)}.
-   */
-  @SkylarkCallable(name = "targets", structField = true, documented = false)
-  public SkylarkClassObject getTargets() throws FuncallException {
-    // TODO(bazel-team): Remove function in June 2015.
-    throw new FuncallException("ctx.targets has been removed, use ctx.attr instead");
-  }
-
   @SkylarkCallable(name = "workspace_name", structField = true,
       doc = "Returns the workspace name as defined in the WORKSPACE file.")
   public String getWorkspaceName() {
