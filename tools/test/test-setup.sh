1e6f863437d20a4e6803c9d84611463ee5b02245
Han-Wen Nienhuys <hanwen@google.com>
2015-06-03 14:39:55 +0000

    Print TestLogHelper.HEADER_DELIMITER from test-setup.sh.
    This magic header marks where --test_output=streamed should start
    printing.
    
    Fixes #203.
    
    --
    MOS_MIGRATED_REVID=95105573

--- tools/test/test-setup.sh
+++ tools/test/test-setup.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright 2015 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # shift stderr to stdout.
 exec 2>&1
 
@@ -12,5 +26,8 @@ DIR="$TEST_SRCDIR"
 # the entire source tree. By chdir'ing to the runfiles root, tests only
 # have direct access to their declared dependencies.
 cd "$DIR" || { echo "Could not chdir $DIR"; exit 1; }
+
+# This header marks where --test_output=streamed will start being printed.
+echo "-----------------------------------------------------------------------------"
 
 "./$@"
