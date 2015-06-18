80c698a8d95db1e22c5faa2bed62bbf362d0f38b
Eric Fellheimer <felly@google.com>
2015-06-18 13:40:02 +0000

    Minor cleanup: Unnecessary "static" on interface.
    --
    MOS_MIGRATED_REVID=96302792

--- src/main/java/com/google/devtools/build/lib/vfs/UnixGlob.java
+++ src/main/java/com/google/devtools/build/lib/vfs/UnixGlob.java
@@ -294,7 +294,7 @@ public final class UnixGlob {
   /**
    * Filesystem calls required for glob().
    */
-  public static interface FilesystemCalls {
+  public interface FilesystemCalls {
     /**
      * Get directory entries and their types.
      */
