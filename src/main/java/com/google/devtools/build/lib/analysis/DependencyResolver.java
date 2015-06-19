e39be2031d2929b32df539b146fb0cb19e54098a
Laurent Le Brun <laurentlb@google.com>
2015-06-19 17:43:40 +0000

    Skylark: Fix crash when computed attribute has the wrong type
    --
    MOS_MIGRATED_REVID=96417471

--- src/main/java/com/google/devtools/build/lib/analysis/DependencyResolver.java
+++ src/main/java/com/google/devtools/build/lib/analysis/DependencyResolver.java
@@ -345,8 +345,11 @@ public abstract class DependencyResolver {
     }
   }
 
-  private void resolveLateBoundAttributes(Rule rule, BuildConfiguration configuration,
-      AttributeMap attributeMap, Iterable<Attribute> attributes,
+  private void resolveLateBoundAttributes(
+      Rule rule,
+      BuildConfiguration configuration,
+      AttributeMap attributeMap,
+      Iterable<Attribute> attributes,
       ImmutableSortedKeyListMultimap.Builder<Attribute, LabelAndConfiguration> builder)
       throws EvalException {
     for (Attribute attribute : attributes) {
@@ -385,17 +388,28 @@ public abstract class DependencyResolver {
         if (EvalUtils.isNullOrNone(actualValue)) {
           continue;
         }
-        if (attribute.getType() == Type.LABEL) {
-          Label label;
-          label = Type.LABEL.cast(actualValue);
-          builder.put(attribute, LabelAndConfiguration.of(label, actualConfig));
-        } else if (attribute.getType() == Type.LABEL_LIST) {
-          for (Label label : Type.LABEL_LIST.cast(actualValue)) {
+        try {
+          if (attribute.getType() == Type.LABEL) {
+            Label label = Type.LABEL.cast(actualValue);
             builder.put(attribute, LabelAndConfiguration.of(label, actualConfig));
+          } else if (attribute.getType() == Type.LABEL_LIST) {
+            for (Label label : Type.LABEL_LIST.cast(actualValue)) {
+              builder.put(attribute, LabelAndConfiguration.of(label, actualConfig));
+            }
+          } else {
+            throw new IllegalStateException(
+                String.format(
+                    "Late bound attribute '%s' is not a label or a label list",
+                    attribute.getName()));
           }
-        } else {
-          throw new IllegalStateException(String.format(
-              "Late bound attribute '%s' is not a label or a label list", attribute.getName()));
+        } catch (ClassCastException e) {
+          throw new EvalException(
+              rule.getLocation(),
+              String.format(
+                  "When computing the default value of %s, expected '%s', got '%s'",
+                  attribute.getName(),
+                  attribute.getType(),
+                  EvalUtils.getDataTypeName(actualValue, true)));
         }
       }
     }
