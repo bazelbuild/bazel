67eb5365ba04cc6724a1e2e45df4ff95b6f05c53
Han-Wen Nienhuys <hanwen@google.com>
2015-07-07 14:31:55 +0000

    Open-source workspace_status_command option.
    --
    MOS_MIGRATED_REVID=97670493

--- src/main/java/com/google/devtools/build/lib/analysis/WorkspaceStatusAction.java
+++ src/main/java/com/google/devtools/build/lib/analysis/WorkspaceStatusAction.java
@@ -23,8 +23,10 @@ import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.actions.ArtifactFactory;
 import com.google.devtools.build.lib.actions.ArtifactOwner;
 import com.google.devtools.build.lib.actions.Executor.ActionContext;
+import com.google.devtools.build.lib.util.OptionsUtils;
 import com.google.devtools.build.lib.vfs.FileSystemUtils;
 import com.google.devtools.build.lib.vfs.Path;
+import com.google.devtools.build.lib.vfs.PathFragment;
 import com.google.devtools.common.options.Option;
 import com.google.devtools.common.options.OptionsBase;
 
@@ -59,6 +61,15 @@ public abstract class WorkspaceStatusAction extends AbstractAction {
         category = "misc",
         help = "Embed source control revision or release label in binary")
     public String embedLabel;
+
+    @Option(name = "workspace_status_command",
+        defaultValue = "",
+        category = "misc",
+        converter = OptionsUtils.PathFragmentConverter.class,
+        help = "A command invoked at the beginning of the build to provide status "
+            + "information about the workspace in the form of key/value pairs.  "
+            + "See the User's Manual for the full specification.")
+    public PathFragment workspaceStatusCommand;
   }
 
   /**
