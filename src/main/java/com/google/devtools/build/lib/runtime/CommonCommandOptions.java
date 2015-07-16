768b5ccdfad99e3ead5bd8c1c861b3f52506f748
Laszlo Csomor <laszlocsomor@google.com>
2015-07-16 12:47:38 +0000

    Say explicitly that --config=X ignores non-existent .rc file config sections.
    --
    MOS_MIGRATED_REVID=98394325

--- src/main/java/com/google/devtools/build/lib/runtime/CommonCommandOptions.java
+++ src/main/java/com/google/devtools/build/lib/runtime/CommonCommandOptions.java
@@ -103,7 +103,8 @@ public class CommonCommandOptions extends OptionsBase {
           category = "misc",
           allowMultiple = true,
           help = "Selects additional config sections from the rc files; for every <command>, it "
-              + "also pulls in the options from <command>:<config> if such a section exists. "
+              + "also pulls in the options from <command>:<config> if such a section exists; "
+              + "if the section does not exist, this flag is ignored. "
               + "Note that it is currently only possible to provide these options on the "
               + "command line, not in the rc files. The config sections and flag combinations "
               + "they are equivalent to are located in the tools/*.blazerc config files.")
