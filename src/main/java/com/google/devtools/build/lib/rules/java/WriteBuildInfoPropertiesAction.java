1ce99b9c2ef0ae4495bc6b0ea76f2c716264f7d4
Ulf Adams <ulfjack@google.com>
2015-07-17 10:12:42 +0000

    Move another test into open source blaze.
    --
    MOS_MIGRATED_REVID=98478887

--- src/main/java/com/google/devtools/build/lib/rules/java/WriteBuildInfoPropertiesAction.java
+++ src/main/java/com/google/devtools/build/lib/rules/java/WriteBuildInfoPropertiesAction.java
@@ -11,7 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 package com.google.devtools.build.lib.rules.java;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
