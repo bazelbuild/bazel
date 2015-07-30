f48d997cc2be42bb035e5ceb74f40a074d850df0
Nathan Harmata <nharmata@google.com>
2015-07-30 17:10:22 +0000

    Fix ClassCastException.
    --
    MOS_MIGRATED_REVID=99487015

--- src/main/java/com/google/devtools/build/lib/skyframe/SkyframeExecutor.java
+++ src/main/java/com/google/devtools/build/lib/skyframe/SkyframeExecutor.java
@@ -399,7 +399,7 @@ public abstract class SkyframeExecutor implements WalkableGraphFactory {
             // We evaluate in keepGoing mode because in the case that the graph does not store its
             // edges, nokeepGoing builds are not allowed, whereas keepGoing builds are always
             // permitted.
-            EvaluationResult<ActionLookupValue> result = buildDriver.evaluate(
+            EvaluationResult<SkyValue> result = buildDriver.evaluate(
                 ImmutableList.of(key), true, ResourceUsage.getAvailableProcessors(),
                 errorEventListener);
             if (!result.hasError()) {
