cc07634ad8819c40df1fd5d3484661d3fd3061f3
Erik Kuefler <ekuefler@gmail.com>
2015-08-17 08:41:54 +0000

    Tweak CI script to not run tests tagged as manual.
    --
    Change-Id: I424db210611f94b986eda872626a97415221fcea
    Reviewed-on: https://bazel-review.googlesource.com/1800
    MOS_MIGRATED_REVID=100806076

--- scripts/ci/ci.sh
+++ scripts/ci/ci.sh
@@ -55,14 +55,20 @@ for file in $(git diff --name-only ${COMMIT_RANGE} ); do
 done
 
 # Query for the associated buildables
-buildables=$(bazel query --keep_going --noshow_progress "kind(.*_binary, rdeps(//..., set(${files[@]})))")
+buildables=$(bazel query \
+    --keep_going \
+    --noshow_progress \
+    "kind(.*_binary, rdeps(//..., set(${files[@]})))")
 # Run the tests if there were results
 if [[ ! -z $buildables ]]; then
   echo "Building binaries"
   bazel build $buildables
 fi
 
-tests=$(bazel query --keep_going --noshow_progress "kind(test, rdeps(//..., set(${files[@]})))")
+tests=$(bazel query \
+    --keep_going \
+    --noshow_progress \
+    "kind(test, rdeps(//..., set(${files[@]}))) except attr('tags', 'manual', //...)")
 # Run the tests if there were results
 if [[ ! -z $tests ]]; then
   echo "Running tests"
