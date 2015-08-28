5d83e0f0db359c9133b058034e936bbad5604dd2
Rumou Duan <rduan@google.com>
2015-08-28 15:23:49 +0000

    Fix required fragments for Bazel j2objc_library rule.
    --
    MOS_MIGRATED_REVID=101777944

--- src/main/java/com/google/devtools/build/lib/bazel/rules/objc/BazelJ2ObjcLibraryRule.java
+++ src/main/java/com/google/devtools/build/lib/bazel/rules/objc/BazelJ2ObjcLibraryRule.java
@@ -21,9 +21,11 @@ import com.google.devtools.build.lib.analysis.RuleDefinition;
 import com.google.devtools.build.lib.analysis.RuleDefinitionEnvironment;
 import com.google.devtools.build.lib.packages.RuleClass;
 import com.google.devtools.build.lib.packages.RuleClass.Builder;
+import com.google.devtools.build.lib.rules.java.J2ObjcConfiguration;
 import com.google.devtools.build.lib.rules.objc.J2ObjcAspect;
 import com.google.devtools.build.lib.rules.objc.J2ObjcLibrary;
 import com.google.devtools.build.lib.rules.objc.J2ObjcLibraryBaseRule;
+import com.google.devtools.build.lib.rules.objc.ObjcConfiguration;
 
 /**
  * Concrete implementation of J2ObjCLibraryBaseRule.
@@ -33,6 +35,7 @@ public class BazelJ2ObjcLibraryRule implements RuleDefinition {
   @Override
   public RuleClass build(Builder builder, RuleDefinitionEnvironment env) {
     return builder
+        .requiresConfigurationFragments(J2ObjcConfiguration.class, ObjcConfiguration.class)
           /* <!-- #BLAZE_RULE(j2objc_library).ATTRIBUTE(deps) -->
           A list of <code>j2objc_library</code>, <code>java_library</code>
           and <code>java_import</code> targets that contain
