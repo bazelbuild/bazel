2193f329c2ca9eeda397d2c64ffa7b1e02767553
Michajlo Matijkiw <michajlo@google.com>
2015-08-31 21:47:36 +0000

    Make FileSystemUtils#asByteSource() private again
    --
    MOS_MIGRATED_REVID=101976891

--- src/main/java/com/google/devtools/build/lib/vfs/FileSystemUtils.java
+++ src/main/java/com/google/devtools/build/lib/vfs/FileSystemUtils.java
@@ -424,10 +424,7 @@ public class FileSystemUtils {
     }
   }
 
-  /**
-   * Return the path as a ByteSource.
-   */
-  public static ByteSource asByteSource(final Path path) {
+  private static ByteSource asByteSource(final Path path) {
     return new ByteSource() {
       @Override public InputStream openStream() throws IOException {
         return path.getInputStream();
