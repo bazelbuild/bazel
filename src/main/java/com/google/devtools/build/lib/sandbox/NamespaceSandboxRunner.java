def70167c86c8dbc37d4ac8832e6fa12bdf3baa8
Philipp Wollermann <philwo@google.com>
2015-09-08 16:55:02 +0000

    sandbox: We have to move all generated outputs, not just regular files.
    Fix for a part of bug #397.
    
    --
    MOS_MIGRATED_REVID=102564902

--- src/main/java/com/google/devtools/build/lib/sandbox/NamespaceSandboxRunner.java
+++ src/main/java/com/google/devtools/build/lib/sandbox/NamespaceSandboxRunner.java
@@ -163,8 +163,8 @@ public class NamespaceSandboxRunner {
       Path source = sandboxExecRoot.getRelative(output.getExecPathString());
       Path target = execRoot.getRelative(output.getExecPathString());
       FileSystemUtils.createDirectoryAndParents(target.getParentDirectory());
-      if (source.isFile()) {
-        Files.move(new File(source.getPathString()), new File(target.getPathString()));
+      if (source.isFile() || source.isSymbolicLink()) {
+        Files.move(source.getPathFile(), target.getPathFile());
       }
     }
   }
