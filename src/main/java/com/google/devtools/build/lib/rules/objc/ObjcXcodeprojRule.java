cdbe3683b75def2ecb5a69b3d5e62a3038b2c9b8
Chris Parsons <cparsons@google.com>
2015-09-10 16:47:58 +0000

    Add experimental_ios_test to the list of rules that objc_xcodeproj can depend on.
    --
    MOS_MIGRATED_REVID=102763340

--- src/main/java/com/google/devtools/build/lib/rules/objc/ObjcXcodeprojRule.java
+++ src/main/java/com/google/devtools/build/lib/rules/objc/ObjcXcodeprojRule.java
@@ -52,6 +52,7 @@ public class ObjcXcodeprojRule implements RuleDefinition {
                 "ios_extension",
                 "ios_framework",
                 "ios_framework_binary",
+                "experimental_ios_test",
                 "ios_test",
                 "objc_bundle_library",
                 "objc_import",
