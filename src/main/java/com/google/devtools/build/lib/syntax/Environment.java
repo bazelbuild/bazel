f494f3ed4995adbf41b2678d60606080ceafb441
Janak Ramakrishnan <janakr@google.com>
2015-09-14 21:29:56 +0000

    Hack serialization of Environment$Extension so that it deserializes, although not correctly. Attempts to use it will still most likely fail, but won't crash.
    --
    MOS_MIGRATED_REVID=103029603

--- src/main/java/com/google/devtools/build/lib/syntax/Environment.java
+++ src/main/java/com/google/devtools/build/lib/syntax/Environment.java
@@ -230,6 +230,11 @@ public final class Environment implements Freezable {
     BaseExtension(Environment env) {
       this.bindings = ImmutableMap.copyOf(env.globalFrame.bindings);
     }
+
+    // Hack to allow serialization.
+    BaseExtension() {
+      this.bindings = ImmutableMap.of();
+    }
   }
 
   /**
@@ -247,6 +252,12 @@ public final class Environment implements Freezable {
     public Extension(Environment env) {
       super(env);
       this.transitiveContentHashCode = env.getTransitiveContentHashCode();
+    }
+
+    // Hack to allow serialization.
+    private Extension() {
+      super();
+      this.transitiveContentHashCode = null;
     }
 
     @VisibleForTesting // This is only used in one test.
