1c2eeaca1d813bfb22a77cab640502d72abedb9f
Florian Weikert <fwe@google.com>
2015-10-28 10:00:53 +0000

    Bugfix: Host fragments could not be accessed properly through Skylark.
    RuleContext returned fragments for the target configuration, even when Skylark requested fragments for the host configuration.
    This Cl solves this bug. Since injecting a custom BuildConfiguration into out tests is surprisingly difficult, I tested this fix manually:
    
    I wrote a custom bzl file with
    
    def custom_rule_impl(ctx):
      print("target = {}, host = {}".format(ctx.fragments.cpp.cpu, ctx.host_fragments.cpp.cpu))
    
    and built the rule with --cpu ppc.
    
    Output before the fix:  target = ppc, host = ppc.
    Output after the fix:   target = ppc, host = k8.
    
    --
    MOS_MIGRATED_REVID=106479714

--- src/main/java/com/google/devtools/build/lib/analysis/RuleContext.java
+++ src/main/java/com/google/devtools/build/lib/analysis/RuleContext.java
@@ -284,7 +284,7 @@ public final class RuleContext extends TargetContext
         + "in %s configuration in order to access it.%s",
         rule.getRuleClass(), name, FragmentCollection.getConfigurationName(config),
         additionalErrorMessage);
-    return getConfiguration().getFragment(fragment);
+    return getConfiguration(config).getFragment(fragment);
   }
 
   @Nullable
@@ -295,7 +295,8 @@ public final class RuleContext extends TargetContext
 
   @Nullable
   public Fragment getSkylarkFragment(String name, ConfigurationTransition config) {
-    Class<? extends Fragment> fragmentClass = getConfiguration().getSkylarkFragmentByName(name);
+    Class<? extends Fragment> fragmentClass =
+        getConfiguration(config).getSkylarkFragmentByName(name);
     if (fragmentClass == null) {
       return null;
     }
