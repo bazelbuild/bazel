7e3f7ac372ffd704f97d53f39f8ff1c3dda6ff1a
Carmi Grushko <carmi@google.com>
2015-11-16 17:35:57 +0000

    Add an explicit way to get the canonical string-representation of a Label. Motivation: "toString" doesn't describe what representation it returns, and can be called implicitly on string-conversion. Users are then forced to read the javadoc to figure out what they get.
    --
    MOS_MIGRATED_REVID=107946973

--- src/main/java/com/google/devtools/build/lib/cmdline/Label.java
+++ src/main/java/com/google/devtools/build/lib/cmdline/Label.java
@@ -287,6 +287,15 @@ public final class Label implements Comparable<Label>, Serializable, SkylarkPrin
    */
   @Override
   public String toString() {
+    return getCanonicalForm();
+  }
+
+  /**
+   * Renders this label in canonical form.
+   *
+   * <p>invariant: {@code parseAbsolute(x.toString()).equals(x)}
+   */
+  public String getCanonicalForm() {
     return packageIdentifier.getRepository() + "//" + packageIdentifier.getPackageFragment()
         + ":" + name;
   }
