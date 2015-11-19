bf6281d165b9cfc6c8b9931f235293f50d48284c
Damien Martin-Guillerez <dmarting@google.com>
2015-11-19 16:41:33 +0000

    Normalize ijar's header to 'The Bazel Authors'.
    As per discussion with Alan, just use the same copyright than
    for the rest of Bazel and adds Alan as an author.
    
    --
    MOS_MIGRATED_REVID=108250408

--- third_party/ijar/zip.cc
+++ third_party/ijar/zip.cc
@@ -1,6 +1,4 @@
-// Copyright 2007 Alan Donovan. All rights reserved.
-//
-// Author: Alan Donovan <adonovan@google.com>
+// Copyright 2015 The Bazel Authors. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
