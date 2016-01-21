4076e443cce29400554fbc758850c5cb3d8ed419
Carmi Grushko <carmi@google.com>
2016-01-21 20:05:42 +0000

    Allow filtering out some extra-actions related to Java proto compilation.
    --
    MOS_MIGRATED_REVID=112709717

--- src/main/java/com/google/devtools/build/lib/analysis/RuleConfiguredTargetBuilder.java
+++ src/main/java/com/google/devtools/build/lib/analysis/RuleConfiguredTargetBuilder.java
@@ -17,6 +17,8 @@ import static com.google.devtools.build.lib.analysis.ExtraActionUtils.createExtr
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.devtools.build.lib.actions.Action;
 import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.analysis.ExtraActionArtifactsProvider.ExtraArtifactSet;
 import com.google.devtools.build.lib.analysis.LicensesProvider.TargetLicense;
@@ -62,6 +64,7 @@ public final class RuleConfiguredTargetBuilder {
   private RunfilesSupport runfilesSupport;
   private Artifact executable;
   private ImmutableList<Artifact> mandatoryStampFiles;
+  private ImmutableSet<Action> actionsWithoutExtraAction = ImmutableSet.of();
 
   public RuleConfiguredTargetBuilder(RuleContext ruleContext) {
     this.ruleContext = ruleContext;
@@ -111,7 +114,7 @@ public final class RuleConfiguredTargetBuilder {
     }
 
     ExtraActionArtifactsProvider extraActionsProvider =
-        createExtraActionProvider(ruleContext);
+        createExtraActionProvider(actionsWithoutExtraAction, ruleContext);
     if (mandatoryStampFiles != null && !mandatoryStampFiles.isEmpty()) {
       extraActionsProvider = ExtraActionArtifactsProvider.create(
           extraActionsProvider.getExtraActionArtifacts(),
@@ -331,6 +334,15 @@ public final class RuleConfiguredTargetBuilder {
    */
   public RuleConfiguredTargetBuilder setMandatoryStampFiles(ImmutableList<Artifact> files) {
     this.mandatoryStampFiles = files;
+    return this;
+  }
+
+
+  /**
+   * Set the extra action pseudo actions.
+   */
+  public RuleConfiguredTargetBuilder setActionsWithoutExtraAction(ImmutableSet<Action> actions) {
+    this.actionsWithoutExtraAction = actions;
     return this;
   }
 }
