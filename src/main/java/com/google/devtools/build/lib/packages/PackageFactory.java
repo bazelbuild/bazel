f948e83629f677633e3f61fb49b2937de891b378
Kristina Chodorow <kchodorow@google.com>
2016-01-26 15:55:54 +0000

    Describe some of the more likely license types
    Fixes #643.
    
    --
    MOS_MIGRATED_REVID=113055336

--- src/main/java/com/google/devtools/build/lib/packages/PackageFactory.java
+++ src/main/java/com/google/devtools/build/lib/packages/PackageFactory.java
@@ -750,7 +750,21 @@ public final class PackageFactory {
    * TODO(bazel-team): Remove in favor of package.licenses.
    */
   @SkylarkSignature(name = "licenses", returnType = Runtime.NoneType.class,
-      doc = "Declare the license(s) for the code in the current package.",
+      doc = "Declare the license(s) for the code in the current package. Legal license types "
+          + "include:\n"
+          + "<dl>"
+          + "<dt><code>restricted</code></dt><dd>Requires mandatory source distribution.</dd>"
+          + "<dt><code>reciprocal</code></dt><dd>Allows usage of software freely in "
+          + "<b>unmodified</b> form. Any modifications must be made freely available.</dd>"
+          + "<dt><code>notice</code></dt><dd>Original or modified third-party software may be "
+          + "shipped without danger nor encumbering other sources. All of the licenses in this "
+          + "category do, however, have an \"original Copyright notice\" or "
+          + "\"advertising clause\", wherein any external distributions must include the notice "
+          + "or clause specified in the license.</dd>"
+          + "<dt><code>permissive</code></dt><dd>Code that is under a license but does not "
+          + "require a notice.</dd>"
+          + "<dt><code>unencumbered</code></dt><dd>Public domain, free for any use.</dd>"
+          + "</dl>",
       mandatoryPositionals = {
         @Param(name = "license_strings", type = SkylarkList.class, generic1 = String.class,
             doc = "A list of strings, the names of the licenses used.")},
