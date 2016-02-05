d65e97a5a771be2e7bfd0de313fd0d1226b36298
Greg Estren <gregce@google.com>
2016-02-05 20:13:18 +0000

    The filegroup "path" attribute is only used to expose FilegroupPathProvider, which no other Bazel logic uses. So undocument it to prevent confusion.
    Note the implementation logic still exists in FileGroup.java,
    so we don't want to remove the attribute outright.
    
    --
    MOS_MIGRATED_REVID=113973569

--- src/main/java/com/google/devtools/build/lib/bazel/rules/common/BazelFilegroupRule.java
+++ src/main/java/com/google/devtools/build/lib/bazel/rules/common/BazelFilegroupRule.java
@@ -67,7 +67,8 @@ public final class BazelFilegroupRule implements RuleDefinition {
           <code>filegroup</code> to find the name of the directory holding the files.
         </p>
         <!-- #END_BLAZE_RULE.ATTRIBUTE -->*/
-        .add(attr("path", STRING))
+        .add(attr("path", STRING)
+            .undocumented("only used to expose FilegroupPathProvider, which is not currently used"))
         .build();
   }
 
