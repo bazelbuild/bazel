cc8fd606f2f1878cbb5ed0ff8b533e514818c9dd
Dmitry Lomov <dslomov@google.com>
2016-02-18 11:20:48 +0000

    Windows: use default shell env for ijar execution, so that .dlls can be found.
    On Windows, ijar.exe needs msys-2.0.dll and zlib1.dll in PATH.
    
    Needed for #276.
    
    --
    MOS_MIGRATED_REVID=114947735

--- src/main/java/com/google/devtools/build/lib/rules/java/BaseJavaCompilationHelper.java
+++ src/main/java/com/google/devtools/build/lib/rules/java/BaseJavaCompilationHelper.java
@@ -172,6 +172,10 @@ public class BaseJavaCompilationHelper {
           .addInput(inputJar)
           .addOutput(interfaceJar)
           .setExecutable(ijarTarget)
+          // On Windows, ijar.exe needs msys-2.0.dll and zlib1.dll in PATH.
+          // Use default shell environment so that those can be found.
+          // TODO(dslomov): revisit this. If ijar is not msys-dependent, this is not needed.
+          .useDefaultShellEnvironment()
           .addArgument(inputJar.getExecPathString())
           .addArgument(interfaceJar.getExecPathString())
           .setProgressMessage("Extracting interface " + ruleContext.getLabel())
