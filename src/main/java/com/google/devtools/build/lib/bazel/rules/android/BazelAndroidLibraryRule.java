184be1c7858cbc6bb8bb93c0a0a487a08c914cb6
Michael Staib <mstaib@google.com>
2016-02-27 00:00:46 +0000

    Stop using preprocessed .aidl files for types in the same android_library.
    This makes very little sense, because we only do this when compiling
    them within a rule. The preprocessed files are not shipped to other rules,
    meaning we treat things differently depending on the dependency
    structure. And, worst of all, this can lead to a bug in the aidl compiler,
    which causes the preprocessed definition to take precedence over the input
    file, which causes certain modifiers to be stripped away.
    
    Also, Gradle doesn't do it, and that's proof enough that this is no longer
    the way to go, if ever it was.
    
    Unfortunately, this causes a problem: the preprocessing had an effect, in
    that all preprocessed types are available without the use of imports. This
    means that .aidl files which were previously legal before this change may
    now be broken, if they relied on this behavior. But, those .aidl files are
    actually not legal according to the .aidl specification. Unfortunate, but
    the right thing to do.
    
    This CL also updates the idl documentation. Which, let's face it, could
    probably have used some improvement.
    
    RELNOTES[INC]: .aidl files correctly require import statements for types
    defined in the same package and the same android_library.
    
    --
    MOS_MIGRATED_REVID=115718918

--- src/main/java/com/google/devtools/build/lib/bazel/rules/android/BazelAndroidLibraryRule.java
+++ src/main/java/com/google/devtools/build/lib/bazel/rules/android/BazelAndroidLibraryRule.java
@@ -78,4 +78,77 @@ ${IMPLICIT_OUTPUTS}
 <p>Examples of Android rules can be found in the <code>examples/android</code> directory of the
 Bazel source tree.
 
+<p id="android_library_examples.idl_import_root">The following example shows
+how to set <code>idl_import_root</code>.
+Let <code>//java/bazel/helloandroid/BUILD</code> contain:</p>
+<pre class="code">
+android_library(
+    name = "parcelable",
+    srcs = ["MyParcelable.java"], # bazel.helloandroid.MyParcelable
+
+    # MyParcelable.aidl will be used as import for other .aidl
+    # files that depend on it, but will not be compiled.
+    idl_parcelables = ["MyParcelable.aidl"] # bazel.helloandroid.MyParcelable
+
+    # We don't need to specify idl_import_root since the aidl file
+    # which declares bazel.helloandroid.MyParcelable
+    # is present at java/bazel/helloandroid/MyParcelable.aidl
+    # underneath a java root (java/).
+)
+
+android_library(
+    name = "foreign_parcelable",
+    srcs = ["src/android/helloandroid/OtherParcelable.java"], # android.helloandroid.OtherParcelable
+    idl_parcelables = [
+        "src/android/helloandroid/OtherParcelable.aidl" # android.helloandroid.OtherParcelable
+    ],
+
+    # We need to specify idl_import_root because the aidl file which
+    # declares android.helloandroid.OtherParcelable is not positioned
+    # at android/helloandroid/OtherParcelable.aidl under a normal java root.
+    # Setting idl_import_root to "src" in //java/bazel/helloandroid
+    # adds java/bazel/helloandroid/src to the list of roots
+    # the aidl compiler will search for imported types.
+    idl_import_root = "src",
+)
+
+# Here, OtherInterface.aidl has an "import android.helloandroid.CallbackInterface;" statement.
+android_library(
+    name = "foreign_interface",
+    idl_srcs = [
+        "src/android/helloandroid/OtherInterface.aidl" # android.helloandroid.OtherInterface
+        "src/android/helloandroid/CallbackInterface.aidl" # android.helloandroid.CallbackInterface
+    ],
+
+    # As above, idl_srcs which are not correctly positioned under a java root
+    # must have idl_import_root set. Otherwise, OtherInterface (or any other
+    # interface in a library which depends on this one) will not be able
+    # to find CallbackInterface when it is imported.
+    idl_import_root = "src",
+)
+
+# MyParcelable.aidl is imported by MyInterface.aidl, so the generated
+# MyInterface.java requires MyParcelable.class at compile time.
+# Depending on :parcelable ensures that aidl compilation of MyInterface.aidl
+# specifies the correct import roots and can access MyParcelable.aidl, and
+# makes MyParcelable.class available to Java compilation of MyInterface.java
+# as usual.
+android_library(
+    name = "idl",
+    idl_srcs = ["MyInterface.aidl"],
+    deps = [":parcelable"],
+)
+
+# Here, ServiceParcelable uses and thus depends on ParcelableService,
+# when it's compiled, but ParcelableService also uses ServiceParcelable,
+# which creates a circular dependency.
+# As a result, these files must be compiled together, in the same android_library.
+android_library(
+    name = "circular_dependencies",
+    srcs = ["ServiceParcelable.java"],
+    idl_srcs = ["ParcelableService.aidl"],
+    idl_parcelables = ["ServiceParcelable.aidl"],
+)
+</pre>
+
 <!-- #END_BLAZE_RULE -->*/
