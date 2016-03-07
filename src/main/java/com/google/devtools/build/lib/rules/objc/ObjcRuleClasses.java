1c6288d9a9863c7ebf60d09ac4fa51fcadfdac56
Googler <noreply@google.com>
2016-03-07 23:13:43 +0000

    Replace objc_dummy.m with objc_dummy.mm.
    --
    MOS_MIGRATED_REVID=116593165

--- src/main/java/com/google/devtools/build/lib/rules/objc/ObjcRuleClasses.java
+++ src/main/java/com/google/devtools/build/lib/rules/objc/ObjcRuleClasses.java
@@ -517,7 +517,7 @@ public class ObjcRuleClasses {
           .add(attr("$xcodegen", LABEL).cfg(HOST).exec()
               .value(env.getToolsLabel("//tools/objc:xcodegen")))
           .add(attr("$dummy_source", LABEL)
-              .value(env.getToolsLabel("//tools/objc:objc_dummy.m")))
+              .value(env.getToolsLabel("//tools/objc:objc_dummy.mm")))
           .build();
     }
     @Override
