bazelbuild/bazel

commit a49f02536ef62ddf6ef5a17b0f7c1a35aaf81098

FancyTerminalEventHandler: manually break lines in progress bar
Manually break lines in the progress bar (and only in the progress bar!)
that are longer than the terminal width. In this way, we avoid any
confusion about when the terminal advances a line; in particular, when
the terminal is wider than we believe, we do not erase back into the
scrollback buffer.

--
Change-Id: I19f21bdbd6e6af4665d1e712b921f1a15c7d901c
Reviewed-on: https://bazel-review.googlesource.com/#/c/3041
MOS_MIGRATED_REVID=116138487

src/main/java/com/google/devtools/build/lib/runtime/FancyTerminalEventHandler.java
@@ -21,6 +21,9 @@ import com.google.devtools.build.lib.events.Event;
 import com.google.devtools.build.lib.events.EventKind;
 import com.google.devtools.build.lib.util.Pair;
 import com.google.devtools.build.lib.util.io.AnsiTerminal;
+import com.google.devtools.build.lib.util.io.AnsiTerminalWriter;
+import com.google.devtools.build.lib.util.io.LineCountingAnsiTerminalWriter;
+import com.google.devtools.build.lib.util.io.LineWrappingAnsiTerminalWriter;
 import com.google.devtools.build.lib.util.io.OutErr;
 
 import org.joda.time.Duration;
@@ -261,33 +264,29 @@ public class FancyTerminalEventHandler extends BlazeCommandEventHandler {
       }
     }
 
+    LineCountingAnsiTerminalWriter countingWriter = new LineCountingAnsiTerminalWriter(terminal);
+    AnsiTerminalWriter terminalWriter =
+        new LineWrappingAnsiTerminalWriter(countingWriter, terminalWidth - 1);
+
     if (useColor) {
-      terminal.textGreen();
+      terminalWriter.okStatus();
     }
-    int prefixWidth = prefix.length();
-    terminal.writeString(prefix);
-    terminal.resetTerminal();
+    terminalWriter.append(prefix);
+    terminalWriter.normal();
     if (showTimestamp) {
       String timestamp = timestamp();
-      prefixWidth += timestamp.length();
-      terminal.writeString(timestamp);
+      terminalWriter.append(timestamp);
     }
-    int numLines = 0;
     Iterator<String> lines = LINEBREAK_SPLITTER.split(rest).iterator();
     String firstLine = lines.next();
-    terminal.writeString(firstLine);
-    // Subtract one, because when the line length is the same as the terminal
-    // width, the terminal doesn't line-advance, so we don't want to erase
-    // two lines.
-    numLines += (prefixWidth + firstLine.length() - 1) / terminalWidth + 1;
-    crlf();
+    terminalWriter.append(firstLine);
+    terminalWriter.newline();
     while (lines.hasNext()) {
       String line = lines.next();
-      terminal.writeString(line);
-      crlf();
-      numLines += (line.length() - 1) / terminalWidth + 1;
+      terminalWriter.append(line);
+      terminalWriter.newline();
     }
-    numLinesPreviousErasable = numLines;
+    numLinesPreviousErasable = countingWriter.getWrittenLines();
   }
 
   /**

commit 2224b3e4b7e29489c766487afdbdfa96e8c1c0ef

Only break lines if cursor motion is in use
Manual breaking lines was introduced in a49f0253 to ensure Bazel knows
the number of lines written, so the correct number of lines can be
erased when the progress bar is updated. As this is only an issue if curses
are in use, that wrapping as only added to the FancyTerminalEventHandler.
However, Bazel distinguishes between two uses of curses
- curses to add color to the output, and
- curses to control the cursor and erase outdated progress bars.
If the former is used, but not the latter, it is not necessary to wrap
lines; in fact, not doing so is more backwards compatible while still
keeping the line-eating bug fixed. So don't break lines in this case.

--
Change-Id: Ieef8ab9410a071c35609a395f45cd56653115426
Reviewed-on: https://bazel-review.googlesource.com/#/c/3170
MOS_MIGRATED_REVID=118587980

src/main/java/com/google/devtools/build/lib/runtime/FancyTerminalEventHandler.java
@@ -265,8 +265,11 @@ public class FancyTerminalEventHandler extends BlazeCommandEventHandler {
     }
 
     LineCountingAnsiTerminalWriter countingWriter = new LineCountingAnsiTerminalWriter(terminal);
-    AnsiTerminalWriter terminalWriter =
-        new LineWrappingAnsiTerminalWriter(countingWriter, terminalWidth - 1);
+    AnsiTerminalWriter terminalWriter = countingWriter;
+
+    if (useCursorControls) {
+      terminalWriter = new LineWrappingAnsiTerminalWriter(terminalWriter, terminalWidth - 1);
+    }
 
     if (useColor) {
       terminalWriter.okStatus();

src/test/shell/integration/BUILD
@@ -32,6 +32,13 @@ sh_test(
 )
 
 sh_test(
+    name = "ui_test",
+    size = "medium",
+    srcs = ["ui_test.sh"],
+    data = [":test-deps"],
+)
+
+sh_test(
     name = "rc_options_test",
     size = "medium",
     srcs = ["rc_options_test.sh"],

src/test/shell/integration/ui_test.sh
@@ -1,0 +1,79 @@
+#!/bin/bash
+#
+# Copyright 2016 The Bazel Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# An end-to-end test that Bazel's experimental UI produces reasonable output.
+
+# Load test environment
+source $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/testenv.sh \
+  || { echo "testenv.sh not found!" >&2; exit 1; }
+
+create_and_cd_client
+put_bazel_on_path
+write_default_bazelrc
+
+#### SETUP #############################################################
+
+set -e
+
+function set_up() {
+  mkdir -p pkg
+  cat > pkg/true.sh <<EOF
+#!/bin/sh
+exit 0
+EOF
+  chmod 755 pkg/true.sh
+  cat > pkg/BUILD <<EOF
+sh_test(
+  name = "true",
+  srcs = ["true.sh"],
+)
+EOF
+}
+
+#### TESTS #############################################################
+
+function test_basic_progress() {
+  bazel test --curses=yes --color=yes pkg:true 2>$TEST_log || fail "bazel test failed"
+  # some progress indicator is shown
+  expect_log '\[[0-9,]* / [0-9,]*\]'
+  # something is written in green
+  expect_log $'\x1b\[32m'
+  # curses are used to delete at least one line
+  expect_log $'\x1b\[1A\x1b\[K'
+}
+
+function test_line_wrapping() {
+  bazel test --curses=yes --color=yes --terminal_columns=5 pkg:true 2>$TEST_log || fail "bazel test failed"
+  # curses are used to delete at least one line
+  expect_log $'\x1b\[1A\x1b\[K'
+  # something is written in green
+  expect_log $'\x1b\[32m'
+  # lines are wrapped, hence at least one line should end with backslash
+  expect_log '\\'$'\r''$'
+}
+
+function test_noline_wrapping_color_nocurses() {
+  bazel test --curses=no --color=yes --terminal_columns=5 pkg:true 2>$TEST_log || fail "bazel test failed"
+  # something is written in green
+  expect_log $'\x1b\[32m'
+  # no lines are deleted
+  expect_not_log $'\x1b\[K'
+  # as no line wrapping occurs, no backlsash should be before a carriage return
+  expect_not_log '\\'$'\r'
+}
+
+
+run_suite "Basic integration tests for the standard UI"