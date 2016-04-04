b456840f5e9053b41a2620ea791f042f0897783c
Lukacs Berki <lberki@google.com>
2016-04-04 15:12:41 +0000

    Replace an exception message check with a class check. It's saner.
    --
    MOS_MIGRATED_REVID=118940625

--- src/main/java/com/google/devtools/build/lib/server/RPCServer.java
+++ src/main/java/com/google/devtools/build/lib/server/RPCServer.java
@@ -32,6 +32,7 @@ import com.google.devtools.build.lib.util.io.StreamMultiplexer;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.PathFragment;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -248,14 +249,12 @@ public final class RPCServer {
               return;
             }
           }
+        } catch (EOFException e) {
+          LOG.info("Connection to the client lost: "
+                         + e.getMessage());
         } catch (IOException e) {
-          if (e.getMessage().equals("reached end of stream")) {
-            LOG.info("Connection to the client lost: "
-                           + e.getMessage());
-          } else {
-            // Other cases: print the stack for debugging.
-            printStack(e);
-          }
+          // Something else happened. Print a stack trace for debugging.
+          printStack(e);
         }
       }
     } finally {
