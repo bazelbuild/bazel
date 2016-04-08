fd308b61cc5a74eab583689a5d750cacab0a6124
Damien Martin-Guillerez <dmarting@google.com>
2016-04-08 12:07:32 +0000

    Allow Skylark repository rule to be private
    We want sometimes to wrap them around a macro and make it impossible
    to call them directly. So allow them to be private.
    
    --
    MOS_MIGRATED_REVID=119362967

--- src/main/java/com/google/devtools/build/lib/bazel/repository/skylark/SkylarkRepositoryModule.java
+++ src/main/java/com/google/devtools/build/lib/bazel/repository/skylark/SkylarkRepositoryModule.java
@@ -140,11 +140,6 @@ public class SkylarkRepositoryModule {
         throws EvalException, InterruptedException {
       String ruleClassName = ast.getFunction().getName();
       try {
-        if (ruleClassName.startsWith("_")) {
-          throw new EvalException(
-              ast.getLocation(),
-              "Invalid rule class name '" + ruleClassName + "', cannot be private");
-        }
         RuleClass ruleClass = builder.build(ruleClassName);
         PackageContext context = PackageFactory.getContext(env, ast);
         @SuppressWarnings("unchecked")
