2152bc174ee5be48b95d976bdd3e4f6a2b6e2c71
Cal Peyser <cpeyser@google.com>
2016-04-22 17:08:59 +0000

    Bind path to xcrunwrapper in workspace files.
    --
    MOS_MIGRATED_REVID=120558192

---
+++ src/test/shell/bazel/bound_targets_test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Copyright 2016 The Bazel Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Load test environment
+source $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/test-setup.sh \
+  || { echo "test-setup.sh not found!" >&2; exit 1; }
+
+function test_build_xcrunwrapper() {
+  rm WORKSPACE
+  ln -sv ${workspace_file} WORKSPACE
+
+  bazel build //external:xcrunwrapper || fail "should build xcrunwrapper"
+}
+
+run_suite "bound targets integration test"
