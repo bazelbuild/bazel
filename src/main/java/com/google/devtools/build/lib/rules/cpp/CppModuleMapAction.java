8bc7f437258523fffc4946f99f6e20539d8782dc
Manuel Klimek <klimek@google.com>
2016-04-25 14:02:12 +0000

    Fix cache invalidation problem in CppModuleMapAction.
    When a file changes its execution root (for example, a previously checked in
    file now becoming a generated file), the cache key would be the same, using
    a broken module map.
    
    --
    MOS_MIGRATED_REVID=120703120

--- src/main/java/com/google/devtools/build/lib/rules/cpp/CppModuleMapAction.java
+++ src/main/java/com/google/devtools/build/lib/rules/cpp/CppModuleMapAction.java
@@ -177,11 +177,11 @@ public final class CppModuleMapAction extends AbstractFileWriteAction {
     f.addString(GUID);
     f.addInt(privateHeaders.size());
     for (Artifact artifact : privateHeaders) {
-      f.addPath(artifact.getRootRelativePath());
+      f.addPath(artifact.getExecPath());
     }
     f.addInt(publicHeaders.size());
     for (Artifact artifact : publicHeaders) {
-      f.addPath(artifact.getRootRelativePath());
+      f.addPath(artifact.getExecPath());
     }
     f.addInt(dependencies.size());
     for (CppModuleMap dep : dependencies) {
