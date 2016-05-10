82404ea33569314584e3b66bd338def467f1188a
David Chen <dzc@google.com>
2016-05-10 18:47:54 +0000

    Fix name for $java_import_base rule documentation.
    The name for JavaImportBaseRule in the rule documentation was written as
    java_import, which caused conflicts with the actual java_import rule
    (BazelJavaImportRule) in the BE docgen, causing the $java_import_base rule
    documentation to be displayed rather than that of java_import in the
    instance of the bazel.io site brought up using scripts/serve-docs.sh.
    
    --
    MOS_MIGRATED_REVID=121970456

--- src/main/java/com/google/devtools/build/lib/rules/java/JavaImportBaseRule.java
+++ src/main/java/com/google/devtools/build/lib/rules/java/JavaImportBaseRule.java
@@ -82,7 +82,7 @@ public class JavaImportBaseRule implements RuleDefinition {
         .build();
   }
 }
-/*<!-- #BLAZE_RULE (NAME = java_import, TYPE = LIBRARY, FAMILY = Java) -->
+/*<!-- #BLAZE_RULE (NAME = $java_import_base, TYPE = LIBRARY, FAMILY = Java) -->
 
   <p>This rule allows the use of precompiled JAR files as libraries for
   <code><a href="#java_library">java_library</a></code> rules.</p>
