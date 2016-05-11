f16397f33108116119e28952660370fc2e9d67a3
Klaus Aehlig <aehlig@linta.de>
2016-05-11 12:33:09 +0000

    On FreeBSD, don't assume bash to be installed under /bin
    Instead, take the path from the environment, if provided, otherwise
    use the path /usr/local/bin/bash of the default installation.
    
    --
    Change-Id: Ida48967a2dc9b4ed321959b140391b3d44844035
    Reviewed-on: https://bazel-review.googlesource.com/#/c/3600
    MOS_MIGRATED_REVID=122045593

--- src/main/java/com/google/devtools/build/lib/bazel/rules/BazelConfiguration.java
+++ src/main/java/com/google/devtools/build/lib/bazel/rules/BazelConfiguration.java
@@ -64,6 +64,15 @@ public class BazelConfiguration extends Fragment {
         return;
       }
     }
+    if (OS.getCurrent() == OS.FREEBSD) {
+      String path = System.getenv("BAZEL_SH");
+      if (path != null) {
+        builder.put("sh", new PathFragment(path));
+      } else {
+        builder.put("sh", new PathFragment("/usr/local/bin/bash"));
+      }
+      return;
+    }
     builder.put("sh", new PathFragment("/bin/bash"));
   }
 
