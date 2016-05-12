3ae058d0fea53dd629d639ec8007c8c443ab47f2
Michael Staib <mstaib@google.com>
2016-05-12 17:35:56 +0000

    Finally remove the android_jack attribute from android_sdk.
    Deletes the now-useless android_jack attribute; its functionality has
    already been turned off, so there is not much to remove besides the
    attribute definition.
    
    --
    MOS_MIGRATED_REVID=122171309

--- src/main/java/com/google/devtools/build/lib/rules/android/AndroidRuleClasses.java
+++ src/main/java/com/google/devtools/build/lib/rules/android/AndroidRuleClasses.java
@@ -346,10 +346,6 @@ public final class AndroidRuleClasses {
           .add(attr("aidl", LABEL).mandatory().cfg(HOST).allowedFileTypes(ANY_FILE).exec())
           .add(attr("android_jar", LABEL).mandatory().cfg(HOST).allowedFileTypes(JavaSemantics.JAR))
           .add(attr("shrinked_android_jar", LABEL).mandatory().cfg(HOST).allowedFileTypes(ANY_FILE))
-          .add(
-              attr("android_jack", LABEL)
-                  .cfg(HOST)
-                  .allowedFileTypes(ANY_FILE))
           .add(attr("annotations_jar", LABEL).mandatory().cfg(HOST).allowedFileTypes(ANY_FILE))
           .add(attr("main_dex_classes", LABEL).mandatory().cfg(HOST).allowedFileTypes(ANY_FILE))
           .add(attr("apkbuilder", LABEL).mandatory().cfg(HOST).allowedFileTypes(ANY_FILE).exec())
