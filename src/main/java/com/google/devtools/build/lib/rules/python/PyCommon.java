cbd8789f46258d3d900bc69ed6cd36c1c46c51bb
Googler <noreply@google.com>
2016-05-17 14:24:47 +0000

    Improve progress message of the actions triggered by --precompile_python. Typical output looks like:    Compiling Python my/package/module.py
    --
    MOS_MIGRATED_REVID=122522202

--- src/main/java/com/google/devtools/build/lib/rules/python/PyCommon.java
+++ src/main/java/com/google/devtools/build/lib/rules/python/PyCommon.java
@@ -488,7 +488,7 @@ public final class PyCommon {
     SpawnAction.Builder builder = new SpawnAction.Builder()
         .setResources(PY_COMPILE_RESOURCE_SET)
         .setExecutable(pythonBinary)
-        .setProgressMessage("Compiling Python")
+        .setProgressMessage("Compiling Python " + source.prettyPrint())
         .addInputArgument(
             ruleContext.getPrerequisiteArtifact(pythonPrecompileAttribute, Mode.HOST))
         .setMnemonic("PyCompile");
