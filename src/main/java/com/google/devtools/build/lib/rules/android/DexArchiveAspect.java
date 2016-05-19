ab63a1224c9b7c1e85ab50f8ecf0a543519371fe
Googler <noreply@google.com>
2016-05-19 15:56:19 +0000

    Rollback of commit 9f046cba37de6088b2f81717bc263889a5146d86.
    *** Reason for rollback ***
    
    Rollback of commit e37c55eccbd4516b2db7aaf58ef95209dfad3ed4 is complete so rolling this semantically unrelated change forward again
    
    *** Original change description ***
    
    Automated [] rollback of commit b8946eabd60a199a66a1892701d52d9801c7fb1a.
    
    --
    MOS_MIGRATED_REVID=122736257

--- src/main/java/com/google/devtools/build/lib/rules/android/DexArchiveAspect.java
+++ src/main/java/com/google/devtools/build/lib/rules/android/DexArchiveAspect.java
@@ -71,10 +71,6 @@ public final class DexArchiveAspect extends NativeAspectClass implements Configu
   @Override
   public ConfiguredAspect create(ConfiguredTarget base, RuleContext ruleContext,
       AspectParameters params) throws InterruptedException {
-    if (AndroidCommon.getAndroidConfig(ruleContext).getIncrementalDexingBinaries().isEmpty()) {
-      // Dex archives will never be used, so don't bother setting them up.
-      return new ConfiguredAspect.Builder(NAME, ruleContext).build();
-    }
     checkState(base.getProvider(DexArchiveProvider.class) == null,
         "dex archive natively generated: %s", ruleContext.getLabel());
 
