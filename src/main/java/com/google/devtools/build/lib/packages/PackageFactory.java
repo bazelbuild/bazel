4c839b5a7e034fb81bcb6b8235e9be3ea0a9a3d9
Nathan Harmata <nharmata@google.com>
2016-06-06 18:57:38 +0000

    ErrorProne (http://errorprone.info/) suggested fixes for PackageFactory.
    --
    MOS_MIGRATED_REVID=124161557

--- src/main/java/com/google/devtools/build/lib/packages/PackageFactory.java
+++ src/main/java/com/google/devtools/build/lib/packages/PackageFactory.java
@@ -274,7 +274,7 @@ public final class PackageFactory {
       this.globCache = globCache;
     }
 
-    private class Token extends Globber.Token {
+    private static class Token extends Globber.Token {
       public final List<String> includes;
       public final List<String> excludes;
       public final boolean excludeDirs;
@@ -935,11 +935,11 @@ public final class PackageFactory {
     if (val instanceof TriState) {
       switch ((TriState) val) {
         case AUTO:
-          return new Integer(-1);
+          return Integer.valueOf(-1);
         case YES:
-          return new Integer(1);
+          return Integer.valueOf(1);
         case NO:
-          return new Integer(0);
+          return Integer.valueOf(0);
       }
     }
 
