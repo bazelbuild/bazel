57c9dc92bd592d05b8a459e470afc085c24f8adb
Klaus Aehlig <aehlig@google.com>
2016-06-27 13:40:45 +0000

    Move LoggingTerminalWriter out of test
    ...so that it can be used by the main code base.
    
    --
    Change-Id: Ibf59d74ab1b0cca9bd9406bb76ee6e71189653bf
    Reviewed-on: https://bazel-review.googlesource.com/#/c/3903
    MOS_MIGRATED_REVID=125950656

--- src/main/java/com/google/devtools/build/lib/util/io/LoggingTerminalWriter.java
+++ src/main/java/com/google/devtools/build/lib/util/io/LoggingTerminalWriter.java
@@ -11,9 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package com.google.devtools.build.lib.testutil;
-
-import com.google.devtools.build.lib.util.io.AnsiTerminalWriter;
+package com.google.devtools.build.lib.util.io;
 
 import java.io.IOException;
 
--- src/test/java/com/google/devtools/build/lib/util/io/PositionAwareAnsiTerminalWriterTest.java
+++ src/test/java/com/google/devtools/build/lib/util/io/PositionAwareAnsiTerminalWriterTest.java
@@ -15,8 +15,6 @@ package com.google.devtools.build.lib.util.io;
 
 import static org.junit.Assert.assertEquals;
 
-import com.google.devtools.build.lib.testutil.LoggingTerminalWriter;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
