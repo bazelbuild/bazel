e69f2ee04dd76a628c19db565c6f9118fe66471c
Ulf Adams <ulfjack@google.com>
2016-06-30 07:51:07 +0000

    Add help text for the --invocation_policy option of canonicalize-flags.
    --
    MOS_MIGRATED_REVID=126275468

--- src/main/java/com/google/devtools/build/lib/runtime/commands/CanonicalizeCommand.java
+++ src/main/java/com/google/devtools/build/lib/runtime/commands/CanonicalizeCommand.java
@@ -52,7 +52,8 @@ public final class CanonicalizeCommand implements BlazeCommand {
     public String forCommand;
 
     @Option(name = "invocation_policy",
-        defaultValue = "null")
+        defaultValue = "",
+        help = "Applies an invocation policy to the options to be canonicalized.")
     public String invocationPolicy;
   }
 
