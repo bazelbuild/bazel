5eb6a9e040ed6691f117383abe572f9d8186d9cf
Googler <noreply@google.com>
2016-07-06 15:30:21 +0000

    When building ObjectiveC++, pass the flag -std=gnu++11. This is a rollback of a rollback, with the change that gnu++11 is used instead of c++11. A [] with this change was run, and the resulting handful of compile errors were addressed.
    Modified: blaze objc target handling, osx CROSSTOOL and mock osx CROSSTOOL.
    
    RELNOTES: When building ObjectiveC++, pass the flag -std=gnu++11.
    
    --
    MOS_MIGRATED_REVID=126700947

--- src/main/java/com/google/devtools/build/lib/rules/objc/CompilationSupport.java
+++ src/main/java/com/google/devtools/build/lib/rules/objc/CompilationSupport.java
@@ -497,6 +497,7 @@ public final class CompilationSupport {
         .add(CLANG);
     if (ObjcRuleClasses.CPP_SOURCES.matches(sourceFile.getExecPath())) {
       commandLine.add("-stdlib=libc++");
+      commandLine.add("-std=gnu++11");
     }
 
     if (compilationArtifacts.hasSwiftSources()) {
@@ -1155,7 +1156,8 @@ public final class CompilationSupport {
     if (objcProvider.is(USES_CPP)) {
       commandLine
           .add(CLANG_PLUSPLUS)
-          .add("-stdlib=libc++");
+          .add("-stdlib=libc++")
+          .add("-std=gnu++11");
     } else {
       commandLine.add(CLANG);
     }
