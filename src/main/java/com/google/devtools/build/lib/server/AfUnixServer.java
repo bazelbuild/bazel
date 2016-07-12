feb1321044e3b7be34f9c1d7dacf9881d99a7327
Kristina Chodorow <kchodorow@google.com>
2016-07-12 09:37:25 +0000

    Rollback of commit a3381b6ac136a0cab3ba86020c739fe16b42cee9.
    *** Reason for rollback ***
    
    Broke bazel_rules_test
    
    See https://github.com/bazelbuild/bazel/issues/1501.
    
    *** Original change description ***
    
    
    Fix default for temporary directories to honor TMPDIR
    
    ...and only use the hard-coded "/tmp" as default for the default.
    Note that is unchanged that blaze.rpcserver.tmpdir still overrides.
    
    --
    MOS_MIGRATED_REVID=127179462

--- src/main/java/com/google/devtools/build/lib/server/AfUnixServer.java
+++ src/main/java/com/google/devtools/build/lib/server/AfUnixServer.java
@@ -329,14 +329,7 @@ public final class AfUnixServer extends RPCServer {
     // (All this extra complexity is just used in tests... *sigh*).
     if (socketFile.toString().length() >= 104) { // = UNIX_PATH_MAX
       Path socketLink = socketFile;
-      String tmpDirDefault = System.getenv("TMPDIR");
-      if (tmpDirDefault == null
-          || tmpDirDefault.length() > 104 - "/blaze-4294967296/server.socket".length()) {
-        // Default for unset TMPDIR, or if TMPDIR is so that the resulting
-        // path would be too long.
-        tmpDirDefault = "/tmp";
-      }
-      String tmpDir = System.getProperty("blaze.rpcserver.tmpdir", tmpDirDefault);
+      String tmpDir = System.getProperty("blaze.rpcserver.tmpdir", "/tmp");
       socketFile = createTempSocketDirectory(socketFile.getRelative(tmpDir)).
           getRelative("server.socket");
       LOG.info("Using symlinked socket at " + socketFile);
