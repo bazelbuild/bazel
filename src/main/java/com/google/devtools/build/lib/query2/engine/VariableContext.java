3aa7e131c27e8f344871b7e72fc1fec5c2ba77a9
Nathan Harmata <nharmata@google.com>
2016-07-14 19:42:43 +0000

    Fix embarassing typo in VariableContext#with.
    Turns out that our existing test coverage was insufficient for catching this (I
    misread the tests during development of unknown commit and thus incorrectly
    thought we had test coverage for this).
    
    --
    MOS_MIGRATED_REVID=127461716

--- src/main/java/com/google/devtools/build/lib/query2/engine/VariableContext.java
+++ src/main/java/com/google/devtools/build/lib/query2/engine/VariableContext.java
@@ -52,7 +52,7 @@ public class VariableContext<T> {
       Set<T> value) {
     ImmutableMap.Builder<String, Set<T>> newContextBuilder = ImmutableMap.builder();
     for (Map.Entry<String, Set<T>> entry : variableContext.context.entrySet()) {
-      if (entry.getKey().equals(name)) {
+      if (!entry.getKey().equals(name)) {
         // The binding of 'name' to 'value' should override any existing binding of name in
         // 'variableContext'. These are the semantics we want in order for nested let-expressions
         // to have the semantics we want.
