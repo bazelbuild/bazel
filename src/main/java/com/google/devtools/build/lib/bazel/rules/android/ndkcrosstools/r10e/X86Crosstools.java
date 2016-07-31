2fef1cba540010de8924193ac24346166384f553
sergey <sergey.chelombitko@gmail.com>
2016-07-31 19:59:27 +0300

    Fix typo in X86Crosstools

--- src/main/java/com/google/devtools/build/lib/bazel/rules/android/ndkcrosstools/r10e/X86Crosstools.java
+++ src/main/java/com/google/devtools/build/lib/bazel/rules/android/ndkcrosstools/r10e/X86Crosstools.java
@@ -218,6 +218,6 @@ class X86Crosstools {
             .addCompilerFlag("-O0")
             .addCompilerFlag("-g")
             .addCompilerFlag("-fno-omit-frame-pointer")
-            .addCompilerFlag("-fnostrict-aliasing"));
+            .addCompilerFlag("-fno-strict-aliasing"));
   }
 }
