29a4982e83c469bce380874d431e32b841f1a0c4
Philipp Wollermann <philwo@google.com>
2016-08-16 15:48:32 +0000

    Let Bazel use sandboxing during bootstrap, if it's available.
    --
    MOS_MIGRATED_REVID=130406085

--- scripts/bootstrap/bootstrap.sh
+++ scripts/bootstrap/bootstrap.sh
@@ -33,14 +33,12 @@ fi
 if [ "${JAVA_VERSION}" = "1.7" ]; then
   : ${BAZEL_ARGS:=--java_toolchain=//src/java_tools/buildjar:bootstrap_toolchain_jdk7 \
         --host_java_toolchain=//src/java_tools/buildjar:bootstrap_toolchain_jdk7 \
-        --define JAVA_VERSION=1.7 \
-        --genrule_strategy=standalone --spawn_strategy=standalone \
+        --define JAVA_VERSION=1.7 --ignore_unsupported_sandboxing \
         "${EXTRA_BAZEL_ARGS:-}"}
 else
   : ${BAZEL_ARGS:=--java_toolchain=//src/java_tools/buildjar:bootstrap_toolchain \
         --host_java_toolchain=//src/java_tools/buildjar:bootstrap_toolchain \
-        --strategy=Javac=worker --worker_quit_after_build \
-        --genrule_strategy=standalone --spawn_strategy=standalone \
+        --strategy=Javac=worker --worker_quit_after_build --ignore_unsupported_sandboxing \
         "${EXTRA_BAZEL_ARGS:-}"}
 fi
 
