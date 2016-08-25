e1f6380e5d80a2a98b159e4025c338e344a6c824
Michajlo Matijkiw <michajlo@google.com>
2016-08-25 21:03:47 +0000

    GRPC server waits for termination after clean command
    There's a race between sending the finished message and shutdownNow. Use
    shutdown instead to let the finished message send.
    
    My IDE also auto-deleted some unnecessary casts, hope that drive by change is
    ok.
    
    --
    MOS_MIGRATED_REVID=131328436

--- src/main/java/com/google/devtools/build/lib/server/GrpcServerImpl.java
+++ src/main/java/com/google/devtools/build/lib/server/GrpcServerImpl.java
@@ -232,7 +232,7 @@ public class GrpcServerImpl extends RPCServer {
     random.nextBytes(bytes);
     StringBuilder result = new StringBuilder();
     for (byte b : bytes) {
-      result.append(Integer.toHexString(((int) b) + 128));
+      result.append(Integer.toHexString(b + 128));
     }
 
     return result.toString();
@@ -276,8 +276,7 @@ public class GrpcServerImpl extends RPCServer {
 
       while (true) {
         if (!wasIdle && idle) {
-          shutdownTime = BlazeClock.nanoTime()
-              + ((long) maxIdleSeconds) * 1000L * NANOSECONDS_IN_MS;
+          shutdownTime = BlazeClock.nanoTime() + maxIdleSeconds * 1000L * NANOSECONDS_IN_MS;
         }
 
         try {
@@ -444,12 +443,12 @@ public class GrpcServerImpl extends RPCServer {
               break;
 
             case CLEAN:
-              server.shutdownNow();
+              server.shutdown();
               break;
 
             case EXPUNGE:
               disableShutdownHooks();
-              server.shutdownNow();
+              server.shutdown();
               break;
           }
         }
