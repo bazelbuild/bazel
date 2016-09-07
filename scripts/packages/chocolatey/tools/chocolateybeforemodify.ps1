7a7252acb02cba9fa93b01cbf0b1df62488ba9c6
Peter Mounce <petermounce@improbable.io>
2016-09-07 10:25:35 +0100

    Problem: Bar to use on Windows is too high
    Solution: Lower the bar, by creating a chocolatey package and attendent scripts to build and publish, so that people can use bazel without needing to first build it.

---
+++ scripts/packages/chocolatey/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,9 @@
+﻿# This runs in 0.9.10+ before upgrade and uninstall.
+# Use this file to do things like stop services prior to upgrade or uninstall.
+# NOTE: It is an anti-pattern to call chocolateyUninstall.ps1 from here. If you
+#  need to uninstall an MSI prior to upgrade, put the functionality in this
+#  file without calling the uninstall script. Make it idempotent in the
+#  uninstall script so that it doesn't fail when it is already uninstalled.
+# NOTE: For upgrades - like the uninstall script, this script always runs from 
+#  the currently installed version, not from the new upgraded package version.
+
