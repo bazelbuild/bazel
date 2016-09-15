39acfc83d527255989d7bc7ae3daa5b4948c2e28
Irina Iancu <elenairina@google.com>
2016-09-15 06:55:38 +0000

    opensource blaze_javabase_test
    --
    MOS_MIGRATED_REVID=133224703

---
+++ src/test/shell/integration/bazel_javabase_test.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Copyright 2016 The Bazel Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Tests that our --host_javabase startup selection algorithm works.
+
+# Load test environment
+source $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/testenv.sh \
+  || { echo "testenv.sh not found!" >&2; exit 1; }
+
+# The javabase that java programs were built with in this (outer) bazel invocation.
+# This must be done before unsetting JAVA_RUNFILES below.
+declare -r BLAZE_JAVABASE=$(blaze_javabase)
+
+put_bazel_on_path
+create_and_cd_client
+
+function test_use_depot_javabase() {
+  bazel --batch version >& $TEST_log || fail "Couldn't run Bazel"
+  expect_not_log "Couldn't find java at"
+  expect_not_log "Problem with java installation"
+}
+
+function test_fallback_depot_javabase() {
+  bazel --batch --host_javabase=/does/not/exist version >& $TEST_log ||
+    (expect_log "Couldn't find java at" &&
+     expect_not_log "Problem with java installation")
+}
+
+function test_specified_javabase() {
+  bazel --batch --host_javabase=$BLAZE_JAVABASE version >& $TEST_log || fail "Couldn't run Bazel"
+  expect_not_log "Couldn't find java at"
+  expect_not_log "Problem with java installation"
+}
+
+run_suite "Tests of specifying custom javabase."
