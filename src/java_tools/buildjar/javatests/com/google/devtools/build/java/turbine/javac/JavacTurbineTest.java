8afb99be1b8fc6e5f4d7e794c04e73f132548074
Liam Miller-Cushon <cushon@google.com>
2016-09-21 23:53:35 +0000

    Use -source 8 -target 8 in turbine integration tests
    --
    MOS_MIGRATED_REVID=133894022

--- src/java_tools/buildjar/javatests/com/google/devtools/build/java/turbine/javac/JavacTurbineTest.java
+++ src/java_tools/buildjar/javatests/com/google/devtools/build/java/turbine/javac/JavacTurbineTest.java
@@ -115,7 +115,7 @@ public class JavacTurbineTest {
         .addBootClassPathEntries(
             ImmutableList.copyOf(Splitter.on(':').split(System.getProperty("sun.boot.class.path"))))
         .setOutputDeps(outputDeps.toString())
-        .addAllJavacOpts(Arrays.asList("-source", "7", "-target", "7"))
+        .addAllJavacOpts(Arrays.asList("-source", "8", "-target", "8"))
         .setTargetLabel("//test")
         .setRuleKind("java_library");
   }
@@ -156,7 +156,7 @@ public class JavacTurbineTest {
 
     String text = textify(outputs.get("Hello.class"));
     String[] expected = {
-      "// class version 51.0 (51)",
+      "// class version 52.0 (52)",
       "// access flags 0x20",
       "class Hello {",
       "",
@@ -186,7 +186,7 @@ public class JavacTurbineTest {
 
     String text = textify(outputs.get("Hello.class"));
     String[] expected = {
-      "// class version 51.0 (51)",
+      "// class version 52.0 (52)",
       "// access flags 0x20",
       "class Hello {",
       "",
@@ -283,7 +283,7 @@ public class JavacTurbineTest {
     {
       String text = textify(outputs.get("Generated.class"));
       String[] expected = {
-        "// class version 51.0 (51)",
+        "// class version 52.0 (52)",
         "// access flags 0x21",
         "public class Generated {",
         "",
@@ -300,7 +300,7 @@ public class JavacTurbineTest {
     {
       String text = textify(outputs.get("Hello.class"));
       String[] expected = {
-        "// class version 51.0 (51)",
+        "// class version 52.0 (52)",
         "// access flags 0x20",
         "class Hello {",
         "",
@@ -669,7 +669,7 @@ public class JavacTurbineTest {
 
     String text = textify(outputs.get("Const.class"));
     String[] expected = {
-      "// class version 51.0 (51)",
+      "// class version 52.0 (52)",
       "// access flags 0x20",
       "class Const {",
       "",
@@ -719,7 +719,7 @@ public class JavacTurbineTest {
 
     String text = textify(outputs.get("TheEnum.class"));
     String[] expected = {
-      "// class version 51.0 (51)",
+      "// class version 52.0 (52)",
       "// access flags 0x4031",
       "// signature Ljava/lang/Enum<LTheEnum;>;",
       "// declaration: TheEnum extends java.lang.Enum<TheEnum>",
@@ -832,7 +832,7 @@ public class JavacTurbineTest {
 
     String text = textify(outputs.get("Hello.class"));
     String[] expected = {
-      "// class version 51.0 (51)",
+      "// class version 52.0 (52)",
       "// access flags 0x20",
       "class Hello extends Super  {",
       "",
@@ -867,7 +867,7 @@ public class JavacTurbineTest {
 
     String text = textify(outputs.get("Hello.class"));
     String[] expected = {
-      "// class version 51.0 (51)",
+      "// class version 52.0 (52)",
       "// access flags 0x20",
       "class Hello {",
       "",
@@ -998,7 +998,7 @@ public class JavacTurbineTest {
 
     String text = textify(outputs.get("Hello.class"));
     String[] expected = {
-      "// class version 51.0 (51)",
+      "// class version 52.0 (52)",
       "// access flags 0x20",
       "class Hello {",
       "",
@@ -1247,11 +1247,8 @@ public class JavacTurbineTest {
     assertThat(outputs.keySet()).containsExactly("Hello.class");
 
     String text = textify(outputs.get("Hello.class"));
-    System.err.println(">>>");
-    System.err.println(text);
-    System.err.println(">>>");
     String[] expected = {
-      "// class version 51.0 (51)",
+      "// class version 52.0 (52)",
       "// access flags 0x20",
       "class Hello {",
       "",
