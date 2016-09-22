70f88f518b0b74027fc6a88cf13105d848c3f86b
Xin Gao <xingao@google.com>
2016-09-22 14:01:48 +0000

    Push missing testdata directory to Github.
    --
    MOS_MIGRATED_REVID=133949075

---
+++ src/test/shell/bazel/testdata/bazel_toolchain_test_data/hello.cc
@@ -0,0 +1,23 @@
+// Copyright 2016 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  std::cout << "Hello! sqrt(time) = " << std::sqrt(time(NULL)) << std::endl;
+  return EXIT_SUCCESS;
+}
