14251a4ca0d61be24cd96506b9538b1f54047417
Klaus Aehlig <aehlig@google.com>
2016-09-29 16:05:06 +0000

    Add infrastructural build events
    To organize a clean build event stream, certain infrastructural events
    are necessary.
    - If the build is aborted prematurely, certain expected events will not happen;
      hence we have to close those leafs with an event indicating that the actual event
      did not happen, as the build was aborted.
    - Occasionally, we update on progress of the build, using special events; those are
      also used to chain failure events that come before the build events for which they
      are root cause are reported.
    Add those.
    
    --
    Change-Id: Ibf86a313eb678822754b5e1f1e9d7f1a2cbeaaef
    Reviewed-on: https://bazel-review.googlesource.com/#/c/6274
    MOS_MIGRATED_REVID=134667915

---
+++ src/main/java/com/google/devtools/build/lib/buildeventstream/ProgressEvent.java
@@ -0,0 +1,65 @@
+// Copyright 2016 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.devtools.build.lib.buildeventstream;
+
+import com.google.common.collect.ImmutableList;
+import java.util.Collection;
+
+/**
+ * A {@link BuildEvent} reporting about progress.
+ *
+ * <p>Events of this type are used to report updates on the progress of the build. They are also
+ * used to chain in failure events where the canonical parents (e.g., test suites) can only be
+ * reported later.
+ */
+public final class ProgressEvent extends GenericBuildEvent {
+
+  /** The {@link BuildEventId} of the first progress event to be reported. */
+  public static BuildEventId INITIAL_PROGRESS_UPDATE = BuildEventId.progressId(0);
+
+  private ProgressEvent(BuildEventId id, Collection<BuildEventId> children) {
+    super(id, children);
+  }
+
+  @Override
+  public BuildEventStreamProtos.BuildEvent asStreamProto() {
+    return GenericBuildEvent.protoChaining(this)
+        .setProgress(BuildEventStreamProtos.Progress.newBuilder().build())
+        .build();
+  }
+
+  /** Create a regular progress update with the given running number. */
+  public static BuildEvent progressUpdate(int number) {
+    BuildEventId id = BuildEventId.progressId(number);
+    BuildEventId next = BuildEventId.progressId(number + 1);
+    return new ProgressEvent(id, ImmutableList.of(next));
+  }
+
+  /** Create a progress update event also chaining in a given id. */
+  public static BuildEvent progressChainIn(int number, BuildEventId chainIn) {
+    BuildEventId id = BuildEventId.progressId(number);
+    BuildEventId next = BuildEventId.progressId(number + 1);
+    return new ProgressEvent(id, ImmutableList.of(next, chainIn));
+  }
+
+  /**
+   * A progress update event with a given id, that has no children (and hence usually is the last
+   * progress event in the stream).
+   */
+  public static BuildEvent finalProgressUpdate(int number) {
+    BuildEventId id = BuildEventId.progressId(number);
+    return new ProgressEvent(id, ImmutableList.of());
+  }
+}
