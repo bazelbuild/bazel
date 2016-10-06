30946d976f765b86ee628b20f1b5da04541a0a5f
Nayana-ibm <nayana.thorat@gmail.com>
2016-10-06 01:21:38 -0400

    Adding support for Linux s390x

--- src/main/java/com/google/devtools/build/lib/analysis/config/BuildConfiguration.java
+++ src/main/java/com/google/devtools/build/lib/analysis/config/BuildConfiguration.java
@@ -382,6 +382,8 @@ public final class BuildConfiguration {
                 return "ppc";
               case ARM:
                 return "arm";
+              case S390X:
+                return "s390x";
               default:
                 return "unknown";
             }
--- src/main/java/com/google/devtools/build/lib/util/CPU.java
+++ src/main/java/com/google/devtools/build/lib/util/CPU.java
@@ -27,6 +27,7 @@ public enum CPU {
   X86_64("x86_64", ImmutableSet.of("amd64", "x86_64", "x64")),
   PPC("ppc", ImmutableSet.of("ppc", "ppc64", "ppc64le")),
   ARM("arm", ImmutableSet.of("arm", "armv7l")),
+  S390X("s390x", ImmutableSet.of("s390x", "s390")),
   UNKNOWN("unknown", ImmutableSet.<String>of());
 
   private final String canonicalName;
