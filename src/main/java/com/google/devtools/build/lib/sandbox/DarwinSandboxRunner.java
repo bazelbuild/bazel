69e172698af83488b267d3070d90f9dd6fb00cbb
Yue Gan <yueg@google.com>
2016-10-06 15:13:28 +0000

    Deny unix-socket in sandbox script by default. Fixes #1881.
    --
    MOS_MIGRATED_REVID=135360041

--- src/main/java/com/google/devtools/build/lib/sandbox/DarwinSandboxRunner.java
+++ src/main/java/com/google/devtools/build/lib/sandbox/DarwinSandboxRunner.java
@@ -129,8 +129,6 @@ final class DarwinSandboxRunner extends SandboxRunner {
 
       out.println("(allow network* (local ip \"localhost:*\"))");
       out.println("(allow network* (remote ip \"localhost:*\"))");
-      out.println("(allow network* (remote unix-socket (subpath \"/\")))");
-      out.println("(allow network* (local unix-socket (subpath \"/\")))");
 
       for (Path inaccessiblePath : inaccessiblePaths) {
         out.println("(deny file-read* (subpath \"" + inaccessiblePath + "\"))");
