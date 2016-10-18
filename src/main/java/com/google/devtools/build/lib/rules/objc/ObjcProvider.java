58b6aed2c006745bf121a0e4c516b895566d6034
Chris Parsons <cparsons@google.com>
2016-10-18 21:47:27 +0000

    Expose multi-arch artifacts to objc skylark providers
    --
    MOS_MIGRATED_REVID=136523173

--- src/main/java/com/google/devtools/build/lib/rules/objc/ObjcProvider.java
+++ src/main/java/com/google/devtools/build/lib/rules/objc/ObjcProvider.java
@@ -110,7 +110,6 @@ public final class ObjcProvider extends SkylarkClassObject implements Transitive
   /** Combined-architecture binaries to include in the final bundle. */
   public static final Key<Artifact> MULTI_ARCH_LINKED_BINARIES =
       new Key<>(STABLE_ORDER, "combined_arch_linked_binary", Artifact.class);
-
   /** Combined-architecture dynamic libraries to include in the final bundle. */
   public static final Key<Artifact> MULTI_ARCH_DYNAMIC_LIBRARIES =
       new Key<>(STABLE_ORDER, "combined_arch_dynamic_library", Artifact.class);
@@ -414,7 +413,10 @@ public final class ObjcProvider extends SkylarkClassObject implements Transitive
           BUNDLE_FILE,
           BUNDLE_IMPORT_DIR,
           XCASSETS_DIR,
-          FRAMEWORK_DIR);
+          FRAMEWORK_DIR,
+          MULTI_ARCH_LINKED_BINARIES,
+          MULTI_ARCH_LINKED_ARCHIVES,
+          MULTI_ARCH_DYNAMIC_LIBRARIES);
 
   /**
    * Returns the skylark key for the given string, or null if no such key exists or is available
