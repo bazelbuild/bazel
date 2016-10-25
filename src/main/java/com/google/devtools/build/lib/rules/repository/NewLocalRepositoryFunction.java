e08f11c37cfe349c8b687d97100bf4a3af21719a
John Cater <jcater@google.com>
2016-10-25 20:09:39 +0000

    Adds a check that the new local repository path exists and is a directory.
    Fixes #1981.
    
    --
    Change-Id: I16a96be3f4f9de66e608b1914a2554613ddc096a
    Reviewed-on: https://bazel-review.googlesource.com/c/6910/
    MOS_MIGRATED_REVID=137192543

--- src/main/java/com/google/devtools/build/lib/rules/repository/NewLocalRepositoryFunction.java
+++ src/main/java/com/google/devtools/build/lib/rules/repository/NewLocalRepositoryFunction.java
@@ -18,6 +18,8 @@ import com.google.devtools.build.lib.analysis.BlazeDirectories;
 import com.google.devtools.build.lib.analysis.RuleDefinition;
 import com.google.devtools.build.lib.packages.Rule;
 import com.google.devtools.build.lib.skyframe.DirectoryListingValue;
+import com.google.devtools.build.lib.skyframe.FileSymlinkException;
+import com.google.devtools.build.lib.skyframe.FileValue;
 import com.google.devtools.build.lib.skyframe.InconsistentFilesystemException;
 import com.google.devtools.build.lib.vfs.FileSystem;
 import com.google.devtools.build.lib.vfs.Path;
@@ -25,6 +27,7 @@ import com.google.devtools.build.lib.vfs.PathFragment;
 import com.google.devtools.build.lib.vfs.RootedPath;
 import com.google.devtools.build.skyframe.SkyFunction.Environment;
 import com.google.devtools.build.skyframe.SkyFunctionException;
+import com.google.devtools.build.skyframe.SkyFunctionException.Transience;
 import com.google.devtools.build.skyframe.SkyKey;
 import com.google.devtools.build.skyframe.SkyValue;
 import java.io.IOException;
@@ -55,26 +58,63 @@ public class NewLocalRepositoryFunction extends RepositoryFunction {
     FileSystem fs = directories.getOutputBase().getFileSystem();
     Path path = fs.getPath(pathFragment);
 
+    RootedPath dirPath = RootedPath.toRootedPath(fs.getRootDirectory(), path);
+
+    try {
+      FileValue dirFileValue =
+          (FileValue)
+              env.getValueOrThrow(
+                  FileValue.key(dirPath),
+                  IOException.class,
+                  FileSymlinkException.class,
+                  InconsistentFilesystemException.class);
+      if (dirFileValue == null) {
+        return null;
+      }
+
+      if (!dirFileValue.exists()) {
+        throw new RepositoryFunctionException(
+            new IOException(
+                "Expected directory at "
+                    + dirPath.asPath().getPathString()
+                    + " but it does not exist."),
+            Transience.PERSISTENT);
+      }
+      if (!dirFileValue.isDirectory()) {
+        // Someone tried to create a local repository from a file.
+        throw new RepositoryFunctionException(
+            new IOException(
+                "Expected directory at "
+                    + dirPath.asPath().getPathString()
+                    + " but it is not a directory."),
+            Transience.PERSISTENT);
+      }
+    } catch (IOException e) {
+      throw new RepositoryFunctionException(e, Transience.PERSISTENT);
+    } catch (FileSymlinkException e) {
+      throw new RepositoryFunctionException(new IOException(e), Transience.PERSISTENT);
+    } catch (InconsistentFilesystemException e) {
+      throw new RepositoryFunctionException(new IOException(e), Transience.PERSISTENT);
+    }
+
     // fetch() creates symlinks to each child under 'path' and DiffAwareness handles checking all
     // of these files and directories for changes. However, if a new file/directory is added
     // directly under 'path', Bazel doesn't know that this has to be symlinked in. Thus, this
     // creates a dependency on the contents of the 'path' directory.
-    SkyKey dirKey = DirectoryListingValue.key(
-        RootedPath.toRootedPath(fs.getRootDirectory(), path));
+    SkyKey dirKey = DirectoryListingValue.key(dirPath);
     DirectoryListingValue directoryValue;
     try {
       directoryValue = (DirectoryListingValue) env.getValueOrThrow(
           dirKey, InconsistentFilesystemException.class);
     } catch (InconsistentFilesystemException e) {
-      throw new RepositoryFunctionException(
-          new IOException(e), SkyFunctionException.Transience.PERSISTENT);
+      throw new RepositoryFunctionException(new IOException(e), Transience.PERSISTENT);
     }
     if (directoryValue == null) {
       return null;
     }
 
     // Link x/y/z to /some/path/to/y/z.
-    if (!symlinkLocalRepositoryContents(outputDirectory, fs.getPath(pathFragment))) {
+    if (!symlinkLocalRepositoryContents(outputDirectory, path)) {
       return null;
     }
 
@@ -87,7 +127,7 @@ public class NewLocalRepositoryFunction extends RepositoryFunction {
       try {
         workspaceFile.delete();
       } catch (IOException e) {
-        throw new RepositoryFunctionException(e, SkyFunctionException.Transience.TRANSIENT);
+        throw new RepositoryFunctionException(e, Transience.TRANSIENT);
       }
     }
     createWorkspaceFile(outputDirectory, rule.getTargetKind(), rule.getName());
--- src/test/shell/bazel/local_repository_test.sh
+++ src/test/shell/bazel/local_repository_test.sh
@@ -1087,6 +1087,7 @@ EOF
 # Regression test for https://github.com/bazelbuild/bazel/issues/792
 function test_build_all() {
   local r=$TEST_TMPDIR/r
+  rm -rf $r
   mkdir -p $r
   touch $r/WORKSPACE
   cat > $r/BUILD <<'EOF'
@@ -1120,6 +1121,7 @@ EOF
 # Regression test for #1697.
 function test_overwrite_build_file() {
   local r=$TEST_TMPDIR/r
+  rm -rf $r
   mkdir -p $r
   touch $r/WORKSPACE
   cat > $r/BUILD <<'EOF'
@@ -1145,6 +1147,96 @@ genrule(
 EOF
   bazel build @r//... &> $TEST_log || fail "Build failed"
   assert_contains "orig" $r/BUILD
+}
+
+function test_new_local_repository_path_not_existing() {
+  local r=$TEST_TMPDIR/r
+  rm -rf $r
+  cat > WORKSPACE <<EOF
+new_local_repository(
+    name = "r",
+    path = "$TEST_TMPDIR/r",
+    build_file_content = """
+genrule(
+    name = "rewrite",
+    cmd = "echo bar > \$@",
+    outs = ["rewrite.out"],
+)
+""",
+)
+EOF
+  bazel build @r//... &> $TEST_log && fail "Build succeeded unexpectedly"
+  expect_log "does not exist"
+}
+
+function test_new_local_repository_path_not_directory() {
+  local r=$TEST_TMPDIR/r
+  rm -rf $r
+  touch $r
+  cat > WORKSPACE <<EOF
+new_local_repository(
+    name = "r",
+    path = "$TEST_TMPDIR/r",
+    build_file_content = """
+genrule(
+    name = "rewrite",
+    cmd = "echo bar > \$@",
+    outs = ["rewrite.out"],
+)
+""",
+)
+EOF
+  bazel build @r//... &> $TEST_log && fail "Build succeeded unexpectedly"
+  expect_log "is not a directory"
+}
+
+function test_new_local_repository_path_symlink_to_dir() {
+  local r=$TEST_TMPDIR/r
+  local s=$TEST_TMPDIR/s
+  rm -rf $r
+  rm -rf $s
+  mkdir -p $s
+  ln -s $s $r
+
+  cat > WORKSPACE <<EOF
+new_local_repository(
+    name = "r",
+    path = "$TEST_TMPDIR/r",
+    build_file_content = """
+genrule(
+    name = "rewrite",
+    cmd = "echo bar > \$@",
+    outs = ["rewrite.out"],
+)
+""",
+)
+EOF
+  bazel build @r//:rewrite &> $TEST_log || fail "Build failed"
+}
+
+function test_new_local_repository_path_symlink_to_file() {
+  local r=$TEST_TMPDIR/r
+  local s=$TEST_TMPDIR/s
+  rm -rf $r
+  rm -rf $s
+  touch $s
+  ln -s $s $r
+
+  cat > WORKSPACE <<EOF
+new_local_repository(
+    name = "r",
+    path = "$TEST_TMPDIR/r",
+    build_file_content = """
+genrule(
+    name = "rewrite",
+    cmd = "echo bar > \$@",
+    outs = ["rewrite.out"],
+)
+""",
+)
+EOF
+  bazel build @r//:rewrite &> $TEST_log && fail "Build succeeded unexpectedly"
+  expect_log "is not a directory"
 }
 
 run_suite "local repository tests"
