af915535c2bbaf7c9058b54b0276ebd99f3b5584
Luis Fernando Pino Duque <lpino@google.com>
2016-10-28 15:56:11 +0000

    Rollback of commit b6301a5f7628d5a7e11abc6c1115918d42c6fba8.
    *** Reason for rollback ***
    
    Breaks blaze_util_test for Bazel https://github.com/bazelbuild/bazel/issues/1999
    
    *** Original change description ***
    
    Uncomment lines inside blaze_util_tests since the tests are now
    passing.
    
    --
    MOS_MIGRATED_REVID=137516264

--- src/test/cpp/blaze_util_test.cc
+++ src/test/cpp/blaze_util_test.cc
@@ -193,10 +193,11 @@ TEST_F(BlazeUtilTest, MakeDirectories) {
   ASSERT_EQ(0750, filestat.st_mode & 0777);
 
   // srcdir shouldn't be writable.
-  string srcdir = blaze_util::JoinPath(test_src_dir, "x/y/z");
-  ok = MakeDirectories(srcdir, 0755);
-  ASSERT_EQ(-1, ok);
-  ASSERT_EQ(EACCES, errno);
+  // TODO(ulfjack): Fix this!
+//  string srcdir = blaze_util::JoinPath(test_src_dir, "x/y/z");
+//  ok = MakeDirectories(srcdir, 0755);
+//  ASSERT_EQ(-1, ok);
+//  ASSERT_EQ(EACCES, errno);
 
   // Can't make a dir out of a file.
   string non_dir = blaze_util::JoinPath(dir, "w");
@@ -223,9 +224,10 @@ TEST_F(BlazeUtilTest, MakeDirectories) {
   ASSERT_TRUE(Symlink("/", symlink));
 
   // These perms will force a chmod()
-  ok = MakeDirectories(symlink, 0000);
-  ASSERT_EQ(-1, ok);
-  ASSERT_EQ(EPERM, errno);
+  // TODO(ulfjack): Fix this!
+//  ok = MakeDirectories(symlink, 0000);
+//  ASSERT_EQ(-1, ok);
+//  ASSERT_EQ(EPERM, errno);
 
   // Edge cases.
   ASSERT_EQ(-1, MakeDirectories("", 0755));
@@ -239,8 +241,9 @@ TEST_F(BlazeUtilTest, HammerMakeDirectories) {
   ASSERT_STRNE(tmp_dir, NULL);
 
   string path = blaze_util::JoinPath(tmp_dir, "x/y/z");
-  ASSERT_LE(0, fork());
-  ASSERT_EQ(0, MakeDirectories(path, 0755));
+  // TODO(ulfjack): Fix this!
+//  ASSERT_LE(0, fork());
+//  ASSERT_EQ(0, MakeDirectories(path, 0755));
 }
 
 }  // namespace blaze
