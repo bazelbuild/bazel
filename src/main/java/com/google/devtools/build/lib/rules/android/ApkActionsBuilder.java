c01050519f54906e513b2eebe4440aa20924021d
Adam Michael <ajmichael@google.com>
2016-11-16 00:35:42 +0000

    Normalize timestamps inside of APKs built with SingleJar.
    --
    MOS_MIGRATED_REVID=139265482

--- src/main/java/com/google/devtools/build/lib/rules/android/ApkActionsBuilder.java
+++ src/main/java/com/google/devtools/build/lib/rules/android/ApkActionsBuilder.java
@@ -257,6 +257,7 @@ public class ApkActionsBuilder {
         .setExecutionInfo(executionInfo)
         .addArgument("--exclude_build_data")
         .addArgument("--compression")
+        .addArgument("--normalize")
         .addArgument("--output")
         .addOutputArgument(compressedApk);
     setSingleJarAsExecutable(ruleContext, compressedApkActionBuilder);
@@ -307,6 +308,7 @@ public class ApkActionsBuilder {
         .setExecutionInfo(executionInfo)
         .addArgument("--exclude_build_data")
         .addArgument("--dont_change_compression")
+        .addArgument("--normalize")
         .addArgument("--sources")
         .addInputArgument(compressedApk)
         .addArgument("--output")
