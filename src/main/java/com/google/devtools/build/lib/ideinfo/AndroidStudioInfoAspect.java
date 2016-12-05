84e7bbc03d22a98e1036c9a3abf1d451e602348b
Googler <noreply@google.com>
2016-12-05 21:47:20 +0000

    Pass isExternal field to blaze ide info proto
    Roll forward after fixing tests. The intellij aspect tests now
    correctly retains the required toolchain targets in the WORKSPACE file.
    
    --
    PiperOrigin-RevId: 141093346
    MOS_MIGRATED_REVID=141093346

--- src/main/java/com/google/devtools/build/lib/ideinfo/AndroidStudioInfoAspect.java
+++ src/main/java/com/google/devtools/build/lib/ideinfo/AndroidStudioInfoAspect.java
@@ -19,6 +19,7 @@ import static com.google.devtools.build.lib.packages.Attribute.ConfigurationTran
 import static com.google.devtools.build.lib.packages.Attribute.attr;
 import static com.google.devtools.build.lib.packages.BuildType.LABEL;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Function;
 import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
@@ -630,11 +631,13 @@ public class AndroidStudioInfoAspect extends NativeAspectClass implements Config
     return makeArtifactLocation(root, relativePath);
   }
 
-  private static ArtifactLocation makeArtifactLocation(Root root, PathFragment relativePath) {
+  @VisibleForTesting
+  static ArtifactLocation makeArtifactLocation(Root root, PathFragment relativePath) {
     return ArtifactLocation.newBuilder()
         .setRootExecutionPathFragment(root.getExecPath().toString())
         .setRelativePath(relativePath.toString())
         .setIsSource(root.isSourceRoot())
+        .setIsExternal(!root.isMainRepo())
         .build();
   }
 
@@ -643,6 +646,7 @@ public class AndroidStudioInfoAspect extends NativeAspectClass implements Config
         .setRootExecutionPathFragment(resourceDir.getRootExecutionPathFragment().toString())
         .setRelativePath(resourceDir.getRelativePath().toString())
         .setIsSource(resourceDir.isSource())
+        .setIsExternal(false)
         .build();
   }
 
