79fdab0dd4d04ab87bf27abbb1b12a09aae5b82b
Googler <noreply@google.com>
2016-12-13 22:45:03 +0000

    Fixing typo.
    --
    PiperOrigin-RevId: 141942246
    MOS_MIGRATED_REVID=141942246

--- src/main/java/com/google/devtools/build/lib/rules/java/ProguardHelper.java
+++ src/main/java/com/google/devtools/build/lib/rules/java/ProguardHelper.java
@@ -453,7 +453,7 @@ public abstract class ProguardHelper {
           output.getUsage(),
           output.getConstantStringObfuscatedMapping(),
           output.getConfig())
-          .setProgressMessage("Trimming binary with Proguard: Obfuscation and Final Ouput Pass")
+          .setProgressMessage("Trimming binary with Proguard: Obfuscation and Final Output Pass")
           .addArgument("-runtype FINAL")
           .addArgument("-laststageoutput")
           .addInputArgument(lastStageOutput)
