71e37222e3f61cf670b62373a59b87692da1d1c9
Marcel Hlopko <hlopko@google.com>
2017-01-03 12:01:28 +0000

    Cleanup initialization of expandIfTrue/False in CcToolchainFeatures.FlagGroup constructor
    --
    PiperOrigin-RevId: 143429631
    MOS_MIGRATED_REVID=143429631

--- src/main/java/com/google/devtools/build/lib/rules/cpp/CcToolchainFeatures.java
+++ src/main/java/com/google/devtools/build/lib/rules/cpp/CcToolchainFeatures.java
@@ -18,6 +18,7 @@ import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Predicate;
+import com.google.common.base.Strings;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
@@ -377,8 +378,8 @@ public class CcToolchainFeatures implements Serializable {
       this.usedVariables = usedVariables.build();
       this.expandables = expandables.build();
       this.expandIfAllAvailable = ImmutableSet.copyOf(flagGroup.getExpandIfAllAvailableList());
-      this.expandIfTrue = flagGroup.getExpandIfTrue();
-      this.expandIfFalse = flagGroup.getExpandIfFalse();
+      this.expandIfTrue = Strings.emptyToNull(flagGroup.getExpandIfTrue());
+      this.expandIfFalse = Strings.emptyToNull(flagGroup.getExpandIfFalse());
     }
     
     @Override
