d82e69cef3f52f27c418c515f8af6af57d55aaf4
Joe Finney <spxtr@google.com>
2017-01-10 17:36:27 -0800

    Bump python-gflags to version 3.1.0.
    The new version adds support for Python 3.
    
    Change-Id: I88f322c6e9f35699fe5f708f52c5b84fb00a5b4b

--- third_party/py/gflags/gflags/flags_modules_for_testing/module_baz.py
+++ third_party/py/gflags/gflags/flags_modules_for_testing/module_baz.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-
-# Copyright (c) 2009, Google Inc.
-# All rights reserved.
+# Copyright 2014 Google Inc. All Rights Reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
