67a53d22ea83936cd048ceb454b0d81273ee2a79
Yue Gan <yueg@google.com>
2017-02-03 11:27:12 +0000

    Benchmark part 2 (preparation for benchmark) Builders: tools to benchmark the performance. BuildCases: provide all targets for Builders to build.
    Fixes BazelBuilderTest
    
    --
    PiperOrigin-RevId: 146459295
    MOS_MIGRATED_REVID=146459295

---
+++ src/tools/benchmark/java/com/google/devtools/build/benchmark/BazelBuildCase.java
@@ -0,0 +1,124 @@
+// Copyright 2017 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.devtools.build.benchmark;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.devtools.build.benchmark.codegenerator.JavaCodeGenerator;
+import com.google.devtools.build.lib.shell.CommandException;
+import com.google.devtools.build.lib.vfs.FileSystem;
+import com.google.devtools.build.lib.vfs.FileSystemUtils;
+import com.google.devtools.build.lib.vfs.FileSystems;
+import java.io.IOException;
+import java.nio.file.Path;
+
+/** Provides all build target information for Bazel. */
+final class BazelBuildCase implements BuildCase {
+
+  private static final ImmutableMap<String, String> BUILD_TARGET_NAME_TO_DESCRIPTION =
+      ImmutableMap.of(
+          "AFewFiles", "Target: A Few Files",
+          "ManyFiles", "Target: Many Files",
+          "LongChainedDeps", "Target: Long Chained Deps",
+          "ParallelDeps", "Target: Parallel Deps");
+  private static final String WORKSPACE_FILE_NAME = "WORKSPACE";
+  private static final ImmutableList<BuildTargetConfig> defaultBuildTargetConfigs =
+      getDefaultBuildTargetConfigs();
+  private static final boolean INCLUDE_TARGET_A_FEW_FILES = true;
+  private static final boolean INCLUDE_TARGET_MANY_FILES = true;
+  private static final boolean INCLUDE_TARGET_LONG_CHAINED_DEPS = true;
+  private static final boolean INCLUDE_TARGET_PARALLEL_DEPS = true;
+
+  private static final BuildEnvConfig FULL_CLEAN_BUILD_CONFIG =
+      BuildEnvConfig.newBuilder()
+          .setDescription("Full clean build")
+          .setCleanBeforeBuild(true)
+          .setIncremental(false)
+          .build();
+  private static final BuildEnvConfig INCREMENTAL_BUILD_CONFIG =
+      BuildEnvConfig.newBuilder()
+          .setDescription("Incremental build")
+          .setCleanBeforeBuild(false)
+          .setIncremental(true)
+          .build();
+  private static final ImmutableList<BuildEnvConfig> BUILD_ENV_CONFIGS =
+      ImmutableList.of(FULL_CLEAN_BUILD_CONFIG, INCREMENTAL_BUILD_CONFIG);
+
+  private static final FileSystem fileSystem = FileSystems.initDefaultAsJavaIo();
+
+  @Override
+  public ImmutableList<BuildTargetConfig> getBuildTargetConfigs() {
+    return defaultBuildTargetConfigs;
+  }
+
+  @Override
+  public ImmutableList<String> getCodeVersions(Builder builder, String from, String to)
+      throws IOException, CommandException {
+    return builder.getCodeVersionsBetween(from, to);
+  }
+
+  @Override
+  public ImmutableList<BuildEnvConfig> getBuildEnvConfigs() {
+    return BUILD_ENV_CONFIGS;
+  }
+
+  // TODO(yueg): configurable target, we may not want to run benchmark for all kinds of target
+  @Override
+  public void prepareGeneratedCode(Path copyDir, Path generatedCodePath) throws IOException {
+    // Prepare generated code for copy
+    if (!copyDir.toFile().exists()) {
+      JavaCodeGenerator.generateNewProject(
+          copyDir.toString(),
+          INCLUDE_TARGET_A_FEW_FILES,
+          INCLUDE_TARGET_MANY_FILES,
+          INCLUDE_TARGET_LONG_CHAINED_DEPS,
+          INCLUDE_TARGET_PARALLEL_DEPS);
+    }
+
+    // Clean generated code path
+    if (generatedCodePath.toFile().exists()) {
+      try {
+        FileSystemUtils.deleteTreesBelow(fileSystem.getPath(generatedCodePath.toString()));
+      } catch (IOException e) {
+        throw new IOException("Failed to clean directory for generated code", e);
+      }
+    } else {
+      generatedCodePath.toFile().mkdirs();
+    }
+
+    // Copy
+    try {
+      FileSystemUtils.copyTreesBelow(
+          fileSystem.getPath(copyDir.toString()), fileSystem.getPath(generatedCodePath.toString()));
+    } catch (IOException e) {
+      throw new IOException("Failed to copy generated code", e);
+    }
+    if (!generatedCodePath.resolve(WORKSPACE_FILE_NAME).toFile().createNewFile()) {
+      throw new IOException("Failed to create workspace file");
+    }
+  }
+
+  private static ImmutableList<BuildTargetConfig> getDefaultBuildTargetConfigs() {
+    ImmutableList.Builder<BuildTargetConfig> builder = ImmutableList.builder();
+    for (ImmutableMap.Entry<String, String> entry : BUILD_TARGET_NAME_TO_DESCRIPTION.entrySet()) {
+      builder.add(
+          BuildTargetConfig.newBuilder()
+              .setBuildTarget(entry.getKey())
+              .setDescription(entry.getValue())
+              .build());
+    }
+    return builder.build();
+  }
+}
---
+++ src/tools/benchmark/java/com/google/devtools/build/benchmark/BazelBuilder.java
@@ -0,0 +1,159 @@
+// Copyright 2017 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.devtools.build.benchmark;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import com.google.common.collect.ImmutableList;
+import com.google.devtools.build.lib.shell.Command;
+import com.google.devtools.build.lib.shell.CommandException;
+import com.google.devtools.build.lib.shell.CommandResult;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/** Class that provides all needed feature of Bazel for benchmark. */
+class BazelBuilder implements Builder {
+
+  private static final Logger logger = Logger.getLogger(BazelBuilder.class.getName());
+
+  private static final String BAZEL_BINARY_PATH = "bazel-bin/src/bazel";
+  private static final Pattern ELAPSED_TIME_PATTERN = Pattern.compile("(?<=Elapsed time: )[0-9.]+");
+  private static final String DEFAULT_GIT_REPO = "https://github.com/bazelbuild/bazel.git";
+
+  private final Path generatedCodeDir;
+  private final Path builderDir;
+  private Path buildBinary = null;
+  private String currentCodeVersion = "";
+
+  BazelBuilder(Path generatedCodeDir, Path builderDir) {
+    this.generatedCodeDir = generatedCodeDir;
+    this.builderDir = builderDir;
+  }
+
+  @Override
+  public ImmutableList<String> getCodeVersionsBetween(String from, String to)
+      throws CommandException {
+    String[] gitLogCommand = {"git", "log", from + ".." + to, "--pretty=format:%H", "--reverse"};
+    Command cmd = new Command(gitLogCommand, null, builderDir.toFile());
+    CommandResult result = cmd.execute();
+    String output = new String(result.getStdout(), UTF_8).trim();
+    return ImmutableList.copyOf(output.split("\n"));
+  }
+
+  @Override
+  public Path getBuildBinary(String codeVersion) throws IOException, CommandException {
+    if (buildBinary != null && currentCodeVersion.equals(codeVersion)) {
+      return buildBinary;
+    }
+
+    // git checkout codeVersion
+    String[] checkoutCommand = {"git", "checkout", codeVersion};
+    Command cmd = new Command(checkoutCommand, null, builderDir.toFile());
+    cmd.execute();
+
+    // bazel build src:bazel
+    String[] buildBazelCommand = {"bazel", "build", "src:bazel"};
+    cmd = new Command(buildBazelCommand, null, builderDir.toFile());
+    CommandResult result = cmd.execute();
+
+    // Get binary path, bazel output is in stderr
+    String output = new String(result.getStderr(), UTF_8).trim();
+    if (!output.contains(BAZEL_BINARY_PATH)) {
+      throw new IOException("Bazel binary " + BAZEL_BINARY_PATH + " is not in output of build.");
+    }
+    buildBinary = builderDir.resolve(BAZEL_BINARY_PATH);
+    currentCodeVersion = codeVersion;
+    return buildBinary;
+  }
+
+  @Override
+  public ImmutableList<String> getCommandFromConfig(
+      BuildTargetConfig targetConfig, BuildEnvConfig envConfig) {
+    return ImmutableList.<String>builder()
+        .add("build")
+        .add(targetConfig.getBuildTarget())
+        .addAll(envConfig.getBuildArgsList())
+        .build();
+  }
+
+  @Override
+  public double buildAndGetElapsedTime(Path buildBinary, ImmutableList<String> args)
+      throws CommandException {
+    List<String> cmdList = new ArrayList<>();
+    cmdList.add(buildBinary.toString());
+    cmdList.addAll(args);
+    String[] cmdArr = new String[cmdList.size()];
+    cmdArr = cmdList.toArray(cmdArr);
+
+    // Run build command
+    Command cmd = new Command(cmdArr, null, generatedCodeDir.toFile());
+    CommandResult result = cmd.execute();
+
+    // Get elapsed time from output
+    String output = new String(result.getStderr(), UTF_8).trim();
+    Matcher m = ELAPSED_TIME_PATTERN.matcher(output);
+
+    if (m.find()) {
+      try {
+        return (Double.parseDouble(m.group(0)));
+      } catch (NumberFormatException e) {
+        // Should not be here since we look for [0-9.]+
+        logger.log(Level.SEVERE, "Cannot parse " + m.group(0));
+      }
+    }
+    throw new CommandException(cmd, "Command didn't provide parsable output.");
+  }
+
+  @Override
+  public void clean() throws CommandException {
+    String[] cleanCommand = {"bazel", "clean"};
+    Command cmd = new Command(cleanCommand, null, generatedCodeDir.toFile());
+    cmd.execute();
+  }
+
+  @Override
+  public void prepare() throws IOException, CommandException {
+    prepareFromGitRepo(DEFAULT_GIT_REPO);
+  }
+
+  void prepareFromGitRepo(String gitRepo) throws IOException, CommandException {
+    if (builderDir.toFile().exists() && !builderDir.toFile().isDirectory()) {
+      try {
+        Files.delete(builderDir);
+      } catch (IOException e) {
+        throw new IOException(builderDir + " is a file and cannot be deleted", e);
+      }
+    }
+    if (Files.notExists(builderDir)) {
+      try {
+        Files.createDirectories(builderDir);
+      } catch (IOException e) {
+        throw new IOException("Failed to create directory for bazel", e);
+      }
+
+      String[] gitCloneCommand = {"git", "clone", gitRepo, "."};
+      Command cmd = new Command(gitCloneCommand, null, builderDir.toFile());
+      cmd.execute();
+    }
+    // Assume the directory is what we need if not empty
+  }
+}
