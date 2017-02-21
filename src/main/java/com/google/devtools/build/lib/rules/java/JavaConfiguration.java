d0b45b51c6cfd469adf15e3a294ed918fe24d50f
Damien Martin-Guillerez <dmarting@google.com>
2017-02-21 17:56:02 +0000

    Rollback of commit bd3be73b84001dbdb2f05297f215e8eb5bacf0e1.
    *** Reason for rollback ***
    
    To deprecate that flag, we need to fix #2539 first.
    
    *** Original change description ***
    
    Deprecate the legacy_java_test flag, now that nolegacy_java_test has been the default for quite a while now.
    
    RELNOTES[INC]: legacy_java_test flag will not have any effect now.
    
    --
    PiperOrigin-RevId: 148107667
    MOS_MIGRATED_REVID=148107667

--- src/main/java/com/google/devtools/build/lib/rules/java/JavaConfiguration.java
+++ src/main/java/com/google/devtools/build/lib/rules/java/JavaConfiguration.java
@@ -144,6 +144,9 @@ public final class JavaConfiguration extends Fragment {
   private final JavaOptimizationMode javaOptimizationMode;
   private final Label javaToolchain;
 
+  // TODO(dmarting): remove once we have a proper solution for #2539
+  private final boolean legacyBazelJavaTest;
+
   JavaConfiguration(
       boolean generateJavaDeps,
       List<String> defaultJvmFlags,
@@ -169,6 +172,7 @@ public final class JavaConfiguration extends Fragment {
     this.bundleTranslations = javaOptions.bundleTranslations;
     this.javaToolchain = javaToolchain;
     this.javaOptimizationMode = javaOptions.javaOptimizationMode;
+    this.legacyBazelJavaTest = javaOptions.legacyBazelJavaTest;
     this.strictDepsJavaProtos = javaOptions.strictDepsJavaProtos;
     this.enforceOneVersion = javaOptions.enforceOneVersion;
 
@@ -326,6 +330,14 @@ public final class JavaConfiguration extends Fragment {
    */
   public JavaOptimizationMode getJavaOptimizationMode() {
     return javaOptimizationMode;
+  }
+
+  /**
+   * Returns true if java_test in Bazel should behave in legacy mode that existed before we
+   * open-sourced our test runner.
+   */
+  public boolean useLegacyBazelJavaTest() {
+    return legacyBazelJavaTest;
   }
 
   /**
