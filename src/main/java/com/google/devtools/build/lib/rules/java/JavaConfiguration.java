4d454a96832b311589a3849632b68cb3f0bd1915
Philipp Wollermann <philwo@google.com>
2017-03-03 12:56:03 +0000

    Remove unused javacOpts field and its getter from JavaConfiguration.
    They were only used by a test that can just as well use the tokenized
    version of the javacOpts.
    
    --
    PiperOrigin-RevId: 149102073
    MOS_MIGRATED_REVID=149102073

--- src/main/java/com/google/devtools/build/lib/rules/java/JavaConfiguration.java
+++ src/main/java/com/google/devtools/build/lib/rules/java/JavaConfiguration.java
@@ -136,7 +136,6 @@ public final class JavaConfiguration extends Fragment {
   private final ImmutableList<String> defaultJvmFlags;
   private final ImmutableList<String> checkedConstraints;
   private final StrictDepsMode strictJavaDeps;
-  private final ImmutableList<String> javacOpts;
   private final Label proguardBinary;
   private final ImmutableList<Label> extraProguardSpecs;
   private final TriState bundleTranslations;
@@ -166,7 +165,6 @@ public final class JavaConfiguration extends Fragment {
     this.defaultJvmFlags = ImmutableList.copyOf(defaultJvmFlags);
     this.checkedConstraints = ImmutableList.copyOf(javaOptions.checkedConstraints);
     this.strictJavaDeps = javaOptions.strictJavaDeps;
-    this.javacOpts = ImmutableList.copyOf(javaOptions.javacOpts);
     this.proguardBinary = javaOptions.proguard;
     this.extraProguardSpecs = ImmutableList.copyOf(javaOptions.extraProguardSpecs);
     this.bundleTranslations = javaOptions.bundleTranslations;
@@ -274,10 +272,6 @@ public final class JavaConfiguration extends Fragment {
    */
   public Label getJavaLauncherLabel() {
     return javaLauncherLabel;
-  }
-
-  public ImmutableList<String> getJavacOpts() {
-    return javacOpts;
   }
 
   /**
