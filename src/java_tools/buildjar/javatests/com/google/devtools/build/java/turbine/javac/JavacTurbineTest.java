3be4459460b579cce1568a54931f454de0b452d7
Liam Miller-Cushon <cushon@google.com>
2017-03-17 00:14:35 +0000

    Don't declare parameters of type Iterable<Path>
    context: https://bugs.openjdk.java.net/browse/JDK-8150111
    
    --
    PiperOrigin-RevId: 150390207
    MOS_MIGRATED_REVID=150390207

--- src/java_tools/buildjar/javatests/com/google/devtools/build/java/turbine/javac/JavacTurbineTest.java
+++ src/java_tools/buildjar/javatests/com/google/devtools/build/java/turbine/javac/JavacTurbineTest.java
@@ -55,6 +55,7 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.LinkedHashMap;
@@ -437,7 +438,7 @@ public class JavacTurbineTest {
   }
 
   private void compileLib(
-      Path jar, Iterable<Path> classpath, Iterable<? extends JavaFileObject> units)
+      Path jar, Collection<Path> classpath, Iterable<? extends JavaFileObject> units)
       throws IOException {
     final Path outdir = temp.newFolder().toPath();
     JavacFileManager fm = new JavacFileManager(new Context(), false, UTF_8);
