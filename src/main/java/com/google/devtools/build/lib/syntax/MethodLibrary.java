e835a3f037e83fb4abb85a3eb5c2fe653a219477
brandjon <brandjon@google.com>
2017-04-10 14:55:13 +0000

    Fix/clarify doc examples for string *strip() methods
    RELNOTES: None
    PiperOrigin-RevId: 152686923

--- src/main/java/com/google/devtools/build/lib/syntax/MethodLibrary.java
+++ src/main/java/com/google/devtools/build/lib/syntax/MethodLibrary.java
@@ -183,7 +183,7 @@ public class MethodLibrary {
         "Returns a copy of the string where trailing characters that appear in <code>chars</code>"
             + "are removed."
             + "<pre class=\"language-python\">"
-            + "\"abcba\".rstrip(\"ba\") == \"abc\""
+            + "\"abcbaa\".rstrip(\"ab\") == \"abc\""
             + "</pre>",
     parameters = {
       @Param(name = "self", type = String.class, doc = "This string."),
@@ -212,7 +212,7 @@ public class MethodLibrary {
         "Returns a copy of the string where trailing characters that appear in <code>chars</code>"
             + "are removed."
             + "<pre class=\"language-python\">"
-            + "\"abcba\".strip(\"ba\") == \"abc\""
+            + "\"aabcbcbaa\".strip(\"ab\") == \"cbc\""
             + "</pre>",
     parameters = {
       @Param(name = "self", type = String.class, doc = "This string."),
