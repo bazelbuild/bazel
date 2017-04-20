2c4dd1f3176371473667c551cd5a5ae5992f8154
Philipp Wollermann <philwo@google.com>
2017-04-20 18:35:16 +0200

    Bundled JDK changes for the release process.
    - Make the default Bazel release artifacts include the bundled JDK.
    - Create additional Bazel release artifacts without a bundled JDK.
    
    Change-Id: If87c986507ae8e7dbbf0e8f163dd0073a206b265
    PiperOrigin-RevId: 153718827

--- scripts/ci/windows/compile_windows.sh
+++ scripts/ci/windows/compile_windows.sh
@@ -48,11 +48,13 @@ fi
 ${BOOTSTRAP_BAZEL} --bazelrc=${BAZELRC:-/dev/null} --nomaster_bazelrc build \
     --embed_label=${release_label} --stamp \
     ${MSVC_OPTS} \
-    //src:bazel
+    //src:bazel //src:bazel_with_jdk
 
-
-# Copy the resulting artifact.
+# Copy the resulting artifacts.
 mkdir -p output/ci
-cp bazel-bin/src/bazel output/ci/bazel${MSVC_LABEL}-$(get_full_release_name).exe
+cp bazel-bin/src/bazel output/ci/bazel${MSVC_LABEL}-$(get_full_release_name)-without-jdk.exe
+cp bazel-bin/src/bazel_with_jdk output/ci/bazel${MSVC_LABEL}-$(get_full_release_name).exe
 cp bazel-bin/src/bazel output/bazel.exe
+zip -j output/ci/bazel${MSVC_LABEL}-$(get_full_release_name)-without-jdk.zip output/bazel.exe
+cp -f bazel-bin/src/bazel_with_jdk output/bazel.exe
 zip -j output/ci/bazel${MSVC_LABEL}-$(get_full_release_name).zip output/bazel.exe
