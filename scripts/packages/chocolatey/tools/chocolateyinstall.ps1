65ff1543c161014e0bbcf20cec189a0a76888c2d
Peter Mounce <petermounce@improbable.io>
2017-05-29 14:43:53 +0200

    Make msysless flavour be the default inside chocolatey
    This attempts to fix #3006.
    
    This change will presumably have to be reversed once msysless binary name changes to become the default.
    
    It would be simpler to name the default binary for being default, to avoid special casing (see https://github.com/bazelbuild/bazel/issues/3006#issuecomment-301440068).
    
    Closes #3066.
    
    PiperOrigin-RevId: 157390473

--- scripts/packages/chocolatey/tools/chocolateyinstall.ps1
+++ scripts/packages/chocolatey/tools/chocolateyinstall.ps1
@@ -11,35 +11,18 @@ write-host "Type: $($paramsText.GetType())"
 
 $packageDir = Split-Path -parent $toolsDir
 
-
 Install-ChocolateyZipPackage -PackageName "$packageName" `
   -Url64bit "$($paramsText[0])" `
   -Checksum64 "$($paramsText[1])" `
   -ChecksumType64 "sha256" `
   -UnzipLocation "$packageDir"
 
-write-host "Ensure that msys2 dll is present in PATH to allow bazel to be run from non-msys2 shells"
-$pp = Get-PackageParameters $env:chocolateyPackageParameters
-$msys2Path = ($pp.msys2Path, "c:\tools\msys64" -ne $null)[0]
-Install-ChocolateyPath -PathToInstall "$($msys2Path)\usr\bin" -PathType "Machine"
-
-$addToMsysPath = ($packageDir -replace '^([a-zA-Z]):\\(.*)','/$1/$2') -replace '\\','/'
 write-host @"
 bazel installed to $packageDir
 
-To use it in powershell or cmd, you should ensure your PATH environment variable contains
-  $($msys2Path)\usr\bin
-BEFORE both
-  c:\windows\system32 (because bash-on-windows' bash.exe will be found here, if it's installed)
-  any references to msysgit (like c:\program files (x86)\git\bin or c:\program files (x86)\git\cmd) (because git's vendored version of msys2 will interfere with the real msys2)
-
-To use it in msys2, you should add that to your msys2 PATH:
-  export PATH=$($addToMsysPath):`$PATH
-
-You also need, in your msys2 environment (adjust paths for your system):
-  export JAVA_HOME="`$(ls -d C:/Program\ Files/Java/jdk* | sort | tail -n 1)`"
-  export BAZEL_SH=c:/tools/msys64/usr/bin/bash.exe
-  export BAZEL_PYTHON=c:/tools/python2/python.exe
+You also need, in your environment variables (adjust paths for your system):
+  BAZEL_SH=c:\tools\msys64\usr\bin\bash.exe
+  BAZEL_PYTHON=c:\tools\python2\python.exe
 
 See also https://bazel.build/docs/windows.html
 "@
