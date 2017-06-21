3c566c6697a3a0e574f0cc7da2da9fb16be35cb7
mschaller <mschaller@google.com>
2017-06-22 00:11:44 +0200

    Use targets' labels' package identifier for query output=package
    Subinclude targets' packages are the package containing the load
    statement. Subinclude targets' labels are the bzl files they refer to.
    The latter has the appropriate package to display for --output=package.
    
    Fixes #3122.
    
    PiperOrigin-RevId: 159750840

--- src/test/shell/bazel/local_repository_test.sh
+++ src/test/shell/bazel/local_repository_test.sh
@@ -427,6 +427,35 @@ EOF
   expect_log "//external:my_repo"
 }
 
+function test_repository_package_query() {
+  mkdir a b b/b
+  echo "local_repository(name='b', path='b')" > WORKSPACE
+  echo "sh_library(name='a', deps=['@b//b'])" > a/BUILD
+  touch b/WORKSPACE
+  echo "sh_library(name='b')" > b/b/BUILD
+  bazel query --output package "deps(//a)" >& $TEST_log || fail "query failed"
+  expect_log "a"
+  expect_log "@b//b"
+}
+
+function test_repository_buildfiles_package_query() {
+  mkdir a b b/b b/c
+  echo "local_repository(name='b', path='b')" > WORKSPACE
+  echo "sh_library(name='a', deps=['@b//b'])" > a/BUILD
+  touch b/WORKSPACE b/c/BUILD
+  cat > b/b/BUILD <<EOF
+load('//c:lib.bzl', 'x')
+sh_library(
+    name = "b"
+)
+EOF
+  echo "x = 2" > b/c/lib.bzl
+  bazel query --output package "buildfiles(deps(//a))" >& $TEST_log || fail "query failed"
+  expect_log "a"
+  expect_log "@b//b"
+  expect_log "@b//c"
+}
+
 function test_warning() {
   local bar=$TEST_TMPDIR/bar
   rm -rf "$bar"
