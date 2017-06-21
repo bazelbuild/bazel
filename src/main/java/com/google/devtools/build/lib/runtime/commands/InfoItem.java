89659810e3048782dfb5e308e39aa8a0727e464e
kchodorow <kchodorow@google.com>
2017-06-21 18:15:36 +0200

    Set correct execroot for info
    Fixes #3055.
    
    RELNOTES: `bazel info execution_root` returns the corrrect directory name
    for the execution root.
    PiperOrigin-RevId: 159701171

--- src/main/java/com/google/devtools/build/lib/runtime/commands/InfoItem.java
+++ src/main/java/com/google/devtools/build/lib/runtime/commands/InfoItem.java
@@ -181,7 +181,8 @@ public abstract class InfoItem {
     public byte[] get(Supplier<BuildConfiguration> configurationSupplier, CommandEnvironment env)
         throws AbruptExitException {
       checkNotNull(env);
-      return print(env.getDirectories().getExecRoot());
+      return print(env.getDirectories().getExecRoot(
+          configurationSupplier.get().getMainRepositoryName()));
     }
   }
 
