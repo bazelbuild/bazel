f49468c9e65483b93fa3c956f66f5503eb3211cd
lberki <lberki@google.com>
2017-07-18 10:35:52 +0200

    Add an undocumented option to remove the separation between the bin and genfiles directories.
    RELNOTES: None.
    PiperOrigin-RevId: 162325236

--- src/main/java/com/google/devtools/build/lib/analysis/config/BuildConfiguration.java
+++ src/main/java/com/google/devtools/build/lib/analysis/config/BuildConfiguration.java
@@ -464,6 +464,16 @@ public final class BuildConfiguration implements BuildEvent {
    */
   public static class Options extends FragmentOptions implements Cloneable {
     @Option(
+      name = "experimental_separate_genfiles_directory",
+      defaultValue = "true",
+      category = "semantics",
+      documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
+      effectTags = { OptionEffectTag.AFFECTS_OUTPUTS },
+      help = "Whether to have a separate genfiles directory or fold it into the bin directory"
+    )
+
+    public boolean separateGenfilesDirectory;
+    @Option(
       name = "define",
       converter = Converters.AssignmentConverter.class,
       defaultValue = "",
@@ -1134,6 +1144,7 @@ public final class BuildConfiguration implements BuildEvent {
       host.useDynamicConfigurations = useDynamicConfigurations;
       host.commandLineBuildVariables = commandLineBuildVariables;
       host.enforceConstraints = enforceConstraints;
+      host.separateGenfilesDirectory = separateGenfilesDirectory;
 
       if (fallback) {
         // In the fallback case, we have already tried the target options and they didn't work, so
@@ -1293,6 +1304,8 @@ public final class BuildConfiguration implements BuildEvent {
   private final Root coverageDirectoryForMainRepository;
   private final Root testlogsDirectoryForMainRepository;
   private final Root middlemanDirectoryForMainRepository;
+
+  private final boolean separateGenfilesDirectory;
 
   // Cache this value for quicker access. We don't cache it inside BuildOptions because BuildOptions
   // is mutable, so a cached value there could fall out of date when it's updated.
@@ -1519,6 +1532,7 @@ public final class BuildConfiguration implements BuildEvent {
     this.buildOptions = buildOptions.clone();
     this.actionsEnabled = buildOptions.enableActions();
     this.options = buildOptions.get(Options.class);
+    this.separateGenfilesDirectory = options.separateGenfilesDirectory;
     this.mainRepositoryName = RepositoryName.createFromValidStrippedName(repositoryName);
     this.dynamicTransitionMapper = dynamicTransitionMapper;
 
@@ -2234,6 +2248,10 @@ public final class BuildConfiguration implements BuildEvent {
   }
 
   public Root getGenfilesDirectory(RepositoryName repositoryName) {
+    if (!separateGenfilesDirectory) {
+      return getBinDirectory(repositoryName);
+    }
+
     return repositoryName.isMain() || repositoryName.equals(mainRepositoryName)
         ? genfilesDirectoryForMainRepository
         : OutputDirectory.GENFILES.getRoot(
@@ -2558,7 +2576,7 @@ public final class BuildConfiguration implements BuildEvent {
     return options.useLLVMCoverageMapFormat;
   }
 
-  /** If false, AnalysisEnviroment doesn't register any actions created by the ConfiguredTarget. */
+  /** If false, AnalysisEnvironment doesn't register any actions created by the ConfiguredTarget. */
   public boolean isActionsEnabled() {
     return actionsEnabled;
   }
