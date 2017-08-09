99965197bd3eb418a28b87360e4bfb10275db21e
elenairina <elenairina@google.com>
2017-08-09 17:07:33 +0200

    Revert collect_coverage change that enables the new Java coverage
    in blaze. This should be enabled only after the blaze release, otherwise it makes code coverage drop, mainly due to including auto-generated files in the coverage report.
    
    PiperOrigin-RevId: 164724689

--- src/main/java/com/google/devtools/build/lib/rules/test/TestRunnerAction.java
+++ src/main/java/com/google/devtools/build/lib/rules/test/TestRunnerAction.java
@@ -460,6 +460,8 @@ public class TestRunnerAction extends AbstractAction implements NotifyOnActionCa
       env.put("COVERAGE_MANIFEST", getCoverageManifest().getExecPathString());
       env.put("COVERAGE_DIR", getCoverageDirectory().getPathString());
       env.put("COVERAGE_OUTPUT_FILE", getCoverageData().getExecPathString());
+      // TODO(elenairina): Remove this after the next blaze release (after 2017.07.30).
+      env.put("NEW_JAVA_COVERAGE_IMPL", "True");
     }
   }
 
