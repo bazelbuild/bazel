d1c59169921d9aa3d066f9694517454221dac089
laurentlb <laurentlb@google.com>
2017-08-10 22:15:22 +0200

    Fix AST visitor for for loops and comprehensions.
    RELNOTES: None.
    PiperOrigin-RevId: 164893252

--- src/main/java/com/google/devtools/build/lib/syntax/SyntaxTreeVisitor.java
+++ src/main/java/com/google/devtools/build/lib/syntax/SyntaxTreeVisitor.java
@@ -66,18 +66,17 @@ public class SyntaxTreeVisitor {
   public void visit(@SuppressWarnings("unused") Identifier node) {}
 
   public void visit(AbstractComprehension node) {
-    visitAll(node.getOutputExpressions());
-
     for (ListComprehension.Clause clause : node.getClauses()) {
       if (clause.getLValue() != null) {
         visit(clause.getLValue());
       }
       visit(clause.getExpression());
     }
+    visitAll(node.getOutputExpressions());
   }
 
   public void visit(ForStatement node) {
-    visit(node.getVariable().getExpression());
+    visit(node.getVariable());
     visit(node.getCollection());
     visitAll(node.getBlock());
   }
