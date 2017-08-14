ad87c1379a60a090e328f27b1ea5ed2295a2f17e
Klaus Aehlig <aehlig@google.com>
2017-08-14 12:20:39 +0200

    Fix product name in directory messages
    If bazel runs under emacs, it emits messages about changing
    of directories.  In those messages, fix the product name.
    
    Change-Id: I233a46db741dc4cd0dc5bfbaadbf2ad09c04952e
    PiperOrigin-RevId: 165159721

--- src/main/java/com/google/devtools/build/lib/buildtool/ExecutionTool.java
+++ src/main/java/com/google/devtools/build/lib/buildtool/ExecutionTool.java
@@ -403,7 +403,10 @@ public class ExecutionTool {
 
     if (request.isRunningInEmacs()) {
       // The syntax of this message is tightly constrained by lisp/progmodes/compile.el in emacs
-      request.getOutErr().printErrLn("blaze: Entering directory `" + getExecRoot() + "/'");
+      request
+          .getOutErr()
+          .printErrLn(
+              env.getRuntime().getProductName() + ": Entering directory `" + getExecRoot() + "/'");
     }
     boolean buildCompleted = false;
     try {
@@ -445,7 +448,10 @@ public class ExecutionTool {
     } finally {
       env.recordLastExecutionTime();
       if (request.isRunningInEmacs()) {
-        request.getOutErr().printErrLn("blaze: Leaving directory `" + getExecRoot() + "/'");
+        request
+            .getOutErr()
+            .printErrLn(
+                env.getRuntime().getProductName() + ": Leaving directory `" + getExecRoot() + "/'");
       }
       if (buildCompleted) {
         getReporter().handle(Event.progress("Building complete."));
