bazelbuild/bazel

commit 9626bb4923c74c6d3c09b7438eb24b32191053df

remote: Refactor RemoteSpawnRunner to distinquish between remote executor, remote cache and local executor errors.
This is a no-op refactoring and clears the way to integrate a change
that no longer uploads to the remote cache if a previous remote cache
interaction failed.

RELNOTES: None.
PiperOrigin-RevId: 164966432

src/main/java/com/google/devtools/build/lib/remote/RemoteSpawnRunner.java
@@ -31,6 +31,7 @@ import com.google.devtools.build.lib.exec.SpawnResult.Status;
 import com.google.devtools.build.lib.exec.SpawnRunner;
 import com.google.devtools.build.lib.remote.Digests.ActionKey;
 import com.google.devtools.build.lib.remote.TreeNodeRepository.TreeNode;
+import com.google.devtools.build.lib.util.io.FileOutErr;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.PathFragment;
 import com.google.devtools.remoteexecution.v1test.Action;
@@ -107,73 +108,110 @@ class RemoteSpawnRunner implements SpawnRunner {
 
     // Look up action cache, and reuse the action output if it is found.
     ActionKey actionKey = Digests.computeActionKey(action);
+    boolean acceptCachedResult = options.remoteAcceptCached && Spawns.mayBeCached(spawn);
+    boolean uploadLocalResults = options.remoteUploadLocalResults;
+
     try {
-      boolean acceptCachedResult = options.remoteAcceptCached && Spawns.mayBeCached(spawn);
-      ActionResult result =
+      // Try to lookup the action in the action cache.
+      ActionResult cachedResult =
           acceptCachedResult
               ? remoteCache.getCachedActionResult(actionKey)
               : null;
-      if (result != null) {
-        // We don't cache failed actions, so we know the outputs exist.
-        // For now, download all outputs locally; in the future, we can reuse the digests to
-        // just update the TreeNodeRepository and continue the build.
+      if (cachedResult != null) {
         try {
-          remoteCache.download(result, execRoot, policy.getFileOutErr());
-          return new SpawnResult.Builder()
-              .setStatus(Status.SUCCESS)  // Even if the action failed with non-zero exit code.
-              .setExitCode(result.getExitCode())
-              .build();
+          return downloadRemoteResults(cachedResult, policy.getFileOutErr());
         } catch (CacheNotFoundException e) {
-          acceptCachedResult = false; // Retry the action remotely and invalidate the results.
+          // Intentionally left empty. No cache hit, so we fall through to local or
+          // remote execution.
         }
       }
+    } catch (IOException e) {
+      return execLocallyOrFail(spawn, policy, inputMap, actionKey,
+          options.remoteUploadLocalResults, e);
+    }
 
-      if (remoteExecutor == null) {
-        return execLocally(spawn, policy, inputMap, options.remoteUploadLocalResults,
-            remoteCache, actionKey);
-      }
+    if (remoteExecutor == null) {
+      // Remote execution is disabled and so execute the spawn on the local machine.
+      return execLocally(spawn, policy, inputMap, uploadLocalResults, remoteCache, actionKey);
+    }
 
+    try {
       // Upload the command and all the inputs into the remote cache.
       remoteCache.ensureInputsPresent(repository, execRoot, inputRoot, command);
-      // TODO(olaola): set BuildInfo and input total bytes as well.
-      ExecuteRequest.Builder request =
-          ExecuteRequest.newBuilder()
-              .setInstanceName(options.remoteInstanceName)
-              .setAction(action)
-              .setTotalInputFileCount(inputMap.size())
-              .setSkipCacheLookup(!acceptCachedResult);
-      ExecuteResponse reply = remoteExecutor.executeRemotely(request.build());
-      result = reply.getResult();
-      if (options.remoteLocalFallback && result.getExitCode() != 0) {
-        return execLocally(spawn, policy, inputMap, options.remoteUploadLocalResults,
-            remoteCache, actionKey);
-      }
-      remoteCache.download(result, execRoot, policy.getFileOutErr());
-      return new SpawnResult.Builder()
-          .setStatus(Status.SUCCESS)  // Even if the action failed with non-zero exit code.
-          .setExitCode(result.getExitCode())
-          .build();
     } catch (IOException e) {
-      if (options.remoteLocalFallback) {
-        return execLocally(spawn, policy, inputMap, options.remoteUploadLocalResults,
-            remoteCache, actionKey);
-      }
-
-      String message = "";
-      if (e instanceof RetryException
-          && ((RetryException) e).causedByStatusCode(Code.UNAVAILABLE)) {
-        message = "The remote executor/cache is unavailable";
-      } else if (e instanceof CacheNotFoundException) {
-        message = "Failed to download from remote cache";
-      } else {
-        message = "Error in remote cache/executor";
-      }
-      // TODO(olaola): reuse the ErrorMessage class for these errors.
-      if (verboseFailures) {
-        message += "\n" + Throwables.getStackTraceAsString(e);
-      }
-      throw new EnvironmentalExecException(message, e, true);
+      return execLocallyOrFail(spawn, policy, inputMap, actionKey,
+          options.remoteUploadLocalResults, e);
     }
+
+    final ActionResult result;
+    try {
+      result = executeRemotely(action, inputMap.size(), acceptCachedResult);
+    } catch (IOException e) {
+      return execLocallyOrFail(spawn, policy, inputMap, actionKey, uploadLocalResults, e);
+    }
+
+    boolean executionFailed = result.getExitCode() != 0;
+    if (options.remoteLocalFallback && executionFailed) {
+      return execLocally(spawn, policy, inputMap, options.remoteUploadLocalResults,
+          remoteCache, actionKey);
+    }
+
+    try {
+      return downloadRemoteResults(result, policy.getFileOutErr());
+    } catch (IOException e) {
+      return execLocallyOrFail(spawn, policy, inputMap, actionKey,
+          options.remoteUploadLocalResults, e);
+    }
+  }
+
+  private SpawnResult downloadRemoteResults(ActionResult result, FileOutErr outErr)
+      throws ExecException, IOException, InterruptedException {
+    remoteCache.download(result, execRoot, outErr);
+    return new SpawnResult.Builder()
+        .setStatus(Status.SUCCESS)  // Even if the action failed with non-zero exit code.
+        .setExitCode(result.getExitCode())
+        .build();
+  }
+
+  private ActionResult executeRemotely(Action action, int numInputFiles,
+      boolean acceptCachedResult) throws IOException, InterruptedException {
+    // TODO(olaola): set BuildInfo and input total bytes as well.
+    ExecuteRequest.Builder request =
+        ExecuteRequest.newBuilder()
+            .setInstanceName(options.remoteInstanceName)
+            .setAction(action)
+            .setTotalInputFileCount(numInputFiles)
+            .setSkipCacheLookup(!acceptCachedResult);
+    ExecuteResponse reply = remoteExecutor.executeRemotely(request.build());
+    return reply.getResult();
+  }
+
+  private SpawnResult execLocallyOrFail(Spawn spawn, SpawnExecutionPolicy policy,
+      SortedMap<PathFragment, ActionInput> inputMap, ActionKey actionKey,
+      boolean uploadLocalResults, IOException cause)
+      throws ExecException, InterruptedException, IOException {
+    if (options.remoteLocalFallback) {
+      return execLocally(spawn, policy, inputMap, uploadLocalResults,
+          remoteCache, actionKey);
+    }
+    throw new EnvironmentalExecException(errorMessage(cause), cause, true);
+  }
+
+  private String errorMessage(IOException e) {
+    String message = "";
+    if (e instanceof RetryException
+        && ((RetryException) e).causedByStatusCode(Code.UNAVAILABLE)) {
+      message = "The remote executor/cache is unavailable";
+    } else if (e instanceof CacheNotFoundException) {
+      message = "Failed to download from remote cache";
+    } else {
+      message = "Error in remote cache/executor";
+    }
+    // TODO(olaola): reuse the ErrorMessage class for these errors.
+    if (verboseFailures) {
+      message += "\n" + Throwables.getStackTraceAsString(e);
+    }
+    return message;
   }
 
   static Action buildAction(

commit 65e0f78e71ef91f95186471d1d82f5cc33084e53

Fixing #3552: re-execute cached orphaned Actions.
A bug was introduced in patch 9626bb4923c74c6d3c09b7438eb24b32191053df, where a cache miss would not result in action re-execution, making the cache miss non-recoverable.

RELNOTES: fixes #3552
PiperOrigin-RevId: 165434579

src/main/java/com/google/devtools/build/lib/remote/RemoteSpawnRunner.java
@@ -126,8 +126,9 @@ class RemoteSpawnRunner implements SpawnRunner {
         try {
           return downloadRemoteResults(cachedResult, policy.getFileOutErr());
         } catch (CacheNotFoundException e) {
-          // Intentionally left empty. No cache hit, so we fall through to local or
-          // remote execution.
+          // No cache hit, so we fall through to local or remote execution.
+          // We set acceptCachedResult to false in order to force the action re-execution.
+          acceptCachedResult = false;
         }
       }
     } catch (IOException e) {

src/test/java/com/google/devtools/build/lib/remote/GrpcRemoteExecutionClientTest.java
@@ -728,4 +728,82 @@ public class GrpcRemoteExecutionClientTest {
       assertThat(((CacheNotFoundException) t).getMissingDigest()).isEqualTo(stdOutDigest);
     }
   }
+
+  @Test
+  public void remotelyReExecuteOrphanedCachedActions() throws Exception {
+    final Digest stdOutDigest = Digests.computeDigestUtf8("stdout");
+    final ActionResult actionResult =
+        ActionResult.newBuilder().setStdoutDigest(stdOutDigest).build();
+    serviceRegistry.addService(
+        new ActionCacheImplBase() {
+          @Override
+          public void getActionResult(
+              GetActionResultRequest request, StreamObserver<ActionResult> responseObserver) {
+            responseObserver.onNext(actionResult);
+            responseObserver.onCompleted();
+          }
+        });
+    serviceRegistry.addService(
+        new ByteStreamImplBase() {
+          @Override
+          public void read(ReadRequest request, StreamObserver<ReadResponse> responseObserver) {
+            // All reads are a cache miss.
+            responseObserver.onError(Status.NOT_FOUND.asRuntimeException());
+          }
+
+          @Override
+          public StreamObserver<WriteRequest> write(
+              StreamObserver<WriteResponse> responseObserver) {
+            return new StreamObserver<WriteRequest>() {
+              @Override
+              public void onNext(WriteRequest request) {}
+
+              @Override
+              public void onCompleted() {
+                responseObserver.onCompleted();
+              }
+
+              @Override
+              public void onError(Throwable t) {
+                fail("An error occurred: " + t);
+              }
+            };
+          }
+        });
+    serviceRegistry.addService(
+        new ExecutionImplBase() {
+          @Override
+          public void execute(ExecuteRequest request, StreamObserver<Operation> responseObserver) {
+            assertThat(request.getSkipCacheLookup()).isTrue(); // Action will be re-executed.
+            responseObserver.onNext(
+                Operation.newBuilder()
+                    .setDone(true)
+                    .setResponse(
+                        Any.pack(ExecuteResponse.newBuilder().setResult(actionResult).build()))
+                    .build());
+            responseObserver.onCompleted();
+          }
+        });
+    serviceRegistry.addService(
+        new ContentAddressableStorageImplBase() {
+          @Override
+          public void findMissingBlobs(
+              FindMissingBlobsRequest request,
+              StreamObserver<FindMissingBlobsResponse> responseObserver) {
+            // Nothing is missing.
+            responseObserver.onNext(FindMissingBlobsResponse.getDefaultInstance());
+            responseObserver.onCompleted();
+          }
+        });
+
+    try {
+      client.exec(simpleSpawn, simplePolicy);
+      fail("Expected an exception");
+    } catch (EnvironmentalExecException expected) {
+      assertThat(expected).hasMessageThat().contains("Failed to download from remote cache");
+      Throwable t = expected.getCause();
+      assertThat(t).isInstanceOf(CacheNotFoundException.class);
+      assertThat(((CacheNotFoundException) t).getMissingDigest()).isEqualTo(stdOutDigest);
+    }
+  }
 }