e9c788a9711e8f780de7eb5bd3e1bed7994eb88c
thomasvl <thomasvl@google.com>
2017-09-08 21:44:06 +0200

    Remove swift_library and tests, lives in the apple rules repo now.
    RELNOTES: The swift_library rule that is distributed with Bazel has been deleted. Users who wish to compile Swift should use the rules in https://github.com/bazelbuild/rules_apple instead.
    PiperOrigin-RevId: 168032330

--- examples/swift/constants.swift
+++
@@ -1,4 +0,0 @@
-class Constants {
-  static var x = 2
-  static var y = 3
-}
--- examples/swift/foo.swift
+++
@@ -1,9 +0,0 @@
-import class examples_BarLib.Multiplier
-
-public class Foo {
-  public init() {}
-
-  public func multiply() -> Int {
-    return Multiplier().multiply(a: Constants.x, b: Constants.y)
-  }
-}
