ec91d20a0960ad9d67d27db7c8d1ecdd425d8085
Laszlo Csomor <laszlocsomor@google.com>
2017-09-27 06:17:44 -0400

    Android, Python tools: bugfix in junction.py
    junction.TempJunction now can create all parent
    directories of the junction target if they don't
    exist.
    
    Change-Id: I3e9cf34e78a3eb1ef9415036b791843a3b37f7c1
    PiperOrigin-RevId: 170176180

--- tools/android/junction_test.py
+++ tools/android/junction_test.py
@@ -23,34 +23,47 @@ from tools.android import junction
 class JunctionTest(test_base.TestBase):
   """Unit tests for junction.py."""
 
-  def testCreateJunction(self):
+  def _AssertCreateJunctionWhenTargetsParentsDontExist(self, max_path=None):
 
     def tempdir():
       return self.ScratchDir("junc temp")
 
-    target = self.ScratchDir("junc target")
-    # Make the `target` path a non-normalized Windows path with a space in it.
-    # TempJunction should still work.
-    target = os.path.dirname(target) + "/junc target"
+    target = self.Path("this directory/should not\\yet exist")
+    self.assertFalse(os.path.exists(os.path.dirname(os.path.dirname(target))))
+    # Make the `target` path a non-normalized Windows path with a space in it
+    # which doesn't even exist.
+    # TempJunction should still work; it should:
+    # - normalize the path, and
+    # - create all directories on the path
+    # target = os.path.dirname(target) + "/junc target"
     juncpath = None
-    with junction.TempJunction(target, testonly_mkdtemp=tempdir) as j:
+    with junction.TempJunction(
+        target, testonly_mkdtemp=tempdir, testonly_maxpath=max_path) as j:
       juncpath = j
       # Ensure that `j` created the junction.
       self.assertTrue(os.path.exists(target))
       self.assertTrue(os.path.exists(juncpath))
+      self.assertTrue(juncpath.endswith(os.path.join("junc temp", "j")))
+      self.assertTrue(os.path.isabs(juncpath))
       # Create a file under the junction.
-      filepath = os.path.join(juncpath, "file.txt")
+      filepath = os.path.join(juncpath, "some file.txt")
       with open(filepath, "w") as f:
         f.write("hello")
       # Ensure we can reach the file via the junction and the target directory.
-      self.assertTrue(os.path.exists(os.path.join(target, "file.txt")))
-      self.assertTrue(os.path.exists(os.path.join(juncpath, "file.txt")))
+      self.assertTrue(os.path.exists(os.path.join(target, "some file.txt")))
+      self.assertTrue(os.path.exists(os.path.join(juncpath, "some file.txt")))
     # Ensure that after the `with` block the junction and temp directories no
     # longer exist, but we can still reach the file via the target directory.
-    self.assertTrue(os.path.exists(os.path.join(target, "file.txt")))
-    self.assertFalse(os.path.exists(os.path.join(juncpath, "file.txt")))
+    self.assertTrue(os.path.exists(os.path.join(target, "some file.txt")))
+    self.assertFalse(os.path.exists(os.path.join(juncpath, "some file.txt")))
     self.assertFalse(os.path.exists(juncpath))
     self.assertFalse(os.path.exists(os.path.dirname(juncpath)))
+
+  def testCreateJunctionWhenTargetsParentsDontExistAndPathIsShort(self):
+    self._AssertCreateJunctionWhenTargetsParentsDontExist()
+
+  def testCreateJunctionWhenTargetsParentsDontExistAndPathIsLong(self):
+    self._AssertCreateJunctionWhenTargetsParentsDontExist(1)
 
   def testCannotCreateJunction(self):
 
