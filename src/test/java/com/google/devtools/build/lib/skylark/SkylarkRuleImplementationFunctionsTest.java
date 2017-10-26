bazelbuild/bazel

commit ca77f608e486bf7aa762565d25bf7b9e30f2268c

Extend TemplateExpander to handle $(func param) expansion
Rewrite the Expander to use the new functionality; also rewrite the Skylark
expand_location function to use it.

PiperOrigin-RevId: 173280839

src/test/java/com/google/devtools/build/lib/skylark/SkylarkRuleImplementationFunctionsTest.java
@@ -172,7 +172,7 @@ public class SkylarkRuleImplementationFunctionsTest extends SkylarkTestCase {
       setupSkylarkFunction(line);
       fail();
     } catch (EvalException e) {
-      assertThat(e).hasMessage(errorMsg);
+      assertThat(e).hasMessageThat().isEqualTo(errorMsg);
     }
   }
 

src/main/java/com/google/devtools/build/lib/analysis/ConfigurationMakeVariableContext.java
@@ -105,4 +105,9 @@ public class ConfigurationMakeVariableContext implements TemplateContext {
     }
     return SkylarkDict.<String, String>copyOf(null, map);
   }
+
+  @Override
+  public String lookupFunction(String name, String param) throws ExpansionException {
+    throw new ExpansionException(String.format("$(%s) not defined", name));
+  }
 }

src/main/java/com/google/devtools/build/lib/analysis/Expander.java
@@ -19,6 +19,7 @@ import com.google.common.collect.ImmutableMap;
 import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.analysis.LocationExpander.Options;
 import com.google.devtools.build.lib.analysis.stringtemplate.ExpansionException;
+import com.google.devtools.build.lib.analysis.stringtemplate.TemplateContext;
 import com.google.devtools.build.lib.analysis.stringtemplate.TemplateExpander;
 import com.google.devtools.build.lib.cmdline.Label;
 import com.google.devtools.build.lib.shell.ShellUtils;
@@ -38,37 +39,26 @@ public final class Expander {
   }
 
   private final RuleContext ruleContext;
-  private final ConfigurationMakeVariableContext makeVariableContext;
-  @Nullable private final LocationExpander locationExpander;
+  private final TemplateContext templateContext;
 
-  private Expander(
-      RuleContext ruleContext,
-      ConfigurationMakeVariableContext makeVariableContext,
-      @Nullable LocationExpander locationExpander) {
+  Expander(RuleContext ruleContext, TemplateContext templateContext) {
     this.ruleContext = ruleContext;
-    this.makeVariableContext = makeVariableContext;
-    this.locationExpander = locationExpander;
-  }
-
-  Expander(
-      RuleContext ruleContext,
-      ConfigurationMakeVariableContext makeVariableContext) {
-    this(ruleContext, makeVariableContext, null);
+    this.templateContext = templateContext;
   }
 
   /**
    * Returns a new instance that also expands locations using the default configuration of
-   * {@link LocationExpander}.
+   * {@link LocationTemplateContext}.
    */
   public Expander withLocations(Options... options) {
-    LocationExpander newLocationExpander =
-        new LocationExpander(ruleContext, options);
-    return new Expander(ruleContext, makeVariableContext, newLocationExpander);
+    TemplateContext newTemplateContext =
+        new LocationTemplateContext(templateContext, ruleContext, options);
+    return new Expander(ruleContext, newTemplateContext);
   }
 
   /**
    * Returns a new instance that also expands locations, passing {@link Options#ALLOW_DATA} to the
-   * underlying {@link LocationExpander}.
+   * underlying {@link LocationTemplateContext}.
    */
   public Expander withDataLocations() {
     return withLocations(Options.ALLOW_DATA);
@@ -76,7 +66,7 @@ public final class Expander {
 
   /**
    * Returns a new instance that also expands locations, passing {@link Options#ALLOW_DATA} and
-   * {@link Options#EXEC_PATHS} to the underlying {@link LocationExpander}.
+   * {@link Options#EXEC_PATHS} to the underlying {@link LocationTemplateContext}.
    */
   public Expander withDataExecLocations() {
     return withLocations(Options.ALLOW_DATA, Options.EXEC_PATHS);
@@ -84,12 +74,12 @@ public final class Expander {
 
   /**
    * Returns a new instance that also expands locations, passing the given location map, as well as
-   * {@link Options#EXEC_PATHS} to the underlying {@link LocationExpander}.
+   * {@link Options#EXEC_PATHS} to the underlying {@link LocationTemplateContext}.
    */
   public Expander withExecLocations(ImmutableMap<Label, ImmutableCollection<Artifact>> locations) {
-    LocationExpander newLocationExpander =
-        new LocationExpander(ruleContext, locations, Options.EXEC_PATHS);
-    return new Expander(ruleContext, makeVariableContext, newLocationExpander);
+    TemplateContext newTemplateContext =
+        new LocationTemplateContext(templateContext, ruleContext, locations, Options.EXEC_PATHS);
+    return new Expander(ruleContext, newTemplateContext);
   }
 
   /**
@@ -145,14 +135,15 @@ public final class Expander {
    * @param expression the string to expand.
    * @return the expansion of "expression".
    */
-  public String expand(String attributeName, String expression) {
-    if (locationExpander != null) {
-      expression = locationExpander.expandAttribute(attributeName, expression);
-    }
+  public String expand(@Nullable String attributeName, String expression) {
     try {
-      return TemplateExpander.expand(expression, makeVariableContext);
+      return TemplateExpander.expand(expression, templateContext);
     } catch (ExpansionException e) {
-      ruleContext.attributeError(attributeName, e.getMessage());
+      if (attributeName == null) {
+        ruleContext.ruleError(e.getMessage());
+      } else {
+        ruleContext.attributeError(attributeName, e.getMessage());
+      }
       return expression;
     }
   }
@@ -214,7 +205,7 @@ public final class Expander {
   @Nullable
   public String expandSingleMakeVariable(String attrName, String expression) {
     try {
-      return TemplateExpander.expandSingleVariable(expression, makeVariableContext);
+      return TemplateExpander.expandSingleVariable(expression, templateContext);
     } catch (ExpansionException e) {
       ruleContext.attributeError(attrName, e.getMessage());
       return expression;

src/main/java/com/google/devtools/build/lib/analysis/LocationExpander.java
@@ -60,7 +60,7 @@ import javax.annotation.Nullable;
  *
  * <p>DO NOT USE DIRECTLY! Use RuleContext.getExpander() instead.
  */
-public class LocationExpander {
+final class LocationExpander {
 
   /**
    * List of options to tweak the LocationExpander.
@@ -329,7 +329,7 @@ public class LocationExpander {
    * @param labelMap map of labels to build artifacts
    * @return map of all possible target locations
    */
-  private static Map<Label, Collection<Artifact>> buildLocationMap(
+  static Map<Label, Collection<Artifact>> buildLocationMap(
       RuleContext ruleContext,
       Map<Label, ? extends Collection<Artifact>> labelMap,
       boolean allowDataAttributeEntriesInLabel) {

src/main/java/com/google/devtools/build/lib/analysis/LocationTemplateContext.java
@@ -1,0 +1,111 @@
+// Copyright 2017 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.devtools.build.lib.analysis;
+
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+import com.google.common.collect.ImmutableCollection;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.devtools.build.lib.actions.Artifact;
+import com.google.devtools.build.lib.analysis.LocationExpander.LocationFunction;
+import com.google.devtools.build.lib.analysis.LocationExpander.Options;
+import com.google.devtools.build.lib.analysis.stringtemplate.ExpansionException;
+import com.google.devtools.build.lib.analysis.stringtemplate.TemplateContext;
+import com.google.devtools.build.lib.cmdline.Label;
+import java.util.Collection;
+import java.util.Map;
+import java.util.function.Function;
+import javax.annotation.Nullable;
+
+/**
+ * Expands $(location) and $(locations) tags inside target attributes. You can specify something
+ * like this in the BUILD file:
+ *
+ * <pre>
+ * somerule(name='some name',
+ *          someopt = [ '$(location //mypackage:myhelper)' ],
+ *          ...)
+ * </pre>
+ *
+ * and location will be substituted with //mypackage:myhelper executable output.
+ *
+ * <p>Note that this expander will always expand labels in srcs, deps, and tools attributes, with
+ * data being optional.
+ *
+ * <p>DO NOT USE DIRECTLY! Use RuleContext.getExpander() instead.
+ */
+final class LocationTemplateContext implements TemplateContext {
+  private final TemplateContext delegate;
+  private final Function<String, String> locationFunction;
+  private final Function<String, String> locationsFunction;
+
+  private LocationTemplateContext(
+      TemplateContext delegate,
+      Label root,
+      Supplier<Map<Label, Collection<Artifact>>> locationMap,
+      boolean execPaths) {
+    this.delegate = delegate;
+    this.locationFunction = new LocationFunction(root, locationMap, execPaths, false);
+    this.locationsFunction = new LocationFunction(root, locationMap, execPaths, true);
+  }
+
+  private LocationTemplateContext(
+      TemplateContext delegate,
+      RuleContext ruleContext,
+      @Nullable ImmutableMap<Label, ImmutableCollection<Artifact>> labelMap,
+      ImmutableSet<Options> options) {
+    this(
+        delegate,
+        ruleContext.getLabel(),
+        // Use a memoizing supplier to avoid eagerly building the location map.
+        Suppliers.memoize(
+            () -> LocationExpander.buildLocationMap(
+                ruleContext, labelMap, options.contains(Options.ALLOW_DATA))),
+        options.contains(Options.EXEC_PATHS));
+  }
+
+  public LocationTemplateContext(
+      TemplateContext delegate,
+      RuleContext ruleContext,
+      @Nullable ImmutableMap<Label, ImmutableCollection<Artifact>> labelMap,
+      LocationExpander.Options... options) {
+    this(delegate, ruleContext, labelMap, ImmutableSet.copyOf(options));
+  }
+
+  public LocationTemplateContext(
+      TemplateContext delegate, RuleContext ruleContext, LocationExpander.Options... options) {
+    this(delegate, ruleContext, null, ImmutableSet.copyOf(options));
+  }
+
+  @Override
+  public String lookupVariable(String name) throws ExpansionException {
+    return delegate.lookupVariable(name);
+  }
+
+  @Override
+  public String lookupFunction(String name, String param) throws ExpansionException {
+    try {
+      if ("location".equals(name)) {
+        return locationFunction.apply(param);
+      } else if ("locations".equals(name)) {
+        return locationsFunction.apply(param);
+      }
+    } catch (IllegalStateException e) {
+      throw new ExpansionException(e.getMessage(), e);
+    }
+    return delegate.lookupFunction(name, param);
+  }
+}

src/main/java/com/google/devtools/build/lib/analysis/RuleContext.java
@@ -48,6 +48,7 @@ import com.google.devtools.build.lib.analysis.config.FragmentCollection;
 import com.google.devtools.build.lib.analysis.config.PatchTransition;
 import com.google.devtools.build.lib.analysis.configuredtargets.RuleConfiguredTarget.Mode;
 import com.google.devtools.build.lib.analysis.fileset.FilesetProvider;
+import com.google.devtools.build.lib.analysis.stringtemplate.TemplateContext;
 import com.google.devtools.build.lib.cmdline.Label;
 import com.google.devtools.build.lib.cmdline.RepositoryName;
 import com.google.devtools.build.lib.collect.ImmutableSortedKeyListMultimap;
@@ -954,8 +955,8 @@ public final class RuleContext extends TargetContext
     initConfigurationMakeVariableContext(ImmutableList.copyOf(makeVariableSuppliers));
   }
 
-  public Expander getExpander(ConfigurationMakeVariableContext makeVariableContext) {
-    return new Expander(this, makeVariableContext);
+  public Expander getExpander(TemplateContext templateContext) {
+    return new Expander(this, templateContext);
   }
 
   public Expander getExpander() {

src/main/java/com/google/devtools/build/lib/analysis/skylark/SkylarkRuleImplementationFunctions.java
@@ -20,11 +20,12 @@ import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.analysis.AliasProvider;
 import com.google.devtools.build.lib.analysis.CommandHelper;
 import com.google.devtools.build.lib.analysis.FileProvider;
-import com.google.devtools.build.lib.analysis.LocationExpander;
 import com.google.devtools.build.lib.analysis.Runfiles;
 import com.google.devtools.build.lib.analysis.RunfilesProvider;
 import com.google.devtools.build.lib.analysis.TransitiveInfoCollection;
 import com.google.devtools.build.lib.analysis.configuredtargets.AbstractConfiguredTarget;
+import com.google.devtools.build.lib.analysis.stringtemplate.ExpansionException;
+import com.google.devtools.build.lib.analysis.stringtemplate.TemplateContext;
 import com.google.devtools.build.lib.cmdline.Label;
 import com.google.devtools.build.lib.events.Location;
 import com.google.devtools.build.lib.skylarkinterface.Param;
@@ -318,16 +319,34 @@ public class SkylarkRuleImplementationFunctions {
             throws EvalException {
           ctx.checkMutable("expand_location");
           try {
-            return new LocationExpander(
-                    ctx.getRuleContext(),
-                    makeLabelMap(targets.getContents(TransitiveInfoCollection.class, "targets")),
-                    LocationExpander.Options.EXEC_PATHS)
-                .expand(input);
+            return ctx
+                .getRuleContext()
+                .getExpander(new FakeTemplateContext())
+                .withExecLocations(
+                    makeLabelMap(targets.getContents(TransitiveInfoCollection.class, "targets")))
+                .expand(/*attributeName=*/null, input);
           } catch (IllegalStateException ise) {
             throw new EvalException(loc, ise);
           }
         }
       };
+
+  /**
+   * This class only exists for backwards compatibility. Previously, we were using LocationExpander
+   * directly, which passes through all unrecognized $() expressions.
+   */
+  private static final class FakeTemplateContext implements TemplateContext {
+    @Override
+    public String lookupVariable(String name) throws ExpansionException {
+      return String.format("$$(%s)", name);
+    }
+
+    @Override
+    public String lookupFunction(String name, String param) throws ExpansionException {
+      // Variables get recursively expanded, functions don't.
+      return String.format("$(%s %s)", name, param);
+    }
+  }
 
   /**
    * Builds a map: Label -> List of files from the given labels

src/main/java/com/google/devtools/build/lib/analysis/stringtemplate/ExpansionException.java
@@ -21,4 +21,8 @@ public class ExpansionException extends Exception {
   public ExpansionException(String message) {
     super(message);
   }
+
+  public ExpansionException(String message, Throwable cause) {
+    super(message, cause);
+  }
 }

commit 4d6dbbbe4010315096ffa7986f94604f35d8672b

Add a regression test for expand_location
I accidentally broke expand_location in https://github.com/bazelbuild/bazel/commit/ca77f608e486bf7aa762565d25bf7b9e30f2268c, so here's a test that
would have prevented that.

PiperOrigin-RevId: 173513845

src/test/java/com/google/devtools/build/lib/skylark/SkylarkRuleImplementationFunctionsTest.java
@@ -507,6 +507,16 @@ public class SkylarkRuleImplementationFunctionsTest extends SkylarkTestCase {
         "ruleContext.expand_location('$(locations :abc)')");
   }
 
+  /** Regression test to check that expand_location allows ${var} and $$. */
+  @Test
+  public void testExpandLocationWithDollarSignsAndCurlys() throws Exception {
+    SkylarkRuleContext ruleContext = createRuleContext("//foo:bar");
+    assertThat((String)
+        evalRuleContextCode(
+            ruleContext, "ruleContext.expand_location('${abc} $(echo) $$ $')"))
+        .isEqualTo("${abc} $(echo) $$ $");
+  }
+
   /**
    * Invokes ctx.expand_location() with the given parameters and checks whether this led to the
    * expected result