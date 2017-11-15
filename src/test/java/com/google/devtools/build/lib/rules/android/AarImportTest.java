fa666634a15890fc90f02b4bab8b17c936ab8cb6
ajmichael <ajmichael@google.com>
2017-11-15 12:16:19 -0800

    Actually run AarImportTest ?
    Also, fix it to not rely on the Android split transition being disabled.
    
    RELNOTES: None
    PiperOrigin-RevId: 175861670

--- src/test/java/com/google/devtools/build/lib/rules/android/AarImportTest.java
+++ src/test/java/com/google/devtools/build/lib/rules/android/AarImportTest.java
@@ -255,16 +255,22 @@ public class AarImportTest extends BuildViewTestCase {
   public void testExportsManifest() throws Exception {
     Artifact binaryMergedManifest =
         getConfiguredTarget("//java:app").getProvider(ApkProvider.class).getMergedManifest();
-    assertThat(getGeneratingAction(binaryMergedManifest).getInputs())
+    // Compare root relative path strings instead of artifacts due to difference in configuration
+    // caused by the Android split transition.
+    assertThat(
+        Iterables.transform(
+            getGeneratingAction(binaryMergedManifest).getInputs(),
+            Artifact::getRootRelativePathString))
         .containsAllOf(getAndroidManifest("//a:foo"), getAndroidManifest("//a:bar"));
   }
 
-  private Artifact getAndroidManifest(String aarImport) throws Exception {
+  private String getAndroidManifest(String aarImport) throws Exception {
     return getConfiguredTarget(aarImport)
         .getProvider(AndroidResourcesProvider.class)
         .getDirectAndroidResources()
         .toList()
         .get(0)
-        .getManifest();
+        .getManifest()
+        .getRootRelativePathString();
   }
 }
