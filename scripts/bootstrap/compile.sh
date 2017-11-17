41584252323b8f4410b6aecb29db68cb7b6a2de8
Jakob Buchgraber <buchgr@google.com>
2017-11-17 08:52:24 -0800

    third_party/grpc: Update to version 1.7.0
    Change-Id: I9ae0c56844db45e28eb5a08e8180514aa2729fdf
    PiperOrigin-RevId: 176114077

--- scripts/bootstrap/compile.sh
+++ scripts/bootstrap/compile.sh
@@ -18,7 +18,7 @@
 
 PROTO_FILES=$(ls src/main/protobuf/*.proto src/main/java/com/google/devtools/build/lib/buildeventstream/proto/*.proto)
 LIBRARY_JARS=$(find third_party -name '*.jar' | grep -Fv /javac-9-dev-r3297-4.jar | grep -Fv /javac-9-dev-4023-3.jar | grep -Fv /javac7.jar | grep -Fv JavaBuilder | grep -Fv third_party/guava | grep -Fv third_party/guava | grep -ve third_party/grpc/grpc.*jar | tr "\n" " ")
-GRPC_JAVA_VERSION=1.6.1
+GRPC_JAVA_VERSION=1.7.0
 GRPC_LIBRARY_JARS=$(find third_party/grpc -name '*.jar' | grep -e .*${GRPC_JAVA_VERSION}.*jar | tr "\n" " ")
 GUAVA_VERSIONE=23.1
 GUAVA_JARS=$(find third_party/guava -name '*.jar' | grep -e .*${GUAVA_VERSION}.*jar | tr "\n" " ")
