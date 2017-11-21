7f72af113960386006598150e880a79b5f0bc2e1
Klaus Aehlig <aehlig@google.com>
2017-11-21 08:22:32 -0800

    Fix //src/test/shell/integration:progress_reporting_test
    The test case test_failed_actions_with_keep_going was never correct
    and only worked by accident: it relied on bazel starting at least
    two actions, if for there are at least two where all the dependencies
    are already met; however bazel is clever and analyzes the resources
    of the system it is running in, and if they are small will only run one
    genrule at a time. Fix this by telling it to run 2 actions and lying
    about the available resources of the local system.
    
    Also increase the test size and reenable the test for remote execution.
    Fixes #3995.
    
    Change-Id: Ic84de17a98a2e74c24901d4efb38d7631de4690a
    PiperOrigin-RevId: 176511026

--- src/test/shell/integration/progress_reporting_test.sh
+++ src/test/shell/integration/progress_reporting_test.sh
@@ -273,7 +273,15 @@ genrule(
 )
 EOF
 
-  bazel build -k -s "//${pkg}:"{top,longrun} --progress_report_interval=1 \
+  # The whole test setup relies on bazel running the actions for
+  # :dep and :longrun in parallel. However, bazel normally analyzes
+  # the environment and bases the action scheduling on this; so we
+  # need to tell bazel to (maybe even contrafactually) believe that
+  # there are enough local resources for two genrules to run in parallel.
+  # Give enough head room so that the test won't break again if we tweak
+  # our assumptions about local resource usage.
+  bazel build -j 2 --local_resources=2048000,32,32 \
+       -k -s "//${pkg}:"{top,longrun} --progress_report_interval=1 \
        >& "$TEST_log" && fail "build succeeded"
   expect_log "\[3 / 4\] Still waiting for 1 job to complete:"
   expect_log "^ *Executing genrule //${pkg}:longrun"
