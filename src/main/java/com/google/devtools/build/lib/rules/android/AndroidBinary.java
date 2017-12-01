dec03295e7e65c6a86fb60f02358c98cf499eefb
ajmichael <ajmichael@google.com>
2017-12-01 14:14:47 -0800

    Remove duplicated method from AndroidBinary.
    RELNOTES: None
    PiperOrigin-RevId: 177638999

--- src/main/java/com/google/devtools/build/lib/rules/android/AndroidBinary.java
+++ src/main/java/com/google/devtools/build/lib/rules/android/AndroidBinary.java
@@ -689,11 +689,6 @@ public abstract class AndroidBinary implements RuleConfiguredTargetFactory {
     return deployJar;
   }
 
-  private static JavaOptimizationMode getJavaOptimizationMode(RuleContext ruleContext) {
-    return ruleContext.getConfiguration().getFragment(JavaConfiguration.class)
-        .getJavaOptimizationMode();
-  }
-
   /**
    * Applies the proguard specifications, and creates a ProguardedJar. Proguard's output artifacts
    * are added to the given {@code filesBuilder}.
@@ -770,7 +765,7 @@ public abstract class AndroidBinary implements RuleConfiguredTargetFactory {
             semantics,
             proguardOutputMap);
     outputs.addAllToSet(failures);
-    JavaOptimizationMode optMode = getJavaOptimizationMode(ruleContext);
+    JavaOptimizationMode optMode = ProguardHelper.getJavaOptimizationMode(ruleContext);
     ruleContext.registerAction(
         new FailAction(
             ruleContext.getActionOwner(),
