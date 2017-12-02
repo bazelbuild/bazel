d926bc40260549b997a6a5a1e82d9e7999dbb65e
Akira Baruah <akira.baruah@gmail.com>
2017-12-02 02:00:49 -0800

    Spoof gflags pseudo-package __name__ for backwards compatibility.
    The core gflags package is `third_party.py.gflags.gflags`, but many downstream
    projects and tools use the pseudo-package `third_party.py.gflags`. gflags raises
    DuplicateFlagError when defining default flags from packages with different
    names, so this fix adjusts the pseudo-package's __name__ attribute to match that
    of the core package.

--- third_party/py/gflags/__init__.py
+++ third_party/py/gflags/__init__.py
@@ -1,1 +1,6 @@
+# gflags raises DuplicateFlagError when defining default flags from packages
+# with different names, so this pseudo-package must mimic the core gflags
+# package name.
+__name__ += ".gflags"  # i.e. "third_party.py.gflags.gflags"
+
 from gflags import *
