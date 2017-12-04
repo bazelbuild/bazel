bazelbuild/bazel

commit e8d32b7c922f65539b74357711d5ad6b70934115

Switch ExtraActionFactory and GenRuleBase to the Expander API
This changes the order in which expansions happen, which could change the
semantics in subtle ways.

PiperOrigin-RevId: 174518778

src/main/java/com/google/devtools/build/lib/analysis/CommandHelper.java
@@ -159,23 +159,10 @@ public final class CommandHelper {
   }
 
   /**
-   * Resolves a command, and expands known locations for $(location) variables. This method supports
-   * legacy heuristic label expansion, which replaces strings that look like labels with their
-   * corresponding file names. Use {@link #resolveCommandAndExpandLabels} instead.
-   */
-  public String resolveCommandAndHeuristicallyExpandLabels(
-      String command, @Nullable String attribute, boolean enableLegacyHeuristicLabelExpansion) {
-    command = resolveCommandAndExpandLabels(command, attribute, false);
-    if (enableLegacyHeuristicLabelExpansion) {
-      command = expandLabels(command, labelMap);
-    }
-    return command;
-  }
-
-  /**
    * Resolves a command, and expands known locations for $(location)
    * variables.
    */
+  @Deprecated // Only exists to support a legacy Skylark API.
   public String resolveCommandAndExpandLabels(
       String command, @Nullable String attribute, boolean allowDataInLabel) {
     LocationExpander expander;
@@ -201,7 +188,7 @@ public final class CommandHelper {
    * <p>If the expansion fails, an attribute error is reported and the original
    * expression is returned.
    */
-  private <T extends Iterable<Artifact>> String expandLabels(String expr, Map<Label, T> labelMap) {
+  public String expandLabelsHeuristically(String expr) {
     try {
       return LabelExpander.expand(expr, labelMap, ruleContext.getLabel());
     } catch (LabelExpander.NotUniqueExpansionException nuee) {

src/main/java/com/google/devtools/build/lib/rules/genrule/GenRuleBase.java
@@ -82,18 +82,6 @@ public abstract class GenRuleBase implements RuleConfiguredTargetFactory {
   }
 
   /**
-   * Returns an {@link Iterable} of {@link NestedSet}s, which will be added to the genrule's inputs
-   * using the {@link NestedSetBuilder#addTransitive} method.
-   *
-   * <p>GenRule implementations can override this method to better control what inputs are needed
-   * for specific command inputs.
-   */
-  protected Iterable<NestedSet<Artifact>> getExtraInputArtifacts(
-      RuleContext ruleContext, String command) {
-    return ImmutableList.of();
-  }
-
-  /**
    * Returns {@code true} if the rule should be stamped.
    *
    * <p>Genrule implementations can set this based on the rule context, including by defining their
@@ -153,17 +141,22 @@ public abstract class GenRuleBase implements RuleConfiguredTargetFactory {
       return null;
     }
 
-    String baseCommand = commandHelper.resolveCommandAndHeuristicallyExpandLabels(
-        ruleContext.attributes().get("cmd", Type.STRING),
-        "cmd",
-        ruleContext.attributes().get("heuristic_label_expansion", Type.BOOLEAN));
+    String baseCommand = ruleContext.attributes().get("cmd", Type.STRING);
+    // Expand template variables and functions.
+    String command = ruleContext
+        .getExpander(new CommandResolverContext(ruleContext, resolvedSrcs, filesToBuild))
+        .withExecLocations()
+        .expand("cmd", baseCommand);
 
-    // Adds the genrule environment setup script before the actual shell command
-    String command = String.format("source %s; %s",
+    // Heuristically expand things that look like labels.
+    if (ruleContext.attributes().get("heuristic_label_expansion", Type.BOOLEAN)) {
+      command = commandHelper.expandLabelsHeuristically(command);
+    }
+
+    // Add the genrule environment setup script before the actual shell command.
+    command = String.format("source %s; %s",
         ruleContext.getPrerequisiteArtifact("$genrule_setup", Mode.HOST).getExecPath(),
-        baseCommand);
-
-    command = resolveCommand(command, ruleContext, resolvedSrcs, filesToBuild);
+        command);
 
     String messageAttr = ruleContext.attributes().get("message", Type.STRING);
     String message = messageAttr.isEmpty() ? "Executing genrule" : messageAttr;
@@ -205,10 +198,6 @@ public abstract class GenRuleBase implements RuleConfiguredTargetFactory {
       // If javac is used, silently throw in the jdk filegroup as a dependency.
       // Note we expand Java-related variables with the *host* configuration.
       inputs.addTransitive(JavaHelper.getHostJavabaseInputs(ruleContext));
-    }
-
-    for (NestedSet<Artifact> extraInputs : getExtraInputArtifacts(ruleContext, baseCommand)) {
-      inputs.addTransitive(extraInputs);
     }
 
     if (isStampingEnabled(ruleContext)) {
@@ -262,19 +251,6 @@ public abstract class GenRuleBase implements RuleConfiguredTargetFactory {
       return Iterables.getOnlyElement(filesToBuild);
     }
     return null;
-  }
-
-  /**
-   * Resolves any variables, including make and genrule-specific variables, in the command and
-   * returns the expanded command.
-   *
-   * <p>GenRule implementations may override this method to perform additional expansions.
-   */
-  protected String resolveCommand(String command, final RuleContext ruleContext,
-      final NestedSet<Artifact> resolvedSrcs, final NestedSet<Artifact> filesToBuild) {
-    return ruleContext
-        .getExpander(new CommandResolverContext(ruleContext, resolvedSrcs, filesToBuild))
-        .expand("cmd", command);
   }
 
   /**

src/test/java/com/google/devtools/build/lib/bazel/rules/genrule/GenRuleCommandSubstitutionTest.java
@@ -93,7 +93,7 @@ public class GenRuleCommandSubstitutionTest extends BuildViewTestCase {
     eventCollector.clear();
 
     genrule("$(location foo bar");
-    assertExpansionFails("unterminated $(location) expression", "//test");
+    assertExpansionFails("unterminated variable reference", "//test");
 
     genrule("$(location");
     assertExpansionFails("unterminated variable reference", "//test");
@@ -221,7 +221,7 @@ public class GenRuleCommandSubstitutionTest extends BuildViewTestCase {
     eventCollector.clear();
 
     genrule("$(locations foo bar");
-    assertExpansionFails("unterminated $(locations) expression", "//test");
+    assertExpansionFails("unterminated variable reference", "//test");
 
     genrule("$(locations");
     assertExpansionFails("unterminated variable reference", "//test");

src/main/java/com/google/devtools/build/lib/analysis/Expander.java
@@ -73,6 +73,14 @@ public final class Expander {
   }
 
   /**
+   * Returns a new instance that also expands locations, passing {@link Options#EXEC_PATHS} to the
+   * underlying {@link LocationTemplateContext}.
+   */
+  public Expander withExecLocations() {
+    return withLocations(Options.EXEC_PATHS);
+  }
+
+  /**
    * Returns a new instance that also expands locations, passing the given location map, as well as
    * {@link Options#EXEC_PATHS} to the underlying {@link LocationTemplateContext}.
    */

src/main/java/com/google/devtools/build/lib/rules/extra/ExtraActionFactory.java
@@ -54,8 +54,7 @@ public final class ExtraActionFactory implements RuleConfiguredTargetFactory {
     List<String>outputTemplates =
         context.attributes().get("out_templates", Type.STRING_LIST);
 
-    String command = commandHelper.resolveCommandAndExpandLabels(
-        context.attributes().get("cmd", Type.STRING), "cmd", /*allowDataInLabel=*/true);
+    String command = context.attributes().get("cmd", Type.STRING);
     // This is a bit of a hack. We want to run the MakeVariableExpander first, so we expand $ on
     // variables that are expanded below with $$, which gets reverted to $ by the
     // MakeVariableExpander. This allows us to expand package-specific make variables in the
@@ -67,7 +66,10 @@ public final class ExtraActionFactory implements RuleConfiguredTargetFactory {
     command = command.replace("$(output ", "$$(output ");
     ConfigurationMakeVariableContext makeVariableContext = new ConfigurationMakeVariableContext(
         context, context.getTarget().getPackage(), context.getConfiguration());
-    command = context.getExpander(makeVariableContext).expand("cmd", command);
+    command = context
+        .getExpander(makeVariableContext)
+        .withDataExecLocations()
+        .expand("cmd", command);
 
     boolean requiresActionOutput =
         context.attributes().get("requires_action_output", Type.BOOLEAN);

commit 515ba8967aeb32be50783bf6c60f0de6e6bdba8d

Switch GenRuleBase to use the new Expander API
This is a roll-forward of https://github.com/bazelbuild/bazel/commit/e8d32b7c922f65539b74357711d5ad6b70934115, which broke some genrules, but without
some cleanup changes which I'm submitting separately, and with a fix for the
bug.

The problem was that I switched from withExecLocations(labels) to
withExecLocations(); the original code was in CommandHelper, and the new
code in GenRuleBase, so this was not obvious.

Also, we didn't have test coverage for this case - note that the specified
labels are _added_ to the default map of labels, rather than replacing the
default map of labels. This only matters if the dependent rule provides a
GenRuleSourcesProvider, which only a single (Google-internal) rule does.

PiperOrigin-RevId: 177802902

src/main/java/com/google/devtools/build/lib/analysis/CommandHelper.java
@@ -144,6 +144,10 @@ public final class CommandHelper {
     return toolsRunfilesSuppliers;
   }
 
+  public ImmutableMap<Label, ImmutableCollection<Artifact>> getLabelMap() {
+    return labelMap;
+  }
+
   // Returns the value in the specified corresponding to 'key', creating and
   // inserting an empty container if absent.  We use Map not Multimap because
   // we need to distinguish the cases of "empty value" and "absent key".
@@ -159,23 +163,10 @@ public final class CommandHelper {
   }
 
   /**
-   * Resolves a command, and expands known locations for $(location) variables. This method supports
-   * legacy heuristic label expansion, which replaces strings that look like labels with their
-   * corresponding file names. Use {@link #resolveCommandAndExpandLabels} instead.
-   */
-  public String resolveCommandAndHeuristicallyExpandLabels(
-      String command, @Nullable String attribute, boolean enableLegacyHeuristicLabelExpansion) {
-    command = resolveCommandAndExpandLabels(command, attribute, false);
-    if (enableLegacyHeuristicLabelExpansion) {
-      command = expandLabels(command, labelMap);
-    }
-    return command;
-  }
-
-  /**
    * Resolves a command, and expands known locations for $(location)
    * variables.
    */
+  @Deprecated // Only exists to support a legacy Skylark API.
   public String resolveCommandAndExpandLabels(
       String command, @Nullable String attribute, boolean allowDataInLabel) {
     LocationExpander expander;
@@ -200,7 +191,7 @@ public final class CommandHelper {
    * <p>If the expansion fails, an attribute error is reported and the original
    * expression is returned.
    */
-  private <T extends Iterable<Artifact>> String expandLabels(String expr, Map<Label, T> labelMap) {
+  public String expandLabelsHeuristically(String expr) {
     try {
       return LabelExpander.expand(expr, labelMap, ruleContext.getLabel());
     } catch (LabelExpander.NotUniqueExpansionException nuee) {

src/main/java/com/google/devtools/build/lib/rules/genrule/GenRuleBase.java
@@ -141,17 +141,22 @@ public abstract class GenRuleBase implements RuleConfiguredTargetFactory {
       return null;
     }
 
-    String baseCommand = commandHelper.resolveCommandAndHeuristicallyExpandLabels(
-        ruleContext.attributes().get("cmd", Type.STRING),
-        "cmd",
-        ruleContext.attributes().get("heuristic_label_expansion", Type.BOOLEAN));
+    String baseCommand = ruleContext.attributes().get("cmd", Type.STRING);
+    // Expand template variables and functions.
+    String command = ruleContext
+        .getExpander(new CommandResolverContext(ruleContext, resolvedSrcs, filesToBuild))
+        .withExecLocations(commandHelper.getLabelMap())
+        .expand("cmd", baseCommand);
 
-    // Adds the genrule environment setup script before the actual shell command
-    String command = String.format("source %s; %s",
+    // Heuristically expand things that look like labels.
+    if (ruleContext.attributes().get("heuristic_label_expansion", Type.BOOLEAN)) {
+      command = commandHelper.expandLabelsHeuristically(command);
+    }
+
+    // Add the genrule environment setup script before the actual shell command.
+    command = String.format("source %s; %s",
         ruleContext.getPrerequisiteArtifact("$genrule_setup", Mode.HOST).getExecPath(),
-        baseCommand);
-
-    command = resolveCommand(command, ruleContext, resolvedSrcs, filesToBuild);
+        command);
 
     String messageAttr = ruleContext.attributes().get("message", Type.STRING);
     String message = messageAttr.isEmpty() ? "Executing genrule" : messageAttr;
@@ -246,19 +251,6 @@ public abstract class GenRuleBase implements RuleConfiguredTargetFactory {
       return Iterables.getOnlyElement(filesToBuild);
     }
     return null;
-  }
-
-  /**
-   * Resolves any variables, including make and genrule-specific variables, in the command and
-   * returns the expanded command.
-   *
-   * <p>GenRule implementations may override this method to perform additional expansions.
-   */
-  protected String resolveCommand(String command, final RuleContext ruleContext,
-      final NestedSet<Artifact> resolvedSrcs, final NestedSet<Artifact> filesToBuild) {
-    return ruleContext
-        .getExpander(new CommandResolverContext(ruleContext, resolvedSrcs, filesToBuild))
-        .expand("cmd", command);
   }
 
   /**

src/test/java/com/google/devtools/build/lib/bazel/rules/genrule/GenRuleCommandSubstitutionTest.java
@@ -93,7 +93,7 @@ public class GenRuleCommandSubstitutionTest extends BuildViewTestCase {
     eventCollector.clear();
 
     genrule("$(location foo bar");
-    assertExpansionFails("unterminated $(location) expression", "//test");
+    assertExpansionFails("unterminated variable reference", "//test");
 
     genrule("$(location");
     assertExpansionFails("unterminated variable reference", "//test");
@@ -221,7 +221,7 @@ public class GenRuleCommandSubstitutionTest extends BuildViewTestCase {
     eventCollector.clear();
 
     genrule("$(locations foo bar");
-    assertExpansionFails("unterminated $(locations) expression", "//test");
+    assertExpansionFails("unterminated variable reference", "//test");
 
     genrule("$(locations");
     assertExpansionFails("unterminated variable reference", "//test");