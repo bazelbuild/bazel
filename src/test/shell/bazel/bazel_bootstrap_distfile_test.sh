1664fce92fd4aa9776b939dd470ed743b512aad9
Klaus Aehlig <aehlig@google.com>
2017-12-15 06:38:59 -0800

    bazel_bootstrap_distfile_test: run a more simple setup
    The bootstrap-distfile test aims at verifying the completeness of
    the distribution archive. The functionality of workers is a different
    issue, so separate those concerns and do the test bootstrap in a
    simple setup, calling javac standalone (see #1250). Also, reduce
    the output to make the test log more readable.
    
    Improves on #4306.
    
    Change-Id: I839523628cd35f2deefc1a4dfdf0cf58a4d1f51a
    PiperOrigin-RevId: 179186615

--- src/test/shell/bazel/bazel_bootstrap_distfile_test.sh
+++ src/test/shell/bazel/bazel_bootstrap_distfile_test.sh
@@ -39,7 +39,8 @@ function test_bootstrap()  {
     export SOURCE_DATE_EPOCH=1501234567
     unzip -q "${DISTFILE}"
     find . -type f -exec chmod u+w {} \;
-    ./compile.sh || fail "Expected to be able to bootstrap bazel"
+    env EXTRA_BAZZEL_ARGS"--curses=no --strategy=Javac=standalone" ./compile.sh \
+        || fail "Expected to be able to bootstrap bazel"
     ./output/bazel version > "${TEST_log}" || fail "Generated bazel not working"
     ./output/bazel shutdown
     expect_log "${SOURCE_DATE_EPOCH}"
