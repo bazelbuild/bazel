84bf5dbebc002ac30159149df97b8206649e9449
dannark <dannark@google.com>
2017-12-21 11:36:59 -0800

    Remove package name from license.
    RELNOTES: None
    PiperOrigin-RevId: 179838832

--- src/test/java/com/google/devtools/build/lib/rules/android/AndroidLocalTestTest.java
+++ src/test/java/com/google/devtools/build/lib/rules/android/AndroidLocalTestTest.java
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.package com.google.devtools.build.lib.rules.android;
+// limitations under the License.
 package com.google.devtools.build.lib.rules.android;
 
 import static com.google.common.truth.Truth.assertThat;
