a4ac4a154da69927731f9d432913978f6e807a55
Tony Liang <tony.liang@rubrik.com>
2018-01-11 09:59:22 -0800

    Concatenate reference config files in singlejar
    Merge reference.conf files in single jar since [akka will break](https://doc.akka.io/docs/akka/current/general/configuration.html#when-using-jarjar-onejar-assembly-or-any-jar-bundler) if reference.conf files are overwritten.
    The feature is added to both java and cpp implementations of singlejar to concatenate reference.conf files to keep behaviors consistent. For the cpp implementation, using `OutputJar.ExtraCombiner` in singlejar_main.cc feels less intrusive than adding a new `Concatenator` in the output_jar code.
    
    Bazel-dev discussion on the issue is [here](https://groups.google.com/forum/#!topic/bazel-dev/C30dwAPL708).
    
    Closes #4413.
    
    PiperOrigin-RevId: 181622277

--- src/tools/singlejar/singlejar_main.cc
+++ src/tools/singlejar/singlejar_main.cc
@@ -28,5 +28,7 @@ int main(int argc, char *argv[]) {
   } else {
     output_jar.ExtraCombiner("META-INF/desugar_deps", new NullCombiner());
   }
+  output_jar.ExtraCombiner("reference.conf",
+                           new Concatenator("reference.conf"));
   return output_jar.Doit(&options);
 }
