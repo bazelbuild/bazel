40d7f49b3e0320bd37b1ea4dcae7022a273f732e
Laszlo Csomor <laszlocsomor@google.com>
2018-01-23 02:24:53 -0800

    bazel_bootstrap_distfile_test: don't find & chmod
    Do not run "find & chmod" on the extracted files,
    because:
    (a) it's unnecessary, we can delete them in the
        trap statement just fine, and
    (b) it takes an impressive 20 minutes on Windows,
        probably because process creation is a lot
        slower on Windows than on Unixes and each
        `chmod` runs as a subprocess of `find`
    
    See https://github.com/bazelbuild/bazel/issues/4503
    
    Change-Id: I47d97f00b875716997c197a51602fb6ea7728109
    PiperOrigin-RevId: 182905086

--- src/test/shell/bazel/bazel_bootstrap_distfile_test.sh
+++ src/test/shell/bazel/bazel_bootstrap_distfile_test.sh
@@ -38,7 +38,6 @@ function test_bootstrap()  {
     cd "${WRKDIR}" || fail "Could not change to work directory"
     export SOURCE_DATE_EPOCH=1501234567
     unzip -q "${DISTFILE}"
-    find . -type f -exec chmod u+w {} \;
     env EXTRA_BAZEL_ARGS="--curses=no --strategy=Javac=standalone" ./compile.sh \
         || fail "Expected to be able to bootstrap bazel"
     ./output/bazel version > "${TEST_log}" || fail "Generated bazel not working"
