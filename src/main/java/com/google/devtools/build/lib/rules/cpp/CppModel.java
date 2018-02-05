03ad9155f765fcdc0ccf4aae30a4def9914ea0b4
jmmv <jmmv@google.com>
2018-02-05 07:55:56 -0800

    Automated rollback of commit 17830c25a816d67ba79e783acaa6a085b516bd43.
    *** Reason for rollback ***
    
    Causes Blaze to crash when building Exoblaze as shown in b/72936965.
    Confirmed as root cause by rolling back this CL, building a Blaze
    from HEAD, and successfully using it to build Exoblaze.
    
    *** Original change description ***
    
    C++: Remove last instatiation of CppModel outside CcLibraryHelper.
    
    RELNOTES:none
    PiperOrigin-RevId: 184528551

--- src/main/java/com/google/devtools/build/lib/rules/cpp/CppModel.java
+++ src/main/java/com/google/devtools/build/lib/rules/cpp/CppModel.java
@@ -185,6 +185,7 @@ public final class CppModel {
   private final Predicate<String> coptsFilter;
   private boolean fake;
   private boolean maySaveTemps;
+  private boolean onlySingleOutput;
   private CcCompilationOutputs compilationOutputs;
 
   // link model
@@ -271,6 +272,17 @@ public final class CppModel {
    */
   public CppModel setFake(boolean fake) {
     this.fake = fake;
+    return this;
+  }
+
+  /**
+   * If set, the CppModel only creates a single .o output that can be linked into a dynamic library,
+   * i.e., it never generates both PIC and non-PIC outputs. Otherwise it creates outputs that can be
+   * linked into both static binaries and dynamic libraries (if both require PIC or both require
+   * non-PIC, then it still only creates a single output). Defaults to false.
+   */
+  public CppModel setOnlySingleOutput(boolean onlySingleOutput) {
+    this.onlySingleOutput = onlySingleOutput;
     return this;
   }
 
@@ -466,7 +478,7 @@ public final class CppModel {
 
     // Either you're only making a dynamic library (onlySingleOutput) or pic should be used
     // in all cases.
-    if (usePicForBinaries) {
+    if (onlySingleOutput || usePicForBinaries) {
       if (picFeatureEnabled) {
         return false;
       }
