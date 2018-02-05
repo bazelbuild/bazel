3f268bf9d151107d014b1757192a8f299889eb46
Peter Mounce <petermounce@improbable.io>
2018-02-05 17:58:11 +0000

    iterate a bit more...

--- scripts/packages/chocolatey/build.sh
+++ scripts/packages/chocolatey/build.sh
@@ -4,12 +4,13 @@ set -eux -o pipefail
 
 # Empty string is valid for package fix version
 PACKAGE_FIX_VERSION="${PACKAGE_FIX_VERSION:-}"
-if [ -z "${VERSION+x}" ]; then echo "VERSION is unset" && exit 1; else echo "VERSION is set."; fi
-if [ -z "${BAZEL_RELEASE_NOTES_URL+x}" ]; then echo "BAZEL_RELEASE_NOTES_URL is unset" && exit 1; else echo "BAZEL_RELEASE_NOTES_URL is set."; fi
-if [ -z "${BAZEL_WINDOWS_ZIP_URL+x}" ]; then echo "BAZEL_WINDOWS_ZIP_URL is unset" && exit 1; else echo "BAZEL_WINDOWS_ZIP_URL is set."; fi
-if [ -z "${BAZEL_WINDOWS_ZIP_SHA256+x}" ]; then echo "BAZEL_WINDOWS_ZIP_SHA256 is unset" && exit 1; else echo "BAZEL_WINDOWS_ZIP_SHA256 is set."; fi
-if [ -z "${CHOCOLATEY_API_KEY+x}" ]; then echo "CHOCOLATEY_API_KEY is unset" && exit 1; else echo "CHOCOLATEY_API_KEY is set."; fi
+if [[ -z "${VERSION+x}" ]]; then echo "VERSION is unset" && exit 1; else echo "VERSION is set."; fi
+if [[ -z "${BAZEL_RELEASE_NOTES_URL+x}" ]]; then echo "BAZEL_RELEASE_NOTES_URL is unset" && exit 1; else echo "BAZEL_RELEASE_NOTES_URL is set."; fi
+if [[ -z "${BAZEL_WINDOWS_ZIP_URL+x}" ]]; then echo "BAZEL_WINDOWS_ZIP_URL is unset" && exit 1; else echo "BAZEL_WINDOWS_ZIP_URL is set."; fi
+if [[ -z "${BAZEL_WINDOWS_ZIP_SHA256+x}" ]]; then echo "BAZEL_WINDOWS_ZIP_SHA256 is unset" && exit 1; else echo "BAZEL_WINDOWS_ZIP_SHA256 is set."; fi
+if [[ -z "${CHOCOLATEY_API_KEY+x}" ]]; then echo "CHOCOLATEY_API_KEY is unset" && exit 1; else echo "CHOCOLATEY_API_KEY is set."; fi
 
+if [[ -n "${PACKAGE_FIX_VERSION}" ]]; then PACKAGE_FIX_VERSION=".${PACKAGE_FIX_VERSION}"; fi
 pushd "bazel-package"
   package_name="bazel"
   # clean slate
@@ -31,16 +32,18 @@ EOMultiLine
   curl -o "tools/LICENSE" "https://raw.githubusercontent.com/bazelbuild/bazel/master/LICENSE"
   # adjust the license to have the header
   echo -e "From: https://raw.githubusercontent.com/bazelbuild/bazel/master/LICENSE\n\n$(cat tools/LICENSE)" > "tools/LICENSE"
-  # run `choco pack ./bazel.nuspec`
+
+  echo "running 'choco pack ./bazel.nuspec' inside docker..."
   docker run \
     --rm \
     --mount "type=bind,src=$(pwd),dst=/work" \
     linuturk/mono-choco \
     pack --verbose --debug "/work/${package_name}.nuspec" --outputdirectory "/work"
-  # run `choco push <package> --key <key>`
+
+  echo "running 'choco push <package> --key <key>' inside docker..."
   docker run \
     --rm \
     --mount "type=bind,src=$(pwd),dst=/work" \
     linuturk/mono-choco \
-    push --verbose --debug "${package_name}.${VERSION}.nupkg" --timeout "30" --apikey="${CHOCOLATEY_API_KEY}"
+    push --verbose --debug "${package_name}.${VERSION}${PACKAGE_FIX_VERSION}.nupkg" --timeout "30" --apikey="${CHOCOLATEY_API_KEY}"
 popd
