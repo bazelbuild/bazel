7f279e0071bec6bb50f74d9e39006ff1b6c1c87d
Philipp Wollermann <philwo@google.com>
2018-02-07 20:38:27 +0100

    Update failed_testlogs.py

--- .buildkite/failed_testlogs.py
+++ .buildkite/failed_testlogs.py
@@ -3,22 +3,14 @@ import sys
 from urllib.parse import urlparse
 
 def main(bep_path):
-  raw_data = ""
-  with open(bep_path) as f:
-    raw_data = f.read()
-  decoder = json.JSONDecoder()
-
-  pos = 0
-  while pos < len(raw_data):
-    json_dict, size = decoder.raw_decode(raw_data[pos:])
-    if "testResult" in json_dict:
-      test_result = json_dict["testResult"]
+  with open(bep_path, "r") as f:
+    for key, test_result in json.read(f).items():
+      if key != "testResult":
+        continue
       if test_result["status"] != "PASSED":
-        outputs = test_result["testActionOutput"]
-        for output in outputs:
+        for output in test_result["testActionOutput"]:
           if output["name"] == "test.log":
             print(urlparse(output["uri"]).path)
-    pos += size + 1
 
 if __name__ == '__main__':
   main(sys.argv[1])
