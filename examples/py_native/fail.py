0f9c6ea574918dda094cf5423fa3822112846c30
Mouad Benchchaoui <mbenchchaoui@newstore.com>
2018-02-14 05:09:15 -0800

    Make __init__.py files creation optional
    Introduce a new attribute to py_binary and py_test to control whether to
    create `__init__.py` or not.
    
    Fixes https://github.com/bazelbuild/rules_python/issues/55
    
    Closes #4470.
    
    PiperOrigin-RevId: 185672243

--- examples/py_native/fail.py
+++ examples/py_native/fail.py
@@ -1,6 +1,6 @@
 """A tiny example binary for the native Python rules of Bazel."""
 import unittest
-from examples.py_native.lib import GetNumber
+from lib import GetNumber
 
 
 class TestGetNumber(unittest.TestCase):
