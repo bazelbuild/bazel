737797094fc580e613e9054b9698f9716a7a5e15
Laszlo Csomor <laszlocsomor@google.com>
2018-02-27 16:58:52 +0100

    DONT SUBMIT: add more commits

--- src/test/shell/bazel/bazel_windows_example_test.sh
+++ src/test/shell/bazel/bazel_windows_example_test.sh
@@ -62,159 +62,174 @@ function assert_binary_run_from_subdir() {
 #
 # Native rules
 #
-function test_cpp() {
-  local cpp_pkg=examples/cpp
-  assert_build_output ./bazel-bin/${cpp_pkg}/libhello-lib.a ${cpp_pkg}:hello-world
-  assert_build_output ./bazel-bin/${cpp_pkg}/hello-world.pdb ${cpp_pkg}:hello-world --output_groups=pdb_file
-  assert_build_output ./bazel-bin/${cpp_pkg}/hello-world.pdb -c dbg ${cpp_pkg}:hello-world --output_groups=pdb_file
-  assert_build -c opt ${cpp_pkg}:hello-world --output_groups=pdb_file
-  test -f ./bazel-bin/${cpp_pkg}/hello-world.pdb && fail "PDB file should not be generated in OPT mode"
-  assert_build ${cpp_pkg}:hello-world
-  ./bazel-bin/${cpp_pkg}/hello-world foo >& $TEST_log \
-    || fail "./bazel-bin/${cpp_pkg}/hello-world foo execution failed"
-  expect_log "Hello foo"
-  assert_test_ok "//examples/cpp:hello-success_test"
-  assert_test_fails "//examples/cpp:hello-fail_test"
-}
-
-function test_cpp_alwayslink() {
-  mkdir -p cpp/main
-  cat >cpp/main/BUILD <<EOF
-cc_library(
-    name = "lib",
-    srcs = ["lib.cc"],
-    alwayslink = 1,
-)
-cc_library(
-    name = "main",
-    srcs = ["main.cc"],
-)
-cc_binary(
-    name = "bin",
-    deps = [":main", ":lib"],
-)
-EOF
-
-  cat >cpp/main/lib.cc <<EOF
-extern int global_variable;
-int init() {
-    ++global_variable;
-    return global_variable;
-}
-int x = init();
-int y = init();
-EOF
-
-  cat >cpp/main/main.cc <<EOF
-#include<stdio.h>
-int global_variable = 0;
-int main(void) {
-    printf("global : %d\n", global_variable);
-    return 0;
-}
-EOF
-  assert_build //cpp/main:bin
-  ./bazel-bin/cpp/main/bin >& $TEST_log \
-    || fail "//cpp/main:bin execution failed"
-  expect_log "global : 2"
-}
-
-function test_java() {
-  local java_pkg=examples/java-native/src/main/java/com/example/myproject
-
-  assert_build_output ./bazel-bin/${java_pkg}/libhello-lib.jar ${java_pkg}:hello-lib
-  assert_build_output ./bazel-bin/${java_pkg}/libcustom-greeting.jar ${java_pkg}:custom-greeting
-  assert_build_output ./bazel-bin/${java_pkg}/hello-world ${java_pkg}:hello-world
-  assert_build_output ./bazel-bin/${java_pkg}/hello-resources ${java_pkg}:hello-resources
-  assert_binary_run_from_subdir "bazel-bin/${java_pkg}/hello-world foo" "Hello foo"
-}
+# function test_cpp() {
+#   local cpp_pkg=examples/cpp
+#   assert_build_output ./bazel-bin/${cpp_pkg}/libhello-lib.a ${cpp_pkg}:hello-world
+#   assert_build_output ./bazel-bin/${cpp_pkg}/hello-world.pdb ${cpp_pkg}:hello-world --output_groups=pdb_file
+#   assert_build_output ./bazel-bin/${cpp_pkg}/hello-world.pdb -c dbg ${cpp_pkg}:hello-world --output_groups=pdb_file
+#   assert_build -c opt ${cpp_pkg}:hello-world --output_groups=pdb_file
+#   test -f ./bazel-bin/${cpp_pkg}/hello-world.pdb && fail "PDB file should not be generated in OPT mode"
+#   assert_build ${cpp_pkg}:hello-world
+#   ./bazel-bin/${cpp_pkg}/hello-world foo >& $TEST_log \
+#     || fail "./bazel-bin/${cpp_pkg}/hello-world foo execution failed"
+#   expect_log "Hello foo"
+#   assert_test_ok "//examples/cpp:hello-success_test"
+#   assert_test_fails "//examples/cpp:hello-fail_test"
+# }
+# 
+# function test_cpp_alwayslink() {
+#   mkdir -p cpp/main
+#   cat >cpp/main/BUILD <<EOF
+# cc_library(
+#     name = "lib",
+#     srcs = ["lib.cc"],
+#     alwayslink = 1,
+# )
+# cc_library(
+#     name = "main",
+#     srcs = ["main.cc"],
+# )
+# cc_binary(
+#     name = "bin",
+#     deps = [":main", ":lib"],
+# )
+# EOF
+# 
+#   cat >cpp/main/lib.cc <<EOF
+# extern int global_variable;
+# int init() {
+#     ++global_variable;
+#     return global_variable;
+# }
+# int x = init();
+# int y = init();
+# EOF
+# 
+#   cat >cpp/main/main.cc <<EOF
+# #include<stdio.h>
+# int global_variable = 0;
+# int main(void) {
+#     printf("global : %d\n", global_variable);
+#     return 0;
+# }
+# EOF
+#   assert_build //cpp/main:bin
+#   ./bazel-bin/cpp/main/bin >& $TEST_log \
+#     || fail "//cpp/main:bin execution failed"
+#   expect_log "global : 2"
+# }
+# 
+# function test_java() {
+#   local java_pkg=examples/java-native/src/main/java/com/example/myproject
+# 
+#   assert_build_output ./bazel-bin/${java_pkg}/libhello-lib.jar ${java_pkg}:hello-lib
+#   assert_build_output ./bazel-bin/${java_pkg}/libcustom-greeting.jar ${java_pkg}:custom-greeting
+#   assert_build_output ./bazel-bin/${java_pkg}/hello-world ${java_pkg}:hello-world
+#   assert_build_output ./bazel-bin/${java_pkg}/hello-resources ${java_pkg}:hello-resources
+#   assert_binary_run_from_subdir "bazel-bin/${java_pkg}/hello-world foo" "Hello foo"
+# }
 
 function test_java_test() {
   setup_javatest_support
   local java_native_tests=//examples/java-native/src/test/java/com/example/myproject
   local java_native_main=//examples/java-native/src/main/java/com/example/myproject
 
+  assert_build "${java_native_tests}:custom"
+  jarfile="bazel-bin/examples/java-native/src/main/java/com/example/myproject/libcustom-greeting.jar"
+  if [[ ! -f $jarfile ]]; then fail "FAIL 1" ; fi
+  if ! unzip -l $jarfile | grep -q greeting.txt ; then fail "FAIL 2" ; fi
+  if ! unzip $jarfile greeting.txt -d $TEST_TMPDIR ; then fail "FAIL 3"; fi
+  echo "DEBUG 1 ===="
+  hexdump -C $TEST_TMPDIR
+  echo "DEBUG 2 ===="
+  fail "CICA"
+
+  assert_binary_run_from_subdir "bazel-bin/${java_native_tests}/custom foo" "CICA"
+
   assert_build "-- //examples/java-native/... -${java_native_main}:hello-error-prone"
   assert_build_fails "${java_native_main}:hello-error-prone" \
       "Did you mean 'result = b == -1;'?"
+  assert_build "${java_native_tests}:custom"
+  assert_binary_run_from_subdir "bazel-bin/${java_native_tests}/custom foo" "CICA"
+
   assert_test_ok "${java_native_tests}:hello"
   assert_test_ok "${java_native_tests}:custom"
   assert_test_fails "${java_native_tests}:fail"
   assert_test_fails "${java_native_tests}:resource-fail"
 }
 
-function test_native_python() {
-  # On windows, we build a python executable zip as the python binary
-  assert_build //examples/py_native:bin
-  # run the python package directly
-  ./bazel-bin/examples/py_native/bin >& $TEST_log \
-    || fail "//examples/py_native:bin execution failed"
-  expect_log "Fib(5) == 8"
-  # Using python <zipfile> to run the python package
-  python ./bazel-bin/examples/py_native/bin.zip >& $TEST_log \
-    || fail "//examples/py_native:bin execution failed"
-  expect_log "Fib(5) == 8"
-  assert_test_ok //examples/py_native:test
-  assert_test_fails //examples/py_native:fail
-}
-
-function test_native_python_with_python3() {
-  PYTHON3_PATH=${PYTHON3_PATH:-/c/Program Files/Anaconda3}
-  if [ ! -x "${PYTHON3_PATH}/python.exe" ]; then
-    warn "Python3 binary not found under $PYTHON3_PATH, please set PYTHON3_PATH correctly"
-  else
-    # Shutdown bazel to ensure python path get updated.
-    export BAZEL_PYTHON="${PYTHON3_PATH}/python.exe"
-    export PATH="${PYTHON3_PATH}:$PATH"
-    test_native_python
-  fi
-}
-
-function test_python_test_with_data() {
-  touch BUILD
-  touch WORKSPACE
-
-  mkdir data
-  cat >data/BUILD <<EOF
-filegroup(
-  name = "test_data",
-  srcs = ["data.txt"],
-  visibility = ["//visibility:public"],
-)
-EOF
-
-  cat >data/data.txt <<EOF
-hello world
-EOF
-
-  mkdir src
-  cat >src/BUILD <<EOF
-py_test(
-  name = "data_test",
-  srcs = ["data_test.py"],
-  data = ["//data:test_data"],
-)
-EOF
-
-  cat >src/data_test.py <<EOF
-import unittest
-import os
-
-class MyTest(unittest.TestCase):
-
-  def test_data(self):
-    with open("data/data.txt") as f:
-      line = f.readline().strip()
-      self.assertEqual(line, "hello world")
-
-if __name__ == '__main__':
-  print("CWD = " + os.getcwd())
-  unittest.main()
-EOF
-
-  bazel test --test_output=errors //src:data_test >& $TEST_log \
-    || fail "Test //src:test failed while expecting success"
-}
+# function test_native_python() {
+#   # On windows, we build a python executable zip as the python binary
+#   assert_build //examples/py_native:bin
+#   # run the python package directly
+#   ./bazel-bin/examples/py_native/bin >& $TEST_log \
+#     || fail "//examples/py_native:bin execution failed"
+#   expect_log "Fib(5) == 8"
+#   # Using python <zipfile> to run the python package
+#   python ./bazel-bin/examples/py_native/bin.zip >& $TEST_log \
+#     || fail "//examples/py_native:bin execution failed"
+#   expect_log "Fib(5) == 8"
+#   assert_test_ok //examples/py_native:test
+#   assert_test_fails //examples/py_native:fail
+# }
+# 
+# function test_native_python_with_python3() {
+#   PYTHON3_PATH=${PYTHON3_PATH:-/c/Program Files/Anaconda3}
+#   if [ ! -x "${PYTHON3_PATH}/python.exe" ]; then
+#     warn "Python3 binary not found under $PYTHON3_PATH, please set PYTHON3_PATH correctly"
+#   else
+#     # Shutdown bazel to ensure python path get updated.
+#     export BAZEL_PYTHON="${PYTHON3_PATH}/python.exe"
+#     export PATH="${PYTHON3_PATH}:$PATH"
+#     test_native_python
+#   fi
+# }
+# 
+# function test_python_test_with_data() {
+#   touch BUILD
+#   touch WORKSPACE
+# 
+#   mkdir data
+#   cat >data/BUILD <<EOF
+# filegroup(
+#   name = "test_data",
+#   srcs = ["data.txt"],
+#   visibility = ["//visibility:public"],
+# )
+# EOF
+# 
+#   cat >data/data.txt <<EOF
+# hello world
+# EOF
+# 
+#   mkdir src
+#   cat >src/BUILD <<EOF
+# py_test(
+#   name = "data_test",
+#   srcs = ["data_test.py"],
+#   data = ["//data:test_data"],
+# )
+# EOF
+# 
+#   cat >src/data_test.py <<EOF
+# import unittest
+# import os
+# 
+# class MyTest(unittest.TestCase):
+# 
+#   def test_data(self):
+#     with open("data/data.txt") as f:
+#       line = f.readline().strip()
+#       self.assertEqual(line, "hello world")
+# 
+# if __name__ == '__main__':
+#   print("CWD = " + os.getcwd())
+#   unittest.main()
+# EOF
+# 
+#   bazel test --test_output=errors //src:data_test >& $TEST_log \
+#     || fail "Test //src:test failed while expecting success"
+# }
 
 run_suite "examples on Windows"
 
