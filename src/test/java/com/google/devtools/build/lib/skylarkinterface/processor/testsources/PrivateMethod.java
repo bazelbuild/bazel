46f7106d0b20ae0ba245c3609545600ae379cea4
cparsons <cparsons@google.com>
2018-03-02 17:30:02 -0800

    Fix SkylarkCallableProcessor test source package to appropriately reflect the directory
    RELNOTES: None.
    PiperOrigin-RevId: 187692669

--- src/test/java/com/google/devtools/build/lib/skylarkinterface/processor/testsources/PrivateMethod.java
+++ src/test/java/com/google/devtools/build/lib/skylarkinterface/processor/testsources/PrivateMethod.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.devtools.build.lib.analysis.skylarkinterface.processor.testsources;
+package com.google.devtools.build.lib.skylarkinterface.processor.testsources;
 
 import com.google.devtools.build.lib.skylarkinterface.SkylarkCallable;
 
