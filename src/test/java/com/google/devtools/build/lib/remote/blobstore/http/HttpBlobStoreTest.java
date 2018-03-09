70e52520882f1d40a9233e5a2c4fc31cf181bd5e
Jakob Buchgraber <buchgr@google.com>
2018-03-09 14:14:16 +0100

    increase timeout

--- src/test/java/com/google/devtools/build/lib/remote/blobstore/http/HttpBlobStoreTest.java
+++ src/test/java/com/google/devtools/build/lib/remote/blobstore/http/HttpBlobStoreTest.java
@@ -96,7 +96,7 @@ public class HttpBlobStoreTest {
 
       Credentials credentials = newCredentials();
       HttpBlobStore blobStore =
-          new HttpBlobStore(new URI("http://localhost:" + serverPort), 1, credentials);
+          new HttpBlobStore(new URI("http://localhost:" + serverPort), 5, credentials);
       ByteArrayOutputStream out = Mockito.spy(new ByteArrayOutputStream());
       blobStore.get("key", out);
       assertThat(out.toString(Charsets.US_ASCII.name())).isEqualTo("File Contents");
@@ -125,7 +125,7 @@ public class HttpBlobStoreTest {
 
       Credentials credentials = newCredentials();
       HttpBlobStore blobStore =
-          new HttpBlobStore(new URI("http://localhost:" + serverPort), 1, credentials);
+          new HttpBlobStore(new URI("http://localhost:" + serverPort), 5, credentials);
       byte[] data = "File Contents".getBytes(Charsets.US_ASCII);
       ByteArrayInputStream in = new ByteArrayInputStream(data);
       blobStore.put("key", data.length, in);
@@ -153,7 +153,7 @@ public class HttpBlobStoreTest {
 
       Credentials credentials = newCredentials();
       HttpBlobStore blobStore =
-          new HttpBlobStore(new URI("http://localhost:" + serverPort), 1, credentials);
+          new HttpBlobStore(new URI("http://localhost:" + serverPort), 5, credentials);
       blobStore.get("key", new ByteArrayOutputStream());
       fail("Exception expected.");
     } catch(Exception e) {
@@ -178,7 +178,7 @@ public class HttpBlobStoreTest {
 
       Credentials credentials = newCredentials();
       HttpBlobStore blobStore =
-          new HttpBlobStore(new URI("http://localhost:" + serverPort), 1, credentials);
+          new HttpBlobStore(new URI("http://localhost:" + serverPort), 5, credentials);
       blobStore.put("key", 1, new ByteArrayInputStream(new byte[] {0}));
       fail("Exception expected.");
     } catch(Exception e) {
