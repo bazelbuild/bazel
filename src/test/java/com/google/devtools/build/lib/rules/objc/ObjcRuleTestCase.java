892456e71d35df1b0009c1ea734238d60519da3d
jcater <jcater@google.com>
2018-03-15 07:21:38 -0700

    Ensure that only ObjC tests initialize the MockObjcSupport.
    PiperOrigin-RevId: 189181919

--- src/test/java/com/google/devtools/build/lib/rules/objc/ObjcRuleTestCase.java
+++ src/test/java/com/google/devtools/build/lib/rules/objc/ObjcRuleTestCase.java
@@ -85,6 +85,7 @@ import com.google.devtools.build.xcode.bundlemerge.proto.BundleMergeProtos;
 import com.google.devtools.build.xcode.bundlemerge.proto.BundleMergeProtos.BundleFile;
 import com.google.devtools.build.xcode.bundlemerge.proto.BundleMergeProtos.MergeZip;
 import com.google.devtools.build.xcode.plmerge.proto.PlMergeProtos;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
@@ -95,7 +96,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import javax.annotation.Nullable;
-import org.junit.Before;
 
 /**
  * Superclass for all Obj-C rule tests.
@@ -260,13 +260,11 @@ public abstract class ObjcRuleTestCase extends BuildViewTestCase {
     return getFirstArtifactEndingWith(artifacts, suffix).getExecPathString();
   }
 
-  @Before
-  public final void initializeMockToolsConfig() throws Exception {
+  @Override
+  public void initializeMockClient() throws IOException {
+    super.initializeMockClient();
     MockObjcSupport.setup(mockToolsConfig);
     MockProtoSupport.setup(mockToolsConfig);
-
-    // Set flags required by objc builds.
-    useConfiguration();
   }
 
   protected static String frameworkDir(ConfiguredTarget target) {
