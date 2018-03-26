504f8b70f931ea930b353e04db819fb547941031
lberki <lberki@google.com>
2018-03-26 02:20:31 -0700

    Remove the unused label //tools/defaults:coverage .
    RELNOTES: None.
    PiperOrigin-RevId: 190434178

--- src/main/java/com/google/devtools/build/lib/rules/cpp/CppOptions.java
+++ src/main/java/com/google/devtools/build/lib/rules/cpp/CppOptions.java
@@ -989,7 +989,7 @@ public class CppOptions extends FragmentOptions {
       }
     }
 
-    return ImmutableMap.of("CROSSTOOL", crosstoolLabels, "COVERAGE", ImmutableSet.<Label>of());
+    return ImmutableMap.of("CROSSTOOL", crosstoolLabels);
   }
 
   /**
