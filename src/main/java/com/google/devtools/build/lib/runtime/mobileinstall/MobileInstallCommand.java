0b42970d33ce35a2e1a63bf52128c1307a97c168
ajmichael <ajmichael@google.com>
2018-04-04 09:37:31 -0700

    Fix crash from mobile-install with --device but no --adb_args.
    Fixes #4922.
    
    RELNOTES: None
    PiperOrigin-RevId: 191602647

--- src/main/java/com/google/devtools/build/lib/runtime/mobileinstall/MobileInstallCommand.java
+++ src/main/java/com/google/devtools/build/lib/runtime/mobileinstall/MobileInstallCommand.java
@@ -262,7 +262,7 @@ public class MobileInstallCommand implements BlazeCommand {
     if (!adbOptions.adb.isEmpty()) {
       cmdLine.add("--adb=" + adbOptions.adb);
     }
-    for (String adbArg : adbOptions.adbArgs) {
+    for (String adbArg : adbOptions.getAdbArgs()) {
       if (!adbArg.isEmpty()) {
         cmdLine.add("--adb_arg=" + adbArg);
       }
