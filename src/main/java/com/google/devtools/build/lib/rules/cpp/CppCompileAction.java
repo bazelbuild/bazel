f869ace8e05a222c2dc053e81e7af2808ff6dfae
shahan <shahan@google.com>
2018-04-09 11:04:02 -0700

    Fix Artifact.getPath() call in CppCompileAction.validateInclusions
    PiperOrigin-RevId: 192153123

--- src/main/java/com/google/devtools/build/lib/rules/cpp/CppCompileAction.java
+++ src/main/java/com/google/devtools/build/lib/rules/cpp/CppCompileAction.java
@@ -891,9 +891,9 @@ public class CppCompileAction extends AbstractAction
         // This call can never match the declared include sources (they would be matched above).
         // There are no declared include sources we need to warn about, so use an empty set here.
         if (isDeclaredIn(input, warnIncludeDirs, ImmutableSet.<Artifact>of())) {
-          warnings.add(input.getPath().toString());
+          warnings.add(input.getExecPath().toString());
         } else {
-          errors.add(input.getPath().toString());
+          errors.add(input.getExecPath().toString());
         }
       }
     }
