a18b11ed6674f285590d62a1121a22d1fb7e95ca
Laszlo Csomor <laszlocsomor@google.com>
2018-04-18 00:38:57 -0700

    windows: GetOutputRoot() returns GetHomeDir()
    This is an immediate fix for a very nasty bug:
    https://github.com/bazelbuild/bazel/issues/5038
    
    Change-Id: I5e4f9fa13e5ac785514bc0dc4ce6cba9a88f33bb
    
    Closes #5039.
    
    Change-Id: I5e4f9fa13e5ac785514bc0dc4ce6cba9a88f33bb
    PiperOrigin-RevId: 193315571

--- src/main/cpp/blaze_util_windows.cc
+++ src/main/cpp/blaze_util_windows.cc
@@ -202,19 +202,12 @@ string GetSelfPath() {
 }
 
 string GetOutputRoot() {
-  for (const char* i : {"TMPDIR", "TEMPDIR", "TMP", "TEMP"}) {
-    string tmpdir(GetEnv(i));
-    if (!tmpdir.empty()) {
-      return tmpdir;
-    }
+  string home = GetHomeDir();
+  if (home.empty()) {
+    BAZEL_DIE(blaze_exit_code::LOCAL_ENVIRONMENTAL_ERROR)
+        << "Cannot find a good output root. Use the --output_user_root flag.";
   }
-
-  WCHAR buffer[kWindowsPathBufferSize] = {0};
-  if (!::GetTempPathW(kWindowsPathBufferSize, buffer)) {
-    die(blaze_exit_code::LOCAL_ENVIRONMENTAL_ERROR,
-        "GetOutputRoot: GetTempPathW: %s", GetLastErrorString().c_str());
-  }
-  return string(blaze_util::WstringToCstring(buffer).get());
+  return home;
 }
 
 string GetHomeDir() {
