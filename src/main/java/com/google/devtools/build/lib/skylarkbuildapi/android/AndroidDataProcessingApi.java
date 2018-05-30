ab259ee0dc6637030338f74823ce4aba7592cf95
asteinb <asteinb@google.com>
2018-05-30 09:04:03 -0700

    Fix Skylark type typo
    Fortunately or unfortunately, Skylark doesn't seem to validate against this
    type, so tests don't pick this mistyping up.
    
    RELNOTES: none
    PiperOrigin-RevId: 198568282

--- src/main/java/com/google/devtools/build/lib/skylarkbuildapi/android/AndroidDataProcessingApi.java
+++ src/main/java/com/google/devtools/build/lib/skylarkbuildapi/android/AndroidDataProcessingApi.java
@@ -572,7 +572,7 @@ public interface AndroidDataProcessingApi<
             positional = false,
             defaultValue = "[]",
             type = SkylarkList.class,
-            generic1 = AndroidAssetsInfoApi.class,
+            generic1 = TransitiveInfoCollectionApi.class,
             named = true,
             doc =
                 "Dependency targets. Providers will be extracted from these dependencies for each"
