1333b7efff7cc500e5eaee4e53f7484018d59679
cparsons <cparsons@google.com>
2018-06-12 11:12:24 -0700

    Add skylarkbuildapi bootstraps for apple, cpp, platform, and testing packages
    RELNOTES: None.
    PiperOrigin-RevId: 200246780

---
+++ src/main/java/com/google/devtools/build/lib/skylarkbuildapi/apple/AppleBootstrap.java
@@ -0,0 +1,35 @@
+// Copyright 2018 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.devtools.build.lib.skylarkbuildapi.apple;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.devtools.build.lib.skylarkbuildapi.Bootstrap;
+
+/**
+ * {@link Bootstrap} for skylark objects related to apple rules.
+ */
+public class AppleBootstrap implements Bootstrap {
+
+  private final AppleCommonApi<?, ?, ?, ?> appleCommon;
+
+  public AppleBootstrap(AppleCommonApi<?, ?, ?, ?> appleCommon) {
+    this.appleCommon = appleCommon;
+  }
+
+  @Override
+  public void addBindingsToBuilder(ImmutableMap.Builder<String, Object> builder) {
+    builder.put("apple_common", appleCommon);
+  }
+}
