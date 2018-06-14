3a5780d020517342137ef48ca3fbad1df69e9f33
plf <plf@google.com>
2018-06-14 02:07:40 -0700

    C++: Fixes error reporting for cc_import
    RELNOTES:none
    PiperOrigin-RevId: 200525022

--- src/main/java/com/google/devtools/build/lib/rules/cpp/CcImport.java
+++ src/main/java/com/google/devtools/build/lib/rules/cpp/CcImport.java
@@ -157,7 +157,7 @@ public abstract class CcImport implements RuleConfiguredTargetFactory {
       // library required at linking time is the same as the one required at execution time.
       if (!targetWindows) {
         dynamicLibraryForLinkingList = dynamicLibrariesForRuntimeList;
-      } else if (staticLibrary == null) {
+      } else if (sharedLibrary != null) {
         ruleContext.ruleError(
           "'interface library' must be specified when using cc_import for shared library on"
         + " Windows");
