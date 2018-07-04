bazelbuild/bazel

commit d3f7f7ae1369834b5f81d62474d20541d8b1a918

bep: Make the BuildEventArtifactUploader async.
This changes the BuildEventArtifactUploader to an async interface,
thereby no longer potentially delaying event delivery over the
eventbus. Additionally, the BES transport is changed to start
uploading local files immediately as the events are delivered.

RELNOTES: None
PiperOrigin-RevId: 202694121

src/main/java/com/google/devtools/build/lib/buildeventstream/transports/BinaryFormatFileTransport.java
@@ -14,6 +14,9 @@
 
 package com.google.devtools.build.lib.buildeventstream.transports;
 
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.MoreExecutors;
 import com.google.devtools.build.lib.buildeventstream.ArtifactGroupNamer;
 import com.google.devtools.build.lib.buildeventstream.BuildEvent;
 import com.google.devtools.build.lib.buildeventstream.BuildEventArtifactUploader;
@@ -45,10 +48,18 @@ public final class BinaryFormatFileTransport extends FileTransport {
 
   @Override
   public synchronized void sendBuildEvent(BuildEvent event, final ArtifactGroupNamer namer) {
-    BuildEventStreamProtos.BuildEvent protoEvent = asStreamProto(event, namer);
-    if (protoEvent == null) {
-      return;
-    }
-    write(protoEvent);
+    Futures.addCallback(asStreamProto(event, namer),
+        new FutureCallback<BuildEventStreamProtos.BuildEvent>() {
+          @Override
+          public void onSuccess(BuildEventStreamProtos.BuildEvent protoEvent) {
+            write(protoEvent);
+          }
+
+          @Override
+          public void onFailure(Throwable t) {
+            // Intentionally left empty. The error handling happens in
+            // FileTransport.
+          }
+        }, MoreExecutors.directExecutor());
   }
 }

src/main/java/com/google/devtools/build/lib/buildeventstream/transports/FileTransport.java
@@ -17,7 +17,9 @@ package com.google.devtools.build.lib.buildeventstream.transports;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Function;
 import com.google.common.base.Throwables;
+import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.MoreExecutors;
@@ -39,7 +41,6 @@ import java.util.Set;
 import java.util.function.Consumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.annotation.Nullable;
 
 /**
  * Non-blocking file transport.
@@ -54,7 +55,6 @@ abstract class FileTransport implements BuildEventTransport {
   private final BuildEventProtocolOptions options;
   private final BuildEventArtifactUploader uploader;
   private final Consumer<AbruptExitException> exitFunc;
-  private boolean errored;
 
   @VisibleForTesting
   final AsynchronousFileOutputStream out;
@@ -111,57 +111,62 @@ abstract class FileTransport implements BuildEventTransport {
    * a side effect. May return {@code null} if there was an interrupt. This method is not
    * thread-safe.
    */
-  @Nullable
-  protected BuildEventStreamProtos.BuildEvent asStreamProto(
+  protected ListenableFuture<BuildEventStreamProtos.BuildEvent> asStreamProto(
       BuildEvent event, ArtifactGroupNamer namer) {
     checkNotNull(event);
-    PathConverter pathConverter = uploadReferencedFiles(event.referencedLocalFiles());
-    if (pathConverter == null) {
-      return null;
-    }
 
-    BuildEventContext context =
-        new BuildEventContext() {
+    return Futures.transform(
+        uploadReferencedFiles(event.referencedLocalFiles()),
+        new Function<PathConverter, BuildEventStreamProtos.BuildEvent>() {
           @Override
-          public PathConverter pathConverter() {
-            return pathConverter;
-          }
+          public BuildEventStreamProtos.BuildEvent apply(PathConverter pathConverter) {
+            BuildEventContext context =
+                new BuildEventContext() {
+                  @Override
+                  public PathConverter pathConverter() {
+                    return pathConverter;
+                  }
 
-          @Override
-          public ArtifactGroupNamer artifactGroupNamer() {
-            return namer;
-          }
+                  @Override
+                  public ArtifactGroupNamer artifactGroupNamer() {
+                    return namer;
+                  }
 
-          @Override
-          public BuildEventProtocolOptions getOptions() {
-            return options;
+                  @Override
+                  public BuildEventProtocolOptions getOptions() {
+                    return options;
+                  }
+                };
+            return event.asStreamProto(context);
           }
-        };
-    return event.asStreamProto(context);
+        },
+        MoreExecutors.directExecutor());
   }
 
   /**
    * Returns a {@link PathConverter} for the uploaded files, or {@code null} when the uploaded
    * failed.
    */
-  private @Nullable PathConverter uploadReferencedFiles(Set<Path> artifacts) {
+  private ListenableFuture<PathConverter> uploadReferencedFiles(Set<Path> artifacts) {
     checkNotNull(artifacts);
 
-    if (errored) {
-      return null;
-    }
-    try {
-      return uploader.upload(artifacts);
-    } catch (IOException e) {
-      errored = true;
-      exitFunc.accept(
-          new AbruptExitException(
-              Throwables.getStackTraceAsString(e), ExitCode.PUBLISH_ERROR, e));
-    } catch (InterruptedException e) {
-      errored = true;
-      exitFunc.accept(new AbruptExitException(ExitCode.INTERRUPTED, e));
-      Thread.currentThread().interrupt();
-    }
-    return null;
+    ListenableFuture<PathConverter> upload = uploader.upload(artifacts);
+    Futures.addCallback(
+        upload,
+        new FutureCallback<PathConverter>() {
+          @Override
+          public void onSuccess(PathConverter result) {
+            // Intentionally left empty.
+          }
+
+          @Override
+          public void onFailure(Throwable t) {
+            exitFunc.accept(
+                new AbruptExitException(
+                    Throwables.getStackTraceAsString(t), ExitCode.PUBLISH_ERROR, t));
+          }
+        },
+        MoreExecutors.directExecutor());
+    return upload;
   }
 }

commit 138d28a08490ca672dc183e8a658482dbc7034f1

bep: guarantee event ordering in file transports.
Commit d3f7f7ae introduced a bug in the FileTransport implementations
where events might not be written to file in the correct order. This
change fixes this.

src/main/java/com/google/devtools/build/lib/buildeventstream/transports/BinaryFormatFileTransport.java
@@ -24,14 +24,19 @@ import com.google.devtools.build.lib.buildeventstream.BuildEventProtocolOptions;
 import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos;
 import com.google.devtools.build.lib.buildeventstream.BuildEventTransport;
 import com.google.devtools.build.lib.util.AbruptExitException;
+import com.google.protobuf.CodedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.function.Consumer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * A simple {@link BuildEventTransport} that writes a varint delimited binary representation of
  * {@link BuildEvent} protocol buffers to a file.
  */
 public final class BinaryFormatFileTransport extends FileTransport {
+
   BinaryFormatFileTransport(
       String path,
       BuildEventProtocolOptions options,
@@ -47,19 +52,16 @@ public final class BinaryFormatFileTransport extends FileTransport {
   }
 
   @Override
-  public synchronized void sendBuildEvent(BuildEvent event, final ArtifactGroupNamer namer) {
-    Futures.addCallback(asStreamProto(event, namer),
-        new FutureCallback<BuildEventStreamProtos.BuildEvent>() {
-          @Override
-          public void onSuccess(BuildEventStreamProtos.BuildEvent protoEvent) {
-            write(protoEvent);
-          }
-
-          @Override
-          public void onFailure(Throwable t) {
-            // Intentionally left empty. The error handling happens in
-            // FileTransport.
-          }
-        }, MoreExecutors.directExecutor());
+  protected byte[] serializeEvent(BuildEventStreamProtos.BuildEvent buildEvent) {
+    final int size = buildEvent.getSerializedSize();
+    ByteArrayOutputStream bos =
+        new ByteArrayOutputStream(CodedOutputStream.computeUInt32SizeNoTag(size) + size);
+    try {
+      buildEvent.writeDelimitedTo(bos);
+    } catch (IOException e) {
+      throw new RuntimeException("Unexpected error serializing protobuf to in memory outputstream.",
+          e);
+    }
+    return bos.toByteArray();
   }
 }

src/main/java/com/google/devtools/build/lib/buildeventstream/transports/FileTransport.java
@@ -15,6 +15,7 @@
 package com.google.devtools.build.lib.buildeventstream.transports;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static java.lang.String.format;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Function;
@@ -24,6 +25,7 @@ import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.MoreExecutors;
+import com.google.common.util.concurrent.SettableFuture;
 import com.google.devtools.build.lib.buildeventstream.ArtifactGroupNamer;
 import com.google.devtools.build.lib.buildeventstream.BuildEvent;
 import com.google.devtools.build.lib.buildeventstream.BuildEvent.LocalFile;
@@ -37,12 +39,20 @@ import com.google.devtools.build.lib.util.AbruptExitException;
 import com.google.devtools.build.lib.util.ExitCode;
 import com.google.devtools.build.lib.util.io.AsynchronousFileOutputStream;
 import com.google.devtools.build.lib.vfs.Path;
-import com.google.protobuf.Message;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Collection;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
 import java.util.function.Consumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.annotation.concurrent.ThreadSafe;
 
 /**
  * Non-blocking file transport.
@@ -57,55 +67,129 @@ abstract class FileTransport implements BuildEventTransport {
   private final BuildEventProtocolOptions options;
   private final BuildEventArtifactUploader uploader;
   private final Consumer<AbruptExitException> exitFunc;
-
-  @VisibleForTesting
-  final AsynchronousFileOutputStream out;
+  @VisibleForTesting final SequentialWriter writer;
 
   FileTransport(
       String path,
       BuildEventProtocolOptions options,
       BuildEventArtifactUploader uploader,
-      Consumer<AbruptExitException> exitFunc)
-          throws IOException {
+      Consumer<AbruptExitException> exitFunc) {
     this.uploader = uploader;
     this.options = options;
     this.exitFunc = exitFunc;
-    out = new AsynchronousFileOutputStream(path);
+    this.writer = new SequentialWriter(path, this::serializeEvent, exitFunc);
   }
 
-  // Silent wrappers to AsynchronousFileOutputStream methods.
+  @ThreadSafe
+  @VisibleForTesting
+  static final class SequentialWriter implements Runnable {
+    private static final Logger logger = Logger.getLogger(SequentialWriter.class.getName());
+    private static final ListenableFuture<BuildEventStreamProtos.BuildEvent> CLOSE =
+        Futures.immediateCancelledFuture();
 
-  protected void write(Message m) {
-    try {
-      out.write(m);
-    } catch (Exception e) {
-      logger.log(Level.SEVERE, e.getMessage(), e);
+    private final Thread writerThread;
+    @VisibleForTesting OutputStream out;
+    private final Function<BuildEventStreamProtos.BuildEvent, byte[]> serializeFunc;
+    private final Consumer<AbruptExitException> exitFunc;
+
+    @VisibleForTesting
+    final BlockingQueue<ListenableFuture<BuildEventStreamProtos.BuildEvent>> pendingWrites =
+        new LinkedBlockingDeque<>();
+
+    private final SettableFuture<Void> closeFuture = SettableFuture.create();
+
+    SequentialWriter(
+        String path,
+        Function<BuildEventStreamProtos.BuildEvent, byte[]> serializeFunc,
+        Consumer<AbruptExitException> exitFunc) {
+      try {
+        this.out = new BufferedOutputStream(new FileOutputStream(path));
+      } catch (FileNotFoundException e) {
+        this.out = new ByteArrayOutputStream(0);
+        closeNow();
+        exitFunc.accept(new AbruptExitException(
+            format("Couldn't open BEP file '%s' for writing.", path),
+            ExitCode.PUBLISH_ERROR, e));
+      }
+      this.writerThread = new Thread(this);
+      this.serializeFunc = serializeFunc;
+      this.exitFunc = exitFunc;
+      writerThread.start();
     }
-  }
 
-  protected void write(String s) {
-    try {
-      out.write(s);
-    } catch (Exception e) {
-      logger.log(Level.SEVERE, e.getMessage(), e);
+    @Override
+    public void run() {
+      ListenableFuture<BuildEventStreamProtos.BuildEvent> buildEventF;
+      try {
+        while ((buildEventF = pendingWrites.take()) != CLOSE) {
+          BuildEventStreamProtos.BuildEvent buildEvent = buildEventF.get();
+          byte[] serialized = serializeFunc.apply(buildEvent);
+          out.write(serialized);
+        }
+      } catch (Exception e) {
+        exitFunc.accept(
+            new AbruptExitException(
+                "Failed to write BEP events to file.", ExitCode.PUBLISH_ERROR, e));
+        pendingWrites.clear();
+        logger.log(Level.SEVERE, "Failed to write BEP events to file.", e);
+      } finally {
+        try {
+          closeFuture.set(null);
+          out.flush();
+          out.close();
+        } catch (IOException e) {
+          logger.log(Level.SEVERE, "Failed to close BEP file output stream.", e);
+        }
+      }
+    }
+
+    public void closeNow() {
+      if (closeFuture.isDone()) {
+        return;
+      }
+      try {
+        pendingWrites.clear();
+        pendingWrites.put(CLOSE);
+      } catch (InterruptedException e) {
+        logger.log(Level.SEVERE, "Failed to immediately close the sequential writer.", e);
+      }
+    }
+
+    public ListenableFuture<Void> close() {
+      if (closeFuture.isDone()) {
+        return closeFuture;
+      }
+      try {
+        pendingWrites.put(CLOSE);
+      } catch (InterruptedException e) {
+        closeNow();
+        logger.log(Level.SEVERE, "Failed to close the sequential writer.", e);
+        closeFuture.set(null);
+      }
+      return closeFuture;
     }
   }
 
   @Override
-  public synchronized ListenableFuture<Void> close() {
-    return Futures.catching(
-        out.closeAsync(),
-        Throwable.class,
-        (t) -> {
-          logger.log(Level.SEVERE, t.getMessage(), t);
-          return null;
-        },
-        MoreExecutors.directExecutor());
+  public void sendBuildEvent(BuildEvent event, ArtifactGroupNamer namer) {
+    if (writer.closeFuture.isDone()) {
+      return;
+    }
+    if (!writer.pendingWrites.add(asStreamProto(event, namer))) {
+      logger.log(Level.SEVERE, "Failed to add BEP event to the write queue");
+    }
+  }
+
+  protected abstract byte[] serializeEvent(BuildEventStreamProtos.BuildEvent buildEvent);
+
+  @Override
+  public ListenableFuture<Void> close() {
+    return writer.close();
   }
 
   @Override
-  public void closeNow() {
-    out.closeNow();
+  public synchronized void closeNow() {
+    writer.closeNow();
   }
 
   /**
@@ -119,28 +203,25 @@ abstract class FileTransport implements BuildEventTransport {
 
     return Futures.transform(
         uploadReferencedFiles(event.referencedLocalFiles()),
-        new Function<PathConverter, BuildEventStreamProtos.BuildEvent>() {
-          @Override
-          public BuildEventStreamProtos.BuildEvent apply(PathConverter pathConverter) {
-            BuildEventContext context =
-                new BuildEventContext() {
-                  @Override
-                  public PathConverter pathConverter() {
-                    return pathConverter;
-                  }
+        pathConverter -> {
+          BuildEventContext context =
+              new BuildEventContext() {
+                @Override
+                public PathConverter pathConverter() {
+                  return pathConverter;
+                }
 
-                  @Override
-                  public ArtifactGroupNamer artifactGroupNamer() {
-                    return namer;
-                  }
+                @Override
+                public ArtifactGroupNamer artifactGroupNamer() {
+                  return namer;
+                }
 
-                  @Override
-                  public BuildEventProtocolOptions getOptions() {
-                    return options;
-                  }
-                };
-            return event.asStreamProto(context);
-          }
+                @Override
+                public BuildEventProtocolOptions getOptions() {
+                  return options;
+                }
+              };
+          return event.asStreamProto(context);
         },
         MoreExecutors.directExecutor());
   }

src/main/java/com/google/devtools/build/lib/buildeventstream/transports/JsonFormatFileTransport.java
@@ -14,6 +14,7 @@
 
 package com.google.devtools.build.lib.buildeventstream.transports;
 
+import com.google.common.base.Charsets;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.MoreExecutors;
@@ -27,6 +28,7 @@ import com.google.devtools.build.lib.util.AbruptExitException;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.util.JsonFormat;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.function.Consumer;
 
 /**
@@ -49,28 +51,17 @@ public final class JsonFormatFileTransport extends FileTransport {
   }
 
   @Override
-  public synchronized void sendBuildEvent(BuildEvent event, final ArtifactGroupNamer namer) {
-    Futures.addCallback(asStreamProto(event, namer),
-        new FutureCallback<BuildEventStreamProtos.BuildEvent>() {
-          @Override
-          public void onSuccess(BuildEventStreamProtos.BuildEvent protoEvent) {
-            String protoJsonRepresentation;
-            try {
-              protoJsonRepresentation =
-                  JsonFormat.printer().omittingInsignificantWhitespace().print(protoEvent) + "\n";
-            } catch (InvalidProtocolBufferException e) {
-              // We don't expect any unknown Any fields in our protocol buffer. Nevertheless, handle
-              // the exception gracefully and, at least, return valid JSON with an id field.
-              protoJsonRepresentation =
-                  "{\"id\" : \"unknown\", \"exception\" : \"InvalidProtocolBufferException\"}\n";
-            }
-            write(protoJsonRepresentation);
-          }
-
-          @Override
-          public void onFailure(Throwable t) {
-            // Intentionally left empty. The error handling happens in FileTransport.
-          }
-        }, MoreExecutors.directExecutor());
+  protected byte[] serializeEvent(BuildEventStreamProtos.BuildEvent buildEvent) {
+    String protoJsonRepresentation;
+    try {
+      protoJsonRepresentation =
+          JsonFormat.printer().omittingInsignificantWhitespace().print(buildEvent) + "\n";
+    } catch (InvalidProtocolBufferException e) {
+      // We don't expect any unknown Any fields in our protocol buffer. Nevertheless, handle
+      // the exception gracefully and, at least, return valid JSON with an id field.
+      protoJsonRepresentation =
+          "{\"id\" : \"unknown\", \"exception\" : \"InvalidProtocolBufferException\"}\n";
+    }
+    return protoJsonRepresentation.getBytes(Charsets.UTF_8);
   }
 }

src/main/java/com/google/devtools/build/lib/buildeventstream/transports/TextFormatFileTransport.java
@@ -14,6 +14,7 @@
 
 package com.google.devtools.build.lib.buildeventstream.transports;
 
+import com.google.common.base.Charsets;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.MoreExecutors;
@@ -50,19 +51,8 @@ public final class TextFormatFileTransport extends FileTransport {
   }
 
   @Override
-  public synchronized void sendBuildEvent(BuildEvent event, final ArtifactGroupNamer namer) {
-    Futures.addCallback(asStreamProto(event, namer),
-        new FutureCallback<BuildEventStreamProtos.BuildEvent>() {
-          @Override
-          public void onSuccess(BuildEventStreamProtos.BuildEvent protoEvent) {
-            String protoTextRepresentation = TextFormat.printToString(protoEvent);
-            write("event {\n" + protoTextRepresentation + "}\n\n");
-          }
-
-          @Override
-          public void onFailure(Throwable t) {
-            // Intentionally left empty. The error handling happens in FileTransport.
-          }
-        }, MoreExecutors.directExecutor());
+  protected byte[] serializeEvent(BuildEventStreamProtos.BuildEvent buildEvent) {
+    String protoTextRepresentation = TextFormat.printToString(buildEvent);
+    return ("event {\n" + protoTextRepresentation + "}\n\n").getBytes(Charsets.UTF_8);
   }
 }

src/test/java/com/google/devtools/build/lib/buildeventstream/transports/BinaryFormatFileTransportTest.java
@@ -16,21 +16,38 @@ package com.google.devtools.build.lib.buildeventstream.transports;
 
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.devtools.build.lib.buildeventstream.BuildEventArtifactUploader.LOCAL_FILES_UPLOADER;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.when;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.SettableFuture;
 import com.google.devtools.build.lib.buildeventstream.ArtifactGroupNamer;
 import com.google.devtools.build.lib.buildeventstream.BuildEvent;
+import com.google.devtools.build.lib.buildeventstream.BuildEvent.LocalFile.LocalFileType;
+import com.google.devtools.build.lib.buildeventstream.BuildEventArtifactUploader;
 import com.google.devtools.build.lib.buildeventstream.BuildEventContext;
+import com.google.devtools.build.lib.buildeventstream.BuildEventId;
 import com.google.devtools.build.lib.buildeventstream.BuildEventProtocolOptions;
 import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos;
 import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos.BuildStarted;
 import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos.Progress;
 import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos.TargetComplete;
+import com.google.devtools.build.lib.buildeventstream.PathConverter;
+import com.google.devtools.build.lib.buildeventstream.PathConverter.FileUriPathConverter;
+import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.common.options.Options;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -138,8 +155,8 @@ public class BinaryFormatFileTransportTest {
             output.getAbsolutePath(), defaultOpts, LOCAL_FILES_UPLOADER, (e) -> {});
 
     // Close the stream.
-    transport.out.close();
-    assertThat(transport.out.isOpen()).isFalse();
+    transport.writer.out.close();
+    assertThat(transport.writer.pendingWrites.isEmpty()).isTrue();
 
     // This should not throw an exception.
     transport.sendBuildEvent(buildEvent, artifactGroupNamer);
@@ -167,16 +184,118 @@ public class BinaryFormatFileTransportTest {
 
     transport.sendBuildEvent(buildEvent, artifactGroupNamer);
     Future<Void> closeFuture = transport.close();
+    closeFuture.get();
     // This should not throw an exception, but also not perform any write.
     transport.sendBuildEvent(buildEvent, artifactGroupNamer);
 
-    closeFuture.get();
-    assertThat(transport.out.isOpen()).isFalse();
+    assertThat(transport.writer.pendingWrites.isEmpty()).isTrue();
 
     // There should have only been one write.
     try (InputStream in = new FileInputStream(output)) {
       assertThat(BuildEventStreamProtos.BuildEvent.parseDelimitedFrom(in)).isEqualTo(started);
       assertThat(in.available()).isEqualTo(0);
+    }
+  }
+
+  @Test
+  public void testWritesWithUploadDelays() throws Exception {
+    // Test that events are written in order if the first event
+    // has to wait a bit for local file uploads to finish.
+
+    Path file1 = Mockito.mock(Path.class);
+    when(file1.getBaseName()).thenReturn("file1");
+    Path file2 = Mockito.mock(Path.class);
+    when(file2.getBaseName()).thenReturn("file2");
+    BuildEvent event1 = new WithLocalFileEvent(file1);
+    BuildEvent event2 = new WithLocalFileEvent(file2);
+
+    BuildEventArtifactUploader uploader =
+        files -> {
+          if (files.containsKey(file1)) {
+            LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(200));
+          }
+          return Futures.immediateFuture(new FileUriPathConverter());
+        };
+
+    File output = tmp.newFile();
+    BinaryFormatFileTransport transport =
+        new BinaryFormatFileTransport(output.getAbsolutePath(), defaultOpts, uploader, (e) -> {});
+    transport.sendBuildEvent(event1, artifactGroupNamer);
+    transport.sendBuildEvent(event2, artifactGroupNamer);
+    transport.close().get();
+
+    assertThat(transport.writer.pendingWrites.isEmpty()).isTrue();
+
+    try (InputStream in = new FileInputStream(output)) {
+      assertThat(BuildEventStreamProtos.BuildEvent.parseDelimitedFrom(in))
+          .isEqualTo(event1.asStreamProto(null));
+      assertThat(BuildEventStreamProtos.BuildEvent.parseDelimitedFrom(in))
+          .isEqualTo(event2.asStreamProto(null));
+      assertThat(in.available()).isEqualTo(0);
+    }
+  }
+
+  @Test
+  public void testCloseWaitsForWritesToFinish() throws Exception {
+    // Test that .close() waits for all writes to finish.
+
+    Path file1 = Mockito.mock(Path.class);
+    when(file1.getBaseName()).thenReturn("file1");
+    BuildEvent event = new WithLocalFileEvent(file1);
+
+    SettableFuture<PathConverter> upload = SettableFuture.create();
+    BuildEventArtifactUploader uploader = files -> upload;
+
+    File output = tmp.newFile();
+    BinaryFormatFileTransport transport =
+        new BinaryFormatFileTransport(output.getAbsolutePath(), defaultOpts, uploader, (e) -> {});
+    transport.sendBuildEvent(event, artifactGroupNamer);
+    ListenableFuture<Void> closeFuture = transport.close();
+
+    upload.set(PathConverter.NO_CONVERSION);
+
+    closeFuture.get();
+
+    try (InputStream in = new FileInputStream(output)) {
+      assertThat(BuildEventStreamProtos.BuildEvent.parseDelimitedFrom(in))
+          .isEqualTo(event.asStreamProto(null));
+      assertThat(in.available()).isEqualTo(0);
+    }
+  }
+
+  private class WithLocalFileEvent implements BuildEvent {
+
+    int id;
+    Path file;
+
+    WithLocalFileEvent(Path file) {
+      this.file = file;
+    }
+
+    @Override
+    public Collection<LocalFile> referencedLocalFiles() {
+      return Collections.singleton(new LocalFile(file, LocalFileType.OUTPUT));
+    }
+
+    @Override
+    public BuildEventStreamProtos.BuildEvent asStreamProto(BuildEventContext context) {
+      return BuildEventStreamProtos.BuildEvent.newBuilder()
+          .setId(BuildEventId.progressId(id).asStreamProto())
+          .setProgress(
+              BuildEventStreamProtos.Progress.newBuilder()
+                  .setStdout("basename: " + file.getBaseName())
+                  .build())
+          .build();
+    }
+
+    @Override
+    public BuildEventId getEventId() {
+      return BuildEventId.progressId(id);
+    }
+
+    @Override
+    public Collection<BuildEventId> getChildrenEvents() {
+      return ImmutableList.of(BuildEventId.progressId(id + 1));
     }
   }
 }