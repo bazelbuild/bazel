95f9835d74f0ced140afce447a47ea725a822d75
Andy Scott <andy.g.scott@gmail.com>
2018-07-10 11:57:22 -0700

    Use short file names; re-enable sandbox for related tests

--- src/test/java/com/google/devtools/build/lib/remote/blobstore/http/HttpBlobStoreTest.java
+++ src/test/java/com/google/devtools/build/lib/remote/blobstore/http/HttpBlobStoreTest.java
@@ -30,7 +30,6 @@ import com.google.api.client.util.Preconditions;
 import com.google.auth.Credentials;
 import com.google.common.base.Charsets;
 import com.google.devtools.build.lib.remote.blobstore.http.HttpBlobStoreTest.NotAuthorizedHandler.ErrorType;
-import com.google.devtools.build.lib.testutil.TestUtils;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
@@ -119,7 +118,7 @@ public class HttpBlobStoreTest {
 
   private static DomainSocketAddress newDomainSocketAddress() {
     try {
-      File file = File.createTempFile("bazel", ".sock", TestUtils.tmpDirFile());
+      File file = File.createTempFile("bazel", ".sock", new File("/tmp/"));
       file.delete();
       return new DomainSocketAddress(file.getAbsoluteFile());
     } catch (Exception e) {
