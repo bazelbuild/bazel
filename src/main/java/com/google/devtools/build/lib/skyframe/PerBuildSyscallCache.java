af399544072e150877dbb90150b4351671b1d848
Googler <noreply@google.com>
2018-07-11 01:24:58 -0700

    Fix obvious bug. I am not going to write a test as this function doesn't even seem to be used.
    RELNOTES: None.
    PiperOrigin-RevId: 204084726

--- src/main/java/com/google/devtools/build/lib/skyframe/PerBuildSyscallCache.java
+++ src/main/java/com/google/devtools/build/lib/skyframe/PerBuildSyscallCache.java
@@ -82,7 +82,7 @@ public class PerBuildSyscallCache implements UnixGlob.FilesystemCalls {
       }
       CacheBuilder<Object, Object> readdirCacheBuilder = CacheBuilder.newBuilder();
       if (maxReaddirs != UNSET) {
-        readdirCacheBuilder = readdirCacheBuilder.maximumSize(maxStats);
+        readdirCacheBuilder = readdirCacheBuilder.maximumSize(maxReaddirs);
       }
       if (concurrencyLevel != UNSET) {
         statCacheBuilder = statCacheBuilder.concurrencyLevel(concurrencyLevel);
