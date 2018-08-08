78712b1d2d80c32ddff769eaedfdd5cbcf2f868f
iirina <elenairina@google.com>
2018-08-08 15:34:31 +0200

    collect_cc_coverage.sh: Added initial test and documentation.

--- tools/test/collect_cc_coverage.sh
+++ tools/test/collect_cc_coverage.sh
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-<<<<<<< HEAD
 # This script collects code coverage data for C++ sources, after the tests
 # were executed.
 #
