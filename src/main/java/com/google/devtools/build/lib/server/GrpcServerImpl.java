f05c0a48dc298ad3e90f3acb399cc53c2944a801
Benjamin Peterson <bp@benjamin.pe>
2018-08-08 05:29:34 -0700

    Add a option to disable idle gc.
    If a Bazel server is idle for 10 seconds, it unconditionally triggers a full-scale Java GC via System.gc(). This behavior doesn't have clear benefits and causes Bazel to steal resources from whatever the user does after invoking Bazel. This CL adds a startup option, --idle_server_tasks, to toggle the idle GC behavior.
    
    Also, add some logging for when idle GC is enabled, so it's easier to evaluate its effects. Example of logging:
    ```
    180718 17:43:04.609:I 247 [com.google.devtools.build.lib.server.IdleServerTasks.lambda$idle$0] [Idle GC] used: 157MB -> 15MB, committed: 421MB -> 422MB
    ```
    
    Fixes https://github.com/bazelbuild/bazel/issues/5589.
    
    Closes #5628.
    
    PiperOrigin-RevId: 207869996

--- src/main/java/com/google/devtools/build/lib/server/GrpcServerImpl.java
+++ src/main/java/com/google/devtools/build/lib/server/GrpcServerImpl.java
@@ -152,10 +152,17 @@ public class GrpcServerImpl implements RPCServer {
    */
   public static class Factory implements RPCServer.Factory {
     @Override
-    public RPCServer create(BlazeCommandDispatcher dispatcher, Clock clock, int port,
-      Path workspace, Path serverDirectory, int maxIdleSeconds) throws IOException {
+    public RPCServer create(
+        BlazeCommandDispatcher dispatcher,
+        Clock clock,
+        int port,
+        Path workspace,
+        Path serverDirectory,
+        int maxIdleSeconds,
+        boolean idleServerTasks)
+        throws IOException {
       return new GrpcServerImpl(
-          dispatcher, clock, port, workspace, serverDirectory, maxIdleSeconds);
+          dispatcher, clock, port, workspace, serverDirectory, maxIdleSeconds, idleServerTasks);
     }
   }
 
@@ -512,13 +519,21 @@ public class GrpcServerImpl implements RPCServer {
   private final String pidInFile;
   private final List<Path> filesToDeleteAtExit = new ArrayList<>();
   private final int port;
+  private final boolean doIdleServerTasks;
 
   private Server server;
   private IdleServerTasks idleServerTasks;
   boolean serving;
 
-  public GrpcServerImpl(BlazeCommandDispatcher dispatcher, Clock clock, int port,
-      Path workspace, Path serverDirectory, int maxIdleSeconds) throws IOException {
+  public GrpcServerImpl(
+      BlazeCommandDispatcher dispatcher,
+      Clock clock,
+      int port,
+      Path workspace,
+      Path serverDirectory,
+      int maxIdleSeconds,
+      boolean doIdleServerTasks)
+      throws IOException {
     Runtime.getRuntime().addShutdownHook(new Thread() {
       @Override
       public void run() {
@@ -555,20 +570,24 @@ public class GrpcServerImpl implements RPCServer {
 
     pidFileWatcherThread = new PidFileWatcherThread();
     pidFileWatcherThread.start();
-    idleServerTasks = new IdleServerTasks();
-    idleServerTasks.idle();
+    this.doIdleServerTasks = doIdleServerTasks;
+    idle();
   }
 
   private void idle() {
     Preconditions.checkState(idleServerTasks == null);
-    idleServerTasks = new IdleServerTasks();
-    idleServerTasks.idle();
+    if (doIdleServerTasks) {
+      idleServerTasks = new IdleServerTasks();
+      idleServerTasks.idle();
+    }
   }
 
   private void busy() {
-    Preconditions.checkState(idleServerTasks != null);
-    idleServerTasks.busy();
-    idleServerTasks = null;
+    if (doIdleServerTasks) {
+      Preconditions.checkState(idleServerTasks != null);
+      idleServerTasks.busy();
+      idleServerTasks = null;
+    }
   }
 
   private static String generateCookie(SecureRandom random, int byteCount) {
