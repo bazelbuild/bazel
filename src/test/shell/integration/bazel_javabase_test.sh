4398741a3f4291b452de2026b18d85ba9b34dba3
Laszlo Csomor <laszlocsomor@google.com>
2018-08-10 17:21:01 +0200

    Windows,tests: port bazel_javabase_test
    //src/test/shell/integration:bazel_javabase_test
    now runs on Windows.
    
    See https://github.com/bazelbuild/bazel/issues/4292
    
    Change-Id: I1e3f0de7cc56fb5bab07e18c36f4f29947c2b41e

--- src/test/shell/integration/bazel_javabase_test.sh
+++ src/test/shell/integration/bazel_javabase_test.sh
@@ -15,10 +15,45 @@
 
 # Tests that our --host_javabase startup selection algorithm works.
 
-# Load the test setup defined in the parent directory
-CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "${CURRENT_DIR}/../integration_test_setup.sh" \
+# --- begin runfiles.bash initialization ---
+# Copy-pasted from Bazel's Bash runfiles library (tools/bash/runfiles/runfiles.bash).
+set -euo pipefail
+if [[ ! -d "${RUNFILES_DIR:-/dev/null}" && ! -f "${RUNFILES_MANIFEST_FILE:-/dev/null}" ]]; then
+  if [[ -f "$0.runfiles_manifest" ]]; then
+    export RUNFILES_MANIFEST_FILE="$0.runfiles_manifest"
+  elif [[ -f "$0.runfiles/MANIFEST" ]]; then
+    export RUNFILES_MANIFEST_FILE="$0.runfiles/MANIFEST"
+  elif [[ -f "$0.runfiles/bazel_tools/tools/bash/runfiles/runfiles.bash" ]]; then
+    export RUNFILES_DIR="$0.runfiles"
+  fi
+fi
+if [[ -f "${RUNFILES_DIR:-/dev/null}/bazel_tools/tools/bash/runfiles/runfiles.bash" ]]; then
+  source "${RUNFILES_DIR}/bazel_tools/tools/bash/runfiles/runfiles.bash"
+elif [[ -f "${RUNFILES_MANIFEST_FILE:-/dev/null}" ]]; then
+  source "$(grep -m1 "^bazel_tools/tools/bash/runfiles/runfiles.bash " \
+            "$RUNFILES_MANIFEST_FILE" | cut -d ' ' -f 2-)"
+else
+  echo >&2 "ERROR: cannot find @bazel_tools//tools/bash/runfiles:runfiles.bash"
+  exit 1
+fi
+# --- end runfiles.bash initialization ---
+
+source "$(rlocation "io_bazel/src/test/shell/integration_test_setup.sh")" \
   || { echo "integration_test_setup.sh not found!" >&2; exit 1; }
+
+case "$(uname -s | tr [:upper:] [:lower:])" in
+msys*|mingw*|cygwin*)
+  declare -r is_windows=true
+  ;;
+*)
+  declare -r is_windows=false
+  ;;
+esac
+
+if "$is_windows"; then
+  export MSYS_NO_PATHCONV=1
+  export MSYS2_ARG_CONV_EXCL="*"
+fi
 
 function test_use_depot_javabase() {
   bazel --batch version >& $TEST_log || fail "Couldn't run ${PRODUCT_NAME}"
@@ -27,9 +62,17 @@ function test_use_depot_javabase() {
 }
 
 function test_fallback_depot_javabase() {
-  bazel --batch --host_javabase=/does/not/exist version >& $TEST_log ||
-    (expect_log "Couldn't find java at" &&
-     expect_not_log "Problem with java installation")
+  if is_windows; then
+    local -r path=A:/$FUNCNAME/does/not/exist
+  else
+    local -r path=/$FUNCNAME/does/not/exist
+  fi
+  if bazel --batch --host_javabase=$path version >& $TEST_log; then
+    fail "expected failure"
+  else
+    expect_log "Couldn't find java at"
+    expect_not_log "Problem with java installation"
+  fi
 }
 
 run_suite "Tests of specifying custom javabase."
