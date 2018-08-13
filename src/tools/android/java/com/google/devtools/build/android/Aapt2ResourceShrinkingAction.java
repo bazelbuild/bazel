18e76da46b0c15d3309ac9d41f16fbd0468bc672
corysmith <corysmith@google.com>
2018-08-13 13:24:50 -0700

    Enable proto based resource shrinking.
    RELNOTES: None
    PiperOrigin-RevId: 208532627

--- src/tools/android/java/com/google/devtools/build/android/Aapt2ResourceShrinkingAction.java
+++ src/tools/android/java/com/google/devtools/build/android/Aapt2ResourceShrinkingAction.java
@@ -63,7 +63,7 @@ public class Aapt2ResourceShrinkingAction {
   public static final class Aapt2ShrinkOptions extends OptionsBase {
     @Option(
         name = "useProtoApk",
-        defaultValue = "false",
+        defaultValue = "true",
         category = "config",
         documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
         effectTags = {OptionEffectTag.UNKNOWN},
