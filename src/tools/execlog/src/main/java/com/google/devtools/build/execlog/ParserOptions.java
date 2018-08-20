943c17b95f7d89450b70cd3f3712058c0beb04b4
Googler <noreply@google.com>
2018-08-20 09:45:53 -0700

    Expanding execution log parser to reorder a log for a text diff with another.
    RELNOTES: Execution Log Parser can now, when printing it out, reorder the
    actions for easier text diffs
    PiperOrigin-RevId: 209433386

--- src/tools/execlog/src/main/java/com/google/devtools/build/execlog/ParserOptions.java
+++ src/tools/execlog/src/main/java/com/google/devtools/build/execlog/ParserOptions.java
@@ -18,19 +18,39 @@ import com.google.devtools.common.options.Option;
 import com.google.devtools.common.options.OptionDocumentationCategory;
 import com.google.devtools.common.options.OptionEffectTag;
 import com.google.devtools.common.options.OptionsBase;
+import java.util.List;
 
 /** Options for execution log parser. */
 public class ParserOptions extends OptionsBase {
   @Option(
-    name = "log_path",
-    defaultValue = "null",
-    category = "logging",
-    documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
-    effectTags = {OptionEffectTag.UNKNOWN},
-    help = "Location of the log file to parse."
-  )
+      name = "log_path",
+      defaultValue = "null",
+      category = "logging",
+      documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
+      effectTags = {OptionEffectTag.UNKNOWN},
+      allowMultiple = true,
+      help =
+          "Location of the log file to parse. If a second value is specified, both files will be"
+              + " converted. The first log will be processed as-is. The actions in the second log"
+              + " will be reordered to match the first. An action will be matched if the first"
+              + " file contains an action with the same name of the first output. Any actions"
+              + " that cannot be matched to the first file will appear at the end of the log. Note"
+              + " that this reordering fascilitates easier text-based comparisons, but may break"
+              + " any logical order of the actions.")
+  public List<String> logPath;
 
-  public String logPath;
+  @Option(
+      name = "output_path",
+      defaultValue = "null",
+      category = "logging",
+      documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
+      effectTags = {OptionEffectTag.UNKNOWN},
+      allowMultiple = true,
+      help =
+          "Location where to put the output(s). If left empty, the log will be output to stdout."
+              + " If two log paths are specified, needs to be specified and have exactly two"
+              + " paths.")
+  public List<String> outputPath;
 
   @Option(
       name = "restrict_to_runner",
