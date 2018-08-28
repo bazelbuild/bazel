56d125b9a4c8bf9a18bb83e473bc07759c708087
Klaus Aehlig <aehlig@google.com>
2018-08-28 03:13:11 -0700

    experimental UI: in a test group mark non-executing actions
    When several test actions are grouped into a single one line in the status bar,
    mark non-running actions specially, so that the list of times does not give
    the wrong impression that all those actions are already executing for that
    long.
    
    Change-Id: I9690f125d442c54a6100bc414ada4b5f2ffb2e33
    PiperOrigin-RevId: 210515074

--- src/main/java/com/google/devtools/build/lib/runtime/ExperimentalStateTracker.java
+++ src/main/java/com/google/devtools/build/lib/runtime/ExperimentalStateTracker.java
@@ -387,7 +387,7 @@ class ExperimentalStateTracker {
     // anyway, then show at least one sample.
     int remainingWidth = desiredWidth - message.length() - postfix.length();
     if (remainingWidth < 0) {
-      remainingWidth = 5;
+      remainingWidth = "[1s], ".length() + 1;
     }
 
     String sep = "";
@@ -400,7 +400,8 @@ class ExperimentalStateTracker {
       }
       long nanoRuntime = nanoTime - actionState.nanoStartTime;
       long runtimeSeconds = nanoRuntime / NANOS_PER_SECOND;
-      String text = sep + runtimeSeconds + "s";
+      String text =
+          actionState.executing ? sep + runtimeSeconds + "s" : sep + "[" + runtimeSeconds + "s]";
       if (remainingWidth < text.length()) {
         allReported = false;
         break;
