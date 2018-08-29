6f8e36be464829d9cd36e7692653e4cd99d344e9
Klaus Aehlig <aehlig@google.com>
2018-08-29 07:45:22 -0700

    getPackagesUnderDirectory: drop irrelevant blacklisting
    ...instead of asserting this is not the case. In this way, the simple
    blacklisting mechanism (of simply taking all entries) works.
    
    Fixes #5974
    
    Change-Id: I73a4a172ab3a190c69794be4fcbec54312e1fcf9
    PiperOrigin-RevId: 210719889

--- src/main/java/com/google/devtools/build/lib/skyframe/EnvironmentBackedRecursivePackageProvider.java
+++ src/main/java/com/google/devtools/build/lib/skyframe/EnvironmentBackedRecursivePackageProvider.java
@@ -144,12 +144,21 @@ public final class EnvironmentBackedRecursivePackageProvider
     if (blacklistedSubdirectories.contains(directory)) {
       return ImmutableList.of();
     }
-    PathFragment.checkAllPathsAreUnder(blacklistedSubdirectories, directory);
+    ImmutableSet filteredBlacklistedSubdirectories =
+        ImmutableSet.copyOf(
+            Iterables.filter(
+                blacklistedSubdirectories,
+                path -> !path.equals(directory) && path.startsWith(directory)));
 
     LinkedHashSet<PathFragment> packageNames = new LinkedHashSet<>();
     for (Root root : roots) {
-      RecursivePkgValue lookup = (RecursivePkgValue) env.getValue(RecursivePkgValue.key(
-          repository, RootedPath.toRootedPath(root, directory), blacklistedSubdirectories));
+      RecursivePkgValue lookup =
+          (RecursivePkgValue)
+              env.getValue(
+                  RecursivePkgValue.key(
+                      repository,
+                      RootedPath.toRootedPath(root, directory),
+                      filteredBlacklistedSubdirectories));
       if (lookup == null) {
         // Typically a null value from Environment.getValue(k) means that either the key k is
         // missing a dependency or an exception was thrown during evaluation of k. Here, if this
