4548cb594a1e7706c3d3ed5ec4b23a265a69cd67
iirina <elenairina@google.com>
2018-08-30 11:22:57 -0700

    Addressed code review comments
    and changed the logic of coverage result assertion. The results of each
    source file must be asserted separately because lcov does not guarantee
    any particular order. The order can differ for example based on OS or
    version. The source file order in the coverage report is not relevant.

--- src/test/shell/bazel/bazel_cc_code_coverage_test.sh
+++ src/test/shell/bazel/bazel_cc_code_coverage_test.sh
@@ -29,9 +29,8 @@ source "${CURRENT_DIR}/../integration_test_setup.sh" \
 # These are the variables needed by tools/test/collect_cc_coverage.sh
 # They will be properly sub-shelled when invoking the script.
 
-# Directory containing gcno and gcda files. It has to be different
-# than $PWD and $ROOT for the test to be accurate.
-readonly COVERAGE_DIR_VAR="${PWD}/my_coverage_dir"
+# Directory containing gcno and gcda files.
+readonly COVERAGE_DIR_VAR="${PWD}/coverage_dir"
 # Location of gcov.
 readonly COVERAGE_GCOV_PATH_VAR="${PWD}/mygcov"
 # Location from where the code coverage collection was invoked.
@@ -46,6 +45,17 @@ readonly COLLECT_CC_COVERAGE_SCRIPT=tools/test/collect_cc_coverage.sh
 
 # Setup to be run for every test.
 function set_up() {
+   mkdir -p "${COVERAGE_DIR_VAR}"
+
+  # COVERAGE_DIR has to be different than ROOT and PWD for the test to be
+  # accurate.
+  # Checking if the files have the same deviceID and inode is enough to
+  # determine if they are the same.
+  [[ "$(stat -c "%d:%i" ${COVERAGE_DIR_VAR})" == "$(stat -c "%d:%i" ${ROOT_VAR})" ]] \
+      && fail "COVERAGE_DIR_VAR must be different than ROOT_VAR"
+  [[ "$(stat -c "%d:%i" ${COVERAGE_DIR_VAR})" == "$(stat -c "%d:%i" ${PWD})" ]] \
+      && fail "COVERAGE_DIR_VAR must be different than PWD"
+
   # The script expects gcov to be at $COVERAGE_GCOV_PATH.
   cp $( which gcov ) "$COVERAGE_GCOV_PATH_VAR"
 
@@ -56,8 +66,8 @@ function set_up() {
 
   # All generated .gcno files need to be in the manifest otherwise
   # the coverage report will be incomplete.
-  echo "coverage_srcs/a.gcno" >> "$COVERAGE_MANIFEST_VAR"
   echo "coverage_srcs/t.gcno" >> "$COVERAGE_MANIFEST_VAR"
+  echo "coverage_srcs/a.gcno" >> "$COVERAGE_MANIFEST_VAR"
 
   # Create the CC sources.
   mkdir -p coverage_srcs/
@@ -86,24 +96,23 @@ int main(void) {
 }
 EOF
 
-  generate_instrumented_binary coverage_srcs/test coverage_srcs/a.h \
-      coverage_srcs/a.cc coverage_srcs/t.cc
-
-  # g++ generates the notes files in the current directory. The documentation
-  # (https://gcc.gnu.org/onlinedocs/gcc/Gcov-Data-Files.html#Gcov-Data-Files)
-  # says they are placed in the same directory as the object file, but they
-  # are not. Therefore we move them in the same directory.
-  mv *.gcno coverage_srcs/
-
-  generate_gcda_file ./coverage_srcs/test
+  generate_and_execute_instrumented_binary coverage_srcs/test \
+      "$COVERAGE_DIR_VAR/coverage_srcs" \
+      coverage_srcs/a.h coverage_srcs/a.cc coverage_srcs/t.cc
 }
 
-# Reads the list of arguments provided by the caller (using $@) and uses them
-# to produce an instrumented binary using g++. At the same time it generates
+# Generates and executes an instrumented binary:
+#
+# Step 1: Reads the list of arguments provided by the caller (using $@) and uses them
+# to produce an instrumented binary using g++. This step also generates
 # the notes (.gcno) files.
+#
+# Step 2: Executes the instrumented binary. This step also generates the
+# profile data files.
 # - path_to_binary destination of the binary produced by g++
-function generate_instrumented_binary() {
+function generate_and_execute_instrumented_binary() {
   local path_to_binary="${1}"; shift
+  local gcda_directory="${1}"; shift
   # -fprofile-arcs   Instruments $path_to_binary. During execution the binary
   #                  records code coverage information.
   # -ftest-coverage  Produces a notes (.gcno) file that coverage utilities
@@ -113,29 +122,27 @@ function generate_instrumented_binary() {
   # The profile data files need to be at a specific location where the C++
   # coverage scripts expects them to be ($COVERAGE_DIR/path/to/sources/).
   g++ -fprofile-arcs -ftest-coverage \
-      -fprofile-dir="$COVERAGE_DIR_VAR/coverage_srcs" \
-      "$@" -o "$path_to_binary"  && return 0
-  fail "Couldn't produce the instrumented binary for $@ \
-      with path_to_binary $path_to_binary"
-  return 1
-}
+      -fprofile-dir="$gcda_directory" \
+      "$@" -o "$path_to_binary"  \
+       || fail "Couldn't produce the instrumented binary for $@ \
+            with path_to_binary $path_to_binary"
 
-# Executes an instrumented binary and generates the profile data (.gcda) file.
-# The profile data file is placed in the directory specified by -fprofile-dir
-# that was used when compiling the sources, or in the current directory if the
-# flag was not specified.
-# - path_to_binary path of instrumented binary
-function generate_gcda_file() {
-  local path_to_binary="${1}"
-  "$path_to_binary" && return 0
-  fail "Couldn't execute the instrumented binary $path_to_binary"
-  return 1
+   # g++ generates the notes files in the current directory. The documentation
+   # (https://gcc.gnu.org/onlinedocs/gcc/Gcov-Data-Files.html#Gcov-Data-Files)
+   # says they are placed in the same directory as the object file, but they
+   # are not. Therefore we move them in the same directory.
+   mv a.gcno coverage_srcs/a.gcno
+   mv t.gcno coverage_srcs/t.gcno
+
+   # Execute the instrumented binary and generates the profile data (.gcda) file.
+   # The profile data file is placed in $gcda_directory.
+  "$path_to_binary" || fail "Couldn't execute the instrumented binary $path_to_binary"
 }
 
 function tear_down() {
-  rm "$COVERAGE_MANIFEST_VAR"
-  rm "$COVERAGE_GCOV_PATH_VAR"
-  rm "$COVERAGE_OUTPUT_FILE_VAR"
+  rm -f "$COVERAGE_MANIFEST_VAR"
+  rm -f "$COVERAGE_GCOV_PATH_VAR"
+  rm -f "$COVERAGE_OUTPUT_FILE_VAR"
   rm -rf "$COVERAGE_DIR_VAR"
   rm -rf coverage_srcs/
 }
@@ -158,46 +165,68 @@ function run_coverage() {
 
 function test_cc_test_coverage() {
   run_coverage > "$TEST_log"
+  assert_coverage_result
+}
 
-  # After running the test in coverage_srcs/t.cc, the sources covered are the
-  # test itself and the source file a.cc.
-  # For more details about the lcov format see
-  # http://ltp.sourceforge.net/coverage/lcov/geninfo.1.php
-  # The expected result can be constructed manually by following the lcov
-  # documentation and manually checking what lines of code are covered when
-  # running the test.
-  cat <<EOF > expected_result.dat
-TN:
-SF:coverage_srcs/a.cc
-FN:3,_Z1ab
-FNDA:1,_Z1ab
-FNF:1
-FNH:1
-DA:3,1
-DA:4,1
-DA:5,1
-DA:7,0
-LF:4
-LH:3
-end_of_record
-TN:
-SF:coverage_srcs/t.cc
-FN:4,main
-FNDA:1,main
-FNF:1
-FNH:1
-DA:4,1
-DA:5,1
-DA:6,1
-LF:3
-LH:3
-end_of_record
-EOF
+# Asserts that the C++ coverage output is correct.
+#
+# The C++ coverage script places the final coverage result in
+# $COVERAGE_OUTPUT_FILE.
+#
+# The covered files are coverage_srcs/a.cc and coverage_srcs/t.cc.
+function assert_coverage_result() {
+   # The expected total number of lines of COVERAGE_OUTPUT_FILE
+   # is 25. This number can be computed by counting the number
+   # of lines in the variables declared below $expected_result_a_cc
+   # and $expected_result_t_cc, adding 2 lines of empty "TN:" that
+   # is generated for each of the source files.
+   local nr_of_lines=$(wc -l < "$COVERAGE_OUTPUT_FILE_VAR")
+   [[ "$nr_of_lines" == 25 ]] || \
+      fail "Number of lines $nr_of_lines is different than 25"
 
-  # tools/test/collect_cc_coverage.sh places the coverage result in
-  # $COVERAGE_OUTPUT_FILE
-  diff -u expected_result.dat "$COVERAGE_OUTPUT_FILE_VAR" >> "$TEST_log" \
-    || fail "Coverage output file is different than the expected file"
+    # After running the test in coverage_srcs/t.cc, the sources covered are the
+    # test itself and the source file a.cc.
+    # For more details about the lcov format see
+    # http://ltp.sourceforge.net/coverage/lcov/geninfo.1.php
+    # The expected result can be constructed manually by following the lcov
+    # documentation and manually checking what lines of code are covered when
+    # running the test.
+    declare -a expected_result_a_cc=("SF:coverage_srcs/a.cc" \
+    "FN:3,_Z1ab" \
+    "FNDA:1,_Z1ab" \
+    "FNF:1" \
+    "FNH:1" \
+    "DA:3,1" \
+    "DA:4,1" \
+    "DA:5,1" \
+    "DA:7,0" \
+    "LF:4" \
+    "LH:3" \
+    "end_of_record")
+
+    declare -a expected_result_t_cc=("SF:coverage_srcs/t.cc" \
+    "FN:4,main" \
+    "FNDA:1,main" \
+    "FNF:1" \
+    "FNH:1" \
+    "DA:4,1" \
+    "DA:5,1" \
+    "DA:6,1" \
+    "LF:3" \
+    "LH:3" \
+    "end_of_record")
+
+    # Assert that the coverage output file contains the coverage data for the
+    # two cc files.
+    # The results must be asserted separately because the coverage tools (e.g.
+    # lcov, gcov) do not guarantee any particular order. Different versions
+    # can also output the coverage for each source file in a different order.
+    # The order is not important.
+    assert_consecutive_lines_in_file "$COVERAGE_OUTPUT_FILE_VAR" "${expected_result_a_cc[@]}"
+    assert_consecutive_lines_in_file "$COVERAGE_OUTPUT_FILE_VAR" "${expected_result_t_cc[@]}"
+    # Assert that the coverage output file contains the line "TN:" twice
+    # (once for every source).
+    assert_contains_n "TN:" "$COVERAGE_OUTPUT_FILE_VAR" 2
 }
 
 run_suite "Testing tools/test/collect_cc_coverage.sh"
