22d761ab42dfb1b131f1facbf490ccdb6c17b89c
Laszlo Csomor <laszlocsomor@google.com>
2018-09-03 16:37:11 +0200

    Update protobuf to 3.6.1 -- add new files
    Change-Id: I065ff758219b14e291e0efeb4bfd5bc98d822624

---
+++ third_party/protobuf/3.6.1/cmake/protoc.cmake
@@ -0,0 +1,7 @@
+set(protoc_files
+  ${protobuf_source_dir}/src/google/protobuf/compiler/main.cc
+)
+
+add_executable(protoc ${protoc_files})
+target_link_libraries(protoc libprotobuf libprotoc)
+add_executable(protobuf::protoc ALIAS protoc)
