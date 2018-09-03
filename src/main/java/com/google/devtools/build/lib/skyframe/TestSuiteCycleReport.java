22859d844428c6a1bf08e9f3014033ac240614ee
Benjamin Peterson <bp@benjamin.pe>
2018-09-03 13:54:12 -0700

    Add cycle reporting for test_suite expansion.
    Test suite cycles now produce nice cycle errors like this:
    
    ERROR /workspace/BUILD:1:41: in test_suite rule //:b: cycle in dependency graph:
    .-> //:b
    |   //:a
    `-- //:b
    
    Before, test_suite cycles resulted in a crash.
    
    Fixes https://github.com/bazelbuild/bazel/issues/5851.

---
+++ src/main/java/com/google/devtools/build/lib/skyframe/TestSuiteCycleReport.java
@@ -0,0 +1,43 @@
+// Copyright 2018 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.devtools.build.lib.skyframe;
+
+import com.google.devtools.build.lib.cmdline.Label;
+import com.google.devtools.build.lib.pkgcache.PackageProvider;
+import com.google.devtools.build.lib.skyframe.TestsInSuiteValue.TestsInSuiteKey;
+import com.google.devtools.build.skyframe.CycleInfo;
+import com.google.devtools.build.skyframe.SkyKey;
+
+/** Reports cycles occurring in during the expansion of <code>test_suite</code> rules. */
+class TestSuiteCycleReporter extends AbstractLabelCycleReporter {
+
+  public TestSuiteCycleReporter(PackageProvider packageProvider) {
+    super(packageProvider);
+  }
+
+  @Override
+  protected boolean canReportCycle(SkyKey topLevelKey, CycleInfo cycleInfo) {
+    return cycleInfo.getCycle().stream().allMatch(TestsInSuiteKey.class::isInstance);
+  }
+
+  @Override
+  protected boolean shouldSkip(SkyKey key) {
+    return !(key instanceof TestsInSuiteKey);
+  }
+
+  @Override
+  protected Label getLabel(SkyKey key) {
+    return ((TestsInSuiteKey) key).getTestSuiteLabel();
+  }
+}
