a3ae3e2474bc5542161750fb8a358db5c5026908
gregce <gregce@google.com>
2018-09-14 10:56:34 -0700

    Update outdated --config documentation.
    Fixes #6006
    
    PiperOrigin-RevId: 213009242

--- src/main/java/com/google/devtools/build/lib/runtime/CommonCommandOptions.java
+++ src/main/java/com/google/devtools/build/lib/runtime/CommonCommandOptions.java
@@ -59,10 +59,9 @@ public class CommonCommandOptions extends OptionsBase {
     help =
         "Selects additional config sections from the rc files; for every <command>, it "
             + "also pulls in the options from <command>:<config> if such a section exists; "
-            + "if the section does not exist, this flag is ignored. "
-            + "Note that it is currently only possible to provide these options on the "
-            + "command line, not in the rc files. The config sections and flag combinations "
-            + "they are equivalent to are located in the tools/*.blazerc config files."
+            + "if this section doesn't exist in any .rc file, Blaze fails with an error. "
+            + "The config sections and flag combinations they are equivalent to are "
+            + "located in the tools/*.blazerc config files."
   )
   public List<String> configs;
 
