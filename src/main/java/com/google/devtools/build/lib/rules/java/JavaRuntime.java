054a28b3c472ab9c1145727e860f308ac083f57e
cushon <cushon@google.com>
2018-09-17 01:32:09 -0700

    Add a java_runtime.java attribute
    which can be used to specify the artifact for the bin/java executable,
    and which is used to fix the computation of the paths to java_home.
    
    PiperOrigin-RevId: 213232464

--- src/main/java/com/google/devtools/build/lib/rules/java/JavaRuntime.java
+++ src/main/java/com/google/devtools/build/lib/rules/java/JavaRuntime.java
@@ -30,6 +30,8 @@ import com.google.devtools.build.lib.analysis.TemplateVariableInfo;
 import com.google.devtools.build.lib.analysis.configuredtargets.RuleConfiguredTarget.Mode;
 import com.google.devtools.build.lib.cmdline.Label;
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
+import com.google.devtools.build.lib.collect.nestedset.NestedSetBuilder;
+import com.google.devtools.build.lib.packages.RuleClass.ConfiguredTargetFactory.RuleErrorException;
 import com.google.devtools.build.lib.util.OsUtils;
 import com.google.devtools.build.lib.vfs.PathFragment;
 
@@ -42,13 +44,13 @@ public class JavaRuntime implements RuleConfiguredTargetFactory {
   @Override
   public ConfiguredTarget create(RuleContext ruleContext)
       throws InterruptedException, RuleErrorException, ActionConflictException {
-    NestedSet<Artifact> filesToBuild =
-        PrerequisiteArtifacts.nestedSet(ruleContext, "srcs", Mode.TARGET);
+    NestedSetBuilder<Artifact> filesBuilder = NestedSetBuilder.stableOrder();
+    filesBuilder.addTransitive(PrerequisiteArtifacts.nestedSet(ruleContext, "srcs", Mode.TARGET));
     PathFragment javaHome = defaultJavaHome(ruleContext.getLabel());
     if (ruleContext.attributes().isAttributeValueExplicitlySpecified("java_home")) {
       PathFragment javaHomeAttribute =
           PathFragment.create(ruleContext.getExpander().expand("java_home"));
-      if (!filesToBuild.isEmpty() && javaHomeAttribute.isAbsolute()) {
+      if (!filesBuilder.isEmpty() && javaHomeAttribute.isAbsolute()) {
         ruleContext.ruleError("'java_home' with an absolute path requires 'srcs' to be empty.");
       }
 
@@ -63,6 +65,24 @@ public class JavaRuntime implements RuleConfiguredTargetFactory {
     PathFragment javaBinaryRunfilesPath = getRunfilesJavaExecutable(
         javaHome, ruleContext.getLabel());
 
+    Artifact java = ruleContext.getPrerequisiteArtifact("java", Mode.TARGET);
+    if (java != null) {
+      if (javaHome.isAbsolute()) {
+        ruleContext.ruleError("'java_home' with an absolute path requires 'java' to be empty.");
+      }
+      javaBinaryExecPath = java.getExecPath();
+      javaBinaryRunfilesPath = java.getRunfilesPath();
+      if (!isJavaBinary(javaBinaryExecPath)) {
+        ruleContext.ruleError("the path to 'java' must end in 'bin/java'.");
+      }
+      if (ruleContext.hasErrors()) {
+        return null;
+      }
+      javaHome = javaBinaryExecPath.getParentDirectory().getParentDirectory();
+      filesBuilder.add(java);
+    }
+
+    NestedSet<Artifact> filesToBuild = filesBuilder.build();
     NestedSet<Artifact> middleman =
         CompilationHelper.getAggregatingMiddleman(
             ruleContext, Actions.escapeLabel(ruleContext.getLabel()), filesToBuild);
@@ -89,6 +109,11 @@ public class JavaRuntime implements RuleConfiguredTargetFactory {
         .addNativeDeclaredProvider(new JavaRuntimeToolchainInfo(javaRuntime))
         .addNativeDeclaredProvider(templateVariableInfo)
         .build();
+  }
+
+  private static boolean isJavaBinary(PathFragment path) {
+    return path.endsWith(PathFragment.create("bin/java"))
+        || path.endsWith(PathFragment.create("bin/java.exe"));
   }
 
   static PathFragment defaultJavaHome(Label javabase) {
