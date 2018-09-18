f358961125ae909508782f8f1100fb3f8c4f7971
tomlu <tomlu@google.com>
2018-09-18 14:37:18 -0700

    Skip uploading source files to BEP.
    Instead of trusting Artifact#isSource, we resolve all symlinks and compare against the package path.
    
    RELNOTES: None
    PiperOrigin-RevId: 213519335

--- src/main/java/com/google/devtools/build/lib/runtime/NamedArtifactGroup.java
+++ src/main/java/com/google/devtools/build/lib/runtime/NamedArtifactGroup.java
@@ -64,10 +64,7 @@ class NamedArtifactGroup implements BuildEvent {
       if (artifact.isMiddlemanArtifact()) {
         continue;
       }
-      artifacts.add(
-          new LocalFile(
-              pathResolver.toPath(artifact),
-              artifact.isSourceArtifact() ? LocalFileType.SOURCE : LocalFileType.OUTPUT));
+      artifacts.add(new LocalFile(pathResolver.toPath(artifact), LocalFileType.OUTPUT));
     }
     return artifacts.build();
   }
--- src/main/java/com/google/devtools/build/lib/skyframe/SkyframeExecutor.java
+++ src/main/java/com/google/devtools/build/lib/skyframe/SkyframeExecutor.java
@@ -1036,6 +1036,10 @@ public abstract class SkyframeExecutor implements WalkableGraphFactory {
     return pkgLocator.get().getPathEntries();
   }
 
+  public AtomicReference<PathPackageLocator> getPackageLocator() {
+    return pkgLocator;
+  }
+
   protected abstract void invalidate(Predicate<SkyKey> pred);
 
   private static boolean compatibleFileTypes(Dirent.Type oldType, FileStateType newType) {
