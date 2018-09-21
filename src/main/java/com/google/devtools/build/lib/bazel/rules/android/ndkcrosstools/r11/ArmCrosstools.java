6d5229fac0c27ca1529a9f47d669d0742d84fe02
Jason Gavris <jgavris@butterflynetinc.com>
2018-09-21 11:15:25 -0400

    [Android] Add missing -g for debug compilation mode to ARM crosstools
    Resolves #6193

--- src/main/java/com/google/devtools/build/lib/bazel/rules/android/ndkcrosstools/r11/ArmCrosstools.java
+++ src/main/java/com/google/devtools/build/lib/bazel/rules/android/ndkcrosstools/r11/ArmCrosstools.java
@@ -108,6 +108,7 @@ class ArmCrosstools {
         .addCompilationModeFlags(CompilationModeFlags.newBuilder()
             .setMode(CompilationMode.DBG)
             .addCompilerFlag("-O0")
+            .addCompilerFlag("-g")
             .addCompilerFlag("-UNDEBUG")
             .addCompilerFlag("-fno-omit-frame-pointer")
             .addCompilerFlag("-fno-strict-aliasing"));
