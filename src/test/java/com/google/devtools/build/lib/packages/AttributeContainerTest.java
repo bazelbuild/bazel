2a9f1abe295124951ac1444424f23166eed998fd
michajlo <michajlo@google.com>
2018-09-26 13:31:13 -0700

    Remove unnecessary AttributeContainer bits
    setAttributeLocation(Attribute, Location) is only used in tests and doesn't add
    much on top of the public setAttributeLocation(int, Location) API. Delete it
    and inline the attribute index getting logic in the tests.
    
    Took the liberty of refactoring the tests a bit following me breaking them and
    trying to understand why.
    
    PiperOrigin-RevId: 214659408

--- src/test/java/com/google/devtools/build/lib/packages/AttributeContainerTest.java
+++ src/test/java/com/google/devtools/build/lib/packages/AttributeContainerTest.java
@@ -82,39 +82,42 @@ public class AttributeContainerTest {
   public void testAttributeLocation() throws Exception {
     Location location1 = newLocation();
     Location location2 = newLocation();
-    container.setAttributeLocation(attribute1, location1);
-    container.setAttributeLocation(attribute2, location2);
+    container.setAttributeLocation(ruleClass.getAttributeIndex(attribute1.getName()), location1);
+    container.setAttributeLocation(ruleClass.getAttributeIndex(attribute2.getName()), location2);
     assertThat(container.getAttributeLocation(attribute1.getName())).isEqualTo(location1);
     assertThat(container.getAttributeLocation(attribute2.getName())).isEqualTo(location2);
     assertThat(container.getAttributeLocation("nomatch")).isNull();
   }
+
 
   @Test
   public void testPackedState() throws Exception {
     Random rng = new Random();
     // The state packing machinery has special behavior at multiples of 8,
     // so set enough explicit values and locations to exercise that.
-    final int N = 17;
-    Attribute[] attributes = new Attribute[N];
-    for (int i = 0; i < N; ++i) {
-      attributes[i] = ruleClass.getAttribute(i);
+    final int numAttributes = 17;
+    Attribute[] attributes = new Attribute[numAttributes];
+    for (int attributeIndex = 0; attributeIndex < numAttributes; ++attributeIndex) {
+      attributes[attributeIndex] = ruleClass.getAttribute(attributeIndex);
     }
+
+    Location[] locations = new Location[numAttributes];
+    for (int attributeIndex = 0; attributeIndex < numAttributes; ++attributeIndex) {
+      locations[attributeIndex] = newLocation();
+    }
+    // Test relies on checking reference inequality
+    assertThat(locations[0] != locations[1]).isTrue();
+
     Object someValue = new Object();
-    Location[] locations = new Location[N];
-    for (int i = 0; i < N; ++i) {
-      locations[i] = newLocation();
-    }
-    assertThat(locations[0] != locations[1])
-        .isTrue(); // test relies on checking reference inequality
-    for (int explicitCount = 0; explicitCount <= N; ++explicitCount) {
-      for (int locationCount = 0; locationCount <= N; ++locationCount) {
+    for (int explicitCount = 0; explicitCount <= numAttributes; ++explicitCount) {
+      for (int locationCount = 0; locationCount <= numAttributes; ++locationCount) {
         AttributeContainer container = new AttributeContainer(ruleClass);
         // Shuffle the attributes each time through, to exercise
         // different stored indices and orderings.
         Collections.shuffle(Arrays.asList(attributes));
         // Also randomly interleave calls to the two setters.
-        int valuePassKey = rng.nextInt(1 << N);
-        int locationPassKey = rng.nextInt(1 << N);
+        int valuePassKey = rng.nextInt(1 << numAttributes);
+        int locationPassKey = rng.nextInt(1 << numAttributes);
         for (int pass = 0; pass <= 1; ++pass) {
           for (int i = 0; i < explicitCount; ++i) {
             if (pass == ((valuePassKey >> i) & 1)) {
@@ -123,18 +126,21 @@ public class AttributeContainerTest {
           }
           for (int i = 0; i < locationCount; ++i) {
             if (pass == ((locationPassKey >> i) & 1)) {
-              container.setAttributeLocation(attributes[i], locations[i]);
+              container.setAttributeLocation(
+                  ruleClass.getAttributeIndex(attributes[i].getName()),
+                  locations[i]);
             }
           }
         }
-        for (int i = 0; i < N; ++i) {
+
+        for (int i = 0; i < numAttributes; ++i) {
           boolean expected = i < explicitCount;
           assertThat(container.isAttributeValueExplicitlySpecified(attributes[i]))
               .isEqualTo(expected);
-        }
-        for (int i = 0; i < N; ++i) {
-          Location expected = i < locationCount ? locations[i] : null;
-          assertThat(container.getAttributeLocation(attributes[i].getName())).isSameAs(expected);
+
+          Location expectedLocation = i < locationCount ? locations[i] : null;
+          assertThat(container.getAttributeLocation(attributes[i].getName()))
+              .isSameAs(expectedLocation);
         }
       }
     }
