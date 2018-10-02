ac6a4640fd67f26868eba830ccd322b7dcd5cd51
cparsons <cparsons@google.com>
2018-10-02 11:26:47 -0700

    Add an "experimental" disclaimer to AnalysisFailureInfo.
    In practicality, this has no real effect on documentation, as this type remains undocumented (due to 'documented = false' on the annotation), and the object itself cannot be instantiated, but this ensures that developers reading the source code understand that AnalysisFailureInfo is experimental.
    
    RELNOTES: None.
    PiperOrigin-RevId: 215425471

--- src/main/java/com/google/devtools/build/lib/skylarkbuildapi/test/AnalysisFailureInfoApi.java
+++ src/main/java/com/google/devtools/build/lib/skylarkbuildapi/test/AnalysisFailureInfoApi.java
@@ -26,27 +26,27 @@ import com.google.devtools.build.lib.syntax.SkylarkSemantics.FlagIdentifier;
  */
 @SkylarkModule(
     name = "AnalysisFailureInfo",
-    doc =
-        "Encapsulates information about an analysis-phase error which would have occurred during "
-            + "a build. In most builds, an analysis-phase error would result in a build failure "
-            + "and the error description would be output to the console. However, if "
-            + "<code>--allow_analysis_failure</code> is set, targets which would otherwise fail in "
-            + "analysis will instead propagate an instance of this object (and no other provider "
-            + "instances). "
-            + ""
-            + "<p>Under <code>--allow_analysis_failure</code>, <code>AnalysisFailureInfo</code> "
-            + "objects are automatically re-propagated up a dependency tree using the following "
-            + "logic:<ul>"
-            + ""
-            + "<li>If a target fails but none of its direct dependencies propagated "
-            + "<code>AnalysisFailureInfo</code>, then propagate an instance of this "
-            + "provider containing an <code>AnalysisFailure</code> object describing the "
-            + "failure.</li> "
-            + ""
-            + "<li>If one or more of a target's dependencies propagated "
-            + "<code>AnalysisFailureInfo</code>, then propagate a provider with "
-            + "<code>causes</code> equal to the union of the <code>causes</code> of the "
-            + "dependencies.</li></ul>",
+    doc = "<b>Experimental. This API is experimental and subject to change at any time</b><p> "
+        + "Encapsulates information about an analysis-phase error which would have occurred during "
+        + "a build. In most builds, an analysis-phase error would result in a build failure "
+        + "and the error description would be output to the console. However, if "
+        + "<code>--allow_analysis_failure</code> is set, targets which would otherwise fail in "
+        + "analysis will instead propagate an instance of this object (and no other provider "
+        + "instances). "
+        + ""
+        + "<p>Under <code>--allow_analysis_failure</code>, <code>AnalysisFailureInfo</code> "
+        + "objects are automatically re-propagated up a dependency tree using the following "
+        + "logic:<ul>"
+        + ""
+        + "<li>If a target fails but none of its direct dependencies propagated "
+        + "<code>AnalysisFailureInfo</code>, then propagate an instance of this "
+        + "provider containing an <code>AnalysisFailure</code> object describing the "
+        + "failure.</li> "
+        + ""
+        + "<li>If one or more of a target's dependencies propagated "
+        + "<code>AnalysisFailureInfo</code>, then propagate a provider with "
+        + "<code>causes</code> equal to the union of the <code>causes</code> of the "
+        + "dependencies.</li></ul>",
     documented = false)
 public interface AnalysisFailureInfoApi extends SkylarkValue {
 
