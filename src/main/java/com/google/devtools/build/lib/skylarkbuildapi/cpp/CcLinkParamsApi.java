48b6643426a3d9cafd5b854fb49e4e637cdcb859
plf <plf@google.com>
2018-10-09 12:04:37 -0700

    C++: Renames cc_link_params.linkopts to user_link_flags
    This is to be consistent with other places in the API.
    
    RELNOTES:none
    PiperOrigin-RevId: 216397549

--- src/main/java/com/google/devtools/build/lib/skylarkbuildapi/cpp/CcLinkParamsApi.java
+++ src/main/java/com/google/devtools/build/lib/skylarkbuildapi/cpp/CcLinkParamsApi.java
@@ -35,7 +35,7 @@ import com.google.devtools.build.lib.syntax.SkylarkNestedSet;
     doc = "Parameters that affect linking actions.")
 public interface CcLinkParamsApi {
   @SkylarkCallable(
-      name = "linkopts",
+      name = "user_link_flags",
       documented = false,
       allowReturnNones = true,
       structField = true)
