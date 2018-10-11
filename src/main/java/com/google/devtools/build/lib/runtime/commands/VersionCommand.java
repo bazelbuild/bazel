938a53d3d54ede8132109df3c492c47afc63f5a6
ulfjack <ulfjack@google.com>
2018-10-11 09:46:59 -0700

    Add --gnu_format flag to the version command
    This is a prerequisite for #3671, which adds support for bazel -v, which will alias to the new format added here.
    
    PiperOrigin-RevId: 216711652

--- src/main/java/com/google/devtools/build/lib/runtime/commands/VersionCommand.java
+++ src/main/java/com/google/devtools/build/lib/runtime/commands/VersionCommand.java
@@ -13,6 +13,7 @@
 // limitations under the License.
 package com.google.devtools.build.lib.runtime.commands;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.devtools.build.lib.analysis.BlazeVersionInfo;
 import com.google.devtools.build.lib.analysis.NoBuildEvent;
 import com.google.devtools.build.lib.events.Event;
@@ -21,32 +22,68 @@ import com.google.devtools.build.lib.runtime.BlazeCommandResult;
 import com.google.devtools.build.lib.runtime.Command;
 import com.google.devtools.build.lib.runtime.CommandEnvironment;
 import com.google.devtools.build.lib.util.ExitCode;
+import com.google.devtools.common.options.Option;
+import com.google.devtools.common.options.OptionDocumentationCategory;
+import com.google.devtools.common.options.OptionEffectTag;
+import com.google.devtools.common.options.OptionsBase;
 import com.google.devtools.common.options.OptionsParser;
 import com.google.devtools.common.options.OptionsParsingResult;
+import java.io.IOException;
 
 /**
  * The 'blaze version' command, which informs users about the blaze version
  * information.
  */
 @Command(name = "version",
-         options = {},
+         options = { VersionCommand.VersionOptions.class },
          allowResidue = false,
          mustRunInWorkspace = false,
          help = "resource:version.txt",
          shortDescription = "Prints version information for %{product}.")
 public final class VersionCommand implements BlazeCommand {
+  /** Options for the "version" command. */
+  public static class VersionOptions extends OptionsBase {
+    @Option(
+      name = "gnu_format",
+      defaultValue = "false",
+      documentationCategory = OptionDocumentationCategory.OUTPUT_PARAMETERS,
+      effectTags = {OptionEffectTag.AFFECTS_OUTPUTS, OptionEffectTag.EXECUTION},
+      help =
+          "If set, write the version to stdout using the conventions described in the GNU"
+          + " standards."
+    )
+    public boolean gnuFormat;
+  }
+
   @Override
   public void editOptions(OptionsParser optionsParser) {}
 
   @Override
   public BlazeCommandResult exec(CommandEnvironment env, OptionsParsingResult options) {
-    BlazeVersionInfo info = BlazeVersionInfo.instance();
-    if (info.getSummary() == null) {
-      env.getReporter().handle(Event.error("Version information not available"));
+    env.getEventBus().post(new NoBuildEvent());
+    try {
+      env.getReporter().getOutErr().printOutLn(
+          getInfo(
+              env.getRuntime().getProductName(),
+              BlazeVersionInfo.instance(),
+              options.getOptions(VersionOptions.class).gnuFormat));
+    } catch (IOException e) {
+      env.getReporter().handle(Event.error(e.getMessage()));
       return BlazeCommandResult.exitCode(ExitCode.COMMAND_LINE_ERROR);
     }
-    env.getEventBus().post(new NoBuildEvent());
-    env.getReporter().getOutErr().printOutLn(info.getSummary());
     return BlazeCommandResult.exitCode(ExitCode.SUCCESS);
+  }
+
+  @VisibleForTesting
+  static String getInfo(String productName, BlazeVersionInfo info, boolean gnuFormat)
+      throws IOException {
+    if (info.getSummary() == null) {
+      throw new IOException("Version information not available");
+    }
+    if (gnuFormat) {
+      return productName + " " + (info.isReleasedBlaze() ? info.getVersion() : "no_version");
+    } else {
+      return info.getSummary();
+    }
   }
 }
