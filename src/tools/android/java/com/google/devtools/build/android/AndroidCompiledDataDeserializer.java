893e719801c58df3025474a6c5bc27ee3bcb3b4f
jingwen <jingwen@google.com>
2018-10-16 07:32:12 -0700

    Fix Android compiled data deserialization with aapt2 when used with RBE.
    Use `readFully()` to block and read the complete stream of data for the ResourceTable, instead of `read()`, which promises to only "read up to N bytes". Using `read()` can result in an incomplete stream and an unparseable input for protobuf when dealing with large AARs like `com.android.support:support-compat:28.0.0`.
    
    Details: https://github.com/bazelbuild/bazel/issues/6328#issuecomment-430048180
    
    Fixes https://github.com/bazelbuild/bazel/issues/6328
    
    RELNOTES: None.
    PiperOrigin-RevId: 217314300

--- src/tools/android/java/com/google/devtools/build/android/AndroidCompiledDataDeserializer.java
+++ src/tools/android/java/com/google/devtools/build/android/AndroidCompiledDataDeserializer.java
@@ -252,7 +252,7 @@ public class AndroidCompiledDataDeserializer implements AndroidDataDeserializer 
     Preconditions.checkArgument(alignedSize <= Integer.MAX_VALUE);
 
     byte[] tableBytes = new byte[(int) alignedSize];
-    resourceTableStream.read(tableBytes, 0, (int) alignedSize);
+    resourceTableStream.readFully(tableBytes, 0, (int) alignedSize);
     ResourceTable resourceTable = ResourceTable.parseFrom(tableBytes);
 
     readPackages(consumers, resourceTable);
