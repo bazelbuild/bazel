c98371ce6d522191b9e15225ec907bba3030ef7a
Laszlo Csomor <laszlocsomor@google.com>
2018-10-18 12:36:59 +0200

    Windows: add script to build a .msi package
    Also add a test, though there's no build rule to
    wrap it yet.
    
    The generated installer:
    - is a standard .msi installer that users are
      familiar with
    - installs Bazel at a short and safe path (no
      space in path)
    - updates the PATH envvar
    - handles upgrades and parallel installs:
      backwards-compatible versions can be upgraded
      (but not downgraded), while incompatible
      versions may be installed side by side
      (see guids.txt comments)
    
    Example use:
    
    ```
    c:\src\bazel>powershell scripts\packages\wix\make_msi.ps1 ..\bazel-releases\0.26.1\bazel-0.26.1-windows-x86_64.exe
    (...)
    INFO[make_msi_lib.ps1 11:41:33] Creating wixobj
    INFO[make_msi_lib.ps1 11:41:33] Creating msi
    INFO[make_msi_lib.ps1 11:41:42] Done: ..\bazel-releases\0.26.1\bazel-0.26.1-windows-x86_64.msi
    
    c:\src\bazel>start ..\bazel-releases\0.26.1\bazel-0.26.1-windows-x86_64.msi
    ```
    
    TODO(@laszlocsomor) before we can use this:
    - figure out if we should bundle vc_redist with
      the Bazel installer
    - figure out the UpgradeCode for such a combined
      bundle

---
+++ scripts/packages/wix/make_msi_lib.ps1
@@ -0,0 +1,216 @@
+﻿# Copyright 2019 The Bazel Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Set path constants. These are relative to the Bazel source tree root.
+$Icon = "site\images\favicon.ico"
+$ArtworkBig = "scripts\packages\wix\dialog.bmp"
+$ArtworkSmall = "scripts\packages\wix\banner.bmp"
+$Licence = "scripts\packages\wix\license.rtf"
+$Guids = "scripts\packages\wix\guids.txt"
+$Wxs = "scripts\packages\wix\bazelmsi.wxs"
+
+# Exit immediately when a Cmdlet fails.
+$ErrorActionPreference = "Stop"
+
+# Logs a message to stdout.
+function Log-Info {
+    Param([string]$msg)
+    $basename = $PSCommandPath.Substring($PSCommandPath.LastIndexOfAny("/\") + 1)
+    $time = (Get-Date).ToString("HH:mm:ss")
+    Write-Host "INFO[$basename $time] $msg"
+}
+
+function Replace-Slashes {
+    Param([string]$s)
+    return $s.Replace("/", "\")
+}
+
+# Computes the release name from the 'BazelExe'.
+# 'BazelExe' must already have been validated to match this regex.
+# 'ReleaseName' is like "0.28.0rc5"
+# 'Version' is the SemVer part of 'ReleaseName', e.g. "0.28.0"
+function Compute-RelaseNameAndVersion {
+    Param([Parameter(Mandatory=$true)][string]$BazelExe)
+
+    $rel = [regex]::Match($BazelExe, "bazel-([^-]+)-windows.*\.exe$").captures.groups[1].value
+    $ver = [regex]::Match($rel, "^([0-9]+\.[0-9]+\.[0-9]+)(rc[0-9]+)?$").captures.groups[1].value
+    return $rel, $ver
+}
+
+# Generates a new GUID, prints it as uppercase.
+# The WiX Toolkit expects uppercase GUIDs in the .wxs file.
+function Generate-Guid {
+    return [guid]::NewGuid().ToString().ToUpper()
+}
+
+# Returns the UpgradeGuid for this release.
+function Get-UpgradeGuid {
+    Param([Parameter(Mandatory=$true)][string]$release_name)
+
+    $d = @{}
+    $result = $null
+    foreach ($line in Get-Content -Path $Guids) {
+        $line = "$line".Split('#')[0]
+        if ($line) {
+            $k, $v = $line.Split(' ', 2)
+            # Ensure all version prefixes in the file are unique.
+            if ($d.ContainsKey($k)) {
+                Throw "Duplicate relase prefix $k in $Guids"
+            } else {
+                $d[$k] = $null
+            }
+            # Ensure all GUIDs in the file are unique. We can use the same
+            # hashtable because the value domains are distinct.
+            if ($d.ContainsKey($v)) {
+                Throw "Duplicate GUID $v in $Guids"
+            } else {
+                $d[$v] = $null
+            }
+            if (! $result -and $release_name.StartsWith($k)) {
+                $result = "$v"
+                # Do not return yet, so we check that all GUIDs are unique.
+            }
+        }
+    }
+    if ($result) {
+        return "$result"
+    } else {
+        Throw "UpgradeGuid for $release_name not found in $Guids"
+    }
+}
+
+# Returns the Bazel version (as a SemVer string) from the release name.
+function Get-BazelVersion {
+    if ("$ReleaseName" -match "rc[0-9]+$") {
+        return "$ReleaseName".Substring(0, "$ReleaseName".LastIndexOf("rc"))
+    } else {
+        return "$ReleaseName"
+    }
+}
+
+# Downloads and extracts the WiX Toolkit.
+# Returns the path where the tools are (e.g. "candle.exe").
+function Download-Wix {
+    # Create a Bazel workspace just for the Wix tools.
+    #
+    # Downloading the zip with Start-BitsTransfer seems to fail on
+    # permission errors, but Bazel can download and extract it for us.
+    $wix_dir = "${WorkDir}\_wix"
+    New-Item -Path "$wix_dir" -ItemType Directory -Force | Out-Null
+    @"
+load('@bazel_tools//tools/build_defs/repo:http.bzl', 'http_archive')
+
+http_archive(
+    name = "wix_toolset",
+    url = "https://github.com/wixtoolset/wix3/releases/download/wix3111rtm/wix311-binaries.zip",
+    sha256 = "37f0a533b0978a454efb5dc3bd3598becf9660aaf4287e55bf68ca6b527d051d",
+    build_file_content = "exports_files(['candle.exe', 'light.exe'])",
+)
+"@ | Out-File -FilePath "${wix_dir}\WORKSPACE" -Force -Encoding ascii
+
+    $owd = Get-Location
+    $out_base = try {
+            & {
+            Set-Location -Path "$wix_dir"
+            $output = & bazel --max_idle_secs=1 build `
+                            @wix_toolset//:candle.exe @wix_toolset//:light.exe
+            if (! $?) {
+                # $? is a boolean: whether the last native program or Cmdlet was successful.
+                Throw "$output"
+            }
+
+            $out_base = & bazel --max_idle_secs=1 info output_base
+            if ($LASTEXITCODE -ne 0) {
+                # $LASTEXITCODE is an integer, stores the exit code of the last native program.
+                Throw "bazel info failed"
+            }
+            $out_base = Replace-Slashes($out_base)
+            return "$out_base"
+        }
+    } finally {
+        Set-Location $owd
+    }
+
+    return "${out_base}\external\wix_toolset"
+}
+
+# Creates the .wixobj file using candle.exe (the "compiler").
+function Run-Candle {
+    Param([Parameter(Mandatory=$true)][string]$wix_root)
+
+    $out="${WorkDir}\bazelmsi.wixobj"
+
+    $output = & "${wix_root}\candle.exe" `
+        -nologo `
+        -arch x64 `
+        "-dBAZEL_EXE=$BazelExe" `
+        "-dICON=$Icon" `
+        "-dUPGRADE_GUID=$(Get-UpgradeGuid $ReleaseName)" `
+        "-dRELEASE_NAME=$ReleaseName" `
+        "-dVERSION=$(Get-BazelVersion)" `
+        "-dRANDOM_GUID_1=$(Generate-Guid)" `
+        "-dRANDOM_GUID_2=$(Generate-Guid)" `
+        -o "$out" `
+        "$Wxs"
+    if (! $?) {
+        Throw "$output"
+    }
+    return "$out"
+}
+
+# Creates the .msi file using light.exe (the "linker").
+function Run-Light {
+    Param(
+        [Parameter(Mandatory=$true)][string]$wix_root,
+        [Parameter(Mandatory=$true)][string]$wixobj
+    )
+
+    $output = & "${wix_root}\light.exe" `
+        -nologo `
+        -ext WixUIExtension `
+        "-cultures:en-us" `
+        "-dWixUILicenseRtf=$Licence" `
+        "-dWixUIDialogBmp=$ArtworkBig" `
+        "-dWixUIBannerBmp=$ArtworkSmall" `
+        -o "$OutMsi" `
+        "$wixobj"
+    if (! $?) {
+        Throw "$output"
+    }
+}
+
+function Main {
+    Param(
+        [Parameter(Mandatory=$true)]
+        [ValidatePattern("bazel-[0-9]+\.[0-9]+\.[0-9]+(rc[1-9]|rc[1-9][0-9]+)?-windows-x86_64.exe$")]
+        [string]
+        $BazelExe,
+
+        [Parameter(Mandatory=$true)][string]$WorkDir,
+        [Parameter(Mandatory=$true)][string]$OutMsi
+    )
+
+    $ReleaseName, $Version = Compute-RelaseNameAndVersion $BazelExe
+
+    Log-Info "Building packaging tools"
+    $wix_root = Download-Wix
+
+    Log-Info "Creating wixobj"
+    $wixobj = Run-Candle -wix_root "$wix_root"
+
+    Log-Info "Creating msi"
+    Run-Light -wix_root "$wix_root" -wixobj "$wixobj"
+
+    Log-Info "Done: $OutMsi"
+}
---
+++ scripts/packages/wix/make_msi_test.ps1
@@ -0,0 +1,46 @@
+﻿Import-Module .\scripts\packages\wix\make_msi_lib.ps1
+
+function AssertEq {
+    Param(
+        [Parameter(Position = 0)][string]$x,
+        [Parameter(Position = 1)][string]$y
+    )
+    if ($x -ne $y) {
+        Throw "Expected equality of ($x) and ($y)"
+    }
+}
+
+function AssertNe {
+    Param(
+        [Parameter(Position = 0)][string]$x,
+        [Parameter(Position = 1)][string]$y
+    )
+    if ($x -eq $y) {
+        Throw "Expected non-equality of ($x) and ($y)"
+    }
+}
+
+# Tests for Replace-Slashes
+AssertEq $(Replace-Slashes "") ""
+AssertEq $(Replace-Slashes "foo") "foo"
+AssertEq $(Replace-Slashes "foo/bar/baz\qux") "foo\bar\baz\qux"
+
+# Test for Compute-RelaseNameAndVersion
+$rel, $ver = Compute-RelaseNameAndVersion "bazel-1.2.3-windows-x86_64.exe"
+AssertEq "$rel" "1.2.3"
+AssertEq "$ver" "1.2.3"
+$rel, $ver = Compute-RelaseNameAndVersion "bazel-0.99.5rc3-windows-x86_64.exe"
+AssertEq "$rel" "0.99.5rc3"
+AssertEq "$ver" "0.99.5"
+
+# Test for Get-UpgradeGuid
+AssertEq $(Get-UpgradeGuid 0.28.0) "B7864F52-FA13-402E-8334-5CF8FE168728"
+AssertEq $(Get-UpgradeGuid 1.0.0) $(Get-UpgradeGuid 1.0.5)
+AssertEq $(Get-UpgradeGuid 1.0.0) $(Get-UpgradeGuid 1.0.5rc2)
+AssertEq $(Get-UpgradeGuid 1.0.0) $(Get-UpgradeGuid 1.3.3)
+AssertEq $(Get-UpgradeGuid 2.1.0) $(Get-UpgradeGuid 2.0.3rc1)
+AssertNe $(Get-UpgradeGuid 0.28.0) $(Get-UpgradeGuid 0.29.0)
+AssertNe $(Get-UpgradeGuid 1.5.0) $(Get-UpgradeGuid 2.0.3rc1)
+
+
+Write-Host "PASSED"
