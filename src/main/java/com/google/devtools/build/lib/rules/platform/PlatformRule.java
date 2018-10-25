f391b441aafe4f0cd6bed24785ea38c2a1947f3b
John Cater <jcater@google.com>
2018-10-25 11:20:32 -0400

    Add documentation on platform inheritance

--- src/main/java/com/google/devtools/build/lib/rules/platform/PlatformRule.java
+++ src/main/java/com/google/devtools/build/lib/rules/platform/PlatformRule.java
@@ -56,21 +56,25 @@ public class PlatformRule implements RuleDefinition {
                 .allowedFileTypes(FileTypeSet.NO_FILE)
                 .mandatoryProviders(ConstraintValueInfo.PROVIDER.id()))
 
-        /* <!-- #BLAZE_RULE(platform).ATTRIBUTE(remote_execution_properties) -->
-        A string used to configure a remote execution platform. Actual builds make no attempt to
-        interpret this, it is treated as opaque data that can be used by a specific SpawnRunner.
-        <!-- #END_BLAZE_RULE.ATTRIBUTE --> */
-        .add(attr(REMOTE_EXECUTION_PROPS_ATTR, Type.STRING))
-
         /* <!-- #BLAZE_RULE(platform).ATTRIBUTE(parents) -->
         The label of a <code>platform</code> target that this platform should inherit from. Although
         the attribute takes a list, there should be no more than one platform present. Any
         constraint_settings not set directly on this platform will be found in the parent platform.
+        See the section on <a href="#platform_inheritance">Platform Inheritance</a> for details.
         <!-- #END_BLAZE_RULE.ATTRIBUTE --> */
         .add(
             attr(PARENTS_PLATFORM_ATTR, BuildType.LABEL_LIST)
                 .allowedFileTypes(FileTypeSet.NO_FILE)
                 .mandatoryProviders(PlatformInfo.PROVIDER.id()))
+
+        /* <!-- #BLAZE_RULE(platform).ATTRIBUTE(remote_execution_properties) -->
+        A string used to configure a remote execution platform. Actual builds make no attempt to
+        interpret this, it is treated as opaque data that can be used by a specific SpawnRunner.
+        This can include data from the parent platform's "remote_execution_properties" attribute,
+        by using the macro "{PARENT_REMOTE_EXECUTION_PROPERTIES}". See the section on
+        <a href="#platform_inheritance">Platform Inheritance</a> for details.
+        <!-- #END_BLAZE_RULE.ATTRIBUTE --> */
+        .add(attr(REMOTE_EXECUTION_PROPS_ATTR, Type.STRING))
 
         // Undocumented. Indicates that this platform should auto-configure the platform constraints
         // based on the current host OS and CPU settings.
@@ -131,4 +135,93 @@ platform(
 )
 </pre>
 
+<h3 id="platform_inheritance">Platform Inheritance</h4>
+<p>
+  Platforms may use the "parents" attribute to specify another platform that they will inherit
+  constraint values from. Although the "parents" attribute takes a list, no more than a single value
+  is currently supported, and specifying multiple parents is an error.
+</p>
+
+<p>
+  When checking for the value of a constraint setting in a platform, first the values directly set
+  (via the "constraint_values" attribute) are checked, and then the constraint values on the parent.
+  This continues recursively up the chain of parent platforms. In this manner, any values set
+  directly on a platform will override the values set on the parent.
+</p>
+
+<p>
+  Platforms can also inherit the "remote_execution_properties" attribute from the parent platform.
+  The logic for setting the "remote_execution_platform" is as follows when there is a parent
+  platform:
+
+  <ol>
+    <li>
+      If "remote_execution_property" is not set on the child platform, the parent's
+      "remote_execution_properties" will be used.
+    </li>
+    <li>
+      If "remote_execution_property" is set on the child platform, and contains the literal string
+      "{PARENT_REMOTE_EXECUTION_PROPERTIES}", that macro will be replaced with the contents of the
+      parent's "remote_execution_property" attribute.
+    </li>
+    <li>
+      If "remote_execution_property" is set on the child platform, and does not contain the macro,
+      the child's "remote_execution_property" will be used unchanged.
+    </li>
+  </ol>
+</p>
+
+<h4 id="platform_inheritance_examples">Example</h4>
+<pre class="code">
+platform(
+    name = "parent",
+    constraint_values = [
+        "@bazel_tools//platforms:linux",
+        "@bazel_tools//platforms:arm",
+    ],
+    remote_execution_properties = """
+      parent properties
+    """,
+)
+platform(
+    name = "child_a",
+    parents = [":parent"],
+    constraint_values = [
+        "@bazel_tools//platforms:x86_64",
+    ],
+    remote_execution_properties = """
+      child a properties
+    """,
+)
+platform(
+    name = "child_b",
+    parents = [":parent"],
+    remote_execution_properties = """
+      child b properties
+      {PARENT_REMOTE_EXECUTION_PROPERTIES}
+      more child b properties
+    """,
+)
+</pre>
+
+<p>
+  In these examples, the child platforms have the following properties:
+
+  <ul>
+    <li>
+      "child_a" has the constraint values "@bazel_tools//platforms:linux" (inherited from the
+      parent) and "@bazel_tools//platforms:x86_64" (set directly on the platform). It has the
+      "remote_execution_properties" set to "child a properties"
+    </li>
+    <li>
+      "child_b" inherits all constraint values from the parent, and doesn't set any of its own.
+      It has the "remote_execution_properties" set to:
+<pre>
+child b properties
+parent properties
+more child b properties
+</pre>
+    </li>
+  </ul>
+</p>
 <!-- #END_BLAZE_RULE -->*/
