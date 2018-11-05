77b0454f7cc4651f19057e45737426819c9416b2
John Cater <jcater@google.com>
2018-11-05 13:54:34 -0500

    Set a specific formatting specifier for rl/rlim_max.
    Part of error message cleanup, #6594.

--- src/test/cpp/blaze_util_posix_test.cc
+++ src/test/cpp/blaze_util_posix_test.cc
@@ -16,7 +16,6 @@
 #include <sys/resource.h>
 #include <sys/wait.h>
 
-#include <inttypes.h>
 #include <string.h>
 #include <unistd.h>
 
@@ -188,8 +187,8 @@ TEST_F(UnlimitResourcesTest, Coredumps) {
   // limit is non-zero.
   struct rlimit rl = GetrlimitOrDie(RLIMIT_CORE);
   if (rl.rlim_max <= 1) {
-    fprintf(stderr, "Hard resource limit for RLIMIT_CORE is %" PRIuMAX
-            "; cannot test anything meaningful\n", rl.rlim_max);
+    fprintf(stderr, "Hard resource limit for RLIMIT_CORE is %ju"
+            "; cannot test anything meaningful\n", (uintmax_t)rl.rlim_max);
     return;
   }
   rl.rlim_cur = 1;
