eddd26a51f0320ec448c9d6b190a4885a6911eb2
Dmitry Lomov <dslomov@google.com>
2018-11-19 17:27:09 +0100

    Fix Urls
    Change-Id: I152a94f559e2a009102544be343e21b5d0867c91

--- scripts/generate-issue-lists.py
+++ scripts/generate-issue-lists.py
@@ -13,6 +13,7 @@ def search_all_with_label(label):
     return "https://github.com/bazelbuild/bazel/issues?q=is%%3Aissue+label%%3A%s" % label
 
 
+issue_report_format = "   - [--%s](https://github.com/bazelbuild/bazel/issues/%s)"
 
 url_breaking = "https://api.github.com/repos/bazelbuild/bazel/issues?state=all&labels=breaking-change-0.%d" % (release_number)
 search_url_breaking = search_all_with_label("breaking-change-0.%d" % release_number)
@@ -32,14 +33,14 @@ def load_json(url):
 print("[Breaking changes in 0.%d](%s)" % (release_number,search_url_breaking))
 for issue in load_json(url_breaking):
   flag = issue["title"].split(":")[0]
-  print(" [--%s](%s)" % (flag, issue["url"]))
+  print(issue_report_format % (flag, issue["number"]))
 
 print("[0.%d is a migration window for the following changes](%s)" % (release_number, search_url_migration))
 for issue in load_json(url_migration):
   flag = issue["title"].split(":")[0]
-  print(" [--%s](%s)" % (flag, issue["url"]))
+  print(issue_report_format % (flag, issue["number"]))
 
 print("[Breaking changes in the next release (0.%d)](%s)" % (release_number + 1, search_url_breaking_next))
 for issue in load_json(url_breaking_next):
   flag = issue["title"].split(":")[0]
-  print(" [--%s](%s)" % (flag, issue["url"]))
+  print(issue_report_format % (flag, issue["number"]))
