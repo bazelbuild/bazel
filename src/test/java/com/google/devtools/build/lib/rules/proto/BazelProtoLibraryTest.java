d4fc00fafe37c3ddf86dcd08a5fc02552a5c7767
lberki <lberki@google.com>
2018-11-20 08:22:49 -0800

    Remove unnecessary useConfiguration("--proto_compiler=//proto:compiler") statements.
    RELNOTES: None.
    PiperOrigin-RevId: 222249130

--- src/test/java/com/google/devtools/build/lib/rules/proto/BazelProtoLibraryTest.java
+++ src/test/java/com/google/devtools/build/lib/rules/proto/BazelProtoLibraryTest.java
@@ -241,7 +241,6 @@ public class BazelProtoLibraryTest extends BuildViewTestCase {
 
   @Test
   public void testProtoSourceRootWithoutDeps() throws Exception {
-    useConfiguration("--proto_compiler=//proto:compiler");
     scratch.file(
         "x/foo/BUILD",
         "proto_library(",
@@ -261,7 +260,6 @@ public class BazelProtoLibraryTest extends BuildViewTestCase {
 
   @Test
   public void testProtoSourceRootWithoutDeps_notPackageName() throws Exception {
-    useConfiguration("--proto_compiler=//proto:compiler");
     scratch.file(
         "x/foo/BUILD",
         "proto_library(",
@@ -284,7 +282,6 @@ public class BazelProtoLibraryTest extends BuildViewTestCase {
 
   @Test
   public void testProtoSourceRootWithDepsDuplicate() throws Exception {
-    useConfiguration("--proto_compiler=//proto:compiler");
     scratch.file(
         "x/foo/BUILD",
         "proto_library(",
@@ -310,7 +307,6 @@ public class BazelProtoLibraryTest extends BuildViewTestCase {
 
   @Test
   public void testProtoSourceRootWithDeps() throws Exception {
-    useConfiguration("--proto_compiler=//proto:compiler");
     scratch.file(
         "x/foo/BUILD",
         "proto_library(",
@@ -342,7 +338,6 @@ public class BazelProtoLibraryTest extends BuildViewTestCase {
 
   @Test
   public void testExportedProtoSourceRoots() throws Exception {
-    useConfiguration("--proto_compiler=//proto:compiler");
     scratch.file("ad/BUILD",
         "proto_library(name='ad', proto_source_root='ad', srcs=['ad.proto'])");
     scratch.file("ae/BUILD",
