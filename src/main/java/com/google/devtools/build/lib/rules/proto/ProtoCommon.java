b980089b7656aa8ce324f9f95822442f7ecc6c5f
lberki <lberki@google.com>
2018-11-20 02:48:09 -0800

    Remove references to SupportData everywhere except in CcProtoAspect.
    RELNOTES: None.
    PiperOrigin-RevId: 222216840

--- src/main/java/com/google/devtools/build/lib/rules/proto/ProtoCommon.java
+++ src/main/java/com/google/devtools/build/lib/rules/proto/ProtoCommon.java
@@ -292,9 +292,9 @@ public class ProtoCommon {
     NestedSetBuilder<Artifact> result = NestedSetBuilder.stableOrder();
     ImmutableList<Artifact> srcs = ruleContext.getPrerequisiteArtifacts("srcs", TARGET).list();
     if (srcs.isEmpty()) {
-      for (ProtoSupportDataProvider provider :
-          ruleContext.getPrerequisites("deps", TARGET, ProtoSupportDataProvider.class)) {
-        result.addTransitive(provider.getSupportData().getProtosInDirectDeps());
+      for (ProtoSourcesProvider provider :
+          ruleContext.getPrerequisites("deps", TARGET, ProtoSourcesProvider.class)) {
+        result.addTransitive(provider.getProtosInDirectDeps());
       }
     } else {
       for (ProtoSourcesProvider provider :
@@ -312,9 +312,9 @@ public class ProtoCommon {
   @Nullable
   public static NestedSet<Artifact> computeProtosInExportedDeps(RuleContext ruleContext) {
     NestedSetBuilder<Artifact> result = NestedSetBuilder.stableOrder();
-    for (ProtoSupportDataProvider provider :
-        ruleContext.getPrerequisites("exports", TARGET, ProtoSupportDataProvider.class)) {
-      result.addTransitive(provider.getSupportData().getProtosInDirectDeps());
+    for (ProtoSourcesProvider provider :
+        ruleContext.getPrerequisites("exports", TARGET, ProtoSourcesProvider.class)) {
+      result.addTransitive(provider.getProtosInDirectDeps());
     }
     return result.build();
   }
