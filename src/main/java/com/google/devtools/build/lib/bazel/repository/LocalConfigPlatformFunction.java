f4f3947fb4d12bc2f5d3830220cb8c1bf6ade202
John Cater <jcater@google.com>
2018-11-28 15:55:35 -0500

    Added rule and function for local_config_platform repository rule.
    Part of #6849.

---
+++ src/main/java/com/google/devtools/build/lib/bazel/repository/LocalConfigPlatformFunction.java
@@ -0,0 +1,160 @@
+// Copyright 2018 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.devtools.build.lib.bazel.repository;
+
+import com.google.common.collect.ImmutableList;
+import com.google.devtools.build.lib.analysis.BlazeDirectories;
+import com.google.devtools.build.lib.analysis.RuleDefinition;
+import com.google.devtools.build.lib.packages.Rule;
+import com.google.devtools.build.lib.rules.repository.RepositoryDirectoryValue;
+import com.google.devtools.build.lib.rules.repository.RepositoryDirectoryValue.Builder;
+import com.google.devtools.build.lib.rules.repository.RepositoryFunction;
+import com.google.devtools.build.lib.util.CPU;
+import com.google.devtools.build.lib.util.OS;
+import com.google.devtools.build.lib.vfs.Path;
+import com.google.devtools.build.skyframe.SkyFunction.Environment;
+import com.google.devtools.build.skyframe.SkyFunctionException.Transience;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import javax.annotation.Nullable;
+
+/** Create a local repository that describes the auto-detected host platform. */
+public class LocalConfigPlatformFunction extends RepositoryFunction {
+
+  @Override
+  public boolean isLocal(Rule rule) {
+    return true;
+  }
+
+  @Override
+  public Class<? extends RuleDefinition> getRuleDefinition() {
+    return LocalConfigPlatformRule.class;
+  }
+
+  @Override
+  public Builder fetch(
+      Rule rule,
+      Path outputDirectory,
+      BlazeDirectories directories,
+      Environment env,
+      Map<String, String> markerData)
+      throws RepositoryFunctionException {
+
+    CPU hostCpu = CPU.getCurrent();
+    OS hostOs = OS.getCurrent();
+
+    List<String> constraints = generateConstraints(hostCpu, hostOs);
+
+    try {
+      outputDirectory.createDirectoryAndParents();
+      RepositoryFunction.writeFile(
+          outputDirectory, "WORKSPACE", workspaceFileContent(rule.getName()));
+      RepositoryFunction.writeFile(
+          outputDirectory, "BUILD.bazel", buildFileContent(rule.getName()));
+      RepositoryFunction.writeFile(
+          outputDirectory, "constraints.bzl", constraintFileContent(constraints));
+    } catch (IOException e) {
+      throw new RepositoryFunctionException(
+          new IOException("Could not create content for " + rule.getName() + ": " + e.getMessage()),
+          Transience.TRANSIENT);
+    }
+
+    // Return the needed info.
+    return RepositoryDirectoryValue.builder().setPath(outputDirectory);
+  }
+
+  private static List<String> generateConstraints(CPU hostCpu, OS hostOs) {
+    return ImmutableList.of(cpuToConstraint(hostCpu), osToConstraint(hostOs));
+  }
+
+  @Nullable
+  private static String cpuToConstraint(CPU cpu) {
+    switch (cpu) {
+      case X86_32:
+        return "@bazel_tools//platforms:x86_32";
+      case X86_64:
+        return "@bazel_tools//platforms:x86_64";
+      case PPC:
+        return "@bazel_tools//platforms:ppc";
+      case ARM:
+        return "@bazel_tools//platforms:arm";
+      case AARCH64:
+        return "@bazel_tools//platforms:aarch64";
+      case S390X:
+        return "@bazel_tools//platforms:s390x";
+    }
+    // Unknown, so skip it.
+    return null;
+  }
+
+  private static String osToConstraint(OS os) {
+    switch (os) {
+      case DARWIN:
+        return "@bazel_tools//platforms:osx";
+      case FREEBSD:
+        return "@bazel_tools//platforms:freebsd";
+      case LINUX:
+        return "@bazel_tools//platforms:linux";
+      case WINDOWS:
+        return "@bazel_tools//platforms:windows";
+    }
+    // Unknown, so skip it.
+    return null;
+  }
+
+  private static String workspaceFileContent(String repositoryName) {
+    return format(
+        ImmutableList.of(
+            "# DO NOT EDIT: automatically generated WORKSPACE file for local_config_platforms",
+            "workspace(name = \"%s\")"),
+        repositoryName);
+  }
+
+  private static String buildFileContent(String repositoryName) {
+    return format(
+        ImmutableList.of(
+            "# DO NOT EDIT: automatically generated BUILD file for local_config_platforms",
+            "load(':constraints.bzl', 'HOST_CONSTRAINTS')",
+            "platform(name = 'host',",
+            "  # Auto-detected host platform constraints.",
+            "  constraint_values = HOST_CONSTRAINTS,",
+            ")"),
+        repositoryName);
+  }
+
+  private static String constraintFileContent(List<String> constraints) {
+    StringBuilder formattedConstraints = new StringBuilder();
+    for (String constraint : constraints) {
+      if (constraint != null) {
+        formattedConstraints.append("  '").append(constraint).append("',\n");
+      }
+    }
+    return format(
+        ImmutableList.of(
+            "# DO NOT EDIT: automatically generated constraints list for local_config_platforms",
+            "# Auto-detected host platform constraints.",
+            "HOST_CONSTRAINTS = [",
+            "%s]"),
+        formattedConstraints.toString());
+  }
+
+  private static String format(List<String> lines, Object... params) {
+    // Add a newline between each line, and also after the final line.
+    String content = lines.stream().collect(Collectors.joining("\n", "", "\n"));
+    return String.format(content, params);
+  }
+}
