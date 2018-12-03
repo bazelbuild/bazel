453edb5e876659cbb3b978bd0fbe0ed20f1be5b7
brandjon <brandjon@google.com>
2018-12-03 13:03:55 -0800

    Begin work on --experimental_better_python_version_mixing
    This adds the experimental flag, which will be used to gate the new API and semantics. It also adds the new --python_version flag. The flags are undocumented and incomplete.
    
    The Python version options converter is made more strict to reject non "PY2" and "PY3" values at parse time. We make it an invariant that other values besides PY2/PY3/null are illegal and may result in an exception rather than just a user rule error. (The other option is to split these values from the others into a separate enum, but that seems like a lot of unneeded churn.)
    
    New getter/setter methods are added for retrieving and transitioning the Python mode in a way that respects whether or not the new semantics are enabled. A future CL will reroute existing clients to use these new methods.
    
    Work toward #6583. Design doc for new semantics here: https://github.com/bazelbuild/rules_python/blob/master/proposals/2018-10-25-selecting-between-python-2-and-3.md
    
    RELNOTES: None
    PiperOrigin-RevId: 223847968

--- src/main/java/com/google/devtools/build/lib/rules/python/PythonOptions.java
+++ src/main/java/com/google/devtools/build/lib/rules/python/PythonOptions.java
@@ -11,55 +11,136 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 package com.google.devtools.build.lib.rules.python;
 
+import com.google.common.base.Ascii;
+import com.google.common.base.Preconditions;
 import com.google.devtools.build.lib.analysis.config.FragmentOptions;
-import com.google.devtools.common.options.EnumConverter;
+import com.google.devtools.common.options.Converter;
 import com.google.devtools.common.options.Option;
 import com.google.devtools.common.options.OptionDocumentationCategory;
 import com.google.devtools.common.options.OptionEffectTag;
+import com.google.devtools.common.options.OptionMetadataTag;
+import com.google.devtools.common.options.OptionsParsingException;
 import com.google.devtools.common.options.TriState;
 
-/** Python-related command-line options. */
+/**
+ * Python-related command-line options.
+ *
+ * <p>Due to the migration associated with {@code --experimental_better_python_version_mixing} (see
+ * #6583), the Python major version mode ({@code PY2} vs {@code PY3}) is a function of three
+ * separate flags: this experimental feature-guarding flag, the old version flag {@code
+ * --force_python}, and the new version flag {@code --python_version}. See {@link #getPythonVersion}
+ * for more details.
+ */
 public class PythonOptions extends FragmentOptions {
-  /**
-   * Converter for the --force_python option.
-   */
-  public static class PythonVersionConverter extends EnumConverter<PythonVersion> {
-    public PythonVersionConverter() {
-      super(PythonVersion.class, "Python version");
+
+  /** Converter for options that take ({@code PY2} or {@code PY3}). */
+  // We don't use EnumConverter because we want to disallow non-target PythonVersion values.
+  public static class TargetPythonVersionConverter implements Converter<PythonVersion> {
+
+    @Override
+    public PythonVersion convert(String input) throws OptionsParsingException {
+      try {
+        // Although in rule attributes the enum values are case sensitive, the convention from
+        // EnumConverter is that the options parser is case insensitive.
+        input = Ascii.toUpperCase(input);
+        return PythonVersion.parseTargetValue(input);
+      } catch (IllegalArgumentException ex) {
+        throw new OptionsParsingException(
+            "Not a valid Python major version, should be PY2 or PY3", ex);
+      }
+    }
+
+    @Override
+    public String getTypeDescription() {
+      return "PY2 or PY3";
     }
   }
 
   @Option(
-    name = "build_python_zip",
-    defaultValue = "auto",
-    documentationCategory = OptionDocumentationCategory.OUTPUT_PARAMETERS,
-    effectTags = {OptionEffectTag.AFFECTS_OUTPUTS},
-    help = "Build python executable zip; on on Windows, off on other platforms"
-  )
+      name = "build_python_zip",
+      defaultValue = "auto",
+      documentationCategory = OptionDocumentationCategory.OUTPUT_PARAMETERS,
+      effectTags = {OptionEffectTag.AFFECTS_OUTPUTS},
+      help = "Build python executable zip; on on Windows, off on other platforms")
   public TriState buildPythonZip;
 
   @Option(
-    name = "force_python",
-    defaultValue = "null",
-    converter = PythonVersionConverter.class,
-    documentationCategory = OptionDocumentationCategory.OUTPUT_PARAMETERS,
-    effectTags = {OptionEffectTag.LOADING_AND_ANALYSIS, OptionEffectTag.AFFECTS_OUTPUTS},
-    help = "Overrides default_python_version attribute. Can be \"PY2\" or \"PY3\"."
-  )
+      name = "experimental_better_python_version_mixing",
+      defaultValue = "false",
+      // TODO(brandjon): Change to OptionDocumentationCategory.SKYLARK_SEMANTICS when this is
+      // sufficiently implemented/documented. Also fill in the ref in the help text below.
+      documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
+      effectTags = {OptionEffectTag.LOADING_AND_ANALYSIS},
+      metadataTags = {OptionMetadataTag.EXPERIMENTAL},
+      help =
+          "If true, enables use of the `--python_version` flag and the `python_version` "
+              + "attribute for `py_binary` and `py_test`, and uses the new PY2/PY3 version "
+              + "semantics. See <TODO: ADD LINK> for more details.")
+  public boolean experimentalBetterPythonVersionMixing;
+
+  /**
+   * This field should be either null, {@code PY2}, or {@code PY3}. Other {@code PythonVersion}
+   * values do not represent distinct Python versions and are not allowed.
+   *
+   * <p>Null represents that the value is not set by the user. This is only relevant for deciding
+   * whether or not to ignore the old flag, {@code --force_python}. Rule logic can't tell whether or
+   * not this field is null.
+   */
+  @Option(
+      name = "python_version",
+      defaultValue = "null",
+      converter = TargetPythonVersionConverter.class,
+      // TODO(brandjon): Change to OptionDocumentationCategory.GENERIC_INPUTS when this is
+      // sufficiently implemented/documented.
+      documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
+      effectTags = {
+        OptionEffectTag.LOADING_AND_ANALYSIS,
+        OptionEffectTag.AFFECTS_OUTPUTS // because of "-py3" output root
+      },
+      help =
+          "The Python major version mode, either `PY2` or `PY3`. Note that this is overridden by "
+              + "`py_binary` and `py_test` targets (whether or not they specify their version "
+              + "explicitly), so there is usually not much reason to supply this flag.")
+  public PythonVersion pythonVersion;
+
+  /**
+   * This field should be either null, {@code PY2}, or {@code PY3}. Other {@code PythonVersion}
+   * values do not represent distinct Python versions and are not allowed.
+   *
+   * <p>Null represents that the value is not set by the user. When {@code
+   * --experimental_better_python_version_mixing} is false, null means that the default value {@link
+   * PythonVersion#DEFAULT_TARGET_VALUE} should be used, but that it is possible for {@link
+   * PythonVersionTransition} to override this default. When the experimental flag is true, {@code
+   * PythonVersionTransition} can always override the version anyway, and null has the same effect
+   * as setting it to the default.
+   */
+  @Option(
+      name = "force_python",
+      defaultValue = "null",
+      converter = TargetPythonVersionConverter.class,
+      documentationCategory = OptionDocumentationCategory.OUTPUT_PARAMETERS,
+      effectTags = {OptionEffectTag.LOADING_AND_ANALYSIS, OptionEffectTag.AFFECTS_OUTPUTS},
+      help = "Overrides default_python_version attribute. Can be \"PY2\" or \"PY3\".")
   public PythonVersion forcePython;
 
+  /**
+   * This field should be either null, {@code PY2}, or {@code PY3}. Other {@code PythonVersion}
+   * values do not represent distinct Python versions and are not allowed.
+   *
+   * <p>Null is treated the same as the default ({@link PythonVersion#DEFAULT_TARGET_VALUE}).
+   */
   @Option(
-    name = "host_force_python",
-    defaultValue = "null",
-    converter = PythonVersionConverter.class,
-    documentationCategory = OptionDocumentationCategory.OUTPUT_PARAMETERS,
-    effectTags = {OptionEffectTag.LOADING_AND_ANALYSIS, OptionEffectTag.AFFECTS_OUTPUTS},
-    help =
-        "Overrides default_python_version attribute for the host configuration."
-            + " Can be \"PY2\" or \"PY3\"."
-  )
+      name = "host_force_python",
+      defaultValue = "null",
+      converter = TargetPythonVersionConverter.class,
+      documentationCategory = OptionDocumentationCategory.OUTPUT_PARAMETERS,
+      effectTags = {OptionEffectTag.LOADING_AND_ANALYSIS, OptionEffectTag.AFFECTS_OUTPUTS},
+      help =
+          "Overrides default_python_version attribute for the host configuration."
+              + " Can be \"PY2\" or \"PY3\".")
   public PythonVersion hostForcePython;
 
   public PythonVersion getPythonVersion() {
@@ -70,27 +151,96 @@ public class PythonOptions extends FragmentOptions {
     return (forcePython == null) ? defaultVersion : forcePython;
   }
 
+  /**
+   * Returns the Python major version ({@code PY2} or {@code PY3}) that targets should be built for.
+   *
+   * <p>The version is taken from the following in order:
+   *
+   * <ul>
+   *   <li>If {@code --experimental_better_python_version_mixing} is true, then it is the value of
+   *       {@code --python_version} if not null, otherwise {@code --force_python} if not null,
+   *       otherwise {@link PythonVersion#DEFAULT_TARGET_VALUE}.
+   *   <li>If {@code --experimental_better_python_version_mixing} is false, then it is the same
+   *       except {@code --python_version} is ignored.
+   * </ul>
+   */
+  // TODO(brandjon): Eliminate the NEW prefix, make public, use this to replace the above
+  // getPythonVersion() overloads.
+  PythonVersion NEWgetPythonVersion() {
+    if (experimentalBetterPythonVersionMixing) {
+      if (pythonVersion != null) {
+        return pythonVersion;
+      }
+    }
+    return (forcePython != null) ? forcePython : PythonVersion.DEFAULT_TARGET_VALUE;
+  }
+
+  /**
+   * Returns whether a Python version transition to {@code version} is allowed and not a no-op.
+   *
+   * <p>Under the new semantics ({@link #experimentalBetterPythonVersionMixing} is true), version
+   * transitions are always allowed, so this just returns whether the new version is different from
+   * the existing one.
+   *
+   * <p>Under the old semantics ({@link #experimentalBetterPythonVersionMixing} is false), version
+   * transitions are not allowed once the version has already been set ({@link #forcePython} is
+   * non-null). Due to a historical bug, it is also not allowed to transition {@code forcePython} to
+   * the hard-coded default value. Under these constraints, any transition that is allowed is also
+   * not a no-op.
+   *
+   * @throws IllegalArgumentException if {@code version} is not {@code PY2} or {@code PY3}
+   */
+  public boolean canTransitionPythonVersion(PythonVersion version) {
+    Preconditions.checkArgument(PythonVersion.TARGET_VALUES.contains(version));
+    if (experimentalBetterPythonVersionMixing) {
+      PythonVersion currentVersion = NEWgetPythonVersion();
+      return !version.equals(currentVersion);
+    } else {
+      return forcePython == null && !version.equals(PythonVersion.DEFAULT_TARGET_VALUE);
+    }
+  }
+
+  /**
+   * Manipulates the Python version fields so that {@link #getPythonVersion()} returns {@code
+   * version}.
+   *
+   * <p>This method is a mutation on the current instance, so it should only be invoked on a newly
+   * constructed instance. The mutation does not depend on whether or not {@link
+   * #canTransitionPythonVersion} would return true.
+   *
+   * <p>If the old semantics are in effect ({@link #experimentalBetterPythonVersionMixing} is
+   * false), after this method is called {@code transitionPythonVersion} will not be able to change
+   * the version ({@code forcePython} will be non-null).
+   *
+   * @throws IllegalArgumentException if {@code version} is not {@code PY2} or {@code PY3}
+   */
+  public void setPythonVersion(PythonVersion version) {
+    Preconditions.checkArgument(PythonVersion.TARGET_VALUES.contains(version));
+    if (experimentalBetterPythonVersionMixing) {
+      this.pythonVersion = version;
+    } else {
+      this.forcePython = version;
+    }
+  }
+
   @Override
   public FragmentOptions getHost() {
-    PythonOptions hostPythonOpts = (PythonOptions) getDefault();
-    if (hostForcePython != null) {
-      hostPythonOpts.forcePython = hostForcePython;
-    } else {
-      hostPythonOpts.forcePython = PythonVersion.PY2;
-    }
-    hostPythonOpts.buildPythonZip = buildPythonZip;
-    return hostPythonOpts;
+    PythonOptions hostPythonOptions = (PythonOptions) getDefault();
+    hostPythonOptions.experimentalBetterPythonVersionMixing = experimentalBetterPythonVersionMixing;
+    PythonVersion hostVersion =
+        (hostForcePython != null) ? hostForcePython : PythonVersion.DEFAULT_TARGET_VALUE;
+    hostPythonOptions.setPythonVersion(hostVersion);
+    hostPythonOptions.buildPythonZip = buildPythonZip;
+    return hostPythonOptions;
   }
 
   @Option(
-    name = "experimental_build_transitive_python_runfiles",
-    defaultValue = "true",
-    documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
-    effectTags = {OptionEffectTag.LOADING_AND_ANALYSIS, OptionEffectTag.AFFECTS_OUTPUTS},
-    help =
-        "Build the runfiles trees of py_binary targets that appear in the transitive "
-            + "data runfiles of another binary."
-  )
+      name = "experimental_build_transitive_python_runfiles",
+      defaultValue = "true",
+      documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
+      effectTags = {OptionEffectTag.LOADING_AND_ANALYSIS, OptionEffectTag.AFFECTS_OUTPUTS},
+      help =
+          "Build the runfiles trees of py_binary targets that appear in the transitive "
+              + "data runfiles of another binary.")
   public boolean buildTransitiveRunfilesTrees;
 }
-
--- src/test/java/com/google/devtools/build/lib/rules/python/PythonConfigurationTest.java
+++ src/test/java/com/google/devtools/build/lib/rules/python/PythonConfigurationTest.java
@@ -15,30 +15,212 @@
 package com.google.devtools.build.lib.rules.python;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
 import static com.google.devtools.build.lib.testutil.MoreAsserts.assertThrows;
 
+import com.google.devtools.build.lib.analysis.config.BuildConfiguration;
 import com.google.devtools.build.lib.analysis.util.ConfigurationTestCase;
 import com.google.devtools.common.options.OptionsParsingException;
+import com.google.devtools.common.options.TriState;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-/** Tests for {@link PythonConfiguration}. */
+/** Tests for {@link PythonOptions} and {@link PythonConfiguration}. */
 @RunWith(JUnit4.class)
 public class PythonConfigurationTest extends ConfigurationTestCase {
 
-  @Test
-  public void invalidForcePythonValue_NotATargetValue() throws Exception {
-    checkError("'--force_python' argument must be 'PY2' or 'PY3'", "--force_python=PY2AND3");
+  private void ensureDefaultIsPY2() {
+    // Ensure that the expected value differs from the default value, so that if the code under test
+    // ever returns the default value where it shouldn't have, the test doesn't spuriously succeed.
+    assertWithMessage(
+            "This test case is written with the assumption that the default is Python 2. When "
+                + "updating the default to Python 3, flip all the PY2/PY3 constants in the test "
+                + "case and this helper.")
+        .that(PythonVersion.DEFAULT_TARGET_VALUE)
+        .isEqualTo(PythonVersion.PY2);
+  }
+
+  private PythonOptions parsePythonOptions(String... cmdline) throws Exception {
+    BuildConfiguration config = create(cmdline);
+    return config.getOptions().get(PythonOptions.class);
   }
 
   @Test
-  public void invalidForcePythonValue_UnknownValue() {
+  public void invalidTargetPythonValue_NotATargetValue() {
+    OptionsParsingException expected =
+        assertThrows(OptionsParsingException.class, () -> create("--force_python=PY2AND3"));
+    assertThat(expected).hasMessageThat().contains("Not a valid Python major version");
+  }
+
+  @Test
+  public void invalidTargetPythonValue_UnknownValue() {
     OptionsParsingException expected =
         assertThrows(
             OptionsParsingException.class,
             () -> create("--force_python=BEETLEJUICE"));
-    assertThat(expected).hasMessageThat()
-        .contains("While parsing option --force_python=BEETLEJUICE: Not a valid Python version");
+    assertThat(expected).hasMessageThat().contains("Not a valid Python major version");
+  }
+
+  @Test
+  public void getPythonVersion_OldApi_NewFlagIgnored() throws Exception {
+    ensureDefaultIsPY2();
+    // --python_version should be ignored by getPythonVersion (and in fact would be disallowed in
+    // the analysis phase).
+    PythonOptions opts =
+        parsePythonOptions(
+            "--experimental_better_python_version_mixing=false",
+            "--force_python=PY3",
+            "--python_version=PY2");
+    assertThat(opts.NEWgetPythonVersion()).isEqualTo(PythonVersion.PY3);
+  }
+
+  @Test
+  public void getPythonVersion_OldApi_HardcodedDefaultWhenOmitted() throws Exception {
+    ensureDefaultIsPY2();
+    PythonOptions opts = parsePythonOptions("--experimental_better_python_version_mixing=false");
+    assertThat(opts.NEWgetPythonVersion()).isEqualTo(PythonVersion.PY2);
+  }
+
+  @Test
+  public void getPythonVersion_NewApi_NewFlagTakesPrecedence() throws Exception {
+    ensureDefaultIsPY2();
+    // --force_python is superseded by --python_version.
+    PythonOptions opts =
+        parsePythonOptions(
+            "--experimental_better_python_version_mixing=true",
+            "--force_python=PY2",
+            "--python_version=PY3");
+    assertThat(opts.NEWgetPythonVersion()).isEqualTo(PythonVersion.PY3);
+  }
+
+  @Test
+  public void getPythonVersion_NewApi_FallBackOnOldFlag() throws Exception {
+    ensureDefaultIsPY2();
+    // --force_python is used because --python_version is absent.
+    PythonOptions opts =
+        parsePythonOptions(
+            "--experimental_better_python_version_mixing=true", "--force_python=PY3");
+    assertThat(opts.NEWgetPythonVersion()).isEqualTo(PythonVersion.PY3);
+  }
+
+  @Test
+  public void getPythonVersion_NewApi_HardcodedDefaultWhenOmitted() throws Exception {
+    ensureDefaultIsPY2();
+    PythonOptions opts = parsePythonOptions("--experimental_better_python_version_mixing=true");
+    assertThat(opts.NEWgetPythonVersion()).isEqualTo(PythonVersion.PY2);
+  }
+
+  @Test
+  public void canTransitionPythonVersion_OldApi_Yes() throws Exception {
+    ensureDefaultIsPY2();
+    PythonOptions opts =
+        parsePythonOptions(
+            "--experimental_better_python_version_mixing=false",
+            // --python_version should be ignored.
+            "--python_version=PY2");
+    boolean result = opts.canTransitionPythonVersion(PythonVersion.PY3);
+    assertThat(result).isTrue();
+  }
+
+  @Test
+  public void canTransitionPythonVersion_OldApi_NoBecauseAlreadySet() throws Exception {
+    ensureDefaultIsPY2();
+    // --force_python can't be changed once explicitly set.
+    PythonOptions opts =
+        parsePythonOptions(
+            "--experimental_better_python_version_mixing=false", "--force_python=PY2");
+    boolean result = opts.canTransitionPythonVersion(PythonVersion.PY3);
+    assertThat(result).isFalse();
+  }
+
+  @Test
+  public void canTransitionPythonVersion_OldApi_NoBecauseNewValueSameAsDefault() throws Exception {
+    ensureDefaultIsPY2();
+    // --force_python can't be changed to the hard-coded default value.
+    PythonOptions opts = parsePythonOptions("--experimental_better_python_version_mixing=false");
+    boolean result = opts.canTransitionPythonVersion(PythonVersion.PY2);
+    assertThat(result).isFalse();
+  }
+
+  @Test
+  public void canTransitionPythonVersion_NewApi_Yes() throws Exception {
+    PythonOptions opts =
+        parsePythonOptions(
+            "--experimental_better_python_version_mixing=true", "--python_version=PY3");
+    boolean result = opts.canTransitionPythonVersion(PythonVersion.PY2);
+    assertThat(result).isTrue();
+  }
+
+  @Test
+  public void canTransitionPythonVersion_NewApi_No() throws Exception {
+    PythonOptions opts =
+        parsePythonOptions(
+            "--experimental_better_python_version_mixing=true",
+            // Omit --python_version to confirm that we're still seen as already in PY3 mode due to
+            // --force_python.
+            "--force_python=PY3");
+    boolean result = opts.canTransitionPythonVersion(PythonVersion.PY3);
+    assertThat(result).isFalse();
+  }
+
+  @Test
+  public void setPythonVersion_OldApi() throws Exception {
+    PythonOptions opts =
+        parsePythonOptions(
+            "--experimental_better_python_version_mixing=false",
+            "--force_python=PY2",
+            // --python_version should be ignored.
+            "--python_version=PY2");
+    opts.setPythonVersion(PythonVersion.PY3);
+    assertThat(opts.experimentalBetterPythonVersionMixing).isFalse();
+    assertThat(opts.forcePython).isEqualTo(PythonVersion.PY3);
+    assertThat(opts.pythonVersion).isEqualTo(PythonVersion.PY2);
+  }
+
+  @Test
+  public void setPythonVersion_NewApi() throws Exception {
+    PythonOptions opts =
+        parsePythonOptions(
+            "--experimental_better_python_version_mixing=true",
+            // --force_python should be ignored.
+            "--force_python=PY2",
+            "--python_version=PY2");
+    opts.setPythonVersion(PythonVersion.PY3);
+    assertThat(opts.experimentalBetterPythonVersionMixing).isTrue();
+    assertThat(opts.forcePython).isEqualTo(PythonVersion.PY2);
+    assertThat(opts.pythonVersion).isEqualTo(PythonVersion.PY3);
+  }
+
+  @Test
+  public void getHost_OldSemantics() throws Exception {
+    ensureDefaultIsPY2();
+    PythonOptions opts =
+        parsePythonOptions(
+            "--experimental_better_python_version_mixing=false",
+            "--force_python=PY2",
+            "--host_force_python=PY3",
+            "--build_python_zip=true");
+    PythonOptions newOpts = (PythonOptions) opts.getHost();
+    assertThat(newOpts.experimentalBetterPythonVersionMixing).isFalse();
+    assertThat(newOpts.forcePython).isEqualTo(PythonVersion.PY3);
+    assertThat(newOpts.NEWgetPythonVersion()).isEqualTo(PythonVersion.PY3);
+    assertThat(newOpts.buildPythonZip).isEqualTo(TriState.YES);
+  }
+
+  @Test
+  public void getHost_NewSemantics() throws Exception {
+    ensureDefaultIsPY2();
+    PythonOptions opts =
+        parsePythonOptions(
+            "--experimental_better_python_version_mixing=true",
+            "--python_version=PY2",
+            "--host_force_python=PY3",
+            "--build_python_zip=true");
+    PythonOptions newOpts = (PythonOptions) opts.getHost();
+    assertThat(newOpts.experimentalBetterPythonVersionMixing).isTrue();
+    assertThat(newOpts.pythonVersion).isEqualTo(PythonVersion.PY3);
+    assertThat(newOpts.NEWgetPythonVersion()).isEqualTo(PythonVersion.PY3);
+    assertThat(newOpts.buildPythonZip).isEqualTo(TriState.YES);
   }
 }
