5b4f23782b5b530c1d6085499435349105845e20
janakr <janakr@google.com>
2019-01-08 17:12:03 -0800

    Remove untrue @VisibleForTesting annotation on InMemoryNodeEntry#dirtyBuildingState, incorrect even when it was introduced in unknown commit.
    PiperOrigin-RevId: 228428203

--- src/main/java/com/google/devtools/build/skyframe/InMemoryNodeEntry.java
+++ src/main/java/com/google/devtools/build/skyframe/InMemoryNodeEntry.java
@@ -13,7 +13,6 @@
 // limitations under the License.
 package com.google.devtools.build.skyframe;
 
-import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.MoreObjects;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
@@ -141,7 +140,7 @@ public class InMemoryNodeEntry implements NodeEntry {
    * Object encapsulating dirty state of the object between when it is marked dirty and
    * re-evaluated.
    */
-  @VisibleForTesting @Nullable protected volatile DirtyBuildingState dirtyBuildingState = null;
+  @Nullable protected volatile DirtyBuildingState dirtyBuildingState = null;
 
   private static final int NOT_EVALUATING_SENTINEL = -1;
 
