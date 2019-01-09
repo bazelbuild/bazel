dd5e7ff2672afcded4f117be53174ea4a966a247
Benjamin Peterson <bp@benjamin.pe>
2019-01-09 12:51:04 -0800

    Remove period from a EnvironmentalExecException message.
    Closes #7001.
    
    PiperOrigin-RevId: 228570254

--- src/main/java/com/google/devtools/build/lib/exec/AbstractSpawnStrategy.java
+++ src/main/java/com/google/devtools/build/lib/exec/AbstractSpawnStrategy.java
@@ -98,20 +98,18 @@ public abstract class AbstractSpawnStrategy implements SandboxedSpawnActionConte
     }
     SpawnResult spawnResult;
     ExecException ex = null;
-    try {
-      try (CacheHandle cacheHandle = cache.lookup(spawn, context)) {
-        if (cacheHandle.hasResult()) {
-          spawnResult = Preconditions.checkNotNull(cacheHandle.getResult());
-        } else {
-          // Actual execution.
-          spawnResult = spawnRunner.execAsync(spawn, context).get();
-          if (cacheHandle.willStore()) {
-            cacheHandle.store(spawnResult);
-          }
+    try (CacheHandle cacheHandle = cache.lookup(spawn, context)) {
+      if (cacheHandle.hasResult()) {
+        spawnResult = Preconditions.checkNotNull(cacheHandle.getResult());
+      } else {
+        // Actual execution.
+        spawnResult = spawnRunner.execAsync(spawn, context).get();
+        if (cacheHandle.willStore()) {
+          cacheHandle.store(spawnResult);
         }
       }
     } catch (IOException e) {
-      throw new EnvironmentalExecException("Unexpected IO error.", e);
+      throw new EnvironmentalExecException("Unexpected IO error", e);
     } catch (SpawnExecException e) {
       ex = e;
       spawnResult = e.getSpawnResult();
