e8aef3ed6ee16949e4c62b881a90551d69e3ebde
ulfjack <ulfjack@google.com>
2019-01-10 05:46:41 -0800

    Lock down some constructors to prevent nulls
    PiperOrigin-RevId: 228694984

--- src/main/java/com/google/devtools/build/lib/actions/SpawnExecutedEvent.java
+++ src/main/java/com/google/devtools/build/lib/actions/SpawnExecutedEvent.java
@@ -14,6 +14,7 @@
 
 package com.google.devtools.build.lib.actions;
 
+import com.google.common.base.Preconditions;
 import com.google.devtools.build.lib.events.ExtendedEventHandler;
 
 /** This event is fired during the build, when a subprocess is executed. */
@@ -24,8 +25,8 @@ public class SpawnExecutedEvent implements ExtendedEventHandler.ProgressLike {
   public SpawnExecutedEvent(
       Spawn spawn,
       SpawnResult result) {
-    this.spawn = spawn;
-    this.result = result;
+    this.spawn = Preconditions.checkNotNull(spawn);
+    this.result = Preconditions.checkNotNull(result);
   }
 
   /** Returns the Spawn. */
