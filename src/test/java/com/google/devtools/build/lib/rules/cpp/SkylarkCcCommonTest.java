c0142ab86f23b074fe4c04a6bc152ad0eda002b4
hlopko <hlopko@google.com>
2019-01-11 04:10:07 -0800

    Flip --incompatible_linkopts_in_user_link_flags
    Closes #6826.
    
    This is a roll-forward of https://github.com/bazelbuild/bazel/commit/4866f3334b3bf0c8faac577d50bdb9df95f5460d after Bazel 0.22 was cut.
    
    RELNOTES: Incompatible flag `--incompatible_linkopts_in_user_link_flags` has been flipped (https://github.com/bazelbuild/bazel/issues/6826)
    PiperOrigin-RevId: 228861234

--- src/test/java/com/google/devtools/build/lib/rules/cpp/SkylarkCcCommonTest.java
+++ src/test/java/com/google/devtools/build/lib/rules/cpp/SkylarkCcCommonTest.java
@@ -581,13 +581,13 @@ public class SkylarkCcCommonTest extends BuildViewTestCase {
 
   @Test
   public void testEmptyLinkVariables() throws Exception {
-    useConfiguration("--linkopt=-foo");
     assertThat(
             commandLineForVariables(
                 CppActionNames.CPP_LINK_EXECUTABLE,
                 "cc_common.create_link_variables(",
                 "feature_configuration = feature_configuration,",
                 "cc_toolchain = toolchain,",
+                "user_link_flags = [ '-foo' ],",
                 ")"))
         .contains("-foo");
   }
@@ -860,6 +860,7 @@ public class SkylarkCcCommonTest extends BuildViewTestCase {
                 "feature_configuration = feature_configuration,",
                 "cc_toolchain = toolchain,",
                 "is_linking_dynamic_library = True,",
+                "user_link_flags = [ '-pie' ],",
                 ")"))
         .doesNotContain("-pie");
     assertThat(
@@ -870,13 +871,13 @@ public class SkylarkCcCommonTest extends BuildViewTestCase {
                 "feature_configuration = feature_configuration,",
                 "cc_toolchain = toolchain,",
                 "is_linking_dynamic_library = False,",
+                "user_link_flags = [ '-pie' ],",
                 ")"))
         .contains("-pie");
   }
 
   @Test
   public void testIsUsingLinkerLinkVariables() throws Exception {
-    useConfiguration("--linkopt=-i_dont_want_to_see_this_on_archiver_command_line");
     assertThat(
             commandLineForVariables(
                 CppActionNames.CPP_LINK_EXECUTABLE,
@@ -885,6 +886,7 @@ public class SkylarkCcCommonTest extends BuildViewTestCase {
                 "feature_configuration = feature_configuration,",
                 "cc_toolchain = toolchain,",
                 "is_using_linker = True,",
+                "user_link_flags = [ '-i_dont_want_to_see_this_on_archiver_command_line' ],",
                 ")"))
         .contains("-i_dont_want_to_see_this_on_archiver_command_line");
     assertThat(
@@ -895,6 +897,7 @@ public class SkylarkCcCommonTest extends BuildViewTestCase {
                 "feature_configuration = feature_configuration,",
                 "cc_toolchain = toolchain,",
                 "is_using_linker = False,",
+                "user_link_flags = [ '-i_dont_want_to_see_this_on_archiver_command_line' ],",
                 ")"))
         .doesNotContain("-i_dont_want_to_see_this_on_archiver_command_line");
   }
