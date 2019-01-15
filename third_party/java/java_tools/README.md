e1d0d3d39fc59d531b0f6e30cc2b3f432e0fa633
iirina <elenairina@google.com>
2019-01-15 12:39:18 +0100

    Add Turbine

--- third_party/java/java_tools/README.md
+++ third_party/java/java_tools/README.md
@@ -1,60 +1,19 @@
 # Tools required by the Bazel rules for Java.
 
-To update all the Java tools please run from the workspace root:
+To see how to update the tools run:
 
 ```
-./third_party/java/java_tools/update_java_tools.sh
+./third_party/java/java_tools/update_java_tools.sh help
 ```
 
-To update individual Java tools please see the instructions for each tool
-below.
+The following tools were built with bazel 0.21.0 at commit 252c82cd135f1bcdadfc961c127895e9b2347b77
 
-## JavaBuilder
-
-`JavaBuilder` is a tools used by Bazel to compile Java code. It is a wrapper
-around `javac` and also provides:
-
-* Strict Java dependency enforcement
-* [Error prone](http://errorprone.info) checks
-* Header Compilation
-* Reduced Classpath Optimization
-
-The code of JavaBuilder is under //src/java_tools/buildjar.
-
-`JavaBuilder_deploy.jar` was built with bazel 0.21.0 from commit
-[3256863e75f96c1ad9537b5b12f66a0e5b174781](https://github.com/bazelbuild/bazel/commit/3256863e75f96c1ad9537b5b12f66a0e5b174781)
-
-```
-bazel build //src/java_tools/buildjar:JavaBuilder_deploy.jar
-cp -f bazel-bin/src/java_tools/buildjar/JavaBuilder_deploy.jar third_party/java/java_tools/JavaBuilder_deploy.jar
-```
-
-### VanillaJavaBuilder
-
-VanillaJavaBuilder strips the additional features added in `JavaBuilder`. When enabled
-Bazel will invoke `javac` directly.
-
-`VanillaJavaBuilder_deploy.jar` was built with bazel 0.21.0 from commit
-[3256863e75f96c1ad9537b5b12f66a0e5b174781](https://github.com/bazelbuild/bazel/commit/3256863e75f96c1ad9537b5b12f66a0e5b174781)
-
-```
-bazel build //src/java_tools/buildjar:VanillaJavaBuilder_deploy.jar
-cp -f bazel-bin/src/java_tools/buildjar/VanillaJavaBuilder_deploy.jar third_party/java/java_tools/VanillaJavaBuilder_deploy.jar
-```
-
-## GenClassJar
-
-`GenClassJar` was built with bazel 0.21.0 from commit
-[3256863e75f96c1ad9537b5b12f66a0e5b174781](https://github.com/bazelbuild/bazel/commit/3256863e75f96c1ad9537b5b12f66a0e5b174781)
-
-```
-bazel build //src/java_tools/buildjar/java/com/google/devtools/build/buildjar/genclass:GenClass_deploy.jar
-cp -f bazel-bin/src/java_tools/buildjar/java/com/google/devtools/build/buildjar/genclass/GenClass_deploy.jar third_party/java/java_tools/GenClass_deploy.jar
-```
-
-## JUnit Runner
-
-```
-bazel build //src/java_tools/junitrunner/java/com/google/testing/junit/runner:Runner_deploy.jar
-cp -f bazel-bin/src/java_tools/junitrunner/java/com/google/testing/junit/runner/Runner_deploy.jar third_party/java/java_tools/Runner_deploy.jar
-```
+third_party/java/java_tools/JavaBuilder_deploy.jar
+third_party/java/java_tools/VanillaJavaBuilder_deploy.jar
+third_party/java/java_tools/GenClass_deploy.jar
+third_party/java/java_tools/Runner_deploy.jar
+third_party/java/java_tools/ExperimentalRunner_deploy.jar
+third_party/java/java_tools/JacocoCoverage_jarjar_deploy.jar
+third_party/java/java_tools/turbine_deploy.jar
+third_party/java/java_tools/turbine_direct_binary_deploy.jar
+third_party/java/java_tools/SingleJar_deploy.jar
--- third_party/java/java_tools/update_java_tools.sh
+++ third_party/java/java_tools/update_java_tools.sh
@@ -1,4 +1,56 @@
 #!/bin/bash
+#
+# A script to update the checked-in jars corresponding to Java tools used
+# by the Java rules in Bazel.
+#
+# For usage please run
+# ~/third_party/java/java_tools/update_java_tools.sh help
+
+declare -a all_tools=("JavaBuilder" "VanillaJavaBuilder" "GenClass" "Runner" "ExperimentalRunner" "JacocoCoverage" "Turbine" "TurbineDirect" "SingleJar")
+
+declare -A tool_name_to_target=( ["JavaBuilder"]="src/java_tools/buildjar:JavaBuilder_deploy.jar" \
+["VanillaJavaBuilder"]="src/java_tools/buildjar:VanillaJavaBuilder_deploy.jar" \
+["GenClass"]="src/java_tools/buildjar/java/com/google/devtools/build/buildjar/genclass:GenClass_deploy.jar" \
+["Runner"]="src/java_tools/junitrunner/java/com/google/testing/junit/runner:Runner_deploy.jar" \
+["ExperimentalRunner"]="src/java_tools/junitrunner/java/com/google/testing/junit/runner:ExperimentalRunner_deploy.jar" \
+["JacocoCoverage"]="src/java_tools/junitrunner/java/com/google/testing/coverage:JacocoCoverage_jarjar_deploy.jar" \
+["Turbine"]="src/java_tools/buildjar/java/com/google/devtools/build/java/turbine/javac:turbine_deploy.jar" \
+["TurbineDirect"]="src/java_tools/buildjar/java/com/google/devtools/build/java/turbine:turbine_direct_binary_deploy.jar" \
+["SingleJar"]="src/java_tools/singlejar:SingleJar_deploy.jar")
+
+usage="This script updates the checked-in jars corresponding to the tools "\
+"used by the Java rules in Bazel.
+
+To update all the tools simultaneously run from your bazel workspace root:
+~/third_party/java/java_tools/update_java_tools.sh
+
+To update only one or one subset of the tools run
+~/third_party/java/java_tools/update_java_tools.sh tool_1 tool_2 ... tool_n
+
+where tool_i can have one of the values ${all_tools[@]}
+
+For example, to update only JavaBuilder run
+~/third_party/java/java_tools/update_java_tools.sh JavaBuilder
+
+To update JavaBuilder, Turbine and SingleJar run
+~/third_party/java/java_tools/update_java_tools.sh JavaBuilder Turbine SingleJar
+"
+
+if [[ ! -z "$1" && $1 = "help" ]]; then
+  echo "$usage"
+  exit
+fi
+
+tools_to_update=()
+
+if [[ ! -z "$@" ]]
+then
+   tools_to_update=("$@")
+else
+#  tools_to_update=("${JAVA_TOOLS_TARGETS[@]}")
+  tools_to_update=("${all_tools[@]}")
+fi
+
 
 tools=()
 
@@ -14,16 +66,16 @@ function update_tool() {
   tools+=("third_party/java/java_tools/$tool_basename")
 }
 
-update_tool "src/java_tools/buildjar:JavaBuilder_deploy.jar"
-update_tool "src/java_tools/buildjar:VanillaJavaBuilder_deploy.jar"
-update_tool "src/java_tools/buildjar/java/com/google/devtools/build/buildjar/genclass:GenClass_deploy.jar"
-update_tool "src/java_tools/junitrunner/java/com/google/testing/junit/runner:Runner_deploy.jar"
-update_tool "src/java_tools/junitrunner/java/com/google/testing/junit/runner:ExperimentalRunner_deploy.jar"
-update_tool "src/java_tools/junitrunner/java/com/google/testing/coverage:JacocoCoverage_jarjar_deploy.jar"
+for tool in "${tools_to_update[@]}"
+do
+  tool_bazel_target=${tool_name_to_target[$tool]}
+  update_tool "$tool_bazel_target"
+done
 
 bazel_version=$(bazel version | grep "Build label" | cut -d " " -f 3)
 git_head=$(git rev-parse HEAD)
 echo "......"
 echo ""
+
 echo "The following tools were built with bazel $bazel_version at commit $git_head"
 ( IFS=$'\n'; echo "${tools[*]}" )
