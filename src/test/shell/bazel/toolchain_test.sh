0f005371b1a2750e0c1d4a1cf071ae9cfdbf2038
John Cater <jcater@google.com>
2019-01-23 13:35:10 -0800

    Add cycle reporter to handle the case where a non-toolchain is passed?
    ? to register_toolchains.
    
    Fixes #7171.
    
    Closes #7226.
    
    PiperOrigin-RevId: 230592299

--- src/test/shell/bazel/toolchain_test.sh
+++ src/test/shell/bazel/toolchain_test.sh
@@ -1076,6 +1076,117 @@ function test_local_config_platform() {
   expect_log '@local_config_platform//:host'
 }
 
+# Test cycles in registered toolchains, which can only happen when
+# registered_toolchains is called for something that is not actually
+# using the "toolchain" rule.
+function test_registered_toolchain_cycle() {
+
+  # Set up two sets of rules and toolchains, one depending on the other.
+  cat >>lower.bzl <<EOF
+def _lower_toolchain_impl(ctx):
+  message = ctx.attr.message
+  toolchain = platform_common.ToolchainInfo(
+      message=message)
+  return [toolchain]
+
+lower_toolchain = rule(
+    implementation = _lower_toolchain_impl,
+    attrs = {
+        'message': attr.string(),
+    },
+)
+
+def _lower_library_impl(ctx):
+  toolchain = ctx.toolchains['//:lower']
+  print('lower library: %s' % toolchain.message)
+  return []
+
+lower_library = rule(
+    implementation = _lower_library_impl,
+    attrs = {},
+    toolchains = ['//:lower'],
+)
+EOF
+  cat >>upper.bzl <<EOF
+def _upper_toolchain_impl(ctx):
+  tool_message = ctx.toolchains['//:lower'].message
+  message = ctx.attr.message
+  toolchain = platform_common.ToolchainInfo(
+      tool_message=tool_message,
+      message=message)
+  return [toolchain]
+
+upper_toolchain = rule(
+    implementation = _upper_toolchain_impl,
+    attrs = {
+        'message': attr.string(),
+    },
+    toolchains = ['//:lower'],
+)
+
+def _upper_library_impl(ctx):
+  toolchain = ctx.toolchains['//:upper']
+  print('upper library: %s (%s)' % (toolchain.message, toolchain.tool_message))
+  return []
+
+upper_library = rule(
+    implementation = _upper_library_impl,
+    attrs = {},
+    toolchains = ['//:upper'],
+)
+EOF
+
+  # Define the actual targets using these.
+  cat >>BUILD <<EOF
+load('//:lower.bzl', 'lower_toolchain', 'lower_library')
+load('//:upper.bzl', 'upper_toolchain', 'upper_library')
+
+toolchain_type(name = 'lower')
+toolchain_type(name = 'upper')
+
+lower_library(
+    name = 'lower_lib',
+)
+
+lower_toolchain(
+    name = 'lower_toolchain',
+    message = 'hi from lower',
+)
+toolchain(
+    name = 'lower_toolchain_impl',
+    toolchain_type = '//:lower',
+    toolchain = ':lower_toolchain',
+)
+
+upper_library(
+    name = 'upper_lib',
+)
+
+upper_toolchain(
+    name = 'upper_toolchain',
+    message = 'hi from upper',
+)
+toolchain(
+    name = 'upper_toolchain_impl',
+    toolchain_type = '//:upper',
+    toolchain = ':upper_toolchain',
+)
+EOF
+
+  # Finally, set up the misconfigured WORKSPACE file.
+  cat >>WORKSPACE <<EOF
+register_toolchains(
+    '//:upper_toolchain', # Not a toolchain() target!
+    '//:lower_toolchain_impl',
+    )
+EOF
+
+  # Execute the build and check the error message.
+  bazel build //:upper_lib &> $TEST_log && fail "Build succeeded unexpectedly"
+  expect_not_log "java.lang.IllegalStateException"
+  expect_log "Misconfigured toolchains: //:upper_toolchain is declared as a toolchain but has inappropriate dependencies"
+}
+
 # TODO(katre): Test using toolchain-provided make variables from a genrule.
 
 run_suite "toolchain tests"
