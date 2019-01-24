f21ca8ad99d0f0048af21ccf31c18bbb808336f3
Yun Peng <pcloudy@google.com>
2019-01-24 05:34:50 -0800

    Windows: native launcher can run from a long path
    This change fixed a bug in `src/tools/launcher/util/data_parser.cc` when running the native launcher at a long path.
    
    Closes #7222.
    
    Change-Id: I2ed2c6cb32592d6a178df9a69fe31d6df0974f0c
    PiperOrigin-RevId: 230705416

--- src/test/py/bazel/test_base.py
+++ src/test/py/bazel/test_base.py
@@ -310,7 +310,7 @@ class TestBase(unittest.TestCase):
       print('--------------------------')
       print('\n'.join(stderr_lines))
 
-  def RunProgram(self, args, env_remove=None, env_add=None):
+  def RunProgram(self, args, env_remove=None, env_add=None, shell=False):
     """Runs a program (args[0]), waits for it to exit.
 
     Args:
@@ -319,6 +319,8 @@ class TestBase(unittest.TestCase):
         to the program
       env_add: {string: string}; optional; environment variables to pass to
         the program, won't be removed by env_remove.
+      shell: {bool: bool}; optional; whether to use the shell as the program
+        to execute
     Returns:
       (int, [string], [string]) tuple: exit code, stdout lines, stderr lines
     """
@@ -329,7 +331,8 @@ class TestBase(unittest.TestCase):
             stdout=stdout,
             stderr=stderr,
             cwd=self._test_cwd,
-            env=self._EnvMap(env_remove, env_add))
+            env=self._EnvMap(env_remove, env_add),
+            shell=shell)
         exit_code = proc.wait()
 
         stdout.seek(0)
