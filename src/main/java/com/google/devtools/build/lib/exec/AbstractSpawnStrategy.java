98fb290e0b00342f051eadb739b4e8523b67364d
Jakob Buchgraber <buchgr@google.com>
2019-01-28 13:49:45 +0100

    Allow a SpawnRunner to inject output metadata.
    Adds a MetadataInjector type that can be accessed via the
    SpawnExecutionContext.
    
    Progress towards #6862

--- src/main/java/com/google/devtools/build/lib/exec/AbstractSpawnStrategy.java
+++ src/main/java/com/google/devtools/build/lib/exec/AbstractSpawnStrategy.java
@@ -33,6 +33,7 @@ import com.google.devtools.build.lib.actions.SpawnActionContext;
 import com.google.devtools.build.lib.actions.SpawnResult;
 import com.google.devtools.build.lib.actions.SpawnResult.Status;
 import com.google.devtools.build.lib.actions.Spawns;
+import com.google.devtools.build.lib.actions.cache.MetadataHandler;
 import com.google.devtools.build.lib.events.Event;
 import com.google.devtools.build.lib.events.ExtendedEventHandler;
 import com.google.devtools.build.lib.exec.SpawnCache.CacheHandle;
@@ -198,6 +199,11 @@ public abstract class AbstractSpawnStrategy implements SandboxedSpawnActionConte
     @Override
     public MetadataProvider getMetadataProvider() {
       return actionExecutionContext.getMetadataProvider();
+    }
+    
+    @Override
+    public MetadataHandler getMetadataInjector() {
+      return actionExecutionContext.getMetadataHandler();
     }
 
     @Override
