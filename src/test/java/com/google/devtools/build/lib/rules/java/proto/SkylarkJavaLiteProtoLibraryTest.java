aa4dfab27d016abfccbe5c4bdc88c7c760bed06d
cushon <cushon@google.com>
2019-02-04 16:49:56 -0800

    Migrate to find_java_toolchain/find_java_runtime_toolchain for Java toolchain and runtime resolution
    PiperOrigin-RevId: 232392137

--- src/test/java/com/google/devtools/build/lib/rules/java/proto/SkylarkJavaLiteProtoLibraryTest.java
+++ src/test/java/com/google/devtools/build/lib/rules/java/proto/SkylarkJavaLiteProtoLibraryTest.java
@@ -92,6 +92,9 @@ public class SkylarkJavaLiteProtoLibraryTest extends BuildViewTestCase {
     scratch.file(
         "tools/jdk/build_defs.bzl",
         Files.readAllBytes(Runfiles.location("tools/jdk/build_defs.bzl").toPath()));
+    scratch.file(
+        "tools/jdk/toolchain_utils.bzl",
+        Files.readAllBytes(Runfiles.location("tools/jdk/toolchain_utils.bzl").toPath()));
     invalidatePackages();
   }
 
