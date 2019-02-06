44a4fd8b92362073652705ea7e1b7dc5559d6d1c
cushon <cushon@google.com>
2019-02-06 09:34:50 -0800

    Improve a precondition failure message
    PiperOrigin-RevId: 232689469

--- src/main/java/com/google/devtools/build/lib/analysis/Runfiles.java
+++ src/main/java/com/google/devtools/build/lib/analysis/Runfiles.java
@@ -1115,7 +1115,8 @@ public final class Runfiles implements RunfilesApi {
       }
       // The suffix should be the same within any blaze build, except for the EMPTY runfiles, which
       // may have an empty suffix, but that is covered above.
-      Preconditions.checkArgument(suffix.equals(runfiles.suffix));
+      Preconditions.checkArgument(
+          suffix.equals(runfiles.suffix), "%s != %s", suffix, runfiles.suffix);
       if (includeUnconditionalArtifacts) {
         artifactsBuilder.addTransitive(runfiles.getUnconditionalArtifacts());
       }
