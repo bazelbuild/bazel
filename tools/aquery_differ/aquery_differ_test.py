3575797c8cc9aa1bbb09f4c0ab11780b0b528cc9
leba <leba@google.com>
2019-02-08 02:03:12 -0800

    Allow comparison of other attributes in aquery differ tool.
    cmd_line_differ.py was able to compare the difference in the *command lines* run for each action in 2 different aquery results.
    
    This commit adds the following:
    - New flag --attrs, that specify the attributes of actions to be compared.
    - The implementation of --attrs=inputs. Other attributes will be added in subsequent commits.
    - Improved formatting for the output of the script.
    
    RELNOTES[INC]: //tools/cmd_line_differ has been renamed to //tools/aquery_differ & can now compare (in addition to command lines) inputs of actions given 2 aquery results.
    
    PiperOrigin-RevId: 233025799

--- tools/aquery_differ/aquery_differ_test.py
+++ tools/aquery_differ/aquery_differ_test.py
@@ -14,7 +14,7 @@
 
 import unittest
 from src.main.protobuf import analysis_pb2
-from tools.cmd_line_differ import cmd_line_differ
+from tools.aquery_differ import aquery_differ
 from third_party.py import mock
 try:
   # Python 2
@@ -26,15 +26,34 @@ except ImportError:
 
 def make_aquery_output(actions, artifact_paths):
   action_graph = analysis_pb2.ActionGraphContainer()
+
   for artifact_path in artifact_paths:
     next_id = len(action_graph.artifacts)
     artifact = action_graph.artifacts.add()
     artifact.id = str(next_id)
     artifact.exec_path = artifact_path
+
   for next_action in actions:
     action = action_graph.actions.add()
     action.output_ids.extend(next_action["output_ids"])
     action.arguments.extend(next_action["arguments"])
+
+    if "input_dep_set_ids" in next_action:
+      action.input_dep_set_ids.extend(next_action["input_dep_set_ids"])
+
+  return action_graph
+
+
+def make_aquery_output_with_dep_set(actions, artifact_paths, dep_sets):
+  action_graph = make_aquery_output(actions, artifact_paths)
+
+  for ds in dep_sets:
+    next_id = len(action_graph.dep_set_of_files)
+    dep_set = action_graph.dep_set_of_files.add()
+    dep_set.id = str(next_id)
+    dep_set.direct_artifact_ids.extend(ds["direct_artifact_ids"])
+    dep_set.transitive_dep_set_ids.extend(ds["transitive_dep_set_ids"])
+
   return action_graph
 
 
@@ -51,8 +70,10 @@ class CmdLineDifferTest(unittest.TestCase):
         }],
         artifact_paths=["exec/path/zero", "exec/path/one", "exec/path/two"])
     mock_stdout = StringIO()
+    attrs = ["cmdline"]
     with mock.patch("sys.stdout", mock_stdout):
-      cmd_line_differ._aquery_diff(action_graph, action_graph)
+      aquery_differ._aquery_diff(action_graph, action_graph, attrs, "before",
+                                 "after")
       self.assertEqual(mock_stdout.getvalue(), "No difference\n")
 
   def test_no_difference_different_output_files_order(self):
@@ -74,8 +95,9 @@ class CmdLineDifferTest(unittest.TestCase):
         artifact_paths=["exec/path/zero", "exec/path/one"])
 
     mock_stdout = StringIO()
+    attrs = ["cmdline"]
     with mock.patch("sys.stdout", mock_stdout):
-      cmd_line_differ._aquery_diff(first, second)
+      aquery_differ._aquery_diff(first, second, attrs, "before", "after")
       self.assertEqual(mock_stdout.getvalue(), "No difference\n")
 
   def test_first_has_extra_output_files(self):
@@ -102,12 +124,13 @@ class CmdLineDifferTest(unittest.TestCase):
         artifact_paths=["exec/path/zero", "exec/path/one", "exec/path/two"],
     )
 
-    expected_error = ("Aquery output before change contains an action "
+    expected_error = ("Aquery output 'before' change contains an action "
                       "that generates the following outputs that aquery "
-                      "output after change doesn't:\nexec/path/two\n\n")
+                      "output 'after' change doesn't:\nexec/path/two\n\n")
     mock_stdout = StringIO()
+    attrs = ["cmdline"]
     with mock.patch("sys.stdout", mock_stdout):
-      cmd_line_differ._aquery_diff(first, second)
+      aquery_differ._aquery_diff(first, second, attrs, "before", "after")
       self.assertEqual(mock_stdout.getvalue(), expected_error)
 
   def test_second_has_extra_output_files(self):
@@ -134,12 +157,13 @@ class CmdLineDifferTest(unittest.TestCase):
         artifact_paths=["exec/path/zero", "exec/path/one", "exec/path/two"],
     )
 
-    expected_error = ("Aquery output after change contains an action that"
+    expected_error = ("Aquery output 'after' change contains an action that"
                       " generates the following outputs that aquery"
-                      " output before change doesn't:\nexec/path/two\n\n")
+                      " output 'before' change doesn't:\nexec/path/two\n\n")
     mock_stdout = StringIO()
+    attrs = ["cmdline"]
     with mock.patch("sys.stdout", mock_stdout):
-      cmd_line_differ._aquery_diff(first, second)
+      aquery_differ._aquery_diff(first, second, attrs, "before", "after")
       self.assertEqual(mock_stdout.getvalue(), expected_error)
 
   def test_different_command_lines(self):
@@ -171,24 +195,63 @@ class CmdLineDifferTest(unittest.TestCase):
     )
 
     expected_error_one = "\n".join([
-        "Difference in action that generates the following outputs:",
-        "exec/path/two",
-        "Aquery output before change has the following command line:", "-c",
-        "Aquery output after change has the following command line:", "-c",
-        "-d", "\n"
+        "Difference in the action that generates the following output(s):",
+        "\texec/path/two", "--- before", "+++ after", "@@ -1 +1,2 @@", " -c",
+        "+-d", "\n"
     ])
     expected_error_two = "\n".join([
-        "Difference in action that generates the following outputs:",
-        "exec/path/one", "exec/path/zero",
-        "Aquery output before change has the following command line:", "-a",
-        "-d", "Aquery output after change has the following command line:",
-        "-a", "-b", "\n"
+        "Difference in the action that generates the following output(s):",
+        "\texec/path/one", "\texec/path/zero", "--- before", "+++ after",
+        "@@ -1,2 +1,2 @@", " -a", "--d", "+-b", "\n"
     ])
+    attrs = ["cmdline"]
+
     mock_stdout = StringIO()
     with mock.patch("sys.stdout", mock_stdout):
-      cmd_line_differ._aquery_diff(first, second)
+      aquery_differ._aquery_diff(first, second, attrs, "before", "after")
       self.assertIn(expected_error_one, mock_stdout.getvalue())
       self.assertIn(expected_error_two, mock_stdout.getvalue())
+
+  def test_different_inputs(self):
+    first = make_aquery_output_with_dep_set(
+        actions=[{
+            "arguments": [],
+            "output_ids": ["0", "1"],
+            "input_dep_set_ids": ["1"]
+        }],
+        artifact_paths=["exec/path/zero", "exec/path/one"],
+        dep_sets=[{
+            "transitive_dep_set_ids": [],
+            "direct_artifact_ids": ["0"]
+        }, {
+            "transitive_dep_set_ids": ["0"],
+            "direct_artifact_ids": ["1"]
+        }])
+    second = make_aquery_output_with_dep_set(
+        actions=[
+            {
+                "arguments": [],
+                "output_ids": ["0", "1"],
+                "input_dep_set_ids": ["0"]
+            },
+        ],
+        artifact_paths=["exec/path/zero", "exec/path/one"],
+        dep_sets=[{
+            "transitive_dep_set_ids": [],
+            "direct_artifact_ids": ["0"]
+        }])
+
+    expected_error_one = "\n".join([
+        "Difference in the action that generates the following output(s):",
+        "\texec/path/one", "\texec/path/zero", "--- before", "+++ after",
+        "@@ -1,2 +1 @@", "-exec/path/one", " exec/path/zero", "\n"
+    ])
+    attrs = ["inputs"]
+
+    mock_stdout = StringIO()
+    with mock.patch("sys.stdout", mock_stdout):
+      aquery_differ._aquery_diff(first, second, attrs, "before", "after")
+      self.assertIn(expected_error_one, mock_stdout.getvalue())
 
 
 if __name__ == "__main__":
