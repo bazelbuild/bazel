1bfc1a09ed2ec8695893208882bbf938e590944b
Klaus Aehlig <aehlig@google.com>
2019-02-12 08:26:36 -0800

    Progress reporting: also note exceptional end of fetching
    When a repository is fetched, the "experimental UI" also reports
    about the progress of this process. It does so by assuming that
    the latest reported state remains until something new is reported.
    Now, a `fetch` may also terminate by an exception; ensure that
    the end of the fetching is reported in this case as well.
    
    Change-Id: I4a87a0bf400e01e954f44a8b87b72e7bd37b3312
    PiperOrigin-RevId: 233610981

--- src/main/java/com/google/devtools/build/lib/rules/repository/RepositoryDelegatorFunction.java
+++ src/main/java/com/google/devtools/build/lib/rules/repository/RepositoryDelegatorFunction.java
@@ -213,8 +213,14 @@ public final class RepositoryDelegatorFunction implements SkyFunction {
       // Fetching enabled, go ahead.
       env.getListener().post(new RepositoryFetching(repositoryName.getName(), false));
       setupRepositoryRoot(repoRoot);
-      RepositoryDirectoryValue.Builder result =
-          handler.fetch(rule, repoRoot, directories, env, markerData, skyKey);
+      RepositoryDirectoryValue.Builder result = null;
+      try {
+        result = handler.fetch(rule, repoRoot, directories, env, markerData, skyKey);
+      } catch (SkyFunctionException e) {
+        // Upon an exceptional exit, the fetching of that repository is over as well.
+        env.getListener().post(new RepositoryFetching(repositoryName.getName(), true));
+        throw e;
+      }
       if (env.valuesMissing()) {
         env.getListener()
             .post(new RepositoryFetching(repositoryName.getName(), false, "Restarting."));
