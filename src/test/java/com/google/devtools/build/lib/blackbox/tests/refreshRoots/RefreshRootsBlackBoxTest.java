31a787a9c8e6804d5234f20149f8da4af2aaf099
Irina <ichern@google.com>
2019-02-14 19:26:09 +0100

    Quick & dirty invalidation of workspace and .bazelignore files
    - should be better replaced by diffing the real changes
    - to be continued
    - test with dirty WORKSPACE file + deleted node_modules seems to work

--- src/test/java/com/google/devtools/build/lib/blackbox/tests/refreshRoots/RefreshRootsBlackBoxTest.java
+++ src/test/java/com/google/devtools/build/lib/blackbox/tests/refreshRoots/RefreshRootsBlackBoxTest.java
@@ -9,7 +9,6 @@ import com.google.devtools.build.lib.blackbox.junit.AbstractBlackBoxTest;
 import com.google.devtools.build.lib.util.ResourceFileLoader;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.After;
@@ -43,6 +42,42 @@ public class RefreshRootsBlackBoxTest extends AbstractBlackBoxTest {
 
     ProcessResult secondBuildResult = context().bazel().build("//...");
     assertThat(shouldNotBeUpdated(secondBuildResult)).isTrue();
+  }
+  @Test
+  public void testAddRefreshLater() throws Exception {
+    generateProject();
+    // now we remove the refresh roots
+    List<String> properWorkspaceText = context().read("WORKSPACE");
+
+    context().write("WORKSPACE", "workspace(name = \"fine_grained_user_modules\")",
+        "load(\":use_node_modules.bzl\", \"generate_fine_grained_node_modules\")",
+        "generate_fine_grained_node_modules(name = \"generated_node_modules\",",
+        "package_json = \"//:package.json\",)");
+
+    // for the first time, this still works
+    ProcessResult firstBuildResult = context().bazel().build("//...");
+    assertThat(buildSucceeded(firstBuildResult)).isTrue();
+    assertThat(shouldBeUpdated(firstBuildResult)).isTrue();
+    checkProjectFiles();
+
+    // now we delete node_modules and the build fails
+    Path nodeModules = context().getWorkDir().resolve("node_modules");
+    PathUtils.deleteTree(nodeModules);
+
+    ProcessResult secondBuildResult = context().bazel().withErrorCode(1).build("//...");
+    // an attempt to update still works
+    assertThat(shouldBeUpdated(secondBuildResult)).isTrue();
+    // but not the actual files state for Bazel
+    assertThat(buildFailed(secondBuildResult)).isTrue();
+
+    // now change the WORKSPACE file back (and delete node_modules again)
+    PathUtils.deleteTree(nodeModules);
+    context().write("WORKSPACE", properWorkspaceText.toArray(new String[0]));
+
+    ProcessResult thirdBuildResult = context().bazel().build("//...");
+    assertThat(buildSucceeded(thirdBuildResult)).isTrue();
+    assertThat(shouldBeUpdated(thirdBuildResult)).isTrue();
+    checkProjectFiles();
   }
 
   @Test
@@ -116,19 +151,24 @@ public class RefreshRootsBlackBoxTest extends AbstractBlackBoxTest {
   }
 
   private boolean shouldBeUpdated(ProcessResult result) {
-    return shouldXXXBeUpdated(result, true);
+    return findPattern(result, "Should be updated");
   }
 
   private boolean shouldNotBeUpdated(ProcessResult result) {
-    return shouldXXXBeUpdated(result, false);
+    return findPattern(result, "Should not be updated");
   }
 
-  private boolean shouldXXXBeUpdated(ProcessResult result, boolean value) {
+  private boolean buildSucceeded(ProcessResult result) {
+    return findPattern(result, "INFO: Build completed successfully");
+  }
+
+  private boolean buildFailed(ProcessResult result) {
+    return findPattern(result, "FAILED: Build did NOT complete successfully");
+  }
+
+  private boolean findPattern(ProcessResult result, String pattern) {
     String[] lines = result.errString().split("\n");
-    String pattern = value ? "Should be updated" : "Should not be updated";
-    return Arrays.stream(lines).
-        filter(s -> s.startsWith("DEBUG:")).
-        anyMatch(s -> s.endsWith(pattern));
+    return Arrays.stream(lines).anyMatch(s -> s.contains(pattern));
   }
 
   @Override
