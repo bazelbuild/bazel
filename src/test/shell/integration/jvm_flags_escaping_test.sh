8bcf935d6caa6e63981b72ca6436f5ea20d6d9d1
Laszlo Csomor <laszlocsomor@google.com>
2019-02-21 16:20:52 +0100

    test: inline flags, add comments

--- src/test/shell/integration/jvm_flags_escaping_test.sh
+++ src/test/shell/integration/jvm_flags_escaping_test.sh
@@ -69,9 +69,16 @@ fi
 # HELPER FUNCTIONS
 # ----------------------------------------------------------------------
 
+# Writes a java file that prints System.getProperty(argN).
+#
+# The program prints every JVM definition of the form argN, where N >= 0, until
+# the first N is found for which argN is empty.
+#
+# Args:
+# $1: directory (package path) where the file will be written
 function create_java_file_that_prints_jvm_args() {
-  local -r pkg="$1"
-  mkdir -p "$pkg"
+  local -r pkg="$1"; shift
+  mkdir -p "$pkg" || fail "mkdir -p $pkg"
   cat >"$pkg/A.java" <<'eof'
 public class A {
   public static void main(String[] args) {
@@ -88,9 +95,13 @@ public class A {
 eof
 }
 
+# Writes a BUILD file for a java_binary with an untokenizable jvm_flags entry.
+#
+# Args:
+# $1: directory (package path) where the file will be written
 function create_build_file_for_untokenizable_flag() {
-  local -r pkg="$1"
-  mkdir -p "$pkg"
+  local -r pkg="$1"; shift
+  mkdir -p "$pkg" || fail "mkdir -p $pkg"
   cat >"$pkg/BUILD" <<'eof'
 java_binary(
     name = "cannot_tokenize",
@@ -101,9 +112,15 @@ java_binary(
 eof
 }
 
+# Writes a BUILD file for a java_binary with many different jvm_flags entries.
+#
+# Use this together with assert_output_of_the_program_with_many_jvm_flags().
+#
+# Args:
+# $1: directory (package path) where the file will be written
 function create_build_file_with_many_jvm_flags() {
-  local -r pkg="$1"
-  mkdir -p "$pkg"
+  local -r pkg="$1"; shift
+  mkdir -p "$pkg" || fail "mkdir -p $pkg"
   cat >"$pkg/BUILD" <<'eof'
 java_binary(
     name = "x",
@@ -158,6 +175,10 @@ java_binary(
 eof
 }
 
+# Asserts that the $TEST_log contains all JVM definitions of the form argN.
+#
+# See create_build_file_with_many_jvm_flags() and
+# create_java_file_that_prints_jvm_args().
 function assert_output_of_the_program_with_many_jvm_flags() {
   expect_log 'arg0=()'
   expect_log 'arg1=( )'
@@ -204,16 +225,24 @@ function assert_output_of_the_program_with_many_jvm_flags() {
   expect_log 'arg42=("C:\\t u\\w\\")'
 }
 
+# Runs a program, expecting it to succeed. Redirects all output to $TEST_log.
+#
+# Args:
+# $1: path of the program
 function expect_program_runs() {
-  local -r path="$1"
+  local -r path="$1"; shift
   (RUNFILES_DIR= \
    RUNFILES_MANIFEST_FILE= \
    RUNFILES_MANIFEST_ONLY= \
    "$path" >&"$TEST_log" ; ) || fail "expected success"
 }
 
+# Runs a program, expecting it to fail. Redirects all output to $TEST_log.
+#
+# Args:
+# $1: path of the program
 function expect_program_cannot_run() {
-  local -r path="$1"
+  local -r path="$1"; shift
   (RUNFILES_DIR= \
    RUNFILES_MANIFEST_FILE= \
    RUNFILES_MANIFEST_ONLY= \
@@ -225,15 +254,14 @@ function expect_program_cannot_run() {
 # ----------------------------------------------------------------------
 
 function test_no_windows_jvm_flags_escaping() {
-  local -r pkg="${FUNCNAME[0]}"
-  local -r flag="--noincompatible_windows_escape_jvm_flags"
+  local -r pkg="${FUNCNAME[0]}"  # unique package name for this test
 
   create_java_file_that_prints_jvm_args "$pkg"
   create_build_file_with_many_jvm_flags "$pkg"
 
   # On all platforms, Bazel can build the target.
-  bazel build --verbose_failures "$flag" "${pkg}:x" &>"$TEST_log" \
-    || fail "expected success"
+  bazel build --verbose_failures --noincompatible_windows_escape_jvm_flags \
+    "${pkg}:x" &>"$TEST_log" || fail "expected success"
   if "$is_windows"; then
     # On Windows, the target cannot run.
     expect_program_cannot_run "bazel-bin/$pkg/x${EXE_EXT}"
@@ -246,29 +274,27 @@ function test_no_windows_jvm_flags_escaping() {
 }
 
 function test_windows_jvm_flags_escaping() {
-  local -r pkg="${FUNCNAME[0]}"
-  local -r flag="--incompatible_windows_escape_jvm_flags"
+  local -r pkg="${FUNCNAME[0]}"  # unique package name for this test
 
   create_java_file_that_prints_jvm_args "$pkg"
   create_build_file_with_many_jvm_flags "$pkg"
 
   # On all platforms, Bazel can build and run the target.
-  bazel build --verbose_failures "$flag" "${pkg}:x" &>"$TEST_log" \
-    || fail "expected success"
+  bazel build --verbose_failures --incompatible_windows_escape_jvm_flags \
+    "${pkg}:x" &>"$TEST_log" || fail "expected success"
   expect_program_runs "bazel-bin/$pkg/x${EXE_EXT}"
   assert_output_of_the_program_with_many_jvm_flags
 }
 
 function test_untokenizable_jvm_flag_when_escaping_is_disabled() {
-  local -r pkg="${FUNCNAME[0]}"
-  local -r flag="--noincompatible_windows_escape_jvm_flags"
+  local -r pkg="${FUNCNAME[0]}"  # unique package name for this test
 
   create_java_file_that_prints_jvm_args "$pkg"
   create_build_file_for_untokenizable_flag "$pkg"
 
   # On all platforms, Bazel can build the target.
-  bazel build "$flag" --verbose_failures "${pkg}:cannot_tokenize" \
-    2>"$TEST_log" || fail "expected success"
+  bazel build --verbose_failures --noincompatible_windows_escape_jvm_flags \
+    "${pkg}:cannot_tokenize" 2>"$TEST_log" || fail "expected success"
   if "$is_windows"; then
     # On Windows, Bazel will not check the flag. It will just propagate the flag
     # to the launcher, which also just passes it to the JVM.  This is bad, the
@@ -283,21 +309,22 @@ function test_untokenizable_jvm_flag_when_escaping_is_disabled() {
 }
 
 function test_untokenizable_jvm_flag_when_escaping_is_enabled() {
-  local -r pkg="${FUNCNAME[0]}"
-  local -r flag="--incompatible_windows_escape_jvm_flags"
+  local -r pkg="${FUNCNAME[0]}"  # unique package name for this test
+
   create_java_file_that_prints_jvm_args "$pkg"
   create_build_file_for_untokenizable_flag "$pkg"
 
+  local -r flag="--incompatible_windows_escape_jvm_flags"
   if "$is_windows"; then
     # On Windows, Bazel will check the flag. It will just propagate the flag
     # to the launcher, which also just passes it to the JVM.  This is bad, the
     # flag should have been rejected because it cannot be Bash-tokenized.
-    bazel build "$flag" --verbose_failures "${pkg}:cannot_tokenize" \
+    bazel build --verbose_failures "$flag" "${pkg}:cannot_tokenize" \
       2>"$TEST_log" && fail "expected failure" || true
     expect_log "ERROR:.*in jvm_flags attribute of java_binary rule"
   else
     # On other platforms, Bazel will build the target but it fails to run.
-    bazel build "$flag" --verbose_failures "${pkg}:cannot_tokenize" \
+    bazel build --verbose_failures "$flag" "${pkg}:cannot_tokenize" \
       2>"$TEST_log" || fail "expected success"
     expect_program_cannot_run "bazel-bin/$pkg/cannot_tokenize${EXE_EXT}"
     expect_log "syntax error near unexpected token"
