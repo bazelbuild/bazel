046d253791b7f69c1e6444d4aef0625b67be02dc
ishikhman <ishikhman@google.com>
2019-02-28 02:25:30 -0800

    #5256 Set User-Agent Header to http calls
    Set User-Agent header for http remote cache
    
    Closes #7563.
    
    PiperOrigin-RevId: 236083717

--- src/test/java/com/google/devtools/build/lib/runtime/BlazeVersionInfoTest.java
+++ src/test/java/com/google/devtools/build/lib/runtime/BlazeVersionInfoTest.java
@@ -14,11 +14,10 @@
 package com.google.devtools.build.lib.runtime;
 
 import static com.google.common.truth.Truth.assertThat;
-import static java.util.Collections.singletonMap;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.devtools.build.lib.analysis.BlazeVersionInfo;
 import com.google.devtools.build.lib.util.StringUtilities;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
@@ -34,7 +33,7 @@ public class BlazeVersionInfoTest {
 
   @Test
   public void testEmptyVersionInfoMeansNotAvailable() {
-    BlazeVersionInfo info = new BlazeVersionInfo(Collections.<String, String>emptyMap());
+    BlazeVersionInfo info = new BlazeVersionInfo(ImmutableMap.of());
     assertThat(info.isAvailable()).isFalse();
     assertThat(info.getSummary()).isNull();
     assertThat(info.getReleaseName()).isEqualTo("development version");
@@ -42,14 +41,14 @@ public class BlazeVersionInfoTest {
 
   @Test
   public void testReleaseNameIsDevelopmentIfBuildLabelIsNull() {
-    Map<String, String> data = singletonMap("Build label", "");
+    Map<String, String> data = ImmutableMap.of("Build label", "");
     BlazeVersionInfo info = new BlazeVersionInfo(data);
     assertThat(info.getReleaseName()).isEqualTo("development version");
   }
 
   @Test
   public void testReleaseNameIfBuildLabelIsPresent() {
-    Map<String, String> data = singletonMap("Build label", "3/4/2009 (gold)");
+    Map<String, String> data = ImmutableMap.of("Build label", "3/4/2009 (gold)");
     BlazeVersionInfo info = new BlazeVersionInfo(data);
     assertThat(info.getReleaseName()).isEqualTo("release 3/4/2009 (gold)");
   }
@@ -63,5 +62,18 @@ public class BlazeVersionInfoTest {
     BlazeVersionInfo info = new BlazeVersionInfo(data);
     Map<String, String> sortedData = new TreeMap<>(data);
     assertThat(info.getSummary()).isEqualTo(StringUtilities.layoutTable(sortedData));
+  }
+
+  @Test
+  public void testVersionIsHeadIfBuildLabelIsNull() {
+    BlazeVersionInfo info = new BlazeVersionInfo(ImmutableMap.of());
+    assertThat(info.getVersion()).isEqualTo("HEAD");
+  }
+
+  @Test
+  public void testVersionsIIfBuildLabelIsPresent() {
+    Map<String, String> data = ImmutableMap.of("Build label", "123.4");
+    BlazeVersionInfo info = new BlazeVersionInfo(data);
+    assertThat(info.getVersion()).isEqualTo("123.4");
   }
 }
