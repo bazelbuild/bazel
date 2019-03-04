f9c82e0282ffc0d16b53520c5072b0b15e0a582e
djasper <djasper@google.com>
2019-03-04 11:02:10 -0800

    Create less garbage for the common case of not having any aspects.
    RELNOTES: None.
    PiperOrigin-RevId: 236686052

--- src/main/java/com/google/devtools/build/lib/analysis/DependencyResolver.java
+++ src/main/java/com/google/devtools/build/lib/analysis/DependencyResolver.java
@@ -631,9 +631,9 @@ public abstract class DependencyResolver {
    * Filter the set of aspects that are to be propagated according to the set of advertised
    * providers of the dependency.
    */
-  private AspectCollection filterPropagatingAspects(Iterable<Aspect> aspects, Target toTarget)
+  private AspectCollection filterPropagatingAspects(ImmutableList<Aspect> aspects, Target toTarget)
       throws InconsistentAspectOrderException {
-    if (!(toTarget instanceof Rule)) {
+    if (!(toTarget instanceof Rule) || aspects.isEmpty()) {
       return AspectCollection.EMPTY;
     }
 
