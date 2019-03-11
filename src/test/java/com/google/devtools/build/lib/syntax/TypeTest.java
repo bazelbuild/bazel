bazelbuild/bazel

commit 04f28cb87cde05ed2504a5fa974f282ebb727ff5

Automated rollback of commit f74605413f51b6c07e3425528bb878ec534ddf39.
*** Reason for rollback ***

Rollforward with fix for bazel_workspace_status_test.

*** Original change description ***

bazel/packages: make Tristate reject True/False (use 1/0)

Fixes https://github.com/bazelbuild/bazel/issues/7663

RELNOTES: Tristate attributes of native rules now reject True/False (use 1/0)
PiperOrigin-RevId: 237383340

src/main/java/com/google/devtools/build/lib/packages/BuildType.java
@@ -724,12 +724,10 @@ public final class BuildType {
   }
 
   /**
-   * Tristate values are needed for cases where user intent matters.
-   *
-   * <p>Tristate values are not explicitly interchangeable with booleans and are
-   * handled explicitly as TriStates. Prefer Booleans with default values where
-   * possible.  The main use case for TriState values is when a Rule's behavior
-   * must interact with a Flag value in a complicated way.</p>
+   * A TriState value is like a boolean attribute whose default value may be distinguished from
+   * either of the possible explicitly assigned values. TriState attributes may be assigned the
+   * values 0 (NO), 1 (YES), or None (AUTO). TriState is deprecated; use attr.int(values=[-1, 0, 1])
+   * instead.
    */
   private static class TriStateType extends Type<TriState> {
     @Override
@@ -751,7 +749,6 @@ public final class BuildType {
       return "tristate";
     }
 
-    // Like BooleanType, this must handle integers as well.
     @Override
     public TriState convert(Object x, Object what, Object context)
         throws ConversionException {
@@ -759,7 +756,12 @@ public final class BuildType {
         return (TriState) x;
       }
       if (x instanceof Boolean) {
-        return ((Boolean) x) ? TriState.YES : TriState.NO;
+        throw new ConversionException(
+            this,
+            x,
+            "rule attribute (tristate is being replaced by "
+                + "attr.int(values=[-1, 0, 1]), and it no longer accepts Boolean values; "
+                + "instead, use 0 or 1, or None for the default)");
       }
       Integer xAsInteger = INTEGER.convert(x, what, context);
       if (xAsInteger == -1) {

src/test/java/com/google/devtools/build/lib/syntax/TypeTest.java
@@ -142,8 +142,6 @@ public class TypeTest {
     assertThat(BuildType.TRISTATE.convert(1, null)).isEqualTo(TriState.YES);
     assertThat(BuildType.TRISTATE.convert(0, null)).isEqualTo(TriState.NO);
     assertThat(BuildType.TRISTATE.convert(-1, null)).isEqualTo(TriState.AUTO);
-    assertThat(BuildType.TRISTATE.convert(true, null)).isEqualTo(TriState.YES);
-    assertThat(BuildType.TRISTATE.convert(false, null)).isEqualTo(TriState.NO);
     assertThat(BuildType.TRISTATE.convert(TriState.YES, null)).isEqualTo(TriState.YES);
     assertThat(BuildType.TRISTATE.convert(TriState.NO, null)).isEqualTo(TriState.NO);
     assertThat(BuildType.TRISTATE.convert(TriState.AUTO, null)).isEqualTo(TriState.AUTO);
@@ -164,8 +162,8 @@ public class TypeTest {
   }
 
   @Test
-  public void testTriStateDoesNotAcceptStrings() throws Exception {
-    List<String> listOfCases = Lists.newArrayList("bad", "true", "auto", "false");
+  public void testTriStateDoesNotAcceptStringsOrBools() throws Exception {
+    List<?> listOfCases = Lists.newArrayList("bad", "true", "auto", "false", true, false);
     for (Object entry : listOfCases) {
       try {
         BuildType.TRISTATE.convert(entry, null);

src/test/shell/bazel/bazel_workspace_status_test.sh
@@ -107,7 +107,7 @@ cc_library(
     linkstamp="linkstamped_library.cc")
 cc_test(
     name="verify_scm_status",
-    stamp=True,
+    stamp=1,
     srcs=["verify_scm_status.cc"],
     deps=[":linkstamped_library"])
 EOF

commit 3c760780953a031729ff388c9a6ac8461631edaa

bazel/packages: make Tristate accept True/False again
This is in effect a partial rollback of https://github.com/bazelbuild/bazel/commit/04f28cb87cde05ed2504a5fa974f282ebb727ff5
(itself a second attempt of https://github.com/bazelbuild/bazel/commit/6696652ea298f7d9437c5702265573323d5d7f53), which broke various builds:
though we attempted to fix BUILD files that violate the new
stricter rule, we failed to catch them all and deal with
regressions in a timely manner.

Updates https://github.com/bazelbuild/bazel/issues/7663

RELNOTES: Tristate attributes of native rules now temporarily accept True/False again
PiperOrigin-RevId: 237870436

src/main/java/com/google/devtools/build/lib/packages/BuildType.java
@@ -756,12 +756,12 @@ public final class BuildType {
         return (TriState) x;
       }
       if (x instanceof Boolean) {
-        throw new ConversionException(
-            this,
-            x,
-            "rule attribute (tristate is being replaced by "
-                + "attr.int(values=[-1, 0, 1]), and it no longer accepts Boolean values; "
-                + "instead, use 0 or 1, or None for the default)");
+        // TODO(adonovan): re-enable this under flag control; see b/116691720.
+        // throw new ConversionException(this, x,
+        //   "rule attribute (tristate is being replaced by "
+        //       + "attr.int(values=[-1, 0, 1]), and it no longer accepts Boolean values; "
+        //       + "instead, use 0 or 1, or None for the default)");
+        return ((Boolean) x) ? TriState.YES : TriState.NO;
       }
       Integer xAsInteger = INTEGER.convert(x, what, context);
       if (xAsInteger == -1) {

src/test/java/com/google/devtools/build/lib/syntax/TypeTest.java
@@ -146,6 +146,10 @@ public class TypeTest {
     assertThat(BuildType.TRISTATE.convert(TriState.NO, null)).isEqualTo(TriState.NO);
     assertThat(BuildType.TRISTATE.convert(TriState.AUTO, null)).isEqualTo(TriState.AUTO);
     assertThat(collectLabels(BuildType.TRISTATE, TriState.YES)).isEmpty();
+
+    // deprecated:
+    assertThat(BuildType.TRISTATE.convert(true, null)).isEqualTo(TriState.YES);
+    assertThat(BuildType.TRISTATE.convert(false, null)).isEqualTo(TriState.NO);
   }
 
   @Test
@@ -162,8 +166,9 @@ public class TypeTest {
   }
 
   @Test
-  public void testTriStateDoesNotAcceptStringsOrBools() throws Exception {
-    List<?> listOfCases = Lists.newArrayList("bad", "true", "auto", "false", true, false);
+  public void testTriStateDoesNotAcceptStrings() throws Exception {
+    List<?> listOfCases = Lists.newArrayList("bad", "true", "auto", "false");
+    // TODO(adonovan): add booleans true, false to this list; see b/116691720.
     for (Object entry : listOfCases) {
       try {
         BuildType.TRISTATE.convert(entry, null);