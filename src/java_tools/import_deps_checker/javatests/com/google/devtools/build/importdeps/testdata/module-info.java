6ad5a3489ee8a688dcabe34250e0051ab95b5684
Andy Scott <andy.g.scott@gmail.com>
2019-03-11 14:02:15 -0700

    Fix mistake during addition of license header

--- src/java_tools/import_deps_checker/javatests/com/google/devtools/build/importdeps/testdata/module-info.java
+++ src/java_tools/import_deps_checker/javatests/com/google/devtools/build/importdeps/testdata/module-info.java
@@ -12,6 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-:open module com.google.devtools.build.importdeps.testdata {
+open module com.google.devtools.build.importdeps.testdata {
     exports com.google.devtools.build.importdeps.testdata;
 }
