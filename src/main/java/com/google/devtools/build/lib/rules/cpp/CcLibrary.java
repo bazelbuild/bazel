756c9c7e4d9545b288fb82f7993b7a70e059a4ac
dbabkin <dbabkin@google.com>
2019-03-15 08:14:36 -0700

    Implement backsliding protection for cc_* -> proto_library edge.
    Follow up for cc_binary implemented in unknown commit
    
    Rules implemented there are:
    cc_inc_library
    py_wrap_cc
    java_wrap_cc
    go_wrap_cc
    
    Tests added to:
    cc_library
    cc_test
    cc_fake_binary
    
    NO_IFTTT=I do not add validation to []
    
    RELNOTES:none
    PiperOrigin-RevId: 238643883

--- src/main/java/com/google/devtools/build/lib/rules/cpp/CcLibrary.java
+++ src/main/java/com/google/devtools/build/lib/rules/cpp/CcLibrary.java
@@ -114,6 +114,11 @@ public abstract class CcLibrary implements RuleConfiguredTargetFactory {
       boolean addDynamicRuntimeInputArtifactsToRunfiles)
       throws RuleErrorException, InterruptedException {
 
+    semantics.validateDeps(ruleContext);
+    if (ruleContext.hasErrors()) {
+      return;
+    }
+
     final CcCommon common = new CcCommon(ruleContext);
 
     CcToolchainProvider ccToolchain = common.getToolchain();
