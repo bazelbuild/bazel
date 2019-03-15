7a3af9f6f98ce69c1ebd2931817c2664a18cf279
Irina <ichern@google.com>
2019-03-15 05:44:01 -0700

    Add helper method for debug of Bazel run by JUnit black box tests
    Closes #7735.
    
    PiperOrigin-RevId: 238626987

--- src/test/java/com/google/devtools/build/lib/blackbox/framework/BuilderRunner.java
+++ src/test/java/com/google/devtools/build/lib/blackbox/framework/BuilderRunner.java
@@ -51,6 +51,7 @@ public final class BuilderRunner {
   private int errorCode = 0;
   private List<String> flags;
   private boolean shouldFail;
+  private boolean enableDebug;
 
   /**
    * Creates the BuilderRunner
@@ -128,6 +129,17 @@ public final class BuilderRunner {
   public BuilderRunner withTimeout(long timeoutMillis) {
     Preconditions.checkState(this.timeoutMillis > 0);
     this.timeoutMillis = timeoutMillis;
+    return this;
+  }
+
+  /**
+   * Should be used ONLY FOR TESTS DEBUG. Adds "--host_jvm_debug" to the Bazel startup options, so
+   * that the JVM waits for the debugger process to connect before executing any code.
+   *
+   * @return this BuilderRunner instance
+   */
+  public BuilderRunner enableDebug() {
+    this.enableDebug = true;
     return this;
   }
 
@@ -266,6 +278,9 @@ public final class BuilderRunner {
         list.add("--bazelrc");
         list.add(bazelRc.toAbsolutePath().toString());
       }
+    }
+    if (enableDebug) {
+      list.add("--host_jvm_debug");
     }
     list.add(command);
     list.addAll(this.flags);
