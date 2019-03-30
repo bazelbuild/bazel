3cb0c39e229d35ad23fc3a2e9fd40e32b5c9a523
janakr <janakr@google.com>
2019-03-29 20:08:41 -0700

    Tighten the type of NodeEntry#getAllRemainingDirtyDirectDeps since it is always an ImmutableSet.
    PiperOrigin-RevId: 241094312

--- src/main/java/com/google/devtools/build/skyframe/AbstractParallelEvaluator.java
+++ src/main/java/com/google/devtools/build/skyframe/AbstractParallelEvaluator.java
@@ -417,7 +417,7 @@ public abstract class AbstractParallelEvaluator {
           evaluatorContext.getProgressReceiver().stateEnding(skyKey, NodeState.CHECK_DIRTY, -1);
         }
 
-        Set<SkyKey> oldDeps = state.getAllRemainingDirtyDirectDeps();
+        ImmutableSet<SkyKey> oldDeps = state.getAllRemainingDirtyDirectDeps();
         try {
           evaluatorContext
               .getProgressReceiver()
@@ -913,7 +913,7 @@ public abstract class AbstractParallelEvaluator {
   private boolean maybeHandleRegisteringNewlyDiscoveredDepsForDoneEntry(
       SkyKey skyKey,
       NodeEntry entry,
-      Set<SkyKey> oldDeps,
+      ImmutableSet<SkyKey> oldDeps,
       SkyFunctionEnvironment env,
       boolean keepGoing)
       throws InterruptedException {
