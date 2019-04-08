c0e6952538df037c1df206097292be9198a777cf
Laszlo Csomor <laszlocsomor@google.com>
2019-04-08 10:04:01 -0700

    Windows, java launcher: clean up after flag flip
    Remove --incompatible_windows_escape_jvm_flags.
    
    The flag is now flipped to true, and the
    false-branch logic is removed.
    
    See https://github.com/bazelbuild/bazel/issues/7486
    
    RELNOTES[INC]: --incompatible_windows_escape_jvm_flags is enabled by default, and the flag no longer exists
    
    Change-Id: I4ba9a626c28ce9690c0850fa052c302f80bccc1c
    
    Closes #7972.
    
    Change-Id: I4ba9a626c28ce9690c0850fa052c302f80bccc1c
    PiperOrigin-RevId: 242482659

--- src/main/java/com/google/devtools/build/lib/rules/java/JavaOptions.java
+++ src/main/java/com/google/devtools/build/lib/rules/java/JavaOptions.java
@@ -613,27 +613,6 @@ public class JavaOptions extends FragmentOptions {
           "Disables the resource_jars attribute; use java_import and deps or runtime_deps instead.")
   public boolean disallowResourceJars;
 
-  @Option(
-      name = "incompatible_windows_escape_jvm_flags",
-      defaultValue = "true",
-      documentationCategory = OptionDocumentationCategory.OUTPUT_PARAMETERS,
-      effectTags = {
-        OptionEffectTag.ACTION_COMMAND_LINES,
-        OptionEffectTag.AFFECTS_OUTPUTS,
-      },
-      metadataTags = {
-        OptionMetadataTag.INCOMPATIBLE_CHANGE,
-        OptionMetadataTag.TRIGGERED_BY_ALL_INCOMPATIBLE_CHANGES
-      },
-      help =
-          "On Linux/macOS/non-Windows: no-op. On Windows: this flag affects how java_binary and"
-              + " java_test targets are built; in particular, how the launcher of these targets"
-              + " escapes flags at the time of running the java_binary/java_test. When the flag is"
-              + " true, the launcher escapes the JVM flags using Windows-style escaping (correct"
-              + " behavior). When the flag is false, the launcher uses Bash-style escaping"
-              + " (buggy behavior). See https://github.com/bazelbuild/bazel/issues/7072")
-  public boolean windowsEscapeJvmFlags;
-
   Label defaultJavaBase() {
     return Label.parseAbsoluteUnchecked(DEFAULT_JAVABASE);
   }
@@ -694,8 +673,6 @@ public class JavaOptions extends FragmentOptions {
     host.requireJavaToolchainHeaderCompilerDirect = requireJavaToolchainHeaderCompilerDirect;
 
     host.disallowResourceJars = disallowResourceJars;
-
-    host.windowsEscapeJvmFlags = windowsEscapeJvmFlags;
 
     return host;
   }
