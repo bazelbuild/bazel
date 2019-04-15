4c65d3f258c19e4c667ee4102924cf9d86dada3f
Jakob Buchgraber <buchgr@google.com>
2019-04-15 14:10:23 +0200

    address comments

--- src/test/shell/integration/discard_analysis_cache_test.sh
+++ src/test/shell/integration/discard_analysis_cache_test.sh
@@ -16,11 +16,6 @@
 #
 # A test for --discard_analysis_cache.
 
-# Load the test setup defined in the parent directory
-CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "${CURRENT_DIR}/../integration_test_setup.sh" \
-  || { echo "integration_test_setup.sh not found!" >&2; exit 1; }
-
 # --- begin runfiles.bash initialization ---
 if [[ ! -d "${RUNFILES_DIR:-/dev/null}" && ! -f "${RUNFILES_MANIFEST_FILE:-/dev/null}" ]]; then
     if [[ -f "$0.runfiles_manifest" ]]; then
@@ -42,6 +37,9 @@ else
 fi
 # --- end runfiles.bash initialization ---
 
+source "$(rlocation "io_bazel/src/test/shell/integration_test_setup.sh")" \
+  || { echo "integration_test_setup.sh not found!" >&2; exit 1; }
+
 IS_WINDOWS=false
 case "$(uname | tr [:upper:] [:lower:])" in
 msys*|mingw*|cygwin*)
@@ -55,14 +53,10 @@ else
 fi
 
 javabase="$1"
-if [[ $javabase = /* || $javabase =~ [A-Za-z]:[/\\] ]]; then
-  jmaptool="$1/bin/jmap${EXE_EXT}"
-else
-  if [[ $javabase = external/* ]]; then
-    javabase=${javabase#external/}
-  fi
-  jmaptool="$(rlocation "${javabase}/bin/jmap${EXE_EXT}")"
+if [[ $javabase = external/* ]]; then
+  javabase=${javabase#external/}
 fi
+jmaptool="$(rlocation "${javabase}/bin/jmap${EXE_EXT}")"
 
 function write_hello_world_files() {
   mkdir -p hello || fail "mkdir hello failed"
