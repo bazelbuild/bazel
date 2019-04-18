dc242d4d74d93c0da93ff6f03530a0373a7bb7f2
mstaib <mstaib@google.com>
2019-04-18 10:20:13 -0700

    Allow accessing the configuration of a dependency during resolution.
    This is necessary because with trimming, the configuration that a configured
    target was analyzed with may not be the configuration that it was requested
    with.
    
    Progress on #6524.
    
    PiperOrigin-RevId: 244209432

--- src/main/java/com/google/devtools/build/lib/skyframe/ConfiguredTargetFunction.java
+++ src/main/java/com/google/devtools/build/lib/skyframe/ConfiguredTargetFunction.java
@@ -736,12 +736,32 @@ public final class ConfiguredTargetFunction implements SkyFunction {
               }
             }
             try {
+              BuildConfiguration depConfiguration = dep.getConfiguration();
+              BuildConfigurationValue.Key depKey =
+                  depValue.getConfiguredTarget().getConfigurationKey();
+              // Retroactive trimming may change the configuration associated with the dependency.
+              // If it does, we need to get that instance.
+              // TODO(mstaib): doing these individually instead of doing them all at once may end up
+              // being wasteful use of Skyframe. Although these configurations are guaranteed to be
+              // in the Skyframe cache (because the dependency would have had to retrieve them to be
+              // created in the first place), looking them up repeatedly may be slower than just
+              // keeping a local cache and assigning the same configuration to all the CTs which
+              // need it. Profile this and see if there's a better way.
+              if (depKey != null && !depKey.equals(BuildConfigurationValue.key(depConfiguration))) {
+                if (!depConfiguration.trimConfigurationsRetroactively()) {
+                  throw new AssertionError(
+                      "Loading configurations mid-dependency resolution should ONLY happen when "
+                          + "retroactive trimming is enabled.");
+                }
+                depConfiguration =
+                    ((BuildConfigurationValue) env.getValue(depKey)).getConfiguration();
+              }
               result.put(
                   key,
                   new ConfiguredTargetAndData(
                       depValue.getConfiguredTarget(),
                       pkgValue.getPackage().getTarget(depLabel.getName()),
-                      dep.getConfiguration()));
+                      depConfiguration));
             } catch (NoSuchTargetException e) {
               throw new IllegalStateException("Target already verified for " + dep, e);
             }
