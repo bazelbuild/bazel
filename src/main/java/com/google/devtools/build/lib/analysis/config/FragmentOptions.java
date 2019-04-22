116337b62bd1c746ee19f5ac22ca20d18293a22f
Googler <noreply@google.com>
2019-04-22 10:13:36 -0700

    Avoid cloning FragmentOptions if the instance is already normalized.
    RELNOTES: None.
    PiperOrigin-RevId: 244682258

--- src/main/java/com/google/devtools/build/lib/analysis/config/FragmentOptions.java
+++ src/main/java/com/google/devtools/build/lib/analysis/config/FragmentOptions.java
@@ -53,8 +53,10 @@ public abstract class FragmentOptions extends OptionsBase implements Cloneable, 
   }
 
   /**
-   * Creates a new instance of this {@code FragmentOptions} with all flags adjusted to be suitable
-   * for forming configurations.
+   * Returns an instance of {@code FragmentOptions} with all flags adjusted to be suitable for
+   * forming configurations.
+   *
+   * <p>If this instance is already suitable, it will be returned without creating a new instance.
    *
    * <p>Motivation: Sometimes a fragment's physical option values, as set by the options parser, do
    * not correspond to their logical interpretation. For example, an option may need custom code to
@@ -71,7 +73,7 @@ public abstract class FragmentOptions extends OptionsBase implements Cloneable, 
    * method. Step 2) is the responsibility of each transition implementation.
    */
   public FragmentOptions getNormalized() {
-    return clone();
+    return this;
   }
 
   /** Tracks limitations on referring to an option in a {@code config_setting}. */
