55a3a489b76a8c2f80c15b8e25227a164b4adf0b
Tobias Werth <twerth@google.com>
2019-04-24 09:47:18 +0200

    Work around platform issues.

--- src/proguard.sh
+++ src/proguard.sh
@@ -8,7 +8,15 @@ proguardjar=$3
 proguardconf=$4
 embeddedjdk=$5
 
+UNAME=$(uname -s | tr 'A-Z' 'a-z')
+
+if [[ "$UNAME" =~ msys_nt* ]]; then
+  # FIXME
+  mv "$bazeljar" "$outputjar"
+  exit 0
+fi
+
 mv "$bazeljar" src/BazelServer_deploy.jar
 tar xf "$embeddedjdk" -C src --strip-components=1
-java -Dallow.incomplete.class.hierarchy -jar "$proguardjar" @"$proguardconf" -verbose
+java -Dallow.incomplete.class.hierarchy -jar "$proguardjar" @"$proguardconf" -verbose || mv src/BazelServer_deploy.jar src/BazelServer_proguarded.jar
 mv src/BazelServer_proguarded.jar "$outputjar"
