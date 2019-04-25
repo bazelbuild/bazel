924d414d7e91f1e17244a28695a0ee0724f44e3b
iirina <elenairina@google.com>
2019-04-25 10:00:09 +0200

    Add jacoco agent to the java tools release.

--- src/test/shell/bazel/bazel_java_tools_test.sh
+++ src/test/shell/bazel/bazel_java_tools_test.sh
@@ -154,4 +154,12 @@ function test_java_tools_has_ExperimentalRunner() {
   expect_path_in_java_tools "java_tools/ExperimentalRunner_deploy.jar"
 }
 
+function test_java_tools_has_jacocoagent() {
+  expect_path_in_java_tools "java_tools/third_party/java/jacoco/jacocoagent.jar"
+  expect_path_in_java_tools "java_tools/third_party/java/jacoco/org.jacoco.agent-0.7.5.201505241946.jar"
+  expect_path_in_java_tools "java_tools/third_party/java/jacoco/org.jacoco.core-0.7.5.201505241946.jar"
+  expect_path_in_java_tools "java_tools/third_party/java/jacoco/org.jacoco.report-0.7.5.201505241946.jar"
+  expect_path_in_java_tools "java_tools/third_party/java/jacoco/LICENSE"
+}
+
 run_suite "Java tools archive tests"
