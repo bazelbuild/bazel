d0dc6eb9de3181f4998ac9c9e71df9fb90e51a52
John Cater <jcater@google.com>
2019-04-25 07:02:00 -0700

    Allow all native rules to use "exec_compatible_with" to specify
    additional constraints on the execution platform.
    
    Part of work on #8134.
    Fixes #8026.
    
    Closes #8135.
    
    PiperOrigin-RevId: 245227990

--- src/main/java/com/google/devtools/build/lib/packages/RuleClass.java
+++ src/main/java/com/google/devtools/build/lib/packages/RuleClass.java
@@ -762,6 +762,12 @@ public class RuleClass {
 
         for (Attribute attribute : parent.getAttributes()) {
           String attrName = attribute.getName();
+          // TODO(https://github.com/bazelbuild/bazel/issues/8134): Define the attribute on a
+          // standard base class and remove this check entirely.
+          if (attrName.equals(RuleClass.EXEC_COMPATIBLE_WITH_ATTR)) {
+            // Don't inherit: this will be re-created
+            continue;
+          }
           Preconditions.checkArgument(
               !attributes.containsKey(attrName) || attributes.get(attrName).equals(attribute),
               "Attribute %s is inherited multiple times in %s ruleclass",
