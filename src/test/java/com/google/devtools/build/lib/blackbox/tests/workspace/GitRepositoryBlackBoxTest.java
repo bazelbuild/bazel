f7a1214bab460b657dc364a25eb98024e4bbf92e
Irina <ichern@google.com>
2019-05-08 16:42:41 +0200

    Refactor git_repository and new_git_repository rules implementations so that they can be used with Bazel on Windows without MSYS
    - do not use shell scripting, call git binary and use repository_context methods
    - add black box tests for new_git_repository, so that they run on Windows without MSYS
    - prefer fetch to clone, as in that case less content is fetched (with fetch, we specify both the reference and depth in the same command; on contrary, clone will fetch all branches)

---
+++ src/test/java/com/google/devtools/build/lib/blackbox/tests/workspace/GitRepositoryBlackBoxTest.java
@@ -0,0 +1,121 @@
+// Copyright 2019 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.devtools.build.lib.blackbox.tests.workspace;
+
+import com.google.devtools.build.lib.blackbox.framework.BlackBoxTestContext;
+import com.google.devtools.build.lib.blackbox.framework.BuilderRunner;
+import com.google.devtools.build.lib.blackbox.framework.PathUtils;
+import com.google.devtools.build.lib.blackbox.junit.AbstractBlackBoxTest;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.junit.Test;
+
+/**
+ * Black box tests for git_repository/new_git_repository.
+ * On Windows, runs without MSYS {@link WorkspaceTestUtils#bazel}
+ */
+public class GitRepositoryBlackBoxTest extends AbstractBlackBoxTest {
+
+  private static final String HELLO_FROM_EXTERNAL_REPOSITORY = "Hello from GIT repository!";
+
+  @Test
+  public void testNewGitRepository() throws Exception {
+    Path repo = context().getTmpDir().resolve("ext_repo");
+    setupGitRepository(context(), repo);
+
+    String buildFileContent = String.format("%s\n%s", RepoWithRuleWritingTextGenerator.loadRule(""),
+        RepoWithRuleWritingTextGenerator.callRule("call_write_text", "out.txt",
+            HELLO_FROM_EXTERNAL_REPOSITORY));
+    context().write("WORKSPACE",
+        "load(\"@bazel_tools//tools/build_defs/repo:git.bzl\", \"new_git_repository\")",
+        "new_git_repository(",
+        "  name='ext',",
+        String.format("  remote='%s',", PathUtils.pathToFileURI(repo.resolve(".git"))),
+        "  tag='first',",
+        String.format("  build_file_content=\"\"\"%s\"\"\",", buildFileContent),
+        ")");
+
+    // This creates Bazel without MSYS, see implementation for details.
+    BuilderRunner bazel = WorkspaceTestUtils.bazel(context());
+    bazel.build("@ext//:call_write_text");
+    Path outPath = context().resolveBinPath(bazel, "external/ext/out.txt");
+    WorkspaceTestUtils.assertLinesExactly(outPath, HELLO_FROM_EXTERNAL_REPOSITORY);
+  }
+
+  @Test
+  public void testCloneAtCommit() throws Exception {
+    Path repo = context().getTmpDir().resolve("ext_repo");
+    String commit = setupGitRepository(context(), repo);
+
+    String buildFileContent = String.format("%s\n%s", RepoWithRuleWritingTextGenerator.loadRule(""),
+        RepoWithRuleWritingTextGenerator.callRule("call_write_text", "out.txt",
+            HELLO_FROM_EXTERNAL_REPOSITORY));
+    context().write("WORKSPACE",
+        "load(\"@bazel_tools//tools/build_defs/repo:git.bzl\", \"new_git_repository\")",
+        "new_git_repository(",
+        "  name='ext',",
+        String.format("  remote='%s',", PathUtils.pathToFileURI(repo.resolve(".git"))),
+        String.format("  commit='%s',", commit),
+        String.format("  build_file_content=\"\"\"%s\"\"\",", buildFileContent),
+        ")");
+
+    // This creates Bazel without MSYS, see implementation for details.
+    BuilderRunner bazel = WorkspaceTestUtils.bazel(context());
+    bazel.build("@ext//:call_write_text");
+    Path outPath = context().resolveBinPath(bazel, "external/ext/out.txt");
+    WorkspaceTestUtils.assertLinesExactly(outPath, HELLO_FROM_EXTERNAL_REPOSITORY);
+  }
+
+  @Test
+  public void testCloneAtMaster() throws Exception {
+    Path repo = context().getTmpDir().resolve("ext_repo");
+    setupGitRepository(context(), repo);
+
+    String buildFileContent = String.format("%s\n%s", RepoWithRuleWritingTextGenerator.loadRule(""),
+        RepoWithRuleWritingTextGenerator.callRule("call_write_text", "out.txt",
+            HELLO_FROM_EXTERNAL_REPOSITORY));
+    context().write("WORKSPACE",
+        "load(\"@bazel_tools//tools/build_defs/repo:git.bzl\", \"new_git_repository\")",
+        "new_git_repository(",
+        "  name='ext',",
+        String.format("  remote='%s',", PathUtils.pathToFileURI(repo.resolve(".git"))),
+        "  branch='master',",
+        String.format("  build_file_content=\"\"\"%s\"\"\",", buildFileContent),
+        ")");
+
+    // This creates Bazel without MSYS, see implementation for details.
+    BuilderRunner bazel = WorkspaceTestUtils.bazel(context());
+    bazel.build("@ext//:call_write_text");
+    Path outPath = context().resolveBinPath(bazel, "external/ext/out.txt");
+    WorkspaceTestUtils.assertLinesExactly(outPath, HELLO_FROM_EXTERNAL_REPOSITORY);
+  }
+
+  private static String setupGitRepository(BlackBoxTestContext context, Path repo) throws Exception {
+    PathUtils.deleteTree(repo);
+    Files.createDirectories(repo);
+    GitRepositoryHelper gitRepository = new GitRepositoryHelper(context, repo);
+    gitRepository.init();
+
+    RepoWithRuleWritingTextGenerator generator = new RepoWithRuleWritingTextGenerator(repo);
+    generator.withOutputText(HELLO_FROM_EXTERNAL_REPOSITORY)
+        .skipBuildFile()
+        .setupRepository();
+
+    gitRepository.addAll();
+    gitRepository.commit("Initial commit");
+    gitRepository.tag("first");
+    return gitRepository.getHead();
+  }
+}
---
+++ tools/build_defs/repo/git_worker.bzl
@@ -0,0 +1,127 @@
+_GitRepo = provider(
+    doc = "Provider to organize precomputed arguments for calling git.",
+    fields = {
+        "directory": "Working directory path",
+        "shallow": "Defines the depth of a fetch. Either empty, --depth=1, or --shallow-since=<>",
+        "reset_ref": """Reference to use for resetting the git repository.
+Either commit hash, tag or branch.""",
+        "fetch_ref": """Reference for fetching. Can be empty (HEAD), tag or branch.
+Can not be a commit hash, since typically it is forbidden by git servers.""",
+        "remote": "URL of the git repository to fetch from.",
+        "init_submodules": """If True, submodules update command will be called after fetching
+and resetting to the specified reference.""",
+    },
+)
+
+def git_repo(ctx, directory):
+    if ctx.attr.shallow_since:
+        if ctx.attr.tag:
+            fail("shallow_since not allowed if a tag is specified; --depth=1 will be used for tags")
+        if ctx.attr.branch:
+            fail("shallow_since not allowed if a branch is specified; --depth=1 will be used for branches")
+
+    shallow = "--depth=1"
+    if ctx.attr.shallow_since:
+        shallow = "--shallow-since=%s" % ctx.attr.shallow_since
+    if ctx.attr.commit:
+        shallow = ""
+
+    reset_ref = ""
+    fetch_ref = ""
+    if ctx.attr.commit:
+        reset_ref = ctx.attr.commit
+    elif ctx.attr.tag:
+        reset_ref = "tags/" + ctx.attr.tag
+        fetch_ref = "tags/" + ctx.attr.tag + ":tags/" + ctx.attr.tag
+    elif ctx.attr.branch:
+        reset_ref = "origin/" + ctx.attr.branch
+        fetch_ref = ctx.attr.branch
+
+    git_repo = _GitRepo(
+        directory = ctx.path(directory),
+        shallow = shallow,
+        reset_ref = reset_ref,
+        fetch_ref = fetch_ref,
+        remote = ctx.attr.remote,
+        init_submodules = ctx.attr.init_submodules,
+    )
+
+    ctx.report_progress("Cloning %s of %s" % (reset_ref, ctx.attr.remote))
+    if (ctx.attr.verbose):
+        print("git.bzl: Cloning or updating %s repository %s using strip_prefix of [%s]" %
+              (
+                  " (%s)" % shallow if shallow else "",
+                  ctx.name,
+                  ctx.attr.strip_prefix if ctx.attr.strip_prefix else "None",
+              ))
+
+    _update(ctx, git_repo)
+    actual_commit = _get_head_commit(ctx, git_repo)
+    shallow_date = _get_head_date(ctx, git_repo)
+
+    return struct(commit = actual_commit, shallow_since = shallow_date)
+
+def _update(ctx, git_repo):
+    ctx.delete(git_repo.directory)
+
+    init(ctx, git_repo)
+    add_origin(ctx, git_repo, ctx.attr.remote)
+    fetch(ctx, git_repo)
+    reset(ctx, git_repo)
+    clean(ctx, git_repo)
+
+    if git_repo.init_submodules:
+        update_submodules(ctx, git_repo)
+
+def init(ctx, git_repo):
+    cl = ["git", "init", "%s" % git_repo.directory]
+    st = ctx.execute(cl, environment = ctx.os.environ)
+    if st.return_code != 0:
+        _error(ctx.name, cl, st.stderr)
+
+def add_origin(ctx, git_repo, remote):
+    _git(ctx, git_repo, "remote", "add", "origin", remote)
+
+def fetch(ctx, git_repo):
+    _git_maybe_shallow(ctx, git_repo, "fetch", "origin", git_repo.fetch_ref)
+
+def reset(ctx, git_repo):
+    _git_maybe_shallow(ctx, git_repo, "reset", "--hard", git_repo.reset_ref, "--")
+
+def clean(ctx, git_repo):
+    _git(ctx, git_repo, "clean", "-xdf")
+
+def update_submodules(ctx, git_repo):
+    _git(ctx, git_repo, "submodule", "update", "--init", "--checkout", "--force")
+
+def _get_head_commit(ctx, git_repo):
+    return _git(ctx, git_repo, "log", "-n", "1", "--pretty=format:%H")
+
+def _get_head_date(ctx, git_repo):
+    return _git(ctx, git_repo, "log", "-n", "1", "--pretty=format:%cd", "--date=raw")
+
+def _git(ctx, git_repo, command, *args):
+    start = ["git", command]
+    st = _execute(ctx, git_repo, start + list(args))
+    if st.return_code != 0:
+        _error(ctx.name, start + list(args), st.stderr)
+    return st.stdout
+
+def _git_maybe_shallow(ctx, git_repo, command, *args):
+    start = ["git", command]
+    args_list = list(args)
+    st = _execute(ctx, git_repo, start + ["'%s'" % git_repo.shallow] + args_list)
+    if st.return_code != 0:
+        st = _execute(ctx, git_repo, start + args_list)
+        if st.return_code != 0:
+            _error(ctx.name, start + args_list, st.stderr)
+
+def _execute(ctx, git_repo, args):
+    return ctx.execute(
+        args,
+        environment = ctx.os.environ,
+        working_directory = str(git_repo.directory),
+    )
+
+def _error(name, command, stderr):
+    fail("error running '%s' while working with @%s:\n%s" % (" ".join(command).strip(), name, stderr))
