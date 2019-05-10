f20f62274139ce770998efd4403f3fababf9a396
ulfjack <ulfjack@google.com>
2019-05-10 06:46:52 -0700

    Update tests to avoid --noexperimental_ui
    PiperOrigin-RevId: 247602666

--- src/test/shell/bazel/bazel_test_test.sh
+++ src/test/shell/bazel/bazel_test_test.sh
@@ -571,38 +571,36 @@ EOF
   chmod +x true.sh flaky.sh false.sh
 
   # We do not use sandboxing so we can trick to be deterministically flaky
-  # TODO(b/37617303): make test UI-independent
-  bazel --nomaster_bazelrc test --noexperimental_ui --spawn_strategy=standalone //:flaky &> $TEST_log \
+  bazel --nomaster_bazelrc test --experimental_ui_debug_all_events \
+      --spawn_strategy=standalone //:flaky &> $TEST_log \
       || fail "//:flaky should have passed with flaky support"
   [ -f "${FLAKE_FILE}" ] || fail "Flaky test should have created the flake-file!"
 
-  expect_log_once "FAIL: //:flaky (.*/flaky/test_attempts/attempt_1.log)"
-  expect_log_once "PASS: //:flaky"
-  expect_log_once "FLAKY"
+  expect_log_once "FAIL.*: //:flaky (.*/flaky/test_attempts/attempt_1.log)"
+  expect_log_once "PASS.*: //:flaky"
+  expect_log_once "FLAKY: //:flaky"
   cat bazel-testlogs/flaky/test_attempts/attempt_1.log &> $TEST_log
   assert_equals "fail" "$(awk "NR == $(wc -l < $TEST_log)" $TEST_log)"
   assert_equals 1 $(ls bazel-testlogs/flaky/test_attempts/*.log | wc -l)
   cat bazel-testlogs/flaky/test.log &> $TEST_log
   assert_equals "pass" "$(awk "NR == $(wc -l < $TEST_log)" $TEST_log)"
 
-  # TODO(b/37617303): make test UI-independent
-  bazel --nomaster_bazelrc test --noexperimental_ui //:pass &> $TEST_log \
-      || fail "//:pass should have passed"
-  expect_log_once "PASS: //:pass"
-  expect_log_once PASSED
+  bazel --nomaster_bazelrc test --experimental_ui_debug_all_events //:pass \
+      &> $TEST_log || fail "//:pass should have passed"
+  expect_log_once "PASS.*: //:pass"
+  expect_log_once "PASSED"
   [ ! -d bazel-test_logs/pass/test_attempts ] \
     || fail "Got test attempts while expected non for non-flaky tests"
   cat bazel-testlogs/flaky/test.log &> $TEST_log
   assert_equals "pass" "$(tail -1 bazel-testlogs/flaky/test.log)"
 
-  # TODO(b/37617303): make test UI-independent
-  bazel --nomaster_bazelrc test --noexperimental_ui //:fail &> $TEST_log \
-      && fail "//:fail should have failed" \
+  bazel --nomaster_bazelrc test --experimental_ui_debug_all_events //:fail \
+      &> $TEST_log && fail "//:fail should have failed" \
       || true
-  expect_log_n "FAIL: //:fail (.*/fail/test_attempts/attempt_..log)" 2
-  expect_log_once "FAIL: //:fail (.*/fail/test.log)"
-  expect_log_once "FAILED"
-  expect_log_once ".*/fail/test.log$"
+  expect_log_n "FAIL.*: //:fail (.*/fail/test_attempts/attempt_..log)" 2
+  expect_log_once "FAIL.*: //:fail (.*/fail/test.log)"
+  expect_log_once "FAILED: //:fail"
+  expect_log_n ".*/fail/test.log$" 2
   cat bazel-testlogs/fail/test_attempts/attempt_1.log &> $TEST_log
   assert_equals "fail" "$(awk "NR == $(wc -l < $TEST_log)" $TEST_log)"
   assert_equals 2 $(ls bazel-testlogs/fail/test_attempts/*.log | wc -l)
