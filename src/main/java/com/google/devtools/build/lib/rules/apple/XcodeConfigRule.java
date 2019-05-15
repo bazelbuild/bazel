1d8adb0a81c9f2c77e59f7e102f541a193f2c732
dmaclach <dmaclach@google.com>
2019-05-15 12:16:35 -0700

    Finish removing require_defined_version from xcode_version.
    PiperOrigin-RevId: 248380385

--- src/main/java/com/google/devtools/build/lib/rules/apple/XcodeConfigRule.java
+++ src/main/java/com/google/devtools/build/lib/rules/apple/XcodeConfigRule.java
@@ -17,7 +17,6 @@ package com.google.devtools.build.lib.rules.apple;
 import static com.google.devtools.build.lib.packages.Attribute.attr;
 import static com.google.devtools.build.lib.packages.BuildType.LABEL;
 import static com.google.devtools.build.lib.packages.BuildType.LABEL_LIST;
-import static com.google.devtools.build.lib.syntax.Type.BOOLEAN;
 
 import com.google.devtools.build.lib.analysis.BaseRuleClasses;
 import com.google.devtools.build.lib.analysis.RuleDefinition;
@@ -32,7 +31,6 @@ public class XcodeConfigRule implements RuleDefinition {
   public static final String XCODE_CONFIG_ATTR_NAME = ":xcode_config";
   static final String DEFAULT_ATTR_NAME = "default";
   static final String VERSIONS_ATTR_NAME = "versions";
-  static final String REQUIRE_DEFINED_VERSIONS_ATTR_NAME = "require_defined_version";
 
   @Override
   public RuleClass build(RuleClass.Builder builder, RuleDefinitionEnvironment env) {
@@ -59,13 +57,6 @@ public class XcodeConfigRule implements RuleDefinition {
         .add(attr(VERSIONS_ATTR_NAME, LABEL_LIST)
             .allowedRuleClasses("xcode_version")
             .allowedFileTypes()
-            .nonconfigurable("this rule determines configuration"))
-        /* <!-- #BLAZE_RULE(xcode_config).ATTRIBUTE(version) -->
-        Deprecated. This attribute has no effect.
-        <!-- #END_BLAZE_RULE.ATTRIBUTE --> */
-        // TODO(b/64576392): Remove this attribute.
-        .add(attr(REQUIRE_DEFINED_VERSIONS_ATTR_NAME, BOOLEAN)
-            .value(false)
             .nonconfigurable("this rule determines configuration"))
         .build();
   }
