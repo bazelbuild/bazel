bazelbuild/bazel

commit 33f7b7755db0ed5cad1b686b7616183927c72ab1

CppCompileAction: always use continuations
Progress on #6394.

PiperOrigin-RevId: 248317092

src/main/java/com/google/devtools/build/lib/rules/cpp/CppCompileAction.java
@@ -51,7 +51,6 @@ import com.google.devtools.build.lib.actions.ParameterFile.ParameterFileType;
 import com.google.devtools.build.lib.actions.ResourceSet;
 import com.google.devtools.build.lib.actions.SimpleSpawn;
 import com.google.devtools.build.lib.actions.Spawn;
-import com.google.devtools.build.lib.actions.SpawnActionContext;
 import com.google.devtools.build.lib.actions.SpawnContinuation;
 import com.google.devtools.build.lib.actions.SpawnResult;
 import com.google.devtools.build.lib.actions.extra.CppCompileInfo;
@@ -1266,150 +1265,6 @@ public class CppCompileAction extends AbstractAction
             showIncludesFilterForStderr,
             SpawnContinuation.ofBeginExecution(spawn, spawnContext))
         .execute();
-  }
-
-  @Override
-  @ThreadCompatible
-  public ActionResult execute(ActionExecutionContext actionExecutionContext)
-      throws ActionExecutionException, InterruptedException {
-    setModuleFileFlags();
-    if (featureConfiguration.isEnabled(CppRuleClasses.COMPIILER_PARAM_FILE)) {
-      try {
-        paramFileActionInput =
-            new ParamFileActionInput(
-                paramFilePath,
-                compileCommandLine.getCompilerOptions(overwrittenVariables),
-                ParameterFileType.SHELL_QUOTED,
-                StandardCharsets.ISO_8859_1);
-      } catch (CommandLineExpansionException e) {
-        throw new ActionExecutionException(
-            "failed to generate compile command for rule '"
-                + getOwner().getLabel()
-                + ": "
-                + e.getMessage(),
-            this,
-            /* catastrophe= */ false);
-      }
-    }
-
-    if (!shouldScanDotdFiles()) {
-      updateActionInputs(NestedSetBuilder.wrap(Order.STABLE_ORDER, additionalInputs));
-    }
-
-    ActionExecutionContext spawnContext;
-    ShowIncludesFilter showIncludesFilterForStdout;
-    ShowIncludesFilter showIncludesFilterForStderr;
-    if (featureConfiguration.isEnabled(CppRuleClasses.PARSE_SHOWINCLUDES)) {
-      String workspaceName = actionExecutionContext.getExecRoot().getBaseName();
-      showIncludesFilterForStdout =
-          new ShowIncludesFilter(getSourceFile().getFilename(), workspaceName);
-      showIncludesFilterForStderr =
-          new ShowIncludesFilter(getSourceFile().getFilename(), workspaceName);
-      FileOutErr originalOutErr = actionExecutionContext.getFileOutErr();
-      FileOutErr tempOutErr = originalOutErr.childOutErr();
-      spawnContext = actionExecutionContext.withFileOutErr(tempOutErr);
-    } else {
-      spawnContext = actionExecutionContext;
-      showIncludesFilterForStdout = null;
-      showIncludesFilterForStderr = null;
-    }
-
-    // We want the code to be executed in the finally clause, but we need to throw IOException if
-    // it failed, or add the IOException as a suppressed exception if the try block threw an
-    // exception in the first place. Errorprone recommends using AutoCloseable for this.
-    final class Defer implements AutoCloseable {
-      @Override
-      public void close() throws IOException {
-        // If parse_showincludes feature is enabled, instead of parsing dotD file we parse the
-        // output of cl.exe caused by /showIncludes option.
-        if (featureConfiguration.isEnabled(CppRuleClasses.PARSE_SHOWINCLUDES)) {
-          FileOutErr tempOutErr = spawnContext.getFileOutErr();
-          FileOutErr outErr = actionExecutionContext.getFileOutErr();
-          tempOutErr.close();
-          if (tempOutErr.hasRecordedStdout()) {
-            try (InputStream in = tempOutErr.getOutputPath().getInputStream()) {
-              ByteStreams.copy(
-                  in,
-                  showIncludesFilterForStdout.getFilteredOutputStream(outErr.getOutputStream()));
-            }
-          }
-          if (tempOutErr.hasRecordedStderr()) {
-            try (InputStream in = tempOutErr.getErrorPath().getInputStream()) {
-              ByteStreams.copy(
-                  in, showIncludesFilterForStderr.getFilteredOutputStream(outErr.getErrorStream()));
-            }
-          }
-        }
-      }
-    }
-
-    List<SpawnResult> spawnResults;
-    byte[] dotDContents;
-    try (Defer ignored = new Defer()) {
-      Spawn spawn = createSpawn(actionExecutionContext.getClientEnv());
-      SpawnActionContext context = actionExecutionContext.getContext(SpawnActionContext.class);
-      spawnResults = context.exec(spawn, spawnContext);
-      // SpawnActionContext guarantees that the first list entry exists and corresponds to the
-      // executed spawn.
-      dotDContents = getDotDContents(spawnResults.get(0));
-    } catch (ExecException e) {
-      throw e.toActionExecutionException(
-          "C++ compilation of rule '" + getOwner().getLabel() + "'",
-          actionExecutionContext.getVerboseFailures(),
-          this);
-    } catch (IOException e) {
-      throw new ActionExecutionException(e, this, false);
-    } finally {
-      clearAdditionalInputs();
-    }
-    ensureCoverageNotesFilesExist(actionExecutionContext);
-
-    if (!shouldScanDotdFiles()) {
-      return ActionResult.create(spawnResults);
-    }
-
-    // This is the .d file scanning part.
-    CppIncludeExtractionContext scanningContext =
-        actionExecutionContext.getContext(CppIncludeExtractionContext.class);
-    Path execRoot = actionExecutionContext.getExecRoot();
-
-    NestedSet<Artifact> discoveredInputs;
-    if (featureConfiguration.isEnabled(CppRuleClasses.PARSE_SHOWINCLUDES)) {
-      discoveredInputs =
-          discoverInputsFromShowIncludesFilters(
-              execRoot,
-              scanningContext.getArtifactResolver(),
-              showIncludesFilterForStdout,
-              showIncludesFilterForStderr);
-    } else {
-      discoveredInputs =
-          discoverInputsFromDotdFiles(
-              actionExecutionContext,
-              execRoot,
-              scanningContext.getArtifactResolver(),
-              dotDContents);
-    }
-    dotDContents = null; // Garbage collect in-memory .d contents.
-
-    if (discoversInputs()) {
-      // Post-execute "include scanning", which modifies the action inputs to match what the compile
-      // action actually used by incorporating the results of .d file parsing.
-      updateActionInputs(discoveredInputs);
-    } else {
-      Preconditions.checkState(
-          discoveredInputs.isEmpty(),
-          "Discovered inputs without discovering inputs? %s %s",
-          discoveredInputs,
-          this);
-    }
-
-    // hdrs_check: This cannot be switched off for C++ build actions,
-    // because doing so would allow for incorrect builds.
-    // HeadersCheckingMode.NONE should only be used for ObjC build actions.
-    if (needsIncludeValidation) {
-      validateInclusions(actionExecutionContext, discoveredInputs);
-    }
-    return ActionResult.create(spawnResults);
   }
 
   protected byte[] getDotDContents(SpawnResult spawnResult) throws EnvironmentalExecException {

commit ad3d77fd7c80368f592a854e08198f18177a6d0f

Fix error output handling on Windows
This broke in 33f7b7755db0ed5cad1b686b7616183927c72ab1, as the new code
path was not handling this case correctly.

Fixes #8402.

PiperOrigin-RevId: 249201968

src/main/java/com/google/devtools/build/lib/rules/cpp/CppCompileAction.java
@@ -83,7 +83,6 @@ import com.google.devtools.build.lib.vfs.Root;
 import com.google.devtools.build.skyframe.SkyFunction;
 import com.google.devtools.build.skyframe.SkyKey;
 import com.google.devtools.build.skyframe.SkyValue;
-import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -1695,44 +1694,17 @@ public class CppCompileAction extends AbstractAction
         // executed spawn.
         dotDContents = getDotDContents(spawnResults.get(0));
       } catch (ExecException e) {
-        if (featureConfiguration.isEnabled(CppRuleClasses.PARSE_SHOWINCLUDES)) {
-          closeSuppressed(e, spawnExecutionContext.getFileOutErr());
-        }
+        copyTempOutErrToActionOutErr();
         throw e.toActionExecutionException(
             "C++ compilation of rule '" + getOwner().getLabel() + "'",
             actionExecutionContext.getVerboseFailures(),
             CppCompileAction.this);
       } catch (InterruptedException e) {
-        if (featureConfiguration.isEnabled(CppRuleClasses.PARSE_SHOWINCLUDES)) {
-          closeSuppressed(e, spawnExecutionContext.getFileOutErr());
-        }
+        copyTempOutErrToActionOutErr();
         throw e;
       }
 
-      // If parse_showincludes feature is enabled, instead of parsing dotD file we parse the
-      // output of cl.exe caused by /showIncludes option.
-      if (featureConfiguration.isEnabled(CppRuleClasses.PARSE_SHOWINCLUDES)) {
-        try {
-          FileOutErr tempOutErr = spawnExecutionContext.getFileOutErr();
-          FileOutErr outErr = actionExecutionContext.getFileOutErr();
-          tempOutErr.close();
-          if (tempOutErr.hasRecordedStdout()) {
-            try (InputStream in = tempOutErr.getOutputPath().getInputStream()) {
-              ByteStreams.copy(
-                  in,
-                  showIncludesFilterForStdout.getFilteredOutputStream(outErr.getOutputStream()));
-            }
-          }
-          if (tempOutErr.hasRecordedStderr()) {
-            try (InputStream in = tempOutErr.getErrorPath().getInputStream()) {
-              ByteStreams.copy(
-                  in, showIncludesFilterForStderr.getFilteredOutputStream(outErr.getErrorStream()));
-            }
-          }
-        } catch (IOException e) {
-          throw printIOExceptionAndConvertToActionExecutionException(actionExecutionContext, e);
-        }
-      }
+      copyTempOutErrToActionOutErr();
 
       ensureCoverageNotesFilesExist(actionExecutionContext);
 
@@ -1784,11 +1756,30 @@ public class CppCompileAction extends AbstractAction
       return ActionContinuationOrResult.of(ActionResult.create(spawnResults));
     }
 
-    private void closeSuppressed(Exception e, Closeable closeable) {
-      try {
-        closeable.close();
-      } catch (IOException e2) {
-        e.addSuppressed(e2);
+    private void copyTempOutErrToActionOutErr() throws ActionExecutionException {
+      // If parse_showincludes feature is enabled, instead of parsing dotD file we parse the
+      // output of cl.exe caused by /showIncludes option.
+      if (featureConfiguration.isEnabled(CppRuleClasses.PARSE_SHOWINCLUDES)) {
+        try {
+          FileOutErr tempOutErr = spawnExecutionContext.getFileOutErr();
+          FileOutErr outErr = actionExecutionContext.getFileOutErr();
+          tempOutErr.close();
+          if (tempOutErr.hasRecordedStdout()) {
+            try (InputStream in = tempOutErr.getOutputPath().getInputStream()) {
+              ByteStreams.copy(
+                  in,
+                  showIncludesFilterForStdout.getFilteredOutputStream(outErr.getOutputStream()));
+            }
+          }
+          if (tempOutErr.hasRecordedStderr()) {
+            try (InputStream in = tempOutErr.getErrorPath().getInputStream()) {
+              ByteStreams.copy(
+                  in, showIncludesFilterForStderr.getFilteredOutputStream(outErr.getErrorStream()));
+            }
+          }
+        } catch (IOException e) {
+          throw printIOExceptionAndConvertToActionExecutionException(actionExecutionContext, e);
+        }
       }
     }
   }

src/test/py/bazel/bazel_windows_cpp_test.py
@@ -504,5 +504,23 @@ class BazelWindowsCppTest(test_base.TestBase):
     ])
     self.AssertExitCode(exit_code, 0, stderr)
 
+  def testCppErrorShouldBeVisible(self):
+    self.ScratchFile('WORKSPACE')
+    self.ScratchFile('BUILD', [
+        'cc_binary(',
+        '  name = "bad",',
+        '  srcs = ["bad.cc"],',
+        ')',
+    ])
+    self.ScratchFile('bad.cc', [
+        'int main(int argc, char** argv) {',
+        '  this_is_an_error();',
+        '}',
+    ])
+    exit_code, stdout, stderr = self.RunBazel(['build', '//:bad'])
+    self.AssertExitCode(exit_code, 1, stderr)
+    self.assertIn('this_is_an_error', ''.join(stdout))
+
+
 if __name__ == '__main__':
   unittest.main()