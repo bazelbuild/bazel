c43f3d37be038ae4d8be324667fe227c815d7e4a
iirina <elenairina@google.com>
2019-05-22 17:04:34 +0200

    Disable test for current java tools releases.

--- src/test/shell/bazel/bazel_coverage_java_test.sh
+++ src/test/shell/bazel/bazel_coverage_java_test.sh
@@ -356,6 +356,11 @@ EOF
 }
 
 function test_run_jar_in_subprocess_empty_env() {
+  # These features don't work with java tools version javac11-v1.0,
+  # javac10-v3.1, javac9-v1.0 and lower.
+  # TODO(iirina): Remove this statement after new java tools versions are
+  # released.
+  [[ "${JAVA_TOOLS_ZIP}" == "released" ]] && echo "Skipping test" && return
   mkdir -p java/cov
   mkdir -p javatests/cov
   cat >java/cov/BUILD <<EOF
@@ -446,6 +451,11 @@ EOF
 }
 
 function test_runtime_deploy_jar() {
+  # These features don't work with java tools version javac11-v1.0,
+  # javac10-v3.1, javac9-v1.0 and lower.
+  # TODO(iirina): Remove this statement after new java tools versions are
+  # released.
+  [[ "${JAVA_TOOLS_ZIP}" == "released" ]] && echo "Skipping test" && return
   mkdir -p java/cov
   mkdir -p javatests/cov
   cat >java/cov/BUILD <<EOF
@@ -515,6 +525,12 @@ EOF
 }
 
 function test_runtime_and_data_deploy_jars() {
+  # These features don't work with java tools version javac11-v1.0,
+  # javac10-v3.1, javac9-v1.0 and lower.
+  # TODO(iirina): Remove this statement after new java tools versions are
+  # released.
+  [[ "${JAVA_TOOLS_ZIP}" == "released" ]] && echo "Skipping test" && return
+
   mkdir -p java/cov
   mkdir -p javatests/cov
   cat >java/cov/BUILD <<EOF
