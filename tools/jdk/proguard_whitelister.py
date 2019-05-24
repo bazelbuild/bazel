00b1801c85ce999fb284c384a65ba5792d3a925c
Googler <noreply@google.com>
2019-05-23 17:10:44 -0700

    Whitelist -assumevalues for android_library
    PiperOrigin-RevId: 249744844

--- tools/jdk/proguard_whitelister.py
+++ tools/jdk/proguard_whitelister.py
@@ -15,7 +15,7 @@
 """Checks for proguard configuration rules that cannot be combined across libs.
 
 The only valid proguard arguments for a library are -keep, -assumenosideeffects,
-and -dontnote and -dontwarn when they are provided with arguments.
+-assumevalues and -dontnote and -dontwarn when they are provided with arguments.
 Limiting libraries to using these flags prevents drastic, sweeping effects
 (such as obfuscation being disabled) from being inadvertently applied to a
 binary through a library dependency.
@@ -37,8 +37,8 @@ class ProguardConfigValidator(object):
   """Validates a proguard config."""
 
   # Must be a tuple for str.startswith()
-  _VALID_ARGS = ('keep', 'assumenosideeffects', 'adaptresourcefilecontents',
-                 'if')
+  _VALID_ARGS = ('keep', 'assumenosideeffects', 'assumevalues',
+                 'adaptresourcefilecontents', 'if')
 
   def __init__(self, config_path, outconfig_path):
     self._config_path = config_path
