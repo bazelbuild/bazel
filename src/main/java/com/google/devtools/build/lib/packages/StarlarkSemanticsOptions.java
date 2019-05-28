fd5f5db563f8bce36dd408d429192247e073f0fb
laurentlb <laurentlb@google.com>
2019-05-28 10:52:31 -0700

    Flip the flag --incompatible_no_kwargs_in_build_files
    Fixes https://github.com/bazelbuild/bazel/issues/8021
    
    RELNOTES: --incompatible_no_kwargs_in_build_files is enabled by default
    PiperOrigin-RevId: 250317931

--- src/main/java/com/google/devtools/build/lib/packages/StarlarkSemanticsOptions.java
+++ src/main/java/com/google/devtools/build/lib/packages/StarlarkSemanticsOptions.java
@@ -494,7 +494,7 @@ public class StarlarkSemanticsOptions extends OptionsBase implements Serializabl
 
   @Option(
       name = "incompatible_no_kwargs_in_build_files",
-      defaultValue = "false",
+      defaultValue = "true",
       documentationCategory = OptionDocumentationCategory.STARLARK_SEMANTICS,
       effectTags = {OptionEffectTag.BUILD_FILE_SEMANTICS},
       metadataTags = {
