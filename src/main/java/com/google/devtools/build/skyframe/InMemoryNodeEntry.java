4aa1d1a7b98b6169e7fde6cbce9b33f28466bd22
janakr <janakr@google.com>
2019-05-30 09:03:44 -0700

    Logical rollback of unknown commit.
    PiperOrigin-RevId: 250698080

--- src/main/java/com/google/devtools/build/skyframe/InMemoryNodeEntry.java
+++ src/main/java/com/google/devtools/build/skyframe/InMemoryNodeEntry.java
@@ -267,22 +267,6 @@ public class InMemoryNodeEntry implements NodeEntry {
     return ReverseDepsUtility.returnNewElements(this, getOpToStoreBare());
   }
 
-  /**
-   * Highly dangerous method. Used only for testing/debugging. Can only be called on an in-progress
-   * entry that is not dirty and that will not keep edges. Returns all the entry's reverse deps,
-   * which must all be {@link SkyKey}s representing {@link Op#ADD} operations, since that is the
-   * operation that is stored bare. Used for speed, since it avoids making any copies, so should be
-   * much faster than {@link #getInProgressReverseDeps}.
-   */
-  @SuppressWarnings("unchecked")
-  public synchronized Iterable<SkyKey> unsafeGetUnconsolidatedRdeps() {
-    Preconditions.checkState(!isDone(), this);
-    Preconditions.checkState(!isDirty(), this);
-    Preconditions.checkState(keepEdges().equals(KeepEdgesPolicy.NONE), this);
-    Preconditions.checkState(getOpToStoreBare() == OpToStoreBare.ADD, this);
-    return (Iterable<SkyKey>) (List<?>) reverseDepsDataToConsolidate;
-  }
-
   // In this method it is critical that this.lastChangedVersion is set prior to this.value because
   // although this method itself is synchronized, there are unsynchronized consumers of the version
   // and the value.
