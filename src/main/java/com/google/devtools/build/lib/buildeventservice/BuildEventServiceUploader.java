96c5329b6b3b242b4ce695ec837b3bd154d61de6
felly <felly@google.com>
2019-06-03 13:22:36 -0700

    Properly wire up BES half-close. The lack thereof was a simple oversight.
    With this change, --bes_upload_mode=fully_async works properly.
    
    RELNOTES: None
    PiperOrigin-RevId: 251295660

--- src/main/java/com/google/devtools/build/lib/buildeventservice/BuildEventServiceUploader.java
+++ src/main/java/com/google/devtools/build/lib/buildeventservice/BuildEventServiceUploader.java
@@ -154,6 +154,10 @@ public final class BuildEventServiceUploader implements Runnable {
     this.clock = clock;
     this.namer = namer;
     this.eventBus = eventBus;
+    // Ensure the half-close future is closed once the upload is complete. This is usually a no-op,
+    // but makes sure we half-close in case of error / interrupt.
+    closeFuture.addListener(
+        () -> halfCloseFuture.setFuture(closeFuture), MoreExecutors.directExecutor());
   }
 
   BuildEventArtifactUploader getLocalFileUploader() {
@@ -253,6 +257,10 @@ public final class BuildEventServiceUploader implements Runnable {
         uploadThread.interrupt();
       }
     }
+  }
+
+  ListenableFuture<Void> getHalfCloseFuture() {
+    return halfCloseFuture;
   }
 
   private void logAndExitAbruptly(String message, ExitCode exitCode, Throwable cause) {
@@ -422,6 +430,7 @@ public final class BuildEventServiceUploader implements Runnable {
               streamContext.sendOverStream(request);
               streamContext.halfCloseStream();
               halfCloseFuture.set(null);
+              logger.info("BES uploader is half-closed");
             }
             break;
 
