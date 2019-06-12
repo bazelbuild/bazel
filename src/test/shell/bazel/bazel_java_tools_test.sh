bda75e33527f07bfc158268bd6f46a955fb7281d
iirina <elenairina@google.com>
2019-06-12 17:45:34 +0200

    Temp disable test.

--- src/test/shell/bazel/bazel_java_tools_test.sh
+++ src/test/shell/bazel/bazel_java_tools_test.sh
@@ -210,6 +210,10 @@ EOF
 }
 
 function test_java_tools_ijar_builds() {
+  if "$is_windows"; then
+        # TODO(#8614) ijar doesn't build from source on Windows.
+        echo "Skipping test." && return
+  fi
   local java_tools_rlocation=$(rlocation io_bazel/src/java_tools_${JAVA_TOOLS_JAVA_VERSION}.zip)
   local java_tools_zip_file_url="file://${java_tools_rlocation}"
   if "$is_windows"; then
