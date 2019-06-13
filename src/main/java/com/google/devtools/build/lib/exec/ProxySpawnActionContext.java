48a1159af152dd61fa6b171b5e77cdf2c8dc8ea1
Pedro Alvarez <pedro.alvarez@codethink.co.uk>
2019-06-13 11:59:05 +0100

    Add --genrule_strategy to the 'list-based strategy' error message
    This flag could also produce this error message, as I found out when
    building Bazel.

--- src/main/java/com/google/devtools/build/lib/exec/ProxySpawnActionContext.java
+++ src/main/java/com/google/devtools/build/lib/exec/ProxySpawnActionContext.java
@@ -95,8 +95,8 @@ public final class ProxySpawnActionContext implements SpawnActionContext {
       if (listBasedExecutionStrategySelection) {
         throw new UserExecException(
             String.format(
-                "No usable spawn strategy found for spawn with mnemonic %s.  Your --spawn_strategy "
-                    + "or --strategy flags are probably too strict. "
+                "No usable spawn strategy found for spawn with mnemonic %s.  Your --spawn_strategy, "
+                    + "--genrule_strategy or --strategy flags are probably too strict. "
                     + "Visit https://github.com/bazelbuild/bazel/issues/7480 for migration advises",
                 spawn.getMnemonic()));
       } else {
