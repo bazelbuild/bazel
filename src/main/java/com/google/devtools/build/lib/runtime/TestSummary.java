786326ae8b75c50dfee65a5eafe50ad192252370
janakr <janakr@google.com>
2019-06-17 14:31:49 -0700

    Deflake //src/test/shell/integration:test_test by ensuring that the test we want to be skipped will actually be skipped: ensure the action that generates its source file will never get a cache hit, and increase the sleep time so that it will not be built before the other action fails.
    Also add TestSummary#toString, which was useful to me when debugging this, so that I could see which test passed, and if it was cached.
    
    When I added --show_timestamps, I was able to see that any remaining flakes when I added the random numbers but kept the "sleep 20" were due to the failing genrule not running within 20 seconds.
    
    PiperOrigin-RevId: 253662317

--- src/main/java/com/google/devtools/build/lib/runtime/TestSummary.java
+++ src/main/java/com/google/devtools/build/lib/runtime/TestSummary.java
@@ -14,6 +14,7 @@
 package com.google.devtools.build.lib.runtime;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.MoreObjects;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ComparisonChain;
@@ -477,6 +478,18 @@ public class TestSummary implements Comparable<TestSummary>, BuildEventWithOrder
             that.getTarget().getConfigurationChecksum())
         .compare(this.getTotalTestCases(), that.getTotalTestCases())
         .result();
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("target", this.getTarget())
+        .add("status", status)
+        .add("numCached", numCached)
+        .add("numLocalActionCached", numLocalActionCached)
+        .add("actionRan", actionRan)
+        .add("ranRemotely", ranRemotely)
+        .toString();
   }
 
   public List<Long> getTestTimes() {
