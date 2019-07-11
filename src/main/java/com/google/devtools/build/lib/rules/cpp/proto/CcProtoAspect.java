bazelbuild/bazel

commit ce65de4ee8e006137c8d927fa474384c0d940cb2

Create a virtual proto import directory for proto_library rules with generated sources and ones with proto_source_root= set.
Fixes #7157. Third time is the charm!

This time, I made a flag for the fix so that it can be disabled if needed.

RELNOTES: None.
PiperOrigin-RevId: 257360059

src/main/java/com/google/devtools/build/lib/rules/proto/ProtoCommon.java
@@ -194,29 +194,22 @@ public class ProtoCommon {
   // TODO(lberki): This should really be a PathFragment. Unfortunately, it's on the Starlark API of
   // ProtoInfo so it's not an easy change :(
   @Nullable
-  private static Library getProtoSourceRoot(
+  private static Library createLibraryWithoutVirtualSourceRoot(
       RuleContext ruleContext, ImmutableList<Artifact> directSources) {
-    String protoSourceRoot = computeEffectiveProtoSourceRoot(ruleContext);
-    if (ruleContext.hasErrors()) {
-      return null;
-    }
-
-    // This is the same as getPackageIdentifier().getPathUnderExecRoot() due to the check above for
-    // protoSourceRoot == package name, but it's a bit more future-proof.
-    String result =
+    String protoSourceRoot =
         ruleContext
             .getLabel()
             .getPackageIdentifier()
             .getRepository()
             .getPathUnderExecRoot()
-            .getRelative(protoSourceRoot)
             .getPathString();
 
     ImmutableList.Builder<Pair<Artifact, String>> builder = ImmutableList.builder();
     for (Artifact protoSource : directSources) {
       builder.add(new Pair<Artifact, String>(protoSource, null));
     }
-    return new Library(directSources, result.isEmpty() ? "." : result, builder.build());
+    return new Library(
+        directSources, protoSourceRoot.isEmpty() ? "." : protoSourceRoot, builder.build());
   }
 
   private static PathFragment getPathFragmentAttribute(
@@ -243,18 +236,36 @@ public class ProtoCommon {
    * Returns the {@link Library} representing this <code>proto_library</code> rule if import prefix
    * munging is done. Otherwise, returns null.
    */
-  private static Library createVirtualImportDirectoryMaybe(
-      RuleContext ruleContext, ImmutableList<Artifact> protoSources) {
+  private static Library createLibraryWithVirtualSourceRootMaybe(
+      RuleContext ruleContext,
+      ImmutableList<Artifact> protoSources,
+      boolean generatedProtosInVirtualImports) {
     PathFragment importPrefixAttribute = getPathFragmentAttribute(ruleContext, "import_prefix");
     PathFragment stripImportPrefixAttribute =
         getPathFragmentAttribute(ruleContext, "strip_import_prefix");
+    PathFragment protoSourceRootAttribute =
+        getPathFragmentAttribute(ruleContext, "proto_source_root");
+    boolean hasGeneratedSources = false;
 
-    if (importPrefixAttribute == null && stripImportPrefixAttribute == null) {
+    if (generatedProtosInVirtualImports) {
+      for (Artifact protoSource : protoSources) {
+        if (!protoSource.isSourceArtifact()) {
+          hasGeneratedSources = true;
+          break;
+        }
+      }
+    }
+
+    if (importPrefixAttribute == null
+        && stripImportPrefixAttribute == null
+        && protoSourceRootAttribute == null
+        && !hasGeneratedSources) {
       // Simple case, no magic required.
       return null;
     }
 
-    if (!ruleContext.attributes().get("proto_source_root", STRING).isEmpty()) {
+    if (protoSourceRootAttribute != null
+        && (importPrefixAttribute != null || stripImportPrefixAttribute != null)) {
       ruleContext.ruleError(
           "the 'proto_source_root' attribute is incompatible with "
               + "'strip_import_prefix' and 'import_prefix");
@@ -262,30 +273,62 @@ public class ProtoCommon {
     }
 
     PathFragment stripImportPrefix;
-    if (stripImportPrefixAttribute == null) {
-      stripImportPrefix = PathFragment.create(ruleContext.getLabel().getWorkspaceRoot());
-    } else if (stripImportPrefixAttribute.isAbsolute()) {
-      stripImportPrefix =
-          ruleContext
-              .getLabel()
-              .getPackageIdentifier()
-              .getRepository()
-              .getSourceRoot()
-              .getRelative(stripImportPrefixAttribute.toRelative());
-    } else {
-      stripImportPrefix = ruleContext.getPackageDirectory().getRelative(stripImportPrefixAttribute);
-    }
-
     PathFragment importPrefix;
-    if (importPrefixAttribute == null) {
+
+    if (stripImportPrefixAttribute != null || importPrefixAttribute != null) {
+      if (stripImportPrefixAttribute == null) {
+        stripImportPrefix = PathFragment.create(ruleContext.getLabel().getWorkspaceRoot());
+      } else if (stripImportPrefixAttribute.isAbsolute()) {
+        stripImportPrefix =
+            ruleContext
+                .getLabel()
+                .getPackageIdentifier()
+                .getRepository()
+                .getSourceRoot()
+                .getRelative(stripImportPrefixAttribute.toRelative());
+      } else {
+        stripImportPrefix =
+            ruleContext.getPackageDirectory().getRelative(stripImportPrefixAttribute);
+      }
+
+      if (importPrefixAttribute != null) {
+        importPrefix = importPrefixAttribute;
+      } else {
+        importPrefix = PathFragment.EMPTY_FRAGMENT;
+      }
+
+      if (importPrefix.isAbsolute()) {
+        ruleContext.attributeError("import_prefix", "should be a relative path");
+        return null;
+      }
+    } else if (protoSourceRootAttribute != null) {
+      if (!ruleContext.getFragment(ProtoConfiguration.class).enableProtoSourceroot()) {
+        ruleContext.attributeError("proto_source_root", "this attribute is not supported anymore");
+        return null;
+      }
+
+      if (!ruleContext.getLabel().getPackageFragment().equals(protoSourceRootAttribute)) {
+        ruleContext.attributeError(
+            "proto_source_root",
+            "proto_source_root must be the same as the package name ("
+                + ruleContext.getLabel().getPackageName()
+                + ")."
+                + " not '"
+                + protoSourceRootAttribute
+                + "'.");
+
+        return null;
+      }
+
+      stripImportPrefix = ruleContext.getPackageDirectory();
       importPrefix = PathFragment.EMPTY_FRAGMENT;
     } else {
-      importPrefix = importPrefixAttribute;
-    }
+      // Has generated sources, but neither strip_import_prefix nor import_prefix nor
+      // proto_source_root.
+      stripImportPrefix =
+          ruleContext.getLabel().getPackageIdentifier().getRepository().getPathUnderExecRoot();
 
-    if (importPrefix.isAbsolute()) {
-      ruleContext.attributeError("import_prefix", "should be a relative path");
-      return null;
+      importPrefix = PathFragment.EMPTY_FRAGMENT;
     }
 
     ImmutableList.Builder<Artifact> symlinks = ImmutableList.builder();
@@ -308,7 +351,9 @@ public class ProtoCommon {
       symlinks.add(importsPair.second);
 
       if (!ruleContext.getFragment(ProtoConfiguration.class).doNotUseBuggyImportPath()
-          && stripImportPrefixAttribute == null) {
+          && stripImportPrefixAttribute == null
+          && protoSourceRootAttribute == null
+          && !hasGeneratedSources) {
         Pair<PathFragment, Artifact> oldImportsPair =
             computeImports(
                 ruleContext,
@@ -395,29 +440,6 @@ public class ProtoCommon {
     return getProtoSourceRootsOfAttribute(ruleContext, currentProtoSourceRoot, "exports");
   }
 
-  private static String computeEffectiveProtoSourceRoot(RuleContext ruleContext) {
-    if (!ruleContext.attributes().isAttributeValueExplicitlySpecified("proto_source_root")) {
-      return "";
-    }
-
-    if (!ruleContext.getFragment(ProtoConfiguration.class).enableProtoSourceroot()) {
-      ruleContext.attributeError("proto_source_root", "this attribute is not supported anymore");
-      return "";
-    }
-
-    String protoSourceRoot = ruleContext.attributes().get("proto_source_root", STRING);
-    if (!ruleContext.getLabel().getPackageName().equals(protoSourceRoot)) {
-      ruleContext.attributeError(
-          "proto_source_root",
-          "proto_source_root must be the same as the package name ("
-              + ruleContext.getLabel().getPackageName() + ")."
-              + " not '" + protoSourceRoot + "'."
-      );
-    }
-
-    return protoSourceRoot;
-  }
-
   /**
    * Check that .proto files in sources are from the same package. This is done to avoid clashes
    * with the generated sources.
@@ -442,21 +464,20 @@ public class ProtoCommon {
    * Creates the {@link ProtoInfo} for the {@code proto_library} rule associated with {@code
    * ruleContext}.
    */
-  public static ProtoInfo createProtoInfo(RuleContext ruleContext) {
+  public static ProtoInfo createProtoInfo(
+      RuleContext ruleContext, boolean generatedProtosInVirtualImports) {
     checkSourceFilesAreInSamePackage(ruleContext);
     ImmutableList<Artifact> directProtoSources =
         ruleContext.getPrerequisiteArtifacts("srcs", Mode.TARGET).list();
-    Library library = createVirtualImportDirectoryMaybe(ruleContext, directProtoSources);
+    Library library =
+        createLibraryWithVirtualSourceRootMaybe(
+            ruleContext, directProtoSources, generatedProtosInVirtualImports);
     if (ruleContext.hasErrors()) {
       return null;
     }
 
     if (library == null) {
-      library = getProtoSourceRoot(ruleContext, directProtoSources);
-    }
-
-    if (ruleContext.hasErrors()) {
-      return null;
+      library = createLibraryWithoutVirtualSourceRoot(ruleContext, directProtoSources);
     }
 
     NestedSet<Artifact> transitiveProtoSources =

src/main/java/com/google/devtools/build/lib/rules/proto/BazelProtoLibrary.java
@@ -31,7 +31,10 @@ public class BazelProtoLibrary implements RuleConfiguredTargetFactory {
 
   @Override
   public ConfiguredTarget create(RuleContext ruleContext) throws ActionConflictException {
-    ProtoInfo protoInfo = ProtoCommon.createProtoInfo(ruleContext);
+    ProtoInfo protoInfo =
+        ProtoCommon.createProtoInfo(
+            ruleContext,
+            ruleContext.getFragment(ProtoConfiguration.class).generatedProtosInVirtualImports());
     if (ruleContext.hasErrors()) {
       return null;
     }

src/main/java/com/google/devtools/build/lib/rules/proto/ProtoConfiguration.java
@@ -57,6 +57,14 @@ public class ProtoConfiguration extends Fragment implements ProtoConfigurationAp
     public boolean doNotUseBuggyImportPath;
 
     @Option(
+        name = "experimental_generated_protos_in_virtual_imports",
+        defaultValue = "true",
+        documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
+        effectTags = {OptionEffectTag.LOADING_AND_ANALYSIS},
+        help = "If set, generated .proto files are put into a virtual import directory.")
+    public boolean generatedProtosInVirtualImports;
+
+    @Option(
       name = "protocopt",
       allowMultiple = true,
       defaultValue = "",
@@ -306,5 +314,9 @@ public class ProtoConfiguration extends Fragment implements ProtoConfigurationAp
 
   public boolean doNotUseBuggyImportPath() {
     return options.doNotUseBuggyImportPath;
+  }
+
+  public boolean generatedProtosInVirtualImports() {
+    return options.generatedProtosInVirtualImports;
   }
 }

src/test/java/com/google/devtools/build/lib/rules/proto/BazelProtoInfoStarlarkTest.java
@@ -164,7 +164,7 @@ public class BazelProtoInfoStarlarkTest extends BuildViewTestCase /*SkylarkTestC
   public void testProtoSourceRootExportedInStarlark() throws Exception {
 
     scratch.file(
-        "foo/myTsetRule.bzl",
+        "foo/myTestRule.bzl",
         "load('//myinfo:myinfo.bzl', 'MyInfo')",
         "",
         "def _my_test_rule_impl(ctx):",
@@ -180,7 +180,7 @@ public class BazelProtoInfoStarlarkTest extends BuildViewTestCase /*SkylarkTestC
     scratch.file(
         "foo/BUILD",
         "",
-        "load(':myTsetRule.bzl', 'my_test_rule')",
+        "load(':myTestRule.bzl', 'my_test_rule')",
         "my_test_rule(",
         "  name = 'myRule',",
         "  protodep = ':myProto',",
@@ -193,7 +193,8 @@ public class BazelProtoInfoStarlarkTest extends BuildViewTestCase /*SkylarkTestC
 
     ConfiguredTarget ct = getConfiguredTarget("//foo:myRule");
     String protoSourceRoot = (String) getMyInfoFromTarget(ct).getValue("fetched_proto_source_root");
+    String genfiles = getTargetConfiguration().getGenfilesFragment().toString();
 
-    assertThat(protoSourceRoot).isEqualTo("foo");
+    assertThat(protoSourceRoot).isEqualTo(genfiles + "/foo/_virtual_imports/myProto");
   }
 }

src/test/java/com/google/devtools/build/lib/rules/proto/BazelProtoLibraryTest.java
@@ -261,11 +261,13 @@ public class BazelProtoLibraryTest extends BuildViewTestCase {
     );
     ConfiguredTarget protoTarget = getConfiguredTarget("//x/foo:nodeps");
     ProtoInfo sourcesProvider = protoTarget.get(ProtoInfo.PROVIDER);
-    assertThat(sourcesProvider.getTransitiveProtoSourceRoots()).containsExactly("x/foo");
+    String genfiles = getTargetConfiguration().getGenfilesFragment().toString();
 
-    assertThat(getGeneratingSpawnAction(getDescriptorOutput("//x/foo:nodeps"))
-        .getRemainingArguments())
-        .contains("--proto_path=x/foo");
+    assertThat(sourcesProvider.getTransitiveProtoSourceRoots())
+        .containsExactly(genfiles + "/x/foo/_virtual_imports/nodeps");
+    assertThat(
+            getGeneratingSpawnAction(getDescriptorOutput("//x/foo:nodeps")).getRemainingArguments())
+        .contains("--proto_path=" + genfiles + "/x/foo/_virtual_imports/nodeps");
   }
 
   @Test
@@ -308,11 +310,18 @@ public class BazelProtoLibraryTest extends BuildViewTestCase {
     );
     ConfiguredTarget protoTarget = getConfiguredTarget("//x/foo:withdeps");
     ProtoInfo sourcesProvider = protoTarget.get(ProtoInfo.PROVIDER);
-    assertThat(sourcesProvider.getTransitiveProtoSourceRoots()).containsExactly("x/foo");
+    String genfiles = getTargetConfiguration().getGenfilesFragment().toString();
+    assertThat(sourcesProvider.getTransitiveProtoSourceRoots())
+        .containsExactly(
+            genfiles + "/x/foo/_virtual_imports/dep",
+            genfiles + "/x/foo/_virtual_imports/withdeps");
 
-    assertThat(getGeneratingSpawnAction(getDescriptorOutput("//x/foo:withdeps"))
-        .getRemainingArguments())
-        .contains("--proto_path=x/foo");
+    assertThat(
+            getGeneratingSpawnAction(getDescriptorOutput("//x/foo:withdeps"))
+                .getRemainingArguments())
+        .containsAtLeast(
+            "--proto_path=" + genfiles + "/x/foo/_virtual_imports/withdeps",
+            "--proto_path=" + genfiles + "/x/foo/_virtual_imports/dep");
   }
 
   @Test
@@ -340,8 +349,42 @@ public class BazelProtoLibraryTest extends BuildViewTestCase {
     );
     ConfiguredTarget protoTarget = getConfiguredTarget("//x/foo:withdeps");
     ProtoInfo sourcesProvider = protoTarget.get(ProtoInfo.PROVIDER);
+    String genfiles = getTargetConfiguration().getGenfilesFragment().toString();
     assertThat(sourcesProvider.getTransitiveProtoSourceRoots())
-        .containsExactly("x/foo", "x/bar", ".");
+        .containsExactly(
+            genfiles + "/x/foo/_virtual_imports/withdeps",
+            genfiles + "/x/bar/_virtual_imports/dep",
+            ".");
+  }
+
+  @Test
+  public void testExternalRepoWithGeneratedProto() throws Exception {
+    if (!isThisBazel()) {
+      return;
+    }
+
+    FileSystemUtils.appendIsoLatin1(
+        scratch.resolve("WORKSPACE"), "local_repository(name = 'foo', path = '/foo')");
+    invalidatePackages();
+
+    scratch.file("/foo/WORKSPACE");
+    scratch.file(
+        "/foo/x/BUILD",
+        "proto_library(name='x', srcs=['generated.proto'])",
+        "genrule(name='g', srcs=[], outs=['generated.proto'], cmd='')");
+
+    scratch.file("a/BUILD", "proto_library(name='a', srcs=['a.proto'], deps=['@foo//x:x'])");
+
+    String genfiles = getTargetConfiguration().getGenfilesFragment().toString();
+    ConfiguredTarget a = getConfiguredTarget("//a:a");
+    ProtoInfo aInfo = a.get(ProtoInfo.PROVIDER);
+    assertThat(aInfo.getTransitiveProtoSourceRoots())
+        .containsExactly(".", genfiles + "/external/foo/x/_virtual_imports/x");
+
+    ConfiguredTarget x = getConfiguredTarget("@foo//x:x");
+    ProtoInfo xInfo = x.get(ProtoInfo.PROVIDER);
+    assertThat(xInfo.getTransitiveProtoSourceRoots())
+        .containsExactly(genfiles + "/external/foo/x/_virtual_imports/x");
   }
 
   @Test
@@ -380,7 +423,9 @@ public class BazelProtoLibraryTest extends BuildViewTestCase {
     // exported proto source roots should be the source root of the rule and the direct source roots
     // of its exports and nothing else (not the exports of its exports or the deps of its exports
     // or the exports of its deps)
-    assertThat(c.get(ProtoInfo.PROVIDER).getExportedProtoSourceRoots()).containsExactly("a", "c");
+    String genfiles = getTargetConfiguration().getGenfilesFragment().toString();
+    assertThat(c.get(ProtoInfo.PROVIDER).getExportedProtoSourceRoots())
+        .containsExactly(genfiles + "/a/_virtual_imports/a", genfiles + "/c/_virtual_imports/c");
   }
 
   @Test
@@ -396,7 +441,10 @@ public class BazelProtoLibraryTest extends BuildViewTestCase {
     ConfiguredTarget protoTarget = getConfiguredTarget("//x/foo:banana");
     ProtoInfo sourcesProvider = protoTarget.get(ProtoInfo.PROVIDER);
 
-    assertThat(sourcesProvider.getDirectProtoSourceRoot()).isEqualTo("x/foo");
+    String genfiles = getTargetConfiguration().getGenfilesFragment().toString();
+
+    assertThat(sourcesProvider.getDirectProtoSourceRoot())
+        .isEqualTo(genfiles + "/x/foo/_virtual_imports/banana");
   }
 
   @Test

src/test/shell/bazel/bazel_proto_library_test.sh
@@ -121,6 +121,7 @@ $proto_library_name(
 $proto_library_name(
   name = "phonenumber_proto",
   srcs = ["phonenumber/phonenumber.proto"],
+  $proto_source_root
 )
 EOF
 
@@ -381,7 +382,7 @@ EOF
 function test_proto_source_root() {
   write_workspace ""
   write_setup "proto_library" "proto_source_root = 'x/person'" ""
-  bazel build //x/person:person_proto > "$TEST_log" || fail "Expected success"
+  bazel build --verbose_failures //x/person:person_proto > "$TEST_log" || fail "Expected success"
 }
 
 function test_proto_source_root_fails() {
@@ -413,13 +414,6 @@ function test_proto_source_root_glob() {
   write_workspace ""
   write_regression_setup
   bazel build //proto_library/src:all >& "$TEST_log" || fail "Expected success"
-}
-
-function test_proto_source_root_glob() {
-  write_workspace ""
-  write_regression_setup
-  bazel build //proto_library/src:all --strict_proto_deps=off >& "$TEST_log" \
-      || fail "Expected success"
 }
 
 function test_proto_source_root_multiple_workspaces() {

commit 46c95dcbe2889e515a2b44b9dec00d6c13f7e9fa

Make proto blacklisting work when virtual import directories are present.
This fixes the fallout from https://github.com/bazelbuild/bazel/commit/ce65de4ee8e006137c8d927fa474384c0d940cb2 .

Afterparty for #7157.

RELNOTES: None.
PiperOrigin-RevId: 257573726

src/main/java/com/google/devtools/build/lib/rules/cpp/proto/CcProtoAspect.java
@@ -269,7 +269,7 @@ public abstract class CcProtoAspect extends NativeAspectClass implements Configu
     private boolean areSrcsBlacklisted() {
       return !new ProtoSourceFileBlacklist(
               ruleContext, getProtoToolchainProvider().blacklistedProtos())
-          .checkSrcs(protoInfo.getDirectProtoSources(), "cc_proto_library");
+          .checkSrcs(protoInfo.getOriginalDirectProtoSources(), "cc_proto_library");
     }
 
     private FeatureConfiguration getFeatureConfiguration() {

src/main/java/com/google/devtools/build/lib/rules/proto/ProtoCommon.java
@@ -511,6 +511,7 @@ public class ProtoCommon {
     ProtoInfo protoInfo =
         new ProtoInfo(
             library.getSources(),
+            directProtoSources,
             library.getSourceRoot(),
             transitiveProtoSources,
             transitiveProtoSourceRoots,

src/main/java/com/google/devtools/build/lib/rules/proto/ProtoInfo.java
@@ -52,6 +52,7 @@ public final class ProtoInfo extends NativeInfo implements ProtoInfoApi<Artifact
   public static final String LEGACY_SKYLARK_NAME = "proto";
 
   private final ImmutableList<Artifact> directProtoSources;
+  private final ImmutableList<Artifact> originalDirectProtoSources;
   private final String directProtoSourceRoot;
   private final NestedSet<Artifact> transitiveProtoSources;
   private final NestedSet<String> transitiveProtoSourceRoots;
@@ -68,6 +69,7 @@ public final class ProtoInfo extends NativeInfo implements ProtoInfoApi<Artifact
   @AutoCodec.Instantiator
   public ProtoInfo(
       ImmutableList<Artifact> directProtoSources,
+      ImmutableList<Artifact> originalDirectProtoSources,
       String directProtoSourceRoot,
       NestedSet<Artifact> transitiveProtoSources,
       NestedSet<String> transitiveProtoSourceRoots,
@@ -82,6 +84,7 @@ public final class ProtoInfo extends NativeInfo implements ProtoInfoApi<Artifact
       Location location) {
     super(PROVIDER, location);
     this.directProtoSources = directProtoSources;
+    this.originalDirectProtoSources = originalDirectProtoSources;
     this.directProtoSourceRoot = directProtoSourceRoot;
     this.transitiveProtoSources = transitiveProtoSources;
     this.transitiveProtoSourceRoots = transitiveProtoSourceRoots;
@@ -96,10 +99,19 @@ public final class ProtoInfo extends NativeInfo implements ProtoInfoApi<Artifact
     this.transitiveDescriptorSets = transitiveDescriptorSets;
   }
 
-  /** The proto sources of the {@code proto_library} declaring this provider. */
+  /**
+   * The proto source files that are used in compiling this {@code proto_library}.
+   *
+   * <p>Different from {@link #getOriginalDirectProtoSources()} when a virtual import root is used.
+   */
   @Override
   public ImmutableList<Artifact> getDirectProtoSources() {
     return directProtoSources;
+  }
+
+  /** The proto sources of the {@code proto_library} declaring this provider. */
+  public ImmutableList<Artifact> getOriginalDirectProtoSources() {
+    return originalDirectProtoSources;
   }
 
   /** The source root of the current library. */

src/test/java/com/google/devtools/build/lib/rules/proto/ProtoCompileActionBuilderTest.java
@@ -65,6 +65,7 @@ public class ProtoCompileActionBuilderTest {
       NestedSet<Pair<Artifact, String>> exportedProtos) {
     return new ProtoInfo(
         directProtos,
+        directProtos,
         "",
         transitiveProtos,
         transitiveProtoSourceRoots,