258be48ed87b457f79e6a034934b406f3446cab8
buchgr <buchgr@google.com>
2019-07-24 06:55:32 -0700

    fix bazelci postsubmit
    - merge bazel_docker_sandboxing_light_test into bazel_docker_sandboxing_test
    - delete broken docker sandbox test case.
    
    RELNOTES: None
    PiperOrigin-RevId: 259732237

--- src/test/shell/bazel/bazel_docker_sandboxing_test.sh
+++ src/test/shell/bazel/bazel_docker_sandboxing_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2016 The Bazel Authors. All rights reserved.
+# Copyright 2019 The Bazel Authors. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,43 +14,60 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -u
-DISTFILE=$(rlocation io_bazel/${1#./})
-shift 1
-
 # Load the test setup defined in the parent directory
 source $(rlocation io_bazel/src/test/shell/integration_test_setup.sh) \
   || { echo "integration_test_setup.sh not found!" >&2; exit 1; }
 
-# Test that Bazel can be build itself with Docker sandboxing.
-#
-# This test has to be run with "bazel test --action_env=PATH --action_env=HOME", because the inner
-# Bazel needs to be able to pass $HOME and $PATH to "docker" so that it can download the container
-# from gcr.io.
-function test_build_bazel_using_docker()  {
-    unzip -qo "${DISTFILE}" &> $TEST_log || fail "Could not unzip Bazel's distfile"
+# Platform container is picked up if it specified.
+# --remote_default_platform_properties is ignored in this case
+function test_platform_container() {
+  mkdir -p t
+  cat >t/BUILD <<'EOF'
+platform(
+  name = "bad_docker",
+  remote_execution_properties = """
+    properties:{
+      name: "container-image"
+      value: "docker://bad_platform_container"
+    }"""
+)
 
-    # The first set of flags comes from the instructions on the bazel-toolchains
-    # website: https://releases.bazel.build/bazel-toolchains.html
-    #
-    # The second set of flags enables the Docker sandbox in Bazel.
-    bazel build \
-      --host_javabase=@bazel_toolchains//configs/ubuntu16_04_clang/1.0:jdk8 \
-      --javabase=@bazel_toolchains//configs/ubuntu16_04_clang/1.0:jdk8 \
-      --host_java_toolchain=@bazel_tools//tools/jdk:toolchain_hostjdk8 \
-      --java_toolchain=@bazel_tools//tools/jdk:toolchain_hostjdk8 \
-      --crosstool_top=@bazel_toolchains//configs/ubuntu16_04_clang/1.0/bazel_0.15.0/default:toolchain \
-      --action_env=BAZEL_DO_NOT_DETECT_CPP_TOOLCHAIN=1 \
-      --extra_toolchains=@bazel_toolchains//configs/ubuntu16_04_clang/1.0/bazel_0.15.0/cpp:cc-toolchain-clang-x86_64-default \
-      --extra_execution_platforms=@bazel_toolchains//configs/ubuntu16_04_clang/1.0:rbe_ubuntu1604 \
-      --host_platform=@bazel_toolchains//configs/ubuntu16_04_clang/1.0:rbe_ubuntu1604 \
-      --platforms=@bazel_toolchains//configs/ubuntu16_04_clang/1.0:rbe_ubuntu1604 \
-      \
-      --experimental_enable_docker_sandbox --experimental_docker_verbose \
-      --spawn_strategy=docker --strategy=Javac=docker --genrule_strategy=sandboxed \
-      --define=EXECUTOR=remote \
-      //src:bazel \
-      &> $TEST_log || fail "bazel build with Docker failed"
+genrule(
+  name = "echo",
+  outs = ["out.txt"],
+  cmd = "echo hello > $@",
+)
+EOF
+
+  bazel build  \
+    --extra_execution_platforms=//t:bad_docker \
+    --experimental_enable_docker_sandbox --experimental_docker_verbose \
+    --spawn_strategy=docker \
+    --remote_default_platform_properties="properties:{name:\"container-image\" value:\"docker://bad_flag_container\"}"\
+    //t:echo \
+    &> $TEST_log && fail "Expected build to fail, it succeded"
+  grep "bad_platform_container" $TEST_log || fail "Wrong container was chosen"
+}
+
+# If platform container is not specified, --remote_default_platform_properties
+# are picked up
+function test_flag_container() {
+  mkdir -p t
+  cat >t/BUILD <<'EOF'
+genrule(
+  name = "echo",
+  outs = ["out.txt"],
+  cmd = "echo hello > $@",
+)
+EOF
+
+  bazel build  \
+    --experimental_enable_docker_sandbox --experimental_docker_verbose \
+    --spawn_strategy=docker \
+    --remote_default_platform_properties="properties:{name:\"container-image\" value:\"docker://bad_flag_container\"}"\
+    //t:echo \
+    &> $TEST_log && fail "Expected build to fail, it succeded"
+  grep "bad_flag_container" $TEST_log || fail "Wrong container was chosen"
 }
 
 run_suite "bazel docker sandboxing test"
