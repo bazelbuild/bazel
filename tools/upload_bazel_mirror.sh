472bc8ac28b38515aba1a4c7396879f179e1f33e
Yannic Bonenberger <contact@yannic-bonenberger.com>
2019-07-25 09:28:15 +0200

    Sanitize inputs

--- tools/upload_bazel_mirror.sh
+++ tools/upload_bazel_mirror.sh
@@ -18,31 +18,36 @@
 # `https://github.com/bazelbuild/<rules_xyz>/archive/<version>.tar.gz`
 # and uploads it to the cannonical location on `https://mirror.bazel.build`.
 
-set -eo pipefail
+set -euo pipefail
 
-function print_usage {
-  echo "Usage: bazel run //tools:upload_bazel_rules -- <rules_xyz> <version>"
+function validate_input {
+  local n="$1"
+  local input="$2"
+  # Valid inputs contain only alphanumeric letters or [_-.]
+  # and must be between 3 (e.g. numbered releases like 0.1 or 1.0) and 40
+  # (e.g. git commit hashes) characters long.
+  if [[ ! "${input}" =~ ^[a-zA-Z0-9_\\-\\.]{3,40}$ ]]; then
+    echo "Argument ${n} with value '${input}' contains invalid characters," \
+         "or is not between 3 and 40 characters long"
+    exit 1
+  fi
 }
 
-REPO="$1"
-if [ -z "$REPO" ]; then
-  print_usage
+if [ "$#" -ne 2 ]; then
+  echo "Usage: bazel run //tools:upload_bazel_mirror -- <rules_xyz> <version>"
   exit 1
 fi
+
+REPO="$1"
+validate_input 1 "${REPO}"
 
 # TODO(yannic): Add option to get latest commit or release from GitHub API.
 VERSION="$2"
-if [ -z "$VERSION" ]; then
-  print_usage
-  exit 1
-fi
-
-# From now on, fail if there are any unbound variables.
-set -u
+validate_input 2 "${VERSION}"
 
 # Create a temp directory to hold the versioned tarball,
 # and clean it up when the script exits.
-tmpdir=$(mktemp -d)
+tmpdir="$(mktemp -d)"
 function cleanup {
   rm -rf "$tmpdir"
 }
