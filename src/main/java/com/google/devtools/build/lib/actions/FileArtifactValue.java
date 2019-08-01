90a55a994b5f5ae5ad928052ed77351055c28945
lberki <lberki@google.com>
2019-08-01 11:24:54 -0700

    Fix even more fallout of https://github.com/bazelbuild/bazel/commit/f7eee1edc1f8dfc6d436894db2eb672bb11bc962 .
    RELNOTES: None.
    PiperOrigin-RevId: 261161639

--- src/main/java/com/google/devtools/build/lib/actions/FileArtifactValue.java
+++ src/main/java/com/google/devtools/build/lib/actions/FileArtifactValue.java
@@ -478,7 +478,9 @@ public abstract class FileArtifactValue implements SkyValue, HasDigest {
     @Override
     public String toString() {
       return MoreObjects.toStringHelper(this)
-          .add("digest", BaseEncoding.base16().lowerCase().encode(digest))
+          .add(
+              "digest",
+              digest == null ? "(null)" : BaseEncoding.base16().lowerCase().encode(digest))
           .add("size", size)
           .add("proxy", proxy)
           .toString();
@@ -486,6 +488,12 @@ public abstract class FileArtifactValue implements SkyValue, HasDigest {
 
     @Override
     public boolean couldBeModifiedSince(FileArtifactValue o) {
+      if (digest != null && o.getDigest() != null) {
+        // TODO(lberki): This sometimes compares between RemoteFileArtifactValue and
+        // RegularFileArtifactValue. Pretty unintuitive, that.
+        return !Arrays.equals(digest, o.getDigest()) || getSize() != o.getSize();
+      }
+
       if (!(o instanceof RegularFileArtifactValue)) {
         return true;
       }
@@ -495,11 +503,7 @@ public abstract class FileArtifactValue implements SkyValue, HasDigest {
         return true;
       }
 
-      if (digest != null && lastKnown.digest != null) {
-        return !Arrays.equals(digest, lastKnown.digest);
-      } else {
-        return !Objects.equals(proxy, lastKnown.proxy);
-      }
+      return !Objects.equals(proxy, lastKnown.proxy);
     }
 
     @Override
@@ -562,6 +566,17 @@ public abstract class FileArtifactValue implements SkyValue, HasDigest {
     @Override
     public FileContentsProxy getContentsProxy() {
       throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean couldBeModifiedSince(FileArtifactValue o) {
+      if (digest != null && o.getDigest() != null) {
+        // TODO(lberki): This sometimes compares between RemoteFileArtifactValue and
+        // RegularFileArtifactValue. Pretty unintuitive, that.
+        return !Arrays.equals(digest, o.getDigest()) || getSize() != o.getSize();
+      }
+
+      return true;
     }
 
     @Override
