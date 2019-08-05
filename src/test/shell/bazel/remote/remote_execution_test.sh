960217631abdcab0a7ed95e2ab10acd55f636639
lberki <lberki@google.com>
2019-08-05 02:20:51 -0700

    Automated rollback of commit 0f0a0d58725603cf2f1c175963360b525718a195.
    *** Reason for rollback ***
    
    Breaks some Google-internal targets.
    
    *** Original change description ***
    
    Enable sandboxing and remote caching for the "exclusive" tag
    
    There's no good reason why tests tagged with exclusive shouldn't
    be able to use sandboxing and/or remote caching. The current
    limitations have been kept from open sourcing Bazel and have
    never been revisited since.
    
    Remote execution will continue to be disabled because Bazel
    can't control what else is running on a remote machine.
    
    Closes #8983.
    
    PiperOrigin-RevId: 261644804

--- src/test/shell/bazel/remote/remote_execution_test.sh
+++ src/test/shell/bazel/remote/remote_execution_test.sh
@@ -1131,34 +1131,6 @@ EOF
   expect_log "uri:.*bytestream://localhost"
 }
 
-function test_exclusive_tag() {
-  # Test that the exclusive tag works with the remote cache.
-  mkdir -p a
-  cat > a/success.sh <<'EOF'
-#!/bin/sh
-exit 0
-EOF
-  chmod 755 a/success.sh
-  cat > a/BUILD <<'EOF'
-sh_test(
-  name = "success_test",
-  srcs = ["success.sh"],
-  tags = ["exclusive"],
-)
-EOF
-
-  bazel test \
-    --remote_cache=grpc://localhost:${worker_port} \
-    //a:success_test || fail "Failed to test //a:success_test"
-
-  bazel test \
-    --remote_cache=grpc://localhost:${worker_port} \
-    --nocache_test_results \
-    //a:success_test >& $TEST_log || fail "Failed to test //a:success_test"
-
-  expect_log "remote cache hit"
-}
-
 # TODO(alpha): Add a test that fails remote execution when remote worker
 # supports sandbox.
 
