338829f2633e91ae0492ee4169446465e10b5994
Artem Zinnatullin <ceo@artemzin.com>
2019-08-05 06:46:28 -0700

    Fix retrying of SocketTimeoutExceptions in HttpConnector
    As part of investigation of #8974 I found that recent change 982e0b8398ea26ce1f5323b9e928990ae8845473 broke retries of `SocketTimeoutException` in `HttpConnector`, intention of that change was good but lack of tests resulted in broken logic.
    
    IntelliJ highlights the problem ? branch with `instanceof SocketTimeoutException` would have never been executed:
    
    <img width="764" alt="Screen Shot 2019-07-29 at 3 26 11 PM" src="https://user-images.githubusercontent.com/967132/62089179-d7bfa400-b21c-11e9-882a-1c1fe1fcb683.png">
    
    ---
    
    This PR adds missing tests and fixes the logic to still present `SocketTimeoutException` as `IOException` for upstream consumers while handling it properly internally in the `HttpConnector`.
    
    Closes #9008.
    
    PiperOrigin-RevId: 261675244

--- src/test/java/com/google/devtools/build/lib/testutil/ManualSleeper.java
+++ src/test/java/com/google/devtools/build/lib/testutil/ManualSleeper.java
@@ -17,12 +17,17 @@ package com.google.devtools.build.lib.testutil;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import com.google.devtools.build.lib.util.Pair;
 import com.google.devtools.build.lib.util.Sleeper;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /** Fake sleeper for testing. */
 public final class ManualSleeper implements Sleeper {
 
   private final ManualClock clock;
+  private final List<Pair<Long, Runnable>> scheduledRunnables = new ArrayList<>(0);
 
   public ManualSleeper(ManualClock clock) {
     this.clock = checkNotNull(clock);
@@ -31,6 +36,31 @@ public final class ManualSleeper implements Sleeper {
   @Override
   public void sleepMillis(long milliseconds) throws InterruptedException {
     checkArgument(milliseconds >= 0, "sleeper can't time travel");
-    clock.advanceMillis(milliseconds);
+    final long resultedCurrentTimeMillis = clock.advanceMillis(milliseconds);
+
+    Iterator<Pair<Long, Runnable>> iterator = scheduledRunnables.iterator();
+
+    // Run those scheduled Runnables who's time has come.
+    while (iterator.hasNext()) {
+      Pair<Long, Runnable> scheduledRunnable = iterator.next();
+
+      if (resultedCurrentTimeMillis >= scheduledRunnable.first) {
+        iterator.remove();
+        scheduledRunnable.second.run();
+      }
+    }
+  }
+
+  /**
+   * Schedules a given {@link Runnable} to run when this Sleeper's clock has been adjusted with
+   * {@link #sleepMillis(long)} by {@code delayMilliseconds} or greater.
+   *
+   * @param runnable runnable to run, must not throw exceptions.
+   * @param delayMilliseconds delay in milliseconds from current value of {@link ManualClock} used
+   *     by this {@link ManualSleeper}.
+   */
+  public void scheduleRunnable(Runnable runnable, long delayMilliseconds) {
+    checkArgument(delayMilliseconds >= 0, "sleeper can't time travel");
+    scheduledRunnables.add(new Pair<>(clock.currentTimeMillis() + delayMilliseconds, runnable));
   }
 }
