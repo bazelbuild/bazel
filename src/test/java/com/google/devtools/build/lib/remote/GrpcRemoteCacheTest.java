bazelbuild/bazel

commit a6b5b0540a0167f84437127d3dd6cef804286d6f

Streamline uploading of stdout and stderr
This change removes extra upload logic for stdout/stderr. Besides
the streamlined code this also saves two round trips uploading an
action that produced both stdout and stderr.

Closes #9025.

PiperOrigin-RevId: 261288280

src/main/java/com/google/devtools/build/lib/remote/AbstractRemoteActionCache.java
@@ -640,8 +640,10 @@ public abstract class AbstractRemoteActionCache implements AutoCloseable {
     private final Path execRoot;
     private final boolean allowSymlinks;
     private final boolean uploadSymlinks;
-    private final Map<Digest, Path> digestToFile;
-    private final Map<Digest, Chunker> digestToChunkers;
+    private final Map<Digest, Path> digestToFile = new HashMap<>();
+    private final Map<Digest, Chunker> digestToChunkers = new HashMap<>();
+    private Digest stderrDigest;
+    private Digest stdoutDigest;
 
     /**
      * Create an UploadManifest from an ActionResult builder and an exec root. The ActionResult
@@ -658,9 +660,17 @@ public abstract class AbstractRemoteActionCache implements AutoCloseable {
       this.execRoot = execRoot;
       this.uploadSymlinks = uploadSymlinks;
       this.allowSymlinks = allowSymlinks;
+    }
 
-      this.digestToFile = new HashMap<>();
-      this.digestToChunkers = new HashMap<>();
+    public void setStdoutStderr(FileOutErr outErr) throws IOException {
+      if (outErr.getErrorPath().exists()) {
+        stderrDigest = digestUtil.compute(outErr.getErrorPath());
+        addFile(stderrDigest, outErr.getErrorPath());
+      }
+      if (outErr.getOutputPath().exists()) {
+        stdoutDigest = digestUtil.compute(outErr.getOutputPath());
+        addFile(stdoutDigest, outErr.getOutputPath());
+      }
     }
 
     /**
@@ -745,6 +755,16 @@ public abstract class AbstractRemoteActionCache implements AutoCloseable {
      */
     public Map<Digest, Chunker> getDigestToChunkers() {
       return digestToChunkers;
+    }
+
+    @Nullable
+    public Digest getStdoutDigest() {
+      return stdoutDigest;
+    }
+
+    @Nullable
+    public Digest getStderrDigest() {
+      return stderrDigest;
     }
 
     private void addFileSymbolicLink(Path file, PathFragment target) throws IOException {

src/main/java/com/google/devtools/build/lib/remote/GrpcRemoteCache.java
@@ -38,7 +38,6 @@ import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Ascii;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Throwables;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Maps;
@@ -419,6 +418,7 @@ public class GrpcRemoteCache extends AbstractRemoteActionCache {
             options.incompatibleRemoteSymlinks,
             options.allowSymlinkUpload);
     manifest.addFiles(files);
+    manifest.setStdoutStderr(outErr);
     manifest.addAction(actionKey, action, command);
 
     Map<HashCode, Chunker> filesToUpload = Maps.newHashMap();
@@ -449,33 +449,12 @@ public class GrpcRemoteCache extends AbstractRemoteActionCache {
       uploader.uploadBlobs(filesToUpload, /*forceUpload=*/true);
     }
 
-    // TODO(olaola): inline small stdout/stderr here.
-    if (outErr.getErrorPath().exists()) {
-      Digest stderr = uploadFileContents(outErr.getErrorPath());
-      result.setStderrDigest(stderr);
+    if (manifest.getStderrDigest() != null) {
+      result.setStderrDigest(manifest.getStderrDigest());
     }
-    if (outErr.getOutputPath().exists()) {
-      Digest stdout = uploadFileContents(outErr.getOutputPath());
-      result.setStdoutDigest(stdout);
+    if (manifest.getStdoutDigest() != null) {
+      result.setStdoutDigest(manifest.getStdoutDigest());
     }
-  }
-
-  /**
-   * Put the file contents cache if it is not already in it. No-op if the file is already stored in
-   * cache. The given path must be a full absolute path.
-   *
-   * @return The key for fetching the file contents blob from cache.
-   */
-  private Digest uploadFileContents(Path file) throws IOException, InterruptedException {
-    Digest digest = digestUtil.compute(file);
-    ImmutableSet<Digest> missing = getMissingDigests(ImmutableList.of(digest));
-    if (!missing.isEmpty()) {
-      uploader.uploadBlob(
-          HashCode.fromString(digest.getHash()),
-          Chunker.builder().setInput(digest.getSizeBytes(), file).build(),
-          /* forceUpload=*/ true);
-    }
-    return digest;
   }
   
   // Execution Cache API

commit ee31742e33c648a745623b2ac1435663bd215c56

Fix incorrect stdout/stderr in remote action cache. Fixes #9072
Commit a6b5b0540 introduced a bug where the stdout/test.log of
an action was incorrectly added as an output file to the
ActionResult protobuf. Fortunately, we found the bug on Windows
because one cannot delete a file while it's still being open.

PAIR=pcloudy

src/main/java/com/google/devtools/build/lib/remote/AbstractRemoteActionCache.java
@@ -665,11 +665,11 @@ public abstract class AbstractRemoteActionCache implements AutoCloseable {
     public void setStdoutStderr(FileOutErr outErr) throws IOException {
       if (outErr.getErrorPath().exists()) {
         stderrDigest = digestUtil.compute(outErr.getErrorPath());
-        addFile(stderrDigest, outErr.getErrorPath());
+        digestToFile.put(stderrDigest, outErr.getErrorPath());
       }
       if (outErr.getOutputPath().exists()) {
         stdoutDigest = digestUtil.compute(outErr.getOutputPath());
-        addFile(stdoutDigest, outErr.getOutputPath());
+        digestToFile.put(stdoutDigest, outErr.getOutputPath());
       }
     }
 

src/test/java/com/google/devtools/build/lib/remote/GrpcRemoteCacheTest.java
@@ -90,6 +90,7 @@ import io.grpc.stub.StreamObserver;
 import io.grpc.util.MutableHandlerRegistry;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -626,7 +627,7 @@ public class GrpcRemoteCacheTest {
   }
 
   @Test
-  public void testUploadCacheHits() throws Exception {
+  public void testUpload() throws Exception {
     final GrpcRemoteCache client = newClient();
     final Digest fooDigest =
         fakeFileCache.createScratchInput(ActionInputHelper.fromPath("a/foo"), "xyz");
@@ -639,6 +640,15 @@ public class GrpcRemoteCacheTest {
     final Digest cmdDigest = DIGEST_UTIL.compute(command.toByteArray());
     Action action = Action.newBuilder().setCommandDigest(cmdDigest).build();
     final Digest actionDigest = DIGEST_UTIL.compute(action.toByteArray());
+
+    outErr.getOutputStream().write("foo out".getBytes(UTF_8));
+    outErr.getOutputStream().close();
+    outErr.getErrorStream().write("foo err".getBytes(UTF_8));
+    outErr.getOutputStream().close();
+
+    final Digest stdoutDigest = DIGEST_UTIL.compute(outErr.getOutputPath());
+    final Digest stderrDigest = DIGEST_UTIL.compute(outErr.getErrorPath());
+
     serviceRegistry.addService(
         new ContentAddressableStorageImplBase() {
           @Override
@@ -646,7 +656,8 @@ public class GrpcRemoteCacheTest {
               FindMissingBlobsRequest request,
               StreamObserver<FindMissingBlobsResponse> responseObserver) {
             assertThat(request.getBlobDigestsList())
-                .containsExactly(cmdDigest, actionDigest, fooDigest, barDigest);
+                .containsExactly(cmdDigest, actionDigest, fooDigest, barDigest, stdoutDigest,
+                    stderrDigest);
             // Nothing is missing.
             responseObserver.onNext(FindMissingBlobsResponse.getDefaultInstance());
             responseObserver.onCompleted();
@@ -663,6 +674,8 @@ public class GrpcRemoteCacheTest {
         outErr,
         result);
     ActionResult.Builder expectedResult = ActionResult.newBuilder();
+    expectedResult.setStdoutDigest(stdoutDigest);
+    expectedResult.setStderrDigest(stderrDigest);
     expectedResult.addOutputFilesBuilder().setPath("a/foo").setDigest(fooDigest);
     expectedResult
         .addOutputFilesBuilder()