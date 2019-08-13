6653efbcc2611884af145c356cf77a35155c1bfb
twerth <twerth@google.com>
2019-08-13 04:38:40 -0700

    Only expand incompatible flag to build-like or query commands.
    RELNOTES: None
    PiperOrigin-RevId: 263112065

--- src/main/java/com/google/devtools/build/lib/runtime/BlazeCommandDispatcher.java
+++ src/main/java/com/google/devtools/build/lib/runtime/BlazeCommandDispatcher.java
@@ -273,7 +273,8 @@ public class BlazeCommandDispatcher implements CommandDispatcher {
     CommonCommandOptions commonOptions = options.getOptions(CommonCommandOptions.class);
     // We cannot flip an incompatible flag that expands to other flags, so we do it manually here.
     // If an option is specified explicitly, we give that preference.
-    if (commonOptions.enableProfileByDefault
+    if ((commandAnnotation.builds() || "query".equals(commandName))
+        && commonOptions.enableProfileByDefault
         && (!options.containsExplicitOption("experimental_generate_json_trace_profile")
             || commonOptions.enableTracer)) {
       commonOptions.enableTracer = true;
