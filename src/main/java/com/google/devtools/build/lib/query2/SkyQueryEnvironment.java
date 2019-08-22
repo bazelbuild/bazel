7a079c40744d11726c567c41fb61bc79836f1db3
michajlo <michajlo@google.com>
2019-08-22 09:24:28 -0700

    Clean up some unchecked/rawtype warnings
    RELNOTES: None
    PiperOrigin-RevId: 264851514

--- src/main/java/com/google/devtools/build/lib/query2/SkyQueryEnvironment.java
+++ src/main/java/com/google/devtools/build/lib/query2/SkyQueryEnvironment.java
@@ -356,9 +356,10 @@ public class SkyQueryEnvironment extends AbstractBlazeQueryEnvironment<Target>
     TargetPattern.Parser targetPatternParser = new TargetPattern.Parser(parserPrefix);
     String universeScopePattern = Iterables.getOnlyElement(universeScope);
     return QueryExpressionMapper.compose(
-        new RdepsToAllRdepsQueryExpressionMapper(targetPatternParser, universeScopePattern),
-        new FilteredDirectRdepsInUniverseExpressionMapper(
-            targetPatternParser, universeScopePattern));
+        ImmutableList.of(
+            new RdepsToAllRdepsQueryExpressionMapper(targetPatternParser, universeScopePattern),
+            new FilteredDirectRdepsInUniverseExpressionMapper(
+                targetPatternParser, universeScopePattern)));
   }
 
   @Override
