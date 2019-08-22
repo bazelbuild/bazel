d96e1cdbe71d117f76e55c668bb4a0f976bf0bc0
Kiril Videlov <kiril.videlov@tink.se>
2019-08-22 06:47:00 -0700

    ci: Reverts to using += for setting the files var
    The read expression collected only the last test file. Reverts back to
    the previous version of the script that used += instead of read.
    
    Related issue: https://github.com/bazelbuild/bazel/issues/7088
    
    Closes #7363.
    
    PiperOrigin-RevId: 264823741

--- scripts/ci/ci.sh
+++ scripts/ci/ci.sh
@@ -50,8 +50,8 @@ cd "$(git rev-parse --show-toplevel)"
 # Get a list of the current files in package form by querying Bazel.
 files=()
 for file in $(git diff --name-only ${COMMIT_RANGE} ); do
-  IFS=$'\n' read -r -a files <<< "$(bazel query $file)"
-  bazel query $file
+  files+=($(bazel query $file))
+  echo $(bazel query $file)
 done
 
 # Query for the associated buildables
