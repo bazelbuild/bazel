874315365300a793fb947d15047bc90ef8f8e2e9
George Gensure <ggensure@uber.com>
2019-08-23 10:55:51 -0400

    Support ~ in query label expressions
    Change-Id: Ie2565e53737f69c305cf11ae9cb3cda9a3b39152

--- src/main/java/com/google/devtools/build/lib/query2/engine/Lexer.java
+++ src/main/java/com/google/devtools/build/lib/query2/engine/Lexer.java
@@ -189,7 +189,7 @@ public final class Lexer {
         case '0': case '1': case '2': case '3': case '4': case '5':
         case '6': case '7': case '8': case '9':
         case '*': case '/': case '@': case '.': case '-': case '_':
-        case ':': case '$':
+        case ':': case '$': case '~':
           pos++;
           break;
        default:
