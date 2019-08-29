407cfe160fd738daf327fd6c29c274e6f68789d6
Alessandro Patti <apatti@uber.com>
2019-08-29 08:20:21 -0700

    Accept hpp files as objc headers
    hpp is a valid objc++ file extension. It should be accepted.
    
    Closes #9086.
    
    PiperOrigin-RevId: 266145261

--- src/main/java/com/google/devtools/build/lib/rules/objc/ObjcRuleClasses.java
+++ src/main/java/com/google/devtools/build/lib/rules/objc/ObjcRuleClasses.java
@@ -306,7 +306,7 @@ public class ObjcRuleClasses {
   /**
    * Header files, which are not compiled directly, but may be included/imported from source files.
    */
-  static final FileType HEADERS = FileType.of(".h", ".inc");
+  static final FileType HEADERS = FileType.of(".h", ".inc", ".hpp");
 
   /**
    * Files allowed in the srcs attribute. This includes private headers.
