478235dd4ee1d80c6f669ad9b21af69860934b8b
mstaib <mstaib@google.com>
2019-09-06 13:03:44 -0700

    Move TODOs into a bug.
    RELNOTES: None.
    PiperOrigin-RevId: 267656509

--- src/test/java/com/google/devtools/build/lib/rules/android/AndroidBinaryTest.java
+++ src/test/java/com/google/devtools/build/lib/rules/android/AndroidBinaryTest.java
@@ -4706,7 +4706,8 @@ public class AndroidBinaryTest extends AndroidBuildViewTestCase {
     // Hack: Avoid the Android split transition by turning off fat_apk_cpu/android_cpu.
     // This is necessary because the transition would change the configuration directory, causing
     // the manifest paths in the assertion not to match.
-    // TODO(mstaib): Get the library manifests in the same configuration as the binary gets them.
+    // TODO(b/140634666): Get the library manifests in the same configuration as the binary gets
+    // them.
     useConfiguration(
         "--fat_apk_cpu=", "--android_cpu=", "--android_manifest_merger_order=alphabetical");
     scratch.overwriteFile(
@@ -4778,7 +4779,8 @@ public class AndroidBinaryTest extends AndroidBuildViewTestCase {
     // Hack: Avoid the Android split transition by turning off fat_apk_cpu/android_cpu.
     // This is necessary because the transition would change the configuration directory, causing
     // the manifest paths in the assertion not to match.
-    // TODO(mstaib): Get the library manifests in the same configuration as the binary gets them.
+    // TODO(b/140634666): Get the library manifests in the same configuration as the binary gets
+    // them.
     useConfiguration(
         "--fat_apk_cpu=",
         "--android_cpu=",
