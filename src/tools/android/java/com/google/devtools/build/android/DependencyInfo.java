b46dfcf989669c4fcc3502194b2120654d73055c
dchai <dchai@google.com>
2019-09-10 11:03:43 -0700

    Add value type for encapsuating dependency info
    No semantic changes.
    
    The ultimate goal is to get a String from
      com.google.devtools.build.android.SerializedAndroidData
    to
      com.google.devtools.build.android.resources.FieldInitializer
    to be able to annotate initializers with their provenance.
    
    PiperOrigin-RevId: 268263801

---
+++ src/tools/android/java/com/google/devtools/build/android/DependencyInfo.java
@@ -0,0 +1,41 @@
+// Copyright 2019 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.devtools.build.android;
+
+import com.google.auto.value.AutoValue;
+
+/** Encapsulates information about a Bazel dependency with respect to the current target. */
+@AutoValue
+public abstract class DependencyInfo {
+  public abstract String label();
+
+  public abstract DependencyType dependencyType();
+
+  static DependencyInfo create(String label, DependencyType dependencyType) {
+    return new AutoValue_DependencyInfo(label, dependencyType);
+  }
+
+  /** The type of dependency relationship, in terms of its distance from the current target. */
+  public static enum DependencyType {
+    /**
+     * The current target. This corresponds to the "--primaryData" option of
+     * ResourceProcessorBusyBox.
+     */
+    PRIMARY,
+    /** Direct dependency. */
+    DIRECT,
+    /** Transitive dependency. */
+    TRANSITIVE
+  }
+}
