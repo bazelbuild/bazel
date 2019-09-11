3d7b4d02ad692acfda9752821503cf8cb41bd369
Laszlo Csomor <laszlocsomor@google.com>
2019-09-11 14:28:27 +0200

    Move test to other file
    Change-Id: I636118c40f2c1ef6e2792e1d0ceb07aff9e30880

--- src/test/shell/bazel/runfiles_test.sh
+++ src/test/shell/bazel/runfiles_test.sh
@@ -17,10 +17,36 @@
 # Test runfiles creation
 #
 
-# Load the test setup defined in the parent directory
-CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "${CURRENT_DIR}/../integration_test_setup.sh" \
+# --- begin runfiles.bash initialization v2 ---
+# Copy-pasted from the Bazel Bash runfiles library v2.
+set -uo pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$0.runfiles/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
+# --- end runfiles.bash initialization v2 ---
+
+source "$(rlocation "io_bazel/src/test/shell/integration_test_setup.sh")" \
   || { echo "integration_test_setup.sh not found!" >&2; exit 1; }
+
+case "$(uname -s | tr [:upper:] [:lower:])" in
+msys*|mingw*|cygwin*)
+  declare -r is_windows=true
+  ;;
+*)
+  declare -r is_windows=false
+  ;;
+esac
+
+if "$is_windows"; then
+  export MSYS_NO_PATHCONV=1
+  export MSYS2_ARG_CONV_EXCL="*"
+  export EXT=".exe"
+else
+  export EXT=""
+fi
 
 # Make sure runfiles are created under a custom-named subdirectory when
 # workspace() is specified in the WORKSPACE file.
@@ -48,10 +74,10 @@ public class Noise {
 }
 EOF
 
-  bazel build //foo:foo >& $TEST_log || fail "Build failed"
-  [[ -d bazel-bin/foo/foo.runfiles/$name ]] || fail "$name runfiles directory not created"
-  [[ -d bazel-bin/foo/foo.runfiles/$name/foo ]] || fail "No foo subdirectory under $name"
-  [[ -x bazel-bin/foo/foo.runfiles/$name/foo/foo ]] || fail "No foo executable under $name"
+  bazel build --enable_runfiles=yes //foo:foo >& $TEST_log || fail "Build failed"
+  [[ -d "bazel-bin/foo/foo${EXT}.runfiles/$name" ]] || fail "$name runfiles directory not created"
+  [[ -d "bazel-bin/foo/foo${EXT}.runfiles/$name/foo" ]] || fail "No foo subdirectory under $name"
+  [[ -x "bazel-bin/foo/foo${EXT}.runfiles/$name/foo/foo" ]] || fail "No foo executable under $name"
 }
 
 function test_legacy_runfiles_change() {
@@ -77,19 +103,19 @@ EOF
   cat > thing.cc <<EOF
 int main() { return 0; }
 EOF
-  bazel build --legacy_external_runfiles //:thing &> $TEST_log \
+  bazel build --enable_runfiles=yes --legacy_external_runfiles //:thing &> $TEST_log \
     || fail "Build failed"
-  [[ -d bazel-bin/thing.runfiles/foo/external/bar ]] \
+  [[ -d bazel-bin/thing${EXT}.runfiles/foo/external/bar ]] \
     || fail "bar not found"
 
-  bazel build --nolegacy_external_runfiles //:thing &> $TEST_log \
+  bazel build --enable_runfiles=yes --nolegacy_external_runfiles //:thing &> $TEST_log \
     || fail "Build failed"
-  [[ ! -d bazel-bin/thing.runfiles/foo/external/bar ]] \
+  [[ ! -d bazel-bin/thing${EXT}.runfiles/foo/external/bar ]] \
     || fail "Old bar still found"
 
-  bazel build --legacy_external_runfiles //:thing &> $TEST_log \
+  bazel build --enable_runfiles=yes --legacy_external_runfiles //:thing &> $TEST_log \
     || fail "Build failed"
-  [[ -d bazel-bin/thing.runfiles/foo/external/bar ]] \
+  [[ -d bazel-bin/thing${EXT}.runfiles/foo/external/bar ]] \
     || fail "bar not recreated"
 }
 
@@ -125,16 +151,82 @@ EOF
   bazel build --spawn_strategy=local --nobuild_runfile_links //:test \
     || fail "Building //:test failed"
 
-  [[ ! -f bazel-bin/test.runfiles/foo/data/hello ]] || fail "expected no runfile data/hello"
-  [[ ! -f bazel-bin/test.runfiles/foo/data/world ]] || fail "expected no runfile data/world"
-  [[ ! -f bazel-bin/test.runfiles/MANIFEST ]] || fail "expected output manifest to not exist"
+  [[ ! -f "bazel-bin/test${EXT}.runfiles/foo/data/hello" ]] || fail "expected no runfile data/hello"
+  [[ ! -f "bazel-bin/test${EXT}.runfiles/foo/data/world" ]] || fail "expected no runfile data/world"
+  [[ ! -f "bazel-bin/test${EXT}.runfiles/MANIFEST" ]] || fail "expected output manifest to not exist"
 
   bazel test --spawn_strategy=local --nobuild_runfile_links //:test \
     || fail "Testing //:foo failed"
 
-  [[ -f bazel-bin/test.runfiles/foo/data/hello ]] || fail "expected runfile data/hello to exist"
-  [[ -f bazel-bin/test.runfiles/foo/data/world ]] || fail "expected runfile data/world to exist"
-  [[ -f bazel-bin/test.runfiles/MANIFEST ]] || fail "expected output manifest to exist"
+  [[ -f "bazel-bin/test${EXT}.runfiles/foo/data/hello" ]] || fail "expected runfile data/hello to exist"
+  [[ -f "bazel-bin/test${EXT}.runfiles/foo/data/world" ]] || fail "expected runfile data/world to exist"
+  [[ -f "bazel-bin/test${EXT}.runfiles/MANIFEST" ]] || fail "expected output manifest to exist"
+}
+
+function test_space_in_runfile_path() {
+  mkdir -p 'foo/a a'         # one space
+  mkdir -p 'repo/b b/c   c'  # more than one space
+  touch 'foo/x.txt'          # no space
+  touch 'foo/a a/y.txt'      # space in runfile link and target path
+  touch 'repo/b b/z.txt'     # space in target path only
+  touch 'repo/b b/c   c/w.txt' # space in runfile link and target path in ext.repo
+  touch 'repo/b b/WORKSPACE'
+  cat >'repo/b b/BUILD' <<'eof'
+filegroup(
+    name = "files",
+    srcs = glob(["**"]),
+    visibility = ["//visibility:public"],
+)
+eof
+  cat >WORKSPACE <<'eof'
+workspace(name = "foo_ws")
+local_repository(
+    name = "space_ws",
+    path = "repo/b b",
+)
+eof
+  cat >BUILD <<'eof'
+sh_binary(
+    name = "x",
+    srcs = ["x.sh"],
+    data = ["@space_ws//:files"] + glob(["foo/**"]),
+    deps = ["@bazel_tools//tools/bash/runfiles"],
+)
+eof
+  cat >x.sh <<'eof'
+#!/bin/bash
+# --- begin runfiles.bash initialization v2 ---
+# Copy-pasted from the Bazel Bash runfiles library v2.
+set -uo pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$0.runfiles/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
+# --- end runfiles.bash initialization v2 ---
+echo "Hello from x.sh"
+echo "x=($(rlocation "foo_ws/foo/x.txt"))"
+echo "y=($(rlocation "foo_ws/foo/a a/y.txt"))"
+echo "z=($(rlocation "space_ws/z.txt"))"
+echo "w=($(rlocation "space_ws/c   c/w.txt"))"
+eof
+  chmod +x x.sh
+
+  # Look up runfiles using the runfiles manifest
+  bazel run --enable_runfiles=yes --nobuild_runfile_links //:x >&"$TEST_log"
+  expect_log "Hello from x.sh"
+  expect_log "^x=(.*foo/x.txt)"
+  expect_log "^y=(.*foo/a a/y.txt)"
+  expect_log "^z=(.*space_ws/z.txt)"
+  expect_log "^w=(.*space_ws/c   c/w.txt)"
+
+  # See if runfiles links are generated
+  bazel build --enable_runfiles=yes --build_runfile_links //:x >&"$TEST_log"
+  [[ -e "bazel-bin/x${EXT}.runfiles/foo_ws/foo/x.txt" ]] || fail "Cannot find x.txt"
+  [[ -e "bazel-bin/x${EXT}.runfiles/foo_ws/foo/a a/y.txt" ]] || fail "Cannot find y.txt"
+  [[ -e "bazel-bin/x${EXT}.runfiles/space_ws/z.txt" ]] || fail "Cannot find z.txt"
+  [[ -e "bazel-bin/x${EXT}.runfiles/space_ws/c   c/w.txt" ]] || fail "Cannot find w.txt"
 }
 
 run_suite "runfiles tests"
--- src/test/shell/integration/runfiles_test.sh
+++ src/test/shell/integration/runfiles_test.sh
@@ -59,7 +59,7 @@ if "$is_windows"; then
   export MSYS2_ARG_CONV_EXCL="*"
   export EXT=".exe"
   export EXTRA_BUILD_FLAGS="--incompatible_use_python_toolchains=false \
---enable_runfiles=yes --build_python_zip=0"
+--enable_runfiles --build_python_zip=0"
 else
   export EXT=""
   export EXTRA_BUILD_FLAGS="--incompatible_use_python_toolchains=false"
@@ -320,70 +320,5 @@ EOF
     || fail "Old foo still found"
 }
 
-function test_space_in_runfile_path() {
-  mkdir -p 'foo/a a'         # one space
-  mkdir -p 'repo/b b/c   c'  # more than one space
-  touch 'foo/x.txt'          # no space
-  touch 'foo/a a/y.txt'      # space in runfile link and target path
-  touch 'repo/b b/z.txt'     # space in target path only
-  touch 'repo/b b/c   c/w.txt' # space in runfile link and target path in ext.repo
-  touch 'repo/b b/WORKSPACE'
-  cat >'repo/b b/BUILD' <<'eof'
-filegroup(
-    name = "files",
-    srcs = glob(["**"]),
-    visibility = ["//visibility:public"],
-)
-eof
-  cat >WORKSPACE <<'eof'
-workspace(name = "foo_ws")
-local_repository(
-    name = "space_ws",
-    path = "repo/b b",
-)
-eof
-  cat >BUILD <<'eof'
-sh_binary(
-    name = "x",
-    srcs = ["x.sh"],
-    data = ["@space_ws//:files"] + glob(["foo/**"]),
-    deps = ["@bazel_tools//tools/bash/runfiles"],
-)
-eof
-  cat >x.sh <<'eof'
-#!/bin/bash
-# --- begin runfiles.bash initialization v2 ---
-# Copy-pasted from the Bazel Bash runfiles library v2.
-set -uo pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
-source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
-  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
-  source "$0.runfiles/$f" 2>/dev/null || \
-  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
-  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
-  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
-# --- end runfiles.bash initialization v2 ---
-echo "Hello from x.sh"
-echo "x=($(rlocation "foo_ws/foo/x.txt"))"
-echo "y=($(rlocation "foo_ws/foo/a a/y.txt"))"
-echo "z=($(rlocation "space_ws/z.txt"))"
-echo "w=($(rlocation "space_ws/c   c/w.txt"))"
-eof
-  chmod +x x.sh
-
-  # Look up runfiles using the runfiles manifest
-  bazel run --enable_runfiles=yes --nobuild_runfile_links //:x >&"$TEST_log"
-  expect_log "Hello from x.sh"
-  expect_log "^x=(.*foo/x.txt)"
-  expect_log "^y=(.*foo/a a/y.txt)"
-  expect_log "^z=(.*space_ws/z.txt)"
-  expect_log "^w=(.*space_ws/c   c/w.txt)"
-
-  # See if runfiles links are generated
-  bazel build --enable_runfiles=yes --build_runfile_links //:x >&"$TEST_log"
-  [[ -e "bazel-bin/x${EXT}.runfiles/foo_ws/foo/x.txt" ]] || fail "Cannot find x.txt"
-  [[ -e "bazel-bin/x${EXT}.runfiles/foo_ws/foo/a a/y.txt" ]] || fail "Cannot find y.txt"
-  [[ -e "bazel-bin/x${EXT}.runfiles/space_ws/z.txt" ]] || fail "Cannot find z.txt"
-  [[ -e "bazel-bin/x${EXT}.runfiles/space_ws/c   c/w.txt" ]] || fail "Cannot find w.txt"
-}
 
 run_suite "runfiles"
