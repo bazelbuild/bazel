b8639974deab560c43ca6b86e966c62c71118719
bcsf <bcsf@google.com>
2019-09-25 07:22:53 -0700

    Update aapt2 executable.
    PiperOrigin-RevId: 271118864

--- src/tools/android/java/com/google/devtools/build/android/Aapt2OptimizeAction.java
+++ src/tools/android/java/com/google/devtools/build/android/Aapt2OptimizeAction.java
@@ -51,16 +51,12 @@ class Aapt2OptimizeAction {
                   switch (arg) {
                     case "--shorten-resource-paths":
                     case "--enable-resource-path-shortening":
-                      /* TODO(bcsf): Start using this at the time we drop AAPT2 5880457.
                       return "--shorten-resource-paths";
-                      */
-                      return "--enable-resource-path-shortening";
+
                     case "--collapse-resource-names":
                     case "--enable-resource-name-obfuscation":
-                      /* TODO(bcsf): Start using this at the time we drop AAPT2 5880457.
                       return "--collapse-resource-names";
-                      */
-                      return "--enable-resource-obfuscation";
+
                     case "--resource-name-obfuscation-exemption-list":
                       // TODO(bcsf): This case goes away soon.
                       return "--whitelist-path";
