1b5e4ddee16bb29ebb18c33d8c00e6a694f6d748
Keith Smiley <keithbsmiley@gmail.com>
2019-10-07 05:24:26 -0700

    Fix framework_search_paths var name
    In the documentation this is showing up incorrectly because of the
    variable name.
    
    Closes #9395.
    
    PiperOrigin-RevId: 273268749

--- src/main/java/com/google/devtools/build/lib/rules/objc/ObjcProvider.java
+++ src/main/java/com/google/devtools/build/lib/rules/objc/ObjcProvider.java
@@ -237,11 +237,11 @@ public final class ObjcProvider extends Info implements ObjcProviderApi<Artifact
 
   /**
    * Exec paths of {@code .framework} directories corresponding to frameworks to include in search
-   * paths, but not to link.  These cause -F arguments (framework search paths) to be added to
-   * each compile action, but do not cause -framework (link framework) arguments to be added to
-   * link actions.
+   * paths, but not to link. These cause -F arguments (framework search paths) to be added to each
+   * compile action, but do not cause -framework (link framework) arguments to be added to link
+   * actions.
    */
-  public static final Key<PathFragment> FRAMEWORK_SEARCH_PATH_ONLY =
+  public static final Key<PathFragment> FRAMEWORK_SEARCH_PATHS =
       new Key<>(LINK_ORDER, "framework_search_paths", PathFragment.class);
 
   /** The static library files of user-specified static frameworks. */
@@ -361,7 +361,7 @@ public final class ObjcProvider extends Info implements ObjcProviderApi<Artifact
           DYNAMIC_FRAMEWORK_DIR,
           DYNAMIC_FRAMEWORK_FILE,
           EXPORTED_DEBUG_ARTIFACTS,
-          FRAMEWORK_SEARCH_PATH_ONLY,
+          FRAMEWORK_SEARCH_PATHS,
           FORCE_LOAD_LIBRARY,
           HEADER,
           IMPORTED_LIBRARY,
@@ -425,8 +425,7 @@ public final class ObjcProvider extends Info implements ObjcProviderApi<Artifact
 
   @Override
   public SkylarkNestedSet frameworkSearchPathOnly() {
-    return ObjcProviderSkylarkConverters.convertPathFragmentsToSkylark(
-        get(FRAMEWORK_SEARCH_PATH_ONLY));
+    return ObjcProviderSkylarkConverters.convertPathFragmentsToSkylark(get(FRAMEWORK_SEARCH_PATHS));
   }
 
   @Override
@@ -597,7 +596,7 @@ public final class ObjcProvider extends Info implements ObjcProviderApi<Artifact
           DYNAMIC_FRAMEWORK_FILE,
           FLAG,
           MERGE_ZIP,
-          FRAMEWORK_SEARCH_PATH_ONLY,
+          FRAMEWORK_SEARCH_PATHS,
           HEADER,
           INCLUDE,
           INCLUDE_SYSTEM,
