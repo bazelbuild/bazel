bazelbuild/bazel

commit c87cc91007aa89e33e95a153c448f8b9fc91fa2f

Event with output: do not keep output in memory
Instead just keep the reference to the object describing the location
on disk and dump the content to the output stream when needed.

Change-Id: If64b7e4e408ec487d29fe43d90dd429153e1b251
PiperOrigin-RevId: 273495045

src/main/java/com/google/devtools/build/lib/runtime/UiEventHandler.java
@@ -49,6 +49,7 @@ import com.google.devtools.build.lib.skyframe.LoadingPhaseStartedEvent;
 import com.google.devtools.build.lib.util.io.AnsiTerminal;
 import com.google.devtools.build.lib.util.io.AnsiTerminal.Color;
 import com.google.devtools.build.lib.util.io.AnsiTerminalWriter;
+import com.google.devtools.build.lib.util.io.FileOutErr.OutputReference;
 import com.google.devtools.build.lib.util.io.LineCountingAnsiTerminalWriter;
 import com.google.devtools.build.lib.util.io.LineWrappingAnsiTerminalWriter;
 import com.google.devtools.build.lib.util.io.LoggingTerminalWriter;
@@ -89,6 +90,13 @@ public class UiEventHandler implements EventHandler {
   static final long SHORT_REFRESH_MILLIS = 1000L;
   /** Periodic update interval of a time-dependent progress bar if it cannot be updated in place */
   static final long LONG_REFRESH_MILLIS = 20000L;
+
+  /**
+   * Even if the output is not limited, we restrict the message size to something we can still
+   * handle internally. This is the maximal size specified here. Currently, it is the maximal length
+   * a byte[] can hold.
+   */
+  static final int MAXIMAL_MESSAGE_LENGTH = Integer.MAX_VALUE - 8;
 
   private static final DateTimeFormatter TIMESTAMP_FORMAT =
       DateTimeFormatter.ofPattern("(HH:mm:ss) ");
@@ -404,53 +412,15 @@ public class UiEventHandler implements EventHandler {
               stream.write(event.getMessageBytes());
               stream.flush();
             } else {
-              byte[] message = event.getMessageBytes();
               if (remainingCapacity() < 0) {
                 return;
               }
-              double cap = remainingCapacity(message.length);
-              if (cap < CAPACITY_LIMIT_OUT_ERR_EVENTS) {
-                // Have to ensure the message is not too large.
-                long allowedLength =
-                    Math.max(2 * terminalWidth, Math.round(RELATIVE_OUT_ERR_LIMIT * counter.get()));
-                if (cap < CAPACITY_STRONG_LIMIT_OUT_ERR_EVENTS) {
-                  allowedLength = Math.min(allowedLength, 2 * terminalWidth);
-                }
-                if (message.length > allowedLength) {
-                  // Have to truncate the message
-                  message =
-                      Arrays.copyOfRange(
-                          message, message.length - (int) allowedLength, message.length);
-                  // Mark message as truncated
-                  message[0] = '.';
-                  message[1] = '.';
-                  message[2] = '.';
-                }
+              writeToStream(stream, event.getKind(), event.getMessageReference());
+              stream.flush();
+              if (showProgress && cursorControl) {
+                addProgressBar();
               }
-              int eolIndex = Bytes.lastIndexOf(message, (byte) '\n');
-              if (eolIndex >= 0) {
-                clearProgressBar();
-                terminal.flush();
-                stream.write(event.getKind() == EventKind.STDOUT ? stdoutBuffer : stderrBuffer);
-                stream.write(Arrays.copyOf(message, eolIndex + 1));
-                byte[] restMessage = Arrays.copyOfRange(message, eolIndex + 1, message.length);
-                if (event.getKind() == EventKind.STDOUT) {
-                  stdoutBuffer = restMessage;
-                } else {
-                  stderrBuffer = restMessage;
-                }
-                stream.flush();
-                if (showProgress && cursorControl) {
-                  addProgressBar();
-                }
-                terminal.flush();
-              } else {
-                if (event.getKind() == EventKind.STDOUT) {
-                  stdoutBuffer = Bytes.concat(stdoutBuffer, message);
-                } else {
-                  stderrBuffer = Bytes.concat(stderrBuffer, message);
-                }
-              }
+              terminal.flush();
             }
             break;
           case ERROR:
@@ -509,21 +479,63 @@ public class UiEventHandler implements EventHandler {
           case DEPCHECKER:
             break;
         }
-        if (event.getStdErr() != null) {
-          handleLocked(
-              Event.of(
-                  EventKind.STDERR, null, event.getStdErr().getBytes(StandardCharsets.ISO_8859_1)),
-              /* isFollowUp= */ true);
-        }
-        if (event.getStdOut() != null) {
-          handleLocked(
-              Event.of(
-                  EventKind.STDOUT, null, event.getStdOut().getBytes(StandardCharsets.ISO_8859_1)),
-              /* isFollowUp= */ true);
+        if (event.hasStdoutStderr()) {
+          clearProgressBar();
+          terminal.flush();
+          writeToStream(outErr.getErrorStream(), EventKind.STDERR, event.getStdErrReference());
+          outErr.getErrorStream().flush();
+          writeToStream(outErr.getOutputStream(), EventKind.STDOUT, event.getStdOutReference());
+          outErr.getOutputStream().flush();
+          if (showProgress && cursorControl) {
+            addProgressBar();
+          }
+          terminal.flush();
         }
       }
     } catch (IOException e) {
       logger.warning("IO Error writing to output stream: " + e);
+    }
+  }
+
+  private void writeToStream(OutputStream stream, EventKind eventKind, OutputReference reference)
+      throws IOException {
+    byte[] message;
+    double cap = remainingCapacity(reference.getLength());
+    if (cap < CAPACITY_LIMIT_OUT_ERR_EVENTS) {
+      // Have to ensure the message is not too large.
+      long allowedLength =
+          Math.max(2 * terminalWidth, Math.round(RELATIVE_OUT_ERR_LIMIT * counter.get()));
+      if (cap < CAPACITY_STRONG_LIMIT_OUT_ERR_EVENTS) {
+        allowedLength = Math.min(allowedLength, 2 * terminalWidth);
+      }
+      message = reference.getFinalBytes((int) allowedLength);
+      if (reference.getLength() > allowedLength) {
+        // Mark message as truncated
+        message[0] = '.';
+        message[1] = '.';
+        message[2] = '.';
+      }
+    } else {
+      message = reference.getFinalBytes(MAXIMAL_MESSAGE_LENGTH);
+    }
+    int eolIndex = Bytes.lastIndexOf(message, (byte) '\n');
+    if (eolIndex >= 0) {
+      clearProgressBar();
+      terminal.flush();
+      stream.write(eventKind == EventKind.STDOUT ? stdoutBuffer : stderrBuffer);
+      stream.write(Arrays.copyOf(message, eolIndex + 1));
+      byte[] restMessage = Arrays.copyOfRange(message, eolIndex + 1, message.length);
+      if (eventKind == EventKind.STDOUT) {
+        stdoutBuffer = restMessage;
+      } else {
+        stderrBuffer = restMessage;
+      }
+    } else {
+      if (eventKind == EventKind.STDOUT) {
+        stdoutBuffer = Bytes.concat(stdoutBuffer, message);
+      } else {
+        stderrBuffer = Bytes.concat(stderrBuffer, message);
+      }
     }
   }
 

src/main/java/com/google/devtools/build/lib/events/Event.java
@@ -16,6 +16,8 @@ package com.google.devtools.build.lib.events;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 import com.google.common.base.Preconditions;
+import com.google.devtools.build.lib.util.io.FileOutErr;
+import com.google.devtools.build.lib.util.io.FileOutErr.OutputReference;
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Objects;
@@ -31,8 +33,7 @@ public final class Event implements Serializable {
   private final EventKind kind;
   private final Location location;
   private final String message;
-  @Nullable private final String stdout;
-  @Nullable private final String stderr;
+  @Nullable private final FileOutErr outErr;
 
   /**
    * An alternative representation for message.
@@ -53,15 +54,13 @@ public final class Event implements Serializable {
       @Nullable Location location,
       String message,
       @Nullable String tag,
-      @Nullable String stdout,
-      @Nullable String stderr) {
+      @Nullable FileOutErr outErr) {
     this.kind = Preconditions.checkNotNull(kind);
     this.location = location;
     this.message = Preconditions.checkNotNull(message);
     this.messageBytes = null;
     this.tag = tag;
-    this.stdout = stdout;
-    this.stderr = stderr;
+    this.outErr = outErr;
   }
 
   private Event(
@@ -69,15 +68,13 @@ public final class Event implements Serializable {
       @Nullable Location location,
       byte[] messageBytes,
       @Nullable String tag,
-      @Nullable String stdout,
-      @Nullable String stderr) {
+      @Nullable FileOutErr outErr) {
     this.kind = Preconditions.checkNotNull(kind);
     this.location = location;
     this.message = null;
     this.messageBytes = Preconditions.checkNotNull(messageBytes);
     this.tag = tag;
-    this.stdout = stdout;
-    this.stderr = stderr;
+    this.outErr = outErr;
   }
 
   public Event withTag(String tag) {
@@ -85,17 +82,17 @@ public final class Event implements Serializable {
       return this;
     }
     if (this.message != null) {
-      return new Event(this.kind, this.location, this.message, tag, this.stdout, this.stderr);
+      return new Event(this.kind, this.location, this.message, tag, this.outErr);
     } else {
-      return new Event(this.kind, this.location, this.messageBytes, tag, this.stdout, this.stderr);
+      return new Event(this.kind, this.location, this.messageBytes, tag, this.outErr);
     }
   }
 
-  public Event withStdoutStderr(String stdout, String stderr) {
+  public Event withStdoutStderr(FileOutErr outErr) {
     if (this.message != null) {
-      return new Event(this.kind, this.location, this.message, this.tag, stdout, stderr);
+      return new Event(this.kind, this.location, this.message, this.tag, outErr);
     } else {
-      return new Event(this.kind, this.location, this.messageBytes, this.tag, stdout, stderr);
+      return new Event(this.kind, this.location, this.messageBytes, this.tag, outErr);
     }
   }
 
@@ -105,6 +102,13 @@ public final class Event implements Serializable {
 
   public byte[] getMessageBytes() {
     return messageBytes != null ? messageBytes : message.getBytes(UTF_8);
+  }
+
+  /** Provide the message as a reference object. */
+  public OutputReference getMessageReference() {
+    // The message is short and we have it in memory anyway; so just wrap it into
+    // the common interface.
+    return new ArrayOutputReference(getMessageBytes());
   }
 
   public EventKind getKind() {
@@ -119,13 +123,21 @@ public final class Event implements Serializable {
     return tag;
   }
 
+  /** Indicate if any output is associated with this event. */
+  public boolean hasStdoutStderr() {
+    return outErr != null;
+  }
+
   /**
    * Get the stdout bytes associated with this event; typically, the event will report where the
    * output originated from.
    */
   @Nullable
   public String getStdOut() {
-    return stdout;
+    if (outErr == null) {
+      return null;
+    }
+    return outErr.outAsLatin1();
   }
 
   /**
@@ -134,7 +146,20 @@ public final class Event implements Serializable {
    */
   @Nullable
   public String getStdErr() {
-    return stderr;
+    if (outErr == null) {
+      return null;
+    }
+    return outErr.errAsLatin1();
+  }
+
+  /** Get a reference to the associated output. */
+  public OutputReference getStdOutReference() {
+    return outErr.getOutReference();
+  }
+
+  /** Get a reference to the associated output. */
+  public OutputReference getStdErrReference() {
+    return outErr.getErrReference();
   }
 
   /**
@@ -169,7 +194,7 @@ public final class Event implements Serializable {
     // read, so we must take care to only read the field once.
     int h = hashCode;
     if (h == 0) {
-      h = Objects.hash(kind, location, message, tag, Arrays.hashCode(messageBytes), stdout, stderr);
+      h = Objects.hash(kind, location, message, tag, Arrays.hashCode(messageBytes), outErr);
       hashCode = h;
     }
     return h;
@@ -188,8 +213,7 @@ public final class Event implements Serializable {
         && Objects.equals(this.location, that.location)
         && Objects.equals(this.tag, that.tag)
         && Objects.equals(this.message, that.message)
-        && Objects.equals(this.stdout, that.stdout)
-        && Objects.equals(this.stderr, that.stderr)
+        && Objects.equals(this.outErr, that.outErr)
         && Arrays.equals(this.messageBytes, that.messageBytes);
   }
 
@@ -203,7 +227,7 @@ public final class Event implements Serializable {
   }
 
   public static Event of(EventKind kind, @Nullable Location location, String message) {
-    return new Event(kind, location, message, null, null, null);
+    return new Event(kind, location, message, null, null);
   }
 
   /**
@@ -212,12 +236,12 @@ public final class Event implements Serializable {
    * The bytes must be decodable as UTF-8 text.
    */
   public static Event of(EventKind kind, @Nullable Location location, byte[] messageBytes) {
-    return new Event(kind, location, messageBytes, null, null, null);
+    return new Event(kind, location, messageBytes, null, null);
   }
 
   /** Reports an error. */
   public static Event error(@Nullable Location location, String message) {
-    return new Event(EventKind.ERROR, location, message, null, null, null);
+    return new Event(EventKind.ERROR, location, message, null, null);
   }
 
   /** Reports an error. */
@@ -227,7 +251,7 @@ public final class Event implements Serializable {
 
   /** Reports a warning. */
   public static Event warn(@Nullable Location location, String message) {
-    return new Event(EventKind.WARNING, location, message, null, null, null);
+    return new Event(EventKind.WARNING, location, message, null, null);
   }
 
   /** Reports a warning. */
@@ -239,7 +263,7 @@ public final class Event implements Serializable {
    * Reports atemporal statements about the build, i.e. they're true for the duration of execution.
    */
   public static Event info(@Nullable Location location, String message) {
-    return new Event(EventKind.INFO, location, message, null, null, null);
+    return new Event(EventKind.INFO, location, message, null, null);
   }
 
   /**
@@ -251,7 +275,7 @@ public final class Event implements Serializable {
 
   /** Reports a temporal statement about the build. */
   public static Event progress(@Nullable Location location, String message) {
-    return new Event(EventKind.PROGRESS, location, message, null, null, null);
+    return new Event(EventKind.PROGRESS, location, message, null, null);
   }
 
   /** Reports a temporal statement about the build. */
@@ -261,7 +285,7 @@ public final class Event implements Serializable {
 
   /** Reports a debug message. */
   public static Event debug(@Nullable Location location, String message) {
-    return new Event(EventKind.DEBUG, location, message, null, null, null);
+    return new Event(EventKind.DEBUG, location, message, null, null);
   }
 
   /**
@@ -269,5 +293,27 @@ public final class Event implements Serializable {
    */
   public static Event debug(String message) {
     return debug(null, message);
+  }
+
+  private static class ArrayOutputReference implements OutputReference {
+    private final byte[] message;
+
+    ArrayOutputReference(byte[] message) {
+      this.message = message;
+    }
+
+    @Override
+    public long getLength() {
+      return message.length;
+    }
+
+    @Override
+    public byte[] getFinalBytes(int count) {
+      if (count >= message.length) {
+        return message;
+      } else {
+        return Arrays.copyOfRange(message, message.length - count, message.length);
+      }
+    }
   }
 }

src/main/java/com/google/devtools/build/lib/skyframe/SkyframeActionExecutor.java
@@ -1491,8 +1491,7 @@ public final class SkyframeActionExecutor {
       // output into memory; as the output of regular actions (as opposed to test runs) usually is
       // short, so this should not be a problem. If it does turn out to be a problem, we have to
       // pass the outErrbuffer instead.
-      reporter.handle(
-          prefixEvent.withStdoutStderr(outErrBuffer.outAsLatin1(), outErrBuffer.errAsLatin1()));
+      reporter.handle(prefixEvent.withStdoutStderr(outErrBuffer));
     } else {
       reporter.handle(prefixEvent);
     }

src/main/java/com/google/devtools/build/lib/util/io/FileOutErr.java
@@ -140,6 +140,16 @@ public class FileOutErr extends OutErr {
     return getFileErrorStream().getRecordedOutput();
   }
 
+  /** Return a reference to the recorded stderr */
+  public OutputReference getOutReference() {
+    return new FileOutputReference(getFileOutputStream());
+  }
+
+  /** Return a reference to the recorded stdout */
+  public OutputReference getErrReference() {
+    return new FileOutputReference(getFileErrorStream());
+  }
+
   /**
    * Closes and deletes the error stream.
    */
@@ -236,6 +246,8 @@ public class FileOutErr extends OutErr {
 
     abstract Path getFileUnsafe();
 
+    abstract boolean mightHaveOutput();
+
     /** Closes and deletes the output. */
     abstract void clear() throws IOException;
   }
@@ -272,6 +284,11 @@ public class FileOutErr extends OutErr {
     @Override
     String getRecordedOutput() {
       return "";
+    }
+
+    @Override
+    boolean mightHaveOutput() {
+      return false;
     }
 
     @Override
@@ -395,6 +412,11 @@ public class FileOutErr extends OutErr {
     }
 
     @Override
+    boolean mightHaveOutput() {
+      return mightHaveOutput;
+    }
+
+    @Override
     String getRecordedOutput() {
       StringBuilder result = new StringBuilder();
       try {
@@ -472,6 +494,55 @@ public class FileOutErr extends OutErr {
     public synchronized void close() throws IOException {
       if (hasOutputStream()) {
         getOutputStream().close();
+      }
+    }
+  }
+
+  /** Provide access to a sequence of bytes that might be too long to be stored in memory. */
+  public interface OutputReference {
+    /** Return the length of the output. */
+    long getLength();
+
+    /** Return the final up to n bytes of the message. */
+    byte[] getFinalBytes(int count) throws IOException;
+  }
+
+  private static class FileOutputReference implements OutputReference {
+    private final AbstractFileRecordingOutputStream stream;
+    long fileSize;
+
+    FileOutputReference(AbstractFileRecordingOutputStream stream) {
+      this.stream = stream;
+      if (!stream.mightHaveOutput()) {
+        this.fileSize = 0;
+        return;
+      }
+      Path file = stream.getFileUnsafe();
+      try {
+        this.fileSize = file.getFileSize();
+      } catch (IOException ex) {
+        this.fileSize = 0;
+      }
+    }
+
+    @Override
+    public long getLength() {
+      return fileSize;
+    }
+
+    @Override
+    public byte[] getFinalBytes(int count) throws IOException {
+      if (fileSize == 0) {
+        // We used file size 0 to mark any errors in accessing the underlying file.
+        // So stick to this to give a consistent view.
+        return new byte[0];
+      }
+
+      try (InputStream in = stream.getFileUnsafe().getInputStream()) {
+        if (fileSize > count) {
+          in.skip(fileSize - (long) count);
+        }
+        return ByteStreams.toByteArray(in);
       }
     }
   }

commit d3283b15ef938d3d156e300f76588d8875e26272

UI: only readd the progress bar, if it was removed
Fix an error introduced in c87cc91007aa89e33e95a153c448f8b9fc91fa2f that
made adding the progress bar after a potential writes to a stream independent
on whether it was actually removed or not, e.g., because the line to write
wasn't complete yet.

Change-Id: I82bb579d1bce6c1ef56ccfbbac396953c4cb5230
PiperOrigin-RevId: 274550501

src/main/java/com/google/devtools/build/lib/runtime/UiEventHandler.java
@@ -415,12 +415,11 @@ public class UiEventHandler implements EventHandler {
               if (remainingCapacity() < 0) {
                 return;
               }
-              writeToStream(stream, event.getKind(), event.getMessageReference());
-              stream.flush();
-              if (showProgress && cursorControl) {
-                addProgressBar();
-              }
-              terminal.flush();
+              writeToStream(
+                  stream,
+                  event.getKind(),
+                  event.getMessageReference(),
+                  /* readdProgressBar= */ showProgress && cursorControl);
             }
             break;
           case ERROR:
@@ -482,9 +481,17 @@ public class UiEventHandler implements EventHandler {
         if (event.hasStdoutStderr()) {
           clearProgressBar();
           terminal.flush();
-          writeToStream(outErr.getErrorStream(), EventKind.STDERR, event.getStdErrReference());
+          writeToStream(
+              outErr.getErrorStream(),
+              EventKind.STDERR,
+              event.getStdErrReference(),
+              /* readdProgressBar= */ false);
           outErr.getErrorStream().flush();
-          writeToStream(outErr.getOutputStream(), EventKind.STDOUT, event.getStdOutReference());
+          writeToStream(
+              outErr.getOutputStream(),
+              EventKind.STDOUT,
+              event.getStdOutReference(),
+              /* readdProgressBar= */ false);
           outErr.getOutputStream().flush();
           if (showProgress && cursorControl) {
             addProgressBar();
@@ -497,7 +504,8 @@ public class UiEventHandler implements EventHandler {
     }
   }
 
-  private void writeToStream(OutputStream stream, EventKind eventKind, OutputReference reference)
+  private void writeToStream(
+      OutputStream stream, EventKind eventKind, OutputReference reference, boolean readdProgressBar)
       throws IOException {
     byte[] message;
     double cap = remainingCapacity(reference.getLength());
@@ -529,6 +537,11 @@ public class UiEventHandler implements EventHandler {
         stdoutBuffer = restMessage;
       } else {
         stderrBuffer = restMessage;
+      }
+      stream.flush();
+      if (readdProgressBar) {
+        addProgressBar();
+        terminal.flush();
       }
     } else {
       if (eventKind == EventKind.STDOUT) {