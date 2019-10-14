28472b0e9351aa87099baa445f33173bc01e2ab6
ulfjack <ulfjack@google.com>
2019-10-14 09:08:33 -0700

    Simplify TestRunnerAction a bit
    - Remove one continuation wrapper from beginExecution; this is not
      actually necessary anymore since https://github.com/bazelbuild/bazel/commit/415165a55ca0f1656d1254618758d2a30d8c5030
    - Remove duplicate catch clauses
    - Remove manual printing of the stack trace; the
      EnvironmentalExecException now always comes with a stack trace as of
      https://github.com/bazelbuild/bazel/commit/572bc144e77864fb0458155b432547af9cb260a0
    - Remove a micro-optimization
    
    PiperOrigin-RevId: 274591280

--- src/main/java/com/google/devtools/build/lib/analysis/test/TestActionContext.java
+++ src/main/java/com/google/devtools/build/lib/analysis/test/TestActionContext.java
@@ -127,8 +127,7 @@ public interface TestActionContext extends ActionContext {
      * return a completed result, or it may return a continuation with a non-null future
      * representing asynchronous execution.
      */
-    TestAttemptContinuation beginExecution()
-        throws InterruptedException, IOException, ExecException;
+    TestAttemptContinuation beginExecution() throws InterruptedException, IOException;
 
     /**
      * After the first attempt has run, this method is called to determine the maximum number of
@@ -151,7 +150,7 @@ public interface TestActionContext extends ActionContext {
      * attempts are exhausted and then run with another strategy for another set of attempts. This
      * is rarely used, and should ideally be removed.
      */
-    default TestRunnerSpawn getFallbackRunner() throws ExecException, InterruptedException {
+    default TestRunnerSpawn getFallbackRunner() throws ExecException {
       return null;
     }
   }
