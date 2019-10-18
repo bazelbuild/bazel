23134ea9a07266229142186fd042771ffc811640
Googler <noreply@google.com>
2019-10-17 20:07:52 -0700

    bazel packages: delete EmptyEnvironmentExtension
    It appears to be unnecessary.
    
    PiperOrigin-RevId: 275391229

--- src/test/java/com/google/devtools/build/lib/skyframe/PackageLookupFunctionTest.java
+++ src/test/java/com/google/devtools/build/lib/skyframe/PackageLookupFunctionTest.java
@@ -32,8 +32,6 @@ import com.google.devtools.build.lib.cmdline.RepositoryName;
 import com.google.devtools.build.lib.events.NullEventHandler;
 import com.google.devtools.build.lib.packages.BuildFileName;
 import com.google.devtools.build.lib.packages.BuildFileNotFoundException;
-import com.google.devtools.build.lib.packages.PackageFactory;
-import com.google.devtools.build.lib.packages.PackageFactory.EnvironmentExtension;
 import com.google.devtools.build.lib.packages.RuleClassProvider;
 import com.google.devtools.build.lib.packages.WorkspaceFileValue;
 import com.google.devtools.build.lib.pkgcache.PathPackageLocator;
@@ -148,9 +146,6 @@ public abstract class PackageLookupFunctionTest extends FoundationTestCase {
             ruleClassProvider,
             analysisMock
                 .getPackageFactoryBuilderForTesting(directories)
-                .setEnvironmentExtensions(
-                    ImmutableList.<EnvironmentExtension>of(
-                        new PackageFactory.EmptyEnvironmentExtension()))
                 .build(ruleClassProvider, fileSystem),
             directories,
             /*skylarkImportLookupFunctionForInlining=*/ null));
