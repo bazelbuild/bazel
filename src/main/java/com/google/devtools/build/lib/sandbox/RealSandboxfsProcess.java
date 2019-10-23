ab4f976cab5745031f409c77af20676116c7031f
jmmv <jmmv@google.com>
2019-10-23 15:22:55 -0700

    Replace sandboxfs --version parsing with new GnuVersionParser.
    While doing this, get rid of the pre-mount version check performed "out of
    band" to check if sandboxfs can be used.  Instead, do the version check
    within the actual mount attempt -- and when things fail, decide whether we
    have to report an error or not based on whether sandboxfs usage was
    explicitly requested or selected automatically.
    
    RELNOTES: None.
    PiperOrigin-RevId: 276362964

--- src/main/java/com/google/devtools/build/lib/sandbox/RealSandboxfsProcess.java
+++ src/main/java/com/google/devtools/build/lib/sandbox/RealSandboxfsProcess.java
@@ -19,16 +19,16 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.io.ByteStreams;
 import com.google.devtools.build.lib.shell.Subprocess;
 import com.google.devtools.build.lib.shell.SubprocessBuilder;
-import com.google.devtools.build.lib.shell.SubprocessBuilder.StreamAction;
 import com.google.devtools.build.lib.util.OS;
+import com.google.devtools.build.lib.versioning.GnuVersionParser;
+import com.google.devtools.build.lib.versioning.ParseException;
+import com.google.devtools.build.lib.versioning.SemVer;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.PathFragment;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -96,65 +96,6 @@ final class RealSandboxfsProcess implements SandboxfsProcess {
   }
 
   /**
-   * Checks if the given sandboxfs binary is available and is valid.
-   *
-   * @param binary path to the sandboxfs binary that will later be used in the {@link #mount} call.
-   * @return true if the binary looks good, false otherwise
-   */
-  static boolean isAvailable(PathFragment binary) {
-    Subprocess process;
-    try {
-      process =
-          new SubprocessBuilder()
-              .setArgv(binary.getPathString(), "--version")
-              .setStdout(StreamAction.STREAM)
-              .redirectErrorStream(true)
-              .start();
-    } catch (IOException e) {
-      log.warning("sandboxfs binary at " + binary + " seems to be missing; got error: " + e);
-      return false;
-    }
-
-    ByteArrayOutputStream outErrBytes = new ByteArrayOutputStream();
-    try {
-      ByteStreams.copy(process.getInputStream(), outErrBytes);
-    } catch (IOException e) {
-      try {
-        outErrBytes.write(("Failed to read stdout: " + e).getBytes("UTF-8"));
-      } catch (IOException e2) {
-        // Should not really have happened. There is nothing we can do.
-      }
-    }
-    String outErr = outErrBytes.toString().replaceFirst("\n$", "");
-
-    process.waitForUninterruptibly();
-    int exitCode = process.exitValue();
-    if (exitCode == 0) {
-      // TODO(jmmv): Validate the version number and ensure we support it. Would be nice to reuse
-      // the DottedVersion logic from the Apple rules.
-      if (outErr.matches("sandboxfs .*")) {
-        return true;
-      } else {
-        log.warning(
-            "sandboxfs binary at "
-                + binary
-                + " exists but doesn't seem valid; output was: "
-                + outErr);
-        return false;
-      }
-    } else {
-      log.warning(
-          "sandboxfs binary at "
-              + binary
-              + " returned non-zero exit code "
-              + exitCode
-              + " and output "
-              + outErr);
-      return false;
-    }
-  }
-
-  /**
    * Mounts a new sandboxfs instance.
    *
    * <p>The root of the file system instance is left unmapped which means that it remains as
@@ -173,8 +114,12 @@ final class RealSandboxfsProcess implements SandboxfsProcess {
       throws IOException {
     log.info("Mounting sandboxfs (" + binary + ") onto " + mountPoint);
 
-    // TODO(jmmv): Before starting a sandboxfs serving instance, we must query the current version
-    // of sandboxfs and check if we support its communication protocol.
+    GnuVersionParser<SemVer> parser = new GnuVersionParser<>("sandboxfs", SemVer::parse);
+    try {
+      parser.fromProgram(binary);
+    } catch (IOException | ParseException e) {
+      throw new IOException("Failed to get sandboxfs version from " + binary, e);
+    }
 
     ImmutableList.Builder<String> argvBuilder = ImmutableList.builder();
 
