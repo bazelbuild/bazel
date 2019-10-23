e1898a0e50ed031f00008362b12dd401798b4340
jmmv <jmmv@google.com>
2019-10-23 14:16:47 -0700

    Export build files to hook up the versioning package into the build.
    Missed earlier because this package was not yet put to use... and thus things
    broke when trying to depend on it.
    
    RELNOTES: None.
    PiperOrigin-RevId: 276349396

--- src/test/java/com/google/devtools/build/lib/versioning/SemVerTest.java
+++ src/test/java/com/google/devtools/build/lib/versioning/SemVerTest.java
@@ -15,7 +15,7 @@
 package com.google.devtools.build.lib.versioning;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.assertThrows;
+import static com.google.devtools.build.lib.testutil.MoreAsserts.assertThrows;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
