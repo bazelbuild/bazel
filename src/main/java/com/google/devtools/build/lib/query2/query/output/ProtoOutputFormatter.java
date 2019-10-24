ce00ced5e957a94b726b8b429f4a95d6e34a6686
michajlo <michajlo@google.com>
2019-10-24 09:27:17 -0700

    Remove includeLocations and includeRuleInputsAndOutputs subclass hooks
    This was from before we had flags for these things.
    
    PiperOrigin-RevId: 276503421

--- src/main/java/com/google/devtools/build/lib/query2/query/output/ProtoOutputFormatter.java
+++ src/main/java/com/google/devtools/build/lib/query2/query/output/ProtoOutputFormatter.java
@@ -171,7 +171,7 @@ public class ProtoOutputFormatter extends AbstractUnorderedFormatter {
       Build.Rule.Builder rulePb = Build.Rule.newBuilder()
           .setName(rule.getLabel().toString())
           .setRuleClass(rule.getRuleClass());
-      if (includeLocation()) {
+      if (includeLocations) {
         rulePb.setLocation(FormatUtils.getLocation(target, relativeLocations));
       }
       addAttributes(rulePb, rule, extraDataForPostProcess);
@@ -207,7 +207,7 @@ public class ProtoOutputFormatter extends AbstractUnorderedFormatter {
       }
       rulePb.addAllAttribute(
           attributes.stream().distinct().sorted(ATTRIBUTE_NAME).collect(Collectors.toList()));
-      if (includeRuleInputsAndOutputs()) {
+      if (includeRuleInputsAndOutputs) {
         // Add all deps from aspects as rule inputs of current target.
          aspectsDependencies
              .values()
@@ -242,7 +242,7 @@ public class ProtoOutputFormatter extends AbstractUnorderedFormatter {
                        .setGeneratingRule(generatingRule.getLabel().toString())
                        .setName(label.toString());
 
-      if (includeLocation()) {
+      if (includeLocations) {
         output.setLocation(FormatUtils.getLocation(target, relativeLocations));
       }
       targetPb.setType(GENERATED_FILE);
@@ -254,7 +254,7 @@ public class ProtoOutputFormatter extends AbstractUnorderedFormatter {
       Build.SourceFile.Builder input = Build.SourceFile.newBuilder()
           .setName(label.toString());
 
-      if (includeLocation()) {
+      if (includeLocations) {
         input.setLocation(FormatUtils.getLocation(target, relativeLocations));
       }
 
@@ -289,7 +289,7 @@ public class ProtoOutputFormatter extends AbstractUnorderedFormatter {
       SourceFile.Builder input = SourceFile.newBuilder()
                                            .setName(label.toString());
 
-      if (includeLocation()) {
+      if (includeLocations) {
         input.setLocation(FormatUtils.getLocation(target, relativeLocations));
       }
       targetPb.setType(SOURCE_FILE);
@@ -403,14 +403,6 @@ public class ProtoOutputFormatter extends AbstractUnorderedFormatter {
 
   protected boolean includeRuleDefinitionEnvironment() {
     return true;
-  }
-
-  protected boolean includeRuleInputsAndOutputs() {
-    return includeRuleInputsAndOutputs;
-  }
-
-  protected boolean includeLocation() {
-    return includeLocations;
   }
 
   /**
