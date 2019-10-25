e92b2d997875db8db70e0c4ff37e52a94c0d4a35
laszlocsomor <laszlocsomor@google.com>
2019-10-25 02:11:02 -0700

    aquery_differ no longer depends on Abseil.
    This is the only Python script in the Bazel source
    tree that uses abseil-py. I'm planning to remove
    abseil-py from the Bazel source tree.
    
    abseil-py is 1.8 MB, removing it will speed up Git
    checkouts and shrink the distribution archive.
    
    abseil-py is also a maintenance liability, because
    we need to update it occasionally. I ran into that
    just now, trying to update Bazel's Python sources
    to PY3.
    
    PiperOrigin-RevId: 276653655

--- tools/aquery_differ/aquery_differ.py
+++ tools/aquery_differ/aquery_differ.py
@@ -32,24 +32,31 @@ bazel run //tools/aquery_differ:aquery_differ -- \
 --attrs=inputs
 """
 
+from __future__ import print_function
 import difflib
 import os
 import sys
-from absl import app
-from absl import flags
+
 from google.protobuf import text_format
 from src.main.protobuf import analysis_pb2
 from tools.aquery_differ.resolvers.dep_set_resolver import DepSetResolver
+from third_party.py import gflags
 
-flags.DEFINE_string("before", None, "Aquery output before the change")
-flags.DEFINE_string("after", None, "Aquery output after the change")
-flags.DEFINE_enum(
+gflags.DEFINE_string("before", None, "Aquery output before the change")
+gflags.DEFINE_string("after", None, "Aquery output after the change")
+gflags.DEFINE_enum(
     "input_type", "proto", ["proto", "textproto"],
     "The format of the aquery proto input. One of 'proto' and 'textproto.")
-flags.DEFINE_multi_enum("attrs", ["cmdline"], ["inputs", "cmdline"],
-                        "Attributes of the actions to be compared.")
-flags.mark_flag_as_required("before")
-flags.mark_flag_as_required("after")
+gflags.DEFINE_multistring(
+    "attrs", ["cmdline"], "Attributes of the actions to be compared. Values " +
+    "must be ones of [\"inputs\", \"cmdline\"]")
+gflags.MarkFlagAsRequired("before")
+gflags.MarkFlagAsRequired("after")
+
+gflags.RegisterValidator(
+    "attrs",
+    lambda values: all(v in ["inputs", "cmdline"] for v in values),
+    message="value should be one of [\"inputs\", \"cmdline\"]")
 
 WHITE = "\033[37m%s\033[0m"
 CYAN = "\033[36m%s\033[0m"
@@ -236,11 +243,11 @@ def to_absolute_path(path):
       return path
 
 
-def main(unused_argv):
-  before_file = to_absolute_path(flags.FLAGS.before)
-  after_file = to_absolute_path(flags.FLAGS.after)
-  input_type = flags.FLAGS.input_type
-  attrs = flags.FLAGS.attrs
+def main():
+  before_file = to_absolute_path(gflags.FLAGS.before)
+  after_file = to_absolute_path(gflags.FLAGS.after)
+  input_type = gflags.FLAGS.input_type
+  attrs = gflags.FLAGS.attrs
 
   before_proto = analysis_pb2.ActionGraphContainer()
   after_proto = analysis_pb2.ActionGraphContainer()
@@ -261,4 +268,4 @@ def main(unused_argv):
 
 
 if __name__ == "__main__":
-  app.run(main)
+  main()
