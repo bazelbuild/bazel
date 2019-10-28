f16d9495a74793c8c99fcc8d139462b3ad725940
Googler <noreply@google.com>
2019-10-28 12:51:15 -0700

    bazel syntax: remove spurious import
    Also, remove spurious repeated srcs file.
    
    PiperOrigin-RevId: 277123549

--- src/main/java/com/google/devtools/build/lib/syntax/StarlarkFile.java
+++ src/main/java/com/google/devtools/build/lib/syntax/StarlarkFile.java
@@ -17,7 +17,6 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.hash.HashCode;
 import com.google.devtools.build.lib.events.Event;
 import com.google.devtools.build.lib.events.Location;
-import com.google.devtools.build.lib.syntax.Parser.ParseResult;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
@@ -54,7 +53,7 @@ public final class StarlarkFile extends Node {
 
   private static StarlarkFile create(
       List<Statement> preludeStatements,
-      ParseResult result,
+      Parser.ParseResult result,
       String contentHashCode,
       boolean allowImportInternal) {
     ImmutableList.Builder<Statement> statementsbuilder =
