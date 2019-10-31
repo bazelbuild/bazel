a303420e6ec7bdb84308dc7c3edc924960b65daf
Laszlo Csomor <laszlocsomor@google.com>
2019-10-31 10:55:47 +0100

    PathCasing dep. on FileState
    Change-Id: I4487fff77c06a79dc7d965f225ffb0d4bb45c452

--- src/main/java/com/google/devtools/build/lib/skyframe/PathCasingLookupFunction.java
+++ src/main/java/com/google/devtools/build/lib/skyframe/PathCasingLookupFunction.java
@@ -16,22 +16,27 @@ package com.google.devtools.build.lib.skyframe;
 
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
+import com.google.devtools.build.lib.actions.FileStateType;
+import com.google.devtools.build.lib.actions.FileStateValue;
 import com.google.devtools.build.lib.vfs.Dirent;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.Root;
 import com.google.devtools.build.lib.vfs.RootedPath;
 import com.google.devtools.build.lib.vfs.RootedPathAndCasing;
 import com.google.devtools.build.skyframe.SkyFunction;
+import com.google.devtools.build.skyframe.SkyFunctionException;
 import com.google.devtools.build.skyframe.SkyKey;
 import com.google.devtools.build.skyframe.SkyValue;
 import javax.annotation.Nullable;
+import java.io.IOException;
 import java.util.Map;
 
 /** SkyFunction for {@link PathCasingLookupValue}s. */
 public final class PathCasingLookupFunction implements SkyFunction {
 
   @Override
-  public PathCasingLookupValue compute(SkyKey skyKey, Environment env) throws InterruptedException {
+  public PathCasingLookupValue compute(SkyKey skyKey, Environment env)
+      throws PathCasingLookupFunctionException, InterruptedException {
     RootedPathAndCasing arg = (RootedPathAndCasing) skyKey.argument();
     RootedPath parent = arg.getPath().getParentDirectory();
 
@@ -49,31 +54,59 @@ public final class PathCasingLookupFunction implements SkyFunction {
 
     SkyKey pathCasingKey = PathCasingLookupValue.key(parent);
     SkyKey dirListKey = DirectoryListingValue.key(parent);
-    Map<SkyKey, SkyValue> values = env.getValues(ImmutableList.of(pathCasingKey, dirListKey));
+    SkyKey fileStateKey = FileStateValue.key(arg.getPath());
+    Map<SkyKey, SkyValue> values =
+        env.getValues(ImmutableList.of(pathCasingKey, dirListKey, fileStateKey));
     if (env.valuesMissing()) {
       return null;
     }
 
     PathCasingLookupValue parentCasing = (PathCasingLookupValue) values.get(pathCasingKey);
+
     if (!parentCasing.isCorrect()) {
       return PathCasingLookupValue.BAD;
     }
 
+    if (((FileStateValue) values.get(fileStateKey)).getType() == FileStateType.NONEXISTENT) {
+      // Parent's casing is good, though it may or may not exist.
+      // Child is missing, so by definition its casing is also good.
+      //
+      // Example of this scenario:
+      //   path = "[/tmp/non-existent][non-existent]", does not exist
+      //     parent = "[/tmp/non-existent][]", also does not exist
+      //       grandparent = "[/][tmp]", exists and has correct casing
+      //     so parent has correct casing
+      //   so path has correct casing.
+      return PathCasingLookupValue.GOOD;
+    }
+
     DirectoryListingValue parentList = (DirectoryListingValue) values.get(dirListKey);
     String expected = arg.getPath().getRootRelativePath().getBaseName();
+
     if (Strings.isNullOrEmpty(expected)) {
       expected = arg.getPath().asPath().getBaseName();
     }
     Dirent child = parentList.getDirents().maybeGetDirent(expected);
+    if (child == null) {
+      throw new PathCasingLookupFunctionException(
+          new IOException(
+              String.format("'%s' exists but not listed by its parent directory", arg.getPath())));
+    }
 
     return child != null && expected.equals(child.getName())
         ? PathCasingLookupValue.GOOD
-        : PathCasingLookupValue.BAD;  // or missing
+        : PathCasingLookupValue.BAD;
   }
 
   @Nullable
   @Override
   public String extractTag(SkyKey skyKey) {
     return null;
+  }
+
+  public static final class PathCasingLookupFunctionException extends SkyFunctionException {
+    public PathCasingLookupFunctionException(IOException e) {
+      super(e, Transience.TRANSIENT);
+    }
   }
 }
