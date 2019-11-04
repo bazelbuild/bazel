05b6f172d4fe4410685344cabc64f100cb9839d5
jmmv <jmmv@google.com>
2019-11-04 04:22:12 -0800

    Add support for sandboxfs 0.2 and newer.
    This change adds support for the new reconfigurations protocol in sandboxfs
    0.2.0 and above without breaking compatibility with the 0.1.x series.
    
    To do so, this splits the RealSandboxfsProcess class into two subclasses,
    moving all protocol-specific logic into them.  Note that the mount flags
    could also vary across versions, but as they do not yet, I'm keeping them
    in the generic parent class.
    
    As part of this change, I'm also adding unit tests for each of the
    supported versions.  These are very rudimentary and do not guarantee that
    Bazel can communicate with sandboxfs.  However, they provide a certain
    level of safety during refactorings of the Bazel code because these tests
    ensure that the protocol spoken by Bazel does not change unexpectedly.
    
    RELNOTES: None.
    PiperOrigin-RevId: 278350105

--- src/main/java/com/google/devtools/build/lib/sandbox/RealSandboxfsProcess.java
+++ src/main/java/com/google/devtools/build/lib/sandbox/RealSandboxfsProcess.java
@@ -14,10 +14,7 @@
 
 package com.google.devtools.build.lib.sandbox;
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkNotNull;
-import static java.nio.charset.StandardCharsets.UTF_8;
-
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.devtools.build.lib.shell.Subprocess;
@@ -28,20 +25,32 @@ import com.google.devtools.build.lib.versioning.ParseException;
 import com.google.devtools.build.lib.versioning.SemVer;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.PathFragment;
-import com.google.gson.stream.JsonWriter;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.StringWriter;
-import java.util.List;
 import java.util.logging.Logger;
 import javax.annotation.Nullable;
 
-/** A sandboxfs implementation that uses an external sandboxfs binary to manage the mount point. */
-final class RealSandboxfsProcess implements SandboxfsProcess {
+/**
+ * A sandboxfs implementation that uses an external sandboxfs binary to manage the mount point.
+ *
+ * <p>This class implements common code to generalize the interactions with sandboxfs, but delegates
+ * to its subclassess once the version of sandboxfs in use has been determined. The subclasses
+ * implement logic specific to each version to provide compatibility with the different versions of
+ * sandboxfs that the user might have installed.
+ */
+abstract class RealSandboxfsProcess implements SandboxfsProcess {
+
   private static final Logger log = Logger.getLogger(RealSandboxfsProcess.class.getName());
+
+  /**
+   * Contains the {@code --allow} flag to pass to sandboxfs.
+   *
+   * <p>On macOS, we need to allow users other than self to access the sandboxfs instance. This is
+   * necessary because macOS's amfid, which runs as root, has to have access to the binaries within
+   * the sandbox in order to validate signatures. See:
+   * http://jmmv.dev/2017/10/fighting-execs-sandboxfs-macos.html
+   */
+  @VisibleForTesting
+  static final String ALLOW_FLAG = OS.getCurrent() == OS.DARWIN ? "--allow=other" : "--allow=self";
 
   /** Directory on which the sandboxfs is serving. */
   private final Path mountPoint;
@@ -50,18 +59,6 @@ final class RealSandboxfsProcess implements SandboxfsProcess {
    * Process handle to the sandboxfs instance.  Null only after {@link #destroy()} has been invoked.
    */
   private @Nullable Subprocess process;
-
-  /**
-   * Writer with which to send data to the sandboxfs instance.  Null only after {@link #destroy()}
-   * has been invoked.
-   */
-  private @Nullable BufferedWriter processStdIn;
-
-  /**
-   * Reader with which to receive data from the sandboxfs instance.  Null only after
-   * {@link #destroy()} has been invoked.
-   */
-  private @Nullable BufferedReader processStdOut;
 
   /**
    * Shutdown hook to stop the sandboxfs instance on abrupt termination.  Null only after
@@ -74,14 +71,9 @@ final class RealSandboxfsProcess implements SandboxfsProcess {
    *
    * @param process process handle for the already-running sandboxfs instance
    */
-  private RealSandboxfsProcess(Path mountPoint, Subprocess process) {
+  RealSandboxfsProcess(Path mountPoint, Subprocess process) {
     this.mountPoint = mountPoint;
-
     this.process = process;
-    this.processStdIn = new BufferedWriter(
-        new OutputStreamWriter(process.getOutputStream(), UTF_8));
-    this.processStdOut = new BufferedReader(
-        new InputStreamReader(process.getInputStream(), UTF_8));
 
     this.shutdownHook =
         new Thread(
@@ -116,21 +108,16 @@ final class RealSandboxfsProcess implements SandboxfsProcess {
     log.info("Mounting sandboxfs (" + binary + ") onto " + mountPoint);
 
     GnuVersionParser<SemVer> parser = new GnuVersionParser<>("sandboxfs", SemVer::parse);
+    SemVer version;
     try {
-      parser.fromProgram(binary);
+      version = parser.fromProgram(binary);
     } catch (IOException | ParseException e) {
       throw new IOException("Failed to get sandboxfs version from " + binary, e);
     }
 
     ImmutableList.Builder<String> argvBuilder = ImmutableList.builder();
-
     argvBuilder.add(binary.getPathString());
-
-    // On macOS, we need to allow users other than self to access the sandboxfs instance.  This is
-    // necessary because macOS's amfid, which runs as root, has to have access to the binaries
-    // within the sandbox in order to validate signatures. See:
-    // http://julio.meroh.net/2017/10/fighting-execs-sandboxfs-macos.html
-    argvBuilder.add(OS.getCurrent() == OS.DARWIN ? "--allow=other" : "--allow=self");
+    argvBuilder.add(ALLOW_FLAG);
 
     // TODO(jmmv): Pass flags to enable sandboxfs' debugging support (--listen_address and --debug)
     // when requested by the user via --sandbox_debug.  Tricky because we have to figure out how to
@@ -148,7 +135,12 @@ final class RealSandboxfsProcess implements SandboxfsProcess {
         "PATH", System.getenv("PATH")));
 
     Subprocess process = processBuilder.start();
-    RealSandboxfsProcess sandboxfs = new RealSandboxfsProcess(mountPoint, process);
+    RealSandboxfsProcess sandboxfs;
+    if (version.compareTo(SemVer.from(0, 2)) >= 0) {
+      sandboxfs = new RealSandboxfs02Process(mountPoint, process);
+    } else {
+      sandboxfs = new RealSandboxfs01Process(mountPoint, process);
+    }
     try {
       // Create an empty sandbox to ensure sandboxfs is successfully serving.
       sandboxfs.createSandbox("empty", ImmutableList.of());
@@ -176,110 +168,21 @@ final class RealSandboxfsProcess implements SandboxfsProcess {
       shutdownHook = null;
     }
 
-    if (processStdIn != null) {
+    if (process != null) {
       try {
-        processStdIn.close();
+        process.getOutputStream().close();
       } catch (IOException e) {
         log.warning("Failed to close sandboxfs's stdin pipe: " + e);
       }
-      processStdIn = null;
-    }
 
-    if (processStdOut != null) {
       try {
-        processStdOut.close();
+        process.getInputStream().close();
       } catch (IOException e) {
         log.warning("Failed to close sandboxfs's stdout pipe: " + e);
       }
-      processStdOut = null;
-    }
 
-    if (process != null) {
       process.destroyAndWait();
       process = null;
     }
-  }
-
-  /**
-   * Pushes a new configuration to sandboxfs and waits for acceptance.
-   *
-   * @param config the configuration chunk to push to sandboxfs
-   * @throws IOException if sandboxfs cannot be reconfigured either because of an error in the
-   *     configuration or because we failed to communicate with the subprocess
-   */
-  private synchronized void reconfigure(String config) throws IOException {
-    checkNotNull(processStdIn, "sandboxfs already has been destroyed");
-    processStdIn.write(config);
-    processStdIn.write("\n\n");
-    processStdIn.flush();
-
-    checkNotNull(processStdOut, "sandboxfs has already been destroyed");
-    String done = processStdOut.readLine();
-    if (done == null) {
-      throw new IOException("premature end of output from sandboxfs");
-    }
-    if (!done.equals("Done")) {
-      throw new IOException("received unknown string from sandboxfs: " + done + "; expected Done");
-    }
-  }
-
-  /** Encodes a mapping into JSON. */
-  @SuppressWarnings("UnnecessaryParentheses")
-  private static void writeMapping(JsonWriter writer, PathFragment root, Mapping mapping)
-      throws IOException {
-    writer.beginObject();
-    {
-      writer.name("Mapping");
-      writer.value((root.getRelative(mapping.path().toRelative())).getPathString());
-      writer.name("Target");
-      writer.value(mapping.target().getPathString());
-      writer.name("Writable");
-      writer.value(mapping.writable());
-    }
-    writer.endObject();
-  }
-
-  @Override
-  @SuppressWarnings("UnnecessaryParentheses")
-  public void createSandbox(String name, List<Mapping> mappings) throws IOException {
-    checkArgument(!PathFragment.containsSeparator(name));
-    PathFragment root = PathFragment.create("/").getRelative(name);
-
-    StringWriter stringWriter = new StringWriter();
-    try (JsonWriter writer = new JsonWriter(stringWriter)) {
-      writer.beginArray();
-      for (Mapping mapping : mappings) {
-        writer.beginObject();
-        {
-          writer.name("Map");
-          writeMapping(writer, root, mapping);
-        }
-        writer.endObject();
-      }
-      writer.endArray();
-    }
-    reconfigure(stringWriter.toString());
-  }
-
-  @Override
-  @SuppressWarnings("UnnecessaryParentheses")
-  public void destroySandbox(String name) throws IOException {
-    checkArgument(!PathFragment.containsSeparator(name));
-    PathFragment root = PathFragment.create("/").getRelative(name);
-
-    StringWriter stringWriter = new StringWriter();
-    try (JsonWriter writer = new JsonWriter(stringWriter)) {
-      writer.beginArray();
-      {
-        writer.beginObject();
-        {
-          writer.name("Unmap");
-          writer.value(root.getPathString());
-        }
-        writer.endObject();
-      }
-      writer.endArray();
-    }
-    reconfigure(stringWriter.toString());
   }
 }
