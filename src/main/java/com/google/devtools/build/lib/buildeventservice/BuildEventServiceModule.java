91c15eab48969ef5181846ab151cbc74e827d0ef
felly <felly@google.com>
2019-12-02 13:33:05 -0800

    No longer log remotely on BES when closing in afterCommand().
    PiperOrigin-RevId: 283406020

--- src/main/java/com/google/devtools/build/lib/buildeventservice/BuildEventServiceModule.java
+++ src/main/java/com/google/devtools/build/lib/buildeventservice/BuildEventServiceModule.java
@@ -547,9 +547,7 @@ public abstract class BuildEventServiceModule<BESOptionsT extends BuildEventServ
         // This should not occur, but close with an internal error if a {@link BuildEventStreamer}
         // bug manifests as an unclosed streamer.
         googleLogger.atWarning().log("Attempting to close BES streamer after command");
-        String msg = "BES was not properly closed";
-        LoggingUtil.logToRemote(Level.WARNING, msg, new IllegalStateException(msg));
-        reporter.handle(Event.warn(msg));
+        reporter.handle(Event.warn("BES was not properly closed"));
         forceShutdownBuildEventStreamer();
       }
 
