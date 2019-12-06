63c255cef72a51b37e840ea6395ef57aaff19c5d
dchai <dchai@google.com>
2019-12-05 16:37:49 -0800

    Fix a case where aapt1 was still being used.
    The fix is just to delete the aapt1 branch; since
    --incompatible_prohibit_aapt1 is on, this code should never be used.  It was
    because a builder was not being set correctly:
    
    https://github.com/bazelbuild/bazel/blob/c9d58a8cbf4ac57bce32b852246c76e4cf61bb58/src/main/java/com/google/devtools/build/lib/rules/android/ResourceApk.java#L316
    
    See
    https://github.com/bazelbuild/bazel/commit/f54c2aeec89e9de176815ce68a11f3f8dbe50c93
    which fixed another case where the builder pattern led to code not being updated
    correctly.
    
    PiperOrigin-RevId: 284081002

--- src/main/java/com/google/devtools/build/lib/rules/android/AndroidResourcesProcessorBuilder.java
+++ src/main/java/com/google/devtools/build/lib/rules/android/AndroidResourcesProcessorBuilder.java
@@ -13,6 +13,7 @@
 // limitations under the License.
 package com.google.devtools.build.lib.rules.android;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
 import com.google.devtools.build.lib.actions.Artifact;
@@ -60,7 +61,6 @@ public class AndroidResourcesProcessorBuilder {
   private Artifact mergedResourcesOut;
   private boolean isLibrary;
   private boolean crunchPng = true;
-  private AndroidAaptVersion aaptVersion;
   private boolean throwOnResourceConflict;
   private String packageUnderTest;
   private boolean isTestWithResources = false;
@@ -160,7 +160,7 @@ public class AndroidResourcesProcessorBuilder {
   }
 
   public AndroidResourcesProcessorBuilder targetAaptVersion(AndroidAaptVersion aaptVersion) {
-    this.aaptVersion = aaptVersion;
+    Preconditions.checkArgument(aaptVersion == AndroidAaptVersion.AAPT2);
     return this;
   }
 
@@ -212,19 +212,8 @@ public class AndroidResourcesProcessorBuilder {
     AndroidResources databindingProcessedResources = dataBindingContext.processResources(
         dataContext, primaryResources, processedManifest.getPackage());
 
-    if (aaptVersion == AndroidAaptVersion.AAPT2) {
-      createAapt2ApkAction(
-          dataContext,
-          databindingProcessedResources,
-          primaryAssets,
-          primaryManifest);
-    } else {
-      createAaptAction(
-          dataContext,
-          databindingProcessedResources,
-          primaryAssets,
-          primaryManifest);
-    }
+    createAapt2ApkAction(
+        dataContext, databindingProcessedResources, primaryAssets, primaryManifest);
 
     // Wrap the parsed resources
     ParsedAndroidResources parsedResources =
