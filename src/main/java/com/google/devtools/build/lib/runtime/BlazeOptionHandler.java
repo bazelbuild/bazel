b42c81eb59138cae3a3bf7e365423825a111128f
aldersondrive <alderson@google.com>
2019-12-07 15:47:27 -0500

    Allow building the Bazel server for OpenBSD.

--- src/main/java/com/google/devtools/build/lib/runtime/BlazeOptionHandler.java
+++ src/main/java/com/google/devtools/build/lib/runtime/BlazeOptionHandler.java
@@ -326,6 +326,8 @@ public final class BlazeOptionHandler {
         return "windows";
       case FREEBSD:
         return "freebsd";
+      case OPENBSD:
+        return "openbsd";
       default:
         return OS.getCurrent().getCanonicalName();
     }
