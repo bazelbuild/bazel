bfd5d0f219f8385c51cec4769c019f1d9a7c7998
ulfjack <ulfjack@google.com>
2019-12-16 08:01:22 -0800

    Cleanup {BuildOptions,BuildConfigValue}.equals
    PiperOrigin-RevId: 285772942

--- src/main/java/com/google/devtools/build/lib/analysis/config/BuildOptions.java
+++ src/main/java/com/google/devtools/build/lib/analysis/config/BuildOptions.java
@@ -1121,7 +1121,7 @@ public final class BuildOptions implements Cloneable, Serializable {
       if (this == o) {
         return true;
       }
-      if (o == null || getClass() != o.getClass()) {
+      if (!(o instanceof OptionsDiffForReconstruction)) {
         return false;
       }
       OptionsDiffForReconstruction that = (OptionsDiffForReconstruction) o;
