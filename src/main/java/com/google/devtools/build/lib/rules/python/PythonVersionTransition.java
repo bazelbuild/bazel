9a458c59ec07822de3111390d40d40b26d83afa4
gregce <gregce@google.com>
2020-01-03 10:39:20 -0800

    Stop memory waste from BuildOptions instance proliferation.
    Conceptually, creating BuildOptions instances should be cheap. But BuildOptions#diffForReconstructionCache keeps references to each instance. For a large graph with > 100,000 transitions, this can waste gigabytes of memory. While these references are weak/soft (so theoretically garbage-collectible), this can still OOM builds if lots of instances are created quickly.
    
    This change has two pieces:
    
    1) Creates a new cache that essentially interns BuildOptions. So transitioning from one configuration to another doesn't intrinsically produce a new BuildOptions instance.
    
    2) Applies this cache to the exec transition and Python version transition logic. This especially benefits builds where every rule depends on a Python tool that doesn't use the default Python version.
    
    Over the longer term we could revisit the purpose of BuildOptions#diffForReconstructionCache. Eliminating that cache might get us back to a world where BuildOptions instantiation is indeed cheap and we don't have to worry about these issues.
    
    RELNOTES: none
    PiperOrigin-RevId: 288013814

--- src/main/java/com/google/devtools/build/lib/rules/python/PythonVersionTransition.java
+++ src/main/java/com/google/devtools/build/lib/rules/python/PythonVersionTransition.java
@@ -16,6 +16,7 @@ package com.google.devtools.build.lib.rules.python;
 
 import com.google.common.base.Preconditions;
 import com.google.devtools.build.lib.analysis.config.BuildOptions;
+import com.google.devtools.build.lib.analysis.config.BuildOptionsCache;
 import com.google.devtools.build.lib.analysis.config.transitions.PatchTransition;
 import com.google.errorprone.annotations.Immutable;
 import java.util.Objects;
@@ -61,6 +62,11 @@ public abstract class PythonVersionTransition implements PatchTransition {
   @Override
   public abstract int hashCode();
 
+  // We added this cache after observing an O(100,000)-node build graph that applied multiple exec
+  // transitions to Python 3 tools on every node. Before this cache, this produced O(100,000)
+  // BuildOptions instances that consumed about a gigabyte of memory.
+  private static final BuildOptionsCache<PythonVersion> cache = new BuildOptionsCache<>();
+
   @Override
   public BuildOptions patch(BuildOptions options) {
     PythonVersion newVersion = determineNewVersion(options);
@@ -70,10 +76,15 @@ public abstract class PythonVersionTransition implements PatchTransition {
     if (!opts.canTransitionPythonVersion(newVersion)) {
       return options;
     }
-    BuildOptions newOptions = options.clone();
-    PythonOptions newOpts = newOptions.get(PythonOptions.class);
-    newOpts.setPythonVersion(newVersion);
-    return newOptions;
+    return cache.applyTransition(
+        options,
+        newVersion,
+        () -> {
+          BuildOptions newOptions = options.clone();
+          PythonOptions newOpts = newOptions.get(PythonOptions.class);
+          newOpts.setPythonVersion(newVersion);
+          return newOptions;
+        });
   }
 
   /** A Python version transition that switches to the value specified in the constructor. */
