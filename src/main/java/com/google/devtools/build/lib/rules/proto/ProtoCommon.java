66c60121b980b2e1e0baa330eca76840f09f243c
Yannic <contact@yannic-bonenberger.com>
2020-01-13 03:45:41 -0800

    proto_lang_toolchain: Add original sources of ProtoInfo to blacklistedProtos
    Fixes #10484
    
    Closes #10493.
    
    PiperOrigin-RevId: 289411825

--- src/main/java/com/google/devtools/build/lib/rules/proto/ProtoCommon.java
+++ src/main/java/com/google/devtools/build/lib/rules/proto/ProtoCommon.java
@@ -127,6 +127,19 @@ public class ProtoCommon {
     return result.build();
   }
 
+  private static NestedSet<Artifact> computeTransitiveOriginalProtoSources(
+      RuleContext ruleContext, ImmutableList<Artifact> originalProtoSources) {
+    NestedSetBuilder<Artifact> result = NestedSetBuilder.naiveLinkOrder();
+
+    result.addAll(originalProtoSources);
+
+    for (ProtoInfo dep : ruleContext.getPrerequisites("deps", Mode.TARGET, ProtoInfo.PROVIDER)) {
+      result.addTransitive(dep.getOriginalTransitiveProtoSources());
+    }
+
+    return result.build();
+  }
+
   static NestedSet<Artifact> computeDependenciesDescriptorSets(RuleContext ruleContext) {
     NestedSetBuilder<Artifact> result = NestedSetBuilder.stableOrder();
 
@@ -462,6 +475,8 @@ public class ProtoCommon {
 
     NestedSet<Artifact> transitiveProtoSources =
         computeTransitiveProtoSources(ruleContext, library.getSources());
+    NestedSet<Artifact> transitiveOriginalProtoSources =
+        computeTransitiveOriginalProtoSources(ruleContext, directProtoSources);
     NestedSet<String> transitiveProtoSourceRoots =
         computeTransitiveProtoSourceRoots(ruleContext, library.getSourceRoot());
 
@@ -491,8 +506,8 @@ public class ProtoCommon {
     ProtoInfo protoInfo =
         new ProtoInfo(
             library.getSources(),
-            directProtoSources,
             library.getSourceRoot(),
+            transitiveOriginalProtoSources,
             transitiveProtoSources,
             transitiveProtoSourceRoots,
             strictImportableProtosForDependents,
