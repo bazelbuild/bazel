093e867c5c335a7b0feef881b548865c6d775fdc
cushon <cushon@google.com>
2020-01-13 15:17:00 -0800

    Remove an unused JavaBuilder flag
    PiperOrigin-RevId: 289530364

--- src/java_tools/buildjar/java/com/google/devtools/build/buildjar/OptionsParser.java
+++ src/java_tools/buildjar/java/com/google/devtools/build/buildjar/OptionsParser.java
@@ -26,7 +26,6 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Deque;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
@@ -73,7 +72,6 @@ public final class OptionsParser {
   private String sourceGenDir;
   private String generatedSourcesOutputJar;
   private String manifestProtoPath;
-  private final Set<String> sourceRoots = new HashSet<>();
 
   private final List<String> sourceFiles = new ArrayList<>();
   private final List<String> sourceJars = new ArrayList<>();
@@ -160,9 +158,6 @@ public final class OptionsParser {
           break;
         case "--output_manifest_proto":
           manifestProtoPath = getArgument(argQueue, arg);
-          break;
-        case "--source_roots":
-          collectFlagArguments(sourceRoots, argQueue, "-");
           break;
         case "--sources":
           collectFlagArguments(sourceFiles, argQueue, "-");
@@ -403,10 +398,6 @@ public final class OptionsParser {
 
   public String getManifestProtoPath() {
     return manifestProtoPath;
-  }
-
-  public Set<String> getSourceRoots() {
-    return sourceRoots;
   }
 
   public List<String> getSourceFiles() {
