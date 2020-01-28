e6fc98d371bda99082a55bb3e5ec228e8e68c5dc
aman <bora.amanalambora@gmail.com>
2020-01-28 10:23:15 +0530

    labelkind test

--- src/test/shell/integration/configured_query_test.sh
+++ src/test/shell/integration/configured_query_test.sh
@@ -90,6 +90,20 @@ EOF
  assert_contains "name: \"//$pkg:japanese\"" output
 }
 
+function test_basic_query_output_labelkind() {
+  local -r pkg=$FUNCNAME
+  mkdir -p $pkg
+  cat > $pkg/BUILD <<EOF
+sh_library(name='maple', deps=[':japanese'])
+sh_library(name='japanese')
+EOF
+
+ bazel cquery --output=label_kind "deps(//$pkg:maple)" > output 2>"$TEST_log" || fail "Expected success"
+
+ assert_contains "name: \"//$pkg:maple\"" output
+ assert_contains "name: \"//$pkg:japanese\"" output
+}
+
 function test_respects_selects() {
   local -r pkg=$FUNCNAME
   mkdir -p $pkg
