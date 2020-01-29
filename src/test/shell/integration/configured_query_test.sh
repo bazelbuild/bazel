9525c240b19c0a1c8ea7b4eb1c0530926b12af57
aman <bora.amanalambora@gmail.com>
2020-01-29 15:48:38 +0530

    labelkind test-edits

--- src/test/shell/integration/configured_query_test.sh
+++ src/test/shell/integration/configured_query_test.sh
@@ -99,9 +99,6 @@ sh_library(name='japanese')
 EOF
 
  bazel cquery --output=label_kind "deps(//$pkg:maple)" > output 2>"$TEST_log" || fail "Expected success"
-
- # assert_contains "name: \"//$pkg:maple\"" output
- # assert_contains "name: \"//$pkg:japanese\"" output
 }
 
 function test_respects_selects() {
