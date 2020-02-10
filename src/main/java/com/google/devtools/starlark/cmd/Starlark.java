39f3db259239783dfa9c70ee382ee3b58d0660e3
adonovan <adonovan@google.com>
2020-02-10 08:49:27 -0800

    devtools/starlark: move command into cmd/ subdirectory
    ...so that the interpreter can (in due course) move into starlark/.
    
    PiperOrigin-RevId: 294229033

--- src/main/java/com/google/devtools/starlark/cmd/Starlark.java
+++ src/main/java/com/google/devtools/starlark/cmd/Starlark.java
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package com.google.devtools.starlark;
+package com.google.devtools.starlark.cmd;
 
 import com.google.devtools.build.lib.events.Event;
 import com.google.devtools.build.lib.syntax.EvalException;
