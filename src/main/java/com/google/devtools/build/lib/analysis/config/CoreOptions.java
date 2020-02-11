3a81b3aee432413e6dba9f66527176866c9e337e
Googler <noreply@google.com>
2020-02-10 17:54:03 -0800

    Increase the default value for analysis_testing_deps_limit to 600.
    PiperOrigin-RevId: 294346640

--- src/main/java/com/google/devtools/build/lib/analysis/config/CoreOptions.java
+++ src/main/java/com/google/devtools/build/lib/analysis/config/CoreOptions.java
@@ -564,7 +564,7 @@ public class CoreOptions extends FragmentOptions implements Cloneable {
 
   @Option(
       name = "analysis_testing_deps_limit",
-      defaultValue = "500",
+      defaultValue = "600",
       documentationCategory = OptionDocumentationCategory.TESTING,
       effectTags = {OptionEffectTag.LOADING_AND_ANALYSIS},
       help =
