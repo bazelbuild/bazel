ad7e4e3f5b60cae3ae5335fb7ce0254184069330
ichern <ichern@google.com>
2020-02-11 06:40:02 -0800

    Fix algorithm for finding non-phony inputs for all phony targets for DAG
    Before, it worked correctly only for tree cases and did not take into
    account possible dependencies between current target's inputs, queueing
    them in the "random" order.
    Use DFS with two passes instead of BFS.
    Add a test that would fail with the previous algorithm.
    
    Closes #10751.
    
    PiperOrigin-RevId: 294427359

--- src/main/java/com/google/devtools/build/lib/bazel/rules/ninja/actions/NinjaPhonyTargetsUtil.java
+++ src/main/java/com/google/devtools/build/lib/bazel/rules/ninja/actions/NinjaPhonyTargetsUtil.java
@@ -17,6 +17,7 @@ package com.google.devtools.build.lib.bazel.rules.ninja.actions;
 import static com.google.common.base.Preconditions.checkState;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableSortedMap;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
@@ -27,14 +28,14 @@ import com.google.devtools.build.lib.bazel.rules.ninja.parser.NinjaTarget;
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
 import com.google.devtools.build.lib.collect.nestedset.NestedSetBuilder;
 import com.google.devtools.build.lib.collect.nestedset.Order;
+import com.google.devtools.build.lib.util.Pair;
 import com.google.devtools.build.lib.vfs.PathFragment;
 import java.util.ArrayDeque;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.SortedMap;
-import java.util.SortedSet;
+import javax.annotation.Nullable;
 
 /**
  * An utility class for gathering non-phony input dependencies of phony {@link NinjaTarget} objects
@@ -43,6 +44,7 @@ import java.util.SortedSet;
  * <p>Cycles are detected and {@link GenericParsingException} is thrown in that case.
  */
 public class NinjaPhonyTargetsUtil {
+
   private NinjaPhonyTargetsUtil() {}
 
   @VisibleForTesting
@@ -61,8 +63,7 @@ public class NinjaPhonyTargetsUtil {
     // will be already computed.
     // The sorting is linear, as we are only checking each input of each node once (we use already).
     List<NinjaTarget> topoOrderedTargets = Lists.newArrayListWithCapacity(phonyTargets.size());
-    SortedSet<NinjaTarget> alreadyVisited =
-        Sets.newTreeSet(Comparator.comparing(t -> Iterables.getOnlyElement(t.getAllOutputs())));
+    Set<NinjaTarget> alreadyVisited = Sets.newHashSet();
     for (Map.Entry<PathFragment, NinjaTarget> entry : phonyTargets.entrySet()) {
       NinjaTarget target = entry.getValue();
       topoOrderedTargets.addAll(topoOrderSubGraph(phonyTargets, alreadyVisited, target));
@@ -81,13 +82,7 @@ public class NinjaPhonyTargetsUtil {
           // Phony target must have only one output (alias); it is checked during parsing.
           PathFragment phonyName = Iterables.getOnlyElement(phonyInput.getAllOutputs());
           NestedSet<T> alreadyComputedSet = result.get(phonyName);
-          if (alreadyComputedSet == null) {
-            // If the target's paths were not computed, then the topo sorting was not successful,
-            // which means that there are cycles in phony targets dependencies.
-            throw new GenericParsingException(
-                String.format(
-                    "Detected a dependency cycle involving the phony target '%s'", phonyName));
-          }
+          Preconditions.checkNotNull(alreadyComputedSet);
           builder.addTransitive(alreadyComputedSet);
         } else {
           // The input is the usual file.
@@ -106,32 +101,53 @@ public class NinjaPhonyTargetsUtil {
    * (direct and transitive inputs). The list is ordered from leaves to their dependents; for any
    * node all its direct and transitive inputs are preceding it in the list.
    *
-   * <p>Function does BFS starting from the NinjaTarget, records the order of visiting, and reverses
-   * the result. The nodes that were already visited in previous iterations are skipped, because
-   * they have already been added to the resulting aggregated list.
+   * <p>Function does DFS starting from the NinjaTarget, with two phases: in initial processing: 1)
+   * if the target was already computed, nothing happens 2) the target is checked for cycle and
+   * marked in cycleProtection set, its phony inputs are queued (put in the beginning of the queue)
+   * for initial processing 3) the target is queued after its inputs for post-processing in
+   * post-processing, the target is recorded into resulting list; all its inputs should have been
+   * already written to that list on the previous steps
    */
   private static List<NinjaTarget> topoOrderSubGraph(
       ImmutableSortedMap<PathFragment, NinjaTarget> phonyTargets,
-      SortedSet<NinjaTarget> alreadyVisited,
-      NinjaTarget target) {
+      Set<NinjaTarget> alreadyVisited,
+      NinjaTarget target)
+      throws GenericParsingException {
+    Set<NinjaTarget> cycleProtection = Sets.newHashSet();
     List<NinjaTarget> fragment = Lists.newArrayList();
-    ArrayDeque<NinjaTarget> queue = new ArrayDeque<>();
-    queue.add(target);
+    ArrayDeque<Pair<NinjaTarget, Boolean>> queue = new ArrayDeque<>();
+    queue.add(Pair.of(target, true));
     while (!queue.isEmpty()) {
-      NinjaTarget currentTarget = queue.remove();
-      if (alreadyVisited.add(currentTarget)) {
-        // If not visited, put all phony inputs into the queue.
-        fragment.add(currentTarget);
+      Pair<NinjaTarget, Boolean> pair = queue.remove();
+      NinjaTarget currentTarget = pair.getFirst();
+      if (pair.getSecond()) {
+        // Initial processing: checking all the phony inputs of the current target.
+        if (alreadyVisited.contains(currentTarget)) {
+          continue;
+        }
+        if (!cycleProtection.add(currentTarget)) {
+          throw new GenericParsingException(
+              String.format(
+                  "Detected a dependency cycle involving the phony target '%s'",
+                  Iterables.getOnlyElement(currentTarget.getAllOutputs())));
+        }
+        // Adding <phony-inputs-of-current-target> for initial processing in front of
+        // <current-target>
+        // for post-processing into the queue.
+        queue.addFirst(Pair.of(currentTarget, false));
         for (PathFragment input : currentTarget.getAllInputs()) {
           NinjaTarget phonyInput = phonyTargets.get(input);
           if (phonyInput != null) {
-            queue.add(phonyInput);
+            queue.addFirst(Pair.of(phonyInput, true));
           }
         }
+      } else {
+        // Post processing: all inputs should have been processed and added to fragment.
+        cycleProtection.remove(currentTarget);
+        alreadyVisited.add(currentTarget);
+        fragment.add(currentTarget);
       }
     }
-    // Preconditions were added after their dependants -> reverse the list to get the topo order.
-    Collections.reverse(fragment);
     return fragment;
   }
 
@@ -140,6 +156,7 @@ public class NinjaPhonyTargetsUtil {
    * tests simpler.
    */
   public interface InputArtifactCreator<T> {
+    @Nullable
     T createArtifact(PathFragment pathFragment) throws GenericParsingException;
   }
 }
