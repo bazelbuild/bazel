123f2b9ee929297cd35ea67ce3f9c4f29d84e4a3
Yun Peng <pcloudy@google.com>
2020-02-12 04:34:00 -0800

    Remove git notes usage from release process
    Previously we store release name, candidate number, cherry-picks and
    release notes info in git repo using `git notes`.  To remove this
    complexity, we remove all git notes usage in this change.
    
    Instead of using git notes, we parse release name and candidate number
    from branch name or tag name.  For cherry-picks, we rely on the git
    patch-id to find the original commit hash. And for release notes, there
    is no need to store it in git notes, because we always manually modify
    the generated release note.
    
    Closes #10741.
    
    PiperOrigin-RevId: 294639345

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -24,8 +24,9 @@ set -eu
 #     Also prepare an email for announcing the release.
 
 # Load common.sh
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "$(dirname ${SCRIPT_DIR})/release/common.sh"
+BUILD_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$(dirname ${BUILD_SCRIPT_DIR})/release/common.sh"
+source "$(dirname ${BUILD_SCRIPT_DIR})/release/relnotes.sh"
 
 if ! command -v gsutil &>/dev/null; then
   echo "Required tool 'gsutil' not found. Please install it:"
@@ -100,14 +101,14 @@ function generate_email() {
         "%url%" "$(generate_from_template "${RELEASE_CANDIDATE_URL}" "${args[@]}")"
     )
     generate_from_template \
-        "$(cat "${SCRIPT_DIR}/rc_email.txt")" \
+        "$(cat "${BUILD_SCRIPT_DIR}/rc_email.txt")" \
         "${args[@]}"
   elif [ -n "${release_name}" ]; then
     args+=(
         "%url%" "$(generate_from_template "${RELEASE_URL}" "${args[@]}")"
     )
     generate_from_template \
-        "$(cat "${SCRIPT_DIR}/release_email.txt")" "${args[@]}"
+        "$(cat "${BUILD_SCRIPT_DIR}/release_email.txt")" "${args[@]}"
   fi
 }
 
