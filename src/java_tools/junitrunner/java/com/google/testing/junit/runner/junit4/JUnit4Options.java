08666adc4d3902e56adb45b467c0333cf059dbfe
Googler <noreply@google.com>
2020-02-27 13:59:52 -0800

    Add TESTBRIDGE_TEST_RUNNER_FAIL_FAST to JUnit4
     - Suppport Bazel protocol for stopping test runner execution upon first failure.
     - Reduces test failure result latency by returning the test action as soon as it fails
     - Implemented using TESTBRIDGE env variable (similar to --testonly flag)
     - Important behavior change in StoppedUserException handling: no longer propagates the exception. This is done so that tests still generates test.xml output
    
      Bazel flag: --test_runner_fail_fast
      Environment variable: TESTBRIDGE_TEST_RUNNER_FAIL_FAST=1
      Change on test runners: Stop and return upon first test failure if TESTBRIDGE_TEST_FAIL_FAST=1
    
    PiperOrigin-RevId: 297681623

--- src/java_tools/junitrunner/java/com/google/testing/junit/runner/junit4/JUnit4Options.java
+++ src/java_tools/junitrunner/java/com/google/testing/junit/runner/junit4/JUnit4Options.java
@@ -36,6 +36,9 @@ class JUnit4Options {
   // This gets passed in by the build system.
   private static final String TESTBRIDGE_TEST_ONLY = "TESTBRIDGE_TEST_ONLY";
 
+  // This gets passed in by the build system.
+  private static final String TESTBRIDGE_TEST_RUNNER_FAIL_FAST = "TESTBRIDGE_TEST_RUNNER_FAIL_FAST";
+
   /**
    * Parses the given array of arguments and returns a JUnit4Options
    * object representing the parsed arguments.
@@ -73,22 +76,37 @@ class JUnit4Options {
     if (testFilter != null && optionsMap.get(TEST_INCLUDE_FILTER_OPTION) == null) {
       optionsMap.put(TEST_INCLUDE_FILTER_OPTION, testFilter);
     }
-
-    return new JUnit4Options(optionsMap.get(TEST_INCLUDE_FILTER_OPTION),
-                             optionsMap.get(TEST_EXCLUDE_FILTER_OPTION),
-                             unparsedArgs.toArray(new String[0]));
+    boolean testRunnerFailFast = "1".equals(envVars.get(TESTBRIDGE_TEST_RUNNER_FAIL_FAST));
+    return new JUnit4Options(
+        testRunnerFailFast,
+        optionsMap.get(TEST_INCLUDE_FILTER_OPTION),
+        optionsMap.get(TEST_EXCLUDE_FILTER_OPTION),
+        unparsedArgs.toArray(new String[0]));
   }
 
+  private final boolean testRunnerFailFast;
   private final String testIncludeFilter;
   private final String testExcludeFilter;
   private final String[] unparsedArgs;
 
   // VisibleForTesting
-  JUnit4Options(@Nullable String testIncludeFilter, @Nullable String testExcludeFilter,
-                String[] unparsedArgs) {
+  JUnit4Options(
+      boolean testRunnerFailFast,
+      @Nullable String testIncludeFilter,
+      @Nullable String testExcludeFilter,
+      String[] unparsedArgs) {
+    this.testRunnerFailFast = testRunnerFailFast;
     this.testIncludeFilter = testIncludeFilter;
     this.testExcludeFilter = testExcludeFilter;
     this.unparsedArgs = unparsedArgs;
+  }
+
+  /**
+   * Returns the value of the {@code test_runner_fail_fast} option, or <code>false<code/> if
+   * it was not specified.
+   */
+  boolean getTestRunnerFailFast() {
+    return testRunnerFailFast;
   }
 
   /**
