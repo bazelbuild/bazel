d5352f851b7e11d2c3738214f67a91029bbfe284
Jingwen Chen <jingwen@google.com>
2020-03-02 10:23:52 -0800

    Ninja integration: parse "pool" declarations and identifiers in rules
    This adds a new parser to parse "pool" declarations and "pool" identifiers in Ninja rules, and adds them into NinjaScope. We currently don't do anything special with these yet, and are just doing enough so that we parse Ninja files correctly.
    
    Without this, the following will fail:
    
    ```ninja
    rule some_name
        pool = some_pool
        command = echo hello
    ```
    
    ```starlark
    ninja_graph(
       name = "simple",
       main = "simple.ninja",
       ninja_srcs = [
           "simple.ninja",
       ],
       output_root = "out",
    )
    ```
    
    ```
    ERROR: in ninja_graph rule //:simple: Expected [IDENTIFIER], but got pool in fragment:
    rule some_name
        pool = some_pool
        command = echo hello
    
    ERROR: Analysis of target '//:simple' failed; build aborted: Analysis of target '//:simple' failed; build aborted
    FAILED: Build did NOT complete successfully (6 packages loaded, 11 targets configured)
    ```
    
    Closes #10868.
    
    PiperOrigin-RevId: 298385134

--- src/main/java/com/google/devtools/build/lib/bazel/rules/ninja/parser/NinjaParser.java
+++ src/main/java/com/google/devtools/build/lib/bazel/rules/ninja/parser/NinjaParser.java
@@ -75,11 +75,9 @@ public class NinjaParser implements DeclarationConsumer {
         NinjaRule rule = parser.parseNinjaRule();
         parseResult.addRule(declarationStart, rule);
         break;
-      case ERROR:
-        throw new GenericParsingException(lexer.getError());
-      case ZERO:
-      case EOF:
-        return;
+      case POOL:
+        parseResult.addPool(declarationStart, parser.parseNinjaPool());
+        break;
       case INCLUDE:
         NinjaVariableValue includeStatement = parser.parseIncludeStatement();
         NinjaPromise<NinjaFileParseResult> includeFuture =
@@ -113,11 +111,30 @@ public class NinjaParser implements DeclarationConsumer {
         parseResult.addTarget(targetFragment);
         break;
       case DEFAULT:
-      case POOL:
         // Do nothing.
         break;
-      default:
-        throw new UnsupportedOperationException("Unknown type of Ninja token.");
+      case ZERO:
+      case EOF:
+        return;
+      case COLON:
+      case EQUALS:
+      case ESCAPED_TEXT:
+      case INDENT:
+      case NEWLINE:
+      case PIPE:
+      case PIPE2:
+      case TEXT:
+      case VARIABLE:
+        throw new UnsupportedOperationException(
+            "Unsupported type of Ninja lexeme for the parser at this state: "
+                + token.name()
+                + ". These are unexpected lexemes at this state for the parser, and running into a "
+                + "token of this lexeme hints that the parser did not progress the lexer to a "
+                + "valid state during a previous parsing step.");
+      case ERROR:
+        throw new GenericParsingException(lexer.getError());
+        // Omit default case on purpose. Explicitly specify *all* NinjaToken enum cases above or the
+        // compilation will fail.
     }
   }
 }
