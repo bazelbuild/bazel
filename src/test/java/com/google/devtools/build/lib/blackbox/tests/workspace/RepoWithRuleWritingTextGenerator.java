33444dba095f4eabb82bd7a46fa5f92e7e979b22
Tony Aiuto <aiuto@google.com>
2020-03-12 23:45:18 -0400

    Got it down to two tests failing.
    //src/test/java/com/google/devtools/build/lib/blackbox/tests/workspace:GitRepositoryBlackBoxTest FAILED in 50.5s
    //src/test/java/com/google/devtools/build/lib/blackbox/tests/workspace:WorkspaceBlackBoxTest FAILED in 68.7s
    
    The entire test scheme is wonky, in that it depends on pkg_tar in the first place.
    Something is going wrong with the indirect loading of the rules via
    RepoWithRuleWritingTextGenerator

--- src/test/java/com/google/devtools/build/lib/blackbox/tests/workspace/RepoWithRuleWritingTextGenerator.java
+++ src/test/java/com/google/devtools/build/lib/blackbox/tests/workspace/RepoWithRuleWritingTextGenerator.java
@@ -124,30 +124,40 @@ public class RepoWithRuleWritingTextGenerator {
    * @throws IOException if was not able to create or write to files
    */
   Path setupRepository() throws IOException {
-    Path workspace = PathUtils.writeFileInDir(root, "WORKSPACE",
-        "load(\"@bazel_tools//tools/build_defs/repo:http.bzl\", \"http_archive\")",
-        "http_archive(",
-        "   name = \"rules_pkg\",",
-        "   sha256 = \"5bdc04987af79bd27bc5b00fe30f59a858f77ffa0bd2d8143d5b31ad8b1bd71c\",",
-        "   urls = [",
-        "       \"https://mirror.bazel.build/github.com/bazelbuild/rules_pkg/rules_pkg-0.2.0.tar.gz\",",
-        "       \"https://github.com/bazelbuild/rules_pkg/releases/download/0.2.0/rules_pkg-0.2.0.tar.gz\",",
-        "   ],",
-        ")",
-        "load(\"@rules_pkg//:deps.bzl\", \"rules_pkg_dependencies\")",
-        "rules_pkg_dependencies()"
-        );
+    Path workspace = generateWorkspaceFile();
     PathUtils.writeFileInDir(root, HELPER_FILE, WRITE_TEXT_TO_FILE);
     if (generateBuildFile) {
       PathUtils.writeFileInDir(
           root,
           "BUILD",
-          "load(\"@rules_pkg//:pkg.bzl\", \"pkg_tar\")",
+          "load('@rules_pkg//:pkg.bzl', 'pkg_tar')",
           loadRule(""),
           callRule(target, outFile, outputText),
-          String.format("pkg_tar(name = \"%s\", srcs = glob([\"*\"]),)", getPkgTarTarget()));
+          String.format("pkg_tar(name = '%s', srcs = glob(['*']),)", getPkgTarTarget()));
     }
     return workspace.getParent();
+  }
+
+  /**
+   * Generates the minimal WORKSPACE file for the associated BUILD file.
+   */
+  Path generateWorkspaceFile() throws IOException {
+    if (generateBuildFile) {
+      return PathUtils.writeFileInDir(root, "WORKSPACE",
+          "load('@bazel_tools//tools/build_defs/repo:http.bzl', 'http_archive')",
+          "http_archive(",
+          "   name = 'rules_pkg',",
+          "   sha256 = '352c090cc3d3f9a6b4e676cf42a6047c16824959b438895a76c2989c6d7c246a',",
+          "   urls = [",
+          "       'https://mirror.bazel.build/github.com/bazelbuild/rules_pkg/releases/download/0.2.5/rules_pkg-0.2.5.tar.gz',",
+          "       'https://github.com/bazelbuild/rules_pkg/releases/download/0.2.5/rules_pkg-0.2.5.tar.gz',",
+          "   ],",
+          ")",
+          "load('@rules_pkg//:deps.bzl', 'rules_pkg_dependencies')",
+          "rules_pkg_dependencies()"
+          );
+    }
+    return PathUtils.writeFileInDir(root, "WORKSPACE");
   }
 
   /**
