d9c7cac7d5d7bb083490f987d096291daee378b5
michajlo <michajlo@google.com>
2020-03-27 08:53:52 -0700

    Replace MoreAsserts.assertThrows with Assert.assertThrows
    Generated with grep, sed, and some magic.
    
    PiperOrigin-RevId: 303338366

--- src/java_tools/singlejar/javatests/com/google/devtools/build/singlejar/SingleJarTest.java
+++ src/java_tools/singlejar/javatests/com/google/devtools/build/singlejar/SingleJarTest.java
@@ -15,8 +15,8 @@
 package com.google.devtools.build.singlejar;
 
 import static com.google.common.truth.Truth.assertThat;
-import static com.google.devtools.build.lib.testutil.MoreAsserts.assertThrows;
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.Assert.assertThrows;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
