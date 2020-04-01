bazelbuild/bazel

commit b2ae211e821f9b43fd59d52c71410572096b3c24

Require non-null descriptions for MiddlemanActions
Prevent an uninformative "null for <target>" name from being presented
for MiddlemanAction prettyPrint by mandating a description not be null.
Use a descriptive name for the CcCompilationHelper middleman based on
the objc implementation, populated from CcCommon. Limit the objc
interpretation within the helper to the matching objc purposes.

Closes #10360.

PiperOrigin-RevId: 284967632

src/main/java/com/google/devtools/build/lib/rules/cpp/CcCommon.java
@@ -766,6 +766,12 @@ public final class CcCommon {
         virtualToOriginalHeaders);
   }
 
+  public String getPurpose(CppSemantics semantics) {
+    return semantics.getClass().getSimpleName()
+        + "_build_arch_"
+        + ruleContext.getConfiguration().getMnemonic();
+  }
+
   public static ImmutableList<String> getCoverageFeatures(CppConfiguration cppConfiguration) {
     ImmutableList.Builder<String> coverageFeatures = ImmutableList.builder();
     if (cppConfiguration.collectCodeCoverage()) {

src/main/java/com/google/devtools/build/lib/rules/cpp/CcModule.java
@@ -1556,6 +1556,7 @@ public abstract class CcModule
       throw new EvalException(location, "Either PIC or no PIC actions have to be created.");
     }
 
+    CcCommon common = new CcCommon(actions.getRuleContext());
     CcCompilationHelper helper =
         new CcCompilationHelper(
                 actions.asActionRegistry(location, actions),
@@ -1597,7 +1598,8 @@ public abstract class CcModule
             .addAdditionalCompilationInputs(headersForClifDoNotUseThisParam)
             .addAdditionalCompilationInputs(
                 additionalInputs.getContents(Artifact.class, "additional_inputs"))
-            .addAditionalIncludeScanningRoots(headersForClifDoNotUseThisParam);
+            .addAditionalIncludeScanningRoots(headersForClifDoNotUseThisParam)
+            .setPurpose(common.getPurpose(getSemantics()));
     if (disallowNopicOutputs) {
       helper.setGenerateNoPicAction(false);
     }

src/main/java/com/google/devtools/build/lib/actions/MiddlemanAction.java
@@ -46,6 +46,7 @@ public final class MiddlemanAction extends AbstractAction {
     super(owner, inputs, outputs);
     Preconditions.checkNotNull(middlemanType);
     Preconditions.checkArgument(Iterables.getOnlyElement(outputs).isMiddlemanArtifact(), outputs);
+    Preconditions.checkNotNull(description);
     this.description = description;
     this.middlemanType = middlemanType;
   }

src/main/java/com/google/devtools/build/lib/rules/cpp/CcCompilationHelper.java
@@ -260,7 +260,7 @@ public final class CcCompilationHelper {
   private final CcToolchainProvider ccToolchain;
   private final FdoContext fdoContext;
   private boolean generateModuleMap = true;
-  private String purpose = null;
+  private String purpose = "cc_compilation_middleman";
   private boolean generateNoPicAction;
   private boolean generatePicAction;
   private boolean isCodeCoverageEnabled = true;
@@ -344,6 +344,7 @@ public final class CcCompilationHelper {
     setLooseIncludeDirs(common.getLooseIncludeDirs());
     addSystemIncludeDirs(common.getSystemIncludeDirs());
     setCoptsFilter(common.getCoptsFilter());
+    setPurpose(common.getPurpose(semantics));
     return this;
   }
 
@@ -1062,8 +1063,8 @@ public final class CcCompilationHelper {
    * @param purpose must be a string which is suitable for use as a filename. A single rule may have
    *     many middlemen with distinct purposes.
    */
-  public CcCompilationHelper setPurpose(@Nullable String purpose) {
-    this.purpose = purpose;
+  public CcCompilationHelper setPurpose(String purpose) {
+    this.purpose = Preconditions.checkNotNull(purpose);
     return this;
   }
 
@@ -1277,7 +1278,8 @@ public final class CcCompilationHelper {
     String outputNamePrefixDir = null;
     // purpose is only used by objc rules, it ends with either "_non_objc_arc" or "_objc_arc".
     // Here we use it to distinguish arc and non-arc compilation.
-    if (purpose != null) {
+    Preconditions.checkNotNull(purpose);
+    if (purpose.endsWith("_objc_arc")) {
       outputNamePrefixDir = purpose.endsWith("_non_objc_arc") ? "non_arc" : "arc";
     }
     outputNameMap = calculateOutputNameMapByType(compilationUnitSources, outputNamePrefixDir);

src/main/java/com/google/devtools/build/lib/rules/cpp/CcImport.java
@@ -167,6 +167,7 @@ public abstract class CcImport implements RuleConfiguredTargetFactory {
             .addPublicHeaders(common.getHeaders())
             .setHeadersCheckingMode(HeadersCheckingMode.STRICT)
             .setCodeCoverageEnabled(CcCompilationHelper.isCodeCoverageEnabled(ruleContext))
+            .setPurpose(common.getPurpose(semantics))
             .compile();
 
     Map<String, NestedSet<Artifact>> outputGroups =

src/main/java/com/google/devtools/build/lib/rules/cpp/proto/CcProtoAspect.java
@@ -303,6 +303,7 @@ public abstract class CcProtoAspect extends NativeAspectClass implements Configu
     private CcCompilationHelper initializeCompilationHelper(
         FeatureConfiguration featureConfiguration, List<TransitiveInfoCollection> deps)
         throws InterruptedException {
+      CcCommon common = new CcCommon(ruleContext);
       CcToolchainProvider toolchain = ccToolchain(ruleContext);
       CcCompilationHelper helper =
           new CcCompilationHelper(
@@ -318,7 +319,8 @@ public abstract class CcProtoAspect extends NativeAspectClass implements Configu
                       ruleContext.getRule(), ruleContext.isAllowTagsPropagation()))
               .addCcCompilationContexts(CppHelper.getCompilationContextsFromDeps(deps))
               .addCcCompilationContexts(
-                  ImmutableList.of(CcCompilationHelper.getStlCcCompilationContext(ruleContext)));
+                  ImmutableList.of(CcCompilationHelper.getStlCcCompilationContext(ruleContext)))
+              .setPurpose(common.getPurpose(cppSemantics));
       // Don't instrument the generated C++ files even when --collect_code_coverage is set.
       helper.setCodeCoverageEnabled(false);
 

src/main/java/com/google/devtools/build/lib/rules/objc/CompilationSupport.java
@@ -530,7 +530,7 @@ public class CompilationSupport {
     return new Pair<>(compilationOutputsBuilder.build(), ImmutableMap.copyOf(mergedOutputGroups));
   }
 
-  private ObjcCppSemantics createObjcCppSemantics(
+  ObjcCppSemantics createObjcCppSemantics(
       ObjcProvider objcProvider, Collection<Artifact> privateHdrs, Artifact pchHdr) {
     Iterable<Artifact> extraInputs = getExtraIncludeProcessingInputs(privateHdrs, pchHdr);
     return new ObjcCppSemantics(

src/main/java/com/google/devtools/build/lib/rules/objc/ObjcLibrary.java
@@ -90,6 +90,7 @@ public class ObjcLibrary implements RuleConfiguredTargetFactory {
     J2ObjcEntryClassProvider j2ObjcEntryClassProvider = new J2ObjcEntryClassProvider.Builder()
       .addTransitive(ruleContext.getPrerequisites("deps", Mode.TARGET,
           J2ObjcEntryClassProvider.class)).build();
+    ObjcProvider objcProvider = common.getObjcProvider();
     CcCompilationContext ccCompilationContext =
         CcCompilationContext.builder(
                 ruleContext, ruleContext.getConfiguration(), ruleContext.getLabel())
@@ -97,12 +98,16 @@ public class ObjcLibrary implements RuleConfiguredTargetFactory {
                 CompilationAttributes.Builder.fromRuleContext(ruleContext).build().hdrs().toList())
             .addTextualHdrs(common.getTextualHdrs())
             .addDeclaredIncludeSrcs(common.getTextualHdrs())
+            .setPurpose(
+                compilationSupport
+                    .createObjcCppSemantics(
+                        objcProvider, /* privateHdrs= */ ImmutableList.of(), /* pchHdr= */ null)
+                    .getPurpose())
             .build();
 
     CcLinkingContext ccLinkingContext =
         buildCcLinkingContext(ruleContext.getLabel(), common, ruleContext.getSymbolGenerator());
 
-    ObjcProvider objcProvider = common.getObjcProvider();
     return ObjcRuleClasses.ruleConfiguredTarget(ruleContext, filesToBuild.build())
         .addNativeDeclaredProvider(objcProvider)
         .addSkylarkTransitiveInfo(ObjcProvider.SKYLARK_NAME, objcProvider)

commit f6d205c7ac2ba2d59cdb45e3a2a2908b66ca871b

C++: Fix null pointer exception introduced by https://github.com/bazelbuild/bazel/commit/b2ae211e821f9b43fd59d52c71410572096b3c24.
Fixes #10897.

RELNOTES:none
PiperOrigin-RevId: 304148267

src/main/java/com/google/devtools/build/lib/rules/cpp/CcCommon.java
@@ -162,12 +162,17 @@ public final class CcCommon {
   private final FdoContext fdoContext;
 
   public CcCommon(RuleContext ruleContext) {
-    this.ruleContext = ruleContext;
-    this.ccToolchain =
+    this(
+        ruleContext,
         Preconditions.checkNotNull(
-            CppHelper.getToolchainUsingDefaultCcToolchainAttribute(ruleContext));
+            CppHelper.getToolchainUsingDefaultCcToolchainAttribute(ruleContext)));
+  }
+
+  public CcCommon(RuleContext ruleContext, CcToolchainProvider ccToolchain) {
+    this.ruleContext = ruleContext;
     this.cppConfiguration = ruleContext.getFragment(CppConfiguration.class);
     this.fdoContext = ccToolchain.getFdoContext();
+    this.ccToolchain = ccToolchain;
   }
 
   /**

src/main/java/com/google/devtools/build/lib/rules/cpp/CcModule.java
@@ -1653,7 +1653,7 @@ public abstract class CcModule
       throw Starlark.errorf("Either PIC or no PIC actions have to be created.");
     }
 
-    CcCommon common = new CcCommon(actions.getRuleContext());
+    CcCommon common = new CcCommon(actions.getRuleContext(), ccToolchainProvider);
     CcCompilationHelper helper =
         new CcCompilationHelper(
                 actions.asActionRegistry(actions),

src/test/java/com/google/devtools/build/lib/rules/cpp/SkylarkCcCommonTest.java
@@ -5995,7 +5995,7 @@ public class SkylarkCcCommonTest extends BuildViewTestCase {
         "    for dep in ctx.attr._deps:",
         "        dep_compilation_contexts.append(dep[CcInfo].compilation_context)",
         "        dep_linking_contexts.append(dep[CcInfo].linking_context)",
-        "    toolchain = ctx.attr._cc_toolchain[cc_common.CcToolchainInfo]",
+        "    toolchain = ctx.attr._my_cc_toolchain[cc_common.CcToolchainInfo]",
         "    feature_configuration = cc_common.configure_features(",
         "        ctx = ctx,",
         "        cc_toolchain=toolchain,",
@@ -6047,7 +6047,7 @@ public class SkylarkCcCommonTest extends BuildViewTestCase {
             + " default=['//foo:extra_compiler_input']),",
         "      '_deps': attr.label_list(default=['//foo:dep1', '//foo:dep2']),",
         "      'aspect_deps': attr.label_list(aspects=[_cc_aspect]),",
-        "      '_cc_toolchain': attr.label(default =",
+        "      '_my_cc_toolchain': attr.label(default =",
         "          configuration_field(fragment = 'cpp', name = 'cc_toolchain'))",
         "    },",
         fragments,