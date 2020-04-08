68075bf361f897984856187c0b91711386a47eaa
jcater <jcater@google.com>
2020-04-08 09:56:13 -0700

    Split AspectCompletor and TargetCompletor into new files.
    This removes a dependency cycle between CompletionFunction and AspectCompletionValue.
    
    PiperOrigin-RevId: 305498258

--- src/main/java/com/google/devtools/build/lib/skyframe/SkyframeExecutor.java
+++ src/main/java/com/google/devtools/build/lib/skyframe/SkyframeExecutor.java
@@ -568,11 +568,11 @@ public abstract class SkyframeExecutor implements WalkableGraphFactory {
     map.put(SkyFunctions.EXTERNAL_PACKAGE, new ExternalPackageFunction());
     map.put(
         SkyFunctions.TARGET_COMPLETION,
-        CompletionFunction.targetCompletionFunction(
+        TargetCompletor.targetCompletionFunction(
             pathResolverFactory, skyframeActionExecutor::getExecRoot));
     map.put(
         SkyFunctions.ASPECT_COMPLETION,
-        CompletionFunction.aspectCompletionFunction(
+        AspectCompletor.aspectCompletionFunction(
             pathResolverFactory, skyframeActionExecutor::getExecRoot));
     map.put(SkyFunctions.TEST_COMPLETION, new TestCompletionFunction());
     map.put(
