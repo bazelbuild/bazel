6475cd235f0e3d1fb147b07be455c1cd98528ab3
Jingwen Chen <jingwen@google.com>
2020-04-08 15:09:17 +0800

    Fix //src/main/java/com/google/devtools/build/lib:gen_command-line-reference and add regression tests.
    09924273efc7050a2e342cd89b823a619ca073f8 adds a new required
    --failure_detail_out flag, which broke the docgen genrule because its
    invoking the Bazel server directly without the flag.

---
+++ src/test/java/com/google/devtools/build/docgen/generated_docs_build_test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# A smoke file test for generated doc files.
+
+set -euo pipefail
+
+# --- begin runfiles.bash initialization v2 ---
+# Copy-pasted from the Bazel Bash runfiles library v2.
+set -uo pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+    source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+    source "$0.runfiles/$f" 2>/dev/null || \
+    source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+    source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+    { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
+# --- end runfiles.bash initialization v2 ---
+
+EXPECTED_ZIPS="build-encyclopedia.zip skylark-library.zip"
+
+for out in $EXPECTED_ZIPS; do
+    filepath="$(rlocation io_bazel/src/main/java/com/google/devtools/build/lib/$out)"
+    # Test that the file has a corrected ZIP structure.
+    zipinfo "$filepath" 1>/dev/null
+done
+
+COMMAND_LINE_REF_FILE="$(rlocation io_bazel/src/main/java/com/google/devtools/build/lib/command-line-reference.html)"
+# Test that the file contains (at least) the title string.
+grep "Command-Line Reference" "$COMMAND_LINE_REF_FILE" 1>/dev/null
