d177a2b4035dfb0d6932a9b0519016eb106f76cb
Googler <noreply@google.com>
2020-04-10 17:42:08 -0700

    Generate one Adapter Class per Desugar-shadowed API Method.
    #desugar #new-android-api-support #desugar-shadowed-api
    
    - This is to resolve the following one-version violation case:
      In Jar A, there is a reference to ClassT#MethodX,
      In Jar B, there is a reference to ClassT#MethodY.
      Before this change, the desugar tool generates two versions of ClassTAdapter classes are one that contains adapter for MethodX and the other for MethodY
    - In this change, the desugar tool mangles a hash of the desugar-shadowed API into the name of an adapter class. Consequently, each adapter is a single method class.
    - Future plans include looking into merge these adapter classes
    
    PiperOrigin-RevId: 305976378

--- src/test/java/com/google/devtools/build/android/desugar/corelibadapter/ShadowedAndroidApiAdapterTest.java
+++ src/test/java/com/google/devtools/build/android/desugar/corelibadapter/ShadowedAndroidApiAdapterTest.java
@@ -16,11 +16,10 @@
 
 package com.google.devtools.build.android.desugar.corelibadapter;
 
-import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.truth.Truth.assertThat;
+import static com.google.devtools.build.android.desugar.testing.junit.DesugarTestHelpers.findMethodInvocationSites;
 import static com.google.devtools.build.android.desugar.testing.junit.DesugarTestHelpers.getRuntimePathsFromJvmFlag;
 
-import com.google.common.collect.ImmutableList;
 import com.google.devtools.build.android.desugar.testing.junit.AsmNode;
 import com.google.devtools.build.android.desugar.testing.junit.DesugarRule;
 import com.google.devtools.build.android.desugar.testing.junit.DesugarRunner;
@@ -31,13 +30,10 @@ import com.google.devtools.build.android.desugar.testing.junit.ParameterValueSou
 import com.google.devtools.build.android.desugar.testing.junit.RuntimeMethodHandle;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
-import java.util.Arrays;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
 
 /** Tests for accessing private constructors from another class within a nest. */
@@ -277,16 +273,18 @@ public class ShadowedAndroidApiAdapterTest {
               round = 1)
           MethodNode method) {
     assertThat(
-            findMethodInvocations(
+            findMethodInvocationSites(
                 method,
                 "com/google/devtools/build/android/desugar/typeadapter/javadesugar/testing/CuboidConverter",
-                "from"))
+                "from",
+                ".*"))
         .isEmpty();
     assertThat(
-            findMethodInvocations(
+            findMethodInvocationSites(
                 method,
                 "com/google/devtools/build/android/desugar/typeadapter/javadesugar/testing/CuboidConverter",
-                "to"))
+                "to",
+                ".*"))
         .isNotEmpty();
   }
 
@@ -298,22 +296,25 @@ public class ShadowedAndroidApiAdapterTest {
               round = 1)
           MethodNode method) {
     assertThat(
-            findMethodInvocations(
+            findMethodInvocationSites(
                 method,
-                "com/google/devtools/build/android/desugar/typeadapter/android/testing/CuboidInflaterAdapter",
-                "inflateStatic"))
+                "com/google/devtools/build/android/desugar/typeadapter/android/testing/CuboidInflater\\$.*\\$Adapter",
+                "inflateStatic",
+                ".*"))
         .isNotEmpty();
     assertThat(
-            findMethodInvocations(
+            findMethodInvocationSites(
                 method,
                 "com/google/devtools/build/android/desugar/typeadapter/javadesugar/testing/CuboidConverter",
-                "from"))
+                "from",
+                ".*"))
         .isEmpty();
     assertThat(
-            findMethodInvocations(
+            findMethodInvocationSites(
                 method,
                 "com/google/devtools/build/android/desugar/typeadapter/javadesugar/testing/CuboidConverter",
-                "to"))
+                "to",
+                ".*"))
         .isEmpty();
   }
 
@@ -325,33 +326,25 @@ public class ShadowedAndroidApiAdapterTest {
               round = 1)
           MethodNode method) {
     assertThat(
-            findMethodInvocations(
+            findMethodInvocationSites(
                 method,
-                "com/google/devtools/build/android/desugar/typeadapter/android/testing/CuboidInflaterAdapter",
-                "inflateInstance"))
+                "com/google/devtools/build/android/desugar/typeadapter/android/testing/CuboidInflater\\$.*\\$Adapter",
+                "inflateInstance",
+                ".*"))
         .isNotEmpty();
     assertThat(
-            findMethodInvocations(
+            findMethodInvocationSites(
                 method,
                 "com/google/devtools/build/android/desugar/typeadapter/javadesugar/testing/CuboidConverter",
-                "from"))
+                "from",
+                ".*"))
         .isEmpty();
     assertThat(
-            findMethodInvocations(
+            findMethodInvocationSites(
                 method,
                 "com/google/devtools/build/android/desugar/typeadapter/javadesugar/testing/CuboidConverter",
-                "to"))
+                "to",
+                ".*"))
         .isEmpty();
-  }
-
-  private static ImmutableList<MethodInsnNode> findMethodInvocations(
-      MethodNode enclosingMethod, String invokedMethodOwner, String invokedMethodName) {
-    AbstractInsnNode[] instructions = enclosingMethod.instructions.toArray();
-    return Arrays.stream(instructions)
-        .filter(node -> node.getType() == AbstractInsnNode.METHOD_INSN)
-        .map(node -> (MethodInsnNode) node)
-        .filter(
-            node -> invokedMethodOwner.equals(node.owner) && invokedMethodName.equals(node.name))
-        .collect(toImmutableList());
   }
 }
