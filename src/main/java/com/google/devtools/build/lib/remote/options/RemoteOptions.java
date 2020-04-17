4c6d0fc49b4183044cd37c87b60619b9cf7112d6
Mostyn Bramley-Moore <mostyn@antipode.se>
2020-04-17 09:02:19 +0200

    Tweak the --experimental_remote_downloader help text
    * Use the official name ("remote asset API") so it's easier to find.
    * Add a link to the API definition.
    * Drop a redundant sentence about how to disable TLS.

--- src/main/java/com/google/devtools/build/lib/remote/options/RemoteOptions.java
+++ src/main/java/com/google/devtools/build/lib/remote/options/RemoteOptions.java
@@ -91,9 +91,10 @@ public final class RemoteOptions extends OptionsBase {
       documentationCategory = OptionDocumentationCategory.REMOTE,
       effectTags = {OptionEffectTag.UNKNOWN},
       help =
-          "A URI of a remote downloader endpoint. The supported schemas are grpc and grpcs"
-              + " (grpc with TLS enabled). If no schema is provided bazel will default to grpcs."
-              + " Specify grpc:// schema to disable TLS.")
+          "A Remote Asset API endpoint URI, to be used as a remote download proxy."
+              + " The supported schemas are grpc and grpcs (grpc with TLS enabled)."
+              + " If no schema is provided bazel will default to grpcs."
+              + " See: https://github.com/bazelbuild/remote-apis/blob/master/build/bazel/remote/asset/v1/remote_asset.proto")
   public String remoteDownloader;
 
   @Option(
