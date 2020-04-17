e7b0f04584ef73372eb377c28e21e09d7a6ff694
Mostyn Bramley-Moore <mostyn@antipode.se>
2020-04-17 09:05:15 +0200

    Drive-by doc cleanup: add missing space between sentences

--- src/main/java/com/google/devtools/build/lib/remote/options/RemoteOptions.java
+++ src/main/java/com/google/devtools/build/lib/remote/options/RemoteOptions.java
@@ -81,7 +81,7 @@ public final class RemoteOptions extends OptionsBase {
       help =
           "A URI of a caching endpoint. The supported schemas are http, https, grpc and grpcs"
               + " (grpc with TLS enabled). If no schema is provided bazel will default to grpcs."
-              + " Specify grpc:// or http:// schema to disable TLS.See"
+              + " Specify grpc:// or http:// schema to disable TLS. See"
               + " https://docs.bazel.build/versions/master/remote-caching.html")
   public String remoteCache;
 
