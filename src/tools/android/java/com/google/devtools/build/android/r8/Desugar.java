b5f647313e64dd827bc3dbce89963922d4b665ef
sgjesse <sgjesse@google.com>
2020-05-11 06:23:15 -0700

    D8 desugar: Handle overlapping bootclasspath and classpath
    Also filter out known platform classes from classpath.
    
    RELNOTES: None.
    PiperOrigin-RevId: 310899100

--- src/tools/android/java/com/google/devtools/build/android/r8/Desugar.java
+++ src/tools/android/java/com/google/devtools/build/android/r8/Desugar.java
@@ -407,19 +407,23 @@ public class Desugar {
     }
   }
 
-  private void desugar(ClassFileResourceProvider classpath, Path input, Path output)
+  private void desugar(
+      List<ClassFileResourceProvider> bootclasspathProviders,
+      ClassFileResourceProvider classpath,
+      Path input,
+      Path output)
       throws CompilationFailedException {
     checkArgument(!Files.isDirectory(input), "Input must be a jar (%s is a directory)", input);
     DependencyCollector dependencyCollector = createDependencyCollector();
     OutputConsumer consumer = new OutputConsumer(output, dependencyCollector);
-    D8.run(
+    D8Command.Builder builder =
         D8Command.builder(new DesugarDiagnosticsHandler(consumer))
-            .addLibraryFiles(options.bootclasspath)
             .addClasspathResourceProvider(classpath)
             .addProgramFiles(input)
             .setMinApiLevel(options.minSdkVersion)
-            .setProgramConsumer(consumer)
-            .build());
+            .setProgramConsumer(consumer);
+    bootclasspathProviders.forEach(builder::addLibraryResourceProvider);
+    D8.run(builder.build());
   }
 
   private void desugar() throws CompilationFailedException, IOException {
@@ -429,11 +433,23 @@ public class Desugar {
     for (Path path : options.classpath) {
       classpathProvidersBuilder.add(new ArchiveClassFileProvider(path));
     }
+    ImmutableList.Builder<ClassFileResourceProvider> bootclasspathProvidersBuilder =
+        ImmutableList.builder();
+    for (Path path : options.bootclasspath) {
+      bootclasspathProvidersBuilder.add(new ArchiveClassFileProvider(path));
+    }
+    ImmutableList<ClassFileResourceProvider> bootclasspathProviders =
+        bootclasspathProvidersBuilder.build();
     OrderedClassFileResourceProvider classpathProvider =
-        new OrderedClassFileResourceProvider(classpathProvidersBuilder.build());
+        new OrderedClassFileResourceProvider(
+            bootclasspathProviders, classpathProvidersBuilder.build());
     // Desugar the input jars into the specified output jars.
     for (int i = 0; i < options.inputJars.size(); i++) {
-      desugar(classpathProvider, options.inputJars.get(i), options.outputJars.get(i));
+      desugar(
+          bootclasspathProviders,
+          classpathProvider,
+          options.inputJars.get(i),
+          options.outputJars.get(i));
     }
   }
 
--- src/tools/android/java/com/google/devtools/build/android/r8/desugar/OrderedClassFileResourceProvider.java
+++ src/tools/android/java/com/google/devtools/build/android/r8/desugar/OrderedClassFileResourceProvider.java
@@ -29,15 +29,29 @@ public class OrderedClassFileResourceProvider implements ClassFileResourceProvid
   private final Set<String> descriptors = Sets.newHashSet();
   private final Map<String, ClassFileResourceProvider> descriptorToProvider = new HashMap<>();
 
-  public OrderedClassFileResourceProvider(ImmutableList<ClassFileResourceProvider> providers) {
-    // Collect all descriptors provided and the first provider providing each.
-    for (ClassFileResourceProvider provider : providers) {
+  public OrderedClassFileResourceProvider(
+      ImmutableList<ClassFileResourceProvider> bootclasspathProviders,
+      ImmutableList<ClassFileResourceProvider> classfileProviders) {
+    final Set<String> bootclasspathDescriptors = Sets.newHashSet();
+    for (ClassFileResourceProvider provider : classfileProviders) {
+      // Collect all descriptors provided and the first provider providing each.
+      bootclasspathProviders.forEach(p -> bootclasspathDescriptors.addAll(p.getClassDescriptors()));
       for (String descriptor : provider.getClassDescriptors()) {
-        if (descriptors.add(descriptor)) {
+        // Pick first definition of classpath class and filter out platform classes
+        // from classpath if present.
+        if (!bootclasspathDescriptors.contains(descriptor)
+            && !isPlatformDescriptor(descriptor)
+            && descriptors.add(descriptor)) {
           descriptorToProvider.put(descriptor, provider);
         }
       }
     }
+  }
+
+  private static boolean isPlatformDescriptor(String descriptor) {
+    // See b/153106333.
+    return descriptor.startsWith("Landroid/car/content/")
+        && !descriptor.startsWith("Landroid/car/test/");
   }
 
   @Override
