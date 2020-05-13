a8a50cfa3279e008f2848c9ee569a1fb706c7d51
michajlo <michajlo@google.com>
2020-05-12 19:10:18 -0700

    Make the number of packages we log metrics for configurable and add tests
    Sets the default to 10, down from the original 50, which could generate a lot
    of output.
    
    This cl is mostly boilerplate in tests. On one hand I may have gone overboard,
    on the other this makes it hard to avoid adding tests if functionality becomes
    more complex, and I'd feel awfully bad if a silly mistake here caused a crash
    bug.
    
    PiperOrigin-RevId: 311247814

--- src/main/java/com/google/devtools/build/lib/packages/metrics/ExtremaPackageLoadingListener.java
+++ src/main/java/com/google/devtools/build/lib/packages/metrics/ExtremaPackageLoadingListener.java
@@ -13,94 +13,132 @@
 // limitations under the License.
 package com.google.devtools.build.lib.packages.metrics;
 
-import com.google.common.base.Joiner;
-import com.google.common.collect.ImmutableList;
-import com.google.common.flogger.GoogleLogger;
+import com.google.common.base.Preconditions;
 import com.google.devtools.build.lib.cmdline.PackageIdentifier;
 import com.google.devtools.build.lib.collect.Extrema;
 import com.google.devtools.build.lib.packages.Package;
 import com.google.devtools.build.lib.packages.PackageLoadingListener;
 import com.google.devtools.build.lib.syntax.StarlarkSemantics;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import javax.annotation.concurrent.GuardedBy;
 
 /** Tracks per-invocation extreme package loading events. */
 class ExtremaPackageLoadingListener implements PackageLoadingListener {
-  private static final GoogleLogger logger = GoogleLogger.forEnclosingClass();
+  @GuardedBy("this")
+  private int currentNumPackagesToTrack;
 
-  private static final int NUM_EXTREME_PACKAGES_TO_TRACK = 50;
+  @GuardedBy("this")
+  private Extrema<PackageIdentifierAndLong> slowestPackagesToLoad;
 
-  private static final ExtremaPackageLoadingListener instance = new ExtremaPackageLoadingListener();
+  @GuardedBy("this")
+  private Extrema<PackageIdentifierAndLong> largestPackages;
 
-  public static ExtremaPackageLoadingListener getInstance() {
-    return instance;
+  @GuardedBy("this")
+  private Extrema<PackageIdentifierAndLong> packagesWithMostTransitiveLoads;
+
+  @GuardedBy("this")
+  private Extrema<PackageIdentifierAndLong> packagesWithMostComputationSteps;
+
+  ExtremaPackageLoadingListener() {
+    this.currentNumPackagesToTrack = 0;
+    this.slowestPackagesToLoad = Extrema.max(0);
+    this.largestPackages = Extrema.max(0);
+    this.packagesWithMostTransitiveLoads = Extrema.max(0);
+    this.packagesWithMostComputationSteps = Extrema.max(0);
   }
 
-  private final Extrema<PackageIdentifierAndLong> slowestPackagesToLoad =
-      Extrema.max(NUM_EXTREME_PACKAGES_TO_TRACK);
-  private final Extrema<PackageIdentifierAndLong> largestPackages =
-      Extrema.max(NUM_EXTREME_PACKAGES_TO_TRACK);
-  private final Extrema<PackageIdentifierAndLong> packagesWithMostTransitiveLoads =
-      Extrema.max(NUM_EXTREME_PACKAGES_TO_TRACK);
-  private final Extrema<PackageIdentifierAndLong> packagesWithMostComputationSteps =
-      Extrema.max(NUM_EXTREME_PACKAGES_TO_TRACK);
-
-  private ExtremaPackageLoadingListener() {}
-
-  @Override
-  public void onLoadingCompleteAndSuccessful(
-      Package pkg, StarlarkSemantics starlarkSemantics, long loadTimeNanos) {
-    long loadTimeMillis = TimeUnit.MILLISECONDS.convert(loadTimeNanos, TimeUnit.NANOSECONDS);
-    synchronized (slowestPackagesToLoad) {
-      slowestPackagesToLoad.aggregate(
-          new PackageIdentifierAndLong(pkg.getPackageIdentifier(), loadTimeMillis));
-    }
-
-    synchronized (packagesWithMostComputationSteps) {
-      packagesWithMostComputationSteps.aggregate(
-          new PackageIdentifierAndLong(pkg.getPackageIdentifier(), pkg.getComputationSteps()));
-    }
-
-    synchronized (largestPackages) {
-      largestPackages.aggregate(
-          new PackageIdentifierAndLong(pkg.getPackageIdentifier(), pkg.getTargets().size()));
-    }
-
-    synchronized (packagesWithMostTransitiveLoads) {
-      packagesWithMostTransitiveLoads.aggregate(
-          new PackageIdentifierAndLong(
-              pkg.getPackageIdentifier(), pkg.getStarlarkFileDependencies().size()));
-    }
-  }
-
-  public void logAndResetExtrema() {
-    if (slowestPackagesToLoad.isEmpty()) {
-      // One empty means they're all empty, skip logging.
+  synchronized void setNumPackagesToTrack(int numPackagesToTrack) {
+    Preconditions.checkArgument(numPackagesToTrack >= 0, "num packages must be >= 0");
+    if (numPackagesToTrack == currentNumPackagesToTrack) {
+      // Micro-optimization to avoid turning over collections.
+      clear();
       return;
     }
-    maybeLogExtremaHelper("Slowest packages (ms)", slowestPackagesToLoad);
-    maybeLogExtremaHelper("Largest packages (num targets)", largestPackages);
-    maybeLogExtremaHelper("Packages with most computation steps", packagesWithMostComputationSteps);
-    maybeLogExtremaHelper(
-        "Packages with most transitive loads (num bzl files)", packagesWithMostTransitiveLoads);
 
+    currentNumPackagesToTrack = numPackagesToTrack;
+    this.slowestPackagesToLoad = Extrema.max(currentNumPackagesToTrack);
+    this.largestPackages = Extrema.max(currentNumPackagesToTrack);
+    this.packagesWithMostTransitiveLoads = Extrema.max(currentNumPackagesToTrack);
+    this.packagesWithMostComputationSteps = Extrema.max(currentNumPackagesToTrack);
+  }
+
+  @Override
+  public synchronized void onLoadingCompleteAndSuccessful(
+      Package pkg, StarlarkSemantics starlarkSemantics, long loadTimeNanos) {
+    if (currentNumPackagesToTrack == 0) {
+      // Micro-optimization - no need to track.
+      return;
+    }
+
+    long loadTimeMillis = TimeUnit.MILLISECONDS.convert(loadTimeNanos, TimeUnit.NANOSECONDS);
+    slowestPackagesToLoad.aggregate(
+        new PackageIdentifierAndLong(pkg.getPackageIdentifier(), loadTimeMillis));
+
+    packagesWithMostComputationSteps.aggregate(
+        new PackageIdentifierAndLong(pkg.getPackageIdentifier(), pkg.getComputationSteps()));
+
+    largestPackages.aggregate(
+        new PackageIdentifierAndLong(pkg.getPackageIdentifier(), pkg.getTargets().size()));
+
+    packagesWithMostTransitiveLoads.aggregate(
+        new PackageIdentifierAndLong(
+            pkg.getPackageIdentifier(), pkg.getStarlarkFileDependencies().size()));
+  }
+
+  synchronized TopPackages getAndResetTopPackages() {
+    TopPackages result =
+        new TopPackages(
+            slowestPackagesToLoad.getExtremeElements(),
+            packagesWithMostComputationSteps.getExtremeElements(),
+            largestPackages.getExtremeElements(),
+            packagesWithMostTransitiveLoads.getExtremeElements());
+    clear();
+    return result;
+  }
+
+  private synchronized void clear() {
     slowestPackagesToLoad.clear();
     packagesWithMostComputationSteps.clear();
     largestPackages.clear();
     packagesWithMostTransitiveLoads.clear();
   }
 
-  private static void maybeLogExtremaHelper(
-      String logLinePrefix, Extrema<PackageIdentifierAndLong> extrema) {
-    ImmutableList<PackageIdentifierAndLong> extremeElements = extrema.getExtremeElements();
-    if (!extremeElements.isEmpty()) {
-      logger.atInfo().log(
-          "%s: %s",
-          logLinePrefix,
-          Joiner.on(", ").join(extremeElements));
+  static class TopPackages {
+    private final List<PackageIdentifierAndLong> slowestPackages;
+    private final List<PackageIdentifierAndLong> packagesWithMostComputationSteps;
+    private final List<PackageIdentifierAndLong> largestPackages;
+    private final List<PackageIdentifierAndLong> packagesWithMostTransitiveLoads;
+
+    private TopPackages(
+        List<PackageIdentifierAndLong> slowestPackages,
+        List<PackageIdentifierAndLong> packagesWithMostComputationSteps,
+        List<PackageIdentifierAndLong> largestPackages,
+        List<PackageIdentifierAndLong> packagesWithMostTransitiveLoads) {
+      this.slowestPackages = slowestPackages;
+      this.packagesWithMostComputationSteps = packagesWithMostComputationSteps;
+      this.largestPackages = largestPackages;
+      this.packagesWithMostTransitiveLoads = packagesWithMostTransitiveLoads;
+    }
+
+    List<PackageIdentifierAndLong> getSlowestPackages() {
+      return slowestPackages;
+    }
+
+    List<PackageIdentifierAndLong> getPackagesWithMostComputationSteps() {
+      return packagesWithMostComputationSteps;
+    }
+
+    List<PackageIdentifierAndLong> getLargestPackages() {
+      return largestPackages;
+    }
+
+    List<PackageIdentifierAndLong> getPackagesWithMostTransitiveLoads() {
+      return packagesWithMostTransitiveLoads;
     }
   }
 
-  private static class PackageIdentifierAndLong implements Comparable<PackageIdentifierAndLong> {
+  static class PackageIdentifierAndLong implements Comparable<PackageIdentifierAndLong> {
     private final PackageIdentifier pkgId;
     private final long val;
 
--- src/main/java/com/google/devtools/build/lib/packages/metrics/PackageMetricsModule.java
+++ src/main/java/com/google/devtools/build/lib/packages/metrics/PackageMetricsModule.java
@@ -13,15 +13,50 @@
 // limitations under the License.
 package com.google.devtools.build.lib.packages.metrics;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
+import com.google.common.flogger.GoogleLogger;
 import com.google.devtools.build.lib.analysis.ConfiguredRuleClassProvider;
 import com.google.devtools.build.lib.packages.Package;
 import com.google.devtools.build.lib.packages.PackageLoadingListener;
+import com.google.devtools.build.lib.packages.metrics.ExtremaPackageLoadingListener.PackageIdentifierAndLong;
+import com.google.devtools.build.lib.packages.metrics.ExtremaPackageLoadingListener.TopPackages;
 import com.google.devtools.build.lib.runtime.BlazeModule;
+import com.google.devtools.build.lib.runtime.CommandEnvironment;
 import com.google.devtools.build.lib.vfs.FileSystem;
+import com.google.devtools.common.options.Option;
+import com.google.devtools.common.options.OptionDocumentationCategory;
+import com.google.devtools.common.options.OptionEffectTag;
+import com.google.devtools.common.options.OptionsBase;
+import java.util.List;
 import javax.annotation.Nullable;
 
 /** Provides logging for extreme package-loading events. */
 public class PackageMetricsModule extends BlazeModule {
+  private static final GoogleLogger logger = GoogleLogger.forEnclosingClass();
+
+  /** Options for {@link PackageMetricsModule}. */
+  public static class Options extends OptionsBase {
+    @Option(
+        name = "log_top_n_packages",
+        defaultValue = "10",
+        documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
+        effectTags = {OptionEffectTag.BAZEL_MONITORING},
+        help = "Configures number of packages included in top-package INFO logging, <= 0 disables.")
+    public int numberOfPackagesToTrack;
+  }
+
+  private final ExtremaPackageLoadingListener packageLoadingListener;
+
+  public PackageMetricsModule() {
+    this(new ExtremaPackageLoadingListener());
+  }
+
+  @VisibleForTesting
+  PackageMetricsModule(ExtremaPackageLoadingListener packageLoadingListener) {
+    this.packageLoadingListener = packageLoadingListener;
+  }
 
   @Nullable
   @Override
@@ -29,11 +64,36 @@ public class PackageMetricsModule extends BlazeModule {
       Package.Builder.Helper packageBuilderHelper,
       ConfiguredRuleClassProvider ruleClassProvider,
       FileSystem fs) {
-    return ExtremaPackageLoadingListener.getInstance();
+    return packageLoadingListener;
+  }
+
+  @Override
+  public Iterable<Class<? extends OptionsBase>> getCommonCommandOptions() {
+    return ImmutableList.of(Options.class);
+  }
+
+  @Override
+  public void beforeCommand(CommandEnvironment commandEnvironment) {
+    Options options = commandEnvironment.getOptions().getOptions(Options.class);
+    packageLoadingListener.setNumPackagesToTrack(Math.max(options.numberOfPackagesToTrack, 0));
   }
 
   @Override
   public void afterCommand() {
-    ExtremaPackageLoadingListener.getInstance().logAndResetExtrema();
+    TopPackages topPackages = packageLoadingListener.getAndResetTopPackages();
+    logIfNonEmpty("Slowest packages (ms)", topPackages.getSlowestPackages());
+    logIfNonEmpty("Largest packages (num targets)", topPackages.getLargestPackages());
+    logIfNonEmpty(
+        "Packages with most computation steps", topPackages.getPackagesWithMostComputationSteps());
+    logIfNonEmpty(
+        "Packages with most transitive loads (num bzl files)",
+        topPackages.getPackagesWithMostTransitiveLoads());
+  }
+
+  private static void logIfNonEmpty(
+      String logLinePrefix, List<PackageIdentifierAndLong> extremeElements) {
+    if (!extremeElements.isEmpty()) {
+      logger.atInfo().log("%s: %s", logLinePrefix, Joiner.on(", ").join(extremeElements));
+    }
   }
 }
