35aeb9d56d57b1ef474170f9ab0a2d36e8836e3a
cushon <cushon@google.com>
2020-05-18 09:09:38 -0700

    Fix suppression of AutoValueImmutableFields in BazelModuleContext
    Fixes https://github.com/bazelbuild/bazel/issues/11411
    
    PiperOrigin-RevId: 312090740

--- src/main/java/com/google/devtools/build/lib/packages/BazelModuleContext.java
+++ src/main/java/com/google/devtools/build/lib/packages/BazelModuleContext.java
@@ -31,6 +31,7 @@ public abstract class BazelModuleContext {
    * itself and all files it transitively loads.
    */
   @SuppressWarnings("AutoValueImmutableFields")
+  @AutoValue.CopyAnnotations
   public abstract byte[] bzlTransitiveDigest();
 
   /**
