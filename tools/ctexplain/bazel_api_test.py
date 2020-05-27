718c781f38aff9c93128e120842cdb6f96e32ed1
Greg Estren <gregestren@gmail.com>
2020-05-27 18:12:26 -0400

    Fill out more tests.

--- tools/ctexplain/bazel_api_test.py
+++ tools/ctexplain/bazel_api_test.py
@@ -43,7 +43,15 @@ class BazelApiTest(test_base.TestBase):
             os.linesep.join(stderr))
 
     def testTransitiveFragmentsAccuracy(self):
-        pass
+        self.ScratchFile('testapp/BUILD', [
+            'filegroup(name = "fg", srcs = ["a.file"])',
+            'filegroup(name = "ccfg", srcs = [":ccbin"])',
+            'cc_binary(name = "ccbin", srcs = ["ccbin.cc"])'
+        ])
+        (success1, stderr1, cts1)  = self._bazel_api.cquery(["//testapp:fg"])
+        self.assertNotIn("CppConfiguration", cts1[0].transitive_fragments)
+        (success2, stderr2, cts2)  = self._bazel_api.cquery(["//testapp:ccfg"])
+        self.assertIn("CppConfiguration", cts2[0].transitive_fragments)
         
     def testGetTargetConfig(self):
         self.ScratchFile('testapp/BUILD', [
@@ -69,7 +77,6 @@ class BazelApiTest(test_base.TestBase):
         ])
         query = ["//testapp:fg", "--universe_scope=//testapp:g"]
         (success, stderr, cts)  = self._bazel_api.cquery(query)
-        print(stderr)
         config = self._bazel_api.get_config(cts[0].config_hash)
         self.assertTrue(isinstance(config, HostConfiguration))
         # We don't currently populate or read a host configuration's details.
@@ -88,10 +95,36 @@ class BazelApiTest(test_base.TestBase):
         self.assertEqual(len(config.options), 0)
 
     def testConfigWithDefines(self):
-        pass
+        self.ScratchFile('testapp/BUILD', [
+            'filegroup(name = "fg", srcs = ["a.file"])',
+        ])
+        cquery_args = ["//testapp:fg", "--define", "a=b"]
+        (success, stderr, cts)  = self._bazel_api.cquery(cquery_args)
+        config = self._bazel_api.get_config(cts[0].config_hash)
+        user_defined_options = config.options["user-defined"]
+        self.assertIsNotNone(user_defined_options)
+        self.assertDictEqual(user_defined_options, {'--define:a': 'b'})
 
     def testConfigWithStarlarkFlags(self):
-        pass
-    
+        self.ScratchFile('testapp/defs.bzl', [
+            'def _flag_impl(settings, attr):',
+            '  pass',
+            'string_flag = rule(',
+            '  implementation = _flag_impl,',
+            '  build_setting = config.string(flag = True)'
+            ')'
+        ])
+        self.ScratchFile('testapp/BUILD', [
+            'load(":defs.bzl", "string_flag")',
+            'string_flag(name = "my_flag", build_setting_default = "nada")',
+            'filegroup(name = "fg", srcs = ["a.file"])',
+        ])
+        cquery_args = ["//testapp:fg", "--//testapp:my_flag", "algo"]
+        (success, stderr, cts)  = self._bazel_api.cquery(cquery_args)
+        config = self._bazel_api.get_config(cts[0].config_hash)
+        user_defined_options = config.options["user-defined"]
+        self.assertIsNotNone(user_defined_options)
+        self.assertDictEqual(user_defined_options, {'//testapp:my_flag': 'algo'})
+
 if __name__ == "__main__":
     unittest.main()
