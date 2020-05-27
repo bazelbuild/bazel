9df4f7e338a73378a726b4a364b76a9694767d81
Greg Estren <gregestren@gmail.com>
2020-05-27 18:21:11 -0400

    Add copyright disclaimer.

--- tools/ctexplain/bazel_api.py
+++ tools/ctexplain/bazel_api.py
@@ -1,4 +1,18 @@
 # Lint as: python3
+# Copyright 2020 The Bazel Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """API for Bazel calls for config, cquery, and required fragment info.
 
 There's no Python Bazel API so we invoke Bazel as a subprocess.
--- tools/ctexplain/ctexplain.py
+++ tools/ctexplain/ctexplain.py
@@ -1,16 +1,22 @@
 # Lint as: python3
+# Copyright 2020 The Bazel Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from tools.ctexplain.bazel_api import BazelApi
 
 bazel_api = BazelApi()
-print("Ready to go.")
-k = bazel_api.cquery(["//tools/ctexplain/examples/simple:split"])
-print("status code: " + str(k[0]))
-print("stderr: " + str(k[1], "utf-8"))
-print("=" * 30)
-print("results: " + str(k[2]))
 
-print("==" * 30)
-print(f"Let's get the config for {k[2][0].config_hash}:")
-config = bazel_api.get_config(k[2][0].config_hash)
-print(config)
+# TODO(gregce): move all logic to a _lib library so we can easily include
+# end-to-end testing. We'll only handle flag parsing here, which we pass
+# into the main invoker as standard Python args.
