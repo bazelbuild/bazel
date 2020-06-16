feffb17ca68d50465647c3d3a7c7dae1282484fa
Googler <noreply@google.com>
2020-06-16 12:10:52 -0700

    Add java_binary.jvm_flags_for_cds_image_creation attribute.
    PiperOrigin-RevId: 316731139

--- src/main/java/com/google/devtools/build/lib/rules/java/JavaBinary.java
+++ src/main/java/com/google/devtools/build/lib/rules/java/JavaBinary.java
@@ -542,6 +542,11 @@ public class JavaBinary implements RuleConfiguredTargetFactory {
       commandLine.addFormatted("-XX:SharedArchiveConfigFile=%s", configFile.getExecPath());
     }
     commandLine.add("-cp").addExecPath(merged);
+    if (ruleContext.getRule().isAttrDefined("jvm_flags_for_cds_image_creation", Type.STRING_LIST)) {
+      commandLine.addAll(
+          ruleContext.getExpander().withDataLocations().list("jvm_flags_for_cds_image_creation"));
+    }
+
     SpawnAction.Builder spawnAction =
         new SpawnAction.Builder()
             .setExecutable(javaRuntime.javaBinaryExecPathFragment())
