2c7c67fb7f6c283935ff49406dccd381b2679f37
mschaller <mschaller@google.com>
2020-06-25 13:43:17 -0700

    Remove unused undetailed AbruptExitException constructor
    RELNOTES: None.
    PiperOrigin-RevId: 318339576

--- src/main/java/com/google/devtools/build/lib/util/AbruptExitException.java
+++ src/main/java/com/google/devtools/build/lib/util/AbruptExitException.java
@@ -27,11 +27,6 @@ public class AbruptExitException extends Exception {
 
   private final DetailedExitCode detailedExitCode;
 
-  public AbruptExitException(String message, ExitCode exitCode, Throwable cause) {
-    super(message, cause);
-    this.detailedExitCode = DetailedExitCode.justExitCode(exitCode);
-  }
-
   public AbruptExitException(DetailedExitCode detailedExitCode) {
     super(detailedExitCode.getFailureDetail().getMessage());
     this.detailedExitCode = detailedExitCode;
