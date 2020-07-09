bazelbuild/bazel

commit 87b4608ea2c6f594847790ef5c1c18d6bc476374

bazel packages: record load graph (DAG over Modules) in Module and Package
This is a preparatory step for a new, rich query output format that will provide
all the information needed to implement stardoc as a client of blaze, thus allowing
us to delete the fakebuildapi implementations and the starlarkbuildapi abstractions.

In some situations this could increase live heap usage by causing a package
to keep its loaded modules live for longer. Specifically, if packages P and Q
both load module M, then M is changed and package Q is reloaded, the stale
package P remains live, and it holds a reference to the stale M, whereas before
it did not. The next time P is loaded, the stale P and M are evicted.
Note that today, P already keeps alive any modules required by rules instantiated
by P, so an additional cost will be incurred only if P uses functions ("macros")
or data defined in M, but no rules. I expect the effect to be quite marginal.

A number of opportunities for simplification (and memory saving) have been
identified and will be acted on in a follow-up.

Also:
- define BazelModuleContext.of helper function.

RELNOTES: N/A
PiperOrigin-RevId: 320283510

src/main/java/com/google/devtools/build/lib/packages/PackageFactory.java
@@ -431,7 +431,7 @@ public final class PackageFactory {
       PackageIdentifier packageId,
       RootedPath buildFile,
       StarlarkFile file,
-      Map<String, Module> loadedModules,
+      ImmutableMap<String, Module> loadedModules,
       ImmutableList<Label> starlarkFileDependencies,
       RuleVisibility defaultVisibility,
       StarlarkSemantics starlarkSemantics,
@@ -718,7 +718,7 @@ public final class PackageFactory {
       Globber globber,
       RuleVisibility defaultVisibility,
       StarlarkSemantics semantics,
-      Map<String, Module> loadedModules,
+      ImmutableMap<String, Module> loadedModules,
       ImmutableList<Label> starlarkFileDependencies,
       ImmutableMap<RepositoryName, RepositoryName> repositoryMapping)
       throws InterruptedException {
@@ -735,10 +735,16 @@ public final class PackageFactory {
             // Let's give the BUILD file a chance to set default_visibility once,
             // by resetting the PackageBuilder.defaultVisibilitySet flag.
             .setDefaultVisibilitySet(false)
+            // TODO(adonovan): opt: compute starlarkFileDependencies by visiting loadedModules DAG,
+            // and simplify callers of evaluateBuildFile up to BzlLoadValue.
             .setStarlarkFileDependencies(starlarkFileDependencies)
             .setWorkspaceName(workspaceName)
             .setThirdPartyLicenceExistencePolicy(
                 ruleClassProvider.getThirdPartyLicenseExistencePolicy());
+    if (packageSettings.recordLoadedModules()) {
+      pkgBuilder.setLoads(loadedModules);
+    }
+
     StoredEventHandler eventHandler = new StoredEventHandler();
     if (!buildPackage(
         pkgBuilder,
@@ -761,7 +767,7 @@ public final class PackageFactory {
       PackageIdentifier packageId,
       StarlarkFile file,
       StarlarkSemantics semantics,
-      Map<String, Module> loadedModules,
+      ImmutableMap<String, Module> loadedModules,
       PackageContext pkgContext)
       throws InterruptedException {
 

src/main/java/com/google/devtools/build/lib/skyframe/BzlLoadFunction.java
@@ -566,7 +566,7 @@ public class BzlLoadFunction implements SkyFunction {
       throw BzlLoadFailedException.starlarkErrors(filePath);
     }
 
-    // Process the load statements in the file,
+    // Process load statements in .bzl file (recursive .bzl -> .bzl loads),
     // resolving labels relative to the current repo mapping.
     ImmutableMap<RepositoryName, RepositoryName> repoMapping = getRepositoryMapping(key, env);
     if (repoMapping == null) {
@@ -605,13 +605,13 @@ public class BzlLoadFunction implements SkyFunction {
     // loads. Loop iteration order matches the source order of load statements.
     Fingerprint fp = new Fingerprint();
     fp.addBytes(astLookupValue.getDigest());
-    Map<String, Module> loadedModules = Maps.newHashMapWithExpectedSize(loads.size());
+    Map<String, Module> loadedModules = Maps.newLinkedHashMapWithExpectedSize(loads.size());
     ImmutableList.Builder<StarlarkFileDependency> fileDependencies =
         ImmutableList.builderWithExpectedSize(loads.size());
     for (int i = 0; i < loads.size(); i++) {
       String loadString = loads.get(i).first;
       BzlLoadValue v = bzlLoads.get(i);
-      loadedModules.put(loadString, v.getModule());
+      loadedModules.put(loadString, v.getModule()); // dups ok
       fileDependencies.add(v.getDependency());
       fp.addBytes(v.getTransitiveDigest());
     }
@@ -620,7 +620,16 @@ public class BzlLoadFunction implements SkyFunction {
     Module module =
         Module.withPredeclared(
             starlarkSemantics, getPredeclaredEnvironment(key, starlarkBuiltinsValue));
-    module.setClientData(BazelModuleContext.create(label, transitiveDigest));
+
+    // Record the module's filename, label, digest, and the set of modules it loads,
+    // forming a complete representation of the load DAG.
+    module.setClientData(
+        BazelModuleContext.create(
+            label,
+            file.getStartLocation().file(),
+            ImmutableMap.copyOf(loadedModules),
+            transitiveDigest));
+
     // executeBzlFile may post events to the Environment's handler, but events do not matter when
     // caching BzlLoadValues. Note that executing the module mutates it.
     executeBzlFile(
@@ -631,10 +640,8 @@ public class BzlLoadFunction implements SkyFunction {
         starlarkSemantics,
         env.getListener(),
         repoMapping);
-    BzlLoadValue result =
-        new BzlLoadValue(
-            module, transitiveDigest, new StarlarkFileDependency(label, fileDependencies.build()));
-    return result;
+    return new BzlLoadValue(
+        module, transitiveDigest, new StarlarkFileDependency(label, fileDependencies.build()));
   }
 
   private static ImmutableMap<RepositoryName, RepositoryName> getRepositoryMapping(

src/main/java/com/google/devtools/build/lib/skyframe/PackageFunction.java
@@ -691,15 +691,16 @@ public class PackageFunction implements SkyFunction {
     }
 
     // Process the loaded modules.
-    Map<String, Module> loadedModules = Maps.newHashMapWithExpectedSize(loads.size());
+    Map<String, Module> loadedModules = Maps.newLinkedHashMapWithExpectedSize(loads.size());
     ImmutableList.Builder<StarlarkFileDependency> fileDependencies = ImmutableList.builder();
     for (int i = 0; i < loads.size(); i++) {
       String loadString = loads.get(i).first;
       BzlLoadValue v = bzlLoads.get(i);
-      loadedModules.put(loadString, v.getModule());
+      loadedModules.put(loadString, v.getModule()); // dups ok
       fileDependencies.add(v.getDependency());
     }
-    return new BzlLoadResult(loadedModules, transitiveClosureOfLabels(fileDependencies.build()));
+    return new BzlLoadResult(
+        ImmutableMap.copyOf(loadedModules), transitiveClosureOfLabels(fileDependencies.build()));
   }
 
   /**
@@ -1506,12 +1507,14 @@ public class PackageFunction implements SkyFunction {
   }
 
   /** A simple value class to store the result of the Starlark loads. */
+  // TODO(adonovan): make private. Provide accessor for sole use in WorkspaceFileFunction.
+  // Then eliminate once fileDependencies is gone.
   static final class BzlLoadResult {
-    final Map<String, Module> loadedModules;
+    final ImmutableMap<String, Module> loadedModules;
     final ImmutableList<Label> fileDependencies;
 
     private BzlLoadResult(
-        Map<String, Module> loadedModules, ImmutableList<Label> fileDependencies) {
+        ImmutableMap<String, Module> loadedModules, ImmutableList<Label> fileDependencies) {
       this.loadedModules = loadedModules;
       this.fileDependencies = fileDependencies;
     }

src/main/java/com/google/devtools/build/lib/skyframe/StarlarkFileDependency.java
@@ -20,6 +20,8 @@ import com.google.devtools.build.lib.skyframe.serialization.autocodec.AutoCodec;
 import java.util.Objects;
 
 /** A value class representing a node in a DAG that mirrors the load graph of a Starlark file. */
+// TODO(adonovan): opt: eliminate this class; it is redundant w.r.t. the Module DAG.
+// See comment at setStarlarkFileDependencies call in PackageFactory.
 @AutoCodec
 public class StarlarkFileDependency {
 

src/main/java/com/google/devtools/build/lib/analysis/starlark/StarlarkAttrModule.java
@@ -138,9 +138,7 @@ public final class StarlarkAttrModule implements StarlarkAttrModuleApi {
         builder.defaultValue(
             defaultValue,
             new BuildType.LabelConversionContext(
-                ((BazelModuleContext)
-                        Module.ofInnermostEnclosingStarlarkFunction(thread).getClientData())
-                    .label(),
+                BazelModuleContext.of(Module.ofInnermostEnclosingStarlarkFunction(thread)).label(),
                 BazelStarlarkContext.from(thread).getRepoMapping()),
             DEFAULT_ARG);
       }

src/main/java/com/google/devtools/build/lib/analysis/starlark/StarlarkRuleClassFunctions.java
@@ -348,7 +348,7 @@ public class StarlarkRuleClassFunctions implements StarlarkRuleFunctionsApi<Arti
     builder.setConfiguredTargetFunction(implementation);
     // Information about the .bzl module containing the call that created the rule class.
     BazelModuleContext bzlModule =
-        (BazelModuleContext) Module.ofInnermostEnclosingStarlarkFunction(thread).getClientData();
+        BazelModuleContext.of(Module.ofInnermostEnclosingStarlarkFunction(thread));
     builder.setRuleDefinitionEnvironmentLabelAndDigest(
         bzlModule.label(), bzlModule.bzlTransitiveDigest());
 
@@ -872,8 +872,7 @@ public class StarlarkRuleClassFunctions implements StarlarkRuleFunctionsApi<Arti
     } else {
       // This is the label of the innermost BUILD/.bzl file on the current call stack.
       parentLabel =
-          ((BazelModuleContext) Module.ofInnermostEnclosingStarlarkFunction(thread).getClientData())
-              .label();
+          BazelModuleContext.of(Module.ofInnermostEnclosingStarlarkFunction(thread)).label();
     }
 
     try {

commit 39e42d147010911fbda1e2c6f0606b74dd263e78

bazel skyframe: remove redundant representation of the Module load graph
commit 87b4608ea2c6f594847790ef5c1c18d6bc476374 caused the .bzl module graph to be materialized directly,
to provide richer information in blaze query. This change removes the
previous representation which merely recorded the transitive set of labels.

PiperOrigin-RevId: 320409577

src/main/java/com/google/devtools/build/lib/packages/PackageFactory.java
@@ -19,6 +19,7 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
 import com.google.devtools.build.lib.cmdline.Label;
 import com.google.devtools.build.lib.cmdline.LabelConstants;
 import com.google.devtools.build.lib.cmdline.LabelValidator;
@@ -432,7 +433,6 @@ public final class PackageFactory {
       RootedPath buildFile,
       StarlarkFile file,
       ImmutableMap<String, Module> loadedModules,
-      ImmutableList<Label> starlarkFileDependencies,
       RuleVisibility defaultVisibility,
       StarlarkSemantics starlarkSemantics,
       Globber globber)
@@ -449,7 +449,6 @@ public final class PackageFactory {
           defaultVisibility,
           starlarkSemantics,
           loadedModules,
-          starlarkFileDependencies,
           repositoryMapping);
     } catch (InterruptedException e) {
       globber.onInterrupt();
@@ -542,7 +541,6 @@ public final class PackageFactory {
                 buildFile,
                 file,
                 /*loadedModules=*/ ImmutableMap.<String, Module>of(),
-                /*starlarkFileDependencies=*/ ImmutableList.<Label>of(),
                 /*defaultVisibility=*/ ConstantRuleVisibility.PUBLIC,
                 semantics,
                 globber)
@@ -719,7 +717,6 @@ public final class PackageFactory {
       RuleVisibility defaultVisibility,
       StarlarkSemantics semantics,
       ImmutableMap<String, Module> loadedModules,
-      ImmutableList<Label> starlarkFileDependencies,
       ImmutableMap<RepositoryName, RepositoryName> repositoryMapping)
       throws InterruptedException {
     Package.Builder pkgBuilder =
@@ -735,9 +732,9 @@ public final class PackageFactory {
             // Let's give the BUILD file a chance to set default_visibility once,
             // by resetting the PackageBuilder.defaultVisibilitySet flag.
             .setDefaultVisibilitySet(false)
-            // TODO(adonovan): opt: compute starlarkFileDependencies by visiting loadedModules DAG,
-            // and simplify callers of evaluateBuildFile up to BzlLoadValue.
-            .setStarlarkFileDependencies(starlarkFileDependencies)
+            // TODO(adonovan): opt: don't precompute this value, which is rarely needed
+            // and can be derived from Package.loads (if available) on demand.
+            .setStarlarkFileDependencies(transitiveClosureOfLabels(loadedModules))
             .setWorkspaceName(workspaceName)
             .setThirdPartyLicenceExistencePolicy(
                 ruleClassProvider.getThirdPartyLicenseExistencePolicy());
@@ -758,6 +755,23 @@ public final class PackageFactory {
     pkgBuilder.addPosts(eventHandler.getPosts());
     pkgBuilder.addEvents(eventHandler.getEvents());
     return pkgBuilder;
+  }
+
+  private static ImmutableList<Label> transitiveClosureOfLabels(
+      ImmutableMap<String, Module> loads) {
+    Set<Label> set = Sets.newLinkedHashSet();
+    transitiveClosureOfLabelsRec(set, loads);
+    return ImmutableList.copyOf(set);
+  }
+
+  private static void transitiveClosureOfLabelsRec(
+      Set<Label> set, ImmutableMap<String, Module> loads) {
+    for (Module m : loads.values()) {
+      BazelModuleContext ctx = BazelModuleContext.of(m);
+      if (set.add(ctx.label())) {
+        transitiveClosureOfLabelsRec(set, ctx.loads());
+      }
+    }
   }
 
   // Validates and executes a parsed BUILD file, returning true on success,

src/main/java/com/google/devtools/build/lib/skyframe/BUILD
@@ -198,7 +198,6 @@ java_library(
         ":skyframe_aware_action",
         ":skyframe_incremental_build_monitor",
         ":skylark_module_cycle_reporter",
-        ":starlark_file_dependency",
         ":state_informing_sky_function_environment",
         ":target_pattern_error_function",
         ":target_pattern_phase_value",
@@ -2308,21 +2307,10 @@ java_library(
 )
 
 java_library(
-    name = "starlark_file_dependency",
-    srcs = ["StarlarkFileDependency.java"],
-    deps = [
-        "//src/main/java/com/google/devtools/build/lib/cmdline",
-        "//src/main/java/com/google/devtools/build/lib/skyframe/serialization/autocodec",
-        "//third_party:guava",
-    ],
-)
-
-java_library(
     name = "bzl_load_value",
     srcs = ["BzlLoadValue.java"],
     deps = [
         ":sky_functions",
-        ":starlark_file_dependency",
         "//src/main/java/com/google/devtools/build/lib/cmdline",
         "//src/main/java/com/google/devtools/build/lib/concurrent",
         "//src/main/java/com/google/devtools/build/lib/skyframe/serialization/autocodec",

src/main/java/com/google/devtools/build/lib/skyframe/BzlLoadFunction.java
@@ -76,8 +76,8 @@ import javax.annotation.Nullable;
  * <p>Given a {@link Label} referencing a .bzl file, attempts to locate the file and load it. The
  * Label must be absolute, and must not reference the special {@code external} package. If loading
  * is successful, returns a {@link BzlLoadValue} that encapsulates the loaded {@link Module} and its
- * transitive digest and {@link StarlarkFileDependency} information. If loading is unsuccessful,
- * throws a {@link BzlLoadFunctionException} that encapsulates the cause of the failure.
+ * transitive digest information. If loading is unsuccessful, throws a {@link
+ * BzlLoadFunctionException} that encapsulates the cause of the failure.
  *
  * <p>This Skyframe function supports a special "inlining" mode in which all (indirectly) recursive
  * calls to {@code BzlLoadFunction} are made in the same thread rather than through Skyframe. The
@@ -606,13 +606,10 @@ public class BzlLoadFunction implements SkyFunction {
     Fingerprint fp = new Fingerprint();
     fp.addBytes(astLookupValue.getDigest());
     Map<String, Module> loadedModules = Maps.newLinkedHashMapWithExpectedSize(loads.size());
-    ImmutableList.Builder<StarlarkFileDependency> fileDependencies =
-        ImmutableList.builderWithExpectedSize(loads.size());
     for (int i = 0; i < loads.size(); i++) {
       String loadString = loads.get(i).first;
       BzlLoadValue v = bzlLoads.get(i);
       loadedModules.put(loadString, v.getModule()); // dups ok
-      fileDependencies.add(v.getDependency());
       fp.addBytes(v.getTransitiveDigest());
     }
     byte[] transitiveDigest = fp.digestAndReset();
@@ -640,8 +637,7 @@ public class BzlLoadFunction implements SkyFunction {
         starlarkSemantics,
         env.getListener(),
         repoMapping);
-    return new BzlLoadValue(
-        module, transitiveDigest, new StarlarkFileDependency(label, fileDependencies.build()));
+    return new BzlLoadValue(module, transitiveDigest);
   }
 
   private static ImmutableMap<RepositoryName, RepositoryName> getRepositoryMapping(

src/main/java/com/google/devtools/build/lib/skyframe/BzlLoadValue.java
@@ -34,21 +34,13 @@ import java.util.Objects;
  */
 public class BzlLoadValue implements SkyValue {
 
-  private final Module module; // .bzl module
+  private final Module module; // .bzl module (and indirectly, the entire load DAG)
   private final byte[] transitiveDigest; // of .bzl file and load dependencies
 
-  /**
-   * The immediate Starlark file dependency descriptor class corresponding to this value. Using this
-   * reference it's possible to reach the transitive closure of Starlark files on which this
-   * Starlark file depends.
-   */
-  private final StarlarkFileDependency dependency;
-
   @VisibleForTesting
-  public BzlLoadValue(Module module, byte[] transitiveDigest, StarlarkFileDependency dependency) {
+  public BzlLoadValue(Module module, byte[] transitiveDigest) {
     this.module = Preconditions.checkNotNull(module);
     this.transitiveDigest = Preconditions.checkNotNull(transitiveDigest);
-    this.dependency = Preconditions.checkNotNull(dependency);
   }
 
   /** Returns the .bzl module. */
@@ -59,11 +51,6 @@ public class BzlLoadValue implements SkyValue {
   /** Returns the digest of the .bzl module and its transitive load dependencies. */
   public byte[] getTransitiveDigest() {
     return transitiveDigest;
-  }
-
-  /** Returns the root of a DAG whose structure mirrors the transitive loads of this file. */
-  public StarlarkFileDependency getDependency() {
-    return dependency;
   }
 
   private static final Interner<Key> keyInterner = BlazeInterners.newWeakInterner();

src/main/java/com/google/devtools/build/lib/skyframe/PackageFunction.java
@@ -627,11 +627,11 @@ public class PackageFunction implements SkyFunction {
   }
 
   /**
-   * Fetch the Starlark loads for this BUILD file. If any of them haven't been computed yet, returns
-   * null.
+   * Fetch the Starlark loads for this BUILD file, in source order. If any of them haven't been
+   * computed yet, returns null.
    */
   @Nullable
-  static BzlLoadResult fetchLoadsFromBuildFile(
+  static ImmutableMap<String, Module> fetchLoadsFromBuildFile(
       RootedPath buildFilePath,
       PackageIdentifier packageId,
       ImmutableMap<RepositoryName, RepositoryName> repoMapping,
@@ -692,15 +692,12 @@ public class PackageFunction implements SkyFunction {
 
     // Process the loaded modules.
     Map<String, Module> loadedModules = Maps.newLinkedHashMapWithExpectedSize(loads.size());
-    ImmutableList.Builder<StarlarkFileDependency> fileDependencies = ImmutableList.builder();
     for (int i = 0; i < loads.size(); i++) {
       String loadString = loads.get(i).first;
       BzlLoadValue v = bzlLoads.get(i);
       loadedModules.put(loadString, v.getModule()); // dups ok
-      fileDependencies.add(v.getDependency());
     }
-    return new BzlLoadResult(
-        ImmutableMap.copyOf(loadedModules), transitiveClosureOfLabels(fileDependencies.build()));
+    return ImmutableMap.copyOf(loadedModules);
   }
 
   /**
@@ -783,22 +780,6 @@ public class PackageFunction implements SkyFunction {
     ImmutableMap<String, Integer> loadToChunkMap = workspaceFileValue.getLoadToChunkMap();
     String loadString = loadLabel.toString();
     return loadToChunkMap.getOrDefault(loadString, workspaceChunk);
-  }
-
-  private static ImmutableList<Label> transitiveClosureOfLabels(
-      ImmutableList<StarlarkFileDependency> immediateDeps) {
-    Set<Label> transitiveClosure = Sets.newHashSet();
-    transitiveClosureOfLabels(immediateDeps, transitiveClosure);
-    return ImmutableList.copyOf(transitiveClosure);
-  }
-
-  private static void transitiveClosureOfLabels(
-      ImmutableList<StarlarkFileDependency> immediateDeps, Set<Label> transitiveClosure) {
-    for (StarlarkFileDependency dep : immediateDeps) {
-      if (transitiveClosure.add(dep.getLabel())) {
-        transitiveClosureOfLabels(dep.getDependencies(), transitiveClosure);
-      }
-    }
   }
 
   @Nullable
@@ -1262,9 +1243,9 @@ public class PackageFunction implements SkyFunction {
         file = StarlarkFile.parseWithPrelude(input, preludeStatements, options);
         fileSyntaxCache.put(packageId, file);
       }
-      BzlLoadResult loadResult;
+      ImmutableMap<String, Module> loadedModules;
       try {
-        loadResult =
+        loadedModules =
             fetchLoadsFromBuildFile(
                 buildFilePath,
                 packageId,
@@ -1279,7 +1260,7 @@ public class PackageFunction implements SkyFunction {
         fileSyntaxCache.invalidate(packageId);
         throw e;
       }
-      if (loadResult == null) {
+      if (loadedModules == null) {
         return null;
       }
       // From here on, either of the following must happen:
@@ -1300,8 +1281,7 @@ public class PackageFunction implements SkyFunction {
               packageId,
               buildFilePath,
               file,
-              loadResult.loadedModules,
-              loadResult.fileDependencies,
+              loadedModules,
               defaultVisibility,
               starlarkSemantics,
               globberWithSkyframeGlobDeps);
@@ -1503,20 +1483,6 @@ public class PackageFunction implements SkyFunction {
                 .setPackageLoading(PackageLoading.newBuilder().setCode(packageLoadingCode).build())
                 .build());
       }
-    }
-  }
-
-  /** A simple value class to store the result of the Starlark loads. */
-  // TODO(adonovan): make private. Provide accessor for sole use in WorkspaceFileFunction.
-  // Then eliminate once fileDependencies is gone.
-  static final class BzlLoadResult {
-    final ImmutableMap<String, Module> loadedModules;
-    final ImmutableList<Label> fileDependencies;
-
-    private BzlLoadResult(
-        ImmutableMap<String, Module> loadedModules, ImmutableList<Label> fileDependencies) {
-      this.loadedModules = loadedModules;
-      this.fileDependencies = fileDependencies;
     }
   }
 }

src/main/java/com/google/devtools/build/lib/skyframe/StarlarkFileDependency.java
@@ -1,68 +1,0 @@
-// Copyright 2014 The Bazel Authors. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package com.google.devtools.build.lib.skyframe;
-
-import com.google.common.collect.ImmutableList;
-import com.google.devtools.build.lib.cmdline.Label;
-import com.google.devtools.build.lib.skyframe.serialization.autocodec.AutoCodec;
-import java.util.Objects;
-
-/** A value class representing a node in a DAG that mirrors the load graph of a Starlark file. */
-// TODO(adonovan): opt: eliminate this class; it is redundant w.r.t. the Module DAG.
-// See comment at setStarlarkFileDependencies call in PackageFactory.
-@AutoCodec
-public class StarlarkFileDependency {
-
-  private final Label label;
-  private final ImmutableList<StarlarkFileDependency> dependencies;
-
-  public StarlarkFileDependency(Label label, ImmutableList<StarlarkFileDependency> dependencies) {
-    this.label = label;
-    this.dependencies = dependencies;
-  }
-
-  /**
-   * Returns the list of direct Starlark file dependencies of the Starlark extension file
-   * corresponding to this object.
-   */
-  public ImmutableList<StarlarkFileDependency> getDependencies() {
-    return dependencies;
-  }
-
-  /** Returns the Label of the Starlark extension file corresponding to this object. */
-  public Label getLabel() {
-    return label;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) {
-      return true;
-    }
-    if (!(obj instanceof StarlarkFileDependency)) {
-      return false;
-    }
-    StarlarkFileDependency other = (StarlarkFileDependency) obj;
-    if (!label.equals(other.getLabel())) {
-      return false;
-    }
-    return dependencies.equals(other.getDependencies());
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(label, dependencies);
-  }
-}

src/main/java/com/google/devtools/build/lib/skyframe/WorkspaceFileFunction.java
@@ -26,7 +26,6 @@ import com.google.devtools.build.lib.packages.RuleClassProvider;
 import com.google.devtools.build.lib.packages.WorkspaceFactory;
 import com.google.devtools.build.lib.packages.WorkspaceFileValue;
 import com.google.devtools.build.lib.packages.WorkspaceFileValue.WorkspaceFileKey;
-import com.google.devtools.build.lib.skyframe.PackageFunction.BzlLoadResult;
 import com.google.devtools.build.lib.syntax.Module;
 import com.google.devtools.build.lib.syntax.Mutability;
 import com.google.devtools.build.lib.syntax.StarlarkFile;
@@ -123,7 +122,7 @@ public class WorkspaceFileFunction implements SkyFunction {
             prevValue.getPackage(), prevValue.getLoadedModules(), prevValue.getBindings());
       }
       StarlarkFile ast = workspaceASTValue.getASTs().get(key.getIndex());
-      BzlLoadResult loadResult =
+      ImmutableMap<String, Module> loadedModules =
           PackageFunction.fetchLoadsFromBuildFile(
               workspaceFile,
               rootPackage,
@@ -132,10 +131,10 @@ public class WorkspaceFileFunction implements SkyFunction {
               key.getIndex(),
               env,
               bzlLoadFunctionForInlining);
-      if (loadResult == null) {
+      if (loadedModules == null) {
         return null;
       }
-      parser.execute(ast, loadResult.loadedModules, key);
+      parser.execute(ast, loadedModules, key);
     } catch (NoSuchPackageException e) {
       throw new WorkspaceFileFunctionException(e, Transience.PERSISTENT);
     } catch (NameConflictException e) {

src/test/java/com/google/devtools/build/lib/packages/util/PackageFactoryApparatus.java
@@ -16,7 +16,6 @@ package com.google.devtools.build.lib.packages.util;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
-import com.google.devtools.build.lib.cmdline.Label;
 import com.google.devtools.build.lib.cmdline.LabelConstants;
 import com.google.devtools.build.lib.cmdline.PackageIdentifier;
 import com.google.devtools.build.lib.events.Event;
@@ -186,8 +185,7 @@ public class PackageFactoryApparatus {
             globber,
             ConstantRuleVisibility.PUBLIC,
             StarlarkSemantics.DEFAULT,
-            ImmutableMap.<String, Module>of(),
-            ImmutableList.<Label>of(),
+            /*loadedModules=*/ ImmutableMap.<String, Module>of(),
             /*repositoryMapping=*/ ImmutableMap.of());
     Package result;
     try {

src/test/java/com/google/devtools/build/lib/skyframe/BzlLoadFunctionTest.java
@@ -23,6 +23,7 @@ import com.google.common.collect.ImmutableMap;
 import com.google.devtools.build.lib.analysis.util.BuildViewTestCase;
 import com.google.devtools.build.lib.clock.BlazeClock;
 import com.google.devtools.build.lib.cmdline.Label;
+import com.google.devtools.build.lib.packages.BazelModuleContext;
 import com.google.devtools.build.lib.packages.ConstantRuleVisibility;
 import com.google.devtools.build.lib.packages.StarlarkSemanticsOptions;
 import com.google.devtools.build.lib.pkgcache.PackageOptions;
@@ -185,7 +186,8 @@ public class BzlLoadFunctionTest extends BuildViewTestCase {
   private void checkSuccessfulLookup(String label) throws Exception {
     SkyKey skyKey = key(label);
     EvaluationResult<BzlLoadValue> result = get(skyKey);
-    assertThat(label).isEqualTo(result.get(skyKey).getDependency().getLabel().toString());
+    assertThat(label)
+        .isEqualTo(BazelModuleContext.of(result.get(skyKey).getModule()).label().toString());
   }
 
   @Test