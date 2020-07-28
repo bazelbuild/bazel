608cd0d0844a1585b460d172c7c6d0e69f033b37
twerth <twerth@google.com>
2020-07-28 01:18:24 -0700

    Add mnemonic and progress message for JSA creation.
    RELNOTES: None
    PiperOrigin-RevId: 323527956

--- src/main/java/com/google/devtools/build/lib/rules/java/JavaBinary.java
+++ src/main/java/com/google/devtools/build/lib/rules/java/JavaBinary.java
@@ -556,6 +556,8 @@ public class JavaBinary implements RuleConfiguredTargetFactory {
     spawnAction
         .setExecutable(javaRuntime.javaBinaryExecPathFragment())
         .addCommandLine(commandLine.build())
+        .setMnemonic("JavaJSA")
+        .setProgressMessage("Dumping Java Shared Archive %s", jsa.prettyPrint())
         .addOutput(jsa)
         .addInput(classlist)
         .addInput(merged)
