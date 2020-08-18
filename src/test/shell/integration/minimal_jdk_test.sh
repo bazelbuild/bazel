19151730766f335cb7addd43465ce837a350c2aa
Grzegorz Lukasik <glukasik@nuro.ai>
2020-08-18 15:21:50 -0700

    Incrase minimal_jdk_size 275M -> 280M.
    Looks adding zstd slightly increased current size and getting an error:
    
       Size of install_base is 281984 kB, expected it to be less than 281600 kB

--- src/test/shell/integration/minimal_jdk_test.sh
+++ src/test/shell/integration/minimal_jdk_test.sh
@@ -42,13 +42,13 @@ export BAZEL_SUFFIX="_jdk_minimal"
 source "$(rlocation "io_bazel/src/test/shell/integration_test_setup.sh")" \
   || { echo "integration_test_setup.sh not found!" >&2; exit 1; }
 
-# Bazel's install base is < 275MB with minimal JDK and > 300MB with an all
+# Bazel's install base is < 280MB with minimal JDK and > 300MB with an all
 # modules JDK.
-function test_size_less_than_275MB() {
+function test_size_less_than_280MB() {
   bazel info
   ib=$(bazel info install_base)
   size=$(du -s "$ib" | cut -d\	 -f1)
-  maxsize=$((1024*275))
+  maxsize=$((1024*280))
   if [ $size -gt $maxsize ]; then
     fail "Size of install_base is $size kB, expected it to be less than $maxsize kB."
   fi
