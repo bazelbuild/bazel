e3b1262795522aab59e30da72192d205c72722c7
Grzegorz Lukasik <glukasik@nuro.ai>
2020-08-19 07:39:54 -0700

    Loading native library first using System and then from runfiles

--- third_party/zstd-jni/src/main/java/com/github/luben/zstd/util/Native.java
+++ third_party/zstd-jni/src/main/java/com/github/luben/zstd/util/Native.java
@@ -1,19 +1,15 @@
 package com.github.luben.zstd.util;
 
-import java.io.File;
-import java.io.FileOutputStream;
+import com.google.devtools.build.runfiles.Runfiles;
 import java.io.IOException;
-import java.io.InputStream;
 import java.lang.UnsatisfiedLinkError;
+
 
 public enum Native {
     ;
 
     private static final String libnameShort = "zstd-jni";
     private static final String libname = "lib" + libnameShort;
-    private static final String errorMsg = "Unsupported OS/arch, cannot find " +
-        resourceName() + " or load " + libnameShort + " from system libraries. Please " +
-        "try building from source the jar or providing " + libname + " in your system.";
 
     private static String osName() {
         String os = System.getProperty("os.name").toLowerCase().replace(' ', '_');
@@ -26,10 +22,6 @@ public enum Native {
         }
     }
 
-    private static String osArch() {
-        return System.getProperty("os.arch");
-    }
-
     private static String libExtension() {
         if (osName().contains("os_x") || osName().contains("darwin")) {
             return "dylib";
@@ -40,8 +32,8 @@ public enum Native {
         }
     }
 
-    private static String resourceName() {
-        return "/" + osName() + "/" + osArch() + "/" + libname + "." + libExtension();
+    private static String libraryFilename() {
+        return libname + "." + libExtension();
     }
 
     private static boolean loaded = false;
@@ -51,89 +43,37 @@ public enum Native {
     }
 
     public static synchronized void load() {
-        load(null);
-    }
-
-    public static synchronized void load(final File tempFolder) {
         if (loaded) {
             return;
         }
-
-        String resourceName = resourceName();
-        InputStream is = Native.class.getResourceAsStream(resourceName);
-        if (is == null) {
-            // fall-back to loading the zstd-jni from the system library path.
-            // It also cover loading on Android.
-            try {
-                System.loadLibrary(libnameShort);
-                loaded = true;
-                return;
-            } catch (UnsatisfiedLinkError e) {
-                UnsatisfiedLinkError err = new UnsatisfiedLinkError(e.getMessage() + "\n" + errorMsg);
-                err.setStackTrace(e.getStackTrace());
-                throw err;
-            }
-        }
-        File tempLib = null;
-        FileOutputStream out = null;
         try {
-            tempLib = File.createTempFile(libname, "." + libExtension(), tempFolder);
-            // try to delete on exit, does not work on Windows
-            tempLib.deleteOnExit();
-            // copy to tempLib
-            out = new FileOutputStream(tempLib);
-            byte[] buf = new byte[4096];
-            while (true) {
-                int read = is.read(buf);
-                if (read == -1) {
-                    break;
-                }
-                out.write(buf, 0, read);
-            }
-            try {
-                out.flush();
-                out.close();
-                out = null;
-            } catch (IOException e) {
-                // ignore
-            }
-            try {
-                System.load(tempLib.getAbsolutePath());
-            } catch (UnsatisfiedLinkError e) {
-                // fall-back to loading the zstd-jni from the system library path
-                try {
-                    System.loadLibrary(libnameShort);
-                } catch (UnsatisfiedLinkError e1) {
-                    // display error in case problem with loading from temp folder
-                    // and from system library path - concatenate both messages
-                    UnsatisfiedLinkError err = new UnsatisfiedLinkError(
-                            e.getMessage() + "\n" +
-                            e1.getMessage() + "\n"+
-                            errorMsg);
-                    err.setStackTrace(e1.getStackTrace());
-                    throw err;
-                }
-            }
-            loaded = true;
-        } catch (IOException e) {
-            // IO errors in extacting and writing the shared object in the temp dir
-            ExceptionInInitializerError err = new ExceptionInInitializerError(
-                    "Cannot unpack " + libname + ": " + e.getMessage());
-            err.setStackTrace(e.getStackTrace());
-            throw err;
+            System.loadLibrary(libnameShort);
+        } catch (UnsatisfiedLinkError e) {
+            // Try to load from runfiles, esp. for Windows.
+            loadFromRunfiles();
         }
-        finally {
-            try {
-                is.close();
-                if (out != null) {
-                    out.close();
-                }
-                if (tempLib != null && tempLib.exists()) {
-                    tempLib.delete();
-                }
-            } catch (IOException e) {
-                // ignore
+    }
+
+    private static synchronized void loadFromRunfiles() {
+        Runfiles runfiles = null;
+        try {
+            runfiles = Runfiles.create();
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to locate runfiles after failing to load " +
+                                       libnameShort + " from system. Giving up.", e);
+        }
+
+        String rloc = runfiles.rlocation("io_bazel/third_party/zstd-jni/" + libraryFilename());
+        if (rloc == null) {
+            rloc = runfiles.rlocation("bazel_tools/third_party/zstd-jni/" + libraryFilename());
+            if (rloc == null) {
+                throw new RuntimeException("Unable to find JNI library in runfiles: " + libraryFilename());
             }
+        }
+        try {
+            System.load(rloc);
+        } catch (UnsatisfiedLinkError e) {
+            throw new RuntimeException("Unable to load JNI library from runfiles: " + rloc, e);
         }
     }
 }
