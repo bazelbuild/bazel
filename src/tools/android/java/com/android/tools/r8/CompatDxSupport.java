4d695ee780ef7961c77a292035d5da8150a12d75
sgjesse <sgjesse@google.com>
2020-08-19 02:12:25 -0700

    Remove support for --minimal-main-dex
    RELNOTES: None
    PiperOrigin-RevId: 327395717

--- src/tools/android/java/com/android/tools/r8/CompatDxSupport.java
+++ src/tools/android/java/com/android/tools/r8/CompatDxSupport.java
@@ -21,8 +21,7 @@ import java.lang.reflect.Method;
  * are not in the public API.
  */
 public class CompatDxSupport {
-  public static void run(D8Command command, boolean minimalMainDex)
-      throws CompilationFailedException {
+  public static void run(D8Command command) throws CompilationFailedException {
     try {
       // bazel can point to both an full r8.jar and to the shrunken r8.jar (r8lib.jar), as the
       // r8.jar is currently referenced from the Android SDK build-tools, where different versions
@@ -32,17 +31,14 @@ public class CompatDxSupport {
       // supported in the public D8 API.
       Class<?> androidAppClass = Class.forName("com.android.tools.r8.utils.AndroidApp");
       Class<?> internalOptionsClass = Class.forName("com.android.tools.r8.utils.InternalOptions");
-      runOnFullJar(command, minimalMainDex, androidAppClass, internalOptionsClass);
+      runOnFullJar(command, androidAppClass, internalOptionsClass);
     } catch (ClassNotFoundException e) {
       D8.run(command);
     }
   }
 
   public static void runOnFullJar(
-      D8Command command,
-      boolean minimalMainDex,
-      Class<?> androidAppClass,
-      Class<?> internalOptionsClass) {
+      D8Command command, Class<?> androidAppClass, Class<?> internalOptionsClass) {
     Method getInputAppMethod;
     Method getInternalOptionsMethod;
     Method runForTestingMethod;
@@ -67,18 +63,11 @@ public class CompatDxSupport {
       // that are trying to transition.
       try {
         Field enableMainDexListCheckField = internalOptionsClass.getField("enableMainDexListCheck");
-        // DX has a minimal main dex flag. In compat mode only do minimal main dex
-        // if the flag is actually set.
-        Field minimalMainDexField = internalOptionsClass.getField("minimalMainDex");
         try {
           // Use reflection for:
           //   <code>options.enableMainDexListCheck = false;</code>
           // as bazel might link to an old r8.jar which does not have this field.
           enableMainDexListCheckField.setBoolean(options, false);
-          // Use reflection for:
-          //   <code>options.minimalMainDex = minimalMainDex;</code>
-          // as bazel might link to an old r8.jar which does not have this field.
-          minimalMainDexField.setBoolean(options, minimalMainDex);
         } catch (IllegalAccessException e) {
           throw new AssertionError("Unsupported r8.jar", e);
         }
