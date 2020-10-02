8346ea4cfdd9fbd170d51a528fee26f912dad2d5
larsrc <larsrc@google.com>
2020-10-02 10:43:04 -0700

    Use actual name of flag in error message.
    RELNOTES: n/a
    PiperOrigin-RevId: 335055802

--- src/main/java/com/google/devtools/build/lib/worker/WorkerSpawnRunner.java
+++ src/main/java/com/google/devtools/build/lib/worker/WorkerSpawnRunner.java
@@ -202,7 +202,7 @@ final class WorkerSpawnRunner implements SpawnRunner {
       if (protocolFormat == WorkerProtocolFormat.JSON) {
         throw new IOException(
             "Persistent worker protocol format must be set to proto unless"
-                + " --experimentalJsonWorkerProtocol is used");
+                + " --experimental_allow_json_worker_protocol is used");
       }
     }
 
