f885210e1b310df2ceb333892a7345d4a6151c4a
cmita <cmita@google.com>
2020-10-12 08:10:00 -0700

    Automated rollback of commit 66844df726f5a6c1d6f4bc5755d3af0c6ce3662c.
    *** Reason for rollback ***
    
    Breaks internal test for reasons I don't understand yet.
    
    *** Original change description ***
    
    Add source jar to transitive jars in JavaProtoAspect
    
    Ensures the source jar for java_proto_library is included in the set of
    transitive source jars as is expected.
    
    Fixes https://github.com/bazelbuild/bazel/issues/11830
    
    PiperOrigin-RevId: 336664995

--- src/main/java/com/google/devtools/build/lib/rules/java/proto/JavaProtoAspect.java
+++ src/main/java/com/google/devtools/build/lib/rules/java/proto/JavaProtoAspect.java
@@ -236,7 +236,7 @@ public class JavaProtoAspect extends NativeAspectClass implements ConfiguredAspe
                 .build();
         JavaSourceJarsProvider sourceJarsProvider =
             JavaSourceJarsProvider.create(
-                NestedSetBuilder.create(Order.STABLE_ORDER, sourceJar),
+                NestedSetBuilder.<Artifact>emptySet(Order.STABLE_ORDER),
                 ImmutableList.of(sourceJar));
 
         aspect.addProvider(ruleOutputJarsProvider).addProvider(sourceJarsProvider);
