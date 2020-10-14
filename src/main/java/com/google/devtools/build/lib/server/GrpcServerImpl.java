481dd888b6f641d32f224642f7f32e0d169a1669
Dmitry Ivankov <dmitry.ivankov@cognite.com>
2020-10-14 17:45:32 +0200

    fix typo

--- src/main/java/com/google/devtools/build/lib/server/GrpcServerImpl.java
+++ src/main/java/com/google/devtools/build/lib/server/GrpcServerImpl.java
@@ -390,7 +390,7 @@ public class GrpcServerImpl extends CommandServerGrpc.CommandServerImplBase impl
       server =
           NettyServerBuilder.forAddress(address).addService(this).directExecutor()
               // disable auto flow control https://github.com/bazelbuild/bazel/issues/12264
-              .flowControlWindow(NettyServerBuilder.DEFAULT_FLOW_CONTROL_WINDOW);
+              .flowControlWindow(NettyServerBuilder.DEFAULT_FLOW_CONTROL_WINDOW)
 	      .build().start();
     } catch (IOException ipv6Exception) {
       address = new InetSocketAddress("127.0.0.1", port);
