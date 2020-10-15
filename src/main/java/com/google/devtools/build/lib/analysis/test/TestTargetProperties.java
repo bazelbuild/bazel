5e5eb86e5390e9e822848ccc031e0846a520858d
Googler <noreply@google.com>
2020-10-14 18:48:46 -0700

    Roll forward of https://github.com/bazelbuild/bazel/commit/0f0a0d58725603cf2f1c175963360b525718a195 with fixes.
    1. Add --incompatible_exclusive_test_sandboxed flag so users can enable this feature conditionally.
    2. Users who want to run exclusive tests locally can add a 'local' tag to the test.
    
    *** Original change description ***
    
    Enable sandboxing and remote caching for the "exclusive" tag
    
    There's no good reason why tests tagged with exclusive shouldn't
    be able to use sandboxing and/or remote caching. The current
    limitations have been kept from open sourcing Bazel and have
    never been revisited since.
    
    Remote execution will continue to be disabled because Bazel
    can't control what else is running on a remote machine.
    
    Closes #8983.
    
    ***
    
    PiperOrigin-RevId: 337219101

--- src/main/java/com/google/devtools/build/lib/analysis/test/TestTargetProperties.java
+++ src/main/java/com/google/devtools/build/lib/analysis/test/TestTargetProperties.java
@@ -94,8 +94,24 @@ public class TestTargetProperties {
 
     Map<String, String> executionInfo = Maps.newLinkedHashMap();
     executionInfo.putAll(TargetUtils.getExecutionInfo(rule));
-    if (TargetUtils.isLocalTestRule(rule) || TargetUtils.isExclusiveTestRule(rule)) {
-      executionInfo.put(ExecutionRequirements.LOCAL, "");
+
+    boolean incompatibleExclusiveTestSandboxed = false;
+
+    TestConfiguration testConfiguration = ruleContext.getFragment(TestConfiguration.class);
+    if (testConfiguration != null) {
+      incompatibleExclusiveTestSandboxed = testConfiguration.incompatibleExclusiveTestSandboxed();
+    }
+
+    if (incompatibleExclusiveTestSandboxed) {
+      if (TargetUtils.isLocalTestRule(rule)) {
+        executionInfo.put(ExecutionRequirements.LOCAL, "");
+      } else if (TargetUtils.isExclusiveTestRule(rule)) {
+        executionInfo.put(ExecutionRequirements.NO_REMOTE_EXEC, "");
+      }
+    } else {
+      if (TargetUtils.isLocalTestRule(rule) || TargetUtils.isExclusiveTestRule(rule)) {
+        executionInfo.put(ExecutionRequirements.LOCAL, "");
+      }
     }
 
     if (executionRequirements != null) {
