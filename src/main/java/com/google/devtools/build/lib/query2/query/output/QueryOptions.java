02cbcd2cf0ace5479e43228ffc7ceab6853d4cff
Greg Estren <gregestren@gmail.com>
2020-10-19 12:15:13 -0700

    Implement cquery --output=graph
    Thankfully, `query` already has most of the infrastructure necessary to make this
    easy.
    
    query implements graph output (in `GraphOutputFormatter`) over a
    `Digraph<Target>`, which is a generic graph data structure with `Target` payloads.
    All output logic then runs over this data structure. To opt query in, all we have
    to do is create an equivalent `Digraph<ConfiguredTarget>`, which is a simple
    transformation from the backing graph.
    
    This change creates a new generic class for that common logic:
    `GraphOutputWriter`. query's `GraphOutputFormatter` then becomes a simple wrapper
    over that, and the new `GraphOutputFormatterCallback` is cquery's equivalent.
    
    A few differences:
    
     - cquery output is always fully ordered (`--order_output=full`). We could match
       this with query's controllable version, but I don't see a reason to make this
       yet another bit to configured.
     - query output annotates edges with select() conditions. cquery doesn't do this
       because select()s are resolved and removed from the graph after analysis. I
       think we could annotate edges with the *chosen* condition if there was
       demand, but that'd be a followup effort.
    
    Fixes https://github.com/bazelbuild/bazel/issues/10843 (for `cquery`, not `aquery`)
    
    Closes #12248.
    
    PiperOrigin-RevId: 337907070

--- src/main/java/com/google/devtools/build/lib/query2/query/output/QueryOptions.java
+++ src/main/java/com/google/devtools/build/lib/query2/query/output/QueryOptions.java
@@ -53,13 +53,6 @@ public class QueryOptions extends CommonQueryOptions {
       help = "Whether each format is terminated with \\0 instead of newline.")
   public Void isNull;
 
-  @Option(
-      name = "line_terminator_null",
-      defaultValue = "false",
-      documentationCategory = OptionDocumentationCategory.QUERY,
-      effectTags = {OptionEffectTag.TERMINAL_OUTPUT},
-      help = "Whether each format is terminated with \\0 instead of newline.")
-  public boolean lineTerminatorNull;
 
   @Option(
     name = "order_results",
@@ -135,18 +128,6 @@ public class QueryOptions extends CommonQueryOptions {
   public boolean preferUnorderedOutput;
 
   @Option(
-    name = "graph:node_limit",
-    defaultValue = "512",
-    documentationCategory = OptionDocumentationCategory.QUERY,
-    effectTags = {OptionEffectTag.TERMINAL_OUTPUT},
-    help =
-        "The maximum length of the label string for a graph node in the output.  Longer labels"
-            + " will be truncated; -1 means no truncation.  This option is only applicable to"
-            + " --output=graph."
-  )
-  public int graphNodeStringLimit;
-
-  @Option(
       name = "graph:conditional_edges_limit",
       defaultValue = "4",
       documentationCategory = OptionDocumentationCategory.QUERY,
@@ -155,18 +136,6 @@ public class QueryOptions extends CommonQueryOptions {
           "The maximum number of condition labels to show. -1 means no truncation and 0 means no "
               + "annotation. This option is only applicable to --output=graph.")
   public int graphConditionalEdgesLimit;
-
-  @Option(
-    name = "graph:factored",
-    defaultValue = "true",
-    documentationCategory = OptionDocumentationCategory.QUERY,
-    effectTags = {OptionEffectTag.TERMINAL_OUTPUT},
-    help =
-        "If true, then the graph will be emitted 'factored', i.e. topologically-equivalent nodes "
-            + "will be merged together and their labels concatenated. This option is only "
-            + "applicable to --output=graph."
-  )
-  public boolean graphFactored;
 
   @Option(
     name = "xml:line_numbers",
@@ -242,15 +211,6 @@ public class QueryOptions extends CommonQueryOptions {
               + " return 3, no-keep-going will return 7; seven means all failed queries will"
               + " return 7; underlying means failed queries will return the underlying exit code")
   public QueryFailureExitCodeBehavior queryFailureExitCodeBehavior;
-
-  /** Ugly workaround since line terminator option default has to be constant expression. */
-  public String getLineTerminator() {
-    if (lineTerminatorNull) {
-      return "\0";
-    }
-
-    return System.lineSeparator();
-  }
 
   /** Return the current options as a set of QueryEnvironment settings. */
   @Override
