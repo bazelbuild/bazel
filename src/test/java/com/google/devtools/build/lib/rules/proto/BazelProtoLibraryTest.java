4a2415c7f478c454c160eea78c9bcda9f567c67b
Yannic Bonenberger <contact@yannic-bonenberger.com>
2020-11-09 12:14:23 +0100

    Merge SharedProtoLibraryTest into BazelProtoLibraryTest

--- src/test/java/com/google/devtools/build/lib/rules/proto/BazelProtoLibraryTest.java
+++ src/test/java/com/google/devtools/build/lib/rules/proto/BazelProtoLibraryTest.java
@@ -1078,16 +1078,247 @@ public class BazelProtoLibraryTest extends BuildViewTestCase {
 
     {
       Iterable<String> commandLine = paramFileArgsForAction(getDescriptorWriteAction("//x:a"));
-      String genfiles = getTargetConfiguration().getGenfilesFragment(RepositoryName.MAIN).toString();
       assertThat(commandLine)
           .containsAtLeast("-Ix/a.proto=x/a.proto", "-Ia.proto=external/foo/a.proto");
     }
 
     {
       Iterable<String> commandLine = paramFileArgsForAction(getDescriptorWriteAction("//x:c"));
-      String genfiles = getTargetConfiguration().getGenfilesFragment(RepositoryName.MAIN).toString();
       assertThat(commandLine)
           .containsAtLeast("-Ix/c.proto=x/c.proto", "-Ia/b/c.proto=external/foo/a/b/c.proto");
     }
+  }
+
+  // The tests below are for Bazel **and** Blaze.
+  //
+  // TODO(lberki): Extract these into their own test suite (`SharedProtoLibraryTest`).
+
+  /** If this is Blaze, simulate behaviour of Bazel's {@code proto_library} rule. */
+  private void simulateBazel() throws Exception {
+    if (!isThisBazel()) {
+      useConfiguration("--incompatible_generated_protos_in_virtual_imports=true");
+    }
+  }
+
+  /** If this is Bazel, simulate behaviour of Blaze's {@code proto_library} rule. */
+  private void simulateBlaze() throws Exception {
+    if (isThisBazel()) {
+      useConfiguration("--incompatible_generated_protos_in_virtual_imports=false");
+    }
+  }
+
+  private void setUpForSharedTests() throws Exception {
+    if (isThisBazel()) {
+      scratch.file(
+          "proto/defs.bzl",
+          "load('@rules_proto//proto:defs.bzl', proto_lib = 'proto_library')",
+          "def proto_library(**kwargs):",
+          "  proto_lib(**kwargs)");
+    } else {
+      // This is Blaze.
+      scratch.file(
+          "proto/defs.bzl", "def proto_library(**kwargs):", "  native.proto_library(**kwargs)");
+    }
+  }
+
+  @Test
+  public void testProtoLibrary() throws Exception {
+    setUpForSharedTests();
+
+    scratch.file(
+        "x/BUILD",
+        "load('//proto:defs.bzl', 'proto_library')",
+        "proto_library(name='foo', srcs=['a.proto', 'b.proto', 'c.proto'])");
+
+    ProtoInfo provider = getConfiguredTarget("//x:foo").get(ProtoInfo.PROVIDER);
+    assertThat(
+        Iterables.transform(
+            provider.getDirectSources(), s -> s.getSourceFile().getExecPath().getPathString()))
+        .containsExactly("x/a.proto", "x/b.proto", "x/c.proto");
+    assertThat(
+        Iterables.transform(
+            provider.getDirectSources(),
+            s -> s.getOriginalSourceFile().getExecPath().getPathString()))
+        .containsExactly("x/a.proto", "x/b.proto", "x/c.proto");
+    assertThat(
+        Iterables.transform(
+            provider.getDirectSources(), s -> s.getSourceRoot().getSafePathString()))
+        .containsExactly(".", ".", ".");
+    assertThat(
+        Iterables.transform(
+            provider.getDirectSources(), s -> s.getImportPath().getPathString()))
+        .containsExactly("x/a.proto", "x/b.proto", "x/c.proto");
+  }
+
+  @Test
+  public void testProtoLibraryWithoutSources() throws Exception {
+    setUpForSharedTests();
+
+    scratch.file(
+        "x/BUILD", "load('//proto:defs.bzl', 'proto_library')", "proto_library(name='foo')");
+
+    ProtoInfo provider = getConfiguredTarget("//x:foo").get(ProtoInfo.PROVIDER);
+    assertThat(provider.getDirectSources()).isEmpty();
+  }
+
+  @Test
+  public void testProtoLibraryWithVirtualProtoSourceRoot() throws Exception {
+    setUpForSharedTests();
+
+    scratch.file(
+        "x/BUILD",
+        "load('//proto:defs.bzl', 'proto_library')",
+        "proto_library(name='foo', srcs=['a.proto'], import_prefix='foo')");
+
+    String genfiles = getTargetConfiguration().getGenfilesFragment(RepositoryName.MAIN).toString();
+    ProtoInfo provider = getConfiguredTarget("//x:foo").get(ProtoInfo.PROVIDER);
+    assertThat(
+        Iterables.transform(
+            provider.getDirectSources(), s -> s.getSourceFile().getExecPath().getPathString()))
+        .containsExactly(genfiles + "/x/_virtual_imports/foo/foo/x/a.proto");
+    assertThat(
+        Iterables.transform(
+            provider.getDirectSources(),
+            s -> s.getOriginalSourceFile().getExecPath().getPathString()))
+        .containsExactly("x/a.proto");
+    assertThat(
+        Iterables.transform(
+            provider.getDirectSources(), s -> s.getSourceRoot().getSafePathString()))
+        .containsExactly(genfiles + "/x/_virtual_imports/foo");
+    assertThat(
+        Iterables.transform(
+            provider.getDirectSources(), s -> s.getImportPath().getPathString()))
+        .containsExactly("foo/x/a.proto");
+  }
+
+  @Test
+  public void testProtoLibraryWithGeneratedSources_Bazel() throws Exception {
+    setUpForSharedTests();
+    simulateBazel();
+
+    scratch.file(
+        "x/BUILD",
+        "load('//proto:defs.bzl', 'proto_library')",
+        "genrule(name='g', srcs=[], outs=['generated.proto'], cmd='')",
+        "proto_library(name='foo', srcs=['generated.proto'])");
+
+    String genfiles = getTargetConfiguration().getGenfilesFragment(RepositoryName.MAIN).toString();
+    ProtoInfo provider = getConfiguredTarget("//x:foo").get(ProtoInfo.PROVIDER);
+    assertThat(
+        Iterables.transform(
+            provider.getDirectSources(), s -> s.getSourceFile().getExecPath().getPathString()))
+        .containsExactly(genfiles + "/x/_virtual_imports/foo/x/generated.proto");
+    assertThat(
+        Iterables.transform(
+            provider.getDirectSources(),
+            s -> s.getOriginalSourceFile().getExecPath().getPathString()))
+        .containsExactly(genfiles + "/x/generated.proto");
+    assertThat(
+        Iterables.transform(
+            provider.getDirectSources(), s -> s.getSourceRoot().getSafePathString()))
+        .containsExactly(genfiles + "/x/_virtual_imports/foo");
+    assertThat(
+        Iterables.transform(
+            provider.getDirectSources(), s -> s.getImportPath().getPathString()))
+        .containsExactly("x/generated.proto");
+  }
+
+  @Test
+  public void testProtoLibraryWithGeneratedSources_Blaze() throws Exception {
+    setUpForSharedTests();
+    simulateBlaze();
+
+    scratch.file(
+        "x/BUILD",
+        "load('//proto:defs.bzl', 'proto_library')",
+        "genrule(name='g', srcs=[], outs=['generated.proto'], cmd='')",
+        "proto_library(name='foo', srcs=['generated.proto'])");
+
+    String genfiles = getTargetConfiguration().getGenfilesFragment(RepositoryName.MAIN).toString();
+    ProtoInfo provider = getConfiguredTarget("//x:foo").get(ProtoInfo.PROVIDER);
+    assertThat(
+        Iterables.transform(
+            provider.getDirectSources(), s -> s.getSourceFile().getExecPath().getPathString()))
+        .containsExactly(genfiles + "/x/generated.proto");
+    assertThat(
+        Iterables.transform(
+            provider.getDirectSources(),
+            s -> s.getOriginalSourceFile().getExecPath().getPathString()))
+        .containsExactly(genfiles + "/x/generated.proto");
+    assertThat(
+        Iterables.transform(
+            provider.getDirectSources(), s -> s.getSourceRoot().getSafePathString()))
+        .containsExactly(genfiles);
+    assertThat(
+        Iterables.transform(
+            provider.getDirectSources(), s -> s.getImportPath().getPathString()))
+        .containsExactly("x/generated.proto");
+  }
+
+  @Test
+  public void testProtoLibraryWithMixedSources_Bazel() throws Exception {
+    setUpForSharedTests();
+    simulateBazel();
+
+    scratch.file(
+        "x/BUILD",
+        "load('//proto:defs.bzl', 'proto_library')",
+        "genrule(name='g', srcs=[], outs=['generated.proto'], cmd='')",
+        "proto_library(name='foo', srcs=['generated.proto', 'a.proto'])");
+
+    String genfiles = getTargetConfiguration().getGenfilesFragment(RepositoryName.MAIN).toString();
+    ProtoInfo provider = getConfiguredTarget("//x:foo").get(ProtoInfo.PROVIDER);
+    assertThat(
+        Iterables.transform(
+            provider.getDirectSources(), s -> s.getSourceFile().getExecPath().getPathString()))
+        .containsExactly(
+            genfiles + "/x/_virtual_imports/foo/x/generated.proto",
+            genfiles + "/x/_virtual_imports/foo/x/a.proto");
+    assertThat(
+        Iterables.transform(
+            provider.getDirectSources(),
+            s -> s.getOriginalSourceFile().getExecPath().getPathString()))
+        .containsExactly(genfiles + "/x/generated.proto", "x/a.proto");
+    assertThat(
+        Iterables.transform(
+            provider.getDirectSources(), s -> s.getSourceRoot().getSafePathString()))
+        .containsExactly(
+            genfiles + "/x/_virtual_imports/foo", genfiles + "/x/_virtual_imports/foo");
+    assertThat(
+        Iterables.transform(
+            provider.getDirectSources(), s -> s.getImportPath().getPathString()))
+        .containsExactly("x/generated.proto", "x/a.proto");
+  }
+
+  @Test
+  public void testProtoLibraryWithMixedSources_Blaze() throws Exception {
+    setUpForSharedTests();
+    simulateBlaze();
+
+    scratch.file(
+        "x/BUILD",
+        "load('//proto:defs.bzl', 'proto_library')",
+        "genrule(name='g', srcs=[], outs=['generated.proto'], cmd='')",
+        "proto_library(name='foo', srcs=['generated.proto', 'a.proto'])");
+
+    String genfiles = getTargetConfiguration().getGenfilesFragment(RepositoryName.MAIN).toString();
+    ProtoInfo provider = getConfiguredTarget("//x:foo").get(ProtoInfo.PROVIDER);
+    assertThat(
+        Iterables.transform(
+            provider.getDirectSources(), s -> s.getSourceFile().getExecPath().getPathString()))
+        .containsExactly(genfiles + "/x/generated.proto", "x/a.proto");
+    assertThat(
+        Iterables.transform(
+            provider.getDirectSources(),
+            s -> s.getOriginalSourceFile().getExecPath().getPathString()))
+        .containsExactly(genfiles + "/x/generated.proto", "x/a.proto");
+    assertThat(
+        Iterables.transform(
+            provider.getDirectSources(), s -> s.getSourceRoot().getSafePathString()))
+        .containsExactly(genfiles, ".");
+    assertThat(
+        Iterables.transform(
+            provider.getDirectSources(), s -> s.getImportPath().getPathString()))
+        .containsExactly("x/generated.proto", "x/a.proto");
   }
 }
