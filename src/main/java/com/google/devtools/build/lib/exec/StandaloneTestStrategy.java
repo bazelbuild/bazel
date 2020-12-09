9247b2200c564602498f88ab7a9421920d7c096b
Yun Peng <pcloudy@google.com>
2020-12-09 16:03:42 +0100

    Some refactor

--- src/main/java/com/google/devtools/build/lib/exec/StandaloneTestStrategy.java
+++ src/main/java/com/google/devtools/build/lib/exec/StandaloneTestStrategy.java
@@ -401,7 +401,7 @@ public class StandaloneTestStrategy extends TestStrategy {
             Long.toString(result.getWallTime().orElse(Duration.ZERO).getSeconds()),
             Integer.toString(result.exitCode()));
     ImmutableMap.Builder<String, String> envBuilder = ImmutableMap.builder();
-    envBuilder.put("TEST_NAME", action.getTestName()).putAll(testEnv);
+    envBuilder.putAll(testEnv).put("TEST_NAME", action.getTestName());
     if (!action.isSharded()) {
       envBuilder.put("TEST_SHARD_INDEX", "0");
       envBuilder.put("TEST_SHARD_INDEX", "0");
