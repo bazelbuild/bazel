056df7e4dbc3151d57831e082fa0d90e2f9a8c38
Tony Aiuto <aiuto@google.com>
2021-01-06 09:55:39 -0500

    fix typo

--- src/test/shell/testenv.sh
+++ src/test/shell/testenv.sh
@@ -535,8 +535,8 @@ http_archive(
     name = "rules_pkg",
     sha256 = "352c090cc3d3f9a6b4e676cf42a6047c16824959b438895a76c2989c6d7c246a",
     urls = [
-        "https://mirror.bazel.build/github.com/bazelbuild/rules_pkg/rules_pkg-0.2.5.tar.gz",
-        "https://github.com/bazelbuild/rules_pkg/releases/download/0.2.0/rules_pkg-0.2.5.tar.gz",
+        "https://mirror.bazel.build/github.com/bazelbuild/rules_pkg/releases/download/0.2.5/rules_pkg-0.2.5.tar.gz",
+        "https://github.com/bazelbuild/rules_pkg/releases/download/0.2.5/rules_pkg-0.2.5.tar.gz",
     ],
 )
 EOF
