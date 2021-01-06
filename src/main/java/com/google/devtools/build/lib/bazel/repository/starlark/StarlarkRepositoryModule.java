f15bc640eece703d7e98ff10fb160d8fe6829b1a
adonovan <adonovan@google.com>
2021-01-06 09:13:24 -0800

    bazel packages: use EventHandler not EvalException in .bzl "export" operation
    This allows us to delete one of the deprecated EvalException(Location,...)
    constructors. Similar follow-up changes (events not exceptions) will be
    required to eliminate the remaining such constructor.
    
    As a bonus, this approach allows multiple errors to be reported at once.
    
    PiperOrigin-RevId: 350365352

--- src/main/java/com/google/devtools/build/lib/bazel/repository/starlark/StarlarkRepositoryModule.java
+++ src/main/java/com/google/devtools/build/lib/bazel/repository/starlark/StarlarkRepositoryModule.java
@@ -25,6 +25,7 @@ import com.google.devtools.build.lib.analysis.BaseRuleClasses;
 import com.google.devtools.build.lib.analysis.starlark.StarlarkAttrModule.Descriptor;
 import com.google.devtools.build.lib.cmdline.Label;
 import com.google.devtools.build.lib.cmdline.LabelSyntaxException;
+import com.google.devtools.build.lib.events.EventHandler;
 import com.google.devtools.build.lib.packages.AttributeValueSource;
 import com.google.devtools.build.lib.packages.BazelModuleContext;
 import com.google.devtools.build.lib.packages.BazelStarlarkContext;
@@ -141,7 +142,7 @@ public class StarlarkRepositoryModule implements RepositoryModuleApi {
     }
 
     @Override
-    public void export(Label extensionLabel, String exportedName) {
+    public void export(EventHandler handler, Label extensionLabel, String exportedName) {
       this.extensionLabel = extensionLabel;
       this.exportedName = exportedName;
     }
