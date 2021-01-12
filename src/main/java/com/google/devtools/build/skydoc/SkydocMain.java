bazelbuild/bazel

commit 1f9b8ede2a59c3ef67d352f68278ca45fe6f115d

BEGIN_OSS starlark: make load statements bind file-locally
With this change, load statements create file-local bindings,
not global ones; a function that references a load binding
is a closure. There is no longer any need to maintain a
separate hash table in Module.getExportedGlobals that records
the "exported" globals.

The FileOptions.loadBindsGlobally option causes load statements
to create global bindings. It is used for the prelude
(which principally contains load statements), for the REPL,
where load statements in one chunk should be visible in the next,
and for BUILD files, which frequently, and for reasons unclear,
say "load(..., "x"); x=1".

This is a breaking Java API change for Copybara.
END_PUBLIC

PiperOrigin-RevId: 346753608

src/main/java/com/google/devtools/build/skydoc/SkydocMain.java
@@ -275,7 +275,7 @@ public class SkydocMain {
                 Collectors.toMap(AspectInfoWrapper::getIdentifierFunction, Functions.identity()));
 
     // Sort the globals bindings by name.
-    TreeMap<String, Object> sortedBindings = new TreeMap<>(module.getExportedGlobals());
+    TreeMap<String, Object> sortedBindings = new TreeMap<>(module.getGlobals());
 
     for (Entry<String, Object> envEntry : sortedBindings.entrySet()) {
       if (ruleFunctions.containsKey(envEntry.getValue())) {

src/main/java/com/google/devtools/build/lib/skyframe/BzlCompileFunction.java
@@ -160,8 +160,11 @@ public class BzlCompileFunction implements SkyFunction {
     ParserInput input = ParserInput.fromLatin1(bytes, inputName);
     FileOptions options =
         FileOptions.builder()
-            // TODO(adonovan): add this, so that loads can normally be truly local.
-            // .loadBindsGlobally(key.isBuildPrelude())
+            // By default, Starlark load statements create file-local bindings.
+            // However, the BUILD prelude typically contains nothing but load
+            // statements whose bindings are intended to be visible in all BUILD
+            // files. The loadBindsGlobally flag allows us to retrieve them.
+            .loadBindsGlobally(key.isBuildPrelude())
             .restrictStringEscapes(
                 semantics.getBool(BuildLanguageOptions.INCOMPATIBLE_RESTRICT_STRING_ESCAPES))
             .build();

src/main/java/com/google/devtools/build/lib/skyframe/PackageFunction.java
@@ -1384,6 +1384,11 @@ public class PackageFunction implements SkyFunction {
     FileOptions options =
         FileOptions.builder()
             .requireLoadStatementsFirst(false)
+            // For historical reasons, BUILD files are allowed to load a symbol
+            // and then reassign it later. (It is unclear why this is neccessary).
+            // TODO(adonovan): remove this flag and make loads bind file-locally,
+            // as in .bzl files. One can always use a renaming load statement.
+            .loadBindsGlobally(true)
             .allowToplevelRebinding(true)
             .restrictStringEscapes(
                 semantics.getBool(BuildLanguageOptions.INCOMPATIBLE_RESTRICT_STRING_ESCAPES))

src/main/java/com/google/devtools/build/lib/skyframe/WorkspaceFileFunction.java
@@ -127,6 +127,9 @@ public class WorkspaceFileFunction implements SkyFunction {
             // the set of valid load labels, so load statements cannot all
             // be migrated to the top of the file.
             .requireLoadStatementsFirst(false)
+            // Bindings created by load statements in one
+            // chunk must be accessible to later chunks.
+            .loadBindsGlobally(true)
             // Top-level rebinding is permitted because historically
             // WORKSPACE files followed BUILD norms, but this should
             // probably be flipped.

src/main/java/com/google/devtools/build/lib/skyframe/serialization/testutils/TestUtils.java
@@ -103,9 +103,6 @@ public class TestUtils {
   public static void assertModulesEqual(Module module1, Module module2) {
     assertThat(module1.getClientData()).isEqualTo(module2.getClientData());
     assertThat(module1.getGlobals()).containsExactlyEntriesIn(module2.getGlobals()).inOrder();
-    assertThat(module1.getExportedGlobals())
-        .containsExactlyEntriesIn(module2.getExportedGlobals())
-        .inOrder();
     assertThat(module1.getPredeclaredBindings())
         .containsExactlyEntriesIn(module2.getPredeclaredBindings())
         .inOrder();

src/main/java/net/starlark/java/cmd/Main.java
@@ -50,9 +50,9 @@ class Main {
   private static final StarlarkThread thread;
   private static final Module module = Module.create();
 
-  // TODO(adonovan): set load-binds-globally option when we support load,
-  // so that loads bound in one REPL chunk are visible in the next.
-  private static final FileOptions OPTIONS = FileOptions.DEFAULT;
+  // Variables bound by load in one REPL chunk are visible in the next.
+  private static final FileOptions OPTIONS =
+      FileOptions.DEFAULT.toBuilder().allowToplevelRebinding(true).loadBindsGlobally(true).build();
 
   static {
     Mutability mu = Mutability.create("interpreter");

src/main/java/net/starlark/java/eval/Eval.java
@@ -223,32 +223,23 @@ final class Eval {
     Module module = loader.load(moduleName);
     if (module == null) {
       fr.setErrorLocation(node.getStartLocation());
-      throw Starlark.errorf(
-          "file '%s' was not correctly loaded. Make sure the 'load' statement appears in the"
-              + " global scope in your file",
-          moduleName);
+      throw Starlark.errorf("module '%s' not found", moduleName);
     }
-    Map<String, Object> globals = module.getExportedGlobals();
 
     for (LoadStatement.Binding binding : node.getBindings()) {
       // Extract symbol.
       Identifier orig = binding.getOriginalName();
-      Object value = globals.get(orig.getName());
+      Object value = module.getGlobal(orig.getName());
       if (value == null) {
         fr.setErrorLocation(orig.getStartLocation());
         throw Starlark.errorf(
             "file '%s' does not contain symbol '%s'%s",
-            moduleName, orig.getName(), SpellChecker.didYouMean(orig.getName(), globals.keySet()));
+            moduleName,
+            orig.getName(),
+            SpellChecker.didYouMean(orig.getName(), module.getGlobals().keySet()));
       }
 
-      // Define module-local variable.
-      // TODO(adonovan): eventually the default behavior should be that
-      // loads bind file-locally. Either way, the resolver should designate
-      // the proper scope of binding.getLocalName() and this should become
-      // simply assign(binding.getLocalName(), value).
-      // Currently, we update the module but not module.exportedGlobals.
-      // Change it to a local binding now that closures are supported.
-      fn(fr).setGlobal(binding.getLocalName().getBinding().getIndex(), value);
+      assignIdentifier(fr, binding.getLocalName(), value);
     }
   }
 
@@ -349,11 +340,7 @@ final class Eval {
         ((StarlarkFunction.Cell) fr.locals[bind.getIndex()]).x = value;
         break;
       case GLOBAL:
-        // Updates a module binding and sets its 'exported' flag.
-        // (Only load bindings are not exported.
-        // But exportedGlobals does at run time what should be done in the resolver.)
         fn(fr).setGlobal(bind.getIndex(), value);
-        fn(fr).getModule().exportedGlobals.add(id.getName());
         break;
       default:
         throw new IllegalStateException(bind.getScope().toString());

src/main/java/net/starlark/java/eval/Module.java
@@ -56,11 +56,6 @@ public final class Module implements Resolver.Module {
   private final LinkedHashMap<String, Integer> globalIndex = new LinkedHashMap<>();
   private Object[] globals = new Object[8];
 
-  // Names of globals that are exported and can be loaded from other modules.
-  // TODO(adonovan): eliminate this field when the resolver treats loads as local bindings.
-  // Then all globals are exported.
-  final HashSet<String> exportedGlobals = new HashSet<>();
-
   // An optional piece of metadata associated with the module/file.
   // May be set after construction (too obscure to burden the constructors).
   // Its toString appears to Starlark in str(function): "<function f from ...>".
@@ -172,23 +167,6 @@ public final class Module implements Resolver.Module {
       }
     }
     return m.build();
-  }
-
-  /**
-   * Returns a map of bindings that are exported (i.e. symbols declared using `=` and `def`, but not
-   * `load`).
-   */
-  // TODO(adonovan): whether bindings are exported should be decided by the resolver;
-  //  non-exported bindings should never be added to the module.  Delete this,
-  //  once loads bind locally (then all globals will be exported).
-  public ImmutableMap<String, Object> getExportedGlobals() {
-    ImmutableMap.Builder<String, Object> result = new ImmutableMap.Builder<>();
-    for (Map.Entry<String, Object> entry : getGlobals().entrySet()) {
-      if (exportedGlobals.contains(entry.getKey())) {
-        result.put(entry);
-      }
-    }
-    return result.build();
   }
 
   /** Implements the resolver's module interface. */

commit 0f8f2dc2ceb5ea63aa520bb7624aade4aad68fc3

skydoc: pay interest on fakebuildapi technical debt
commit 1f9b8ede2a59c3ef67d352f68278ca45fe6f115d caused symbols defined by load to no longer be recorded in
Module.globals, as the spec requires, but this broke the fragile
hacks used by the FakeStarlarkAttrModuleApi to guess the name of a
provider. This change causes the fakery to more faithfully simulate
Bazel's "export" mechanism (itself a hack) so that providers receive
a name when a top-level "name = provider()" statement is executed.

Also, all built-in providers that use the FakeProviderApi class (instead
of a specialized subclass) now explicitly specify their names at construction,
so that the heuristics get the correct name for nested fields such as cc_common.ToolchainInfo.

Nested fields that define specialized built-in providers still yield the
wrong value, but there are no tests that cover it and no complaints heard
so far, and this CL is penance enough already.

Fixes bazelbuild/stardoc#33

BUG=175703093
PiperOrigin-RevId: 351360957

src/main/java/com/google/devtools/build/skydoc/SkydocMain.java
@@ -34,6 +34,7 @@ import com.google.devtools.build.lib.starlarkbuildapi.java.GeneratedExtensionReg
 import com.google.devtools.build.lib.starlarkbuildapi.java.JavaNativeLibraryInfoApi;
 import com.google.devtools.build.lib.starlarkbuildapi.javascript.JsModuleInfoApi;
 import com.google.devtools.build.skydoc.fakebuildapi.FakeApi;
+import com.google.devtools.build.skydoc.fakebuildapi.FakeProviderApi;
 import com.google.devtools.build.skydoc.fakebuildapi.FakeStructApi;
 import com.google.devtools.build.skydoc.rendering.AspectInfoWrapper;
 import com.google.devtools.build.skydoc.rendering.DocstringParseException;
@@ -421,6 +422,14 @@ public class SkydocMain {
       StarlarkThread thread = new StarlarkThread(mu, semantics);
       // We use the default print handler, which writes to stderr.
       thread.setLoader(imports::get);
+      // Fake Bazel's "export" hack, by which provider symbols
+      // bound to global variables take on the name of the global variable.
+      thread.setPostAssignHook(
+          (name, value) -> {
+            if (value instanceof FakeProviderApi) {
+              ((FakeProviderApi) value).setName(name);
+            }
+          });
 
       Starlark.execFileProgram(prog, module, thread);
     } catch (EvalException ex) {

src/main/java/com/google/devtools/build/skydoc/fakebuildapi/FakeProviderApi.java
@@ -15,6 +15,7 @@
 package com.google.devtools.build.skydoc.fakebuildapi;
 
 import com.google.devtools.build.lib.starlarkbuildapi.core.ProviderApi;
+import javax.annotation.Nullable;
 import net.starlark.java.eval.Dict;
 import net.starlark.java.eval.Printer;
 import net.starlark.java.eval.StarlarkCallable;
@@ -24,12 +25,11 @@ import net.starlark.java.eval.Tuple;
 /** Fake callable implementation of {@link ProviderApi}. */
 public class FakeProviderApi implements StarlarkCallable, ProviderApi {
 
-  /**
-   * Each fake is constructed with a unique name, controlled by this counter being the name suffix.
-   */
-  private static int idCounter = 0;
+  private String name;
 
-  private final String name = "ProviderIdentifier" + idCounter++;
+  public FakeProviderApi(@Nullable String name) {
+    this.name = name;
+  }
 
   @Override
   public Object call(StarlarkThread thread, Tuple args, Dict<String, Object> kwargs) {
@@ -38,7 +38,14 @@ public class FakeProviderApi implements StarlarkCallable, ProviderApi {
 
   @Override
   public String getName() {
-    return name;
+    return name != null ? name : "Unexported Provider";
+  }
+
+  /** Called when provider is "exported" by a top-level assignment {@code name = provider()}. */
+  public void setName(String name) {
+    if (this.name == null) {
+      this.name = name;
+    }
   }
 
   @Override

src/main/java/com/google/devtools/build/skydoc/fakebuildapi/FakeStarlarkAttrModuleApi.java
@@ -259,29 +259,27 @@ public class FakeStarlarkAttrModuleApi implements StarlarkAttrModuleApi {
     return providerNameGroup;
   }
 
-  /**
-   * Returns the name of {@code provider}.
-   *
-   * <p>{@code thread} contains a {@code Map<String, Object>} where the values are built-in objects
-   * or objects defined in the file and the keys are the names of these objects. If a {@code
-   * provider} is in the map, the name of the provider is set as the key of this object in {@code
-   * bindings}. If it is not in the map, the provider may be part of a module in the map and the
-   * name will be set to "Unknown Provider".
-   */
+  // Returns the name of the provider using fragile heuristics.
   private static String providerName(ProviderApi provider, StarlarkThread thread) {
     Module bzl = Module.ofInnermostEnclosingStarlarkFunction(thread);
-    // user-defined provider?
-    for (Map.Entry<String, Object> e : bzl.getGlobals().entrySet()) {
-      if (provider.equals(e.getValue())) {
-        return e.getKey();
-      }
+
+    // Generic fake provider? (e.g. Starlark-defined, or trivial fake)
+    // Return name set at construction, or by "export" operation, if any.
+    if (provider instanceof FakeProviderApi) {
+      return ((FakeProviderApi) provider).getName(); // may be "Unexported Provider"
     }
-    // predeclared provider? (e.g. DefaultInfo)
+
+    // Specialized fake provider? (e.g. DefaultInfo)
+    // Return name under which FakeApi.addPredeclared added it to environment.
+    // (This only works for top-level names such as DefaultInfo, but not for
+    // nested ones such as cc_common.XyzInfo, but that has always been broken;
+    // it is not part of the regression that is b/175703093.)
     for (Map.Entry<String, Object> e : bzl.getPredeclaredBindings().entrySet()) {
       if (provider.equals(e.getValue())) {
         return e.getKey();
       }
     }
+
     return "Unknown Provider";
   }
 }

src/main/java/com/google/devtools/build/skydoc/fakebuildapi/FakeStarlarkRuleFunctionsApi.java
@@ -86,7 +86,7 @@ public class FakeStarlarkRuleFunctionsApi implements StarlarkRuleFunctionsApi<Fi
   @Override
   public ProviderApi provider(String doc, Object fields, StarlarkThread thread)
       throws EvalException {
-    FakeProviderApi fakeProvider = new FakeProviderApi();
+    FakeProviderApi fakeProvider = new FakeProviderApi(null);
     // Field documentation will be output preserving the order in which the fields are listed.
     ImmutableList.Builder<ProviderFieldInfo> providerFieldInfos = ImmutableList.builder();
     if (fields instanceof Sequence) {

src/main/java/com/google/devtools/build/skydoc/fakebuildapi/apple/FakeAppleCommon.java
@@ -81,32 +81,32 @@ public class FakeAppleCommon
 
   @Override
   public ProviderApi getXcodeVersionPropertiesConstructor() {
-    return new FakeProviderApi();
+    return new FakeProviderApi("XcodeProperties");
   }
 
   @Override
   public ProviderApi getXcodeVersionConfigConstructor() {
-    return new FakeProviderApi();
+    return new FakeProviderApi("XcodeVersionConfig");
   }
 
   @Override
   public ProviderApi getObjcProviderConstructor() {
-    return new FakeProviderApi();
+    return new FakeProviderApi("objc");
   }
 
   @Override
   public ProviderApi getAppleDynamicFrameworkConstructor() {
-    return new FakeProviderApi();
+    return new FakeProviderApi("AppleDynamicFramework");
   }
 
   @Override
   public ProviderApi getAppleDylibBinaryConstructor() {
-    return new FakeProviderApi();
+    return new FakeProviderApi("AppleDylibBinary");
   }
 
   @Override
   public ProviderApi getAppleExecutableBinaryConstructor() {
-    return new FakeProviderApi();
+    return new FakeProviderApi("AppleExecutableBinary");
   }
 
   @Override
@@ -116,12 +116,12 @@ public class FakeAppleCommon
 
   @Override
   public ProviderApi getAppleDebugOutputsConstructor() {
-    return new FakeProviderApi();
+    return new FakeProviderApi("AppleDebugOutputs");
   }
 
   @Override
   public ProviderApi getAppleLoadableBundleBinaryConstructor() {
-    return new FakeProviderApi();
+    return new FakeProviderApi("AppleLoadableBundleBinary");
   }
 
   @Override

src/main/java/com/google/devtools/build/skydoc/fakebuildapi/config/FakeConfigStarlarkCommon.java
@@ -25,6 +25,6 @@ public class FakeConfigStarlarkCommon implements ConfigStarlarkCommonApi {
 
   @Override
   public ProviderApi getConfigFeatureFlagProviderConstructor() {
-    return new FakeProviderApi();
+    return new FakeProviderApi("FeatureFlagInfo");
   }
 }

src/main/java/com/google/devtools/build/skydoc/fakebuildapi/cpp/FakeCcModule.java
@@ -73,7 +73,7 @@ public class FakeCcModule
 
   @Override
   public ProviderApi getCcToolchainProvider() {
-    return new FakeProviderApi();
+    return new FakeProviderApi("CcToolchainInfo");
   }
 
   @Override

src/main/java/com/google/devtools/build/skydoc/fakebuildapi/java/FakeJavaCommon.java
@@ -42,7 +42,7 @@ public class FakeJavaCommon
 
   @Override
   public ProviderApi getJavaProvider() {
-    return new FakeProviderApi();
+    return new FakeProviderApi("JavaInfo");
   }
 
   @Override
@@ -118,12 +118,12 @@ public class FakeJavaCommon
 
   @Override
   public ProviderApi getJavaToolchainProvider() {
-    return new FakeProviderApi();
+    return new FakeProviderApi("JavaToolchain");
   }
 
   @Override
   public ProviderApi getJavaRuntimeProvider() {
-    return new FakeProviderApi();
+    return new FakeProviderApi("JavaRuntime");
   }
 
   @Override
@@ -134,7 +134,7 @@ public class FakeJavaCommon
 
   @Override
   public ProviderApi getMessageBundleInfo() {
-    return new FakeProviderApi();
+    return new FakeProviderApi("JavaMessageBundle");
   }
 
   @Override

src/main/java/com/google/devtools/build/skydoc/fakebuildapi/platform/FakePlatformCommon.java
@@ -25,26 +25,26 @@ public class FakePlatformCommon implements PlatformCommonApi {
 
   @Override
   public ProviderApi getMakeVariableProvider() {
-    return new FakeProviderApi();
+    return new FakeProviderApi("TemplateVariableInfo");
   }
 
   @Override
   public ProviderApi getToolchainInfoConstructor() {
-    return new FakeProviderApi();
+    return new FakeProviderApi("ToolchainInfo");
   }
 
   @Override
   public ProviderApi getPlatformInfoConstructor() {
-    return new FakeProviderApi();
+    return new FakeProviderApi("PlatformInfo");
   }
 
   @Override
   public ProviderApi getConstraintSettingInfoConstructor() {
-    return new FakeProviderApi();
+    return new FakeProviderApi("ConstraintSettingInfo");
   }
 
   @Override
   public ProviderApi getConstraintValueInfoConstructor() {
-    return new FakeProviderApi();
+    return new FakeProviderApi("ConstraintValueInfo");
   }
 }