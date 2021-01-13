082d98772690946ed29c157e60640c97a6e1195b
larsrc <larsrc@google.com>
2021-01-13 06:40:59 -0800

    Implement available() method for Windows subprocesses.
    RELNOTES: None
    PiperOrigin-RevId: 351574909

--- src/main/native/windows/processes-jni.cc
+++ src/main/native/windows/processes-jni.cc
@@ -97,6 +97,33 @@ class NativeOutputStream {
 
   void SetHandle(HANDLE handle) { handle_ = handle; }
 
+  jint StreamBytesAvailable(JNIEnv* env) {
+    if (closed_.load() || handle_ == INVALID_HANDLE_VALUE) {
+      error_ = L"";
+      return 0;
+    }
+
+    DWORD avail = 0;
+    if (!::PeekNamedPipe(handle_, NULL, 0, NULL, &avail, NULL)) {
+      // Check if either the other end closed the pipe or we did it with
+      // NativeOutputStream.Close() . In the latter case, we'll get a "system
+      // call interrupted" error.
+      if (GetLastError() == ERROR_BROKEN_PIPE || closed_.load()) {
+        error_ = L"";
+        return 0;
+      } else {
+        DWORD err_code = GetLastError();
+        error_ = bazel::windows::MakeErrorMessage(WSTR(__FILE__), __LINE__,
+                                                  L"nativeStreamBytesAvailable",
+                                                  L"", err_code);
+        return -1;
+      }
+    } else {
+      error_ = L"";
+    }
+    return avail;
+  }
+
   jint ReadStream(JNIEnv* env, jbyteArray java_bytes, jint offset,
                   jint length) {
     JavaByteArray bytes(env, java_bytes);
@@ -210,6 +237,7 @@ class NativeProcess {
       }
     }
 
+    // Set up childs stdin pipe.
     {
       HANDLE pipe_read_h, pipe_write_h;
       if (!CreatePipe(&pipe_read_h, &pipe_write_h, &sa, 0)) {
@@ -220,6 +248,14 @@ class NativeProcess {
       }
       stdin_process = pipe_read_h;
       stdin_ = pipe_write_h;
+
+      // "Our" end of the pipe must not be inherited by the child process
+      if (!SetHandleInformation(pipe_write_h, HANDLE_FLAG_INHERIT, 0)) {
+        DWORD err_code = GetLastError();
+        error_ = bazel::windows::MakeErrorMessage(
+            WSTR(__FILE__), __LINE__, L"nativeCreateProcess", wpath, err_code);
+        return false;
+      }
     }
 
     if (!stdout_is_stream) {
@@ -260,6 +296,13 @@ class NativeProcess {
       }
       stdout_.SetHandle(pipe_read_h);
       stdout_process = pipe_write_h;
+      // "Our" end of the pipe must not be inherited by the child process
+      if (!SetHandleInformation(pipe_read_h, HANDLE_FLAG_INHERIT, 0)) {
+        DWORD err_code = GetLastError();
+        error_ = bazel::windows::MakeErrorMessage(
+            WSTR(__FILE__), __LINE__, L"nativeCreateProcess", wpath, err_code);
+        return false;
+      }
     }
 
     if (stderr_same_handle_as_stdout) {
@@ -314,6 +357,13 @@ class NativeProcess {
       }
       stderr_.SetHandle(pipe_read_h);
       stderr_process = pipe_write_h;
+      // "Our" end of the pipe must not be inherited by the child process
+      if (!SetHandleInformation(pipe_read_h, HANDLE_FLAG_INHERIT, 0)) {
+        DWORD err_code = GetLastError();
+        error_ = bazel::windows::MakeErrorMessage(
+            WSTR(__FILE__), __LINE__, L"nativeCreateProcess", wpath, err_code);
+        return false;
+      }
     }
     return proc_.Create(
         wpath, bazel::windows::GetJavaWstring(env, java_argv_rest),
@@ -441,6 +491,14 @@ Java_com_google_devtools_build_lib_windows_WindowsProcesses_readStream(
   NativeOutputStream* stream =
       reinterpret_cast<NativeOutputStream*>(stream_long);
   return stream->ReadStream(env, java_bytes, offset, length);
+}
+
+extern "C" JNIEXPORT jint JNICALL
+Java_com_google_devtools_build_lib_windows_WindowsProcesses_streamBytesAvailable(
+    JNIEnv* env, jclass clazz, jlong stream_long) {
+  NativeOutputStream* stream =
+      reinterpret_cast<NativeOutputStream*>(stream_long);
+  return stream->StreamBytesAvailable(env);
 }
 
 extern "C" JNIEXPORT jint JNICALL
