dfd6b9797f646f479c8735a5e36181b2a7f071b1
Tony Aiuto <aiuto@google.com>
2021-01-13 13:38:47 -0500

    Move rules_proto and protocolbuffers/ubp to distdir_deps.bzl
    - generate @rules_proto workspace inclusions in tests
    - src/test/java/com/google/devtools/build/lib/blackbox/framework/BlackBoxTestEnvironment.java will be done in a future CL. That will be the first case where we need to generate Java code. It requires some design up front.
    
    Context: #12081

--- src/test/shell/integration/modify_execution_info_test.sh
+++ src/test/shell/integration/modify_execution_info_test.sh
@@ -162,16 +162,9 @@ new_local_repository(
     build_file = "$(rlocation io_bazel/third_party/rules_python/BUILD)",
     workspace_file = "$(rlocation io_bazel/third_party/rules_python/rules_python.WORKSPACE)",
 )
-
-http_archive(
-    name = "rules_proto",
-    strip_prefix = "rules_proto-7e4afce6fe62dbff0a4a03450143146f9f2d7488",
-    sha256 = "8e7d59a5b12b233be5652e3d29f42fba01c7cbab09f6b3a8d0a57ed6d1e9a0da",
-    urls = [
-        "https://mirror.bazel.build/github.com/bazelbuild/rules_proto/archive/7e4afce6fe62dbff0a4a03450143146f9f2d7488.tar.gz",
-        "https://github.com/bazelbuild/rules_proto/archive/7e4afce6fe62dbff0a4a03450143146f9f2d7488.tar.gz",
-    ],
-)
+EOF
+    cat "$(rlocation "io_bazel/src/test/shell/integration/rules_proto_stanza.txt")" >>WORKSPACE
+    cat >> WORKSPACE << EOF
 load("@rules_proto//proto:repositories.bzl", "rules_proto_dependencies", "rules_proto_toolchains")
 rules_proto_dependencies()
 rules_proto_toolchains()
