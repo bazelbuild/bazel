99aa968df04519331bccdd87448c80bb1b4a7df7
John Cater <jcater@google.com>
2021-01-27 07:31:19 -0800

    Enable toolchain resolution for filegroup targets.
    Fixes #12899.
    
    Closes #12901.
    
    PiperOrigin-RevId: 354087352

--- src/main/java/com/google/devtools/build/lib/bazel/rules/common/BazelFilegroupRule.java
+++ src/main/java/com/google/devtools/build/lib/bazel/rules/common/BazelFilegroupRule.java
@@ -75,7 +75,6 @@ public final class BazelFilegroupRule implements RuleDefinition {
             attr("path", STRING)
                 .undocumented(
                     "only used to expose FilegroupPathProvider, which is not currently used"))
-        .useToolchainResolution(false)
         .build();
   }
 
