31553fdcbc724ce1c2928effab6914754e5e3f8e
mai93 <messa@google.com>
2021-02-01 15:07:40 +0200

    Update max path length to be 256 (MAX_PATH - 4)

--- src/main/native/windows/util.cc
+++ src/main/native/windows/util.cc
@@ -195,6 +195,9 @@ static bool Contains(const wstring& s, const WCHAR* substr) {
 }
 
 wstring AsShortPath(wstring path, wstring* result) {
+  // Using MAX_PATH - 4 (256) instead of MAX_PATH to fix https://github.com/bazelbuild/bazel/issues/12310
+  static const size_t kMaxPath = MAX_PATH - 4;
+
   if (path.empty()) {
     result->clear();
     return L"";
@@ -212,7 +215,7 @@ wstring AsShortPath(wstring path, wstring* result) {
     return MakeErrorMessage(WSTR(__FILE__), __LINE__, L"AsShortPath", path,
                             L"path is not normalized");
   }
-  if (path.size() >= MAX_PATH && !HasSeparator(path)) {
+  if (path.size() >= kMaxPath && !HasSeparator(path)) {
     return MakeErrorMessage(WSTR(__FILE__), __LINE__, L"AsShortPath", path,
                             L"path is just a file name but too long");
   }
@@ -221,15 +224,15 @@ wstring AsShortPath(wstring path, wstring* result) {
                             L"path is not absolute");
   }
   // At this point we know the path is either just a file name (shorter than
-  // MAX_PATH), or an absolute, normalized, Windows-style path (of any length).
+  // kMaxPath), or an absolute, normalized, Windows-style path (of any length).
 
   std::replace(path.begin(), path.end(), '/', '\\');
   // Fast-track: the path is already short.
-  if (path.size() < MAX_PATH) {
+  if (path.size() < kMaxPath) {
     *result = path;
     return L"";
   }
-  // At this point we know that the path is at least MAX_PATH long and that it's
+  // At this point we know that the path is at least kMaxPath long and that it's
   // absolute, normalized, and Windows-style.
 
   wstring wlong = wstring(L"\\\\?\\") + path;
@@ -239,10 +242,10 @@ wstring AsShortPath(wstring path, wstring* result) {
   //   did too (though this behavior is not documented on MSDN)
   // - CreateProcess{A,W} only accept an executable of MAX_PATH - 1 length
   // Therefore for our purposes the acceptable shortened length is
-  // MAX_PATH + 4 (null-terminated). That is, MAX_PATH - 1 for the shortened
+  // kMaxPath + 4 (null-terminated). That is, kMaxPath - 1 for the shortened
   // path, plus a potential "\\?\" prefix that's only there if `wlong` also had
   // it and which we'll omit from `result`, plus a null terminator.
-  static const size_t kMaxShortPath = MAX_PATH + 4;
+  static const size_t kMaxShortPath = kMaxPath + 4;
 
   WCHAR wshort[kMaxShortPath];
   DWORD wshort_size = ::GetShortPathNameW(wlong.c_str(), NULL, 0);
