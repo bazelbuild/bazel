20c0355e1091f2747564fb66dff866a5ecaa1d85
Yun Peng <pcloudy@google.com>
2021-02-17 18:35:01 +0100

    Support mips64 and riscv64
    Upstream some patches from the Bazel Debian package to make Bazel build
    on mips64 and riscv64
    
    https://salsa.debian.org/bazel-team/bazel-bootstrap/-/tree/olek-mips-riscv-3/debian/patches
    
    Fixes https://github.com/bazelbuild/bazel/issues/12683

--- src/main/java/com/google/devtools/build/lib/analysis/config/AutoCpuConverter.java
+++ src/main/java/com/google/devtools/build/lib/analysis/config/AutoCpuConverter.java
@@ -68,6 +68,10 @@ public class AutoCpuConverter implements Converter<String> {
               return "aarch64";
             case S390X:
               return "s390x";
+            case MIPS64:
+              return "mips64";
+            case RISCV64:
+              return "riscv64";
             default:
               return "unknown";
           }
@@ -107,6 +111,10 @@ public class AutoCpuConverter implements Converter<String> {
       return Pair.of(CPU.ARM, OS.LINUX);
     } else if (input.equals("s390x")) {
       return Pair.of(CPU.S390X, OS.LINUX);
+    } else if (input.equals("mips64")) {
+      return Pair.of(CPU.MIPS64, OS.LINUX);
+    } else if (input.equals("riscv64")) {
+      return Pair.of(CPU.RISCV64, OS.LINUX);
     }
 
     // Use the auto-detected values.
