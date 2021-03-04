9b32aad570174b5722b57035a4a412df70765d71
Googler <noreply@google.com>
2021-03-04 13:06:18 -0800

    Let Python main source file to have "-" in the path.
    RELNOTES: Release restriction for "-" in the package name for Python sources. Now `py_binary` and `py_test` targets can have main source file with "-" in the path.
    PiperOrigin-RevId: 360986619

--- src/main/java/com/google/devtools/build/lib/rules/python/PyCommon.java
+++ src/main/java/com/google/devtools/build/lib/rules/python/PyCommon.java
@@ -177,6 +177,9 @@ public final class PyCommon {
    */
   @Nullable private final Map<PathFragment, Artifact> convertedFiles;
 
+  // Null if requiresMainFile is false, or the main artifact couldn't be determined.
+  @Nullable private Artifact mainArtifact = null;
+
   private Artifact executable = null;
 
   private NestedSet<Artifact> filesToBuild = null;
@@ -190,16 +193,21 @@ public final class PyCommon {
   // TODO(bazel-team): validateSources is the result of refactoring while preserving
   // legacy behavior across some (but not all) Google-internal uses of PyCommon. Ideally all call
   // sites should be updated to expect the same validation steps.
-  public PyCommon(RuleContext ruleContext, PythonSemantics semantics, boolean validateSources) {
+  public PyCommon(
+      RuleContext ruleContext,
+      PythonSemantics semantics,
+      boolean validateSources,
+      boolean requiresMainFile) {
     this.ruleContext = ruleContext;
     this.semantics = semantics;
     this.version = ruleContext.getFragment(PythonConfiguration.class).getPythonVersion();
     this.sourcesVersion = initSrcsVersionAttr(ruleContext);
     this.dependencyTransitivePythonSources = initDependencyTransitivePythonSources(ruleContext);
     this.transitivePythonSources = initTransitivePythonSources(ruleContext);
+    this.mainArtifact = requiresMainFile ? initMainArtifact(ruleContext) : null;
     this.directPythonSources =
         initAndMaybeValidateDirectPythonSources(
-            ruleContext, semantics, /*validate=*/ validateSources);
+            ruleContext, semantics, /*validate=*/ validateSources, this.mainArtifact);
     this.usesSharedLibraries = initUsesSharedLibraries(ruleContext);
     this.imports = initImports(ruleContext, semantics);
     this.hasPy2OnlySources = initHasPy2OnlySources(ruleContext, this.sourcesVersion);
@@ -259,7 +267,7 @@ public final class PyCommon {
   }
 
   private static List<Artifact> initAndMaybeValidateDirectPythonSources(
-      RuleContext ruleContext, PythonSemantics semantics, boolean validate) {
+      RuleContext ruleContext, PythonSemantics semantics, boolean validate, Artifact mainArtifact) {
     List<Artifact> sourceFiles = new ArrayList<>();
     // TODO(bazel-team): Need to get the transitive deps closure, not just the sources of the rule.
     for (TransitiveInfoCollection src :
@@ -267,9 +275,13 @@ public final class PyCommon {
       // Make sure that none of the sources contain hyphens.
       if (validate
           && semantics.prohibitHyphensInPackagePaths()
-          && Util.containsHyphen(src.getLabel().getPackageFragment())) {
+          && Util.containsHyphen(src.getLabel().getPackageFragment())
+          // It's ok to have hyphens in main file - usually no one imports it.
+          && !src.getLabel().equals(mainArtifact.getOwnerLabel())) {
         ruleContext.attributeError(
-            "srcs", src.getLabel() + ": paths to Python sources may not contain '-'");
+            "srcs",
+            src.getLabel()
+                + ": paths to Python sources (besides the main source) may not contain '-'");
       }
       Iterable<Artifact> pySrcs =
           FileType.filter(
@@ -908,8 +920,8 @@ public final class PyCommon {
   @AutoCodec @AutoCodec.VisibleForSerialization
   static final GeneratedExtension<ExtraActionInfo, PythonInfo> PYTHON_INFO = PythonInfo.pythonInfo;
 
-  /** @return A String that is the full path to the main python entry point. */
-  public String determineMainExecutableSource(boolean withWorkspaceName) {
+  /** Returns the artifact for the main Python source file. */
+  private static Artifact initMainArtifact(RuleContext ruleContext) {
     String mainSourceName;
     Rule target = ruleContext.getRule();
     boolean explicitMain = target.isAttributeValueExplicitlySpecified("main");
@@ -920,6 +932,7 @@ public final class PyCommon {
       }
     } else {
       String ruleName = target.getName();
+      // TODO(blaze-team) Shouldn't the same check apply for all Python targets?
       if (ruleName.endsWith(".py")) {
         ruleContext.attributeError("name", "name must not end in '.py'");
       }
@@ -946,6 +959,18 @@ public final class PyCommon {
 
     if (mainArtifact == null) {
       ruleContext.attributeError("srcs", buildNoMainMatchesErrorText(explicitMain, mainSourceName));
+    }
+
+    return mainArtifact;
+  }
+
+  /**
+   * Returns the path to the main python entry point, or null if this PyCommon was initialized with
+   * {@code requiresMainFile} set to false (or there was an error in determining the main artifact).
+   */
+  @Nullable
+  public String determineMainExecutableSource(boolean withWorkspaceName) {
+    if (mainArtifact == null) {
       return null;
     }
     if (!withWorkspaceName) {
--- src/main/java/com/google/devtools/build/lib/rules/python/PyLibrary.java
+++ src/main/java/com/google/devtools/build/lib/rules/python/PyLibrary.java
@@ -40,7 +40,9 @@ public abstract class PyLibrary implements RuleConfiguredTargetFactory {
   public ConfiguredTarget create(RuleContext ruleContext)
       throws InterruptedException, RuleErrorException, ActionConflictException {
     PythonSemantics semantics = createSemantics();
-    PyCommon common = new PyCommon(ruleContext, semantics, /*validateSources=*/ true);
+    PyCommon common =
+        new PyCommon(
+            ruleContext, semantics, /*validateSources=*/ true, /*requiresMainFile=*/ false);
     semantics.validate(ruleContext, common);
 
     List<Artifact> srcs = common.getPythonSources();
