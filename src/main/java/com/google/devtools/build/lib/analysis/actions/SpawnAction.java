bazelbuild/bazel

commit 94602f05b34fda2556287797a14b7dac64f2f041

SpawnAction.Builder: use callable PathFragment
If you pass an Artifact or FilesToRunProvider to
Builder.setExecutable, it will now use the
callable path, not just the execpath.

Fixes https://github.com/bazelbuild/bazel/issues/13189

Unfortunately, call sites that pass PathFragment
are susceptible to the bug, because PathFragment
is inherently ambiguous: if it's a bare file name,
the shell resolves it against PATH, otherwise
against the current directory.

RELNOTES: none

Closes #13192.

PiperOrigin-RevId: 362889202

src/main/java/com/google/devtools/build/lib/analysis/actions/SpawnAction.java
@@ -962,7 +962,12 @@ public class SpawnAction extends AbstractAction implements CommandAction {
     }
 
     /**
-     * Sets the executable path; the path is interpreted relative to the execution root.
+     * Sets the executable path; the path is interpreted relative to the execution root, unless it's
+     * a bare file name.
+     *
+     * <p><b>Caution</b>: if the executable is a bare file name ("foo"), it will be interpreted
+     * relative to PATH. See https://github.com/bazelbuild/bazel/issues/13189 for details. To avoid
+     * that, use {@link #setExecutable(Artifact)} instead.
      *
      * <p>Calling this method overrides any previous values set via calls to {@link #setExecutable},
      * {@link #setJavaExecutable}, or {@link #setShellCommand}.
@@ -981,7 +986,9 @@ public class SpawnAction extends AbstractAction implements CommandAction {
      */
     public Builder setExecutable(Artifact executable) {
       addTool(executable);
-      return setExecutable(executable.getExecPath());
+      this.executableArgs = CustomCommandLine.builder().addCallablePath(executable.getExecPath());
+      this.isShellCommand = false;
+      return this;
     }
 
     /**
@@ -1007,7 +1014,7 @@ public class SpawnAction extends AbstractAction implements CommandAction {
     public Builder setExecutable(FilesToRunProvider executableProvider) {
       Preconditions.checkArgument(executableProvider.getExecutable() != null,
           "The target does not have an executable");
-      setExecutable(executableProvider.getExecutable().getExecPath());
+      setExecutable(executableProvider.getExecutable());
       return addTool(executableProvider);
     }
 

src/main/java/com/google/devtools/build/lib/analysis/actions/CustomCommandLine.java
@@ -797,6 +797,16 @@ public final class CustomCommandLine extends CommandLine {
     }
 
     /**
+     * Adds an artifact by calling {@link PathFragment#getCallablePathString}.
+     *
+     * <p>Prefer this over manually calling {@link PathFragment#getCallablePathString}, as it avoids
+     * storing a copy of the path string.
+     */
+    public Builder addCallablePath(@Nullable PathFragment value) {
+      return addObjectInternal(new CallablePathFragment(value));
+    }
+
+    /**
      * Adds an artifact by calling {@link PathFragment#getPathString}.
      *
      * <p>Prefer this over manually calling {@link PathFragment#getPathString}, as it avoids storing
@@ -1318,6 +1328,20 @@ public final class CustomCommandLine extends CommandLine {
       } else {
         fingerprint.addString(CommandLineItem.expandToCommandLine(substitutedArg));
       }
+    }
+  }
+
+  /** A {@link PathFragment} that is expanded with {@link PathFragment#getCallablePathString()}. */
+  private static final class CallablePathFragment {
+    public final PathFragment fragment;
+
+    CallablePathFragment(PathFragment fragment) {
+      this.fragment = fragment;
+    }
+
+    @Override
+    public String toString() {
+      return fragment.getCallablePathString();
     }
   }
 }

src/main/java/com/google/devtools/build/lib/analysis/actions/SpawnActionTemplate.java
@@ -361,10 +361,14 @@ public final class SpawnActionTemplate extends ActionKeyCacher
 
     /**
      * Sets the executable path used by expanded actions. The path is interpreted relative to the
-     * execution root.
+     * execution root, unless it's a bare file name.
      *
-     * <p>Calling this method overrides any previous values set via calls to
-     * {@link #setExecutable(Artifact)} and {@link #setExecutable(FilesToRunProvider)}.
+     * <p><b>Caution</b>: if the executable is a bare file name ("foo"), it will be interpreted
+     * relative to PATH. See https://github.com/bazelbuild/bazel/issues/13189 for details. To avoid
+     * that, use {@link #setExecutable(Artifact)} instead.
+     *
+     * <p>Calling this method overrides any previous values set via calls to {@link
+     * #setExecutable(Artifact)} and {@link #setExecutable(FilesToRunProvider)}.
      */
     public Builder setExecutable(PathFragment executable) {
       spawnActionBuilder.setExecutable(executable);

src/main/java/com/google/devtools/build/lib/rules/objc/ProtobufSupport.java
@@ -259,7 +259,7 @@ final class ProtobufSupport {
             .addTransitiveInputs(getAllProtos())
             .addOutputs(getGeneratedProtoOutputs(getAllProtos(), HEADER_SUFFIX))
             .addOutputs(getGeneratedProtoOutputs(getOutputProtos(), SOURCE_SUFFIX))
-            .setExecutable(attributes.getProtoCompiler().getExecPath())
+            .setExecutable(attributes.getProtoCompiler())
             .addCommandLine(getGenerationCommandLine(outputGroupFile, skipGroupFile))
             .build(ruleContext));
 

src/test/java/com/google/devtools/build/lib/analysis/actions/SpawnActionTest.java
@@ -158,6 +158,20 @@ public class SpawnActionTest extends BuildViewTestCase {
   }
 
   @Test
+  public void testBuilderWithExecutableInRootPcakage() throws Exception {
+    Artifact tool = getSourceArtifact("tool.bin");
+    Action[] actions =
+        builder()
+            .setExecutable(tool)
+            .addOutput(destinationArtifact)
+            .build(ActionsTestUtil.NULL_ACTION_OWNER, targetConfig);
+    collectingAnalysisEnvironment.registerAction(actions);
+    SpawnAction action = (SpawnAction) actions[0];
+    assertThat(action.getArguments()).hasSize(1);
+    assertThat(action.getArguments().get(0)).matches("\\.[/\\\\]tool.bin");
+  }
+
+  @Test
   public void testBuilderWithJavaExecutable() throws Exception {
     Action[] actions = builder()
         .addOutput(destinationArtifact)

src/test/shell/bazel/starlark_rule_test.sh
@@ -89,4 +89,53 @@ EOF
   expect_log "error executing command.*not_a_command"
 }
 
+# Regression test for https://github.com/bazelbuild/bazel/issues/13189
+function test_execute_tool_from_root_package() {
+  cat >BUILD <<'EOF'
+load("foo.bzl", "foo")
+
+foo(
+    name = "x",
+    out = "x.out",
+    tool = "bin.sh",
+)
+
+genrule(
+    name = "y",
+    outs = ["y.out"],
+    cmd = "$(location bin.sh) $@",
+    tools = ["bin.sh"],
+)
+EOF
+
+  cat >foo.bzl << 'EOF'
+def _impl(ctx):
+    ctx.actions.run(
+        outputs = [ctx.outputs.out],
+        executable = ctx.executable.tool,
+        arguments = [ctx.outputs.out.path],
+    )
+    return [DefaultInfo(files = depset([ctx.outputs.out]))]
+
+foo = rule(
+    implementation = _impl,
+    attrs = {
+        "tool": attr.label(allow_single_file = True, executable = True, cfg = "host"),
+        "out": attr.output(mandatory = True),
+    },
+)
+EOF
+
+  cat >bin.sh <<'EOF'
+#!/bin/bash
+echo hello $0 > $1
+EOF
+  chmod +x bin.sh
+
+  # //:x would fail without the bugfix of https://github.com/bazelbuild/bazel/issues/13189
+  bazel build //:x &> $TEST_log || fail "Expected sucesss"
+  bazel build //:y &> $TEST_log || fail "Expected success"
+  rm BUILD bin.sh foo.bzl
+}
+
 run_suite "Starlark rule definition tests"

commit 34bf4418e7e6b22311c74af3298c18a05f42cf7c

Fix performance regression in https://github.com/bazelbuild/bazel/commit/94602f05b34fda2556287797a14b7dac64f2f041.
It's caused by setExecutable(FilesToRunProvider) adding an unnecessary call to addTool(Artifact). I'm surprised that this shows up in our performance measurements, but it apparently does.

There is some RAM use hit due to the new CallablePathFragment instances, but I think that's a fair deal because https://github.com/bazelbuild/bazel/commit/94602f05b34fda2556287797a14b7dac64f2f041 fixes a genuine bug.

RELNOTES: None.
PiperOrigin-RevId: 362924067

src/main/java/com/google/devtools/build/lib/analysis/actions/SpawnAction.java
@@ -1014,7 +1014,10 @@ public class SpawnAction extends AbstractAction implements CommandAction {
     public Builder setExecutable(FilesToRunProvider executableProvider) {
       Preconditions.checkArgument(executableProvider.getExecutable() != null,
           "The target does not have an executable");
-      setExecutable(executableProvider.getExecutable());
+      this.executableArgs =
+          CustomCommandLine.builder()
+              .addCallablePath(executableProvider.getExecutable().getExecPath());
+      this.isShellCommand = false;
       return addTool(executableProvider);
     }
 