d2e21cec31f09b27ef3589f47b0779f34077ca7e
John Cater <jcater@google.com>
2021-04-08 10:44:57 -0700

    Renamed ExecGroupCollection to clarify that it is only for Starlark usage.
    Non-starlark usage can go via the ToolchainCollection directly.
    
    PiperOrigin-RevId: 367461392

--- src/main/java/com/google/devtools/build/lib/analysis/starlark/StarlarkRuleContext.java
+++ src/main/java/com/google/devtools/build/lib/analysis/starlark/StarlarkRuleContext.java
@@ -34,7 +34,6 @@ import com.google.devtools.build.lib.analysis.BashCommandConstructor;
 import com.google.devtools.build.lib.analysis.CommandHelper;
 import com.google.devtools.build.lib.analysis.ConfigurationMakeVariableContext;
 import com.google.devtools.build.lib.analysis.DefaultInfo;
-import com.google.devtools.build.lib.analysis.ExecGroupCollection;
 import com.google.devtools.build.lib.analysis.FileProvider;
 import com.google.devtools.build.lib.analysis.FilesToRunProvider;
 import com.google.devtools.build.lib.analysis.LabelExpander;
@@ -697,9 +696,9 @@ public final class StarlarkRuleContext implements StarlarkRuleContextApi<Constra
   }
 
   @Override
-  public ExecGroupCollection execGroups() {
+  public StarlarkExecGroupCollection execGroups() {
     // Create a thin wrapper around the toolchain collection, to expose the Starlark API.
-    return ExecGroupCollection.create(ruleContext.getToolchainContexts());
+    return StarlarkExecGroupCollection.create(ruleContext.getToolchainContexts());
   }
 
   @Override
