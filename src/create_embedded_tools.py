a3578da68cf18c056f990ad991a6c2459421d9e3
Keith Smiley <keithbsmiley@gmail.com>
2021-05-07 17:34:05 -0700

    Delete unused stdredirect dylib
    This stopped being used in this commit
    https://github.com/bazelbuild/rules_apple/commit/79dbaba43d5f6e4b655c9196ec2b045b410917b5
    when Xcode's simctl introduced the `--console-pty` flag. This requires
    this rules change https://github.com/bazelbuild/rules_apple/pull/1152

--- src/create_embedded_tools.py
+++ src/create_embedded_tools.py
@@ -43,7 +43,6 @@ output_paths = [
     ('*zipper', lambda x: 'tools/zip/zipper/zipper'),
     ('*src/objc_tools/*',
      lambda x: 'tools/objc/precomp_' + os.path.basename(x)),
-    ('*xcode*StdRedirect.dylib', lambda x: 'tools/objc/StdRedirect.dylib'),
     ('*xcode*make_hashed_objlist.py',
      lambda x: 'tools/objc/make_hashed_objlist.py'),
     ('*xcode*realpath', lambda x: 'tools/objc/realpath'),
