cf7ac95f72fc6ade0818080f2963f659c86e3425
fwe <fwe@google.com>
2021-05-31 08:58:10 -0700

    Support rolling releases in Bazel release scripts.
    PiperOrigin-RevId: 376692239

--- scripts/release/common.sh
+++ scripts/release/common.sh
@@ -62,9 +62,24 @@ function get_release_candidate() {
 
 # Extract the release name from the git branch name
 function get_release_name() {
-  # Match branch name release-X.X.X-rcY and return X.X.X
-  # or match tag name X.X.X and return X.X.X
-  git_get_branch 2>/dev/null | grep -Po "(?<=release-)([0-9]|\.)*(?=rc)" || git_get_tag | grep -Po "^([0-9]|\.)*$" || true
+  # Match branch name release-X.X.X[-pre.XXXXXXXX.X]rcY and return X.X.X[-pre.XXXXXXXX.X]
+  # or match tag name X.X.X[-pre.XXXXXXXX.X] and return X.X.X[-pre.XXXXXXXX.X]
+   git_get_branch 2>/dev/null | grep -Po "(?<=release-)([0-9]|\.)*(-pre\.[0-9]{8}(\.[0-9]+){1,2})?(?=rc)" || git_get_tag | grep -Po "^([0-9]|\.)*(-pre\.[0-9]{8}(\.[0-9]+){1,2})?$" || true
+}
+
+# Returns whether this is a rolling release (or an RCs of one)
+function is_rolling_release() {
+  if [[ "$(get_release_name)" =~ [0-9]+\.[0-9]+\.[0-9]+-pre\.[0-9]{8}\.[0-9]+(\.[0-9]+)?$ ]]; then
+    echo 1
+  else
+    echo 0
+  fi
+}
+
+# Returns the name of the LTS release that belongs to the current rolling release
+function get_lts_name() {
+  local release_name="$(get_release_name)"
+  echo "${release_name}" | grep -oE "^([0-9]+\.[0-9]+\.[0-9]+)"
 }
 
 # Get the list of commit hashes between two revisions
