bazelbuild/bazel

commit 9a423f8982c668dc4566a038c302614fb0548592

Flip objc_(import|library) to Starlark
This will start using the Starlark version of the rule's by default.

TGP is clean but if problems become apparent after a release, in order to avoid
a rollback, you can add --experimental_builtins_injection_override=-objc_library,-objc_import
to the blazerc.

TESTED=[]

RELNOTES:none
PiperOrigin-RevId: 378859899

src/main/starlark/builtins_bzl/common/exports.bzl
@@ -24,7 +24,7 @@ exported_toplevels = {
     "_builtins_dummy": "overridden value",
 }
 exported_rules = {
-    "-objc_import": objc_import,
-    "-objc_library": objc_library,
+    "+objc_import": objc_import,
+    "+objc_library": objc_library,
 }
 exported_to_java = {}

commit 4bcf2e83c5cb4f459aae815b38f1edd823286a29

Automated rollback of commit ff38dbfc65b8ffe92833e67f12ce616739a3d213.
*** Reason for rollback ***

Causes errors in nightly

*** Original change description ***

Flip objc_(import|library) to Starlark

This will start using the Starlark version of the rule's by default.

TGP is clean but if problems become apparent after a release, in order to avoid
a rollback, you can add --experimental_builtins_injection_override=-objc_library,-objc_import
to the blazerc.

Performance regressions from https://github.com/bazelbuild/bazel/commit/9a423f8982c668dc4566a038c302614fb0548592 are fixed. See Benchmark chip.

TESTED=[]

RELNOTES:none
PiperOrigin-RevId: 383626931

src/main/starlark/builtins_bzl/common/exports.bzl
@@ -24,7 +24,7 @@ exported_toplevels = {
     "_builtins_dummy": "overridden value",
 }
 exported_rules = {
-    "+objc_import": objc_import,
-    "+objc_library": objc_library,
+    "-objc_import": objc_import,
+    "-objc_library": objc_library,
 }
 exported_to_java = {}