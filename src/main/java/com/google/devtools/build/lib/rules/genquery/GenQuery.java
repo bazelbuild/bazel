d2e8b1638de7a2e18e8b36ab7819fa957d2d283e
hvd <hvd@google.com>
2021-08-06 07:25:16 -0700

    Migrate GenQuery to use transitive validation output group
    PiperOrigin-RevId: 389171146

--- src/main/java/com/google/devtools/build/lib/rules/genquery/GenQuery.java
+++ src/main/java/com/google/devtools/build/lib/rules/genquery/GenQuery.java
@@ -32,6 +32,7 @@ import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.actions.Artifact.ArtifactExpander;
 import com.google.devtools.build.lib.actions.MutableActionGraph.ActionConflictException;
 import com.google.devtools.build.lib.analysis.ConfiguredTarget;
+import com.google.devtools.build.lib.analysis.OutputGroupInfo;
 import com.google.devtools.build.lib.analysis.RuleConfiguredTargetBuilder;
 import com.google.devtools.build.lib.analysis.RuleConfiguredTargetFactory;
 import com.google.devtools.build.lib.analysis.RuleContext;
@@ -213,7 +214,8 @@ public class GenQuery implements RuleConfiguredTargetFactory {
                         ruleContext.getConfiguration().legacyExternalRunfiles())
                     .addTransitiveArtifacts(filesToBuild)
                     .build()))
-        .setPropagateValidationActionOutputGroup(false)
+        .addOutputGroup(
+            OutputGroupInfo.VALIDATION_TRANSITIVE, NestedSetBuilder.emptySet(Order.STABLE_ORDER))
         .build();
   }
 
