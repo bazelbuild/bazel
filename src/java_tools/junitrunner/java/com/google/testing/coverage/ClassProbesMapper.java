d7bebb2280b29db05ce089c6097d06bdc47177ea
Charles Mita <cmita@google.com>
2021-08-23 14:39:12 +0200

    Apply Jacoco's filtering to branch coverage analysis.
    Jacoco's filters operate on asm.tree.AbstractInsnNode objects. We track
    these as we walk through the ASM tree in the same way Jacoco does, apply
    the filters to them appropriately, and then map ignored nodes back to
    the Instruction objects we already operated on.
    
    We only handle IFilter::ignore for now; this covers the majority of
    filters.

--- src/java_tools/junitrunner/java/com/google/testing/coverage/ClassProbesMapper.java
+++ src/java_tools/junitrunner/java/com/google/testing/coverage/ClassProbesMapper.java
@@ -14,30 +14,72 @@
 
 package com.google.testing.coverage;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
+import org.jacoco.core.internal.analysis.StringPool;
+import org.jacoco.core.internal.analysis.filter.Filters;
+import org.jacoco.core.internal.analysis.filter.IFilter;
+import org.jacoco.core.internal.analysis.filter.IFilterContext;
 import org.jacoco.core.internal.flow.ClassProbesVisitor;
 import org.jacoco.core.internal.flow.MethodProbesVisitor;
+import org.objectweb.asm.AnnotationVisitor;
+import org.objectweb.asm.Attribute;
 import org.objectweb.asm.FieldVisitor;
 
 /** A visitor that maps each source code line to the probes corresponding to the lines. */
 public class ClassProbesMapper extends ClassProbesVisitor {
   private Map<Integer, BranchExp> classLineToBranchExp;
 
+  private IFilter filter;
+
+  private SimpleFilterContext filterContext;
+
+  private StringPool stringPool;
+
   public Map<Integer, BranchExp> result() {
     return classLineToBranchExp;
   }
 
   /** Create a new probe mapper object. */
-  public ClassProbesMapper() {
+  public ClassProbesMapper(String className) {
     classLineToBranchExp = new TreeMap<Integer, BranchExp>();
+    filter = Filters.all();
+    filterContext = new SimpleFilterContext();
+    stringPool = new StringPool();
+    filterContext.setClassName(stringPool.get(className));
+  }
+
+  @Override
+  public AnnotationVisitor visitAnnotation(final String desc, final boolean visible) {
+    filterContext.addClassAnnotations(desc);
+    return super.visitAnnotation(desc, visible);
+  }
+
+  @Override
+  public void visitAttribute(final Attribute attribute) {
+    filterContext.addClassAttribute(attribute.type);
+  }
+
+  @Override
+  public void visitSource(final String source, final String debug) {
+    filterContext.setSourceFileName(stringPool.get(source));
+    filterContext.setSourceDebugExtension(debug);
+  }
+
+  @Override
+  public void visit(int version, int access, String name, String signature, String superName,
+      String[] interfaces) {
+    filterContext.setSuperClassName(stringPool.get(name));
   }
 
   /** Returns a visitor for mapping method code. */
   @Override
   public MethodProbesVisitor visitMethod(
       int access, String name, String desc, String signature, String[] exceptions) {
-    return new MethodProbesMapper() {
+    return new MethodProbesMapper(filterContext, filter) {
+
       @Override
       public void visitEnd() {
         super.visitEnd();
@@ -55,5 +97,69 @@ public class ClassProbesMapper extends ClassProbesVisitor {
   @Override
   public void visitTotalProbeCount(int count) {
     // Nothing to do. Maybe perform some checks here.
+  }
+  private class SimpleFilterContext implements IFilterContext {
+
+    String sourceFileName;
+    String sourceDebugExtension;
+    String className;
+    String superClassName;
+    Set<String> classAnnotations = new HashSet<>();
+    Set<String> classAttributes = new HashSet<>();
+
+    public void setClassName(String className) {
+      this.className = className;
+    }
+
+    @Override
+    public String getClassName() {
+      return className;
+    }
+
+    public void setSuperClassName(String superClassName) {
+      this.superClassName = superClassName;
+    }
+
+    @Override
+    public String getSuperClassName() {
+      return superClassName;
+    }
+
+    public void addClassAnnotations(String annotation) {
+      classAnnotations.add(annotation);
+    }
+
+    @Override
+    public Set<String> getClassAnnotations() {
+      return classAnnotations;
+    }
+
+    public void addClassAttribute(String attribute) {
+      classAttributes.add(attribute);
+    }
+
+    @Override
+    public Set<String> getClassAttributes() {
+      return classAttributes;
+    }
+
+
+    public void setSourceFileName(String sourceFileName) {
+      this.sourceFileName = sourceFileName;
+    }
+
+    @Override
+    public String getSourceFileName() {
+      return sourceFileName;
+    }
+
+    public void setSourceDebugExtension(String sourceDebugExtension) {
+      this.sourceDebugExtension = sourceDebugExtension;
+    }
+
+    @Override
+    public String getSourceDebugExtension() {
+      return sourceDebugExtension;
+    }
   }
 }
--- src/java_tools/junitrunner/java/com/google/testing/coverage/MethodProbesMapper.java
+++ src/java_tools/junitrunner/java/com/google/testing/coverage/MethodProbesMapper.java
@@ -16,15 +16,23 @@ package com.google.testing.coverage;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 import org.jacoco.core.internal.analysis.Instruction;
+import org.jacoco.core.internal.analysis.filter.IFilter;
+import org.jacoco.core.internal.analysis.filter.IFilterContext;
+import org.jacoco.core.internal.analysis.filter.IFilterOutput;
 import org.jacoco.core.internal.flow.IFrame;
 import org.jacoco.core.internal.flow.LabelInfo;
 import org.jacoco.core.internal.flow.MethodProbesVisitor;
 import org.objectweb.asm.Handle;
 import org.objectweb.asm.Label;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.MethodNode;
 
 /**
  * The mapper is a probes visitor that will cache control flow information as well as keeping track
@@ -57,6 +65,13 @@ public class MethodProbesMapper extends MethodProbesVisitor {
   private Instruction lastInstruction = null;
   private int currentLine = -1;
   private List<Label> currentLabels = new ArrayList<>();
+  private AbstractInsnNode currentInstructionNode = null;
+  private Map<AbstractInsnNode, Instruction> instructionMap = new HashMap<>();
+
+  // Filtering
+  private IFilter filter;
+  private IFilterContext filterContext;
+  private SimpleFilterOutput filterOutput = new SimpleFilterOutput();
 
   // Result
   private Map<Integer, BranchExp> lineToBranchExp = new TreeMap();
@@ -87,6 +102,22 @@ public class MethodProbesMapper extends MethodProbesVisitor {
   private Map<Instruction, Instruction> predecessors = new HashMap<>();
   private Map<Label, Instruction> labelToInsn = new HashMap<>();
 
+  public MethodProbesMapper(IFilterContext filterContext, IFilter filter) {
+    this.filterContext = filterContext;
+    this.filter = filter;
+  }
+
+  @Override
+  public void accept(MethodNode methodNode, MethodVisitor methodVisitor) {
+    methodVisitor.visitCode();
+    for (AbstractInsnNode i : methodNode.instructions) {
+      currentInstructionNode = i;
+      i.accept(methodVisitor);
+    }
+    filter.filter(methodNode, filterContext, filterOutput);
+    methodVisitor.visitEnd();
+  }
+
   /** Visitor method to append a new Instruction */
   private void visitInsn() {
     Instruction instruction = new Instruction(currentLine);
@@ -101,6 +132,7 @@ public class MethodProbesMapper extends MethodProbesVisitor {
     }
     currentLabels.clear(); // Update states
     lastInstruction = instruction;
+    instructionMap.put(currentInstructionNode, instruction);
   }
 
   // Plain visitors: called from adapter when no probe is needed
@@ -317,6 +349,12 @@ public class MethodProbesMapper extends MethodProbesVisitor {
   /** Finishing the method */
   @Override
   public void visitEnd() {
+    HashSet<Instruction> ignoredInstructions = new HashSet<>();
+    for (Map.Entry<AbstractInsnNode, Instruction> entry : instructionMap.entrySet()) {
+      if (filterOutput.ignored.contains(entry.getKey())) {
+        ignoredInstructions.add(entry.getValue());
+      }
+    }
 
     for (Jump jump : jumps) {
       Instruction insn = labelToInsn.get(jump.target);
@@ -328,6 +366,9 @@ public class MethodProbesMapper extends MethodProbesVisitor {
     for (Map.Entry<Integer, Instruction> entry : probeToInsn.entrySet()) {
       int probeId = entry.getKey();
       Instruction ins = entry.getValue();
+      if (ignoredInstructions.contains(ins)) {
+        continue;
+      }
 
       Instruction insn = ins;
       CovExp exp = new ProbeExp(probeId);
@@ -372,6 +413,9 @@ public class MethodProbesMapper extends MethodProbesVisitor {
 
     // Merge branches in the instructions on the same line
     for (Instruction insn : instructions) {
+      if (ignoredInstructions.contains(insn)) {
+        continue;
+      }
       if (insn.getBranchCounter().getTotalCount() > 1) {
         CovExp insnExp = insnToCovExp.get(insn);
         if (insnExp != null && (insnExp instanceof BranchExp)) {
@@ -401,6 +445,29 @@ public class MethodProbesMapper extends MethodProbesVisitor {
       source = i;
       target = l;
       branch = b;
+    }
+  }
+
+  class SimpleFilterOutput implements IFilterOutput {
+
+    private HashSet<AbstractInsnNode> ignored = new HashSet<>();
+    // Handle only ignore for now; most filters only use this.
+    @Override
+    public void ignore(AbstractInsnNode fromInclusive, AbstractInsnNode toInclusive) {
+      for (AbstractInsnNode n = fromInclusive; n != toInclusive; n = n.getNext()) {
+        ignored.add(n);
+      }
+      ignored.add(toInclusive);
+    }
+
+    @Override
+    public void merge(AbstractInsnNode i1, AbstractInsnNode i2) {
+      // TODO(cmita): Handle
+    }
+
+    @Override
+    public void replaceBranches(AbstractInsnNode source, Set<AbstractInsnNode> newTargets) {
+      // TODO(cmita): Handle
     }
   }
 }
