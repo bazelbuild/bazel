bazelbuild/bazel

commit 69bfc65735679d62c33c3f3d718408df30079607

Remove cc_library implicit outputs deadcode
RELNOTES:none
PiperOrigin-RevId: 393961259

src/main/java/com/google/devtools/build/lib/rules/cpp/CcLibrary.java
@@ -22,10 +22,8 @@ import com.google.common.collect.Streams;
 import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.actions.FailAction;
 import com.google.devtools.build.lib.actions.MutableActionGraph.ActionConflictException;
-import com.google.devtools.build.lib.analysis.Allowlist;
 import com.google.devtools.build.lib.analysis.AnalysisUtils;
 import com.google.devtools.build.lib.analysis.ConfiguredTarget;
-import com.google.devtools.build.lib.analysis.DeniedImplicitOutputMarkerProvider;
 import com.google.devtools.build.lib.analysis.FileProvider;
 import com.google.devtools.build.lib.analysis.FilesToRunProvider;
 import com.google.devtools.build.lib.analysis.MakeVariableSupplier.MapBackedMakeVariableSupplier;
@@ -44,7 +42,6 @@ import com.google.devtools.build.lib.collect.nestedset.NestedSetBuilder;
 import com.google.devtools.build.lib.collect.nestedset.Order;
 import com.google.devtools.build.lib.packages.AttributeMap;
 import com.google.devtools.build.lib.packages.BuildType;
-import com.google.devtools.build.lib.packages.ImplicitOutputsFunction;
 import com.google.devtools.build.lib.packages.RawAttributeMapper;
 import com.google.devtools.build.lib.packages.TargetUtils;
 import com.google.devtools.build.lib.packages.Type;
@@ -77,8 +74,6 @@ public abstract class CcLibrary implements RuleConfiguredTargetFactory {
 
   /** A string constant for the name of Windows def file output group. */
   public static final String DEF_FILE_OUTPUT_GROUP_NAME = "def_file";
-
-  public static final String IMPLICIT_OUTPUTS_ALLOWLIST = "allowed_cc_lib_implicit_outputs";
 
   private final CppSemantics semantics;
 
@@ -336,8 +331,7 @@ public abstract class CcLibrary implements RuleConfiguredTargetFactory {
     // contract wrt. implicit output files, if the contract says so. Behavior here differs
     // between Bazel and Blaze.
     CcLinkingOutputs ccLinkingOutputs = CcLinkingOutputs.EMPTY;
-    if (ruleContext.getRule().getImplicitOutputsFunction() != ImplicitOutputsFunction.NONE
-        || !ccCompilationOutputs.isEmpty()) {
+    if (!ccCompilationOutputs.isEmpty()) {
       if (featureConfiguration.isEnabled(CppRuleClasses.TARGETS_WINDOWS)) {
         String dllNameSuffix = CppHelper.getDLLHashSuffix(ruleContext, featureConfiguration);
         linkingHelper.setLinkedDLLNameSuffix(dllNameSuffix);
@@ -502,21 +496,6 @@ public abstract class CcLibrary implements RuleConfiguredTargetFactory {
             CcCommon.mergeOutputGroups(ImmutableList.of(currentOutputGroups, outputGroups.build())))
         .addNativeDeclaredProvider(instrumentedFilesProvider)
         .addProvider(RunfilesProvider.withData(defaultRunfiles.build(), dataRunfiles.build()));
-
-    maybeAddDeniedImplicitOutputsProvider(targetBuilder, ruleContext);
-  }
-
-  private static void maybeAddDeniedImplicitOutputsProvider(
-      RuleConfiguredTargetBuilder targetBuilder, RuleContext ruleContext) {
-    if (ruleContext.getRule().getImplicitOutputsFunction() != ImplicitOutputsFunction.NONE
-        && !Allowlist.isAvailable(ruleContext, IMPLICIT_OUTPUTS_ALLOWLIST)) {
-      targetBuilder.addNativeDeclaredProvider(
-          new DeniedImplicitOutputMarkerProvider(
-              String.format(
-                  "Using implicit outputs from cc_library (%s) is forbidden. Use the rule"
-                      + " cc_implicit_output as an alternative.",
-                  ruleContext.getLabel())));
-    }
   }
 
   private static void warnAboutEmptyLibraries(RuleContext ruleContext,

src/test/java/com/google/devtools/build/lib/rules/cpp/CcCommonTest.java
@@ -102,11 +102,7 @@ public class CcCommonTest extends BuildViewTestCase {
     // We create .a for empty libraries, for simplicity (in Blaze).
     // But we avoid creating .so files for empty libraries,
     // because those have a potentially significant run-time startup cost.
-    if (emptyShouldOutputStaticLibrary()) {
-      assertThat(baseNamesOf(getFilesToBuild(emptylib))).isEqualTo("libemptylib.a");
-    } else {
-      assertThat(getFilesToBuild(emptylib).toList()).isEmpty();
-    }
+    assertThat(getFilesToBuild(emptylib).toList()).isEmpty();
     assertThat(
             emptylib
                 .get(CcInfo.PROVIDER)
@@ -114,10 +110,6 @@ public class CcCommonTest extends BuildViewTestCase {
                 .getDynamicLibrariesForRuntime(/* linkingStatically= */ false)
                 .isEmpty())
         .isTrue();
-  }
-
-  protected boolean emptyShouldOutputStaticLibrary() {
-    return !getAnalysisMock().isThisBazel();
   }
 
   @Test

src/test/java/com/google/devtools/build/lib/rules/cpp/CcLibraryConfiguredTargetTest.java
@@ -1711,33 +1711,6 @@ public class CcLibraryConfiguredTargetTest extends BuildViewTestCase {
   }
 
   @Test
-  public void testImplicitOutputsWhitelistNotOnWhitelist() throws Exception {
-    if (analysisMock.isThisBazel()) {
-      return;
-    }
-    scratch.overwriteFile(
-        "tools/build_defs/cc/whitelists/cc_lib_implicit_outputs/BUILD",
-        "package_group(",
-        "    name = 'allowed_cc_lib_implicit_outputs',",
-        "    packages = [])");
-
-    scratch.file(
-        "foo/BUILD",
-        "filegroup(",
-        "    name = 'denied',",
-        "    srcs = [':libdenied_cc_lib.a'],",
-        ")",
-        "cc_library(",
-        "    name = 'denied_cc_lib',",
-        "    srcs = ['denied_cc_lib.cc'],",
-        ")");
-    checkError(
-        "//foo:denied",
-        "Using implicit outputs from cc_library (//foo:denied_cc_lib) is "
-            + "forbidden. Use the rule cc_implicit_output as an alternative.");
-  }
-
-  @Test
   public void testImplicitOutputsWhitelistOnWhitelist() throws Exception {
     if (analysisMock.isThisBazel()) {
       return;

src/main/java/com/google/devtools/build/lib/analysis/BUILD
@@ -315,7 +315,6 @@ java_library(
         ":constraints/environment_collection",
         ":constraints/supported_environments",
         ":constraints/supported_environments_provider",
-        ":denied_implicit_outputs_marker_provider",
         ":dependency",
         ":dependency_key",
         ":dependency_kind",
@@ -703,15 +702,6 @@ java_library(
         ":configured_target",
         "//src/main/java/com/google/devtools/build/lib/skyframe:build_configuration_value",
         "//third_party:jsr305",
-    ],
-)
-
-java_library(
-    name = "denied_implicit_outputs_marker_provider",
-    srcs = ["DeniedImplicitOutputMarkerProvider.java"],
-    deps = [
-        "//src/main/java/com/google/devtools/build/lib/concurrent",
-        "//src/main/java/com/google/devtools/build/lib/packages",
     ],
 )
 

src/main/java/com/google/devtools/build/lib/rules/cpp/BUILD
@@ -60,7 +60,6 @@ java_library(
         "//src/main/java/com/google/devtools/build/lib/analysis:config/per_label_options",
         "//src/main/java/com/google/devtools/build/lib/analysis:config/transitions/no_transition",
         "//src/main/java/com/google/devtools/build/lib/analysis:configured_target",
-        "//src/main/java/com/google/devtools/build/lib/analysis:denied_implicit_outputs_marker_provider",
         "//src/main/java/com/google/devtools/build/lib/analysis:file_provider",
         "//src/main/java/com/google/devtools/build/lib/analysis:licenses_provider",
         "//src/main/java/com/google/devtools/build/lib/analysis:make_variable_supplier",

src/main/java/com/google/devtools/build/lib/analysis/ConfiguredTargetFactory.java
@@ -226,13 +226,6 @@ public final class ConfiguredTargetFactory {
       Verify.verifyNotNull(rule);
       Artifact artifact = rule.getArtifactByOutputLabel(outputFile.getLabel());
 
-      DeniedImplicitOutputMarkerProvider deniedProvider =
-          rule.get(DeniedImplicitOutputMarkerProvider.PROVIDER);
-      if (deniedProvider != null) {
-        analysisEnvironment.getEventHandler().handle(Event.error(deniedProvider.getErrorMessage()));
-        return null;
-      }
-
       return new OutputFileConfiguredTarget(targetContext, outputFile, rule, artifact);
     } else if (target instanceof InputFile) {
       InputFile inputFile = (InputFile) target;

src/main/java/com/google/devtools/build/lib/analysis/DeniedImplicitOutputMarkerProvider.java
@@ -1,46 +1,0 @@
-// Copyright 2014 The Bazel Authors. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package com.google.devtools.build.lib.analysis;
-
-import com.google.devtools.build.lib.concurrent.ThreadSafety.Immutable;
-import com.google.devtools.build.lib.packages.BuiltinProvider;
-import com.google.devtools.build.lib.packages.NativeInfo;
-
-/**
- * A provider used to signal the denial of implicit outputs.
- *
- * <p>Used to facilitate migration away from using implicit outputs.
- */
-@Immutable
-public class DeniedImplicitOutputMarkerProvider extends NativeInfo {
-  public static final BuiltinProvider<DeniedImplicitOutputMarkerProvider> PROVIDER =
-      new BuiltinProvider<DeniedImplicitOutputMarkerProvider>(
-          "DeniedImplicitOutputMarkerProvider", DeniedImplicitOutputMarkerProvider.class) {};
-
-  private final String errorMessage;
-
-  public DeniedImplicitOutputMarkerProvider(String errorMessage) {
-    this.errorMessage = errorMessage;
-  }
-
-  public String getErrorMessage() {
-    return errorMessage;
-  }
-
-  @Override
-  public BuiltinProvider<DeniedImplicitOutputMarkerProvider> getProvider() {
-    return PROVIDER;
-  }
-}

src/test/java/com/google/devtools/build/lib/buildtool/DanglingSymlinkTest.java
@@ -66,9 +66,8 @@ public class DanglingSymlinkTest extends GoogleBuildIntegrationTestCase {
   @Test
   public void testGeneratedLibs() throws Exception {
     MockGenruleSupport.setup(mockToolsConfig);
+    write("test/liba.so");
     write("test/BUILD",
-        "cc_library(name = 'a',",
-        "           srcs = ['a.cc'])",
         "genrule(name = 'b',",
         "        srcs = ['liba.so'],",
         "        outs = ['libb.so'],",

commit 37cef0889d6e5b3c5165ce77812c80d7999cc8e0

C++: Account for libs with no srcs
https://github.com/bazelbuild/bazel/commit/69bfc65735679d62c33c3f3d718408df30079607 caused breakages because cc_library used to create archives even when there were no srcs. Right now several places in the depot rely on a cc_library providing at least one file, either because they are checking cc_library's output groups or because the cc_library target is part of a $(location ) string. This would require a small cleanup.

Before that cleanup, to keep the same behavior as before, we use CppSemantics.

RELNOTES:none
PiperOrigin-RevId: 394457993

src/main/java/com/google/devtools/build/lib/bazel/rules/cpp/BazelCppSemantics.java
@@ -151,4 +151,9 @@ public class BazelCppSemantics implements AspectLegalCppSemantics {
       AspectDescriptor aspectDescriptor,
       CcToolchainProvider ccToolchain,
       ImmutableSet<String> unsupportedFeatures) {}
+
+  @Override
+  public boolean createEmptyArchive() {
+    return false;
+  }
 }

src/main/java/com/google/devtools/build/lib/rules/cpp/CcLibrary.java
@@ -331,7 +331,7 @@ public abstract class CcLibrary implements RuleConfiguredTargetFactory {
     // contract wrt. implicit output files, if the contract says so. Behavior here differs
     // between Bazel and Blaze.
     CcLinkingOutputs ccLinkingOutputs = CcLinkingOutputs.EMPTY;
-    if (!ccCompilationOutputs.isEmpty()) {
+    if (semantics.createEmptyArchive() || !ccCompilationOutputs.isEmpty()) {
       if (featureConfiguration.isEnabled(CppRuleClasses.TARGETS_WINDOWS)) {
         String dllNameSuffix = CppHelper.getDLLHashSuffix(ruleContext, featureConfiguration);
         linkingHelper.setLinkedDLLNameSuffix(dllNameSuffix);

src/main/java/com/google/devtools/build/lib/rules/cpp/CppSemantics.java
@@ -72,4 +72,6 @@ public interface CppSemantics extends StarlarkValue {
       AspectDescriptor aspectDescriptor,
       CcToolchainProvider ccToolchain,
       ImmutableSet<String> unsupportedFeatures);
+
+  boolean createEmptyArchive();
 }

src/main/java/com/google/devtools/build/lib/rules/cpp/ObjcCppSemantics.java
@@ -121,4 +121,9 @@ public class ObjcCppSemantics implements CppSemantics {
       AspectDescriptor aspectDescriptor,
       CcToolchainProvider ccToolchain,
       ImmutableSet<String> unsupportedFeatures) {}
+
+  @Override
+  public boolean createEmptyArchive() {
+    return false;
+  }
 }

src/test/java/com/google/devtools/build/lib/rules/cpp/BUILD
@@ -218,10 +218,10 @@ java_test(
         "//src/main/java/com/google/devtools/build/lib/actions:artifacts",
         "//src/main/java/com/google/devtools/build/lib/analysis:analysis_cluster",
         "//src/main/java/com/google/devtools/build/lib/analysis:configured_target",
+        "//src/main/java/com/google/devtools/build/lib/analysis:file_provider",
         "//src/main/java/com/google/devtools/build/lib/analysis:test/instrumented_files_info",
         "//src/main/java/com/google/devtools/build/lib/cmdline:cmdline-primitives",
         "//src/main/java/com/google/devtools/build/lib/collect/nestedset",
-        "//src/main/java/com/google/devtools/build/lib/packages",
         "//src/main/java/com/google/devtools/build/lib/rules/cpp",
         "//src/main/java/com/google/devtools/build/lib/skyframe:configured_target_and_data",
         "//src/main/java/com/google/devtools/build/lib/util:filetype",

src/test/java/com/google/devtools/build/lib/rules/cpp/CcCommonTest.java
@@ -102,7 +102,6 @@ public class CcCommonTest extends BuildViewTestCase {
     // We create .a for empty libraries, for simplicity (in Blaze).
     // But we avoid creating .so files for empty libraries,
     // because those have a potentially significant run-time startup cost.
-    assertThat(getFilesToBuild(emptylib).toList()).isEmpty();
     assertThat(
             emptylib
                 .get(CcInfo.PROVIDER)

src/test/java/com/google/devtools/build/lib/rules/cpp/CcLibraryConfiguredTargetTest.java
@@ -28,6 +28,7 @@ import com.google.devtools.build.lib.actions.extra.ExtraActionInfo;
 import com.google.devtools.build.lib.actions.util.ActionsTestUtil;
 import com.google.devtools.build.lib.analysis.ConfiguredRuleClassProvider;
 import com.google.devtools.build.lib.analysis.ConfiguredTarget;
+import com.google.devtools.build.lib.analysis.FileProvider;
 import com.google.devtools.build.lib.analysis.OutputGroupInfo;
 import com.google.devtools.build.lib.analysis.test.InstrumentedFilesInfo;
 import com.google.devtools.build.lib.analysis.util.AnalysisMock;
@@ -35,7 +36,6 @@ import com.google.devtools.build.lib.analysis.util.BuildViewTestCase;
 import com.google.devtools.build.lib.analysis.util.DummyTestFragment;
 import com.google.devtools.build.lib.cmdline.RepositoryName;
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
-import com.google.devtools.build.lib.packages.ImplicitOutputsFunction;
 import com.google.devtools.build.lib.packages.util.Crosstool.CcToolchainConfig;
 import com.google.devtools.build.lib.packages.util.MockCcSupport;
 import com.google.devtools.build.lib.skyframe.ConfiguredTargetAndData;
@@ -1456,8 +1456,7 @@ public class CcLibraryConfiguredTargetTest extends BuildViewTestCase {
     ConfiguredTargetAndData target =
         scratchConfiguredTargetAndData("a", "b", "cc_library(name = 'b', srcs = ['libb.so'])");
 
-    if (target.getTarget().getAssociatedRule().getImplicitOutputsFunction()
-        != ImplicitOutputsFunction.NONE) {
+    if (!analysisMock.isThisBazel()) {
       assertThat(artifactsToStrings(getFilesToBuild(target.getConfiguredTarget())))
           .containsExactly("bin a/libb.a");
     } else {
@@ -2007,5 +2006,19 @@ public class CcLibraryConfiguredTargetTest extends BuildViewTestCase {
     reporter.removeHandler(failFastHandler);
     getConfiguredTarget("//foo:lib");
     assertContainsEvent("requires --experimental_cc_implementation_deps");
+  }
+
+  @Test
+  public void testCcLibraryProducesEmptyArchive() throws Exception {
+    if (analysisMock.isThisBazel()) {
+      return;
+    }
+    scratch.file("foo/BUILD", "cc_library(name = 'foo')");
+    assertThat(
+            getConfiguredTarget("//foo:foo")
+                .getProvider(FileProvider.class)
+                .getFilesToBuild()
+                .toList())
+        .isNotEmpty();
   }
 }

src/test/java/com/google/devtools/build/lib/rules/cpp/MockCppSemantics.java
@@ -83,4 +83,9 @@ public final class MockCppSemantics implements CppSemantics {
       AspectDescriptor aspectDescriptor,
       CcToolchainProvider ccToolchain,
       ImmutableSet<String> unsupportedFeatures) {}
+
+  @Override
+  public boolean createEmptyArchive() {
+    return false;
+  }
 }