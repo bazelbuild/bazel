7c7f50e4006389ee51c7deeeda52ea46939906d4
wyv <wyv@google.com>
2021-09-14 09:34:16 -0700

    Add some documentation on StarlarkMethod about exception handling
    PiperOrigin-RevId: 396614952

--- src/main/java/net/starlark/java/annot/StarlarkMethod.java
+++ src/main/java/net/starlark/java/annot/StarlarkMethod.java
@@ -80,6 +80,10 @@ import java.lang.annotation.Target;
  * unless the method is marked as {@link #allowReturnNones}, in which case {@link Starlark#fromJava}
  * converts the Java null value to {@link Starlark#NONE}. This feature prevents a method whose
  * declared (and documented) result type is T from unexpectedly returning a value of type NoneType.
+ *
+ * <p>The annotated method may throw any checked or unchecked exceptions. When it is invoked,
+ * unchecked exceptions, {@code EvalException}s, and {@code InterruptedException}s are passed
+ * through; all other (checked) exceptions are wrapped in an {@code EvalException} and thrown.
  */
 // TODO(adonovan): rename to StarlarkAttribute and factor Starlark{Method,Field} as subinterfaces.
 @Target({ElementType.METHOD})
