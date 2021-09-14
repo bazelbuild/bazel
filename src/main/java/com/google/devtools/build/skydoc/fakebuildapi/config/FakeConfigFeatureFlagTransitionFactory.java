331e19a6b40d6278dedb9458577e592c6d9dc40d
Googler <noreply@google.com>
2021-09-14 12:31:52 -0700

    Fake StarlarkExposedRuleTransitionFactory for the Starlark common api
    PiperOrigin-RevId: 396656234

--- src/main/java/com/google/devtools/build/skydoc/fakebuildapi/config/FakeConfigFeatureFlagTransitionFactory.java
+++ src/main/java/com/google/devtools/build/skydoc/fakebuildapi/config/FakeConfigFeatureFlagTransitionFactory.java
@@ -1,4 +1,4 @@
-// Copyright 2018 The Bazel Authors. All rights reserved.
+// Copyright 2021 The Bazel Authors. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,24 +14,21 @@
 
 package com.google.devtools.build.skydoc.fakebuildapi.config;
 
+import com.google.devtools.build.lib.analysis.RuleDefinitionEnvironment;
+import com.google.devtools.build.lib.analysis.config.transitions.PatchTransition;
 import com.google.devtools.build.lib.analysis.config.transitions.StarlarkExposedRuleTransitionFactory;
-import com.google.devtools.build.lib.starlarkbuildapi.config.ConfigStarlarkCommonApi;
-import com.google.devtools.build.lib.starlarkbuildapi.core.ProviderApi;
-import com.google.devtools.build.skydoc.fakebuildapi.FakeProviderApi;
+import com.google.devtools.build.lib.packages.RuleClass;
+import com.google.devtools.build.lib.packages.RuleTransitionData;
 
-/**
- * Fake implementation of {@link ConfigStarlarkCommonApi}.
- */
-public class FakeConfigStarlarkCommon implements ConfigStarlarkCommonApi {
+/** Fake callable implementation of {@link StarlarkExposedRuleTransitionFactory}. */
+public class FakeConfigFeatureFlagTransitionFactory
+    implements StarlarkExposedRuleTransitionFactory {
 
   @Override
-  public ProviderApi getConfigFeatureFlagProviderConstructor() {
-    return new FakeProviderApi("FeatureFlagInfo");
-  }
+  public void addToStarlarkRule(RuleDefinitionEnvironment ctx, RuleClass.Builder builder) {}
 
   @Override
-  public StarlarkExposedRuleTransitionFactory createConfigFeatureFlagTransitionFactory(
-      String attribute) {
-    return new FakeConfigFeatureFlagTransitionFactory();
+  public PatchTransition create(RuleTransitionData ruleData) {
+    return null;
   }
 }
