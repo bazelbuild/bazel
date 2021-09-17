d0008855a1e8c6d58b1dccafbc529f6d929a852a
ron-stripe <ron@stripe.com>
2021-09-16 20:06:42 -0700

    added tests, still not sure about disk cache tests
    I think shouldUploadLocalResults is poorly named because it is being used on CAS entries, not just action cache results.
    But it is the CAS entries for both action inputs and action result outputs.
    
    I'm not sure if downloadBlob needs a similar check or not.
    Things seem to work for me without it, but I might not be trying a certain flag setting.

--- src/main/java/com/google/devtools/build/lib/remote/disk/DiskAndRemoteCacheClient.java
+++ src/main/java/com/google/devtools/build/lib/remote/disk/DiskAndRemoteCacheClient.java
@@ -15,7 +15,6 @@ package com.google.devtools.build.lib.remote.disk;
 
 import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
 import static com.google.devtools.build.lib.remote.util.Utils.shouldAcceptCachedResultFromRemoteCache;
-import static com.google.devtools.build.lib.remote.util.Utils.shouldUploadContentToRemoteCache;
 import static com.google.devtools.build.lib.remote.util.Utils.shouldUploadLocalResultsToRemoteCache;
 
 import build.bazel.remote.execution.v2.ActionResult;
@@ -76,7 +75,8 @@ public final class DiskAndRemoteCacheClient implements RemoteCacheClient {
   public ListenableFuture<Void> uploadFile(
       RemoteActionExecutionContext context, Digest digest, Path file) {
     ListenableFuture<Void> future = diskCache.uploadFile(context, digest, file);
-    if (shouldUploadContentToRemoteCache(options, context.getSpawn())) {
+    if (options.isRemoteExecutionEnabled() ||
+        shouldUploadLocalResultsToRemoteCache(options, context.getSpawn())) {
       future =
           Futures.transformAsync(
               future, v -> remoteCache.uploadFile(context, digest, file), directExecutor());
@@ -88,7 +88,8 @@ public final class DiskAndRemoteCacheClient implements RemoteCacheClient {
   public ListenableFuture<Void> uploadBlob(
       RemoteActionExecutionContext context, Digest digest, ByteString data) {
     ListenableFuture<Void> future = diskCache.uploadBlob(context, digest, data);
-    if (shouldUploadContentToRemoteCache(options, context.getSpawn())) {
+    if (options.isRemoteExecutionEnabled() ||
+        shouldUploadLocalResultsToRemoteCache(options, context.getSpawn())) {
       future =
           Futures.transformAsync(
               future, v -> remoteCache.uploadBlob(context, digest, data), directExecutor());
