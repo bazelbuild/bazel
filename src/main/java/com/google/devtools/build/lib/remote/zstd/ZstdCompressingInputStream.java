4f7771d5ea13be584998e02b6fe49cb9ad9ac064
Alessandro Patti <ale812@yahoo.it>
2021-10-17 19:47:49 +0200

    Ensure minimum pipe size

--- src/main/java/com/google/devtools/build/lib/remote/zstd/ZstdCompressingInputStream.java
+++ src/main/java/com/google/devtools/build/lib/remote/zstd/ZstdCompressingInputStream.java
@@ -1,6 +1,7 @@
 package com.google.devtools.build.lib.remote.zstd;
 
 import com.github.luben.zstd.ZstdOutputStream;
+import com.google.common.base.Preconditions;
 
 import java.io.FilterInputStream;
 import java.io.IOException;
@@ -9,6 +10,15 @@ import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 
 public class ZstdCompressingInputStream extends FilterInputStream {
+    // We want the buffer to be able to contain at least:
+    //   - Magic number: 4 bytes
+    //   - FrameHeader 14 bytes
+    //   - Block Header: 3 bytes
+    //   - First block byte
+    // This guarantees that we can always compress at least
+    // 1 byte and write it to the pipe without blocking.
+    static public int MIN_BUFFER_SIZE = 4 + 14 + 3 + 1;
+
     private final PipedInputStream pis;
     private ZstdOutputStream zos;
     private final int size;
@@ -19,6 +29,9 @@ public class ZstdCompressingInputStream extends FilterInputStream {
 
     ZstdCompressingInputStream(InputStream in, int size) throws IOException {
         super(in);
+        Preconditions.checkArgument(
+                size >= MIN_BUFFER_SIZE,
+                String.format("The buffer size must be at least %d bytes", MIN_BUFFER_SIZE));
         this.size = size;
         this.pis = new PipedInputStream(size);
         this.zos = new ZstdOutputStream(new PipedOutputStream(pis));
@@ -26,7 +39,7 @@ public class ZstdCompressingInputStream extends FilterInputStream {
 
     private void reFill() throws IOException {
       byte[] buf = new byte[size];
-      int len = super.read(buf, 0 , Math.max(0, size - pis.available()));
+      int len = super.read(buf, 0 , Math.max(0, size - pis.available() - MIN_BUFFER_SIZE + 1));
       if (len == -1) {
         zos.close();
         zos = null;
