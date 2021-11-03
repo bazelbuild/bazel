3106e3a354de827292250c8325877521b9c69996
ilist <ilist@google.com>
2021-11-03 02:25:58 -0700

    Expose create_proto_info to builtins.
    This is one of two things proto_library rule does (besides creating descriptor set).
    
    It's possible to peel of some layers of create_proto_info to Starlark and this will be done later (now the focus is to get outer layer of proto_library to Starlark).
    
    The last layer of create_proto_info is the creation of ProtoInfo provider, which is currently native. It's used a lot by native lang_proto_libraries, thus rewriting it completely to Starlark would create more problems than benefits at the moment.
    
    The code can only be used from builtins. ProtoInfo provider needs a lot more thought and consideration, before being made public to Starlark.
    
    PiperOrigin-RevId: 407286229

--- src/main/java/com/google/devtools/build/lib/rules/proto/BazelProtoCommon.java
+++ src/main/java/com/google/devtools/build/lib/rules/proto/BazelProtoCommon.java
@@ -14,11 +14,42 @@
 
 package com.google.devtools.build.lib.rules.proto;
 
+import com.google.devtools.build.lib.analysis.starlark.StarlarkRuleContext;
+import com.google.devtools.build.lib.cmdline.Label;
+import com.google.devtools.build.lib.packages.BazelModuleContext;
 import com.google.devtools.build.lib.starlarkbuildapi.proto.ProtoCommonApi;
+import net.starlark.java.annot.Param;
+import net.starlark.java.annot.StarlarkMethod;
+import net.starlark.java.eval.EvalException;
+import net.starlark.java.eval.Module;
+import net.starlark.java.eval.Starlark;
+import net.starlark.java.eval.StarlarkThread;
 
 /** Protocol buffers support for Starlark. */
 public class BazelProtoCommon implements ProtoCommonApi {
   public static final BazelProtoCommon INSTANCE = new BazelProtoCommon();
 
-  private BazelProtoCommon() {}
+  protected BazelProtoCommon() {}
+
+  @StarlarkMethod(
+      name = "create_proto_info",
+      documented = false,
+      parameters = {@Param(name = "ctx", doc = "The rule context")},
+      useStarlarkThread = true)
+  public ProtoInfo createProtoInfo(StarlarkRuleContext ruleContext, StarlarkThread thread)
+      throws EvalException {
+    Label label =
+        ((BazelModuleContext) Module.ofInnermostEnclosingStarlarkFunction(thread).getClientData())
+            .label();
+    if (!label.getPackageIdentifier().getRepository().toString().equals("@_builtins")) {
+      throw Starlark.errorf("Rule in '%s' cannot use private API", label.getPackageName());
+    }
+
+    return ProtoCommon.createProtoInfo(
+        ruleContext.getRuleContext(),
+        ruleContext
+            .getRuleContext()
+            .getFragment(ProtoConfiguration.class)
+            .generatedProtosInVirtualImports());
+  }
 }
