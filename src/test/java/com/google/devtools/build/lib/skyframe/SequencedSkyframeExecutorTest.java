bazelbuild/bazel

commit fd2a779687616bbfb18b5110cbf708a24a681f7a

Simplify SkyframeActionExecutor#printError semantics: don't suppress further output there, instead leave it up to ActionExecutionFunction's control flow. This allows a no-keep-going point failure to print all of its failure messages, which seems reasonable.
Lays a little groundwork for unknown commit.

PiperOrigin-RevId: 362958174

src/main/java/com/google/devtools/build/lib/skyframe/ActionExecutionFunction.java
@@ -147,6 +147,16 @@ public class ActionExecutionFunction implements SkyFunction {
   @Override
   public SkyValue compute(SkyKey skyKey, Environment env)
       throws ActionExecutionFunctionException, InterruptedException {
+    try {
+      return computeInternal(skyKey, env);
+    } catch (ActionExecutionFunctionException | InterruptedException e) {
+      skyframeActionExecutor.recordExecutionError();
+      throw e;
+    }
+  }
+
+  private SkyValue computeInternal(SkyKey skyKey, Environment env)
+      throws ActionExecutionFunctionException, InterruptedException {
     ActionLookupData actionLookupData = (ActionLookupData) skyKey.argument();
     Action action = ActionUtils.getActionForLookupData(env, actionLookupData);
     if (action == null) {
@@ -918,13 +928,12 @@ public class ActionExecutionFunction implements SkyFunction {
         }
 
         skyframeActionExecutor.printError(
-            ArtifactFunction.makeIOExceptionInputFileMessage(input, e), actionForError, null);
+            ArtifactFunction.makeIOExceptionInputFileMessage(input, e), actionForError);
         // We don't create a specific cause for the artifact as we do in #handleMissingFile because
         // it likely has no label, so we'd have to use the Action's label anyway. Just use the
         // default ActionFailed event constructed by ActionExecutionException.
         String message = "discovered input file does not exist";
-        DetailedExitCode code =
-            createDetailedExitCode(message, Code.DISCOVERED_INPUT_DOES_NOT_EXIST);
+        DetailedExitCode code = createDetailedExitCodeForMissingDiscoveredInput(message);
         throw new ActionExecutionException(message, actionForError, false, code);
       }
       if (retrievedMetadata == null) {
@@ -1222,7 +1231,7 @@ public class ActionExecutionFunction implements SkyFunction {
 
     if (!missingArtifactCauses.isEmpty()) {
       for (LabelCause missingInput : missingArtifactCauses) {
-        skyframeActionExecutor.printError(missingInput.getMessage(), action, null);
+        skyframeActionExecutor.printError(missingInput.getMessage(), action);
       }
     }
     // We need to rethrow first exception because it can contain useful error message
@@ -1287,12 +1296,7 @@ public class ActionExecutionFunction implements SkyFunction {
 
     ActionExecutionFunctionExceptionHandler actionExecutionFunctionExceptionHandler =
         new ActionExecutionFunctionExceptionHandler(
-            skyKeyToArtifactSet,
-            inputDeps,
-            action,
-            mandatoryInputs,
-            requestedSkyKeys,
-            skyframeActionExecutor);
+            skyKeyToArtifactSet, inputDeps, action, mandatoryInputs, requestedSkyKeys);
 
     actionExecutionFunctionExceptionHandler.accumulateAndThrowExceptions();
 
@@ -1501,7 +1505,7 @@ public class ActionExecutionFunction implements SkyFunction {
   }
 
   /** Helper subclass for the error-handling logic for ActionExecutionFunction#accumulateInputs. */
-  private static final class ActionExecutionFunctionExceptionHandler {
+  private final class ActionExecutionFunctionExceptionHandler {
     private final Multimap<SkyKey, Artifact> skyKeyToDerivedArtifactSet;
     private final List<
             ValueOrException3<
@@ -1510,7 +1514,6 @@ public class ActionExecutionFunction implements SkyFunction {
     private final Action action;
     private final Set<Artifact> mandatoryInputs;
     private final Iterable<SkyKey> requestedSkyKeys;
-    private final SkyframeActionExecutor skyframeActionExecutor;
     List<LabelCause> missingArtifactCauses = Lists.newArrayListWithCapacity(0);
     List<NestedSet<Cause>> transitiveCauses = Lists.newArrayListWithCapacity(0);
     private ActionExecutionException firstActionExecutionException;
@@ -1523,14 +1526,12 @@ public class ActionExecutionFunction implements SkyFunction {
             inputDeps,
         Action action,
         Set<Artifact> mandatoryInputs,
-        Iterable<SkyKey> requestedSkyKeys,
-        SkyframeActionExecutor skyframeActionExecutor) {
+        Iterable<SkyKey> requestedSkyKeys) {
       this.skyKeyToDerivedArtifactSet = skyKeyToDerivedArtifactSet;
       this.inputDeps = inputDeps;
       this.action = action;
       this.mandatoryInputs = mandatoryInputs;
       this.requestedSkyKeys = requestedSkyKeys;
-      this.skyframeActionExecutor = skyframeActionExecutor;
     }
 
     /**
@@ -1626,7 +1627,7 @@ public class ActionExecutionFunction implements SkyFunction {
 
       if (!missingArtifactCauses.isEmpty()) {
         for (LabelCause missingInput : missingArtifactCauses) {
-          skyframeActionExecutor.printError(missingInput.getMessage(), action, null);
+          skyframeActionExecutor.printError(missingInput.getMessage(), action);
         }
         throw createMissingInputsException(action, missingArtifactCauses);
       }
@@ -1682,11 +1683,11 @@ public class ActionExecutionFunction implements SkyFunction {
         prioritizedDetailedExitCode);
   }
 
-  private static DetailedExitCode createDetailedExitCode(String message, Code detailedCode) {
+  private static DetailedExitCode createDetailedExitCodeForMissingDiscoveredInput(String message) {
     return DetailedExitCode.of(
         FailureDetail.newBuilder()
             .setMessage(message)
-            .setExecution(Execution.newBuilder().setCode(detailedCode))
+            .setExecution(Execution.newBuilder().setCode(Code.DISCOVERED_INPUT_DOES_NOT_EXIST))
             .build());
   }
 }

src/main/java/com/google/devtools/build/lib/skyframe/SkyframeActionExecutor.java
@@ -457,7 +457,6 @@ public final class SkyframeActionExecutor {
       synchronized (reporter) {
         reporter.handle(error);
       }
-      recordExecutionError();
       throw e;
     }
 
@@ -1677,13 +1676,18 @@ public final class SkyframeActionExecutor {
   }
 
   /**
+   * Prints the given error {@code message} ascribed to {@code action}. May be called multiple times
+   * for the same action if there are multiple errors: will print all of them.
+   */
+  void printError(String message, ActionAnalysisMetadata action) {
+    printError(message, action, null);
+  }
+
+  /**
    * For the action 'action' that failed due to 'message' with the output 'actionOutput', notify the
    * user about the error. To notify the user, the method first displays the output of the action
    * and then reports an error via the reporter. The method ensures that the two messages appear
    * next to each other by locking the outErr object where the output is displayed.
-   *
-   * <p>Should not be called for actions that might have failed because the build is shutting down
-   * after an error, since it prints output unconditionally, and such output should be suppressed.
    *
    * @param message The reason why the action failed
    * @param action The action that failed, must not be null.
@@ -1691,12 +1695,12 @@ public final class SkyframeActionExecutor {
    *     display
    */
   @SuppressWarnings("SynchronizeOnNonFinalField")
-  void printError(String message, ActionAnalysisMetadata action, FileOutErr actionOutput) {
+  private void printError(
+      String message, ActionAnalysisMetadata action, @Nullable FileOutErr actionOutput) {
     message = action.describe() + " failed: " + message;
     Event event = Event.error(action.getOwner().getLocation(), message);
     synchronized (reporter) {
       dumpRecordedOutErr(reporter, event, actionOutput);
-      recordExecutionError();
     }
   }
 

commit 01dae4f815f15c1818cbc5acc2eefdc03f6157a0

Allow errors to be printed in a no-keep-going build after an action is interrupted: if the action was interrupted by a file-level error during execution, suppressing further errors means that the user never gets to see the errors constructed during error bubbling.
This does leave the door open to a user manually interrupting the build and then seeing an error from some corner of Blaze (or a local action?) that doesn't react well to being interrupted. We should just fix those if they are found.

This bug was introduced in https://github.com/bazelbuild/bazel/commit/fd2a779687616bbfb18b5110cbf708a24a681f7a, which put ActionExecutionFunction in charge of deciding when to stop printing errors.

PiperOrigin-RevId: 416593263

src/main/java/com/google/devtools/build/lib/skyframe/ActionExecutionFunction.java
@@ -167,7 +167,7 @@ public class ActionExecutionFunction implements SkyFunction {
       throws ActionExecutionFunctionException, InterruptedException {
     try {
       return computeInternal(skyKey, env);
-    } catch (ActionExecutionFunctionException | InterruptedException e) {
+    } catch (ActionExecutionFunctionException e) {
       skyframeActionExecutor.recordExecutionError();
       throw e;
     }

src/test/java/com/google/devtools/build/lib/actions/util/TestAction.java
@@ -114,7 +114,7 @@ public class TestAction extends AbstractAction {
 
   @Override
   public ActionResult execute(ActionExecutionContext actionExecutionContext)
-      throws ActionExecutionException {
+      throws ActionExecutionException, InterruptedException {
     for (Artifact artifact : getInputs().toList()) {
       // Do not check *.optional artifacts - artifacts with such extension are
       // used by tests to specify artifacts that may or may not be missing.
@@ -128,7 +128,7 @@ public class TestAction extends AbstractAction {
 
     try {
       effect.call();
-    } catch (RuntimeException | Error | ActionExecutionException e) {
+    } catch (RuntimeException | Error | ActionExecutionException | InterruptedException e) {
       throw e;
     } catch (Exception e) {
       DetailedExitCode code = CrashFailureDetails.detailedExitCodeForThrowable(e);

src/test/java/com/google/devtools/build/lib/skyframe/ParallelBuilderTest.java
@@ -681,7 +681,7 @@ public class ParallelBuilderTest extends TimestampBuilderTestCase {
               ImmutableSet.of(out)) {
             @Override
             public ActionResult execute(ActionExecutionContext actionExecutionContext)
-                throws ActionExecutionException {
+                throws ActionExecutionException, InterruptedException {
               if (catastrophe && iCopy == 0) {
                 try {
                   Thread.sleep(300); // 300ms

src/test/java/com/google/devtools/build/lib/skyframe/SequencedSkyframeExecutorTest.java
@@ -677,7 +677,7 @@ public final class SequencedSkyframeExecutorTest extends BuildViewTestCase {
 
     @Override
     public ActionResult execute(ActionExecutionContext actionExecutionContext)
-        throws ActionExecutionException {
+        throws ActionExecutionException, InterruptedException {
       ActionResult actionResult = super.execute(actionExecutionContext);
       try {
         getPrimaryOutput().getPath().deleteTree();
@@ -1432,6 +1432,98 @@ public final class SequencedSkyframeExecutorTest extends BuildViewTestCase {
     TrackingAwaiter.INSTANCE.assertNoErrors();
   }
 
+  @Test
+  public void interruptDoesntSuppressErrorOutput() throws Exception {
+    Path root = getExecRoot();
+    PathFragment execPath = PathFragment.create("out").getRelative("dir");
+    PathFragment cyclesourceFragment = PathFragment.create("cyclesource");
+    Artifact.SourceArtifact cycleArtifact =
+        new Artifact.SourceArtifact(
+            ArtifactRoot.asSourceRoot(Root.fromPath(rootDirectory)),
+            cyclesourceFragment,
+            ArtifactOwner.NULL_OWNER);
+    rootDirectory.getRelative(cyclesourceFragment).createSymbolicLink(cyclesourceFragment);
+    ActionLookupKey lc1 = new InjectedActionLookupKey("lc1");
+    Artifact output =
+        DerivedArtifact.create(
+            ArtifactRoot.asDerivedRoot(root, RootType.Output, "out"),
+            execPath.getRelative("cycleOutput"),
+            lc1);
+    Action action1 = new DummyAction(cycleArtifact, output);
+    SkyValue ctValue1 =
+        ValueWithMetadata.normal(
+            createActionLookupValue(action1, lc1),
+            null,
+            NestedSetBuilder.emptySet(Order.STABLE_ORDER),
+            NestedSetBuilder.emptySet(Order.STABLE_ORDER));
+    ActionLookupKey lc2 = new InjectedActionLookupKey("lc2");
+    Artifact output2 =
+        DerivedArtifact.create(
+            ArtifactRoot.asDerivedRoot(root, RootType.Output, "out"),
+            execPath.getRelative("bar"),
+            lc2);
+    CountDownLatch startedSleep = new CountDownLatch(1);
+    @SuppressWarnings("ThreadSleepMillis")
+    Action slowAction =
+        new TestAction(
+            (Callable<Void> & Serializable)
+                () -> {
+                  startedSleep.countDown();
+                  Thread.sleep(TestUtils.WAIT_TIMEOUT_MILLISECONDS);
+                  throw new IllegalStateException("Should have been interrupted");
+                },
+            NestedSetBuilder.emptySet(Order.STABLE_ORDER),
+            ImmutableSet.of(output2));
+    SkyValue ctValue2 =
+        ValueWithMetadata.normal(
+            createActionLookupValue(slowAction, lc2),
+            null,
+            NestedSetBuilder.emptySet(Order.STABLE_ORDER),
+            NestedSetBuilder.emptySet(Order.STABLE_ORDER));
+    skyframeExecutor
+        .getEvaluatorForTesting()
+        .injectGraphTransformerForTesting(
+            NotifyingHelper.makeNotifyingTransformer(
+                (key, type, order, context) -> {
+                  if (EventType.IS_READY.equals(type)
+                      && key instanceof ActionLookupData
+                      && lc1.equals(((ActionLookupData) key).getActionLookupKey())) {
+                    TrackingAwaiter.INSTANCE.awaitLatchAndTrackExceptions(startedSleep, "No sleep");
+                  }
+                }));
+    skyframeExecutor
+        .getDifferencerForTesting()
+        .inject(ImmutableMap.of(lc1, ctValue1, lc2, ctValue2));
+    // Do a null build, so that the skyframe executor initializes the action executor properly.
+    skyframeExecutor.setActionOutputRoot(getOutputPath());
+    skyframeExecutor.setActionExecutionProgressReportingObjects(
+        EMPTY_PROGRESS_SUPPLIER,
+        EMPTY_COMPLETION_RECEIVER,
+        ActionExecutionStatusReporter.create(reporter));
+    skyframeExecutor.buildArtifacts(
+        reporter,
+        ResourceManager.instanceForTestingOnly(),
+        new DummyExecutor(fileSystem, rootDirectory),
+        ImmutableSet.of(),
+        ImmutableSet.of(),
+        ImmutableSet.of(),
+        ImmutableSet.of(),
+        ImmutableSet.of(),
+        options,
+        NULL_CHECKER,
+        null,
+        null,
+        null);
+
+    skyframeExecutor.prepareBuildingForTestingOnly(
+        reporter, new DummyExecutor(fileSystem, rootDirectory), options, NULL_CHECKER, null);
+    reporter.removeHandler(failFastHandler); // Expect errors.
+    evaluate(Artifact.keys(ImmutableList.of(output, output2)));
+    assertContainsEvent(
+        "Test dir/cycleOutput failed: error reading file 'cyclesource': Symlink cycle");
+    assertContainsEvent("Test dir/cycleOutput failed: 1 input file(s) are in error");
+  }
+
   /**
    * Tests that events from action lookup keys (i.e., analysis events) are not stored in execution.
    * This test is actually more extreme than Blaze is, since it skips the analysis phase and so
@@ -1601,7 +1693,7 @@ public final class SequencedSkyframeExecutorTest extends BuildViewTestCase {
 
     @Override
     public ActionResult execute(ActionExecutionContext actionExecutionContext)
-        throws ActionExecutionException {
+        throws ActionExecutionException, InterruptedException {
       ActionResult actionResult = super.execute(actionExecutionContext);
       assertThat(executed.getAndSet(true)).isFalse();
       return actionResult;

src/test/java/com/google/devtools/build/lib/skyframe/TimestampBuilderTestCase.java
@@ -565,7 +565,7 @@ public abstract class TimestampBuilderTestCase extends FoundationTestCase {
 
     @Override
     public ActionResult execute(ActionExecutionContext actionExecutionContext)
-        throws ActionExecutionException {
+        throws ActionExecutionException, InterruptedException {
       ActionResult actionResult = super.execute(actionExecutionContext);
       try {
         FileSystemUtils.copyFile(