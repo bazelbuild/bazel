2172c545c78410fe111ea2e8f8429d7efda98fd2
Googler <noreply@google.com>
2022-02-14 10:21:05 -0800

    Mark incompatible_disable_native_apple_binary_rule as Deprecated, now that it's a no-op. Remove instances of this flag in tests.
    PiperOrigin-RevId: 428545477

--- src/test/java/com/google/devtools/build/lib/testutil/TestConstants.java
+++ src/test/java/com/google/devtools/build/lib/testutil/TestConstants.java
@@ -130,8 +130,7 @@ public class TestConstants {
           "--incompatible_py3_is_default=false",
           "--incompatible_py2_outputs_are_suffixed=false",
           // TODO(#7849): Remove after flag flip.
-          "--incompatible_use_toolchain_resolution_for_java_rules",
-          "--incompatible_disable_native_apple_binary_rule=false");
+          "--incompatible_use_toolchain_resolution_for_java_rules");
 
   /** Partial query to filter out implicit dependencies of C/C++ rules. */
   public static final String CC_DEPENDENCY_CORRECTION =
