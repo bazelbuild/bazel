29d46eb41a413430498ab033f84a8d960edfb6fb
ehaugh <ehaugh@google.com>
2022-02-16 13:42:29 -0800

    Include more information about configurations in cquery proto formatted outout
    1. Add a repeated Configuration field to CqueryResult, and fill in the
    checksum, platform, and mnemonic fields.
    
    2. Add a configuration_id field to ConfiguredTarget, and deprecate the existing
    configuration field in ConfiguredTarget. The configuration_id field is an index
    that points to the Configuration message stored in CqueryResult, following the
    same pattern as ActionGraphContainer. This avoids duplicating the same
    Configuration message for each ConfiguredTarget.
    
    RELNOTES: Include more information about configurations in cquery proto
    formatted output. This deprecates the configuration field of
    AnalysisProtosV2.ConfiguredTarget, and adds a new field, configuration_id, to
    be used instead.
    PiperOrigin-RevId: 429129916

--- src/main/java/com/google/devtools/build/lib/query2/cquery/ProtoOutputFormatterCallback.java
+++ src/main/java/com/google/devtools/build/lib/query2/cquery/ProtoOutputFormatterCallback.java
@@ -13,11 +13,17 @@
 // limitations under the License.
 package com.google.devtools.build.lib.query2.cquery;
 
+import static com.google.common.collect.ImmutableList.toImmutableList;
+
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Ordering;
+import com.google.devtools.build.lib.actions.BuildConfigurationEvent;
 import com.google.devtools.build.lib.analysis.AnalysisProtosV2;
+import com.google.devtools.build.lib.analysis.AnalysisProtosV2.Configuration;
 import com.google.devtools.build.lib.analysis.config.ConfigMatchingProvider;
+import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos;
 import com.google.devtools.build.lib.cmdline.Label;
 import com.google.devtools.build.lib.events.ExtendedEventHandler;
 import com.google.devtools.build.lib.packages.Attribute;
@@ -29,6 +35,8 @@ import com.google.devtools.build.lib.query2.proto.proto2api.Build;
 import com.google.devtools.build.lib.query2.proto.proto2api.Build.QueryResult;
 import com.google.devtools.build.lib.query2.query.aspectresolvers.AspectResolver;
 import com.google.devtools.build.lib.query2.query.output.ProtoOutputFormatter;
+import com.google.devtools.build.lib.skyframe.BuildConfigurationKey;
+import com.google.devtools.build.lib.skyframe.ConfiguredTargetKey;
 import com.google.devtools.build.lib.skyframe.SkyframeExecutor;
 import com.google.protobuf.Message;
 import com.google.protobuf.TextFormat;
@@ -36,7 +44,9 @@ import com.google.protobuf.util.JsonFormat;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /** Proto output formatter for cquery results. */
 class ProtoOutputFormatterCallback extends CqueryThreadsafeCallback {
@@ -58,9 +68,37 @@ class ProtoOutputFormatterCallback extends CqueryThreadsafeCallback {
     }
   }
 
+  private static class ConfigurationCache {
+    private final Map<BuildConfigurationEvent, Integer> cache = new HashMap<>();
+
+    public int getId(BuildConfigurationEvent buildConfigurationEvent) {
+      return cache.computeIfAbsent(buildConfigurationEvent, event -> cache.size() + 1);
+    }
+
+    public ImmutableList<Configuration> getConfigurations() {
+      return cache.entrySet().stream()
+          .map(
+              entry -> {
+                BuildConfigurationEvent event = entry.getKey();
+                String checksum = event.getEventId().getConfiguration().getId();
+                BuildEventStreamProtos.Configuration configProto =
+                    event.asStreamProto(/* unusedConverters= */ null).getConfiguration();
+
+                return AnalysisProtosV2.Configuration.newBuilder()
+                    .setChecksum(checksum)
+                    .setMnemonic(configProto.getMnemonic())
+                    .setPlatformName(configProto.getPlatformName())
+                    .setId(entry.getValue())
+                    .build();
+              })
+          .collect(toImmutableList());
+    }
+  }
+
   private final OutputType outputType;
   private final AspectResolver resolver;
   private final SkyframeExecutor skyframeExecutor;
+  private final ConfigurationCache configurationCache = new ConfigurationCache();
   private final JsonFormat.Printer jsonPrinter = JsonFormat.printer();
 
   private AnalysisProtosV2.CqueryResult.Builder protoResult;
@@ -127,6 +165,7 @@ class ProtoOutputFormatterCallback extends CqueryThreadsafeCallback {
 
   @VisibleForTesting
   public AnalysisProtosV2.CqueryResult getProtoResult() {
+    protoResult.addAllConfigurations(configurationCache.getConfigurations());
     return protoResult.build();
   }
 
@@ -150,6 +189,18 @@ class ProtoOutputFormatterCallback extends CqueryThreadsafeCallback {
         String checksum = keyedConfiguredTarget.getConfigurationChecksum();
         builder.setConfiguration(
             AnalysisProtosV2.Configuration.newBuilder().setChecksum(String.valueOf(checksum)));
+
+        ConfiguredTargetKey configuredTargetKey = keyedConfiguredTarget.getConfiguredTargetKey();
+        // Some targets don't have a configuration, e.g. InputFileConfiguredTarget
+        if (configuredTargetKey != null) {
+          BuildConfigurationKey configurationKey = configuredTargetKey.getConfigurationKey();
+          if (configurationKey != null) {
+            BuildConfigurationEvent buildConfigurationEvent =
+                getConfiguration(configurationKey).toBuildEvent();
+            int id = configurationCache.getId(buildConfigurationEvent);
+            builder.setConfigurationId(id);
+          }
+        }
       }
 
       protoResult.addResults(builder.build());
--- src/test/java/com/google/devtools/build/lib/query2/cquery/ProtoOutputFormatterCallbackTest.java
+++ src/test/java/com/google/devtools/build/lib/query2/cquery/ProtoOutputFormatterCallbackTest.java
@@ -14,12 +14,15 @@
 package com.google.devtools.build.lib.query2.cquery;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.extensions.proto.ProtoTruth.assertThat;
 import static com.google.devtools.build.lib.packages.Attribute.attr;
 import static com.google.devtools.build.lib.packages.BuildType.LABEL_LIST;
 
 import com.google.common.collect.Iterables;
 import com.google.common.eventbus.EventBus;
 import com.google.devtools.build.lib.analysis.AnalysisProtosV2;
+import com.google.devtools.build.lib.analysis.AnalysisProtosV2.Configuration;
+import com.google.devtools.build.lib.analysis.AnalysisProtosV2.ConfiguredTarget;
 import com.google.devtools.build.lib.analysis.config.TransitionFactories;
 import com.google.devtools.build.lib.analysis.util.MockRule;
 import com.google.devtools.build.lib.events.Event;
@@ -34,10 +37,10 @@ import com.google.devtools.build.lib.query2.proto.proto2api.Build;
 import com.google.devtools.build.lib.query2.query.aspectresolvers.AspectResolver.Mode;
 import com.google.devtools.build.lib.util.FileTypeSet;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -118,7 +121,7 @@ public class ProtoOutputFormatterCallbackTest extends ConfiguredTargetQueryTest 
   }
 
   @Test
-  public void testConfigurationHash() throws Exception {
+  public void testConfigurations() throws Exception {
     FooPatchTransition attributePatchTransition = new FooPatchTransition("SET BY PATCH");
 
     MockRule ruleWithPatch =
@@ -130,32 +133,101 @@ public class ProtoOutputFormatterCallbackTest extends ConfiguredTargetQueryTest 
                         attr("deps", LABEL_LIST)
                             .allowedFileTypes(FileTypeSet.ANY_FILE)
                             .cfg(TransitionFactories.of(attributePatchTransition))));
+    MockRule parentRuleClass =
+        () ->
+            MockRule.define(
+                "parent_rule",
+                (builder, env) ->
+                    builder.add(attr("deps", LABEL_LIST).allowedFileTypes(FileTypeSet.ANY_FILE)));
 
-    helper.useRuleClassProvider(setRuleClassProviders(ruleWithPatch, getSimpleRule()).build());
+    helper.useRuleClassProvider(
+        setRuleClassProviders(ruleWithPatch, parentRuleClass, getSimpleRule()).build());
 
     writeFile(
         "test/BUILD",
-        "my_rule(name = 'my_rule',",
-        "  deps = [':patched'],",
+        "parent_rule(name = 'parent_rule',",
+        "  deps = [':transition_rule'],",
+        ")",
+        "my_rule(name = 'transition_rule',",
+        "  deps = [':patched', ':dep'],",
         ")",
         "simple_rule(name = 'dep')");
 
-    // Assert checksum from proto is proper checksum.
-    AnalysisProtosV2.ConfiguredTarget myRuleProto =
-        Iterables.getOnlyElement(getOutput("//test:my_rule").getResultsList());
-    KeyedConfiguredTarget myRule = Iterables.getOnlyElement(eval("//test:my_rule"));
+    AnalysisProtosV2.CqueryResult cqueryResult = getOutput("deps(//test:parent_rule)");
+    List<Configuration> configurations = cqueryResult.getConfigurationsList();
+    assertThat(configurations).hasSize(2);
 
-    assertThat(myRuleProto.getConfiguration().getChecksum())
-        .isEqualTo(myRule.getConfigurationChecksum());
+    assertThat(configurations)
+        .ignoringFieldDescriptors(
+            Configuration.getDescriptor().findFieldByName("checksum"),
+            Configuration.getDescriptor().findFieldByName("id"))
+        .containsExactly(
+            Configuration.newBuilder().setMnemonic("k8-fastbuild").setPlatformName("k8").build(),
+            Configuration.newBuilder().setMnemonic("k8-fastbuild").setPlatformName("k8").build());
 
-    // Assert checksum for two configured targets in proto are not the same.
-    List<AnalysisProtosV2.ConfiguredTarget> protoDeps =
-        getOutput("deps(//test:my_rule)").getResultsList();
-    assertThat(protoDeps).hasSize(2);
+    List<ConfiguredTarget> resultsList = cqueryResult.getResultsList();
 
-    Iterator<AnalysisProtosV2.ConfiguredTarget> protoDepsIterator = protoDeps.iterator();
-    assertThat(protoDepsIterator.next().getConfiguration().getChecksum())
-        .isNotEqualTo(protoDepsIterator.next().getConfiguration().getChecksum());
+    ConfiguredTarget parentRuleProto = getRuleProtoByName(resultsList, "//test:parent_rule");
+    Set<KeyedConfiguredTarget> keyedTargets = eval("deps(//test:parent_rule)");
+
+    KeyedConfiguredTarget parentRule = getKeyedTargetByLabel(keyedTargets, "//test:parent_rule");
+    assertThat(parentRuleProto.getConfiguration().getChecksum())
+        .isEqualTo(parentRule.getConfigurationChecksum());
+
+    Configuration parentConfiguration =
+        getConfigurationForId(configurations, parentRuleProto.getConfigurationId());
+    assertThat(parentConfiguration.getChecksum()).isEqualTo(parentRule.getConfigurationChecksum());
+
+    ConfiguredTarget transitionRuleProto =
+        getRuleProtoByName(resultsList, "//test:transition_rule");
+    KeyedConfiguredTarget transitionRule =
+        getKeyedTargetByLabel(keyedTargets, "//test:transition_rule");
+    assertThat(transitionRuleProto.getConfiguration().getChecksum())
+        .isEqualTo(transitionRule.getConfigurationChecksum());
+
+    Configuration transitionConfiguration =
+        getConfigurationForId(configurations, transitionRuleProto.getConfigurationId());
+    assertThat(transitionConfiguration.getChecksum())
+        .isEqualTo(transitionRule.getConfigurationChecksum());
+
+    ConfiguredTarget depRuleProto = getRuleProtoByName(resultsList, "//test:dep");
+    Configuration depRuleConfiguration =
+        getConfigurationForId(configurations, depRuleProto.getConfigurationId());
+    KeyedConfiguredTarget depRule = getKeyedTargetByLabel(keyedTargets, "//test:dep");
+
+    assertThat(depRuleProto.getConfiguration().getChecksum())
+        .isEqualTo(depRule.getConfigurationChecksum());
+
+    // Assert the proto checksums for targets in different configurations are not the same.
+    assertThat(depRuleConfiguration.getChecksum())
+        .isNotEqualTo(transitionConfiguration.getChecksum());
+
+    // Targets without a configuration have a configuration_id of 0.
+    ConfiguredTarget fileTargetProto =
+        resultsList.stream()
+            .filter(result -> "//test:patched".equals(result.getTarget().getSourceFile().getName()))
+            .findAny()
+            .orElseThrow();
+    assertThat(fileTargetProto.getConfigurationId()).isEqualTo(0);
+  }
+
+  private KeyedConfiguredTarget getKeyedTargetByLabel(
+      Set<KeyedConfiguredTarget> keyedTargets, String label) {
+    return Iterables.getOnlyElement(
+        keyedTargets.stream()
+            .filter(t -> label.equals(t.getConfiguredTarget().getLabel().getCanonicalForm()))
+            .collect(Collectors.toSet()));
+  }
+
+  private Configuration getConfigurationForId(List<Configuration> configurations, int id) {
+    return configurations.stream().filter(c -> c.getId() == id).findAny().orElseThrow();
+  }
+
+  private ConfiguredTarget getRuleProtoByName(List<ConfiguredTarget> resultsList, String s) {
+    return resultsList.stream()
+        .filter(result -> s.equals(result.getTarget().getRule().getName()))
+        .findAny()
+        .orElseThrow();
   }
 
   @Test
