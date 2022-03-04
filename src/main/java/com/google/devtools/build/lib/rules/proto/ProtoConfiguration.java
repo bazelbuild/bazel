b32b304ee4fbdc14d628a6b3d497a75ecf0fb47d
Ivo List <ilist@google.com>
2022-03-04 15:14:42 +0000

    Add import.

--- src/main/java/com/google/devtools/build/lib/rules/proto/ProtoConfiguration.java
+++ src/main/java/com/google/devtools/build/lib/rules/proto/ProtoConfiguration.java
@@ -21,6 +21,7 @@ import com.google.devtools.build.lib.analysis.config.CoreOptionConverters.Strict
 import com.google.devtools.build.lib.analysis.config.Fragment;
 import com.google.devtools.build.lib.analysis.config.FragmentOptions;
 import com.google.devtools.build.lib.analysis.config.RequiresOptions;
+import com.google.devtools.build.lib.analysis.starlark.annotations.StarlarkConfigurationField;
 import com.google.devtools.build.lib.cmdline.Label;
 import com.google.devtools.build.lib.concurrent.ThreadSafety.Immutable;
 import com.google.devtools.build.lib.starlarkbuildapi.ProtoConfigurationApi;
