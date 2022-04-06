53b9cb8637c0faddc6b122a1daab72bcc274bdec
twerth <twerth@google.com>
2022-04-06 01:22:44 -0700

    Catch NumberFormatException while trying to parse thread id.
    Fixes #14949.
    
    PiperOrigin-RevId: 439775241

--- src/main/java/com/google/devtools/build/lib/profiler/Profiler.java
+++ src/main/java/com/google/devtools/build/lib/profiler/Profiler.java
@@ -1021,7 +1021,14 @@ public final class Profiler {
         return MAX_SORT_INDEX;
       }
 
-      long extractedNumber = Long.parseLong(numberMatcher.group());
+      long extractedNumber;
+      try {
+        extractedNumber = Long.parseLong(numberMatcher.group());
+      } catch (NumberFormatException e) {
+        // If the number cannot be parsed, e.g. is larger than a long, the actual position is not
+        // really relevant.
+        return MAX_SORT_INDEX;
+      }
 
       if (threadName.startsWith("skyframe-evaluator")) {
         return SKYFRAME_EVALUATOR_SHIFT + extractedNumber;
