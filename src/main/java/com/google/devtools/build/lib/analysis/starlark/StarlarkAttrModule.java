0d0df1951a7a9a7744028054f83386a08cf83e5f
Yannic <yannic@engflow.com>
2022-04-12 11:59:01 -0700

    Add incompatible flag to disable cfg="host" from Starlark
    Host transitions are problematic where the host and exec environment are
    different (e.g., with Remote Execution). This change adds an incompatible
    flag that fails analysis if Starlark rules use `cfg = "host"`.
    
    Migrating is pretty straigt forward (replace `host` with `exec`) and
    should be automatable by buildifier (although I haven't tried to do so).
    
    Closes #14383.
    
    PiperOrigin-RevId: 441253060

--- src/main/java/com/google/devtools/build/lib/analysis/starlark/StarlarkAttrModule.java
+++ src/main/java/com/google/devtools/build/lib/analysis/starlark/StarlarkAttrModule.java
@@ -41,6 +41,8 @@ import com.google.devtools.build.lib.packages.StarlarkProviderIdentifier;
 import com.google.devtools.build.lib.packages.Type;
 import com.google.devtools.build.lib.packages.Type.ConversionException;
 import com.google.devtools.build.lib.packages.Type.LabelClass;
+import com.google.devtools.build.lib.packages.semantics.BuildLanguageOptions;
+import com.google.devtools.build.lib.starlarkbuildapi.NativeComputedDefaultApi;
 import com.google.devtools.build.lib.starlarkbuildapi.StarlarkAttrModuleApi;
 import com.google.devtools.build.lib.util.FileType;
 import com.google.devtools.build.lib.util.FileTypeSet;
@@ -226,7 +228,16 @@ public final class StarlarkAttrModule implements StarlarkAttrModuleApi {
             "late-bound attributes must not have a split configuration transition");
       }
       if (trans.equals("host")) {
-        builder.cfg(HostTransition.createFactory());
+        boolean disableStarlarkHostTransitions =
+            thread
+                .getSemantics()
+                .getBool(BuildLanguageOptions.INCOMPATIBLE_DISABLE_STARLARK_HOST_TRANSITIONS);
+        if (disableStarlarkHostTransitions) {
+          throw new EvalException(
+              "'cfg = \"host\"' is deprecated and should no longer be used. Please use "
+                  + "'cfg = \"exec\"' instead.");
+        }
+        builder.cfg(ExecutionTransitionFactory.create());
       } else if (trans.equals("exec")) {
         builder.cfg(ExecutionTransitionFactory.create());
       } else if (trans instanceof ExecutionTransitionFactory) {
@@ -252,8 +263,8 @@ public final class StarlarkAttrModule implements StarlarkAttrModuleApi {
         // android_split_transition because users of those transitions should already know about
         // them.
         throw Starlark.errorf(
-            "cfg must be either 'host', 'target', 'exec' or a starlark defined transition defined"
-                + " by the exec() or transition() functions.");
+            "cfg must be either 'target', 'exec' or a starlark defined transition defined by the "
+                + "exec() or transition() functions.");
       }
     }
 
