cbb6318873585c89d5fee726a99eb4034cfdc684
Chenchu Kolli <ckolli@google.com>
2022-05-09 19:34:34 -0500

    Unify sandbox/remote handling of empty TreeArtifact inputs (#15449)
    Actions that take a TreeArtifact as input should see a corresponding
    directory even if the TreeArtifact is empty.
    
    Previously, SandboxHelpers contained special handling for the case of
    empty TreeArtifact action inputs to ensure that they are added to the
    sandbox as empty directories. As pointed out in a comment, this logic
    should live in SpawnInputExpander, where it would also apply to remote
    execution.
    
    This commit adds a integration tests for this previously untested case,
    extracts the logic into SpawnInputExpander and adapts
    DirectoryTreeBuilder to handle empty TreeArtifacts when creating the
    Merkle trees.
    
    Note: The Merkle tree builder now reports an error when it encounters a
    non-empty TreeArtifact. Such an artifact should have been expanded by
    SpawnInputExpander and if it wasn't, e.g. because it wasn't properly
    registered with Skyframe, it can't be expanded at this point anyway.
    The tests uncovered that the spawn for split coverage postprocessing declared the
    coverage dir artifact as such an input. In this case, it can simply be
    removed as the coverage script creates the coverage dir if it doesn't
    exist.
    
    Closes #15276.
    
    PiperOrigin-RevId: 446452587
    
    Co-authored-by: Fabian Meumertzheim <fabian@meumertzhe.im>

--- src/main/java/com/google/devtools/build/lib/worker/WorkerSpawnRunner.java
+++ src/main/java/com/google/devtools/build/lib/worker/WorkerSpawnRunner.java
@@ -192,8 +192,6 @@ final class WorkerSpawnRunner implements SpawnRunner {
         inputFiles =
             helpers.processInputFiles(
                 context.getInputMapping(PathFragment.EMPTY_FRAGMENT),
-                spawn,
-                context.getArtifactExpander(),
                 execRoot);
       }
       SandboxOutputs outputs = helpers.getOutputs(spawn);
@@ -251,7 +249,10 @@ final class WorkerSpawnRunner implements SpawnRunner {
     }
 
     List<ActionInput> inputs =
-        ActionInputHelper.expandArtifacts(spawn.getInputFiles(), context.getArtifactExpander());
+        ActionInputHelper.expandArtifacts(
+            spawn.getInputFiles(),
+            context.getArtifactExpander(),
+            /* keepEmptyTreeArtifacts= */ false);
 
     for (ActionInput input : inputs) {
       byte[] digestBytes = inputFileCache.getMetadata(input).getDigest();
