f02e2fad3d12955ce0b2b808b1d0d7c3cffb4f4f
sgjesse <sgjesse@google.com>
2022-05-11 00:30:03 -0700

    Consistent min API level for desugar and dexing
    PiperOrigin-RevId: 447922944

--- src/tools/android/java/com/google/devtools/build/android/r8/CoreLibraryDesugar.java
+++ src/tools/android/java/com/google/devtools/build/android/r8/CoreLibraryDesugar.java
@@ -100,7 +100,7 @@ public class CoreLibraryDesugar {
 
     @Option(
         name = "min_sdk_version",
-        defaultValue = "1",
+        defaultValue = "13", // Same as Constants.MIN_API_LEVEL.
         category = "misc",
         documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
         effectTags = {OptionEffectTag.UNKNOWN},
