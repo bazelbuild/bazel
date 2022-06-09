c6f71c127004ce7627ad6bd2d6aaa305a7cfd3d6
Googler <jhorvitz@google.com>
2022-06-09 08:39:24 -0700

    Automated rollback of commit a0a0d09debd090fbe74bba59b5d6d3107aaf97e9.
    *** Reason for rollback ***
    
    b/235458248
    
    *** Original change description ***
    
    Create a dedicated graph lookup reason for rewinding and add a wildcard to `createIfAbsentBatch` to match `getBatch`.
    
    PiperOrigin-RevId: 453933527
    Change-Id: If633aa38c86ebf17ab7d0eac16c03117a66546cb

--- src/test/java/com/google/devtools/build/skyframe/NotifyingHelper.java
+++ src/test/java/com/google/devtools/build/skyframe/NotifyingHelper.java
@@ -17,6 +17,7 @@ import com.google.auto.value.AutoValue;
 import com.google.common.base.Joiner;
 import com.google.common.base.MoreObjects;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Maps.EntryTransformer;
 import com.google.devtools.build.lib.concurrent.ThreadSafety.ThreadSafe;
 import com.google.devtools.build.lib.util.GroupedList;
 import com.google.devtools.build.lib.util.GroupedList.GroupedListHelper;
@@ -54,6 +55,15 @@ public class NotifyingHelper {
 
   protected final Listener graphListener;
 
+  protected final EntryTransformer<SkyKey, ThinNodeEntry, NodeEntry> wrapEntry =
+      new EntryTransformer<SkyKey, ThinNodeEntry, NodeEntry>() {
+        @Nullable
+        @Override
+        public NotifyingNodeEntry transformEntry(SkyKey key, @Nullable ThinNodeEntry nodeEntry) {
+          return wrapEntry(key, nodeEntry);
+        }
+      };
+
   NotifyingHelper(Listener graphListener) {
     this.graphListener = new ErrorRecordingDelegatingListener(graphListener);
   }
@@ -86,7 +96,8 @@ public class NotifyingHelper {
         notifyingHelper.graphListener.accept(key, EventType.GET_BATCH, Order.BEFORE, reason);
       }
       return Maps.transformEntries(
-          delegate.getBatch(requestor, reason, keys), notifyingHelper::wrapEntry);
+          delegate.getBatch(requestor, reason, keys),
+          notifyingHelper.wrapEntry);
     }
 
     @Nullable
@@ -118,13 +129,14 @@ public class NotifyingHelper {
 
     @Override
     public Map<SkyKey, ? extends NodeEntry> createIfAbsentBatch(
-        @Nullable SkyKey requestor, Reason reason, Iterable<? extends SkyKey> keys)
+        @Nullable SkyKey requestor, Reason reason, Iterable<SkyKey> keys)
         throws InterruptedException {
       for (SkyKey key : keys) {
         notifyingHelper.graphListener.accept(key, EventType.CREATE_IF_ABSENT, Order.BEFORE, null);
       }
       return Maps.transformEntries(
-          delegate.createIfAbsentBatch(requestor, reason, keys), notifyingHelper::wrapEntry);
+          delegate.createIfAbsentBatch(requestor, reason, keys),
+          notifyingHelper.wrapEntry);
     }
 
     @Override
