f4d90fb4e0563278a6d04001239702ec5b376b00
Benjamin Peterson <benjamin@engflow.com>
2022-06-14 13:08:24 -0700

    Disable some `MaybeFailOnceUploadService` assertions when an RPC fails.
    If `onNext()` sends a failure, there is little value in trying to assertions in `onComplete()`. Remote tests logs had NPEs and other scary exceptions due to failed assertions in the RPC error case.

--- src/test/java/com/google/devtools/build/lib/remote/ByteStreamUploaderTest.java
+++ src/test/java/com/google/devtools/build/lib/remote/ByteStreamUploaderTest.java
@@ -1574,6 +1574,7 @@ public class ByteStreamUploaderTest {
         private HashCode digestHash;
         private byte[] receivedData;
         private long nextOffset;
+        private boolean failed;
 
         @Override
         public void onNext(WriteRequest writeRequest) {
@@ -1594,6 +1595,7 @@ public class ByteStreamUploaderTest {
           if (shouldFail) {
             uploadsFailedOnce.add(digestHash);
             response.onError(Status.INTERNAL.asException());
+            failed = true;
             return;
           }
 
@@ -1612,6 +1614,9 @@ public class ByteStreamUploaderTest {
 
         @Override
         public void onCompleted() {
+          if (failed) {
+            return;
+          }
           byte[] expectedBlob = blobsByHash.get(digestHash);
           assertThat(receivedData).isEqualTo(expectedBlob);
 
