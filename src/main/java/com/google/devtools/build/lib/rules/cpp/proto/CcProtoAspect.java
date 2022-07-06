5c53672a718b6218cc8b7f654d5065ac7a81f2ac
Googler <twerth@google.com>
2022-07-06 03:50:18 -0700

    Add @Nullable annotations where they were missing.
    PiperOrigin-RevId: 459214181
    Change-Id: I32d73dd0734d950f3aafac32cee5959ec9ba5658

--- src/main/java/com/google/devtools/build/lib/rules/cpp/proto/CcProtoAspect.java
+++ src/main/java/com/google/devtools/build/lib/rules/cpp/proto/CcProtoAspect.java
@@ -73,6 +73,7 @@ import com.google.devtools.build.lib.vfs.PathFragment;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import javax.annotation.Nullable;
 
 /** Part of the implementation of cc_proto_library. */
 public abstract class CcProtoAspect extends NativeAspectClass implements ConfiguredAspectFactory {
@@ -96,6 +97,7 @@ public abstract class CcProtoAspect extends NativeAspectClass implements Configu
   }
 
   @Override
+  @Nullable
   public ConfiguredAspect create(
       ConfiguredTargetAndData ctadBase,
       RuleContext ruleContext,
