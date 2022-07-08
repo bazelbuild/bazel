bd47127d9d8b60bf89796b22499d57bc5ed2ee92
Yannic Bonenberger <yannic@engflow.com>
2022-07-08 01:40:43 -0700

    [credentialhelper] Add types to communicate with the subprocess
    This change adds `GetCredentials{Request,Response}`, which we'll use to
    pass data in `stdin` to the credental helper subprocess, and read the
    response from `stdout` of the subprocess.
    
    Progress on https://github.com/bazelbuild/proposals/blob/main/designs/2022-06-07-bazel-credential-helpers.md#proposal
    
    Closes #15803.
    
    PiperOrigin-RevId: 459701706
    Change-Id: Icbd9fc491546ee5599d5a9d04680671b06a91a85

---
+++ src/main/java/com/google/devtools/build/lib/authandtls/credentialhelper/GetCredentialsRequest.java
@@ -0,0 +1,103 @@
+// Copyright 2022 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.devtools.build.lib.authandtls.credentialhelper;
+
+import com.google.auto.value.AutoValue;
+import com.google.common.base.Preconditions;
+import com.google.errorprone.annotations.Immutable;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.TypeAdapter;
+import com.google.gson.annotations.JsonAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+import java.io.IOException;
+import java.net.URI;
+import java.util.Locale;
+
+/**
+ * Request for the {@code get} command of the <a
+ * href="https://github.com/bazelbuild/proposals/blob/main/designs/2022-06-07-bazel-credential-helpers.md#proposal">Credential
+ * Helper Protocol</a>.
+ */
+@AutoValue
+@AutoValue.CopyAnnotations
+@Immutable
+@JsonAdapter(GetCredentialsRequest.GsonTypeAdapter.class)
+public abstract class GetCredentialsRequest {
+  /** Returns the {@link URI} this request is for. */
+  public abstract URI getUri();
+
+  /** Returns a new builder for {@link GetCredentialsRequest}. */
+  public static Builder newBuilder() {
+    return new AutoValue_GetCredentialsRequest.Builder();
+  }
+
+  /** Builder for {@link GetCredentialsRequest}. */
+  @AutoValue.Builder
+  public abstract static class Builder {
+    /** Sets the {@link URI} this request is for. */
+    public abstract Builder setUri(URI uri);
+
+    /** Returns the newly constructed {@link GetCredentialsRequest}. */
+    public abstract GetCredentialsRequest build();
+  }
+
+  /** GSON adapter for GetCredentialsRequest. */
+  public static final class GsonTypeAdapter extends TypeAdapter<GetCredentialsRequest> {
+    @Override
+    public void write(JsonWriter writer, GetCredentialsRequest value) throws IOException {
+      Preconditions.checkNotNull(writer);
+      Preconditions.checkNotNull(value);
+
+      writer.beginObject();
+      writer.name("uri").value(value.getUri().toString());
+      writer.endObject();
+    }
+
+    @Override
+    public GetCredentialsRequest read(JsonReader reader) throws IOException {
+      Preconditions.checkNotNull(reader);
+
+      Builder request = newBuilder();
+
+      if (reader.peek() != JsonToken.BEGIN_OBJECT) {
+        throw new JsonSyntaxException(
+            String.format(Locale.US, "Expected object, got %s", reader.peek()));
+      }
+      reader.beginObject();
+      while (reader.hasNext()) {
+        String name = reader.nextName();
+        switch (name) {
+          case "uri":
+            if (reader.peek() != JsonToken.STRING) {
+              throw new JsonSyntaxException(
+                  String.format(
+                      Locale.US, "Expected value of 'url' to be a string, got %s", reader.peek()));
+            }
+            request.setUri(URI.create(reader.nextString()));
+            break;
+
+          default:
+            // We intentionally ignore unknown keys to achieve forward compatibility with requests
+            // coming from newer tools.
+            reader.skipValue();
+        }
+      }
+      reader.endObject();
+      return request.build();
+    }
+  }
+}
---
+++ src/test/java/com/google/devtools/build/lib/authandtls/credentialhelper/GetCredentialsResponseTest.java
@@ -0,0 +1,158 @@
+// Copyright 2022 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.devtools.build.lib.authandtls.credentialhelper;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
+import com.google.common.collect.ImmutableList;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Tests for {@link GetCredentialsResponse}. */
+@RunWith(JUnit4.class)
+public class GetCredentialsResponseTest {
+  private static final Gson GSON = new Gson();
+
+  @Test
+  public void parseValid() {
+    assertThat(GSON.fromJson("{}", GetCredentialsResponse.class).getHeaders()).isEmpty();
+    assertThat(GSON.fromJson("{\"headers\": {}}", GetCredentialsResponse.class).getHeaders())
+        .isEmpty();
+
+    GetCredentialsResponse.Builder expectedResponseBuilder = GetCredentialsResponse.newBuilder();
+    expectedResponseBuilder.headersBuilder().put("a", ImmutableList.of());
+    expectedResponseBuilder.headersBuilder().put("b", ImmutableList.of("b"));
+    expectedResponseBuilder.headersBuilder().put("c", ImmutableList.of("c", "c"));
+    GetCredentialsResponse expectedResponse = expectedResponseBuilder.build();
+
+    assertThat(
+            GSON.fromJson(
+                "{\"headers\": {\"c\": [\"c\", \"c\"], \"a\": [], \"b\": [\"b\"]}}",
+                GetCredentialsResponse.class))
+        .isEqualTo(expectedResponse);
+  }
+
+  @Test
+  public void parseWithExtraFields() {
+    assertThat(GSON.fromJson("{\"foo\": 123}", GetCredentialsResponse.class).getHeaders())
+        .isEmpty();
+    assertThat(
+            GSON.fromJson("{\"foo\": 123, \"bar\": []}", GetCredentialsResponse.class).getHeaders())
+        .isEmpty();
+
+    GetCredentialsResponse.Builder expectedResponseBuilder = GetCredentialsResponse.newBuilder();
+    expectedResponseBuilder.headersBuilder().put("a", ImmutableList.of());
+    expectedResponseBuilder.headersBuilder().put("b", ImmutableList.of("b"));
+    expectedResponseBuilder.headersBuilder().put("c", ImmutableList.of("c", "c"));
+    GetCredentialsResponse expectedResponse = expectedResponseBuilder.build();
+
+    assertThat(
+            GSON.fromJson(
+                "{\"foo\": 123, \"headers\": {\"c\": [\"c\", \"c\"], \"a\": [], \"b\": [\"b\"]},"
+                    + " \"bar\": 123}",
+                GetCredentialsResponse.class))
+        .isEqualTo(expectedResponse);
+  }
+
+  @Test
+  public void parseInvalid() {
+    assertThrows(
+        JsonSyntaxException.class, () -> GSON.fromJson("[]", GetCredentialsResponse.class));
+    assertThrows(
+        JsonSyntaxException.class, () -> GSON.fromJson("\"foo\"", GetCredentialsResponse.class));
+    assertThrows(JsonSyntaxException.class, () -> GSON.fromJson("1", GetCredentialsResponse.class));
+  }
+
+  @Test
+  public void parseInvalidHeadersEnvelope() {
+    assertThrows(
+        JsonSyntaxException.class,
+        () -> GSON.fromJson("{\"headers\": null}", GetCredentialsResponse.class));
+    assertThrows(
+        JsonSyntaxException.class,
+        () -> GSON.fromJson("{\"headers\": \"foo\"}", GetCredentialsResponse.class));
+    assertThrows(
+        JsonSyntaxException.class,
+        () -> GSON.fromJson("{\"headers\": []}", GetCredentialsResponse.class));
+    assertThrows(
+        JsonSyntaxException.class,
+        () -> GSON.fromJson("{\"headers\": 1}", GetCredentialsResponse.class));
+  }
+
+  @Test
+  public void parseInvalidHeadersValue() {
+    assertThrows(
+        JsonSyntaxException.class,
+        () -> GSON.fromJson("{\"headers\": {\"a\": null}}", GetCredentialsResponse.class));
+    assertThrows(
+        JsonSyntaxException.class,
+        () -> GSON.fromJson("{\"headers\": {\"a\": 1}}", GetCredentialsResponse.class));
+    assertThrows(
+        JsonSyntaxException.class,
+        () -> GSON.fromJson("{\"headers\": {\"a\": {}}}", GetCredentialsResponse.class));
+    assertThrows(
+        JsonSyntaxException.class,
+        () -> GSON.fromJson("{\"headers\": {\"a\": \"a\"}}", GetCredentialsResponse.class));
+    assertThrows(
+        JsonSyntaxException.class,
+        () -> GSON.fromJson("{\"headers\": {\"a\": [null]}}", GetCredentialsResponse.class));
+    assertThrows(
+        JsonSyntaxException.class,
+        () -> GSON.fromJson("{\"headers\": {\"a\": [\"a\", null]}}", GetCredentialsResponse.class));
+    assertThrows(
+        JsonSyntaxException.class,
+        () -> GSON.fromJson("{\"headers\": {\"a\": [null, \"a\"]}}", GetCredentialsResponse.class));
+    assertThrows(
+        JsonSyntaxException.class,
+        () -> GSON.fromJson("{\"headers\": {\"a\": [\"a\", 1]}}", GetCredentialsResponse.class));
+    assertThrows(
+        JsonSyntaxException.class,
+        () -> GSON.fromJson("{\"headers\": {\"a\": [1, \"a\"]}}", GetCredentialsResponse.class));
+    assertThrows(
+        JsonSyntaxException.class,
+        () -> GSON.fromJson("{\"headers\": {\"a\": [\"a\", []]}}", GetCredentialsResponse.class));
+    assertThrows(
+        JsonSyntaxException.class,
+        () -> GSON.fromJson("{\"headers\": {\"a\": [[], \"a\"]}}", GetCredentialsResponse.class));
+    assertThrows(
+        JsonSyntaxException.class,
+        () -> GSON.fromJson("{\"headers\": {\"a\": [\"a\", {}]}}", GetCredentialsResponse.class));
+    assertThrows(
+        JsonSyntaxException.class,
+        () -> GSON.fromJson("{\"headers\": {\"a\": [{}, \"a\"]}}", GetCredentialsResponse.class));
+  }
+
+  @Test
+  public void serializeEmptyHeaders() {
+    GetCredentialsResponse expectedResponse = GetCredentialsResponse.newBuilder().build();
+    assertThat(GSON.toJson(expectedResponse)).isEqualTo("{}");
+  }
+
+  @Test
+  public void roundTrip() {
+    GetCredentialsResponse.Builder expectedResponseBuilder = GetCredentialsResponse.newBuilder();
+    expectedResponseBuilder.headersBuilder().put("a", ImmutableList.of());
+    expectedResponseBuilder.headersBuilder().put("b", ImmutableList.of("b"));
+    expectedResponseBuilder.headersBuilder().put("c", ImmutableList.of("c", "c"));
+    GetCredentialsResponse expectedResponse = expectedResponseBuilder.build();
+
+    assertThat(GSON.fromJson(GSON.toJson(expectedResponse), GetCredentialsResponse.class))
+        .isEqualTo(expectedResponse);
+  }
+}
