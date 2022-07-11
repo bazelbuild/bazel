b6d8ea06c37b8d1700da3542696c541d0730c2b5
Googler <gnish@google.com>
2022-07-11 03:57:49 -0700

    Internal change.
    PiperOrigin-RevId: 460174313
    Change-Id: I9c3c7581cbced79f224b8b1e940aa9b413ade640

--- src/main/java/com/google/devtools/build/lib/starlarkbuildapi/cpp/CcModuleApi.java
+++ src/main/java/com/google/devtools/build/lib/starlarkbuildapi/cpp/CcModuleApi.java
@@ -1259,6 +1259,13 @@ public interface CcModuleApi<
             positional = false,
             named = true,
             defaultValue = "unbound"),
+        @Param(
+            name = "test_only_target",
+            positional = false,
+            named = true,
+            documented = false,
+            allowedTypes = {@ParamType(type = Boolean.class)},
+            defaultValue = "unbound"),
       })
   Tuple createLinkingContextFromCompilationOutputs(
       StarlarkActionFactoryT starlarkActionFactoryApi,
@@ -1278,6 +1285,7 @@ public interface CcModuleApi<
       Object stamp,
       Object linkedDllNameSuffix,
       Object winDefFile,
+      Object testOnlyTarget,
       StarlarkThread thread)
       throws InterruptedException, EvalException;
 
