943e8cfb9970ca86f88ba60c944601573b366db5
Googler <wyv@google.com>
2022-07-11 08:40:14 -0700

    Parse command-line options twice; the second time with the main repo mapping
    This CL adds an additional parsing round for all command-line options. The second round passes the main repo mapping as a conversion context, so any label-typed options can correctly go through repo mapping.
    
    Similarly, when options are set in a Starlark transition, they'd also go through repo mapping. (except for Starlark-defined options, which will be fixed in a follow-up CL)
    
    https://github.com/bazelbuild/bazel/issues/14852
    
    PiperOrigin-RevId: 460221378
    Change-Id: I3aab3ee02cd5097743172edbe95d33d5e140300a

--- src/main/java/com/google/devtools/build/lib/analysis/config/StarlarkDefinedConfigTransition.java
+++ src/main/java/com/google/devtools/build/lib/analysis/config/StarlarkDefinedConfigTransition.java
@@ -23,6 +23,7 @@ import com.google.common.collect.ImmutableSortedMap;
 import com.google.common.collect.Ordering;
 import com.google.common.collect.Sets;
 import com.google.devtools.build.lib.cmdline.Label;
+import com.google.devtools.build.lib.cmdline.Label.PackageContext;
 import com.google.devtools.build.lib.cmdline.LabelSyntaxException;
 import com.google.devtools.build.lib.cmdline.RepositoryMapping;
 import com.google.devtools.build.lib.events.Event;
@@ -77,6 +78,7 @@ public abstract class StarlarkDefinedConfigTransition implements ConfigurationTr
   private final ImmutableMap<String, String> inputsCanonicalizedToGiven;
   private final ImmutableMap<String, String> outputsCanonicalizedToGiven;
   private final Location location;
+  private final Label.PackageContext packageContext;
 
   private StarlarkDefinedConfigTransition(
       List<String> inputs,
@@ -86,11 +88,16 @@ public abstract class StarlarkDefinedConfigTransition implements ConfigurationTr
       Location location)
       throws EvalException {
     this.location = location;
+    packageContext = Label.PackageContext.of(parentLabel.getPackageIdentifier(), repoMapping);
 
     this.outputsCanonicalizedToGiven =
         getCanonicalizedSettings(repoMapping, parentLabel, outputs, Settings.OUTPUTS);
     this.inputsCanonicalizedToGiven =
         getCanonicalizedSettings(repoMapping, parentLabel, inputs, Settings.INPUTS);
+  }
+
+  public PackageContext getPackageContext() {
+    return packageContext;
   }
 
   /**
