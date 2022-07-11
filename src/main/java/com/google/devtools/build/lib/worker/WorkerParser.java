bazelbuild/bazel

commit cb2cd9fd2b65311da927777c35939701add5b879

Adds flag `--experimental_worker_strict_flagfiles` that checks if the worker argument list conforms to the spec.
The spec calls for only one flag file, at the end of the argument list. However, the implementation allows for
multiple flag files with startup arguments possible between or after. This has led to subtle bugs.

We could also make the spec conform to the implementation, but there are fewer surprises possible with the spec, and
it makes it much clearer that there is a distinction between startup args and per-request args.

We would like to make it even more explicit, and possibly at the same time allow differing flags for workers and
non-workers (e.g. for JIT/GC tweaking), but that would be a larger change.

PiperOrigin-RevId: 458415814
Change-Id: I7eacfa7e6344348f66b6c83139c3bcd893bdd70b

src/main/java/com/google/devtools/build/lib/worker/WorkerParser.java
@@ -15,9 +15,9 @@
 package com.google.devtools.build.lib.worker;
 
 import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.MoreObjects;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
 import com.google.common.hash.HashCode;
 import com.google.devtools.build.lib.actions.ExecException;
 import com.google.devtools.build.lib.actions.ExecutionRequirements.WorkerProtocolFormat;
@@ -42,13 +42,17 @@ import java.util.regex.Pattern;
  * and a separate list of flag files. The result is encapsulated as a {@link WorkerConfig}.
  */
 class WorkerParser {
-  public static final String ERROR_MESSAGE_PREFIX =
+  private static final String ERROR_MESSAGE_PREFIX =
       "Worker strategy cannot execute this %s action, ";
-  public static final String REASON_NO_FLAGFILE =
-      "because the command-line arguments do not contain at least one @flagfile or --flagfile=";
+  private static final String REASON_NO_FLAGFILE =
+      "because the command-line arguments do not contain exactly one @flagfile or --flagfile=";
+  private static final String REASON_EXCESS_FLAGFILE =
+      "because the command-line arguments has a @flagfile or --flagfile= argument before the end";
+  private static final String REASON_NO_FINAL_FLAGFILE =
+      "because the command-line arguments does not end with a @flagfile or --flagfile= argument";
 
   /** Pattern for @flagfile.txt and --flagfile=flagfile.txt */
-  private static final Pattern FLAG_FILE_PATTERN = Pattern.compile("(?:@|--?flagfile=)(.+)");
+  private static final Pattern FLAG_FILE_PATTERN = Pattern.compile("(?:@[^@]|--?flagfile=.)(.*)");
 
   /** The global execRoot. */
   private final Path execRoot;
@@ -148,26 +152,38 @@ class WorkerParser {
    * persistent worker ({@code workerArgs}) and the part that goes into the {@code WorkRequest}
    * protobuf ({@code flagFiles}).
    */
-  private ImmutableList<String> splitSpawnArgsIntoWorkerArgsAndFlagFiles(
+  @VisibleForTesting
+  ImmutableList<String> splitSpawnArgsIntoWorkerArgsAndFlagFiles(
       Spawn spawn, List<String> flagFiles) throws UserExecException {
     ImmutableList.Builder<String> workerArgs = ImmutableList.builder();
-    for (String arg : spawn.getArguments()) {
-      if (FLAG_FILE_PATTERN.matcher(arg).matches()) {
-        flagFiles.add(arg);
-      } else {
-        workerArgs.add(arg);
+    if (workerOptions.strictFlagfiles) {
+      ImmutableList<String> args = spawn.getArguments();
+      if (args.isEmpty()) {
+        throwFlagFileFailure(REASON_NO_FLAGFILE, spawn);
+      }
+      if (!FLAG_FILE_PATTERN.matcher(Iterables.getLast(args)).matches()) {
+        throwFlagFileFailure(REASON_NO_FINAL_FLAGFILE, spawn);
+      }
+      flagFiles.add(Iterables.getLast(args));
+      for (int i = 0; i < args.size() - 1; i++) {
+        if (FLAG_FILE_PATTERN.matcher(args.get(i)).matches()) {
+          throwFlagFileFailure(REASON_EXCESS_FLAGFILE, spawn);
+        } else {
+          workerArgs.add(args.get(i));
+        }
+      }
+    } else {
+      for (String arg : spawn.getArguments()) {
+        if (FLAG_FILE_PATTERN.matcher(arg).matches()) {
+          flagFiles.add(arg);
+        } else {
+          workerArgs.add(arg);
+        }
       }
     }
 
     if (flagFiles.isEmpty()) {
-      throw new UserExecException(
-          FailureDetails.FailureDetail.newBuilder()
-              .setMessage(
-                  String.format(ERROR_MESSAGE_PREFIX + REASON_NO_FLAGFILE, spawn.getMnemonic()))
-              .setWorker(
-                  FailureDetails.Worker.newBuilder()
-                      .setCode(FailureDetails.Worker.Code.NO_FLAGFILE))
-              .build());
+      throwFlagFileFailure(REASON_NO_FLAGFILE, spawn);
     }
 
     ImmutableList.Builder<String> mnemonicFlags = ImmutableList.builder();
@@ -176,10 +192,16 @@ class WorkerParser {
         .filter(entry -> entry.getKey().equals(spawn.getMnemonic()))
         .forEach(entry -> mnemonicFlags.add(entry.getValue()));
 
-    return workerArgs
-        .add("--persistent_worker")
-        .addAll(MoreObjects.firstNonNull(mnemonicFlags.build(), ImmutableList.of()))
-        .build();
+    return workerArgs.add("--persistent_worker").addAll(mnemonicFlags.build()).build();
+  }
+
+  private void throwFlagFileFailure(String reason, Spawn spawn) throws UserExecException {
+    throw new UserExecException(
+        FailureDetails.FailureDetail.newBuilder()
+            .setMessage(String.format(ERROR_MESSAGE_PREFIX + reason, spawn.getMnemonic()))
+            .setWorker(
+                FailureDetails.Worker.newBuilder().setCode(FailureDetails.Worker.Code.NO_FLAGFILE))
+            .build());
   }
 
   /** A pair of the {@link WorkerKey} and the list of flag files. */

src/main/java/com/google/devtools/build/lib/worker/WorkerOptions.java
@@ -195,4 +195,15 @@ public class WorkerOptions extends OptionsBase {
               + " per work request. Only workers that have the 'supports-multiplex-sandboxing' "
               + "execution requirement will be sandboxed.")
   public boolean multiplexSandboxing;
+
+  @Option(
+      name = "experimental_worker_strict_flagfiles",
+      defaultValue = "false",
+      documentationCategory = OptionDocumentationCategory.UNCATEGORIZED,
+      effectTags = {OptionEffectTag.EXECUTION},
+      help =
+          "If enabled, actions arguments for workers that do not follow the worker specification"
+              + " will cause an error. Worker arguments must have exactly one @flagfile argument"
+              + " as the last of its list of arguments.")
+  public boolean strictFlagfiles;
 }

src/test/java/com/google/devtools/build/lib/worker/BUILD
@@ -42,6 +42,8 @@ java_library(
         "//src/main/java/com/google/devtools/build/lib/actions",
         "//src/main/java/com/google/devtools/build/lib/actions:execution_requirements",
         "//src/main/java/com/google/devtools/build/lib/actions:localhost_capacity",
+        "//src/main/java/com/google/devtools/build/lib/buildeventstream/proto:build_event_stream_java_proto",
+        "//src/main/java/com/google/devtools/build/lib/cmdline",
         "//src/main/java/com/google/devtools/build/lib/collect/nestedset",
         "//src/main/java/com/google/devtools/build/lib/sandbox:sandbox_helpers",
         "//src/main/java/com/google/devtools/build/lib/shell",
@@ -50,6 +52,7 @@ java_library(
         "//src/main/java/com/google/devtools/build/lib/worker",
         "//src/main/java/com/google/devtools/build/lib/worker:worker_key",
         "//src/main/java/com/google/devtools/build/lib/worker:worker_spawn_runner",
+        "//src/main/java/net/starlark/java/syntax",
         "//src/test/java/com/google/devtools/build/lib/actions/util",
         "//third_party:guava",
     ],

src/test/java/com/google/devtools/build/lib/worker/TestUtils.java
@@ -19,12 +19,16 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSortedMap;
 import com.google.common.hash.HashCode;
+import com.google.devtools.build.lib.actions.ActionOwner;
+import com.google.devtools.build.lib.actions.BuildConfigurationEvent;
 import com.google.devtools.build.lib.actions.ExecutionRequirements;
 import com.google.devtools.build.lib.actions.ExecutionRequirements.WorkerProtocolFormat;
 import com.google.devtools.build.lib.actions.ResourceSet;
 import com.google.devtools.build.lib.actions.SimpleSpawn;
 import com.google.devtools.build.lib.actions.Spawn;
 import com.google.devtools.build.lib.actions.util.ActionsTestUtil;
+import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos;
+import com.google.devtools.build.lib.cmdline.Label;
 import com.google.devtools.build.lib.collect.nestedset.NestedSetBuilder;
 import com.google.devtools.build.lib.collect.nestedset.Order;
 import com.google.devtools.build.lib.shell.Subprocess;
@@ -36,6 +40,7 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import net.starlark.java.syntax.Location;
 
 /** Utilities that come in handy when unit-testing the worker code. */
 class TestUtils {
@@ -44,9 +49,14 @@ class TestUtils {
 
   /** A helper method to create a fake Spawn with the given execution info. */
   static Spawn createSpawn(ImmutableMap<String, String> executionInfo) {
+    return createSpawn(ImmutableList.of(), executionInfo);
+  }
+
+  static Spawn createSpawn(
+      ImmutableList<String> arguments, ImmutableMap<String, String> executionInfo) {
     return new SimpleSpawn(
         new ActionsTestUtil.NullAction(),
-        /* arguments= */ ImmutableList.of(),
+        arguments,
         /* environment= */ ImmutableMap.of(),
         executionInfo,
         /* inputs= */ NestedSetBuilder.emptySet(Order.STABLE_ORDER),
@@ -54,6 +64,21 @@ class TestUtils {
         ResourceSet.ZERO);
   }
 
+  static ActionOwner createActionOwner(String mnemonic) {
+    return ActionOwner.create(
+        Label.parseAbsoluteUnchecked("//null/action:owner"),
+        ImmutableList.of(),
+        new Location("dummy-file", 0, 0),
+        mnemonic,
+        "dummy-kind",
+        "dummy-configuration",
+        new BuildConfigurationEvent(
+            BuildEventStreamProtos.BuildEventId.getDefaultInstance(),
+            BuildEventStreamProtos.BuildEvent.getDefaultInstance()),
+        null,
+        ImmutableMap.of(),
+        null);
+  }
   /** A helper method to create a WorkerKey through WorkerParser. */
   static WorkerKey createWorkerKey(
       WorkerProtocolFormat protocolFormat,
@@ -98,7 +123,7 @@ class TestUtils {
         protocolFormat,
         fs,
         /* mnemonic= */ "dummy",
-        /* proxied= */ true,
+        /* multiplex= */ true,
         /* sandboxed= */ true,
         dynamic,
         /* args...= */ "arg1",

src/test/java/com/google/devtools/build/lib/worker/WorkerParserTest.java
@@ -16,10 +16,18 @@ package com.google.devtools.build.lib.worker;
 
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.devtools.build.lib.actions.ExecutionRequirements.SUPPORTS_MULTIPLEX_SANDBOXING;
+import static org.junit.Assert.assertThrows;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import com.google.devtools.build.lib.actions.Spawn;
+import com.google.devtools.build.lib.actions.UserExecException;
 import com.google.devtools.build.lib.vfs.DigestHashFunction;
 import com.google.devtools.build.lib.vfs.FileSystem;
 import com.google.devtools.build.lib.vfs.inmemoryfs.InMemoryFileSystem;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -119,5 +127,103 @@ public class WorkerParserTest {
     assertThat(keyDynamicMultiplexSandboxing.isMultiplex()).isTrue();
     assertThat(keyDynamicMultiplexSandboxing.isSandboxed()).isTrue();
     assertThat(keyDynamicMultiplexSandboxing.getWorkerTypeName()).isEqualTo("multiplex-worker");
+  }
+
+  @Test
+  public void splitSpawnArgsIntoWorkerArgsAndFlagFiles_splitsArgsBasicCase()
+      throws UserExecException {
+    WorkerOptions options = new WorkerOptions();
+    options.workerExtraFlags = ImmutableList.of();
+    WorkerParser parser = new WorkerParser(null, options, null, null);
+
+    Spawn spawn = TestUtils.createSpawn(ImmutableList.of("--foo", "@bar"), ImmutableMap.of());
+    List<String> flagFiles = new ArrayList<>();
+    ImmutableList<String> args = parser.splitSpawnArgsIntoWorkerArgsAndFlagFiles(spawn, flagFiles);
+    assertThat(args).containsExactly("--foo", "--persistent_worker").inOrder();
+    assertThat(flagFiles).containsExactly("@bar");
+  }
+
+  @Test
+  public void splitSpawnArgsIntoWorkerArgsAndFlagFiles_addsExtras() throws UserExecException {
+    WorkerOptions options = new WorkerOptions();
+    options.workerExtraFlags =
+        ImmutableList.of(
+            Maps.immutableEntry("Null", "--qux"),
+            Maps.immutableEntry("Other action", "--should_not_appear"),
+            Maps.immutableEntry("Null", "--quxify"));
+    WorkerParser parser = new WorkerParser(null, options, null, null);
+    Spawn spawn = TestUtils.createSpawn(ImmutableList.of("--foo", "@bar"), ImmutableMap.of());
+
+    List<String> flagFiles = new ArrayList<>();
+    ImmutableList<String> args = parser.splitSpawnArgsIntoWorkerArgsAndFlagFiles(spawn, flagFiles);
+
+    assertThat(args).containsExactly("--foo", "--persistent_worker", "--qux", "--quxify").inOrder();
+    assertThat(flagFiles).containsExactly("@bar");
+  }
+
+  @Test
+  public void splitSpawnArgsIntoWorkerArgsAndFlagFiles_addsFlagFiles() throws UserExecException {
+    WorkerOptions options = new WorkerOptions();
+    options.workerExtraFlags = ImmutableList.of();
+    options.strictFlagfiles = false;
+    WorkerParser parser = new WorkerParser(null, options, null, null);
+    Spawn spawn =
+        TestUtils.createSpawn(
+            ImmutableList.of("--foo", "--flagfile=bar", "@@escaped", "@bar", "@bartoo", "--final"),
+            ImmutableMap.of());
+
+    List<String> flagFiles = new ArrayList<>();
+    ImmutableList<String> args = parser.splitSpawnArgsIntoWorkerArgsAndFlagFiles(spawn, flagFiles);
+
+    assertThat(args)
+        .containsExactly("--foo", "@@escaped", "--final", "--persistent_worker")
+        .inOrder();
+    assertThat(flagFiles).containsExactly("--flagfile=bar", "@bar", "@bartoo").inOrder();
+  }
+
+  @Test
+  public void splitSpawnArgsIntoWorkerArgsAndFlagFiles_addsFlagFilesStrict()
+      throws UserExecException {
+    WorkerOptions options = new WorkerOptions();
+    options.workerExtraFlags = ImmutableList.of();
+    options.strictFlagfiles = true;
+    WorkerParser parser = new WorkerParser(null, options, null, null);
+    Spawn spawn =
+        TestUtils.createSpawn(
+            ImmutableList.of("--foo", "@@escaped", "--final", "@bar"), ImmutableMap.of());
+
+    List<String> flagFiles = new ArrayList<>();
+    ImmutableList<String> args = parser.splitSpawnArgsIntoWorkerArgsAndFlagFiles(spawn, flagFiles);
+
+    assertThat(args)
+        .containsExactly("--foo", "@@escaped", "--final", "--persistent_worker")
+        .inOrder();
+    assertThat(flagFiles).containsExactly("@bar");
+  }
+
+  @Test
+  public void splitSpawnArgsIntoWorkerArgsAndFlagFiles_strictFlagFiles() throws UserExecException {
+    assertIllegalFlags("Must have args");
+    assertIllegalFlags("Must have a flagfile", "--foo", "--final");
+    assertIllegalFlags("Flagfile must be at the end", "@earlyFile", "--final");
+    assertIllegalFlags("Only one flagfile allowed", "@earlyFile", "--final", "@lateFile");
+    assertIllegalFlags(
+        "Only one flagfile allowed, regardless of syntax",
+        "--flagfile=foo",
+        "--final",
+        "@lateFile");
+  }
+
+  private void assertIllegalFlags(String message, String... args) {
+    WorkerOptions options = new WorkerOptions();
+    options.workerExtraFlags = ImmutableList.of();
+    options.strictFlagfiles = true;
+    WorkerParser parser = new WorkerParser(null, options, null, null);
+    Spawn spawn = TestUtils.createSpawn(ImmutableList.copyOf(args), ImmutableMap.of());
+
+    assertThrows(
+        message,
+        UserExecException.class,
+        () -> parser.splitSpawnArgsIntoWorkerArgsAndFlagFiles(spawn, new ArrayList<>()));
   }
 }

commit 107a54edcf75dc026cf643df82c13fe32a1eb043

Fix flag file regexp broken by https://github.com/bazelbuild/bazel/commit/cb2cd9fd2b65311da927777c35939701add5b879.
PiperOrigin-RevId: 460216776
Change-Id: I31f5ce3a4a7ce76d635ba5b0ce90f0eac41df0ad

src/main/java/com/google/devtools/build/lib/worker/WorkerParser.java
@@ -51,8 +51,8 @@ class WorkerParser {
   private static final String REASON_NO_FINAL_FLAGFILE =
       "because the command-line arguments does not end with a @flagfile or --flagfile= argument";
 
-  /** Pattern for @flagfile.txt and --flagfile=flagfile.txt */
-  private static final Pattern FLAG_FILE_PATTERN = Pattern.compile("(?:@[^@]|--?flagfile=.)(.*)");
+  /** Pattern for @flagfile.txt and --flagfile=flagfile.txt. This doesn't handle @@-escapes. */
+  private static final Pattern FLAG_FILE_PATTERN = Pattern.compile("(?:@|--?flagfile=)(.+)");
 
   /** The global execRoot. */
   private final Path execRoot;
@@ -147,6 +147,10 @@ class WorkerParser {
         protocolFormat);
   }
 
+  private static boolean isFlagFileArg(String arg) {
+    return FLAG_FILE_PATTERN.matcher(arg).matches() && !arg.startsWith("@@");
+  }
+
   /**
    * Splits the command-line arguments of the {@code Spawn} into the part that is used to start the
    * persistent worker ({@code workerArgs}) and the part that goes into the {@code WorkRequest}
@@ -161,12 +165,12 @@ class WorkerParser {
       if (args.isEmpty()) {
         throwFlagFileFailure(REASON_NO_FLAGFILE, spawn);
       }
-      if (!FLAG_FILE_PATTERN.matcher(Iterables.getLast(args)).matches()) {
+      if (!isFlagFileArg(Iterables.getLast(args))) {
         throwFlagFileFailure(REASON_NO_FINAL_FLAGFILE, spawn);
       }
       flagFiles.add(Iterables.getLast(args));
       for (int i = 0; i < args.size() - 1; i++) {
-        if (FLAG_FILE_PATTERN.matcher(args.get(i)).matches()) {
+        if (isFlagFileArg(args.get(i))) {
           throwFlagFileFailure(REASON_EXCESS_FLAGFILE, spawn);
         } else {
           workerArgs.add(args.get(i));
@@ -174,7 +178,7 @@ class WorkerParser {
       }
     } else {
       for (String arg : spawn.getArguments()) {
-        if (FLAG_FILE_PATTERN.matcher(arg).matches()) {
+        if (isFlagFileArg(arg)) {
           flagFiles.add(arg);
         } else {
           workerArgs.add(arg);