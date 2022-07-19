e4f1c9e5d109a906e911d88499bcc6962945ca13
Yannic Bonenberger <yannic@engflow.com>
2022-07-19 12:13:34 +0200

    hacky credential cache

--- src/main/java/com/google/devtools/build/lib/authandtls/credentialhelper/CredentialHelperCredentials.java
+++ src/main/java/com/google/devtools/build/lib/authandtls/credentialhelper/CredentialHelperCredentials.java
@@ -1,11 +1,18 @@
 package com.google.devtools.build.lib.authandtls.credentialhelper;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.CacheLoader;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.google.auth.Credentials;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
+import com.google.devtools.build.lib.events.Event;
 import java.io.IOException;
 import java.net.URI;
+import java.time.Duration;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
 
@@ -17,6 +24,37 @@ public class CredentialHelperCredentials extends Credentials {
   private final CredentialHelperProvider credentialHelperProvider;
   private final CredentialHelperEnvironment credentialHelperEnvironment;
   private final Optional<Credentials> fallbackCredentials;
+
+  private final LoadingCache<URI, GetCredentialsResponse> cache =
+      Caffeine.newBuilder()
+          .expireAfterWrite(Duration.ofSeconds(5))
+          .build(new CacheLoader<URI, GetCredentialsResponse>() {
+    @Override
+    public GetCredentialsResponse load(URI uri) throws IOException, InterruptedException {
+      Preconditions.checkNotNull(uri);
+
+      Optional<GetCredentialsResponse> response = loadInternal(uri);
+      if (response.isPresent()) {
+        return response.get();
+      }
+
+      return null;
+    }
+
+    private Optional<GetCredentialsResponse> loadInternal(URI uri) throws IOException, InterruptedException {
+      Preconditions.checkNotNull(uri);
+
+      credentialHelperEnvironment.getEventReporter().handle(Event.debug(String.format(Locale.US, "Invoking credential helper for %s", uri)));
+
+      Optional<CredentialHelper> maybeCredentialHelper = credentialHelperProvider.findCredentialHelper(uri);
+      if (!maybeCredentialHelper.isPresent()) {
+        return Optional.empty();
+      }
+      CredentialHelper credentialHelper = maybeCredentialHelper.get();
+
+      return Optional.of(credentialHelper.getCredentials(credentialHelperEnvironment, uri));
+    }
+  });
 
   public CredentialHelperCredentials(
       CredentialHelperProvider credentialHelperProvider,
@@ -57,17 +95,9 @@ public class CredentialHelperCredentials extends Credentials {
       URI uri) throws IOException {
     Preconditions.checkNotNull(uri);
 
-    Optional<CredentialHelper> maybeCredentialHelper = credentialHelperProvider.findCredentialHelper(uri);
-    if (!maybeCredentialHelper.isPresent()) {
+    GetCredentialsResponse response = cache.get(uri);
+    if (response == null) {
       return Optional.empty();
-    }
-    CredentialHelper credentialHelper = maybeCredentialHelper.get();
-
-    GetCredentialsResponse response;
-    try {
-      response = credentialHelper.getCredentials(credentialHelperEnvironment, uri);
-    } catch (InterruptedException e) {
-      throw new RuntimeException(e);
     }
 
     // The cast is needed to convert value type of map from `ImmutableList` to `List`.
@@ -86,8 +116,12 @@ public class CredentialHelperCredentials extends Credentials {
 
   @Override
   public void refresh() throws IOException {
+    credentialHelperEnvironment.getEventReporter().handle(Event.debug("Invalidating all cached credentials"));
+
     if (fallbackCredentials.isPresent()) {
       fallbackCredentials.get().refresh();
     }
+
+    cache.invalidateAll();
   }
 }
