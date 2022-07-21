308fba5c47cbb2ee8cc461c5ca55dad2658c0e60
Yannic Bonenberger <yannic@engflow.com>
2022-07-21 06:29:07 -0700

    Add CommandLinePathFactory to CommandEnvironment
    Progress on #15856
    
    Closes #15905.
    
    PiperOrigin-RevId: 462369707
    Change-Id: If8dd493227b67dd4ffe30c9e0af4d8f71de4fb9f

--- src/main/java/com/google/devtools/build/lib/runtime/CommandEnvironment.java
+++ src/main/java/com/google/devtools/build/lib/runtime/CommandEnvironment.java
@@ -104,6 +104,8 @@ public class CommandEnvironment {
   private final ImmutableList<Any> commandExtensions;
   private final ImmutableList.Builder<Any> responseExtensions = ImmutableList.builder();
   private final Consumer<String> shutdownReasonConsumer;
+  private final BuildResultListener buildResultListener;
+  private final CommandLinePathFactory commandLinePathFactory;
 
   private OutputService outputService;
   private TopDownActionCache topDownActionCache;
@@ -266,6 +268,11 @@ public class CommandEnvironment {
         repoEnvFromOptions.put(name, value);
       }
     }
+    this.buildResultListener = new BuildResultListener();
+    this.eventBus.register(this.buildResultListener);
+
+    this.commandLinePathFactory =
+        CommandLinePathFactory.create(runtime.getFileSystem(), directories);
   }
 
   private Path computeWorkingDirectory(CommonCommandOptions commandOptions)
@@ -839,5 +846,13 @@ public class CommandEnvironment {
 
   public void addResponseExtensions(Iterable<Any> extensions) {
     responseExtensions.addAll(extensions);
+  }
+
+  public BuildResultListener getBuildResultListener() {
+    return buildResultListener;
+  }
+
+  public CommandLinePathFactory getCommandLinePathFactory() {
+    return commandLinePathFactory;
   }
 }
