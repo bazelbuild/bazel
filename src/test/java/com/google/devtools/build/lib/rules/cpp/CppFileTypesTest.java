a24d1bc30ded24af0fd33646dcbd62a9a07e98b7
Fabian Meumertzheim <fabian@meumertzhe.im>
2022-07-27 09:36:17 +0200

    Make cpp assembly file extensions case sensitive again (#15986)
    This fixes an issue introduced by PR #14005 where .s and .S
    extensions were handled case-insensitive on Windows so the action
    assemble was triggered instead of preprocess_assemble.
    
    Closes #14131.
    
    PiperOrigin-RevId: 412005097
    
    Co-authored-by: Simon Bjorklen <simon.bjorklen@gmail.com>

--- src/test/java/com/google/devtools/build/lib/rules/cpp/CppFileTypesTest.java
+++ src/test/java/com/google/devtools/build/lib/rules/cpp/CppFileTypesTest.java
@@ -57,4 +57,16 @@ public class CppFileTypesTest {
     assertThat(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("libA.so.if.exp")).isFalse();
     assertThat(CppFileTypes.VERSIONED_SHARED_LIBRARY.matches("libA.so.if.lib")).isFalse();
   }
+
+  @Test
+  public void testCaseSensitiveAssemblyFiles() {
+    assertThat(CppFileTypes.ASSEMBLER_WITH_C_PREPROCESSOR.matches("foo.S")).isTrue();
+    assertThat(CppFileTypes.ASSEMBLER_WITH_C_PREPROCESSOR.matches("foo.s")).isFalse();
+    assertThat(CppFileTypes.PIC_ASSEMBLER.matches("foo.pic.s")).isTrue();
+    assertThat(CppFileTypes.PIC_ASSEMBLER.matches("foo.pic.S")).isFalse();
+    assertThat(CppFileTypes.ASSEMBLER.matches("foo.s")).isTrue();
+    assertThat(CppFileTypes.ASSEMBLER.matches("foo.asm")).isTrue();
+    assertThat(CppFileTypes.ASSEMBLER.matches("foo.pic.s")).isFalse();
+    assertThat(CppFileTypes.ASSEMBLER.matches("foo.S")).isFalse();
+  }
 }
