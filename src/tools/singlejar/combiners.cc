ee7569e31d7b4896ea5e37f80101e1c42e62cdf3
Benjamin Peterson <benjamin@engflow.com>
2022-08-02 11:22:31 -0700

    Remove unused MULTI_RELEASE_LENGTH constant.

--- src/tools/singlejar/combiners.cc
+++ src/tools/singlejar/combiners.cc
@@ -187,7 +187,6 @@ bool PropertyCombiner::Merge(const CDH * /*cdh*/, const LH * /*lh*/) {
 ManifestCombiner::~ManifestCombiner() {}
 
 static const char *MULTI_RELEASE = "Multi-Release: true";
-static const size_t MULTI_RELEASE_LENGTH = strlen(MULTI_RELEASE);
 
 static const char *MULTI_RELEASE_PREFIX = "Multi-Release: ";
 static const size_t MULTI_RELEASE_PREFIX_LENGTH = strlen(MULTI_RELEASE_PREFIX);
