2cfc4bd79bfc876b2d296195a623be0e30b41403
Googler <cushon@google.com>
2022-08-22 14:18:21 -0700

    Use validation actions for[]
    PiperOrigin-RevId: 469282079
    Change-Id: If78552b1b3fb347b648dc265c677ccb08d4a2267

--- src/main/java/com/google/devtools/build/lib/rules/java/JavaOptions.java
+++ src/main/java/com/google/devtools/build/lib/rules/java/JavaOptions.java
@@ -452,6 +452,16 @@ public class JavaOptions extends FragmentOptions {
   public boolean enforceOneVersionOnJavaTests;
 
   @Option(
+      name = "experimental_one_version_enforcement_validation_action",
+      defaultValue = "false",
+      documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
+      effectTags = {OptionEffectTag.UNKNOWN},
+      help =
+          "When enabled, and with experimental_one_version_enforcement set to a non-NONE value,"
+              + " enforce one version using validation actions.")
+  public boolean enforceOneVersionValidationAction;
+
+  @Option(
       name = "experimental_allow_runtime_deps_on_neverlink",
       defaultValue = "true",
       documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
@@ -645,6 +655,7 @@ public class JavaOptions extends FragmentOptions {
     host.importDepsCheckingLevel = importDepsCheckingLevel;
     // java_test targets can be used as a host tool, Ex: as a validating tool on a genrule.
     host.enforceOneVersionOnJavaTests = enforceOneVersionOnJavaTests;
+    host.enforceOneVersionValidationAction = enforceOneVersionValidationAction;
     host.allowRuntimeDepsOnNeverLink = allowRuntimeDepsOnNeverLink;
     host.addTestSupportToCompileTimeDeps = addTestSupportToCompileTimeDeps;
 
