2fb91c1ca5da6116af89ace14d108cba21ea1fc5
Richard Levasseur <rlevasseur@google.com>
2022-09-12 13:52:01 -0700

    Make macRequiresDarwinForExecution work for Starlark implementation
    The Starlark implementation uses platform-based detection, and Apple platforms
    aren't setup by default in the mock environment. To fix, setup our own
    platform, which is what tests for other languages do.
    
    PiperOrigin-RevId: 473836224
    Change-Id: Iaa2e3ae84ae32c0e8d6be9b4e9416b4f2d52a9ca

--- src/test/java/com/google/devtools/build/lib/rules/python/PyTestConfiguredTargetTest.java
+++ src/test/java/com/google/devtools/build/lib/rules/python/PyTestConfiguredTargetTest.java
@@ -18,6 +18,7 @@ import static com.google.common.truth.Truth.assertThat;
 
 import com.google.devtools.build.lib.actions.ExecutionRequirements;
 import com.google.devtools.build.lib.analysis.test.ExecutionInfo;
+import com.google.devtools.build.lib.testutil.TestConstants;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -32,7 +33,17 @@ public class PyTestConfiguredTargetTest extends PyExecutableConfiguredTargetTest
   @Test
   public void macRequiresDarwinForExecution() throws Exception {
     getAnalysisMock().ccSupport().setupCcToolchainConfigForCpu(mockToolsConfig, "darwin_x86_64");
-    useConfiguration("--cpu=darwin_x86_64");
+    // The default mock environment doesn't have platform_mappings (which map --cpu to a platform),
+    // nor does it have Apple platforms defined, so we have to set one up ourselves.
+    mockToolsConfig.create(
+        "mockplatforms/BUILD",
+        "platform(",
+        "    name='macos',",
+        String.format(
+            "    constraint_values=['%sos:macos'],", TestConstants.CONSTRAINTS_PACKAGE_ROOT),
+        ")",
+        "");
+    useConfiguration("--cpu=darwin_x86_64", "--platforms=//mockplatforms:macos");
     scratch.file(
         "pkg/BUILD", //
         "py_test(",
