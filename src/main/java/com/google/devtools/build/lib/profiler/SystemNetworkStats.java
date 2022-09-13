3dd17e697296859e82430d13d86fabe745fabfd3
Googler <chiwang@google.com>
2022-09-13 06:28:30 -0700

    Collect system network usages in Linux.
    https://github.com/bazelbuild/bazel/commit/e382cb2fd7b02e557a513f93a58eafd42ab234b0 added supported to collect system network during an invocation by using `sysctl` calls which only works on macOS.
    This change adds support to Linux by directly reading from `/proc/net/dev`.
    
    PiperOrigin-RevId: 474002025
    Change-Id: I57c07004cd6fcbd3139a9c52d849515c50f67940

--- src/main/java/com/google/devtools/build/lib/profiler/SystemNetworkStats.java
+++ src/main/java/com/google/devtools/build/lib/profiler/SystemNetworkStats.java
@@ -13,9 +13,15 @@
 // limitations under the License.
 package com.google.devtools.build.lib.profiler;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import com.google.auto.value.AutoValue;
+import com.google.common.base.Splitter;
 import com.google.devtools.build.lib.jni.JniLoader;
+import com.google.devtools.build.lib.util.OS;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,6 +29,8 @@ import java.util.Map;
 
 /** Utility class for query system network stats. */
 public class SystemNetworkStats {
+  private static final Splitter SPLITTER = Splitter.on(" ").omitEmptyStrings().trimResults();
+
   static {
     JniLoader.loadJni();
   }
@@ -50,8 +58,36 @@ public class SystemNetworkStats {
 
   public static Map<String, NetIoCounter> getNetIoCounters() throws IOException {
     Map<String, NetIoCounter> counters = new HashMap<>();
-    getNetIoCountersNative(counters);
+    switch (OS.getCurrent()) {
+      case LINUX:
+        getNetIoCountersLinux(counters);
+        break;
+      default:
+        getNetIoCountersNative(counters);
+    }
     return counters;
+  }
+
+  private static void getNetIoCountersLinux(Map<String, NetIoCounter> counters) throws IOException {
+    List<String> lines = Files.readAllLines(Paths.get("/proc/net/dev"), UTF_8);
+
+    // skip table header (first 2 lines)
+    for (String line : lines.subList(2, lines.size())) {
+      int colonAt = line.indexOf(':');
+      if (colonAt < 0) {
+        continue;
+      }
+      String name = line.substring(0, colonAt).strip();
+      long[] fields =
+          SPLITTER.splitToStream(line.substring(colonAt + 1)).mapToLong(Long::parseLong).toArray();
+      if (fields.length > 9) {
+        long bytesRecv = fields[0];
+        long packetsRecv = fields[1];
+        long bytesSent = fields[8];
+        long packetsSent = fields[9];
+        counters.put(name, NetIoCounter.create(bytesSent, bytesRecv, packetsSent, packetsRecv));
+      }
+    }
   }
 
   /** Value class for network interface address. */
