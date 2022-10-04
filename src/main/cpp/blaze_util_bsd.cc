7cccf1cb5f7a0bbd57c1819646ac8f333a61386f
Googler <noreply@google.com>
2022-10-04 04:48:05 -0700

    Update the default system javabase to `/usr/local/jdk-17` on OpenBSD.
    These days JDK 17 is the current LTS release and the most recent JDK available on OpenBSD, so it's a sensible default. JDK 8 is probably not what most users want anymore.
    
    Of course, users who want to use a different JDK can do so by setting `JAVA_HOME`. The default JDK [applies only when `JAVA_HOME` is not set or is unusable](https://github.com/bazelbuild/bazel/blob/cc71db2a465716ba175d3defaa6032107bf1dd90/src/main/cpp/blaze_util_bsd.cc#L199-L213).
    
    Why not also update the default for FreeBSD (on line 17)? Because the FreeBSD port of Bazel always sets the default to be whatever JDK was used to build Bazel (it [replaces the default path with a placeholder](https://github.com/freebsd/freebsd-ports/blob/79464085c1abdb6ae2c52cf59f7b77add3b6741c/devel/bazel/files/patch-src_main_cpp_blaze_util_bsd.cc#L7-L8), then [replaces this placeholder with the `JAVA_HOME` that was set at build time](https://github.com/freebsd/freebsd-ports/blob/79464085c1abdb6ae2c52cf59f7b77add3b6741c/devel/bazel/Makefile#L78-L80)). In other words, changing the default path for FreeBSD also would only create extra work for the maintainer of the FreeBSD port, without achieving any actual benefit.
    
    PiperOrigin-RevId: 478751809
    Change-Id: Ic84a07ad1ae2c6a8cdee45e142b99ce1c6f9758d

--- src/main/cpp/blaze_util_bsd.cc
+++ src/main/cpp/blaze_util_bsd.cc
@@ -16,7 +16,7 @@
 # define HAVE_PROCSTAT
 # define STANDARD_JAVABASE "/usr/local/openjdk8"
 #elif defined(__OpenBSD__)
-# define STANDARD_JAVABASE "/usr/local/jdk-1.8.0"
+# define STANDARD_JAVABASE "/usr/local/jdk-17"
 #else
 # error This BSD is not supported
 #endif
