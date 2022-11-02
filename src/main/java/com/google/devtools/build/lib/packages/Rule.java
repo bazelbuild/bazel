bbc221f60bc8c9177470529d85c3e47a5d9aaf21
Tony Aiuto <aiuto@google.com>
2022-11-01 17:10:20 -0700

    Improve the check for being a package metadata rule
    This allows the refactoring which will happen after default_applicable_licenses is renamed to default_package_metadata. The current behavior is intended to prevent
    `applicable_licenses` from being set on a `license` rule. The required behavior is that we don't set `applicable_licenses` on any of the metadata rules.
    
    Future changes might have to take into account the ability to set the license for a tool rule within `rules_package`.
    
    For background, see https://docs.google.com/document/d/1uyJjkKbE8kV8EinakaR9q-Un25zCukhoH_dRBkWHSKQ/edit#heading=h.izpa22p82m6c
    
    Closes #16596.
    
    PiperOrigin-RevId: 485457037
    Change-Id: Ifb105f646ae0c291a6841b3ddb84ed536e9d71e3

--- src/main/java/com/google/devtools/build/lib/packages/Rule.java
+++ src/main/java/com/google/devtools/build/lib/packages/Rule.java
@@ -884,7 +884,7 @@ public class Rule implements Target, DependencyFilter.AttributeInfoProvider {
     // have old-style licenses. This is hardcoding the representation
     // of new-style rules, but it's in the old-style licensing code path
     // and will ultimately be removed.
-    if (ruleClass.isBazelLicense()) {
+    if (ruleClass.isPackageMetadataRule()) {
       return License.NO_LICENSE;
     } else if (isAttrDefined("licenses", BuildType.LICENSE)
         && isAttributeValueExplicitlySpecified("licenses")) {
