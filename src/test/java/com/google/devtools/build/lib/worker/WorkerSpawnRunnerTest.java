bazelbuild/bazel

commit a556969326107423738fbf8a80bad9aaf9935578

Replace Path with RootedPath in SandboxHelpers and maintain a set of roots in SandboxHelpers.
This will make it possible to replace the root of inputs easily.

Work towards #3236.

RELNOTES: None.
PiperOrigin-RevId: 492432489
Change-Id: I896cd02001338779a1a9a7073131a86ea189ab51

src/main/java/com/google/devtools/build/lib/sandbox/DarwinSandboxedSpawnRunner.java
@@ -232,8 +232,7 @@ final class DarwinSandboxedSpawnRunner extends AbstractSandboxSpawnRunner {
 
     SandboxInputs inputs =
         helpers.processInputFiles(
-            context.getInputMapping(PathFragment.EMPTY_FRAGMENT),
-            execRoot);
+            context.getInputMapping(PathFragment.EMPTY_FRAGMENT), execRoot, execRoot, null);
     SandboxOutputs outputs = helpers.getOutputs(spawn);
 
     final Path sandboxConfigPath = sandboxPath.getRelative("sandbox.sb");

src/main/java/com/google/devtools/build/lib/sandbox/DockerSandboxedSpawnRunner.java
@@ -222,8 +222,7 @@ final class DockerSandboxedSpawnRunner extends AbstractSandboxSpawnRunner {
 
     SandboxInputs inputs =
         helpers.processInputFiles(
-            context.getInputMapping(PathFragment.EMPTY_FRAGMENT),
-            execRoot);
+            context.getInputMapping(PathFragment.EMPTY_FRAGMENT), execRoot, execRoot, null);
     SandboxOutputs outputs = helpers.getOutputs(spawn);
 
     Duration timeout = context.getTimeout();

src/main/java/com/google/devtools/build/lib/sandbox/ProcessWrapperSandboxedSpawnRunner.java
@@ -111,8 +111,7 @@ final class ProcessWrapperSandboxedSpawnRunner extends AbstractSandboxSpawnRunne
 
     SandboxInputs inputs =
         helpers.processInputFiles(
-            context.getInputMapping(PathFragment.EMPTY_FRAGMENT),
-            execRoot);
+            context.getInputMapping(PathFragment.EMPTY_FRAGMENT), execRoot, execRoot, null);
     SandboxOutputs outputs = helpers.getOutputs(spawn);
 
     if (sandboxfsProcess != null) {

src/main/java/com/google/devtools/build/lib/sandbox/WindowsSandboxedSpawnRunner.java
@@ -71,8 +71,7 @@ final class WindowsSandboxedSpawnRunner extends AbstractSandboxSpawnRunner {
 
     SandboxInputs readablePaths =
         helpers.processInputFiles(
-            context.getInputMapping(PathFragment.EMPTY_FRAGMENT),
-            execRoot);
+            context.getInputMapping(PathFragment.EMPTY_FRAGMENT), execRoot, execRoot, null);
 
     ImmutableSet.Builder<Path> writablePaths = ImmutableSet.builder();
     writablePaths.addAll(getWritableDirs(execRoot, environment));

src/main/java/com/google/devtools/build/lib/worker/WorkerSpawnRunner.java
@@ -187,7 +187,7 @@ final class WorkerSpawnRunner implements SpawnRunner {
           Profiler.instance().profile(ProfilerTask.WORKER_SETUP, "Setting up inputs")) {
         inputFiles =
             helpers.processInputFiles(
-                context.getInputMapping(PathFragment.EMPTY_FRAGMENT), execRoot);
+                context.getInputMapping(PathFragment.EMPTY_FRAGMENT), execRoot, execRoot, null);
       }
       SandboxOutputs outputs = helpers.getOutputs(spawn);
 

src/test/java/com/google/devtools/build/lib/worker/WorkerSpawnRunnerTest.java
@@ -165,7 +165,8 @@ public class WorkerSpawnRunnerTest {
             spawn,
             key,
             context,
-            new SandboxInputs(ImmutableMap.of(), ImmutableMap.of(), ImmutableMap.of()),
+            new SandboxInputs(
+                ImmutableMap.of(), ImmutableMap.of(), ImmutableMap.of(), ImmutableMap.of()),
             SandboxOutputs.create(ImmutableSet.of(), ImmutableSet.of()),
             ImmutableList.of(),
             inputFileCache,
@@ -279,7 +280,8 @@ public class WorkerSpawnRunnerTest {
                 spawn,
                 key,
                 context,
-                new SandboxInputs(ImmutableMap.of(), ImmutableMap.of(), ImmutableMap.of()),
+                new SandboxInputs(
+                    ImmutableMap.of(), ImmutableMap.of(), ImmutableMap.of(), ImmutableMap.of()),
                 SandboxOutputs.create(ImmutableSet.of(), ImmutableSet.of()),
                 ImmutableList.of(),
                 inputFileCache,
@@ -338,7 +340,8 @@ public class WorkerSpawnRunnerTest {
                 spawn,
                 key,
                 context,
-                new SandboxInputs(ImmutableMap.of(), ImmutableMap.of(), ImmutableMap.of()),
+                new SandboxInputs(
+                    ImmutableMap.of(), ImmutableMap.of(), ImmutableMap.of(), ImmutableMap.of()),
                 SandboxOutputs.create(ImmutableSet.of(), ImmutableSet.of()),
                 ImmutableList.of(),
                 inputFileCache,
@@ -393,7 +396,8 @@ public class WorkerSpawnRunnerTest {
                 spawn,
                 key,
                 context,
-                new SandboxInputs(ImmutableMap.of(), ImmutableMap.of(), ImmutableMap.of()),
+                new SandboxInputs(
+                    ImmutableMap.of(), ImmutableMap.of(), ImmutableMap.of(), ImmutableMap.of()),
                 SandboxOutputs.create(ImmutableSet.of(), ImmutableSet.of()),
                 ImmutableList.of(),
                 inputFileCache,
@@ -444,7 +448,8 @@ public class WorkerSpawnRunnerTest {
             spawn,
             key,
             context,
-            new SandboxInputs(ImmutableMap.of(), ImmutableMap.of(), ImmutableMap.of()),
+            new SandboxInputs(
+                ImmutableMap.of(), ImmutableMap.of(), ImmutableMap.of(), ImmutableMap.of()),
             SandboxOutputs.create(ImmutableSet.of(), ImmutableSet.of()),
             ImmutableList.of(),
             inputFileCache,
@@ -497,7 +502,8 @@ public class WorkerSpawnRunnerTest {
                     spawn,
                     key,
                     context,
-                    new SandboxInputs(ImmutableMap.of(), ImmutableMap.of(), ImmutableMap.of()),
+                    new SandboxInputs(
+                        ImmutableMap.of(), ImmutableMap.of(), ImmutableMap.of(), ImmutableMap.of()),
                     SandboxOutputs.create(ImmutableSet.of(), ImmutableSet.of()),
                     ImmutableList.of(),
                     inputFileCache,

src/main/java/com/google/devtools/build/lib/sandbox/AbstractContainerizingSandboxedSpawn.java
@@ -25,6 +25,7 @@ import com.google.devtools.build.lib.sandbox.SandboxHelpers.SandboxOutputs;
 import com.google.devtools.build.lib.vfs.FileSystemUtils;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.PathFragment;
+import com.google.devtools.build.lib.vfs.RootedPath;
 import java.io.IOException;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -133,9 +134,9 @@ public abstract class AbstractContainerizingSandboxedSpawn implements SandboxedS
     for (PathFragment fragment : inputsToCreate) {
       Path key = sandboxExecRoot.getRelative(fragment);
       if (inputs.getFiles().containsKey(fragment)) {
-        Path fileDest = inputs.getFiles().get(fragment);
+        RootedPath fileDest = inputs.getFiles().get(fragment);
         if (fileDest != null) {
-          copyFile(fileDest, key);
+          copyFile(fileDest.asPath(), key);
         } else {
           FileSystemUtils.createEmptyFile(key);
         }

src/main/java/com/google/devtools/build/lib/sandbox/LinuxSandboxedSpawnRunner.java
@@ -218,7 +218,8 @@ final class LinuxSandboxedSpawnRunner extends AbstractSandboxSpawnRunner {
     ImmutableSet<Path> writableDirs = getWritableDirs(sandboxExecRoot, environment);
 
     SandboxInputs inputs =
-        helpers.processInputFiles(context.getInputMapping(PathFragment.EMPTY_FRAGMENT), execRoot);
+        helpers.processInputFiles(
+            context.getInputMapping(PathFragment.EMPTY_FRAGMENT), execRoot, execRoot, null);
     SandboxOutputs outputs = helpers.getOutputs(spawn);
 
     Duration timeout = context.getTimeout();

commit 182988347ef02399bc8fbc31a0ad972f0ea72210

Make all sandboxed strategies work with Filesets on their inputs.
This was broken by https://github.com/bazelbuild/bazel/commit/a556969326107423738fbf8a80bad9aaf9935578 and partially fixed by https://github.com/bazelbuild/bazel/commit/70691f2b76be0b9530e49c3df18356925843b465 . Only
partially, because in my infinite wisdom, I didn't realize that it's not only
the Linux sandbox that can have this problem.

To avoid further issues, I now plumb the package roots to every sandboxed spawn
runner, even the Windows one, even though Bazel doesn't have Filesets.

RELNOTES: None.
PiperOrigin-RevId: 495271813
Change-Id: I20626c3863bf56018c7c72ac4a21f7d02e3ffc34

src/main/java/com/google/devtools/build/lib/sandbox/DarwinSandboxedSpawnRunner.java
@@ -37,6 +37,7 @@ import com.google.devtools.build.lib.util.OS;
 import com.google.devtools.build.lib.vfs.FileSystem;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.PathFragment;
+import com.google.devtools.build.lib.vfs.Root;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
@@ -102,6 +103,7 @@ final class DarwinSandboxedSpawnRunner extends AbstractSandboxSpawnRunner {
 
   private final SandboxHelpers helpers;
   private final Path execRoot;
+  private final ImmutableList<Root> packageRoots;
   private final boolean allowNetwork;
   private final ProcessWrapper processWrapper;
   private final Path sandboxBase;
@@ -139,6 +141,7 @@ final class DarwinSandboxedSpawnRunner extends AbstractSandboxSpawnRunner {
     super(cmdEnv);
     this.helpers = helpers;
     this.execRoot = cmdEnv.getExecRoot();
+    this.packageRoots = cmdEnv.getPackageLocator().getPathEntries();
     this.allowNetwork = helpers.shouldAllowNetwork(cmdEnv.getOptions());
     this.alwaysWritableDirs = getAlwaysWritableDirs(cmdEnv.getRuntime().getFileSystem());
     this.processWrapper = ProcessWrapper.fromCommandEnvironment(cmdEnv);
@@ -236,7 +239,7 @@ final class DarwinSandboxedSpawnRunner extends AbstractSandboxSpawnRunner {
             context.getInputMapping(PathFragment.EMPTY_FRAGMENT),
             execRoot,
             execRoot,
-            ImmutableList.of(),
+            packageRoots,
             null);
     SandboxOutputs outputs = helpers.getOutputs(spawn);
 

src/main/java/com/google/devtools/build/lib/sandbox/DockerSandboxedSpawnRunner.java
@@ -45,6 +45,7 @@ import com.google.devtools.build.lib.util.OS;
 import com.google.devtools.build.lib.util.ProcessUtils;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.PathFragment;
+import com.google.devtools.build.lib.vfs.Root;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -142,6 +143,7 @@ final class DockerSandboxedSpawnRunner extends AbstractSandboxSpawnRunner {
 
   private final SandboxHelpers helpers;
   private final Path execRoot;
+  private final ImmutableList<Root> packageRoots;
   private final boolean allowNetwork;
   private final Path dockerClient;
   private final ProcessWrapper processWrapper;
@@ -179,6 +181,7 @@ final class DockerSandboxedSpawnRunner extends AbstractSandboxSpawnRunner {
     super(cmdEnv);
     this.helpers = helpers;
     this.execRoot = cmdEnv.getExecRoot();
+    this.packageRoots = cmdEnv.getPackageLocator().getPathEntries();
     this.allowNetwork = helpers.shouldAllowNetwork(cmdEnv.getOptions());
     this.dockerClient = dockerClient;
     this.processWrapper = ProcessWrapper.fromCommandEnvironment(cmdEnv);
@@ -225,7 +228,7 @@ final class DockerSandboxedSpawnRunner extends AbstractSandboxSpawnRunner {
             context.getInputMapping(PathFragment.EMPTY_FRAGMENT),
             execRoot,
             execRoot,
-            ImmutableList.of(),
+            packageRoots,
             null);
     SandboxOutputs outputs = helpers.getOutputs(spawn);
 

src/main/java/com/google/devtools/build/lib/sandbox/ProcessWrapperSandboxedSpawnRunner.java
@@ -28,6 +28,7 @@ import com.google.devtools.build.lib.sandbox.SandboxHelpers.SandboxOutputs;
 import com.google.devtools.build.lib.util.OS;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.PathFragment;
+import com.google.devtools.build.lib.vfs.Root;
 import java.io.IOException;
 import java.time.Duration;
 import javax.annotation.Nullable;
@@ -42,6 +43,7 @@ final class ProcessWrapperSandboxedSpawnRunner extends AbstractSandboxSpawnRunne
   private final SandboxHelpers helpers;
   private final ProcessWrapper processWrapper;
   private final Path execRoot;
+  private final ImmutableList<Root> packageRoots;
   private final Path sandboxBase;
   private final LocalEnvProvider localEnvProvider;
   @Nullable private final SandboxfsProcess sandboxfsProcess;
@@ -69,6 +71,7 @@ final class ProcessWrapperSandboxedSpawnRunner extends AbstractSandboxSpawnRunne
     this.helpers = helpers;
     this.processWrapper = ProcessWrapper.fromCommandEnvironment(cmdEnv);
     this.execRoot = cmdEnv.getExecRoot();
+    this.packageRoots = cmdEnv.getPackageLocator().getPathEntries();
     this.localEnvProvider = LocalEnvProvider.forCurrentOs(cmdEnv.getClientEnv());
     this.sandboxBase = sandboxBase;
     this.sandboxfsProcess = sandboxfsProcess;
@@ -115,7 +118,7 @@ final class ProcessWrapperSandboxedSpawnRunner extends AbstractSandboxSpawnRunne
             context.getInputMapping(PathFragment.EMPTY_FRAGMENT),
             execRoot,
             execRoot,
-            ImmutableList.of(),
+            packageRoots,
             null);
     SandboxOutputs outputs = helpers.getOutputs(spawn);
 

src/main/java/com/google/devtools/build/lib/sandbox/WindowsSandboxedSpawnRunner.java
@@ -27,6 +27,7 @@ import com.google.devtools.build.lib.runtime.CommandEnvironment;
 import com.google.devtools.build.lib.sandbox.SandboxHelpers.SandboxInputs;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.PathFragment;
+import com.google.devtools.build.lib.vfs.Root;
 import java.io.IOException;
 import java.time.Duration;
 
@@ -35,6 +36,7 @@ final class WindowsSandboxedSpawnRunner extends AbstractSandboxSpawnRunner {
 
   private final SandboxHelpers helpers;
   private final Path execRoot;
+  private final ImmutableList<Root> packageRoots;
   private final PathFragment windowsSandbox;
   private final LocalEnvProvider localEnvProvider;
   private final Duration timeoutKillDelay;
@@ -55,6 +57,7 @@ final class WindowsSandboxedSpawnRunner extends AbstractSandboxSpawnRunner {
     super(cmdEnv);
     this.helpers = helpers;
     this.execRoot = cmdEnv.getExecRoot();
+    this.packageRoots = cmdEnv.getPackageLocator().getPathEntries();
     this.windowsSandbox = windowsSandboxPath;
     this.timeoutKillDelay = timeoutKillDelay;
     this.localEnvProvider = new WindowsLocalEnvProvider(cmdEnv.getClientEnv());
@@ -75,7 +78,7 @@ final class WindowsSandboxedSpawnRunner extends AbstractSandboxSpawnRunner {
             context.getInputMapping(PathFragment.EMPTY_FRAGMENT),
             execRoot,
             execRoot,
-            ImmutableList.of(),
+            packageRoots,
             null);
 
     ImmutableSet.Builder<Path> writablePaths = ImmutableSet.builder();

src/main/java/com/google/devtools/build/lib/worker/WorkerModule.java
@@ -176,6 +176,7 @@ public class WorkerModule extends BlazeModule {
         new WorkerSpawnRunner(
             new SandboxHelpers(),
             env.getExecRoot(),
+            env.getPackageLocator().getPathEntries(),
             workerPool,
             env.getReporter(),
             localEnvProvider,

src/main/java/com/google/devtools/build/lib/worker/WorkerSpawnRunner.java
@@ -61,6 +61,7 @@ import com.google.devtools.build.lib.util.io.FileOutErr;
 import com.google.devtools.build.lib.vfs.FileSystemUtils;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.PathFragment;
+import com.google.devtools.build.lib.vfs.Root;
 import com.google.devtools.build.lib.vfs.XattrProvider;
 import com.google.devtools.build.lib.worker.WorkerProtocol.WorkRequest;
 import com.google.devtools.build.lib.worker.WorkerProtocol.WorkResponse;
@@ -90,6 +91,7 @@ final class WorkerSpawnRunner implements SpawnRunner {
 
   private final SandboxHelpers helpers;
   private final Path execRoot;
+  private final ImmutableList<Root> packageRoots;
   private final WorkerPool workers;
   private final ExtendedEventHandler reporter;
   private final BinTools binTools;
@@ -104,6 +106,7 @@ final class WorkerSpawnRunner implements SpawnRunner {
   public WorkerSpawnRunner(
       SandboxHelpers helpers,
       Path execRoot,
+      ImmutableList<Root> packageRoots,
       WorkerPool workers,
       ExtendedEventHandler reporter,
       LocalEnvProvider localEnvProvider,
@@ -116,6 +119,7 @@ final class WorkerSpawnRunner implements SpawnRunner {
       Clock clock) {
     this.helpers = helpers;
     this.execRoot = execRoot;
+    this.packageRoots = packageRoots;
     this.workers = checkNotNull(workers);
     this.reporter = reporter;
     this.binTools = binTools;
@@ -191,7 +195,7 @@ final class WorkerSpawnRunner implements SpawnRunner {
                 context.getInputMapping(PathFragment.EMPTY_FRAGMENT),
                 execRoot,
                 execRoot,
-                ImmutableList.of(),
+                packageRoots,
                 null);
       }
       SandboxOutputs outputs = helpers.getOutputs(spawn);

src/test/java/com/google/devtools/build/lib/worker/WorkerSpawnRunnerTest.java
@@ -146,6 +146,7 @@ public class WorkerSpawnRunnerTest {
         new WorkerSpawnRunner(
             new SandboxHelpers(),
             fs.getPath("/execRoot"),
+            ImmutableList.of(),
             createWorkerPool(),
             reporter,
             localEnvProvider,
@@ -198,6 +199,7 @@ public class WorkerSpawnRunnerTest {
         new WorkerSpawnRunner(
             new SandboxHelpers(),
             execRoot,
+            ImmutableList.of(),
             createWorkerPool(),
             reporter,
             localEnvProvider,
@@ -258,12 +260,13 @@ public class WorkerSpawnRunnerTest {
         new WorkerSpawnRunner(
             new SandboxHelpers(),
             fs.getPath("/execRoot"),
+            ImmutableList.of(),
             createWorkerPool(),
             reporter,
             localEnvProvider,
             /* binTools= */ null,
             resourceManager,
-            /* runfilesTreeUpdater=*/ null,
+            /* runfilesTreeUpdater= */ null,
             options,
             metricsCollector,
             SyscallCache.NO_CACHE,
@@ -309,12 +312,13 @@ public class WorkerSpawnRunnerTest {
         new WorkerSpawnRunner(
             new SandboxHelpers(),
             fs.getPath("/execRoot"),
+            ImmutableList.of(),
             createWorkerPool(),
             reporter,
             localEnvProvider,
             /* binTools= */ null,
             resourceManager,
-            /* runfilesTreeUpdater=*/ null,
+            /* runfilesTreeUpdater= */ null,
             workerOptions,
             metricsCollector,
             SyscallCache.NO_CACHE,
@@ -375,12 +379,13 @@ public class WorkerSpawnRunnerTest {
         new WorkerSpawnRunner(
             new SandboxHelpers(),
             fs.getPath("/execRoot"),
+            ImmutableList.of(),
             createWorkerPool(),
             reporter,
             localEnvProvider,
             /* binTools= */ null,
             resourceManager,
-            /* runfilesTreeUpdater=*/ null,
+            /* runfilesTreeUpdater= */ null,
             workerOptions,
             metricsCollector,
             SyscallCache.NO_CACHE,
@@ -427,6 +432,7 @@ public class WorkerSpawnRunnerTest {
         new WorkerSpawnRunner(
             new SandboxHelpers(),
             fs.getPath("/execRoot"),
+            ImmutableList.of(),
             createWorkerPool(),
             reporter,
             localEnvProvider,
@@ -476,6 +482,7 @@ public class WorkerSpawnRunnerTest {
         new WorkerSpawnRunner(
             new SandboxHelpers(),
             fs.getPath("/execRoot"),
+            ImmutableList.of(),
             createWorkerPool(),
             reporter,
             localEnvProvider,