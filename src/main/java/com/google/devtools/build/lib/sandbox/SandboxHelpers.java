bazelbuild/bazel

commit a556969326107423738fbf8a80bad9aaf9935578

Replace Path with RootedPath in SandboxHelpers and maintain a set of roots in SandboxHelpers.
This will make it possible to replace the root of inputs easily.

Work towards #3236.

RELNOTES: None.
PiperOrigin-RevId: 492432489
Change-Id: I896cd02001338779a1a9a7073131a86ea189ab51

src/main/java/com/google/devtools/build/lib/sandbox/SandboxHelpers.java
@@ -38,6 +38,8 @@ import com.google.devtools.build.lib.vfs.FileSystemUtils;
 import com.google.devtools.build.lib.vfs.FileSystemUtils.MoveResult;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.PathFragment;
+import com.google.devtools.build.lib.vfs.Root;
+import com.google.devtools.build.lib.vfs.RootedPath;
 import com.google.devtools.build.lib.vfs.Symlinks;
 import com.google.devtools.common.options.OptionsParsingResult;
 import java.io.IOException;
@@ -236,9 +238,9 @@ public final class SandboxHelpers {
    */
   static Optional<PathFragment> getExpectedSymlinkDestination(
       PathFragment fragment, SandboxInputs inputs) {
-    Path file = inputs.getFiles().get(fragment);
+    RootedPath file = inputs.getFiles().get(fragment);
     if (file != null) {
-      return Optional.of(file.asFragment());
+      return Optional.of(file.asPath().asFragment());
     }
     return Optional.ofNullable(inputs.getSymlinks().get(fragment));
   }
@@ -347,27 +349,30 @@ public final class SandboxHelpers {
     private static final AtomicInteger tempFileUniquifierForVirtualInputWrites =
         new AtomicInteger();
 
-    private final Map<PathFragment, Path> files;
+    private final Map<PathFragment, RootedPath> files;
     private final Map<VirtualActionInput, byte[]> virtualInputs;
     private final Map<PathFragment, PathFragment> symlinks;
+    private final Map<Root, Path> sourceRootBindMounts;
 
     private static final SandboxInputs EMPTY_INPUTS =
-        new SandboxInputs(ImmutableMap.of(), ImmutableMap.of(), ImmutableMap.of());
+        new SandboxInputs(
+            ImmutableMap.of(), ImmutableMap.of(), ImmutableMap.of(), ImmutableMap.of());
 
     public SandboxInputs(
-        Map<PathFragment, Path> files,
+        Map<PathFragment, RootedPath> files,
         Map<VirtualActionInput, byte[]> virtualInputs,
-        Map<PathFragment, PathFragment> symlinks) {
+        Map<PathFragment, PathFragment> symlinks,
+        Map<Root, Path> sourceRootBindMounts) {
       this.files = files;
       this.virtualInputs = virtualInputs;
       this.symlinks = symlinks;
+      this.sourceRootBindMounts = sourceRootBindMounts;
     }
-
     public static SandboxInputs getEmptyInputs() {
       return EMPTY_INPUTS;
     }
 
-    public Map<PathFragment, Path> getFiles() {
+    public Map<PathFragment, RootedPath> getFiles() {
       return files;
     }
 
@@ -420,10 +425,16 @@ public final class SandboxHelpers {
      * included.
      */
     public SandboxInputs limitedCopy(Set<PathFragment> allowed) {
+      Map<PathFragment, RootedPath> limitedFiles = Maps.filterKeys(files, allowed::contains);
+      Map<PathFragment, PathFragment> limitedSymlinks =
+          Maps.filterKeys(symlinks, allowed::contains);
+      Set<Root> usedRoots =
+          new HashSet<>(Maps.transformValues(limitedFiles, RootedPath::getRoot).values());
+      Map<Root, Path> limitedSourceRoots =
+          Maps.filterKeys(sourceRootBindMounts, usedRoots::contains);
+
       return new SandboxInputs(
-          Maps.filterKeys(files, allowed::contains),
-          ImmutableMap.of(),
-          Maps.filterKeys(symlinks, allowed::contains));
+          limitedFiles, ImmutableMap.of(), limitedSymlinks, limitedSourceRoots);
     }
 
     @Override
@@ -456,20 +467,30 @@ public final class SandboxHelpers {
    *
    * @throws IOException if processing symlinks fails
    */
-  public SandboxInputs processInputFiles(Map<PathFragment, ActionInput> inputMap, Path execRoot)
+  public SandboxInputs processInputFiles(
+      Map<PathFragment, ActionInput> inputMap,
+      Path execRootPath,
+      Path withinSandboxExecRootPath,
+      Path sandboxSourceRoots)
       throws IOException {
-    Map<PathFragment, Path> inputFiles = new TreeMap<>();
+    Root withinSandboxExecRoot = Root.fromPath(withinSandboxExecRootPath);
+    Root execRoot =
+        withinSandboxExecRootPath.equals(execRootPath)
+            ? withinSandboxExecRoot
+            : Root.fromPath(execRootPath);
+
+    Map<PathFragment, RootedPath> inputFiles = new TreeMap<>();
     Map<PathFragment, PathFragment> inputSymlinks = new TreeMap<>();
     Map<VirtualActionInput, byte[]> virtualInputs = new HashMap<>();
+    Map<Root, Root> sourceRootToSandboxSourceRoot = new TreeMap<>();
 
     for (Map.Entry<PathFragment, ActionInput> e : inputMap.entrySet()) {
       PathFragment pathFragment = e.getKey();
       ActionInput actionInput = e.getValue();
-
       if (actionInput instanceof VirtualActionInput) {
         byte[] digest =
             SandboxInputs.materializeVirtualInput(
-                (VirtualActionInput) actionInput, execRoot, /* isExecRootSandboxed=*/ true);
+                (VirtualActionInput) actionInput, execRootPath, /* isExecRootSandboxed= */ true);
         virtualInputs.put((VirtualActionInput) actionInput, digest);
       }
 
@@ -477,14 +498,24 @@ public final class SandboxHelpers {
         Path inputPath = execRoot.getRelative(actionInput.getExecPath());
         inputSymlinks.put(pathFragment, inputPath.readSymbolicLink());
       } else {
-        Path inputPath =
-            actionInput instanceof EmptyActionInput
-                ? null
-                : execRoot.getRelative(actionInput.getExecPath());
+        RootedPath inputPath;
+
+        if (actionInput instanceof EmptyActionInput) {
+          inputPath = null;
+        } else {
+          inputPath = RootedPath.toRootedPath(execRoot, actionInput.getExecPath());
+        }
+
         inputFiles.put(pathFragment, inputPath);
       }
     }
-    return new SandboxInputs(inputFiles, virtualInputs, inputSymlinks);
+
+    Map<Root, Path> sandboxRootToSourceRoot = new TreeMap<>();
+    for (Map.Entry<Root, Root> entry : sourceRootToSandboxSourceRoot.entrySet()) {
+      sandboxRootToSourceRoot.put(entry.getValue(), entry.getKey().asPath());
+    }
+
+    return new SandboxInputs(inputFiles, virtualInputs, inputSymlinks, sandboxRootToSourceRoot);
   }
 
   /** The file and directory outputs of a sandboxed spawn. */

src/main/java/com/google/devtools/build/lib/sandbox/AbstractContainerizingSandboxedSpawn.java
@@ -25,6 +25,7 @@ import com.google.devtools.build.lib.sandbox.SandboxHelpers.SandboxOutputs;
 import com.google.devtools.build.lib.vfs.FileSystemUtils;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.PathFragment;
+import com.google.devtools.build.lib.vfs.RootedPath;
 import java.io.IOException;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -133,9 +134,9 @@ public abstract class AbstractContainerizingSandboxedSpawn implements SandboxedS
     for (PathFragment fragment : inputsToCreate) {
       Path key = sandboxExecRoot.getRelative(fragment);
       if (inputs.getFiles().containsKey(fragment)) {
-        Path fileDest = inputs.getFiles().get(fragment);
+        RootedPath fileDest = inputs.getFiles().get(fragment);
         if (fileDest != null) {
-          copyFile(fileDest, key);
+          copyFile(fileDest.asPath(), key);
         } else {
           FileSystemUtils.createEmptyFile(key);
         }

src/main/java/com/google/devtools/build/lib/sandbox/DarwinSandboxedSpawnRunner.java
@@ -232,8 +232,7 @@ final class DarwinSandboxedSpawnRunner extends AbstractSandboxSpawnRunner {
 
     SandboxInputs inputs =
         helpers.processInputFiles(
-            context.getInputMapping(PathFragment.EMPTY_FRAGMENT),
-            execRoot);
+            context.getInputMapping(PathFragment.EMPTY_FRAGMENT), execRoot, execRoot, null);
     SandboxOutputs outputs = helpers.getOutputs(spawn);
 
     final Path sandboxConfigPath = sandboxPath.getRelative("sandbox.sb");

src/main/java/com/google/devtools/build/lib/sandbox/DockerSandboxedSpawnRunner.java
@@ -222,8 +222,7 @@ final class DockerSandboxedSpawnRunner extends AbstractSandboxSpawnRunner {
 
     SandboxInputs inputs =
         helpers.processInputFiles(
-            context.getInputMapping(PathFragment.EMPTY_FRAGMENT),
-            execRoot);
+            context.getInputMapping(PathFragment.EMPTY_FRAGMENT), execRoot, execRoot, null);
     SandboxOutputs outputs = helpers.getOutputs(spawn);
 
     Duration timeout = context.getTimeout();

src/main/java/com/google/devtools/build/lib/sandbox/LinuxSandboxedSpawnRunner.java
@@ -218,7 +218,8 @@ final class LinuxSandboxedSpawnRunner extends AbstractSandboxSpawnRunner {
     ImmutableSet<Path> writableDirs = getWritableDirs(sandboxExecRoot, environment);
 
     SandboxInputs inputs =
-        helpers.processInputFiles(context.getInputMapping(PathFragment.EMPTY_FRAGMENT), execRoot);
+        helpers.processInputFiles(
+            context.getInputMapping(PathFragment.EMPTY_FRAGMENT), execRoot, execRoot, null);
     SandboxOutputs outputs = helpers.getOutputs(spawn);
 
     Duration timeout = context.getTimeout();

src/main/java/com/google/devtools/build/lib/sandbox/ProcessWrapperSandboxedSpawnRunner.java
@@ -111,8 +111,7 @@ final class ProcessWrapperSandboxedSpawnRunner extends AbstractSandboxSpawnRunne
 
     SandboxInputs inputs =
         helpers.processInputFiles(
-            context.getInputMapping(PathFragment.EMPTY_FRAGMENT),
-            execRoot);
+            context.getInputMapping(PathFragment.EMPTY_FRAGMENT), execRoot, execRoot, null);
     SandboxOutputs outputs = helpers.getOutputs(spawn);
 
     if (sandboxfsProcess != null) {

src/main/java/com/google/devtools/build/lib/sandbox/SandboxfsSandboxedSpawn.java
@@ -27,6 +27,7 @@ import com.google.devtools.build.lib.sandbox.SandboxHelpers.SandboxOutputs;
 import com.google.devtools.build.lib.vfs.FileSystemUtils;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.PathFragment;
+import com.google.devtools.build.lib.vfs.RootedPath;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -320,7 +321,7 @@ class SandboxfsSandboxedSpawn implements SandboxedSpawn {
     // created once we encounter the first symlink in the list of inputs.
     Map<PathFragment, PathFragment> symlinks = null;
 
-    for (Map.Entry<PathFragment, Path> entry : inputs.getFiles().entrySet()) {
+    for (Map.Entry<PathFragment, RootedPath> entry : inputs.getFiles().entrySet()) {
       if (entry.getValue() == null) {
         if (emptyFile == null) {
           Path emptyFilePath = scratchDir.getRelative("empty");
@@ -328,11 +329,11 @@ class SandboxfsSandboxedSpawn implements SandboxedSpawn {
           emptyFile = emptyFilePath.asFragment();
         }
       } else {
-        if (sandboxfsMapSymlinkTargets && entry.getValue().isSymbolicLink()) {
+        if (sandboxfsMapSymlinkTargets && entry.getValue().asPath().isSymbolicLink()) {
           if (symlinks == null) {
             symlinks = new HashMap<>();
           }
-          computeSymlinkMappings(entry.getKey(), entry.getValue(), symlinks);
+          computeSymlinkMappings(entry.getKey(), entry.getValue().asPath(), symlinks);
         }
       }
     }
@@ -348,13 +349,13 @@ class SandboxfsSandboxedSpawn implements SandboxedSpawn {
         (mapper) -> {
           mapper.map(rootFragment, scratchDir.asFragment(), true);
 
-          for (Map.Entry<PathFragment, Path> entry : inputs.getFiles().entrySet()) {
+          for (Map.Entry<PathFragment, RootedPath> entry : inputs.getFiles().entrySet()) {
             PathFragment target;
             if (entry.getValue() == null) {
               checkNotNull(finalEmptyFile, "Must have been initialized above by matching logic");
               target = finalEmptyFile;
             } else {
-              target = entry.getValue().asFragment();
+              target = entry.getValue().asPath().asFragment();
             }
             mapper.map(rootFragment.getRelative(entry.getKey()), target, false);
           }

src/main/java/com/google/devtools/build/lib/sandbox/WindowsSandboxUtil.java
@@ -24,6 +24,7 @@ import com.google.devtools.build.lib.shell.SubprocessBuilder;
 import com.google.devtools.build.lib.shell.SubprocessBuilder.StreamAction;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.PathFragment;
+import com.google.devtools.build.lib.vfs.RootedPath;
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -106,7 +107,7 @@ public final class WindowsSandboxUtil {
     private Path stdoutPath;
     private Path stderrPath;
     private Set<Path> writableFilesAndDirectories = ImmutableSet.of();
-    private Map<PathFragment, Path> readableFilesAndDirectories = new TreeMap<>();
+    private Map<PathFragment, RootedPath> readableFilesAndDirectories = new TreeMap<>();
     private Set<Path> inaccessiblePaths = ImmutableSet.of();
     private boolean useDebugMode = false;
     private List<String> commandArguments = ImmutableList.of();
@@ -165,7 +166,7 @@ public final class WindowsSandboxUtil {
     /** Sets the files or directories to make readable for the sandboxed process, if any. */
     @CanIgnoreReturnValue
     public CommandLineBuilder setReadableFilesAndDirectories(
-        Map<PathFragment, Path> readableFilesAndDirectories) {
+        Map<PathFragment, RootedPath> readableFilesAndDirectories) {
       this.readableFilesAndDirectories = readableFilesAndDirectories;
       return this;
     }
@@ -212,8 +213,8 @@ public final class WindowsSandboxUtil {
       for (Path writablePath : writableFilesAndDirectories) {
         commandLineBuilder.add("-w", writablePath.getPathString());
       }
-      for (Path readablePath : readableFilesAndDirectories.values()) {
-        commandLineBuilder.add("-r", readablePath.getPathString());
+      for (RootedPath readablePath : readableFilesAndDirectories.values()) {
+        commandLineBuilder.add("-r", readablePath.asPath().getPathString());
       }
       for (Path writablePath : inaccessiblePaths) {
         commandLineBuilder.add("-b", writablePath.getPathString());

commit 58d89169dc9f7bcd2867f1bfe26b570e517efd87

Make the sandbox handle generated files in Filesets.
This was broken by https://github.com/bazelbuild/bazel/commit/a556969326107423738fbf8a80bad9aaf9935578.

RELNOTES: None.
PiperOrigin-RevId: 499193037
Change-Id: Ic8fe504d9d34340b5c37724fe4c994647566eb81

src/main/java/com/google/devtools/build/lib/sandbox/SandboxHelpers.java
@@ -471,9 +471,9 @@ public final class SandboxHelpers {
    * Returns the appropriate {@link RootedPath} for a Fileset symlink.
    *
    * <p>Filesets are weird because sometimes exec paths of the {@link ActionInput}s in them are not
-   * relative, as exec paths should be, but absolute and point to under one of the package roots. In
-   * order to handle this, if we find such an absolute exec path, we iterate over the package path
-   * entries to turn it into a {@link RootedPath}.
+   * relative, as exec paths should be, but absolute and point to under one of the package roots or
+   * the execroot. In order to handle this, if we find such an absolute exec path, we iterate over
+   * possible base directories.
    *
    * <p>The inputs to this function should be symlinks that are contained within Filesets; in
    * particular, this is different from "unresolved symlinks" in that Fileset contents are regular
@@ -481,11 +481,18 @@ public final class SandboxHelpers {
    * symlinks are symlinks for which the important content is the result of {@code readlink()}
    */
   private static RootedPath processFilesetSymlink(
-      PathFragment symlink, ImmutableList<Root> packageRoots) {
+      PathFragment symlink,
+      Root execRootWithinSandbox,
+      PathFragment execRootFragment,
+      ImmutableList<Root> packageRoots) {
     for (Root packageRoot : packageRoots) {
       if (packageRoot.contains(symlink)) {
         return RootedPath.toRootedPath(packageRoot, packageRoot.relativize(symlink));
       }
+    }
+
+    if (symlink.startsWith(execRootFragment)) {
+      return RootedPath.toRootedPath(execRootWithinSandbox, symlink.relativeTo(execRootFragment));
     }
 
     throw new IllegalStateException(
@@ -565,7 +572,9 @@ public final class SandboxHelpers {
           if (execPath.isAbsolute()) {
             // This happens for ActionInputs that are part of Filesets (see the Javadoc on
             // processFilesetSymlink())
-            inputPath = processFilesetSymlink(actionInput.getExecPath(), packageRoots);
+            inputPath =
+                processFilesetSymlink(
+                    actionInput.getExecPath(), execRoot, execRootPath.asFragment(), packageRoots);
           } else {
             inputPath = RootedPath.toRootedPath(execRoot, actionInput.getExecPath());
           }