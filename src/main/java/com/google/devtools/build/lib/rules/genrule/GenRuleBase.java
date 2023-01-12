bazelbuild/bazel

commit 6baebe710147d1d1e0003cfe7490c259b7a15c38

Factor out source collection in genrule.
PiperOrigin-RevId: 407285118

src/main/java/com/google/devtools/build/lib/rules/genrule/GenRuleBase.java
@@ -14,6 +14,8 @@
 
 package com.google.devtools.build.lib.rules.genrule;
 
+import static com.google.common.collect.ImmutableMap.toImmutableMap;
+
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
@@ -68,6 +70,23 @@ public abstract class GenRuleBase implements RuleConfiguredTargetFactory {
    */
   protected abstract boolean isStampingEnabled(RuleContext ruleContext);
 
+  private ImmutableMap<Label, NestedSet<Artifact>> collectSources(
+      List<? extends TransitiveInfoCollection> srcs) {
+    ImmutableMap.Builder<Label, NestedSet<Artifact>> labelMap = ImmutableMap.builder();
+
+    for (TransitiveInfoCollection dep : srcs) {
+      // This target provides specific types of files for genrules.
+      GenRuleSourcesProvider provider = dep.getProvider(GenRuleSourcesProvider.class);
+      NestedSet<Artifact> files =
+          (provider != null)
+              ? provider.getGenruleFiles()
+              : dep.getProvider(FileProvider.class).getFilesToBuild();
+      labelMap.put(AliasProvider.getDependencyLabel(dep), files);
+    }
+
+    return labelMap.build();
+  }
+
   enum CommandType {
     BASH,
     WINDOWS_BATCH,
@@ -104,7 +123,6 @@ public abstract class GenRuleBase implements RuleConfiguredTargetFactory {
       throws InterruptedException, RuleErrorException, ActionConflictException {
     NestedSet<Artifact> filesToBuild =
         NestedSetBuilder.wrap(Order.STABLE_ORDER, ruleContext.getOutputArtifacts());
-    NestedSetBuilder<Artifact> resolvedSrcsBuilder = NestedSetBuilder.stableOrder();
 
     if (filesToBuild.isEmpty()) {
       ruleContext.attributeError("outs", "Genrules without outputs don't make sense");
@@ -121,22 +139,18 @@ public abstract class GenRuleBase implements RuleConfiguredTargetFactory {
 
     Pair<CommandType, String> cmdTypeAndAttr = determineCommandTypeAndAttribute(ruleContext);
 
-    ImmutableMap.Builder<Label, Iterable<Artifact>> labelMap = ImmutableMap.builder();
-    for (TransitiveInfoCollection dep : ruleContext.getPrerequisites("srcs")) {
-      // This target provides specific types of files for genrules.
-      GenRuleSourcesProvider provider = dep.getProvider(GenRuleSourcesProvider.class);
-      NestedSet<Artifact> files = (provider != null)
-          ? provider.getGenruleFiles()
-          : dep.getProvider(FileProvider.class).getFilesToBuild();
-      resolvedSrcsBuilder.addTransitive(files);
-      // The CommandHelper class makes an explicit copy of this in the constructor, so flattening
-      // here should be benign.
-      labelMap.put(AliasProvider.getDependencyLabel(dep), files.toList());
-    }
-    NestedSet<Artifact> resolvedSrcs = resolvedSrcsBuilder.build();
+    ImmutableMap<Label, NestedSet<Artifact>> labelMap =
+        collectSources(ruleContext.getPrerequisites("srcs"));
+    NestedSet<Artifact> resolvedSrcs = NestedSetBuilder.fromNestedSets(labelMap.values()).build();
 
+    // The CommandHelper class makes an explicit copy of this in the constructor, so flattening
+    // here should be benign.
     CommandHelper commandHelper =
-        commandHelperBuilder(ruleContext).addLabelMap(labelMap.build()).build();
+        commandHelperBuilder(ruleContext)
+            .addLabelMap(
+                labelMap.entrySet().stream()
+                    .collect(toImmutableMap(Map.Entry::getKey, e -> e.getValue().toList())))
+            .build();
 
     if (ruleContext.hasErrors()) {
       return null;

commit d7fbadbe23db9d62e08da280fba75b9927aac776

bazel: Fix a `genrule()` crash when `srcs` have different depset order
This crash was introduced by https://github.com/bazelbuild/bazel/commit/6baebe710147d1d1e0003cfe7490c259b7a15c38 which switched the code away
from explicit `NestedSetBuilder.stableOrder()`.

Unfortunately, the replacement `NestedSetBuilder.fromNestedSets()` call
always uses the order of the first `NestedSet` as the order of the
merged set.

This means that a `genrule()` whose `srcs` have different depset order
will crash with an error about incompatible ordering.

To fix it, let's switch back to `.stableOrder()`, which allows multiple
depsets with different order to be combined.

PiperOrigin-RevId: 501424644
Change-Id: I4ea322993215d42850f05d389bda20c1b0f0feb4

src/main/java/com/google/devtools/build/lib/rules/genrule/GenRuleBase.java
@@ -140,7 +140,9 @@ public abstract class GenRuleBase implements RuleConfiguredTargetFactory {
 
     ImmutableMap<Label, NestedSet<Artifact>> labelMap =
         collectSources(ruleContext.getPrerequisites("srcs"));
-    NestedSet<Artifact> resolvedSrcs = NestedSetBuilder.fromNestedSets(labelMap.values()).build();
+    NestedSetBuilder<Artifact> resolvedSrcsBuilder = NestedSetBuilder.stableOrder();
+    labelMap.values().forEach(resolvedSrcsBuilder::addTransitive);
+    NestedSet<Artifact> resolvedSrcs = resolvedSrcsBuilder.build();
 
     // The CommandHelper class makes an explicit copy of this in the constructor, so flattening
     // here should be benign.