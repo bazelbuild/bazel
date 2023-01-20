94754f11fa433f991917af5dfc44c1f769074d1f
Yun Peng <pcloudy@google.com>
2023-01-20 04:19:01 +0000

    Increase the install base size limit

--- src/test/shell/integration/minimal_jdk_test.sh
+++ src/test/shell/integration/minimal_jdk_test.sh
@@ -42,13 +42,13 @@ export BAZEL_SUFFIX="_jdk_minimal"
 source "$(rlocation "io_bazel/src/test/shell/integration_test_setup.sh")" \
   || { echo "integration_test_setup.sh not found!" >&2; exit 1; }
 
-# Bazel's install base is < 330MB with minimal JDK and > 330MB with an all
+# Bazel's install base is < 337MB with minimal JDK and > 337MB with an all
 # modules JDK.
-function test_size_less_than_330MB() {
+function test_size_less_than_337MB() {
   bazel info
   ib=$(bazel info install_base)
   size=$(du -s "$ib" | cut -d\	 -f1)
-  maxsize=$((1024*330))
+  maxsize=$((1024*337))
   if [ $size -gt $maxsize ]; then
     echo "$ib was too big:" 1>&2
     du -a "$ib" 1>&2
