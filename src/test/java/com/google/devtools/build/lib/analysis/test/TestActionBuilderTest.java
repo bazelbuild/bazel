ddd04cbf087b481aa6ee2740f09f91eb3a7561cc
Austin Schuh <austin.linux@gmail.com>
2023-02-01 17:47:26 -0800

    Support running tests on the target platform
    This adds an --use_target_platform_for_tests option that changes tests
    to use the execution properties from the target platform rather than
    the host platform. I believe that the code is currently incorrect -
    if the host and target platforms differ, then tests are cross-compiled
    for the target platform, but use the execution properties for the host
    platform.
    
    This matters for remote execution, where host and target platform may
    be different CPU architectures and operating systems (e.g., compiling
    on x64 Linux and running on ARM64 Mac). Currently, such tests will
    typically fail to run if they contain architecture or OS-specific code.
    
    Based on work by Ulf Adams <ulf@engflow.com> and updated by
    tristan.maat@codethink.co.uk
    
    Progress on #10799.
    
    RELNOTES: Add --use_target_platform_for_tests which uses the target platform for executing tests instead of the execution platform.

--- src/test/java/com/google/devtools/build/lib/analysis/test/TestActionBuilderTest.java
+++ src/test/java/com/google/devtools/build/lib/analysis/test/TestActionBuilderTest.java
@@ -37,6 +37,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -471,6 +472,144 @@ public class TestActionBuilderTest extends BuildViewTestCase {
     TestRunnerAction testAction = (TestRunnerAction) getGeneratingAction(testStatusList.get(0));
     ImmutableMap<String, String> executionInfo = testAction.getExecutionInfo();
     assertThat(executionInfo).containsExactly("key", "good");
+  }
+
+  /**
+   * Overriding the exec group from within the test with --use_target_platform_for_tests.
+   *
+   * <p>This is the same test as testOverrideExecGroup with --use_target_platform_for_tests and a
+   * target platform.
+   */
+  @Test
+  public void testOverrideTestExecGroup() throws Exception {
+    useConfiguration("--use_target_platform_for_tests=true", "--platforms=//:linux_aarch64");
+    scratch.file(
+        "some_test.bzl",
+        "def _some_test_impl(ctx):",
+        "    script = ctx.actions.declare_file(ctx.attr.name + '.sh')",
+        "    ctx.actions.write(script, 'shell script goes here', is_executable = True)",
+        "    return [",
+        "        DefaultInfo(executable = script),",
+        "        testing.ExecutionInfo({}, exec_group = 'custom_group'),",
+        "    ]",
+        "",
+        "some_test = rule(",
+        "    implementation = _some_test_impl,",
+        "    exec_groups = {'custom_group': exec_group()},",
+        "    test = True,",
+        ")");
+    scratch.file(
+        "BUILD",
+        "load(':some_test.bzl', 'some_test')",
+        "platform(",
+        "    name = 'linux_aarch64',",
+        "    constraint_values = ['@platforms//os:linux', '@platforms//cpu:aarch64'],",
+        ")",
+        "some_test(",
+        "    name = 'custom_exec_group_test',",
+        "    exec_properties = {'test.key': 'bad', 'custom_group.key': 'good'},",
+        ")");
+    ImmutableList<Artifact.DerivedArtifact> testStatusList =
+        getTestStatusArtifacts("//:custom_exec_group_test");
+    TestRunnerAction testAction = (TestRunnerAction) getGeneratingAction(testStatusList.get(0));
+    ImmutableMap<String, String> executionInfo = testAction.getExecutionInfo();
+    assertThat(executionInfo).containsExactly("key", "good");
+  }
+
+  /**
+   * Adding exec_properties from the platform with --use_target_platform_for_tests.
+   */
+  @Test
+  public void testTargetTestExecGroup() throws Exception {
+    useConfiguration(
+        "--use_target_platform_for_tests=true",
+        "--platforms=//:linux_aarch64",
+        "--host_platform=//:linux_x86");
+    scratch.file(
+        "some_test.bzl",
+        "def _some_test_impl(ctx):",
+        "    script = ctx.actions.declare_file(ctx.attr.name + '.sh')",
+        "    ctx.actions.write(script, 'shell script goes here', is_executable = True)",
+        "    return [",
+        "        DefaultInfo(executable = script),",
+        "    ]",
+        "",
+        "some_test = rule(",
+        "    implementation = _some_test_impl,",
+        "    test = True,",
+        ")");
+    scratch.file(
+        "BUILD",
+        "load(':some_test.bzl', 'some_test')",
+        "platform(",
+        "    name = 'linux_x86',",
+        "    constraint_values = ['@platforms//os:linux', '@platforms//cpu:x86_64'],",
+        "    exec_properties = {'keyhost': 'bad'},",
+        ")",
+        "platform(",
+        "    name = 'linux_aarch64',",
+        "    constraint_values = ['@platforms//os:linux', '@platforms//cpu:aarch64'],",
+        "    exec_properties = {'key2': 'good'},",
+        ")",
+        "some_test(",
+        "    name = 'exec_group_test',",
+        "    exec_properties = {'key': 'bad'},",
+        ")");
+    ImmutableList<Artifact.DerivedArtifact> testStatusList =
+        getTestStatusArtifacts("//:exec_group_test");
+    TestRunnerAction testAction = (TestRunnerAction) getGeneratingAction(testStatusList.get(0));
+    assertThat(testAction.getExecutionPlatform().label().getName()).isEqualTo("linux_aarch64");
+
+    ImmutableMap<String, String> executionInfo = testAction.getExecutionInfo();
+    assertThat(executionInfo).containsExactly("key2", "good");
+  }
+
+  /**
+   * Adding test specific exec_properties with --use_target_platform_for_tests.
+   */
+  @Test @Ignore("https://github.com/bazelbuild/bazel/issues/17466")
+  public void testTargetTestExecGroupInheritance() throws Exception {
+    useConfiguration(
+        "--use_target_platform_for_tests=true",
+        "--platforms=//:linux_aarch64",
+        "--host_platform=//:linux_x86");
+    scratch.file(
+        "some_test.bzl",
+        "def _some_test_impl(ctx):",
+        "    script = ctx.actions.declare_file(ctx.attr.name + '.sh')",
+        "    ctx.actions.write(script, 'shell script goes here', is_executable = True)",
+        "    return [",
+        "        DefaultInfo(executable = script),",
+        "    ]",
+        "",
+        "some_test = rule(",
+        "    implementation = _some_test_impl,",
+        "    test = True,",
+        ")");
+    scratch.file(
+        "BUILD",
+        "load(':some_test.bzl', 'some_test')",
+        "platform(",
+        "    name = 'linux_x86',",
+        "    constraint_values = ['@platforms//os:linux', '@platforms//cpu:x86_64'],",
+        "    exec_properties = {'keyhost': 'bad'},",
+        ")",
+        "platform(",
+        "    name = 'linux_aarch64',",
+        "    constraint_values = ['@platforms//os:linux', '@platforms//cpu:aarch64'],",
+        "    exec_properties = {'key2': 'good'},",
+        ")",
+        "some_test(",
+        "    name = 'exec_group_test',",
+        "    exec_properties = {'test.key': 'good', 'key': 'bad'},",
+        ")");
+    ImmutableList<Artifact.DerivedArtifact> testStatusList =
+        getTestStatusArtifacts("//:exec_group_test");
+    TestRunnerAction testAction = (TestRunnerAction) getGeneratingAction(testStatusList.get(0));
+    assertThat(testAction.getExecutionPlatform().label().getName()).isEqualTo("linux_aarch64");
+
+    ImmutableMap<String, String> executionInfo = testAction.getExecutionInfo();
+    assertThat(executionInfo).containsExactly("key2", "good", "key", "good");
   }
 
   @Test
