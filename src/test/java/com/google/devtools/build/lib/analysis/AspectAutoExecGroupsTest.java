aa29b51bc4b63879e6c24416823029b606ca9a17
Googler <kotlaja@google.com>
2023-02-10 02:40:20 -0800

    Automatic exec groups - Add tests for a custom rule with a custom aspect
    PiperOrigin-RevId: 508608232
    Change-Id: I3d5546641bf172b583ac1b2be740993dc98fa1f3

--- src/test/java/com/google/devtools/build/lib/analysis/AspectAutoExecGroupsTest.java
+++ src/test/java/com/google/devtools/build/lib/analysis/AspectAutoExecGroupsTest.java
@@ -17,6 +17,7 @@ package com.google.devtools.build.lib.analysis;
 import static com.google.common.truth.Truth.assertThat;
 
 import com.google.common.collect.ObjectArrays;
+import com.google.devtools.build.lib.actions.Action;
 import com.google.devtools.build.lib.analysis.starlark.StarlarkExecGroupCollection;
 import com.google.devtools.build.lib.analysis.util.BuildViewTestCase;
 import com.google.devtools.build.lib.cmdline.Label;
@@ -123,6 +124,94 @@ public class AspectAutoExecGroupsTest extends BuildViewTestCase {
     };
 
     super.useConfiguration(ObjectArrays.concat(flags, args, String.class));
+  }
+
+  /**
+   * Creates custom rule which produces action with {@code actionParameters}, adds {@code
+   * extraAttributes}, defines {@code toolchains}, and adds custom exec groups from {@code
+   * execGroups}. Depending on {@code actionRunCommand} parameter, {@code actions.run} or {@code
+   * actions.run_shell} is created. This rule also defines an aspect on its {@code deps} attribute.
+   */
+  private void createCustomRule(
+      String action,
+      String actionParameters,
+      String extraAttributes,
+      String toolchains,
+      String execGroups)
+      throws Exception {
+    scratch.file(
+        "test/defs.bzl",
+        "load('//test:aspect.bzl', 'custom_aspect')",
+        "def _impl(ctx):",
+        "  output_jar = ctx.actions.declare_file(ctx.label.name + '_dummy_output.jar')",
+        "  " + action + "(",
+        actionParameters,
+        "    outputs = [output_jar],",
+        action.equals("ctx.actions.run")
+            ? (actionParameters.contains("executable =") // avoid adding executable parameter twice
+                ? ""
+                : "executable = ctx.toolchains['//rule:toolchain_type_1'].tool,")
+            : "    command = 'echo',",
+        "  )",
+        "  return [DefaultInfo(files = depset([output_jar]))]",
+        "custom_rule = rule(",
+        "  implementation = _impl,",
+        "  attrs = {",
+        "    '_tool': attr.label(default = '//toolchain:a_tool', cfg = 'exec', executable = True),",
+        "    'deps': attr.label_list(aspects = [custom_aspect]),",
+        extraAttributes,
+        "  },",
+        "  exec_groups = {",
+        execGroups,
+        "  },",
+        "  toolchains = " + toolchains + ",",
+        ")");
+    scratch.file(
+        "test/BUILD",
+        "load('//test:defs.bzl', 'custom_rule')",
+        "custom_rule(name = 'custom_rule_dep')",
+        "custom_rule(name = 'custom_rule_name', deps= ['custom_rule_dep'])");
+  }
+
+  /**
+   * Creates custom aspect which produces action with `{@code actionParameters}, adds {@code
+   * extraAttributes}, defines {@code toolchains}, and adds custom exec groups from {@code
+   * execGroups}. Depending on {@code actionRunCommand} parameter, {@code actions.run} or {@code
+   * actions.run_shell} is created.
+   */
+  private void createCustomAspect(
+      String action,
+      String actionParameters,
+      String extraAttributes,
+      String toolchains,
+      String execGroups)
+      throws Exception {
+    scratch.file(
+        "test/aspect.bzl",
+        "OutputFile = provider(fields = {'file': 'Output file', 'exec_groups': 'Exec groups'})",
+        "def _impl(target, ctx):",
+        "  output_jar = ctx.actions.declare_file(ctx.label.name + '_dummy_output_aspect.jar')",
+        "  " + action + "(",
+        actionParameters,
+        "    outputs = [output_jar],",
+        action.equals("ctx.actions.run")
+            ? "    executable = '//toolchain:foo_toolchain',"
+            : "    command = 'echo',",
+        "  )",
+        "  exec_groups = ctx.exec_groups",
+        "  return [OutputFile(file = output_jar, exec_groups = exec_groups)]",
+        "custom_aspect = aspect(",
+        "  implementation = _impl,",
+        "  attrs = {",
+        "     ",
+        extraAttributes,
+        "  },",
+        "  attr_aspects = ['deps'],",
+        "  exec_groups = {",
+        execGroups,
+        "  },",
+        "  toolchains = " + toolchains + ",",
+        ")");
   }
 
   /**
@@ -330,5 +419,86 @@ public class AspectAutoExecGroupsTest extends BuildViewTestCase {
 
     assertThat(aspectExecGroups.getToolchainCollectionForTesting().keySet())
         .containsExactly("default-exec-group", "//rule:toolchain_type_1");
+  }
+
+  @Test
+  @TestParameters({
+    "{action: ctx.actions.run}",
+    "{action: ctx.actions.run_shell}",
+  })
+  public void execGroups_customAspectOnCustomRule(String action) throws Exception {
+    String customExecGroups =
+        "    'aspect_custom_exec_group': exec_group(\n"
+            + "      exec_compatible_with = ['//platforms:constraint_1'],\n"
+            + "      toolchains = ['//rule:toolchain_type_1'],\n"
+            + "    ),\n";
+    createCustomAspect(
+        /* action= */ action,
+        /* actionParameters= */ "toolchain = '//rule:toolchain_type_2',",
+        /* extraAttributes= */ "",
+        /* toolchains= */ "['//rule:toolchain_type_2']",
+        /* execGroups= */ customExecGroups);
+    createCustomRule(
+        /* action= */ action,
+        /* actionParameters= */ "toolchain = '//rule:toolchain_type_1',",
+        /* extraAttributes= */ "",
+        /* toolchains= */ "['//rule:toolchain_type_1']",
+        /* execGroups= */ "");
+    useConfiguration("--incompatible_auto_exec_groups");
+
+    ConfiguredTarget target = getConfiguredTarget("//test:custom_rule_name");
+    ConfiguredTarget targetDep = (ConfiguredTarget) getRuleContext(target).getPrerequisite("deps");
+    ConfiguredAspect configuredAspect = getAspect("//test:aspect.bzl%custom_aspect");
+    StarlarkExecGroupCollection aspectExecGroups =
+        getExecGroupsFromAspectProvider(configuredAspect);
+
+    assertThat(getRuleContext(target).getExecGroups().execGroups().keySet())
+        .containsExactly("//rule:toolchain_type_1");
+    assertThat(getRuleContext(targetDep).getExecGroups().execGroups().keySet())
+        .containsExactly("//rule:toolchain_type_1");
+    assertThat(aspectExecGroups.getToolchainCollectionForTesting().keySet())
+        .containsExactly(
+            "//rule:toolchain_type_2", "default-exec-group", "aspect_custom_exec_group");
+  }
+
+  @Test
+  @TestParameters({
+    "{action: ctx.actions.run}",
+    "{action: ctx.actions.run_shell}",
+  })
+  public void execPlatforms_customAspectOnCustomRule(String action) throws Exception {
+    String customExecGroups =
+        "    'aspect_custom_exec_group': exec_group(\n"
+            + "      exec_compatible_with = ['//platforms:constraint_1'],\n"
+            + "      toolchains = ['//rule:toolchain_type_1'],\n"
+            + "    ),\n";
+    createCustomAspect(
+        /* action= */ action,
+        /* actionParameters= */ "toolchain = '//rule:toolchain_type_2',",
+        /* extraAttributes= */ "",
+        /* toolchains= */ "['//rule:toolchain_type_2']",
+        /* execGroups= */ customExecGroups);
+    createCustomRule(
+        /* action= */ action,
+        /* actionParameters= */ "toolchain = '//rule:toolchain_type_1',",
+        /* extraAttributes= */ "",
+        /* toolchains= */ "['//rule:toolchain_type_1']",
+        /* execGroups= */ "");
+    useConfiguration("--incompatible_auto_exec_groups");
+
+    ConfiguredTarget target = getConfiguredTarget("//test:custom_rule_name");
+    ConfiguredTarget targetDep = (ConfiguredTarget) getRuleContext(target).getPrerequisite("deps");
+    ConfiguredAspect configuredAspect = getAspect("//test:aspect.bzl%custom_aspect");
+    Action targetAction = getGeneratingAction(target, "test/custom_rule_name_dummy_output.jar");
+    Action targetDepAction =
+        getGeneratingAction(targetDep, "test/custom_rule_dep_dummy_output.jar");
+    Action aspectAction = (Action) configuredAspect.getActions().get(0);
+
+    assertThat(targetAction.getOwner().getExecutionPlatform().label())
+        .isEqualTo(Label.parseCanonical("//platforms:platform_1"));
+    assertThat(targetDepAction.getOwner().getExecutionPlatform().label())
+        .isEqualTo(Label.parseCanonical("//platforms:platform_1"));
+    assertThat(aspectAction.getOwner().getExecutionPlatform().label())
+        .isEqualTo(Label.parseCanonical("//platforms:platform_2"));
   }
 }
