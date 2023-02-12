fec9f8808bcc17d9a446c749f7b2e4c9d484b0d4
Keith Smiley <keithbsmiley@gmail.com>
2023-02-12 15:03:14 -0800

    Update rules_cc

--- src/test/shell/bazel/android/android_integration_test.sh
+++ src/test/shell/bazel/android/android_integration_test.sh
@@ -213,7 +213,7 @@ android_binary(
 EOF
   cat > MODULE.bazel << 'EOF'
 # Required for android_integration_test_with_platforms
-bazel_dep(name = "platforms", version = "0.0.5")
+bazel_dep(name = "platforms", version = "0.0.6")
 EOF
 
   bazel clean
