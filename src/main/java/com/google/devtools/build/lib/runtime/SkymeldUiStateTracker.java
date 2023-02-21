51e6c38ae808df4f2bf8fb29b4c5173e06ac014a
Googler <wyv@google.com>
2023-02-21 06:00:53 -0800

    Properly report repo fetch progress during main repo mapping computation
    This ends up as simple as firing an event and letting UiEventHandler know that the build has started.
    
    Fixes https://github.com/bazelbuild/bazel/issues/16927.
    
    Fixes https://github.com/bazelbuild/bazel/issues/16338.
    
    PiperOrigin-RevId: 511177491
    Change-Id: I479b116cf896731b7238410ce14f7e249da6c390

--- src/main/java/com/google/devtools/build/lib/runtime/SkymeldUiStateTracker.java
+++ src/main/java/com/google/devtools/build/lib/runtime/SkymeldUiStateTracker.java
@@ -35,6 +35,7 @@ final class SkymeldUiStateTracker extends UiStateTracker {
     // We explicitly define a starting status, which can be used to determine what to display in
     // cases before the build has started.
     BUILD_NOT_STARTED,
+    COMPUTING_MAIN_REPO_MAPPING,
     BUILD_STARTED,
     TARGET_PATTERN_PARSING,
     LOADING_COMPLETE,
@@ -77,6 +78,9 @@ final class SkymeldUiStateTracker extends UiStateTracker {
     switch (buildStatus) {
       case BUILD_NOT_STARTED:
         return;
+      case COMPUTING_MAIN_REPO_MAPPING:
+        writeBaseProgress("Computing main repo mapping", "", terminalWriter);
+        break;
       case BUILD_STARTED:
         writeBaseProgress("Loading", "", terminalWriter);
         break;
@@ -149,6 +153,11 @@ final class SkymeldUiStateTracker extends UiStateTracker {
         terminalWriter.newline().append("    " + progress.getSecond());
       }
     }
+  }
+
+  @Override
+  void mainRepoMappingComputationStarted() {
+    buildStatus = BuildStatus.COMPUTING_MAIN_REPO_MAPPING;
   }
 
   @Override
