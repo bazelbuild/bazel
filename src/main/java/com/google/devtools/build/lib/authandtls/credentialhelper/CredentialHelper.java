c1d4b2744a021cb0835bb6982de0e790abca3608
Googler <chiwang@google.com>
2023-02-23 09:40:26 -0800

    Correctly set default subprocess factory when loading class `SubprocessBuilder`.
    Currently, there are two subprocess factories: `JavaSubprocessFactory` and `WindowsSubprocessFactory`. The default factory is set to `JavaSubprocessFactory` when loading class `SubprocessBuidler`.
    
    We call `setDefaultSubprocessFactory` to set the default factory, when creating BlazeRuntime, by checking the OS, which set the default factory to WindowsSubprocessFactory on Windows. However, for integration tests written in Java, `setDefaultSubprocessFactory` is not called. So we use `JavaSubprocessFactory` even on Windows for these tests which doesn't correctly terminate process tree.
    
    This CL fixes that by calling `setDefaultSubprocessFactory` when loading class `SubprocessBuilder`.
    
    PiperOrigin-RevId: 511810605
    Change-Id: I55ca32caa0f81160e027780d671bf46a6bd6c266

--- src/main/java/com/google/devtools/build/lib/authandtls/credentialhelper/CredentialHelper.java
+++ src/main/java/com/google/devtools/build/lib/authandtls/credentialhelper/CredentialHelper.java
@@ -23,6 +23,7 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.io.CharStreams;
 import com.google.devtools.build.lib.profiler.Profiler;
 import com.google.devtools.build.lib.profiler.SilentCloseable;
+import com.google.devtools.build.lib.shell.JavaSubprocessFactory;
 import com.google.devtools.build.lib.shell.Subprocess;
 import com.google.devtools.build.lib.shell.SubprocessBuilder;
 import com.google.devtools.build.lib.vfs.Path;
@@ -145,7 +146,9 @@ public final class CredentialHelper {
     Preconditions.checkNotNull(environment);
     Preconditions.checkNotNull(args);
 
-    return new SubprocessBuilder()
+    // Force using JavaSubprocessFactory on Windows, because for some reasons,
+    // WindowsSubprocessFactory cannot redirect stdin to subprocess.
+    return new SubprocessBuilder(JavaSubprocessFactory.INSTANCE)
         .setArgv(ImmutableList.<String>builder().add(path.getPathString()).add(args).build())
         .setWorkingDirectory(environment.getWorkspacePath().getPathFile())
         .setEnv(environment.getClientEnvironment())
