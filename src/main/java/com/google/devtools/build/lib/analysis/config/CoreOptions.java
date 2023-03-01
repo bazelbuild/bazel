770e928c4ec82c289fdd5ab9562d1b583cb797cd
keertk <110264242+keertk@users.noreply.github.com>
2023-03-01 09:25:31 -0800

    Update exec to host

--- src/main/java/com/google/devtools/build/lib/analysis/config/CoreOptions.java
+++ src/main/java/com/google/devtools/build/lib/analysis/config/CoreOptions.java
@@ -1008,8 +1008,8 @@ public class CoreOptions extends FragmentOptions implements Cloneable {
     host.checkLicenses = checkLicenses;
 
     // === Pass on C++ compiler features.
-    exec.incompatibleUseHostFeatures = incompatibleUseHostFeatures;
-    exec.hostFeatures = ImmutableList.copyOf(hostFeatures);
+    host.incompatibleUseHostFeatures = incompatibleUseHostFeatures;
+    host.hostFeatures = ImmutableList.copyOf(hostFeatures);
     if (incompatibleUseHostFeatures) {
       host.defaultFeatures = ImmutableList.copyOf(hostFeatures);
     } else {
