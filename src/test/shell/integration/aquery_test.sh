e92e12bdb60617534b3ef4e02fe48f98d25207ae
Keerthana Kumar <keerthanakumar@google.com>
2023-03-19 16:06:30 -0700

    Allows --query_file to be used for cquery and aquery too.

--- src/test/shell/integration/aquery_test.sh
+++ src/test/shell/integration/aquery_test.sh
@@ -1665,6 +1665,25 @@ EOF
   fi
 }
 
+function test_does_not_fail_horribly_with_file() {
+  rm -rf peach
+  mkdir -p peach
+  cat > "peach/BUILD" <<'EOF'
+genrule(
+    name = "bar",
+    srcs = ["dummy.txt"],
+    outs = ["bar_out.txt"],
+    cmd = "echo unused > bar_out.txt",
+)
+EOF
+
+  echo "//peach:bar" > query_file
+  bazel aquery --query_file=query_file > $TEST_log
+
+  expect_log "Target: //peach:bar" "look in $TEST_log"
+  expect_log "ActionKey:"
+}
+
 function test_cpp_compile_action_env() {
   local pkg="${FUNCNAME[0]}"
   mkdir -p "$pkg"
