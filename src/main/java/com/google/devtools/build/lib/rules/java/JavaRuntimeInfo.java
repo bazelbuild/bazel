2d04c91327cadb3f0d53bceea117d3939a25e143
hvadehra <hvd@google.com>
2023-04-03 14:49:17 +0200

    [6.2.0] Add version to JavaRuntimeInfo (#17913)
    * Add version to JavaRuntimeInfo.
    
    1. As suggested in https://github.com/bazelbuild/bazel/issues/6354#issuecomment-1310489402, add a JDK version attribute to the java_runtime rule. This will allow changing behavior based on the Java runtime's version.
    
    2. As an application of the above, pass -Djava.security.manager=allow to Java tests on JDK 17+. This makes the Bazel Java test runner to work on JDK 19. Fixes https://github.com/bazelbuild/bazel/issues/14502.
    
    3. To make the above generally useful, the remote_java_repository and local_java_repository must set the new version attribute in the java_runtime rules that they create. This means the old static jdk.BUILD file no longer suffices. Move the contents of jdk.BUILD into a Starlark file, so the version can be interpolated in by JDK repository rules and macros. (This isn't the nicest, but local_java_repository is not a repository rule, so the template cannot be in a non-Starlark file.)
    
    Closes #17775.
    
    PiperOrigin-RevId: 518860040
    Change-Id: I8223b6407dd09528a4e5a6bf12354e5fc68278c6
    
    (cherry picked from commit 7556e1107b666d10b660470a571631463c7eb4ec)
    
    * Use string.replace() instead of string.format() to avoid issues with select{} in the template string
    
    * Remove test and fix formatting
    
    * Fix starlark_repository_test
    
    Change the grep pattern to exactly match what the test is looking for. The old pattern would match BUILD file content from the jdk_build_file.bzl template
    
    ---------
    
    Co-authored-by: Benjamin Peterson <benjamin@engflow.com>

--- src/main/java/com/google/devtools/build/lib/rules/java/JavaRuntimeInfo.java
+++ src/main/java/com/google/devtools/build/lib/rules/java/JavaRuntimeInfo.java
@@ -51,7 +51,8 @@ public final class JavaRuntimeInfo extends NativeInfo implements JavaRuntimeInfo
       PathFragment javaBinaryRunfilesPath,
       NestedSet<Artifact> hermeticInputs,
       @Nullable Artifact libModules,
-      ImmutableList<CcInfo> hermeticStaticLibs) {
+      ImmutableList<CcInfo> hermeticStaticLibs,
+      int version) {
     return new JavaRuntimeInfo(
         javaBaseInputs,
         javaHome,
@@ -60,7 +61,8 @@ public final class JavaRuntimeInfo extends NativeInfo implements JavaRuntimeInfo
         javaBinaryRunfilesPath,
         hermeticInputs,
         libModules,
-        hermeticStaticLibs);
+        hermeticStaticLibs,
+        version);
   }
 
   @Override
@@ -124,6 +126,7 @@ public final class JavaRuntimeInfo extends NativeInfo implements JavaRuntimeInfo
   private final NestedSet<Artifact> hermeticInputs;
   @Nullable private final Artifact libModules;
   private final ImmutableList<CcInfo> hermeticStaticLibs;
+  private final int version;
 
   private JavaRuntimeInfo(
       NestedSet<Artifact> javaBaseInputs,
@@ -133,7 +136,8 @@ public final class JavaRuntimeInfo extends NativeInfo implements JavaRuntimeInfo
       PathFragment javaBinaryRunfilesPath,
       NestedSet<Artifact> hermeticInputs,
       @Nullable Artifact libModules,
-      ImmutableList<CcInfo> hermeticStaticLibs) {
+      ImmutableList<CcInfo> hermeticStaticLibs,
+      int version) {
     this.javaBaseInputs = javaBaseInputs;
     this.javaHome = javaHome;
     this.javaBinaryExecPath = javaBinaryExecPath;
@@ -142,6 +146,7 @@ public final class JavaRuntimeInfo extends NativeInfo implements JavaRuntimeInfo
     this.hermeticInputs = hermeticInputs;
     this.libModules = libModules;
     this.hermeticStaticLibs = hermeticStaticLibs;
+    this.version = version;
   }
 
   /** All input artifacts in the javabase. */
@@ -221,6 +226,11 @@ public final class JavaRuntimeInfo extends NativeInfo implements JavaRuntimeInfo
   @Override
   public Depset starlarkJavaBaseInputs() {
     return Depset.of(Artifact.TYPE, javaBaseInputs());
+  }
+
+  @Override
+  public int version() {
+    return version;
   }
 
   @Override
