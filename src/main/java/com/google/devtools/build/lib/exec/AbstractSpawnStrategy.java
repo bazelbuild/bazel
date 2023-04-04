3fb7374dfe279a880993e33c68731b142ed66ab8
keertk <keerthanakumar@google.com>
2023-04-03 18:00:10 -0700

    Update AbstractSpawnStrategy.java

--- src/main/java/com/google/devtools/build/lib/exec/AbstractSpawnStrategy.java
+++ src/main/java/com/google/devtools/build/lib/exec/AbstractSpawnStrategy.java
@@ -79,12 +79,12 @@ public abstract class AbstractSpawnStrategy implements SandboxedSpawnStrategy {
 
   private final SpawnInputExpander spawnInputExpander;
   private final SpawnRunner spawnRunner;
-  private final boolean verboseFailures;
+  private final ExecutionOptions executionOptions;
 
   protected AbstractSpawnStrategy(Path execRoot, SpawnRunner spawnRunner, boolean verboseFailures) {
     this.spawnInputExpander = new SpawnInputExpander(execRoot, false);
     this.spawnRunner = spawnRunner;
-    this.verboseFailures = verboseFailures;
+    this.executionOptions = executionOptions;
   }
 
   /**
@@ -208,7 +208,8 @@ public abstract class AbstractSpawnStrategy implements SandboxedSpawnStrategy {
       String message =
           !Strings.isNullOrEmpty(resultMessage)
               ? resultMessage
-              : CommandFailureUtils.describeCommandFailure(verboseFailures, cwd, spawn);
+              : CommandFailureUtils.describeCommandFailure(
+                  executionOptions.verboseFailures, cwd, spawn);
       throw new SpawnExecException(message, spawnResult, /*forciblyRunRemotely=*/ false);
     }
     return ImmutableList.of(spawnResult);
