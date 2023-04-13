b64bc17d2cc0961a609f931136e194d64fc32d41
Googler <yuyueny@google.com>
2023-04-13 11:03:36 -0700

    This change addresses the potential issues when some Package node is removed from `InMemoryGraph`
    * When package value is missing for the removed package node;
    * When the node is still dirty;
    
    Refactor is also done in `InMemoryGraphImpl#remove(key)` method so that `ConcurrentHashMap` lookup only happens once.
    
    PiperOrigin-RevId: 524048911
    Change-Id: I497bc3fb715d093579db92c7eee68bdcea798b19

--- src/test/java/com/google/devtools/build/skyframe/InMemoryGraphTest.java
+++ src/test/java/com/google/devtools/build/skyframe/InMemoryGraphTest.java
@@ -17,6 +17,8 @@ import static com.google.common.truth.Truth.assertThat;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
+import com.google.devtools.build.lib.cmdline.PackageIdentifier;
+import com.google.devtools.build.lib.skyframe.PackageValue;
 import com.google.devtools.build.skyframe.InMemoryGraphImpl.EdgelessInMemoryGraphImpl;
 import com.google.devtools.build.skyframe.QueryableGraph.Reason;
 import org.junit.Test;
@@ -126,5 +128,27 @@ public class InMemoryGraphTest extends GraphTest {
     // cleaning up re-interns the cat instance back to the weak interner, and thus, no new instance
     // is created.
     assertThat(SkyKeyWithSkyKeyInterner.create("cat")).isSameInstanceAs(cat);
+  }
+
+  @Test
+  public void removePackageNode_notPresentInGraph() throws Exception {
+    PackageIdentifier packageIdentifier = PackageIdentifier.createUnchecked("repo", "hello");
+    PackageValue.Key packageKey = PackageValue.key(packageIdentifier);
+
+    graph.remove(packageKey);
+    assertThat(graph.get(null, Reason.OTHER, packageKey)).isNull();
+  }
+
+  @Test
+  public void removePackageNode_noValueWeakInternLabelsNoCrash() throws Exception {
+    PackageIdentifier packageIdentifier = PackageIdentifier.createUnchecked("repo", "hello");
+    PackageValue.Key packageKey = PackageValue.key(packageIdentifier);
+
+    graph.createIfAbsentBatch(null, Reason.OTHER, ImmutableList.of(packageKey));
+    NodeEntry entry = graph.get(null, Reason.OTHER, packageKey);
+    assertThat(entry.toValue()).isNull();
+
+    graph.remove(packageKey);
+    assertThat(graph.get(null, Reason.OTHER, packageKey)).isNull();
   }
 }
