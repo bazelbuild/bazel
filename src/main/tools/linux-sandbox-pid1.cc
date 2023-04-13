055f7ad12d2791bf93b4b11654b64f1f8932e3bd
Ryang20718 <ryang@waabi.ai>
2023-04-13 00:03:49 +0000

    add recursive mount bind

--- src/main/tools/linux-sandbox-pid1.cc
+++ src/main/tools/linux-sandbox-pid1.cc
@@ -441,7 +441,9 @@ static void MakeFilesystemMostlyReadOnly() {
 static void MountProc() {
   // Mount a new proc on top of the old one, because the old one still refers to
   // our parent PID namespace.
-  if (mount("/proc", "/proc", "proc", MS_NODEV | MS_NOEXEC | MS_NOSUID,
+  // Some Docker runtimes, such as nvidia, mounted proc already for gpu use 
+  // Without Recursive Bind, attempting to mount nvidia drivers would fail.
+  if (mount("/proc", "/proc", "proc", MS_REC | MS_BIND | MS_NODEV | MS_NOEXEC | MS_NOSUID,
             nullptr) < 0) {
     DIE("mount");
   }
