fe97f2090828340a0687116bcd973fc529bcca7f
Brentley Jones <github@brentleyjones.com>
2023-04-24 21:31:57 -0700

    Make `--incompatible_remote_build_event_upload_respect_no_cache` a no-op
    Also removed unneeded tests.
    
    RELNOTES: `--incompatible_remote_build_event_upload_respect_no_cache` is now a no-op.
    
    Closes #17989.
    
    PiperOrigin-RevId: 526843711
    Change-Id: I1305514cf39c29aca9208beb2569d725370b0179

--- src/main/java/com/google/devtools/build/lib/remote/RemoteModule.java
+++ src/main/java/com/google/devtools/build/lib/remote/RemoteModule.java
@@ -64,7 +64,6 @@ import com.google.devtools.build.lib.remote.common.RemoteExecutionClient;
 import com.google.devtools.build.lib.remote.downloader.GrpcRemoteDownloader;
 import com.google.devtools.build.lib.remote.http.HttpException;
 import com.google.devtools.build.lib.remote.logging.LoggingInterceptor;
-import com.google.devtools.build.lib.remote.options.RemoteBuildEventUploadMode;
 import com.google.devtools.build.lib.remote.options.RemoteOptions;
 import com.google.devtools.build.lib.remote.options.RemoteOutputsMode;
 import com.google.devtools.build.lib.remote.util.DigestUtil;
@@ -756,17 +755,7 @@ public final class RemoteModule extends BlazeModule {
 
   // Separating the conditions for readability.
   private boolean shouldParseNoCacheOutputs() {
-    if (remoteOptions == null
-        || remoteOptions.remoteBuildEventUploadMode != RemoteBuildEventUploadMode.ALL
-        || !remoteOptions.incompatibleRemoteBuildEventUploadRespectNoCache) {
-      return false;
-    }
-
-    if (actionContextProvider == null || actionContextProvider.getRemoteCache() == null) {
-      return false;
-    }
-
-    return buildEventArtifactUploaderFactoryDelegate.get() != null;
+    return false;
   }
 
   private void parseNoCacheOutputs(AnalysisResult analysisResult) {
