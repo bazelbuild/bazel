2ebd0f791db8c11ba9b2430e4ae8196b3687de23
Oscar Garzon <oscar.garzon@ciencias.unam.mx>
2023-04-25 04:29:29 -0700

    Added an example using includes attribute for cc_import
    closes #15222
    
    Closes #18024.
    
    PiperOrigin-RevId: 526926623
    Change-Id: I8255186b99220f4e833a18d25b2dc19dcdee6cdf

--- src/main/java/com/google/devtools/build/lib/bazel/rules/cpp/BazelCcImportRule.java
+++ src/main/java/com/google/devtools/build/lib/bazel/rules/cpp/BazelCcImportRule.java
@@ -154,6 +154,16 @@ cc_binary(
 )
 </pre>
 
+<code>cc_import</code> supports an include attribute. For example:
+<pre class="code">
+  cc_import(
+  name = "curl_lib",
+  hdrs = glob(["vendor/curl/include/curl/*.h"]),
+  includes = [ "vendor/curl/include" ],
+  shared_library = "vendor/curl/lib/.libs/libcurl.dylib",
+)
+</pre>
+
 
 
 </p>
