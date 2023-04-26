eadeb73cb68fb4e93e6641263030a299fb682dd5
Googler <wilwell@google.com>
2023-04-26 09:07:26 -0700

    Move process info collection is `PsInfoCollector`.
    After this CL we collect ps snapshots not oftener than every 500ms in `PsInfoCollector` and this is the only source of information about processes.
    
    This functionality will be used in next CLs in `CollectLocalResourceUsage` to collect info about memory usage from all descendant processes of Blaze.
    
    PiperOrigin-RevId: 527282736
    Change-Id: I2cfa641452e7b47916fc63b4902f23d22986e358

--- src/test/java/com/google/devtools/build/lib/util/PsInfoCollectorTest.java
+++ src/test/java/com/google/devtools/build/lib/util/PsInfoCollectorTest.java
@@ -16,9 +16,11 @@ package com.google.devtools.build.lib.util;
 import static com.google.common.truth.Truth.assertThat;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import org.junit.Test;
@@ -29,12 +31,13 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public final class PsInfoCollectorTest {
 
+  private final PsInfoCollector spyCollector = spy(PsInfoCollector.instance());
+
   @Test
-  public void testCollectStats_ignoreSpaces() throws Exception {
+  public void testCollectStats_ignoreSpaces() {
     String psOutput = "    PID  \t  PPID \t  RSS\n   2 1 3216 \t\n  \t 3 1 \t 4096 \t";
     InputStream psStream = new ByteArrayInputStream(psOutput.getBytes(UTF_8));
     Process process = mock(Process.class);
-
     when(process.getInputStream()).thenReturn(psStream);
 
     ImmutableMap<Long, PsInfoCollector.PsInfo> pidToPsInfo =
@@ -47,5 +50,47 @@ public final class PsInfoCollectorTest {
             3L,
             PsInfoCollector.PsInfo.create(3, 1, 4096));
     assertThat(pidToPsInfo).isEqualTo(expectedPidToPsInfo);
+  }
+
+  @Test
+  public void testCollectStats_multipleSubprocesses() {
+    // pstree of these processes
+    // 0-+-1---3-+-7
+    //   |       `-8
+    //   |-2-+-4
+    //   |   `-9
+    //   |-5
+    //   `-10
+
+    // ps command results:
+    // PID PPID RSS
+    // 1   0    3216
+    // 2   0    4232
+    // 3   1    1234
+    // 4   2    1001
+    // 5   0    40000
+    // 7   3    2345
+    // 8   3    3456
+    // 9   2    1032
+    // 10  0    1024
+    ImmutableMap<Long, PsInfoCollector.PsInfo> psInfos =
+        ImmutableMap.of(
+            1L, PsInfoCollector.PsInfo.create(1, 0, 3216),
+            2L, PsInfoCollector.PsInfo.create(2, 0, 4232),
+            3L, PsInfoCollector.PsInfo.create(3, 1, 1234),
+            4L, PsInfoCollector.PsInfo.create(4, 2, 1001),
+            5L, PsInfoCollector.PsInfo.create(5, 0, 40000),
+            7L, PsInfoCollector.PsInfo.create(7, 3, 2345),
+            8L, PsInfoCollector.PsInfo.create(8, 3, 3456),
+            9L, PsInfoCollector.PsInfo.create(9, 2, 1032),
+            10L, PsInfoCollector.PsInfo.create(10, 0, 1024));
+    ImmutableSet<Long> pids = ImmutableSet.of(1L, 2L, 5L, 6L);
+    when(spyCollector.collectDataFromPs()).thenReturn(psInfos);
+
+    PsInfoCollector.ResourceSnapshot resourceSnapshot = spyCollector.collectResourceUsage(pids);
+
+    ImmutableMap<Long, Integer> expectedMemoryUsageByPid =
+        ImmutableMap.of(1L, 3216 + 1234 + 2345 + 3456, 2L, 4232 + 1001 + 1032, 5L, 40000);
+    assertThat(resourceSnapshot.getPidToMemoryInKb()).isEqualTo(expectedMemoryUsageByPid);
   }
 }
