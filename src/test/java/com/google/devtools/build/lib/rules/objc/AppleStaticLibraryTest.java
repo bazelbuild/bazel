e8cedf459936895f939906474d9f9878a8a45edc
Googler <waltl@google.com>
2023-04-26 14:11:44 -0700

    Remove obsolete xcrun/xcode_config implicit attributes
    PiperOrigin-RevId: 527370956
    Change-Id: I34730e2cd7c914444d32d8b3e1f79556933f5cf0

--- src/test/java/com/google/devtools/build/lib/rules/objc/AppleStaticLibraryTest.java
+++ src/test/java/com/google/devtools/build/lib/rules/objc/AppleStaticLibraryTest.java
@@ -79,7 +79,6 @@ public class AppleStaticLibraryTest extends ObjcRuleTestCase {
   public final void setup() throws Exception {
     scratch.file("test_starlark/BUILD");
     RepositoryName toolsRepo = TestConstants.TOOLS_REPOSITORY;
-    String toolsLoc = toolsRepo + "//tools/objc";
 
     scratch.file(
         "test_starlark/apple_static_library.bzl",
@@ -134,10 +133,6 @@ public class AppleStaticLibraryTest extends ObjcRuleTestCase {
         "        '_xcode_config': attr.label(",
         "            default=configuration_field(",
         "                fragment='apple', name='xcode_config_label'),),",
-        "        '_xcrunwrapper': attr.label(",
-        "            executable=True,",
-        "            cfg='exec',",
-        "            default=Label('" + toolsLoc + ":xcrunwrapper')),",
         "        'additional_linker_inputs': attr.label_list(",
         "            allow_files = True,",
         "        ),",
