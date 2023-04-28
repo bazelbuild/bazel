034f6464a19c560ba6d9e0159183075177d7ffb9
keertk <keerthanakumar@google.com>
2023-04-28 13:28:31 +0000

    Fix message generation of `ActionExecutionException` (#18257)
    This fixes an issue introduced by commit f95b80d166eb46ba4e4c0fb4b998f6e12206ee17, which resulted in certain exception message being printed twice.
    
    Closes #18243.
    
    PiperOrigin-RevId: 527848438
    Change-Id: Ic0f7a4a0e3bdf07c1c520647dbb4b41d29e05648
    
    Co-authored-by: Fabian Meumertzheim <fabian@meumertzhe.im>

--- src/main/java/com/google/devtools/build/lib/actions/ActionExecutionException.java
+++ src/main/java/com/google/devtools/build/lib/actions/ActionExecutionException.java
@@ -57,7 +57,7 @@ public class ActionExecutionException extends Exception implements DetailedExcep
       ActionAnalysisMetadata action,
       boolean catastrophe,
       DetailedExitCode detailedExitCode) {
-    super(combineMessages(message, cause), cause);
+    super(message, cause);
     this.action = action;
     this.catastrophe = catastrophe;
     this.detailedExitCode = checkNotNull(detailedExitCode);
@@ -96,7 +96,7 @@ public class ActionExecutionException extends Exception implements DetailedExcep
       NestedSet<Cause> rootCauses,
       boolean catastrophe,
       DetailedExitCode detailedExitCode) {
-    super(combineMessages(message, cause), cause);
+    super(message, cause);
     this.action = action;
     this.rootCauses = rootCauses;
     this.catastrophe = catastrophe;
@@ -202,13 +202,5 @@ public class ActionExecutionException extends Exception implements DetailedExcep
    */
   public boolean showError() {
     return getMessage() != null;
-  }
-
-  @Nullable
-  private static String combineMessages(String message, @Nullable Throwable cause) {
-    if (cause == null || cause.getMessage() == null) {
-      return message;
-    }
-    return message + ": " + cause.getMessage();
   }
 }
