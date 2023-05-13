5fbc5bd2cddcf8aa4c1bfd704ed9ea1f7b447ffa
Fabian Meumertzheim <fabian@meumertzhe.im>
2023-05-13 11:51:06 +0200

    Improve comment

--- src/main/java/com/google/devtools/build/lib/starlarkbuildapi/StarlarkRuleContextApi.java
+++ src/main/java/com/google/devtools/build/lib/starlarkbuildapi/StarlarkRuleContextApi.java
@@ -557,8 +557,10 @@ public interface StarlarkRuleContextApi<ConstraintValueT extends ConstraintValue
               @ParamType(type = Depset.class, generic1 = SymlinkEntryApi.class)
             },
             doc =
-                "Either a SymlinkEntry depset or the map of symlinks to be added to the runfiles"
-                    + " with the workspace name added as a prefix. See <a"
+                "Either a SymlinkEntry depset or the map of symlinks to be added to the runfiles."
+                    + " Symlinks are always added under the main workspace's runfiles directory"
+                    + " (e.g. <code>&lt;runfiles_root>/_main/&lt;symlink_path></code>, <b>not</b>" +
+                    " the directory corresponding to the current target's repository. See <a"
                     + " href=\"https://bazel.build/extending/rules#runfiles_symlinks\">Runfiles"
                     + " symlinks</a> in the rules guide."),
         @Param(
