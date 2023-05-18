bazelbuild/bazel

commit ab5a1f14067b614be7152ce2ab61c08048e5aa25

Eliminate RuleClassProvider#getEnvironment
Callers who want to enumerate the built-in symbols of a .bzl file (not accounting for injection) can now call BazelStarlarkEnvironment#getUninjectedBuildBzlEnv instead. This previously wasn't possible in all contexts because BazelStarlarkEnvironment used to require a PackageFactory.

The new method also contains the (uninjected) `native` object. Updated some callers to account for this.

PiperOrigin-RevId: 533117799
Change-Id: I30d367356c123fad6c50faa0a9d712ce28102c98

src/main/java/com/google/devtools/build/docgen/SymbolFamilies.java
@@ -20,7 +20,6 @@ import com.google.devtools.build.docgen.starlark.StarlarkDocExpander;
 import com.google.devtools.build.docgen.starlark.StarlarkDocPage;
 import com.google.devtools.build.lib.analysis.ConfiguredRuleClassProvider;
 import com.google.devtools.build.lib.util.Classpath.ClassPathException;
-import com.google.devtools.build.skydoc.fakebuildapi.FakeStarlarkNativeModuleApi;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -51,7 +50,8 @@ public class SymbolFamilies {
             collectNativeRules(
                 expander.ruleExpander, configuredRuleClassProvider, inputDirs, denyList));
     this.globals = Starlark.UNIVERSE;
-    this.bzlGlobals = collectBzlGlobals(configuredRuleClassProvider);
+    this.bzlGlobals =
+        configuredRuleClassProvider.getBazelStarlarkEnvironment().getUninjectedBuildBzlEnv();
     this.allDocPages = StarlarkDocumentationCollector.getAllDocPages(expander);
   }
 
@@ -97,16 +97,6 @@ public class SymbolFamilies {
         new ProtoFileBuildEncyclopediaProcessor(linkExpander, provider);
     processor.generateDocumentation(inputDirs, "", denyList);
     return processor.getNativeRules();
-  }
-
-  /** Collects symbols predefined in BZL files. */
-  private ImmutableMap<String, Object> collectBzlGlobals(ConfiguredRuleClassProvider provider) {
-    // StarlarkNativeModuleApi is faked, because we don't have a PackageFactory here
-    // and we can't use BazelStarlarkEnvironment.
-    return ImmutableMap.<String, Object>builder()
-        .put("native", new FakeStarlarkNativeModuleApi())
-        .putAll(provider.getEnvironment())
-        .buildOrThrow();
   }
 
   private ConfiguredRuleClassProvider createRuleClassProvider(String classProvider)

src/main/java/com/google/devtools/build/lib/analysis/ConfiguredRuleClassProvider.java
@@ -692,8 +692,6 @@ public /*final*/ class ConfiguredRuleClassProvider
 
   private final PrerequisiteValidator prerequisiteValidator;
 
-  private final ImmutableMap<String, Object> environment;
-
   private final BazelStarlarkEnvironment bazelStarlarkEnvironment;
 
   private final ImmutableList<SymlinkDefinition> symlinkDefinitions;
@@ -762,7 +760,7 @@ public /*final*/ class ConfiguredRuleClassProvider
     // BazelStarlarkEnvironment, or obviating the need for this method entirely.
     ImmutableMap<String, Object> nativeRuleSpecificBindings =
         createNativeRuleSpecificBindings(starlarkAccessibleTopLevels, starlarkBootstraps);
-    this.environment = createEnvironment(nativeRuleSpecificBindings);
+    ImmutableMap<String, Object> environment = createEnvironment(nativeRuleSpecificBindings);
     // If needed, we could allow the version to be customized by the builder e.g. for unit testing,
     // but at the moment it suffices to use the production value unconditionally.
     String version = BlazeVersionInfo.instance().getVersion();
@@ -900,11 +898,6 @@ public /*final*/ class ConfiguredRuleClassProvider
       }
     }
     return mapBuilder.buildOrThrow();
-  }
-
-  @Override
-  public ImmutableMap<String, Object> getEnvironment() {
-    return environment;
   }
 
   @Override

src/main/java/com/google/devtools/build/lib/packages/RuleClassProvider.java
@@ -57,18 +57,6 @@ public interface RuleClassProvider extends RuleDefinitionEnvironment {
   Map<String, NativeAspectClass> getNativeAspectClassMap();
 
   /**
-   * Returns the Starlark builtins registered with this RuleClassProvider.
-   *
-   * <p>Does not account for builtins injection. Excludes universal bindings (e.g. True, len).
-   *
-   * <p>See {@link BazelStarlarkEnvironment#getUninjectedBuildBzlNativeBindings} for the canonical
-   * determination of the bzl environment (before injection).
-   */
-  // TODO(b/280446865): See about eliminating this accessor, in favor of
-  // BazelStarlarkEnvironment#getUninjectedBuildBzlEnv().
-  ImmutableMap<String, Object> getEnvironment();
-
-  /**
    * Returns the {@link BazelStarlarkEnvironment}, which is the final determiner of the BUILD and
    * .bzl environment (with and without builtins injection).
    */

commit 9adb393f3f88c3b9496b16c75da6337c5c6e7af7

Fix docgen for "native" object
https://github.com/bazelbuild/bazel/commit/ab5a1f14067b614be7152ce2ab61c08048e5aa25 broke it by emitting the real `native` object, which is technically a slightly fancy struct, in place of a fake that inherited StarlarkNativeModuleApi, which has the proper doc annotations. This reverts the behavior.

PiperOrigin-RevId: 533282950
Change-Id: I68023bf7033663a118b176b5e3329f22301dd68d

src/main/java/com/google/devtools/build/docgen/SymbolFamilies.java
@@ -20,6 +20,7 @@ import com.google.devtools.build.docgen.starlark.StarlarkDocExpander;
 import com.google.devtools.build.docgen.starlark.StarlarkDocPage;
 import com.google.devtools.build.lib.analysis.ConfiguredRuleClassProvider;
 import com.google.devtools.build.lib.util.Classpath.ClassPathException;
+import com.google.devtools.build.skydoc.fakebuildapi.FakeStarlarkNativeModuleApi;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -50,8 +51,7 @@ public class SymbolFamilies {
             collectNativeRules(
                 expander.ruleExpander, configuredRuleClassProvider, inputDirs, denyList));
     this.globals = Starlark.UNIVERSE;
-    this.bzlGlobals =
-        configuredRuleClassProvider.getBazelStarlarkEnvironment().getUninjectedBuildBzlEnv();
+    this.bzlGlobals = collectBzlGlobals(configuredRuleClassProvider);
     this.allDocPages = StarlarkDocumentationCollector.getAllDocPages(expander);
   }
 
@@ -81,6 +81,22 @@ public class SymbolFamilies {
   // Returns a mapping between type names and module/type documentation.
   public ImmutableMap<Category, ImmutableList<StarlarkDocPage>> getAllDocPages() {
     return allDocPages;
+  }
+
+  /** Collects symbols predefined in BZL files. */
+  private ImmutableMap<String, Object> collectBzlGlobals(ConfiguredRuleClassProvider provider) {
+    // StarlarkNativeModuleApi is faked because we want to inherit the documentation carried in its
+    // annotations, whereas the real "native" object is just a bare struct.
+    ImmutableMap.Builder<String, Object> env = ImmutableMap.builder();
+    env.put("native", new FakeStarlarkNativeModuleApi());
+    for (Map.Entry<String, Object> entry :
+        provider.getBazelStarlarkEnvironment().getUninjectedBuildBzlEnv().entrySet()) {
+      if (entry.getKey().equals("native")) {
+        continue;
+      }
+      env.put(entry);
+    }
+    return env.buildOrThrow();
   }
 
   /*