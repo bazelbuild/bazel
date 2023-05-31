bazelbuild/bazel

commit 975866a3e09212766bd353a90a8c42efe91d53ab

Respect rules_java as the source of truth for java toolchains
Context: https://github.com/bazelbuild/bazel/discussions/18373

Currently the definitions of java tools, remote JDKs, and Java toolchains are duplicated in both Bazel sources and rules_java, and they go out of sync quite often. By default, the default toolchains shipped with the Bazel binary uses the one in Bazel sources except when Bzlmod is enabled.

https://github.com/bazelbuild/rules_java/pull/110 syncs java toolchain related files from Bazel sources to rules_java and this PR removes most of the definitions in Bazel source and uses rules_java as the source of truth.

Significant changes in this PR:

- Removed all java tools definitions from distdir_deps.bzl
- Removed all remote JDK definitions from distdir_deps.bzl except the ones for to be embedded within Bazel binary.
- Changed `jdk.WORKSPACE.tmpl` to load java tools and JDKs from rules_java as WORKSPACE suffix.
- Removed the `jdk_http_archives` hack for some shell tests. That dates back to https://github.com/bazelbuild/bazel/pull/8361, I don't think it's needed since we have the WORKSPACE suffix by default.
- When building Bazel with Bazel, we were using the java toolchains shipped in `@bazel_tools` of the Bazel binary, now we use the java toolchains from the `rules_java` defined in distdir_deps.bzl. This enables us to build Bazel itself with the latest java toolchain, therefore can detect issues earlier.
- Removed `_for_testing` suffixes for the shared repo hack for Bazel CI since the java tools and remote JDKs versions we load to build Bazel are the same as the ones shipped in the Bazel binary we built (via `jdk.WORKSPACE.tmpl`) and used in the integration tests. They all come from the rules_java version in distdir_deps.bzl.
- Removed the `test_WORKSPACE_files` hack for `workspace_resolved_test.sh`.
- Mocked rules_java in some integration tests to avoid needing to download it or its transitive dependencies.

Breaking changes:

tools/jdk/BUILD
@@ -12,18 +12,14 @@ filegroup(
         "BUILD.java_tools",
         "BUILD.java_tools_prebuilt",
         "BUILD-jdk",  # Tools are build from the workspace for tests.
-        "DumpPlatformClassPath.java",
         "default_java_toolchain.bzl",
-        "fail_rule.bzl",
         "java_toolchain_alias.bzl",
-        "jdk_build_file.bzl",
         "local_java_repository.bzl",
         "nosystemjdk/README",
         "proguard_whitelister.py",
         "proguard_whitelister_test.py",
         "proguard_whitelister_test_input.pgcfg",
         "remote_java_repository.bzl",
-        "toolchain_utils.bzl",
         ":java_stub_template.txt",
     ],
 )

tools/jdk/toolchain_utils.bzl
@@ -1,58 +1,0 @@
-# Copyright 2019 The Bazel Authors. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""
-Finds the Java toolchain.
-
-Returns the toolchain if enabled, and falls back to a toolchain constructed from
-legacy toolchain selection.
-"""
-
-def find_java_toolchain(ctx, target):
-    """
-    Finds the Java toolchain.
-
-    If the Java toolchain is in use, returns it.  Otherwise, returns a Java
-    toolchain derived from legacy toolchain selection.
-
-    Args:
-      ctx: The rule context for which to find a toolchain.
-      target: A java_toolchain target (for legacy toolchain resolution).
-
-    Returns:
-      A JavaToolchainInfo.
-    """
-
-    _ignore = [ctx]
-
-    return target[java_common.JavaToolchainInfo]
-
-def find_java_runtime_toolchain(ctx, target):
-    """
-    Finds the Java runtime.
-
-    If the Java toolchain is in use, returns it.  Otherwise, returns a Java
-    runtime derived from legacy toolchain selection.
-
-    Args:
-      ctx: The rule context for which to find a toolchain.
-      target: A java_runtime target (for legacy toolchain resolution).
-
-    Returns:
-      A JavaRuntimeInfo.
-    """
-
-    _ignore = [ctx]
-
-    return target[java_common.JavaRuntimeInfo]

src/BUILD
@@ -564,24 +564,19 @@ filegroup(
         "@bazel_skylib//:WORKSPACE",
         "@com_google_protobuf//:WORKSPACE",
         "@remote_coverage_tools//:WORKSPACE",
-        "@remote_java_tools_darwin_arm64_for_testing//:WORKSPACE",
-        "@remote_java_tools_darwin_x86_64_for_testing//:WORKSPACE",
-        "@remote_java_tools_for_testing//:WORKSPACE",
-        "@remote_java_tools_linux_for_testing//:WORKSPACE",
-        "@remote_java_tools_test//:WORKSPACE",
-        "@remote_java_tools_test_darwin_arm64//:WORKSPACE",
-        "@remote_java_tools_test_darwin_x86_64//:WORKSPACE",
-        "@remote_java_tools_test_linux//:WORKSPACE",
-        "@remote_java_tools_test_windows//:WORKSPACE",
-        "@remote_java_tools_windows_for_testing//:WORKSPACE",
-        "@remotejdk11_linux_aarch64_for_testing//:WORKSPACE",
-        "@remotejdk11_linux_for_testing//:WORKSPACE",
-        "@remotejdk11_linux_ppc64le_for_testing//:WORKSPACE",
-        "@remotejdk11_linux_s390x_for_testing//:WORKSPACE",
-        "@remotejdk11_macos_aarch64_for_testing//:WORKSPACE",
-        "@remotejdk11_macos_for_testing//:WORKSPACE",
-        "@remotejdk11_win_arm64_for_testing//:WORKSPACE",
-        "@remotejdk11_win_for_testing//:WORKSPACE",
+        "@remote_java_tools//:GenClass",
+        "@remote_java_tools_darwin_arm64//:java_tools/src/tools/singlejar/singlejar_local",
+        "@remote_java_tools_darwin_x86_64//:java_tools/src/tools/singlejar/singlejar_local",
+        "@remote_java_tools_linux//:java_tools/src/tools/singlejar/singlejar_local",
+        "@remote_java_tools_windows//:java_tools/src/tools/singlejar/singlejar_local.exe",
+        "@remotejdk11_linux//:WORKSPACE",
+        "@remotejdk11_linux_aarch64//:WORKSPACE",
+        "@remotejdk11_linux_ppc64le//:WORKSPACE",
+        "@remotejdk11_linux_s390x//:WORKSPACE",
+        "@remotejdk11_macos//:WORKSPACE",
+        "@remotejdk11_macos_aarch64//:WORKSPACE",
+        "@remotejdk11_win//:WORKSPACE",
+        "@remotejdk11_win_arm64//:WORKSPACE",
         "@rules_cc//:WORKSPACE",
         "@rules_java//:WORKSPACE",
         "@rules_pkg//:WORKSPACE",
@@ -589,11 +584,7 @@ filegroup(
         "@rules_python//:WORKSPACE",
         "@rules_testing//:WORKSPACE.bazel",
     ] + [
-        "@openjdk%s_%s_archive//:WORKSPACE" % (version, os)
-        for version in ("17", "20")
-        for os in ("darwin", "darwin_aarch64", "linux", "windows") + (("linux_s390x", "windows_arm64") if version != "20" else ())
-    ] + [
-        "@remotejdk%s_%s_for_testing//:WORKSPACE" % (version, os)
+        "@remotejdk%s_%s//:WORKSPACE" % (version, os)
         for version in ("17", "20")
         for os in ("macos", "macos_aarch64", "linux", "win") + (("linux_s390x", "win_arm64") if version != "20" else ())
     ],

src/main/java/com/google/devtools/build/lib/bazel/rules/java/BUILD
@@ -89,27 +89,10 @@ gen_workspace_stanza(
     urls = {urls},
     """,
     repos = [
-        "remote_java_tools",
-        "remote_java_tools_linux",
-        "remote_java_tools_windows",
-        "remote_java_tools_darwin_x86_64",
-        "remote_java_tools_darwin_arm64",
         "rules_cc",
         "rules_java",
         "rules_proto",
         "rules_python",
-        "remotejdk11_linux",
-        "remotejdk11_linux_aarch64",
-        "remotejdk11_linux_ppc64le",
-        "remotejdk11_linux_s390x",
-        "remotejdk11_macos",
-        "remotejdk11_macos_aarch64",
-        "remotejdk11_win",
-        "remotejdk11_win_arm64",
-    ] + [
-        "remotejdk%s_%s" % (version, os)
-        for version in ("17", "20")
-        for os in ("macos", "macos_aarch64", "linux", "linux_aarch64", "win") + (("linux_s390x", "win_arm64") if version != "20" else ())
     ],
     template = "jdk.WORKSPACE.tmpl",
     visibility = ["//:__pkg__"],

src/test/java/com/google/devtools/build/lib/analysis/mock/BUILD
@@ -20,6 +20,7 @@ java_library(
     ]),
     data = [
         "//tools/jdk:srcs",
+        "@rules_java//toolchains:srcs",
     ],
     resources = [
         "cc_toolchain_config.bzl",

src/test/shell/bazel/BUILD
@@ -230,7 +230,6 @@ sh_test(
         ":test-deps",
         "//src:java_tools_prebuilt_zip",
         "//src:java_tools_zip",
-        "//src/test/shell/bazel/testdata:jdk_http_archives_filegroup",
         "@bazel_tools//tools/bash/runfiles",
     ],
     tags = ["local"],
@@ -248,7 +247,6 @@ sh_test(
     ],
     data = [
         ":test-deps",
-        "//src/test/shell/bazel/testdata:jdk_http_archives_filegroup",
         "@bazel_tools//tools/bash/runfiles",
     ],
     exec_compatible_with = ["//:highcpu_machine"],
@@ -277,7 +275,6 @@ JAVA_VERSIONS_COVERAGE = ("11", "17")
             ":test-deps",
             "//src:java_tools_prebuilt_zip",
             "//src:java_tools_zip",
-            "//src/test/shell/bazel/testdata:jdk_http_archives_filegroup",
             "@bazel_tools//tools/bash/runfiles",
         ],
         exec_compatible_with = ["//:highcpu_machine"],
@@ -302,7 +299,6 @@ JAVA_VERSIONS_COVERAGE = ("11", "17")
         ],
         data = [
             ":test-deps",
-            "//src/test/shell/bazel/testdata:jdk_http_archives_filegroup",
             "@bazel_tools//tools/bash/runfiles",
         ],
         # This test is only run by the java_tools binaries pipeline.
@@ -488,7 +484,6 @@ sh_test(
     ],
     data = [
         ":test-deps",
-        "//src/test/shell/bazel/testdata:jdk_http_archives_filegroup",
     ],
     tags = [
         "no_windows",
@@ -512,7 +507,6 @@ sh_test(
         ],
         data = [
             ":test-deps",
-            "//src/test/shell/bazel/testdata:jdk_http_archives_filegroup",
         ],
         tags = [
             "no_windows",
@@ -540,7 +534,6 @@ sh_test(
             ":test-deps",
             "//src:java_tools_prebuilt_zip",
             "//src:java_tools_zip",
-            "//src/test/shell/bazel/testdata:jdk_http_archives_filegroup",
             "//tools/test/CoverageOutputGenerator/java/com/google/devtools/coverageoutputgenerator:coverage_output_generator_repo",
         ],
         tags = ["no_windows"],
@@ -939,20 +932,13 @@ sh_test(
     tags = ["no_windows"],
 )
 
-filegroup(
-    name = "test_WORKSPACE",
-    srcs = [
-        "@test_WORKSPACE_files//:archives",
-    ],
-)
-
 sh_test(
     name = "workspace_resolved_test",
     size = "large",
     srcs = ["workspace_resolved_test.sh"],
     data = [
         ":test-deps",
-        ":test_WORKSPACE",
+        "@bazel_tools//tools/bash/runfiles",
     ],
     shard_count = 22,
     tags = [

src/test/shell/bazel/remote/BUILD
@@ -25,7 +25,6 @@ sh_test(
         ":remote_utils",
         ":uds_proxy.py",
         "//src/test/shell/bazel:test-deps",
-        "//src/test/shell/bazel/testdata:jdk_http_archives_filegroup",
         "//src/tools/remote:worker",
         "@bazel_tools//tools/bash/runfiles",
     ],
@@ -40,7 +39,6 @@ sh_test(
     data = [
         ":remote_utils",
         "//src/test/shell/bazel:test-deps",
-        "//src/test/shell/bazel/testdata:jdk_http_archives_filegroup",
         "//src/tools/remote:worker",
         "@bazel_tools//tools/bash/runfiles",
     ],

src/test/shell/bazel/testdata/BUILD
@@ -1,5 +1,3 @@
-load("//:distdir_deps.bzl", "gen_workspace_stanza")
-
 package(default_testonly = True)
 
 filegroup(
@@ -26,44 +24,6 @@ filegroup(
         "//src/test/shell/bazel/testdata:embedded_tools_srcs_deps",
     ],
     visibility = ["//src/test/shell/bazel:__pkg__"],
-)
-
-filegroup(
-    name = "jdk_http_archives_filegroup",
-    srcs = [
-        "//src/test/shell/bazel/testdata:jdk_http_archives",
-    ],
-    visibility = ["//src/test/shell/bazel:__subpackages__"],
-)
-
-gen_workspace_stanza(
-    name = "workspace_with_rules_java",
-    out = "jdk_http_archives",
-    repo_clause = """
-  name = "{repo}",
-  sha256 = "{sha256}",
-  strip_prefix = {strip_prefix},
-  urls = {urls},
-""",
-    repos = [
-        "remote_java_tools_test",
-        "remote_java_tools_test_linux",
-        "remote_java_tools_test_windows",
-        "remote_java_tools_test_darwin_x86_64",
-        "remote_java_tools_test_darwin_arm64",
-        "openjdk11_linux_archive",
-        "openjdk11_linux_s390x_archive",
-        "openjdk11_darwin_archive",
-        "openjdk11_darwin_aarch64_archive",
-        "openjdk11_windows_archive",
-        "openjdk11_windows_arm64_archive",
-    ] + [
-        "openjdk%s_%s_archive" % (version, os)
-        for version in ("17", "20")
-        for os in ("linux", "darwin", "darwin_aarch64", "windows") + (("linux_s390x", "windows_arm64") if version != "20" else ())
-    ],
-    template = "jdk_http_archives.tmpl",
-    visibility = ["//:__pkg__"],
 )
 
 exports_files(["zstd_test_archive.tar.zst"])

commit ec62770d1463182fdf337ed765aaa95b7371306b

Add back @bazel_tools//tools/jdk/toolchain_utils.bzl
Fix some downstream breakages caused by 975866a3e09212766bd353a90a8c42efe91d53ab
https://buildkite.com/bazel/bazel-at-head-plus-downstream/builds/3065#_

RELNOTES: None
PiperOrigin-RevId: 536733220
Change-Id: I0954d62939a0ae1f8c3a3f5ce444bddeb932e34b

tools/jdk/BUILD
@@ -20,6 +20,7 @@ filegroup(
         "proguard_whitelister_test.py",
         "proguard_whitelister_test_input.pgcfg",
         "remote_java_repository.bzl",
+        "toolchain_utils.bzl",
         ":java_stub_template.txt",
     ],
 )

tools/jdk/toolchain_utils.bzl
@@ -1,0 +1,24 @@
+# Copyright 2023 The Bazel Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Redirect symbols from rules_java to keep backward-compatibility."""
+
+load(
+    "@rules_java//toolchains:toolchain_utils.bzl",
+    _find_java_runtime_toolchain = "find_java_runtime_toolchain",
+    _find_java_toolchain = "find_java_toolchain",
+)
+
+find_java_toolchain = _find_java_toolchain
+find_java_runtime_toolchain = _find_java_runtime_toolchain