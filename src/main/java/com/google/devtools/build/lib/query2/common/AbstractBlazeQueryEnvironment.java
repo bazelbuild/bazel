bazelbuild/bazel

commit daf80769da862e3a5f9de51f3844cbdf8f9dea66

Add a fast path to `AbstractBlazeQueryEnvironment#transformAsync` for when the given `QueryTaskFuture` is already done.
This slightly mitigates `StackOverflowError` issues in the query engine for deeply-nested expressions that use this helper method. The issue is that subclasses of `AbstractBlazeQueryEnvironment` use a single-threaded query engine, so everything is evaluated in-line, and the `directExecutor()` usage adds lots of frames to stacks.

PiperOrigin-RevId: 534993130
Change-Id: Ib078eb55e971950c381990ad2614542bc6d904b0

src/main/java/com/google/devtools/build/lib/query2/common/AbstractBlazeQueryEnvironment.java
@@ -359,9 +359,18 @@ public abstract class AbstractBlazeQueryEnvironment<T>
   @Override
   public <T1, T2> QueryTaskFuture<T2> transformAsync(
       QueryTaskFuture<T1> future, Function<T1, QueryTaskFuture<T2>> function) {
+    QueryTaskFutureImpl<T1> futureImpl = (QueryTaskFutureImpl<T1>) future;
+    if (futureImpl.isDone()) {
+      // Due to how our subclasses use single-threaded query engines, in practice
+      // futureImpl will always already be done. Therefore this is a fast-path to make it harder to
+      // stack overflow on deeply nested expressions whose evaluation involves #transformAsync.
+      //
+      // TODO(b/283225081): Do something more effective and more pervasive.
+      return function.apply(futureImpl.getIfSuccessful());
+    }
     return QueryTaskFutureImpl.ofDelegate(
         Futures.transformAsync(
-            (QueryTaskFutureImpl<T1>) future,
+            futureImpl,
             input -> (QueryTaskFutureImpl<T2>) function.apply(input),
             directExecutor()));
   }

src/test/java/com/google/devtools/build/lib/query2/testutil/AbstractQueryTest.java
@@ -64,6 +64,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import org.junit.After;
@@ -2184,6 +2185,19 @@ public abstract class AbstractQueryTest<T> {
         "deps(//foo:gen) + //foo:out + //foo:pg + //foo:other-pg",
         "deps(//foo:out)",
         Setting.NO_IMPLICIT_DEPS);
+  }
+
+  @Test
+  public void testDeepNestedLet() throws Exception {
+    writeFile("foo/BUILD", "sh_library(name = 'foo')");
+
+    // We used to get a StackOverflowError at this depth. We're still vulnerable to stack overflows
+    // at higher depths, due to how the query engine works.
+    int nestingDepth = 500;
+    String queryString =
+        Joiner.on(" + ").join(Collections.nCopies(nestingDepth, "let x = //foo:foo in $x"));
+
+    assertThat(evalToString(queryString)).isEqualTo("//foo:foo");
   }
 
   protected void writeBzlmodBuildFiles() throws Exception {

commit 46e9ece5983321e423689494f84cdca4cf061cc9

Fix blatant bug introduced by https://github.com/bazelbuild/bazel/commit/daf80769da862e3a5f9de51f3844cbdf8f9dea66.
That CL tried to introduce a fast path in `AbstractBlazeQueryEnvironment#transformAsync` for the situation where the input `QueryTaskFuture` is already done, but it handled only the obvious sub-case where the `QueryTaskFuture` was done and successful. It's also possible for the `QueryTaskFuture` to be done and cancelled (corresponding to an interrupt being observed during evaluation of a sub-expression) or done and unsuccessful (corresponding to a failure during evaluation of a sub-expression).

PiperOrigin-RevId: 537919715
Change-Id: I8b427c494e0f8df4a206749ec1801b606a310c6a

src/main/java/com/google/devtools/build/lib/query2/common/AbstractBlazeQueryEnvironment.java
@@ -366,7 +366,15 @@ public abstract class AbstractBlazeQueryEnvironment<T>
       // stack overflow on deeply nested expressions whose evaluation involves #transformAsync.
       //
       // TODO(b/283225081): Do something more effective and more pervasive.
-      return function.apply(futureImpl.getIfSuccessful());
+      T1 t1;
+      try {
+        t1 = futureImpl.getChecked();
+      } catch (QueryException e) {
+        return immediateFailedFuture(e);
+      } catch (InterruptedException e) {
+        return immediateCancelledFuture();
+      }
+      return function.apply(t1);
     }
     return QueryTaskFutureImpl.ofDelegate(
         Futures.transformAsync(

src/test/java/com/google/devtools/build/lib/query2/testutil/AbstractQueryTest.java
@@ -2154,6 +2154,18 @@ public abstract class AbstractQueryTest<T> {
   }
 
   @Test
+  public void testUnsuccessfulInnerFutureInNestedLetTransformAsyncFastPath() throws Exception {
+    // Not actually needed for the behavior being tested, but needed for the cquery and aquery test
+    // subclasses that infer and load a universe.
+    writeFile("foo/BUILD", "sh_library(name = 'foo')");
+    EvalThrowsResult result =
+        evalThrows("let x = let y = //foo in $nope in $x", /* unconditionallyThrows= */ true);
+    assertThat(result.getMessage()).contains("undefined variable 'nope'");
+    assertThat(result.getMessage()).doesNotContain("java.lang.IllegalStateException");
+    assertQueryCode(result.getFailureDetail(), Query.Code.VARIABLE_UNDEFINED);
+  }
+
+  @Test
   public void testUnconditionalQueryException() throws Exception {
     // The query expression being evaluated needs to be of the form "e1 + e2", where evaluation of
     // "e1" throws a QueryException even in keepGoing mode. See cl/141772584.