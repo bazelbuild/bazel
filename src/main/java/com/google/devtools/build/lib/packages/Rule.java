bazelbuild/bazel

commit 91978396182d9c8756cb2585122d350185a1d3f6

Don't store a parsed `RuleVisibility` in `Rule`.
When a rule is loaded, only validate the attribute value. Parse the raw attribute value into a `RuleVisibility` on demand when needed.

This saves a lot of memory from `PackageGroupsRuleVisibility` instances, many of which may belong to rules that are loaded but never depended on. This also removes a field from `Rule`. Although it doesn't move the class below an 8-byte threshold yet, I have more planned changes that will.

`bind` rules are special-cased in `Rule` since they are always public and have no `visibility` attribute. See the change in `WorkspaceFactoryHelper`, which needed to be accounted for.

In order to offset some of the cpu/garbage cost of computing `RuleVisibility` on demand, provide an alternative, more efficient method `getVisibilityDependencyLabels`, which is used by `DependencyResolver`, which only needs the dep labels and not a whole `RuleVisibility` instance.

PiperOrigin-RevId: 517199212
Change-Id: Id76adbfc87d7359675de36cdf284e8495cd0ee77

src/main/java/com/google/devtools/build/lib/packages/Rule.java
@@ -27,6 +27,7 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableListMultimap;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Iterators;
 import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.SetMultimap;
@@ -122,7 +123,6 @@ public class Rule implements Target, DependencyFilter.AttributeInfoProvider {
    */
   private byte[] attrBytes;
 
-  private RuleVisibility visibility;
   private boolean containsErrors = false;
 
   /**
@@ -163,10 +163,6 @@ public class Rule implements Target, DependencyFilter.AttributeInfoProvider {
     this.callstack = checkNotNull(callstack);
     this.attrValues = new Object[ruleClass.getAttributeCount()];
     this.attrBytes = new byte[bitSetSize()];
-  }
-
-  void setVisibility(RuleVisibility visibility) {
-    this.visibility = visibility;
   }
 
   void setContainsErrors() {
@@ -1064,39 +1060,71 @@ public class Rule implements Target, DependencyFilter.AttributeInfoProvider {
     return getRuleClass() + " rule " + label;
   }
 
- /**
-   * Returns the effective visibility of this Rule. Visibility is computed from
+  /**
+   * Returns the effective visibility of this rule. For most rules, visibility is computed from
    * these sources in this order of preference:
-   *   - 'visibility' attribute
-   *   - 'default_visibility;' attribute of package() declaration
-   *   - public.
+   *
+   * <ol>
+   *   <li>'visibility' attribute
+   *   <li>Package default visibility ('default_visibility' attribute of package() declaration)
+   * </ol>
    */
   @Override
   public RuleVisibility getVisibility() {
+    List<Label> rawLabels = getRawVisibilityLabels();
+    return rawLabels == null
+        ? getDefaultVisibility()
+        // The attribute value was already validated when it was set, so call the unchecked method.
+        : RuleVisibility.parseUnchecked(rawLabels);
+  }
+
+  @Override
+  public Iterable<Label> getVisibilityDependencyLabels() {
+    List<Label> rawLabels = getRawVisibilityLabels();
+    if (rawLabels == null) {
+      return getDefaultVisibility().getDependencyLabels();
+    }
+    RuleVisibility constantVisibility = RuleVisibility.parseIfConstant(rawLabels);
+    if (constantVisibility != null) {
+      return constantVisibility.getDependencyLabels();
+    }
+    // Filter out labels like :__pkg__ and :__subpackages__.
+    return Iterables.filter(rawLabels, label -> PackageSpecification.fromLabel(label) == null);
+  }
+
+  @Override
+  public List<Label> getVisibilityDeclaredLabels() {
+    List<Label> rawLabels = getRawVisibilityLabels();
+    return rawLabels == null ? getDefaultVisibility().getDeclaredLabels() : rawLabels;
+  }
+
+  public boolean isVisibilitySpecified() {
+    return ruleClass.getName().equals("bind") || isAttributeValueExplicitlySpecified("visibility");
+  }
+
+  @Nullable
+  @SuppressWarnings("unchecked")
+  private List<Label> getRawVisibilityLabels() {
+    Integer visibilityIndex = ruleClass.getAttributeIndex("visibility");
+    if (visibilityIndex == null) {
+      return null;
+    }
+    return (List<Label>) getRawAttrValue(visibilityIndex);
+  }
+
+  private RuleVisibility getDefaultVisibility() {
+    if (ruleClass.getName().equals("bind")) {
+      return RuleVisibility.PUBLIC; // bind rules are always public.
+    }
     // Temporary logic to relax config_setting's visibility enforcement while depot migrations set
     // visibility settings properly (legacy code may have visibility settings that would break if
     // enforced). See https://github.com/bazelbuild/bazel/issues/12669. Ultimately this entire
     // conditional should be removed.
-    if (ruleClass.getName().equals("config_setting")) {
-      ConfigSettingVisibilityPolicy policy = pkg.getConfigSettingVisibilityPolicy();
-      if (visibility != null) {
-        return visibility; // Use explicitly set visibility
-      } else if (policy == ConfigSettingVisibilityPolicy.DEFAULT_PUBLIC) {
-        return ConstantRuleVisibility.PUBLIC; // Default: //visibility:public.
-      } else {
-        return pkg.getDefaultVisibility(); // Default: same as all other rules.
-      }
-    }
-
-    // All other rules.
-    if (visibility != null) {
-      return visibility;
+    if (ruleClass.getName().equals("config_setting")
+        && pkg.getConfigSettingVisibilityPolicy() == ConfigSettingVisibilityPolicy.DEFAULT_PUBLIC) {
+      return RuleVisibility.PUBLIC; // Default: //visibility:public.
     }
     return pkg.getDefaultVisibility();
-  }
-
-  public boolean isVisibilitySpecified() {
-    return visibility != null;
   }
 
   @Override

src/main/java/com/google/devtools/build/lib/query2/query/output/XmlOutputFormatter.java
@@ -263,13 +263,13 @@ class XmlOutputFormatter extends AbstractUnorderedFormatter {
   }
 
   private static void addPackageGroupsToElement(Document doc, Element parent, Target target) {
-    for (Label visibilityDependency : target.getVisibility().getDependencyLabels()) {
+    for (Label visibilityDependency : target.getVisibilityDependencyLabels()) {
       Element elem = doc.createElement("package-group");
       elem.setAttribute("name", visibilityDependency.toString());
       parent.appendChild(elem);
     }
 
-    for (Label visibilityDeclaration : target.getVisibility().getDeclaredLabels()) {
+    for (Label visibilityDeclaration : target.getVisibilityDeclaredLabels()) {
       Element elem = doc.createElement("visibility-label");
       elem.setAttribute("name", visibilityDeclaration.toString());
       parent.appendChild(elem);

src/main/java/com/google/devtools/build/lib/analysis/ConfiguredTargetFactory.java
@@ -14,6 +14,7 @@
 
 package com.google.devtools.build.lib.analysis;
 
+import static com.google.common.base.Preconditions.checkState;
 import static java.util.stream.Collectors.joining;
 
 import com.google.common.annotations.VisibleForTesting;
@@ -54,7 +55,6 @@ import com.google.devtools.build.lib.packages.Aspect;
 import com.google.devtools.build.lib.packages.Attribute;
 import com.google.devtools.build.lib.packages.ConfigurationFragmentPolicy;
 import com.google.devtools.build.lib.packages.ConfigurationFragmentPolicy.MissingFragmentPolicy;
-import com.google.devtools.build.lib.packages.ConstantRuleVisibility;
 import com.google.devtools.build.lib.packages.EnvironmentGroup;
 import com.google.devtools.build.lib.packages.InputFile;
 import com.google.devtools.build.lib.packages.OutputFile;
@@ -118,43 +118,42 @@ public final class ConfiguredTargetFactory {
       EventHandler reporter,
       Target target) {
     RuleVisibility ruleVisibility = target.getVisibility();
-    if (ruleVisibility instanceof ConstantRuleVisibility) {
-      return ((ConstantRuleVisibility) ruleVisibility).isPubliclyVisible()
-          ? PUBLIC_VISIBILITY
-          : PRIVATE_VISIBILITY;
-    } else if (ruleVisibility instanceof PackageGroupsRuleVisibility) {
-      PackageGroupsRuleVisibility packageGroupsVisibility =
-          (PackageGroupsRuleVisibility) ruleVisibility;
-
-      NestedSetBuilder<PackageGroupContents> result = NestedSetBuilder.stableOrder();
-      for (Label groupLabel : packageGroupsVisibility.getPackageGroups()) {
-        // PackageGroupsConfiguredTargets are always in the package-group configuration.
-        TransitiveInfoCollection group = findVisibilityPrerequisite(prerequisiteMap, groupLabel);
-        PackageSpecificationProvider provider = null;
-        // group == null can only happen if the package group list comes
-        // from a default_visibility attribute, because in every other case,
-        // this missing link is caught during transitive closure visitation or
-        // if the RuleConfiguredTargetGraph threw out a visibility edge
-        // because if would have caused a cycle. The filtering should be done
-        // in a single place, ConfiguredTargetGraph, but for now, this is the
-        // minimally invasive way of providing a sane error message in case a
-        // cycle is created by a visibility attribute.
-        if (group != null) {
-          provider = group.get(PackageGroupConfiguredTarget.PROVIDER);
-        }
-        if (provider != null) {
-          result.addTransitive(provider.getPackageSpecifications());
-        } else {
-          reporter.handle(Event.error(target.getLocation(),
-              String.format("Label '%s' does not refer to a package group", groupLabel)));
-        }
-      }
-
-      result.add(packageGroupsVisibility.getDirectPackages());
-      return result.build();
-    } else {
-      throw new IllegalStateException("unknown visibility");
+    if (ruleVisibility.equals(RuleVisibility.PUBLIC)) {
+      return PUBLIC_VISIBILITY;
     }
+    if (ruleVisibility.equals(RuleVisibility.PRIVATE)) {
+      return PRIVATE_VISIBILITY;
+    }
+    checkState(ruleVisibility instanceof PackageGroupsRuleVisibility, ruleVisibility);
+    PackageGroupsRuleVisibility packageGroupsVisibility =
+        (PackageGroupsRuleVisibility) ruleVisibility;
+
+    NestedSetBuilder<PackageGroupContents> result = NestedSetBuilder.stableOrder();
+    for (Label groupLabel : packageGroupsVisibility.getPackageGroups()) {
+      // PackageGroupsConfiguredTargets are always in the package-group configuration.
+      TransitiveInfoCollection group = findVisibilityPrerequisite(prerequisiteMap, groupLabel);
+      PackageSpecificationProvider provider = null;
+      // group == null can only happen if the package group list comes from a default_visibility
+      // attribute, because in every other case, this missing link is caught during transitive
+      // closure visitation or if the RuleConfiguredTargetGraph threw out a visibility edge because
+      // if would have caused a cycle. The filtering should be done in a single place,
+      // ConfiguredTargetGraph, but for now, this is the minimally invasive way of providing a sane
+      // error message in case a cycle is created by a visibility attribute.
+      if (group != null) {
+        provider = group.get(PackageGroupConfiguredTarget.PROVIDER);
+      }
+      if (provider != null) {
+        result.addTransitive(provider.getPackageSpecifications());
+      } else {
+        reporter.handle(
+            Event.error(
+                target.getLocation(),
+                String.format("Label '%s' does not refer to a package group", groupLabel)));
+      }
+    }
+
+    result.add(packageGroupsVisibility.getDirectPackages());
+    return result.build();
   }
 
   @Nullable
@@ -512,7 +511,7 @@ public final class ConfiguredTargetFactory {
         missingFragments.add(fragment);
       }
     }
-    Preconditions.checkState(!missingFragments.isEmpty());
+    checkState(!missingFragments.isEmpty());
     return "all rules of type "
         + ruleClass.getName()
         + " require the presence of all of ["

src/main/java/com/google/devtools/build/lib/analysis/DependencyResolver.java
@@ -52,7 +52,6 @@ import com.google.devtools.build.lib.packages.ExecGroup;
 import com.google.devtools.build.lib.packages.InputFile;
 import com.google.devtools.build.lib.packages.OutputFile;
 import com.google.devtools.build.lib.packages.PackageGroup;
-import com.google.devtools.build.lib.packages.PackageGroupsRuleVisibility;
 import com.google.devtools.build.lib.packages.Rule;
 import com.google.devtools.build.lib.packages.RuleClass;
 import com.google.devtools.build.lib.packages.RuleTransitionData;
@@ -60,7 +59,6 @@ import com.google.devtools.build.lib.packages.Target;
 import com.google.devtools.build.lib.packages.Type;
 import com.google.devtools.build.lib.util.OrderedSetMultimap;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -110,7 +108,7 @@ public abstract class DependencyResolver {
           .setPropagatingAspects(ImmutableList.of());
     }
 
-    public DependencyKey.Builder getDependencyKeyBuilder() {
+    DependencyKey.Builder getDependencyKeyBuilder() {
       return DependencyKey.builder()
           .setLabel(getLabel())
           .setExecutionPlatformLabel(getExecutionPlatformLabel());
@@ -239,17 +237,17 @@ public abstract class DependencyResolver {
       this.attributeMap = attributeMap;
     }
 
-    public OrderedSetMultimap<DependencyKind, Label> labels() {
+    OrderedSetMultimap<DependencyKind, Label> labels() {
       return labels;
     }
 
     @Nullable // Non-null for rules and output files when there are aspects that apply to files.
-    public ConfiguredAttributeMapper attributeMap() {
+    ConfiguredAttributeMapper attributeMap() {
       return attributeMap;
     }
   }
 
-  private final DependencyLabels computeDependencyLabels(
+  private static DependencyLabels computeDependencyLabels(
       TargetAndConfiguration node,
       Iterable<Aspect> aspects,
       ImmutableMap<Label, ConfigMatchingProvider> configConditions,
@@ -261,11 +259,11 @@ public abstract class DependencyResolver {
 
     // TODO(bazel-team): Figure out a way to implement the below (and partiallyResolveDependencies)
     // using LabelVisitationUtils.
-    Rule fromRule = null;
+    Rule fromRule;
     ConfiguredAttributeMapper attributeMap = null;
     if (target instanceof OutputFile) {
       Preconditions.checkNotNull(config);
-      visitTargetVisibility(node, outgoingLabels);
+      addVisibilityDepLabels(target.getVisibilityDependencyLabels(), outgoingLabels);
       Rule rule = ((OutputFile) target).getGeneratingRule();
       outgoingLabels.put(OUTPUT_FILE_RULE_DEPENDENCY, rule.getLabel());
       if (Iterables.any(aspects, a -> a.getDefinition().applyToFiles())) {
@@ -273,10 +271,8 @@ public abstract class DependencyResolver {
         resolveAttributes(getAspectAttributes(aspects), outgoingLabels, rule, attributeMap, config);
       }
       addToolchainDeps(toolchainContexts, outgoingLabels);
-    } else if (target instanceof InputFile) {
-      visitTargetVisibility(node, outgoingLabels);
-    } else if (target instanceof EnvironmentGroup) {
-      visitTargetVisibility(node, outgoingLabels);
+    } else if (target instanceof InputFile || target instanceof EnvironmentGroup) {
+      addVisibilityDepLabels(target.getVisibilityDependencyLabels(), outgoingLabels);
     } else if (target instanceof Rule) {
       fromRule = (Rule) target;
       attributeMap = ConfiguredAttributeMapper.of(fromRule, configConditions, config);
@@ -297,7 +293,7 @@ public abstract class DependencyResolver {
    * should <b>NOT</b> get the {@link Target} instances representing the targets of the dependency
    * edges as an argument.
    */
-  private OrderedSetMultimap<DependencyKind, PartiallyResolvedDependency>
+  private static OrderedSetMultimap<DependencyKind, PartiallyResolvedDependency>
       partiallyResolveDependencies(
           OrderedSetMultimap<DependencyKind, Label> outgoingLabels,
           @Nullable Rule fromRule,
@@ -442,7 +438,7 @@ public abstract class DependencyResolver {
    * being calculated as an argument or its attributes and it should <b>NOT</b> do anything with the
    * keys of {@code partiallyResolvedDeps} other than passing them on to the output map.
    */
-  private OrderedSetMultimap<DependencyKind, DependencyKey> fullyResolveDependencies(
+  private static OrderedSetMultimap<DependencyKind, DependencyKey> fullyResolveDependencies(
       OrderedSetMultimap<DependencyKind, PartiallyResolvedDependency> partiallyResolvedDeps,
       Map<Label, Target> targetMap,
       BuildConfigurationValue originalConfiguration,
@@ -496,7 +492,7 @@ public abstract class DependencyResolver {
     }
   }
 
-  private void visitRule(
+  private static void visitRule(
       TargetAndConfiguration node,
       Iterable<Aspect> aspects,
       ConfiguredAttributeMapper attributeMap,
@@ -513,11 +509,12 @@ public abstract class DependencyResolver {
       throw new Failure(rule.getLocation(), ex.getMessage());
     }
 
-    visitTargetVisibility(node, outgoingLabels);
+    Iterable<Label> visibilityDepLabels = rule.getVisibilityDependencyLabels();
+    addVisibilityDepLabels(visibilityDepLabels, outgoingLabels);
     resolveAttributes(getAttributes(rule, aspects), outgoingLabels, rule, attributeMap, ruleConfig);
 
     // Add the rule's visibility labels (which may come from the rule or from package defaults).
-    addExplicitDeps(outgoingLabels, rule, "visibility", rule.getVisibility().getDependencyLabels());
+    addExplicitDeps(outgoingLabels, rule, "visibility", visibilityDepLabels);
 
     // Add package default constraints when the rule doesn't explicitly declare them.
     //
@@ -560,7 +557,7 @@ public abstract class DependencyResolver {
     addToolchainDeps(toolchainContexts, outgoingLabels);
   }
 
-  private void addToolchainDeps(
+  private static void addToolchainDeps(
       ToolchainCollection<ToolchainContext> toolchainContexts,
       OrderedSetMultimap<DependencyKind, Label> outgoingLabels) {
     if (toolchainContexts != null) {
@@ -573,7 +570,7 @@ public abstract class DependencyResolver {
     }
   }
 
-  private void resolveAttributes(
+  private static void resolveAttributes(
       Iterable<AttributeDependencyKind> attributeDependencyKinds,
       OrderedSetMultimap<DependencyKind, Label> outgoingLabels,
       Rule rule,
@@ -610,7 +607,7 @@ public abstract class DependencyResolver {
     }
   }
 
-  private <T> void resolveAttribute(
+  private static <T> void resolveAttribute(
       Attribute attribute,
       Type<T> type,
       AttributeDependencyKind dependencyKind,
@@ -656,7 +653,7 @@ public abstract class DependencyResolver {
 
   @Nullable
   @VisibleForTesting(/* used to test LateBoundDefaults' default values */ )
-  public static <FragmentT> Object resolveLateBoundDefault(
+  static <FragmentT> Object resolveLateBoundDefault(
       Rule rule,
       AttributeMap attributeMap,
       Attribute attribute,
@@ -691,11 +688,11 @@ public abstract class DependencyResolver {
    * @param attrName the name of the attribute to add dependency labels to
    * @param labels the dependencies to add
    */
-  private void addExplicitDeps(
+  private static void addExplicitDeps(
       OrderedSetMultimap<DependencyKind, Label> outgoingLabels,
       Rule rule,
       String attrName,
-      Collection<Label> labels) {
+      Iterable<Label> labels) {
     if (!rule.isAttrDefined(attrName, BuildType.LABEL_LIST)
         && !rule.isAttrDefined(attrName, BuildType.NODEP_LABEL_LIST)) {
       return;
@@ -753,7 +750,7 @@ public abstract class DependencyResolver {
   }
 
   /** Returns the attributes that should be visited for this rule/aspect combination. */
-  private ImmutableList<AttributeDependencyKind> getAttributes(
+  private static ImmutableList<AttributeDependencyKind> getAttributes(
       Rule rule, Iterable<Aspect> aspects) {
     ImmutableList.Builder<AttributeDependencyKind> result = ImmutableList.builder();
     // If processing aspects, aspect attribute names may conflict with the attribute names of
@@ -770,13 +767,14 @@ public abstract class DependencyResolver {
     return result.build();
   }
 
-  private ImmutableList<AttributeDependencyKind> getAspectAttributes(Iterable<Aspect> aspects) {
+  private static ImmutableList<AttributeDependencyKind> getAspectAttributes(
+      Iterable<Aspect> aspects) {
     ImmutableList.Builder<AttributeDependencyKind> result = ImmutableList.builder();
     addAspectAttributes(aspects, new HashSet<>(), result);
     return result.build();
   }
 
-  private void addAspectAttributes(
+  private static void addAspectAttributes(
       Iterable<Aspect> aspects,
       Set<String> aspectProcessedAttributes,
       ImmutableList.Builder<AttributeDependencyKind> attributes) {
@@ -837,21 +835,16 @@ public abstract class DependencyResolver {
     }
   }
 
-  private void visitTargetVisibility(
-      TargetAndConfiguration node, OrderedSetMultimap<DependencyKind, Label> outgoingLabels) {
-    Target target = node.getTarget();
-    outgoingLabels.putAll(VISIBILITY_DEPENDENCY, target.getVisibility().getDependencyLabels());
+  private static void addVisibilityDepLabels(
+      Iterable<Label> labels, OrderedSetMultimap<DependencyKind, Label> outgoingLabels) {
+    outgoingLabels.putAll(VISIBILITY_DEPENDENCY, labels);
   }
 
-  private void checkPackageGroupVisibility(Rule fromRule, Map<Label, Target> targetMap)
+  private static void checkPackageGroupVisibility(Rule fromRule, Map<Label, Target> targetMap)
       throws Failure {
-    if (!(fromRule.getVisibility() instanceof PackageGroupsRuleVisibility)) {
-      return;
-    }
-
-    for (Label label : fromRule.getVisibility().getDependencyLabels()) {
-      if (targetMap.get(label) != null
-          && !targetMap.get(label).getTargetKind().equals(PackageGroup.targetKind())) {
+    for (Label label : fromRule.getVisibilityDependencyLabels()) {
+      Target target = targetMap.get(label);
+      if (target != null && !target.getTargetKind().equals(PackageGroup.targetKind())) {
         throw new Failure(
             fromRule.getLocation(),
             String.format("Label '%s' does not refer to a package group.", label));

src/main/java/com/google/devtools/build/lib/packages/AggregatingAttributeMapper.java
@@ -395,7 +395,7 @@ public class AggregatingAttributeMapper extends AbstractAttributeMapper {
       // This special case for the visibility attribute is needed because its value is replaced
       // with an empty list during package loading if it is public or private in order not to visit
       // the package called 'visibility'.
-      return ImmutableList.of(type.cast(rule.getVisibility().getDeclaredLabels()));
+      return ImmutableList.of(type.cast(rule.getVisibilityDeclaredLabels()));
     }
 
     // For any other attribute, just return its direct value.

commit 2ddf91015f41f2d7fbb21d58156123bf23e921bd

Remove AttributeValueSource in the query output package
This class can be inlined (the "visibility" special case isn't actually special; nobody uses `PACKAGE`).

This allows us to remove `Package#isDefaultVisibilitySet`.

This also accidentally fixes a minor regression introduced in https://github.com/bazelbuild/bazel/commit/91978396182d9c8756cb2585122d350185a1d3f6 that made `bazel query` output for `bind` rules always output a public visibility. Since there's no way to specify visibility for `bind` rules anyway (it makes no sense), the query output for `bind` rules now never includes the `visibility` attribute, just like in 6.2.1.

PiperOrigin-RevId: 538178953
Change-Id: I2c6d025c7ef8b6dba39d3d9738f92c578ad06fb9

src/main/java/com/google/devtools/build/lib/packages/Rule.java
@@ -1125,10 +1125,6 @@ public class Rule implements Target, DependencyFilter.AttributeInfoProvider {
     return rawLabels == null ? getDefaultVisibility().getDeclaredLabels() : rawLabels;
   }
 
-  public boolean isVisibilitySpecified() {
-    return ruleClass.getName().equals("bind") || isAttributeValueExplicitlySpecified("visibility");
-  }
-
   @Nullable
   @SuppressWarnings("unchecked")
   private List<Label> getRawVisibilityLabels() {

src/main/java/com/google/devtools/build/lib/query2/query/output/AttributeValueSource.java
@@ -1,46 +1,0 @@
-// Copyright 2014 The Bazel Authors. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package com.google.devtools.build.lib.query2.query.output;
-
-import com.google.devtools.build.lib.packages.Attribute;
-import com.google.devtools.build.lib.packages.Rule;
-
-/** Where the value of an attribute comes from. */
-public enum AttributeValueSource {
-  /** Explicitly specified on the rule. */
-  RULE,
-
-  /** Package default. */
-  PACKAGE,
-
-  /** Rule class default. */
-  DEFAULT;
-
-  public static AttributeValueSource forRuleAndAttribute(Rule rule, Attribute attr) {
-    if (attr.getName().equals("visibility")) {
-      if (rule.isVisibilitySpecified()) {
-        return AttributeValueSource.RULE;
-      } else if (rule.getPackage().isDefaultVisibilitySet()) {
-        return AttributeValueSource.PACKAGE;
-      } else {
-        return AttributeValueSource.DEFAULT;
-      }
-    } else {
-      return rule.isAttributeValueExplicitlySpecified(attr)
-          ? AttributeValueSource.RULE
-          : AttributeValueSource.DEFAULT;
-    }
-  }
-}

src/main/java/com/google/devtools/build/lib/query2/query/output/BuildOutputFormatter.java
@@ -121,9 +121,7 @@ public class BuildOutputFormatter extends AbstractUnorderedFormatter {
               .append(lineTerm);
           continue;
         }
-        AttributeValueSource attributeValueSource =
-            AttributeValueSource.forRuleAndAttribute(rule, attr);
-        if (attributeValueSource != AttributeValueSource.RULE) {
+        if (!rule.isAttributeValueExplicitlySpecified(attr)) {
           continue; // Don't print default values.
         }
 

src/main/java/com/google/devtools/build/lib/query2/query/output/XmlOutputFormatter.java
@@ -158,10 +158,7 @@ class XmlOutputFormatter extends AbstractUnorderedFormatter {
       elem = doc.createElement("rule");
       elem.setAttribute("class", rule.getRuleClass());
       for (Attribute attr : rule.getAttributes()) {
-        AttributeValueSource attributeValueSource =
-            AttributeValueSource.forRuleAndAttribute(rule, attr);
-        if (attributeValueSource == AttributeValueSource.RULE
-            || queryOptions.xmlShowDefaultValues) {
+        if (rule.isAttributeValueExplicitlySpecified(attr) || queryOptions.xmlShowDefaultValues) {
           // TODO(b/162524370): mayTreatMultipleAsNone should be true for types that drop multiple
           //  values.
           Iterable<Object> values =