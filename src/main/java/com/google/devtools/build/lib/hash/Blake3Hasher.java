a5249cb7c409afcb0ac16f2d831812d2f3c52b36
Tyler Williams <williams.tyler@gmail.com>
2023-06-11 22:05:07 -0700

    Remove AbstractHasher

--- src/main/java/com/google/devtools/build/lib/hash/Blake3Hasher.java
+++ src/main/java/com/google/devtools/build/lib/hash/Blake3Hasher.java
@@ -1,11 +1,16 @@
 package com.google.devtools.build.lib.hash;
 
+import static com.google.common.base.Preconditions.checkState;
+
+import com.google.common.hash.Funnel;
 import com.google.common.hash.HashCode;
 import com.google.common.hash.Hasher;
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.nio.charset.Charset;
 
-public class Blake3Hasher extends AbstractHasher {
+public class Blake3Hasher implements Hasher {
   // These constants match the native definitions in:
   // https://github.com/BLAKE3-team/BLAKE3/blob/master/c/blake3.h
   public static final int KEY_LEN = 32;
@@ -19,6 +24,11 @@ public class Blake3Hasher extends AbstractHasher {
 
   private ByteBuffer buffer;
   private long hasher = -1;
+  private boolean isDone;
+
+  public Blake3Hasher() {
+    isDone = false;
+  }
 
   private boolean isAllocated() {
     return (hasher != -1);
@@ -59,6 +69,9 @@ public class Blake3Hasher extends AbstractHasher {
   public byte[] getOutput(int outputLength) throws IllegalArgumentException {
     byte[] retByteArray = new byte[outputLength];
 
+    checkState(!isDone);
+    isDone = true;
+
     if (!isAllocated() && buffer != null) {
       Blake3JNI.blake3_hasher_oneshot(
           hasher, buffer, buffer.position(), retByteArray, outputLength);
@@ -74,33 +87,98 @@ public class Blake3Hasher extends AbstractHasher {
     return retByteArray;
   }
 
-  // The following overrides allow us to implement Hasher.
-  @Override
+  /* The following methods implement the {Hasher} interface. */
+
+  @CanIgnoreReturnValue
   public Hasher putBytes(ByteBuffer b) {
     buffer = b;
     return this;
   }
 
-  @Override
+  @CanIgnoreReturnValue
   public Hasher putBytes(byte[] bytes, int off, int len) {
     update(bytes, off, len);
     return this;
   }
 
-  @Override
+  @CanIgnoreReturnValue
   public Hasher putBytes(byte[] bytes) {
     update(bytes, 0, bytes.length);
     return this;
   }
 
-  @Override
+  @CanIgnoreReturnValue
   public Hasher putByte(byte b) {
     update(new byte[] {b});
     return this;
   }
 
-  @Override
   public HashCode hash() {
     return HashCode.fromBytes(getOutput(OUT_LEN));
+  }
+
+  @CanIgnoreReturnValue
+  public final Hasher putBoolean(boolean b) {
+    return putByte(b ? (byte) 1 : (byte) 0);
+  }
+
+  @CanIgnoreReturnValue
+  public final Hasher putDouble(double d) {
+    return putLong(Double.doubleToRawLongBits(d));
+  }
+
+  @CanIgnoreReturnValue
+  public final Hasher putFloat(float f) {
+    return putInt(Float.floatToRawIntBits(f));
+  }
+
+  @CanIgnoreReturnValue
+  public Hasher putUnencodedChars(CharSequence charSequence) {
+    for (int i = 0, len = charSequence.length(); i < len; i++) {
+      putChar(charSequence.charAt(i));
+    }
+    return this;
+  }
+
+  @CanIgnoreReturnValue
+  public Hasher putString(CharSequence charSequence, Charset charset) {
+    return putBytes(charSequence.toString().getBytes(charset));
+  }
+
+  @CanIgnoreReturnValue
+  public Hasher putShort(short s) {
+    putByte((byte) s);
+    putByte((byte) (s >>> 8));
+    return this;
+  }
+
+  @CanIgnoreReturnValue
+  public Hasher putInt(int i) {
+    putByte((byte) i);
+    putByte((byte) (i >>> 8));
+    putByte((byte) (i >>> 16));
+    putByte((byte) (i >>> 24));
+    return this;
+  }
+
+  @CanIgnoreReturnValue
+  public Hasher putLong(long l) {
+    for (int i = 0; i < 64; i += 8) {
+      putByte((byte) (l >>> i));
+    }
+    return this;
+  }
+
+  @CanIgnoreReturnValue
+  public Hasher putChar(char c) {
+    putByte((byte) c);
+    putByte((byte) (c >>> 8));
+    return this;
+  }
+
+  @CanIgnoreReturnValue
+  public <T extends Object> Hasher putObject(T instance, Funnel<? super T> funnel) {
+    funnel.funnel(instance, this);
+    return this;
   }
 }
