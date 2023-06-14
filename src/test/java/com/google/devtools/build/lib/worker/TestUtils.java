2f7625c51c07c3405bf800521e3d2d1e89ce26a9
Googler <wilwell@google.com>
2023-06-14 03:17:36 -0700

    Add new test in WorkerLifecycleManager.
    In this tests we check that even if we have same mnemonic but different worker keys WorkerLifecycleManager handles this situation correctly.
    
    PiperOrigin-RevId: 540218661
    Change-Id: I5e410ce5498ca276dc7db7eda9b39fdd49477c98

--- src/test/java/com/google/devtools/build/lib/worker/TestUtils.java
+++ src/test/java/com/google/devtools/build/lib/worker/TestUtils.java
@@ -97,6 +97,16 @@ class TestUtils {
     return createWorkerKey(protocolFormat, fs, false);
   }
 
+  static WorkerKey createWorkerKey(String mnemonic, FileSystem fs, boolean sandboxed) {
+    return createWorkerKey(
+        WorkerProtocolFormat.PROTO,
+        fs,
+        mnemonic,
+        /* multiplex= */ false,
+        sandboxed,
+        /* dynamic= */ false);
+  }
+
   static WorkerKey createWorkerKey(String mnemonic, FileSystem fs) {
     return createWorkerKey(
         WorkerProtocolFormat.PROTO,
