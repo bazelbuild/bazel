ac0f83041f82aea7a8b72bf41c8021de167a641a
Ian (Hee) Cha <heec@google.com>
2023-06-21 16:33:29 -0700

    Allow module extension usages to be isolated (#18727)
    If `isolate = True` is specified on `use_extension`, that particular usage will be isolated from all other usages, both in the same and other modules.
    
    Module extensions can check whether they are isolated (e.g. in case they can only be used in this way) via `module_ctx.is_isolated`.
    
    Closes #18529.
    
    PiperOrigin-RevId: 541823020
    Change-Id: I68a7b49914bbc1fd50df2fda7a0af1e47421bb92
    
    Co-authored-by: Fabian Meumertzheim <fabian@meumertzhe.im>

--- src/main/java/com/google/devtools/build/lib/bazel/bzlmod/ModuleExtensionId.java
+++ src/main/java/com/google/devtools/build/lib/bazel/bzlmod/ModuleExtensionId.java
@@ -17,15 +17,41 @@ package com.google.devtools.build.lib.bazel.bzlmod;
 
 import com.google.auto.value.AutoValue;
 import com.google.devtools.build.lib.cmdline.Label;
+import java.util.Optional;
 
 /** A unique identifier for a {@link ModuleExtension}. */
 @AutoValue
 public abstract class ModuleExtensionId {
+
+  /** A unique identifier for a single isolated usage of a fixed module extension. */
+  @AutoValue
+  abstract static class IsolationKey {
+    /** The module which contains this isolated usage of a module extension. */
+    public abstract ModuleKey getModule();
+
+    /** Whether this isolated usage specified {@code dev_dependency = True}. */
+    public abstract boolean isDevUsage();
+
+    /**
+     * The 0-based index of this isolated usage within the module's isolated usages of the same
+     * module extension and with the same {@link #isDevUsage()} value.
+     */
+    public abstract int getIsolatedUsageIndex();
+
+    public static IsolationKey create(
+        ModuleKey module, boolean isDevUsage, int isolatedUsageIndex) {
+      return new AutoValue_ModuleExtensionId_IsolationKey(module, isDevUsage, isolatedUsageIndex);
+    }
+  }
+
   public abstract Label getBzlFileLabel();
 
   public abstract String getExtensionName();
 
-  public static ModuleExtensionId create(Label bzlFileLabel, String extensionName) {
-    return new AutoValue_ModuleExtensionId(bzlFileLabel, extensionName);
+  public abstract Optional<IsolationKey> getIsolationKey();
+
+  public static ModuleExtensionId create(
+      Label bzlFileLabel, String extensionName, Optional<IsolationKey> isolationKey) {
+    return new AutoValue_ModuleExtensionId(bzlFileLabel, extensionName, isolationKey);
   }
 }
