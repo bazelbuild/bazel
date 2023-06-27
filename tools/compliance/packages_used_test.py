63e5d819b95036fe638f203a9f57c9620b974cc0
Tony Aiuto <aiuto@google.com>
2023-06-26 21:19:07 -0400

    linty

--- tools/compliance/packages_used_test.py
+++ tools/compliance/packages_used_test.py
@@ -1,3 +1,16 @@
+# Copyright 2023 The Bazel Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """Smoke test for packages_used."""
 
 import json
@@ -36,7 +49,9 @@ class PackagesUsedTest(unittest.TestCase):
         "@remoteapis//:build_bazel_remote_execution_v2_remote_execution_proto",
         content["packages"])
 
-
+  def test_package_info(self):
+    # Placeholder: There are no package_info declarations yet.
+    pass
 
 if __name__ == "__main__":
   unittest.main()
