0e899c5d9289771908648ce4362c45c2171def8d
Googler <twigg@google.com>
2023-06-30 08:44:25 -0700

    Better centralize package defaults into PackageArgs
    Package defaults are currently somewhat haphazardly specified in Package and PackageArgs.
    Instead, more consistently get package defaults from PackageArgs:
    * Remove all getDefault??? parameters from Package and Package.Builder. Instead declare getPackageArgs to get the container of all package defaults.
    * Move defaultHdrsCheck into PackageArgs (was in Package) as it IS a package default being set by the package() function. See NOTE below.
    * PackageArgs.processParams now uses a select statement for both readability and mild theoretical performance gains.
    * AttributeMap now just needs to simply declare an abstract getPackageArgs rather than an ever-growing list of getDefault???
    
    Ultimately, this should clarify mutability a bit. The result of Package.getPackageArgs is 'done' and thus should no longer change. In contrast, Package.Builder.getPartialPackageArgs is subject to change as the build file is further processed.
    
    NOTE: This does allow Bazel users to specify default_hdrs_check inside of package() (in order to make handling this attribute more consistent with all of the others).
    
    As future work:
    PackageSerializer can almost assuredly be simplified to more clearly and directly serialize/deserialize PackageArgs rather than inline this ser-des process into itself.
    
    Remove Package.Builder.getPartialPackageArgs. This is clearly incomplete and the result of these reads depends on the location of package() in the BUILD file.
    
    default_hdrs_check likely needs further cleanup to act more consistently with the other attributes.
    
    Need to be more consistent on where getPackageArgs is called (in a ComputedDefault attached to an attribute versus transitively in CobinguredTargetFunction via RuleContext lookups). This is likely the underlying culprit for some attributes acting strangely under query.
    
    The default_settings portion of the output formatters should likely be removed in favor of something more principled (either just relying on having those attributes be populated via ComputedDefault or dumping all of PackageArgs into default_settings).
    
    PiperOrigin-RevId: 544666167
    Change-Id: I2251aaf7b4f2d21372eec60d333679cc6d851b7f

--- src/main/java/com/google/devtools/build/lib/packages/AttributeMap.java
+++ src/main/java/com/google/devtools/build/lib/packages/AttributeMap.java
@@ -130,16 +130,5 @@ public interface AttributeMap {
   /** Same as {@link #visitAllLabels} but for attributes matching a {@link DependencyFilter}. */
   void visitLabels(DependencyFilter filter, BiConsumer<Attribute, Label> consumer);
 
-  // TODO(bazel-team): These methods are here to support computed defaults that inherit
-  // package-level default values. Instead, we should auto-inherit and remove the computed
-  // defaults. If we really need to give access to package-level defaults, we should come up with
-  // a more generic interface.
-  String getPackageDefaultHdrsCheck();
-
-  boolean isPackageDefaultHdrsCheckSet();
-
-  Boolean getPackageDefaultTestOnly();
-
-  String getPackageDefaultDeprecation();
-
+  PackageArgs getPackageArgs();
 }
