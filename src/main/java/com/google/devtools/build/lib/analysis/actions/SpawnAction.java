bazelbuild/bazel

commit db347988dfd1b4b8cd5685de63d937f2ffa76b25

Add thinlto support for tree artifacts
Add a new feature to bazel: use_lto_native_object_directory.
When this feature is enabled, the thinlto objects are stored in a different directory as the .imports and .thinlto.bc file outputs from the indexing action, and thinlto can be used in the presence of tree artifacts.

Create LtoBackendActionTemplate.java to generate lto backend actions to tree artifact.

Minor: move constants to rules/cpp/CppFileTypes.java and rules/cpp/CppHelper.java for consistency.

Limitations:
There is still no support for .dwo files for tree artifacts, i.e. compiling using fission. TreeArtifacts do not generate minimized bitcode files, only full bitcode files, because of lack of support at the indexing stage.
PiperOrigin-RevId: 544144581
Change-Id: Idc6a638a6fccef6912b79948ea167dd6e651f156

src/main/java/com/google/devtools/build/lib/analysis/actions/SpawnAction.java
@@ -600,7 +600,7 @@ public class SpawnAction extends AbstractAction implements CommandAction {
     public Builder() {}
 
     /** Creates a builder that is a copy of another builder. */
-    Builder(Builder other) {
+    public Builder(Builder other) {
       this.toolsBuilder.addTransitive(other.toolsBuilder.build());
       this.inputsBuilder.addTransitive(other.inputsBuilder.build());
       this.outputs.addAll(other.outputs);
@@ -657,7 +657,7 @@ public class SpawnAction extends AbstractAction implements CommandAction {
     }
 
     @CheckReturnValue
-    SpawnAction buildForActionTemplate(ActionOwner owner) {
+    public SpawnAction buildForActionTemplate(ActionOwner owner) {
       CommandLines.Builder result = CommandLines.builder();
       if (executableArg != null) {
         result.addSingleArgument(executableArg);
@@ -683,12 +683,7 @@ public class SpawnAction extends AbstractAction implements CommandAction {
         ActionEnvironment env) {
       NestedSet<Artifact> tools = toolsBuilder.build();
 
-      // Tools are by definition a subset of the inputs, so make sure they're present there, too.
-      NestedSet<Artifact> inputsAndTools =
-          NestedSetBuilder.<Artifact>stableOrder()
-              .addTransitive(inputsBuilder.build())
-              .addTransitive(tools)
-              .build();
+      NestedSet<Artifact> inputsAndTools = getInputsAndTools();
 
       return createSpawnAction(
           owner,
@@ -786,6 +781,17 @@ public class SpawnAction extends AbstractAction implements CommandAction {
     public Builder addInputs(Iterable<Artifact> artifacts) {
       inputsBuilder.addAll(artifacts);
       return this;
+    }
+
+    /**
+     * Returns the inputs that the spawn action will depend on. Tools are by definition a subset of
+     * the inputs, so they are also present.
+     */
+    public NestedSet<Artifact> getInputsAndTools() {
+      return NestedSetBuilder.<Artifact>stableOrder()
+          .addTransitive(inputsBuilder.build())
+          .addTransitive(toolsBuilder.build())
+          .build();
     }
 
     /** Adds transitive inputs to this action. */

.bazelci/postsubmit.yml
@@ -87,6 +87,7 @@ tasks:
       - "--sandbox_writable_path=$HOME/bazeltest"
       - "--test_env=TEST_INSTALL_BASE=$HOME/bazeltest/install_base"
       - "--test_env=TEST_REPOSITORY_HOME=$OUTPUT_BASE/external"
+      - "--test_tag_filters=-no_1804"
       # Configure and enable tests that require access to the network.
       - "--test_env=REMOTE_NETWORK_ADDRESS=bazel.build:80"
     test_targets:
@@ -138,6 +139,7 @@ tasks:
       - "--sandbox_writable_path=$HOME/bazeltest"
       - "--test_env=TEST_INSTALL_BASE=$HOME/bazeltest/install_base"
       - "--test_env=TEST_REPOSITORY_HOME=$OUTPUT_BASE/external"
+      - "--test_tag_filters=-no_1804"
     test_targets:
       - "//src/test/shell/bazel:cc_integration_test"
     include_json_profile:
@@ -402,6 +404,7 @@ tasks:
       - "--experimental_remote_download_outputs=minimal"
       - "--experimental_inmemory_jdeps_files"
       - "--experimental_inmemory_dotd_files"
+      - "--test_tag_filters=-no_1804"
     test_targets:
       - "//scripts/..."
       - "//src/java_tools/..."

.bazelci/presubmit.yml
@@ -92,6 +92,7 @@ tasks:
       - "--sandbox_writable_path=$HOME/bazeltest"
       - "--test_env=TEST_INSTALL_BASE=$HOME/bazeltest/install_base"
       - "--test_env=TEST_REPOSITORY_HOME=$OUTPUT_BASE/external"
+      - "--test_tag_filters=-no_1804"
       # Configure and enable tests that require access to the network.
       - "--test_env=REMOTE_NETWORK_ADDRESS=bazel.build:80"
     test_targets:
@@ -143,6 +144,7 @@ tasks:
       - "--sandbox_writable_path=$HOME/bazeltest"
       - "--test_env=TEST_INSTALL_BASE=$HOME/bazeltest/install_base"
       - "--test_env=TEST_REPOSITORY_HOME=$OUTPUT_BASE/external"
+      - "--test_tag_filters=-no_1804"
     test_targets:
       - "//src/test/shell/bazel:cc_integration_test"
     include_json_profile:
@@ -471,6 +473,7 @@ tasks:
       - "--experimental_remote_cache_async"
       - "--experimental_remote_merkle_tree_cache"
       - "--remote_download_minimal"
+      - "--test_tag_filters=-no_1804"
     test_targets:
       - "//scripts/..."
       - "//src/java_tools/..."

src/main/java/com/google/devtools/build/lib/rules/cpp/BitcodeFiles.java
@@ -13,9 +13,13 @@
 // limitations under the License.
 package com.google.devtools.build.lib.rules.cpp;
 
+import static com.google.common.collect.ImmutableMap.toImmutableMap;
+
+import com.google.common.collect.ImmutableMap;
 import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
 import com.google.devtools.build.lib.util.Fingerprint;
+import com.google.devtools.build.lib.vfs.PathFragment;
 import javax.annotation.Nullable;
 
 /** Wrapper around a map of bitcode files for purposes of caching its fingerprint. */
@@ -30,6 +34,12 @@ final class BitcodeFiles {
 
   NestedSet<Artifact> getFiles() {
     return files;
+  }
+
+  /** Helper function to get a map from path to artifact */
+  ImmutableMap<PathFragment, Artifact> getFilesArtifactPathMap() {
+    return getFiles().toList().stream()
+        .collect(toImmutableMap(Artifact::getExecPath, artifact -> artifact));
   }
 
   void addToFingerprint(Fingerprint fp) {

src/main/java/com/google/devtools/build/lib/rules/cpp/CcCompilationHelper.java
@@ -1398,10 +1398,12 @@ public final class CcCompilationHelper {
                     source,
                     outputName,
                     builder,
+                    result,
                     ImmutableList.of(
                         ArtifactCategory.GENERATED_HEADER, ArtifactCategory.PROCESSED_HEADER),
                     // If we generate pic actions, we prefer the header actions to use the pic mode.
-                    generatePicAction);
+                    generatePicAction,
+                    bitcodeOutput);
             result.addHeaderTokenFile(headerTokenFile);
             break;
           case SOURCE:
@@ -1412,8 +1414,10 @@ public final class CcCompilationHelper {
                       source,
                       outputName,
                       builder,
+                      result,
                       ImmutableList.of(ArtifactCategory.OBJECT_FILE),
-                      /* usePic= */ false);
+                      /* usePic= */ false,
+                      featureConfiguration.isEnabled(CppRuleClasses.THIN_LTO));
               result.addObjectFile(objectFile);
             }
 
@@ -1424,8 +1428,10 @@ public final class CcCompilationHelper {
                       source,
                       outputName,
                       builder,
+                      result,
                       ImmutableList.of(ArtifactCategory.PIC_OBJECT_FILE),
-                      /* usePic= */ true);
+                      /* usePic= */ true,
+                      featureConfiguration.isEnabled(CppRuleClasses.THIN_LTO));
               result.addPicObjectFile(picObjectFile);
             }
             break;
@@ -1463,8 +1469,10 @@ public final class CcCompilationHelper {
       CppSource source,
       String outputName,
       CppCompileActionBuilder builder,
+      CcCompilationOutputs.Builder result,
       ImmutableList<ArtifactCategory> outputCategories,
-      boolean usePic)
+      boolean usePic,
+      boolean bitcodeOutput)
       throws RuleErrorException, InterruptedException {
     if (usePic) {
       builder = new CppCompileActionBuilder(builder).setPicMode(true);
@@ -1504,6 +1512,24 @@ public final class CcCompilationHelper {
               actionConstructionContext, label, sourceArtifact, outputName, usePic);
     }
 
+    // Currently we do not generate minimized bitcode files for tree artifacts because of issues
+    // with the indexing step.
+    // If ltoIndexTreeArtifact is set to a tree artifact, the minimized bitcode files will be
+    // properly generated and will be an input to the indexing step. However, the lto indexing step
+    // fails. The indexing step finds the full bitcode file by replacing the suffix of the
+    // minimized bitcode file, therefore they have to be in the same directory.
+    // Since the files are in the same directory, the command line artifact expander expands the
+    // tree artifact to both the minimized bitcode files and the full bitcode files, causing an
+    // error that functions are defined twice.
+    // TODO(b/289071777): support for minimized bitcode files.
+    SpecialArtifact ltoIndexTreeArtifact = null;
+
+    if (bitcodeOutput) {
+      Label sourceLabel = source.getLabel();
+      result.addLtoBitcodeFile(
+          outputFiles, ltoIndexTreeArtifact, getCopts(sourceArtifact, sourceLabel));
+    }
+
     ActionOwner actionOwner = null;
     if (actionConstructionContext instanceof RuleContext
         && ((RuleContext) actionConstructionContext).useAutoExecGroups()) {
@@ -1515,6 +1541,7 @@ public final class CcCompilationHelper {
             outputFiles,
             dotdTreeArtifact,
             diagnosticsTreeArtifact,
+            ltoIndexTreeArtifact,
             builder,
             ccToolchain,
             outputCategories,

src/main/java/com/google/devtools/build/lib/rules/cpp/CcModule.java
@@ -429,6 +429,7 @@ public abstract class CcModule
         /* interfaceLibraryBuilder= */ null,
         /* interfaceLibraryOutput= */ null,
         /* ltoOutputRootPrefix= */ null,
+        /* ltoObjRootPrefix= */ null,
         convertFromNoneable(defFile, /* defaultValue= */ null),
         /* fdoContext= */ null,
         Depset.noneableCast(
@@ -941,10 +942,17 @@ public abstract class CcModule
     }
   }
 
+  /**
+   * Create an LTO backend that does not perform any cross-module optimization because Starlark does
+   * not hava support for LTO indexing actions yet.
+   *
+   * <p>TODO(b/128341904): Do cross module optimization once there is Starlark support.
+   */
   @Override
   public LtoBackendArtifacts createLtoBackendArtifacts(
       StarlarkRuleContext starlarkRuleContext,
       String ltoOutputRootPrefixString,
+      String ltoObjRootPrefixString,
       Artifact bitcodeFile,
       FeatureConfigurationForStarlark featureConfigurationForStarlark,
       CcToolchainProvider ccToolchain,
@@ -957,6 +965,7 @@ public abstract class CcModule
     isCalledFromStarlarkCcCommon(thread);
     RuleContext ruleContext = starlarkRuleContext.getRuleContext();
     PathFragment ltoOutputRootPrefix = PathFragment.create(ltoOutputRootPrefixString);
+    PathFragment ltoObjRootPrefix = PathFragment.create(ltoObjRootPrefixString);
     LtoBackendArtifacts ltoBackendArtifacts;
     try {
       ltoBackendArtifacts =
@@ -966,7 +975,9 @@ public abstract class CcModule
               ruleContext.getConfiguration().getOptions(),
               ruleContext.getConfiguration().getFragment(CppConfiguration.class),
               ltoOutputRootPrefix,
+              ltoObjRootPrefix,
               bitcodeFile,
+              /* allBitcodeFiles= */ null,
               starlarkRuleContext.actions().getActionConstructionContext(),
               ruleContext.getRepository(),
               ruleContext.getConfiguration(),
@@ -2802,7 +2813,7 @@ public abstract class CcModule
       helper.addVariableExtension(new UserVariablesExtension(asDict(variablesExtension)));
     }
     if (convertFromNoneable(useShareableArtifactFactory, false)) {
-      helper.setLinkArtifactFactory(CppLinkActionBuilder.SHAREABLE_LINK_ARTIFACT_FACTORY);
+      helper.setLinkArtifactFactory(CppLinkAction.SHAREABLE_LINK_ARTIFACT_FACTORY);
     }
     CcCompilationOutputs compilationOutputs =
         convertFromNoneable(compilationOutputsObject, /* defaultValue= */ null);

src/main/java/com/google/devtools/build/lib/rules/cpp/CppCompileActionTemplate.java
@@ -17,6 +17,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
 import com.google.devtools.build.lib.actions.ActionAnalysisMetadata;
 import com.google.devtools.build.lib.actions.ActionExecutionContext;
 import com.google.devtools.build.lib.actions.ActionExecutionException;
@@ -38,6 +39,7 @@ import com.google.devtools.build.lib.server.FailureDetails;
 import com.google.devtools.build.lib.util.DetailedExitCode;
 import com.google.devtools.build.lib.util.Fingerprint;
 import com.google.devtools.build.lib.vfs.FileSystemUtils;
+import com.google.devtools.build.lib.vfs.PathFragment;
 import javax.annotation.Nullable;
 
 /** An {@link ActionTemplate} that expands into {@link CppCompileAction}s at execution time. */
@@ -48,6 +50,7 @@ public final class CppCompileActionTemplate extends ActionKeyCacher
   private final SpecialArtifact outputTreeArtifact;
   private final SpecialArtifact dotdTreeArtifact;
   private final SpecialArtifact diagnosticsTreeArtifact;
+  private final SpecialArtifact ltoIndexTreeArtifact;
   private final CcToolchainProvider toolchain;
   private final ImmutableList<ArtifactCategory> categories;
   private final ActionOwner actionOwner;
@@ -61,6 +64,7 @@ public final class CppCompileActionTemplate extends ActionKeyCacher
    * @param outputTreeArtifact the TreeArtifact that contains compilation outputs.
    * @param dotdTreeArtifact the TreeArtifact that contains dotd files.
    * @param diagnosticsTreeArtifact the TreeArtifact that contains serialized diagnostics files.
+   * @param ltoIndexTreeArtifact the TreeArtifact that contains lto index files (minimized bitcode).
    * @param cppCompileActionBuilder An almost completely configured {@link CppCompileActionBuilder}
    *     without the input and output files set. It is used as a template to instantiate expanded
    *     {CppCompileAction}s.
@@ -74,6 +78,7 @@ public final class CppCompileActionTemplate extends ActionKeyCacher
       SpecialArtifact outputTreeArtifact,
       SpecialArtifact dotdTreeArtifact,
       SpecialArtifact diagnosticsTreeArtifact,
+      SpecialArtifact ltoIndexTreeArtifact,
       CppCompileActionBuilder cppCompileActionBuilder,
       CcToolchainProvider toolchain,
       ImmutableList<ArtifactCategory> categories,
@@ -82,6 +87,7 @@ public final class CppCompileActionTemplate extends ActionKeyCacher
     this.sourceTreeArtifact = sourceTreeArtifact;
     this.outputTreeArtifact = outputTreeArtifact;
     this.dotdTreeArtifact = dotdTreeArtifact;
+    this.ltoIndexTreeArtifact = ltoIndexTreeArtifact;
     this.diagnosticsTreeArtifact = diagnosticsTreeArtifact;
     this.toolchain = toolchain;
     this.categories = categories;
@@ -146,12 +152,25 @@ public final class CppCompileActionTemplate extends ActionKeyCacher
             TreeFileArtifact.createTemplateExpansionOutput(
                 diagnosticsTreeArtifact, outputName + ".dia", artifactOwner);
       }
+
+      TreeFileArtifact ltoIndexFileArtifact = null;
+      if (ltoIndexTreeArtifact != null) {
+        PathFragment outputFilePathFragment = PathFragment.create(outputName);
+        PathFragment thinltofile =
+            FileSystemUtils.replaceExtension(
+                outputFilePathFragment,
+                Iterables.getOnlyElement(CppFileTypes.LTO_INDEXING_OBJECT_FILE.getExtensions()));
+        ltoIndexFileArtifact =
+            TreeFileArtifact.createTemplateExpansionOutput(
+                ltoIndexTreeArtifact, thinltofile, artifactOwner);
+      }
       expandedActions.add(
           createAction(
               inputTreeFileArtifact,
               outputTreeFileArtifact,
               dotdFileArtifact,
               diagnosticsFileArtifact,
+              ltoIndexFileArtifact,
               privateHeaders));
     }
 
@@ -201,13 +220,15 @@ public final class CppCompileActionTemplate extends ActionKeyCacher
       TreeFileArtifact outputTreeFileArtifact,
       @Nullable Artifact dotdFileArtifact,
       @Nullable Artifact diagnosticsFileArtifact,
+      @Nullable Artifact ltoIndexFileArtifact,
       NestedSet<Artifact> privateHeaders)
       throws ActionExecutionException {
     CppCompileActionBuilder builder =
         new CppCompileActionBuilder(cppCompileActionBuilder)
             .setAdditionalPrunableHeaders(privateHeaders)
             .setSourceFile(sourceTreeFileArtifact)
-            .setOutputs(outputTreeFileArtifact, dotdFileArtifact, diagnosticsFileArtifact);
+            .setOutputs(outputTreeFileArtifact, dotdFileArtifact, diagnosticsFileArtifact)
+            .setLtoIndexingFile(ltoIndexFileArtifact);
 
     CcToolchainVariables.Builder buildVariables =
         CcToolchainVariables.builder(cppCompileActionBuilder.getVariables());
@@ -226,6 +247,12 @@ public final class CppCompileActionTemplate extends ActionKeyCacher
       buildVariables.overrideStringVariable(
           CompileBuildVariables.SERIALIZED_DIAGNOSTICS_FILE.getVariableName(),
           diagnosticsFileArtifact.getExecPathString());
+    }
+
+    if (ltoIndexFileArtifact != null) {
+      buildVariables.overrideStringVariable(
+          CompileBuildVariables.LTO_INDEXING_BITCODE_FILE.getVariableName(),
+          ltoIndexFileArtifact.getExecPathString());
     }
 
     builder.setVariables(buildVariables.build());
@@ -316,10 +343,15 @@ public final class CppCompileActionTemplate extends ActionKeyCacher
 
   @Override
   public ImmutableSet<Artifact> getOutputs() {
-    if (dotdTreeArtifact == null) {
-      return ImmutableSet.of(outputTreeArtifact);
+    ImmutableSet.Builder<Artifact> builder = ImmutableSet.builder();
+    builder.add(outputTreeArtifact);
+    if (dotdTreeArtifact != null) {
+      builder.add(dotdTreeArtifact);
     }
-    return ImmutableSet.of(outputTreeArtifact, dotdTreeArtifact);
+    if (ltoIndexTreeArtifact != null) {
+      builder.add(ltoIndexTreeArtifact);
+    }
+    return builder.build();
   }
 
   @Override

src/main/java/com/google/devtools/build/lib/rules/cpp/CppFileTypes.java
@@ -239,6 +239,13 @@ public final class CppFileTypes {
   // Minimized bitcode file emitted by the ThinLTO compile step and used just for LTO indexing.
   public static final FileType LTO_INDEXING_OBJECT_FILE = FileType.of(".indexing.o");
 
+  // Imports file emitted by the ThinLTO indexing step and used for LTO backend action.
+  public static final FileType LTO_IMPORTS_FILE = FileType.of(".imports");
+
+  // Indexing analysis result file emitted by the ThinLTO indexing step and used for LTO backend
+  // action.
+  public static final FileType LTO_INDEXING_ANALYSIS_FILE = FileType.of(".thinlto.bc");
+
   // TODO(bazel-team): File types should not be read from this hard-coded list but should come from
   // the toolchain instead. See https://github.com/bazelbuild/bazel/issues/17117
   public static final FileType SHARED_LIBRARY =

commit 4714cd70c83de43ba7ab10c29e63af716216770a

Reuse the built set of tools in `SpawnAction.Builder`.
Fixes a memory regression introduced by https://github.com/bazelbuild/bazel/commit/db347988dfd1b4b8cd5685de63d937f2ffa76b25.

PiperOrigin-RevId: 548689761
Change-Id: Ie0881f0c6c6ef3c351b7888057735c58cd2b6ddb

src/main/java/com/google/devtools/build/lib/analysis/actions/SpawnAction.java
@@ -683,7 +683,12 @@ public class SpawnAction extends AbstractAction implements CommandAction {
         ActionEnvironment env) {
       NestedSet<Artifact> tools = toolsBuilder.build();
 
-      NestedSet<Artifact> inputsAndTools = getInputsAndTools();
+      // Don't call getInputsAndTools - it wouldn't reuse the built set of tools.
+      NestedSet<Artifact> inputsAndTools =
+          NestedSetBuilder.<Artifact>stableOrder()
+              .addTransitive(inputsBuilder.build())
+              .addTransitive(tools)
+              .build();
 
       return createSpawnAction(
           owner,
@@ -786,7 +791,11 @@ public class SpawnAction extends AbstractAction implements CommandAction {
     /**
      * Returns the inputs that the spawn action will depend on. Tools are by definition a subset of
      * the inputs, so they are also present.
+     *
+     * <p>Warning: this calls {@link NestedSetBuilder#build} on both inputs and tools.
      */
+    // TODO(antunesi): Refactor so this method isn't needed. Building new NestedSets on every call
+    // is a memory vulnerability, see b/291063247.
     public NestedSet<Artifact> getInputsAndTools() {
       return NestedSetBuilder.<Artifact>stableOrder()
           .addTransitive(inputsBuilder.build())