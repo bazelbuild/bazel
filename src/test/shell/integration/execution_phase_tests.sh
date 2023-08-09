7a489b17fcdb17577244f7447b4985a28c5fe837
Googler <pcloudy@google.com>
2023-08-09 11:13:08 -0700

    Fix copybara to also transform tests with strange names.
    Working towards: https://github.com/bazelbuild/bazel/issues/18957
    
    PiperOrigin-RevId: 555218827
    Change-Id: I43dd9a581bec70f179c07d98f2f17d1cb4e4252e

--- src/test/shell/integration/execution_phase_tests.sh
+++ src/test/shell/integration/execution_phase_tests.sh
@@ -20,9 +20,9 @@
 
 # --- begin runfiles.bash initialization v3 ---
 # Copy-pasted from the Bazel Bash runfiles library v3.
-set -uo pipefail; set +e; f=io_bazel/tools/bash/runfiles/runfiles.bash
-source "${TEST_SRCDIR:-/dev/null}/$f" 2>/dev/null || \
-  source "$(grep -sm1 "^$f " "${TEST_SRCDIR:-/dev/null}/MANIFEST" | cut -f2- -d' ')" 2>/dev/null || \
+set -uo pipefail; set +e; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
   source "$0.runfiles/$f" 2>/dev/null || \
   source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
   source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
@@ -457,3 +457,4 @@ EOF
 }
 
 run_suite "Integration tests of ${PRODUCT_NAME} using the execution phase."
+
