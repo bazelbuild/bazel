f5050278645140ae61c685c8931c47e2c23af613
Jerry Marino <jmarino@squareup.com>
2023-08-14 11:27:13 -0700

    Revert "Make --incompatible_remote_results_ignore_disk a no-op."
    We'd like to use Bazel 7.0 but will need to have an answer still have to
    rely on this featur in a number of repos.
    https://github.com/bazelbuild/bazel/issues/17428
    
    The main changes we'd need are:
    
    1. to filter out several hundreds of GB from the disk cache which caused
       cache corruption on macOS. #17428 has more info about it
    
    2. When we use this feature with RBE it prevents from uploading
       artifacts that are tagged as `no-remote`.
       https://github.com/bazelbuild/bazel-buildfarm/issues/1402 has the
       issue for buildfarm
    
    ** For RBE it doesn't upload the testrunners in this situation
    ```
    TestRunner "a" -> Bundle "b"
    
    TestRunner remote
    Bundle no-remote-cache
    ```
    
    This reverts commit c88f374246c0a7b3a87c58553d3b9ad0ef4e9ad4.

--- src/test/shell/bazel/remote/remote_build_event_uploader_test.sh
+++ src/test/shell/bazel/remote/remote_build_event_uploader_test.sh
@@ -236,6 +236,7 @@ EOF
   bazel build \
       --remote_cache=grpc://localhost:${worker_port} \
       --disk_cache=$cache_dir \
+      --incompatible_remote_results_ignore_disk \
       --remote_upload_local_results=false \
       --remote_build_event_upload=minimal \
       --build_event_json_file=$BEP_JSON \
@@ -265,6 +266,7 @@ EOF
   bazel build \
       --remote_cache=grpc://localhost:${worker_port} \
       --disk_cache=$cache_dir \
+      --incompatible_remote_results_ignore_disk \
       --remote_upload_local_results=false \
       --remote_build_event_upload=all \
       --build_event_json_file=$BEP_JSON \
