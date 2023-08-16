4eb9f583d9a72086b98b6629812db212c14a9d72
Googler <hvd@google.com>
2023-08-16 04:23:33 -0700

    Automated rollback of commit a95f37bb038f06c2a10660ee242428ff5748ce1e.
    *** Reason for rollback ***
    
    Partially revert to not lose API documentation
    
    PiperOrigin-RevId: 557437461
    Change-Id: I53f393a3ad99bad60c90da87884c2c1af57e0c9d

--- src/main/java/com/google/devtools/build/lib/starlarkbuildapi/java/JavaCommonApi.java
+++ src/main/java/com/google/devtools/build/lib/starlarkbuildapi/java/JavaCommonApi.java
@@ -22,16 +22,19 @@ import com.google.devtools.build.lib.collect.nestedset.Depset;
 import com.google.devtools.build.lib.collect.nestedset.Depset.TypeException;
 import com.google.devtools.build.lib.packages.Info;
 import com.google.devtools.build.lib.packages.RuleClass.ConfiguredTargetFactory.RuleErrorException;
+import com.google.devtools.build.lib.packages.semantics.BuildLanguageOptions;
 import com.google.devtools.build.lib.starlarkbuildapi.FileApi;
 import com.google.devtools.build.lib.starlarkbuildapi.StarlarkActionFactoryApi;
 import com.google.devtools.build.lib.starlarkbuildapi.StarlarkRuleContextApi;
 import com.google.devtools.build.lib.starlarkbuildapi.core.ProviderApi;
+import com.google.devtools.build.lib.starlarkbuildapi.cpp.CcInfoApi;
 import com.google.devtools.build.lib.starlarkbuildapi.platform.ConstraintValueInfoApi;
 import net.starlark.java.annot.Param;
 import net.starlark.java.annot.ParamType;
 import net.starlark.java.annot.StarlarkBuiltin;
 import net.starlark.java.annot.StarlarkMethod;
 import net.starlark.java.eval.EvalException;
+import net.starlark.java.eval.NoneType;
 import net.starlark.java.eval.Sequence;
 import net.starlark.java.eval.StarlarkSemantics;
 import net.starlark.java.eval.StarlarkThread;
@@ -51,6 +54,272 @@ public interface JavaCommonApi<
         StarlarkRuleContextT extends StarlarkRuleContextApi<ConstraintValueT>,
         StarlarkActionFactoryT extends StarlarkActionFactoryApi>
     extends StarlarkValue {
+
+  @StarlarkMethod(
+      name = "compile",
+      doc =
+          "Compiles Java source files/jars from the implementation of a Starlark rule and returns "
+              + "a provider that represents the results of the compilation and can be added to "
+              + "the set of providers emitted by this rule.",
+      parameters = {
+        @Param(name = "ctx", positional = true, named = false, doc = "The rule context."),
+        @Param(
+            name = "source_jars",
+            positional = false,
+            named = true,
+            allowedTypes = {@ParamType(type = Sequence.class, generic1 = FileApi.class)},
+            defaultValue = "[]",
+            doc =
+                "A list of the jars to be compiled. At least one of source_jars or source_files"
+                    + " should be specified."),
+        @Param(
+            name = "source_files",
+            positional = false,
+            named = true,
+            allowedTypes = {@ParamType(type = Sequence.class, generic1 = FileApi.class)},
+            defaultValue = "[]",
+            doc =
+                "A list of the Java source files to be compiled. At least one of source_jars or "
+                    + "source_files should be specified."),
+        @Param(name = "output", positional = false, named = true),
+        @Param(
+            name = "output_source_jar",
+            positional = false,
+            named = true,
+            allowedTypes = {
+              @ParamType(type = FileApi.class),
+              @ParamType(type = NoneType.class),
+            },
+            defaultValue = "None",
+            doc = "The output source jar. Optional. Defaults to `{output_jar}-src.jar` if unset."),
+        @Param(
+            name = "javac_opts",
+            positional = false,
+            named = true,
+            allowedTypes = {@ParamType(type = Sequence.class, generic1 = String.class)},
+            defaultValue = "[]",
+            doc = "A list of the desired javac options. Optional."),
+        @Param(
+            name = "deps",
+            positional = false,
+            named = true,
+            allowedTypes = {@ParamType(type = Sequence.class, generic1 = JavaInfoApi.class)},
+            defaultValue = "[]",
+            doc = "A list of dependencies. Optional."),
+        @Param(
+            name = "runtime_deps",
+            positional = false,
+            named = true,
+            allowedTypes = {@ParamType(type = Sequence.class, generic1 = JavaInfoApi.class)},
+            defaultValue = "[]",
+            doc = "A list of runtime dependencies. Optional."),
+        @Param(
+            name = "exports",
+            positional = false,
+            named = true,
+            allowedTypes = {
+              @ParamType(type = Sequence.class, generic1 = JavaInfoApi.class),
+            },
+            defaultValue = "[]",
+            doc = "A list of exports. Optional."),
+        @Param(
+            name = "plugins",
+            positional = false,
+            named = true,
+            allowedTypes = {
+              @ParamType(type = Sequence.class, generic1 = JavaPluginInfoApi.class),
+              @ParamType(type = Sequence.class, generic1 = JavaInfoApi.class)
+            },
+            defaultValue = "[]",
+            doc = "A list of plugins. Optional."),
+        @Param(
+            name = "exported_plugins",
+            positional = false,
+            named = true,
+            allowedTypes = {
+              @ParamType(type = Sequence.class, generic1 = JavaPluginInfoApi.class),
+              @ParamType(type = Sequence.class, generic1 = JavaInfoApi.class)
+            },
+            defaultValue = "[]",
+            doc = "A list of exported plugins. Optional."),
+        @Param(
+            name = "native_libraries",
+            positional = false,
+            allowedTypes = {@ParamType(type = Sequence.class, generic1 = CcInfoApi.class)},
+            named = true,
+            defaultValue = "[]",
+            doc = "CC native library dependencies that are needed for this library."),
+        @Param(
+            name = "annotation_processor_additional_inputs",
+            positional = false,
+            named = true,
+            allowedTypes = {@ParamType(type = Sequence.class, generic1 = FileApi.class)},
+            defaultValue = "[]",
+            doc =
+                "A list of inputs that the Java compilation action will take in addition to the "
+                    + "Java sources for annotation processing."),
+        @Param(
+            name = "annotation_processor_additional_outputs",
+            positional = false,
+            named = true,
+            allowedTypes = {@ParamType(type = Sequence.class, generic1 = FileApi.class)},
+            defaultValue = "[]",
+            doc =
+                "A list of outputs that the Java compilation action will output in addition to "
+                    + "the class jar from annotation processing."),
+        @Param(
+            name = "strict_deps",
+            defaultValue = "'ERROR'",
+            positional = false,
+            named = true,
+            doc =
+                "A string that specifies how to handle strict deps. Possible values: 'OFF', "
+                    + "'ERROR', 'WARN' and 'DEFAULT'. For more details see "
+                    + "${link user-manual#flag--strict_java_deps}. By default 'ERROR'."),
+        @Param(
+            name = "java_toolchain",
+            positional = false,
+            named = true,
+            doc = "A JavaToolchainInfo to be used for this compilation. Mandatory."),
+        @Param(
+            name = "bootclasspath",
+            positional = false,
+            named = true,
+            defaultValue = "None",
+            doc =
+                "A BootClassPathInfo to be used for this compilation. If present, overrides the"
+                    + " bootclasspath associated with the provided java_toolchain. Optional."),
+        @Param(
+            name = "host_javabase",
+            positional = false,
+            named = true,
+            doc =
+                "Deprecated: You can drop this parameter (host_javabase is provided with "
+                    + "java_toolchain)",
+            defaultValue = "None",
+            disableWithFlag = BuildLanguageOptions.INCOMPATIBLE_JAVA_COMMON_PARAMETERS,
+            valueWhenDisabled = "None"),
+        @Param(
+            name = "sourcepath",
+            positional = false,
+            named = true,
+            allowedTypes = {@ParamType(type = Sequence.class, generic1 = FileApi.class)},
+            defaultValue = "[]"),
+        @Param(
+            name = "resources",
+            positional = false,
+            named = true,
+            allowedTypes = {@ParamType(type = Sequence.class, generic1 = FileApi.class)},
+            defaultValue = "[]"),
+        @Param(
+            name = "resource_jars",
+            positional = false,
+            named = true,
+            allowedTypes = {@ParamType(type = Sequence.class, generic1 = FileApi.class)},
+            defaultValue = "[]"),
+        @Param(
+            name = "classpath_resources",
+            positional = false,
+            named = true,
+            allowedTypes = {@ParamType(type = Sequence.class, generic1 = FileApi.class)},
+            defaultValue = "[]"),
+        @Param(name = "neverlink", positional = false, named = true, defaultValue = "False"),
+        @Param(
+            name = "enable_annotation_processing",
+            positional = false,
+            named = true,
+            defaultValue = "True",
+            doc =
+                "Disables annotation processing in this compilation, causing any annotation"
+                    + " processors provided in plugins or in exported_plugins of deps to be"
+                    + " ignored."),
+        @Param(
+            name = "enable_compile_jar_action",
+            positional = false,
+            named = true,
+            defaultValue = "True",
+            doc =
+                "Enables header compilation or ijar creation. If set to False, it forces use of the"
+                    + " full class jar in the compilation classpaths of any dependants. Doing so is"
+                    + " intended for use by non-library targets such as binaries that do not have"
+                    + " dependants."),
+        @Param(
+            name = "enable_jspecify",
+            positional = false,
+            named = true,
+            defaultValue = "True",
+            documented = false),
+        @Param(
+            name = "include_compilation_info",
+            positional = false,
+            named = true,
+            defaultValue = "True",
+            documented = false),
+        @Param(
+            name = "injecting_rule_kind",
+            documented = false,
+            positional = false,
+            named = true,
+            defaultValue = "None",
+            allowedTypes = {
+              @ParamType(type = String.class),
+              @ParamType(type = NoneType.class),
+            }),
+        @Param(
+            name = "add_exports",
+            positional = false,
+            named = true,
+            allowedTypes = {@ParamType(type = Sequence.class, generic1 = String.class)},
+            defaultValue = "[]",
+            doc = "Allow this library to access the given <module>/<package>. Optional."),
+        @Param(
+            name = "add_opens",
+            positional = false,
+            named = true,
+            allowedTypes = {@ParamType(type = Sequence.class, generic1 = String.class)},
+            defaultValue = "[]",
+            doc =
+                "Allow this library to reflectively access the given <module>/<package>."
+                    + " Optional."),
+      },
+      useStarlarkThread = true)
+  default JavaInfoT createJavaCompileAction(
+      StarlarkRuleContextT starlarkRuleContext,
+      Sequence<?> sourceJars, // <FileT> expected.
+      Sequence<?> sourceFiles, // <FileT> expected.
+      FileT outputJar,
+      Object outputSourceJar,
+      Sequence<?> javacOpts, // <String> expected.
+      Sequence<?> deps, // <JavaInfoT> expected.
+      Sequence<?> runtimeDeps, // <JavaInfoT> expected.
+      Sequence<?> exports, // <JavaInfoT> expected.
+      Sequence<?> plugins, // <JavaInfoT> expected.
+      Sequence<?> exportedPlugins, // <JavaInfoT> expected.
+      Sequence<?> nativeLibraries, // <CcInfoT> expected.
+      Sequence<?> annotationProcessorAdditionalInputs, // <FileT> expected.
+      Sequence<?> annotationProcessorAdditionalOutputs, // <FileT> expected.
+      String strictDepsMode,
+      JavaToolchainT javaToolchain,
+      Object bootClassPath,
+      Object hostJavabase,
+      Sequence<?> sourcepathEntries, // <FileT> expected.
+      Sequence<?> resources, // <FileT> expected.
+      Sequence<?> resourceJars, // <FileT> expected.
+      Sequence<?> classpathResources, // <FileT> expected.
+      Boolean neverlink,
+      Boolean enableAnnotationProcessing,
+      Boolean enableCompileJarAction,
+      Boolean enableJSpecify,
+      boolean includeCompilationInfo,
+      Object injectingRuleKind,
+      Sequence<?> addExports, // <String> expected.
+      Sequence<?> addOpens, // <String> expected.
+      StarlarkThread thread)
+      throws EvalException, InterruptedException, RuleErrorException, LabelSyntaxException {
+    // this will never be invoked now that only the Starlark implementation in `@_builtins` is used.
+    // this method declaration exists purely for documentation.
+    throw new UnsupportedOperationException();
+  }
 
   @StarlarkMethod(
       name = "create_header_compilation_action",
