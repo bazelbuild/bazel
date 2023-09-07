fe5345139b498ad78d1f9171c5dd52a08064ea5b
Googler <noreply@google.com>
2023-09-06 22:37:43 -0700

    Automatic code cleanup.
    PiperOrigin-RevId: 563316903
    Change-Id: Ic028b73f5a8ec8db4913642b97e977ec4d19de56

--- src/test/java/com/google/devtools/build/lib/bazel/bzlmod/BzlmodTestUtil.java
+++ src/test/java/com/google/devtools/build/lib/bazel/bzlmod/BzlmodTestUtil.java
@@ -336,11 +336,6 @@ public final class BzlmodTestUtil {
       return this;
     }
 
-    public TestTagBuilder setLocation(String file, int row, int column) {
-      location = Location.fromFileLineColumn(file, row, column);
-      return this;
-    }
-
     public Tag build() {
       return Tag.builder()
           .setTagName(tagName)
