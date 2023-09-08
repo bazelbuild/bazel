bazelbuild/bazel

commit 91d2c114df356b70754683482317fa8e15d084b6

Do not put scheduling dependencies into AllInputs.
This had two negative side effects:
- The nested set unwrapping optimization in ActionExecutionFunction.getInputDepKeys() was negated: .getAllInputs() seems to always wrap defaultInputs into another nested set, but NestedSetBuilder has an optimization that elides the extra instance if it has only one transitive nested set and no direct members (which is the case for C++ actions that are not locally cached) and non-ephemeral nested sets benefit from memoization.
- All C++ headers in the transitive closure of C++ rules were interred into ActionInputMap (see .accumulateInputs()), which resulted in a lot of GC churn.

RELNOTES: None.
PiperOrigin-RevId: 562561387
Change-Id: Ib5c81a61be9b5cf6c3ee0c4884ab894ec45c24e1

src/main/java/com/google/devtools/build/lib/skyframe/ActionExecutionFunction.java
@@ -269,7 +269,8 @@ public final class ActionExecutionFunction implements SkyFunction {
     }
 
     if (!state.hasArtifactData()) {
-      Iterable<SkyKey> depKeys = getInputDepKeys(allInputs, state);
+      Iterable<SkyKey> depKeys =
+          getInputDepKeys(allInputs, action.getSchedulingDependencies(), state);
       SkyframeLookupResult inputDeps = env.getValuesAndExceptions(depKeys);
       if (previousExecution == null) {
         // Do we actually need to find our metadata?
@@ -342,7 +343,7 @@ public final class ActionExecutionFunction implements SkyFunction {
           actionStartTime,
           env,
           allInputs,
-          getInputDepKeys(allInputs, state),
+          getInputDepKeys(allInputs, action.getSchedulingDependencies(), state),
           state);
     } catch (ActionExecutionException e) {
       // In this case we do not report the error to the action reporter because we have already
@@ -363,7 +364,9 @@ public final class ActionExecutionFunction implements SkyFunction {
   }
 
   private static Iterable<SkyKey> getInputDepKeys(
-      NestedSet<Artifact> allInputs, InputDiscoveryState state) {
+      NestedSet<Artifact> allInputs,
+      NestedSet<Artifact> schedulingDependencies,
+      InputDiscoveryState state) {
     // We "unwrap" the NestedSet and evaluate the first layer of direct Artifacts here in order to
     // save memory:
     // - This top layer costs 1 extra ArtifactNestedSetKey node.
@@ -372,10 +375,14 @@ public final class ActionExecutionFunction implements SkyFunction {
     // More details: b/143205147.
     List<SkyKey> directKeys = Artifact.keys(allInputs.getLeaves());
     if (state.requestedArtifactNestedSetKeys == null) {
-      state.requestedArtifactNestedSetKeys =
-          CompactHashSet.create(
-              Lists.transform(allInputs.getNonLeaves(), ArtifactNestedSetKey::create));
+      state.requestedArtifactNestedSetKeys = CompactHashSet.create();
+      for (NestedSet<Artifact> nonLeaf : allInputs.getNonLeaves()) {
+        state.requestedArtifactNestedSetKeys.add(ArtifactNestedSetKey.create(nonLeaf));
+      }
+
+      state.requestedArtifactNestedSetKeys.add(ArtifactNestedSetKey.create(schedulingDependencies));
     }
+
     return Iterables.concat(directKeys, state.requestedArtifactNestedSetKeys);
   }
 
@@ -578,7 +585,6 @@ public final class ActionExecutionFunction implements SkyFunction {
     }
     return new AllInputs(
         allKnownInputs,
-        action.getSchedulingDependencies(),
         actionCacheInputs,
         action.getAllowedDerivedInputs(),
         resolver.packageLookupsRequested);
@@ -586,14 +592,12 @@ public final class ActionExecutionFunction implements SkyFunction {
 
   static class AllInputs {
     final NestedSet<Artifact> defaultInputs;
-    final NestedSet<Artifact> schedulingDependencies;
     @Nullable final NestedSet<Artifact> allowedDerivedInputs;
     @Nullable final List<Artifact> actionCacheInputs;
     @Nullable final List<ContainingPackageLookupValue.Key> packageLookupsRequested;
 
     AllInputs(NestedSet<Artifact> defaultInputs) {
       this.defaultInputs = checkNotNull(defaultInputs);
-      this.schedulingDependencies = NestedSetBuilder.emptySet(Order.STABLE_ORDER);
       this.actionCacheInputs = null;
       this.allowedDerivedInputs = null;
       this.packageLookupsRequested = null;
@@ -601,12 +605,10 @@ public final class ActionExecutionFunction implements SkyFunction {
 
     AllInputs(
         NestedSet<Artifact> defaultInputs,
-        NestedSet<Artifact> schedulingDependencies,
         List<Artifact> actionCacheInputs,
         NestedSet<Artifact> allowedDerivedInputs,
         List<ContainingPackageLookupValue.Key> packageLookupsRequested) {
       this.defaultInputs = checkNotNull(defaultInputs);
-      this.schedulingDependencies = checkNotNull(schedulingDependencies);
       this.allowedDerivedInputs = checkNotNull(allowedDerivedInputs);
       this.actionCacheInputs = checkNotNull(actionCacheInputs);
       this.packageLookupsRequested = packageLookupsRequested;
@@ -623,7 +625,6 @@ public final class ActionExecutionFunction implements SkyFunction {
     NestedSet<Artifact> getAllInputs(boolean prune) {
       NestedSetBuilder<Artifact> builder = new NestedSetBuilder<>(Order.STABLE_ORDER);
       builder.addTransitive(defaultInputs);
-      builder.addTransitive(schedulingDependencies);
 
       if (actionCacheInputs == null) {
         return builder.build();

commit d57e0a71008c49594872160647756c87f63f5fa1

Fix memory regression caused by https://github.com/bazelbuild/bazel/commit/91d2c114df356b70754683482317fa8e15d084b6 .
That change added a dependency on the a nested set that was almost always empty, which resulted an unnecessary and useless Skyframe dependency edge for many actions.

The fix is to check whether the nested set is empty before adding a dependency on it.

Naively, one would think that we could do the same thing with `nonLeaf` above, but `NestedSetBuilder.addTransitive()` already filters out empty nested sets so doing the same emptiness check for `nonLeaf` would be useless.

RELNOTES: None.
PiperOrigin-RevId: 563662609
Change-Id: I4ea3b250f75b9fad8e7532e6e0675469eccb220f

src/main/java/com/google/devtools/build/lib/skyframe/ActionExecutionFunction.java
@@ -380,7 +380,10 @@ public final class ActionExecutionFunction implements SkyFunction {
         state.requestedArtifactNestedSetKeys.add(ArtifactNestedSetKey.create(nonLeaf));
       }
 
-      state.requestedArtifactNestedSetKeys.add(ArtifactNestedSetKey.create(schedulingDependencies));
+      if (!schedulingDependencies.isEmpty()) {
+        state.requestedArtifactNestedSetKeys.add(
+            ArtifactNestedSetKey.create(schedulingDependencies));
+      }
     }
 
     return Iterables.concat(directKeys, state.requestedArtifactNestedSetKeys);