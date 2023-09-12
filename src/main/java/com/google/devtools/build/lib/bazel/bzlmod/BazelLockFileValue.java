0d70b76f37d19ecf3b77df0c1595ef16ca327050
Yun Peng <pcloudy@google.com>
2023-09-12 11:55:19 +0200

    [6.4.0] Cherry pick Bzlmod fixes (#19494)
    Cherry-picked commits:
    
    - 15b1575cff9ad6e7f22d49cf7df2c7f2a3015f53 
    - acb38a619261379ef5a6b5ccd472072e273019a8
    - 07e0d316a345a3cb2593f98525320590bbc56e30
    
    ---------
    
    Co-authored-by: salma-samy <salmasamy@google.com>

--- src/main/java/com/google/devtools/build/lib/bazel/bzlmod/BazelLockFileValue.java
+++ src/main/java/com/google/devtools/build/lib/bazel/bzlmod/BazelLockFileValue.java
@@ -92,6 +92,11 @@ public abstract class BazelLockFileValue implements SkyValue, Postable {
       ImmutableMap<String, String> localOverrideHashes,
       BzlmodFlagsAndEnvVars flags) {
     ImmutableList.Builder<String> moduleDiff = new ImmutableList.Builder<>();
+    if (getLockFileVersion() != BazelLockFileValue.LOCK_FILE_VERSION) {
+      return moduleDiff
+          .add("the version of the lockfile is not compatible with the current Bazel")
+          .build();
+    }
     if (!moduleFileHash.equals(getModuleFileHash())) {
       moduleDiff.add("the root MODULE.bazel has been modified");
     }
