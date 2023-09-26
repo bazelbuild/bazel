c1c12e831b0c28ebeceaf7f217c4caf7835a5b92
Googler <lberki@google.com>
2023-09-26 05:34:53 -0700

    Remove the CallCounts module.
    RELNOTES[INC]: The --call_count_output_path command line option is not supported anymore.
    
    PiperOrigin-RevId: 568509080
    Change-Id: I7b95cd1a736bf6c967f11bf5d61d0dbd1c03d9a0

--- src/main/java/com/google/devtools/build/lib/bazel/Bazel.java
+++ src/main/java/com/google/devtools/build/lib/bazel/Bazel.java
@@ -75,7 +75,6 @@ public final class Bazel {
           com.google.devtools.build.lib.bazel.rules.BazelStrategyModule.class,
           com.google.devtools.build.lib.network.NoOpConnectivityModule.class,
           com.google.devtools.build.lib.buildeventservice.BazelBuildEventServiceModule.class,
-          com.google.devtools.build.lib.profiler.callcounts.CallcountsModule.class,
           com.google.devtools.build.lib.profiler.memory.AllocationTrackerModule.class,
           com.google.devtools.build.lib.profiler.CommandProfilerModule.class,
           com.google.devtools.build.lib.metrics.PostGCMemoryUseRecorder
