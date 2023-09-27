e1eef7c9dd0499aa1a311b009d803eb1a621b04c
Googler <lberki@google.com>
2023-09-27 05:39:41 -0700

    Remove the no-op --incompatible_avoid_conflict_dlls command line option.
    RELNOTES[INC]: THe --incompatible_avoid_conflict_dlls command line option is not available anymore.
    
    PiperOrigin-RevId: 568822990
    Change-Id: Ibc8d2a0378ea0aced8be7a6e3328654a38c5bbb8

--- src/test/py/bazel/bazel_windows_cpp_test.py
+++ src/test/py/bazel/bazel_windows_cpp_test.py
@@ -283,9 +283,7 @@ class BazelWindowsCppTest(test_base.TestBase):
     self.ScratchFile('main/other_main.cc', ['int main() {return 0;}'])
 
     # Building //main:main should succeed
-    self.RunBazel(
-        ['build', '//main:main', '--incompatible_avoid_conflict_dlls']
-    )
+    self.RunBazel(['build', '//main:main'])
     main_bin = os.path.join(bazel_bin, 'main/main.exe')
 
     # Run the main_bin binary to see if it runs successfully
@@ -304,7 +302,6 @@ class BazelWindowsCppTest(test_base.TestBase):
         'build',
         '//main:main',
         '//main:other_main',
-        '--incompatible_avoid_conflict_dlls',
     ])
     other_main_bin = os.path.join(bazel_bin, 'main/other_main.exe')
 
