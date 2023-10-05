70fc6e489264bd9dd6fedb162d77dd8180c7624b
Googler <jcater@google.com>
2023-10-05 08:57:59 -0700

    All uses of FakeAttributeMapper use the empty mapper, so remove all functionality.
    PiperOrigin-RevId: 571033335
    Change-Id: I07124d656960ca729f8a02f752509bdb3714f5a4

--- src/test/java/com/google/devtools/build/lib/testutil/FakeAttributeMapper.java
+++ src/test/java/com/google/devtools/build/lib/testutil/FakeAttributeMapper.java
@@ -13,27 +13,24 @@
 // limitations under the License.
 package com.google.devtools.build.lib.testutil;
 
-import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 
-import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import com.google.devtools.build.lib.cmdline.Label;
 import com.google.devtools.build.lib.packages.Attribute;
 import com.google.devtools.build.lib.packages.AttributeMap;
 import com.google.devtools.build.lib.packages.DependencyFilter;
 import com.google.devtools.build.lib.packages.PackageArgs;
 import com.google.devtools.build.lib.packages.Type;
-import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import javax.annotation.Nullable;
 
 /** Faked implementation of {@link AttributeMap} for use in testing. */
 public class FakeAttributeMapper implements AttributeMap {
-  private final Map<String, FakeAttributeMapperEntry<?>> attrs;
 
-  private FakeAttributeMapper(Map<String, FakeAttributeMapperEntry<?>> attrs) {
-    this.attrs = ImmutableMap.copyOf(attrs);
+  public static FakeAttributeMapper empty() {
+    return new FakeAttributeMapper();
   }
 
   @Override
@@ -43,29 +40,20 @@ public class FakeAttributeMapper implements AttributeMap {
 
   @Override
   public boolean has(String attrName) {
-    return attrs.containsKey(attrName);
+    return false;
   }
 
   @Override
   public <T> boolean has(String attrName, Type<T> type) {
-    FakeAttributeMapperEntry<?> entry = attrs.get(attrName);
-    if (entry == null) {
-      return false;
-    }
-    return entry.type.equals(type);
+    return false;
   }
 
   @Override
   @Nullable
   public <T> T get(String attributeName, Type<T> type) {
-    FakeAttributeMapperEntry<?> entry = attrs.get(attributeName);
-    if (entry == null) {
       // Not specified in attributes or defaults
       assertWithMessage("Attribute " + attributeName + " not in attributes!").fail();
       return null;
-    }
-
-    return entry.validateAndGet(type);
   }
 
   @Override
@@ -75,14 +63,13 @@ public class FakeAttributeMapper implements AttributeMap {
 
   @Override
   public Iterable<String> getAttributeNames() {
-    return attrs.keySet();
+    return ImmutableSet.of();
   }
 
   @Nullable
   @Override
   public Type<?> getAttributeType(String attrName) {
-    FakeAttributeMapperEntry<?> entry = attrs.get(attrName);
-    return entry == null ? null : entry.type;
+    return null;
   }
 
   @Nullable
@@ -93,7 +80,7 @@ public class FakeAttributeMapper implements AttributeMap {
 
   @Override
   public boolean isAttributeValueExplicitlySpecified(String attributeName) {
-    return attrs.containsKey(attributeName);
+    return false;
   }
 
   @Override
@@ -108,43 +95,5 @@ public class FakeAttributeMapper implements AttributeMap {
   @Override
   public PackageArgs getPackageArgs() {
     return PackageArgs.DEFAULT;
-  }
-
-  public static FakeAttributeMapper empty() {
-    return builder().build();
-  }
-
-  public static Builder builder() {
-    return new Builder();
-  }
-
-  /**
-   * Builder to construct a {@link FakeAttributeMapper}. If no attributes are needed, use {@link
-   * #empty()} instead.
-   */
-  public static class Builder {
-    private final ImmutableMap.Builder<String, FakeAttributeMapperEntry<?>> mapBuilder =
-        ImmutableMap.builder();
-
-    private Builder() {}
-
-    public FakeAttributeMapper build() {
-      return new FakeAttributeMapper(mapBuilder.buildOrThrow());
-    }
-  }
-
-  private static class FakeAttributeMapperEntry<T> {
-    private final Type<T> type;
-    private final T value;
-
-    private FakeAttributeMapperEntry(Type<T> type, T value) {
-      this.type = type;
-      this.value = value;
-    }
-
-    private <U> U validateAndGet(Type<U> otherType) {
-      assertThat(type).isSameInstanceAs(otherType);
-      return otherType.cast(value);
-    }
   }
 }
