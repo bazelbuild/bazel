790169a324f3c3396f83e99f4bf5cb479e781522
Fabian Meumertzheim <fabian@meumertzhe.im>
2023-10-05 09:06:28 +0200

    Address review comments

--- src/main/java/com/google/devtools/build/lib/analysis/actions/StrippingPathMapper.java
+++ src/main/java/com/google/devtools/build/lib/analysis/actions/StrippingPathMapper.java
@@ -176,11 +176,6 @@ public final class StrippingPathMapper {
         }
         return MapFn.DEFAULT;
       }
-
-      @Override
-      public Object cacheKey() {
-        return GUID;
-      }
     };
   }
 
