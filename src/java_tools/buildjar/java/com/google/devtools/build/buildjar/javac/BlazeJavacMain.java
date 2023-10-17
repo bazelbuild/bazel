3ff7c32e618c3a2a8e93c14a812eabd1d6f23fef
Googler <cushon@google.com>
2023-10-17 15:11:45 -0700

    Remove a reference to an obsolete class in BlazeJavacMain
    PiperOrigin-RevId: 574278011
    Change-Id: Ifa5b10031ef3a1435dfd54b16c66bc7a4e46388c

--- src/java_tools/buildjar/java/com/google/devtools/build/buildjar/javac/BlazeJavacMain.java
+++ src/java_tools/buildjar/java/com/google/devtools/build/buildjar/javac/BlazeJavacMain.java
@@ -457,7 +457,6 @@ public class BlazeJavacMain {
                   || name.startsWith("com.google.common.base.")
                   || name.startsWith("com.google.common.graph.")
                   || name.startsWith("com.google.common.regex.")
-                  || name.startsWith("org.checkerframework.shaded.dataflow.")
                   || name.startsWith("org.checkerframework.errorprone.dataflow.")
                   || name.startsWith("com.sun.source.")
                   || name.startsWith("com.sun.tools.")
