bazelbuild/bazel

commit 19c0c809abbe4bc70d3d6b493ff966dd41c54768

Ensure lockfile is updated after reset to pre-build state
The events that update the lockfile are important for incremental correctness and thus need to return `true` from `storeForReplay`.

Before this change, if a build creates the lockfile because it didn't exist and the lockfile is then deleted, subsequent builds did not regenerate it as the relevant SkyFunctions wouldn't rerun and the events were not replayed.

Closes #19343.

PiperOrigin-RevId: 561287438
Change-Id: I549f99b896a0095e8ffc35b7bacc8a841a44219a

src/main/java/com/google/devtools/build/lib/bazel/bzlmod/BazelModuleResolutionEvent.java
@@ -36,4 +36,9 @@ public abstract class BazelModuleResolutionEvent implements Postable {
 
   public abstract ImmutableTable<ModuleExtensionId, ModuleKey, ModuleExtensionUsage>
       getExtensionUsagesById();
+
+  @Override
+  public boolean storeForReplay() {
+    return true;
+  }
 }

src/main/java/com/google/devtools/build/lib/bazel/bzlmod/ModuleExtensionResolutionEvent.java
@@ -33,4 +33,9 @@ public abstract class ModuleExtensionResolutionEvent implements Postable {
   public abstract ModuleExtensionId getExtensionId();
 
   public abstract LockFileModuleExtension getModuleExtension();
+
+  @Override
+  public boolean storeForReplay() {
+    return true;
+  }
 }

src/test/py/bazel/bzlmod/bazel_lockfile_test.py
@@ -964,6 +964,48 @@ class BazelLockfileTest(test_base.TestBase):
         stderr,
     )
 
+  def testLockfileRecreatedAfterDeletion(self):
+    self.ScratchFile(
+        'MODULE.bazel',
+        [
+            'lockfile_ext = use_extension("extension.bzl", "lockfile_ext")',
+            'use_repo(lockfile_ext, "hello")',
+        ],
+    )
+    self.ScratchFile('BUILD.bazel')
+    self.ScratchFile(
+        'extension.bzl',
+        [
+            'def _repo_rule_impl(ctx):',
+            '    ctx.file("WORKSPACE")',
+            '    ctx.file("BUILD", "filegroup(name=\'lala\')")',
+            '',
+            'repo_rule = repository_rule(implementation=_repo_rule_impl)',
+            '',
+            'def _module_ext_impl(ctx):',
+            '    repo_rule(name="hello")',
+            '',
+            'lockfile_ext = module_extension(',
+            '    implementation=_module_ext_impl,',
+            ')',
+        ],
+    )
+
+    self.RunBazel(['build', '@hello//:all'])
+
+    # Return the lockfile to the state it had before the
+    # previous build: it didn't exist.
+    with open('MODULE.bazel.lock', 'r') as lock_file:
+      old_data = lock_file.read()
+    os.remove('MODULE.bazel.lock')
+
+    self.RunBazel(['build', '@hello//:all'])
+
+    with open('MODULE.bazel.lock', 'r') as lock_file:
+      new_data = lock_file.read()
+
+    self.assertEqual(old_data, new_data)
+
 
 if __name__ == '__main__':
   unittest.main()

commit aa6ce28e5a8b5bd78c0e496c9180a843c9881fc2

Fix analysis time regression with Bzlmod lockfile
This commit fixes a regression in analysis time caused by 19c0c809abbe4bc70d3d6b493ff966dd41c54768.
Since `BazelModuleResolutionEvent` and `ModuleExtensionResolutionEvent`
are both marked as `storeForReplay`, they are stored in a nested set for
essentially all analysis phase nodes. This resulted in frequent (i.e.,
per target) calls to their `hashCode` methods, which are not cached and
delegated to the likewise uncached methods on large `ImmutableMap` and
`ImmutableTable` instances.

Since there is no need for these events to be deduplicated, switching to
reference equality resolves this issue.

The following analysis phase measurements for a synthetic project with
a single "hub and spokes" module extension and 2,000 repos illustrate
the effect:

* without lockfile: 4.3s
* with lockfile before this commit: 8.3s
* with lockfile after this commit: 4.2s

src/main/java/com/google/devtools/build/lib/bazel/bzlmod/BazelModuleResolutionEvent.java
@@ -14,7 +14,6 @@
 
 package com.google.devtools.build.lib.bazel.bzlmod;
 
-import com.google.auto.value.AutoValue;
 import com.google.common.collect.ImmutableTable;
 import com.google.devtools.build.lib.events.ExtendedEventHandler.Postable;
 
@@ -22,20 +21,39 @@ import com.google.devtools.build.lib.events.ExtendedEventHandler.Postable;
  * After resolving bazel module this event is sent from {@link BazelDepGraphFunction} holding the
  * lockfile value with module updates, and the module extension usages. It will be received in
  * {@link BazelLockFileModule} to be used to update the lockfile content
+ *
+ * <p>Instances of this class are retained in Skyframe nodes and subject to frequent {@link
+ * java.util.Set}-based deduplication. As such, it <b>must</b> have a cheap implementation of {@link
+ * #hashCode()} and {@link #equals(Object)}. It currently uses reference equality since the logic
+ * that creates instances of this class already ensures that there is only one instance per build.
  */
-@AutoValue
-public abstract class BazelModuleResolutionEvent implements Postable {
+public final class BazelModuleResolutionEvent implements Postable {
+
+  private final BazelLockFileValue lockfileValue;
+  private final ImmutableTable<ModuleExtensionId, ModuleKey, ModuleExtensionUsage>
+      extensionUsagesById;
+
+  private BazelModuleResolutionEvent(
+      BazelLockFileValue lockfileValue,
+      ImmutableTable<ModuleExtensionId, ModuleKey, ModuleExtensionUsage> extensionUsagesById) {
+    this.lockfileValue = lockfileValue;
+    this.extensionUsagesById = extensionUsagesById;
+  }
 
   public static BazelModuleResolutionEvent create(
       BazelLockFileValue lockFileValue,
       ImmutableTable<ModuleExtensionId, ModuleKey, ModuleExtensionUsage> extensionUsagesById) {
-    return new AutoValue_BazelModuleResolutionEvent(lockFileValue, extensionUsagesById);
+    return new BazelModuleResolutionEvent(lockFileValue, extensionUsagesById);
   }
 
-  public abstract BazelLockFileValue getLockfileValue();
+  public BazelLockFileValue getLockfileValue() {
+    return lockfileValue;
+  }
 
-  public abstract ImmutableTable<ModuleExtensionId, ModuleKey, ModuleExtensionUsage>
-      getExtensionUsagesById();
+  public ImmutableTable<ModuleExtensionId, ModuleKey, ModuleExtensionUsage>
+      getExtensionUsagesById() {
+    return extensionUsagesById;
+  }
 
   @Override
   public boolean storeForReplay() {

src/main/java/com/google/devtools/build/lib/bazel/bzlmod/ModuleExtensionResolutionEvent.java
@@ -14,30 +14,53 @@
 
 package com.google.devtools.build.lib.bazel.bzlmod;
 
-import com.google.auto.value.AutoValue;
 import com.google.devtools.build.lib.events.ExtendedEventHandler.Postable;
 
 /**
  * After evaluating any module extension this event is sent from {@link SingleExtensionEvalFunction}
  * holding the extension id and the resolution data LockFileModuleExtension. It will be received in
- * {@link BazelLockFileModule} to be used to update the lockfile content
+ * {@link BazelLockFileModule} to be used to update the lockfile content.
+ *
+ * <p>Instances of this class are retained in Skyframe nodes and subject to frequent {@link
+ * java.util.Set}-based deduplication. As such, it <b>must</b> have a cheap implementation of {@link
+ * #hashCode()} and {@link #equals(Object)}. It currently uses reference equality since the logic
+ * that creates instances of this class already ensures that there is only one instance per
+ * extension id.
  */
-@AutoValue
-public abstract class ModuleExtensionResolutionEvent implements Postable {
+public final class ModuleExtensionResolutionEvent implements Postable {
+
+  private final ModuleExtensionId extensionId;
+  private final ModuleExtensionEvalFactors extensionFactors;
+  private final LockFileModuleExtension moduleExtension;
+
+  private ModuleExtensionResolutionEvent(
+      ModuleExtensionId extensionId,
+      ModuleExtensionEvalFactors extensionFactors,
+      LockFileModuleExtension moduleExtension) {
+    this.extensionId = extensionId;
+    this.extensionFactors = extensionFactors;
+    this.moduleExtension = moduleExtension;
+  }
 
   public static ModuleExtensionResolutionEvent create(
       ModuleExtensionId extensionId,
       ModuleExtensionEvalFactors extensionFactors,
       LockFileModuleExtension lockfileModuleExtension) {
-    return new AutoValue_ModuleExtensionResolutionEvent(
+    return new ModuleExtensionResolutionEvent(
         extensionId, extensionFactors, lockfileModuleExtension);
   }
 
-  public abstract ModuleExtensionId getExtensionId();
+  public ModuleExtensionId getExtensionId() {
+    return extensionId;
+  }
 
-  public abstract ModuleExtensionEvalFactors getExtensionFactors();
+  public ModuleExtensionEvalFactors getExtensionFactors() {
+    return extensionFactors;
+  }
 
-  public abstract LockFileModuleExtension getModuleExtension();
+  public LockFileModuleExtension getModuleExtension() {
+    return moduleExtension;
+  }
 
   @Override
   public boolean storeForReplay() {

src/main/java/com/google/devtools/build/lib/events/Reportable.java
@@ -47,6 +47,9 @@ public interface Reportable {
    *
    * <p>This method is not relevant for events which do not originate from {@link
    * com.google.devtools.build.skyframe.SkyFunction} evaluation.
+   *
+   * <p>Classes returning {@code true} should have cheap {@link Object#hashCode()} and {@link
+   * Object#equals(Object)} implementations.
    */
   default boolean storeForReplay() {
     return false;