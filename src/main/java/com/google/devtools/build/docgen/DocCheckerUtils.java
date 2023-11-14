1f9b48bfa1593a09506108b40a5ae50d28a16d1e
Chris Gray <cgray@ftsinc.com>
2023-11-14 12:04:36 -0800

    Change the regular expression used in the doc test to exclude tags found in backticks.

--- src/main/java/com/google/devtools/build/docgen/DocCheckerUtils.java
+++ src/main/java/com/google/devtools/build/docgen/DocCheckerUtils.java
@@ -36,8 +36,8 @@ public class DocCheckerUtils {
       + ">",
       Pattern.CASE_INSENSITIVE);
 
-  private static final Pattern COMMENT_PATTERN = Pattern.compile(
-      "<!--.*?-->",
+  private static final Pattern COMMENT_OR_BACKTICK_PATTERN = Pattern.compile(
+      "<!--.*?-->|`.*`",
       Pattern.CASE_INSENSITIVE);
 
   /**
@@ -57,7 +57,7 @@ public class DocCheckerUtils {
   // TODO(bazel-team): run this on the Starlark docs too.
   @Nullable
   private static String getFirstUnclosedTag(String src, boolean printHelp) {
-    Matcher commentMatcher = COMMENT_PATTERN.matcher(src);
+    Matcher commentMatcher = COMMENT_OR_BACKTICK_PATTERN.matcher(src);
     src = commentMatcher.replaceAll("");
     Matcher tagMatcher = TAG_PATTERN.matcher(src);
     Deque<String> tagStack = new ArrayDeque<>();
