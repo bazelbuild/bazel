f3cf5c2e6cffc00f994f70214c66aba169793a21
Googler <tjgq@google.com>
2023-11-27 02:25:54 -0800

    [7.0.0] Consider each runfiles tree to be a distinct node when walking the spawn inputs.
    This is a similar idea to https://github.com/bazelbuild/bazel/commit/0f55d124c05ee2654c3ff56e0332e71ff1eb437c, where we did the same for tree artifacts.
    
    Currently, the sole beneficiary of this change is the Merkle tree cache, which becomes more efficient in the presence of runfiles trees shared by multiple actions. In the future, we might reuse this code path to produce a more compact execution log.
    
    PiperOrigin-RevId: 585582119
    Change-Id: I2caec62eb26cab6dc2530889ef075acc1d9e93d9

--- src/test/java/com/google/devtools/build/lib/remote/RemoteExecutionServiceTest.java
+++ src/test/java/com/google/devtools/build/lib/remote/RemoteExecutionServiceTest.java
@@ -14,6 +14,7 @@ package com.google.devtools.build.lib.remote;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.google.common.collect.ImmutableMap.toImmutableMap;
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.extensions.proto.ProtoTruth.assertThat;
 import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
@@ -65,16 +66,17 @@ import com.google.devtools.build.lib.actions.ActionUploadStartedEvent;
 import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.actions.ArtifactRoot;
 import com.google.devtools.build.lib.actions.ArtifactRoot.RootType;
-import com.google.devtools.build.lib.actions.EmptyRunfilesSupplier;
 import com.google.devtools.build.lib.actions.ExecutionRequirements;
 import com.google.devtools.build.lib.actions.PathMapper;
 import com.google.devtools.build.lib.actions.ResourceSet;
+import com.google.devtools.build.lib.actions.RunfilesSupplier;
 import com.google.devtools.build.lib.actions.SimpleSpawn;
 import com.google.devtools.build.lib.actions.Spawn;
 import com.google.devtools.build.lib.actions.SpawnResult;
 import com.google.devtools.build.lib.actions.SpawnResult.Status;
 import com.google.devtools.build.lib.actions.StaticInputMetadataProvider;
 import com.google.devtools.build.lib.actions.util.ActionsTestUtil;
+import com.google.devtools.build.lib.analysis.config.BuildConfigurationValue.RunfileSymlinksMode;
 import com.google.devtools.build.lib.clock.JavaClock;
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
 import com.google.devtools.build.lib.collect.nestedset.NestedSetBuilder;
@@ -118,6 +120,8 @@ import com.google.protobuf.ByteString;
 import com.google.testing.junit.testparameterinjector.TestParameter;
 import com.google.testing.junit.testparameterinjector.TestParameterInjector;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
@@ -2051,16 +2055,6 @@ public class RemoteExecutionServiceTest {
     //       cannot Mockito.spy on NestedSet as it is final.
 
     // arrange
-    /*
-     * First:
-     *   /bar/file
-     *   /foo1/file
-     * Second:
-     *   /bar/file
-     *   /foo2/file
-     */
-
-    // arrange
     // Single node NestedSets are folded, so always add a dummy file everywhere.
     ActionInput dummyFile = ActionInputHelper.fromPath("file");
     fakeFileCache.createScratchInput(dummyFile, "file");
@@ -2097,24 +2091,34 @@ public class RemoteExecutionServiceTest {
             .addTransitive(nodeFoo2)
             .build();
 
+    Artifact toolDat = ActionsTestUtil.createArtifact(artifactRoot, "tool.dat");
+    fakeFileCache.createScratchInput(toolDat, "tool.dat");
+
+    RunfilesSupplier runfilesSupplier =
+        createRunfilesSupplier("tools/tool.runfiles", ImmutableList.of(toolDat));
+
     Spawn spawn1 =
         new SimpleSpawn(
             new FakeOwner("foo", "bar", "//dummy:label"),
             /* arguments= */ ImmutableList.of(),
             /* environment= */ ImmutableMap.of(),
             /* executionInfo= */ ImmutableMap.of(),
+            /* runfilesSupplier= */ runfilesSupplier,
             /* inputs= */ nodeRoot1,
             /* outputs= */ ImmutableSet.of(),
             ResourceSet.ZERO);
+
     Spawn spawn2 =
         new SimpleSpawn(
             new FakeOwner("foo", "bar", "//dummy:label"),
             /* arguments= */ ImmutableList.of(),
             /* environment= */ ImmutableMap.of(),
             /* executionInfo= */ ImmutableMap.of(),
+            /* runfilesSupplier= */ runfilesSupplier,
             /* inputs= */ nodeRoot2,
             /* outputs= */ ImmutableSet.of(),
             ResourceSet.ZERO);
+
     FakeSpawnExecutionContext context1 = newSpawnExecutionContext(spawn1);
     FakeSpawnExecutionContext context2 = newSpawnExecutionContext(spawn2);
     remoteOptions.remoteMerkleTreeCache = true;
@@ -2133,7 +2137,7 @@ public class RemoteExecutionServiceTest {
                 PathFragment.EMPTY_FRAGMENT,
                 PathMapper.NOOP.getClass()), // fileset mapping
             ImmutableList.of(
-                EmptyRunfilesSupplier.INSTANCE,
+                PathFragment.create("tools/tool.runfiles"),
                 PathFragment.EMPTY_FRAGMENT,
                 PathMapper.NOOP.getClass()),
             ImmutableList.of(tree, PathFragment.EMPTY_FRAGMENT, PathMapper.NOOP.getClass()),
@@ -2156,7 +2160,7 @@ public class RemoteExecutionServiceTest {
                 PathFragment.EMPTY_FRAGMENT,
                 PathMapper.NOOP.getClass()), // fileset mapping
             ImmutableList.of(
-                EmptyRunfilesSupplier.INSTANCE,
+                PathFragment.create("tools/tool.runfiles"),
                 PathFragment.EMPTY_FRAGMENT,
                 PathMapper.NOOP.getClass()),
             ImmutableList.of(tree, PathFragment.EMPTY_FRAGMENT, PathMapper.NOOP.getClass()),
@@ -2491,6 +2495,47 @@ public class RemoteExecutionServiceTest {
         tempPathGenerator,
         null,
         remoteOutputChecker);
+  }
+
+  private RunfilesSupplier createRunfilesSupplier(String root, Collection<Artifact> artifacts) {
+    return new RunfilesSupplier() {
+      @Override
+      public NestedSet<Artifact> getArtifacts() {
+        throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public ImmutableSet<PathFragment> getRunfilesDirs() {
+        throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public ImmutableMap<PathFragment, Map<PathFragment, Artifact>> getMappings() {
+        return ImmutableMap.of(
+            PathFragment.create(root),
+            artifacts.stream().collect(toImmutableMap(Artifact::getExecPath, a -> a)));
+      }
+
+      @Override
+      public ImmutableList<Artifact> getManifests() {
+        throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public RunfileSymlinksMode getRunfileSymlinksMode(PathFragment runfilesDir) {
+        throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public boolean isBuildRunfileLinks(PathFragment runfilesDir) {
+        throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public RunfilesSupplier withOverriddenRunfilesDir(PathFragment newRunfilesDir) {
+        throw new UnsupportedOperationException();
+      }
+    };
   }
 
   private void createOutputDirectories(Spawn spawn) throws IOException {
