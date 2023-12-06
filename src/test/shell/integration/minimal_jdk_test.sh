0585f5443d2628632905560cc7a50717536f279f
Justin Horvitz <jhorvitz@google.com>
2023-12-06 10:00:28 -0500

    Update minimal_jdk_test.sh

--- src/test/shell/integration/minimal_jdk_test.sh
+++ src/test/shell/integration/minimal_jdk_test.sh
@@ -42,9 +42,9 @@ export BAZEL_SUFFIX="_jdk_minimal"
 source "$(rlocation "io_bazel/src/test/shell/integration_test_setup.sh")" \
   || { echo "integration_test_setup.sh not found!" >&2; exit 1; }
 
-# Bazel's install base is < 370MB with minimal JDK and > 370MB with an all
+# Bazel's install base is < 450MB with minimal JDK and > 450MB with an all
 # modules JDK.
-function test_size_less_than_370MB() {
+function test_size_less_than_450MB() {
   bazel info
   ib=$(bazel info install_base)
   size=$(du -s "$ib" | cut -d\	 -f1)
