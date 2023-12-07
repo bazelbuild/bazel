e8b4db5bad5245dfe534a4a3531e76fb0baa2a15
Googler <tjgq@google.com>
2023-12-07 02:00:06 -0800

    [7.1.0] Move StableSort into the exec package.
    It's an implementation detail of ExpandedSpawnLogContext.
    
    PiperOrigin-RevId: 588706888
    Change-Id: I8292bbdbccae7d5081b9ac5ac88dbc5574eb4127

--- src/main/java/com/google/devtools/build/lib/exec/StableSort.java
+++ src/main/java/com/google/devtools/build/lib/exec/StableSort.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-package com.google.devtools.build.lib.bazel.execlog;
+package com.google.devtools.build.lib.exec;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Multimap;
