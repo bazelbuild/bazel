ab9f2c6a9188e6fbff32fe7dba8c9cba12ea9bca
Christian Scott <christian.s@canva.com>
2023-12-18 09:03:57 +1100

    address feedback from review

--- src/main/java/com/google/devtools/build/lib/remote/util/DigestUtil.java
+++ src/main/java/com/google/devtools/build/lib/remote/util/DigestUtil.java
@@ -13,11 +13,13 @@
 // limitations under the License.
 package com.google.devtools.build.lib.remote.util;
 
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 import build.bazel.remote.execution.v2.Action;
 import build.bazel.remote.execution.v2.Digest;
 import build.bazel.remote.execution.v2.DigestFunction;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.hash.HashCode;
 import com.google.common.io.BaseEncoding;
 import com.google.devtools.build.lib.actions.cache.VirtualActionInput;
@@ -30,8 +32,7 @@ import com.google.protobuf.Message;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 
 /** Utility methods to work with {@link Digest}. */
 public class DigestUtil {
@@ -45,20 +46,14 @@ public class DigestUtil {
     this.digestFunction = getDigestFunctionFromHashFunction(hashFn);
   }
 
-  private static final Set<String> digestHashFunctionNames;
-  static {
-    digestHashFunctionNames = new HashSet<>();
-    for (var value : DigestFunction.Value.values()) {
-      digestHashFunctionNames.add(value.name());
-    }
-  }
+  private static final ImmutableSet<String> DIGEST_FUNCTION_NAMES =
+      Arrays.stream(DigestFunction.Value.values()).map(v -> v.name()).collect(toImmutableSet());
 
   private static DigestFunction.Value getDigestFunctionFromHashFunction(DigestHashFunction hashFn) {
     for (String name : hashFn.getNames()) {
-      if (!digestHashFunctionNames.contains(name)) {
-        continue;
+      if (DIGEST_FUNCTION_NAMES.contains(name)) {
+        return DigestFunction.Value.valueOf(name);
       }
-      return DigestFunction.Value.valueOf(name);
     }
     return DigestFunction.Value.UNKNOWN;
   }
