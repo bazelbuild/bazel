2c51a0c8590e73c88d6a678f1b0583c6aee313c3
Googler <pcloudy@google.com>
2023-12-20 10:23:03 -0800

    Prefer ipv6 network in integration tests on macOS
    PiperOrigin-RevId: 592595418
    Change-Id: I59bfb00ea1ddbea121e32f38453c6055b7a38ad9

--- src/test/py/bazel/test_base.py
+++ src/test/py/bazel/test_base.py
@@ -136,7 +136,10 @@ class TestBase(absltest.TestCase):
         if TestBase.IsDarwin():
           # For reducing SSD usage on our physical Mac machines.
           f.write('common --experimental_repository_cache_hardlinks\n')
-      f.write('common --enable_bzlmod\n')
+      if TestBase.IsDarwin():
+        # Prefer ipv6 network on macOS
+        f.write('startup --host_jvm_args=-Djava.net.preferIPv6Addresses=true\n')
+        f.write('build --jvmopt=-Djava.net.preferIPv6Addresses\n')
     self.CopyFile(
         self.Rlocation('io_bazel/src/test/tools/bzlmod/MODULE.bazel.lock'),
         'MODULE.bazel.lock',
