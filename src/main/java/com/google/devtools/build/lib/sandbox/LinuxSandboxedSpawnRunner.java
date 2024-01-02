fb6658c86164b81205a056a9a54975a62f2f957a
Googler <lberki@google.com>
2024-01-02 10:03:46 -0800

    Fix two issues with `--incompatible_sandbox_hermetic_tmp` that manifested themselves when the output base was under `/tmp`:
    1. Virtual action inputs didn't work. This was a fairly simple omission in the path transformation logic.
    2. Artifacts which are resolved symlinks (i.e. declared using `declare_file`) did not work. This is fixed by keeping track of the realpath of the symlink in `FileArtifactValue` / `TreeArtifactValue`.
    
    Fixes #20515 and #20518.
    
    RELNOTES: None.
    PiperOrigin-RevId: 595145191
    Change-Id: I833025928374c78bc719d8e3be1efb2b2950b9f1

--- src/main/java/com/google/devtools/build/lib/sandbox/LinuxSandboxedSpawnRunner.java
+++ src/main/java/com/google/devtools/build/lib/sandbox/LinuxSandboxedSpawnRunner.java
@@ -281,6 +281,7 @@ final class LinuxSandboxedSpawnRunner extends AbstractSandboxSpawnRunner {
     SandboxInputs inputs =
         helpers.processInputFiles(
             context.getInputMapping(PathFragment.EMPTY_FRAGMENT, /* willAccessRepeatedly= */ true),
+            context.getInputMetadataProvider(),
             execRoot,
             withinSandboxExecRoot,
             packageRoots,
