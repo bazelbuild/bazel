98537240958c9fd8e28aa033a791008c0f60af95
Chi Wawng <coeuvre@gmail.com>
2024-01-05 16:59:58 +0800

    append after close

--- src/test/shell/bazel/bazel_test_test.sh
+++ src/test/shell/bazel/bazel_test_test.sh
@@ -14,11 +14,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -eu
+set -euo pipefail
+
+# --- begin runfiles.bash initialization ---
+if [[ ! -d "${RUNFILES_DIR:-/dev/null}" && ! -f "${RUNFILES_MANIFEST_FILE:-/dev/null}" ]]; then
+  if [[ -f "$0.runfiles_manifest" ]]; then
+    export RUNFILES_MANIFEST_FILE="$0.runfiles_manifest"
+  elif [[ -f "$0.runfiles/MANIFEST" ]]; then
+    export RUNFILES_MANIFEST_FILE="$0.runfiles/MANIFEST"
+  elif [[ -f "$0.runfiles/bazel_tools/tools/bash/runfiles/runfiles.bash" ]]; then
+    export RUNFILES_DIR="$0.runfiles"
+  fi
+fi
+if [[ -f "${RUNFILES_DIR:-/dev/null}/bazel_tools/tools/bash/runfiles/runfiles.bash" ]]; then
+  source "${RUNFILES_DIR}/bazel_tools/tools/bash/runfiles/runfiles.bash"
+elif [[ -f "${RUNFILES_MANIFEST_FILE:-/dev/null}" ]]; then
+  source "$(grep -m1 "^bazel_tools/tools/bash/runfiles/runfiles.bash " \
+            "$RUNFILES_MANIFEST_FILE" | cut -d ' ' -f 2-)"
+else
+  echo >&2 "ERROR: cannot find @bazel_tools//tools/bash/runfiles:runfiles.bash"
+  exit 1
+fi
+# --- end runfiles.bash initialization ---
 
 # Load the test setup defined in the parent directory
-CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "${CURRENT_DIR}/../integration_test_setup.sh" \
+source "$(rlocation "io_bazel/src/test/shell/integration_test_setup.sh")" \
   || { echo "integration_test_setup.sh not found!" >&2; exit 1; }
 
 function set_up_jobcount() {
@@ -1036,25 +1056,6 @@ EOF
   bazel test \
       --incompatible_check_sharding_support \
       //:x  &> $TEST_log || fail "expected success"
-}
-
-function test_append_stderr_to_test_log() {
-  cat <<'EOF' > BUILD
-sh_test(
-    name = 'x',
-    srcs = ['x.sh'],
-)
-EOF
-  cat <<'EOF' > x.sh
-#!/bin/sh
-echo foo > /dev/stderr
-EOF
-  chmod +x x.sh
-
-  bazel test //:x  &> $TEST_log || fail "expected success"
-
-  cat bazel-testlogs/x/test.log > $TEST_log
-  expect_log "foo"
 }
 
 run_suite "bazel test tests"
