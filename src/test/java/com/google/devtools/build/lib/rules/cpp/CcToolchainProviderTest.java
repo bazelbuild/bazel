bazelbuild/bazel

commit 92cba040904c2ccbd0b5a70263de739f72c592ae

Starlarkify `CcToolchainAttributesProvider` and delete it.
After `incompatible_enable_cc_toolchain_resolution` has been made no-op - this provider served no purpose other than being a simple placeholder.
This should be the final cl from the series of preparing `CcToolchainProvider` for Starlarkification. After this I think all blockers are gone and I will try to switch `CcToolchainInfo` to Starlark version.

PiperOrigin-RevId: 593108847
Change-Id: I3b66288df7b123fbcdb9ed699e04ba54ddb1b275

src/main/starlark/builtins_bzl/common/cc/cc_toolchain.bzl
@@ -16,11 +16,18 @@
 
 load(":common/cc/cc_helper.bzl", "cc_helper")
 load(":common/cc/cc_toolchain_provider_helper.bzl", "get_cc_toolchain_provider")
+load(":common/cc/semantics.bzl", "semantics")
 
 cc_internal = _builtins.internal.cc_internal
 ToolchainInfo = _builtins.toplevel.platform_common.ToolchainInfo
 TemplateVariableInfo = _builtins.toplevel.platform_common.TemplateVariableInfo
 apple_common = _builtins.toplevel.apple_common
+FdoProfileInfo = _builtins.internal.FdoProfileInfo
+FdoPrefetchHintsInfo = _builtins.internal.FdoPrefetchHintsInfo
+PropellerOptimizeInfo = _builtins.internal.PropellerOptimizeInfo
+PackageSpecificationInfo = _builtins.toplevel.PackageSpecificationInfo
+CcToolchainConfigInfo = _builtins.toplevel.CcToolchainConfigInfo
+MemProfProfileInfo = _builtins.internal.MemProfProfileInfo
 
 def _validate_toolchain(ctx, is_apple):
     if not is_apple:
@@ -30,20 +37,119 @@ def _validate_toolchain(ctx, is_apple):
              "changed recently, verify that \"xcode-select -p\" is correct and then try: " +
              "\"bazel shutdown\" to re-run Xcode configuration")
 
+def _files(ctx, attr_name):
+    attr = getattr(ctx.attr, attr_name, None)
+    if attr != None and DefaultInfo in attr:
+        return attr[DefaultInfo].files
+    return depset()
+
+def _provider(attr, provider):
+    if attr != None and provider in attr:
+        return attr[provider]
+    return None
+
+def _latebound_libc(ctx, attr_name, implicit_attr_name):
+    if getattr(ctx.attr, implicit_attr_name, None) == None:
+        return attr_name
+    return implicit_attr_name
+
+def _full_inputs_for_link(ctx, linker_files, libc, is_apple_toolchain):
+    if not is_apple_toolchain:
+        return depset(
+            [ctx.file._interface_library_builder, ctx.file._link_dynamic_library_tool],
+            transitive = [linker_files, libc],
+        )
+    return depset(transitive = [linker_files, libc])
+
+def _label(ctx, attr_name):
+    if getattr(ctx.attr, attr_name, None) != None:
+        return getattr(ctx.attr, attr_name).label
+    return None
+
+def _package_specification_provider(ctx, allowlist_name):
+    possible_attr_names = ["_whitelist_" + allowlist_name, "_allowlist_" + allowlist_name]
+    for attr_name in possible_attr_names:
+        if hasattr(ctx.attr, attr_name):
+            package_specification_provider = getattr(ctx.attr, attr_name)[PackageSpecificationInfo]
+            if package_specification_provider != None:
+                return package_specification_provider
+    fail("Allowlist argument for " + allowlist_name + " not found")
+
+def _single_file(ctx, attr_name):
+    files = getattr(ctx.files, attr_name, [])
+    if len(files) > 1:
+        fail(ctx.label.name + " expected a single artifact", attr = attr_name)
+    if len(files) == 1:
+        return files[0]
+    return None
+
+def _attributes(ctx, is_apple):
+    grep_includes = None
+    if not semantics.is_bazel:
+        grep_includes = _single_file(ctx, "_grep_includes")
+
+    latebound_libc = _latebound_libc(ctx, "libc_top", "_libc_top")
+    latebound_target_libc = _latebound_libc(ctx, "libc_top", "_target_libc_top")
+
+    return struct(
+        supports_param_files = ctx.attr.supports_param_files,
+        runtime_solib_dir_base = "_solib__" + cc_internal.escape_label(label = ctx.label),
+        fdo_prefetch_provider = _provider(ctx.attr._fdo_prefetch_hints, FdoPrefetchHintsInfo),
+        propeller_optimize_provider = _provider(ctx.attr._propeller_optimize, PropellerOptimizeInfo),
+        mem_prof_profile_provider = _provider(ctx.attr._memprof_profile, MemProfProfileInfo),
+        cc_toolchain_config_info = _provider(ctx.attr.toolchain_config, CcToolchainConfigInfo),
+        fdo_optimize_artifacts = ctx.files._fdo_optimize,
+        licenses_provider = cc_internal.licenses(ctx = ctx),
+        static_runtime_lib = ctx.attr.static_runtime_lib,
+        dynamic_runtime_lib = ctx.attr.dynamic_runtime_lib,
+        supports_header_parsing = ctx.attr.supports_header_parsing,
+        all_files = _files(ctx, "all_files"),
+        compiler_files = _files(ctx, "compiler_files"),
+        strip_files = _files(ctx, "strip_files"),
+        objcopy_files = _files(ctx, "objcopy_files"),
+        fdo_optimize_label = _label(ctx, "_fdo_optimize"),
+        link_dynamic_library_tool = ctx.file._link_dynamic_library_tool,
+        grep_includes = grep_includes,
+        module_map = ctx.attr.module_map,
+        as_files = _files(ctx, "as_files"),
+        ar_files = _files(ctx, "ar_files"),
+        dwp_files = _files(ctx, "dwp_files"),
+        fdo_optimize_provider = _provider(ctx.attr._fdo_optimize, FdoProfileInfo),
+        module_map_artifact = _single_file(ctx, "module_map"),
+        all_files_including_libc = depset(transitive = [_files(ctx, "all_files"), _files(ctx, latebound_libc)]),
+        fdo_profile_provider = _provider(ctx.attr._fdo_profile, FdoProfileInfo),
+        cs_fdo_profile_provider = _provider(ctx.attr._csfdo_profile, FdoProfileInfo),
+        x_fdo_profile_provider = _provider(ctx.attr._xfdo_profile, FdoProfileInfo),
+        zipper = ctx.file._zipper,
+        linker_files = _full_inputs_for_link(
+            ctx,
+            _files(ctx, "linker_files"),
+            _files(ctx, latebound_libc),
+            is_apple,
+        ),
+        cc_toolchain_label = ctx.label,
+        coverage_files = _files(ctx, "coverage_files"),
+        compiler_files_without_includes = _files(ctx, "compiler_files_without_includes"),
+        libc = _files(ctx, latebound_libc),
+        target_libc = _files(ctx, latebound_target_libc),
+        libc_top_label = _label(ctx, latebound_libc),
+        target_libc_top_label = _label(ctx, latebound_target_libc),
+        if_so_builder = ctx.file._interface_library_builder,
+        allowlist_for_layering_check = _package_specification_provider(ctx, "disabling_parse_headers_and_layering_check_allowed"),
+        build_info_files = _provider(ctx.attr._build_info_translator, OutputGroupInfo),
+    )
+
 def _cc_toolchain_impl(ctx):
     _validate_toolchain(ctx, ctx.attr._is_apple)
     xcode_config_info = None
     if ctx.attr._is_apple:
         xcode_config_info = ctx.attr._xcode_config[apple_common.XcodeVersionConfig]
-    attributes_provider = cc_internal.construct_cc_toolchain_attributes_info(
-        ctx = ctx,
-        is_apple = ctx.attr._is_apple,
-    )
-    providers = [attributes_provider]
-    if attributes_provider.licenses_provider() != None:
-        providers.append(attributes_provider.licenses_provider())
+    attributes = _attributes(ctx, ctx.attr._is_apple)
+    providers = []
+    if attributes.licenses_provider != None:
+        providers.append(attributes.licenses_provider)
 
-    cc_toolchain = get_cc_toolchain_provider(ctx, attributes_provider, xcode_config_info)
+    cc_toolchain = get_cc_toolchain_provider(ctx, attributes, xcode_config_info)
     if cc_toolchain == None:
         fail("This should never happen")
     template_variable_info = TemplateVariableInfo(

src/main/java/com/google/devtools/build/lib/rules/cpp/CcStarlarkInternal.java
@@ -20,8 +20,15 @@ import static com.google.common.collect.ImmutableMap.toImmutableMap;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.devtools.build.docgen.annot.DocCategory;
+import com.google.devtools.build.lib.actions.Actions;
 import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.actions.CommandLineExpansionException;
+import com.google.devtools.build.lib.analysis.LicensesProvider;
+import com.google.devtools.build.lib.analysis.LicensesProvider.TargetLicense;
+import com.google.devtools.build.lib.analysis.LicensesProviderImpl;
+import com.google.devtools.build.lib.analysis.OutputGroupInfo;
+import com.google.devtools.build.lib.analysis.PackageSpecificationProvider;
+import com.google.devtools.build.lib.analysis.RuleContext;
 import com.google.devtools.build.lib.analysis.actions.ActionConstructionContext;
 import com.google.devtools.build.lib.analysis.config.BuildConfigurationValue;
 import com.google.devtools.build.lib.analysis.starlark.StarlarkActionFactory;
@@ -30,8 +37,11 @@ import com.google.devtools.build.lib.cmdline.Label;
 import com.google.devtools.build.lib.collect.nestedset.Depset;
 import com.google.devtools.build.lib.collect.nestedset.Depset.TypeException;
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
+import com.google.devtools.build.lib.collect.nestedset.NestedSetBuilder;
+import com.google.devtools.build.lib.collect.nestedset.Order;
 import com.google.devtools.build.lib.packages.Attribute.ComputedDefault;
 import com.google.devtools.build.lib.packages.AttributeMap;
+import com.google.devtools.build.lib.packages.License;
 import com.google.devtools.build.lib.packages.RuleClass.ConfiguredTargetFactory.RuleErrorException;
 import com.google.devtools.build.lib.packages.StarlarkProvider;
 import com.google.devtools.build.lib.packages.Type;
@@ -84,18 +94,6 @@ public class CcStarlarkInternal implements StarlarkValue {
   }
 
   @StarlarkMethod(
-      name = "construct_cc_toolchain_attributes_info",
-      documented = false,
-      parameters = {
-        @Param(name = "ctx", positional = false, named = true),
-        @Param(name = "is_apple", positional = false, named = true),
-      })
-  public CcToolchainAttributesProvider constructCcToolchainAttributesInfo(
-      StarlarkRuleContext ruleContext, boolean isApple) throws EvalException {
-    return new CcToolchainAttributesProvider(ruleContext.getRuleContext(), isApple);
-  }
-
-  @StarlarkMethod(
       name = "construct_toolchain_provider",
       documented = false,
       parameters = {
@@ -103,7 +101,6 @@ public class CcStarlarkInternal implements StarlarkValue {
         @Param(name = "cpp_config", positional = false, named = true),
         @Param(name = "toolchain_features", positional = false, named = true),
         @Param(name = "tools_directory", positional = false, named = true),
-        @Param(name = "attributes", positional = false, named = true),
         @Param(
             name = "static_runtime_link_inputs",
             positional = false,
@@ -145,13 +142,31 @@ public class CcStarlarkInternal implements StarlarkValue {
         @Param(name = "gcov", positional = false, named = true),
         @Param(name = "vars", positional = false, named = true),
         @Param(name = "xcode_config_info", positional = false, named = true),
+        @Param(name = "all_files", positional = false, named = true),
+        @Param(name = "all_files_including_libc", positional = false, named = true),
+        @Param(name = "compiler_files", positional = false, named = true),
+        @Param(name = "compiler_files_without_includes", positional = false, named = true),
+        @Param(name = "strip_files", positional = false, named = true),
+        @Param(name = "objcopy_files", positional = false, named = true),
+        @Param(name = "as_files", positional = false, named = true),
+        @Param(name = "ar_files", positional = false, named = true),
+        @Param(name = "linker_files", positional = false, named = true),
+        @Param(name = "if_so_builder", positional = false, named = true),
+        @Param(name = "dwp_files", positional = false, named = true),
+        @Param(name = "coverage_files", positional = false, named = true),
+        @Param(name = "supports_param_files", positional = false, named = true),
+        @Param(name = "supports_header_parsing", positional = false, named = true),
+        @Param(name = "link_dynamic_library_tool", positional = false, named = true),
+        @Param(name = "grep_includes", positional = false, named = true),
+        @Param(name = "licenses_provider", positional = false, named = true),
+        @Param(name = "allowlist_for_layering_check", positional = false, named = true),
+        @Param(name = "build_info_files", positional = false, named = true),
       })
   public CcToolchainProvider getCcToolchainProvider(
       StarlarkRuleContext ruleContext,
       Object cppConfigurationObject,
       CcToolchainFeatures toolchainFeatures,
       String toolsDirectoryStr,
-      CcToolchainAttributesProvider attributes,
       Object staticRuntimeLinkInputsObject,
       Object dynamicRuntimeLinkInputsObject,
       String dynamicRuntimeSolibDirStr,
@@ -178,7 +193,26 @@ public class CcStarlarkInternal implements StarlarkValue {
       String ldExecutable,
       String gcovExecutable,
       Object vars,
-      Object xcodeConfigInfoObject)
+      Object xcodeConfigInfoObject,
+      Depset allFiles,
+      Depset allFilesIncludingLibc,
+      Depset compilerFiles,
+      Depset compilerFilesWithoutIncludes,
+      Depset stripFiles,
+      Depset objcopyFiles,
+      Depset asFiles,
+      Depset arFiles,
+      Depset fullInputsForLink,
+      Artifact ifsoBuilder,
+      Depset dwpFiles,
+      Depset coverageFiles,
+      Boolean supportsParamFiles,
+      Boolean supportsHeaderParsing,
+      Artifact linkDynamicLibraryTool,
+      Object grepIncludesObject,
+      Object licensesProviderObject,
+      PackageSpecificationProvider allowlistForLayeringCheck,
+      OutputGroupInfo buildInfoFiles)
       throws EvalException, InterruptedException {
     CppConfiguration cppConfiguration = CcModule.convertFromNoneable(cppConfigurationObject, null);
     PathFragment toolsDirectory = PathFragment.create(toolsDirectoryStr);
@@ -219,68 +253,76 @@ public class CcStarlarkInternal implements StarlarkValue {
     if (xcodeConfigInfoObject != Starlark.NONE) {
       xcodeConfigInfo = (XcodeConfigInfo) xcodeConfigInfoObject;
     }
-    return new CcToolchainProvider(
-        /* cppConfiguration= */ cppConfiguration,
-        /* toolchainFeatures= */ toolchainFeatures,
-        /* crosstoolTopPathFragment= */ toolsDirectory,
-        /* allFiles= */ attributes.getAllFiles(),
-        /* allFilesIncludingLibc= */ attributes.getFullInputsForCrosstool(),
-        /* compilerFiles= */ attributes.getCompilerFiles(),
-        /* compilerFilesWithoutIncludes= */ attributes.getCompilerFilesWithoutIncludes(),
-        /* stripFiles= */ attributes.getStripFiles(),
-        /* objcopyFiles= */ attributes.getObjcopyFiles(),
-        /* asFiles= */ attributes.getAsFiles(),
-        /* arFiles= */ attributes.getArFiles(),
-        /* linkerFiles= */ attributes.getFullInputsForLink(),
-        /* interfaceSoBuilder= */ attributes.getIfsoBuilder(),
-        /* dwpFiles= */ attributes.getDwpFiles(),
-        /* coverageFiles= */ attributes.getCoverage(),
-        /* staticRuntimeLinkInputs= */ staticRuntimeLinkInputsSet,
-        /* dynamicRuntimeLinkInputs= */ dynamicRuntimeLinkInputsSet,
-        /* dynamicRuntimeSolibDir= */ dynamicRuntimeSolibDir,
-        /* ccCompilationContext= */ ccCompilationContext,
-        /* supportsParamFiles= */ attributes.isSupportsParamFiles(),
-        /* supportsHeaderParsing= */ attributes.isSupportsHeaderParsing(),
-        /* buildVariables= */ (CcToolchainVariables) vars,
-        /* builtinIncludeFiles= */ Sequence.cast(
-                builtinIncludeFiles, Artifact.class, "builtin_include_files")
-            .getImmutableList(),
-        /* linkDynamicLibraryTool= */ attributes.getLinkDynamicLibraryTool(),
-        /* grepIncludes= */ attributes.getGrepIncludes(),
-        /* builtInIncludeDirectories= */ builtInIncludeDirectories,
-        /* sysroot= */ sysroot,
-        /* fdoContext= */ fdoContext,
-        /* isToolConfiguration= */ isToolConfiguration,
-        /* licensesProvider= */ attributes.getLicensesProvider(),
-        /* toolPaths= */ ImmutableMap.copyOf(
-            Dict.cast(toolPathsDict, String.class, String.class, "tool_paths")),
-        /* toolchainIdentifier= */ toolchainConfigInfo.getToolchainIdentifier(),
-        /* compiler= */ toolchainConfigInfo.getCompiler(),
-        /* abiGlibcVersion= */ toolchainConfigInfo.getAbiLibcVersion(),
-        /* targetCpu= */ toolchainConfigInfo.getTargetCpu(),
-        /* targetOS= */ toolchainConfigInfo.getCcTargetOs(),
-        /* defaultSysroot= */ defaultSysroot,
-        /* runtimeSysroot= */ runtimeSysroot,
-        /* targetLibc= */ toolchainConfigInfo.getTargetLibc(),
-        /* ccToolchainLabel= */ ruleContext.getRuleContext().getLabel(),
-        /* solibDirectory= */ solibDirectory,
-        /* abi= */ toolchainConfigInfo.getAbiVersion(),
-        /* targetSystemName= */ toolchainConfigInfo.getTargetSystemName(),
-        /* additionalMakeVariables= */ ImmutableMap.copyOf(additionalMakeVariables),
-        /* legacyCcFlagsMakeVariable= */ legacyCcFlagsMakeVariable,
-        /* allowlistForLayeringCheck= */ attributes.getAllowlistForLayeringCheck(),
-        /* objcopyExecutable= */ objcopyExecutable,
-        /* compilerExecutable= */ compilerExecutable,
-        /* preprocessorExecutable= */ preprocessorExecutable,
-        /* nmExecutable= */ nmExecutable,
-        /* objdumpExecutable= */ objdumpExecutable,
-        /* arExecutable= */ arExecutable,
-        /* stripExecutable= */ stripExecutable,
-        /* ldExecutable= */ ldExecutable,
-        /* gcovExecutable= */ gcovExecutable,
-        /* ccToolchainVariablesBuilderFunc */ buildFunc,
-        /* xcodeConfigInfo */ xcodeConfigInfo,
-        /* ccBuildInfoTranslator */ attributes.getCcBuildInfoTranslator());
+    try {
+      return new CcToolchainProvider(
+          /* cppConfiguration= */ cppConfiguration,
+          /* toolchainFeatures= */ toolchainFeatures,
+          /* crosstoolTopPathFragment= */ toolsDirectory,
+          /* allFiles= */ allFiles.getSet(Artifact.class),
+          /* allFilesIncludingLibc= */ allFilesIncludingLibc.getSet(Artifact.class),
+          /* compilerFiles= */ compilerFiles.getSet(Artifact.class),
+          /* compilerFilesWithoutIncludes= */ compilerFilesWithoutIncludes.getSet(Artifact.class),
+          /* stripFiles= */ stripFiles.getSet(Artifact.class),
+          /* objcopyFiles= */ objcopyFiles.getSet(Artifact.class),
+          /* asFiles= */ asFiles.getSet(Artifact.class),
+          /* arFiles= */ arFiles.getSet(Artifact.class),
+          /* linkerFiles= */ fullInputsForLink.getSet(Artifact.class),
+          /* interfaceSoBuilder= */ ifsoBuilder,
+          /* dwpFiles= */ dwpFiles.getSet(Artifact.class),
+          /* coverageFiles= */ coverageFiles.getSet(Artifact.class),
+          /* staticRuntimeLinkInputs= */ staticRuntimeLinkInputsSet,
+          /* dynamicRuntimeLinkInputs= */ dynamicRuntimeLinkInputsSet,
+          /* dynamicRuntimeSolibDir= */ dynamicRuntimeSolibDir,
+          /* ccCompilationContext= */ ccCompilationContext,
+          /* supportsParamFiles= */ supportsParamFiles,
+          /* supportsHeaderParsing= */ supportsHeaderParsing,
+          /* buildVariables= */ (CcToolchainVariables) vars,
+          /* builtinIncludeFiles= */ Sequence.cast(
+                  builtinIncludeFiles, Artifact.class, "builtin_include_files")
+              .getImmutableList(),
+          /* linkDynamicLibraryTool= */ linkDynamicLibraryTool,
+          /* grepIncludes= */ grepIncludesObject == Starlark.NONE
+              ? null
+              : (Artifact) grepIncludesObject,
+          /* builtInIncludeDirectories= */ builtInIncludeDirectories,
+          /* sysroot= */ sysroot,
+          /* fdoContext= */ fdoContext,
+          /* isToolConfiguration= */ isToolConfiguration,
+          /* licensesProvider= */ licensesProviderObject == Starlark.NONE
+              ? null
+              : (LicensesProvider) licensesProviderObject,
+          /* toolPaths= */ ImmutableMap.copyOf(
+              Dict.cast(toolPathsDict, String.class, String.class, "tool_paths")),
+          /* toolchainIdentifier= */ toolchainConfigInfo.getToolchainIdentifier(),
+          /* compiler= */ toolchainConfigInfo.getCompiler(),
+          /* abiGlibcVersion= */ toolchainConfigInfo.getAbiLibcVersion(),
+          /* targetCpu= */ toolchainConfigInfo.getTargetCpu(),
+          /* targetOS= */ toolchainConfigInfo.getCcTargetOs(),
+          /* defaultSysroot= */ defaultSysroot,
+          /* runtimeSysroot= */ runtimeSysroot,
+          /* targetLibc= */ toolchainConfigInfo.getTargetLibc(),
+          /* ccToolchainLabel= */ ruleContext.getRuleContext().getLabel(),
+          /* solibDirectory= */ solibDirectory,
+          /* abi= */ toolchainConfigInfo.getAbiVersion(),
+          /* targetSystemName= */ toolchainConfigInfo.getTargetSystemName(),
+          /* additionalMakeVariables= */ ImmutableMap.copyOf(additionalMakeVariables),
+          /* legacyCcFlagsMakeVariable= */ legacyCcFlagsMakeVariable,
+          /* allowlistForLayeringCheck= */ allowlistForLayeringCheck,
+          /* objcopyExecutable= */ objcopyExecutable,
+          /* compilerExecutable= */ compilerExecutable,
+          /* preprocessorExecutable= */ preprocessorExecutable,
+          /* nmExecutable= */ nmExecutable,
+          /* objdumpExecutable= */ objdumpExecutable,
+          /* arExecutable= */ arExecutable,
+          /* stripExecutable= */ stripExecutable,
+          /* ldExecutable= */ ldExecutable,
+          /* gcovExecutable= */ gcovExecutable,
+          /* ccToolchainVariablesBuilderFunc= */ buildFunc,
+          /* xcodeConfigInfo= */ xcodeConfigInfo,
+          /* ccBuildInfoTranslator= */ buildInfoFiles);
+    } catch (TypeException e) {
+      throw new EvalException(e);
+    }
   }
 
   @StarlarkMethod(
@@ -701,5 +743,38 @@ public class CcStarlarkInternal implements StarlarkValue {
   @StarlarkMethod(name = "BuildSettingInfo", documented = false, structField = true)
   public StarlarkProvider buildSettingInfo() throws EvalException {
     return buildSettingInfo;
+  }
+
+  @StarlarkMethod(
+      name = "escape_label",
+      documented = false,
+      parameters = {
+        @Param(name = "label", positional = false, named = true),
+      })
+  public String escapeLabel(Label label) {
+    return Actions.escapeLabel(label);
+  }
+
+  @StarlarkMethod(
+      name = "licenses",
+      documented = false,
+      parameters = {
+        @Param(name = "ctx", positional = false, named = true),
+      },
+      allowReturnNones = true)
+  @Nullable
+  public LicensesProvider getLicenses(StarlarkRuleContext starlarkRuleContext) {
+    RuleContext ruleContext = starlarkRuleContext.getRuleContext();
+    final License outputLicense =
+        ruleContext.getRule().getToolOutputLicense(ruleContext.attributes());
+    if (outputLicense != null && !outputLicense.equals(License.NO_LICENSE)) {
+      final NestedSet<TargetLicense> license =
+          NestedSetBuilder.create(
+              Order.STABLE_ORDER, new TargetLicense(ruleContext.getLabel(), outputLicense));
+      return new LicensesProviderImpl(
+          license, new TargetLicense(ruleContext.getLabel(), outputLicense));
+    } else {
+      return null;
+    }
   }
 }

src/main/java/com/google/devtools/build/lib/rules/cpp/CcToolchainProvider.java
@@ -56,16 +56,15 @@ import net.starlark.java.eval.StarlarkThread;
 @Immutable
 public final class CcToolchainProvider extends NativeInfo
     implements CcToolchainProviderApi<
-            FeatureConfigurationForStarlark,
-            BranchFdoProfile,
-            FdoContext,
-            ConstraintValueInfo,
-            StarlarkRuleContext,
-            InvalidConfigurationException,
-            CppConfiguration,
-            CcToolchainVariables,
-            OutputGroupInfo>,
-        HasCcToolchainLabel {
+        FeatureConfigurationForStarlark,
+        BranchFdoProfile,
+        FdoContext,
+        ConstraintValueInfo,
+        StarlarkRuleContext,
+        InvalidConfigurationException,
+        CppConfiguration,
+        CcToolchainVariables,
+        OutputGroupInfo> {
 
   public static final BuiltinProvider<CcToolchainProvider> PROVIDER =
       new BuiltinProvider<CcToolchainProvider>("CcToolchainInfo", CcToolchainProvider.class) {};
@@ -622,7 +621,6 @@ public final class CcToolchainProvider extends NativeInfo
     return toolchainFeatures;
   }
 
-  @Override
   public Label getCcToolchainLabel() {
     return ccToolchainLabel;
   }

commit bbc51a06ba4db1c88bfce6dfa3c7b0622cb56d72

Fix `cc_test` coverage broken by https://github.com/bazelbuild/bazel/commit/92cba040904c2ccbd0b5a70263de739f72c592ae
PiperOrigin-RevId: 597562752
Change-Id: I799eb0a1eab433a9c6ceeaf53027570d1cd49be1

src/main/starlark/builtins_bzl/common/cc/cc_toolchain.bzl
@@ -91,6 +91,7 @@ def _attributes(ctx, is_apple):
     latebound_libc = _latebound_libc(ctx, "libc_top", "_libc_top")
     latebound_target_libc = _latebound_libc(ctx, "libc_top", "_target_libc_top")
 
+    all_files = _files(ctx, "all_files")
     return struct(
         supports_param_files = ctx.attr.supports_param_files,
         runtime_solib_dir_base = "_solib__" + cc_internal.escape_label(label = ctx.label),
@@ -103,7 +104,7 @@ def _attributes(ctx, is_apple):
         static_runtime_lib = ctx.attr.static_runtime_lib,
         dynamic_runtime_lib = ctx.attr.dynamic_runtime_lib,
         supports_header_parsing = ctx.attr.supports_header_parsing,
-        all_files = _files(ctx, "all_files"),
+        all_files = all_files,
         compiler_files = _files(ctx, "compiler_files"),
         strip_files = _files(ctx, "strip_files"),
         objcopy_files = _files(ctx, "objcopy_files"),
@@ -128,7 +129,7 @@ def _attributes(ctx, is_apple):
             is_apple,
         ),
         cc_toolchain_label = ctx.label,
-        coverage_files = _files(ctx, "coverage_files"),
+        coverage_files = _files(ctx, "coverage_files") or all_files,
         compiler_files_without_includes = _files(ctx, "compiler_files_without_includes"),
         libc = _files(ctx, latebound_libc),
         target_libc = _files(ctx, latebound_target_libc),

src/test/java/com/google/devtools/build/lib/rules/cpp/CcToolchainProviderTest.java
@@ -347,6 +347,45 @@ public class CcToolchainProviderTest extends BuildViewTestCase {
     assertThat(gcovPath).isEmpty();
   }
 
+  // regression test for b/319501294
+  @Test
+  public void testEmptyCoverageFilesDefaultsToAllFiles() throws Exception {
+    CcToolchainConfig.Builder ccToolchainConfigBuilder = CcToolchainConfig.builder();
+    scratch.file(
+        "a/BUILD",
+        "load(':cc_toolchain_config.bzl', 'cc_toolchain_config')",
+        "filegroup(name='empty')",
+        "filegroup(name='my_files', srcs = ['file1', 'file2'])",
+        "cc_toolchain(",
+        "    name = 'b',",
+        "    all_files = ':my_files',",
+        "    ar_files = ':empty',",
+        "    as_files = ':empty',",
+        "    compiler_files = ':empty',",
+        "    dwp_files = ':empty',",
+        "    linker_files = ':empty',",
+        "    strip_files = ':empty',",
+        "    objcopy_files = ':empty',",
+        "    toolchain_identifier = 'banana',",
+        "    toolchain_config = ':k8-compiler_config',",
+        ")",
+        ccToolchainConfigBuilder.build().getCcToolchainConfigRule(),
+        "cc_library(",
+        "    name = 'lib',",
+        "    toolchains = [':b'],",
+        ")");
+    analysisMock.ccSupport().setupCcToolchainConfig(mockToolsConfig, ccToolchainConfigBuilder);
+    mockToolsConfig.create(
+        "a/cc_toolchain_config.bzl",
+        ResourceLoader.readFromResources(
+            "com/google/devtools/build/lib/analysis/mock/cc_toolchain_config.bzl"));
+
+    CcToolchainProvider provider = getConfiguredTarget("//a:b").get(CcToolchainProvider.PROVIDER);
+
+    assertThat(artifactsToStrings(provider.getCoverageFiles()))
+        .containsExactly("src a/file1", "src a/file2");
+  }
+
   @Test
   public void testLlvmCoverageToolsDefined() throws Exception {
     CcToolchainConfig.Builder ccToolchainConfigBuilder =