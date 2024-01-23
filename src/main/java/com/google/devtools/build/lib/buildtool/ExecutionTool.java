bazelbuild/bazel

commit 36fa60b1805faa7da2c4b5330b4b186740f5f00d

Compute pretty paths using symlink-creation results instead of disk state
This makes pretty-path printing compatible with
--experimental_convenience_symlinks=log_only, where we'd like to pretend the
symlinks exist and still log relative to them, but not actually create them.

Turns out that instead of inferring symlink state from disk we can pretty easily
pass what we know from symlink creation along to pretty-printing. This also
establishes a happens-before relationship in the code, so the whole "only call
this at the right time" concern should be gone. There is a risk that the on-disk
state could be out of sync with what we think it should be, but I'm having a
hard time thinking of how that would be desired...

PiperOrigin-RevId: 600623440
Change-Id: I9afc9aa7b2e45f911682bde3ce7d20d72a1d9dde

src/main/java/com/google/devtools/build/lib/buildtool/ExecutionTool.java
@@ -19,7 +19,6 @@ import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Predicate;
 import com.google.common.base.Stopwatch;
 import com.google.common.base.Throwables;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
@@ -53,7 +52,6 @@ import com.google.devtools.build.lib.analysis.config.BuildConfigurationValue;
 import com.google.devtools.build.lib.analysis.config.BuildOptions;
 import com.google.devtools.build.lib.analysis.config.InvalidConfigurationException;
 import com.google.devtools.build.lib.analysis.test.TestActionContext;
-import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos.ConvenienceSymlink;
 import com.google.devtools.build.lib.buildtool.BuildRequestOptions.ConvenienceSymlinksMode;
 import com.google.devtools.build.lib.buildtool.buildevent.ConvenienceSymlinksIdentifiedEvent;
 import com.google.devtools.build.lib.buildtool.buildevent.ExecutionPhaseCompleteEvent;
@@ -103,6 +101,7 @@ import com.google.devtools.build.lib.vfs.OutputService;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.PathFragment;
 import com.google.devtools.build.lib.vfs.Root;
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
@@ -741,20 +740,28 @@ public class ExecutionTool {
    * ConvenienceSymlinksMode#IGNORE}, then skip any creating or cleaning of convenience symlinks.
    * Otherwise, manage the convenience symlinks and then post a {@link
    * ConvenienceSymlinksIdentifiedEvent} build event.
+   *
+   * @return map of convenience symlink name to target
    */
-  public void handleConvenienceSymlinks(
+  @CanIgnoreReturnValue
+  public ImmutableMap<PathFragment, PathFragment> handleConvenienceSymlinks(
       ImmutableSet<ConfiguredTarget> targetsToBuild, BuildConfigurationValue configuration) {
     try (SilentCloseable c =
         Profiler.instance().profile("ExecutionTool.handleConvenienceSymlinks")) {
-      ImmutableList<ConvenienceSymlink> convenienceSymlinks = ImmutableList.of();
+      OutputDirectoryLinksUtils.SymlinkCreationResult convenienceSymlinks =
+          OutputDirectoryLinksUtils.EMPTY_SYMLINK_CREATION_RESULT;
       if (request.getBuildOptions().experimentalConvenienceSymlinks
           != ConvenienceSymlinksMode.IGNORE) {
         convenienceSymlinks =
             createConvenienceSymlinks(request.getBuildOptions(), targetsToBuild, configuration);
       }
       if (request.getBuildOptions().experimentalConvenienceSymlinksBepEvent) {
-        env.getEventBus().post(new ConvenienceSymlinksIdentifiedEvent(convenienceSymlinks));
+        env.getEventBus()
+            .post(
+                new ConvenienceSymlinksIdentifiedEvent(
+                    convenienceSymlinks.getConvenienceSymlinkProtos()));
       }
+      return convenienceSymlinks.getCreatedSymlinks();
     }
   }
 
@@ -774,7 +781,7 @@ public class ExecutionTool {
    * symlinks aren't created. Furthermore, lingering symlinks from the last build are deleted. This
    * is to prevent confusion by pointing to an outdated directory the current build never used.
    */
-  private ImmutableList<ConvenienceSymlink> createConvenienceSymlinks(
+  private OutputDirectoryLinksUtils.SymlinkCreationResult createConvenienceSymlinks(
       BuildRequestOptions buildRequestOptions,
       ImmutableSet<ConfiguredTarget> targetsToBuild,
       BuildConfigurationValue configuration) {

src/main/java/com/google/devtools/build/lib/buildtool/BuildResult.java
@@ -17,6 +17,7 @@ package com.google.devtools.build.lib.buildtool;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.MoreObjects;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
@@ -33,6 +34,7 @@ import com.google.devtools.build.lib.util.DetailedExitCode;
 import com.google.devtools.build.lib.util.ExitCode;
 import com.google.devtools.build.lib.util.Pair;
 import com.google.devtools.build.lib.vfs.Path;
+import com.google.devtools.build.lib.vfs.PathFragment;
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import com.google.protobuf.ByteString;
 import java.util.ArrayList;
@@ -55,6 +57,7 @@ public final class BuildResult {
   @Nullable private DetailedExitCode detailedExitCode;
 
   private BuildConfigurationValue configuration;
+  private ImmutableMap<PathFragment, PathFragment> convenienceSymlinks = ImmutableMap.of();
   private Collection<ConfiguredTarget> actualTargets;
   private Collection<ConfiguredTarget> testTargets;
   private Collection<ConfiguredTarget> successfulTargets;
@@ -159,6 +162,18 @@ public final class BuildResult {
   /** Returns the build configuration collection used for the build. */
   public BuildConfigurationValue getBuildConfiguration() {
     return configuration;
+  }
+
+  void setConvenienceSymlinks(ImmutableMap<PathFragment, PathFragment> convenienceSymlinks) {
+    this.convenienceSymlinks = convenienceSymlinks;
+  }
+
+  /**
+   * Returns the convenience symlinks for this build in name -> target format (eg blaze-out ->
+   * /symlink/target).
+   */
+  public ImmutableMap<PathFragment, PathFragment> getConvenienceSymlinks() {
+    return convenienceSymlinks;
   }
 
   /** @see #getActualTargets */

src/main/java/com/google/devtools/build/lib/buildtool/BuildResultPrinter.java
@@ -91,10 +91,9 @@ class BuildResultPrinter {
     BlazeRuntime runtime = env.getRuntime();
     String productName = runtime.getProductName();
     PathPrettyPrinter prettyPrinter =
-        OutputDirectoryLinksUtils.getPathPrettyPrinter(
-            runtime.getRuleClassProvider().getSymlinkDefinitions(),
+        new PathPrettyPrinter(
             request.getBuildOptions().getSymlinkPrefix(productName),
-            env.getWorkspace());
+            result.getConvenienceSymlinks());
     OutErr outErr = request.getOutErr();
 
     // Splits aspects based on whether they are validation aspects.

src/main/java/com/google/devtools/build/lib/buildtool/BuildTool.java
@@ -348,8 +348,9 @@ public class BuildTool {
     } finally {
       if (result.getBuildConfiguration() != null) {
         // We still need to do this even in case of an exception.
-        executionTool.handleConvenienceSymlinks(
-            env.getBuildResultListener().getAnalyzedTargets(), result.getBuildConfiguration());
+        result.setConvenienceSymlinks(
+            executionTool.handleConvenienceSymlinks(
+                env.getBuildResultListener().getAnalyzedTargets(), result.getBuildConfiguration()));
       }
       executionTool.unconditionalExecutionPhaseFinalizations(
           executionTimer, env.getSkyframeExecutor());

src/main/java/com/google/devtools/build/lib/buildtool/OutputDirectoryLinksUtils.java
@@ -15,6 +15,7 @@ package com.google.devtools.build.lib.buildtool;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import com.google.common.flogger.GoogleLogger;
@@ -34,10 +35,8 @@ import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.PathFragment;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
 
@@ -91,11 +90,8 @@ public final class OutputDirectoryLinksUtils {
    *
    * <p>A warning is emitted if a symlink would resolve to multiple destinations, or if a filesystem
    * mutation operation fails.
-   *
-   * @return a list of {@link ConvenienceSymlink} messages describing what was created and
-   *     destroyed.
    */
-  static ImmutableList<ConvenienceSymlink> createOutputDirectoryLinks(
+  static SymlinkCreationResult createOutputDirectoryLinks(
       Iterable<SymlinkDefinition> symlinkDefinitions,
       BuildRequestOptions buildRequestOptions,
       String workspaceName,
@@ -111,10 +107,12 @@ public final class OutputDirectoryLinksUtils {
     String symlinkPrefix = buildRequestOptions.getSymlinkPrefix(productName);
     ConvenienceSymlinksMode mode = buildRequestOptions.experimentalConvenienceSymlinks;
     if (NO_CREATE_SYMLINKS_PREFIX.equals(symlinkPrefix)) {
-      return ImmutableList.of();
+      return EMPTY_SYMLINK_CREATION_RESULT;
     }
 
     ImmutableList.Builder<ConvenienceSymlink> convenienceSymlinksBuilder = ImmutableList.builder();
+    ImmutableMap.Builder<PathFragment, PathFragment> createdConvenienceSymlinksBuilder =
+        ImmutableMap.builder();
     List<String> failures = new ArrayList<>();
     List<String> ambiguousLinks = new ArrayList<>();
     Set<String> createdLinks = new LinkedHashSet<>();
@@ -147,6 +145,7 @@ public final class OutputDirectoryLinksUtils {
               Iterables.getOnlyElement(candidatePaths),
               failures,
               convenienceSymlinksBuilder,
+              createdConvenienceSymlinksBuilder,
               logOnly);
         } else {
           removeLink(workspace, linkName, failures, convenienceSymlinksBuilder, logOnly);
@@ -174,7 +173,8 @@ public final class OutputDirectoryLinksUtils {
                       + "distinct configurations",
                   Joiner.on(", ").join(ambiguousLinks))));
     }
-    return convenienceSymlinksBuilder.build();
+    return new SymlinkCreationResult(
+        convenienceSymlinksBuilder.build(), createdConvenienceSymlinksBuilder.buildKeepingLast());
   }
 
   /**
@@ -185,11 +185,8 @@ public final class OutputDirectoryLinksUtils {
    *
    * @param symlinkDefinitions extra symlink types added by the {@link ConfiguredRuleClassProvider}
    * @param workspace the runtime's workspace
-   * @param outputBase the runtime's output directory. Only used with
-   *     remove_all_convenience_symlinks.
    * @param eventHandler the error eventHandler
    * @param symlinkPrefix the symlink prefix which should be removed
-   * @param productName the product name
    */
   public static void removeOutputDirectoryLinks(
       Iterable<SymlinkDefinition> symlinkDefinitions,
@@ -239,6 +236,7 @@ public final class OutputDirectoryLinksUtils {
       Path target,
       List<String> failures,
       ImmutableList.Builder<ConvenienceSymlink> symlinksBuilder,
+      ImmutableMap.Builder<PathFragment, PathFragment> createdSymlinksBuilder,
       boolean logOnly) {
     // Usually the symlink target falls under the output base, and the path in the BEP event should
     // be relative to that output base. In rare cases where the symlink points elsewhere, use the
@@ -253,7 +251,10 @@ public final class OutputDirectoryLinksUtils {
             .setTarget(targetForEvent)
             .setAction(Action.CREATE)
             .build());
+    PathFragment nameFragment = PathFragment.create(name);
     if (logOnly) {
+      // Still report as created - log-only implies we want to pretend it exists.
+      createdSymlinksBuilder.put(nameFragment, target.asFragment());
       return;
     }
     Path link = base.getRelative(name);
@@ -266,6 +267,7 @@ public final class OutputDirectoryLinksUtils {
     }
     try {
       FileSystemUtils.ensureSymbolicLink(link, target);
+      createdSymlinksBuilder.put(nameFragment, target.asFragment());
     } catch (IOException e) {
       failures.add(String.format("cannot create symbolic link %s -> %s:  %s",
           name, target.getPathString(), e.getMessage()));
@@ -372,41 +374,32 @@ public final class OutputDirectoryLinksUtils {
             }
           });
 
-  public static PathPrettyPrinter getPathPrettyPrinter(
-      Iterable<SymlinkDefinition> symlinkDefinitions,
-      String symlinkPrefix,
-      Path workspaceDirectory) {
-    return new PathPrettyPrinter(
-        symlinkPrefix,
-        resolve(getAllLinkDefinitions(symlinkDefinitions), workspaceDirectory, symlinkPrefix));
-  }
+  static final SymlinkCreationResult EMPTY_SYMLINK_CREATION_RESULT =
+      new SymlinkCreationResult(ImmutableList.of(), ImmutableMap.of());
 
-  /**
-   * Creates a path pretty printer, immediately resolving the symlink definitions by reading the
-   * current symlinks _from disk_.
-   */
-  private static Map<PathFragment, PathFragment> resolve(
-      ImmutableList<SymlinkDefinition> symlinkDefinitions,
-      Path workspaceDirectory,
-      String symlinkPrefix) {
-    Map<PathFragment, PathFragment> result = new LinkedHashMap<>();
-    String workspaceBaseName = workspaceDirectory.getBaseName();
-    for (SymlinkDefinition link : symlinkDefinitions) {
-      String linkName = link.getLinkName(symlinkPrefix, workspaceBaseName);
-      PathFragment linkFragment = PathFragment.create(linkName);
-      Path dir = workspaceDirectory.getRelative(linkFragment);
-      try {
-        PathFragment levelOneLinkTarget = dir.readSymbolicLink();
-        if (levelOneLinkTarget.isAbsolute()) {
-          result.put(linkFragment, dir.getRelative(levelOneLinkTarget).asFragment());
-        }
-      } catch (IOException ignored) {
-        // We don't guarantee that the convenience symlinks exist - e.g., we might be running in a
-        // readonly directory. We silently fall back to printing the full path in that case. As an
-        // alternative, we could capture that information when we create the symlinks and pass that
-        // here instead of reading files back from local disk.
-      }
+  /** Describes the outcome of symlink creation. */
+  static class SymlinkCreationResult {
+    private final ImmutableList<ConvenienceSymlink> convenienceSymlinkProtos;
+    private final ImmutableMap<PathFragment, PathFragment> createdSymlinks;
+
+    private SymlinkCreationResult(
+        ImmutableList<ConvenienceSymlink> convenienceSymlinkProtos,
+        ImmutableMap<PathFragment, PathFragment> createdSymlinks) {
+      this.convenienceSymlinkProtos = convenienceSymlinkProtos;
+      this.createdSymlinks = createdSymlinks;
     }
-    return result;
+
+    /** Returns descriptions of what symlinks were created and destroyed. */
+    ImmutableList<ConvenienceSymlink> getConvenienceSymlinkProtos() {
+      return convenienceSymlinkProtos;
+    }
+
+    /**
+     * Returns symlink name -> target mappings of symlinks that were actually created (or in the
+     * case of {@link ConvenienceSymlinksMode#LOG_ONLY}, would have been created).
+     */
+    ImmutableMap<PathFragment, PathFragment> getCreatedSymlinks() {
+      return createdSymlinks;
+    }
   }
 }

src/main/java/com/google/devtools/build/lib/buildtool/PathPrettyPrinter.java
@@ -29,7 +29,8 @@ public final class PathPrettyPrinter {
   private final String symlinkPrefix;
   private final Map<PathFragment, PathFragment> resolvedSymlinks;
 
-  PathPrettyPrinter(String symlinkPrefix, Map<PathFragment, PathFragment> convenienceSymlinks) {
+  public PathPrettyPrinter(
+      String symlinkPrefix, Map<PathFragment, PathFragment> convenienceSymlinks) {
     this.symlinkPrefix = symlinkPrefix;
     this.resolvedSymlinks = convenienceSymlinks;
   }

src/main/java/com/google/devtools/build/lib/runtime/commands/RunCommand.java
@@ -54,7 +54,6 @@ import com.google.devtools.build.lib.buildtool.BuildRequest;
 import com.google.devtools.build.lib.buildtool.BuildRequestOptions;
 import com.google.devtools.build.lib.buildtool.BuildResult;
 import com.google.devtools.build.lib.buildtool.BuildTool;
-import com.google.devtools.build.lib.buildtool.OutputDirectoryLinksUtils;
 import com.google.devtools.build.lib.buildtool.PathPrettyPrinter;
 import com.google.devtools.build.lib.buildtool.buildevent.ExecRequestEvent;
 import com.google.devtools.build.lib.buildtool.buildevent.RunBuildCompleteEvent;
@@ -504,6 +503,7 @@ public class RunCommand implements BlazeCommand {
         targetToRunRunfilesSupport,
         runUnderTarget,
         configuration,
+        result.getConvenienceSymlinks(),
         result.getStopTime());
   }
 
@@ -659,6 +659,7 @@ public class RunCommand implements BlazeCommand {
             redactedCmdLine,
             env,
             builtTargets.configuration,
+            builtTargets.convenienceSymlinks,
             builtTargets.targetToRun,
             builtTargets.runUnderTarget,
             argsFromBinary,
@@ -693,6 +694,7 @@ public class RunCommand implements BlazeCommand {
       List<String> redactedCmdLine,
       CommandEnvironment env,
       BuildConfigurationValue configuration,
+      ImmutableMap<PathFragment, PathFragment> convenienceSymlinks,
       ConfiguredTarget targetToRun,
       ConfiguredTarget runUnderTarget,
       ImmutableList<String> argsFromBinary,
@@ -707,10 +709,7 @@ public class RunCommand implements BlazeCommand {
 
     PathFragment executablePath = executable.getPath().asFragment();
     PathPrettyPrinter prettyPrinter =
-        OutputDirectoryLinksUtils.getPathPrettyPrinter(
-            runtime.getRuleClassProvider().getSymlinkDefinitions(),
-            requestOptions.getSymlinkPrefix(productName),
-            env.getWorkspace());
+        new PathPrettyPrinter(requestOptions.getSymlinkPrefix(productName), convenienceSymlinks);
     PathFragment prettyExecutablePath =
         prettyPrinter.getPrettyPath(executable.getPath().asFragment());
 
@@ -886,6 +885,7 @@ public class RunCommand implements BlazeCommand {
     private final RunfilesSupport targetToRunRunfilesSupport;
     @Nullable private final ConfiguredTarget runUnderTarget;
     private final BuildConfigurationValue configuration;
+    private final ImmutableMap<PathFragment, PathFragment> convenienceSymlinks;
     private final long stopTime;
 
     private BuiltTargets(
@@ -894,12 +894,14 @@ public class RunCommand implements BlazeCommand {
         RunfilesSupport targetToRunRunfilesSupport,
         @Nullable ConfiguredTarget runUnderTarget,
         BuildConfigurationValue configuration,
+        ImmutableMap<PathFragment, PathFragment> convenienceSymlinks,
         long stopTime) {
       this.targetToRun = targetToRun;
       this.runUnderTarget = runUnderTarget;
       this.targetToRunRunfilesDir = targetToRunRunfilesDir;
       this.targetToRunRunfilesSupport = targetToRunRunfilesSupport;
       this.configuration = configuration;
+      this.convenienceSymlinks = convenienceSymlinks;
       this.stopTime = stopTime;
     }
   }

src/main/java/com/google/devtools/build/lib/runtime/commands/TestCommand.java
@@ -15,6 +15,7 @@
 package com.google.devtools.build.lib.runtime.commands;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.devtools.build.lib.analysis.ConfiguredTarget;
 import com.google.devtools.build.lib.analysis.config.CoreOptions;
@@ -23,7 +24,6 @@ import com.google.devtools.build.lib.buildtool.BuildRequestOptions;
 import com.google.devtools.build.lib.buildtool.BuildResult;
 import com.google.devtools.build.lib.buildtool.BuildTool;
 import com.google.devtools.build.lib.buildtool.InstrumentationFilterSupport;
-import com.google.devtools.build.lib.buildtool.OutputDirectoryLinksUtils;
 import com.google.devtools.build.lib.buildtool.PathPrettyPrinter;
 import com.google.devtools.build.lib.buildtool.buildevent.TestingCompleteEvent;
 import com.google.devtools.build.lib.cmdline.RepositoryMapping;
@@ -51,6 +51,7 @@ import com.google.devtools.build.lib.util.DetailedExitCode;
 import com.google.devtools.build.lib.util.InterruptedFailureDetails;
 import com.google.devtools.build.lib.util.io.AnsiTerminalPrinter;
 import com.google.devtools.build.lib.vfs.Path;
+import com.google.devtools.build.lib.vfs.PathFragment;
 import com.google.devtools.common.options.OptionPriority.PriorityCategory;
 import com.google.devtools.common.options.OptionsParser;
 import com.google.devtools.common.options.OptionsParsingException;
@@ -250,12 +251,16 @@ public class TestCommand implements BlazeCommand {
 
     TestResultNotifier notifier =
         new TerminalTestResultNotifier(
-            printer, makeTestLogPathFormatter(options, env), options, mainRepoMapping);
+            printer,
+            makeTestLogPathFormatter(buildResult.getConvenienceSymlinks(), options, env),
+            options,
+            mainRepoMapping);
     return listener.differentialAnalyzeAndReport(
         buildResult.getTestTargets(), buildResult.getSkippedTargets(), validatedTargets, notifier);
   }
 
   private static TestLogPathFormatter makeTestLogPathFormatter(
+      ImmutableMap<PathFragment, PathFragment> convenienceSymlinks,
       OptionsParsingResult options,
       CommandEnvironment env) {
     BlazeRuntime runtime = env.getRuntime();
@@ -266,10 +271,7 @@ public class TestCommand implements BlazeCommand {
     String productName = runtime.getProductName();
     BuildRequestOptions requestOptions = env.getOptions().getOptions(BuildRequestOptions.class);
     PathPrettyPrinter pathPrettyPrinter =
-        OutputDirectoryLinksUtils.getPathPrettyPrinter(
-            runtime.getRuleClassProvider().getSymlinkDefinitions(),
-            requestOptions.getSymlinkPrefix(productName),
-            env.getWorkspace());
+        new PathPrettyPrinter(requestOptions.getSymlinkPrefix(productName), convenienceSymlinks);
     return path -> pathPrettyPrinter.getPrettyPath(path.asFragment()).getPathString();
   }
 }

commit 4bef3a8337e372777bb0e5c296b8619e449c9e57

Fix pretty-print paths for non-skymeld (separate analysis/execution)
While interleaved analysis and execution is on everywhere by default, the old
thing isn't quite dead code, so https://github.com/bazelbuild/bazel/commit/36fa60b1805faa7da2c4b5330b4b186740f5f00d broke it.

PiperOrigin-RevId: 600883379
Change-Id: I0e660d527a4de9005b0ce61cca5a918714990db0

src/main/java/com/google/devtools/build/lib/buildtool/ExecutionTool.java
@@ -101,7 +101,6 @@ import com.google.devtools.build.lib.vfs.OutputService;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.PathFragment;
 import com.google.devtools.build.lib.vfs.Root;
-import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
@@ -398,8 +397,9 @@ public class ExecutionTool {
       createActionLogDirectory();
     }
 
-    handleConvenienceSymlinks(
-        analysisResult.getTargetsToBuild(), analysisResult.getConfiguration());
+    buildResult.setConvenienceSymlinks(
+        handleConvenienceSymlinks(
+            analysisResult.getTargetsToBuild(), analysisResult.getConfiguration()));
 
     BuildRequestOptions options = request.getBuildOptions();
     ActionCache actionCache = null;
@@ -743,7 +743,6 @@ public class ExecutionTool {
    *
    * @return map of convenience symlink name to target
    */
-  @CanIgnoreReturnValue
   public ImmutableMap<PathFragment, PathFragment> handleConvenienceSymlinks(
       ImmutableSet<ConfiguredTarget> targetsToBuild, BuildConfigurationValue configuration) {
     try (SilentCloseable c =