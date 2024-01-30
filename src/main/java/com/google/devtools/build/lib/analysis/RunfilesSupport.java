bazelbuild/bazel

commit 08df6f1ebeaccb43d3aac1058f8d9e28e11acd2b

Move caching from SingleRunfilesSupplier to RunfilesTree.
This removes the last piece of functionality from RunfilesSupplier, thus paving the way for plumbing RunfilesTrees to action execution in through the data path used for plumbing all other kinds of artifact metadata.

(it also paves the way for caching non-test runfiles trees and thus some reduction  in garbage generated, but that's not why I'm doing it)

The strange duplication of sh_test and java_test in the newly added test case is useful because sh_test is the only test rule class that uses the non-Starlark code path.

Also, as a drive-by change, remove SingleRunfilesSupplier.withOverriddenRunfilesDir() (it was unused in production) and SingleRunfilesSupplierTest (SingleRunfilesSupplier is now a very thin shim around Runfiles so it doesn't make much sense to test it)

RELNOTES: None.
PiperOrigin-RevId: 598807682
Change-Id: I627336045923bd0e5f7e4666c68b7bf0401f9bcc

src/main/java/com/google/devtools/build/lib/analysis/RunfilesSupport.java
@@ -23,7 +23,6 @@ import com.google.devtools.build.lib.actions.ActionEnvironment;
 import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.actions.CommandLine;
 import com.google.devtools.build.lib.actions.RunfilesSupplier;
-import com.google.devtools.build.lib.actions.RunfilesSupplier.RunfilesTree;
 import com.google.devtools.build.lib.analysis.SourceManifestAction.ManifestType;
 import com.google.devtools.build.lib.analysis.actions.ActionConstructionContext;
 import com.google.devtools.build.lib.analysis.actions.SymlinkTreeAction;
@@ -37,6 +36,7 @@ import com.google.devtools.build.lib.packages.Type;
 import com.google.devtools.build.lib.vfs.FileSystemUtils;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.PathFragment;
+import java.lang.ref.SoftReference;
 import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.Map;
@@ -86,6 +86,18 @@ public final class RunfilesSupport implements RunfilesSupplier {
     private final Runfiles runfiles;
     private final Artifact repoMappingManifest;
 
+    /**
+     * The cached runfiles mapping. Possible values:
+     *
+     * <ul>
+     *   <li>null if caching is not desired
+     *   <li>A soft reference pointing to null if the cached value is not available (not yet
+     *       computed yet or flushed from RAM)
+     *   <li>A soft reference to the cached value
+     * </ul>
+     */
+    @Nullable private volatile SoftReference<Map<PathFragment, Artifact>> cachedMapping;
+
     private final boolean buildRunfileLinks;
     private final RunfileSymlinksMode runfileSymlinksMode;
 
@@ -94,12 +106,14 @@ public final class RunfilesSupport implements RunfilesSupplier {
         Runfiles runfiles,
         Artifact repoMappingManifest,
         boolean buildRunfileLinks,
+        boolean cacheMapping,
         RunfileSymlinksMode runfileSymlinksMode) {
       this.execPath = execPath;
       this.runfiles = runfiles;
       this.repoMappingManifest = repoMappingManifest;
       this.buildRunfileLinks = buildRunfileLinks;
       this.runfileSymlinksMode = runfileSymlinksMode;
+      this.cachedMapping = cacheMapping ? new SoftReference<>(null) : null;
     }
 
     @Override
@@ -109,8 +123,28 @@ public final class RunfilesSupport implements RunfilesSupplier {
 
     @Override
     public Map<PathFragment, Artifact> getMapping() {
-      return runfiles.getRunfilesInputs(
-          /* eventHandler= */ null, /* location= */ null, repoMappingManifest);
+      if (cachedMapping == null) {
+        return runfiles.getRunfilesInputs(
+            /* eventHandler= */ null, /* location= */ null, repoMappingManifest);
+      }
+
+      Map<PathFragment, Artifact> result = cachedMapping.get();
+      if (result != null) {
+        return result;
+      }
+
+      synchronized (this) {
+        result = cachedMapping.get();
+        if (result != null) {
+          return result;
+        }
+
+        result =
+            runfiles.getRunfilesInputs(
+                /* eventHandler= */ null, /* location= */ null, repoMappingManifest);
+        cachedMapping = new SoftReference<>(result);
+        return result;
+      }
     }
 
     @Override
@@ -161,6 +195,7 @@ public final class RunfilesSupport implements RunfilesSupplier {
         ruleContext.getConfiguration().getRunfileSymlinksMode();
     boolean buildRunfileManifests = ruleContext.getConfiguration().buildRunfileManifests();
     boolean buildRunfileLinks = ruleContext.getConfiguration().buildRunfileLinks();
+    boolean cacheRunfilesMapping = TargetUtils.isTestRule(ruleContext.getTarget());
 
     // Adding run_under target to the runfiles manifest so it would become part
     // of runfiles tree and would be executable everywhere.
@@ -204,6 +239,7 @@ public final class RunfilesSupport implements RunfilesSupplier {
             runfiles,
             repoMappingManifest,
             buildRunfileLinks,
+            cacheRunfilesMapping,
             runfileSymlinksMode);
 
     Artifact runfilesMiddleman =
@@ -445,6 +481,10 @@ public final class RunfilesSupport implements RunfilesSupplier {
   /**
    * Creates and returns a {@link RunfilesSupport} object for the given rule and executable. Note
    * that this method calls back into the passed in rule to obtain the runfiles.
+   *
+   * <p>If the executable is a test, runfiles mappings are cached and re-used between shards. It's a
+   * win since when there is a large number of test shards and/or runs per test, the same runfiles
+   * tree is needed many times.
    */
   public static RunfilesSupport withExecutable(
       RuleContext ruleContext, Runfiles runfiles, Artifact executable) throws InterruptedException {

src/main/java/com/google/devtools/build/lib/analysis/SingleRunfilesSupplier.java
@@ -25,9 +25,7 @@ import com.google.devtools.build.lib.analysis.config.BuildConfigurationValue.Run
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
 import com.google.devtools.build.lib.skyframe.serialization.autocodec.AutoCodec;
 import com.google.devtools.build.lib.vfs.PathFragment;
-import java.lang.ref.SoftReference;
 import java.util.Map;
-import java.util.function.Supplier;
 import javax.annotation.Nullable;
 
 /** {@link RunfilesSupplier} implementation wrapping a single {@link Runfiles} directory mapping. */
@@ -36,33 +34,9 @@ public final class SingleRunfilesSupplier implements RunfilesSupplier, RunfilesT
 
   private final PathFragment runfilesDir;
   private final Runfiles runfiles;
-  private final Supplier<Map<PathFragment, Artifact>> runfilesInputs;
   @Nullable private final Artifact repoMappingManifest;
   private final RunfileSymlinksMode runfileSymlinksMode;
   private final boolean buildRunfileLinks;
-
-  /**
-   * Same as {@link SingleRunfilesSupplier#SingleRunfilesSupplier(PathFragment, Runfiles, boolean,
-   * Artifact, RunfileSymlinksMode, boolean)}, except adds caching for {@linkplain
-   * Runfiles#getRunfilesInputs runfiles inputs}.
-   *
-   * <p>The runfiles inputs are computed lazily and softly cached. Caching is shared across
-   * instances created via {@link #withOverriddenRunfilesDir}.
-   */
-  public static SingleRunfilesSupplier createCaching(
-      PathFragment runfilesDir,
-      Runfiles runfiles,
-      @Nullable Artifact repoMappingManifest,
-      RunfileSymlinksMode runfileSymlinksMode,
-      boolean buildRunfileLinks) {
-    return new SingleRunfilesSupplier(
-        runfilesDir,
-        runfiles,
-        /* runfilesCachingEnabled= */ true,
-        repoMappingManifest,
-        runfileSymlinksMode,
-        buildRunfileLinks);
-  }
 
   /**
    * Create an instance mapping {@code runfiles} to {@code runfilesDir}.
@@ -79,46 +53,9 @@ public final class SingleRunfilesSupplier implements RunfilesSupplier, RunfilesT
       @Nullable Artifact repoMappingManifest,
       RunfileSymlinksMode runfileSymlinksMode,
       boolean buildRunfileLinks) {
-    this(
-        runfilesDir,
-        runfiles,
-        /* runfilesCachingEnabled= */ false,
-        repoMappingManifest,
-        runfileSymlinksMode,
-        buildRunfileLinks);
-  }
-
-  private SingleRunfilesSupplier(
-      PathFragment runfilesDir,
-      Runfiles runfiles,
-      boolean runfilesCachingEnabled,
-      @Nullable Artifact repoMappingManifest,
-      RunfileSymlinksMode runfileSymlinksMode,
-      boolean buildRunfileLinks) {
-    this(
-        runfilesDir,
-        runfiles,
-        runfilesCachingEnabled
-            ? new RunfilesCacher(runfiles, repoMappingManifest)
-            : () ->
-                runfiles.getRunfilesInputs(
-                    /* eventHandler= */ null, /* location= */ null, repoMappingManifest),
-        repoMappingManifest,
-        runfileSymlinksMode,
-        buildRunfileLinks);
-  }
-
-  private SingleRunfilesSupplier(
-      PathFragment runfilesDir,
-      Runfiles runfiles,
-      Supplier<Map<PathFragment, Artifact>> runfilesInputs,
-      @Nullable Artifact repoMappingManifest,
-      RunfileSymlinksMode runfileSymlinksMode,
-      boolean buildRunfileLinks) {
     checkArgument(!runfilesDir.isAbsolute());
     this.runfilesDir = checkNotNull(runfilesDir);
     this.runfiles = checkNotNull(runfiles);
-    this.runfilesInputs = checkNotNull(runfilesInputs);
     this.repoMappingManifest = repoMappingManifest;
     this.runfileSymlinksMode = runfileSymlinksMode;
     this.buildRunfileLinks = buildRunfileLinks;
@@ -127,18 +64,6 @@ public final class SingleRunfilesSupplier implements RunfilesSupplier, RunfilesT
   @Override
   public ImmutableList<RunfilesTree> getRunfilesTrees() {
     return ImmutableList.of(this);
-  }
-
-  public SingleRunfilesSupplier withOverriddenRunfilesDir(PathFragment newRunfilesDir) {
-    return newRunfilesDir.equals(runfilesDir)
-        ? this
-        : new SingleRunfilesSupplier(
-            newRunfilesDir,
-            runfiles,
-            runfilesInputs,
-            repoMappingManifest,
-            runfileSymlinksMode,
-            buildRunfileLinks);
   }
 
   @Override
@@ -153,7 +78,8 @@ public final class SingleRunfilesSupplier implements RunfilesSupplier, RunfilesT
 
   @Override
   public Map<PathFragment, Artifact> getMapping() {
-    return runfilesInputs.get();
+    return runfiles.getRunfilesInputs(
+        /* eventHandler= */ null, /* location= */ null, repoMappingManifest);
   }
 
   @Override
@@ -169,36 +95,5 @@ public final class SingleRunfilesSupplier implements RunfilesSupplier, RunfilesT
   @Override
   public String getWorkspaceName() {
     return runfiles.getSuffix().getPathString();
-  }
-
-  /** Softly caches the result of {@link Runfiles#getRunfilesInputs}. */
-  private static final class RunfilesCacher implements Supplier<Map<PathFragment, Artifact>> {
-
-    private final Runfiles runfiles;
-    @Nullable private final Artifact repoMappingManifest;
-    private volatile SoftReference<Map<PathFragment, Artifact>> ref = new SoftReference<>(null);
-
-    RunfilesCacher(Runfiles runfiles, @Nullable Artifact repoMappingManifest) {
-      this.runfiles = runfiles;
-      this.repoMappingManifest = repoMappingManifest;
-    }
-
-    @Override
-    public Map<PathFragment, Artifact> get() {
-      Map<PathFragment, Artifact> result = ref.get();
-      if (result != null) {
-        return result;
-      }
-      synchronized (this) {
-        result = ref.get();
-        if (result == null) {
-          result =
-              runfiles.getRunfilesInputs(
-                  /*eventHandler=*/ null, /*location=*/ null, repoMappingManifest);
-          ref = new SoftReference<>(result);
-        }
-      }
-      return result;
-    }
   }
 }

src/main/java/com/google/devtools/build/lib/analysis/test/TestActionBuilder.java
@@ -26,7 +26,6 @@ import com.google.devtools.build.lib.actions.ActionOwner;
 import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.actions.ArtifactRoot;
 import com.google.devtools.build.lib.actions.RunfilesSupplier;
-import com.google.devtools.build.lib.actions.RunfilesSupplier.RunfilesTree;
 import com.google.devtools.build.lib.analysis.Allowlist;
 import com.google.devtools.build.lib.analysis.AnalysisEnvironment;
 import com.google.devtools.build.lib.analysis.FileProvider;
@@ -37,7 +36,6 @@ import com.google.devtools.build.lib.analysis.RuleContext;
 import com.google.devtools.build.lib.analysis.RunfilesProvider;
 import com.google.devtools.build.lib.analysis.RunfilesSupport;
 import com.google.devtools.build.lib.analysis.ShToolchain;
-import com.google.devtools.build.lib.analysis.SingleRunfilesSupplier;
 import com.google.devtools.build.lib.analysis.TransitiveInfoCollection;
 import com.google.devtools.build.lib.analysis.actions.LazyWriteNestedSetOfTupleAction;
 import com.google.devtools.build.lib.analysis.config.BuildConfigurationValue;
@@ -353,22 +351,6 @@ public final class TestActionBuilder {
     ImmutableList.Builder<Artifact> coverageArtifacts = ImmutableList.builder();
     ImmutableList.Builder<ActionInput> testOutputs = ImmutableList.builder();
 
-    RunfilesSupplier testRunfilesSupplier;
-    if (shardRuns > 1 || runsPerTest > 1) {
-      // When creating multiple test actions, cache the runfiles mappings across test actions. This
-      // saves a lot of garbage when shard_count and/or runs_per_test is high.
-      RunfilesTree runfilesTree = runfilesSupport.getRunfilesTree();
-      testRunfilesSupplier =
-          SingleRunfilesSupplier.createCaching(
-              runfilesTree.getExecPath(),
-              runfilesSupport.getRunfiles(),
-              runfilesSupport.getRepoMappingManifest(),
-              runfilesTree.getSymlinksMode(),
-              runfilesTree.isBuildRunfileLinks());
-    } else {
-      testRunfilesSupplier = runfilesSupport;
-    }
-
     ActionOwner actionOwner =
         testConfiguration.useTargetPlatformForTests() ? getTestActionOwner() : getOwner();
 
@@ -420,7 +402,7 @@ public final class TestActionBuilder {
             new TestRunnerAction(
                 actionOwner,
                 inputs,
-                testRunfilesSupplier,
+                runfilesSupport,
                 testActionExecutable,
                 testXmlGeneratorExecutable,
                 collectCoverageScript,

src/main/java/com/google/devtools/build/lib/skyframe/serialization/AutoRegistry.java
@@ -58,6 +58,7 @@ public class AutoRegistry {
           "java.io.IOException",
           "java.lang.StackTraceElement",
           "java.lang.invoke.SerializedLambda",
+          "java.lang.ref.SoftReference",
           "java.time.Instant",
           "com.google.common.base.Predicates$InPredicate",
           // Implementation class for com.google.common.base.Optional.

src/main/java/com/google/devtools/build/lib/skyframe/serialization/SoftReferenceCodec.java
@@ -1,0 +1,49 @@
+// Copyright 2024 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.devtools.build.lib.skyframe.serialization;
+
+import com.google.protobuf.CodedInputStream;
+import com.google.protobuf.CodedOutputStream;
+import java.io.IOException;
+import java.lang.ref.SoftReference;
+
+/**
+ * A codec for soft references.
+ *
+ * <p>We must always be prepared for a soft reference to suddenly vanish, so simply not serializing
+ * the referenced object works.
+ */
+@SuppressWarnings({"rawtypes"})
+public class SoftReferenceCodec extends AsyncObjectCodec<SoftReference> {
+  @Override
+  public SoftReference deserializeAsync(
+      AsyncDeserializationContext context, CodedInputStream codedIn)
+      throws SerializationException, IOException {
+    SoftReference result = new SoftReference<>(null);
+    context.registerInitialValue(result);
+    return result;
+  }
+
+  @Override
+  public Class<? extends SoftReference> getEncodedClass() {
+    return SoftReference.class;
+  }
+
+  @Override
+  public void serialize(SerializationContext context, SoftReference obj, CodedOutputStream codedOut)
+      throws SerializationException, IOException {
+    // We don't need to serialize anything; the referenced object is simply discarded since soft
+    // references are only used for caching.
+  }
+}

src/test/java/com/google/devtools/build/lib/analysis/SingleRunfilesSupplierTest.java
@@ -1,152 +1,0 @@
-// Copyright 2015 The Bazel Authors. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package com.google.devtools.build.lib.analysis;
-
-import static com.google.common.collect.ImmutableList.toImmutableList;
-import static com.google.common.truth.Truth.assertThat;
-import static java.util.Arrays.stream;
-
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Iterables;
-import com.google.devtools.build.lib.actions.Artifact;
-import com.google.devtools.build.lib.actions.ArtifactRoot;
-import com.google.devtools.build.lib.actions.ArtifactRoot.RootType;
-import com.google.devtools.build.lib.actions.RunfilesSupplier;
-import com.google.devtools.build.lib.actions.RunfilesSupplier.RunfilesTree;
-import com.google.devtools.build.lib.actions.util.ActionsTestUtil;
-import com.google.devtools.build.lib.analysis.config.BuildConfigurationValue.RunfileSymlinksMode;
-import com.google.devtools.build.lib.vfs.DigestHashFunction;
-import com.google.devtools.build.lib.vfs.PathFragment;
-import com.google.devtools.build.lib.vfs.inmemoryfs.InMemoryFileSystem;
-import java.util.List;
-import java.util.Map;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-
-/** Tests for {@link SingleRunfilesSupplier}. */
-@RunWith(JUnit4.class)
-public final class SingleRunfilesSupplierTest {
-
-  private final ArtifactRoot rootDir =
-      ArtifactRoot.asDerivedRoot(
-          new InMemoryFileSystem(DigestHashFunction.SHA256).getPath("/"),
-          RootType.Output,
-          "fake",
-          "root",
-          "dont",
-          "matter");
-
-  @Test
-  public void testGetArtifactsWithSingleMapping() {
-    List<Artifact> artifacts = mkArtifacts("thing1", "thing2");
-
-    SingleRunfilesSupplier underTest =
-        new SingleRunfilesSupplier(
-            PathFragment.create("notimportant"),
-            mkRunfiles(artifacts),
-            /* repoMappingManifest= */ null,
-            RunfileSymlinksMode.SKIP,
-            /* buildRunfileLinks= */ false);
-
-    assertThat(Iterables.getOnlyElement(underTest.getRunfilesTrees()).getArtifacts().toList())
-        .containsExactlyElementsIn(artifacts);
-  }
-
-  @Test
-  public void withOverriddenRunfilesDir() {
-    SingleRunfilesSupplier original =
-        new SingleRunfilesSupplier(
-            PathFragment.create("old"),
-            Runfiles.EMPTY,
-            /* repoMappingManifest= */ null,
-            RunfileSymlinksMode.SKIP,
-            /* buildRunfileLinks= */ false);
-    RunfilesTree originalTree = Iterables.getOnlyElement(original.getRunfilesTrees());
-
-    PathFragment newDir = PathFragment.create("new");
-    RunfilesSupplier overriddenSupplier = original.withOverriddenRunfilesDir(newDir);
-    RunfilesTree overriddenTree = Iterables.getOnlyElement(overriddenSupplier.getRunfilesTrees());
-
-    assertThat(overriddenTree.getExecPath()).isEqualTo(newDir);
-    assertThat(overriddenTree.getMapping())
-        .isEqualTo(original.getRunfilesTrees().get(0).getMapping());
-    assertThat(overriddenTree.getArtifacts()).isEqualTo(originalTree.getArtifacts());
-  }
-
-  @Test
-  public void withOverriddenRunfilesDir_noChange_sameObject() {
-    PathFragment dir = PathFragment.create("dir");
-    SingleRunfilesSupplier original =
-        new SingleRunfilesSupplier(
-            dir,
-            Runfiles.EMPTY,
-            /* repoMappingManifest= */ null,
-            RunfileSymlinksMode.SKIP,
-            /* buildRunfileLinks= */ false);
-    assertThat(original.withOverriddenRunfilesDir(dir)).isSameInstanceAs(original);
-  }
-
-  @Test
-  public void cachedMappings() {
-    PathFragment dir = PathFragment.create("dir");
-    Runfiles runfiles = mkRunfiles(mkArtifacts("a", "b", "c"));
-    SingleRunfilesSupplier underTest =
-        SingleRunfilesSupplier.createCaching(
-            dir,
-            runfiles,
-            /* repoMappingManifest= */ null,
-            RunfileSymlinksMode.SKIP,
-            /* buildRunfileLinks= */ false);
-
-    Map<PathFragment, Artifact> mapping1 = underTest.getRunfilesTrees().get(0).getMapping();
-    Map<PathFragment, Artifact> mapping2 = underTest.getRunfilesTrees().get(0).getMapping();
-
-    assertThat(mapping1).isEqualTo(runfiles.getRunfilesInputs(null, null, null));
-    assertThat(mapping1).isSameInstanceAs(mapping2);
-  }
-
-  @Test
-  public void cachedMappings_sharedAcrossDirOverrides() {
-    PathFragment oldDir = PathFragment.create("old");
-    PathFragment newDir = PathFragment.create("new");
-    Runfiles runfiles = mkRunfiles(mkArtifacts("a", "b", "c"));
-    SingleRunfilesSupplier original =
-        SingleRunfilesSupplier.createCaching(
-            oldDir,
-            runfiles,
-            /* repoMappingManifest= */ null,
-            RunfileSymlinksMode.SKIP,
-            /* buildRunfileLinks= */ false);
-    SingleRunfilesSupplier overridden = original.withOverriddenRunfilesDir(newDir);
-
-    Map<PathFragment, Artifact> mappingOld = original.getRunfilesTrees().get(0).getMapping();
-    Map<PathFragment, Artifact> mappingNew = overridden.getRunfilesTrees().get(0).getMapping();
-
-    assertThat(mappingOld).isEqualTo(runfiles.getRunfilesInputs(null, null, null));
-    assertThat(mappingNew).isEqualTo(runfiles.getRunfilesInputs(null, null, null));
-    assertThat(mappingOld.get(newDir)).isSameInstanceAs(mappingNew.get(oldDir));
-  }
-
-  private static Runfiles mkRunfiles(Iterable<Artifact> artifacts) {
-    return new Runfiles.Builder("TESTING", false).addArtifacts(artifacts).build();
-  }
-
-  private ImmutableList<Artifact> mkArtifacts(String... paths) {
-    return stream(paths)
-        .map(path -> ActionsTestUtil.createArtifact(rootDir, path))
-        .collect(toImmutableList());
-  }
-}

commit 41acf91534e3b70847a667642ab3acba034a6e6e

Only cache runfiles mappings of tests if they have more than one shard or run per test.
This is an attempt to fix Bazel excessively OOMing at Google. This change was behavior was caused by https://github.com/bazelbuild/bazel/commit/08df6f1ebeaccb43d3aac1058f8d9e28e11acd2b where I believed that the soft reference makes the caching a no-op wrt. memory use.

This does not quite reinstate the same behavior as before https://github.com/bazelbuild/bazel/commit/08df6f1ebeaccb43d3aac1058f8d9e28e11acd2b: firstly, it doesn't take --test_sharding_strategy into account (it could, but I would have had to make TestShardingStrategy public instead of package-internal) and it doesn't garbage collect the soft reference after the all actions of a given test target finish running. It's still an improvement, though, and profiling shows that it has a good chance of fixing the problem.

RELNOTES: None.
PiperOrigin-RevId: 602689571
Change-Id: Ib68c1b88bf47898ae8a59866447d7790bb6eceee

src/main/java/com/google/devtools/build/lib/analysis/RunfilesSupport.java
@@ -29,6 +29,7 @@ import com.google.devtools.build.lib.analysis.actions.SymlinkTreeAction;
 import com.google.devtools.build.lib.analysis.config.BuildConfigurationValue;
 import com.google.devtools.build.lib.analysis.config.BuildConfigurationValue.RunfileSymlinksMode;
 import com.google.devtools.build.lib.analysis.config.RunUnder;
+import com.google.devtools.build.lib.analysis.test.TestConfiguration;
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
 import com.google.devtools.build.lib.concurrent.ThreadSafety.Immutable;
 import com.google.devtools.build.lib.packages.TargetUtils;
@@ -177,6 +178,35 @@ public final class RunfilesSupport implements RunfilesSupplier {
   private final CommandLine args;
   private final ActionEnvironment actionEnvironment;
 
+  private static boolean cacheRunfilesMappings(RuleContext ruleContext) {
+    // The algorithm: only cache runfiles if there is more than one test runner action.
+    // This is because soft references interact with GC in an unexpected way and it's sometimes even
+    // possible to OOM before soft references are collected. Therefore, let's not pay for their
+    // overhead if they are useless (which they are if there is only one test action)
+    if (!TargetUtils.isTestRule(ruleContext.getTarget())) {
+      return false;
+    }
+
+    TestConfiguration testConfiguration = ruleContext.getFragment(TestConfiguration.class);
+    if (testConfiguration == null) {
+      return false;
+    }
+
+    int runsPerTest = testConfiguration.getRunsPerTestForLabel(ruleContext.getLabel());
+    if (runsPerTest > 1) {
+      return true;
+    }
+
+    if (!ruleContext.attributes().has("shard_count", Type.INTEGER)) {
+      return false;
+    }
+
+    int explicitShardCount =
+        ruleContext.attributes().get("shard_count", Type.INTEGER).toIntUnchecked();
+
+    return explicitShardCount > 1;
+  }
+
   /**
    * Creates the RunfilesSupport helper with the given executable and runfiles.
    *
@@ -195,7 +225,6 @@ public final class RunfilesSupport implements RunfilesSupplier {
         ruleContext.getConfiguration().getRunfileSymlinksMode();
     boolean buildRunfileManifests = ruleContext.getConfiguration().buildRunfileManifests();
     boolean buildRunfileLinks = ruleContext.getConfiguration().buildRunfileLinks();
-    boolean cacheRunfilesMapping = TargetUtils.isTestRule(ruleContext.getTarget());
 
     // Adding run_under target to the runfiles manifest so it would become part
     // of runfiles tree and would be executable everywhere.
@@ -239,7 +268,7 @@ public final class RunfilesSupport implements RunfilesSupplier {
             runfiles,
             repoMappingManifest,
             buildRunfileLinks,
-            cacheRunfilesMapping,
+            cacheRunfilesMappings(ruleContext),
             runfileSymlinksMode);
 
     Artifact runfilesMiddleman =