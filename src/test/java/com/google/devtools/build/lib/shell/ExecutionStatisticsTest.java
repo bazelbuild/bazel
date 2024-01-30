9a53dc4e03e5dfdafc877f281ac45bd0e87960dc
Googler <cpovirk@google.com>
2024-01-30 15:27:38 -0800

    Automatic code cleanup.
    PiperOrigin-RevId: 602858303
    Change-Id: Ib5d821808123057e2f011b8a77df749295b3b820

--- src/test/java/com/google/devtools/build/lib/shell/ExecutionStatisticsTest.java
+++ src/test/java/com/google/devtools/build/lib/shell/ExecutionStatisticsTest.java
@@ -15,8 +15,8 @@
 package com.google.devtools.build.lib.shell;
 
 import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth8.assertThat;
 
+import com.google.common.truth.Truth8;
 import com.google.devtools.build.lib.testutil.TestUtils;
 import com.google.devtools.build.lib.vfs.DigestHashFunction;
 import com.google.devtools.build.lib.vfs.FileSystem;
@@ -60,7 +60,7 @@ public final class ExecutionStatisticsTest {
 
     Optional<ExecutionStatistics.ResourceUsage> resourceUsage =
         ExecutionStatistics.getResourceUsage(protoFilename);
-    assertThat(resourceUsage).isEmpty();
+    Truth8.assertThat(resourceUsage).isEmpty();
   }
 
   @Test
@@ -112,7 +112,7 @@ public final class ExecutionStatisticsTest {
 
     Optional<ExecutionStatistics.ResourceUsage> maybeResourceUsage =
         ExecutionStatistics.getResourceUsage(protoFilename);
-    assertThat(maybeResourceUsage).isPresent();
+    Truth8.assertThat(maybeResourceUsage).isPresent();
     ExecutionStatistics.ResourceUsage resourceUsage = maybeResourceUsage.get();
 
     assertThat(resourceUsage.getUserExecutionTime()).isEqualTo(riggedUserExecutionTime);
