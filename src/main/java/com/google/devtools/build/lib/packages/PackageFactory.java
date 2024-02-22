a59deaedc3d92ca87f29c1ec8f5b2ef20a0036bd
Googler <twerth@google.com>
2024-02-22 03:02:07 -0800

    Repeat https://github.com/bazelbuild/bazel/commit/d9b0cfd78efd2130fa24860fcb3e3f7a2170dbf4 for profiling of Starlark file parsing.
    PiperOrigin-RevId: 609306213
    Change-Id: I3227bf3175f7b7121b9e4fc4928f475c9adc1ba4

--- src/main/java/com/google/devtools/build/lib/packages/PackageFactory.java
+++ src/main/java/com/google/devtools/build/lib/packages/PackageFactory.java
@@ -35,7 +35,6 @@ import com.google.devtools.build.lib.packages.WorkspaceFileValue.WorkspaceFileKe
 import com.google.devtools.build.lib.packages.semantics.BuildLanguageOptions;
 import com.google.devtools.build.lib.profiler.Profiler;
 import com.google.devtools.build.lib.profiler.ProfilerTask;
-import com.google.devtools.build.lib.profiler.SilentCloseable;
 import com.google.devtools.build.lib.server.FailureDetails.FailureDetail;
 import com.google.devtools.build.lib.server.FailureDetails.PackageLoading;
 import com.google.devtools.build.lib.server.FailureDetails.PackageLoading.Code;
@@ -549,13 +548,14 @@ public final class PackageFactory {
     StarlarkFile.setParseProfiler(
         new StarlarkFile.ParseProfiler() {
           @Override
-          public Object start(String filename) {
-            return Profiler.instance().profile(ProfilerTask.STARLARK_PARSER, filename);
+          public long start() {
+            return Profiler.nanoTimeMaybe();
           }
 
           @Override
-          public void end(Object span) {
-            ((SilentCloseable) span).close();
+          public void end(long startTimeNanos, String filename) {
+            Profiler.instance()
+                .completeTask(startTimeNanos, ProfilerTask.STARLARK_PARSER, filename);
           }
         });
 
