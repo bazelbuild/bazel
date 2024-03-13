bazelbuild/bazel

commit 5c8d1d06a501a995c81a7ca8de054866d2f4df5c

Freeze mutability right after evaluating the rule's implementation function
This makes the postprocessing safer. That the providers (and the data within them) are frozen is needed before we can change their memory layout. That is, for a follow up change that will reduce their memory footprint.

Move the postprocessing part, call to createTarget, from the end of buildRule call, a level higher, to ConfiguredTargetFactory.
Pass only RuleContext to createTarget, because StarlarkRuleContext is closed.
Fish isDefaultExecutableCreated from StarlarkRuleContext and knit it through createTarget to its consumers.
Inline BadRuleImplementation exception.
Move try-finally block in ConfiguredTargetFactory one level deeper, to mak eit possible to close rule context sooner.
Store lookedUpVariables in RuleContext after StarlarkRuleContext is closed - they are needed when constructing configured target.

PiperOrigin-RevId: 510986457
Change-Id: Ic8837b50dd25a15bd30908d3445873d967394ea2

src/main/java/com/google/devtools/build/lib/analysis/ConfiguredTargetFactory.java
@@ -327,7 +327,7 @@ public final class ConfiguredTargetFactory {
       return erroredConfiguredTargetWithFailures(ruleContext, analysisFailures);
     }
     if (ruleContext.hasErrors()) {
-      return erroredConfiguredTarget(ruleContext);
+      return erroredConfiguredTarget(ruleContext, null);
     }
 
     try {
@@ -353,27 +353,58 @@ public final class ConfiguredTargetFactory {
         return createFailConfiguredTargetForMissingFragmentClass(ruleContext, missingFragmentClass);
       }
 
-      try {
-        ConfiguredTarget target;
-        if (ruleClass.isStarlark()) {
+      final ConfiguredTarget target;
+
+      if (ruleClass.isStarlark()) {
+        final Object rawProviders;
+        final boolean isDefaultExecutableCreated;
+        @Nullable final RequiredConfigFragmentsProvider requiredConfigFragmentsProvider;
+        try {
+          ruleContext.initStarlarkRuleContext();
           // TODO(bazel-team): maybe merge with RuleConfiguredTargetBuilder?
-          target =
-              StarlarkRuleConfiguredTargetUtil.buildRule(
-                  ruleContext, ruleClass.getAdvertisedProviders());
-        } else {
+          rawProviders = StarlarkRuleConfiguredTargetUtil.evalRule(ruleContext);
+        } finally {
+          // TODO(b/268525292): isDefaultExecutableCreated is set to True when
+          // ctx.outputs.executable
+          // is accessed in the implementation. This fragile mechanism should be revised and removed
+          isDefaultExecutableCreated =
+              ruleContext.getStarlarkRuleContext().isDefaultExecutableCreated();
+          requiredConfigFragmentsProvider = ruleContext.getRequiredConfigFragments();
+          ruleContext.close();
+        }
+        if (rawProviders == null) {
+          return erroredConfiguredTarget(ruleContext, requiredConfigFragmentsProvider);
+        }
+        // Because ruleContext was closed, rawProviders are now immutable
+        // Postprocess providers to create the finished target.
+        target =
+            StarlarkRuleConfiguredTargetUtil.createTarget(
+                ruleContext,
+                rawProviders,
+                ruleClass.getAdvertisedProviders(),
+                isDefaultExecutableCreated,
+                requiredConfigFragmentsProvider); // may be null
+        return target != null
+            ? target
+            : erroredConfiguredTarget(ruleContext, requiredConfigFragmentsProvider);
+      } else {
+        try {
           target =
               Preconditions.checkNotNull(
                       ruleClass.getConfiguredTargetFactory(RuleConfiguredTargetFactory.class),
                       "No configured target factory for %s",
                       ruleClass)
                   .create(ruleContext);
+
+        } finally {
+          // close() is required if the native rule created StarlarkRuleContext to perform any
+          // Starlark evaluation, i.e. using the @_builtins mechanism.
+          ruleContext.close();
         }
-        return target != null ? target : erroredConfiguredTarget(ruleContext);
-      } finally {
-        ruleContext.close();
+        return target != null ? target : erroredConfiguredTarget(ruleContext, null);
       }
     } catch (RuleErrorException ruleErrorException) {
-      return erroredConfiguredTarget(ruleContext);
+      return erroredConfiguredTarget(ruleContext, null);
     }
   }
 
@@ -428,8 +459,11 @@ public final class ConfiguredTargetFactory {
    * allowed in this build, this returns a stub {@link ConfiguredTarget} which contains information
    * about the failure.
    */
+  // TODO(blaze-team): requiredConfigFragmentsProvider is used for Android feature flags and should
+  // be removed together with them.
   @Nullable
-  private static ConfiguredTarget erroredConfiguredTarget(RuleContext ruleContext)
+  private static ConfiguredTarget erroredConfiguredTarget(
+      RuleContext ruleContext, RequiredConfigFragmentsProvider requiredConfigFragmentsProvider)
       throws ActionConflictException, InterruptedException, AnalysisFailurePropagationException {
     if (ruleContext.getConfiguration().allowAnalysisFailures()) {
       ImmutableList.Builder<AnalysisFailure> analysisFailures = ImmutableList.builder();
@@ -441,6 +475,9 @@ public final class ConfiguredTargetFactory {
       builder.addNativeDeclaredProvider(
           AnalysisFailureInfo.forAnalysisFailures(analysisFailures.build()));
       builder.addProvider(RunfilesProvider.class, RunfilesProvider.simple(Runfiles.EMPTY));
+      if (requiredConfigFragmentsProvider != null) {
+        builder.addProvider(requiredConfigFragmentsProvider);
+      }
       ConfiguredTarget configuredTarget = builder.build();
       if (configuredTarget == null) {
         // See comment in erroredConfiguredTargetWithFailures.

src/main/java/com/google/devtools/build/lib/analysis/RuleConfiguredTargetBuilder.java
@@ -312,14 +312,19 @@ public final class RuleConfiguredTargetBuilder {
   /**
    * Adds {@link RequiredConfigFragmentsProvider} if {@link
    * CoreOptions#includeRequiredConfigFragmentsProvider} isn't {@link
-   * CoreOptions.IncludeConfigFragmentsEnum#OFF}.
+   * CoreOptions.IncludeConfigFragmentsEnum#OFF} and if the provider is not already present.
+   *
+   * <p>For Stalark rules the provider is already added in {@link
+   * com.google.devtools.build.lib.analysis.starlark.StarlarkRuleConfiguredTargetUtil}.
    *
    * <p>See {@link com.google.devtools.build.lib.analysis.config.RequiredFragmentsUtil} for a
    * description of the meaning of this provider's content. That class contains methods that
    * populate the results of {@link RuleContext#getRequiredConfigFragments}.
    */
+  // TODO(blaze-team): Simplify the conditional logic and make it easier to understand.
   private void maybeAddRequiredConfigFragmentsProvider() {
-    if (ruleContext.shouldIncludeRequiredConfigFragmentsProvider()) {
+    if (ruleContext.shouldIncludeRequiredConfigFragmentsProvider()
+        && !providersBuilder.contains(RequiredConfigFragmentsProvider.class)) {
       addProvider(ruleContext.getRequiredConfigFragments());
     }
   }

src/main/java/com/google/devtools/build/lib/analysis/RuleContext.java
@@ -1207,6 +1207,7 @@ public final class RuleContext extends TargetContext
     starlarkThread.mutability().freeze();
     if (starlarkRuleContext != null) {
       starlarkRuleContext.nullify();
+      starlarkRuleContext = null;
     }
   }
 
@@ -1305,8 +1306,16 @@ public final class RuleContext extends TargetContext
     return ruleClassProvider;
   }
 
-  /** Returns the configuration fragments this rule uses. */
+  /**
+   * Returns the configuration fragments this rule uses if it should be included for this rule.
+   * Otherwise it returns null.
+   */
+  @Nullable
   public RequiredConfigFragmentsProvider getRequiredConfigFragments() {
+    if (requiredConfigFragments == null) {
+      return null;
+    }
+
     RequiredConfigFragmentsProvider.Builder merged = null;
 
     // Add variables accessed through ctx.var, if this is a Starlark rule.

commit 559f37955aad9cefdda40c3b4327af33052abf2a

Fix NPE when initStarlarkRuleContext throws, e.g. due output key collision
If ruleContext.initStarlarkRuleContext() throws, the starlark rule context
remains uninitialized, so we cannot use ruleContext.getStarlarkRuleContext();
thus, we must move all calls to ruleContext.getStarlarkRuleContext() out of
the `finally` and into the main `try` body.

This crash was introduced by 5c8d1d06a501a995c81a7ca8de054866d2f4df5c

PiperOrigin-RevId: 615556234
Change-Id: I6567566080b8239b9fc03a83fe5a5f20e21a4d79

src/main/java/com/google/devtools/build/lib/analysis/ConfiguredTargetFactory.java
@@ -376,16 +376,17 @@ public final class ConfiguredTargetFactory {
         final boolean isDefaultExecutableCreated;
         @Nullable final RequiredConfigFragmentsProvider requiredConfigFragmentsProvider;
         try {
+          // must be called before any calls to ruleContext.getStarlarkRuleContext()
           ruleContext.initStarlarkRuleContext();
           // TODO(bazel-team): maybe merge with RuleConfiguredTargetBuilder?
           rawProviders = StarlarkRuleConfiguredTargetUtil.evalRule(ruleContext, ruleClass);
-        } finally {
           // TODO(b/268525292): isDefaultExecutableCreated is set to True when
           // ctx.outputs.executable
           // is accessed in the implementation. This fragile mechanism should be revised and removed
           isDefaultExecutableCreated =
               ruleContext.getStarlarkRuleContext().isDefaultExecutableCreated();
           requiredConfigFragmentsProvider = ruleContext.getRequiredConfigFragments();
+        } finally {
           ruleContext.close();
         }
         if (rawProviders == null) {

src/test/java/com/google/devtools/build/lib/starlark/StarlarkRuleContextTest.java
@@ -932,6 +932,29 @@ public final class StarlarkRuleContextTest extends BuildViewTestCase {
     assertThat(((Artifact) Iterables.getOnlyElement(result)).getFilename()).isEqualTo("d.txt");
   }
 
+  // Regression test for b/329066920
+  @Test
+  public void outputsAddOutput_keyCollision_failsCleanly() throws Exception {
+    scratch.file(
+        "test/rules.bzl",
+        "def _undertest_impl(ctx):",
+        "    pass",
+        "",
+        "undertest_rule = rule(",
+        "    implementation = _undertest_impl,",
+        "    attrs = {'out_collision': attr.output(),},",
+        "    outputs = {'out_collision': '%{name}.out'},",
+        ")");
+    scratch.file(
+        "test/BUILD",
+        "load(':rules.bzl', 'undertest_rule')",
+        "undertest_rule(",
+        "    name = 'undertest',",
+        ")");
+
+    checkError("//test:undertest", "Multiple outputs with the same key: out_collision");
+  }
+
   @Test
   public void testStarlarkRuleContextGetDefaultShellEnv() throws Exception {
     setRuleContext(createRuleContext("//foo:foo"));