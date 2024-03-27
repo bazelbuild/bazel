c576d5da47948680844168ea6414cc45541525a8
salma-samy <salmasamy@google.com>
2024-03-27 08:32:19 -0700

    Use build with no-build for fetching targets (Fixes #21728)
    Using cquery for fetching targets caused some problems #21728 as cquery is not prepared to deal with targets including `...`
    
    Updated the logic to use build command with no-build option, which should operate the same and actually do less work than cquery
    
    PiperOrigin-RevId: 619540572
    Change-Id: Id26a61229c13bd12373478fe17e67200f2760be2

--- src/main/java/com/google/devtools/build/lib/buildtool/CqueryProcessor.java
+++ src/main/java/com/google/devtools/build/lib/buildtool/CqueryProcessor.java
@@ -16,7 +16,6 @@ package com.google.devtools.build.lib.buildtool;
 import com.google.common.collect.ImmutableList;
 import com.google.devtools.build.lib.cmdline.TargetPattern;
 import com.google.devtools.build.lib.packages.semantics.BuildLanguageOptions;
-import com.google.devtools.build.lib.query2.NamedThreadSafeOutputFormatterCallback;
 import com.google.devtools.build.lib.query2.PostAnalysisQueryEnvironment.TopLevelConfigurations;
 import com.google.devtools.build.lib.query2.common.CqueryNode;
 import com.google.devtools.build.lib.query2.cquery.ConfiguredTargetQueryEnvironment;
@@ -27,30 +26,14 @@ import com.google.devtools.build.lib.runtime.CommandEnvironment;
 import com.google.devtools.build.skyframe.SkyKey;
 import com.google.devtools.build.skyframe.WalkableGraph;
 import java.util.Collection;
-import java.util.Optional;
 import net.starlark.java.eval.StarlarkSemantics;
 
 /** Performs {@code cquery} processing. */
 public final class CqueryProcessor extends PostAnalysisQueryProcessor<CqueryNode> {
 
-  /**
-   * Only passed when this is a call from a non query command like Fetch or Vendor, where we don't
-   * need the output printed
-   */
-  private Optional<NamedThreadSafeOutputFormatterCallback<CqueryNode>> noOutputFormatter;
-
   public CqueryProcessor(
       QueryExpression queryExpression, TargetPattern.Parser mainRepoTargetParser) {
     super(queryExpression, mainRepoTargetParser);
-    this.noOutputFormatter = Optional.empty();
-  }
-
-  public CqueryProcessor(
-      QueryExpression queryExpression,
-      TargetPattern.Parser mainRepoTargetParser,
-      Optional<NamedThreadSafeOutputFormatterCallback<CqueryNode>> noOutputFormatter) {
-    this(queryExpression, mainRepoTargetParser);
-    this.noOutputFormatter = noOutputFormatter;
   }
 
   @Override
@@ -83,7 +66,6 @@ public final class CqueryProcessor extends PostAnalysisQueryProcessor<CqueryNode
         request.getTopLevelArtifactContext(),
         request
             .getOptions(CqueryOptions.class)
-            .getLabelPrinter(starlarkSemantics, mainRepoTargetParser.getRepoMapping()),
-        noOutputFormatter);
+            .getLabelPrinter(starlarkSemantics, mainRepoTargetParser.getRepoMapping()));
   }
 }
