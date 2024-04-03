4b0d4533b8e8223d7da920ad44ef89928b3e1cfd
Fabian Meumertzheim <fabian@meumertzhe.im>
2024-04-03 17:24:50 +0200

    Make `cc_shared_library` runnable standalone
    When loaded as a `data` dependency (e.g. for `dlopen`), the runfiles of
    a `cc_shared_library` now contain the `_solib` symlinks of precompiled
    dynamic library dependencies instead of the symlink targets.

--- src/main/starlark/tests/builtins_bzl/cc/cc_shared_library/test_cc_shared_library/python_test.py
+++ src/main/starlark/tests/builtins_bzl/cc/cc_shared_library/test_cc_shared_library/python_test.py
@@ -11,3 +11,25 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# Load the shared library from data and call the "foo" function defined by it to
+# verify that its runfiles are sufficient to run the shared library.
+import ctypes
+import os
+import unittest
+import python.runfiles as runfiles
+
+
+class TestStandaloneSharedLibrary(unittest.TestCase):
+
+  def test_call_foo(self):
+    lib_path = runfiles.Create().Rlocation(os.getenv("FOO_SO"))
+    lib = ctypes.CDLL(lib_path)
+    if lib_path.endswith(".dll"):
+      self.assertEqual(getattr(lib, "?foo@@YAHXZ")(), 42)
+    else:
+      self.assertEqual(lib._Z3foov(), 42)
+
+
+if __name__ == "__main__":
+  unittest.main()
