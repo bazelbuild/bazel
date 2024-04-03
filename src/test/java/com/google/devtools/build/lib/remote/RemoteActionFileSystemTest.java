bazelbuild/bazel

commit 4247c20f1daeeb787e9b5cb64848d398467effb1

Traverse symlinks to directories while collecting a TreeArtifactValue.
As explained in https://github.com/bazelbuild/bazel/issues/20418, when a tree artifact contains a symlink to a directory, it is collected as a single TreeFileArtifact with DirectoryArtifactValue metadata. With this change, the symlink is followed and the directory expanded into its contents, which is more incrementally correct and removes a special case that tree artifact consumers would otherwise have to be aware of.

This also addresses https://github.com/bazelbuild/bazel/issues/21171, which is due to the metadata for the directory contents not being available when building without the bytes, causing the input Merkle tree builder to fail. (While I could have fixed this by falling back to reading the directory contents from the filesystem, I prefer to abide by the principle that input metadata should be collected before execution; source directories are the other case where this isn't true, which I also regard as a bug.)

Fixes https://github.com/bazelbuild/bazel/issues/20418.
Fixes https://github.com/bazelbuild/bazel/issues/21171.

PiperOrigin-RevId: 608389141
Change-Id: I956f3f8a4b1bfd279091e179d1cba3cdd0e5019b

src/main/java/com/google/devtools/build/lib/skyframe/TreeArtifactValue.java
@@ -43,6 +43,7 @@ import com.google.devtools.build.lib.skyframe.serialization.VisibleForSerializat
 import com.google.devtools.build.lib.skyframe.serialization.autocodec.SerializationConstant;
 import com.google.devtools.build.lib.util.Fingerprint;
 import com.google.devtools.build.lib.vfs.Dirent;
+import com.google.devtools.build.lib.vfs.FileStatus;
 import com.google.devtools.build.lib.vfs.IORuntimeException;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.PathFragment;
@@ -487,20 +488,34 @@ public class TreeArtifactValue implements HasDigest, SkyValue {
      * Called for every directory entry encountered during tree traversal, in a nondeterministic
      * order.
      *
-     * <p>Symlinks are not followed during traversal and are simply reported as {@link
-     * Dirent.Type#SYMLINK} regardless of whether they point to a file, directory, or are dangling.
+     * <p>Regular files and directories are reported as {@link Dirent.Type.FILE} or {@link
+     * Dirent.Type.DIRECTORY}, respectively. Directories are traversed recursively.
      *
-     * <p>{@code type} is guaranteed never to be {@link Dirent.Type#UNKNOWN}, since if this type is
-     * encountered, {@link IOException} is immediately thrown without invoking the visitor.
+     * <p>Symlinks that resolve to an existing file or directory are followed and reported as the
+     * regular files or directories they point to, recursively for directories. Symlinks that fail
+     * to resolve to an existing path cause an {@link IOException} to be immediately thrown without
+     * invoking the visitor. Thus, the visitor is never called with a {@link Dirent.Type.SYMLINK}
+     * type.
      *
-     * <p>If the implementation throws {@link IOException}, traversal is immediately halted and the
+     * <p>Special files or files whose type could not be determined, regardless of whether they are
+     * encountered directly or indirectly through symlinks, cause an {@link IOException} to be
+     * immediately thrown without invoking the visitor. Thus, the visitor is never called with a
+     * {@link Dirent.Type.UNKNOWN} type.
+     *
+     * <p>The {@code parentRelativePath} argument is always set to the apparent path relative to the
+     * tree directory root, without resolving any intervening symlinks. The {@code traversedSymlink}
+     * argument is true if at least one symlink was traversed on the way to the entry being
+     * reported.
+     *
+     * <p>If the visitor throws {@link IOException}, traversal is immediately halted and the
      * exception is propagated.
      *
      * <p>This method can be called from multiple threads in parallel during a single call of {@link
      * TreeArtifactVisitor#visitTree(Path, TreeArtifactVisitor)}.
      */
     @ThreadSafe
-    void visit(PathFragment parentRelativePath, Dirent.Type type) throws IOException;
+    void visit(PathFragment parentRelativePath, Dirent.Type type, boolean traversedSymlink)
+        throws IOException;
   }
 
   /** An {@link AbstractQueueVisitor} that visits every file in the tree artifact. */
@@ -519,7 +534,12 @@ public class TreeArtifactValue implements HasDigest, SkyValue {
     }
 
     void run() throws IOException, InterruptedException {
-      execute(() -> visit(PathFragment.EMPTY_FRAGMENT, Dirent.Type.DIRECTORY));
+      execute(
+          () ->
+              visit(
+                  PathFragment.EMPTY_FRAGMENT,
+                  Dirent.Type.DIRECTORY,
+                  /* traversedSymlink= */ false));
       try {
         awaitQuiescence(true);
       } catch (IORuntimeException e) {
@@ -527,25 +547,49 @@ public class TreeArtifactValue implements HasDigest, SkyValue {
       }
     }
 
-    private void visit(PathFragment parentRelativePath, Dirent.Type type) {
+    private void visit(
+        PathFragment parentRelativePath, Dirent.Type type, boolean traversedSymlink) {
       try {
         Path path = parentDir.getRelative(parentRelativePath);
 
-        if (type == Dirent.Type.UNKNOWN) {
-          throw new IOException("Could not determine type of file for " + path.getPathString());
-        }
-
         if (type == Dirent.Type.SYMLINK) {
           checkSymlink(parentRelativePath.getParentDirectory(), path);
+
+          traversedSymlink = true;
+
+          FileStatus statFollow = path.statIfFound(Symlinks.FOLLOW);
+
+          if (statFollow == null) {
+            throw new IOException(
+                String.format(
+                    "Child %s of tree artifact %s is a dangling symbolic link",
+                    parentRelativePath, parentDir));
+          }
+
+          if (statFollow.isFile() && !statFollow.isSpecialFile()) {
+            type = Dirent.Type.FILE;
+          } else if (statFollow.isDirectory()) {
+            type = Dirent.Type.DIRECTORY;
+          } else {
+            type = Dirent.Type.UNKNOWN;
+          }
         }
 
-        visitor.visit(parentRelativePath, type);
+        if (type == Dirent.Type.UNKNOWN) {
+          throw new IOException(
+              String.format(
+                  "Child %s of tree artifact %s has an unsupported type",
+                  parentRelativePath, parentDir));
+        }
+
+        visitor.visit(parentRelativePath, type, traversedSymlink);
 
         if (type == Dirent.Type.DIRECTORY) {
           for (Dirent dirent : path.readdir(Symlinks.NOFOLLOW)) {
             PathFragment childPath = parentRelativePath.getChild(dirent.getName());
             Dirent.Type childType = dirent.getType();
-            execute(() -> visit(childPath, childType));
+            boolean finalTraversedSymlink = traversedSymlink;
+            execute(() -> visit(childPath, childType, finalTraversedSymlink));
           }
         }
       } catch (IOException e) {

src/test/java/com/google/devtools/build/lib/skyframe/TreeArtifactBuildTest.java
@@ -405,6 +405,61 @@ public final class TreeArtifactBuildTest extends TimestampBuilderTestCase {
   }
 
   @Test
+  public void doesNotSetPermissionsAfterTraversingSymlink() throws Exception {
+    SpecialArtifact out = createTreeArtifact("output");
+
+    Path fileTarget = scratch.file("file");
+    writeFile(fileTarget, "file");
+
+    Path dirTarget = scratch.dir("dir");
+    Path dirFileTarget = dirTarget.getChild("file");
+    writeFile(dirFileTarget, "dir/file");
+
+    Action action =
+        new SimpleTestAction(out) {
+          @Override
+          void run(ActionExecutionContext context) throws IOException {
+            out.getPath().getChild("file_link").createSymbolicLink(fileTarget.asFragment());
+            out.getPath().getChild("dir_link").createSymbolicLink(dirTarget.asFragment());
+          }
+        };
+
+    registerAction(action);
+    buildArtifact(out);
+
+    assertThat(fileTarget.isWritable()).isTrue();
+    assertThat(dirTarget.isWritable()).isTrue();
+    assertThat(dirFileTarget.isWritable()).isTrue();
+  }
+
+  @Test
+  public void symlinkLoopRejected() throws Exception {
+    // Failure expected
+    EventCollector eventCollector = new EventCollector(EventKind.ERROR);
+    reporter.removeHandler(failFastHandler);
+    reporter.addHandler(eventCollector);
+
+    SpecialArtifact out = createTreeArtifact("output");
+
+    Action action =
+        new SimpleTestAction(out) {
+          @Override
+          void run(ActionExecutionContext context) throws IOException {
+            writeFile(out.getPath().getRelative("dir/file"), "contents");
+            out.getPath().getRelative("dir/sym").createSymbolicLink(PathFragment.create("../dir"));
+          }
+        };
+
+    registerAction(action);
+    assertThrows(BuildFailedException.class, () -> buildArtifact(out));
+
+    ImmutableList<Event> errors = ImmutableList.copyOf(eventCollector);
+    assertThat(errors).hasSize(2);
+    assertThat(errors.get(0).getMessage()).contains("Too many levels of symbolic links");
+    assertThat(errors.get(1).getMessage()).contains("not all outputs were created or valid");
+  }
+
+  @Test
   public void validRelativeSymlinkAccepted() throws Exception {
     SpecialArtifact out = createTreeArtifact("output");
 
@@ -448,7 +503,9 @@ public final class TreeArtifactBuildTest extends TimestampBuilderTestCase {
 
     List<Event> errors = ImmutableList.copyOf(eventCollector);
     assertThat(errors).hasSize(2);
-    assertThat(errors.get(0).getMessage()).contains("Failed to resolve relative path links/link");
+    assertThat(errors.get(0).getMessage())
+        .contains(
+            "Child links/link of tree artifact " + out.getPath() + " is a dangling symbolic link");
     assertThat(errors.get(1).getMessage()).contains("not all outputs were created or valid");
   }
 
@@ -477,7 +534,9 @@ public final class TreeArtifactBuildTest extends TimestampBuilderTestCase {
 
     List<Event> errors = ImmutableList.copyOf(eventCollector);
     assertThat(errors).hasSize(2);
-    assertThat(errors.get(0).getMessage()).contains("Failed to resolve relative path links/link");
+    assertThat(errors.get(0).getMessage())
+        .contains(
+            "Child links/link of tree artifact " + out.getPath() + " is a dangling symbolic link");
     assertThat(errors.get(1).getMessage()).contains("not all outputs were created or valid");
   }
 
@@ -852,17 +911,18 @@ public final class TreeArtifactBuildTest extends TimestampBuilderTestCase {
     assertThat(artifact2.getPath().getDirectoryEntries()).isEmpty();
   }
 
-  // This happens in the wild. See https://github.com/bazelbuild/bazel/issues/11813.
   @Test
-  public void treeArtifactContainsSymlinkToDirectory() throws Exception {
+  public void treeArtifactWithSymlinkToFile() throws Exception {
     SpecialArtifact treeArtifact = createTreeArtifact("tree");
     registerAction(
-        new SimpleTestAction(/*output=*/ treeArtifact) {
+        new SimpleTestAction(/* output= */ treeArtifact) {
           @Override
           void run(ActionExecutionContext context) throws IOException {
-            PathFragment subdir = PathFragment.create("subdir");
-            touchFile(treeArtifact.getPath().getRelative(subdir).getRelative("file"));
-            treeArtifact.getPath().getRelative("link").createSymbolicLink(subdir);
+            touchFile(treeArtifact.getPath().getRelative("subdir/file"));
+            treeArtifact
+                .getPath()
+                .getRelative("link")
+                .createSymbolicLink(PathFragment.create("subdir/file"));
           }
         });
 
@@ -872,6 +932,29 @@ public final class TreeArtifactBuildTest extends TimestampBuilderTestCase {
         .containsExactly(
             TreeFileArtifact.createTreeOutput(treeArtifact, "subdir/file"),
             TreeFileArtifact.createTreeOutput(treeArtifact, "link"));
+  }
+
+  @Test
+  public void treeArtifactWithSymlinkToDirectory() throws Exception {
+    SpecialArtifact treeArtifact = createTreeArtifact("tree");
+    registerAction(
+        new SimpleTestAction(/* output= */ treeArtifact) {
+          @Override
+          void run(ActionExecutionContext context) throws IOException {
+            touchFile(treeArtifact.getPath().getRelative("subdir/file"));
+            treeArtifact
+                .getPath()
+                .getRelative("link")
+                .createSymbolicLink(PathFragment.create("subdir"));
+          }
+        });
+
+    TreeArtifactValue tree = buildArtifact(treeArtifact);
+
+    assertThat(tree.getChildren())
+        .containsExactly(
+            TreeFileArtifact.createTreeOutput(treeArtifact, "subdir/file"),
+            TreeFileArtifact.createTreeOutput(treeArtifact, "link/file"));
   }
 
   private abstract static class SimpleTestAction extends TestAction {

commit 33a2ce6722379a0cba6bc043a3b39e5309895d61

[7.1.2] Implement RemoteActionFileSystem#statIfFound correctly when the path cannot be canonicalized (#21889)
This improves the error message for a tree artifact containing a
dangling symlink, which regressed in
https://github.com/bazelbuild/bazel/commit/4247c20f1daeeb787e9b5cb64848d398467effb1
(see
https://github.com/bazelbuild/bazel/issues/15454#issuecomment-2011456094).

PiperOrigin-RevId: 617870632
Change-Id: I6847084a52b1e4bb7d8a9384ad6cd5d015dddf1b

Commit
https://github.com/bazelbuild/bazel/commit/b78d73fab1f01e2e8ae29bed667865a42800b626

Co-authored-by: Googler <tjgq@google.com>

src/main/java/com/google/devtools/build/lib/remote/RemoteActionFileSystem.java
@@ -630,13 +630,17 @@ public class RemoteActionFileSystem extends AbstractFileSystemWithCustomStat
   private FileStatus statInternal(PathFragment path, FollowMode followMode, StatSources statSources)
       throws IOException {
     // Canonicalize the path.
-    if (followMode == FollowMode.FOLLOW_ALL) {
-      path = resolveSymbolicLinks(path).asFragment();
-    } else if (followMode == FollowMode.FOLLOW_PARENT) {
-      PathFragment parent = path.getParentDirectory();
-      if (parent != null) {
-        path = resolveSymbolicLinks(parent).asFragment().getChild(path.getBaseName());
+    try {
+      if (followMode == FollowMode.FOLLOW_ALL) {
+        path = resolveSymbolicLinks(path).asFragment();
+      } else if (followMode == FollowMode.FOLLOW_PARENT) {
+        PathFragment parent = path.getParentDirectory();
+        if (parent != null) {
+          path = resolveSymbolicLinks(parent).asFragment().getChild(path.getBaseName());
+        }
       }
+    } catch (FileNotFoundException e) {
+      return null;
     }
 
     // Since the path has been canonicalized, the operations below never need to follow symlinks.

src/main/java/com/google/devtools/build/lib/skyframe/SkyframeActionExecutor.java
@@ -1488,11 +1488,12 @@ public final class SkyframeActionExecutor {
       Action action, Artifact output, Reporter reporter, IOException e) {
     String errorMessage;
     if (e instanceof FileNotFoundException) {
-      errorMessage = String.format("TreeArtifact %s was not created", output.prettyPrint());
+      errorMessage = String.format("output tree artifact %s was not created", output.prettyPrint());
     } else {
       errorMessage =
           String.format(
-              "Error while validating output TreeArtifact %s : %s", output, e.getMessage());
+              "error while validating output tree artifact %s: %s",
+              output.prettyPrint(), e.getMessage());
     }
 
     reporter.handle(Event.error(action.getOwner().getLocation(), errorMessage));

src/main/java/com/google/devtools/build/lib/skyframe/TreeArtifactValue.java
@@ -558,9 +558,7 @@ public class TreeArtifactValue implements HasDigest, SkyValue {
 
           if (statFollow == null) {
             throw new IOException(
-                String.format(
-                    "Child %s of tree artifact %s is a dangling symbolic link",
-                    parentRelativePath, parentDir));
+                String.format("child %s is a dangling symbolic link", parentRelativePath));
           }
 
           if (statFollow.isFile() && !statFollow.isSpecialFile()) {
@@ -574,9 +572,7 @@ public class TreeArtifactValue implements HasDigest, SkyValue {
 
         if (type == Dirent.Type.UNKNOWN) {
           throw new IOException(
-              String.format(
-                  "Child %s of tree artifact %s has an unsupported type",
-                  parentRelativePath, parentDir));
+              String.format("child %s has an unsupported type", parentRelativePath));
         }
 
         visitor.visit(parentRelativePath, type, traversedSymlink);

src/test/java/com/google/devtools/build/lib/remote/RemoteActionFileSystemTest.java
@@ -451,10 +451,42 @@ public final class RemoteActionFileSystemTest extends RemoteActionFileSystemTest
   @Test
   public void statAndExists_notFound() throws Exception {
     RemoteActionFileSystem actionFs = (RemoteActionFileSystem) createActionFileSystem();
-    Artifact artifact = ActionsTestUtil.createArtifact(outputRoot, "out");
-    PathFragment path = artifact.getPath().asFragment();
+    PathFragment path = getOutputPath("does_not_exist");
 
     assertThat(actionFs.exists(path)).isFalse();
+
+    assertThat(actionFs.statIfFound(path, /* followSymlinks= */ true)).isNull();
+
+    assertThrows(
+        FileNotFoundException.class, () -> actionFs.stat(path, /* followSymlinks= */ true));
+  }
+
+  @Test
+  public void statAndExists_isNotDirectory() throws Exception {
+    RemoteActionFileSystem actionFs = (RemoteActionFileSystem) createActionFileSystem();
+    PathFragment nonDirPath = getOutputPath("non_dir");
+    PathFragment path = nonDirPath.getChild("file");
+
+    writeLocalFile(actionFs, nonDirPath, "content");
+
+    assertThat(actionFs.exists(path)).isFalse();
+
+    assertThat(actionFs.statIfFound(path, /* followSymlinks= */ true)).isNull();
+
+    assertThrows(
+        FileNotFoundException.class, () -> actionFs.stat(path, /* followSymlinks= */ true));
+  }
+
+  @Test
+  public void statAndExists_danglingSymlink_notFound() throws Exception {
+    RemoteActionFileSystem actionFs = (RemoteActionFileSystem) createActionFileSystem();
+    PathFragment path = getOutputPath("sym");
+
+    actionFs.getPath(path).createSymbolicLink(PathFragment.create("/does_not_exist"));
+
+    assertThat(actionFs.exists(path)).isFalse();
+
+    assertThat(actionFs.statIfFound(path, /* followSymlinks= */ true)).isNull();
 
     assertThrows(
         FileNotFoundException.class, () -> actionFs.stat(path, /* followSymlinks= */ true));

src/test/java/com/google/devtools/build/lib/skyframe/ActionOutputMetadataStoreTest.java
@@ -260,6 +260,20 @@ public final class ActionOutputMetadataStoreTest {
   }
 
   @Test
+  public void withDanglingSymlinkInTreeArtifactFailsWithException() throws Exception {
+    SpecialArtifact treeArtifact =
+        ActionsTestUtil.createTreeArtifactWithGeneratingAction(outputRoot, "foo/bar");
+    TreeFileArtifact child = TreeFileArtifact.createTreeOutput(treeArtifact, "child");
+    treeArtifact.getPath().createDirectoryAndParents();
+    child.getPath().createSymbolicLink(PathFragment.create("/does_not_exist"));
+
+    ActionOutputMetadataStore store = createStore(/* outputs= */ ImmutableSet.of(treeArtifact));
+
+    IOException e = assertThrows(IOException.class, () -> store.getOutputMetadata(treeArtifact));
+    assertThat(e).hasMessageThat().contains("dangling symbolic link");
+  }
+
+  @Test
   public void resettingOutputs() throws Exception {
     PathFragment path = PathFragment.create("foo/bar");
     Artifact artifact = ActionsTestUtil.createArtifactWithRootRelativePath(outputRoot, path);

src/test/java/com/google/devtools/build/lib/skyframe/TreeArtifactBuildTest.java
@@ -505,9 +505,7 @@ public final class TreeArtifactBuildTest extends TimestampBuilderTestCase {
 
     ImmutableList<Event> errors = ImmutableList.copyOf(eventCollector);
     assertThat(errors).hasSize(2);
-    assertThat(errors.get(0).getMessage())
-        .contains(
-            "Child links/link of tree artifact " + out.getPath() + " is a dangling symbolic link");
+    assertThat(errors.get(0).getMessage()).contains("child links/link is a dangling symbolic link");
     assertThat(errors.get(1).getMessage()).contains("not all outputs were created or valid");
   }
 
@@ -555,9 +553,7 @@ public final class TreeArtifactBuildTest extends TimestampBuilderTestCase {
 
     ImmutableList<Event> errors = ImmutableList.copyOf(eventCollector);
     assertThat(errors).hasSize(2);
-    assertThat(errors.get(0).getMessage())
-        .contains(
-            "Child links/link of tree artifact " + out.getPath() + " is a dangling symbolic link");
+    assertThat(errors.get(0).getMessage()).contains("child links/link is a dangling symbolic link");
     assertThat(errors.get(1).getMessage()).contains("not all outputs were created or valid");
   }
 
@@ -607,9 +603,7 @@ public final class TreeArtifactBuildTest extends TimestampBuilderTestCase {
 
     ImmutableList<Event> errors = ImmutableList.copyOf(eventCollector);
     assertThat(errors).hasSize(2);
-    assertThat(errors.get(0).getMessage())
-        .contains(
-            "Child links/link of tree artifact " + out.getPath() + " is a dangling symbolic link");
+    assertThat(errors.get(0).getMessage()).contains("child links/link is a dangling symbolic link");
     assertThat(errors.get(1).getMessage()).contains("not all outputs were created or valid");
   }
 

src/test/java/com/google/devtools/build/lib/skyframe/TreeArtifactValueTest.java
@@ -419,9 +419,7 @@ public final class TreeArtifactValueTest {
         assertThrows(
             IOException.class,
             () -> TreeArtifactValue.visitTree(treeDir, (child, type, traversedSymlink) -> {}));
-    assertThat(e)
-        .hasMessageThat()
-        .contains("Child symlink of tree artifact /tree is a dangling symbolic link");
+    assertThat(e).hasMessageThat().contains("child symlink is a dangling symbolic link");
   }
 
   @Test
@@ -456,9 +454,7 @@ public final class TreeArtifactValueTest {
         assertThrows(
             IOException.class,
             () -> TreeArtifactValue.visitTree(treeDir, (child, type, traversedSymlink) -> {}));
-    assertThat(e)
-        .hasMessageThat()
-        .contains("Child unknown of tree artifact /tree has an unsupported type");
+    assertThat(e).hasMessageThat().contains("child unknown has an unsupported type");
   }
 
   @Test
@@ -523,9 +519,7 @@ public final class TreeArtifactValueTest {
         assertThrows(
             IOException.class,
             () -> TreeArtifactValue.visitTree(treeDir, (child, type, traversedSymlink) -> {}));
-    assertThat(e)
-        .hasMessageThat()
-        .contains("Child sym of tree artifact /tree has an unsupported type");
+    assertThat(e).hasMessageThat().contains("child sym has an unsupported type");
   }
 
   @Test