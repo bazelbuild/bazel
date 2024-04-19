bazelbuild/bazel

commit 8a1584e77c5c71e98a8cac87e6c5df6ee5d94c63

Migrate `AvailableXcodesInfo`, `XcodeProperties`, and `XcodeVersionRuleData` to builtin Starlark (with Starlark-consistent names).
The names of these things being very similar is likely to be confusing in the future, but we can address that after we've migrated them to apple_support. Fortunately they're only really used to communicate between the Xcode rules internally, so refactoring them shouldn't have many downstream effects.

This change does not cover `XcodeConfigInfo`, since it's a bit more complicated.

PiperOrigin-RevId: 625652603
Change-Id: Ic3c024f4b4f60c8fa93f68aa61e939215c965227

src/main/starlark/builtins_bzl/common/xcode/providers.bzl
@@ -1,0 +1,104 @@
+# Copyright 2024 The Bazel Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Definitions of providers used by the Xcode rules and their clients."""
+
+AvailableXcodesInfo = provider(
+    doc = """\
+The available Xcode versions computed from the `available_xcodes` rule.
+""",
+    fields = {
+        "available_versions": """\
+The available Xcode versions from `available_xcodes`.
+""",
+        "default_version": """\
+The default Xcode version from `available_xcodes`.
+""",
+    },
+)
+
+def _xcode_version_properties_info_init(
+        xcode_version,
+        default_ios_sdk_version = "8.4",
+        default_macos_sdk_version = "9.0",
+        default_tvos_sdk_version = "10.11",
+        default_watchos_sdk_version = "2.0",
+        default_visionos_sdk_version = "1.0"):
+    # Ensure that all fields get default values if they weren't specified.
+    return {
+        "xcode_version": xcode_version,
+        "default_ios_sdk_version": default_ios_sdk_version,
+        "default_macos_sdk_version": default_macos_sdk_version,
+        "default_tvos_sdk_version": default_tvos_sdk_version,
+        "default_watchos_sdk_version": default_watchos_sdk_version,
+        "default_visionos_sdk_version": default_visionos_sdk_version,
+    }
+
+XcodeVersionPropertiesInfo, _new_xcode_version_properties_info = provider(
+    doc = """\
+Information about a specific Xcode version, such as its default SDK versions.
+""",
+    fields = {
+        "xcode_version": """\
+A string representing the Xcode version number, or `None` if it is unknown.
+""",
+        "default_ios_sdk_version": """\
+A string representing the default iOS SDK version number for this version of
+Xcode, or `None` if it is unknown.
+""",
+        "default_macos_sdk_version": """\
+A string representing the default macOS SDK version number for this version of
+Xcode, or `None` if it is unknown.
+""",
+        "default_tvos_sdk_version": """\
+A string representing the default tvOS SDK version number for this version of
+Xcode, or `None` if it is unknown.
+""",
+        "default_watchos_sdk_version": """\
+A string representing the default watchOS SDK version number for this version of
+Xcode, or `None` if it is unknown.
+""",
+        "default_visionos_sdk_version": """\
+A string representing the default visionOS SDK version number for this version
+of Xcode, or `None` if it is unknown.
+""",
+    },
+    init = _xcode_version_properties_info_init,
+)
+
+XcodeVersionRuleInfo = provider(
+    doc = """\
+The information in a single target of the `xcode_version` rule. A single target
+of this rule contains an official version label decided by Apple, a number of
+supported aliases one might use to reference this version, and various
+properties of the Xcode version (such as default SDK versions).
+
+For example, one may want to reference official Xcode version 7.0.1 using the
+"7" or "7.0" aliases. This official version of Xcode may have a default
+supported iOS SDK of 9.0.
+""",
+    fields = {
+        "aliases": """\
+A list of strings denoting aliases that can be used to reference this Xcode
+version.
+""",
+        "label": """\
+The build `Label` of the `xcode_version` target that propagated this provider.
+""",
+        "xcode_version_properties": """\
+An `XcodeVersionPropertiesInfo` provider that contains the details about this
+Xcode version, such as its default SDK versions.
+""",
+    },
+)

src/main/starlark/builtins_bzl/common/xcode/xcode_version.bzl
@@ -14,11 +14,11 @@
 
 """Rule definition for the xcode_version rule."""
 
-load(":common/objc/apple_common.bzl", "apple_common")
+load(":common/xcode/providers.bzl", "XcodeVersionPropertiesInfo", "XcodeVersionRuleInfo")
 
 def _xcode_version_impl(ctx):
-    xcode_version_properties = apple_common.XcodeProperties(
-        version = ctx.attr.version,
+    xcode_version_properties = XcodeVersionPropertiesInfo(
+        xcode_version = ctx.attr.version,
         default_ios_sdk_version = ctx.attr.default_ios_sdk_version,
         default_visionos_sdk_version = ctx.attr.default_visionos_sdk_version,
         default_watchos_sdk_version = ctx.attr.default_watchos_sdk_version,
@@ -27,9 +27,9 @@ def _xcode_version_impl(ctx):
     )
     return [
         xcode_version_properties,
-        _builtins.internal.XcodeVersionRuleData(
+        XcodeVersionRuleInfo(
             label = ctx.label,
-            xcode_properties = xcode_version_properties,
+            xcode_version_properties = xcode_version_properties,
             aliases = ctx.attr.aliases,
         ),
         DefaultInfo(runfiles = ctx.runfiles()),

src/main/java/com/google/devtools/build/lib/bazel/rules/ObjcRules.java
@@ -21,9 +21,6 @@ import com.google.devtools.build.lib.analysis.ConfiguredRuleClassProvider;
 import com.google.devtools.build.lib.analysis.ConfiguredRuleClassProvider.RuleSet;
 import com.google.devtools.build.lib.cmdline.RepositoryName;
 import com.google.devtools.build.lib.rules.apple.AppleConfiguration;
-import com.google.devtools.build.lib.rules.apple.AvailableXcodesInfo;
-import com.google.devtools.build.lib.rules.apple.XcodeVersionProperties;
-import com.google.devtools.build.lib.rules.apple.XcodeVersionRuleData;
 import com.google.devtools.build.lib.rules.core.CoreRules;
 import com.google.devtools.build.lib.rules.objc.AppleStarlarkCommon;
 import com.google.devtools.build.lib.rules.objc.AppleToolchain;
@@ -58,10 +55,7 @@ public class ObjcRules implements RuleSet {
     builder.addRuleDefinition(new EmptyRule("xcode_config_alias") {});
     builder.addRuleDefinition(new EmptyRule("xcode_version") {});
 
-    builder.addStarlarkBuiltinsInternal("AvailableXcodesInfo", AvailableXcodesInfo.PROVIDER);
     builder.addStarlarkBuiltinsInternal("XcodeConfigInfo", XcodeConfigInfo.PROVIDER);
-    builder.addStarlarkBuiltinsInternal("XcodeProperties", XcodeVersionProperties.PROVIDER);
-    builder.addStarlarkBuiltinsInternal("XcodeVersionRuleData", XcodeVersionRuleData.PROVIDER);
 
     builder.addStarlarkBuiltinsInternal("apple_common", new AppleStarlarkCommon());
     builder.addStarlarkBootstrap(new AppleBootstrap());

src/main/java/com/google/devtools/build/lib/rules/apple/AvailableXcodesApi.java
@@ -1,57 +1,0 @@
-// Copyright 2024 The Bazel Authors. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-package com.google.devtools.build.lib.rules.apple;
-
-import com.google.devtools.build.docgen.annot.StarlarkConstructor;
-import com.google.devtools.build.lib.starlarkbuildapi.FileApi;
-import com.google.devtools.build.lib.starlarkbuildapi.core.ProviderApi;
-import net.starlark.java.annot.Param;
-import net.starlark.java.annot.ParamType;
-import net.starlark.java.annot.StarlarkMethod;
-import net.starlark.java.eval.EvalException;
-import net.starlark.java.eval.NoneType;
-import net.starlark.java.eval.Sequence;
-import net.starlark.java.eval.StarlarkThread;
-
-/** Provider structure for {@code AvailableXcodesInfo} provider. */
-interface AvailableXcodesApi<FileT extends FileApi> extends ProviderApi {
-
-  @StarlarkMethod(
-      name = "AvailableXcodesApi",
-      useStarlarkThread = true,
-      parameters = {
-        @Param(
-            name = "versions",
-            positional = false,
-            named = true,
-            defaultValue = "None",
-            allowedTypes = {@ParamType(type = NoneType.class), @ParamType(type = Sequence.class)},
-            documented = false),
-        @Param(
-            name = "default",
-            positional = false,
-            named = true,
-            defaultValue = "None",
-            allowedTypes = {
-              @ParamType(type = NoneType.class),
-              @ParamType(type = XcodeVersionRuleData.class)
-            },
-            documented = false),
-      },
-      selfCall = true,
-      documented = false)
-  @StarlarkConstructor
-  public AvailableXcodesInfo createInfo(
-      Object availableXcodes, Object defaultVersion, StarlarkThread thread) throws EvalException;
-}

src/main/java/com/google/devtools/build/lib/rules/apple/AvailableXcodesInfo.java
@@ -1,91 +1,0 @@
-// Copyright 2017 The Bazel Authors. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-package com.google.devtools.build.lib.rules.apple;
-
-import com.google.devtools.build.lib.actions.Artifact;
-import com.google.devtools.build.lib.concurrent.ThreadSafety.Immutable;
-import com.google.devtools.build.lib.packages.BuiltinProvider;
-import com.google.devtools.build.lib.packages.NativeInfo;
-import javax.annotation.Nullable;
-import net.starlark.java.annot.StarlarkMethod;
-import net.starlark.java.eval.EvalException;
-import net.starlark.java.eval.Sequence;
-import net.starlark.java.eval.Starlark;
-import net.starlark.java.eval.StarlarkThread;
-
-/** The available Xcode versions computed from the {@code available_xcodes} rule. */
-@Immutable
-public class AvailableXcodesInfo extends NativeInfo {
-  /** Starlark name for this provider. */
-  public static final String STARLARK_NAME = "AvailableXcodesInfo";
-
-  /** Provider identifier for {@link AvailableXcodesInfo}. */
-  public static final BuiltinProvider<AvailableXcodesInfo> PROVIDER = new Provider();
-
-  private final Iterable<XcodeVersionRuleData> availableXcodes;
-  private final XcodeVersionRuleData defaultVersion;
-
-  public AvailableXcodesInfo(
-      Iterable<XcodeVersionRuleData> availableXcodes, XcodeVersionRuleData defaultVersion) {
-    this.availableXcodes = availableXcodes;
-    this.defaultVersion = defaultVersion;
-  }
-
-  @Override
-  public BuiltinProvider<AvailableXcodesInfo> getProvider() {
-    return PROVIDER;
-  }
-
-  /** Returns the available Xcode versions from {@code available_xcodes}. */
-  @StarlarkMethod(
-      name = "available_versions",
-      structField = true,
-      doc = "Returns the available Xcode versions from available_xcodes.")
-  public Iterable<XcodeVersionRuleData> getAvailableVersions() {
-    return availableXcodes;
-  }
-
-  /** Returns the default Xcode version from {@code available_xcodes}. */
-  @StarlarkMethod(
-      name = "default_version",
-      structField = true,
-      doc = "Returns the default Xcode version from available_xcodes.")
-  public XcodeVersionRuleData getDefaultVersion() {
-    return defaultVersion;
-  }
-
-  /** Provider class for {@link XcodeVersionRuleData} objects. */
-  public static class Provider extends BuiltinProvider<AvailableXcodesInfo>
-      implements AvailableXcodesApi<Artifact> {
-    private Provider() {
-      super(AvailableXcodesInfo.STARLARK_NAME, AvailableXcodesInfo.class);
-    }
-
-    @Override
-    @SuppressWarnings("unchecked")
-    public AvailableXcodesInfo createInfo(
-        Object availableXcodes, Object defaultVersion, StarlarkThread thread) throws EvalException {
-      Sequence<XcodeVersionRuleData> availableXcodesSequence =
-          nullIfNone(availableXcodes, Sequence.class);
-      return new AvailableXcodesInfo(
-          Sequence.cast(availableXcodesSequence, XcodeVersionRuleData.class, "availableXcodes"),
-          nullIfNone(defaultVersion, XcodeVersionRuleData.class));
-    }
-
-    @Nullable
-    private static <T> T nullIfNone(Object object, Class<T> type) {
-      return object != Starlark.NONE ? type.cast(object) : null;
-    }
-  }
-}

src/main/java/com/google/devtools/build/lib/rules/apple/XcodeVersionProperties.java
@@ -1,259 +1,0 @@
-// Copyright 2016 The Bazel Authors. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package com.google.devtools.build.lib.rules.apple;
-
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Optional;
-import com.google.common.base.Strings;
-import com.google.devtools.build.lib.concurrent.ThreadSafety.Immutable;
-import com.google.devtools.build.lib.packages.BuiltinProvider;
-import com.google.devtools.build.lib.packages.NativeInfo;
-import com.google.devtools.build.lib.starlarkbuildapi.apple.XcodePropertiesApi;
-import java.util.Objects;
-import javax.annotation.Nullable;
-import net.starlark.java.annot.StarlarkMethod;
-import net.starlark.java.eval.EvalException;
-import net.starlark.java.eval.Starlark;
-import net.starlark.java.eval.StarlarkThread;
-
-/** A tuple containing information about a version of Xcode and its properties. */
-@Immutable
-public class XcodeVersionProperties extends NativeInfo implements XcodePropertiesApi {
-  /** Starlark identifier for XcodeVersionProperties provider. */
-  public static final Provider PROVIDER = new Provider();
-
-  /** Starlark name for the XcodeVersionProperties provider. */
-  public static final String STARLARK_NAME = "XcodeProperties";
-
-  @VisibleForTesting public static final String DEFAULT_IOS_SDK_VERSION = "8.4";
-  @VisibleForTesting public static final String DEFAULT_VISIONOS_SDK_VERSION = "1.0";
-  @VisibleForTesting public static final String DEFAULT_WATCHOS_SDK_VERSION = "2.0";
-  @VisibleForTesting public static final String DEFAULT_MACOS_SDK_VERSION = "10.11";
-  @VisibleForTesting public static final String DEFAULT_TVOS_SDK_VERSION = "9.0";
-
-  private final Optional<DottedVersion> xcodeVersion;
-  private final DottedVersion defaultIosSdkVersion;
-  private final DottedVersion defaultVisionosSdkVersion;
-  private final DottedVersion defaultWatchosSdkVersion;
-  private final DottedVersion defaultTvosSdkVersion;
-  private final DottedVersion defaultMacosSdkVersion;
-
-  /**
-   * Creates and returns a tuple representing no known Xcode property information (defaults are used
-   * where applicable).
-   */
-  // TODO(bazel-team): The Xcode version should be a well-defined value, either specified by the
-  // user, evaluated on the local system, or set to a sensible default.
-  // Unfortunately, until the local system evaluation hook is created, this constraint would break
-  // some users.
-  @StarlarkMethod(
-      name = "unknownXcodeVersionProperties",
-      documented = false,
-      useStarlarkThread = true)
-  public XcodeVersionProperties unknownXcodeVersionProperties(StarlarkThread thread) {
-    return new XcodeVersionProperties(null);
-  }
-
-  /**
-   * Constructor for when only the Xcode version is specified, but no property information is
-   * specified.
-   */
-  public XcodeVersionProperties(Object xcodeVersion) {
-    this(xcodeVersion, null, null, null, null, null);
-  }
-
-  /**
-   * General constructor. Some (nullable) properties may be left unspecified. In these cases, a
-   * semi-sensible default will be assigned to the property value.
-   */
-  XcodeVersionProperties(
-      @Nullable Object xcodeVersion,
-      @Nullable String defaultIosSdkVersion,
-      @Nullable String defaultVisionosSdkVersion,
-      @Nullable String defaultWatchosSdkVersion,
-      @Nullable String defaultTvosSdkVersion,
-      @Nullable String defaultMacosSdkVersion) {
-    this.xcodeVersion =
-        Starlark.isNullOrNone(xcodeVersion)
-            ? Optional.absent()
-            : Optional.of((DottedVersion) xcodeVersion);
-    this.defaultIosSdkVersion =
-        Strings.isNullOrEmpty(defaultIosSdkVersion)
-            ? DottedVersion.fromStringUnchecked(DEFAULT_IOS_SDK_VERSION)
-            : DottedVersion.fromStringUnchecked(defaultIosSdkVersion);
-    this.defaultVisionosSdkVersion =
-        Strings.isNullOrEmpty(defaultVisionosSdkVersion)
-            ? DottedVersion.fromStringUnchecked(DEFAULT_VISIONOS_SDK_VERSION)
-            : DottedVersion.fromStringUnchecked(defaultVisionosSdkVersion);
-    this.defaultWatchosSdkVersion =
-        Strings.isNullOrEmpty(defaultWatchosSdkVersion)
-            ? DottedVersion.fromStringUnchecked(DEFAULT_WATCHOS_SDK_VERSION)
-            : DottedVersion.fromStringUnchecked(defaultWatchosSdkVersion);
-    this.defaultTvosSdkVersion =
-        Strings.isNullOrEmpty(defaultTvosSdkVersion)
-            ? DottedVersion.fromStringUnchecked(DEFAULT_TVOS_SDK_VERSION)
-            : DottedVersion.fromStringUnchecked(defaultTvosSdkVersion);
-    this.defaultMacosSdkVersion =
-        Strings.isNullOrEmpty(defaultMacosSdkVersion)
-            ? DottedVersion.fromStringUnchecked(DEFAULT_MACOS_SDK_VERSION)
-            : DottedVersion.fromStringUnchecked(defaultMacosSdkVersion);
-  }
-
-  @Override
-  public Provider getProvider() {
-    return PROVIDER;
-  }
-
-  /** Returns the Xcode version, or null if the Xcode version is unknown. */
-  @Nullable
-  @Override
-  public String getXcodeVersionString() {
-    if (xcodeVersion.isPresent()) {
-      return xcodeVersion.get().toString();
-    }
-    return null;
-  }
-
-  /** Returns the default iOS SDK version to use if this Xcode version is in use. */
-  @Nullable
-  @Override
-  public String getDefaultIosSdkVersionString() {
-    return defaultIosSdkVersion != null ? defaultIosSdkVersion.toString() : null;
-  }
-
-  /** Returns the default visionOS SDK version to use if this Xcode version is in use. */
-  @Nullable
-  @Override
-  public String getDefaultVisionosSdkVersionString() {
-    return defaultVisionosSdkVersion != null ? defaultVisionosSdkVersion.toString() : null;
-  }
-
-  /** Returns the default watchOS SDK version to use if this Xcode version is in use. */
-  @Nullable
-  @Override
-  public String getDefaultWatchosSdkVersionString() {
-    return defaultWatchosSdkVersion != null ? defaultWatchosSdkVersion.toString() : null;
-  }
-
-  /** Returns the default tvOS SDK version to use if this Xcode version is in use. */
-  @Nullable
-  @Override
-  public String getDefaultTvosSdkVersionString() {
-    return defaultTvosSdkVersion != null ? defaultTvosSdkVersion.toString() : null;
-  }
-
-  /** Returns the default macOS SDK version to use if this Xcode version is in use. */
-  @Nullable
-  @Override
-  public String getDefaultMacosSdkVersionString() {
-    return defaultMacosSdkVersion != null ? defaultMacosSdkVersion.toString() : null;
-  }
-
-  /** Returns the Xcode version, or {@link Optional#absent} if the Xcode version is unknown. */
-  public Optional<DottedVersion> getXcodeVersion() {
-    return xcodeVersion;
-  }
-
-  @Nullable
-  public DottedVersion getDefaultIosSdkVersion() {
-    return defaultIosSdkVersion;
-  }
-
-  @Nullable
-  public DottedVersion getDefaultVisionosSdkVersion() {
-    return defaultVisionosSdkVersion;
-  }
-
-  @Nullable
-  public DottedVersion getDefaultWatchosSdkVersion() {
-    return defaultWatchosSdkVersion;
-  }
-
-  @Nullable
-  public DottedVersion getDefaultTvosSdkVersion() {
-    return defaultTvosSdkVersion;
-  }
-
-  @Nullable
-  public DottedVersion getDefaultMacosSdkVersion() {
-    return defaultMacosSdkVersion;
-  }
-
-  @Override
-  public boolean equals(Object other) {
-    if (other == null) {
-      return false;
-    }
-    if (!(other instanceof XcodeVersionProperties otherData)) {
-      return false;
-    }
-    return xcodeVersion.equals(otherData.getXcodeVersion())
-        && defaultIosSdkVersion.equals(otherData.getDefaultIosSdkVersion())
-        && defaultVisionosSdkVersion.equals(otherData.getDefaultVisionosSdkVersion())
-        && defaultWatchosSdkVersion.equals(otherData.getDefaultWatchosSdkVersion())
-        && defaultTvosSdkVersion.equals(otherData.getDefaultTvosSdkVersion())
-        && defaultMacosSdkVersion.equals(otherData.getDefaultMacosSdkVersion());
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(
-        xcodeVersion,
-        defaultIosSdkVersion,
-        defaultVisionosSdkVersion,
-        defaultWatchosSdkVersion,
-        defaultTvosSdkVersion,
-        defaultMacosSdkVersion);
-  }
-
-  /** Provider class for {@link XcodeVersionProperties} objects. */
-  public static class Provider extends BuiltinProvider<XcodeVersionProperties>
-      implements XcodePropertiesApi.Provider {
-    private Provider() {
-      super(XcodePropertiesApi.NAME, XcodeVersionProperties.class);
-    }
-
-    @Override
-    public XcodePropertiesApi createInfo(
-        Object starlarkVersion,
-        Object starlarkDefaultIosSdkVersion,
-        Object starlarkDefaultVisionosSdkVersion,
-        Object starlarkDefaultWatchosSdkVersion,
-        Object starlarkDefaultTvosSdkVersion,
-        Object starlarkDefaultMacosSdkVersion,
-        StarlarkThread thread)
-        throws EvalException {
-      return new XcodeVersionProperties(
-          Starlark.isNullOrNone(starlarkVersion)
-              ? null
-              : DottedVersion.fromStringUnchecked((String) starlarkVersion),
-          Starlark.isNullOrNone(starlarkDefaultIosSdkVersion)
-              ? null
-              : (String) starlarkDefaultIosSdkVersion,
-          Starlark.isNullOrNone(starlarkDefaultVisionosSdkVersion)
-              ? null
-              : (String) starlarkDefaultVisionosSdkVersion,
-          Starlark.isNullOrNone(starlarkDefaultWatchosSdkVersion)
-              ? null
-              : (String) starlarkDefaultWatchosSdkVersion,
-          Starlark.isNullOrNone(starlarkDefaultTvosSdkVersion)
-              ? null
-              : (String) starlarkDefaultTvosSdkVersion,
-          Starlark.isNullOrNone(starlarkDefaultMacosSdkVersion)
-              ? null
-              : (String) starlarkDefaultMacosSdkVersion);
-    }
-  }
-}

src/main/java/com/google/devtools/build/lib/rules/apple/XcodeVersionProviderApi.java
@@ -1,63 +1,0 @@
-// Copyright 2024 The Bazel Authors. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-package com.google.devtools.build.lib.rules.apple;
-
-import com.google.devtools.build.docgen.annot.StarlarkConstructor;
-import com.google.devtools.build.lib.starlarkbuildapi.FileApi;
-import com.google.devtools.build.lib.starlarkbuildapi.core.ProviderApi;
-import net.starlark.java.annot.Param;
-import net.starlark.java.annot.ParamType;
-import net.starlark.java.annot.StarlarkMethod;
-import net.starlark.java.eval.EvalException;
-import net.starlark.java.eval.NoneType;
-import net.starlark.java.eval.Sequence;
-import net.starlark.java.eval.StarlarkThread;
-
-/** Provider structure for {@code XcodeVersionRuleData} */
-interface XcodeVersionProviderApi<FileT extends FileApi> extends ProviderApi {
-
-  @StarlarkMethod(
-      name = "XcodeVersionRuleData",
-      useStarlarkThread = true,
-      parameters = {
-        @Param(
-            name = "label",
-            positional = false,
-            named = true,
-            defaultValue = "None",
-            documented = false),
-        @Param(
-            name = "xcode_properties",
-            positional = false,
-            named = true,
-            defaultValue = "None",
-            documented = false),
-        @Param(
-            name = "aliases",
-            positional = false,
-            named = true,
-            defaultValue = "None",
-            allowedTypes = {@ParamType(type = NoneType.class), @ParamType(type = Sequence.class)},
-            documented = false),
-      },
-      selfCall = true,
-      documented = false)
-  @StarlarkConstructor
-  public XcodeVersionRuleData createInfo(
-      Object starlarkLabel,
-      Object starlarkXcodeProperties,
-      Object starlarkAliases,
-      StarlarkThread thread)
-      throws EvalException;
-}

commit 6f953cb037558b17e99018d8f5d190c6421c7c21

Swap back default macOS and tvOS SDK versions
These seem to have been accidentally switched around in https://github.com/bazelbuild/bazel/commit/8a1584e77c5c71e98a8cac87e6c5df6ee5d94c63, which caused the following error message to show when I try to build any C++ code on my Sonoma M1 Macbook:

    clang: error: invalid version number in '-mmacosx-version-min=9.0'

Turns out my XCode was out of date anyway, but this should probably still be fixed.

PiperOrigin-RevId: 626345237
Change-Id: I80b67b11e8bf4786cd4c5771d318d564ec170209

src/main/starlark/builtins_bzl/common/xcode/providers.bzl
@@ -178,8 +178,8 @@ def _xcode_version_properties_info_init(
         *,
         xcode_version,
         default_ios_sdk_version = "8.4",
-        default_macos_sdk_version = "9.0",
-        default_tvos_sdk_version = "10.11",
+        default_macos_sdk_version = "10.11",
+        default_tvos_sdk_version = "9.0",
         default_watchos_sdk_version = "2.0",
         default_visionos_sdk_version = "1.0"):
     # Ensure that all fields get default values if they weren't specified.

src/main/starlark/builtins_bzl/common/xcode/xcode_version.bzl
@@ -42,8 +42,8 @@ xcode_version = rule(
         "default_ios_sdk_version": attr.string(default = "8.4", doc = "The iOS SDK version that is used by default when this version of Xcode is being used. The `--ios_sdk_version` build flag will override the value specified here.", mandatory = False),
         "default_visionos_sdk_version": attr.string(default = "1.0", doc = "The visionOS SDK version that is used by default when this version of Xcode is being used.", mandatory = False),
         "default_watchos_sdk_version": attr.string(default = "2.0", doc = "The watchOS SDK version that is used by default when this version of Xcode is being used. The `--watchos_sdk_version` build flag will override the value specified here.", mandatory = False),
-        "default_tvos_sdk_version": attr.string(default = "10.11", doc = "The tvOS SDK version that is used by default when this version of Xcode is being used. The `--tvos_sdk_version` build flag will override the value specified here.", mandatory = False),
-        "default_macos_sdk_version": attr.string(default = "9.0", doc = "The macOS SDK version that is used by default when this version of Xcode is being used. The `--macos_sdk_version` build flag will override the value specified here.", mandatory = False),
+        "default_tvos_sdk_version": attr.string(default = "9.0", doc = "The tvOS SDK version that is used by default when this version of Xcode is being used. The `--tvos_sdk_version` build flag will override the value specified here.", mandatory = False),
+        "default_macos_sdk_version": attr.string(default = "10.11", doc = "The macOS SDK version that is used by default when this version of Xcode is being used. The `--macos_sdk_version` build flag will override the value specified here.", mandatory = False),
     },
     implementation = _xcode_version_impl,
 )