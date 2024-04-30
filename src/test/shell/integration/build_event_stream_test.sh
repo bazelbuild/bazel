727632539bd58dbcf54a9a52a7da15eb0e7c49e2
Googler <hvd@google.com>
2024-04-30 07:15:33 -0700

    Fix flakiness in `build_event_stream_test.sh`
    One of the two targets may be left incomplete if not using `-k`
    
    Before:
    ```
    //third_party/bazel/src/test/shell/integration:build_event_stream_test
    FAILED in 23 out of 100 in 144.5s
      Stats over 100 runs: max = 144.5s, min = 35.9s, avg = 62.3s, dev = 14.9s
    ```
    
    After:
    ```
    //third_party/bazel/src/test/shell/integration:build_event_stream_test
    PASSED in 87.9s
      Stats over 100 runs: max = 87.9s, min = 41.0s, avg = 61.5s, dev = 11.3s
    ```
    PiperOrigin-RevId: 629403266
    Change-Id: I00665f7d1405b655440076160af4fd6f37b24417

--- src/test/shell/integration/build_event_stream_test.sh
+++ src/test/shell/integration/build_event_stream_test.sh
@@ -1259,12 +1259,12 @@ EOF
   [ `grep unconfigured_label event_id_types | wc -l` -eq 1 ] \
       || fail "not precisely one unconfigured_label event id"
 
-  (bazel build --noenable_bzlmod --build_event_text_file="${TEST_log}" :badfilegroup :doesnotexist \
+  (bazel build -k --noenable_bzlmod --build_event_text_file="${TEST_log}" :badfilegroup :doesnotexist \
     && fail "Expected failure") || :
   # There should be precisely two events with target_completed as event id type
   (echo 'g/^id/+1p'; echo 'q') | ed "${TEST_log}" 2>&1 | tail -n +2 > event_id_types
   [ `grep target_completed event_id_types | wc -l` -eq 2 ] \
-      || fail "not precisely one target_completed event id"
+      || fail "not precisely two target_completed event ids"
   # Moreover, we expect precisely one event identified by an unconfigured label
   [ `grep unconfigured_label event_id_types | wc -l` -eq 1 ] \
       || fail "not precisely one unconfigured_label event id"
