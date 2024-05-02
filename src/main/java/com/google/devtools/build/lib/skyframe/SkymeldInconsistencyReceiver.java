32a9ac9cfc8ae7ff7f3e12d0940fc1ab35545173
Googler <yuyueny@google.com>
2024-05-02 08:07:40 -0700

    No public description
    PiperOrigin-RevId: 630066239
    Change-Id: I55487da41dce9aec5b6dc65266ffc003ffa3d9fb

--- src/main/java/com/google/devtools/build/lib/skyframe/SkymeldInconsistencyReceiver.java
+++ src/main/java/com/google/devtools/build/lib/skyframe/SkymeldInconsistencyReceiver.java
@@ -32,9 +32,7 @@ public class SkymeldInconsistencyReceiver implements GraphInconsistencyReceiver 
   // below might be unexpected.
   private static final ImmutableMap<SkyFunctionName, SkyFunctionName>
       SKYMELD_EXPECTED_MISSING_CHILDREN =
-          ImmutableMap.of(
-              SkyFunctions.ACTION_EXECUTION, SkyFunctions.GLOB,
-              SkyFunctions.GLOB, SkyFunctions.GLOB);
+          ImmutableMap.of(SkyFunctions.ACTION_EXECUTION, SkyFunctions.GLOB);
 
   private final boolean heuristicallyDropNodes;
 
