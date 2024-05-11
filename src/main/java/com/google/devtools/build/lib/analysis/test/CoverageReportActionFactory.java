3b44cec1635b31ac1ecfd1ba7ed41e90efc30338
Fabian Meumertzheim <fabian@meumertzhe.im>
2024-05-10 17:07:49 -0700

    Announce combined coverage report on the BES
    This allows BES consumers to access this file without guessing its path and existence, as well as when the actual coverage generation action runs locally.
    
    Also removes an obsolete `CoverageReport` event.
    
    RELNOTES: The combined coverage report produced via `--combined_report=lcov` is now announced on the BES via the new `CoverageReport` event.
    
    Closes #22171.
    
    PiperOrigin-RevId: 632641499
    Change-Id: I0c323a371ec2fdd085bea23a772a85b72c52093f

--- src/main/java/com/google/devtools/build/lib/analysis/test/CoverageReportActionFactory.java
+++ src/main/java/com/google/devtools/build/lib/analysis/test/CoverageReportActionFactory.java
@@ -15,6 +15,7 @@
 package com.google.devtools.build.lib.analysis.test;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 import com.google.common.eventbus.EventBus;
 import com.google.devtools.build.lib.actions.ActionAnalysisMetadata;
 import com.google.devtools.build.lib.actions.ActionKeyContext;
@@ -71,6 +72,10 @@ public interface CoverageReportActionFactory {
 
     public Collection<Artifact> getCoverageOutputs() {
       return coverageReportAction.getOutputs();
+    }
+
+    public Artifact getCoverageReportArtifact() {
+      return Iterables.getOnlyElement(coverageReportAction.getOutputs());
     }
   }
 
