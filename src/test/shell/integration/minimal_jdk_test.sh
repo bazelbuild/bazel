7025bb88432d53bb7c05c813a99522cba5e1ace0
Googler <jcater@google.com>
2024-05-14 11:23:35 -0700

    Update Bazel auto-value version and enable AutoBuilder.
    Work towards starlark options parser cleanup: #22365.
    
    PiperOrigin-RevId: 633649782
    Change-Id: I73180180142b5b23e91f1ee8c845ea606cfdc85e

--- src/test/shell/integration/minimal_jdk_test.sh
+++ src/test/shell/integration/minimal_jdk_test.sh
@@ -42,13 +42,13 @@ export BAZEL_SUFFIX="_jdk_minimal"
 source "$(rlocation "io_bazel/src/test/shell/integration_test_setup.sh")" \
   || { echo "integration_test_setup.sh not found!" >&2; exit 1; }
 
-# Bazel's install base is < 465MB with minimal JDK and > 465MB with an all
+# Bazel's install base is < 480MB with minimal JDK and > 480MB with an all
 # modules JDK.
-function test_size_less_than_465MB() {
+function test_size_less_than_480MB() {
   bazel info
   ib=$(bazel info install_base)
   size=$(du -s "$ib" | cut -d\	 -f1)
-  maxsize=$((1024*465))
+  maxsize=$((1024*480))
   if [ $size -gt $maxsize ]; then
     echo "$ib was too big:" 1>&2
     du -a "$ib" 1>&2
