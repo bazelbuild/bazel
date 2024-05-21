c121335e43bf65f20e9a05f970598aac4c898eb2
Fabian Meumertzheim <fabian@meumertzhe.im>
2024-05-21 13:39:42 -0700

    Remove stale extension factors
    JSON-based merge conflict resolution for `MODULE.bazel.lock` can end up producing entries with, e.g., both a `general` and an `os:Linux` factor result when the OS/arch dependence of the extension changes. `BazelLockFileModule` now invalidates each factor result individually.
    
    Also sort the extension factors.
    
    Closes #22378.
    
    PiperOrigin-RevId: 635913837
    Change-Id: I0064098806c856f16e8f4c0270f609f06cebc945

--- src/main/java/com/google/devtools/build/lib/bazel/bzlmod/ModuleExtensionEvalFactors.java
+++ src/main/java/com/google/devtools/build/lib/bazel/bzlmod/ModuleExtensionEvalFactors.java
@@ -15,7 +15,10 @@
 package com.google.devtools.build.lib.bazel.bzlmod;
 
 import com.google.auto.value.AutoValue;
+import com.google.common.base.Splitter;
 import com.ryanharter.auto.value.gson.GenerateTypeAdapter;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This object holds the evaluation factors for module extensions in the lockfile, such as the
@@ -24,7 +27,14 @@ import com.ryanharter.auto.value.gson.GenerateTypeAdapter;
  */
 @AutoValue
 @GenerateTypeAdapter
-public abstract class ModuleExtensionEvalFactors {
+public abstract class ModuleExtensionEvalFactors implements Comparable<ModuleExtensionEvalFactors> {
+
+  private static final String OS_KEY = "os:";
+  private static final String ARCH_KEY = "arch:";
+
+  // This is used when the module extension doesn't depend on os or arch, to indicate that
+  // its value is "general" and can be used with any platform
+  private static final String GENERAL_EXTENSION = "general";
 
   /** Returns the OS this extension is evaluated on, or empty if it doesn't depend on the os */
   public abstract String getOs();
@@ -39,7 +49,51 @@ public abstract class ModuleExtensionEvalFactors {
     return getOs().isEmpty() && getArch().isEmpty();
   }
 
+  public boolean hasSameDependenciesAs(ModuleExtensionEvalFactors other) {
+    return getOs().isEmpty() == other.getOs().isEmpty()
+        && getArch().isEmpty() == other.getArch().isEmpty();
+  }
+
+  @Override
+  public String toString() {
+    if (isEmpty()) {
+      return GENERAL_EXTENSION;
+    }
+
+    List<String> parts = new ArrayList<>();
+    if (!getOs().isEmpty()) {
+      parts.add(OS_KEY + getOs());
+    }
+    if (!getArch().isEmpty()) {
+      parts.add(ARCH_KEY + getArch());
+    }
+    return String.join(",", parts);
+  }
+
+  @Override
+  public int compareTo(ModuleExtensionEvalFactors o) {
+    return toString().compareTo(o.toString());
+  }
+
   public static ModuleExtensionEvalFactors create(String os, String arch) {
     return new AutoValue_ModuleExtensionEvalFactors(os, arch);
+  }
+
+  public static ModuleExtensionEvalFactors parse(String s) {
+    if (s.equals(GENERAL_EXTENSION)) {
+      return ModuleExtensionEvalFactors.create("", "");
+    }
+
+    String os = "";
+    String arch = "";
+    var extParts = Splitter.on(',').splitToList(s);
+    for (String part : extParts) {
+      if (part.startsWith(OS_KEY)) {
+        os = part.substring(OS_KEY.length());
+      } else if (part.startsWith(ARCH_KEY)) {
+        arch = part.substring(ARCH_KEY.length());
+      }
+    }
+    return ModuleExtensionEvalFactors.create(os, arch);
   }
 }
