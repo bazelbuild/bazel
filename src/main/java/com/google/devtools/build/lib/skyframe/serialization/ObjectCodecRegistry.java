5697d0a60713a6260876ea57cfbb9ce9cc832d26
Googler <shahan@google.com>
2024-05-21 13:12:50 -0700

    Support serialization constants in testutils/Fingerprinter.
    PiperOrigin-RevId: 635905107
    Change-Id: Ic520a4ac424288fbd8b6b39d579fd4eb41e66789

--- src/main/java/com/google/devtools/build/lib/skyframe/serialization/ObjectCodecRegistry.java
+++ src/main/java/com/google/devtools/build/lib/skyframe/serialization/ObjectCodecRegistry.java
@@ -170,7 +170,7 @@ public class ObjectCodecRegistry {
   }
 
   @Nullable
-  Integer maybeGetTagForConstant(Object object) {
+  public Integer maybeGetTagForConstant(Object object) {
     return referenceConstantsMap.get(object);
   }
 
