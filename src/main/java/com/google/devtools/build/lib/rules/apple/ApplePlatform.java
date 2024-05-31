bazelbuild/bazel

commit 8e783b065c66c005db26d85e9ec6e8e28c42b2f2

Migrate apple_common.platform_type to Starlark
PiperOrigin-RevId: 637642691
Change-Id: Ie57faf7dba4b1603c667e8b7c2a1207c6b79e6ba

src/main/java/com/google/devtools/build/lib/rules/apple/ApplePlatform.java
@@ -22,7 +22,6 @@ import com.google.devtools.build.lib.packages.Provider;
 import com.google.devtools.build.lib.packages.StarlarkInfo;
 import com.google.devtools.build.lib.packages.StructImpl;
 import com.google.devtools.build.lib.starlarkbuildapi.apple.ApplePlatformApi;
-import com.google.devtools.build.lib.starlarkbuildapi.apple.ApplePlatformTypeApi;
 import java.util.HashMap;
 import java.util.Locale;
 import javax.annotation.Nullable;
@@ -70,11 +69,10 @@ public enum ApplePlatform implements ApplePlatformApi {
 
   private final String starlarkKey;
   private final String nameInPlist;
-  private final PlatformType platformType;
+  private final String platformType;
   private final boolean isDevice;
 
-  ApplePlatform(
-      String starlarkKey, String nameInPlist, PlatformType platformType, boolean isDevice) {
+  ApplePlatform(String starlarkKey, String nameInPlist, String platformType, boolean isDevice) {
     this.starlarkKey = starlarkKey;
     this.nameInPlist = Preconditions.checkNotNull(nameInPlist);
     this.platformType = platformType;
@@ -87,7 +85,7 @@ public enum ApplePlatform implements ApplePlatformApi {
   }
 
   @Override
-  public PlatformType getType() {
+  public String getType() {
     return platformType;
   }
 
@@ -109,9 +107,9 @@ public enum ApplePlatform implements ApplePlatformApi {
    */
   public String getTargetPlatform() {
     if (platformType == PlatformType.CATALYST) {
-      return PlatformType.IOS.starlarkKey;
+      return PlatformType.IOS;
     }
-    return platformType.starlarkKey;
+    return platformType;
   }
 
   /**
@@ -169,7 +167,7 @@ public enum ApplePlatform implements ApplePlatformApi {
    * @param platformType platform type that the given cpu value is implied for
    * @param arch architecture representation, such as 'arm64'
    */
-  public static boolean is32Bit(PlatformType platformType, String arch) {
+  public static boolean is32Bit(String platformType, String arch) {
     return BIT_32_TARGET_CPUS.contains(cpuStringForTarget(platformType, arch));
   }
 
@@ -179,9 +177,9 @@ public enum ApplePlatform implements ApplePlatformApi {
    * @param platformType platform type that the given cpu value is implied for
    * @param arch architecture representation, such as 'arm64'
    */
-  public static String cpuStringForTarget(PlatformType platformType, String arch) {
+  public static String cpuStringForTarget(String platformType, String arch) {
     switch (platformType) {
-      case MACOS:
+      case PlatformType.MACOS:
         return String.format("darwin_%s", arch);
       default:
         return String.format("%s_%s", platformType.toString(), arch);
@@ -195,7 +193,7 @@ public enum ApplePlatform implements ApplePlatformApi {
    * @param arch architecture representation, such as 'arm64'
    * @throws IllegalArgumentException if there is no valid apple platform for the given target cpu
    */
-  public static ApplePlatform forTarget(PlatformType platformType, String arch) {
+  public static ApplePlatform forTarget(String platformType, String arch) {
     return forTargetCpu(cpuStringForTarget(platformType, arch));
   }
 
@@ -244,68 +242,23 @@ public enum ApplePlatform implements ApplePlatformApi {
     }
   }
 
+  // LINT.IfChange
+  // TODO(b/331163027): Remove this duplicate of common/objc/apple_platform.PLATFORM_TYPE
   /**
-   * Value used to describe Apple platform "type". A {@link ApplePlatform} is implied from a
-   * platform type (for example, watchOS) together with a cpu value (for example, armv7).
+   * The former enum PlatformType is being migrated to a Starlark struct PLATFORM_TYPEin
+   * builtins_bzl/common/objc/apple_platform.bzl. During the migration, PlatformType has been
+   * converted to a static class hosting string constants as Java duplicates of
+   * apple_platform.PLATFORM_TYPE.
    */
-  // TODO(cparsons): Use these values in static retrieval methods in this class.
-  @Immutable
-  public enum PlatformType implements ApplePlatformTypeApi {
-    IOS("ios"),
-    VISIONOS("visionos"),
-    WATCHOS("watchos"),
-    TVOS("tvos"),
-    MACOS("macos"),
-    CATALYST("catalyst");
+  public static class PlatformType { // implements ApplePlatformTypeApi {
+    public static final String IOS = "ios";
+    public static final String VISIONOS = "visionos";
+    public static final String WATCHOS = "watchos";
+    public static final String TVOS = "tvos";
+    public static final String MACOS = "macos";
+    public static final String CATALYST = "catalyst";
 
-    /**
-     * The key used to access the enum value as a field in the Starlark apple_common.platform_type
-     * struct.
-     */
-    private final String starlarkKey;
-
-    PlatformType(String starlarkKey) {
-      this.starlarkKey = starlarkKey;
-    }
-
-    @Override
-    public boolean isImmutable() {
-      return true; // immutable and Starlark-hashable
-    }
-
-    @Override
-    public String toString() {
-      return name().toLowerCase(Locale.ROOT);
-    }
-
-    /**
-     * Returns the {@link PlatformType} with given name (case insensitive).
-     *
-     * @throws UnsupportedPlatformTypeException if the name does not match a valid platform type.
-     */
-    public static PlatformType fromString(String name) throws UnsupportedPlatformTypeException {
-      for (PlatformType platformType : PlatformType.values()) {
-        if (name.equalsIgnoreCase(platformType.toString())) {
-          return platformType;
-        }
-      }
-      throw new UnsupportedPlatformTypeException(
-          String.format("Unsupported platform type \"%s\"", name));
-    }
-
-    /** Returns a Starlark struct that contains the instances of this enum. */
-    public static StructImpl getStarlarkStruct() {
-      Provider constructor = new BuiltinProvider<StructImpl>("platform_types", StructImpl.class) {};
-      HashMap<String, Object> fields = new HashMap<>();
-      for (PlatformType type : values()) {
-        fields.put(type.starlarkKey, type);
-      }
-      return StarlarkInfo.create(constructor, fields, Location.BUILTIN);
-    }
-
-    @Override
-    public void repr(Printer printer) {
-      printer.append(toString());
-    }
+    private PlatformType() {}
   }
+  // LINT.ThenChange(//src/main/starlark/builtins_bzl/common/objc/apple_platform.bzl)
 }

src/main/java/com/google/devtools/build/lib/rules/apple/AppleCommandLineOptions.java
@@ -15,6 +15,7 @@
 package com.google.devtools.build.lib.rules.apple;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Ascii;
 import com.google.devtools.build.lib.analysis.config.CoreOptionConverters.LabelConverter;
 import com.google.devtools.build.lib.analysis.config.CoreOptionConverters.LabelListConverter;
 import com.google.devtools.build.lib.analysis.config.FragmentOptions;
@@ -22,6 +23,7 @@ import com.google.devtools.build.lib.cmdline.Label;
 import com.google.devtools.build.lib.rules.apple.AppleConfiguration.ConfigurationDistinguisher;
 import com.google.devtools.build.lib.rules.apple.ApplePlatform.PlatformType;
 import com.google.devtools.build.lib.util.CPU;
+import com.google.devtools.common.options.Converter;
 import com.google.devtools.common.options.Converters.CommaSeparatedOptionListConverter;
 import com.google.devtools.common.options.EnumConverter;
 import com.google.devtools.common.options.Option;
@@ -203,14 +205,14 @@ public class AppleCommandLineOptions extends FragmentOptions {
 
   @Option(
       name = "apple_platform_type",
-      defaultValue = "MACOS",
+      defaultValue = "macos",
       converter = PlatformTypeConverter.class,
       documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
       effectTags = {OptionEffectTag.BAZEL_INTERNAL_CONFIGURATION},
       help =
           "Don't set this value from the command line - it is derived from other flags and "
               + "configuration transitions derived from rule attributes")
-  public PlatformType applePlatformType;
+  public String applePlatformType;
 
   @Option(
     name = "apple_split_cpu",
@@ -358,21 +360,21 @@ public class AppleCommandLineOptions extends FragmentOptions {
   public DottedVersion getMinimumOsVersion() {
     DottedVersion.Option option;
     switch (applePlatformType) {
-      case IOS:
-      case CATALYST:
+      case PlatformType.IOS:
+      case PlatformType.CATALYST:
         option = iosMinimumOs;
         break;
-      case MACOS:
+      case PlatformType.MACOS:
         option = macosMinimumOs;
         break;
-      case TVOS:
+      case PlatformType.TVOS:
         option = tvosMinimumOs;
         break;
-      case VISIONOS:
+      case PlatformType.VISIONOS:
         // TODO: Replace with CppOptions.minimumOsVersion
         option = DottedVersion.option(DottedVersion.fromStringUnchecked("1.0"));
         break;
-      case WATCHOS:
+      case PlatformType.WATCHOS:
         option = watchosMinimumOs;
         break;
       default:
@@ -390,10 +392,18 @@ public class AppleCommandLineOptions extends FragmentOptions {
     }
   }
 
-  /** Flag converter for {@link PlatformType}. */
-  public static final class PlatformTypeConverter extends EnumConverter<PlatformType> {
-    public PlatformTypeConverter() {
-      super(PlatformType.class, "Apple platform type");
+  /** Flag converter for PlatformType string flag, just converting to lowercase. */
+  public static final class PlatformTypeConverter extends Converter.Contextless<String> {
+    public PlatformTypeConverter() {}
+
+    @Override
+    public String convert(String input) {
+      return Ascii.toLowerCase(input);
+    }
+
+    @Override
+    public final String getTypeDescription() {
+      return "a string";
     }
   }
 }

src/main/java/com/google/devtools/build/lib/rules/apple/AppleConfiguration.java
@@ -39,7 +39,7 @@ import net.starlark.java.eval.StarlarkValue;
 /** A configuration containing flags required for Apple platforms and tools. */
 @Immutable
 @RequiresOptions(options = {AppleCommandLineOptions.class})
-public class AppleConfiguration extends Fragment implements AppleConfigurationApi<PlatformType> {
+public class AppleConfiguration extends Fragment implements AppleConfigurationApi {
   /** Environment variable name for the developer dir of the selected Xcode. */
   public static final String DEVELOPER_DIR_ENV_NAME = "DEVELOPER_DIR";
 
@@ -72,7 +72,7 @@ public class AppleConfiguration extends Fragment implements AppleConfigurationAp
   @VisibleForTesting
   static final String DEFAULT_IOS_CPU = CPU.getCurrent() == CPU.AARCH64 ? "sim_arm64" : "x86_64";
 
-  private final PlatformType applePlatformType;
+  private final String applePlatformType;
   private final ConfigurationDistinguisher configurationDistinguisher;
   private final Label xcodeConfigLabel;
   private final AppleCommandLineOptions options;
@@ -215,7 +215,7 @@ public class AppleConfiguration extends Fragment implements AppleConfigurationAp
   }
 
   private static String getSingleArchitecture(
-      PlatformType applePlatformType, AppleCpus appleCpus, boolean removeEnvironmentPrefix) {
+      String applePlatformType, AppleCpus appleCpus, boolean removeEnvironmentPrefix) {
     // The removeEnvironmentPrefix argument is used to remove the environment data from the CPU
     // - e.g. whether the target CPU is for simulator, device or catalyst. For older CPUs,
     // no environment may be provided.
@@ -228,22 +228,22 @@ public class AppleConfiguration extends Fragment implements AppleConfigurationAp
     return cpu;
   }
 
-  private static String getPrefixedAppleCpu(PlatformType applePlatformType, AppleCpus appleCpus) {
+  private static String getPrefixedAppleCpu(String applePlatformType, AppleCpus appleCpus) {
     if (!Strings.isNullOrEmpty(appleCpus.appleSplitCpu())) {
       return appleCpus.appleSplitCpu();
     }
     switch (applePlatformType) {
-      case IOS:
+      case PlatformType.IOS:
         return appleCpus.iosMultiCpus().get(0);
-      case VISIONOS:
+      case PlatformType.VISIONOS:
         return appleCpus.visionosCpus().get(0);
-      case WATCHOS:
+      case PlatformType.WATCHOS:
         return appleCpus.watchosCpus().get(0);
-      case TVOS:
+      case PlatformType.TVOS:
         return appleCpus.tvosCpus().get(0);
-      case MACOS:
+      case PlatformType.MACOS:
         return appleCpus.macosCpus().get(0);
-      case CATALYST:
+      case PlatformType.CATALYST:
         return appleCpus.catalystCpus().get(0);
       default:
         throw new IllegalArgumentException("Unhandled platform type " + applePlatformType);
@@ -273,7 +273,7 @@ public class AppleConfiguration extends Fragment implements AppleConfigurationAp
    * @throws IllegalArgumentException if {@code --apple_platform_type} is set (via prior
    *     configuration transition) yet does not match {@code platformType}
    */
-  public List<String> getMultiArchitectures(PlatformType platformType) {
+  public List<String> getMultiArchitectures(String platformType) {
     if (!Strings.isNullOrEmpty(appleCpus.appleSplitCpu())) {
       if (applePlatformType != platformType) {
         throw new IllegalArgumentException(
@@ -283,17 +283,17 @@ public class AppleConfiguration extends Fragment implements AppleConfigurationAp
       return ImmutableList.of(appleCpus.appleSplitCpu());
     }
     switch (platformType) {
-      case IOS:
+      case PlatformType.IOS:
         return appleCpus.iosMultiCpus();
-      case VISIONOS:
+      case PlatformType.VISIONOS:
         return appleCpus.visionosCpus();
-      case WATCHOS:
+      case PlatformType.WATCHOS:
         return appleCpus.watchosCpus();
-      case TVOS:
+      case PlatformType.TVOS:
         return appleCpus.tvosCpus();
-      case MACOS:
+      case PlatformType.MACOS:
         return appleCpus.macosCpus();
-      case CATALYST:
+      case PlatformType.CATALYST:
         return appleCpus.catalystCpus();
       default:
         throw new IllegalArgumentException("Unhandled platform type " + platformType);
@@ -322,17 +322,17 @@ public class AppleConfiguration extends Fragment implements AppleConfigurationAp
    */
   // TODO(bazel-team): This should support returning multiple platforms.
   @Override
-  public ApplePlatform getMultiArchPlatform(PlatformType platformType) {
+  public ApplePlatform getMultiArchPlatform(String platformType) {
     List<String> architectures = getMultiArchitectures(platformType);
     switch (platformType) {
-      case IOS:
+      case PlatformType.IOS:
         for (String arch : architectures) {
           if (ApplePlatform.forTarget(PlatformType.IOS, arch) == ApplePlatform.IOS_DEVICE) {
             return ApplePlatform.IOS_DEVICE;
           }
         }
         return ApplePlatform.IOS_SIMULATOR;
-      case VISIONOS:
+      case PlatformType.VISIONOS:
         for (String arch : architectures) {
           if (ApplePlatform.forTarget(PlatformType.VISIONOS, arch)
               == ApplePlatform.VISIONOS_DEVICE) {
@@ -340,23 +340,23 @@ public class AppleConfiguration extends Fragment implements AppleConfigurationAp
           }
         }
         return ApplePlatform.VISIONOS_SIMULATOR;
-      case WATCHOS:
+      case PlatformType.WATCHOS:
         for (String arch : architectures) {
           if (ApplePlatform.forTarget(PlatformType.WATCHOS, arch) == ApplePlatform.WATCHOS_DEVICE) {
             return ApplePlatform.WATCHOS_DEVICE;
           }
         }
         return ApplePlatform.WATCHOS_SIMULATOR;
-      case TVOS:
+      case PlatformType.TVOS:
         for (String arch : architectures) {
           if (ApplePlatform.forTarget(PlatformType.TVOS, arch) == ApplePlatform.TVOS_DEVICE) {
             return ApplePlatform.TVOS_DEVICE;
           }
         }
         return ApplePlatform.TVOS_SIMULATOR;
-      case MACOS:
+      case PlatformType.MACOS:
         return ApplePlatform.MACOS;
-      case CATALYST:
+      case PlatformType.CATALYST:
         return ApplePlatform.CATALYST;
       default:
         throw new IllegalArgumentException("Unsupported platform type " + platformType);
@@ -436,7 +436,7 @@ public class AppleConfiguration extends Fragment implements AppleConfigurationAp
       throws Fragment.OutputDirectoriesContext.AddToMnemonicException {
     List<String> components = new ArrayList<>();
     if (!appleCpus.appleSplitCpu().isEmpty()) {
-      components.add(applePlatformType.toString().toLowerCase());
+      components.add(applePlatformType);
       components.add(appleCpus.appleSplitCpu());
 
       if (options.getMinimumOsVersion() != null) {

src/main/java/com/google/devtools/build/lib/rules/objc/AppleStarlarkCommon.java
@@ -21,7 +21,6 @@ import com.google.devtools.build.lib.analysis.starlark.StarlarkRuleContext;
 import com.google.devtools.build.lib.packages.Provider;
 import com.google.devtools.build.lib.packages.StructImpl;
 import com.google.devtools.build.lib.rules.apple.ApplePlatform;
-import com.google.devtools.build.lib.rules.apple.ApplePlatform.PlatformType;
 import com.google.devtools.build.lib.rules.apple.DottedVersion;
 import com.google.devtools.build.lib.rules.cpp.CcInfo;
 import com.google.devtools.build.lib.starlarkbuildapi.objc.AppleCommonApi;
@@ -40,7 +39,6 @@ public class AppleStarlarkCommon
   @VisibleForTesting
   public static final String NOT_SET_ERROR = "Value for key %s must be a set, instead found %s.";
 
-  @Nullable private StructImpl platformType;
   @Nullable private StructImpl platform;
 
   public AppleStarlarkCommon() {}
@@ -53,10 +51,8 @@ public class AppleStarlarkCommon
 
   @Override
   public StructImpl getPlatformTypeStruct() {
-    if (platformType == null) {
-      platformType = PlatformType.getStarlarkStruct();
-    }
-    return platformType;
+    // Implemented in builtin Starlark; this is just for docs.
+    throw new UnsupportedOperationException();
   }
 
   @Override

src/main/java/com/google/devtools/build/lib/rules/objc/ObjcConfiguration.java
@@ -27,7 +27,6 @@ import com.google.devtools.build.lib.concurrent.ThreadSafety.Immutable;
 import com.google.devtools.build.lib.packages.AttributeMap;
 import com.google.devtools.build.lib.packages.BuiltinRestriction;
 import com.google.devtools.build.lib.packages.Type;
-import com.google.devtools.build.lib.rules.apple.ApplePlatform.PlatformType;
 import com.google.devtools.build.lib.rules.apple.DottedVersion;
 import com.google.devtools.build.lib.starlarkbuildapi.apple.ObjcConfigurationApi;
 import javax.annotation.Nullable;
@@ -37,7 +36,7 @@ import net.starlark.java.eval.StarlarkThread;
 /** A compiler configuration containing flags required for Objective-C compilation. */
 @Immutable
 @RequiresOptions(options = {ObjcCommandLineOptions.class})
-public class ObjcConfiguration extends Fragment implements ObjcConfigurationApi<PlatformType> {
+public class ObjcConfiguration extends Fragment implements ObjcConfigurationApi {
   @VisibleForTesting
   static final ImmutableList<String> DBG_COPTS =
       ImmutableList.of("-O0", "-DDEBUG=1", "-fstack-protector", "-fstack-protector-all", "-g");

src/main/java/com/google/devtools/build/lib/starlarkbuildapi/apple/AppleConfigurationApi.java
@@ -27,8 +27,7 @@ import net.starlark.java.eval.StarlarkValue;
     name = "apple",
     doc = "A configuration fragment for Apple platforms.",
     category = DocCategory.CONFIGURATION_FRAGMENT)
-public interface AppleConfigurationApi<ApplePlatformTypeApiT extends ApplePlatformTypeApi>
-    extends StarlarkValue {
+public interface AppleConfigurationApi extends StarlarkValue {
 
   @StarlarkMethod(
       name = "single_arch_cpu",
@@ -62,7 +61,7 @@ public interface AppleConfigurationApi<ApplePlatformTypeApiT extends ApplePlatfo
             named = false,
             doc = "The apple platform type.")
       })
-  ApplePlatformApi getMultiArchPlatform(ApplePlatformTypeApiT platformType);
+  ApplePlatformApi getMultiArchPlatform(String platformType);
 
   @Nullable
   @StarlarkMethod(

src/main/java/com/google/devtools/build/lib/starlarkbuildapi/apple/ApplePlatformApi.java
@@ -50,7 +50,7 @@ public interface ApplePlatformApi extends StarlarkValue {
       name = "platform_type",
       doc = "Returns the platform type of this platform.",
       structField = true)
-  ApplePlatformTypeApi getType();
+  String getType();
 
   /**
    * Returns true if this platform is a device platform, or false if this is a simulator platform.

src/main/java/com/google/devtools/build/lib/starlarkbuildapi/apple/ApplePlatformTypeApi.java
@@ -1,38 +1,0 @@
-// Copyright 2018 The Bazel Authors. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package com.google.devtools.build.lib.starlarkbuildapi.apple;
-
-import com.google.devtools.build.docgen.annot.DocCategory;
-import net.starlark.java.annot.StarlarkBuiltin;
-import net.starlark.java.eval.StarlarkValue;
-
-/** Interface for a descriptor for an Apple platform type, such as such as iOS or macOS. */
-@StarlarkBuiltin(
-    name = "apple_platform_type",
-    category = DocCategory.BUILTIN,
-    doc =
-        "Describes an Apple \"platform type\", such as iOS, macOS, tvOS, visionOS, or watchOS. This"
-            + " is distinct from a \"platform\", which is the platform type combined with one or"
-            + " more CPU architectures.<p>Specific instances of this type can be retrieved by"
-            + " accessing the fields of the <a"
-            + " href='../toplevel/apple_common.html#platform_type'>apple_common.platform_type</a>:<br><ul>"
-            + "<li><code>apple_common.platform_type.ios</code></li>"
-            + "<li><code>apple_common.platform_type.macos</code></li>"
-            + "<li><code>apple_common.platform_type.tvos</code></li><li><code>apple_common.platform_type.watchos</code></li></ul><p>Likewise,"
-            + " the platform type of an existing platform value can be retrieved using its"
-            + " <code>platform_type</code> field.<p>Platform types can be converted to a lowercase"
-            + " string (e.g., <code>ios</code> or <code>macos</code>) using the <a"
-            + " href='../globals/all.html#str'>str</a> function.")
-public interface ApplePlatformTypeApi extends StarlarkValue {}

commit 6e12cf7c69fbf5d0b7b40286fe01b26d9f60756b

Fix the platform_type string compares that were broken by https://github.com/bazelbuild/bazel/commit/8e783b065c66c005db26d85e9ec6e8e28c42b2f2 (Migrate apple_common.platform_type to Starlark)
PiperOrigin-RevId: 639007512
Change-Id: If47b4d7f543245e24e21ca2556e74782a0cd7be6

src/main/java/com/google/devtools/build/lib/rules/apple/ApplePlatform.java
@@ -116,7 +116,7 @@ public enum ApplePlatform implements ApplePlatformApi {
    * its own value.
    */
   public String getTargetPlatform() {
-    if (platformType == PlatformType.CATALYST) {
+    if (platformType.equals(PlatformType.CATALYST)) {
       return PlatformType.IOS;
     }
     return platformType;
@@ -130,7 +130,7 @@ public enum ApplePlatform implements ApplePlatformApi {
    * represented in this enumerated type.
    */
   public String getTargetEnvironment() {
-    if (platformType == PlatformType.CATALYST) {
+    if (platformType.equals(PlatformType.CATALYST)) {
       return "macabi";
     }
     return isDevice ? "device" : "simulator";