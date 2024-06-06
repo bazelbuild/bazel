de6c41500bccc70aa7f5673bdf266f8b5e437803
Googler <jingwen@google.com>
2024-06-06 01:27:19 -0700

    Split `SkyframeTests` into individual `java_test`s, improve `BUILD` granularity and test history tracking.
    This halves the build time for a single change to a test file (9.6s [1] to 4.9s [2]) by not rebuilding `SkyframeTests_lib` all the time.
    
    This target was added in 2015 (unknown commit) and has been growing since.
    
    Also add a missing `java_test` for `MacOSXFsEventsDiffAwarenessTest.java`. It was excluded from `SkyframeTests_lib` and didn't have a corresponding test target, and all of its test methods was annotated with `@Ignore`. We could also delete it since it hasn't been running at all.
    
    PiperOrigin-RevId: 640810575
    Change-Id: I88a37beb64d5908ccd48099bd8a7146026153642

--- src/test/java/com/google/devtools/build/lib/skyframe/MacOSXFsEventsDiffAwarenessTest.java
+++ src/test/java/com/google/devtools/build/lib/skyframe/MacOSXFsEventsDiffAwarenessTest.java
@@ -27,6 +27,7 @@ import com.google.devtools.build.lib.testing.common.FakeOptions;
 import com.google.devtools.build.lib.vfs.ModifiedFileSet;
 import com.google.devtools.build.lib.vfs.PathFragment;
 import com.google.devtools.common.options.OptionsProvider;
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
@@ -39,6 +40,7 @@ import java.util.Set;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicReference;
 import org.junit.After;
 import org.junit.Before;
@@ -112,6 +114,7 @@ public class MacOSXFsEventsDiffAwarenessTest {
    * @param rawPaths the files to expect in the view
    * @return the new view
    */
+  @CanIgnoreReturnValue
   private View assertDiff(View view1, Iterable<String> rawPaths)
       throws IncompatibleViewException, BrokenDiffAwarenessException, InterruptedException {
     Set<PathFragment> allPaths = new HashSet<>();
@@ -215,19 +218,21 @@ public class MacOSXFsEventsDiffAwarenessTest {
     dirToFilesToCreate
         .asMap()
         .forEach(
-            (dir, files) ->
-                executor.submit(
-                    () -> {
-                      try {
-                        scratchDir(dir);
-                        for (String file : files) {
-                          scratchFile(file);
+            (dir, files) -> {
+              Future<?> unused =
+                  executor.submit(
+                      () -> {
+                        try {
+                          scratchDir(dir);
+                          for (String file : files) {
+                            scratchFile(file);
+                          }
+                        } catch (IOException e) {
+                          firstError.compareAndSet(null, e);
                         }
-                      } catch (IOException e) {
-                        firstError.compareAndSet(null, e);
-                      }
-                      latch.countDown();
-                    }));
+                        latch.countDown();
+                      });
+            });
     latch.await();
     executor.shutdown();
     IOException e = firstError.get();
