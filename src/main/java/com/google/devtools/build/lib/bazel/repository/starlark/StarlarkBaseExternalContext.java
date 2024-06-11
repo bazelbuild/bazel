8c362a33c591042c1046f15f6a908ba78a4c1171
David Sanderson <32687193+dws@users.noreply.github.com>
2024-06-11 01:56:44 -0700

    Clarify /rules/lib/builtins/repository_ctx description of default working_directory for execute()
    Fixes #21897 per feedback from @meteorcloudy.
    
    Closes #22669.
    
    PiperOrigin-RevId: 642185332
    Change-Id: I188efa9bf0567d14fdd1b67583f51bebd65af295

--- src/main/java/com/google/devtools/build/lib/bazel/repository/starlark/StarlarkBaseExternalContext.java
+++ src/main/java/com/google/devtools/build/lib/bazel/repository/starlark/StarlarkBaseExternalContext.java
@@ -1725,7 +1725,8 @@ public abstract class StarlarkBaseExternalContext implements AutoCloseable, Star
             named = true,
             doc =
                 "Working directory for command execution.\n"
-                    + "Can be relative to the repository root or absolute."),
+                    + "Can be relative to the repository root or absolute.\n"
+                    + "The default is the repository root."),
       })
   public StarlarkExecutionResult execute(
       Sequence<?> arguments, // <String> or <StarlarkPath> or <Label> expected
