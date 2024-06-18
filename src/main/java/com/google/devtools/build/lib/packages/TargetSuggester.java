9368c6020b59dd808c64a2f979ed42cf3ca4c76c
Googler <dacek@google.com>
2024-06-18 05:53:32 -0700

    Re-format comments in TargetSuggester.
    PiperOrigin-RevId: 644353610
    Change-Id: I8afb2286f2a781bbadbb7dc7d7ffaf934f7596e6

--- src/main/java/com/google/devtools/build/lib/packages/TargetSuggester.java
+++ src/main/java/com/google/devtools/build/lib/packages/TargetSuggester.java
@@ -37,8 +37,15 @@ public final class TargetSuggester {
    * Given a nonexistent target and the targets in its package, suggest what the user may have
    * intended based on lexicographic closeness to the possibilities.
    *
-   * <p>This will be pretty printed in the following form No suggested targets -> "". Suggested
-   * target "a" -> "a". Suggested targets "a", "b", "c" -> "a, b, or c".
+   * <p>This will be pretty printed in the following forms:
+   *
+   * <p>No suggested targets -> "".
+   *
+   * <p>Suggested target "a" -> "a".
+   *
+   * <p>Suggested targets "a", "b" -> "a, or b"
+   *
+   * <p>Suggested targets "a", "b", "c" -> "a, b, or c".
    */
   static String suggestTargets(String input, Set<String> words) {
     ImmutableList<String> suggestedTargets = suggestedTargets(input, words);
@@ -46,8 +53,8 @@ public final class TargetSuggester {
   }
 
   /**
-   * Given a requested target and a Set of targets in the same package, return a list of strings
-   * closest based on edit distance.
+   * Given a requested target and a Set of targets in the same package, return a list of the targets
+   * closest to the requested target based on edit distance.
    *
    * <p>If any strings are identical minus capitalization changes, they will be returned. If any
    * other strings are exactly 1 character off, they will be returned. Otherwise, the 10 nearest
@@ -115,7 +122,7 @@ public final class TargetSuggester {
 
   /**
    * Create a pretty-printable String for a list. Joiner doesn't currently support multiple
-   * separators so this is a custom roll for now. Returns a comma-delimited list with " or " before
+   * separators so this is a custom roll for now. Returns a comma-delimited list with ", or " before
    * the last element.
    */
   @VisibleForTesting
@@ -126,9 +133,7 @@ public final class TargetSuggester {
     } else if (targets.size() == 1) {
       targetString = targets.get(0);
     } else {
-
       String firstPart = Joiner.on(", ").join(targets.subList(0, targets.size() - 1));
-
       targetString = Joiner.on(", or ").join(firstPart, Iterables.getLast(targets));
     }
     return " (did you mean " + targetString + "?)";
