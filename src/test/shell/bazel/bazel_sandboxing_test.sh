ff59e73da42ba4fd8abc18f36a80b2d30dcd73a0
Googler <tjgq@google.com>
2024-06-19 04:28:01 -0700

    Fix permissions on tree artifact subdirectories as needed to move them out of the sandbox.
    Similar to https://github.com/bazelbuild/bazel/commit/547f0c6955942e6aafd279adbb2f5368a7a2347e, but also includes the r and x bits.
    
    Fixes #22260.
    
    PiperOrigin-RevId: 644705156
    Change-Id: I5354db76dd2c375a6e686fb4808649053b24c500

--- src/test/shell/bazel/bazel_sandboxing_test.sh
+++ src/test/shell/bazel/bazel_sandboxing_test.sh
@@ -257,8 +257,9 @@ EOF
   bazel build --test_output=streamed :a &>$TEST_log || fail "expected build to succeed"
 }
 
-# Regression test for https://github.com/bazelbuild/bazel/issues/20032.
-function test_read_only_tree_artifact() {
+# Regression test for https://github.com/bazelbuild/bazel/issues/20032 and
+# https://github.com/bazelbuild/bazel/issues/22260.
+function test_permissionless_tree_artifact() {
   create_workspace_with_default_repos WORKSPACE
 
   cat > def.bzl <<'EOF'
@@ -266,7 +267,7 @@ def _r(ctx):
   d = ctx.actions.declare_directory(ctx.label.name)
   ctx.actions.run_shell(
     outputs = [d],
-    command = "touch $1/file.txt && chmod -w $1",
+    command = "touch $1/file.txt && chmod 000 $1",
     arguments = [d.path],
   )
   return DefaultInfo(files = depset([d]))
