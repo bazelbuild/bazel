a45b5112855e8f14bc71e8bd77bd1363f218218d
Jordan Mele <SiliconSoldier@outlook.com.au>
2024-07-01 08:15:39 +0000

    Make recommendation for specific, improve readability with multi-line text blocks

--- src/main/java/com/google/devtools/build/lib/bazel/repository/starlark/StarlarkBaseExternalContext.java
+++ src/main/java/com/google/devtools/build/lib/bazel/repository/starlark/StarlarkBaseExternalContext.java
@@ -598,20 +598,23 @@ public abstract class StarlarkBaseExternalContext implements AutoCloseable, Star
 
   @StarlarkMethod(
       name = "download",
-      doc =
-          "Downloads a file to the output path for the provided url and returns a struct"
-              + " containing <code>success</code>, a flag which is <code>true</code> if the"
-              + " download completed successfully, and if successful, a hash of the file"
-              + " with the fields <code>sha256</code> and <code>integrity</code>."
-              + "Setting an explicit <code>canonical_id</code> is highly recommended. e.g."
-              + "<pre class='language-python'>\n"
-              + "load(\"@bazel_tools//tools/build_defs/repo:cache.bzl\", \"get_default_canonical_id\")\n"
-              + "# ...\n"
-              + "    repository_ctx.download(\n"
-              + "        url = urls,\n"
-              + "        canonical_id = get_default_canonical_id(repository_ctx, urls),\n"
-              + "    ),\n"
-              + "</pre>",
+      doc = """
+        Downloads a file to the output path for the provided url and returns a struct containing \
+        <code>success</code>, a flag which is <code>true</code> if the download completed \
+        successfully, and if successful, a hash of the file with the fields <code>sha256</code> \
+        and <code>integrity</code>. \
+        When <code>sha256</code> or <code>integrity</code> is user specified, setting an explicit \
+        <code>canonical_id</code> is highly recommended. e.g.\
+        <pre class='language-python'>
+        load("@bazel_tools//tools/build_defs/repo:cache.bzl", "get_default_canonical_id")
+        # ...
+            repository_ctx.download(
+                url = urls,
+                integrity = integrity
+                canonical_id = get_default_canonical_id(repository_ctx, urls),
+            ),
+        </pre>
+        """,
       useStarlarkThread = true,
       parameters = {
         @Param(
@@ -787,20 +790,23 @@ public abstract class StarlarkBaseExternalContext implements AutoCloseable, Star
 
   @StarlarkMethod(
       name = "download_and_extract",
-      doc =
-          "Downloads a file to the output path for the provided url, extracts it, and returns a"
-              + " struct containing <code>success</code>, a flag which is <code>true</code> if the"
-              + " download completed successfully, and if successful, a hash of the file with the"
-              + " fields <code>sha256</code> and <code>integrity</code>."
-              + "Setting an explicit <code>canonical_id</code> is highly recommended. e.g."
-              + "<pre class='language-python'>\n"
-              + "load(\"@bazel_tools//tools/build_defs/repo:cache.bzl\", \"get_default_canonical_id\")\n"
-              + "# ...\n"
-              + "    repository_ctx.download_and_extract(\n"
-              + "        url = urls,\n"
-              + "        canonical_id = get_default_canonical_id(repository_ctx, urls),\n"
-              + "    ),\n"
-              + "</pre>",
+      doc = """
+        Downloads a file to the output path for the provided url, extracts it, and returns a \
+        struct containing <code>success</code>, a flag which is <code>true</code> if the download \
+        completed successfully, and if successful, a hash of the file with the fields \
+        <code>sha256</code> and <code>integrity</code>. \
+        When <code>sha256</code> or <code>integrity</code> is user specified, setting an explicit \
+        <code>canonical_id</code> is highly recommended. e.g.\
+        <pre class='language-python'>
+        load("@bazel_tools//tools/build_defs/repo:cache.bzl", "get_default_canonical_id")
+        # ...
+            repository_ctx.download_and_extract(
+                url = urls,
+                integrity = integrity
+                canonical_id = get_default_canonical_id(repository_ctx, urls),
+            ),
+        </pre>
+        """,
       useStarlarkThread = true,
       parameters = {
         @Param(
