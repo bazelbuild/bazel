a36c09f61eb0a10727b13ddd62987a8caf7af45c
Googler <noreply@google.com>
2024-07-17 14:01:51 -0700

    Remove obsolete TODO
    The test was re-enabled, so remove the TODO to re-enable the test.
    
    RELNOTES:none
    PiperOrigin-RevId: 653354141
    Change-Id: Ifbecdcf44cfd48166fb8265fd2b18236ce34e058

--- src/main/starlark/tests/builtins_bzl/cc/cc_shared_library/test_cc_shared_library/cc_shared_library_integration_test.sh
+++ src/main/starlark/tests/builtins_bzl/cc/cc_shared_library/test_cc_shared_library/cc_shared_library_integration_test.sh
@@ -85,5 +85,4 @@ test_shared_library_user_link_flags
 test_shared_library_symbols
 test_binary
 test_cc_test
-# TODO(b/353560944): Re-enable once underlying failure is identified.
 test_number_of_linked_libs
