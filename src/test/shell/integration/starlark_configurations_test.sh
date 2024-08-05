b140ab036a82f067f6961996f621c10008e58627
Yun Peng <pcloudy@google.com>
2024-08-05 10:22:39 +0000

    Fix werid src/test/shell/integration/starlark_configurations_test.sh

--- src/test/shell/integration/starlark_configurations_test.sh
+++ src/test/shell/integration/starlark_configurations_test.sh
@@ -815,7 +815,7 @@ EOF
 
   bazel build //$pkg:demo >& "$TEST_log" && fail "Expected failure"
   expect_not_log "crashed due to an internal error"
-  expect_log "`cfg` must be set to a transition object initialized by the transition() function"
+  expect_log '`cfg` must be set to a transition object initialized by the transition() function'
 }
 
 function write_attr_list_transition() {
