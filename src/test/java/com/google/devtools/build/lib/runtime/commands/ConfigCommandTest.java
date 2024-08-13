9f6a05b74440d99626bb200c6cda7c2756e297ef
Yun Peng <pcloudy@google.com>
2024-08-13 18:24:35 +0200

    Fix //src/test/java/com/google/devtools/build/lib/runtime:ConfigCommandTest

--- src/test/java/com/google/devtools/build/lib/runtime/commands/ConfigCommandTest.java
+++ src/test/java/com/google/devtools/build/lib/runtime/commands/ConfigCommandTest.java
@@ -119,9 +119,6 @@ public class ConfigCommandTest extends BuildIntegrationTestCase {
     params.addAll(TestConstants.PRODUCT_SPECIFIC_FLAGS);
     params.add("//test:buildme");
     params.add("--nobuild"); // Execution phase isn't necessary to collect configurations.
-    // TODO: Enable Bzlmod for this test
-    // https://github.com/bazelbuild/bazel/issues/19823
-    params.add("--noenable_bzlmod");
     Collections.addAll(params, args);
     dispatcher.exec(params, "my client", outErr);
   }
@@ -138,9 +135,6 @@ public class ConfigCommandTest extends BuildIntegrationTestCase {
     params.addAll(TestConstants.PRODUCT_SPECIFIC_FLAGS);
     params.add("//test:buildme_with_transition");
     params.add("--nobuild"); // Execution phase isn't necessary to collect configurations.
-    // TODO: Enable Bzlmod for this test
-    // https://github.com/bazelbuild/bazel/issues/19823
-    params.add("--noenable_bzlmod");
     Collections.addAll(params, args);
     dispatcher.exec(params, "my client", outErr);
   }
@@ -153,9 +147,6 @@ public class ConfigCommandTest extends BuildIntegrationTestCase {
   private RecordingOutErr callConfigCommand(String... args) throws Exception {
     List<String> params = Lists.newArrayList("config");
     params.add("--output=json");
-    // TODO: Enable Bzlmod for this test
-    // https://github.com/bazelbuild/bazel/issues/19823
-    params.add("--noenable_bzlmod");
     Collections.addAll(params, args);
     RecordingOutErr recordingOutErr = new RecordingOutErr();
     dispatcher.exec(params, "my client", recordingOutErr);
