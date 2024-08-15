7765ceef0e069481e151e5b4afba892b1e1caf6f
Googler <jingwen@google.com>
2024-08-15 01:43:56 -0700

    Open source Starlark functionality for Skyframe serialization.
    PiperOrigin-RevId: 663222557
    Change-Id: I8b66b29f3ec15f1a82d1d8569d8fba51f22d817b

--- src/main/java/com/google/devtools/build/lib/skyframe/serialization/CommonSerializationConstants.java
+++ src/main/java/com/google/devtools/build/lib/skyframe/serialization/CommonSerializationConstants.java
@@ -14,12 +14,18 @@
 package com.google.devtools.build.lib.skyframe.serialization;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSortedMap;
 import com.google.devtools.build.lib.actions.ArtifactRoot;
 import com.google.devtools.build.lib.analysis.BlazeDirectories;
 import com.google.devtools.build.lib.analysis.ConfiguredRuleClassProvider;
 import com.google.devtools.build.lib.analysis.config.OutputDirectories.OutputDirectory;
 import com.google.devtools.build.lib.analysis.test.TestConfiguration;
+import com.google.devtools.build.lib.packages.BazelStarlarkEnvironment;
+import com.google.devtools.build.lib.packages.StructProvider;
 import com.google.devtools.build.lib.vfs.Root;
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
+import net.starlark.java.eval.Starlark;
+import net.starlark.java.syntax.Location;
 
 /** Objects that should be serialized as constants in Skyframe serialization. */
 public final class CommonSerializationConstants {
@@ -63,5 +69,39 @@ public final class CommonSerializationConstants {
       }
     }
     return referenceConstants.build();
+  }
+
+  @CanIgnoreReturnValue
+  public static ObjectCodecRegistry.Builder addStarlarkFunctionality(
+      ObjectCodecRegistry.Builder builder, ConfiguredRuleClassProvider ruleClassProvider) {
+    BazelStarlarkEnvironment starlarkEnv = ruleClassProvider.getBazelStarlarkEnvironment();
+    builder
+        .addReferenceConstant(StructProvider.STRUCT)
+        .addReferenceConstant(Starlark.NONE)
+        .addReferenceConstant(Location.BUILTIN)
+        .addReferenceConstants(
+            ImmutableSortedMap.copyOf(starlarkEnv.getUninjectedBuildBzlEnv()).values());
+
+    // Make reference constants for all the native module's potential elements, so that something
+    // like myvar = native.cc_test in a .bzl file doesn't cause problems (otherwise we'd have to
+    // know how to serialize native.cc_test).
+    //
+    // Some of these elements may be overridden to Starlark values by builtins injection; see
+    // StarlarkBuiltinsFunction. The native module object itself is not registered because it is
+    // constructed during builtins injection.
+    //
+    // TODO(b/111564291): how do we get access to all other Starlark built-ins (ones in
+    // apple_common, for instance) and register those? Currently most of those objects are fairly
+    // simple to serialize, but that may change in the future. Also be mindful of whether
+    // StarlarkSemantics (i.e., incompatible/experimental flags) can affect the bindings we see
+    // here. [brandjon: May be able to use the new method
+    // BazelStarlarkEnvironment#getUninjectedBuildBzlEnv.]
+    builder
+        .addReferenceConstants(
+            ImmutableSortedMap.copyOf(starlarkEnv.getUninjectedBuildBzlNativeBindings()).values())
+        .addReferenceConstants(
+            ImmutableSortedMap.copyOf(starlarkEnv.getWorkspaceBzlNativeBindings()).values());
+
+    return builder;
   }
 }
