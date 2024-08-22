e5a083d47e7174357997c851c20521e03a9c80ce
Michael Siegrist <michaelsiegrist88@gmail.com>
2024-08-22 10:36:42 -0700

    Fix ml path for Windows clang-cl cc toolchain
    Assembly files are not valid inputs for `clang-cl.exe`; the MSVC `ml64.exe` must be used instead.
    
    Fixes #23128.
    
    Closes #23337.
    
    PiperOrigin-RevId: 666406544
    Change-Id: Ia7a5fc4702f08a5754145ca286c079d1a4f0e204

--- src/test/py/bazel/bazel_windows_cpp_test.py
+++ src/test/py/bazel/bazel_windows_cpp_test.py
@@ -777,26 +777,61 @@ class BazelWindowsCppTest(test_base.TestBase):
             ')',
         ],
     )
-    self.ScratchFile('BUILD', [
-        'platform(',
-        '  name = "windows_clang",',
-        '  constraint_values = [',
-        '    "@platforms//cpu:x86_64",',
-        '    "@platforms//os:windows",',
-        '    "@bazel_tools//tools/cpp:clang-cl",',
-        '  ]',
-        ')',
-        '',
-        'cc_binary(',
-        '  name = "main",',
-        '  srcs = ["main.cc"],',
-        ')',
-    ])
-    self.ScratchFile('main.cc', [
-        'int main() {',
-        '  return 0;',
-        '}',
-    ])
+    self.ScratchFile(
+        'BUILD',
+        [
+            'platform(',
+            '  name = "windows_clang",',
+            '  constraint_values = [',
+            '    "@platforms//cpu:x86_64",',
+            '    "@platforms//os:windows",',
+            '    "@bazel_tools//tools/cpp:clang-cl",',
+            '  ]',
+            ')',
+            '',
+            'cc_binary(',
+            '  name = "main",',
+            '  srcs = [    "main.cc",',
+            '    "inc.asm",',  # Test assemble action_config
+            '    "dec.S",',  # Test preprocess-assemble action_config
+            '  ],',
+            ')',
+        ],
+    )
+    self.ScratchFile(
+        'main.cc',
+        [
+            'int main() {',
+            '  return 0;',
+            '}',
+        ],
+    )
+    self.ScratchFile(
+        'inc.asm',
+        [
+            '.code',
+            'PUBLIC increment',
+            'increment PROC x:WORD',
+            '  xchg rcx,rax',
+            '  inc rax',
+            '  ret',
+            'increment EndP',
+            'END',
+        ],
+    )
+    self.ScratchFile(
+        'dec.S',
+        [
+            '.code',
+            'PUBLIC decrement',
+            'decrement PROC x:WORD',
+            '  xchg rcx,rax',
+            '  dec rax',
+            '  ret',
+            'decrement EndP',
+            'END',
+        ],
+    )
     exit_code, _, stderr = self.RunBazel(['build', '-s', '//:main'])
     self.AssertExitCode(exit_code, 0, stderr)
     self.assertIn('clang-cl.exe', ''.join(stderr))
