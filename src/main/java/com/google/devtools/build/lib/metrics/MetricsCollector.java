bazelbuild/bazel

commit cb42a1dd08ab984943c1b85a5ae83dd1afd2d564

Automated rollback of commit bcbd956399c8322ede781ee5e2514de6171ca962.
*** Reason for rollback ***

https://github.com/bazelbuild/bazel/commit/4d6513b54fa07321115cac350553b29914d43a65 was innocent.

The issue was caused by a synchronization issue: even though BuildEventStreamer expects no events to be posted after BuildCompleteEvent, there is no guarantee that the UI thread does not do so and if it does, it breaks the logic that sets last_message.

This happens because progress events in response to console messages announce the next progress event in turn and last_message is set when there are no more announced but not posted events. Thus, any console write after BuildCompleteEvent but before the last event announced before BuildCompleteEvent is posted results in an event that is announced but never posted.

*** Original change description ***

Rollback of https://github.com/bazelbuild/bazel/commit/4d6513b54fa07321115cac350553b29914d43a65 due to user breakage reports.

PiperOrigin-RevId: 636821481
Change-Id: I9c2f8a51889fc03c054233beb31873d1dc63fd08

src/main/java/com/google/devtools/build/lib/metrics/MetricsCollector.java
@@ -28,7 +28,6 @@ import com.google.devtools.build.lib.actions.TotalAndConfiguredTargetOnlyMetric;
 import com.google.devtools.build.lib.actions.cache.PostableActionCacheStats;
 import com.google.devtools.build.lib.analysis.AnalysisPhaseCompleteEvent;
 import com.google.devtools.build.lib.analysis.AnalysisPhaseStartedEvent;
-import com.google.devtools.build.lib.analysis.NoBuildRequestFinishedEvent;
 import com.google.devtools.build.lib.bugreport.BugReport;
 import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos.BuildMetrics;
 import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos.BuildMetrics.ActionSummary;
@@ -48,7 +47,7 @@ import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos.Bui
 import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos.BuildMetrics.TimingMetrics;
 import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos.BuildMetrics.WorkerMetrics;
 import com.google.devtools.build.lib.buildeventstream.BuildEventStreamProtos.BuildMetrics.WorkerPoolMetrics;
-import com.google.devtools.build.lib.buildtool.BuildPrecompleteEvent;
+import com.google.devtools.build.lib.buildtool.CommandPrecompleteEvent;
 import com.google.devtools.build.lib.buildtool.buildevent.ExecutionPhaseCompleteEvent;
 import com.google.devtools.build.lib.buildtool.buildevent.ExecutionStartingEvent;
 import com.google.devtools.build.lib.clock.BlazeClock;
@@ -293,19 +292,11 @@ class MetricsCollector {
     buildGraphMetrics.setPostInvocationSkyframeNodeCount(event.getGraphSize());
   }
 
+  // This needs to be done in CommandPrecompleteEvent because the metrics are reported on the BEP,
+  // which is closed in BlazeModule.afterCommand().
   @SuppressWarnings("unused")
   @Subscribe
-  public void onBuildPrecompleteEvent(BuildPrecompleteEvent event) {
-    postBuildMetricsEvent();
-  }
-
-  @SuppressWarnings("unused") // Used reflectively
-  @Subscribe
-  public void onNoBuildRequestFinishedEvent(NoBuildRequestFinishedEvent event) {
-    postBuildMetricsEvent();
-  }
-
-  private void postBuildMetricsEvent() {
+  public void onCommandPrecompleteEvent(CommandPrecompleteEvent event) {
     env.getEventBus().post(new BuildMetricsEvent(createBuildMetrics()));
   }
 

src/main/java/com/google/devtools/build/lib/runtime/BlazeCommandDispatcher.java
@@ -704,7 +704,7 @@ public class BlazeCommandDispatcher implements CommandDispatcher {
       }
 
       needToCallAfterCommand = false;
-      var newResult = runtime.afterCommand(env, result);
+      var newResult = runtime.afterCommand(/* forceKeepStateForTesting= */ false, env, result);
       if (newResult.getExitCode().equals(ExitCode.REMOTE_CACHE_EVICTED)) {
         var executionOptions =
             Preconditions.checkNotNull(options.getOptions(ExecutionOptions.class));
@@ -725,7 +725,7 @@ public class BlazeCommandDispatcher implements CommandDispatcher {
       return result;
     } finally {
       if (needToCallAfterCommand) {
-        BlazeCommandResult newResult = runtime.afterCommand(env, result);
+        BlazeCommandResult newResult = runtime.afterCommand(false, env, result);
         if (!newResult.equals(result)) {
           logger.atWarning().log("afterCommand yielded different result: %s %s", result, newResult);
         }

src/test/shell/integration/build_event_stream_test.sh
@@ -1464,6 +1464,26 @@ function test_skyframe_stats() {
   expect_not_log '"skyfunctionName":"BUILD_INFO","count":'
 }
 
+function test_build_metrics() {
+  mkdir -p a
+  cat > a/BUILD <<'EOF'
+sh_test(name="a", srcs=["a.sh"])
+EOF
+
+  cat > a/a.sh <<'EOF'
+#!/bin/bash
+exit 0
+EOF
+
+  chmod +x a/a.sh
+
+  bazel build --build_event_text_file=bep.txt //a:a || fail "build failed"
+  assert_contains "^build_metrics {" bep.txt
+
+  bazel test --build_event_text_file=bep.txt //a:a || fail "build failed"
+  assert_contains "^build_metrics {" bep.txt
+}
+
 function test_packages_loaded_contains_only_successfully_loaded_packages() {
   mkdir just-to-get-packages-needed-for-toolchain-resolution
   cat > just-to-get-packages-needed-for-toolchain-resolution/BUILD <<'EOF'

src/main/java/com/google/devtools/build/lib/buildeventservice/BuildEventServiceModule.java
@@ -91,6 +91,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -399,7 +400,10 @@ public abstract class BuildEventServiceModule<OptionsT extends BuildEventService
       return;
     }
     if (bepTransports.isEmpty()) {
-      // Exit early if there are no transports to stream to.
+      // Exit early if there are no transports to stream to. However, report that the set of
+      // transports has been determined so that interested parties always get this event if there
+      // was no error during setting up the transports.
+      reporter.post(new AnnounceBuildEventTransportsEvent(bepTransports));
       return;
     }
 
@@ -536,6 +540,9 @@ public abstract class BuildEventServiceModule<OptionsT extends BuildEventService
               "waiting for BES close for invocation " + this.invocationId)) {
         Uninterruptibles.getUninterruptibly(Futures.allAsList(transportFutures.values()));
       }
+    } catch (CancellationException e) {
+      // This is expected if the upload needs to be cancelled for some reason, e.g. an error
+      // interrupting the build.
     } catch (ExecutionException e) {
       // Futures.withTimeout wraps the TimeoutException in an ExecutionException when the future
       // times out.

src/main/java/com/google/devtools/build/lib/buildtool/BuildTool.java
@@ -678,12 +678,6 @@ public class BuildTool {
     }
 
     InterruptedException ie = null;
-    try {
-      env.getSkyframeExecutor().notifyCommandComplete(env.getReporter());
-    } catch (InterruptedException e) {
-      env.getReporter().handle(Event.error("Build interrupted during command completion"));
-      ie = e;
-    }
 
     // The stop time has to be captured before we send the BuildCompleteEvent.
     result.setStopTime(runtime.getClock().currentTimeMillis());
@@ -697,9 +691,7 @@ public class BuildTool {
         env.getReporter().handle(Event.error("Build interrupted during command completion"));
         ie = e;
       }
-      // Ensure deterministic ordering of doing the metrics upload before everything else that
-      // happens when BuildCompleteEvent is posted.
-      env.getEventBus().post(new BuildPrecompleteEvent());
+
       env.getEventBus()
           .post(
               new BuildCompleteEvent(

src/main/java/com/google/devtools/build/lib/buildtool/CommandPrecompleteEvent.java
@@ -14,7 +14,10 @@
 package com.google.devtools.build.lib.buildtool;
 
 /**
- * This event is fired from BuildTool#stopRequest() just before {@link
- * com.google.devtools.build.lib.buildtool.buildevent.BuildCompleteEvent}.
+ * This event is fired before at the end of every command before {@link
+ * com.google.devtools.build.lib.runtime.BlazeModule#afterCommand()} is called.
+ *
+ * <p>Its purpose is to give a chance for event bus listeners to do things that need to happen at
+ * the end of every command but before {@code afterCommand()}.
  */
-public final class BuildPrecompleteEvent {}
+public final class CommandPrecompleteEvent {}

src/main/java/com/google/devtools/build/lib/buildtool/buildevent/TestingCompleteEvent.java
@@ -34,6 +34,9 @@ public class TestingCompleteEvent extends BuildCompletingEvent {
    * @param finishTimeMillis the finish time in milliseconds since the epoch.
    */
   public TestingCompleteEvent(ExitCode exitCode, long finishTimeMillis) {
-    super(exitCode, finishTimeMillis, ImmutableList.of(BuildEventIdUtil.buildToolLogs()));
+    super(
+        exitCode,
+        finishTimeMillis,
+        ImmutableList.of(BuildEventIdUtil.buildToolLogs(), BuildEventIdUtil.buildMetrics()));
   }
 }

src/main/java/com/google/devtools/build/lib/metrics/BUILD
@@ -40,7 +40,6 @@ java_library(
         "//src/main/java/com/google/devtools/build/lib/actions:analysis_graph_stats_event",
         "//src/main/java/com/google/devtools/build/lib/analysis:analysis_phase_complete_event",
         "//src/main/java/com/google/devtools/build/lib/analysis:analysis_phase_started_event",
-        "//src/main/java/com/google/devtools/build/lib/analysis:no_build_request_finished_event",
         "//src/main/java/com/google/devtools/build/lib/bugreport",
         "//src/main/java/com/google/devtools/build/lib/buildeventstream/proto:build_event_stream_java_proto",
         "//src/main/java/com/google/devtools/build/lib/clock",

commit dcc36690049750c3df6b67b06c1e830d6c57b67c

Report CPU and wall times in profiler output for build commands again.
This was broken in https://github.com/bazelbuild/bazel/commit/cb42a1dd08ab984943c1b85a5ae83dd1afd2d564. The reason is that MetricsCollector assumed that the profiler was active while calling finishTimingMetrics(), which used to be the case before that change. However, that change made finishTimingMetrics() be called in CommandPrecompleteEvent and (somewhat confusingly) BuildSummaryStatsModule shuts down the profiler in BuildCompleteEvent, which happens before.

Ideally, we'd shut down the profiler at the last minute, but we can't do that because we publish the profiler data to the BEP, which in turn shuts down in BuildCompleteEvent. So we must shut down the profiler in BuildCompleteEvent, but that doesn't happen on non-build commands so we must also shut it down some place that is called on every command. This of course means that we lose profiler events between BuildCompleteEvent and the actual end of the command.

The profiler is changed so that timing data is available after .stop(), which makes MetricsCollector work again.

RELNOTES: None.
PiperOrigin-RevId: 671727112
Change-Id: Ibf336a7a387449a3edbaffb3d399e5b5e9ed0ff4

src/main/java/com/google/devtools/build/lib/metrics/MetricsCollector.java
@@ -217,7 +217,7 @@ class MetricsCollector {
   public void onSomeExecutionStarted(SomeExecutionStartedEvent event) {
     if (event.countedInExecutionTime()) {
       if (executionStarted.compareAndSet(false, true)) {
-        Duration elapsedWallTime = Profiler.elapsedTimeMaybe();
+        Duration elapsedWallTime = Profiler.getProfileElapsedTime();
         if (elapsedWallTime != null) {
           timingMetrics.setActionsExecutionStartInMs(elapsedWallTime.toMillis());
         }
@@ -490,11 +490,11 @@ class MetricsCollector {
   }
 
   private TimingMetrics finishTimingMetrics() {
-    Duration elapsedWallTime = Profiler.elapsedTimeMaybe();
+    Duration elapsedWallTime = Profiler.getProfileElapsedTime();
     if (elapsedWallTime != null) {
       timingMetrics.setWallTimeInMs(elapsedWallTime.toMillis());
     }
-    Duration cpuTime = Profiler.getProcessCpuTimeMaybe();
+    Duration cpuTime = Profiler.getServerProcessCpuTime();
     if (cpuTime != null) {
       timingMetrics.setCpuTimeInMs(cpuTime.toMillis());
     }

src/main/java/com/google/devtools/build/lib/profiler/Profiler.java
@@ -17,6 +17,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Preconditions;
 import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
@@ -306,9 +307,12 @@ public final class Profiler {
 
   private Clock clock;
   private Set<ProfilerTask> profiledTasks;
-  private volatile long profileStartTime;
+  private volatile boolean active = false;
   private volatile boolean recordAllDurations = false;
-  private Duration profileCpuStartTime;
+  private Duration profileCpuStartTime = Duration.ZERO;
+  private Duration profileCpuEndTime = Duration.ZERO;
+  private Duration profileStartTime = Duration.ZERO;
+  private Duration profileEndTime = Duration.ZERO;
 
   /**
    * The reference to the current writer, if any. If the referenced writer is null, then disk writes
@@ -376,7 +380,8 @@ public final class Profiler {
   // stop instead? However, this is currently only called from one location in a module, and that
   // can't call stop itself. What to do?
   public synchronized ImmutableList<StatRecorder> getTasksHistograms() {
-    return isActive() ? ImmutableList.copyOf(tasksHistograms) : ImmutableList.of();
+    Preconditions.checkState(isActive());
+    return ImmutableList.copyOf(tasksHistograms);
   }
 
   public static Profiler instance() {
@@ -393,14 +398,12 @@ public final class Profiler {
     return -1;
   }
 
-  // Returns the elapsed wall clock time since the profile has been started or null if inactive.
-  @Nullable
-  public static Duration elapsedTimeMaybe() {
-    if (instance.isActive()) {
-      return Duration.ofNanos(instance.clock.nanoTime())
-          .minus(Duration.ofNanos(instance.profileStartTime));
-    }
-    return null;
+  // Returns the elapsed wall clock time since the profile has been started.
+  public static Duration getProfileElapsedTime() {
+    Duration endTime =
+        instance.isActive() ? Duration.ofNanos(instance.clock.nanoTime()) : instance.profileEndTime;
+
+    return endTime.minus(instance.profileStartTime);
   }
 
   private static Duration getProcessCpuTime() {
@@ -409,13 +412,10 @@ public final class Profiler {
     return Duration.ofNanos(bean.getProcessCpuTime());
   }
 
-  // Returns the CPU time since the profile has been started or null if inactive.
-  @Nullable
-  public static Duration getProcessCpuTimeMaybe() {
-    if (instance().isActive()) {
-      return getProcessCpuTime().minus(instance().profileCpuStartTime);
-    }
-    return null;
+  // Returns the CPU time since the profile has been started.
+  public static Duration getServerProcessCpuTime() {
+    Duration cpuEndTime = instance.isActive() ? getProcessCpuTime() : instance.profileCpuEndTime;
+    return cpuEndTime.minus(instance.profileCpuStartTime);
   }
 
   /**
@@ -447,7 +447,8 @@ public final class Profiler {
       boolean collectTaskHistograms,
       LocalResourceCollector localResourceCollector)
       throws IOException {
-    checkState(!isActive(), "Profiler already active");
+    checkState(!active, "Profiler already active");
+
     initHistograms();
 
     this.profiledTasks = profiledTasks.isEmpty() ? profiledTasks : EnumSet.copyOf(profiledTasks);
@@ -484,8 +485,9 @@ public final class Profiler {
     this.writerRef.set(writer);
 
     // Activate profiler.
-    profileStartTime = execStartTimeNanos;
+    profileStartTime = Duration.ofNanos(execStartTimeNanos);
     profileCpuStartTime = getProcessCpuTime();
+    active = true;
 
     this.localResourceCollector = localResourceCollector;
     // Start collecting Bazel and system-wide CPU metric collection.
@@ -553,24 +555,41 @@ public final class Profiler {
    * will no longer be recorded in the profile.
    */
   public synchronized void stop() throws IOException {
-    if (!isActive()) {
+    if (!active) {
       return;
     }
 
     collectActionCounts();
-
     localResourceCollector.stop();
 
     // Log a final event to update the duration of ProfilePhase.FINISH.
     logEvent(ProfilerTask.INFO, "Finishing");
-    JsonTraceFileWriter writer = writerRef.getAndSet(null);
-    if (writer != null) {
-      writer.shutdown();
-      writer = null;
+    try {
+      JsonTraceFileWriter writer = writerRef.getAndSet(null);
+      if (writer != null) {
+        writer.shutdown();
+        writer = null;
+      }
+    } finally {
+      profileCpuEndTime = getProcessCpuTime();
+      profileEndTime = Duration.ofNanos(clock.nanoTime());
+      active = false;
     }
+  }
+
+  /**
+   * Clears the records the profiler instance keeps.
+   *
+   * <p>Should always be called between a {@link #stop()} and a subsequent {@link #start}.
+   */
+  public synchronized void clear() {
+    Preconditions.checkState(!active);
+
     Arrays.fill(tasksHistograms, null);
-    profileStartTime = 0L;
-    profileCpuStartTime = null;
+    profileStartTime = Duration.ZERO;
+    profileEndTime = Duration.ZERO;
+    profileCpuStartTime = Duration.ZERO;
+    profileCpuEndTime = Duration.ZERO;
 
     for (SlowestTaskAggregator aggregator : slowestTasks) {
       if (aggregator != null) {
@@ -583,7 +602,7 @@ public final class Profiler {
 
   /** Returns true iff profiling is currently enabled. */
   public boolean isActive() {
-    return profileStartTime != 0L;
+    return active;
   }
 
   public boolean isProfiling(ProfilerTask type) {

src/main/java/com/google/devtools/build/lib/runtime/BlazeCommandDispatcher.java
@@ -729,11 +729,13 @@ public class BlazeCommandDispatcher implements CommandDispatcher {
 
       try {
         Profiler.instance().stop();
-        MemoryProfiler.instance().stop();
       } catch (IOException e) {
         env.getReporter()
             .handle(Event.error("Error while writing profile file: " + e.getMessage()));
       }
+
+      Profiler.instance().clear();
+      MemoryProfiler.instance().stop();
 
       // Swallow IOException, as we are already in a finally clause
       Flushables.flushQuietly(outErr.getOutputStream());

src/test/java/com/google/devtools/build/lib/profiler/ProfilerTest.java
@@ -64,6 +64,7 @@ public final class ProfilerTest {
   @Before
   public void setManualClock() {
     BlazeClock.setClock(clock);
+    profiler.clear();
   }
 
   @AfterClass
@@ -730,21 +731,6 @@ public final class ProfilerTest {
             /* collectSkyframeCounts= */ false));
     profiler.logSimpleTask(badClock.nanoTime(), ProfilerTask.INFO, "some task");
     profiler.stop();
-  }
-
-  /** Checks that the histograms are cleared in the stop call. */
-  @Test
-  public void testEmptyTaskHistograms() throws Exception {
-    startUnbuffered(getAllProfilerTasks());
-    profiler.logSimpleTaskDuration(
-        Profiler.nanoTimeMaybe(), Duration.ofSeconds(10), ProfilerTask.INFO, "foo");
-    for (StatRecorder recorder : profiler.tasksHistograms) {
-      assertThat(recorder).isNotNull();
-    }
-    profiler.stop();
-    for (StatRecorder recorder : profiler.tasksHistograms) {
-      assertThat(recorder).isNull();
-    }
   }
 
   @Test

src/test/shell/integration/build_event_stream_test.sh
@@ -1478,9 +1478,12 @@ EOF
   chmod +x a/a.sh
 
   bazel build --build_event_text_file=bep.txt //a:a || fail "build failed"
-  assert_contains "^build_metrics {" bep.txt
 
-  bazel test --build_event_text_file=bep.txt //a:a || fail "build failed"
+  assert_contains "^build_metrics {" bep.txt
+  assert_contains "cpu_time_in_ms: " bep.txt
+
+  # Check if null builds still have build metrics
+  bazel build --build_event_text_file=bep.txt //a:a || fail "build failed"
   assert_contains "^build_metrics {" bep.txt
 }
 