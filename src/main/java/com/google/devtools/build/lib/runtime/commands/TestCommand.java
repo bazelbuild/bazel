76b42af100b0a1079a25a7ba24bffcd6bc53f6e0
Googler <kotlaja@google.com>
2024-09-17 11:00:41 -0700

    Automated rollback of commit 750f0a1c05f56403e3b4a3d08b6d7de6ce3ed7f7.
    *** Reason for rollback ***
    
    This CL breaks internal tests.
    
    *** Original change description ***
    
    Fix `_validation` output group merging
    
    While https://github.com/bazelbuild/bazel/commit/cd725834eba3edb53b7a7dd9867363ef1fe00e57 made it so that a rule and an aspect can both specify validation outputs, it accidentally didn't actually merge the `_validation` output groups.
    
    By adding the missing merge logic and having the `ValidateTarget` aspect depend on all other aspects, both a rule and an aspect returning a validation output is now f...
    
    ***
    
    PiperOrigin-RevId: 675632532
    Change-Id: I25f19f22f5dbcce19ea801e88e9ba8595afcf428

--- src/main/java/com/google/devtools/build/lib/runtime/commands/TestCommand.java
+++ src/main/java/com/google/devtools/build/lib/runtime/commands/TestCommand.java
@@ -19,7 +19,6 @@ import static com.google.devtools.build.lib.runtime.Command.BuildPhase.EXECUTES;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
-import com.google.devtools.build.lib.analysis.AspectCollection;
 import com.google.devtools.build.lib.analysis.ConfiguredTarget;
 import com.google.devtools.build.lib.analysis.config.CoreOptions;
 import com.google.devtools.build.lib.buildtool.BuildRequest;
@@ -244,7 +243,7 @@ public class TestCommand implements BlazeCommand {
     if (buildRequest.useValidationAspect()) {
       validatedTargets =
           buildResult.getSuccessfulAspects().stream()
-              .filter(key -> AspectCollection.VALIDATION_ASPECT_NAME.equals(key.getAspectName()))
+              .filter(key -> BuildRequest.VALIDATION_ASPECT_NAME.equals(key.getAspectName()))
               .map(AspectKey::getBaseConfiguredTargetKey)
               .collect(ImmutableSet.toImmutableSet());
     } else {
