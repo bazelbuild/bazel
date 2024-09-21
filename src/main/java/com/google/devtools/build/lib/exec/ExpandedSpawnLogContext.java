5b2421ad3c92cd686dc321f5fd799953d09249df
Fabian Meumertzheim <fabian@meumertzhe.im>
2024-09-21 19:20:18 +0200

    Mark tool runfiles as such in expanded execution log
    The expanded execution log did not mark any runfiles as tools, but the (local and remote) worker implementations explicitly do check whether a runfiles middleman is a tool.
    
    Also uses a previously unused test parameter in `testRunfileSymlinkFileWithDirectoryContents`.

--- src/main/java/com/google/devtools/build/lib/exec/ExpandedSpawnLogContext.java
+++ src/main/java/com/google/devtools/build/lib/exec/ExpandedSpawnLogContext.java
@@ -13,14 +13,20 @@
 // limitations under the License.
 package com.google.devtools.build.lib.exec;
 
+import static com.google.common.collect.ImmutableList.toImmutableList;
+
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.flogger.GoogleLogger;
 import com.google.devtools.build.lib.actions.AbstractAction;
 import com.google.devtools.build.lib.actions.ActionInput;
+import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.actions.Artifact.TreeFileArtifact;
 import com.google.devtools.build.lib.actions.ExecException;
 import com.google.devtools.build.lib.actions.FileArtifactValue.UnresolvedSymlinkArtifactValue;
 import com.google.devtools.build.lib.actions.InputMetadataProvider;
+import com.google.devtools.build.lib.actions.RunfilesArtifactValue;
+import com.google.devtools.build.lib.actions.RunfilesTree;
 import com.google.devtools.build.lib.actions.Spawn;
 import com.google.devtools.build.lib.actions.SpawnResult;
 import com.google.devtools.build.lib.actions.Spawns;
@@ -152,6 +158,15 @@ public class ExpandedSpawnLogContext extends SpawnLogContext {
       builder.addAllEnvironmentVariables(getEnvironmentVariables(spawn));
 
       ImmutableSet<? extends ActionInput> toolFiles = spawn.getToolFiles().toSet();
+      ImmutableList<PathFragment> toolRunfilesDirectories =
+          toolFiles.stream()
+              .filter(
+                  actionInput ->
+                      actionInput instanceof Artifact artifact && artifact.isMiddlemanArtifact())
+              .map(inputMetadataProvider::getRunfilesMetadata)
+              .map(RunfilesArtifactValue::getRunfilesTree)
+              .map(RunfilesTree::getExecPath)
+              .collect(toImmutableList());
 
       try (SilentCloseable c1 = Profiler.instance().profile("logSpawn/inputs")) {
         for (Map.Entry<PathFragment, ActionInput> e : inputMap.entrySet()) {
@@ -160,14 +175,18 @@ public class ExpandedSpawnLogContext extends SpawnLogContext {
 
           if (input instanceof VirtualActionInput.EmptyActionInput) {
             // Do not include a digest, as it's a waste of space.
-            builder.addInputsBuilder().setPath(displayPath.getPathString());
+            builder
+                .addInputsBuilder()
+                .setPath(displayPath.getPathString())
+                .setIsTool(toolRunfilesDirectories.stream().anyMatch(displayPath::startsWith));
             continue;
           }
 
           boolean isTool =
               toolFiles.contains(input)
                   || input instanceof TreeFileArtifact treeFileArtifact
-                      && toolFiles.contains(treeFileArtifact.getParent());
+                      && toolFiles.contains(treeFileArtifact.getParent())
+                  || toolRunfilesDirectories.stream().anyMatch(displayPath::startsWith);
 
           Path contentPath = fileSystem.getPath(execRoot.getRelative(input.getExecPathString()));
 
