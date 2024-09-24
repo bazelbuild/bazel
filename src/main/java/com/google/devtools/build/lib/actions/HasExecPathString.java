abe1f660a314621b949e839275ccc1bff7ebafca
Googler <jhorvitz@google.com>
2024-09-24 13:21:17 -0700

    Extract a `HasExecPath` interface from `ActionInput`.
    PiperOrigin-RevId: 678372011
    Change-Id: I640e2e8adceed167ce87d039cbc24b490beec58f

---
+++ src/main/java/com/google/devtools/build/lib/actions/HasExecPathString.java
@@ -0,0 +1,30 @@
+// Copyright 2024 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+
+package com.google.devtools.build.lib.actions;
+
+/**
+ * A file that has an exec path string.
+ *
+ * <p>Mainly implemented by classes that also implement {@link ActionInput}, but has other
+ * implementations in the Google-internal spawn execution service.
+ */
+public interface HasExecPathString {
+
+  /**
+   * Returns the path to this file.
+   *
+   * <p>The path may be relative to the exec root or absolute.
+   */
+  String getExecPathString();
+}
