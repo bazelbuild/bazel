45df2a5ef85d25c727e3d1dbe157edb6e363cb89
Daniel Wagner-Hall <dwagnerhall@apple.com>
2024-09-24 19:53:33 +0100

    [7.4.0] cquery: Factor RepositoryName into fetching ignored packages (#23727)
    Previously this code was assuming that all cquery target patterns should
    use the root repository's .bazelignore contents which was incorrect.
    
    Because the way this manifested itself was that when cquery drives
    populating the graph and when it queries it, it would use different
    keys, this led to misleading messages about packages not being in scope.
    
    Closes #22142.
    
    PiperOrigin-RevId: 628477138
    Change-Id: I84e3381848b843ca9813acdd0762a3b3e4711a90

--- src/main/java/com/google/devtools/build/lib/query2/PostAnalysisQueryEnvironment.java
+++ src/main/java/com/google/devtools/build/lib/query2/PostAnalysisQueryEnvironment.java
@@ -32,6 +32,7 @@ import com.google.devtools.build.lib.analysis.TargetAndConfiguration;
 import com.google.devtools.build.lib.analysis.config.BuildConfigurationValue;
 import com.google.devtools.build.lib.analysis.configuredtargets.RuleConfiguredTarget;
 import com.google.devtools.build.lib.cmdline.Label;
+import com.google.devtools.build.lib.cmdline.RepositoryName;
 import com.google.devtools.build.lib.cmdline.TargetParsingException;
 import com.google.devtools.build.lib.cmdline.TargetPattern;
 import com.google.devtools.build.lib.collect.compacthashset.CompactHashSet;
@@ -253,12 +254,12 @@ public abstract class PostAnalysisQueryEnvironment<T> extends AbstractBlazeQuery
         ((ConfiguredTargetValue) getConfiguredTargetValue(key)).getConfiguredTarget());
   }
 
-  public InterruptibleSupplier<ImmutableSet<PathFragment>>
-      getIgnoredPackagePrefixesPathFragments() {
+  public InterruptibleSupplier<ImmutableSet<PathFragment>> getIgnoredPackagePrefixesPathFragments(
+      RepositoryName repositoryName) {
     return () -> {
       IgnoredPackagePrefixesValue ignoredPackagePrefixesValue =
           (IgnoredPackagePrefixesValue)
-              walkableGraphSupplier.get().getValue(IgnoredPackagePrefixesValue.key());
+              walkableGraphSupplier.get().getValue(IgnoredPackagePrefixesValue.key(repositoryName));
       return ignoredPackagePrefixesValue == null
           ? ImmutableSet.of()
           : ignoredPackagePrefixesValue.getPatterns();
