61f49cc23d0d502e437a31667ebd1ae8acb55ca5
bazel.build machine account <ci.bazel@gmail.com>
2024-10-01 10:42:01 +0200

    [7.4.0] Allow decompression of nupkg (#23818)
    In some (legacy) projects, nuget is used to handle C++ packages.
    Nuget packages are working fine with WORKSPACE `http_archive` directives
    when explicitly specifying `type = "zip"`.
    
    However, when migrating to bzlmod, we ran into issues when the url in
    source.json is a nupkg file.
    This PR resolves that issue and allows consumption of nupkg archives as
    Bazel modules.
    
    Closes #23645.
    
    PiperOrigin-RevId: 680636414
    Change-Id: Ia7fd3fc738a2dd68b1063dba0dc847c5b0668401
    
    Commit
    https://github.com/bazelbuild/bazel/commit/b38703665f1324b04ce12d4a84443077510f8ee4
    
    Co-authored-by: Dennis van den Berg <dennispvandenberg@gmail.com>

--- src/main/java/com/google/devtools/build/lib/bazel/repository/starlark/StarlarkBaseExternalContext.java
+++ src/main/java/com/google/devtools/build/lib/bazel/repository/starlark/StarlarkBaseExternalContext.java
@@ -861,7 +861,7 @@ When <code>sha256</code> or <code>integrity</code> is user specified, setting an
                 The archive type of the downloaded file. By default, the archive type is \
                 determined from the file extension of the URL. If the file has no \
                 extension, you can explicitly specify either "zip", "jar", "war", \
-                "aar", "tar", "tar.gz", "tgz", "tar.xz", "txz", ".tar.zst", \
+                "aar", "nupkg", "tar", "tar.gz", "tgz", "tar.xz", "txz", ".tar.zst", \
                 ".tzst", "tar.bz2", ".tbz", ".ar", or ".deb" here.
                 """),
         @Param(
