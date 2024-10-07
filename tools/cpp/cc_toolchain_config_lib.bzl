bazelbuild/bazel

commit 1c4e78a9ceab9794140324d64833c628e382a3da

Move cc toolchains to rules_cc
Fixes https://github.com/bazelbuild/bazel/issues/23809
Closes #23891.

PiperOrigin-RevId: 683287734
Change-Id: I461eb49b58918cc174765af936f69de25406d8a2

tools/cpp/osx_cc_wrapper.sh.tpl
@@ -1,138 +1,0 @@
-#!/bin/bash
-#
-# Copyright 2015 The Bazel Authors. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# OS X relpath is not really working. This is a wrapper script around gcc
-# to simulate relpath behavior.
-#
-# This wrapper uses install_name_tool to replace all paths in the binary
-# (bazel-out/.../path/to/original/library.so) by the paths relative to
-# the binary. It parses the command line to behave as rpath is supposed
-# to work.
-#
-# See https://blogs.oracle.com/dipol/entry/dynamic_libraries_rpath_and_mac
-# on how to set those paths for Mach-O binaries.
-#
-set -eu
-
-LIBS=
-LIB_PATHS=
-LIB_DIRS=
-RPATHS=
-OUTPUT=
-
-function parse_option() {
-    local -r opt="$1"
-    if [[ "${OUTPUT}" = "1" ]]; then
-        OUTPUT=$opt
-    elif [[ "$opt" =~ ^-l(.*)$ ]]; then
-        LIBS="${BASH_REMATCH[1]} $LIBS"
-    elif [[ "$opt" =~ ^(.*)\.so$ ]]; then
-        LIB_PATHS="${opt} $LIB_PATHS"
-    elif [[ "$opt" =~ ^(.*)\.dylib$ ]]; then
-        LIB_PATHS="${opt} $LIB_PATHS"
-    elif [[ "$opt" =~ ^-L(.*)$ ]]; then
-        LIB_DIRS="${BASH_REMATCH[1]} $LIB_DIRS"
-    elif [[ "$opt" =~ ^\@loader_path/(.*)$ ]]; then
-        RPATHS="${BASH_REMATCH[1]} ${RPATHS}"
-    elif [[ "$opt" = "-o" ]]; then
-        # output is coming
-        OUTPUT=1
-    fi
-}
-
-# let parse the option list
-for i in "$@"; do
-    if [[ "$i" = @* && -r "${i:1}" ]]; then
-        while IFS= read -r opt
-        do
-            parse_option "$opt"
-        done < "${i:1}" || exit 1
-    else
-        parse_option "$i"
-    fi
-done
-
-# Set-up the environment
-%{env}
-
-# Call the C++ compiler
-%{cc} "$@"
-
-# Generate an empty file if header processing succeeded.
-if [[ "${OUTPUT}" == *.h.processed ]]; then
-  echo -n > "${OUTPUT}"
-fi
-
-function get_library_path() {
-    for libdir in ${LIB_DIRS}; do
-        if [ -f ${libdir}/lib$1.so ]; then
-            echo "${libdir}/lib$1.so"
-        elif [ -f ${libdir}/lib$1.dylib ]; then
-            echo "${libdir}/lib$1.dylib"
-        fi
-    done
-}
-
-# A convenient method to return the actual path even for non symlinks
-# and multi-level symlinks.
-function get_realpath() {
-    local previous="$1"
-    local next=$(readlink "${previous}")
-    while [ -n "${next}" ]; do
-        previous="${next}"
-        next=$(readlink "${previous}")
-    done
-    echo "${previous}"
-}
-
-# Get the path of a lib inside a tool
-function get_otool_path() {
-    # the lib path is the path of the original lib relative to the workspace
-    get_realpath $1 | sed 's|^.*/bazel-out/|bazel-out/|'
-}
-
-function call_install_name() {
-    /usr/bin/xcrun install_name_tool -change $(get_otool_path "$1") \
-        "@loader_path/$2/$3" "${OUTPUT}"
-}
-
-# Do replacements in the output
-for rpath in ${RPATHS}; do
-    for lib in ${LIBS}; do
-        unset libname
-        if [ -f "$(dirname ${OUTPUT})/${rpath}/lib${lib}.so" ]; then
-            libname="lib${lib}.so"
-        elif [ -f "$(dirname ${OUTPUT})/${rpath}/lib${lib}.dylib" ]; then
-            libname="lib${lib}.dylib"
-        fi
-        # ${libname-} --> return $libname if defined, or undefined otherwise. This is to make
-        # this set -e friendly
-        if [[ -n "${libname-}" ]]; then
-            libpath=$(get_library_path ${lib})
-            if [ -n "${libpath}" ]; then
-                call_install_name "${libpath}" "${rpath}" "${libname}"
-            fi
-        fi
-    done
-    for libpath in ${LIB_PATHS}; do
-        if [ -f "$libpath" ]; then
-            libname=$(basename "$libpath")
-            if [ -f "$(dirname ${OUTPUT})/${rpath}/${libname}" ]; then
-                call_install_name "${libpath}" "${rpath}" "${libname}"
-            fi
-        fi
-    done
-done

MODULE.bazel
@@ -23,7 +23,7 @@ bazel_dep(name = "stardoc", version = "0.7.1", repo_name = "io_bazel_skydoc")
 bazel_dep(name = "zstd-jni", version = "1.5.2-3.bcr.1")
 bazel_dep(name = "blake3", version = "1.5.1.bcr.1")
 bazel_dep(name = "zlib", version = "1.3.1.bcr.3")
-bazel_dep(name = "rules_cc", version = "0.0.10")
+bazel_dep(name = "rules_cc", version = "0.0.12")
 bazel_dep(name = "rules_java", version = "8.0.1")
 bazel_dep(name = "rules_graalvm", version = "0.11.1")
 bazel_dep(name = "rules_proto", version = "6.0.2")

src/MODULE.tools
@@ -10,10 +10,6 @@ bazel_dep(name = "buildozer", version = "7.1.2")
 bazel_dep(name = "platforms", version = "0.0.10")
 bazel_dep(name = "zlib", version = "1.3.1.bcr.3")
 
-cc_configure = use_extension("//tools/cpp:cc_configure.bzl", "cc_configure_extension")
-use_repo(cc_configure, "local_config_cc", "local_config_cc_toolchains")
-register_toolchains("@local_config_cc_toolchains//:all")
-
 xcode_configure = use_extension("//tools/osx:xcode_configure.bzl", "xcode_configure_extension")
 use_repo(xcode_configure, "local_config_xcode")
 
@@ -42,7 +38,7 @@ use_repo(buildozer_binary, "buildozer_binary")
 # Dependencies used to auto-load removed symbols and rules from Bazel (due to Starlarkification)
 # See also:  --incompatible_autoload_externally, AutoloadSymbols
 bazel_dep(name = "rules_java", version = "8.0.1")
-bazel_dep(name = "rules_cc", version = "0.0.10")
+bazel_dep(name = "rules_cc", version = "0.0.12")
 bazel_dep(name = "rules_python", version = "0.36.0")
 bazel_dep(name = "rules_shell", version = "0.1.1")
 # add rules_android

src/main/java/com/google/devtools/build/lib/bazel/rules/cpp/BUILD
@@ -62,12 +62,12 @@ gen_workspace_stanza(
     name = "workspace_with_rules_cc",
     out = "cc_configure.WORKSPACE",
     postamble = """
+        load("@bazel_tools//tools/cpp:cc_configure.bzl", "cc_configure")
         cc_configure()
     """,
     preamble = """
         load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
         load("@bazel_tools//tools/build_defs/repo:utils.bzl", "maybe")
-        load("@bazel_tools//tools/cpp:cc_configure.bzl", "cc_configure")
     """,
     repos = ["rules_cc"],
     use_maybe = 1,

src/main/starlark/tests/builtins_bzl/cc_builtin_tests.sh
@@ -61,6 +61,7 @@ function test_starlark_cc() {
   mkdir -p "src/conditions"
   cp "$(rlocation "io_bazel/src/conditions/BUILD")" "src/conditions/BUILD"
 
+  add_rules_cc "MODULE.bazel"
   cat >> MODULE.bazel<<EOF
 bazel_dep(name = "test_repo", repo_name = "my_test_repo")
 local_path_override(

src/test/py/bazel/bazel_windows_cpp_test.py
@@ -25,9 +25,10 @@ class BazelWindowsCppTest(test_base.TestBase):
         'MODULE.bazel',
         [
             'bazel_dep(name = "platforms", version = "0.0.9")',
+            'bazel_dep(name = "rules_cc", version = "0.0.12")',
             (
                 'cc_configure ='
-                ' use_extension("@bazel_tools//tools/cpp:cc_configure.bzl",'
+                ' use_extension("@rules_cc//cc:extensions.bzl",'
                 ' "cc_configure_extension")'
             ),
             'use_repo(cc_configure, "local_config_cc")',

src/test/py/bazel/bazel_windows_test.py
@@ -24,9 +24,10 @@ class BazelWindowsTest(test_base.TestBase):
         'MODULE.bazel',
         [
             'bazel_dep(name = "platforms", version = "0.0.9")',
+            'bazel_dep(name = "rules_cc", version = "0.0.12")',
             (
                 'cc_configure ='
-                ' use_extension("@bazel_tools//tools/cpp:cc_configure.bzl",'
+                ' use_extension("@rules_cc//cc:extensions.bzl",'
                 ' "cc_configure_extension")'
             ),
             'use_repo(cc_configure, "local_config_cc")',

commit e531e7ba2358498f68f2598fc9ef9a5d4f71fd1d

Add back osx_cc_wrapper.sh.tpl
Still used by apple_support

Fix postsubmit breakage caused by https://github.com/bazelbuild/bazel/commit/1c4e78a9ceab9794140324d64833c628e382a3da:
https://buildkite.com/bazel/bazel-bazel/builds/29644

PiperOrigin-RevId: 683346932
Change-Id: I6ff020a578920cb502527d0a9dc79f5c53cf2a93

tools/cpp/cc_toolchain_config_lib.bzl
@@ -26,6 +26,7 @@ load(
     _make_variable = "make_variable",
     _tool = "tool",
     _tool_path = "tool_path",
+    _variable_with_value = "variable_with_value",
     _with_feature_set = "with_feature_set",
 )
 
@@ -41,3 +42,4 @@ make_variable = _make_variable
 tool = _tool
 tool_path = _tool_path
 with_feature_set = _with_feature_set
+variable_with_value = _variable_with_value

tools/cpp/osx_cc_wrapper.sh.tpl
@@ -1,0 +1,138 @@
+#!/bin/bash
+#
+# Copyright 2015 The Bazel Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# OS X relpath is not really working. This is a wrapper script around gcc
+# to simulate relpath behavior.
+#
+# This wrapper uses install_name_tool to replace all paths in the binary
+# (bazel-out/.../path/to/original/library.so) by the paths relative to
+# the binary. It parses the command line to behave as rpath is supposed
+# to work.
+#
+# See https://blogs.oracle.com/dipol/entry/dynamic_libraries_rpath_and_mac
+# on how to set those paths for Mach-O binaries.
+#
+set -eu
+
+LIBS=
+LIB_PATHS=
+LIB_DIRS=
+RPATHS=
+OUTPUT=
+
+function parse_option() {
+    local -r opt="$1"
+    if [[ "${OUTPUT}" = "1" ]]; then
+        OUTPUT=$opt
+    elif [[ "$opt" =~ ^-l(.*)$ ]]; then
+        LIBS="${BASH_REMATCH[1]} $LIBS"
+    elif [[ "$opt" =~ ^(.*)\.so$ ]]; then
+        LIB_PATHS="${opt} $LIB_PATHS"
+    elif [[ "$opt" =~ ^(.*)\.dylib$ ]]; then
+        LIB_PATHS="${opt} $LIB_PATHS"
+    elif [[ "$opt" =~ ^-L(.*)$ ]]; then
+        LIB_DIRS="${BASH_REMATCH[1]} $LIB_DIRS"
+    elif [[ "$opt" =~ ^\@loader_path/(.*)$ ]]; then
+        RPATHS="${BASH_REMATCH[1]} ${RPATHS}"
+    elif [[ "$opt" = "-o" ]]; then
+        # output is coming
+        OUTPUT=1
+    fi
+}
+
+# let parse the option list
+for i in "$@"; do
+    if [[ "$i" = @* && -r "${i:1}" ]]; then
+        while IFS= read -r opt
+        do
+            parse_option "$opt"
+        done < "${i:1}" || exit 1
+    else
+        parse_option "$i"
+    fi
+done
+
+# Set-up the environment
+%{env}
+
+# Call the C++ compiler
+%{cc} "$@"
+
+# Generate an empty file if header processing succeeded.
+if [[ "${OUTPUT}" == *.h.processed ]]; then
+  echo -n > "${OUTPUT}"
+fi
+
+function get_library_path() {
+    for libdir in ${LIB_DIRS}; do
+        if [ -f ${libdir}/lib$1.so ]; then
+            echo "${libdir}/lib$1.so"
+        elif [ -f ${libdir}/lib$1.dylib ]; then
+            echo "${libdir}/lib$1.dylib"
+        fi
+    done
+}
+
+# A convenient method to return the actual path even for non symlinks
+# and multi-level symlinks.
+function get_realpath() {
+    local previous="$1"
+    local next=$(readlink "${previous}")
+    while [ -n "${next}" ]; do
+        previous="${next}"
+        next=$(readlink "${previous}")
+    done
+    echo "${previous}"
+}
+
+# Get the path of a lib inside a tool
+function get_otool_path() {
+    # the lib path is the path of the original lib relative to the workspace
+    get_realpath $1 | sed 's|^.*/bazel-out/|bazel-out/|'
+}
+
+function call_install_name() {
+    /usr/bin/xcrun install_name_tool -change $(get_otool_path "$1") \
+        "@loader_path/$2/$3" "${OUTPUT}"
+}
+
+# Do replacements in the output
+for rpath in ${RPATHS}; do
+    for lib in ${LIBS}; do
+        unset libname
+        if [ -f "$(dirname ${OUTPUT})/${rpath}/lib${lib}.so" ]; then
+            libname="lib${lib}.so"
+        elif [ -f "$(dirname ${OUTPUT})/${rpath}/lib${lib}.dylib" ]; then
+            libname="lib${lib}.dylib"
+        fi
+        # ${libname-} --> return $libname if defined, or undefined otherwise. This is to make
+        # this set -e friendly
+        if [[ -n "${libname-}" ]]; then
+            libpath=$(get_library_path ${lib})
+            if [ -n "${libpath}" ]; then
+                call_install_name "${libpath}" "${rpath}" "${libname}"
+            fi
+        fi
+    done
+    for libpath in ${LIB_PATHS}; do
+        if [ -f "$libpath" ]; then
+            libname=$(basename "$libpath")
+            if [ -f "$(dirname ${OUTPUT})/${rpath}/${libname}" ]; then
+                call_install_name "${libpath}" "${rpath}" "${libname}"
+            fi
+        fi
+    done
+done