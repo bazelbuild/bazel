0fdb36f8a5cf79fda9931b6e3f119bb5f41f0701
Fabian Meumertzheim <fabian@meumertzhe.im>
2024-10-16 08:31:58 -0700

    Propagate `linkopts` of header-only libraries to `cc_shared_library`
    Fixes #21884
    Fixes #23053
    
    Closes #23215.
    
    PiperOrigin-RevId: 686518093
    Change-Id: I1a6699b169fe4291ad0dda3be7e74f38c8e37bf1

--- src/main/starlark/tests/builtins_bzl/cc/cc_shared_library/test/foo.cc
+++ src/main/starlark/tests/builtins_bzl/cc/cc_shared_library/test/foo.cc
@@ -11,12 +11,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#include "src/main/starlark/tests/builtins_bzl/cc/cc_shared_library/test_cc_shared_library/bar.h"
-#include "src/main/starlark/tests/builtins_bzl/cc/cc_shared_library/test_cc_shared_library/baz.h"
-#include "src/main/starlark/tests/builtins_bzl/cc/cc_shared_library/test_cc_shared_library/direct_so_file_cc_lib.h"
-#include "src/main/starlark/tests/builtins_bzl/cc/cc_shared_library/test_cc_shared_library/direct_so_file_cc_lib2.h"
-#include "src/main/starlark/tests/builtins_bzl/cc/cc_shared_library/test_cc_shared_library/qux.h"
-#include "src/main/starlark/tests/builtins_bzl/cc/cc_shared_library/test_cc_shared_library3/diff_pkg.h"
+#include "src/main/starlark/tests/builtins_bzl/cc/cc_shared_library/test/bar.h"
+#include "src/main/starlark/tests/builtins_bzl/cc/cc_shared_library/test/baz.h"
+#include "src/main/starlark/tests/builtins_bzl/cc/cc_shared_library/test/direct_so_file_cc_lib.h"
+#include "src/main/starlark/tests/builtins_bzl/cc/cc_shared_library/test/direct_so_file_cc_lib2.h"
+#include "src/main/starlark/tests/builtins_bzl/cc/cc_shared_library/test/qux.h"
+#include "src/main/starlark/tests/builtins_bzl/cc/cc_shared_library/test3/diff_pkg.h"
 
 int foo() {
   diff_pkg();
