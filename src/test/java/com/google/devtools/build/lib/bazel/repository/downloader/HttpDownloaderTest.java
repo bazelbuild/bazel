b02c06748c93a126db95261a77a57903e9dc002d
bazel.build machine account <ci.bazel@gmail.com>
2024-10-17 10:37:46 +0200

    [7.4.0] Properly cancel repository downloads (#24001)
    When cancelling an asynchronous repository download task, an interrupt
    signal is sent to the download thread. This doesn't mean that the
    download stops immediately. Avoid restarting a download until the
    previous download has actually stopped, so that the new download is able
    to clean old data without crashing (on Windows).
    
    Fixes #21773
    
    Closes #23837.
    
    PiperOrigin-RevId: 686175953
    Change-Id: I8d75f905b739d38b6cb430d5b5e84fda9a2d14e3
    
    Commit
    https://github.com/bazelbuild/bazel/commit/48338d2ff33586a0d053e358da6b94e23fe0e2eb
    
    Co-authored-by: Fredrik Medley <fredrik@meroton.com>

--- src/test/java/com/google/devtools/build/lib/bazel/repository/downloader/HttpDownloaderTest.java
+++ src/test/java/com/google/devtools/build/lib/bazel/repository/downloader/HttpDownloaderTest.java
@@ -54,6 +54,7 @@ import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.Phaser;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.After;
 import org.junit.Ignore;
@@ -786,6 +787,7 @@ public class HttpDownloaderTest {
       Map<String, String> clientEnv,
       String context)
       throws IOException, InterruptedException {
+    Phaser downloadPhaser = new Phaser();
     try (ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
       Future<Path> future =
           downloadManager.startDownload(
@@ -799,8 +801,12 @@ public class HttpDownloaderTest {
               output,
               eventHandler,
               clientEnv,
-              context);
-      return downloadManager.finalizeDownload(future);
+              context,
+              downloadPhaser);
+      Path downloadedPath = downloadManager.finalizeDownload(future);
+      // Should not be in the download phase.
+      assertThat(downloadPhaser.getPhase()).isNotEqualTo(0);
+      return downloadedPath;
     }
   }
 }
