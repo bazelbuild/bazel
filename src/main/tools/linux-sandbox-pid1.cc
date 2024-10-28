cf9e21b3776cb619dffa5c8b479fbba59b3d50d1
Fabian Meumertzheim <fabian@meumertzhe.im>
2024-10-28 15:20:55 +0100

    Allow running `--sandbox_debug` command multiple times

--- src/main/tools/linux-sandbox-pid1.cc
+++ src/main/tools/linux-sandbox-pid1.cc
@@ -73,7 +73,7 @@ static int global_child_pid;
 
 // Helper methods
 static void CreateFile(const char *path) {
-  int handle = open(path, O_CREAT | O_WRONLY | O_EXCL, 0666);
+  int handle = open(path, O_CREAT | O_WRONLY | O_TRUNC | O_NOFOLLOW, 0666);
   if (handle < 0) {
     DIE("open");
   }
@@ -87,6 +87,15 @@ static void CreateFile(const char *path) {
 // certain filesystems (e.g. XFS).
 static void LinkFile(const char *path) {
   if (link("tmp/empty_file", path) < 0) {
+    if (errno == EEXIST) {
+      // Try to remove the existing file first.
+      if (unlink(path) < 0) {
+        DIE("unlink %s", path);
+      }
+      if (link("tmp/empty_file", path) == 0) {
+        return;
+      }
+    }
     DIE("link %s", path);
   }
 }
