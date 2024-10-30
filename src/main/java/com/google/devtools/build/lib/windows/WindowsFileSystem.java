bazelbuild/bazel

commit a0a72265ea37270c110630f528127e18939f4016

Performance optimization for isSymbolicLink() calls on Windows
Current implementation for WindowsFileSystem.isSymbolicLink() makes unnecessary system calls which significantly affect the performance. Original code goes through the following:
- AbstractFileSystemWithCustomStat.isSymbolicLink
  - WindowsFileSystem.stat
    - WindowsFileSystem.getIoFile
    - JavaIoFileSystem.getNioPath
    - Files.readAttributes
    - WindowsFileSystem.fileIsSymbolicLink
    - WindowsFileOperations.getLastChangeTime
This implementation skips most of them.

Closes #24047.

PiperOrigin-RevId: 690964117
Change-Id: I2b407d9c69af62e770684d868d04e60d7ce1773e

src/main/java/com/google/devtools/build/lib/windows/WindowsFileSystem.java
@@ -27,6 +27,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.attribute.DosFileAttributes;
+import javax.annotation.Nullable;
 
 /** File system implementation for Windows. */
 @ThreadSafe
@@ -148,14 +149,14 @@ public class WindowsFileSystem extends JavaIoFileSystem {
       throw new FileNotFoundException(path + ERR_NO_SUCH_FILE_OR_DIR);
     }
 
-    final boolean isSymbolicLink = !followSymlinks && fileIsSymbolicLink(file);
-    final long lastChangeTime =
-        WindowsFileOperations.getLastChangeTime(getNioPath(path).toString(), followSymlinks);
     FileStatus status =
         new FileStatus() {
+          @Nullable volatile Boolean isSymbolicLink; // null if not yet known
+          volatile long lastChangeTime = -1;
+
           @Override
           public boolean isFile() {
-            return !isSymbolicLink && (attributes.isRegularFile() || isSpecialFile());
+            return !isSymbolicLink() && (attributes.isRegularFile() || isSpecialFile());
           }
 
           @Override
@@ -163,16 +164,19 @@ public class WindowsFileSystem extends JavaIoFileSystem {
             // attributes.isOther() returns false for symlinks but returns true for junctions.
             // Bazel treats junctions like symlinks. So let's return false here for junctions.
             // This fixes https://github.com/bazelbuild/bazel/issues/9176
-            return !isSymbolicLink && attributes.isOther();
+            return !isSymbolicLink() && attributes.isOther();
           }
 
           @Override
           public boolean isDirectory() {
-            return !isSymbolicLink && attributes.isDirectory();
+            return !isSymbolicLink() && attributes.isDirectory();
           }
 
           @Override
           public boolean isSymbolicLink() {
+            if (isSymbolicLink == null) {
+              isSymbolicLink = !followSymlinks && fileIsSymbolicLink(file);
+            }
             return isSymbolicLink;
           }
 
@@ -187,7 +191,12 @@ public class WindowsFileSystem extends JavaIoFileSystem {
           }
 
           @Override
-          public long getLastChangeTime() {
+          public long getLastChangeTime() throws IOException {
+            if (lastChangeTime == -1) {
+              lastChangeTime =
+                  WindowsFileOperations.getLastChangeTime(
+                      getNioPath(path).toString(), followSymlinks);
+            }
             return lastChangeTime;
           }
 
@@ -205,6 +214,11 @@ public class WindowsFileSystem extends JavaIoFileSystem {
         };
 
     return status;
+  }
+
+  @Override
+  protected boolean isSymbolicLink(PathFragment path) {
+    return fileIsSymbolicLink(getIoFile(path));
   }
 
   @Override

src/main/java/com/google/devtools/build/lib/windows/BUILD
@@ -42,5 +42,6 @@ java_library(
         "//src/main/java/com/google/devtools/build/lib/vfs",
         "//src/main/java/com/google/devtools/build/lib/vfs:pathfragment",
         "//third_party:guava",
+        "//third_party:jsr305",
     ],
 )

commit e858a5552ed0ff7500fc3e7788cb370d9d920e7d

Fix new data races in `WindowsFileSystem`
a0a72265ea37270c110630f528127e18939f4016 introduced a non-benign data race by not following the "single read" requirement for a benign data race during lazy initialization.

src/main/java/com/google/devtools/build/lib/windows/WindowsFileSystem.java
@@ -151,8 +151,8 @@ public class WindowsFileSystem extends JavaIoFileSystem {
 
     FileStatus status =
         new FileStatus() {
-          @Nullable volatile Boolean isSymbolicLink; // null if not yet known
-          volatile long lastChangeTime = -1;
+          @Nullable Boolean isSymbolicLink; // null if not yet known
+          long lastChangeTime = -1;
 
           @Override
           public boolean isFile() {
@@ -174,10 +174,12 @@ public class WindowsFileSystem extends JavaIoFileSystem {
 
           @Override
           public boolean isSymbolicLink() {
-            if (isSymbolicLink == null) {
-              isSymbolicLink = !followSymlinks && fileIsSymbolicLink(file);
+            Boolean localIsSymbolicLink = isSymbolicLink;
+            if (localIsSymbolicLink == null) {
+              localIsSymbolicLink = !followSymlinks && fileIsSymbolicLink(file);
+              isSymbolicLink = localIsSymbolicLink;
             }
-            return isSymbolicLink;
+            return localIsSymbolicLink;
           }
 
           @Override
@@ -192,12 +194,14 @@ public class WindowsFileSystem extends JavaIoFileSystem {
 
           @Override
           public long getLastChangeTime() throws IOException {
-            if (lastChangeTime == -1) {
-              lastChangeTime =
+            long localLastChangeTime = lastChangeTime;
+            if (localLastChangeTime == -1) {
+              localLastChangeTime =
                   WindowsFileOperations.getLastChangeTime(
                       getNioPath(path).toString(), followSymlinks);
+              lastChangeTime = localLastChangeTime;
             }
-            return lastChangeTime;
+            return localLastChangeTime;
           }
 
           @Override