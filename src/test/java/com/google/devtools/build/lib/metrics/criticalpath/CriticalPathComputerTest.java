bazelbuild/bazel

commit 09874b5b722c3fc0902e73bd5545ead129b72f36

Fix missing critical path components due to change-pruning.
The `CritialPathComputer` listens action execution related events to re-construct the action graph. And compute the critical path out of it.

However, during an incremental build, actions might be change-pruned if all of their inputs are not changed after re-execution. It means the action execution events will not be emitted for these actions. The `CriticalPathComputer` will only see a partial action graph resulting in wrong critical path.

This CL fixes that by querying skyframe for the missing generating action in the case of change-pruning.

PiperOrigin-RevId: 691464331
Change-Id: I70c9afbc56ff8c5375cfeff01071be2d6ed95494

src/main/java/com/google/devtools/build/lib/metrics/criticalpath/BUILD
@@ -21,6 +21,7 @@ java_library(
         "//src/main/java/com/google/devtools/build/lib/cmdline",
         "//src/main/java/com/google/devtools/build/lib/concurrent",
         "//src/main/java/com/google/devtools/build/lib/skyframe/rewinding:action_rewound_event",
+        "//src/main/java/com/google/devtools/build/skyframe",
         "//third_party:flogger",
         "//third_party:guava",
         "//third_party:jsr305",

src/main/java/com/google/devtools/build/lib/metrics/criticalpath/CriticalPathComputer.java
@@ -36,6 +36,7 @@ import com.google.devtools.build.lib.actions.SpawnExecutedEvent;
 import com.google.devtools.build.lib.actions.SpawnMetrics;
 import com.google.devtools.build.lib.actions.SpawnResult;
 import com.google.devtools.build.lib.skyframe.rewinding.ActionRewoundEvent;
+import com.google.devtools.build.skyframe.WalkableGraph;
 import java.time.Duration;
 import java.util.Comparator;
 import java.util.List;
@@ -45,6 +46,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BinaryOperator;
 import java.util.stream.Stream;
+import javax.annotation.Nullable;
 import javax.annotation.concurrent.ThreadSafe;
 
 /**
@@ -75,12 +77,14 @@ public class CriticalPathComputer {
   private final ConcurrentMap<Artifact, CriticalPathComponent> outputArtifactToComponent =
       Maps.newConcurrentMap();
   private final ActionKeyContext actionKeyContext;
+  @Nullable private final WalkableGraph graph;
 
   /** Maximum critical path found. */
   private final AtomicReference<CriticalPathComponent> maxCriticalPath = new AtomicReference<>();
 
-  public CriticalPathComputer(ActionKeyContext actionKeyContext) {
+  public CriticalPathComputer(ActionKeyContext actionKeyContext, @Nullable WalkableGraph graph) {
     this.actionKeyContext = actionKeyContext;
+    this.graph = graph;
   }
 
   /**
@@ -301,7 +305,7 @@ public class CriticalPathComputer {
    */
   @Subscribe
   @AllowConcurrentEvents
-  public void actionComplete(ActionCompletionEvent event) {
+  public void actionComplete(ActionCompletionEvent event) throws InterruptedException {
     Action action = event.getAction();
     CriticalPathComponent component =
         Preconditions.checkNotNull(
@@ -336,9 +340,10 @@ public class CriticalPathComputer {
       long finishTimeNanos,
       Action action,
       CriticalPathComponent component,
-      String finalizeReason) {
+      String finalizeReason)
+      throws InterruptedException {
     for (Artifact input : action.getInputs().toList()) {
-      addArtifactDependency(component, input, finishTimeNanos);
+      addArtifactDependency(component, input, startTimeNanos, finishTimeNanos);
     }
     if (Duration.ofNanos(finishTimeNanos - startTimeNanos).compareTo(Duration.ofMillis(-5)) < 0) {
       // See note in {@link Clock#nanoTime} about non increasing subsequent #nanoTime calls.
@@ -352,8 +357,23 @@ public class CriticalPathComputer {
 
   /** If "input" is a generated artifact, link its critical path to the one we're building. */
   private void addArtifactDependency(
-      CriticalPathComponent actionStats, Artifact input, long componentFinishNanos) {
+      CriticalPathComponent actionStats,
+      Artifact input,
+      long componentStartNanos,
+      long componentFinishNanos)
+      throws InterruptedException {
     CriticalPathComponent depComponent = outputArtifactToComponent.get(input);
+
+    if (depComponent == null && !input.isSourceArtifact() && graph != null) {
+      // The generating action of the input is missing. It happens when the action was change
+      // pruned in an incremental build. Query skyframe for the Action data.
+      if (Actions.getGeneratingAction(graph, input) instanceof Action action) {
+        depComponent = tryAddComponent(createComponent(action, componentStartNanos));
+        finalizeActionStat(
+            componentStartNanos, componentStartNanos, action, depComponent, "change pruning");
+      }
+    }
+
     // Typically, the dep component should already be finished since its output was used as an input
     // for a just-completed action. However, we tolerate it still running for (a) action rewinding
     // and (b) the rare case that an action depending on a previously-cached shared action sees a

src/test/java/com/google/devtools/build/lib/metrics/criticalpath/BUILD
@@ -19,18 +19,23 @@ java_test(
     deps = [
         "//src/main/java/com/google/devtools/build/lib/actions",
         "//src/main/java/com/google/devtools/build/lib/actions:action_lookup_data",
+        "//src/main/java/com/google/devtools/build/lib/actions:action_lookup_key",
         "//src/main/java/com/google/devtools/build/lib/actions:artifacts",
         "//src/main/java/com/google/devtools/build/lib/actions:localhost_capacity",
         "//src/main/java/com/google/devtools/build/lib/clock",
         "//src/main/java/com/google/devtools/build/lib/metrics/criticalpath",
         "//src/main/java/com/google/devtools/build/lib/skyframe/rewinding:action_rewound_event",
+        "//src/main/java/com/google/devtools/build/lib/util:pair",
         "//src/main/java/com/google/devtools/build/lib/vfs",
         "//src/main/java/com/google/devtools/build/lib/vfs:pathfragment",
+        "//src/main/java/com/google/devtools/build/skyframe",
+        "//src/main/java/com/google/devtools/build/skyframe:skyframe-objects",
         "//src/test/java/com/google/devtools/build/lib/actions/util",
         "//src/test/java/com/google/devtools/build/lib/exec/util",
         "//src/test/java/com/google/devtools/build/lib/testutil",
         "//third_party:error_prone_annotations",
         "//third_party:guava",
+        "//third_party:jsr305",
         "//third_party:junit4",
         "//third_party:mockito",
         "//third_party:truth",

src/test/java/com/google/devtools/build/lib/metrics/criticalpath/CriticalPathComputerTest.java
@@ -24,9 +24,12 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.devtools.build.lib.actions.Action;
+import com.google.devtools.build.lib.actions.ActionAnalysisMetadata;
 import com.google.devtools.build.lib.actions.ActionCompletionEvent;
 import com.google.devtools.build.lib.actions.ActionKeyContext;
 import com.google.devtools.build.lib.actions.ActionLookupData;
+import com.google.devtools.build.lib.actions.ActionLookupKey;
+import com.google.devtools.build.lib.actions.ActionLookupValue;
 import com.google.devtools.build.lib.actions.ActionStartedEvent;
 import com.google.devtools.build.lib.actions.AggregatedSpawnMetrics;
 import com.google.devtools.build.lib.actions.Artifact;
@@ -51,14 +54,20 @@ import com.google.devtools.build.lib.exec.util.FakeActionInputFileCache;
 import com.google.devtools.build.lib.skyframe.rewinding.ActionRewoundEvent;
 import com.google.devtools.build.lib.testutil.FoundationTestCase;
 import com.google.devtools.build.lib.testutil.ManualClock;
+import com.google.devtools.build.lib.util.Pair;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.PathFragment;
 import com.google.devtools.build.lib.vfs.Root;
+import com.google.devtools.build.skyframe.SkyKey;
+import com.google.devtools.build.skyframe.SkyValue;
+import com.google.devtools.build.skyframe.WalkableGraph;
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import javax.annotation.Nullable;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -87,7 +96,7 @@ public class CriticalPathComputerTest extends FoundationTestCase {
   @Before
   public void createComputer() {
     clock = new ManualClock();
-    computer = new CriticalPathComputer(new ActionKeyContext());
+    computer = new CriticalPathComputer(new ActionKeyContext(), /* graph= */ null);
   }
 
   private static void assertActionMatches(Action action, CriticalPathComponent component) {
@@ -997,6 +1006,144 @@ public class CriticalPathComputerTest extends FoundationTestCase {
             mock(ActionLookupData.class)));
     assertThat(Iterables.getOnlyElement(computer.aggregate().components()).getAction())
         .isEqualTo(parentAction);
+  }
+
+  @Test
+  public void testChangePruning() throws Exception {
+    MockAction action1 =
+        new MockAction(ImmutableSet.of(), ImmutableSet.of(derivedArtifact("test/action1.out")));
+    MockAction action2 =
+        new MockAction(
+            ImmutableSet.of(derivedArtifact("test/action1.out")),
+            ImmutableSet.of(derivedArtifact("test/action2.out")));
+    MockAction action3 =
+        new MockAction(
+            ImmutableList.of(derivedArtifact("test/action2.out")),
+            ImmutableSet.of(derivedArtifact("test/action3.out")));
+    MockAction action4 =
+        new MockAction(
+            ImmutableList.of(
+                derivedArtifact("test/action1.out"), derivedArtifact("test/action3.out")),
+            ImmutableSet.of(derivedArtifact("test/action4.out")));
+
+    computer =
+        new CriticalPathComputer(
+            new ActionKeyContext(),
+            new WalkableGraph() {
+              @Nullable
+              @Override
+              public SkyValue getValue(SkyKey key) throws InterruptedException {
+                if (key instanceof ActionLookupKey) {
+                  return new ActionLookupValue() {
+                    @Override
+                    public ImmutableList<ActionAnalysisMetadata> getActions() {
+                      return ImmutableList.of(action3);
+                    }
+                  };
+                }
+                throw new UnsupportedOperationException();
+              }
+
+              @Override
+              public Map<SkyKey, SkyValue> getSuccessfulValues(Iterable<? extends SkyKey> keys)
+                  throws InterruptedException {
+                throw new UnsupportedOperationException();
+              }
+
+              @Override
+              public Map<SkyKey, Exception> getMissingAndExceptions(Iterable<SkyKey> keys)
+                  throws InterruptedException {
+                throw new UnsupportedOperationException();
+              }
+
+              @Nullable
+              @Override
+              public Exception getException(SkyKey key) throws InterruptedException {
+                throw new UnsupportedOperationException();
+              }
+
+              @Override
+              public boolean isCycle(SkyKey key) throws InterruptedException {
+                throw new UnsupportedOperationException();
+              }
+
+              @Override
+              public Map<SkyKey, Iterable<SkyKey>> getDirectDeps(Iterable<SkyKey> keys)
+                  throws InterruptedException {
+                throw new UnsupportedOperationException();
+              }
+
+              @Override
+              public Iterable<SkyKey> getDirectDeps(SkyKey key) throws InterruptedException {
+                throw new UnsupportedOperationException();
+              }
+
+              @Override
+              public Map<SkyKey, Iterable<SkyKey>> getReverseDeps(Iterable<? extends SkyKey> keys)
+                  throws InterruptedException {
+                throw new UnsupportedOperationException();
+              }
+
+              @Override
+              public Map<SkyKey, Pair<SkyValue, Iterable<SkyKey>>> getValueAndRdeps(
+                  Iterable<SkyKey> keys) throws InterruptedException {
+                throw new UnsupportedOperationException();
+              }
+            });
+
+    // Action 1 - 0s - 1s
+    long action1Start = clock.nanoTime();
+    computer.actionStarted(new ActionStartedEvent(action1, action1Start));
+    clock.advanceMillis(1000);
+    computer.actionComplete(
+        new ActionCompletionEvent(
+            action1Start,
+            clock.nanoTime(),
+            action1,
+            new FakeActionInputFileCache(),
+            mock(ActionLookupData.class)));
+    // Action 2 - 1s - 3s
+    long action2Start = clock.nanoTime();
+    computer.actionStarted(new ActionStartedEvent(action2, action2Start));
+    clock.advanceMillis(2000);
+    computer.actionComplete(
+        new ActionCompletionEvent(
+            action2Start,
+            clock.nanoTime(),
+            action2,
+            new FakeActionInputFileCache(),
+            mock(ActionLookupData.class)));
+    // Action 3 - 3s - 3s, change pruned, no events
+    // Action 4 - 3s - 6s
+    long action4Start = clock.nanoTime();
+    computer.actionStarted(new ActionStartedEvent(action4, action4Start));
+    clock.advanceMillis(3000);
+    computer.actionComplete(
+        new ActionCompletionEvent(
+            action4Start,
+            clock.nanoTime(),
+            action4,
+            new FakeActionInputFileCache(),
+            mock(ActionLookupData.class)));
+
+    // The total run time should be 6s (Action 1 + Action 2 + Action 4) since Action 3 is
+    // change-pruned.
+    assertThat(computer.getMaxCriticalPath().getAggregatedElapsedTime())
+        .isEqualTo(Duration.ofSeconds(6));
+    AggregatedCriticalPath criticalPath = computer.aggregate();
+    assertThat(criticalPath.components()).hasSize(4);
+    // Action 4 has a run time of 2 seconds
+    assertThat(criticalPath.components().get(0).prettyPrintAction()).contains("action4.out");
+    assertThat(criticalPath.components().get(0).getElapsedTime()).isEqualTo(Duration.ofSeconds(3));
+    // Action 3 has a run time of 0 seconds
+    assertThat(criticalPath.components().get(1).prettyPrintAction()).contains("action3.out");
+    assertThat(criticalPath.components().get(1).getElapsedTime()).isEqualTo(Duration.ZERO);
+    // Action 2 has a run time of 2 seconds
+    assertThat(criticalPath.components().get(2).prettyPrintAction()).contains("action2.out");
+    assertThat(criticalPath.components().get(2).getElapsedTime()).isEqualTo(Duration.ofSeconds(2));
+    // Action 1 has a run time of 2 seconds
+    assertThat(criticalPath.components().get(3).prettyPrintAction()).contains("action1.out");
+    assertThat(criticalPath.components().get(3).getElapsedTime()).isEqualTo(Duration.ofSeconds(1));
   }
 
   private void simulateActionExec(Action action, int totalTime) throws InterruptedException {

src/main/java/com/google/devtools/build/lib/actions/BUILD
@@ -199,6 +199,7 @@ java_library(
         "//src/main/java/com/google/devtools/build/lib/vfs",
         "//src/main/java/com/google/devtools/build/lib/vfs:ospathpolicy",
         "//src/main/java/com/google/devtools/build/lib/vfs:pathfragment",
+        "//src/main/java/com/google/devtools/build/skyframe",
         "//src/main/java/com/google/devtools/build/skyframe:skyframe-objects",
         "//src/main/java/com/google/devtools/common/options",
         "//src/main/java/net/starlark/java/eval",

commit 4d5c88b56ce3eb6dcdc6ae81b0088e573bdbe992

Fix regression in clean build caused by https://github.com/bazelbuild/bazel/commit/09874b5b722c3fc0902e73bd5545ead129b72f36 in critical path computing.
Previously, we always query skyframe for missing components. In clean build, this is  a waste since there is no change pruning.

This CL fixes that by only query skyframe if there might be change pruning, indicated by the newly added event FilesModifiedEvent.

PiperOrigin-RevId: 696809273
Change-Id: I026ca3d668afc3401d203afcb0084166422ba495

src/main/java/com/google/devtools/build/lib/analysis/BUILD
@@ -594,6 +594,11 @@ java_library(
 )
 
 java_library(
+    name = "files_modified_event",
+    srcs = ["FilesModifiedEvent.java"],
+)
+
+java_library(
     name = "build_setting_provider",
     srcs = ["BuildSettingProvider.java"],
     deps = [

src/main/java/com/google/devtools/build/lib/analysis/FilesModifiedEvent.java
@@ -1,0 +1,19 @@
+// Copyright 2024 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.devtools.build.lib.analysis;
+
+/**
+ * An event that is fired after Bazel detected modified source, or output, files from last build.
+ */
+public record FilesModifiedEvent(int numModifiedFiles) {}

src/main/java/com/google/devtools/build/lib/metrics/criticalpath/BUILD
@@ -17,6 +17,7 @@ java_library(
     deps = [
         "//src/main/java/com/google/devtools/build/lib/actions",
         "//src/main/java/com/google/devtools/build/lib/actions:artifacts",
+        "//src/main/java/com/google/devtools/build/lib/analysis:files_modified_event",
         "//src/main/java/com/google/devtools/build/lib/clock",
         "//src/main/java/com/google/devtools/build/lib/cmdline",
         "//src/main/java/com/google/devtools/build/lib/concurrent",

src/main/java/com/google/devtools/build/lib/metrics/criticalpath/CriticalPathComputer.java
@@ -14,6 +14,7 @@
 
 package com.google.devtools.build.lib.metrics.criticalpath;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Comparators;
 import com.google.common.collect.ImmutableList;
@@ -35,6 +36,7 @@ import com.google.devtools.build.lib.actions.DiscoveredInputsEvent;
 import com.google.devtools.build.lib.actions.SpawnExecutedEvent;
 import com.google.devtools.build.lib.actions.SpawnMetrics;
 import com.google.devtools.build.lib.actions.SpawnResult;
+import com.google.devtools.build.lib.analysis.FilesModifiedEvent;
 import com.google.devtools.build.lib.skyframe.rewinding.ActionRewoundEvent;
 import com.google.devtools.build.skyframe.WalkableGraph;
 import java.time.Duration;
@@ -42,6 +44,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BinaryOperator;
@@ -78,6 +81,8 @@ public class CriticalPathComputer {
       Maps.newConcurrentMap();
   private final ActionKeyContext actionKeyContext;
   @Nullable private final WalkableGraph graph;
+
+  private final AtomicBoolean queryGraph = new AtomicBoolean(false);
 
   /** Maximum critical path found. */
   private final AtomicReference<CriticalPathComponent> maxCriticalPath = new AtomicReference<>();
@@ -132,6 +137,18 @@ public class CriticalPathComputer {
 
   public Map<Artifact, CriticalPathComponent> getCriticalPathComponentsMap() {
     return outputArtifactToComponent;
+  }
+
+  @VisibleForTesting
+  void setQueryGraph(boolean queryGraph) {
+    this.queryGraph.set(queryGraph);
+  }
+
+  @Subscribe
+  public void onFilesModified(FilesModifiedEvent event) {
+    // Only allow querying the graph if we have modified files from last build: only in this case
+    // change-pruning could happen.
+    queryGraph.set(event.numModifiedFiles() > 0);
   }
 
   /** Changes the phase of the action */
@@ -364,7 +381,7 @@ public class CriticalPathComputer {
       throws InterruptedException {
     CriticalPathComponent depComponent = outputArtifactToComponent.get(input);
 
-    if (depComponent == null && !input.isSourceArtifact() && graph != null) {
+    if (depComponent == null && !input.isSourceArtifact() && graph != null && queryGraph.get()) {
       // The generating action of the input is missing. It happens when the action was change
       // pruned in an incremental build. Query skyframe for the Action data.
       if (Actions.getGeneratingAction(graph, input) instanceof Action action) {

src/main/java/com/google/devtools/build/lib/skyframe/BUILD
@@ -267,6 +267,7 @@ java_library(
         "//src/main/java/com/google/devtools/build/lib/analysis:constraints/incompatible_target_checker",
         "//src/main/java/com/google/devtools/build/lib/analysis:dependency_kind",
         "//src/main/java/com/google/devtools/build/lib/analysis:exec_group_collection",
+        "//src/main/java/com/google/devtools/build/lib/analysis:files_modified_event",
         "//src/main/java/com/google/devtools/build/lib/analysis:incompatible_platform_provider",
         "//src/main/java/com/google/devtools/build/lib/analysis:inconsistent_aspect_order_exception",
         "//src/main/java/com/google/devtools/build/lib/analysis:inconsistent_null_config_exception",

src/main/java/com/google/devtools/build/lib/skyframe/SequencedSkyframeExecutor.java
@@ -39,6 +39,7 @@ import com.google.devtools.build.lib.analysis.AspectValue;
 import com.google.devtools.build.lib.analysis.BlazeDirectories;
 import com.google.devtools.build.lib.analysis.ConfiguredTarget;
 import com.google.devtools.build.lib.analysis.ConfiguredTargetValue;
+import com.google.devtools.build.lib.analysis.FilesModifiedEvent;
 import com.google.devtools.build.lib.analysis.WorkspaceStatusAction.Factory;
 import com.google.devtools.build.lib.analysis.actions.TemplateExpansionException;
 import com.google.devtools.build.lib.analysis.configuredtargets.RuleConfiguredTarget;
@@ -496,6 +497,7 @@ public class SequencedSkyframeExecutor extends SkyframeExecutor {
     Profiler.instance()
         .logSimpleTask(startTime, stopTime, ProfilerTask.INFO, "detectModifiedOutputFiles");
     long duration = stopTime - startTime;
+    getEventBus().post(new FilesModifiedEvent(modifiedFiles.get()));
     outputTreeDiffCheckingDuration = duration > 0 ? Duration.ofNanos(duration) : Duration.ZERO;
   }
 

src/test/java/com/google/devtools/build/lib/metrics/criticalpath/BUILD
@@ -39,5 +39,6 @@ java_test(
         "//third_party:junit4",
         "//third_party:mockito",
         "//third_party:truth",
+        "@maven//:com_google_testparameterinjector_test_parameter_injector",
     ],
 )

src/test/java/com/google/devtools/build/lib/metrics/criticalpath/CriticalPathComputerTest.java
@@ -61,6 +61,8 @@ import com.google.devtools.build.skyframe.SkyKey;
 import com.google.devtools.build.skyframe.SkyValue;
 import com.google.devtools.build.skyframe.WalkableGraph;
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
+import com.google.testing.junit.testparameterinjector.TestParameter;
+import com.google.testing.junit.testparameterinjector.TestParameterInjector;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Collections;
@@ -70,10 +72,9 @@ import javax.annotation.Nullable;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
 /** Unit tests for {@link CriticalPathComputer}. */
-@RunWith(JUnit4.class)
+@RunWith(TestParameterInjector.class)
 public class CriticalPathComputerTest extends FoundationTestCase {
 
   private ManualClock clock;
@@ -939,7 +940,7 @@ public class CriticalPathComputerTest extends FoundationTestCase {
   }
 
   @Test
-  public void testChangePruning() throws Exception {
+  public void testChangePruning(@TestParameter boolean queryGraph) throws Exception {
     MockAction action1 =
         new MockAction(ImmutableSet.of(), ImmutableSet.of(derivedArtifact("test/action1.out")));
     MockAction action2 =
@@ -1020,6 +1021,7 @@ public class CriticalPathComputerTest extends FoundationTestCase {
                 throw new UnsupportedOperationException();
               }
             });
+    computer.setQueryGraph(queryGraph);
 
     // Action 1 - 0s - 1s
     long action1Start = clock.nanoTime();
@@ -1056,24 +1058,44 @@ public class CriticalPathComputerTest extends FoundationTestCase {
             new FakeActionInputFileCache(),
             mock(ActionLookupData.class)));
 
-    // The total run time should be 6s (Action 1 + Action 2 + Action 4) since Action 3 is
-    // change-pruned.
-    assertThat(computer.getMaxCriticalPath().getAggregatedElapsedTime())
-        .isEqualTo(Duration.ofSeconds(6));
-    AggregatedCriticalPath criticalPath = computer.aggregate();
-    assertThat(criticalPath.components()).hasSize(4);
-    // Action 4 has a run time of 2 seconds
-    assertThat(criticalPath.components().get(0).prettyPrintAction()).contains("action4.out");
-    assertThat(criticalPath.components().get(0).getElapsedTime()).isEqualTo(Duration.ofSeconds(3));
-    // Action 3 has a run time of 0 seconds
-    assertThat(criticalPath.components().get(1).prettyPrintAction()).contains("action3.out");
-    assertThat(criticalPath.components().get(1).getElapsedTime()).isEqualTo(Duration.ZERO);
-    // Action 2 has a run time of 2 seconds
-    assertThat(criticalPath.components().get(2).prettyPrintAction()).contains("action2.out");
-    assertThat(criticalPath.components().get(2).getElapsedTime()).isEqualTo(Duration.ofSeconds(2));
-    // Action 1 has a run time of 2 seconds
-    assertThat(criticalPath.components().get(3).prettyPrintAction()).contains("action1.out");
-    assertThat(criticalPath.components().get(3).getElapsedTime()).isEqualTo(Duration.ofSeconds(1));
+    if (queryGraph) {
+      // The total run time should be 6s (Action 1 + Action 2 + Action 4) since Action 3 is
+      // change-pruned.
+      assertThat(computer.getMaxCriticalPath().getAggregatedElapsedTime())
+          .isEqualTo(Duration.ofSeconds(6));
+      AggregatedCriticalPath criticalPath = computer.aggregate();
+      assertThat(criticalPath.components()).hasSize(4);
+      // Action 4 has a run time of 3 seconds
+      assertThat(criticalPath.components().get(0).prettyPrintAction()).contains("action4.out");
+      assertThat(criticalPath.components().get(0).getElapsedTime())
+          .isEqualTo(Duration.ofSeconds(3));
+      // Action 3 has a run time of 0 seconds
+      assertThat(criticalPath.components().get(1).prettyPrintAction()).contains("action3.out");
+      assertThat(criticalPath.components().get(1).getElapsedTime()).isEqualTo(Duration.ZERO);
+      // Action 2 has a run time of 2 seconds
+      assertThat(criticalPath.components().get(2).prettyPrintAction()).contains("action2.out");
+      assertThat(criticalPath.components().get(2).getElapsedTime())
+          .isEqualTo(Duration.ofSeconds(2));
+      // Action 1 has a run time of 1 seconds
+      assertThat(criticalPath.components().get(3).prettyPrintAction()).contains("action1.out");
+      assertThat(criticalPath.components().get(3).getElapsedTime())
+          .isEqualTo(Duration.ofSeconds(1));
+    } else {
+      // The total run time should be 3s (Action 1 + Action 4) since Action 3 is change-pruned and
+      // queryGraph is false.
+      assertThat(computer.getMaxCriticalPath().getAggregatedElapsedTime())
+          .isEqualTo(Duration.ofSeconds(4));
+      AggregatedCriticalPath criticalPath = computer.aggregate();
+      assertThat(criticalPath.components()).hasSize(2);
+      // Action 4 has a run time of 3 seconds
+      assertThat(criticalPath.components().get(0).prettyPrintAction()).contains("action4.out");
+      assertThat(criticalPath.components().get(0).getElapsedTime())
+          .isEqualTo(Duration.ofSeconds(3));
+      // Action 1 has a run time of 1 seconds
+      assertThat(criticalPath.components().get(1).prettyPrintAction()).contains("action1.out");
+      assertThat(criticalPath.components().get(1).getElapsedTime())
+          .isEqualTo(Duration.ofSeconds(1));
+    }
   }
 
   private void simulateActionExec(Action action, int totalTime) throws InterruptedException {