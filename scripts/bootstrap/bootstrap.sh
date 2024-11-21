572ff083d105c0fd05a025432318d3c102954cab
Googler <pcloudy@google.com>
2024-11-21 07:29:28 -0800

    Ensure local python toolchain is used in bootstrap test in a better way
    A follow up on https://github.com/bazelbuild/bazel/commit/b1c6ad32d637923aed885bca7849662a98df4363
    
    PiperOrigin-RevId: 698773577
    Change-Id: I090a0d421c466a2126888bc11ca8b95dc623ce1d

--- scripts/bootstrap/bootstrap.sh
+++ scripts/bootstrap/bootstrap.sh
@@ -40,8 +40,7 @@ _BAZEL_ARGS="--spawn_strategy=standalone \
       --repository_cache=derived/repository_cache \
       --repo_env=BAZEL_HTTP_RULES_URLS_AS_DEFAULT_CANONICAL_ID=0 \
       --extra_toolchains=//scripts/bootstrap:all \
-      --extra_toolchains=@rules_python//python:autodetecting_toolchain \
-      --@rules_python//python/config_settings:exec_tools_toolchain=disabled \
+      --extra_toolchains=@rules_python//python/runtime_env_toolchains:all \
       --enable_bzlmod \
       --check_direct_dependencies=error \
       --lockfile_mode=update \
