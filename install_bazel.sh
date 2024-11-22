f8ea780e3405bed25a8e89bfd845615307774043
Oliver Koenigsberger <oliver.koenigsberger@bmw.de>
2024-11-22 14:13:54 +0100

    Add bazelisk installation script

---
+++ install_bazel.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://github.com/bazelbuild/bazelisk/releases/download/v1.8.1/bazelisk-linux-amd64
+chmod +x bazelisk-linux-amd64
+sudo mv bazelisk-linux-amd64 /usr/local/bin/bazel
+     # make sure you get the binary available in $PATH> which bazel
+bazel is /usr/local/bin/bazel
