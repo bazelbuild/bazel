3a0e995e7e93620857fa3cd0cb9bab3c95111947
Oliver Koenigsberger <oliver.koenigsberger@bmw.de>
2024-11-22 14:40:43 +0100

    switch is to which

--- install_bazel.sh
+++ install_bazel.sh
@@ -4,4 +4,4 @@ wget https://github.com/bazelbuild/bazelisk/releases/download/v1.8.1/bazelisk-li
 chmod +x bazelisk-linux-amd64
 sudo mv bazelisk-linux-amd64 /usr/local/bin/bazel
      # make sure you get the binary available in $PATH> which bazel
-bazel is /usr/local/bin/bazel
+which bazel
