bazelbuild/bazel

commit 3dcb585fb2f24c75d472df286ccd2891ff7f39b0

Take a shared lock on the install base from the server side.
Progress on #2109.

PiperOrigin-RevId: 704253963
Change-Id: Ib5ef94bbcbad769ca19ac89f97e1d7f82eba4dba

src/main/cpp/blaze.cc
@@ -472,6 +472,9 @@ static vector<string> GetServerExeArgs(const blaze_util::Path &jvm_path,
   result.push_back("--install_base=" +
                    startup_options.install_base.AsCommandLineArgument());
   result.push_back("--install_md5=" + install_md5);
+  if (startup_options.lock_install_base) {
+    result.push_back("--lock_install_base");
+  }
   result.push_back("--output_base=" +
                    startup_options.output_base.AsCommandLineArgument());
   result.push_back("--workspace_directory=" +

src/main/java/com/google/devtools/build/lib/runtime/BlazeServerStartupOptions.java
@@ -105,6 +105,17 @@ public class BlazeServerStartupOptions extends OptionsBase {
       help = "This launcher option is intended for use only by tests.")
   public String installMD5;
 
+  @Option(
+      name = "lock_install_base",
+      defaultValue = "true", // NOTE: only for documentation, value is always passed by the client.
+      documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
+      effectTags = {OptionEffectTag.BAZEL_INTERNAL_CONFIGURATION},
+      metadataTags = {OptionMetadataTag.HIDDEN},
+      help =
+          "Whether the server should hold a lock on the install base while running, to prevent"
+              + " another server from attempting to garbage collect it.")
+  public boolean lockInstallBase;
+
   /* Note: The help string in this option applies to the client code; not
    * the server code. The server code will only accept a non-empty path; it's
    * the responsibility of the client to compute a proper default if

src/main/java/com/google/devtools/build/lib/BUILD
@@ -471,6 +471,7 @@ java_library(
         "//src/main/java/com/google/devtools/build/lib/util:debug-logger-configurator",
         "//src/main/java/com/google/devtools/build/lib/util:detailed_exit_code",
         "//src/main/java/com/google/devtools/build/lib/util:exit_code",
+        "//src/main/java/com/google/devtools/build/lib/util:file_system_lock",
         "//src/main/java/com/google/devtools/build/lib/util:heap_offset_helper",
         "//src/main/java/com/google/devtools/build/lib/util:interrupted_failure_details",
         "//src/main/java/com/google/devtools/build/lib/util:logging",

src/main/java/com/google/devtools/build/lib/runtime/BlazeRuntime.java
@@ -93,6 +93,7 @@ import com.google.devtools.build.lib.util.CustomFailureDetailPublisher;
 import com.google.devtools.build.lib.util.DebugLoggerConfigurator;
 import com.google.devtools.build.lib.util.DetailedExitCode;
 import com.google.devtools.build.lib.util.ExitCode;
+import com.google.devtools.build.lib.util.FileSystemLock;
 import com.google.devtools.build.lib.util.InterruptedFailureDetails;
 import com.google.devtools.build.lib.util.LoggingUtil;
 import com.google.devtools.build.lib.util.Pair;
@@ -201,6 +202,10 @@ public final class BlazeRuntime implements BugReport.BlazeRuntimeInterface {
 
   private final InstrumentationOutputFactory instrumentationOutputFactory;
 
+  @SuppressWarnings("unused")
+  @Nullable
+  private final FileSystemLock installBaseLock;
+
   private BlazeRuntime(
       FileSystem fileSystem,
       QueryEnvironmentFactory queryEnvironmentFactory,
@@ -224,7 +229,8 @@ public final class BlazeRuntime implements BugReport.BlazeRuntimeInterface {
       BuildEventArtifactUploaderFactoryMap buildEventArtifactUploaderFactoryMap,
       ImmutableMap<String, AuthHeadersProvider> authHeadersProviderMap,
       RepositoryRemoteExecutorFactory repositoryRemoteExecutorFactory,
-      InstrumentationOutputFactory instrumentationOutputFactory) {
+      InstrumentationOutputFactory instrumentationOutputFactory,
+      FileSystemLock installBaseLock) {
     // Server state
     this.fileSystem = fileSystem;
     this.blazeModules = blazeModules;
@@ -255,6 +261,7 @@ public final class BlazeRuntime implements BugReport.BlazeRuntimeInterface {
         Preconditions.checkNotNull(authHeadersProviderMap, "authHeadersProviderMap");
     this.repositoryRemoteExecutorFactory = repositoryRemoteExecutorFactory;
     this.instrumentationOutputFactory = instrumentationOutputFactory;
+    this.installBaseLock = installBaseLock;
   }
 
   public BlazeWorkspace initWorkspace(BlazeDirectories directories, BinTools binTools)
@@ -1303,6 +1310,26 @@ public final class BlazeRuntime implements BugReport.BlazeRuntimeInterface {
 
     FileSystem fs = MoreObjects.firstNonNull(maybeFsForBuildArtifacts, nativeFs);
 
+    FileSystemLock installBaseLock = null;
+    if (startupOptions.lockInstallBase) {
+      // Acquire a shared lock on the install base to prevent it from being garbage collected by
+      // another server while this server is running. Note that the client must already hold a
+      // shared lock on the install base at this time (which it will release once it successfully
+      // connects to the server), so failure to obtain the lock is not expected.
+      // The lock is never released explicitly, so as not to risk releasing it while the install
+      // base is still in use. It goes away when the server process dies.
+      try {
+        installBaseLock =
+            FileSystemLock.getShared(
+                nativeFs.getPath(installBase.replaceName(installBase.getBaseName() + ".lock")));
+      } catch (IOException e) {
+        throw createFilesystemExitException(
+            "Failed to acquire shared lock on install base: " + e.getMessage(),
+            Filesystem.Code.FAILED_TO_LOCK_INSTALL_BASE,
+            e);
+      }
+    }
+
     SubscriberExceptionHandler currentHandlerValue = null;
     for (BlazeModule module : blazeModules) {
       SubscriberExceptionHandler newHandler = module.getEventBusAndAsyncExceptionHandler();
@@ -1377,7 +1404,8 @@ public final class BlazeRuntime implements BugReport.BlazeRuntimeInterface {
             .setStartupOptionsProvider(options)
             .setClock(clock)
             .setAbruptShutdownHandler(abruptShutdownHandler)
-            .setEventBusExceptionHandler(subscriberExceptionHandler);
+            .setEventBusExceptionHandler(subscriberExceptionHandler)
+            .setInstallBaseLock(installBaseLock);
 
     if (TestType.isInTest() && System.getenv("NO_CRASH_ON_LOGGING_IN_TEST") == null) {
       LoggingUtil.installRemoteLogger(getTestCrashLogger());
@@ -1556,6 +1584,7 @@ public final class BlazeRuntime implements BugReport.BlazeRuntimeInterface {
     private String productName;
     private ActionKeyContext actionKeyContext;
     private BugReporter bugReporter = BugReporter.defaultInstance();
+    @Nullable private FileSystemLock installBaseLock;
 
     @VisibleForTesting
     public BlazeRuntime build() throws AbruptExitException {
@@ -1669,7 +1698,8 @@ public final class BlazeRuntime implements BugReport.BlazeRuntimeInterface {
               serverBuilder.getBuildEventArtifactUploaderMap(),
               serverBuilder.getAuthHeadersProvidersMap(),
               serverBuilder.getRepositoryRemoteExecutorFactory(),
-              serverBuilder.createInstrumentationOutputFactory());
+              serverBuilder.createInstrumentationOutputFactory(),
+              installBaseLock);
       AutoProfiler.setClock(runtime.getClock());
       BugReport.setRuntime(runtime);
       return runtime;
@@ -1728,6 +1758,12 @@ public final class BlazeRuntime implements BugReport.BlazeRuntimeInterface {
     public Builder setEventBusExceptionHandler(
         SubscriberExceptionHandler eventBusExceptionHandler) {
       this.eventBusExceptionHandler = eventBusExceptionHandler;
+      return this;
+    }
+
+    @CanIgnoreReturnValue
+    public Builder setInstallBaseLock(FileSystemLock installBaseLock) {
+      this.installBaseLock = installBaseLock;
       return this;
     }
 

src/main/protobuf/failure_details.proto
@@ -541,6 +541,7 @@ message Filesystem {
     DEFAULT_DIGEST_HASH_FUNCTION_INVALID_VALUE = 6
         [(metadata) = { exit_code: 2 }];
     FILESYSTEM_JNI_NOT_AVAILABLE = 8 [(metadata) = { exit_code: 36 }];
+    FAILED_TO_LOCK_INSTALL_BASE = 12 [(metadata) = { exit_code: 36 }];
 
     reserved 7, 9, 10; // For internal use
   }

src/test/java/com/google/devtools/build/lib/testutil/BUILD
@@ -155,6 +155,7 @@ java_binary(
         "-Xlog:all=warning:stderr",
     ],
     main_class = "com.google.devtools.build.lib.testutil.ExternalFileSystemLockHelper",
+    deps = ["//third_party:guava"],
 )
 
 java_library(

src/test/java/com/google/devtools/build/lib/testutil/ExternalFileSystemLock.java
@@ -43,12 +43,12 @@ public class ExternalFileSystemLock implements AutoCloseable {
 
   private ExternalFileSystemLock(Path lockPath, boolean shared) throws IOException {
       String binaryPath = Runfiles.preload().withSourceRepository("").rlocation(HELPER_PATH);
-      this.subprocess =
-          new SubprocessBuilder()
-              .setArgv(
-                  ImmutableList.of(
-                      binaryPath, lockPath.getPathString(), shared ? "shared" : "exclusive"))
-              .start();
+    this.subprocess =
+        new SubprocessBuilder()
+            .setArgv(
+                ImmutableList.of(
+                    binaryPath, lockPath.getPathString(), shared ? "shared" : "exclusive", "sleep"))
+            .start();
       // Wait for child to report that the lock has been acquired.
       // We could read the entire stdout/stderr here to obtain additional debugging information,
       // but for some reason that hangs forever on Windows, even if we close them on the child side.

src/test/java/com/google/devtools/build/lib/testutil/ExternalFileSystemLockHelper.java
@@ -13,40 +13,66 @@
 // limitations under the License.
 package com.google.devtools.build.lib.testutil;
 
+import com.google.common.collect.ImmutableSet;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
 import java.nio.file.Files;
+import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
 /**
- * A helper binary that holds a shared or exclusive lock on a file.
+ * A helper program that attempts to obtain a shared or exclusive lock on a file and optionally
+ * sleeps forever while holding it.
  *
- * <p>Do not use this directly in a test. Use {@link ExternalFileSystemLock} instead.
+ * <p>The arguments are as follows:
+ *
+ * <ol>
+ *   <li>The path of the file to lock.
+ *   <li>One of "shared" or "exclusive", indicating the type of lock to obtain.
+ *   <li>One of "sleep" or "exit", indicating whether to sleep forever or exit immediately once the
+ *       lock is held.
+ * </ol>
+ *
+ * <p>Does not block waiting for the lock, exiting immediately if it's already held.
+ *
+ * <p>Once the lock is held, prints '!' to stdout.
+ *
+ * <p>In a Java test, prefer {@link ExternalFileSystemLock} over using this directly.
  */
 public final class ExternalFileSystemLockHelper {
   private ExternalFileSystemLockHelper() {}
 
+  private static final ImmutableSet<OpenOption> OPEN_OPTIONS =
+      ImmutableSet.of(StandardOpenOption.READ, StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+
   public static void main(String[] args) throws IOException, InterruptedException {
-    if (args.length != 2) {
-      throw new IOException("bad arguments");
+    if (args.length != 3
+        || !(args[1].equals("shared") || args[1].equals("exclusive"))
+        || !(args[2].equals("sleep") || args[2].equals("exit"))) {
+      throw new IOException("invalid arguments");
     }
+
     Path path = Path.of(args[0]).toAbsolutePath();
-    Files.createDirectories(path.getParent());
     boolean shared = args[1].equals("shared");
-    try (FileChannel channel =
-            FileChannel.open(
-                path,
-                StandardOpenOption.READ,
-                StandardOpenOption.WRITE,
-                StandardOpenOption.CREATE);
-        FileLock lock = channel.lock(0, Long.MAX_VALUE, shared)) {
+    boolean sleep = args[2].equals("sleep");
+
+    Files.createDirectories(path.getParent());
+    try (FileChannel channel = FileChannel.open(path, OPEN_OPTIONS);
+        FileLock lock = channel.tryLock(0, Long.MAX_VALUE, shared)) {
+      if (lock == null) {
+        throw new IOException("lock already held");
+      }
+
       // Signal parent that the lock is held.
       System.out.println("!");
-      // Block until killed by parent.
-      while (true) {
-        Thread.sleep(1000);
+
+      // If so requested, block until killed by parent.
+      if (sleep) {
+        while (true) {
+          Thread.sleep(1000);
+        }
       }
     }
   }

commit 4176d23b802e4934bc7ca1ece4e5692e0a0843b9

Fix forward for breakage caused by https://github.com/bazelbuild/bazel/commit/3dcb585fb2f24c75d472df286ccd2891ff7f39b0.
The --lock_install_base startup flag must default to false, so that the client can obtain the desired behavior by either explicitly enabling or omitting it (explicitly disabling is problematic).

PiperOrigin-RevId: 704347325
Change-Id: I55b9b324c2c16035356673291c254e6629105d8e

src/main/cpp/blaze.cc
@@ -473,6 +473,10 @@ static vector<string> GetServerExeArgs(const blaze_util::Path &jvm_path,
                    startup_options.install_base.AsCommandLineArgument());
   result.push_back("--install_md5=" + install_md5);
   if (startup_options.lock_install_base) {
+    // This flag is not user-settable. Its sole purpose is to alter the behavior
+    // for Blaze and Bazel. Do *not* explicitly set it to disabled, because at
+    // Google we rely on the ability to run the client code against a server
+    // built before the flag was added.
     result.push_back("--lock_install_base");
   }
   result.push_back("--output_base=" +

src/main/cpp/startup_options.h
@@ -155,7 +155,8 @@ class StartupOptions {
   blaze_util::Path install_base;
 
   // Whether the install base should be locked before use.
-  // This is always true for Bazel, but overridden for Blaze at Google.
+  // Not user-settable, only used for client/server communication.
+  // Hardcoded to true for Bazel, false for Blaze.
   bool lock_install_base;
 
   // Override more finegrained rc file flags and ignore them all.

src/main/java/com/google/devtools/build/lib/runtime/BlazeServerStartupOptions.java
@@ -107,7 +107,7 @@ public class BlazeServerStartupOptions extends OptionsBase {
 
   @Option(
       name = "lock_install_base",
-      defaultValue = "true", // NOTE: only for documentation, value is always passed by the client.
+      defaultValue = "false", // NOTE: only for documentation, value is always passed by the client.
       documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
       effectTags = {OptionEffectTag.BAZEL_INTERNAL_CONFIGURATION},
       metadataTags = {OptionMetadataTag.HIDDEN},