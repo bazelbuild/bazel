720e15efee88dfe115eb2c3b184225e2c0d42ff0
Fabian Meumertzheim <fabian@meumertzhe.im>
2024-12-23 14:13:45 -0800

    Forward `@bazel_tools//tools/sh:toolchain_type` to `rules_shell`
    This is required for compatibility with modules that rely on the auto-registered `sh_toolchain` to obtain a shebang, e.g. `rules_js`.
    
    Also remove implementations and toolchain registrations in favor of forwarders.
    
    Closes #24748.
    
    PiperOrigin-RevId: 709149200
    Change-Id: I758cf403c24f6b5e6f00bbf5b4e0313a30773f78

--- src/test/java/com/google/devtools/build/lib/skyframe/packages/BazelPackageLoaderTest.java
+++ src/test/java/com/google/devtools/build/lib/skyframe/packages/BazelPackageLoaderTest.java
@@ -79,10 +79,6 @@ public final class BazelPackageLoaderTest extends AbstractPackageLoaderTest {
         "def xcode_configure(*args, **kwargs):",
         "    pass");
     FileSystemUtils.writeIsoLatin1(tools.getRelative("tools/sh/BUILD"), "");
-    FileSystemUtils.writeIsoLatin1(
-        tools.getRelative("tools/sh/sh_configure.bzl"),
-        "def sh_configure(*args, **kwargs):",
-        "    pass");
     FileSystemUtils.writeIsoLatin1(tools.getRelative("tools/build_defs/repo/BUILD"));
     FileSystemUtils.writeIsoLatin1(
         tools.getRelative("tools/build_defs/repo/http.bzl"),
