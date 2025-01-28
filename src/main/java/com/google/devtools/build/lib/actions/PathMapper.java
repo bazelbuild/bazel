48b4996966bc462f85955139c27529521b227808
Fabian Meumertzheim <fabian@meumertzhe.im>
2025-01-28 12:34:48 +0100

    Address comments

--- src/main/java/com/google/devtools/build/lib/actions/PathMapper.java
+++ src/main/java/com/google/devtools/build/lib/actions/PathMapper.java
@@ -267,8 +267,8 @@ final class PathMapperConstants {
           .weakKeys()
           .build(sourceRoot -> new PathMapper.MappedArtifactRoot(sourceRoot.getExecPath()));
 
-  private static final PathFragment BAZEL_OUT = PathFragment.create("bazel-out");
-  private static final PathFragment BLAZE_OUT = PathFragment.create("blaze-out");
+  private static final PathFragment BAZEL_OUT = PathFragment.create("bazel-out/");
+  private static final PathFragment BLAZE_OUT = PathFragment.create("blaze-out/");
 
   /**
    * A special instance for use in {@link AbstractAction#computeKey} when path mapping is generally
