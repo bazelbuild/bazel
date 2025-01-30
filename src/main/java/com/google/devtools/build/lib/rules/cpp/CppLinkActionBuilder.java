1b597aee91e2395725b47cd8af06afcf4f29a46f
Fabian Meumertzheim <fabian@meumertzhe.im>
2025-01-30 13:26:31 -0800

    Add support for path mapping to `CppArchive`
    This mostly requires wiring up the existing machinery for structured variables and (most) link build variables.
    
    Utility methods on `PathMappers` are moved to `PathMapper` so that they can be dependend on by `AbstractCommandLine` without creating a cycle.
    
    Work towards https://github.com/bazelbuild/bazel/discussions/22658#discussioncomment-11966720
    Work towards #6526
    
    Closes #25081.
    
    PiperOrigin-RevId: 721499678
    Change-Id: I4551f268093c7b851791a41deb57292b2c23afb7

--- src/main/java/com/google/devtools/build/lib/rules/cpp/CppLinkActionBuilder.java
+++ src/main/java/com/google/devtools/build/lib/rules/cpp/CppLinkActionBuilder.java
@@ -29,6 +29,7 @@ import com.google.devtools.build.lib.actions.ArtifactRoot;
 import com.google.devtools.build.lib.actions.ParameterFile;
 import com.google.devtools.build.lib.analysis.RuleContext;
 import com.google.devtools.build.lib.analysis.actions.ActionConstructionContext;
+import com.google.devtools.build.lib.analysis.actions.PathMappers;
 import com.google.devtools.build.lib.analysis.config.BuildConfigurationValue;
 import com.google.devtools.build.lib.analysis.config.PerLabelOptions;
 import com.google.devtools.build.lib.cmdline.Label;
@@ -849,16 +850,16 @@ public class CppLinkActionBuilder {
 
     CcToolchainVariables.Builder buildVariables =
         LinkBuildVariables.setupLinkingVariables(
-            output.getExecPathString(),
+            output,
             SolibSymlinkAction.getDynamicLibrarySoname(
                 output.getRootRelativePath(),
                 /* preserveName= */ linkType != LinkTargetType.NODEPS_DYNAMIC_LIBRARY,
                 linkActionConstruction.getContext().getConfiguration().getMnemonic()),
-            thinltoParamFile != null ? thinltoParamFile.getExecPathString() : null,
+            thinltoParamFile,
             toolchain,
             featureConfiguration,
-            toolchain.getInterfaceSoBuilder().getExecPathString(),
-            interfaceOutput != null ? interfaceOutput.getExecPathString() : null,
+            toolchain.getInterfaceSoBuilder(),
+            interfaceOutput,
             fdoContext);
 
     ImmutableList<String> userLinkFlags =
@@ -1120,7 +1121,8 @@ public class CppLinkActionBuilder {
         linkCommandLine,
         linkActionConstruction.getConfig().getActionEnvironment(),
         toolchainEnv,
-        ImmutableMap.copyOf(executionInfo));
+        ImmutableMap.copyOf(executionInfo),
+        PathMappers.getOutputPathsMode(linkActionConstruction.getConfig()));
   }
 
   /** Returns the output of this action as a {@link LibraryInput} or null if it is an executable. */
