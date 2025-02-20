cc6a4ca0bc7eebf271a8c08e1bf28cba3ea8623d
Fabian Meumertzheim <fabian@meumertzhe.im>
2025-02-20 13:14:54 +0100

    Fix Windows bootstrap build

--- src/main/native/common.cc
+++ src/main/native/common.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <jni.h>
+#include "src/main/native/jni.h"
 
 namespace blaze_jni {
 
