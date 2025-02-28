bazelbuild/bazel

commit 62b38cbb28a28cb037f27d02632827eeff85f7fb

Move the logic to download artifacts for targets under test into RemoteOutputChecker.
Fixes #17884.

PiperOrigin-RevId: 535568892
Change-Id: I2ff8fadfb43ae6fae6c290616e9e730332cb6e91

src/main/java/com/google/devtools/build/lib/remote/RemoteOutputChecker.java
@@ -13,9 +13,12 @@
 // limitations under the License.
 package com.google.devtools.build.lib.remote;
 
+import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.devtools.build.lib.packages.TargetUtils.isTestRuleName;
+import static com.google.devtools.build.lib.skyframe.CoverageReportValue.COVERAGE_REPORT_KEY;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 import com.google.devtools.build.lib.actions.ActionInput;
 import com.google.devtools.build.lib.actions.Artifact;
@@ -29,6 +32,7 @@ import com.google.devtools.build.lib.analysis.ProviderCollection;
 import com.google.devtools.build.lib.analysis.TopLevelArtifactContext;
 import com.google.devtools.build.lib.analysis.TopLevelArtifactHelper;
 import com.google.devtools.build.lib.analysis.configuredtargets.RuleConfiguredTarget;
+import com.google.devtools.build.lib.analysis.test.TestProvider;
 import com.google.devtools.build.lib.clock.Clock;
 import java.util.Set;
 import java.util.function.Supplier;
@@ -95,7 +99,7 @@ public class RemoteOutputChecker implements RemoteArtifactChecker {
     var targetsToTest = analysisResult.getTargetsToTest();
     if (targetsToTest != null) {
       for (var target : targetsToTest) {
-        addTopLevelTarget(target, target, analysisResult::getTopLevelContext);
+        addTargetUnderTest(target, analysisResult.getArtifactsToBuild());
       }
     }
   }
@@ -104,7 +108,7 @@ public class RemoteOutputChecker implements RemoteArtifactChecker {
       ProviderCollection target,
       @Nullable ConfiguredTarget configuredTarget,
       Supplier<TopLevelArtifactContext> topLevelArtifactContextSupplier) {
-    if (shouldDownloadToplevelOutputs(configuredTarget)) {
+    if (shouldAddTopLevelTarget(configuredTarget)) {
       var topLevelArtifactContext = topLevelArtifactContextSupplier.get();
       var artifactsToBuild =
           TopLevelArtifactHelper.getAllArtifactsToBuild(target, topLevelArtifactContext)
@@ -147,14 +151,34 @@ public class RemoteOutputChecker implements RemoteArtifactChecker {
     }
   }
 
+  private void addTargetUnderTest(
+      ProviderCollection target, ImmutableSet<Artifact> artifactsToBuild) {
+    TestProvider testProvider = checkNotNull(target.getProvider(TestProvider.class));
+    if (downloadToplevel && commandMode == CommandMode.TEST) {
+      // In test mode, download the outputs of the test runner action.
+      toplevelArtifactsToDownload.addAll(testProvider.getTestParams().getOutputs());
+    }
+    if (commandMode == CommandMode.COVERAGE) {
+      // In coverage mode, download the per-test and aggregated coverage files.
+      // Do this even for MINIMAL, since coverage (unlike test) doesn't produce any observable
+      // results other than outputs.
+      toplevelArtifactsToDownload.addAll(testProvider.getTestParams().getCoverageArtifacts());
+      for (Artifact artifactToBuild : artifactsToBuild) {
+        if (artifactToBuild.getArtifactOwner().equals(COVERAGE_REPORT_KEY)) {
+          toplevelArtifactsToDownload.add(artifactToBuild);
+        }
+      }
+    }
+  }
+
   public void addInputToDownload(ActionInput file) {
     inputsToDownload.add(file);
   }
 
-  private boolean shouldDownloadToplevelOutputs(@Nullable ConfiguredTarget configuredTarget) {
+  private boolean shouldAddTopLevelTarget(@Nullable ConfiguredTarget configuredTarget) {
     switch (commandMode) {
       case RUN:
-        // Always download outputs of toplevel targets in RUN mode
+        // Always download outputs of toplevel targets in run mode.
         return true;
       case COVERAGE:
       case TEST:

src/main/java/com/google/devtools/build/lib/remote/BUILD
@@ -35,7 +35,6 @@ java_library(
             "Retrier.java",
             "RemoteOutputChecker.java",
             "AbstractActionInputPrefetcher.java",
-            "ToplevelArtifactsDownloader.java",
             "LeaseService.java",
         ],
     ),
@@ -51,7 +50,6 @@ java_library(
         ":abstract_action_input_prefetcher",
         ":lease_service",
         ":remote_output_checker",
-        ":toplevel_artifacts_downloader",
         "//src/main/java/com/google/devtools/build/lib:build-request-options",
         "//src/main/java/com/google/devtools/build/lib:runtime",
         "//src/main/java/com/google/devtools/build/lib:runtime/command_line_path_factory",
@@ -195,6 +193,7 @@ java_library(
         "//src/main/java/com/google/devtools/build/lib/analysis:top_level_artifact_context",
         "//src/main/java/com/google/devtools/build/lib/clock",
         "//src/main/java/com/google/devtools/build/lib/packages",
+        "//src/main/java/com/google/devtools/build/lib/skyframe:coverage_report_value",
         "//third_party:guava",
         "//third_party:jsr305",
     ],
@@ -219,25 +218,6 @@ java_library(
         "//third_party:guava",
         "//third_party:jsr305",
         "//third_party:rxjava3",
-    ],
-)
-
-java_library(
-    name = "toplevel_artifacts_downloader",
-    srcs = ["ToplevelArtifactsDownloader.java"],
-    deps = [
-        ":abstract_action_input_prefetcher",
-        "//src/main/java/com/google/devtools/build/lib/actions",
-        "//src/main/java/com/google/devtools/build/lib/actions:action_input_helper",
-        "//src/main/java/com/google/devtools/build/lib/actions:artifacts",
-        "//src/main/java/com/google/devtools/build/lib/analysis:analysis_cluster",
-        "//src/main/java/com/google/devtools/build/lib/analysis:test/coverage_report",
-        "//src/main/java/com/google/devtools/build/lib/util",
-        "//src/main/java/com/google/devtools/build/lib/vfs",
-        "//src/main/java/com/google/devtools/build/lib/vfs:pathfragment",
-        "//third_party:flogger",
-        "//third_party:guava",
-        "//third_party:jsr305",
     ],
 )
 

src/main/java/com/google/devtools/build/lib/remote/RemoteActionFileSystem.java
@@ -560,25 +560,6 @@ public class RemoteActionFileSystem extends DelegateFileSystem {
     return null;
   }
 
-  public FileArtifactValue getOutputMetadataForTopLevelArtifactDownloader(ActionInput input)
-      throws IOException {
-    RemoteFileInfo remoteFile =
-        remoteOutputTree.getRemoteFileInfo(
-            execRoot.getRelative(input.getExecPath()), /* followSymlinks= */ true);
-    if (remoteFile != null) {
-      return remoteFile.getMetadata();
-    }
-
-    // TODO(tjgq): This should not work.
-    // The astute reader will notice that when this method is called, the artifact to be downloaded
-    // is an *output* artifact from the point of view of this RemoteActionFileSystem. The way this
-    // apparently works is that this is a SingleBuildFileCache, which then stat()s the actual file
-    // system, where the output file is materialized in mysterious ways.
-    //
-    // For further bafflement, see the comment at ToplevelArtifactsDownloader.downloadTestOutput().
-    return fileCache.getInputMetadata(input);
-  }
-
   @Nullable
   @VisibleForTesting
   FileArtifactValue getInputMetadata(ActionInput input) {

src/main/java/com/google/devtools/build/lib/remote/RemoteModule.java
@@ -134,7 +134,6 @@ public final class RemoteModule extends BlazeModule {
   @Nullable private ExecutorService executorService;
   @Nullable private RemoteActionContextProvider actionContextProvider;
   @Nullable private RemoteActionInputFetcher actionInputFetcher;
-  @Nullable private ToplevelArtifactsDownloader toplevelArtifactsDownloader;
   @Nullable private RemoteOptions remoteOptions;
   @Nullable private RemoteOutputService remoteOutputService;
   @Nullable private TempPathGenerator tempPathGenerator;
@@ -884,7 +883,6 @@ public final class RemoteModule extends BlazeModule {
     remoteDownloaderSupplier.set(null);
     actionContextProvider = null;
     actionInputFetcher = null;
-    toplevelArtifactsDownloader = null;
     remoteOptions = null;
     remoteOutputService = null;
     tempPathGenerator = null;
@@ -1013,20 +1011,6 @@ public final class RemoteModule extends BlazeModule {
       env.getEventBus().register(actionInputFetcher);
       builder.setActionInputPrefetcher(actionInputFetcher);
       actionContextProvider.setActionInputFetcher(actionInputFetcher);
-
-      toplevelArtifactsDownloader =
-          new ToplevelArtifactsDownloader(
-              actionInputFetcher,
-              env.getExecRoot().asFragment(),
-              (path) -> {
-                FileSystem fileSystem = path.getFileSystem();
-                if (fileSystem instanceof RemoteActionFileSystem) {
-                  RemoteActionFileSystem rafs = (RemoteActionFileSystem) path.getFileSystem();
-                  return rafs::getOutputMetadataForTopLevelArtifactDownloader;
-                }
-                return null;
-              });
-      env.getEventBus().register(toplevelArtifactsDownloader);
 
       var leaseService =
           new LeaseService(

src/main/java/com/google/devtools/build/lib/remote/ToplevelArtifactsDownloader.java
@@ -1,117 +1,0 @@
-// Copyright 2022 The Bazel Authors. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-package com.google.devtools.build.lib.remote;
-
-import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.util.concurrent.Futures.addCallback;
-import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
-
-import com.google.common.collect.ImmutableList;
-import com.google.common.eventbus.AllowConcurrentEvents;
-import com.google.common.eventbus.Subscribe;
-import com.google.common.flogger.GoogleLogger;
-import com.google.common.util.concurrent.FutureCallback;
-import com.google.common.util.concurrent.ListenableFuture;
-import com.google.devtools.build.lib.actions.ActionInput;
-import com.google.devtools.build.lib.actions.ActionInputHelper;
-import com.google.devtools.build.lib.actions.ActionInputPrefetcher.MetadataSupplier;
-import com.google.devtools.build.lib.actions.ActionInputPrefetcher.Priority;
-import com.google.devtools.build.lib.analysis.test.CoverageReport;
-import com.google.devtools.build.lib.analysis.test.TestAttempt;
-import com.google.devtools.build.lib.util.Pair;
-import com.google.devtools.build.lib.vfs.Path;
-import com.google.devtools.build.lib.vfs.PathFragment;
-import javax.annotation.Nullable;
-
-/**
- * Class that uses {@link AbstractActionInputPrefetcher} to download artifacts of toplevel targets
- * in the background.
- */
-public class ToplevelArtifactsDownloader {
-  private static final GoogleLogger logger = GoogleLogger.forEnclosingClass();
-
-  private final AbstractActionInputPrefetcher actionInputPrefetcher;
-  private final PathFragment execRoot;
-  private final PathToMetadataSupplier pathToMetadataSupplier;
-
-  public ToplevelArtifactsDownloader(
-      AbstractActionInputPrefetcher actionInputPrefetcher,
-      PathFragment execRoot,
-      PathToMetadataSupplier pathToMetadataSupplier) {
-    this.actionInputPrefetcher = actionInputPrefetcher;
-    this.execRoot = execRoot;
-    this.pathToMetadataSupplier = pathToMetadataSupplier;
-  }
-
-  /**
-   * Interface that converts a {@link Path} into a {@link MetadataSupplier} suitable for retrieving
-   * metadata for that path.
-   *
-   * <p>{@link ToplevelArtifactsDownloader} may only used in conjunction with filesystems that
-   * implement {@link MetadataSupplier}.
-   */
-  public interface PathToMetadataSupplier {
-    @Nullable
-    MetadataSupplier getMetadataSupplier(Path path);
-  }
-
-  private void downloadTestOutput(Path path) {
-    // Since the event is fired within action execution, the skyframe doesn't know the outputs of
-    // test actions yet, so we can't get their metadata through skyframe. However, since the path
-    // belongs to a filesystem that implements MetadataProvider, we use it to get the metadata.
-    //
-    // If the test hit action cache, the filesystem is local filesystem because the actual test
-    // action didn't get the chance to execute. In this case the MetadataProvider is null, which
-    // is fine because test outputs are already downloaded (otherwise the action cache wouldn't
-    // have been hit).
-    MetadataSupplier metadataSupplier = pathToMetadataSupplier.getMetadataSupplier(path);
-    if (metadataSupplier != null) {
-      // RemoteActionFileSystem#getInput returns null for undeclared test outputs.
-      ActionInput input = ActionInputHelper.fromPath(path.asFragment().relativeTo(execRoot));
-      ListenableFuture<Void> future =
-          actionInputPrefetcher.prefetchFiles(
-              ImmutableList.of(input), metadataSupplier, Priority.LOW);
-      addCallback(
-          future,
-          new FutureCallback<Void>() {
-            @Override
-            public void onSuccess(Void unused) {}
-
-            @Override
-            public void onFailure(Throwable throwable) {
-              logger.atWarning().withCause(throwable).log(
-                  "Failed to download test output %s.", path);
-            }
-          },
-          directExecutor());
-    }
-  }
-
-  @Subscribe
-  @AllowConcurrentEvents
-  public void onTestAttempt(TestAttempt event) {
-    for (Pair<String, Path> pair : event.getFiles()) {
-      Path path = checkNotNull(pair.getSecond());
-      downloadTestOutput(path);
-    }
-  }
-
-  @Subscribe
-  @AllowConcurrentEvents
-  public void onCoverageReport(CoverageReport event) {
-    for (var file : event.getFiles()) {
-      downloadTestOutput(file);
-    }
-  }
-}

src/test/java/com/google/devtools/build/lib/remote/RemoteActionFileSystemTest.java
@@ -330,47 +330,6 @@ public final class RemoteActionFileSystemTest extends RemoteActionFileSystemTest
   }
 
   @Test
-  public void getMetadata_fromRemoteOutputTree_forDeclaredOutput() throws Exception {
-    Artifact artifact = ActionsTestUtil.createArtifact(outputRoot, "out");
-    RemoteActionFileSystem actionFs =
-        (RemoteActionFileSystem)
-            createActionFileSystem(new ActionInputMap(0), ImmutableList.of(artifact));
-
-    FileArtifactValue metadata =
-        injectRemoteFile(actionFs, artifact.getPath().asFragment(), "content");
-
-    assertThat(actionFs.getOutputMetadataForTopLevelArtifactDownloader(artifact))
-        .isEqualTo(metadata);
-  }
-
-  @Test
-  public void getMetadata_fromRemoteOutputTree_forUndeclaredOutput() throws Exception {
-    Artifact artifact = ActionsTestUtil.createArtifact(outputRoot, "out");
-    RemoteActionFileSystem actionFs = (RemoteActionFileSystem) createActionFileSystem();
-
-    FileArtifactValue metadata =
-        injectRemoteFile(actionFs, artifact.getPath().asFragment(), "content");
-
-    assertThat(actionFs.getOutputMetadataForTopLevelArtifactDownloader(artifact))
-        .isEqualTo(metadata);
-  }
-
-  @Test
-  public void getMetadata_fromFileCache_forOutputFile() throws Exception {
-    Artifact artifact = ActionsTestUtil.createArtifact(outputRoot, "out");
-    FileArtifactValue metadata =
-        FileArtifactValue.createForNormalFile(new byte[] {1, 2, 3}, /* proxy= */ null, 42);
-    RemoteActionFileSystem actionFs =
-        createActionFileSystem(
-            new ActionInputMap(0),
-            ImmutableList.of(),
-            new StaticInputMetadataProvider(ImmutableMap.of(artifact, metadata)));
-
-    assertThat(actionFs.getOutputMetadataForTopLevelArtifactDownloader(artifact))
-        .isEqualTo(metadata);
-  }
-
-  @Test
   public void getMetadata_notFound() throws Exception {
     Artifact artifact = ActionsTestUtil.createArtifact(outputRoot, "out");
     RemoteActionFileSystem actionFs = (RemoteActionFileSystem) createActionFileSystem();

commit f470cb9f6bdfd96408facda8342ff41103af1e00

[8.2.0] Also download test outputs for coverage commands when building without the bytes. (#25416)
This was an unintentional regression introduced by
https://github.com/bazelbuild/bazel/commit/62b38cbb28a28cb037f27d02632827eeff85f7fb.

PiperOrigin-RevId: 732063949
Change-Id: I619f17077dd8409cbe75662d3a680847f39102ef

src/main/java/com/google/devtools/build/lib/remote/RemoteOutputChecker.java
@@ -209,8 +209,9 @@ public class RemoteOutputChecker implements RemoteArtifactChecker {
 
   private void addTargetUnderTest(ProviderCollection target) {
     TestProvider testProvider = checkNotNull(target.getProvider(TestProvider.class));
-    if (outputsMode != RemoteOutputsMode.MINIMAL && commandMode == CommandMode.TEST) {
-      // In test mode, download the outputs of the test runner action.
+    if (outputsMode != RemoteOutputsMode.MINIMAL
+        && (commandMode == CommandMode.TEST || commandMode == CommandMode.COVERAGE)) {
+      // In test or coverage mode, download the outputs of the test runner action.
       addOutputsToDownload(testProvider.getTestParams().getOutputs());
     }
     if (commandMode == CommandMode.COVERAGE) {

src/test/shell/bazel/remote/build_without_the_bytes_test.sh
@@ -601,9 +601,9 @@ EOF
 }
 
 function test_download_toplevel_test_rule() {
-  # Test that when using --remote_download_toplevel with bazel test only
-  # the test.log and test.xml file are downloaded but not the test binary.
-  # However when building a test then the test binary should be downloaded.
+  # Test that using --remote_download_toplevel downloads test.log and test.xml
+  # for a test or coverage command, but the test binary is only downloaded for
+  # a build command.
 
   mkdir -p a
   cat > a/BUILD <<EOF
@@ -618,29 +618,34 @@ EOF
 int main() { std::cout << "Hello test!" << std::endl; return 0; }
 EOF
 
-  # When invoking bazel test only test.log and test.xml should be downloaded.
   bazel test \
     --remote_executor=grpc://localhost:${worker_port} \
     --remote_download_toplevel \
-    //a:test >& $TEST_log || fail "Failed to test //a:test with remote execution"
+    //a:test >& $TEST_log || fail "Expected success"
 
-  (! [[ -f bazel-bin/a/test ]]) \
-  || fail "Expected test binary bazel-bin/a/test to not be downloaded"
+  assert_exists bazel-testlogs/a/test/test.log
+  assert_exists bazel-testlogs/a/test/test.xml
+  assert_not_exists bazel-bin/a/test
 
-  [[ -f bazel-testlogs/a/test/test.log ]] \
-  || fail "Expected toplevel output bazel-testlogs/a/test/test.log to be downloaded"
+  rm -rf bazel-out
 
-  [[ -f bazel-testlogs/a/test/test.xml ]] \
-  || fail "Expected toplevel output bazel-testlogs/a/test/test.log to be downloaded"
+  bazel coverage \
+    --remote_executor=grpc://localhost:${worker_port} \
+    --remote_download_toplevel \
+    //a:test >& $TEST_log || fail "Expected success"
 
-  # When invoking bazel build the test binary should be downloaded.
+  assert_exists bazel-testlogs/a/test/test.log
+  assert_exists bazel-testlogs/a/test/test.xml
+  assert_not_exists bazel-bin/a/test
+
+  rm -rf bazel-out
+
   bazel build \
     --remote_executor=grpc://localhost:${worker_port} \
     --remote_download_toplevel \
-    //a:test >& $TEST_log || fail "Failed to build //a:test with remote execution"
+    //a:test >& $TEST_log || fail "Expected success"
 
-  ([[ -f bazel-bin/a/test ]]) \
-  || fail "Expected test binary bazel-bin/a/test to be downloaded"
+  assert_exists bazel-bin/a/test
 }
 
 function do_test_non_test_toplevel_targets() {