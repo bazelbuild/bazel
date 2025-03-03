a34d1d57d330c559ddb006e4fe5d6cdaa8ed06ff
Googler <cparsons@google.com>
2025-03-03 13:57:14 -0800

    Relax callback type (for internal implementations)
    PiperOrigin-RevId: 733050209
    Change-Id: Ie3d53deaad5c193f0efffba45a27fb666119faae

--- src/main/java/com/google/devtools/build/lib/query2/engine/QueryUtil.java
+++ src/main/java/com/google/devtools/build/lib/query2/engine/QueryUtil.java
@@ -39,10 +39,10 @@ public final class QueryUtil {
 
   private QueryUtil() { }
 
-  /** A {@link Callback} that can aggregate all the partial results into one set. */
-  public interface AggregateAllCallback<T, S extends Set<T>> extends Callback<T> {
-    /** Returns a {@link Set} of all the results. */
-    S getResult();
+  /** A {@link Callback} that can aggregate all the partial results into a single value. */
+  public interface AggregateAllCallback<T, V> extends Callback<T> {
+    /** Returns a value representing a combination of all the partial results. */
+    V getResult();
   }
 
   /** A {@link OutputFormatterCallback} that is also a {@link AggregateAllCallback}. */
