9dbba46efd5ad992247c332f575c7d7324e9477c
Googler <hvd@google.com>
2025-03-20 04:40:45 -0700

    Migrate the `JavaStarlarkApiTest.testJavaInfoGetTransitiveRuntimeDeps` test to Starlark
    The test is removed from Bazel and added in `@rules_java`
    
    PiperOrigin-RevId: 738754200
    Change-Id: If3d844e40c6bb4cd5ab622df76a332281c4eefb5

--- src/test/java/com/google/devtools/build/lib/rules/java/JavaStarlarkApiTest.java
+++ src/test/java/com/google/devtools/build/lib/rules/java/JavaStarlarkApiTest.java
@@ -159,63 +159,6 @@ public class JavaStarlarkApiTest extends BuildViewTestCase {
     return result.build();
   }
 
-  @Test
-  public void testJavaInfoGetTransitiveRuntimeDeps() throws Exception {
-    scratch.file(
-        "foo/extension.bzl",
-        """
-        load("@rules_java//java/common:java_info.bzl", "JavaInfo")
-        result = provider()
-
-        def _impl(ctx):
-            return [result(property = ctx.attr.dep[JavaInfo].transitive_runtime_jars)]
-
-        my_rule = rule(_impl, attrs = {"dep": attr.label()})
-        """);
-
-    scratch.file(
-        "foo/BUILD",
-        """
-        load("@rules_java//java:defs.bzl", "java_library")
-        load(":extension.bzl", "my_rule")
-
-        java_library(
-            name = "my_java_lib_c",
-            srcs = ["java/C.java"],
-        )
-
-        java_library(
-            name = "my_java_lib_b",
-            srcs = ["java/B.java"],
-            deps = [":my_java_lib_c"],
-        )
-
-        java_library(
-            name = "my_java_lib_a",
-            srcs = ["java/A.java"],
-            deps = [":my_java_lib_b"],
-        )
-
-        my_rule(
-            name = "my_starlark_rule",
-            dep = ":my_java_lib_a",
-        )
-        """);
-    assertNoEvents();
-    ConfiguredTarget myRuleTarget = getConfiguredTarget("//foo:my_starlark_rule");
-    StructImpl info =
-        (StructImpl)
-            myRuleTarget.get(
-                new StarlarkProvider.Key(
-                    keyForBuild(Label.parseCanonical("//foo:extension.bzl")), "result"));
-
-    Depset sourceJars = (Depset) info.getValue("property");
-
-    assertThat(prettyArtifactNames(sourceJars.getSet(Artifact.class)))
-        .containsExactly(
-            "foo/libmy_java_lib_a.jar", "foo/libmy_java_lib_b.jar", "foo/libmy_java_lib_c.jar");
-  }
-
   /** Tests that JavaInfo provides information about transitive native libraries in Starlark. */
   @Test
   public void javaInfo_getTransitiveNativeLibraries() throws Exception {
