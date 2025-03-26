bazelbuild/bazel

commit 7e538803d775929beb3c9af1cd2e87667d373bbe

Use rule machinery for handling Symbolic Macro attrs in order to reduce the memory overhead of symbolic macros.
In particular, RuleOrMacroInstance holds shared logic between Rule and MacroInstance, and both RuleClass and MacroClass now hold an AttributeProvider, which gives access to their Attributes.

Together, this gives us:
- Use of the package's list interner for attributes
- Map of Attributes is now a list (the key/name is already stored in the attribute)
- Default values of Attributes are not stored
- Compact attr representation once the macro is frozen
- Attrs are stored as Native types instead of Starlark types

As a side effect of doing this, Attribute.valueToStarlark() now works with Selects.

PiperOrigin-RevId: 739568597
Change-Id: Ia6219f3105ede816c08105fec6c0df03d7b8a5e6

src/main/java/com/google/devtools/build/lib/packages/MacroInstance.java
@@ -18,17 +18,16 @@ import static java.util.Objects.requireNonNull;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import com.google.devtools.build.lib.cmdline.Label;
 import com.google.devtools.build.lib.cmdline.PackageIdentifier;
+import com.google.devtools.build.lib.events.Event;
+import com.google.devtools.build.lib.events.EventHandler;
 import com.google.devtools.build.lib.packages.BuildType.SelectorList;
-import com.google.devtools.build.lib.packages.Type.ConversionException;
+import com.google.devtools.build.lib.packages.Package.Declarations;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Consumer;
 import javax.annotation.Nullable;
 import net.starlark.java.eval.EvalException;
-import net.starlark.java.eval.Starlark;
 
 /**
  * Represents a use of a symbolic macro in a package.
@@ -40,7 +39,7 @@ import net.starlark.java.eval.Starlark;
  *
  * <p>Macro instance names are not guaranteed to be unique within a package; see {@link #getId}.
  */
-public final class MacroInstance {
+public final class MacroInstance extends RuleOrMacroInstance {
 
   // TODO: #19922 - If we want to save the cost of a field here, we can merge pkg and parent into a
   // single field of type Object, and walk up the parent hierarchy to answer getPackage() queries.
@@ -52,20 +51,8 @@ public final class MacroInstance {
 
   private final int sameNameDepth;
 
-  // Order isn't guaranteed, sort before dumping. You can use the schema map
-  // MacroClass#getAttributes for a guaranteed order.
-  private final ImmutableMap<String, Object> attrValues;
-
-  // TODO(#19922): Consider switching to more optimized, indexed representation for attributes, as
-  // in Rule.
-
   /**
-   * Instantiates the given macro class with the given attribute values.
-   *
-   * <p>{@code attrValues} must have already been normalized based on the types of the attributes;
-   * see {@link MacroClass#instantiateMacro}. Values for the {@code "name"} and {@code "visibility"}
-   * attributes must exist with the correct types, and the {@code "visibility"} value must satisfy
-   * {@link RuleVisibility#validate}.
+   * Instantiates the given macro class.
    *
    * <p>{@code sameNameDepth} is the number of macro instances that this one is inside of that share
    * its name. For most instances it is 1, but for the main submacro of a parent macro it is one
@@ -79,18 +66,17 @@ public final class MacroInstance {
   // making the constructor private and moving instantiateMacro() to this class.
   public MacroInstance(
       Package.Metadata packageMetadata,
+      Declarations packageDeclarations,
       @Nullable MacroInstance parent,
       MacroClass macroClass,
-      Map<String, Object> attrValues,
-      int sameNameDepth)
-      throws EvalException {
+      Label label,
+      int sameNameDepth) {
+    super(label, packageDeclarations, macroClass.getAttributeProvider().getAttributeCount());
     this.packageMetadata = packageMetadata;
     this.parent = parent;
     this.macroClass = macroClass;
-    this.attrValues = ImmutableMap.copyOf(attrValues);
     Preconditions.checkArgument(sameNameDepth > 0);
     this.sameNameDepth = sameNameDepth;
-    Preconditions.checkArgument(macroClass.getAttributes().keySet().equals(attrValues.keySet()));
   }
 
   /** Returns the Package.Metadata of the package in which this instance was created. */
@@ -146,13 +132,12 @@ public final class MacroInstance {
     return getName() + ":" + sameNameDepth;
   }
 
-  /**
-   * Returns the name of this instance, as given in the {@code name = ...} attribute in the calling
-   * BUILD file or macro.
-   */
-  public String getName() {
-    // Type and existence enforced by RuleClass.NAME_ATTRIBUTE.
-    return (String) Preconditions.checkNotNull(attrValues.get("name"));
+  @Override
+  public RuleVisibility getDefaultVisibility() {
+    return RuleVisibility.parseUnchecked(
+        ImmutableList.of(
+            Label.createUnvalidated(
+                macroClass.getDefiningBzlLabel().getPackageIdentifier(), "__pkg__")));
   }
 
   /**
@@ -167,19 +152,10 @@ public final class MacroInstance {
    */
   public ImmutableList<Label> getActualVisibility() {
     @SuppressWarnings("unchecked")
-    List<Label> visibility = (List<Label>) Preconditions.checkNotNull(attrValues.get("visibility"));
+    List<Label> visibility = (List<Label>) Preconditions.checkNotNull(getAttr("visibility"));
     return ImmutableList.copyOf(visibility);
   }
 
-  /**
-   * Dictionary of attributes for this instance.
-   *
-   * <p>Contains all attributes, as seen after processing by {@link
-   * MacroClass#instantiateAndAddMacro}.
-   */
-  public ImmutableMap<String, Object> getAttrValues() {
-    return attrValues;
-  }
 
   /**
    * Returns the package containing the .bzl file from which this macro instance's macro class was
@@ -201,7 +177,7 @@ public final class MacroInstance {
    * visibility privilege to us.
    */
   public void visitExplicitAttributeLabels(Consumer<Label> consumer) {
-    for (Attribute attribute : macroClass.getAttributes().values()) {
+    for (Attribute attribute : macroClass.getAttributeProvider().getAttributes()) {
       String name = attribute.getName();
       Type<?> type = attribute.getType();
       if (name.startsWith("_")) {
@@ -210,36 +186,17 @@ public final class MacroInstance {
       if (type.getLabelClass() != Type.LabelClass.DEPENDENCY) {
         continue;
       }
-      Object value = attrValues.get(name);
-      if (value == Starlark.NONE) {
-        continue;
-      }
+      Object value = getAttr(name, type);
       visitAttributeLabels(value, type, attribute, consumer);
     }
   }
 
   // Separate method needed to satisfy type system w.r.t. Type<T>.
   // `value` is either a T or SelectorList<T>.
-  private static <T> void visitAttributeLabels(
+  private <T> void visitAttributeLabels(
       Object value, Type<T> type, Attribute attribute, Consumer<Label> consumer) {
-    // The attribute value is stored as a Starlark value. Convert it to the internal type as would
-    // be used in rules, so we can apply visitLabels() machinery to it. selectableConvert() will
-    // yield either a T or a BuildType.SelectorList.
-    Object convertedValue;
-    try {
-      convertedValue =
-          BuildType.selectableConvert(
-              type,
-              value,
-              "macro attribute (internal)",
-              // No string -> Label conversion is being done here.
-              /* context= */ null,
-              // Macros always preserve selects as selects.
-              /* simplifyUnconditionalSelects= */ false);
-    } catch (ConversionException e) {
-      // TODO: #19922 - The fact that we have to do this seems like a signal that we should
-      // transition to storing macro attribute values as native-typed attributes in the future.
-      throw new IllegalStateException("Could not convert macro attribute value internally", e);
+    if (value == null) {
+      return;
     }
 
     // Unlike rules, null attribute values are disallowed here by construction (the attrValues
@@ -252,9 +209,9 @@ public final class MacroInstance {
           }
         };
 
-    if (convertedValue instanceof SelectorList) {
+    if (value instanceof SelectorList) {
       @SuppressWarnings("unchecked") // safe by precondition assumption
-      SelectorList<T> selectorList = (SelectorList<T>) convertedValue;
+      SelectorList<T> selectorList = (SelectorList<T>) value;
       AggregatingAttributeMapper.visitLabelsInSelect(
           selectorList,
           attribute,
@@ -264,9 +221,29 @@ public final class MacroInstance {
           /* includeKeys= */ false,
           /* includeValues= */ true);
     } else {
-      T castValue = type.cast(convertedValue);
+      T castValue = type.cast(value);
       type.visitLabels(visitor, castValue, attribute);
     }
+  }
+
+  @Override
+  public AttributeProvider getAttributeProvider() {
+    return macroClass.getAttributeProvider();
+  }
+
+  @Override
+  void reportError(String message, EventHandler eventHandler) {
+    eventHandler.handle(Event.error(message));
+  }
+
+  @Override
+  public boolean isRuleInstance() {
+    return false;
+  }
+
+  @Override
+  public boolean isRuleCreatedInLegacyMacro() {
+    return false;
   }
 
   /**

src/main/java/com/google/devtools/build/docgen/BuildDocCollector.java
@@ -207,7 +207,7 @@ public class BuildDocCollector {
         if (ruleClass.isDocumented()) {
           Class<? extends RuleDefinition> ruleDefinition =
               ruleClassProvider.getRuleClassDefinition(ruleDoc.getRuleName()).getClass();
-          for (Attribute attribute : ruleClass.getAttributes()) {
+          for (Attribute attribute : ruleClass.getAttributeProvider().getAttributes()) {
             if (!attribute.isDocumented()) {
               continue;
             }

src/main/java/com/google/devtools/build/lib/analysis/DependencyResolutionHelpers.java
@@ -510,7 +510,8 @@ public final class DependencyResolutionHelpers {
         && !rule.isAttrDefined(attrName, BuildType.NODEP_LABEL_LIST)) {
       return;
     }
-    Attribute attribute = rule.getRuleClassObject().getAttributeByName(attrName);
+    Attribute attribute =
+        rule.getRuleClassObject().getAttributeProvider().getAttributeByName(attrName);
     outgoingLabels.putAll(AttributeDependencyKind.forRule(attribute), labels);
   }
 
@@ -528,7 +529,8 @@ public final class DependencyResolutionHelpers {
     // `ctx.rule.attr` with rule attributes taking precedence then aspects' attributes based on the
     // aspect order in the aspects path (lowest order to highest).
 
-    List<Attribute> ruleAttributes = rule.getRuleClassObject().getAttributes();
+    List<Attribute> ruleAttributes =
+        rule.getRuleClassObject().getAttributeProvider().getAttributes();
     for (Attribute attribute : ruleAttributes) {
         result.add(AttributeDependencyKind.forRule(attribute));
       ruleAndBaseAspectsProcessedAttributes.add(attribute.getName());

src/main/java/com/google/devtools/build/lib/analysis/RequiredFragmentsUtil.java
@@ -248,7 +248,7 @@ public final class RequiredFragmentsUtil {
             .attributes(attributeMap)
             .analysisData(starlarkExecTransition)
             .build();
-    for (Attribute attribute : target.getRuleClassObject().getAttributes()) {
+    for (Attribute attribute : target.getRuleClassObject().getAttributeProvider().getAttributes()) {
       if (attribute.getTransitionFactory() != null) {
         attribute
             .getTransitionFactory()

src/main/java/com/google/devtools/build/lib/analysis/RuleContext.java
@@ -2169,7 +2169,7 @@ public class RuleContext extends TargetContext
       // but we no longer locations for individual attributes.
       // We should record the instantiation call stack in each rule
       // and report the position of its topmost frame here.
-      return rule.wasCreatedByMacro()
+      return rule.isRuleCreatedInLegacyMacro()
           ? String.format(
               ". Since this rule was created by the macro '%s', the error might have been "
                   + "caused by the macro implementation",

src/main/java/com/google/devtools/build/lib/analysis/starlark/StarlarkRuleClassFunctions.java
@@ -466,12 +466,12 @@ public class StarlarkRuleClassFunctions implements StarlarkRuleFunctionsApi {
       return ImmutableList.of();
     } else if (inheritAttrsArg instanceof RuleFunction ruleFunction) {
       verifyInheritAttrsArgExportedIfExportable(ruleFunction);
-      return ruleFunction.getRuleClass().getAttributes();
+      return ruleFunction.getRuleClass().getAttributeProvider().getAttributes();
     } else if (inheritAttrsArg instanceof MacroFunction macroFunction) {
       verifyInheritAttrsArgExportedIfExportable(macroFunction);
-      return macroFunction.getMacroClass().getAttributes().values().asList();
+      return macroFunction.getMacroClass().getAttributeProvider().getAttributes();
     } else if (inheritAttrsArg.equals(COMMON_ATTRIBUTES_NAME)) {
-      return baseRule.getAttributes();
+      return baseRule.getAttributeProvider().getAttributes();
     }
     throw Starlark.errorf(
         "Invalid 'inherit_attrs' value %s; expected a rule, a macro, or \"common\"",
@@ -1390,7 +1390,8 @@ public class StarlarkRuleClassFunctions implements StarlarkRuleFunctionsApi {
   }
 
   private static ImmutableSet<String> getLegacyAnyTypeAttrs(RuleClass ruleClass) {
-    Attribute attr = ruleClass.getAttributeByNameMaybe("$legacy_any_type_attrs");
+    Attribute attr =
+        ruleClass.getAttributeProvider().getAttributeByNameMaybe("$legacy_any_type_attrs");
     if (attr == null
         || attr.getType() != STRING_LIST
         || !(attr.getDefaultValueUnchecked() instanceof List<?>)) {
@@ -1677,7 +1678,7 @@ public class StarlarkRuleClassFunctions implements StarlarkRuleFunctionsApi {
           // users.
           // The less magic the better. Do not give in those temptations!
           Dict.Builder<String, Object> initializerKwargs = Dict.builder();
-          for (var attr : currentRuleClass.getAttributes()) {
+          for (var attr : currentRuleClass.getAttributeProvider().getAttributes()) {
             if ((attr.isPublic() && attr.starlarkDefined()) || attr.getName().equals("name")) {
               if (kwargs.containsKey(attr.getName())) {
                 Object value = kwargs.get(attr.getName());
@@ -1724,7 +1725,8 @@ public class StarlarkRuleClassFunctions implements StarlarkRuleFunctionsApi {
                   ALLOWLIST_RULE_EXTENSION_API_EXPERIMENTAL);
             }
             String nativeName = arg.startsWith("_") ? "$" + arg.substring(1) : arg;
-            Attribute attr = currentRuleClass.getAttributeByNameMaybe(nativeName);
+            Attribute attr =
+                currentRuleClass.getAttributeProvider().getAttributeByNameMaybe(nativeName);
             if (attr != null && !attr.starlarkDefined()) {
               throw Starlark.errorf(
                   "Initializer can only set Starlark defined attributes, not '%s'", arg);
@@ -1770,7 +1772,7 @@ public class StarlarkRuleClassFunctions implements StarlarkRuleFunctionsApi {
     }
 
     private static void validateRulePropagatedAspects(RuleClass ruleClass) throws EvalException {
-      for (Attribute attribute : ruleClass.getAttributes()) {
+      for (Attribute attribute : ruleClass.getAttributeProvider().getAttributes()) {
         attribute.validateRulePropagatedAspectsParameters(ruleClass);
       }
     }

commit fa6b1b7ec8deb727d437cd6f7f0324bfc7a6d6b8

Reverse a RAM regression introduced by https://github.com/bazelbuild/bazel/commit/7e538803d775929beb3c9af1cd2e87667d373bbe
That CL factored some content of `Rule` into a new base class, `RuleOrMacroInstance`. In doing so, it added two new fields to `Rule` instances, causing a 1% RAM regression on a large `query` invocation.

This CL eliminates the two extra fields.

`attrCount`, the number of attributes in the rule or macro's schema (not to be confused with the number of attributes actually stored on the instance, which may change after freezing/compacting), can be obtained from the schema via the subclass's `ruleClass` or `macroClass` field without storing it as an additional field on the base class. We have to take care not to do this retrieval of the subclass field while executing the super constructor (i.e. before the subclass field is initialized). That's solved by threading `attrCount` into `bitSetSize` as a parameter.

(A prior draft of this CL went through some indirect initialization logic whereby the super constructor called an abstract method to initialized the subclass field. Thanks to arostovtsev@ for pointing out that this was unnecessary.)

The second field, `packageDeclarations`, is replaced by an abstract getter that already exists on `Rule` and which is added to `MacroInstance` in this CL. It's probably not really needed on `MacroInstance`, but we can eliminate that latter; the cost of an extra field on a macro is nothing like the cost of an extra field on a rule.

PiperOrigin-RevId: 740793903
Change-Id: Iec65f20ff459e33c3961b16d9d51d0d4a22f0e00

src/main/java/com/google/devtools/build/lib/packages/MacroInstance.java
@@ -27,7 +27,6 @@ import com.google.devtools.build.lib.packages.Package.Declarations;
 import java.util.List;
 import java.util.function.Consumer;
 import javax.annotation.Nullable;
-import net.starlark.java.eval.EvalException;
 
 /**
  * Represents a use of a symbolic macro in a package.
@@ -45,6 +44,11 @@ public final class MacroInstance extends RuleOrMacroInstance {
   // single field of type Object, and walk up the parent hierarchy to answer getPackage() queries.
   private final Package.Metadata packageMetadata;
 
+  // TODO(bazel-team): This is only needed for RuleOrMacroInstance#getPackageDeclarations(), which
+  // is used by the attribute mapper logic. That might only be needed for rules rather than macros.
+  // Consider removing it and pushing getPackageDeclarations() down to Rule.
+  private final Package.Declarations packageDeclarations;
+
   @Nullable private final MacroInstance parent;
 
   private final MacroClass macroClass;
@@ -57,9 +61,6 @@ public final class MacroInstance extends RuleOrMacroInstance {
    * <p>{@code sameNameDepth} is the number of macro instances that this one is inside of that share
    * its name. For most instances it is 1, but for the main submacro of a parent macro it is one
    * more than the parent's depth.
-   *
-   * @throws EvalException if there's a problem with the attribute values (currently, only thrown if
-   *     the {@code visibility} value is invalid)
    */
   // TODO: #19922 - Better encapsulate the invariant around attrValues, by either transitioning to
   // storing internal-typed values (the way Rules do) instead of Starlark-typed values, or else just
@@ -71,8 +72,9 @@ public final class MacroInstance extends RuleOrMacroInstance {
       MacroClass macroClass,
       Label label,
       int sameNameDepth) {
-    super(label, packageDeclarations, macroClass.getAttributeProvider().getAttributeCount());
+    super(label, macroClass.getAttributeProvider().getAttributeCount());
     this.packageMetadata = packageMetadata;
+    this.packageDeclarations = packageDeclarations;
     this.parent = parent;
     this.macroClass = macroClass;
     Preconditions.checkArgument(sameNameDepth > 0);
@@ -82,6 +84,11 @@ public final class MacroInstance extends RuleOrMacroInstance {
   /** Returns the Package.Metadata of the package in which this instance was created. */
   public Package.Metadata getPackageMetadata() {
     return packageMetadata;
+  }
+
+  @Override
+  Declarations getPackageDeclarations() {
+    return packageDeclarations;
   }
 
   /**

src/main/java/com/google/devtools/build/lib/packages/Rule.java
@@ -112,7 +112,7 @@ public class Rule extends RuleOrMacroInstance implements Target {
       RuleClass ruleClass,
       Location location,
       @Nullable CallStack.Node interiorCallStack) {
-    super(label, pkg.getDeclarations(), ruleClass.getAttributeProvider().getAttributeCount());
+    super(label, ruleClass.getAttributeProvider().getAttributeCount());
     this.pkg = checkNotNull(pkg);
     this.ruleClass = checkNotNull(ruleClass);
     this.location = checkNotNull(location);

src/main/java/com/google/devtools/build/lib/packages/RuleOrMacroInstance.java
@@ -43,9 +43,6 @@ public abstract class RuleOrMacroInstance implements DependencyFilter.AttributeI
   static final String GENERATOR_LOCATION = "generator_location";
 
   private static final int ATTR_SIZE_THRESHOLD = 126;
-  private final int attrCount;
-
-  @Nullable private final Declarations packageDeclarations;
 
   /**
    * For {@link Rule}s, the length of this instance's generator name if it is a prefix of its name,
@@ -68,12 +65,10 @@ public abstract class RuleOrMacroInstance implements DependencyFilter.AttributeI
    */
   int generatorNamePrefixLength = 0;
 
-  RuleOrMacroInstance(Label label, Declarations packageDeclarations, int attrCount) {
+  RuleOrMacroInstance(Label label, int attrCount) {
     this.label = checkNotNull(label);
-    this.packageDeclarations = packageDeclarations;
     this.attrValues = new Object[attrCount];
-    this.attrCount = attrCount;
-    this.attrBytes = new byte[bitSetSize()];
+    this.attrBytes = new byte[bitSetSize(attrCount)];
   }
 
   /**
@@ -260,6 +255,7 @@ public abstract class RuleOrMacroInstance implements DependencyFilter.AttributeI
    */
   @Nullable
   Object getAttrIfStored(int attrIndex) {
+    int attrCount = getAttributeProvider().getAttributeCount();
     checkPositionIndex(attrIndex, attrCount - 1);
     return switch (getAttrState()) {
       case MUTABLE -> attrValues[attrIndex];
@@ -271,7 +267,7 @@ public abstract class RuleOrMacroInstance implements DependencyFilter.AttributeI
         if (attrBytes.length == 0) {
           yield null;
         }
-        int bitSetSize = bitSetSize();
+        int bitSetSize = bitSetSize(attrCount);
         int index = binarySearchAttrBytes(bitSetSize, attrIndex, 0xff);
         yield index < 0 ? null : attrValues[index - bitSetSize];
       }
@@ -408,7 +404,7 @@ public abstract class RuleOrMacroInstance implements DependencyFilter.AttributeI
       indicesToStore.set(i);
     }
 
-    if (attrCount < ATTR_SIZE_THRESHOLD) {
+    if (getAttributeProvider().getAttributeCount() < ATTR_SIZE_THRESHOLD) {
       freezeSmall(indicesToStore);
     } else {
       freezeLarge(indicesToStore);
@@ -464,7 +460,8 @@ public abstract class RuleOrMacroInstance implements DependencyFilter.AttributeI
 
   AttrState getAttrState() {
     // This check works because the name attribute is never stored, so the compact representation
-    // of attrValues will always have length < attrCount.
+    // of attrValues will always have length < attr count.
+    int attrCount = getAttributeProvider().getAttributeCount();
     if (attrValues.length == attrCount) {
       return AttrState.MUTABLE;
     }
@@ -472,7 +469,7 @@ public abstract class RuleOrMacroInstance implements DependencyFilter.AttributeI
   }
 
   /** Calculates the number of bytes necessary to have an explicit bit for each attribute. */
-  private int bitSetSize() {
+  private static int bitSetSize(int attrCount) {
     // ceil(attrCount / 8)
     return (attrCount + 7) / 8;
   }
@@ -548,9 +545,11 @@ public abstract class RuleOrMacroInstance implements DependencyFilter.AttributeI
     return rawLabels != null ? rawLabels : getDefaultVisibility().getDeclaredLabels();
   }
 
+  abstract Declarations getPackageDeclarations();
+
   @Nullable
   public PackageArgs getPackageArgs() {
-    return packageDeclarations.getPackageArgs();
+    return getPackageDeclarations().getPackageArgs();
   }
 
   abstract void reportError(String message, EventHandler eventHandler);