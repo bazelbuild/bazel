c836ac69b8f552f46ebc55681ee0fe140f0a991c
Googler <ilist@google.com>
2025-04-25 11:57:51 -0700

    Use LibraryToLink in LibariesToLinkCollector without converting them to LegacyLinkerInputs
    The change is no-op.
    
    It reduces a bit of peak heap consumption, but not a lot.
    
    PiperOrigin-RevId: 751507152
    Change-Id: Ie9b55318560ab1e1703afb32c43275b908735e85

--- src/main/java/com/google/devtools/build/lib/rules/cpp/CcStarlarkInternal.java
+++ src/main/java/com/google/devtools/build/lib/rules/cpp/CcStarlarkInternal.java
@@ -18,7 +18,6 @@ import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.devtools.build.lib.skyframe.BzlLoadValue.keyForBuild;
 
 import com.google.common.base.Ascii;
-import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
@@ -53,7 +52,6 @@ import com.google.devtools.build.lib.rules.cpp.CcToolchainVariables.LibraryToLin
 import com.google.devtools.build.lib.rules.cpp.CcToolchainVariables.SequenceBuilder;
 import com.google.devtools.build.lib.rules.cpp.CcToolchainVariables.VariableValue;
 import com.google.devtools.build.lib.rules.cpp.CppLinkActionBuilder.LinkActionConstruction;
-import com.google.devtools.build.lib.rules.cpp.LegacyLinkerInputs.LibraryInput;
 import com.google.devtools.build.lib.rules.cpp.LibrariesToLinkCollector.CollectedLibrariesToLink;
 import com.google.devtools.build.lib.rules.cpp.Link.LinkTargetType;
 import com.google.devtools.build.lib.rules.cpp.Link.LinkingMode;
@@ -70,7 +68,6 @@ import net.starlark.java.annot.StarlarkBuiltin;
 import net.starlark.java.annot.StarlarkMethod;
 import net.starlark.java.eval.Dict;
 import net.starlark.java.eval.EvalException;
-import net.starlark.java.eval.Mutability;
 import net.starlark.java.eval.NoneType;
 import net.starlark.java.eval.Sequence;
 import net.starlark.java.eval.Starlark;
@@ -703,28 +700,6 @@ public class CcStarlarkInternal implements StarlarkValue {
       }
     }
     return StarlarkList.immutableCopyOf(staticLibraries.build());
-  }
-
-  public static StarlarkList<LibraryInput> convertLibraryToLinkListToLinkerInputList(
-      Sequence<LibraryToLink> librariesToLink, boolean preferStaticLibs, boolean preferPicLibs) {
-    ImmutableList.Builder<LibraryInput> libraryInputsBuilder = ImmutableList.builder();
-    for (LibraryToLink libraryToLink : librariesToLink) {
-      LibraryInput libraryInputToUse;
-      if (preferStaticLibs) {
-        libraryInputToUse = libraryToLink.getStaticLibraryInput(preferPicLibs);
-        if (libraryInputToUse == null) {
-          libraryInputToUse = libraryToLink.getInterfaceOrDynamicLibraryInput();
-        }
-      } else {
-        libraryInputToUse = libraryToLink.getInterfaceOrDynamicLibraryInput();
-        if (libraryInputToUse == null) {
-          libraryInputToUse = libraryToLink.getStaticLibraryInput(preferPicLibs);
-        }
-      }
-      Preconditions.checkNotNull(libraryInputToUse);
-      libraryInputsBuilder.add(libraryInputToUse);
-    }
-    return StarlarkList.copyOf(Mutability.IMMUTABLE, libraryInputsBuilder.build());
   }
 
   @StarlarkMethod(
