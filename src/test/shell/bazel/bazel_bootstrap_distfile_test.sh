91bfaaca62fcba849ea60efa4db0c57a881cdd4f
Fabian Meumertzheim <fabian@meumertzhe.im>
2025-05-23 21:39:25 +0200

    Update bazel_bootstrap_distfile_test.sh

--- src/test/shell/bazel/bazel_bootstrap_distfile_test.sh
+++ src/test/shell/bazel/bazel_bootstrap_distfile_test.sh
@@ -145,7 +145,7 @@ EOF
       build --nobuild --repository_cache=derived/repository_cache --repo_contents_cache= \
       --override_repository=$(cat derived/maven/MAVEN_CANONICAL_REPO_NAME)=derived/maven \
       --java_language_version=${JAVA_VERSION} --tool_java_language_version=${JAVA_VERSION} \
-      --tool_java_runtime_version=local_jdk \
+      --java_runtime_version=local_jdk --tool_java_runtime_version=local_jdk \
       --extra_toolchains=@rules_python//python/runtime_env_toolchains:all \
       --extra_toolchains=fake_java_toolchain:all \
       src:bazel_nojdk &> "${TEST_log}" || fail "analysis with bootstrapped Bazel failed"
