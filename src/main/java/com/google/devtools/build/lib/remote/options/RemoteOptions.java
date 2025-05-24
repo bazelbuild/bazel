32779e7f8cb279f48f99a3b5468b3fd7abae7362
Patrice Arruda <patrice.arruda@mongodb.com>
2025-05-24 00:04:52 +0000

    SERVER-104338: Omit warnings if a specific artifact could not be found.
    This patchset contains a new flag named
    experimental_remote_download_omit_local_fetch_warning_url
    that omits warnings being displayed to the user if an artifact
    could not be fetched remotely from engflow. This requires the
    experimental_remote_downloader_local_fallback to be set for true.
    
    See go/omit-bazel-remote-fetch-errors for more context.

--- src/main/java/com/google/devtools/build/lib/remote/options/RemoteOptions.java
+++ src/main/java/com/google/devtools/build/lib/remote/options/RemoteOptions.java
@@ -151,20 +151,6 @@ public final class RemoteOptions extends CommonRemoteOptions {
   public boolean remoteDownloaderLocalFallback;
 
   @Option(
-      name = "experimental_remote_downloader_propagate_credentials",
-      defaultValue = "false",
-      documentationCategory = OptionDocumentationCategory.REMOTE,
-      effectTags = {OptionEffectTag.UNKNOWN},
-      help =
-          "Whether to propagate credentials from netrc and credential helper to the remote"
-              + " downloader server. The server implementation needs to support the new"
-              + " `http_header_url:<url-index>:<header-key>` qualifier where the `<url-index>` is a"
-              + " 0-based position of the URL inside the FetchBlobRequest's `uris` field. The"
-              + " URL-specific headers should take precedence over the global headers.")
-  public boolean remoteDownloaderPropagateCredentials;
-
-
-  @Option(
     name = "experimental_remote_download_omit_local_fetch_warning_url",
     defaultValue = "null",
     documentationCategory = OptionDocumentationCategory.REMOTE,
