e69e65b6a78c813ad406d63041ab797aa8344a90
Fabian Meumertzheim <fabian@meumertzhe.im>
2025-05-26 05:50:34 -0700

    [8.3.0] Avoid digesting files in `AbstractActionInputPrefetcher` if possible
    Remote metadata that is later materialized as a local file frequently hits the code path in `AbstractActionInputPrefetcher` that checks for discrepancies between the metadata and the local filesystem state. Since computing the digest is potentially costly and often unnecessary in this case, short-circuit by comparing sizes and contents proxies, if available.
    
    Also add comments to `Path#statNullable` and `Path#statIfFound` to more clearly explain the subtle difference.
    
    Closes #26146.
    
    PiperOrigin-RevId: 763396727
    Change-Id: I081d6d8b4efba661348746ba56c39120d18c31c3
    (cherry picked from commit 914542ba3b97e5cef10127177bef610f7d2ee0e4)

--- src/main/java/com/google/devtools/build/lib/vfs/Path.java
+++ src/main/java/com/google/devtools/build/lib/vfs/Path.java
@@ -291,12 +291,22 @@ public class Path implements Comparable<Path>, FileType.HasFileType {
     return fileSystem.stat(asFragment(), followSymlinks.toBoolean());
   }
 
-  /** Like stat(), but returns null on file-nonexistence instead of throwing. */
+  /**
+   * Like stat(), but returns null in case of any error instead of throwing.
+   *
+   * <p>Use {@link #statIfFound()} instead to throw for errors due to any causes other than
+   * non-existence.
+   */
   public FileStatus statNullable() {
     return statNullable(Symlinks.FOLLOW);
   }
 
-  /** Like stat(), but returns null on file-nonexistence instead of throwing. */
+  /**
+   * Like stat(), but returns null in case of any error instead of throwing.
+   *
+   * <p>Use {@link #statIfFound(Symlinks)} instead to throw for errors due to any causes other than
+   * non-existence.
+   */
   public FileStatus statNullable(Symlinks symlinks) {
     return fileSystem.statNullable(asFragment(), symlinks.toBoolean());
   }
@@ -305,6 +315,8 @@ public class Path implements Comparable<Path>, FileType.HasFileType {
    * Like {@link #stat}, but may return null if the file is not found (corresponding to {@code
    * ENOENT} and {@code ENOTDIR} in Unix's stat(2) function) instead of throwing. Follows symbolic
    * links.
+   *
+   * <p>Use {@link #statNullable(Symlinks)} instead to ignore all types of errors.
    */
   public FileStatus statIfFound() throws IOException {
     return fileSystem.statIfFound(asFragment(), true);
@@ -313,6 +325,8 @@ public class Path implements Comparable<Path>, FileType.HasFileType {
   /**
    * Like {@link #stat}, but may return null if the file is not found (corresponding to {@code
    * ENOENT} and {@code ENOTDIR} in Unix's stat(2) function) instead of throwing.
+   *
+   * <p>Use {@link #statNullable(Symlinks)} instead to ignore all types of errors.
    *
    * @param followSymlinks if {@link Symlinks#FOLLOW}, and this path denotes a symbolic link, the
    *     link is dereferenced until a file other than a symbolic link is found
