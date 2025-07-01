fcc523b4adc2ff1b2f6b9fb4d7b9fa037adaf526
Fabian Meumertzheim <fabian@meumertzhe.im>
2025-07-01 09:51:22 -0700

    Fix docs for `InputMetadataProvider` and `SpawnExecutionContext`
    The `expandArtifacts` method does not expand runfiles trees and `prefetchInputs` doesn't have parameters.
    
    Closes #26446.
    
    PiperOrigin-RevId: 778082609
    Change-Id: Id02a9220ba5cb6d78dcb27d55128b05a1c518df9

--- src/main/java/com/google/devtools/build/lib/exec/SpawnRunner.java
+++ src/main/java/com/google/devtools/build/lib/exec/SpawnRunner.java
@@ -159,19 +159,6 @@ public interface SpawnRunner {
      * Prefetches the Spawns input files to the local machine. There are cases where Bazel runs on a
      * network file system, and prefetching the files in parallel is a significant performance win.
      * This should only be called by local strategies when local execution is imminent.
-     *
-     * <p>Should be called with the equivalent of: <code>
-     * policy.prefetchInputs(
-     *      Iterables.filter(policy.getInputMapping().values(), Predicates.notNull()));
-     * </code>
-     *
-     * <p>Note in particular that {@link #getInputMapping} may return {@code null} values, but this
-     * method does not accept {@code null} values.
-     *
-     * <p>The reason why this method requires passing in the inputs is that getInputMapping may be
-     * slow to compute, so if the implementation already called it, we don't want to compute it
-     * again. I suppose we could require implementations to memoize getInputMapping (but not compute
-     * it eagerly), and that may change in the future.
      */
     ListenableFuture<Void> prefetchInputs();
 
