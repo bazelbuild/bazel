bazelbuild/bazel

commit 2dd545b4e4fe35aa04a70eacd5ab1ba621b3a09c

Implement CppModuleMapAction as a "simple" file write action in Starlark.
We can implement the required logic in a callback passed to "map_each"
that operates on a struct containing all the relevant data. This avoids
the need for multiple closures to handle the different cases.

We cannot preserve the mnemonic "CppModuleMap" due to the use of a
simple write action.

PiperOrigin-RevId: 770576415
Change-Id: I3ab9dc15f8c6806f2e73cdf80f805e0a67c2debe

src/main/starlark/builtins_bzl/common/cc/compile/cc_compilation_helper.bzl
@@ -176,6 +176,138 @@ def _collect_module_maps(deps, cc_toolchain_compilation_context, additional_cpp_
 
     return module_maps
 
+def _module_map_struct_to_module_map_content(parameters, tree_expander):
+    lines = []
+    module_map = parameters.module_map
+    lines.append("module \"%s\" {" % module_map.name())
+    lines.append("  export *")
+
+    def expanded(artifacts):
+        expanded = []
+        for artifact in artifacts:
+            if artifact.is_directory:
+                expanded.extend(tree_expander.expand(artifact))
+            else:
+                expanded.append(artifact)
+        return expanded
+
+    def add_header(path, visibility, can_compile):
+        header_line = []
+        if parameters.generate_submodules:
+            lines.append("  module \"" + path + "\" {")
+            lines.append("    export *")
+            header_line.append("  ")
+        header_line.append("  ")
+        if visibility:
+            header_line.append(visibility)
+            header_line.append(" ")
+        should_compile = parameters.compiled_module and not path.endswith(".inc")
+        if not can_compile or not should_compile:
+            header_line.append("textual ")
+        header_line.append("header \"")
+        header_line.append(parameters.leading_periods)
+        header_line.append(path)
+        header_line.append("\"")
+        lines.append("".join(header_line))
+        if parameters.generate_submodules:
+            lines.append("  }")
+
+    added_paths = set()
+    for header in expanded(parameters.public_headers):
+        if header.path in added_paths:
+            continue
+        add_header(path = header.path, visibility = "", can_compile = True)
+        added_paths.add(header.path)
+
+    for header in expanded(parameters.private_headers):
+        if header.path in added_paths:
+            continue
+        add_header(path = header.path, visibility = "private", can_compile = True)
+        added_paths.add(header.path)
+
+    for header in parameters.separate_module_headers:
+        if header.path in added_paths:
+            continue
+        add_header(path = header.path, visibility = "", can_compile = False)
+        added_paths.add(header.path)
+
+    for path in parameters.additional_exported_headers:
+        if path in added_paths:
+            continue
+        add_header(path = path, visibility = "", can_compile = False)
+        added_paths.add(path)
+
+    for dep in parameters.dependency_module_maps:
+        lines.append("  use \"" + dep.name() + "\"")
+
+    if parameters.separate_module_headers:
+        separate_name = module_map.name() + ".sep"
+        lines.append("  use \"" + separate_name + "\"")
+        lines.append("}")
+        lines.append("module \"" + separate_name + "\" {")
+        lines.append("  export *")
+
+        added_paths = set()
+        for header in parameters.separate_module_headers:
+            if header.path in added_paths:
+                continue
+            add_header(path = header.path, visibility = "", can_compile = True)
+            added_paths.add(header.path)
+
+        for dep in parameters.dependency_module_maps:
+            lines.append("  use \"" + dep.name() + "\"")
+
+    lines.append("}")
+
+    if parameters.extern_dependencies:
+        for dep in parameters.dependency_module_maps:
+            lines.append(
+                "extern module \"" + dep.name() + "\" \"" +
+                parameters.leading_periods + dep.file().path + "\"",
+            )
+
+    return lines
+
+def _create_module_map_action(
+        actions,
+        module_map,
+        private_headers,
+        public_headers,
+        dependency_module_maps,
+        additional_exported_headers,
+        separate_module_headers,
+        compiled_module,
+        module_map_home_is_cwd,
+        generate_submodules,
+        extern_dependencies):
+    content = actions.args()
+    content.set_param_file_format("multiline")
+    segments_to_exec_path = module_map.file().path.count("/")
+    leading_periods = "" if module_map_home_is_cwd else "../" * segments_to_exec_path
+    data_struct = struct(
+        module_map = module_map,
+        public_headers = public_headers,
+        private_headers = private_headers,
+        dependency_module_maps = dependency_module_maps,
+        additional_exported_headers = additional_exported_headers,
+        separate_module_headers = separate_module_headers,
+        compiled_module = compiled_module,
+        generate_submodules = generate_submodules,
+        extern_dependencies = extern_dependencies,
+        leading_periods = leading_periods,
+    )
+    content.add_all([data_struct], map_each = _module_map_struct_to_module_map_content)
+
+    # We need to add all tree artifacts to the args object directly so we they can be
+    # expanded in the _module_map_struct_to_module_map_content callback function.
+    # We don't want to do anything with them at this point, so the map_each callback should be a
+    # simple null function.
+    tree_artifacts = [h for h in private_headers if h.is_directory]
+    tree_artifacts += [h for h in public_headers if h.is_directory]
+    content.add_all(tree_artifacts, map_each = lambda x: None, allow_closure = True)
+
+    actions.write(module_map.file(), content = content, is_executable = True)
+
 def _init_cc_compilation_context(
         # DO NOT use ctx, this is a temporary placeholder
         # to avoid adding a new field to CcCompilationHelper.
@@ -323,20 +455,19 @@ def _init_cc_compilation_context(
             private_headers_for_module_map_action = private_headers_artifacts
             if _enabled(feature_configuration, "exclude_private_headers_in_module_maps"):
                 private_headers_for_module_map_action = []
-            dependent_module_maps = _collect_module_maps(deps + implementation_deps, cc_toolchain_compilation_context, additional_cpp_module_maps)
-            cc_internal.create_module_map_action(
+            dependency_module_maps = _collect_module_maps(deps + implementation_deps, cc_toolchain_compilation_context, additional_cpp_module_maps)
+            _create_module_map_action(
                 actions = actions,
-                feature_configuration = feature_configuration,
                 module_map = module_map,
                 public_headers = public_headers_for_module_map_action,
                 separate_module_headers = separate_public_headers.module_map_headers,
-                dependent_module_maps = dependent_module_maps,
+                dependency_module_maps = dependency_module_maps,
                 private_headers = private_headers_for_module_map_action,
                 additional_exported_headers = additional_exported_headers,
                 compiled_module = compiled,
                 module_map_home_is_cwd = _enabled(feature_configuration, "module_map_home_cwd"),
                 generate_submodules = _enabled(feature_configuration, "generate_submodules"),
-                without_extern_dependencies = not _enabled(feature_configuration, "module_map_without_extern_module"),
+                extern_dependencies = not _enabled(feature_configuration, "module_map_without_extern_module"),
             )
 
         if generates_pic_header_module:

src/test/java/com/google/devtools/build/lib/actions/util/BUILD
@@ -57,6 +57,7 @@ java_library(
         "//src/main/java/com/google/devtools/common/options",
         "//src/main/java/net/starlark/java/syntax",
         "//src/main/protobuf:action_cache_java_proto",
+        "//src/test/java/com/google/devtools/build/lib/exec/util",
         "//src/test/java/com/google/devtools/build/lib/testutil",
         "//third_party:error_prone_annotations",
         "//third_party:guava",

src/test/java/com/google/devtools/build/lib/rules/cpp/BUILD
@@ -270,6 +270,7 @@ java_test(
     deps = [
         "//src/main/java/com/google/devtools/build/lib/actions",
         "//src/main/java/com/google/devtools/build/lib/actions:artifacts",
+        "//src/main/java/com/google/devtools/build/lib/analysis:actions/abstract_file_write_action",
         "//src/main/java/com/google/devtools/build/lib/analysis:analysis_cluster",
         "//src/main/java/com/google/devtools/build/lib/analysis:configured_target",
         "//src/main/java/com/google/devtools/build/lib/analysis:file_provider",
@@ -478,23 +479,6 @@ java_test(
         "//src/test/java/com/google/devtools/build/lib/actions/util",
         "//src/test/java/com/google/devtools/build/lib/analysis/util",
         "//src/test/java/com/google/devtools/build/lib/packages:testutil",
-        "//third_party:guava",
-        "//third_party:junit4",
-        "//third_party:truth",
-    ],
-)
-
-java_test(
-    name = "CppModuleMapActionTest",
-    srcs = ["CppModuleMapActionTest.java"],
-    deps = [
-        "//src/main/java/com/google/devtools/build/lib/actions",
-        "//src/main/java/com/google/devtools/build/lib/actions:artifacts",
-        "//src/main/java/com/google/devtools/build/lib/analysis/config:core_options",
-        "//src/main/java/com/google/devtools/build/lib/rules/cpp",
-        "//src/main/java/com/google/devtools/build/lib/vfs:pathfragment",
-        "//src/test/java/com/google/devtools/build/lib/actions/util",
-        "//src/test/java/com/google/devtools/build/lib/testutil",
         "//third_party:guava",
         "//third_party:junit4",
         "//third_party:truth",

src/main/java/com/google/devtools/build/lib/rules/cpp/CcStarlarkInternal.java
@@ -29,8 +29,6 @@ import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.actions.CommandLineExpansionException;
 import com.google.devtools.build.lib.actions.CommandLines.CommandLineAndParamFileInfo;
 import com.google.devtools.build.lib.actions.ParameterFile.ParameterFileType;
-import com.google.devtools.build.lib.analysis.RuleContext;
-import com.google.devtools.build.lib.analysis.actions.PathMappers;
 import com.google.devtools.build.lib.analysis.actions.SymlinkAction;
 import com.google.devtools.build.lib.analysis.config.BuildConfigurationValue;
 import com.google.devtools.build.lib.analysis.config.PerLabelOptions;
@@ -368,60 +366,6 @@ public class CcStarlarkInternal implements StarlarkValue {
                     .stream()
                     .map(PathFragment::create)
                     .collect(toImmutableList())));
-  }
-
-  @StarlarkMethod(
-      name = "create_module_map_action",
-      documented = false,
-      parameters = {
-        @Param(name = "actions", positional = false, named = true),
-        @Param(name = "feature_configuration", positional = false, named = true),
-        @Param(name = "module_map", positional = false, named = true),
-        @Param(name = "private_headers", positional = false, named = true),
-        @Param(name = "public_headers", positional = false, named = true),
-        @Param(name = "dependent_module_maps", positional = false, named = true),
-        @Param(name = "additional_exported_headers", positional = false, named = true),
-        @Param(name = "separate_module_headers", positional = false, named = true),
-        @Param(name = "compiled_module", positional = false, named = true),
-        @Param(name = "module_map_home_is_cwd", positional = false, named = true),
-        @Param(name = "generate_submodules", positional = false, named = true),
-        @Param(name = "without_extern_dependencies", positional = false, named = true),
-      })
-  public void createModuleMapAction(
-      StarlarkActionFactory actions,
-      FeatureConfigurationForStarlark featureConfigurationForStarlark,
-      CppModuleMap moduleMap,
-      Sequence<?> privateHeaders,
-      Sequence<?> publicHeaders,
-      Sequence<?> dependentModuleMaps,
-      Sequence<?> additionalExportedHeaders,
-      Sequence<?> separateModuleHeaders,
-      Boolean compiledModule,
-      Boolean moduleMapHomeIsCwd,
-      Boolean generateSubmodules,
-      Boolean withoutExternDependencies)
-      throws EvalException {
-    RuleContext ruleContext = actions.getRuleContext();
-    ruleContext.registerAction(
-        new CppModuleMapAction(
-            ruleContext.getActionOwner(),
-            moduleMap,
-            Sequence.cast(privateHeaders, Artifact.class, "private_headers"),
-            Sequence.cast(publicHeaders, Artifact.class, "public_headers"),
-            Sequence.cast(dependentModuleMaps, CppModuleMap.class, "dependent_module_maps"),
-            Sequence.cast(additionalExportedHeaders, String.class, "additional_exported_headers")
-                .stream()
-                .map(PathFragment::create)
-                .collect(toImmutableList()),
-            Sequence.cast(separateModuleHeaders, Artifact.class, "separate_module_headers"),
-            compiledModule,
-            moduleMapHomeIsCwd,
-            generateSubmodules,
-            withoutExternDependencies,
-            PathMappers.getOutputPathsMode(ruleContext.getConfiguration()),
-            ruleContext
-                .getConfiguration()
-                .modifiedExecutionInfo(ImmutableMap.of(), CppModuleMapAction.MNEMONIC)));
   }
 
   @SerializationConstant @VisibleForSerialization

commit 7ea80bd9258785ad932c8a67270cd7cc957cf0a7

Restore path mapping support with `layering_check`
`path_mapping_test` didn't set `CC=clang` and thus didn't actually cover the case of compilation with `--features=layering_check`. As a result, it wasn't noticed that 2dd545b4e4fe35aa04a70eacd5ab1ba621b3a09c regressed path mapping support for this feature.

Fixing this requires wiring up `ParameterFileWriteAction` with path mapping and allowing it to be opted in via execution info, as well as restoring the `CppModuleMap` mnemonic it had before Starlarkification.

RELNOTES: `ctx.actions.write` now supports path mapping when passed an `Args` object. Use the `mnemonics` attribute to assign it a dedicated mnemonic, which can then be used with `--modify_execution_info` to opt in to path mapping (see https://github.com/bazelbuild/bazel/discussions/22658 for details on path mapping).

Closes #26287.

PiperOrigin-RevId: 783412764
Change-Id: I46c1977c41fc02cc3a350e0c85220128221b0dc3

src/main/java/com/google/devtools/build/lib/analysis/BUILD
@@ -1341,12 +1341,14 @@ java_library(
     srcs = ["actions/ParameterFileWriteAction.java"],
     deps = [
         ":actions/abstract_file_write_action",
+        ":actions/path_mappers",
         "//src/main/java/com/google/devtools/build/lib/actions",
         "//src/main/java/com/google/devtools/build/lib/actions:artifacts",
         "//src/main/java/com/google/devtools/build/lib/actions:commandline_item",
+        "//src/main/java/com/google/devtools/build/lib/actions:execution_requirements",
+        "//src/main/java/com/google/devtools/build/lib/analysis/config:build_configuration",
         "//src/main/java/com/google/devtools/build/lib/analysis/config:core_options",
         "//src/main/java/com/google/devtools/build/lib/collect/nestedset",
-        "//src/main/java/com/google/devtools/build/lib/concurrent",
         "//src/main/java/com/google/devtools/build/lib/concurrent:thread_safety",
         "//src/main/java/com/google/devtools/build/lib/util",
         "//src/main/java/com/google/devtools/build/lib/util:deterministic_writer",

src/main/java/com/google/devtools/build/lib/analysis/actions/ParameterFileWriteAction.java
@@ -19,6 +19,7 @@ import static java.nio.charset.StandardCharsets.ISO_8859_1;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.io.BaseEncoding;
 import com.google.devtools.build.lib.actions.ActionExecutionContext;
 import com.google.devtools.build.lib.actions.ActionKeyContext;
@@ -28,11 +29,13 @@ import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.actions.CommandLine;
 import com.google.devtools.build.lib.actions.CommandLineExpansionException;
 import com.google.devtools.build.lib.actions.ExecException;
+import com.google.devtools.build.lib.actions.ExecutionRequirements;
 import com.google.devtools.build.lib.actions.InputMetadataProvider;
 import com.google.devtools.build.lib.actions.ParameterFile;
 import com.google.devtools.build.lib.actions.ParameterFile.ParameterFileType;
 import com.google.devtools.build.lib.actions.PathMapper;
 import com.google.devtools.build.lib.actions.UserExecException;
+import com.google.devtools.build.lib.analysis.config.BuildConfigurationValue;
 import com.google.devtools.build.lib.analysis.config.CoreOptions;
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
 import com.google.devtools.build.lib.collect.nestedset.NestedSetBuilder;
@@ -58,9 +61,9 @@ public final class ParameterFileWriteAction extends AbstractFileWriteAction {
 
   private final CommandLine commandLine;
   private final ParameterFileType type;
-  private final boolean hasInputArtifactToExpand;
   private final boolean makeExecutable;
   private final String mnemonic;
+  private final boolean usePathStripping;
 
   /**
    * Creates a new instance.
@@ -84,7 +87,9 @@ public final class ParameterFileWriteAction extends AbstractFileWriteAction {
         commandLine,
         type,
         makeExecutable,
-        AbstractFileWriteAction.MNEMONIC);
+        AbstractFileWriteAction.MNEMONIC,
+        /* executionInfo= */ ImmutableMap.of(),
+        CoreOptions.OutputPathsMode.OFF);
   }
 
   /**
@@ -98,6 +103,9 @@ public final class ParameterFileWriteAction extends AbstractFileWriteAction {
    * @param type the type of the file
    * @param makeExecutable whether the output file should be made executable
    * @param mnemonic the mnemonic for this action, or null if the default should be used
+   * @param executionInfo the execution info for this action (only supports-path-mapping is used)
+   * @param outputPathsMode the output paths mode obtained via {@link
+   *     PathMappers#getOutputPathsMode(BuildConfigurationValue)}
    */
   public ParameterFileWriteAction(
       ActionOwner owner,
@@ -106,13 +114,19 @@ public final class ParameterFileWriteAction extends AbstractFileWriteAction {
       CommandLine commandLine,
       ParameterFileType type,
       boolean makeExecutable,
-      String mnemonic) {
+      String mnemonic,
+      ImmutableMap<String, String> executionInfo,
+      CoreOptions.OutputPathsMode outputPathsMode) {
     super(owner, inputs, output);
     this.commandLine = commandLine;
     this.type = type;
-    this.hasInputArtifactToExpand = !inputs.isEmpty();
     this.makeExecutable = makeExecutable;
     this.mnemonic = mnemonic;
+    // Save memory by not storing the full execution info, but only what matters for this particular
+    // action.
+    this.usePathStripping =
+        PathMappers.getEffectiveOutputPathsMode(outputPathsMode, getMnemonic(), executionInfo)
+            == CoreOptions.OutputPathsMode.STRIP;
   }
 
   @Override
@@ -123,6 +137,17 @@ public final class ParameterFileWriteAction extends AbstractFileWriteAction {
   @Override
   public String getMnemonic() {
     return mnemonic;
+  }
+
+  @Override
+  public ImmutableMap<String, String> getExecutionInfo() {
+    return usePathStripping
+        ? ImmutableMap.of(ExecutionRequirements.SUPPORTS_PATH_MAPPING, "")
+        : ImmutableMap.of();
+  }
+
+  private CoreOptions.OutputPathsMode getOutputPathsMode() {
+    return usePathStripping ? CoreOptions.OutputPathsMode.STRIP : CoreOptions.OutputPathsMode.OFF;
   }
 
   @VisibleForTesting
@@ -138,7 +163,7 @@ public final class ParameterFileWriteAction extends AbstractFileWriteAction {
    * includeParamFile option is flag-guarded with warning regarding output size to user.
    *
    * <p>TODO(b/161359171): The list of arguments will be incorrect if the arguments contain tree
-   * artifacts.
+   * artifacts or path mapping is used.
    */
   public Iterable<String> getArguments()
       throws CommandLineExpansionException, InterruptedException {
@@ -156,7 +181,7 @@ public final class ParameterFileWriteAction extends AbstractFileWriteAction {
   @Nullable
   @Override
   public String getStarlarkContent() throws IOException, EvalException, InterruptedException {
-    if (hasInputArtifactToExpand) {
+    if (!getInputs().isEmpty()) {
       // Tree artifact information isn't available at analysis time.
       return null;
     }
@@ -171,10 +196,16 @@ public final class ParameterFileWriteAction extends AbstractFileWriteAction {
   public DeterministicWriter newDeterministicWriter(ActionExecutionContext ctx)
       throws ExecException, InterruptedException {
     final ArgChunk arguments;
+    // Other actions consuming this parameter file may have path mapping disabled due to inputs
+    // conflicting across configurations, in which case paths written to the file will not match.
+    // Since this depends on the consumer but the decision is only made at execution time, it is not
+    // clear how to improve that situation. Actions that are prone to such collisions should avoid
+    // depending on parameter files.
+    var pathMapper = PathMappers.create(this, getOutputPathsMode(), /* isStarlarkAction= */ false);
     try {
       InputMetadataProvider inputMetadataProvider =
           Preconditions.checkNotNull(ctx.getInputMetadataProvider());
-      arguments = commandLine.expand(inputMetadataProvider, PathMapper.NOOP);
+      arguments = commandLine.expand(inputMetadataProvider, pathMapper);
     } catch (CommandLineExpansionException e) {
       throw new UserExecException(
           e,
@@ -183,21 +214,23 @@ public final class ParameterFileWriteAction extends AbstractFileWriteAction {
               .setSpawn(Spawn.newBuilder().setCode(Code.COMMAND_LINE_EXPANSION_FAILURE))
               .build());
     }
-    return new ParamFileWriter(arguments, type);
+    return new ParamFileWriter(arguments, pathMapper, type);
   }
 
   private static class ParamFileWriter implements DeterministicWriter {
     private final ArgChunk arguments;
+    private final PathMapper pathMapper;
     private final ParameterFileType type;
 
-    ParamFileWriter(ArgChunk arguments, ParameterFileType type) {
+    ParamFileWriter(ArgChunk arguments, PathMapper pathMapper, ParameterFileType type) {
       this.arguments = arguments;
+      this.pathMapper = pathMapper;
       this.type = type;
     }
 
     @Override
     public void writeTo(OutputStream out) throws IOException {
-      ParameterFile.writeParameterFile(out, arguments.arguments(PathMapper.NOOP), type);
+      ParameterFile.writeParameterFile(out, arguments.arguments(pathMapper), type);
     }
   }
 
@@ -210,7 +243,11 @@ public final class ParameterFileWriteAction extends AbstractFileWriteAction {
     fp.addString(GUID);
     fp.addString(type.toString());
     commandLine.addToFingerprint(
-        actionKeyContext, inputMetadataProvider, CoreOptions.OutputPathsMode.OFF, fp);
+        actionKeyContext,
+        inputMetadataProvider,
+        PathMappers.getEffectiveOutputPathsMode(
+            getOutputPathsMode(), getMnemonic(), getExecutionInfo()),
+        fp);
   }
 
   @Override
@@ -227,7 +264,11 @@ public final class ParameterFileWriteAction extends AbstractFileWriteAction {
       // tell if two contents are equal or not.
       var fp = new Fingerprint();
       commandLine.addToFingerprint(
-          new ActionKeyContext(), null, CoreOptions.OutputPathsMode.OFF, fp);
+          new ActionKeyContext(),
+          null,
+          PathMappers.getEffectiveOutputPathsMode(
+              getOutputPathsMode(), getMnemonic(), getExecutionInfo()),
+          fp);
       message.append(BaseEncoding.base16().lowerCase().encode(fp.digestAndReset()));
       message.append(
           "\n"

src/main/java/com/google/devtools/build/lib/analysis/starlark/StarlarkActionFactory.java
@@ -43,6 +43,7 @@ import com.google.devtools.build.lib.analysis.actions.AbstractFileWriteAction;
 import com.google.devtools.build.lib.analysis.actions.BuildInfoFileWriteAction;
 import com.google.devtools.build.lib.analysis.actions.FileWriteAction;
 import com.google.devtools.build.lib.analysis.actions.ParameterFileWriteAction;
+import com.google.devtools.build.lib.analysis.actions.PathMappers;
 import com.google.devtools.build.lib.analysis.actions.SpawnAction;
 import com.google.devtools.build.lib.analysis.actions.StarlarkAction;
 import com.google.devtools.build.lib.analysis.actions.Substitution;
@@ -354,7 +355,9 @@ public class StarlarkActionFactory implements StarlarkActionFactoryApi {
               args.build(getMainRepoMappingSupplier()),
               args.getParameterFileType(),
               isExecutable,
-              mnemonic);
+              mnemonic,
+              ruleContext.getConfiguration().modifiedExecutionInfo(ImmutableMap.of(), mnemonic),
+              PathMappers.getOutputPathsMode(ruleContext.getConfiguration()));
     } else {
       throw new AssertionError("Unexpected type: " + content.getClass().getSimpleName());
     }

src/main/starlark/builtins_bzl/common/cc/compile/cc_compilation_helper.bzl
@@ -311,7 +311,7 @@ def _create_module_map_action(
     tree_artifacts += [h for h in public_headers if h.is_directory]
     content.add_all(tree_artifacts, map_each = lambda x: None, allow_closure = True)
 
-    actions.write(module_map.file(), content = content, is_executable = True)
+    actions.write(module_map.file(), content = content, is_executable = True, mnemonic = "CppModuleMap")
 
 def _init_cc_compilation_context(
         # DO NOT use ctx, this is a temporary placeholder

src/test/java/com/google/devtools/build/lib/analysis/actions/BUILD
@@ -116,6 +116,7 @@ java_test(
         "//src/main/java/com/google/devtools/build/lib/analysis:actions/abstract_file_write_action",
         "//src/main/java/com/google/devtools/build/lib/analysis:actions/custom_command_line",
         "//src/main/java/com/google/devtools/build/lib/analysis:actions/parameter_file_write_action",
+        "//src/main/java/com/google/devtools/build/lib/analysis/config:core_options",
         "//src/main/java/com/google/devtools/build/lib/collect/nestedset",
         "//src/main/java/com/google/devtools/build/lib/events",
         "//src/main/java/com/google/devtools/build/lib/skyframe:tree_artifact_value",

src/test/java/com/google/devtools/build/lib/analysis/actions/ParamFileWriteActionTest.java
@@ -34,6 +34,7 @@ import com.google.devtools.build.lib.actions.FileArtifactValue;
 import com.google.devtools.build.lib.actions.ParameterFile.ParameterFileType;
 import com.google.devtools.build.lib.actions.ThreadStateReceiver;
 import com.google.devtools.build.lib.actions.util.ActionsTestUtil;
+import com.google.devtools.build.lib.analysis.config.CoreOptions;
 import com.google.devtools.build.lib.analysis.util.ActionTester;
 import com.google.devtools.build.lib.analysis.util.BuildViewTestCase;
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
@@ -60,7 +61,7 @@ public class ParamFileWriteActionTest extends BuildViewTestCase {
   private SpecialArtifact treeArtifact;
 
   @Before
-  public void createArtifacts() throws Exception  {
+  public void createArtifacts() throws Exception {
     Path execRoot = scratch.getFileSystem().getPath("/exec");
     rootDir = ArtifactRoot.asDerivedRoot(execRoot, RootType.OUTPUT, "out");
     outputArtifact = getBinArtifactWithNoOwner("destination.txt");
@@ -73,8 +74,8 @@ public class ParamFileWriteActionTest extends BuildViewTestCase {
     Action action =
         createParameterFileWriteAction(
             NestedSetBuilder.emptySet(Order.STABLE_ORDER), createNormalCommandLine(), false);
-    assertThat(Artifact.toRootRelativePaths(action.getOutputs())).containsExactly(
-        "destination.txt");
+    assertThat(Artifact.toRootRelativePaths(action.getOutputs()))
+        .containsExactly("destination.txt");
   }
 
   @Test
@@ -154,7 +155,9 @@ public class ParamFileWriteActionTest extends BuildViewTestCase {
         commandLine,
         ParameterFileType.UNQUOTED,
         executable,
-        AbstractFileWriteAction.MNEMONIC);
+        AbstractFileWriteAction.MNEMONIC,
+        /* executionInfo= */ ImmutableMap.of(),
+        CoreOptions.OutputPathsMode.OFF);
   }
 
   private static CommandLine createNormalCommandLine() {

src/test/shell/bazel/path_mapping_test.sh
@@ -740,6 +740,7 @@ std::string AsGreeting(const std::string& name) {
 EOF
 
   bazel run \
+    --repo_env=CC=clang \
     --verbose_failures \
     --experimental_output_paths=strip \
     --modify_execution_info=CppCompile=+supports-path-mapping,CppModuleMap=+supports-path-mapping,CppArchive=+supports-path-mapping \
@@ -754,6 +755,7 @@ EOF
   expect_not_log 'remote cache hit'
 
   bazel run \
+    --repo_env=CC=clang \
     --verbose_failures \
     --experimental_output_paths=strip \
     --modify_execution_info=CppCompile=+supports-path-mapping,CppModuleMap=+supports-path-mapping,CppArchive=+supports-path-mapping \