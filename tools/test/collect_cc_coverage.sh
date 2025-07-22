9ab83c2ff2a6c01d271f59d3e8baf82b69d69725
KrishnaM256 <magarkrishna2564@gmail.com>
2025-07-22 13:14:01 +0530

    corrected uses_gcov function and used alias with select

--- tools/test/collect_cc_coverage.sh
+++ tools/test/collect_cc_coverage.sh
@@ -186,9 +186,7 @@ function gcov_coverage() {
 }
 
 function main() {
-  if uses_gcov; then
-      init_gcov
-  fi
+  init_gcov
 
   # If llvm code coverage is used, we output the raw code coverage report in
   # the $COVERAGE_OUTPUT_FILE. This report will not be converted to any other
