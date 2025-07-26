36204aed2ffbbaaaff5201b3304b93a2ce3e3b47
Valentin Grigorev <valentin.grigorev@jetbrains.com>
2025-07-26 15:57:36 +0200

    Fix escaping of arguments passed to LaunchProcess() in JavaBinaryLauncher::CreateClasspathJar on Windows

--- src/tools/launcher/java_launcher.cc
+++ src/tools/launcher/java_launcher.cc
@@ -280,8 +280,8 @@ wstring JavaBinaryLauncher::CreateClasspathJar(const wstring& classpath) {
   wstring jar_bin = this->Rlocation(this->GetLaunchInfoByKey(JAR_BIN_PATH));
   vector<wstring> arguments;
   arguments.push_back(L"cvfm");
-  arguments.push_back(manifest_jar_path);
-  arguments.push_back(jar_manifest_file_path);
+  arguments.push_back(bazel::windows::WindowsEscapeArg(manifest_jar_path));
+  arguments.push_back(bazel::windows::WindowsEscapeArg(jar_manifest_file_path));
 
   if (this->LaunchProcess(jar_bin, arguments, /* suppressOutput */ true) != 0) {
     die(L"Couldn't create classpath jar: %s", manifest_jar_path.c_str());
