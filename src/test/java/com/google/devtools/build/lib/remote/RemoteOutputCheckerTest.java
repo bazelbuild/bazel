3d251755fd3eb7186a31c5bd6cc0e2d10cca54fc
Fabian Meumertzheim <fabian@meumertzhe.im>
2025-07-29 10:35:58 -0700

    Optimize `RemoteOutputChecker.addOutputToDownload`
    With `--remote_download_toplevel`, `RemoteOutputChecker#addOutputToDownload` is transitively called by `BuildDriverFunction`, which unconditionally runs for each top-level target on each (even ultimately no-op) build. It's applied to each top-level output as well as runfiles, making it a very hot code path.
    
    This change exploits the fact that artifact paths are never prefixes of each other to simplify and speed up the data structure that maintains the paths to download with BwoB. Instead of a trie built out of nested `ConcurrentHashMap`s, it uses a single `ConcurrentSkipListSet` to look up paths via `floor`. This avoids all string operations as well as a number of hash maps lookup linear in the number of segments of a path.
    
    The following benchmark results show the effect of the change on the Bazel repo itself (disabling as much I/O as possible to make the result less noisy):
    
    ```
    $ hyperfine --warmup 5 --runs 20 './bazel-before --nosystem_rc --nohome_rc build --nobuild_runfile_links --noexperimental_check_output_files --incompatible_strict_action_env --disk_cache=/Users/fmeum/bazel-disk //src/test/...'
    
      Time (mean ± σ):     512.4 ms ±  50.7 ms    [User: 13.6 ms, System: 12.3 ms]
      Range (min … max):   447.0 ms … 643.5 ms    20 runs
    
    $ hyperfine --warmup 5 --runs 20 './bazel-after --nosystem_rc --nohome_rc build --nobuild_runfile_links --noexperimental_check_output_files --incompatible_strict_action_env --disk_cache=/Users/fmeum/bazel-disk //src/test/...'
    
      Time (mean ± σ):     303.6 ms ±  36.9 ms    [User: 14.3 ms, System: 13.4 ms]
      Range (min … max):   270.0 ms … 383.6 ms    20 runs
    ```
    
    Closes #26639.
    
    PiperOrigin-RevId: 788514662
    Change-Id: Ida0fc99bc7ab741418189e04b9fd98ad944dc2b6
    
    Fix comparator used in ConcurrentArtifactPathTrie
    
    The default comparator for `PathFragment` may sort unrelated paths between a path and a child path, which results in `contains` behaving incorrectly as soon as paths such as `foo/bar` and `foo/bar-baz` are present in the set. This is a bug introduced by #26639.
    
    This is fixed by introducing a new `Comparator` for `PathFragment` that sorts `/` lower than any other character. This will also be used in future work on #21378 and is thus placed in `PathFragment`. It may become the default in the future, but not until Blaze has been confirmed to not rely on the order.
    
    Closes #26714.
    
    PiperOrigin-RevId: 792519419
    Change-Id: I8bf8fc6b15cc5bcf0d303057a828fbd644032bab
    
    Closes #26640

---
+++ src/test/java/com/google/devtools/build/lib/remote/RemoteOutputCheckerTest.java
@@ -0,0 +1,57 @@
+// Copyright 2025 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.devtools.build.lib.remote;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.collect.ImmutableList;
+import com.google.devtools.build.lib.actions.ArtifactRoot;
+import com.google.devtools.build.lib.actions.util.ActionsTestUtil;
+import com.google.devtools.build.lib.clock.JavaClock;
+import com.google.devtools.build.lib.remote.options.RemoteOutputsMode;
+import com.google.devtools.build.lib.vfs.DigestHashFunction;
+import com.google.devtools.build.lib.vfs.FileSystem;
+import com.google.devtools.build.lib.vfs.PathFragment;
+import com.google.devtools.build.lib.vfs.inmemoryfs.InMemoryFileSystem;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Tests for {@link RemoteOutputChecker} */
+@RunWith(JUnit4.class)
+public class RemoteOutputCheckerTest {
+  private final RemoteOutputChecker remoteOutputChecker =
+      new RemoteOutputChecker(
+          new JavaClock(), "build", RemoteOutputsMode.MINIMAL, ImmutableList.of());
+  private final FileSystem fs = new InMemoryFileSystem(DigestHashFunction.SHA256);
+  private final ArtifactRoot execRoot =
+      ArtifactRoot.asDerivedRoot(fs.getPath("/execroot"), ArtifactRoot.RootType.Output, "out");
+
+  @Test
+  public void testShouldDownloadOutput() {
+    remoteOutputChecker.addOutputToDownload(
+        ActionsTestUtil.createTreeArtifactWithGeneratingAction(execRoot, "foo/bar"));
+    remoteOutputChecker.addOutputToDownload(
+        ActionsTestUtil.createArtifact(execRoot, "foo/bar-baz"));
+    assertThat(remoteOutputChecker.shouldDownloadOutput(PathFragment.create("out/foo/bar-quz")))
+        .isFalse();
+    assertThat(remoteOutputChecker.shouldDownloadOutput(PathFragment.create("out/foo/bar")))
+        .isTrue();
+    assertThat(
+            remoteOutputChecker.shouldDownloadOutput(PathFragment.create("out/foo/bar/data.txt")))
+        .isTrue();
+    assertThat(remoteOutputChecker.shouldDownloadOutput(PathFragment.create("out/foo/bar-baz")))
+        .isTrue();
+  }
+}
