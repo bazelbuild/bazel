c22e4d056c07102eb73e817c4af6115c14c64e17
Krishna Magar <magarkrishna2564@gmail.com>
2025-08-26 01:13:38 +0530

    fix java coverage on windows

--- src/tools/launcher/java_launcher.cc
+++ src/tools/launcher/java_launcher.cc
@@ -49,6 +49,7 @@ static constexpr const char* JAR_BIN_PATH = "jar_bin_path";
 static constexpr const char* CLASSPATH = "classpath";
 static constexpr const char* JAVA_START_CLASS = "java_start_class";
 static constexpr const char* JVM_FLAGS = "jvm_flags";
+static constexpr const char* JACOCO_MAIN_CLASS = "jacoco_main_class";
 
 // Check if a string start with a certain prefix.
 // If it's true, store the substring without the prefix in value.
@@ -303,7 +304,10 @@ ExitCode JavaBinaryLauncher::Launch() {
     java_runfiles = this->GetRunfilesPath();
   }
   SetEnv(L"JAVA_RUNFILES", java_runfiles);
-
+  wstring jacoco_main_class = this->GetLaunchInfoByKeyOrEmpty(JACOCO_MAIN_CLASS);
+  if (!jacoco_main_class.empty()) {
+    SetEnv(L"JACOCO_MAIN_CLASS", jacoco_main_class);
+  }
   // Print Java binary path if needed
   wstring java_bin = this->Rlocation(this->GetLaunchInfoByKey(JAVA_BIN_PATH),
                                      /*has_workspace_name =*/true);
