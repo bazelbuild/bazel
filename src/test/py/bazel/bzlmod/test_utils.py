bazelbuild/bazel

commit c5a562b6b5f718b39b8211fa1cf47169cfedb300

Overlay the registry `MODULE.bazel` file on the module repo
RELNOTES: Modules backed by `http_archive` or `git_repository` no longer require a MODULE.bazel file to be contained in the source archive.

Fixes #26217

Closes #26332.

PiperOrigin-RevId: 778139801
Change-Id: Ie1d219ce1c219c8bae7657be929e54b9b467abf9

src/test/py/bazel/bzlmod/test_utils.py
@@ -227,8 +227,16 @@ class BazelRegistry:
     for foldername, _, filenames in os.walk(str(src_dir)):
       for filename in filenames:
         filepath = os.path.join(foldername, filename)
-        zip_obj.write(filepath,
-                      str(pathlib.Path(filepath).relative_to(src_dir)))
+        arcname = str(pathlib.Path(filepath).relative_to(src_dir))
+        if filename == 'MODULE.bazel':
+          # Verify that MODULE.bazel doesn't have to be included in the archive
+          # by writing an effectively empty one.
+          zip_obj.writestr(
+              arcname,
+              '# FAKE\nmodule(name = "%s", version = "%s")\n' % (name, version),
+          )
+        else:
+          zip_obj.write(filepath, arcname)
     zip_obj.close()
     return zip_path
 

src/main/java/com/google/devtools/build/lib/bazel/bzlmod/ArchiveRepoSpecBuilder.java
@@ -68,18 +68,6 @@ public class ArchiveRepoSpecBuilder {
   }
 
   @CanIgnoreReturnValue
-  public ArchiveRepoSpecBuilder setPatchCmds(ImmutableList<String> patchCmds) {
-    attrBuilder.put("patch_cmds", patchCmds);
-    return this;
-  }
-
-  @CanIgnoreReturnValue
-  public ArchiveRepoSpecBuilder setPatchStrip(int patchStrip) {
-    attrBuilder.put("patch_args", ImmutableList.of("-p" + patchStrip));
-    return this;
-  }
-
-  @CanIgnoreReturnValue
   public ArchiveRepoSpecBuilder setRemotePatches(ImmutableMap<String, String> remotePatches) {
     attrBuilder.put("remote_patches", remotePatches);
     return this;
@@ -95,6 +83,13 @@ public class ArchiveRepoSpecBuilder {
             .collect(toImmutableMap(Entry::getKey, e -> e.getValue().integrity()));
     attrBuilder.put("remote_file_urls", remoteFiles);
     attrBuilder.put("remote_file_integrity", remoteFilesIntegrity);
+    return this;
+  }
+
+  @CanIgnoreReturnValue
+  public ArchiveRepoSpecBuilder setRemoteModuleFile(RemoteFile remoteModuleFile) {
+    attrBuilder.put("remote_module_file_urls", remoteModuleFile.urls());
+    attrBuilder.put("remote_module_file_integrity", remoteModuleFile.integrity());
     return this;
   }
 

src/main/java/com/google/devtools/build/lib/bazel/bzlmod/GitRepoSpecBuilder.java
@@ -75,18 +75,11 @@ public class GitRepoSpecBuilder {
   }
 
   @CanIgnoreReturnValue
-  public GitRepoSpecBuilder setPatches(List<Label> patches) {
-    return setAttr("patches", patches);
-  }
-
-  @CanIgnoreReturnValue
-  public GitRepoSpecBuilder setPatchArgs(List<String> patchArgs) {
-    return setAttr("patch_args", patchArgs);
-  }
-
-  @CanIgnoreReturnValue
-  public GitRepoSpecBuilder setPatchCmds(List<String> patchCmds) {
-    return setAttr("patch_cmds", patchCmds);
+  public GitRepoSpecBuilder setRemoteModuleFile(
+      ArchiveRepoSpecBuilder.RemoteFile remoteModuleFile) {
+    attrBuilder.put("remote_module_file_urls", remoteModuleFile.urls());
+    attrBuilder.put("remote_module_file_integrity", remoteModuleFile.integrity());
+    return this;
   }
 
   public RepoSpec build() {

src/main/java/com/google/devtools/build/lib/bazel/bzlmod/IndexRegistry.java
@@ -22,6 +22,7 @@ import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.devtools.build.lib.bazel.bzlmod.ArchiveRepoSpecBuilder.RemoteFile;
 import com.google.devtools.build.lib.bazel.bzlmod.Version.ParseException;
 import com.google.devtools.build.lib.bazel.repository.downloader.Checksum;
 import com.google.devtools.build.lib.bazel.repository.downloader.Checksum.MissingChecksumException;
@@ -145,9 +146,10 @@ public class IndexRegistry implements Registry {
       maybeContent = Optional.of(doGrabFile(downloadManager, url, eventHandler, useChecksum));
       return maybeContent.get();
     } finally {
-      if ((knownFileHashesMode == KnownFileHashesMode.USE_AND_UPDATE
-              || knownFileHashesMode == KnownFileHashesMode.USE_IMMUTABLE_AND_UPDATE)
-          && useChecksum) {
+      // We intentionally don't check knownFileHashesMode here: The checksums of module files are
+      // always needed for the remote_module_file_integrity attributes of the http_archive backing
+      // the module repo.
+      if (useChecksum) {
         eventHandler.post(RegistryFileDownloadEvent.create(url, maybeContent));
       }
     }
@@ -240,10 +242,13 @@ public class IndexRegistry implements Registry {
   public ModuleFile getModuleFile(
       ModuleKey key, ExtendedEventHandler eventHandler, DownloadManager downloadManager)
       throws IOException, InterruptedException, NotFoundException {
-    String url =
-        constructUrl(getUrl(), "modules", key.name(), key.version().toString(), "MODULE.bazel");
+    String url = constructModuleFileUrl(key);
     byte[] content = grabFile(url, eventHandler, downloadManager, /* useChecksum= */ true);
     return ModuleFile.create(content, url);
+  }
+
+  private String constructModuleFileUrl(ModuleKey key) {
+    return constructUrl(getUrl(), "modules", key.name(), key.version().toString(), "MODULE.bazel");
   }
 
   /** Represents fields available in {@code bazel_registry.json} for the registry. */
@@ -333,7 +338,10 @@ public class IndexRegistry implements Registry {
 
   @Override
   public RepoSpec getRepoSpec(
-      ModuleKey key, ExtendedEventHandler eventHandler, DownloadManager downloadManager)
+      ModuleKey key,
+      ImmutableMap<String, Optional<Checksum>> moduleFileHashes,
+      ExtendedEventHandler eventHandler,
+      DownloadManager downloadManager)
       throws IOException, InterruptedException {
     String jsonUrl = getSourceJsonUrl(key);
     Optional<String> jsonString =
@@ -348,8 +356,14 @@ public class IndexRegistry implements Registry {
       case "archive" -> {
         ArchiveSourceJson typedSourceJson =
             parseJson(jsonString.get(), jsonUrl, ArchiveSourceJson.class);
+        var moduleFileUrl = constructModuleFileUrl(key);
+        var moduleFileChecksum = moduleFileHashes.get(moduleFileUrl).get();
         return createArchiveRepoSpec(
-            typedSourceJson, getBazelRegistryJson(eventHandler, downloadManager), key);
+            typedSourceJson,
+            moduleFileUrl,
+            moduleFileChecksum,
+            getBazelRegistryJson(eventHandler, downloadManager),
+            key);
       }
       case "local_path" -> {
         LocalPathSourceJson typedSourceJson =
@@ -360,7 +374,9 @@ public class IndexRegistry implements Registry {
       case "git_repository" -> {
         GitRepoSourceJson typedSourceJson =
             parseJson(jsonString.get(), jsonUrl, GitRepoSourceJson.class);
-        return createGitRepoSpec(typedSourceJson);
+        var moduleFileUrl = constructModuleFileUrl(key);
+        var moduleFileChecksum = moduleFileHashes.get(moduleFileUrl).get();
+        return createGitRepoSpec(typedSourceJson, moduleFileUrl, moduleFileChecksum);
       }
       default ->
           throw new IOException(
@@ -424,7 +440,11 @@ public class IndexRegistry implements Registry {
   }
 
   private RepoSpec createArchiveRepoSpec(
-      ArchiveSourceJson sourceJson, Optional<BazelRegistryJson> bazelRegistryJson, ModuleKey key)
+      ArchiveSourceJson sourceJson,
+      String moduleFileUrl,
+      Checksum moduleFileChecksum,
+      Optional<BazelRegistryJson> bazelRegistryJson,
+      ModuleKey key)
       throws IOException {
     URL sourceUrl = sourceJson.url;
     if (sourceUrl == null) {
@@ -498,12 +518,16 @@ public class IndexRegistry implements Registry {
         .setStripPrefix(Strings.nullToEmpty(sourceJson.stripPrefix))
         .setRemotePatches(remotePatches.buildOrThrow())
         .setOverlay(overlay)
+        .setRemoteModuleFile(
+            new RemoteFile(
+                moduleFileChecksum.toSubresourceIntegrity(), ImmutableList.of(moduleFileUrl)))
         .setRemotePatchStrip(sourceJson.patchStrip)
         .setArchiveType(sourceJson.archiveType)
         .build();
   }
 
-  private RepoSpec createGitRepoSpec(GitRepoSourceJson sourceJson) {
+  private RepoSpec createGitRepoSpec(
+      GitRepoSourceJson sourceJson, String moduleFileUrl, Checksum moduleFileChecksum) {
     return new GitRepoSpecBuilder()
         .setRemote(sourceJson.remote)
         .setCommit(sourceJson.commit)
@@ -512,6 +536,9 @@ public class IndexRegistry implements Registry {
         .setInitSubmodules(sourceJson.initSubmodules)
         .setVerbose(sourceJson.verbose)
         .setStripPrefix(sourceJson.stripPrefix)
+        .setRemoteModuleFile(
+            new RemoteFile(
+                moduleFileChecksum.toSubresourceIntegrity(), ImmutableList.of(moduleFileUrl)))
         .build();
   }
 

src/main/java/com/google/devtools/build/lib/bazel/bzlmod/Registry.java
@@ -16,6 +16,7 @@
 package com.google.devtools.build.lib.bazel.bzlmod;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.devtools.build.lib.bazel.repository.downloader.Checksum;
 import com.google.devtools.build.lib.bazel.repository.downloader.DownloadManager;
 import com.google.devtools.build.lib.events.ExtendedEventHandler;
 import com.google.devtools.build.skyframe.NotComparableSkyValue;
@@ -50,7 +51,10 @@ public interface Registry extends NotComparableSkyValue {
    * by {@code key} should be materialized as a repo.
    */
   RepoSpec getRepoSpec(
-      ModuleKey key, ExtendedEventHandler eventHandler, DownloadManager downloadManager)
+      ModuleKey key,
+      ImmutableMap<String, Optional<Checksum>> moduleFileHashes,
+      ExtendedEventHandler eventHandler,
+      DownloadManager downloadManager)
       throws IOException, InterruptedException;
 
   /**

src/main/java/com/google/devtools/build/lib/bazel/bzlmod/RegistryFileDownloadEvent.java
@@ -34,8 +34,8 @@ public record RegistryFileDownloadEvent(String uri, Optional<Checksum> checksum)
   static ImmutableMap<String, Optional<Checksum>> collectToMap(Collection<Postable> postables) {
     ImmutableMap.Builder<String, Optional<Checksum>> builder = ImmutableMap.builder();
     for (Postable postable : postables) {
-      if (postable instanceof RegistryFileDownloadEvent event) {
-        builder.put(event.uri(), event.checksum());
+      if (postable instanceof RegistryFileDownloadEvent(String uri, Optional<Checksum> checksum)) {
+        builder.put(uri, checksum);
       }
     }
     return builder.buildKeepingLast();

src/main/java/com/google/devtools/build/lib/bazel/bzlmod/RepoSpecFunction.java
@@ -45,13 +45,23 @@ public class RepoSpecFunction implements SkyFunction {
     if (registry == null) {
       return null;
     }
+    ModuleFileValue moduleFileValue =
+        (ModuleFileValue) env.getValue(ModuleFileValue.key(key.moduleKey()));
+    if (moduleFileValue == null) {
+      return null;
+    }
 
     StoredEventHandler downloadEvents = new StoredEventHandler();
     RepoSpec repoSpec;
     try (SilentCloseable c =
         Profiler.instance()
             .profile(ProfilerTask.BZLMOD, () -> "compute repo spec: " + key.moduleKey())) {
-      repoSpec = registry.getRepoSpec(key.moduleKey(), downloadEvents, this.downloadManager);
+      repoSpec =
+          registry.getRepoSpec(
+              key.moduleKey(),
+              moduleFileValue.registryFileHashes(),
+              downloadEvents,
+              this.downloadManager);
     } catch (IOException e) {
       throw new RepoSpecException(
           ExternalDepsException.withCauseAndMessage(

src/test/java/com/google/devtools/build/lib/bazel/bzlmod/FakeRegistry.java
@@ -80,7 +80,10 @@ public class FakeRegistry implements Registry {
 
   @Override
   public RepoSpec getRepoSpec(
-      ModuleKey key, ExtendedEventHandler eventHandler, DownloadManager downloadManager) {
+      ModuleKey key,
+      ImmutableMap<String, Optional<Checksum>> moduleFileHashes,
+      ExtendedEventHandler eventHandler,
+      DownloadManager downloadManager) {
     RepoSpec repoSpec =
         LocalPathRepoSpecs.create(rootPath + "/" + key.getCanonicalRepoNameWithVersion().getName());
     eventHandler.post(

commit 9d961b96f5d601b8fd743dffadc313e6b407c1d1

Don't store local registry hashes in the lockfile
This regressed in c5a562b6b5f718b39b8211fa1cf47169cfedb300 since the relevant test had become ineffective when it was incorrectly ported to HTTP registries.

Fixes #26835

Closes #26841.

PiperOrigin-RevId: 800357483
Change-Id: I86e3a4927cc97fb44ac8b5ceb205a6d738a0de6d

src/main/java/com/google/devtools/build/lib/bazel/bzlmod/BazelLockFileModule.java
@@ -146,13 +146,23 @@ public class BazelLockFileModule extends BlazeModule {
                       /* hasUsages= */ depGraphValue.getExtensionUsagesTable()::containsRow,
                       /* reproducible= */ false);
 
+              // Bazel may track the hashes of files fetched from local registries for internal
+              // purposes, but those should never show up in the lockfile for two reasons:
+              // - they are not needed for reproducibility, as local registries are assumed to be
+              //   under the user's control, just like CLI flags;
+              // - they would contribute absolute paths and thus aren't portable.
+              var remoteRegistryFileHashes =
+                  ImmutableSortedMap.copyOf(
+                      Maps.filterKeys(
+                          moduleResolutionValue.getRegistryFileHashes(),
+                          url -> !url.startsWith("file:")));
+
               // Create an updated version of the lockfile, keeping only the extension results from
               // the old lockfile that are still up-to-date and adding the newly resolved
               // extension results, as long as any of them are not known to be reproducible.
               BazelLockFileValue newLockfile =
                   BazelLockFileValue.builder()
-                      .setRegistryFileHashes(
-                          ImmutableSortedMap.copyOf(moduleResolutionValue.getRegistryFileHashes()))
+                      .setRegistryFileHashes(remoteRegistryFileHashes)
                       .setSelectedYankedVersions(moduleResolutionValue.getSelectedYankedVersions())
                       .setModuleExtensions(notReproducibleExtensionInfos)
                       .build();

src/test/py/bazel/bzlmod/bazel_lockfile_test.py
@@ -1610,62 +1610,68 @@ class BazelLockfileTest(test_base.TestBase):
 
   def testLockfileWithNoUserSpecificPath(self):
     self.my_registry = BazelRegistry(os.path.join(self._test_cwd, 'registry'))
-    self.my_registry.start()
-    try:
-      self.my_registry.setModuleBasePath('projects')
-      patch_file = self.ScratchFile(
-          'ss.patch',
-          [
-              '--- a/aaa.cc',
-              '+++ b/aaa.cc',
-              '@@ -1,6 +1,6 @@',
-              ' #include <stdio.h>',
-              ' #include "aaa.h"',
-              ' void hello_aaa(const std::string& caller) {',
-              '-    std::string lib_name = "aaa@1.1-1";',
-              '+    std::string lib_name = "aaa@1.1-1 (remotely patched)";',
-              '     printf("%s => %s\\n", caller.c_str(), lib_name.c_str());',
-              ' }',
-          ],
-      )
-      # Module with a local patch & extension
-      self.my_registry.createCcModule(
-          'ss',
-          '1.3-1',
-          {'ext': '1.0'},
-          patches=[patch_file],
-          patch_strip=1,
-          extra_module_file_contents=[
-              'my_ext = use_extension("@ext//:ext.bzl", "ext")',
-              'use_repo(my_ext, "justRepo")',
-          ],
-      )
-      ext_src = [
-          'def _repo_impl(ctx): ctx.file("BUILD")',
-          'repo = repository_rule(_repo_impl)',
-          'def _ext_impl(ctx): repo(name=justRepo)',
-          'ext=module_extension(_ext_impl)',
-      ]
-      self.my_registry.createLocalPathModule('ext', '1.0', 'ext')
-      scratchFile(self.my_registry.projects.joinpath('ext', 'BUILD'))
-      scratchFile(self.my_registry.projects.joinpath('ext', 'ext.bzl'), ext_src)
+    self.my_registry.setModuleBasePath('projects')
+    patch_file = self.ScratchFile(
+        'ss.patch',
+        [
+            '--- a/aaa.cc',
+            '+++ b/aaa.cc',
+            '@@ -1,6 +1,6 @@',
+            ' #include <stdio.h>',
+            ' #include "aaa.h"',
+            ' void hello_aaa(const std::string& caller) {',
+            '-    std::string lib_name = "aaa@1.1-1";',
+            '+    std::string lib_name = "aaa@1.1-1 (remotely patched)";',
+            '     printf("%s => %s\\n", caller.c_str(), lib_name.c_str());',
+            ' }',
+        ],
+    )
+    overlay_file = self.ScratchFile(
+        'ss.overlay',
+        [
+            'def _repo_impl(ctx): ctx.file("BUILD")',
+            'repo = repository_rule(_repo_impl)',
+            'def _ext_impl(ctx): repo(name=justRepo)',
+            'ext=module_extension(_ext_impl)',
+        ],
+    )
+    # Module with a local patch, overlay & extension
+    self.my_registry.createCcModule(
+        'ss',
+        '1.3-1',
+        {'ext': '1.0'},
+        patches=[patch_file],
+        patch_strip=1,
+        overlay={
+            'ext.bzl': overlay_file,
+        },
+        extra_module_file_contents=[
+            'my_ext = use_extension("@ext//:ext.bzl", "ext")',
+            'use_repo(my_ext, "justRepo")',
+        ],
+    )
+    self.my_registry.createLocalPathModule('ext', '1.0', 'ext')
+    scratchFile(self.my_registry.projects.joinpath('ext', 'BUILD'))
 
-      self.ScratchFile(
-          'MODULE.bazel',
-          [
-              'bazel_dep(name = "ss", version = "1.3-1")',
-          ],
-      )
-      self.ScratchFile('BUILD.bazel', ['filegroup(name = "lala")'])
-      self.RunBazel(
-          ['build', '--registry=file:///%workspace%/registry', '//:lala']
-      )
+    self.ScratchFile(
+        'MODULE.bazel',
+        [
+            'bazel_dep(name = "ss", version = "1.3-1")',
+        ],
+    )
+    self.ScratchFile('BUILD.bazel', ['filegroup(name = "lala")'])
+    self.RunBazel(
+        ['build', '--registry=' + self.my_registry.getLocalURL(), '//:lala']
+    )
 
-      with open('MODULE.bazel.lock', 'r') as f:
-        self.assertNotIn(self.my_registry.getURL(), f.read())
-
-    finally:
-      self.my_registry.stop()
+    with open('MODULE.bazel.lock', 'r') as f:
+      module_file = f.read()
+      # Ensure no user-specific path is in the lockfile, but only check the last
+      # two segments to avoid false negatives caused by minor differences in the
+      # absolute path segment (e.g. casing or symbolic links).
+      workspace_basename = pathlib.Path(self._test_cwd).name
+      forbidden_path = pathlib.Path(workspace_basename, 'registry')
+      self.assertNotIn(str(forbidden_path), module_file)
 
   def testExtensionEvaluationRerunsIfDepGraphOrderChanges(self):
     self.ScratchFile(

src/test/py/bazel/bzlmod/test_utils.py
@@ -66,6 +66,7 @@ class Module:
     self.strip_prefix = ''
     self.module_dot_bazel = None
     self.patches = []
+    self.overlay = {}
     self.patch_strip = 0
     self.archive_type = None
 
@@ -81,6 +82,10 @@ class Module:
   def set_patches(self, patches, patch_strip):
     self.patches = patches
     self.patch_strip = patch_strip
+    return self
+
+  def set_overlay(self, overlay):
+    self.overlay = overlay
     return self
 
   def set_archive_type(self, archive_type):
@@ -120,6 +125,10 @@ class BazelRegistry:
   def getURL(self):
     """Return the URL of this registry."""
     return self.http_server.getURL()
+
+  def getLocalURL(self):
+    """Return the local file:// URL of this registry."""
+    return self.root.as_uri()
 
   def generateCcSource(
       self,
@@ -267,6 +276,15 @@ class BazelRegistry:
         source['patches'][patch.name] = integrity(read(patch))
         shutil.copy(str(patch), str(patch_dir))
 
+    if module.overlay:
+      overlay_dir = module_dir.joinpath('overlay')
+      overlay_dir.mkdir()
+      source['overlay'] = {}
+      for overlay_rel_path, overlay_file in module.overlay.items():
+        file = pathlib.Path(overlay_file)
+        source['overlay'][overlay_rel_path] = integrity(read(file))
+        shutil.copy(str(file), str(overlay_dir.joinpath(overlay_rel_path)))
+
     if module.archive_type:
       source['archive_type'] = module.archive_type
 
@@ -281,6 +299,7 @@ class BazelRegistry:
       repo_names=None,
       patches=None,
       patch_strip=0,
+      overlay=None,
       archive_pattern=None,
       archive_type=None,
       extra_module_file_contents=None,
@@ -300,6 +319,8 @@ class BazelRegistry:
     module.set_module_dot_bazel(src_dir.joinpath('MODULE.bazel'))
     if patches:
       module.set_patches(patches, patch_strip)
+    if overlay:
+      module.set_overlay(overlay)
     if archive_type:
       module.set_archive_type(archive_type)
 