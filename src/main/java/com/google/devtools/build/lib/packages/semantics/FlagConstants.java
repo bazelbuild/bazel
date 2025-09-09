0522f8e58bf864005207eb8f09162d35ddd1f678
Ivo List <ilist@google.com>
2025-09-09 06:41:35 -0700

    Disable autoloads for Python, Java, Proto, Shell and Android
    For Bazel 9 release, all the loads should be explicitly added to the repositories.
    
    To temporary reenable pass in flag:
    ```
      --incompatible_autoload_externally=@rules_python,+java_common,+JavaInfo,+JavaPluginInfo,ProguardSpecProvider,java_binary,java_import,java_library,java_plugin,java_test,java_runtime,java_toolchain,java_package_configuration,@com_google_protobuf,@rules_shell,@rules_android,+@rules_cc
    ```
    
    Downstream test: https://buildkite.com/bazel/bazel-at-head-plus-downstream/builds/4929
    
    New failures:
    - Bazelisk
    
    Upstream fixes:
    https://github.com/bazelbuild/bazel-central-registry/pull/5818
    
    RELNOTES[INC]: autoloads are disabled, issue https://github.com/bazelbuild/bazel/issues/23043
    
    Closes #26906.
    
    PiperOrigin-RevId: 804886816
    Change-Id: I36b2636926351e3d07bab3921e25ed9d4eeb6ff2

--- src/main/java/com/google/devtools/build/lib/packages/semantics/FlagConstants.java
+++ src/main/java/com/google/devtools/build/lib/packages/semantics/FlagConstants.java
@@ -26,13 +26,6 @@ class FlagConstants {
 
   // TODO - ilist@: once Java providers are removed, the whole line can be compressed to "@rules_java"
   public static final String DEFAULT_INCOMPATIBLE_AUTOLOAD_EXTERNALLY =
-      "@rules_python," +
-      "+java_common,+JavaInfo,+JavaPluginInfo,ProguardSpecProvider," +
-      "java_binary,java_import,java_library,java_plugin,java_test," +
-      "java_runtime,java_toolchain,java_package_configuration," +
-      "@com_google_protobuf," +
-      "@rules_shell," +
-      "@rules_android," +
       "+@rules_cc";
 
   public static final String DEFAULT_EXPERIMENTAL_STARLARK_TYPES = "false";
