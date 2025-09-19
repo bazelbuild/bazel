08db044daf53e20ab7e8e8a040c48673b251a990
Fabian Meumertzheim <fabian@meumertzhe.im>
2025-09-19 16:36:20 +0200

    Add test

--- src/test/py/bazel/bzlmod/repo_contents_cache_test.py
+++ src/test/py/bazel/bzlmod/repo_contents_cache_test.py
@@ -14,7 +14,9 @@
 # limitations under the License.
 # pylint: disable=g-long-ternary
 
+import json
 import os
+import shutil
 import tempfile
 import time
 
@@ -34,8 +36,12 @@ class RepoContentsCacheTest(test_base.TestBase):
         [
             'build --verbose_failures',
             'common --repo_contents_cache=%s' % self.repo_contents_cache,
-        ],
+            'common --experimental_check_external_repository_files=%s' % str(self.checkExternalRepositoryFiles()).lower(),
+        ]
     )
+
+  def checkExternalRepositoryFiles(self):
+    return os.getenv('CHECK_EXTERNAL_REPOSITORY_FILES') == 'True'
 
   def hasCacheEntry(self):
     for l1 in os.listdir(self.repo_contents_cache):
@@ -53,6 +59,18 @@ class RepoContentsCacheTest(test_base.TestBase):
         return
       time.sleep(0.5)
     self.fail('repo contents cache still not empty after 5 seconds')
+
+  def repoDir(self, repo_name, cwd=None):
+    _, stdout, _ = self.RunBazel(['info', 'output_base'], cwd=cwd)
+    self.assertLen(stdout, 1)
+    output_base = stdout[0].strip()
+
+    _, stdout, _ = self.RunBazel(['mod', 'dump_repo_mapping', ''], cwd=cwd)
+    self.assertLen(stdout, 1)
+    mapping = json.loads(stdout[0])
+    canonical_repo_name = mapping[repo_name]
+
+    return output_base + '/external/' + canonical_repo_name
 
   def testCachedAfterCleanExpunge(self):
     self.ScratchFile(
@@ -76,6 +94,12 @@ class RepoContentsCacheTest(test_base.TestBase):
     # First fetch: not cached
     _, _, stderr = self.RunBazel(['build', '@my_repo//:haha'])
     self.assertIn('JUST FETCHED', '\n'.join(stderr))
+    # Verify that the repo directory under the output base is a symlink into
+    # the repo contents cache.
+    repo_dir = self.repoDir('my_repo')
+    self.assertTrue(os.path.islink(repo_dir))
+    target_path = os.readlink(repo_dir)
+    self.assertTrue(target_path.startswith(self.repo_contents_cache))
 
     # After expunging: cached
     self.RunBazel(['clean', '--expunge'])
@@ -295,7 +319,9 @@ class RepoContentsCacheTest(test_base.TestBase):
     # GC'd while server is alive: not cached, but also no crash
     self.sleepUntilCacheEmpty()
     _, _, stderr = self.RunBazel(['build', '@my_repo//:haha'])
-    self.assertIn('JUST FETCHED', '\n'.join(stderr))
+    stderr = '\n'.join(stderr)
+    self.assertIn('JUST FETCHED', stderr)
+    self.assertNotIn('WARNING', stderr)
 
   def testGc_singleServer_gcAfterCacheMiss(self):
     self.ScratchFile(
@@ -328,7 +354,9 @@ class RepoContentsCacheTest(test_base.TestBase):
     # GC'd while server is alive: not cached, but also no crash
     self.sleepUntilCacheEmpty()
     _, _, stderr = self.RunBazel(['build', '@my_repo//:haha'])
-    self.assertIn('JUST FETCHED', '\n'.join(stderr))
+    stderr = '\n'.join(stderr)
+    self.assertIn('JUST FETCHED', stderr)
+    self.assertNotIn('WARNING', stderr)
 
   def testGc_multipleServers(self):
     module_bazel_lines = [
@@ -383,13 +411,17 @@ class RepoContentsCacheTest(test_base.TestBase):
         ],
         cwd=dir_a,
     )
-    self.assertIn('JUST FETCHED', '\n'.join(stderr))
+    stderr = '\n'.join(stderr)
+    self.assertIn('JUST FETCHED', stderr)
+    self.assertNotIn('WARNING', stderr)
 
     # GC'd while B's server is alive (after B's earlier cache hit):
     # not cached, but also no crash
     self.sleepUntilCacheEmpty()
     _, _, stderr = self.RunBazel(['build', '@my_repo//:haha'], cwd=dir_b)
-    self.assertIn('JUST FETCHED', '\n'.join(stderr))
+    stderr = '\n'.join(stderr)
+    self.assertIn('JUST FETCHED', stderr)
+    self.assertNotIn('WARNING', stderr)
 
   def testReverseDependencyDirection(self):
     # Set up two repos that retain their predeclared input hashes across two
@@ -451,6 +483,107 @@ class RepoContentsCacheTest(test_base.TestBase):
     self.assertIn('.-> @@+repo+foo', stderr)
     self.assertIn('|   @@+repo+bar', stderr)
     self.assertIn('`-- @@+repo+foo', stderr)
+
+  def testRepoContentsCacheDeleted(self):
+    repo_contents_cache = self.ScratchDir('repo_contents_cache')
+    workspace = self.ScratchDir('workspace')
+
+    self.ScratchFile(
+      'workspace/MODULE.bazel',
+      [
+        'repo = use_repo_rule("//:repo.bzl", "repo")',
+        'repo(name = "my_repo")',
+      ],
+    )
+    self.ScratchFile('workspace/BUILD.bazel')
+    self.ScratchFile(
+      'workspace/repo.bzl',
+      [
+        'def _repo_impl(rctx):',
+        '  rctx.file("BUILD", "filegroup(name=\'haha\')")',
+        '  print("JUST FETCHED")',
+        '  return rctx.repo_metadata(reproducible=True)',
+        'repo = repository_rule(_repo_impl)',
+      ],
+    )
+    # First fetch: not cached
+    _, _, stderr = self.RunBazel(
+      ['build', '@my_repo//:haha', '--repo_contents_cache=%s' % repo_contents_cache],
+      cwd=workspace,
+    )
+    self.assertIn('JUST FETCHED', '\n'.join(stderr))
+
+    # Second fetch: cached
+    _, _, stderr = self.RunBazel(
+      ['build', '@my_repo//:haha', '--repo_contents_cache=%s' % repo_contents_cache],
+      cwd=workspace,
+    )
+    self.assertNotIn('JUST FETCHED', '\n'.join(stderr))
+
+    # Delete the entire repo contents cache and fetch again: not cached
+    shutil.rmtree(repo_contents_cache)
+    _, _, stderr = self.RunBazel(
+      ['build', '@my_repo//:haha', '--repo_contents_cache=%s' % repo_contents_cache],
+      cwd=workspace,
+    )
+    stderr = '\n'.join(stderr)
+    self.assertIn('JUST FETCHED', stderr)
+    self.assertNotIn('WARNING', stderr)
+
+  def testEntryCorrupted(self):
+    self.ScratchFile(
+      'MODULE.bazel',
+      [
+        'repo = use_repo_rule("//:repo.bzl", "repo")',
+        'repo(name = "my_repo")',
+      ],
+    )
+    self.ScratchFile('BUILD.bazel')
+    self.ScratchFile(
+      'repo.bzl',
+      [
+        'def _repo_impl(rctx):',
+        '  rctx.file("BUILD", "filegroup(name=\'haha\')")',
+        '  print("JUST FETCHED")',
+        '  return rctx.repo_metadata(reproducible=True)',
+        'repo = repository_rule(_repo_impl)',
+      ],
+    )
+    # First fetch: not cached
+    _, _, stderr = self.RunBazel(['build', '@my_repo//:haha'])
+    self.assertIn('JUST FETCHED', '\n'.join(stderr))
+
+    # After expunging: cached
+    self.RunBazel(['clean', '--expunge'])
+    _, _, stderr = self.RunBazel(['build', '@my_repo//:haha'])
+    self.assertNotIn('JUST FETCHED', '\n'.join(stderr))
+
+    # Corrupt the only cache entry
+    found = False
+    for l1 in os.listdir(self.repo_contents_cache):
+      l1_path = os.path.join(self.repo_contents_cache, l1)
+      if os.path.isdir(l1_path) and 'my_repo' in l1:
+        for l2 in os.listdir(l1_path):
+          if not l2.endswith('.recorded_inputs'):
+            with open(os.path.join(l1_path, l2, 'BUILD'), 'w') as f:
+              f.write('filegroup(name="corrupted")\n')
+            found = True
+    self.assertTrue(found, 'failed to find cache entry to corrupt')
+
+    _, _, stderr = self.RunBazel(['build', '@my_repo//:haha'])
+    stderr = '\n'.join(stderr)
+    if self.checkExternalRepositoryFiles():
+      # Modification is detected and triggers a refetch with a warning.
+      self.assertIn('JUST FETCHED', stderr)
+      self.assertIn('WARNING: Repository \'@@+repo+my_repo\' will be fetched again since the file', stderr)
+    else:
+      # Modification is not detected while the server is running.
+      self.assertNotIn('JUST FETCHED', stderr)
+      self.assertNotIn('WARNING', stderr)
+
+      # Modification is picked up after server restart.
+      self.RunBazel(['shutdown'])
+      self.RunBazel(['build', '@my_repo//:corrupted'])
 
 
 if __name__ == '__main__':
