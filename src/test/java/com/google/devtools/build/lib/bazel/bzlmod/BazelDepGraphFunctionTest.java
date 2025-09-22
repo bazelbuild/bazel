a88ab64ba4ecfb548690b0ee502872912d669eef
Ara Nguyen <aranguyen@google.com>
2025-09-22 16:03:46 +0000

    fix test

--- src/test/java/com/google/devtools/build/lib/bazel/bzlmod/BazelDepGraphFunctionTest.java
+++ src/test/java/com/google/devtools/build/lib/bazel/bzlmod/BazelDepGraphFunctionTest.java
@@ -173,19 +173,19 @@ public class BazelDepGraphFunctionTest extends FoundationTestCase {
                     .addDep("my_dep_1", createModuleKey("dep", "1.0"))
                     .addDep("my_dep_2", createModuleKey("dep", "2.0"))
                     .addDep("rules_cc", createModuleKey("rules_cc", "1.0"))
-                    .setFlagAliases(ImmutableList.of())
+                    .setFlagAliases(ImmutableMap.of())
                     .build())
             .put(
                 createModuleKey("dep", "1.0"),
                 buildModule("dep", "1.0")
                     .addDep("rules_java", createModuleKey("rules_java", ""))
-                    .setFlagAliases(ImmutableList.of())
+                    .setFlagAliases(ImmutableMap.of())
                     .build())
-            .put(createModuleKey("dep", "2.0"), buildModule("dep", "2.0").setFlagAliases(ImmutableList.of()).build())
-            .put(createModuleKey("rules_cc", "1.0"), buildModule("rules_cc", "1.0").setFlagAliases(ImmutableList.of()).build())
+            .put(createModuleKey("dep", "2.0"), buildModule("dep", "2.0").setFlagAliases(ImmutableMap.of()).build())
+            .put(createModuleKey("rules_cc", "1.0"), buildModule("rules_cc", "1.0").setFlagAliases(ImmutableMap.of()).build())
             .put(
                 createModuleKey("rules_java", ""),
-                buildModule("rules_java", "1.0").setKey(createModuleKey("rules_java", "")).setFlagAliases(ImmutableList.of()).build())
+                buildModule("rules_java", "1.0").setKey(createModuleKey("rules_java", "")).setFlagAliases(ImmutableMap.of()).build())
             .buildOrThrow();
 
     resolutionFunctionMock.setDepGraph(depGraph);
@@ -246,7 +246,7 @@ public class BazelDepGraphFunctionTest extends FoundationTestCase {
                 createModuleExtensionUsage("@rje//:defs.bzl", "maven", "av", "autovalue"))
             .addExtensionUsage(
                 createModuleExtensionUsage("@rpy//:defs.bzl", "pip", "numpy", "numpy"))
-            .setFlagAliases(ImmutableList.of())
+            .setFlagAliases(ImmutableMap.of())
             .build();
     ModuleKey depKey = createModuleKey("dep", "2.0");
     Module dep =
@@ -259,7 +259,7 @@ public class BazelDepGraphFunctionTest extends FoundationTestCase {
                 createModuleExtensionUsage("//:defs.bzl", "myext", "oneext", "myext"))
             .addExtensionUsage(
                 createModuleExtensionUsage("//incredible:conflict.bzl", "myext", "twoext", "myext"))
-            .setFlagAliases(ImmutableList.of())
+            .setFlagAliases(ImmutableMap.of())
             .build();
     ImmutableMap<ModuleKey, Module> depGraph =
         ImmutableMap.of(
@@ -268,9 +268,9 @@ public class BazelDepGraphFunctionTest extends FoundationTestCase {
             depKey,
             dep,
             rjeKey,
-            buildModule("rules_jvm_external", "1.0").setKey(rjeKey).setFlagAliases(ImmutableList.of()).build(),
+            buildModule("rules_jvm_external", "1.0").setKey(rjeKey).setFlagAliases(ImmutableMap.of()).build(),
             rpyKey,
-            buildModule("rules_python", "2.0").setKey(rpyKey).setFlagAliases(ImmutableList.of()).build());
+            buildModule("rules_python", "2.0").setKey(rpyKey).setFlagAliases(ImmutableMap.of()).build());
 
     ModuleExtensionId maven =
         ModuleExtensionId.create(
@@ -350,7 +350,7 @@ public class BazelDepGraphFunctionTest extends FoundationTestCase {
         buildModule("module", "1.0")
             .setKey(ModuleKey.ROOT)
             .addExtensionUsage(createModuleExtensionUsage("@foo//:defs.bzl", "bar"))
-            .setFlagAliases(ImmutableList.of())
+            .setFlagAliases(ImmutableMap.of())
             .build();
     ImmutableMap<ModuleKey, Module> depGraph = ImmutableMap.of(ModuleKey.ROOT, root);
 
