3cd9ab62809848d4896f01626403c577ccfa58b5
Fabian Meumertzheim <fabian@meumertzhe.im>
2025-10-01 08:03:12 +0200

    Address most comments from 2nd review

--- src/main/java/com/google/devtools/build/lib/remote/merkletree/MerkleTreeComputer.java
+++ src/main/java/com/google/devtools/build/lib/remote/merkletree/MerkleTreeComputer.java
@@ -21,16 +21,13 @@ import build.bazel.remote.execution.v2.NodeProperty;
 import com.github.benmanes.caffeine.cache.AsyncCache;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
-import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSortedMap;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Iterators;
 import com.google.common.collect.Lists;
-import com.google.common.util.concurrent.ListenableFuture;
 import com.google.devtools.build.lib.actions.ActionInput;
 import com.google.devtools.build.lib.actions.ActionInputHelper;
 import com.google.devtools.build.lib.actions.Artifact;
@@ -67,10 +64,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Deque;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -123,7 +118,7 @@ public final class MerkleTreeComputer {
       NodeProperties.newBuilder()
           .addProperties(NodeProperty.newBuilder().setName("bazel_tool_input"))
           .build();
-  private static final List<Map.Entry<PathFragment, ActionInput>> END_OF_INPUTS_SENTINEL =
+  private static final ImmutableList<Map.Entry<PathFragment, ActionInput>> END_OF_INPUTS_SENTINEL =
       ImmutableList.of(entry(PathFragment.EMPTY_FRAGMENT, VirtualActionInput.EMPTY_MARKER));
   private static final PathFragment ROOT_FAKE_PATH_SEGMENT = PathFragment.create("root");
 
@@ -144,7 +139,9 @@ public final class MerkleTreeComputer {
   private static final Cache<FileArtifactValue, MerkleTree.RootOnly> persistentNonToolSubTreeCache =
       Caffeine.newBuilder().weakKeys().build();
 
-  @Nullable private static volatile Scrubber lastScrubber;
+  // @GuardedBy("MerkleTreeComputer.class") for writes, reads use double-checked locking
+  @Nullable
+  private static volatile Scrubber lastScrubber;
 
   private final DigestUtil digestUtil;
   @Nullable private final MerkleTreeUploader remoteExecutionCache;
@@ -170,138 +167,6 @@ public final class MerkleTreeComputer {
         new MerkleTree.Uploadable(
             new MerkleTree.RootOnly.BlobsUploaded(emptyDigest, 0, 0),
             ImmutableMap.of(emptyDigest, emptyBlob));
-  }
-
-  /**
-   * A representation of the inputs to a remotely executed action represented as a Merkle tree.
-   *
-   * <p>Every tree has a digest, which is the digest of the tree's root directory. The subtrees and
-   * the blobs they contain may have been discarded or never computed in the first place, for
-   * example, because they have already been uploaded to the remote cache or because the tree is
-   * being built only to check for a remote cache hit.
-   */
-  public sealed interface MerkleTree {
-    /** The digest of the tree's root directory. */
-    Digest digest();
-
-    /** The total number of regular files and symlinks in this tree, including all subtrees. */
-    long inputFiles();
-
-    /**
-     * The total number of content bytes in this tree, including all subtrees. This includes both
-     * file contents and the protos describing directories.
-     */
-    long inputBytes();
-
-    /** Returns the root of this tree, which may be the current instance. */
-    RootOnly root();
-
-    /**
-     * A {@link MerkleTree} that doesn't retain any blobs, either because they have already been
-     * uploaded or because only the root digest is needed (e.g., for a remote cache check).
-     */
-    sealed interface RootOnly extends MerkleTree {
-      @Override
-      default RootOnly root() {
-        return this;
-      }
-
-      /**
-       * A {@link MerkleTree} that retains no blobs since all of them have recently been uploaded to
-       * the remote cache.
-       */
-      record BlobsUploaded(Digest digest, long inputFiles, long inputBytes) implements RootOnly {}
-
-      /**
-       * A {@link MerkleTree} that retains no blobs since they were discarded during the computation
-       * (e.g., because they aren't needed for a remote cache check).
-       */
-      record BlobsDiscarded(Digest digest, long inputFiles, long inputBytes) implements RootOnly {}
-    }
-
-    /** A {@link MerkleTree} that retains all blobs that still need to be uploaded. */
-    final class Uploadable implements MerkleTree {
-      private final RootOnly.BlobsUploaded root;
-      private final ImmutableMap<Digest, /* byte[] | Path | VirtualActionInput */ Object> blobs;
-
-      private Uploadable(RootOnly.BlobsUploaded root, ImmutableMap<Digest, Object> blobs) {
-        this.root = root;
-        this.blobs = blobs;
-      }
-
-      public Digest digest() {
-        return root().digest();
-      }
-
-      public long inputFiles() {
-        return root().inputFiles();
-      }
-
-      public long inputBytes() {
-        return root().inputBytes();
-      }
-
-      public ImmutableSet<Digest> allDigests() {
-        return blobs.keySet();
-      }
-
-      @VisibleForTesting
-      public ImmutableMap<Digest, Object> blobs() {
-        return blobs;
-      }
-
-      @Override
-      public RootOnly root() {
-        return root;
-      }
-
-      /**
-       * Returns a future that tracks the upload of the blob with the given digest, or {@link
-       * Optional#empty()} if there is no blob with the given digest.
-       */
-      public Optional<ListenableFuture<Void>> upload(
-          MerkleTreeUploader uploader,
-          RemoteActionExecutionContext context,
-          RemotePathResolver remotePathResolver,
-          Digest digest) {
-        return switch (blobs.get(digest)) {
-          case byte[] data -> Optional.of(uploader.upload(context, digest, data));
-          case Path path -> Optional.of(uploader.upload(context, remotePathResolver, digest, path));
-          case VirtualActionInput virtualActionInput ->
-              Optional.of(uploader.upload(context, digest, virtualActionInput));
-          case null -> Optional.empty();
-          default -> throw new IllegalStateException("Unexpected blob type: " + blobs.get(digest));
-        };
-      }
-    }
-  }
-
-  /** The basic cache operations needed to upload a {@link MerkleTree} and its associated blobs. */
-  public interface MerkleTreeUploader {
-    ListenableFuture<Void> upload(RemoteActionExecutionContext context, Digest digest, byte[] data);
-
-    ListenableFuture<Void> upload(
-        RemoteActionExecutionContext context,
-        RemotePathResolver remotePathResolver,
-        Digest digest,
-        Path path);
-
-    ListenableFuture<Void> upload(
-        RemoteActionExecutionContext context, Digest digest, VirtualActionInput virtualActionInput);
-
-    /**
-     * Ensures that all inputs as well as metadata protos in the given Merkle tree are present in
-     * the remote cache by querying for and uploading missing blobs.
-     *
-     * @param force if true, all blobs in the tree are uploaded even if they have already been
-     *     uploaded before.
-     */
-    void ensureInputsPresent(
-        RemoteActionExecutionContext context,
-        MerkleTree.Uploadable merkleTree,
-        boolean force,
-        RemotePathResolver remotePathResolver)
-        throws IOException, InterruptedException;
   }
 
   /** Specifies which blobs should be retained in the Merkle tree. */
