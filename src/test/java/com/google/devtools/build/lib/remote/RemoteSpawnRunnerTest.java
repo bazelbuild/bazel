57e75bbab84411597522299f999c006b03c3a389
Fabian Meumertzheim <fabian@meumertzhe.im>
2025-10-07 17:30:43 +0200

    Replace RxJava with direct style

--- src/test/java/com/google/devtools/build/lib/remote/RemoteSpawnRunnerTest.java
+++ src/test/java/com/google/devtools/build/lib/remote/RemoteSpawnRunnerTest.java
@@ -15,7 +15,6 @@ package com.google.devtools.build.lib.remote;
 
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.extensions.proto.ProtoTruth.assertThat;
-import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
 import static org.junit.Assert.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
@@ -1163,7 +1162,6 @@ public class RemoteSpawnRunnerTest {
     ExecutionOptions executionOptions = Options.parse(ExecutionOptions.class, flag).getOptions();
     RemoteExecutionService remoteExecutionService =
         new RemoteExecutionService(
-            directExecutor(),
             reporter,
             /* verboseFailures= */ true,
             execRoot,
@@ -1700,7 +1698,6 @@ public class RemoteSpawnRunnerTest {
     RemoteExecutionService service =
         spy(
             new RemoteExecutionService(
-                directExecutor(),
                 reporter,
                 /* verboseFailures= */ true,
                 execRoot,
