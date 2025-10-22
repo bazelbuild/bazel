8603c6e4e5ca9aeb085ee645c4fdf191d1a11ecd
Will Stranton <2659963+willstranton@users.noreply.github.com>
2025-10-22 02:29:24 +0000

    Remove unicode file and test for SevenZDecompressor
    See discussion in #27269 - there was some internal testing difference for
    unicode files that we weren't able to figure out.  It works locally on my local
    mac computer but did not work inside Google for some reason.  Since this is the
    only decompression test that tried to test unicode files, we decided to drop
    it.  The thinking is that this would also occur in the other decompression
    tests if they also had a unicode file.  My own integration testing shows that
    it does work.

--- src/test/java/com/google/devtools/build/lib/bazel/repository/decompressor/SevenZDecompressorTest.java
+++ src/test/java/com/google/devtools/build/lib/bazel/repository/decompressor/SevenZDecompressorTest.java
@@ -1,22 +1,20 @@
 package com.google.devtools.build.lib.bazel.repository.decompressor;
 
-import com.google.devtools.build.lib.util.StringEncoding;
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.devtools.build.lib.bazel.repository.decompressor.TestArchiveDescriptor.INNER_FOLDER_NAME;
+import static com.google.devtools.build.lib.bazel.repository.decompressor.TestArchiveDescriptor.ROOT_FOLDER_NAME;
+
 import com.google.devtools.build.lib.vfs.Dirent;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.Symlinks;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static com.google.common.truth.Truth.assertThat;
-import static com.google.devtools.build.lib.bazel.repository.decompressor.TestArchiveDescriptor.INNER_FOLDER_NAME;
-import static com.google.devtools.build.lib.bazel.repository.decompressor.TestArchiveDescriptor.ROOT_FOLDER_NAME;
 
 /** Tests .7z decompression. */
 @RunWith(JUnit4.class)
@@ -24,11 +22,10 @@ public class SevenZDecompressorTest {
   @Rule public TestName name = new TestName();
 
   /**
-   * .7z file, created with two files:
+   * .7z file, created with one file:
    *
    * <ul>
    *   <li>root_folder/another_folder/regularFile
-   *   <li>root_folder/another_folder/ünïcödëFïlë.txt
    * </ul>
    *
    * Compressed with command "7zz a test_decompress_archive.7z root_folder"
@@ -36,7 +33,6 @@ public class SevenZDecompressorTest {
   private static final String ARCHIVE_NAME = "test_decompress_archive.7z";
 
   private static final String REGULAR_FILENAME = "regularFile";
-  private static final String UNICODE_FILENAME = "ünïcödëFïlë.txt";
 
   /** Provides a test filesystem descriptor for a test. NOTE: unique per individual test ONLY. */
   private TestArchiveDescriptor archiveDescriptor() throws Exception {
@@ -116,24 +112,6 @@ public class SevenZDecompressorTest {
             .collect(Collectors.toList());
     assertThat(files).contains("renamedFile");
     assertThat(fileDir.getRelative("renamedFile").getFileSize()).isNotEqualTo(0);
-  }
-
-  /** Test that Unicode filenames are handled. **/
-  @Test
-  public void testUnicodeFilename() throws Exception {
-    Path outputDir = decompress(archiveDescriptor().createDescriptorBuilder().build());
-
-    Path path = outputDir.getRelative(ROOT_FOLDER_NAME).getRelative(INNER_FOLDER_NAME);
-    List<String> filenames =
-        path.readdir(Symlinks.NOFOLLOW).stream()
-            .map((Dirent::getName))
-            .map(StringEncoding::internalToUnicode)
-            .collect(Collectors.toList());
-    assertThat(filenames).contains(UNICODE_FILENAME);
-
-    Path unicodeFile = path.getRelative(StringEncoding.unicodeToInternal(UNICODE_FILENAME));
-    assertThat(unicodeFile.exists()).isTrue();
-    assertThat(unicodeFile.getFileSize()).isNotEqualTo(0);
   }
 
   private Path decompress(DecompressorDescriptor descriptor) throws Exception {
