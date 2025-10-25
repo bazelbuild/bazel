23bd21969c36b8c77d297c3b363faec67bee3087
Fabian Meumertzheim <fabian@meumertzhe.im>
2025-10-25 21:58:40 +0200

    Remove case DETAILED_IO_EXCEPTION from switch statement

--- src/main/java/com/google/devtools/build/lib/skyframe/ArtifactFunction.java
+++ src/main/java/com/google/devtools/build/lib/skyframe/ArtifactFunction.java
@@ -1,4 +1,4 @@
-// Copyright 2014 The Bazel Authors. All rights reserved.
+ // Copyright 2014 The Bazel Authors. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -360,7 +360,6 @@ public final class ArtifactFunction implements SkyFunction {
           throw new ArtifactFunctionException(
               SourceArtifactException.create(artifact, e), Transience.PERSISTENT);
         case INCONSISTENT_FILESYSTEM:
-        case DETAILED_IO_EXCEPTION:
           throw new ArtifactFunctionException(
               SourceArtifactException.create(artifact, e), Transience.TRANSIENT);
         case GENERATED_PATH_CONFLICT:
