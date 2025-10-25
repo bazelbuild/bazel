774203a706a1dc93b4e6a102329184502caf0e57
Will Stranton <2659963+willstranton@users.noreply.github.com>
2025-10-25 04:22:01 +0000

    Add support for decompressing bz2, gz, xz, zst
    Bazel already supports decompressing tar files compressed with bz2, gz, xz, and
    zst. This change adds support for decompressing individual files (not in a tar bundle) that are compressed with those compression algorithms.
    
    In other words, support for decompression:
    
    before: .tar.bz2, .tar.gz, .tar.xz, .tar.zst
    after : .tar.bz2, .bz2, .tar.gz, .gz, .tar.xz, .xz, .tar.zst, .zst
    
    Fixes: #20125

---
+++ src/main/java/com/google/devtools/build/lib/bazel/repository/decompressor/Bz2Function.java
@@ -0,0 +1,23 @@
+package com.google.devtools.build.lib.bazel.repository.decompressor;
+
+import com.google.devtools.build.lib.bazel.repository.decompressor.DecompressorValue.Decompressor;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
+import org.apache.commons.compress.compressors.bzip2.BZip2Utils;
+
+public class Bz2Function extends CompressedFunction {
+  public static final Decompressor INSTANCE = new Bz2Function();
+
+  @Override
+  protected InputStream getDecompressorStream(BufferedInputStream compressedInputStream)
+      throws IOException {
+    return new BZip2CompressorInputStream(compressedInputStream, true);
+  }
+
+  @Override
+  protected String getUncompressedFileName(InputStream in, String compressedFileName) {
+    return BZip2Utils.getUncompressedFileName(compressedFileName);
+  }
+}
