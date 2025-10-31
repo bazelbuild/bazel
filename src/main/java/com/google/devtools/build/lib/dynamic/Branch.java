d9275ae6ac772ac2211ea8f427b9f289c05ab072
Fabian Meumertzheim <fabian@meumertzhe.im>
2025-10-31 16:17:22 +0100

    Fix

--- src/main/java/com/google/devtools/build/lib/dynamic/Branch.java
+++ src/main/java/com/google/devtools/build/lib/dynamic/Branch.java
@@ -19,6 +19,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import com.google.common.collect.ImmutableList;
 import com.google.common.flogger.GoogleLogger;
 import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
 import com.google.common.util.concurrent.SettableFuture;
 import com.google.devtools.build.lib.actions.ActionExecutionContext;
 import com.google.devtools.build.lib.actions.DynamicStrategyRegistry.DynamicMode;
@@ -48,8 +49,10 @@ abstract class Branch implements Callable<ImmutableList<SpawnResult>> {
    * #callImpl(ActionExecutionContext)} yet.
    */
   protected final AtomicBoolean starting = new AtomicBoolean(true);
+
   /** The {@link Spawn} this branch is running. */
   protected final Spawn spawn;
+
   /**
    * The {@link SettableFuture} with the results from running the spawn. Must not be null if
    * execution succeeded.
@@ -61,6 +64,7 @@ abstract class Branch implements Callable<ImmutableList<SpawnResult>> {
    * This object is shared between the local and remote branch of an action.
    */
   protected final AtomicReference<DynamicMode> strategyThatCancelled;
+
   /**
    * Semaphore that indicates whether this branch is done, i.e. either completed or cancelled. This
    * is needed to wait for the branch to finish its own cleanup (e.g. terminating subprocesses) once
@@ -70,6 +74,8 @@ abstract class Branch implements Callable<ImmutableList<SpawnResult>> {
 
   protected final DynamicExecutionOptions options;
   protected final ActionExecutionContext context;
+
+  protected Branch otherBranch;
 
   /**
    * Creates a new branch of dynamic execution.
@@ -128,6 +134,41 @@ abstract class Branch implements Callable<ImmutableList<SpawnResult>> {
   /** Executes this branch using the provided executor. */
   public void execute(ListeningExecutorService executor) {
     future.setFuture(executor.submit(this));
+  }
+
+  /** Sets up the {@link Future} used in the current branch to know what other branch to cancel. */
+  protected void prepareFuture(Branch otherBranch) {
+    this.otherBranch = otherBranch;
+    future.addListener(
+        () -> {
+          if (starting.compareAndSet(true, false)) {
+            // If the current branch got cancelled before even starting, we release its semaphore
+            // for it.
+            done.release();
+          }
+          // If the current branch succeeds, there is no need to keep the other branch running.
+          // If the current branch fails, cancel the other branch as well. However, that one may
+          // in turn cancel us, thus causing an interruption. Don't consider that a failure as
+          // we otherwise risk canceling both branches.
+          var state = future.state();
+          if (state == Future.State.SUCCESS
+              || (state == Future.State.FAILED
+                  && !(future.exceptionNow() instanceof InterruptedException))) {
+            otherBranch.cancel();
+          }
+          if (options.debugSpawnScheduler) {
+            logger.atInfo().log(
+                "In listener callback, the future of the remote branch is %s",
+                future.state().name());
+            try {
+              future.get();
+            } catch (InterruptedException | ExecutionException e) {
+              logger.atInfo().withCause(e).log(
+                  "The future of the remote branch failed with an exception.");
+            }
+          }
+        },
+        MoreExecutors.directExecutor());
   }
 
   /**
