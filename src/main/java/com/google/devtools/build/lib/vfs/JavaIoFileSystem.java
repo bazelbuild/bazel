36735649714589e3c1bbba55f51e0b13d55d8d2c
tjgq <tjgq@google.com>
2025-11-27 03:51:56 -0800

    Improve the FileSystem inheritance hierarchy.
    By repurposing AbstractFileSystem as DiskBackedFileSystem, to be extended only by disk-backed subclasses. This encourages other subclasses to make an explicit decision on how to open files instead of inheriting a potentially incorrect implementation.
    
    As a bonus, we can also dispense with the duplicate profiling logic in UnixFileSystem.
    
    PiperOrigin-RevId: 837473162
    Change-Id: I970d0a9711ecc583b1ecae650703d14d4a5db2a4

--- src/main/java/com/google/devtools/build/lib/vfs/JavaIoFileSystem.java
+++ src/main/java/com/google/devtools/build/lib/vfs/JavaIoFileSystem.java
@@ -43,7 +43,7 @@ import javax.annotation.Nullable;
  * system call - they all are associated with the VFS_STAT task.
  */
 @ThreadSafe
-public class JavaIoFileSystem extends AbstractFileSystem {
+public class JavaIoFileSystem extends DiskBackedFileSystem {
   private static final LinkOption[] NO_LINK_OPTION = new LinkOption[0];
   // This isn't generally safe; we rely on the file system APIs not modifying the array.
   private static final LinkOption[] NOFOLLOW_LINKS_OPTION =
--- src/test/java/com/google/devtools/build/lib/unix/UnixFileSystemTest.java
+++ src/test/java/com/google/devtools/build/lib/unix/UnixFileSystemTest.java
@@ -16,7 +16,13 @@ package com.google.devtools.build.lib.unix;
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 import static org.junit.Assert.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import com.google.devtools.build.lib.profiler.Profiler;
+import com.google.devtools.build.lib.profiler.ProfilerTask;
+import com.google.devtools.build.lib.profiler.TraceProfilerService;
 import com.google.devtools.build.lib.vfs.DigestHashFunction;
 import com.google.devtools.build.lib.vfs.Dirent;
 import com.google.devtools.build.lib.vfs.FileAccessException;
@@ -25,7 +31,12 @@ import com.google.devtools.build.lib.vfs.FileSystemUtils;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.SymlinkAwareFileSystemTest;
 import com.google.devtools.build.lib.vfs.Symlinks;
+import com.google.testing.junit.testparameterinjector.TestParameter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import org.junit.Test;
 
 /** Tests for the {@link com.google.devtools.build.lib.unix.UnixFileSystem} class. */
@@ -118,5 +129,43 @@ public class UnixFileSystemTest extends SymlinkAwareFileSystemTest {
 
     assertThrows(FileAccessException.class, dir::getDirectoryEntries);
     assertThrows(FileAccessException.class, () -> dir.readdir(Symlinks.NOFOLLOW));
+  }
+
+  @Test
+  public void testInputStreamIsFileInputStream(@TestParameter boolean profiling) throws Exception {
+    try (var m = new MaybeWithMockProfiler(profiling);
+        InputStream in = xFile.getInputStream()) {
+      assertThat(in).isInstanceOf(FileInputStream.class);
+    }
+  }
+
+  @Test
+  public void testOutputStreamIsFileOutputStream(@TestParameter boolean profiling)
+      throws Exception {
+    try (var m = new MaybeWithMockProfiler(profiling);
+        OutputStream out = xFile.getOutputStream()) {
+      assertThat(out).isInstanceOf(FileOutputStream.class);
+    }
+  }
+
+  private static class MaybeWithMockProfiler implements AutoCloseable {
+    private final boolean enabled;
+
+    MaybeWithMockProfiler(boolean enabled) {
+      if (enabled) {
+        TraceProfilerService mock = mock(TraceProfilerService.class);
+        when(mock.isActive()).thenReturn(true);
+        when(mock.isProfiling(any(ProfilerTask.class))).thenReturn(true);
+        Profiler.setTraceProfilerServiceForTesting(mock);
+      }
+      this.enabled = enabled;
+    }
+
+    @Override
+    public void close() {
+      if (enabled) {
+        Profiler.setTraceProfilerServiceForTesting(null);
+      }
+    }
   }
 }
