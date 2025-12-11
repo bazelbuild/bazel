f0d8e5550bac43322665ca5d136bf84dfab55509
jhorvitz <jhorvitz@google.com>
2025-12-10 19:14:35 -0800

    Refactor tracking of `EvaluationStats` into `DirtyAndInflightTrackingProgressReceiver` so that alternate evaluator implementations can get the stats.
    PiperOrigin-RevId: 842988880
    Change-Id: I916c903cadcdc1ee339bcd5b449ece842879a197

--- src/main/java/com/google/devtools/build/skyframe/InMemoryMemoizingEvaluator.java
+++ src/main/java/com/google/devtools/build/skyframe/InMemoryMemoizingEvaluator.java
@@ -16,15 +16,9 @@ package com.google.devtools.build.skyframe;
 import static com.google.common.base.Preconditions.checkState;
 
 import com.google.common.annotations.VisibleForTesting;
-import com.google.common.collect.ConcurrentHashMultiset;
 import com.google.common.collect.ImmutableMap;
-import com.google.devtools.build.lib.events.ExtendedEventHandler;
 import com.google.devtools.build.lib.util.TestType;
-import com.google.devtools.build.skyframe.NodeEntry.DirtyType;
-import com.google.devtools.build.skyframe.SkyframeGraphStatsEvent.EvaluationStats;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import javax.annotation.Nullable;
 
 /**
  * An in-memory {@link MemoizingEvaluator} that uses the eager invalidation strategy. This class is,
@@ -36,102 +30,6 @@ import javax.annotation.Nullable;
  * evaluations and {@link Version#constant} for non-incremental evaluations.
  */
 public final class InMemoryMemoizingEvaluator extends AbstractInMemoryMemoizingEvaluator {
-
-  /**
-   * A progress receiver that, in addition to tracking dirty and inflight notes, also collects
-   * stats.
-   */
-  private static final class ProgressReceiver extends DirtyAndInflightTrackingProgressReceiver {
-    // Nodes that were dirtied because one of their transitive dependencies changed
-    private final ConcurrentHashMultiset<SkyFunctionName> dirtied;
-
-    // Nodes that were dirtied because they themselves changed (for example, a leaf node that
-    // represents a file and that changed between builds)
-    private final ConcurrentHashMultiset<SkyFunctionName> changed;
-
-    // Nodes that were built and found different from the previous version
-    private final ConcurrentHashMultiset<SkyFunctionName> built;
-
-    // Nodes that were built and found to be same as the previous version
-    private final ConcurrentHashMultiset<SkyFunctionName> cleaned;
-
-    // Nodes that were computed during the build
-    private final ConcurrentHashMultiset<SkyFunctionName> evaluated;
-
-    private static ConcurrentHashMultiset<SkyFunctionName> createMultiset() {
-      return ConcurrentHashMultiset.create(
-          new ConcurrentHashMap<>(Runtime.getRuntime().availableProcessors(), 0.75f));
-    }
-
-    private ProgressReceiver(EvaluationProgressReceiver progressReceiver) {
-      super(progressReceiver);
-
-      dirtied = createMultiset();
-      changed = createMultiset();
-      built = createMultiset();
-      cleaned = createMultiset();
-      evaluated = createMultiset();
-    }
-
-    private static ImmutableMap<SkyFunctionName, Integer> fromMultiset(
-        ConcurrentHashMultiset<SkyFunctionName> s) {
-      return s.entrySet().stream()
-          .collect(ImmutableMap.toImmutableMap(e -> e.getElement(), e -> e.getCount()));
-    }
-
-    private EvaluationStats aggregateAndReset() {
-      EvaluationStats result =
-          new EvaluationStats(
-              fromMultiset(dirtied),
-              fromMultiset(changed),
-              fromMultiset(built),
-              fromMultiset(cleaned),
-              fromMultiset(evaluated));
-      dirtied.clear();
-      changed.clear();
-      built.clear();
-      cleaned.clear();
-      evaluated.clear();
-      return result;
-    }
-
-    @Override
-    public void dirtied(SkyKey skyKey, DirtyType dirtyType) {
-      super.dirtied(skyKey, dirtyType);
-
-      switch (dirtyType) {
-        case DIRTY -> dirtied.add(skyKey.functionName());
-        case CHANGE -> changed.add(skyKey.functionName());
-        case REWIND -> {} // Should not happen but let's not crash the server due to logging
-      }
-    }
-
-    @Override
-    public void evaluated(
-        SkyKey skyKey,
-        EvaluationState state,
-        @Nullable SkyValue newValue,
-        @Nullable ErrorInfo newError,
-        @Nullable GroupedDeps directDeps) {
-      super.evaluated(skyKey, state, newValue, newError, directDeps);
-
-      if (directDeps == null) {
-        // In this case, no actual evaluation work was done so let's not record it.
-      } else if (state.versionChanged()) {
-        built.add(skyKey.functionName(), 1);
-      } else {
-        cleaned.add(skyKey.functionName(), 1);
-      }
-    }
-
-    @Override
-    public void stateEnding(SkyKey skyKey, NodeState nodeState) {
-      super.stateEnding(skyKey, nodeState);
-      if (nodeState == NodeState.COMPUTE) {
-        evaluated.add(skyKey.functionName());
-      }
-    }
-  }
 
   // Not final only for testing.
   private InMemoryGraph graph;
@@ -168,7 +66,7 @@ public final class InMemoryMemoizingEvaluator extends AbstractInMemoryMemoizingE
     super(
         ImmutableMap.copyOf(skyFunctions),
         differencer,
-        new ProgressReceiver(progressReceiver),
+        new DirtyAndInflightTrackingProgressReceiver(progressReceiver),
         eventFilter,
         emittedEventState,
         graphInconsistencyReceiver,
@@ -178,12 +76,6 @@ public final class InMemoryMemoizingEvaluator extends AbstractInMemoryMemoizingE
         keepEdges
             ? InMemoryGraph.create(usePooledInterning)
             : InMemoryGraph.createEdgeless(usePooledInterning);
-  }
-
-  @Override
-  public void postLoggingStats(ExtendedEventHandler eventHandler) {
-    EvaluationStats evaluationStats = ((ProgressReceiver) progressReceiver).aggregateAndReset();
-    eventHandler.post(new SkyframeGraphStatsEvent(graph.valuesSize(), evaluationStats));
   }
 
   @Override
