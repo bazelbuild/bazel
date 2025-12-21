2c2e629b77ce180c854d4419568c90c002f0cfad
Fabian Meumertzheim <fabian@meumertzhe.im>
2025-12-21 07:07:07 +0100

    Delete `--incompatible_check_sharding_support`
    Thiss flag was flipped over two years ago.

--- src/main/java/com/google/devtools/build/lib/exec/StandaloneTestStrategy.java
+++ src/main/java/com/google/devtools/build/lib/exec/StandaloneTestStrategy.java
@@ -699,23 +699,22 @@ public class StandaloneTestStrategy extends TestStrategy {
     // Do not override a more informative test failure with a generic failure due to the missing
     // shard file, which may have been caused by the test failing before the runner had a chance to
     // touch the file
-    if (testResultDataBuilder.getTestPassed() && testAction.isSharded()) {
-      if (testAction.checkShardingSupport()
-          && !actionExecutionContext
-              .getPathResolver()
-              .convertPath(resolvedPaths.getTestShard())
-              .exists()) {
-        TestExecException e =
-            createTestExecException(
-                TestAction.Code.LOCAL_TEST_PREREQ_UNMET,
-                "Sharding requested, but the test runner did not advertise support for it by "
-                    + "touching TEST_SHARD_STATUS_FILE. Either remove the 'shard_count' attribute, "
-                    + "use a test runner that supports sharding or temporarily disable this check "
-                    + "via --noincompatible_check_sharding_support.");
-        closeSuppressed(e, streamed);
-        closeSuppressed(e, fileOutErr);
-        throw e;
-      }
+    if (testResultDataBuilder.getTestPassed()
+        && testAction.isSharded()
+        && !actionExecutionContext
+            .getPathResolver()
+            .convertPath(resolvedPaths.getTestShard())
+            .exists()) {
+      TestExecException e =
+          createTestExecException(
+              TestAction.Code.LOCAL_TEST_PREREQ_UNMET,
+              """
+              Sharding requested, but the test runner did not advertise support for it by touching \
+              TEST_SHARD_STATUS_FILE. Either remove the 'shard_count' attribute or use a test \
+              runner that supports sharding.""");
+      closeSuppressed(e, streamed);
+      closeSuppressed(e, fileOutErr);
+      throw e;
     }
 
     // SpawnActionContext guarantees the first entry to correspond to the spawn passed in (there
