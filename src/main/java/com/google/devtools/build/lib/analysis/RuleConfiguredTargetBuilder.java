4e84bacc65a3b90d004bd26aad727ca4bad975fc
Fabian Meumertzheim <fabian@meumertzhe.im>
2026-01-07 07:53:08 +0100

    Avoid repo mapping lookup to check builtin restrictions
    BuiltinRestriction currently accounts for 10-20% of the total CPU time for `bazel build --nobuild //src:bazel`. This is improved by avoiding the repeated creation of "non-visible" `RepositoryName` objects, which trigger a `SpellChecker` computation. Instead, the apparent repo names, which are in fact either module names or legacy WORKSPACE repo names that no longer matter in Bazel 9 and later, are turned into canonical repo name prefixes by appending a `+`.
    
    Note that there is still overhead in the 2-3% range even after this change due to repeated linear searches as well as repeated recreations of allowlists in the `check_private_api` C++ Starlark API.

--- src/main/java/com/google/devtools/build/lib/analysis/RuleConfiguredTargetBuilder.java
+++ src/main/java/com/google/devtools/build/lib/analysis/RuleConfiguredTargetBuilder.java
@@ -42,7 +42,6 @@ import com.google.devtools.build.lib.analysis.test.TestProvider;
 import com.google.devtools.build.lib.analysis.test.TestProvider.TestParams;
 import com.google.devtools.build.lib.analysis.test.TestTagsProvider;
 import com.google.devtools.build.lib.cmdline.Label;
-import com.google.devtools.build.lib.cmdline.RepositoryMapping;
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
 import com.google.devtools.build.lib.collect.nestedset.NestedSetBuilder;
 import com.google.devtools.build.lib.collect.nestedset.Order;
@@ -372,8 +371,7 @@ public final class RuleConfiguredTargetBuilder {
       if (rdeLabel != null
           && BuiltinRestriction.isNotAllowed(
               rdeLabel,
-              RepositoryMapping.EMPTY,
-              BuiltinRestriction.INTERNAL_STARLARK_API_ALLOWLIST)) {
+          BuiltinRestriction.INTERNAL_STARLARK_API_ALLOWLIST)) {
         ruleContext.ruleError(rdeLabel + " cannot access the _transitive_validation private API");
         return;
       }
