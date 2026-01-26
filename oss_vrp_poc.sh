5b336303cce505a43bb490d43c5f056e46e27f6e
bugdocto-rgb <bugdocto@gmail.com>
2026-01-26 22:15:57 +0100

    Delete oss_vrp_poc.sh

--- oss_vrp_poc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "--- BAZEL CI SECURITY POC ---"
-echo "Proven: The container is privileged and has access to host docker socket."
-
-# هذا الأمر آمن تماماً، سيقوم فقط بطباعة اسم السيرفر المضيف (Host)
-# لإثبات أننا نتواصل مع نظام Docker الخاص بالمضيف وليس الحاوية.
-docker -H unix:///var/run/docker.sock info | grep "Name:"
-
-echo "--- POC FINISHED ---"
