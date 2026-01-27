d0e83b60c233042fbdd5b7846298084e435ce444
Fabian Meumertzheim <fabian@meumertzhe.im>
2026-01-27 19:42:17 +0100

    Validation action

--- src/main/java/com/google/devtools/build/lib/rules/starlarkdocextract/ValidateBzlLibrary.java
+++ src/main/java/com/google/devtools/build/lib/rules/starlarkdocextract/ValidateBzlLibrary.java
@@ -1,18 +1,26 @@
 package com.google.devtools.build.lib.rules.starlarkdocextract;
 
 import static com.google.devtools.build.lib.rules.starlarkdocextract.StarlarkDocExtract.verifyModuleDeps;
+import static java.util.stream.Collectors.joining;
 
+import com.google.common.collect.ImmutableList;
+import com.google.devtools.build.lib.actions.ActionConflictException;
 import com.google.devtools.build.lib.actions.Artifact;
+import com.google.devtools.build.lib.actions.FailAction;
 import com.google.devtools.build.lib.analysis.CachingAnalysisEnvironment;
 import com.google.devtools.build.lib.analysis.ConfiguredAspect;
 import com.google.devtools.build.lib.analysis.ConfiguredAspectFactory;
 import com.google.devtools.build.lib.analysis.ConfiguredTarget;
+import com.google.devtools.build.lib.analysis.OutputGroupInfo;
 import com.google.devtools.build.lib.analysis.RuleContext;
+import com.google.devtools.build.lib.analysis.actions.FileWriteAction;
 import com.google.devtools.build.lib.cmdline.BazelModuleContext;
 import com.google.devtools.build.lib.cmdline.Label;
 import com.google.devtools.build.lib.cmdline.RepositoryName;
 import com.google.devtools.build.lib.collect.nestedset.Depset;
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
+import com.google.devtools.build.lib.collect.nestedset.NestedSetBuilder;
+import com.google.devtools.build.lib.collect.nestedset.Order;
 import com.google.devtools.build.lib.packages.AspectDefinition;
 import com.google.devtools.build.lib.packages.AspectParameters;
 import com.google.devtools.build.lib.packages.Info;
@@ -20,6 +28,7 @@ import com.google.devtools.build.lib.packages.NativeAspectClass;
 import com.google.devtools.build.lib.packages.RuleClass.ConfiguredTargetFactory.RuleErrorException;
 import com.google.devtools.build.lib.packages.StarlarkInfo;
 import com.google.devtools.build.lib.packages.StarlarkProviderWrapper;
+import com.google.devtools.build.lib.server.FailureDetails;
 import com.google.devtools.build.lib.skyframe.BzlLoadValue;
 import java.util.ArrayList;
 import java.util.List;
@@ -79,7 +88,7 @@ public final class ValidateBzlLibrary extends NativeAspectClass implements Confi
       RuleContext context,
       AspectParameters parameters,
       RepositoryName toolsRepository)
-      throws InterruptedException, RuleErrorException {
+      throws InterruptedException, RuleErrorException, ActionConflictException {
     var mainRepoMapping = context.getAnalysisEnvironment().getMainRepoMapping();
     var libraryInfo = ct.get(StarlarkLibraryInfo.PROVIDER);
     if (libraryInfo == null) {
@@ -98,20 +107,45 @@ public final class ValidateBzlLibrary extends NativeAspectClass implements Confi
     if (valuesMissing) {
       throw new CachingAnalysisEnvironment.MissingDepException("module missing");
     }
-    modules.stream()
-        .map(
-            module ->
-                verifyModuleDeps(module, libraryInfo.getTransitiveSrcs(), mainRepoMapping)
-                    .map(
-                        message ->
-                            "in %s: %s"
-                                .formatted(
-                                    BazelModuleContext.of(module)
-                                        .label()
-                                        .getDisplayForm(mainRepoMapping),
-                                    message)))
-        .flatMap(Optional::stream)
-        .forEach(context::ruleError);
-    return ConfiguredAspect.NonApplicableAspect.INSTANCE;
+    String errors =
+        modules.stream()
+            .map(
+                module ->
+                    verifyModuleDeps(module, libraryInfo.getTransitiveSrcs(), mainRepoMapping)
+                        .map(
+                            message ->
+                                "in %s: %s"
+                                    .formatted(
+                                        BazelModuleContext.of(module)
+                                            .label()
+                                            .getDisplayForm(mainRepoMapping),
+                                        message)))
+            .flatMap(Optional::stream)
+            .collect(joining("\n"));
+    var validationOutput =
+        context.getDerivedArtifact(
+            context
+                .getPackageDirectory()
+                .getRelative(context.getRule().getName() + "_validation_output.txt"),
+            context.getBinDirectory());
+    if (errors.isEmpty()) {
+      context.registerAction(
+          FileWriteAction.createEmptyWithInputs(
+              context.getActionOwner(),
+              NestedSetBuilder.emptySet(Order.STABLE_ORDER),
+              validationOutput));
+    } else {
+      context.registerAction(
+          new FailAction(
+              context.getActionOwner(),
+              ImmutableList.of(validationOutput),
+              errors,
+              FailureDetails.FailAction.Code.VALIDATION_FAILURE));
+    }
+    return ConfiguredAspect.builder(context)
+        .addOutputGroup(
+            OutputGroupInfo.VALIDATION,
+            NestedSetBuilder.create(Order.STABLE_ORDER, validationOutput))
+        .build();
   }
 }
