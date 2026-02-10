bazelbuild/bazel

commit ed98d31d352a41595d9f4561fae501bd67a43826

Add ability for builtin methods to act as types
As a proof of concept, this CL makes the builtin value `list` act as a type, replacing the hardcoded definition in `Types.TYPE_UNIVERSE`. A follow-up will add the other universal types.

The basic design is that a new annotation field, `@StarlarkMethod#isTypeConstructor`, is set true for methods like `MethodLibrary#list`. This causes the `BuiltinFunction` wrapping the method descriptor to itself become a type constructor. The underlying type constructor is obtained from a static method on the class of the annotated method's return type.

In a follow-up CL, the outer type constructor will augment the underlying one with Starlark method information (for `list.append`, etc.), but currently it's just a trivial wrapper. `CallUtils` is responsible for managing the association between the Java class and the appropriate type constructor.

Much thanks to arostovtsev@ for suggesting a simplification over the previous version of this CL, which had an additional `TypeConstructor.Supplier` indirection.

Work toward #28325.

PiperOrigin-RevId: 866529277
Change-Id: Id6e9b94b2434068c88d62b5ab9bfa832f1630f27

src/main/java/net/starlark/java/eval/BuiltinFunction.java
@@ -26,6 +26,7 @@ import net.starlark.java.annot.StarlarkBuiltin;
 import net.starlark.java.annot.StarlarkMethod;
 import net.starlark.java.spelling.SpellChecker;
 import net.starlark.java.syntax.StarlarkType;
+import net.starlark.java.syntax.TypeConstructor;
 
 /**
  * A BuiltinFunction is a callable Starlark value that reflectively invokes a {@link
@@ -38,10 +39,11 @@ import net.starlark.java.syntax.StarlarkType;
     name = "builtin_function_or_method", // (following Python)
     category = "core",
     doc = "The type of a built-in function, defined by Java code.")
-public final class BuiltinFunction implements StarlarkCallable {
+public sealed class BuiltinFunction implements StarlarkCallable
+    permits BuiltinFunction.BuiltinTypeFunction {
 
-  private final Object obj;
-  private final MethodDescriptor desc;
+  protected final Object obj;
+  protected final MethodDescriptor desc;
 
   /**
    * Constructs a {@code BuiltinFunction} for a {@link StarlarkMethod}-annotated method on the given
@@ -50,7 +52,15 @@ public final class BuiltinFunction implements StarlarkCallable {
    * <p>The method must be a proper Starlark method, not a field; i.e., {@link
    * StarlarkMethod#structField} must be false.
    */
-  BuiltinFunction(Object obj, MethodDescriptor desc) {
+  static BuiltinFunction of(Object obj, MethodDescriptor desc, StarlarkSemantics semantics) {
+    if (desc.getTypeConstructorProxy() == null) {
+      return new BuiltinFunction(obj, desc);
+    } else {
+      return new BuiltinTypeFunction(obj, desc, semantics);
+    }
+  }
+
+  private BuiltinFunction(Object obj, MethodDescriptor desc) {
     Preconditions.checkArgument(!desc.isStructField());
     this.obj = obj;
     this.desc = desc;
@@ -510,6 +520,31 @@ public final class BuiltinFunction implements StarlarkCallable {
       throw Starlark.errorf(
           "in call to %s(), parameter '%s' got value of type '%s', want '%s'",
           getName(), param.getName(), Starlark.type(value), param.getTypeErrorMessage());
+    }
+  }
+
+  private static final class BuiltinTypeFunction extends BuiltinFunction
+      implements TypeConstructor {
+
+    private final StarlarkSemantics semantics;
+
+    private BuiltinTypeFunction(Object obj, MethodDescriptor desc, StarlarkSemantics semantics) {
+      super(obj, desc);
+      this.semantics = semantics;
+    }
+
+    @Override
+    public StarlarkType createStarlarkType(ImmutableList<Arg> argsTuple) throws Failure {
+      // The Preconditions checks could morally be done in the constructor for eagerness.
+      // However, this causes the MethodDescriptors of the proxy class to be materialized
+      // while initializing a Module environment using Starlark#addMethods. That's inconvenient
+      // due to circular dependencies (see the bootstrapping in ParamDescriptor#evalDefault)
+      // and because it complicates unit tests where these preconditions fail.
+      Class<?> tcProxy = desc.getTypeConstructorProxy();
+      Preconditions.checkNotNull(tcProxy);
+      TypeConstructor tc = CallUtils.getTypeConstructor(semantics, tcProxy);
+      Preconditions.checkArgument(tc != null, "invalid type constructor proxy: %s", tcProxy);
+      return tc.createStarlarkType(argsTuple);
     }
   }
 }

src/main/java/net/starlark/java/annot/StarlarkMethod.java
@@ -191,6 +191,29 @@ public @interface StarlarkMethod {
   boolean useStarlarkSemantics() default false;
 
   /**
+   * Whether this method can act as a type in a type expression.
+   *
+   * <p>An example would be the {@code list} builtin symbol.
+   *
+   * <p>If true, the class identified by the Java method's return type is taken to be the Java class
+   * whose instances are Starlark values of this Starlark type. For example, {@code list()} is
+   * implemented by {@link MethodLibrary#list}, whose return type is {@link StarlarkList}, and
+   * instances of {@code StarlarkList} are Starlark values of the {@code list} type.
+   *
+   * <p>The return type's class must define a static method with the signature:
+   *
+   * <pre>
+   *     public static TypeConstructor getBaseTypeConstructor() {...}
+   * </pre>
+   *
+   * which is reflectively invoked to obtain an appropriate base type constructor (e.g. {@link
+   * Types#LIST_CONSTRUCTOR}). The builtin processing machinery will wrap this base type constructor
+   * with generated type information for the class's various {@code @StarlarkMethod}s (such as
+   * {@code list.append}).
+   */
+  boolean isTypeConstructor() default false;
+
+  /**
    * If non-empty, the annotated method will only be callable if the given semantic flag is true.
    * Note that at most one of {@link #enableOnlyWithFlag} and {@link #disableWithFlag} can be
    * non-empty.

src/main/java/net/starlark/java/eval/CallUtils.java
@@ -15,13 +15,16 @@
 package net.starlark.java.eval;
 
 import com.google.common.collect.ImmutableMap;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.concurrent.ConcurrentHashMap;
 import javax.annotation.Nullable;
 import net.starlark.java.annot.StarlarkAnnotations;
 import net.starlark.java.annot.StarlarkMethod;
+import net.starlark.java.syntax.TypeConstructor;
 
 /** Helper functions for {@link StarlarkMethod}-annotated methods. */
 final class CallUtils {
@@ -123,6 +126,14 @@ final class CallUtils {
      * <p>The map is keyed on the Starlark field name, and sorted by Java method name.
      */
     ImmutableMap<String, MethodDescriptor> methods;
+
+    /**
+     * The type constructor produced by augmenting this class's base type constructor with method
+     * information; or null if this class cannot be used as a type.
+     *
+     * <p>See {@link StarlarkMethod#isTypeConstructor}.
+     */
+    @Nullable TypeConstructor typeConstructor;
   }
 
   /** Two-layer cache of {@link #buildClassDescriptor}, managed by {@link #getClassDescriptor}. */
@@ -133,6 +144,9 @@ final class CallUtils {
   private static ClassDescriptor buildClassDescriptor(StarlarkSemantics semantics, Class<?> clazz) {
     MethodDescriptor selfCall = null;
     ImmutableMap.Builder<String, MethodDescriptor> methods = ImmutableMap.builder();
+
+    TypeConstructor typeConstructor = getBaseTypeConstructor(clazz);
+    // TODO: #28325 - Programmatically augment this type with the @StarlarkMethods.
 
     // Sort methods by Java name, for determinism.
     Method[] classMethods = clazz.getMethods();
@@ -174,7 +188,70 @@ final class CallUtils {
     ClassDescriptor classDescriptor = new ClassDescriptor();
     classDescriptor.selfCall = selfCall;
     classDescriptor.methods = methods.buildOrThrow();
+    classDescriptor.typeConstructor = typeConstructor;
     return classDescriptor;
+  }
+
+  /**
+   * Returns the base type constructor identified by the given class's {@code
+   * getBaseTypeConstructor()} static method, or null if it does not have one.
+   *
+   * <p>The base type constructor is not the final constructor stored on the {@link
+   * ClassDescriptor}; it lacks type information about the class's methods.
+   *
+   * @throws IllegalArgumentException if the method exists but has an unexpected signature, or if it
+   *     does not evaluate successfully
+   */
+  @Nullable
+  private static TypeConstructor getBaseTypeConstructor(Class<?> clazz) {
+    Method found = null;
+    for (Method m : clazz.getDeclaredMethods()) {
+      if (m.getName().equals("getBaseTypeConstructor")) {
+        if (found != null) {
+          throw new IllegalArgumentException(
+              String.format(
+                  "Class %s has multiple methods named getBaseTypeConstructor", clazz.getName()));
+        }
+        found = m;
+      }
+    }
+    if (found == null) {
+      return null;
+    }
+
+    // Signature check.
+    if (!Modifier.isPublic(found.getModifiers())
+        || !Modifier.isStatic(found.getModifiers())
+        || !found.getReturnType().equals(TypeConstructor.class)
+        || found.getParameterCount() != 0) {
+      throw new IllegalArgumentException(
+          String.format(
+              "Method %s#getBaseTypeConstructor has an invalid signature; "
+                  + "expected 'public static TypeConstructor getBaseTypeConstructor()'",
+              clazz.getName()));
+    }
+
+    try {
+      return (TypeConstructor) found.invoke(null);
+    } catch (IllegalAccessException | InvocationTargetException | RuntimeException e) {
+      throw new IllegalArgumentException(
+          String.format("Error invoking %s#getBaseTypeConstructor", clazz.getName()), e);
+    }
+  }
+
+  /**
+   * Returns the type constructor associated with the given Java class under a given {@code
+   * StarlarkSemantics}, or null if there is none.
+   *
+   * <p>An example would be getting the type constructor for the {@code list} type from the class
+   * {@code StarlarkList}.
+   *
+   * <p>The returned constructor has complete type information about the available Starlark methods
+   * of the class.
+   */
+  @Nullable
+  static TypeConstructor getTypeConstructor(StarlarkSemantics semantics, Class<?> clazz) {
+    return getClassDescriptor(semantics, clazz).typeConstructor;
   }
 
   /**

src/main/java/net/starlark/java/eval/MethodDescriptor.java
@@ -55,6 +55,7 @@ final class MethodDescriptor {
   private final boolean allowReturnNones;
   private final boolean useStarlarkThread;
   private final boolean useStarlarkSemantics;
+  @Nullable private final Class<?> typeConstructorProxy;
   private final boolean positionalsReusableAsJavaArgsVectorIfArgumentCountValid;
   private final StarlarkType starlarkType;
 
@@ -82,7 +83,8 @@ final class MethodDescriptor {
       boolean selfCall,
       boolean allowReturnNones,
       boolean useStarlarkThread,
-      boolean useStarlarkSemantics) {
+      boolean useStarlarkSemantics,
+      boolean isTypeConstructor) {
     this.method = method;
     this.annotation = annotation;
     this.name = name;
@@ -96,6 +98,7 @@ final class MethodDescriptor {
     this.allowReturnNones = allowReturnNones;
     this.useStarlarkThread = useStarlarkThread;
     this.useStarlarkSemantics = useStarlarkSemantics;
+    this.typeConstructorProxy = isTypeConstructor ? method.getReturnType() : null;
 
     Class<?> ret = method.getReturnType();
     if (ret == void.class || ret == boolean.class) {
@@ -231,7 +234,8 @@ final class MethodDescriptor {
         annotation.selfCall(),
         annotation.allowReturnNones(),
         annotation.useStarlarkThread(),
-        annotation.useStarlarkSemantics());
+        annotation.useStarlarkSemantics(),
+        annotation.isTypeConstructor());
   }
 
   private static final Object[] EMPTY = {};
@@ -395,6 +399,11 @@ final class MethodDescriptor {
 
   public StarlarkType getStarlarkType() {
     return starlarkType;
+  }
+
+  @Nullable
+  Class<?> getTypeConstructorProxy() {
+    return typeConstructorProxy;
   }
 
   /**

src/main/java/net/starlark/java/eval/MethodLibrary.java
@@ -395,7 +395,8 @@ class MethodLibrary {
               + "list((2, 3, 2)) == [2, 3, 2]\n"
               + "list({5: \"a\", 2: \"b\", 4: \"c\"}) == [5, 2, 4]</pre>",
       parameters = {@Param(name = "x", defaultValue = "[]", doc = "The object to convert.")},
-      useStarlarkThread = true)
+      useStarlarkThread = true,
+      isTypeConstructor = true)
   public StarlarkList<?> list(StarlarkIterable<?> x, StarlarkThread thread) throws EvalException {
     return StarlarkList.wrap(thread.mutability(), Starlark.toArray(x));
   }

src/main/java/net/starlark/java/eval/Module.java
@@ -256,8 +256,8 @@ public final class Module implements Resolver.Module {
   @Nullable
   public TypeConstructor getTypeConstructor(String name) throws Undefined {
     // TODO: #28325 - Remove usage of TYPE_UNIVERSE here by retrieving type constructor info
-    // from the universal symbols' dynamic values. For the moment we only do this for None.
-    if (!name.equals("None")) {
+    // from the universal symbols' dynamic values. For the moment we only do this for None and list.
+    if (!(name.equals("None") || name.equals("list"))) {
       TypeConstructor constructor = Types.TYPE_UNIVERSE.get(name);
       if (constructor != null) {
         return constructor;

src/main/java/net/starlark/java/eval/ParamDescriptor.java
@@ -232,9 +232,15 @@ final class ParamDescriptor {
     }
 
     // We can't evaluate Starlark code until UNIVERSE is bootstrapped.
-    if (Starlark.UNIVERSE == null) {
-      throw new IllegalStateException("no bootstrap value for " + name + "=" + expr);
-    }
+    Preconditions.checkState(
+        Starlark.UNIVERSE != null,
+        """
+        Attempted to evaluate a builtin method's parameter's default expr ('%s = %s'), prior to \
+        static initialization of Starlark.UNIVERSE. Either avoid this initialization cycle or else \
+        add the expr to the bootstrap list in ParamDescriptor#evalDefault.\
+        """,
+        name,
+        expr);
 
     Module module = Module.create();
     try (Mutability mu = Mutability.create("Builtin param default init")) {

src/main/java/net/starlark/java/eval/Starlark.java
@@ -928,7 +928,7 @@ public final class Starlark {
       if (desc == null) {
         throw errorf("'%s' object is not callable", type(fn));
       }
-      callable = new BuiltinFunction(fn, desc);
+      callable = BuiltinFunction.of(fn, desc, thread.getSemantics());
     }
     return callable;
   }
@@ -1013,7 +1013,7 @@ public final class Starlark {
       if (method.isStructField()) {
         return method.callField(x, semantics, mu);
       } else {
-        return new BuiltinFunction(x, method);
+        return BuiltinFunction.of(x, method, semantics);
       }
     }
 
@@ -1114,7 +1114,7 @@ public final class Starlark {
             String.format("addMethods(%s): method %s has structField=true", cls.getName(), name));
       }
 
-      env.put(name, new BuiltinFunction(v, e.getValue()));
+      env.put(name, BuiltinFunction.of(v, e.getValue(), semantics));
     }
   }
 

commit 6edc9a8b61395ca54794479d2e9fee47a68dd4b6

unbreak bazel_bootstrap_distfile_tar_test
See https://buildkite.com/bazel/bazel-bazel/builds/34619 - https://github.com/bazelbuild/bazel/commit/ed98d31d352a41595d9f4561fae501bd67a43826 caused
bazel_bootstrap_distfile_tar_test to start spuriously failing with
"error: BuiltinTypeFunction has private access in BuiltinFunction".

This appears to be a JDK 21 bug.

PiperOrigin-RevId: 868196856
Change-Id: I376d1cf6c26e2f4ec7194da9aef51ef50fa1480f

src/main/java/net/starlark/java/eval/BuiltinFunction.java
@@ -523,8 +523,17 @@ public sealed class BuiltinFunction implements StarlarkCallable
     }
   }
 
-  private static final class BuiltinTypeFunction extends BuiltinFunction
-      implements TypeConstructor {
+  /**
+   * A {@link BuiltinFunction} whose symbol is also a type constructor; for example, {@code list} is
+   * used both as a function that returns list values ({@code l = list((1, 2, 3))}) and a
+   * constructor for list types ({@code type T = list[int]}).
+   */
+  // Non-private due to what appears to be a javac bug (present at least in JDK 21) causing
+  // scripts/bootstrap/compile.sh and bazel_bootstrap_distfile_tar_test to spuriously fail with
+  // "error: BuiltinTypeFunction has private access in BuiltinFunction".
+  // TODO(bazel-team): check if we can make this class private once Bazel starts using JDK 25 or
+  // newer to bootstrap
+  static final class BuiltinTypeFunction extends BuiltinFunction implements TypeConstructor {
 
     private final StarlarkSemantics semantics;
 