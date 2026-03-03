bazelbuild/bazel

commit d34d6389adb8848e55626b4b45667c3278772c51

Use parseUnsignedLong for getNetIoCountersLinux
We had some users have this logic fail because of huge values in
`/proc/net/dev` like 10586847412485054478.

```
FATAL: bazel crashed due to an internal error. Printing stack trace:
java.lang.NumberFormatException: For input string: "10586847412485054478"
	at java.base/java.lang.NumberFormatException.forInputString(Unknown Source)
	at java.base/java.lang.Long.parseLong(Unknown Source)
	at java.base/java.lang.Long.parseLong(Unknown Source)
	at java.base/java.util.stream.ReferencePipeline$5$1.accept(Unknown Source)
	at java.base/java.util.Iterator.forEachRemaining(Unknown Source)
	at java.base/java.util.Spliterators$IteratorSpliterator.forEachRemaining(Unknown Source)
	at java.base/java.util.stream.AbstractPipeline.copyInto(Unknown Source)
	at java.base/java.util.stream.AbstractPipeline.wrapAndCopyInto(Unknown Source)
	at java.base/java.util.stream.AbstractPipeline.evaluate(Unknown Source)
	at java.base/java.util.stream.AbstractPipeline.evaluateToArrayNode(Unknown Source)
	at java.base/java.util.stream.LongPipeline.toArray(Unknown Source)
	at com.google.devtools.build.lib.profiler.SystemNetworkStatsServiceImpl.getNetIoCountersLinux(SystemNetworkStatsServiceImpl.java:64)
	at com.google.devtools.build.lib.profiler.SystemNetworkStatsServiceImpl.getNetIoCounters(SystemNetworkStatsServiceImpl.java:43)
	at com.google.devtools.build.lib.profiler.NetworkMetricsCollector.collectSystemNetworkUsages(NetworkMetricsCollector.java:66)
	at com.google.devtools.build.lib.profiler.CollectLocalResourceUsage$SystemNetworkUsageCollector.collect(CollectLocalResourceUsage.java:489)
	at com.google.devtools.build.lib.profiler.CollectLocalResourceUsage$Collector.run(CollectLocalResourceUsage.java:200)
```

This is likely because we're using [RDMA over Converged
Ethernet](https://en.wikipedia.org/wiki/RDMA_over_Converged_Ethernet)
which massively inflates the transfer stats.

These values are u64s in the kernel so we should respect that

src/main/java/com/google/devtools/build/lib/profiler/NetworkMetricsCollector.java
@@ -161,12 +161,7 @@ public final class NetworkMetricsCollector {
   }
 
   private static long calcDelta(long prev, long next) {
-    // The next could wrap, and if that happens, assume prev is 0 (best effort).
-    if (next < prev) {
-      return next;
-    } else {
-      return next - prev;
-    }
+    return next - prev;
   }
 
   private static double calcValuePerSec(long deltaValue, double deltaNanos) {

src/main/java/com/google/devtools/build/lib/profiler/SystemNetworkStatsServiceImpl.java
@@ -61,7 +61,10 @@ public class SystemNetworkStatsServiceImpl implements SystemNetworkStatsService 
       }
       String name = line.substring(0, colonAt).strip();
       long[] fields =
-          SPLITTER.splitToStream(line.substring(colonAt + 1)).mapToLong(Long::parseLong).toArray();
+          SPLITTER
+              .splitToStream(line.substring(colonAt + 1))
+              .mapToLong(Long::parseUnsignedLong)
+              .toArray();
       if (fields.length > 9) {
         long bytesRecv = fields[0];
         long packetsRecv = fields[1];

commit 6ce906590c6009279539ea56f125514a3d18e12a

Fix overflow in network metrics counters (https://github.com/bazelbuild/bazel/pull/28856)
This was broken in d34d6389adb8848e55626b4b45667c3278772c51. On macOS
subsequent network updates can reset the counters, which resulted in
overflow. Now if the new value is lower we just replace it with that.
This should be harmless for the Linux use case where this should never
be the case.

Fixes https://github.com/bazelbuild/bazel/issues/28813

Closes #28856.

PiperOrigin-RevId: 877956215
Change-Id: Ic7667494c19fe854edcc7bc8df4b845214484086

src/main/java/com/google/devtools/build/lib/profiler/NetworkMetricsCollector.java
@@ -161,6 +161,9 @@ public final class NetworkMetricsCollector {
   }
 
   private static long calcDelta(long prev, long next) {
+    if (Long.compareUnsigned(next, prev) < 0) {
+      return next;
+    }
     return next - prev;
   }
 