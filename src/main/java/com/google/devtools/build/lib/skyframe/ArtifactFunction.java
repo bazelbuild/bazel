363a74e8fb1b187a0bae9fadbad3dc69f7cb0e80
Fabian Meumertzheim <fabian@meumertzhe.im>
2026-03-06 15:45:55 +0100

    Update ArtifactFunction.java

--- src/main/java/com/google/devtools/build/lib/skyframe/ArtifactFunction.java
+++ src/main/java/com/google/devtools/build/lib/skyframe/ArtifactFunction.java
@@ -169,7 +169,7 @@ public final class ArtifactFunction implements SkyFunction {
     // actions, execute those actions in parallel and then aggregate the action execution results.
     ActionTemplate<?> actionTemplate = artifactDependencies.maybeGetTemplateActionForTreeArtifact();
     if (actionTemplate != null) {
-      if (true) {
+      if (false) {
         mkdirForTreeArtifact(artifact, env, actionTemplate);
       }
       return createTreeArtifactValueFromActionKey(artifactDependencies, env);
