bazelbuild/bazel

commit 1c114e332fd5de02bad84ebab6796c627c2a8c1f

docs: Fix many issues in generated docs (https://github.com/bazelbuild/bazel/pull/29036)
Improves the robustness of the docs2mdx converter to get more of the generated docs working with Mintlify. Also, addresses a few of the invalid HTML expressions written into reference doc strings.

RELNOTES: None

Closes #29036.

PiperOrigin-RevId: 889816861
Change-Id: Id69ca9b2fedcde04070925bb893f242097a9403e

scripts/docs/docs2mdx.py
@@ -50,6 +50,59 @@ _METADATA_PATTERN = re.compile(
     "^((Project|Book):.+\n)", re.MULTILINE
 )
 _TITLE_RE = re.compile(r"^title: '", re.MULTILINE)
+_HTML_COMMENT_RE = re.compile(r"<!--.*?-->", re.DOTALL)
+_ANGLE_BRACKET_LINK_RE = re.compile(r"<(https?://[^>]+)>")
+
+# Across code blocks and similar pre-formatted blocks, these
+# characters must be converted to HTML entities so they don't
+# look like JavaScript blocks.
+_REPLACED_JS_CHARACTERS = {
+    "{": "&lcub;",
+    "}": "&rcub;",
+}
+
+# Inside code blocks, these characters need to be converted
+# to HTML entities to prevent parser errors.
+_REPLACED_CODE_CHARACTERS = {
+    "<": "&lt;",
+    ">": "&gt;",
+    **_REPLACED_JS_CHARACTERS,
+}
+
+
+def _escape_chars(text, replacements):
+  """Escapes characters in a string.
+
+  Args:
+    text: str; string that needs characters escaped.
+    replacements: dict[str, str]; a dictionary mapping characters to escape with
+      their replacements.
+
+  Returns:
+    The escaped version of `text`.
+  """
+  for c in replacements.keys():
+    text = text.replace(c, replacements[c])
+  return text
+
+
+class AcornSafeMarkdownConverter(markdownify.MarkdownConverter):
+  """Custom converter that produces Acorn-parsable MDX output."""
+
+  def convert_code(self, node, text, parent_tags):
+    """Escape sensitive characters in code blocks so they're not mishandled."""
+    text = super().convert_code(node, text, parent_tags)
+    return _escape_chars(text, _REPLACED_CODE_CHARACTERS)
+
+  def escape(self, text, parent_tags):
+    """Custom escape handling."""
+    if not text:
+      return text
+    escaped = super().escape(text, parent_tags)
+
+    # Unescape underscores that are in the middle of words.
+    escaped = re.sub(r"(\w)\\_(\w)", r"\1_\2", escaped)
+    return _escape_chars(escaped, _REPLACED_CODE_CHARACTERS)
 
 
 def _convert_directory(root_dir, mdx_dir):
@@ -85,39 +138,77 @@ def _convert_file(src, dest):
 
 
 def _transform(path, content):
+  content = _pre_markdown_transforms(content)
   md = _html2md(content) if path.endswith(".html") else content
-  return _fix_markdown(md)
+  return _post_markdown_transforms(md)
 
 
 def _html2md(content):
-  return markdownify.markdownify(content, heading_style="ATX")
+  return AcornSafeMarkdownConverter(heading_style="ATX").convert(content)
 
 
-def _fix_markdown(content):
-  """Turns the given md(x) input into valid mdx.
+def _pre_markdown_transforms(content):
+  """Transforms applied to all sources before any markdown conversion.
 
   Args:
-    content: str; content of an .md(x) file.
+    content: str; content of an HTML or .md file.
+
   Returns:
-    The content as valid mdx (str).
+    The file with invalid content removed.
   """
   no_tags = _TAG_RE.sub("", content)
+  no_comments = _HTML_COMMENT_RE.sub("", no_tags)
   # Remove Project: and Book: lines
-  no_metadata = _METADATA_PATTERN.sub("", no_tags, count=2).lstrip()
+  no_metadata = _METADATA_PATTERN.sub("", no_comments, count=2).lstrip()
   no_templates = _TEMPLATE_RE.sub("", no_metadata)
-  no_html_links = _HTML_LINK_RE.sub(_fix_link, no_templates)
-  fixed_headings = (
-      no_html_links
-      if _TITLE_RE.search(no_html_links)
-      else _HEADING_RE.sub("---\ntitle: '\\1'\n---", no_html_links, count=1)
+  escaped_pre = re.sub(
+      r"(?:<pre>)(.*?)(?:</pre>)",
+      _escape_chars_in_pre_blocks,
+      no_templates,
+      flags=re.DOTALL,
   )
-  no_double_empty_lines = fixed_headings.replace("\n\n\n", "\n\n")
+  fixed_headings = (
+      escaped_pre
+      if _TITLE_RE.search(escaped_pre)
+      else _HEADING_RE.sub("---\ntitle: '\\1'\n---", escaped_pre, count=1)
+  )
+  return fixed_headings
+
+
+def _post_markdown_transforms(content):
+  """Transforms applied to all sources after any markdown conversion.
+
+  Args:
+    content: str; content of a converted .mdx file.
+
+  Returns:
+    The content as fully valid .mdx.
+  """
+  no_html_links = _HTML_LINK_RE.sub(_fix_link, content)
+  no_angle_links = _ANGLE_BRACKET_LINK_RE.sub(r"\1", no_html_links)
+  no_double_empty_lines = no_angle_links.replace("\n\n\n", "\n\n")
   return _remove_trailing_whitespaces(no_double_empty_lines)
 
 
 def _remove_trailing_whitespaces(content):
   lines = (l.rstrip() for l in content.split("\n"))
   return "\n".join(lines)
+
+
+def _escape_chars_in_pre_blocks(matches):
+  """Escapes characters in <pre> blocks that cause mdx parse errors.
+
+  Because some <pre> blocks contain valid HTML elements (e.g. links), < and >
+  are not escaped.
+
+  Args:
+    matches: re.Match; an object matching a <pre> block and its content.
+
+  Returns:
+    The <pre> block with properly escaped content.
+  """
+  content = _escape_chars(matches.group(1), _REPLACED_JS_CHARACTERS)
+  return f"<pre>{content}</pre>"
 
 
 def _fix_link(m):

docs/reference/test-encyclopedia.mdx
@@ -449,8 +449,7 @@ The initial environment block shall be composed as follows:
   </tr>
   <tr>
     <td><code>XML_OUTPUT_FILE</code></td>
-    <td>
-      Location to which test actions should write a test result XML output file.
+    <td>Location to which test actions should write a test result XML output file.
       Otherwise, Bazel generates a default XML output file wrapping the test log
       as part of the test action. The XML schema is based on the
       <a href="https://windyroad.com.au/dl/Open%20Source/JUnit.xsd"

src/main/java/com/google/devtools/build/lib/starlarkbuildapi/StarlarkNativeModuleApi.java
@@ -283,8 +283,8 @@ public interface StarlarkNativeModuleApi extends StarlarkValue {
               + " function. <p>The result of this function is the same <code>Label</code> value as"
               + " would be produced by passing the given string to a label-valued attribute of a"
               + " target declared in the BUILD file. <p><i>Usage note:</i> The difference between"
-              + " this function and <a href='../builtins/Label.html#Label'>Label()</a></code> is"
-              + " that <code>Label()</code> uses the context of the package of the"
+              + " this function and <a href='../builtins/Label.html#Label'><code>Label()</code>"
+              + "</a> is that <code>Label()</code> uses the context of the package of the"
               + " <code>.bzl</code> file that called it, not the package of the <code>BUILD</code>"
               + " file. Use <code>Label()</code> when you need to refer to a fixed target that is"
               + " hardcoded into the macro, such as a compiler. Use"

src/main/java/com/google/devtools/build/lib/starlarkbuildapi/StarlarkRuleContextApi.java
@@ -672,7 +672,7 @@ public interface StarlarkRuleContextApi<ConstraintValueT extends ConstraintValue
           already a <code>Label</code>, it is returned unchanged.<p>The result of this function is \
           the same <code>Label</code> value as would be produced by passing the given string to a \
           label-valued attribute of the rule and accessing the corresponding \
-          <a href='../builtins/Target.html#label><code>label</code></a> field.
+          <a href='../builtins/Target.html#label'><code>label</code></a> field.
           <p><i>Usage note:</i> The difference between this function and \
           <a href='../builtins/Label.html#Label'>Label()</a></code> is \
           that <code>Label()</code> uses the context of the package of the <code>.bzl</code> file \

src/main/java/com/google/devtools/build/lib/starlarkbuildapi/StarlarkRuleFunctionsApi.java
@@ -248,7 +248,7 @@ called.
 <p>The following APIs are unavailable within a macro implementation function and any Starlark
 function it transitively calls:
 <ul>
-  <li><a href="/reference/be/functions#package"><code>package()</code>, <code>licenses()</code>
+  <li><a href="/reference/be/functions#package"><code>package()</code>, <code>licenses()</code></a>
   <li><code>environment_group()</code>
   <li><a href="../toplevel/native#glob"><code>native.glob()</code></a> &ndash; instead, you may pass
     a glob into the macro via a label list attribute
@@ -259,7 +259,7 @@ function it transitively calls:
   <li>(for <code>WORKSPACE</code> threads)
     <a href="../globals/workspace#workspace"><code>workspace()</code></a>,
     <a href="../globals/workspace#register_toolchains"><code>register_toolchains()</code></a>,
-    <a href="../globals/workspace#register_execution_platforms><code>register_execution_platforms()</code></a>,
+    <a href="../globals/workspace#register_execution_platforms"><code>register_execution_platforms()</code></a>,
     <a href="../globals/workspace#bind"><code>bind()</code></a>, repository rule instantiation
 </ul>
 """),
@@ -732,9 +732,9 @@ declared.
                     + " <code>executable</code> and <code>test</code> from the parent. Values of"
                     + " <code>fragments</code>, <code>toolchains</code>,"
                     + " <code>exec_compatible_with</code>, and <code>exec_groups</code> are"
-                    + " merged. Legacy or deprecated parameters may not be set. Incoming "
-                    + "configuration transition <code>cfg</code> of parent is applied after this"
-                    + "rule's incoming configuration."),
+                    + " merged. Legacy or deprecated parameters may not be set. Incoming"
+                    + " configuration transition <code>cfg</code> of parent is applied after this"
+                    + " rule's incoming configuration."),
         @Param(
             name = "extendable",
             named = true,

commit e602c70edfbf1f4e779393851e5318989234b654

Bazel Docs: Improve & fix docs2mdx.
Key changes:
- Move header fix from `_pre_markdown_transforms` to `_post_markdown_transforms` so that HTML files are fixed, too. This was a regression introduced by https://github.com/bazelbuild/bazel/commit/1c114e332fd5de02bad84ebab6796c627c2a8c1f.
- Delete anything before the front matter: some files still contain HTML tags / DevSite metadata at the very top.
- Remove CSS <style> sections.

PiperOrigin-RevId: 893137232
Change-Id: Iab5e053a17cc4a7a1769ecf4bc5adddd3a648e3f

scripts/docs/docs2mdx.py
@@ -52,6 +52,9 @@ _METADATA_PATTERN = re.compile(
 _TITLE_RE = re.compile(r"^title: '", re.MULTILINE)
 _HTML_COMMENT_RE = re.compile(r"<!--.*?-->", re.DOTALL)
 _ANGLE_BRACKET_LINK_RE = re.compile(r"<(https?://[^>]+)>")
+_HTML_PRE_PATTERN = re.compile(r"(?:<pre>)(.*?)(?:</pre>)")
+_HTML_STYLE_PATTERN = re.compile(r"^</?style>", re.MULTILINE)
+_MD_FRONT_MATTER_PATTERN = re.compile(r"^---", re.MULTILINE)
 
 # Across code blocks and similar pre-formatted blocks, these
 # characters must be converted to HTML entities so they don't
@@ -161,18 +164,11 @@ def _pre_markdown_transforms(content):
   # Remove Project: and Book: lines
   no_metadata = _METADATA_PATTERN.sub("", no_comments, count=2).lstrip()
   no_templates = _TEMPLATE_RE.sub("", no_metadata)
-  escaped_pre = re.sub(
-      r"(?:<pre>)(.*?)(?:</pre>)",
+  return _HTML_PRE_PATTERN.sub(
       _escape_chars_in_pre_blocks,
       no_templates,
-      flags=re.DOTALL,
+      re.DOTALL,
   )
-  fixed_headings = (
-      escaped_pre
-      if _TITLE_RE.search(escaped_pre)
-      else _HEADING_RE.sub("---\ntitle: '\\1'\n---", escaped_pre, count=1)
-  )
-  return fixed_headings
 
 
 def _post_markdown_transforms(content):
@@ -187,12 +183,46 @@ def _post_markdown_transforms(content):
   no_html_links = _HTML_LINK_RE.sub(_fix_link, content)
   no_angle_links = _ANGLE_BRACKET_LINK_RE.sub(r"\1", no_html_links)
   no_double_empty_lines = no_angle_links.replace("\n\n\n", "\n\n")
-  return _remove_trailing_whitespaces(no_double_empty_lines)
+  no_trailing_whitespaces = _remove_trailing_whitespaces(no_double_empty_lines)
+  fixed_headings = (
+      no_trailing_whitespaces
+      if _TITLE_RE.search(no_trailing_whitespaces)
+      else _HEADING_RE.sub(_fix_title_heading, no_trailing_whitespaces, count=1)
+  )
+  front_matter_first = _remove_anything_before_front_matter(fixed_headings)
+  return _remove_style_sections(front_matter_first)
 
 
 def _remove_trailing_whitespaces(content):
   lines = (l.rstrip() for l in content.split("\n"))
   return "\n".join(lines)
+
+
+def _fix_title_heading(m):
+  title = m.group(1).replace("'", "\\'")
+  return f"---\ntitle: '{title}'\n---"
+
+
+def _remove_anything_before_front_matter(content):
+  if content.startswith("---\n"):
+    return content
+
+  parts = _MD_FRONT_MATTER_PATTERN.split(content, maxsplit=1)
+  if len(parts) == 1:
+    # Technically this only affects files that we need for the old site,
+    # so the better solution would be to stop generating them.
+    return parts[0]
+
+  return f"---{parts[1]}"
+
+
+def _remove_style_sections(content):
+  m = _HTML_STYLE_PATTERN.search(content)
+  if not m:
+    return content
+
+  parts = _HTML_STYLE_PATTERN.split(content)
+  return f"{parts[0]}{parts[2].lstrip()}"
 
 
 def _escape_chars_in_pre_blocks(matches):