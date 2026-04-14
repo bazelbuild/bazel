a4b6d38438f87f5dd43420fba027158e2b26fee8
Keith Smiley <keithbsmiley@gmail.com>
2026-04-13 17:16:11 -0700

    Remove incompatible_use_specific_tool_files
    This was flipped before bazel 1.0 in 030ca7fb98c56bfc63f5dc9e61e6a89c4b274293

--- src/main/java/com/google/devtools/build/lib/rules/cpp/CppCompileActionBuilder.java
+++ src/main/java/com/google/devtools/build/lib/rules/cpp/CppCompileActionBuilder.java
@@ -290,8 +290,7 @@ public final class CppCompileActionBuilder implements StarlarkValue {
     addTransitiveMandatoryInputs(
         getShouldScanIncludes()
             ? compilerFilesWithoutIncludes
-            : configuration.getFragment(CppConfiguration.class).useSpecificToolFiles()
-                    && !getSourceFile().isTreeArtifact()
+            : !getSourceFile().isTreeArtifact()
                 ? (getActionName().equals(CppActionNames.ASSEMBLE)
                     ? ccToolchain.getAsFiles()
                     : ccToolchain.getCompilerFiles())
--- src/test/java/com/google/devtools/build/lib/rules/objc/ObjcLibraryTest.java
+++ src/test/java/com/google/devtools/build/lib/rules/objc/ObjcLibraryTest.java
@@ -2422,8 +2422,6 @@ public class ObjcLibraryTest extends ObjcRuleTestCase {
             srcs = ["a.S"],
         )
         """);
-    useConfiguration("--incompatible_use_specific_tool_files");
-
     ConfiguredTarget target = getConfiguredTarget("//a:a");
     CcToolchainProvider toolchainProvider = CcToolchainProvider.getFromTarget(target);
 
