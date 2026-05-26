bazelbuild/bazel

commit 424a38da6fc7f287b27b5781926176266688af60

In query's BUILD formatter, output multiline attributes in human readable form.
RELNOTES:
In output of `query --output=build`, string attributes containing newline characters are now enclosed in triple double quotes (""") for better readability.
PiperOrigin-RevId: 915114452
Change-Id: I593f584bdb6bf145115a094dc28068d5549c1196

src/main/java/com/google/devtools/build/lib/query2/query/output/BuildOutputFormatter.java
@@ -196,11 +196,16 @@ public class BuildOutputFormatter extends AbstractUnorderedFormatter {
       } else if (value instanceof TriState triState) {
         value = triState.toInt();
       }
+
       return new Printer() {
         // Print labels in their canonical form.
         @Override
         public Printer repr(Object o, StarlarkSemantics semantics) {
-          return super.repr(o instanceof Label label ? labelPrinter.toString(label) : o, semantics);
+          return switch (o) {
+            case String str -> appendPrettyQuoted(str);
+            case Label label -> super.repr(labelPrinter.toString(label), semantics);
+            default -> super.repr(o, semantics);
+          };
         }
       }.repr(value, StarlarkSemantics.DEFAULT).toString();
     }

src/main/java/net/starlark/java/eval/Printer.java
@@ -13,6 +13,8 @@
 // limitations under the License.
 package net.starlark.java.eval;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import java.util.Arrays;
 import java.util.IllegalFormatException;
@@ -36,6 +38,9 @@ public class Printer {
   // indicating a cycle.
   private Object[] stack;
   private int depth;
+
+  private static final char Q1 = '\'';
+  private static final char Q2 = '"';
 
   /** Creates a printer that writes to the given buffer. */
   public Printer(StringBuilder buffer) {
@@ -228,6 +233,74 @@ public class Printer {
       escapeCharacter(c);
     }
     return this.append('"');
+  }
+
+  @CanIgnoreReturnValue
+  private Printer appendTripleQuoted(String s, char quoteChar) {
+    checkArgument(quoteChar == Q1 || quoteChar == Q2, "quoteChar must be ' or \"");
+    String delimiter = String.valueOf(quoteChar).repeat(3);
+    this.append(delimiter);
+    char otherQuote = (quoteChar == Q2 ? Q1 : Q2);
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      if (c == otherQuote || c == '\n') {
+        this.append(c);
+      } else if (c == quoteChar) {
+        // Escape quoteChar only if it would otherwise be interpreted as the start of the closing
+        // delimiter:
+        if (i + 1 == s.length()) {
+          // ... if it immediately precedes the closing delimiter
+          this.append('\\').append(c);
+        } else if (i + 3 <= s.length() && s.substring(i, i + 3).equals(delimiter)) {
+          // ... or if it's part of an embedded triple-quote substring.
+          this.append('\\').append(c);
+        } else {
+          this.append(c);
+        }
+      } else {
+        escapeCharacter(c);
+      }
+    }
+    return this.append(delimiter);
+  }
+
+  /**
+   * Appends a "pretty" quoted string representation of {@code s} to the printer's buffer.
+   *
+   * <p>It heuristically chooses between single-line double quotes, and triple quotes (either ''' or
+   * """) based on the content of the string to minimize escaping.
+   */
+  @CanIgnoreReturnValue
+  public final Printer appendPrettyQuoted(String s) {
+    if (!s.contains("\n")) {
+      return appendQuoted(s);
+    }
+    return appendTripleQuoted(s, determineQuoteChar(s));
+  }
+
+  private static char determineQuoteChar(String s) {
+    int doubleQuotes = 0;
+    int singleQuotes = 0;
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      if (c == Q2) {
+        doubleQuotes++;
+      } else if (c == Q1) {
+        singleQuotes++;
+      }
+    }
+
+    char quoteChar = Q2;
+    boolean startsOrEndsWithDouble = s.startsWith("\"") || s.endsWith("\"");
+    boolean startsOrEndsWithSingle = s.startsWith("'") || s.endsWith("'");
+
+    if (doubleQuotes > singleQuotes) {
+      quoteChar = Q1;
+    } else if (startsOrEndsWithDouble && !startsOrEndsWithSingle) {
+      quoteChar = Q1;
+    }
+
+    return quoteChar;
   }
 
   @CanIgnoreReturnValue

src/test/java/net/starlark/java/eval/PrinterTest.java
@@ -28,8 +28,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 /**
- *  Test properties of the evaluator's datatypes and utility functions
- *  without actually creating any parse trees.
+ * Test properties of the evaluator's datatypes and utility functions without actually creating any
+ * parse trees.
  */
 @RunWith(JUnit4.class)
 public class PrinterTest {
@@ -117,13 +117,13 @@ public class PrinterTest {
     assertThat(formatWithList("", Tuple.of())).isEmpty();
     assertThat(format("%s", "foo")).isEqualTo("foo");
     assertThat(format("%s", 3.14159)).isEqualTo("3.14159");
-    checkFormatPositionalFails("not all arguments converted during string formatting",
-        "%s", 1, 2, 3);
+    checkFormatPositionalFails(
+        "not all arguments converted during string formatting", "%s", 1, 2, 3);
     assertThat(format("%%%s", "foo")).isEqualTo("%foo");
-    checkFormatPositionalFails("not all arguments converted during string formatting",
-        "%%s", "foo");
-    checkFormatPositionalFails("unsupported format character \" \" at index 1 in \"% %s\"",
-        "% %s", "foo");
+    checkFormatPositionalFails(
+        "not all arguments converted during string formatting", "%%s", "foo");
+    checkFormatPositionalFails(
+        "unsupported format character \" \" at index 1 in \"% %s\"", "% %s", "foo");
     assertThat(
             format(
                 "%s",
@@ -138,11 +138,92 @@ public class PrinterTest {
     checkFormatPositionalFails("got string for '%d' format, want int or float", "%d", "1");
     checkFormatPositionalFails(
         "unsupported format character \".\" at index 1 in \"%.3g\"", "%.3g", 1);
-    checkFormatPositionalFails("unsupported format character \".\" at index 1 in \"%.3g\"",
-        "%.3g", 1, 2);
+    checkFormatPositionalFails(
+        "unsupported format character \".\" at index 1 in \"%.3g\"", "%.3g", 1, 2);
     checkFormatPositionalFails(
         "unsupported format character \".\" at index 1 in \"%.s\"", "%.s", 1);
     checkFormatPositionalFails("not enough arguments for format pattern \"%.s\": ()", "%.s");
+  }
+
+  private static String prettyQuoted(String s) {
+    StringBuilder sb = new StringBuilder();
+    new Printer(sb).appendPrettyQuoted(s);
+    return sb.toString();
+  }
+
+  @Test
+  public void testPrettyQuoted() throws Exception {
+    // Single-line strings should use ordinary double quotes.
+    assertThat(prettyQuoted("foo")).isEqualTo("\"foo\"");
+    assertThat(prettyQuoted("foo\"bar")).isEqualTo("\"foo\\\"bar\"");
+    assertThat(prettyQuoted("foo\\bar")).isEqualTo("\"foo\\\\bar\"");
+
+    // Multiline default is triple double quotes
+    assertThat(prettyQuoted("one\ntwo"))
+        .isEqualTo(
+            """
+            \"\"\"one
+            two\"\"\"\
+            """);
+
+    // Escaping inside triple double quotes
+    // Double quotes should be escaped (force """ by having more single quotes)
+    assertThat(prettyQuoted("one\"two 'three' 'four'\nfive"))
+        .isEqualTo(
+            """
+            \"\"\"one"two 'three' 'four'
+            five\"\"\"\
+            """);
+    // Backslashes should be escaped
+    // Escaping inside triple double quotes (backslashes, control characters, hex escapes)
+    assertThat(prettyQuoted("one\\two\none\rtwo\ntabs\tthree\none\u0001two\nthree"))
+        .isEqualTo(
+            """
+            \"\"\"one\\\\two
+            one\\rtwo
+            tabs\\tthree
+            one\\x01two
+            three\"\"\"\
+            """);
+
+    // Heuristic: switch to triple single quotes (''') if double quotes are dominant
+    assertThat(prettyQuoted("one\"two\"three\nfour"))
+        .isEqualTo(
+            """
+            '''one"two"three
+            four'''\
+            """);
+
+    // Escaping inside triple single quotes (''')
+    // Single quotes should be escaped
+    assertThat(prettyQuoted("one\"two\" 'three\nfour"))
+        .isEqualTo(
+            """
+            '''one"two" 'three
+            four'''\
+            """);
+
+    // Heuristic: switch to ''' if starting/ending with double quote (boundary issue)
+    assertThat(prettyQuoted("\"one\ntwo"))
+        .isEqualTo(
+            """
+            '''"one
+            two'''\
+            """);
+    assertThat(prettyQuoted("one\ntwo\""))
+        .isEqualTo(
+            """
+            '''one
+            two"'''\
+            """);
+
+    // Boundary with single quote: keep triple double quotes
+    assertThat(prettyQuoted("'one\ntwo"))
+        .isEqualTo(
+            """
+            \"\"\"'one
+            two\"\"\"\
+            """);
   }
 
   private StarlarkValue createObjWithStr() {

commit 715c77502f3e72e99ca7ab3c4240489b835f7c3d

Fix `query --output=build` handling of None-valued selects on Label-type attrs
`BuildType.Selector` saves `None` selector branch values as `null` internally
if the type's default value is `null`; this is the case for e.g. `attr.label`
and `attr.output`.

In this case, we want to format the value as `None` when printing in BUILD
format, as contrasted with crashing with NPE (which is what started happening
after https://github.com/bazelbuild/bazel/commit/424a38da6fc7f287b27b5781926176266688af60) or printing `"null"` (which we had been doing
before that).

PiperOrigin-RevId: 921589687
Change-Id: I49ec5ec4db187a3179db1b123ce9d4278f4c4a2f

src/main/java/com/google/devtools/build/lib/query2/query/output/BuildOutputFormatter.java
@@ -41,6 +41,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.function.BiPredicate;
 import net.starlark.java.eval.Printer;
+import net.starlark.java.eval.Starlark;
 import net.starlark.java.eval.StarlarkSemantics;
 import net.starlark.java.eval.StarlarkThread;
 
@@ -204,6 +205,9 @@ public class BuildOutputFormatter extends AbstractUnorderedFormatter {
           return switch (o) {
             case String str -> appendPrettyQuoted(str);
             case Label label -> super.repr(labelPrinter.toString(label), semantics);
+            // Nulls can appear e.g. from BuildType.Selector#mapCopy in `reconsructSelect`; a None
+            // value will be mapped to null if the attr type's default value is null.
+            case null -> super.repr(Starlark.NONE, semantics);
             default -> super.repr(o, semantics);
           };
         }