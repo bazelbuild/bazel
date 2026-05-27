bazelbuild/bazel

commit 6956d3c242455c6bb5a6917dd445fd5c1be64603

Prevent production code from loading .bzl/.scl files defined in prototype packages.
This feature is specific to Google's monorepo.

PiperOrigin-RevId: 921613485
Change-Id: I2379d33c93148cc73d73b876767ad5f50d860593

src/main/java/com/google/devtools/build/lib/skyframe/BzlLoadFunction.java
@@ -791,6 +791,8 @@ public class BzlLoadFunction implements SkyFunction {
             programLoads,
             pkg,
             ruleClassProvider::isPackageUnderExperimental,
+            ruleClassProvider::isPackageUnderPrototypes,
+            ruleClassProvider::mayPackageDependOnPrototypes,
             builtins.starlarkSemantics.getBool(BuildLanguageOptions.ALLOW_EXPERIMENTAL_LOADS),
             repoMapping,
             key.isSclDialect(),
@@ -1069,13 +1071,14 @@ public class BzlLoadFunction implements SkyFunction {
       ImmutableList<Pair<String, Location>> loads,
       PackageIdentifier base,
       Predicate<PackageIdentifier> isUnderExperimental,
+      Predicate<PackageIdentifier> isUnderPrototypes,
+      Predicate<PackageIdentifier> mayDependOnPrototypes,
       boolean allowExperimentalLoads,
       RepositoryMapping repoMapping,
       boolean withinSclDialect,
       boolean isSclFlagEnabled,
       @Nullable Label.RepoMappingRecorder repoMappingRecorder) {
     boolean ok = true;
-    boolean baseWithinExperimental = isUnderExperimental.test(base);
 
     ImmutableList.Builder<Label> loadLabels = ImmutableList.builderWithExpectedSize(loads.size());
     for (Pair<String, Location> load : loads) {
@@ -1103,14 +1106,22 @@ public class BzlLoadFunction implements SkyFunction {
             /* withinSclDialect= */ withinSclDialect,
             /* mentionSclInErrorMessage= */ isSclFlagEnabled);
         if (!allowExperimentalLoads
-            && !baseWithinExperimental
-            && isUnderExperimental.test(label.getPackageIdentifier())) {
+            && isUnderExperimental.test(label.getPackageIdentifier())
+            && !isUnderExperimental.test(base)) {
           throw new LabelSyntaxException(
               """
               Cannot load an experimental Starlark file from a non-experimental package.
               Consider moving the loaded file to a non-experimental package.
               To temporarily bypass this error, use --allow_experimental_loads.
               """);
+        }
+        if (isUnderPrototypes.test(label.getPackageIdentifier())
+            && !isUnderExperimental.test(base)
+            && !isUnderPrototypes.test(base)
+            && !mayDependOnPrototypes.test(base)) {
+          throw new LabelSyntaxException(
+              "Cannot load a Starlark file under prototypes from a non-experimental, non-prototypes"
+                  + " package. Consider moving the loaded file to a non-prototype package.");
         }
         loadLabels.add(label);
       } catch (LabelSyntaxException ex) {
@@ -1133,6 +1144,8 @@ public class BzlLoadFunction implements SkyFunction {
       ImmutableList<Pair<String, Location>> loads,
       PackageIdentifier base,
       Predicate<PackageIdentifier> isUnderExperimental,
+      Predicate<PackageIdentifier> isUnderPrototypes,
+      Predicate<PackageIdentifier> mayDependOnPrototypes,
       RepositoryMapping repoMapping,
       StarlarkSemantics starlarkSemantics) {
     return getLoadLabels(
@@ -1140,6 +1153,8 @@ public class BzlLoadFunction implements SkyFunction {
         loads,
         base,
         isUnderExperimental,
+        isUnderPrototypes,
+        mayDependOnPrototypes,
         /* allowExperimentalLoads= */ starlarkSemantics.getBool(
             BuildLanguageOptions.ALLOW_EXPERIMENTAL_LOADS),
         repoMapping,

src/main/java/com/google/devtools/build/lib/skyframe/PackageFunction.java
@@ -1103,6 +1103,8 @@ public abstract class PackageFunction implements SkyFunction {
                 programLoads,
                 packageId,
                 packageFactory.getRuleClassProvider()::isPackageUnderExperimental,
+                packageFactory.getRuleClassProvider()::isPackageUnderPrototypes,
+                packageFactory.getRuleClassProvider()::mayPackageDependOnPrototypes,
                 repositoryMapping,
                 starlarkBuiltinsValue.starlarkSemantics);
         if (loadLabels == null) {

src/main/java/com/google/devtools/build/lib/analysis/ConfiguredRuleClassProvider.java
@@ -743,6 +743,11 @@ public /*final*/ class ConfiguredRuleClassProvider
   }
 
   @Override
+  public boolean mayPackageDependOnPrototypes(PackageIdentifier packageIdentifier) {
+    return prerequisiteValidator.mayDependOnPrototypes(packageIdentifier);
+  }
+
+  @Override
   public String getRunfilesPrefix() {
     return runfilesPrefix;
   }

src/main/java/com/google/devtools/build/lib/analysis/RuleContext.java
@@ -154,6 +154,14 @@ public class RuleContext extends TargetContext
      * of prototypes may not depend on packages that are in prototypes.
      */
     public abstract boolean packageUnderPrototypes(PackageIdentifier packageIdentifier);
+
+    /**
+     * Returns whether the given package is allowed to depend on prototype packages. (If the given
+     * package is itself an experimental or prototype package, this method's result is ignored.)
+     */
+    default boolean mayDependOnPrototypes(PackageIdentifier packageIdentifier) {
+      return false;
+    }
   }
 
   public static final String TOOLCHAIN_ATTR_NAME = "$toolchain";

src/main/java/com/google/devtools/build/lib/packages/RuleClassProvider.java
@@ -36,6 +36,12 @@ public interface RuleClassProvider extends RuleDefinitionEnvironment {
   /** Returns true if a package location is considered to be under prototypes. */
   boolean isPackageUnderPrototypes(PackageIdentifier packageIdentifier);
 
+  /**
+   * Returns true if the given non-experimental, non-prototype package is allowed to depend on (via
+   * target visibility or .bzl load visibility) prototype packages.
+   */
+  boolean mayPackageDependOnPrototypes(PackageIdentifier packageIdentifier);
+
   /** The runfiles prefix. */
   String getRunfilesPrefix();
 

commit 98793561498742ec88b49ab5bca4132e052828f5

Correctly enforce load visibility for prototypes packages
This fixes a bug introduced in https://github.com/bazelbuild/bazel/commit/6956d3c242455c6bb5a6917dd445fd5c1be64603 that would allow load visibility violations across two prototypes packages.

Also clarify some flag docstrings.

PiperOrigin-RevId: 922206838
Change-Id: Id715c6d892b966bdce62c2cf6255998ce9abcb46

src/main/java/com/google/devtools/build/lib/skyframe/BUILD
@@ -226,7 +226,6 @@ java_library(
         ":workspace_status_value",
         "//src/main/java/com/google/devtools/build/lib/actions",
         "//src/main/java/com/google/devtools/build/lib/actions:action_execution_status_reporter",
-        "//src/main/java/com/google/devtools/build/lib/actions:action_input_prefetcher",
         "//src/main/java/com/google/devtools/build/lib/actions:action_lookup_data",
         "//src/main/java/com/google/devtools/build/lib/actions:action_lookup_key",
         "//src/main/java/com/google/devtools/build/lib/actions:action_output_directory_helper",

src/main/java/com/google/devtools/build/lib/skyframe/BzlLoadFunction.java
@@ -826,20 +826,17 @@ public class BzlLoadFunction implements SkyFunction {
     // Validate that the current .bzl file satisfies each loaded dependency's load visibility.
     // Violations are reported as error events (since there can be more than one in a single file)
     // and also trigger a BzlLoadFailedException.
-    //
-    // Experimental and prototype code is exempted from load visibility.
-    if (!ruleClassProvider.isPackageUnderExperimental(pkg)
-        && !ruleClassProvider.isPackageUnderPrototypes(pkg)) {
-      checkLoadVisibilities(
-          pkg,
-          "module " + label.getCanonicalForm(),
-          loadValues,
-          loadKeys,
-          programLoads,
-          /* demoteErrorsToWarnings= */ !builtins.starlarkSemantics.getBool(
-              BuildLanguageOptions.CHECK_BZL_VISIBILITY),
-          env.getListener());
-    }
+    checkLoadVisibilities(
+        pkg,
+        "module " + label.getCanonicalForm(),
+        loadValues,
+        loadKeys,
+        programLoads,
+        /* demoteErrorsToWarnings= */ !builtins.starlarkSemantics.getBool(
+            BuildLanguageOptions.CHECK_BZL_VISIBILITY),
+        ruleClassProvider::isPackageUnderExperimental,
+        ruleClassProvider::isPackageUnderPrototypes,
+        env.getListener());
 
     // Accumulate a transitive digest of the bzl file, the digests of its direct loads, and the
     // digest of the @_builtins pseudo-repository (if applicable).
@@ -1295,13 +1292,28 @@ public class BzlLoadFunction implements SkyFunction {
       List<BzlLoadValue.Key> loadKeys,
       List<Pair<String, Location>> programLoads,
       boolean demoteErrorsToWarnings,
+      Predicate<PackageIdentifier> isUnderExperimental,
+      Predicate<PackageIdentifier> isUnderPrototype,
       EventHandler handler)
       throws BzlLoadFailedException {
+    if (isUnderExperimental.test(requestingPackage)) {
+      // Experimental code is exempted from load visibility.
+      return;
+    }
+    boolean requestingIsPrototype = isUnderPrototype.test(requestingPackage);
+
     boolean foundViolation = false;
     for (int i = 0; i < loadValues.size(); i++) {
-      BzlVisibility loadVisibility = loadValues.get(i).getBzlVisibility();
       Label loadLabel = loadKeys.get(i).getLabel();
       PackageIdentifier loadPackage = loadLabel.getPackageIdentifier();
+      if (requestingIsPrototype && !isUnderPrototype.test(loadPackage)) {
+        // Prototypes can always load from normal packages; there's no load-visibility equivalent
+        // flag for --check_visibility_for_prototypes. But load visibility is still enforced between
+        // two prototypes packages (possibly demoted to a warning, below).
+        continue;
+      }
+
+      BzlVisibility loadVisibility = loadValues.get(i).getBzlVisibility();
       if (!(requestingPackage.equals(loadPackage)
           || loadVisibility.allowsPackage(requestingPackage))) {
         Location loc = programLoads.get(i).second;

src/main/java/com/google/devtools/build/lib/skyframe/PackageFunction.java
@@ -101,6 +101,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import javax.annotation.Nullable;
 import net.starlark.java.eval.EvalException;
 import net.starlark.java.eval.Module;
@@ -701,7 +702,8 @@ public abstract class PackageFunction implements SkyFunction {
       List<BzlLoadValue.Key> keys,
       StarlarkSemantics semantics,
       @Nullable BzlLoadFunction bzlLoadFunctionForInlining,
-      boolean checkVisibility,
+      Predicate<PackageIdentifier> isUnderExperimental,
+      Predicate<PackageIdentifier> isUnderPrototype,
       ActionOnFilesystemErrorCodeLoadingBzlFile actionOnFilesystemErrorCodeLoadingBzlFile)
       throws NoSuchPackageException, InterruptedException {
     List<BzlLoadValue> bzlLoads;
@@ -714,17 +716,17 @@ public abstract class PackageFunction implements SkyFunction {
         return null; // Skyframe deps unavailable
       }
       // Validate that the current BUILD file satisfies each loaded dependency's load visibility.
-      if (checkVisibility) {
-        BzlLoadFunction.checkLoadVisibilities(
-            packageId,
-            requestingFileDescription,
-            bzlLoads,
-            keys,
-            programLoads,
-            /* demoteErrorsToWarnings= */ !semantics.getBool(
-                BuildLanguageOptions.CHECK_BZL_VISIBILITY),
-            env.getListener());
-      }
+      BzlLoadFunction.checkLoadVisibilities(
+          packageId,
+          requestingFileDescription,
+          bzlLoads,
+          keys,
+          programLoads,
+          /* demoteErrorsToWarnings= */ !semantics.getBool(
+              BuildLanguageOptions.CHECK_BZL_VISIBILITY),
+          isUnderExperimental,
+          isUnderPrototype,
+          env.getListener());
     } catch (BzlLoadFailedException e) {
       Throwable rootCause = Throwables.getRootCause(e);
       PackageFunctionException.Builder exceptionBuilder =
@@ -1134,10 +1136,6 @@ public abstract class PackageFunction implements SkyFunction {
         } catch (LabelSyntaxException e) {
           throw new IllegalStateException("Failed to construct label representing BUILD file", e);
         }
-        boolean isUnderExperimental =
-            packageFactory.getRuleClassProvider().isPackageUnderExperimental(packageId);
-        boolean isUnderPrototypes =
-            packageFactory.getRuleClassProvider().isPackageUnderPrototypes(packageId);
         try {
           loadedModules =
               loadBzlModules(
@@ -1148,7 +1146,8 @@ public abstract class PackageFunction implements SkyFunction {
                   keys.build(),
                   starlarkBuiltinsValue.starlarkSemantics,
                   bzlLoadFunctionForInlining,
-                  /* checkVisibility= */ !(isUnderExperimental || isUnderPrototypes),
+                  packageFactory.getRuleClassProvider()::isPackageUnderExperimental,
+                  packageFactory.getRuleClassProvider()::isPackageUnderPrototypes,
                   actionOnFilesystemErrorCodeLoadingBzlFile);
         } catch (NoSuchPackageException e) {
           throw new PackageFunctionException(e, Transience.PERSISTENT);