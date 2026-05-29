4d4c00b1681b734e285c233e790603e91fbe9b5c
Fabian Meumertzheim <fabian@meumertzhe.im>
2026-05-29 20:34:23 +0200

    [9.1.1] Cache CAS interactions at lower layer to fix lost input handling (#29677)
    The error handling for uploads in `UploadTask` depends on the exec path
    of the uploaded file, which wasn't part of the `casUploadCache` cache
    key. Fix this by moving deduplication logic for CAS uploads and
    `FindMissing` calls into `RemoteCacheClient`.
    
    Avoids spurious Bazel failures caused by lost inputs that can't be
    recovered from via build or action rewinding due to
    `CacheNotFoundException` being marked with exec paths of inputs to
    concurrent actions (see the new test case).
    
    No
    
    - [x] I have added tests for the new use cases (if any).
    - [ ] I have updated the documentation (if applicable).
    
    RELNOTES: Fixed an issue that caused Bazel to fail on a lost input even
    with build or action rewinding enabled.
    
    Closes https://github.com/bazelbuild/bazel/pull/29551.
    
    PiperOrigin-RevId: 922775351
    Change-Id: Ia22580325c3bc92b56d202e5c24d3630fc8c27a0
    (cherry picked from commit
    https://github.com/bazelbuild/bazel/commit/100272284dde7a7504e7581604ddcf826788b6c8,
    includes
    https://github.com/bazelbuild/bazel/commit/5572634408f7ead56ebb544ab037ffa125a0154b
    and
    https://github.com/bazelbuild/bazel/commit/8f5af363182885e08179d47313d417e40fa922d5)
    
    Fixes #29575
    
    ---------
    
    Co-authored-by: tjgq <tjgq@google.com>

--- src/main/java/com/google/devtools/build/lib/remote/merkletree/MerkleTree.java
+++ src/main/java/com/google/devtools/build/lib/remote/merkletree/MerkleTree.java
@@ -172,7 +172,8 @@ public sealed interface MerkleTree {
         MerkleTreeUploader uploader,
         RemoteActionExecutionContext context,
         RemotePathResolver remotePathResolver,
-        Digest digest) {
+        Digest digest,
+        boolean force) {
       return switch (blobs.get(digest)) {
         case byte[] data -> Optional.of(uploader.uploadBlob(context, digest, data));
         case VirtualActionInput virtualActionInput ->
@@ -189,7 +190,7 @@ public sealed interface MerkleTree {
                   : MerkleTreeComputer.PATH_ACTION_INPUT_RESOLVER;
           yield Optional.of(
               uploader.uploadFile(
-                  context, remotePathResolver, digest, pathResolver.toPath(actionInput)));
+                  context, remotePathResolver, digest, pathResolver.toPath(actionInput), force));
         }
         case null -> Optional.empty();
         default -> throw new IllegalStateException("Unexpected blob type: " + blobs.get(digest));
