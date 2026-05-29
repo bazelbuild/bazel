93545b74fcbf080285d422b04f0a4fada136ed00
Fabian Meumertzheim <fabian@meumertzhe.im>
2026-05-29 20:34:23 +0200

    [9.1.1] Cache CAS interactions at lower layer to fix lost input handling (#29677)
    The error handling for uploads in `UploadTask` depends on the exec path
    of the uploaded file, which wasn't part of the `casUploadCache` cache
    key. Fix this by moving deduplication logic for CAS uploads and
    `FindMissing` calls into `RemoteCacheClient`.
    
    Avoids spurious Bazel failures caused by lost inputs that can't be
    recovered from via build or action rewinding due to
    `CacheNotFoundException` being marked with exec paths of inputs to
    concurrent actions (see the new test case).
    
    No
    
    - [x] I have added tests for the new use cases (if any).
    - [ ] I have updated the documentation (if applicable).
    
    RELNOTES: Fixed an issue that caused Bazel to fail on a lost input even
    with build or action rewinding enabled.
    
    Closes https://github.com/bazelbuild/bazel/pull/29551.
    
    PiperOrigin-RevId: 922775351
    Change-Id: Ia22580325c3bc92b56d202e5c24d3630fc8c27a0
    (cherry picked from commit
    https://github.com/bazelbuild/bazel/commit/100272284dde7a7504e7581604ddcf826788b6c8,
    includes
    https://github.com/bazelbuild/bazel/commit/5572634408f7ead56ebb544ab037ffa125a0154b
    and
    https://github.com/bazelbuild/bazel/commit/8f5af363182885e08179d47313d417e40fa922d5)
    
    Fixes #29575
    
    ---------
    
    Co-authored-by: tjgq <tjgq@google.com>

--- src/main/java/com/google/devtools/build/lib/remote/GrpcCacheClient.java
+++ src/main/java/com/google/devtools/build/lib/remote/GrpcCacheClient.java
@@ -84,7 +84,7 @@ import javax.annotation.Nullable;
 
 /** A RemoteActionCache implementation that uses gRPC calls to a remote cache server. */
 @ThreadSafe
-public class GrpcCacheClient implements RemoteCacheClient, MissingDigestsFinder {
+public class GrpcCacheClient extends RemoteCacheClient implements MissingDigestsFinder {
   private static final GoogleLogger logger = GoogleLogger.forEnclosingClass();
 
   private final CallCredentialsProvider callCredentialsProvider;
@@ -565,7 +565,7 @@ public class GrpcCacheClient implements RemoteCacheClient, MissingDigestsFinder 
   }
 
   @Override
-  public ListenableFuture<Void> uploadBlob(
+  public ListenableFuture<Void> uploadBlobImpl(
       RemoteActionExecutionContext context, Digest digest, Blob blob) {
     return Futures.catchingAsync(
         uploadChunker(
