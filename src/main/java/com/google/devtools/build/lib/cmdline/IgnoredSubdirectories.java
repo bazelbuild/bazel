5797c0da6cc63f26168d8fd31c7428fb44733790
Fabian Meumertzheim <fabian@meumertzhe.im>
2026-07-30 14:35:02 +0200

    Apply suggestion from @fmeum

--- src/main/java/com/google/devtools/build/lib/cmdline/IgnoredSubdirectories.java
+++ src/main/java/com/google/devtools/build/lib/cmdline/IgnoredSubdirectories.java
@@ -163,10 +163,6 @@ public final class IgnoredSubdirectories {
     ImmutableSet<PathFragment> filteredTraversalExclusions =
         traversalExclusions.stream().filter(p -> p.startsWith(directory)).collect(toImmutableSet());
 
-    // Note that we must not split the path string here: for the root directory, that would result
-    // in an array containing a single empty string instead of an empty array, which in turn would
-    // make canMatchChild() below spuriously match that phantom segment against the first pattern
-    // segment and thus drop every pattern whose first segment is not a wildcard.
     String[] splitDirectory = Iterables.toArray(directory.segments(), String.class);
     ImmutableList.Builder<String> filteredPatterns = ImmutableList.builder();
     for (int i = 0; i < patterns.size(); i++) {
