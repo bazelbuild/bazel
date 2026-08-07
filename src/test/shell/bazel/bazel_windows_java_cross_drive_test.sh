d5edc236e612c2da5722a4d705a7b87d32b3dc9f
tjgq <tjgq@google.com>
2026-08-07 04:00:47 -0700

    [9.3.0] Replace `--java_version` launcher argument with launch info key.
    This is the preferred mechanism to be used by rules_java.
    
    PiperOrigin-RevId: 960839361
    Change-Id: Ice89277c367170d31e7a030ad1c6599e59254631

--- src/test/shell/bazel/bazel_windows_java_cross_drive_test.sh
+++ src/test/shell/bazel/bazel_windows_java_cross_drive_test.sh
@@ -97,6 +97,11 @@ function delete_tmp_drive() {
 }
 
 function test_java_with_jar_under_different_drive() {
+  # TODO(tjgq): Re-enable once rules_java passes Java version in launcher info.
+  if [[ "$JAVA_VERSION" -ge "25" ]]; then
+    return
+  fi
+
   create_tmp_drive
 
   trap delete_tmp_drive EXIT
@@ -127,9 +132,8 @@ EOF
   bazel --output_user_root=${TMP_DRIVE}:/tmp build :hello &> "$TEST_log" \
       || fail "build failed"
 
-  # TODO(tjgq): Remove explicit `--java_version` once rules_java sets it.
-  assert_binary_run_from_subdir "bazel-bin/hello --classpath_limit=0 --java_version=$JAVA_VERSION" \
-    "Java $JAVA_VERSION"
+  assert_binary_run_from_subdir "bazel-bin/hello --classpath_limit=0" \
+      "Java $JAVA_VERSION"
 }
 
 run_suite "Tests that the Java Windows launcher works with cross-drive jars."
