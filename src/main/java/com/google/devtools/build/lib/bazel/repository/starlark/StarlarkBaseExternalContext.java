9ea91a547d356df9541e0c03ab28a0cef765563e
Fabian Meumertzheim <fabian@meumertzhe.im>
2026-09-09 07:52:32 -0700

    Restore interrupt of cancelled repository downloads (#31040)
    ### Description
    
    Restore the interruption of cancelled downloads, which was unintentionally dropped in f2b26596878a0b361b269631a2eddc8ac290dc94.
    
    ### Motivation
    
    ### Build API Changes
    
    No
    
    ### Checklist
    
    - [ ] I have added tests for the new use cases (if any).
    - [ ] I have updated the documentation (if applicable).
    
    ### Release Notes
    
    RELNOTES: None
    
    Closes #31040
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/bazelbuild/bazel/pull/31040 from fmeum:interrupt-cancelled-downloads 5a69f6cbb99688458a64519139fda4db0e9174c2
    PiperOrigin-RevId: 978545732
    Change-Id: I813688fe8752583d602e4423047f3a8d9ffe3557

--- src/main/java/com/google/devtools/build/lib/bazel/repository/starlark/StarlarkBaseExternalContext.java
+++ src/main/java/com/google/devtools/build/lib/bazel/repository/starlark/StarlarkBaseExternalContext.java
@@ -594,7 +594,7 @@ public abstract class StarlarkBaseExternalContext implements AutoCloseable, Star
 
     @Override
     public boolean cancel() {
-      return !future.cancel(false);
+      return !future.cancel(true);
     }
 
     @StarlarkMethod(
