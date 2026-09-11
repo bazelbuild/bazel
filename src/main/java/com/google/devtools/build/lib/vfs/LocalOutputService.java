bazelbuild/bazel

commit 9ed07e33051696b29302015e823ea8f697e7b160

Propagate errors other than non-existence from FileSystem.is* methods.
In cases where the exception cannot be readily handled by the surrounding code, keep the preexisting behavior and leave a TODO for later - the primary objective of this CL is to fix the method signatures and their implementations, not every callsite.

PiperOrigin-RevId: 972530629
Change-Id: I15b637a93d2efa584b799564e28d51a418328ead

src/main/java/com/google/devtools/build/lib/vfs/LocalOutputService.java
@@ -61,26 +61,24 @@ public class LocalOutputService implements OutputService {
       throws AbruptExitException {
     Path outputPath = directories.getOutputPath(workspaceName);
     Path localOutputPath = directories.getLocalOutputPath();
-
-    if (outputPath.isSymbolicLink()) {
-      try {
+    try {
+      if (outputPath.isSymbolicLink()) {
         // Remove the existing symlink first.
         outputPath.delete();
-        if (localOutputPath.exists()) {
-          // Pre-existing local output directory. Move to outputPath.
-          localOutputPath.renameTo(outputPath);
-        }
-      } catch (IOException e) {
-        throw new AbruptExitException(
-            DetailedExitCode.of(
-                FailureDetail.newBuilder()
-                    .setMessage(
-                        "Couldn't handle local output directory symlinks: " + e.getMessage())
-                    .setExecution(
-                        Execution.newBuilder().setCode(Code.LOCAL_OUTPUT_DIRECTORY_SYMLINK_FAILURE))
-                    .build()),
-            e);
       }
+      if (localOutputPath.exists()) {
+        // Pre-existing local output directory. Move to outputPath.
+        localOutputPath.renameTo(outputPath);
+      }
+    } catch (IOException e) {
+      throw new AbruptExitException(
+          DetailedExitCode.of(
+              FailureDetail.newBuilder()
+                  .setMessage("Couldn't handle local output directory symlinks: " + e.getMessage())
+                  .setExecution(
+                      Execution.newBuilder().setCode(Code.LOCAL_OUTPUT_DIRECTORY_SYMLINK_FAILURE))
+                  .build()),
+          e);
     }
     return ModifiedFileSet.EVERYTHING_MODIFIED;
   }

src/main/java/com/google/devtools/build/lib/actions/FileStateValue.java
@@ -149,7 +149,7 @@ public abstract class FileStateValue extends RegularFileValue implements HasDige
       FileStatusWithDigest stat,
       XattrProvider xattrProvider,
       @Nullable TimestampGranularityMonitor tsgm)
-      throws InconsistentFilesystemException {
+      throws IOException {
     checkState(stat.isFile(), path);
 
     if (stat instanceof FileStatusWithMetadata fileStatusWithMetadata) {

src/main/java/com/google/devtools/build/lib/bazel/repository/RepositoryFetchFunction.java
@@ -724,7 +724,13 @@ public final class RepositoryFetchFunction implements SkyFunction {
       throw new RepositoryFunctionException(e, Transience.TRANSIENT);
     }
 
-    if (!outputDirectory.isDirectory()) {
+    boolean isDirectory;
+    try {
+      isDirectory = outputDirectory.isDirectory();
+    } catch (IOException e) {
+      throw new RepositoryFunctionException(e, Transience.TRANSIENT);
+    }
+    if (!isDirectory) {
       throw new RepositoryFunctionException(
           new IOException(repoDefinition.name() + " must create a directory"),
           Transience.TRANSIENT);
@@ -732,7 +738,13 @@ public final class RepositoryFetchFunction implements SkyFunction {
 
     // Make sure the fetched repo has a boundary file.
     if (!RepositoryUtils.isValidRepoRoot(outputDirectory)) {
-      if (outputDirectory.isSymbolicLink()) {
+      boolean isSymbolicLink;
+      try {
+        isSymbolicLink = outputDirectory.isSymbolicLink();
+      } catch (IOException e) {
+        throw new RepositoryFunctionException(e, Transience.TRANSIENT);
+      }
+      if (isSymbolicLink) {
         // The created repo is actually just a symlink to somewhere else (think local_repository).
         // In this case, we shouldn't try to create the repo boundary file ourselves, but report an
         // error instead.
@@ -804,12 +816,12 @@ public final class RepositoryFetchFunction implements SkyFunction {
       String userDefinedPath,
       Environment env)
       throws RepositoryFunctionException, InterruptedException {
-    if (source.isDirectory(Symlinks.NOFOLLOW)) {
-      try {
+    try {
+      if (source.isDirectory(Symlinks.NOFOLLOW)) {
         source.deleteTree();
-      } catch (IOException e) {
-        throw new RepositoryFunctionException(e, Transience.TRANSIENT);
       }
+    } catch (IOException e) {
+      throw new RepositoryFunctionException(e, Transience.TRANSIENT);
     }
     try {
       FileSystemUtils.ensureSymbolicLink(source, destination);

src/main/java/com/google/devtools/build/lib/bazel/repository/starlark/StarlarkPath.java
@@ -184,8 +184,12 @@ public final class StarlarkPath implements StarlarkValue {
           extension to be sensitive to whether the path is a directory or a file, use the \
           <code>watch()</code> method on the context object.
           """)
-  public boolean isDir() {
-    return path.isDirectory();
+  public boolean isDir() throws RepositoryFunctionException {
+    try {
+      return path.isDirectory();
+    } catch (IOException e) {
+      throw new RepositoryFunctionException(e, Transience.TRANSIENT);
+    }
   }
 
   @StarlarkMethod(

src/main/java/com/google/devtools/build/lib/includescanning/PathExistenceCache.java
@@ -20,6 +20,7 @@ import com.google.devtools.build.lib.actions.ArtifactFactory;
 import com.google.devtools.build.lib.concurrent.ThreadSafety.ThreadSafe;
 import com.google.devtools.build.lib.vfs.Path;
 import com.google.devtools.build.lib.vfs.PathFragment;
+import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
@@ -57,7 +58,14 @@ class PathExistenceCache {
           isSource
               ? artifactFactory.getPathFromSourceExecPath(execRoot, execPath)
               : execRoot.getRelative(execPath);
-      newFuture.set(path.isFile());
+      boolean isFile;
+      try {
+        isFile = path.isFile();
+      } catch (IOException e) {
+        // TODO(tjgq): Propagate the error.
+        isFile = false;
+      }
+      newFuture.set(isFile);
     }
     try {
       return Uninterruptibles.getUninterruptibly(existingFuture);
@@ -74,7 +82,14 @@ class PathExistenceCache {
     if (existingFuture == null) {
       existingFuture = newFuture;
       Path path = artifactFactory.getPathFromSourceExecPath(execRoot, execPath);
-      newFuture.set(path.isDirectory());
+      boolean isDirectory;
+      try {
+        isDirectory = path.isDirectory();
+      } catch (IOException e) {
+        // TODO(tjgq): Propagate the error.
+        isDirectory = false;
+      }
+      newFuture.set(isDirectory);
     }
     try {
       return Uninterruptibles.getUninterruptibly(existingFuture);

src/main/java/com/google/devtools/build/lib/packages/Package.java
@@ -366,37 +366,42 @@ public class Package extends Packageoid {
 
   static String getAlternateTargetSuggestion(
       Metadata metadata, String targetName, ImmutableList<Target> otherTargets) {
-    // If there's a file on the disk that's not mentioned in the BUILD file,
-    // produce a more informative error.  NOTE! this code path is only executed
-    // on failure, which is (relatively) very rare.  In the common case no
-    // stat(2) is executed.
-    Path filename = metadata.getPackageDirectory().getRelative(targetName);
-    if (!PathFragment.isNormalized(targetName) || "*".equals(targetName)) {
-      // Don't check for file existence if the target name is not normalized
-      // because the error message would be confusing and wrong. If the
-      // targetName is "foo/bar/.", and there is a directory "foo/bar", it
-      // doesn't mean that "//pkg:foo/bar/." is a valid label.
-      // Also don't check if the target name is a single * character since
-      // it's invalid on Windows.
+    try {
+      // If there's a file on the disk that's not mentioned in the BUILD file,
+      // produce a more informative error.  NOTE! this code path is only executed
+      // on failure, which is (relatively) very rare.  In the common case no
+      // stat(2) is executed.
+      Path filename = metadata.getPackageDirectory().getRelative(targetName);
+      if (!PathFragment.isNormalized(targetName) || "*".equals(targetName)) {
+        // Don't check for file existence if the target name is not normalized
+        // because the error message would be confusing and wrong. If the
+        // targetName is "foo/bar/.", and there is a directory "foo/bar", it
+        // doesn't mean that "//pkg:foo/bar/." is a valid label.
+        // Also don't check if the target name is a single * character since
+        // it's invalid on Windows.
+        return "";
+      } else if (filename.isDirectory()) {
+        return "; however, a source directory of this name exists.  (Perhaps add "
+            + "'exports_files([\""
+            + targetName
+            + "\"])' to "
+            + getRepoRelativeBuildFilePathString(metadata)
+            + ", or define a "
+            + "filegroup?)";
+      } else if (filename.exists()) {
+        return "; however, a source file of this name exists.  (Perhaps add "
+            + "'exports_files([\""
+            + targetName
+            + "\"])' to "
+            + getRepoRelativeBuildFilePathString(metadata)
+            + "?)";
+      } else {
+        return TargetSuggester.suggestTargets(
+            targetName, Lists.transform(otherTargets, Target::getName));
+      }
+    } catch (IOException e) {
+      // Ignore - suggestions are best-effort.
       return "";
-    } else if (filename.isDirectory()) {
-      return "; however, a source directory of this name exists.  (Perhaps add "
-          + "'exports_files([\""
-          + targetName
-          + "\"])' to "
-          + getRepoRelativeBuildFilePathString(metadata)
-          + ", or define a "
-          + "filegroup?)";
-    } else if (filename.exists()) {
-      return "; however, a source file of this name exists.  (Perhaps add "
-          + "'exports_files([\""
-          + targetName
-          + "\"])' to "
-          + getRepoRelativeBuildFilePathString(metadata)
-          + "?)";
-    } else {
-      return TargetSuggester.suggestTargets(
-          targetName, Lists.transform(otherTargets, Target::getName));
     }
   }
 

src/main/java/com/google/devtools/build/lib/remote/RemoteExternalOverlayFileSystem.java
@@ -261,7 +261,7 @@ public final class RemoteExternalOverlayFileSystem extends FileSystem
    * this doesn't happen in {@link #injectRecursively}.
    */
   private void addSymlinkTargetsToPrefetch(
-      List<PathFragment> symlinks, Set<PathFragment> filesToPrefetch) {
+      List<PathFragment> symlinks, Set<PathFragment> filesToPrefetch) throws IOException {
     for (var symlink : symlinks) {
       Path target;
       try {
@@ -772,22 +772,22 @@ public final class RemoteExternalOverlayFileSystem extends FileSystem
   }
 
   @Override
-  public boolean isFile(PathFragment path, boolean followSymlinks) {
+  public boolean isFile(PathFragment path, boolean followSymlinks) throws IOException {
     return fsForPath(path).isFile(path, followSymlinks);
   }
 
   @Override
-  public boolean isSpecialFile(PathFragment path, boolean followSymlinks) {
+  public boolean isSpecialFile(PathFragment path, boolean followSymlinks) throws IOException {
     return fsForPath(path).isSpecialFile(path, followSymlinks);
   }
 
   @Override
-  public boolean isSymbolicLink(PathFragment path) {
+  public boolean isSymbolicLink(PathFragment path) throws IOException {
     return fsForPath(path).isSymbolicLink(path);
   }
 
   @Override
-  public boolean isDirectory(PathFragment path, boolean followSymlinks) {
+  public boolean isDirectory(PathFragment path, boolean followSymlinks) throws IOException {
     return fsForPath(path).isDirectory(path, followSymlinks);
   }
 

src/main/java/com/google/devtools/build/lib/remote/RemoteOutputService.java
@@ -127,20 +127,20 @@ public class RemoteOutputService implements OutputService {
     // is valid. If the previous OutputService redirected the output path to a remote location, we
     // must undo this.
     Path outputPath = directories.getOutputPath(workspaceName);
-    if (outputPath.isSymbolicLink()) {
-      try {
+    try {
+      if (outputPath.isSymbolicLink()) {
         outputPath.delete();
-      } catch (IOException e) {
-        throw new AbruptExitException(
-            DetailedExitCode.of(
-                FailureDetail.newBuilder()
-                    .setMessage(
-                        String.format("Couldn't remove output path symlink: %s", e.getMessage()))
-                    .setExecution(
-                        Execution.newBuilder().setCode(Code.LOCAL_OUTPUT_DIRECTORY_SYMLINK_FAILURE))
-                    .build()),
-            e);
       }
+    } catch (IOException e) {
+      throw new AbruptExitException(
+          DetailedExitCode.of(
+              FailureDetail.newBuilder()
+                  .setMessage(
+                      String.format("Couldn't remove output path symlink: %s", e.getMessage()))
+                  .setExecution(
+                      Execution.newBuilder().setCode(Code.LOCAL_OUTPUT_DIRECTORY_SYMLINK_FAILURE))
+                  .build()),
+          e);
     }
     return ModifiedFileSet.EVERYTHING_MODIFIED;
   }

commit eb09af1d6016b70a9c3de44ddfb7cbc770f5e5f5

Fix LocalOutputService bug introduced in https://github.com/bazelbuild/bazel/commit/9ed07e33051696b29302015e823ea8f697e7b160.
The rename must be guarded by the symlink check, otherwise we might fail to move the directory into an existing path.

PiperOrigin-RevId: 979826740
Change-Id: I19ff51ebe7452f64a98675777632b75edd1022fe

src/main/java/com/google/devtools/build/lib/vfs/LocalOutputService.java
@@ -65,10 +65,10 @@ public class LocalOutputService implements OutputService {
       if (outputPath.isSymbolicLink()) {
         // Remove the existing symlink first.
         outputPath.delete();
-      }
-      if (localOutputPath.exists()) {
-        // Pre-existing local output directory. Move to outputPath.
-        localOutputPath.renameTo(outputPath);
+        if (localOutputPath.exists()) {
+          // Pre-existing local output directory. Move to outputPath.
+          localOutputPath.renameTo(outputPath);
+        }
       }
     } catch (IOException e) {
       throw new AbruptExitException(