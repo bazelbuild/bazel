07eadad587778d786f12b355f922aab5983dd8c8
Fabian Meumertzheim <fabian@meumertzhe.im>
2026-09-14 01:38:44 -0700

    Revert "Generalize Label and RepositoryName getDisplayForm to accept non-main repository mappings." (#31089)
    This reverts commit 3b8f0c563aa3d2ace560a07fd5d1af543efab64d and drops the incorrect TODO that motivated it.
    
    Closes #31089
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/bazelbuild/bazel/pull/31089 from fmeum:revert-labels f75715ded71c4c430bf096c356de1e5019ec32da
    PiperOrigin-RevId: 981006468
    Change-Id: I5d2a436cc9352dd92db823b52efdfba126b3957f

--- src/main/java/com/google/devtools/build/lib/cmdline/PackageIdentifier.java
+++ src/main/java/com/google/devtools/build/lib/cmdline/PackageIdentifier.java
@@ -220,24 +220,22 @@ public final class PackageIdentifier implements SkyKey, Comparable<PackageIdenti
   /**
    * Returns a label representation for this package that is suitable for display. The returned
    * string is as simple as possible while referencing the current package when parsed in the
-   * context of the repository whose repository mapping is provided.
+   * context of the main repository whose repository mapping is provided.
    *
-   * @param repositoryMapping the {@link RepositoryMapping} of the context repository (usually the
-   *     main repository)
+   * @param mainRepositoryMapping the {@link RepositoryMapping} of the main repository
    * @return
    *     <dl>
    *       <dt><code>//some/pkg</code>
-   *       <dd>if this package lives in the context repository (or the main repository if
-   *           repositoryMapping is null)
+   *       <dd>if this package lives in the main repository
    *       <dt><code>@protobuf//some/pkg</code>
    *       <dd>if this package lives in a repository with "protobuf" as apparent name of a
-   *           dependency of the context repository
+   *           dependency of the main module
    *       <dt><code>@@protobuf+//some/pkg</code>
-   *       <dd>if the current package belongs to a repository that is not visible from the context
-   *           repository
+   *       <dd>if the current package belongs to a repository that is not visible from the main
+   *           module
    */
-  public String getDisplayForm(@Nullable RepositoryMapping repositoryMapping) {
-    return repository.getDisplayForm(repositoryMapping) + "//" + pkgName;
+  public String getDisplayForm(@Nullable RepositoryMapping mainRepositoryMapping) {
+    return repository.getDisplayForm(mainRepositoryMapping) + "//" + pkgName;
   }
 
   @Override
