74515d12fe597fbb4386637a51932f8bc0e941e1
gztong666 <g3140679427@163.com>
2026-09-21 17:03:01 +0800

    Preserve paths with spaces in the default lockfile updater

--- src/test/tools/bzlmod/update_default_lock_file.sh
+++ src/test/tools/bzlmod/update_default_lock_file.sh
@@ -32,7 +32,7 @@ function cleanup() {
 
 function generate_lock_file() {
   tmpdir=$(mktemp -d -t bazel.XXXXXX)
-  trap 'cleanup $tmpdir' EXIT
+  trap 'cleanup "$tmpdir"' EXIT
 
   cd "${tmpdir}"
   touch MODULE.bazel
@@ -40,8 +40,8 @@ function generate_lock_file() {
   bazel=$(rlocation io_bazel/src/bazel)
 
   echo "Running: $bazel --batch --ignore_all_rc_files mod deps $@"
-  $bazel --batch --ignore_all_rc_files mod deps "$@"
-  cp ./MODULE.bazel.lock $BUILD_WORKSPACE_DIRECTORY/src/test/tools/bzlmod/MODULE.bazel.lock
+  "$bazel" --batch --ignore_all_rc_files mod deps "$@"
+  cp ./MODULE.bazel.lock "$BUILD_WORKSPACE_DIRECTORY/src/test/tools/bzlmod/MODULE.bazel.lock"
 }
 
 generate_lock_file "$@"
